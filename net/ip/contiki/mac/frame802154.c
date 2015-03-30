e7621796abb43eb2a2a836d81d9b8ed6609b09ee
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-03-30 18:15:37 +0200

    net: contiki: Added buffer size check in frame802154
    Importing patch 565ff0b3dcaca5d0c31ba388447e3f71c914ccc8 from Contiki.
    Original commit by Niclas Finne <nfi@sics.se>
    
    Change-Id: I27014bf0af0770cd7f572655f288b6c382fdd659
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1e7ed734a3d4c9f96184e3f96bbeb567e1b7d8ef

--- net/ip/contiki/mac/frame802154.c
+++ net/ip/contiki/mac/frame802154.c
@@ -4,7 +4,7 @@ DECL|dest_addr_len|member|uint8_t dest_addr_len; /**< Length (in bytes) of desti
 DECL|dest_pid_len|member|uint8_t dest_pid_len; /**< Length (in bytes) of destination PAN ID field */
 DECL|field_length_t|typedef|} field_length_t;
 DECL|field_len|function|field_len(frame802154_t *p, field_length_t *flen)
-DECL|frame802154_create|function|frame802154_create(frame802154_t *p, uint8_t *buf)
+DECL|frame802154_create|function|frame802154_create(frame802154_t *p, uint8_t *buf, int buf_len)
 DECL|frame802154_hdrlen|function|frame802154_hdrlen(frame802154_t *p)
 DECL|frame802154_parse|function|frame802154_parse(uint8_t *data, int len, frame802154_t *pf)
 DECL|get_key_id_len|function|get_key_id_len(uint8_t key_id_mode)
