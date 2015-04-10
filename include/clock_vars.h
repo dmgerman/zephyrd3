a6edea3ed556ab4f3f4a3c99a62ff0b2a330cbaf
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-04-10 17:16:14 -0400

    Reorganization of time calculating macros
    Move time calculation macros from application directory to kernel
    directory, since it is used by a kernel component.
    In order to follow namespace and naming conventions, convert
    macros to SYS_CLOCK_HW_CYCLES_TO_NS* form.
    
    Change-Id: I96e9149c399adc363aed5095ae82a1abc78d2977
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: f51049b81c9f472c5a41d825751415e77cb5789b

--- include/clock_vars.h
+++ include/clock_vars.h
@@ -1,3 +1,7 @@
+DECL|NSEC_PER_USEC|macro|NSEC_PER_USEC
+DECL|SYS_CLOCK_HW_CYCLES_TO_NS64|macro|SYS_CLOCK_HW_CYCLES_TO_NS64
+DECL|SYS_CLOCK_HW_CYCLES_TO_NS_AVG|macro|SYS_CLOCK_HW_CYCLES_TO_NS_AVG
+DECL|SYS_CLOCK_HW_CYCLES_TO_NS|macro|SYS_CLOCK_HW_CYCLES_TO_NS
 DECL|_CLOCK_VARS__H_|macro|_CLOCK_VARS__H_
 DECL|sys_clock_hw_cycles_per_sec|macro|sys_clock_hw_cycles_per_sec
 DECL|sys_clock_ticks_per_sec|macro|sys_clock_ticks_per_sec
