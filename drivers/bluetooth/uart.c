9cb0a104f921d378ce2ec0b5dfce1e83a8183f46
Johan Hedberg <johan.hedberg@intel.com>
2015-04-13 14:38:44 +0300

    Bluetooth: Add HCI UART (H4) driver
    This patch adds a simple HCI UART (H4) driver that currently maps to
    the second UART in the system. The main intention is to use this
    together with qemu for accessing the Bluetooth controller available on
    the host OS side.
    
    The H4 HCI transport protocol is perhaps the simplest of the standard
    HCI transports. It consists of a single byte in the beginning of each
    packet which indicates the type of the packet: HCI event, HCI command,
    ACL data, or SCO data (which we don't use at the moment).
    
    Change-Id: I225a2a2361fbd7cd4ba82ea1f81ddc1271e9e7c2
    Co-authored-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 2581ad93131d1ee1079da98215559bd36d67be1a

---
+++ drivers/bluetooth/uart.c
@@ -0,0 +1,15 @@
+DECL|H4_ACL|macro|H4_ACL
+DECL|H4_CMD|macro|H4_CMD
+DECL|H4_EVT|macro|H4_EVT
+DECL|H4_HEADER_SIZE|macro|H4_HEADER_SIZE
+DECL|H4_SCO|macro|H4_SCO
+DECL|UART|macro|UART
+DECL|bt_uart_acl_recv|function|static int bt_uart_acl_recv(struct bt_buf *buf)
+DECL|bt_uart_evt_recv|function|static int bt_uart_evt_recv(struct bt_buf *buf)
+DECL|bt_uart_init|function|void bt_uart_init(void)
+DECL|bt_uart_isr|function|void bt_uart_isr(void *unused)
+DECL|bt_uart_open|function|static int bt_uart_open()
+DECL|bt_uart_read|function|static int bt_uart_read(int uart, uint8_t *buf, size_t len)
+DECL|bt_uart_send|function|static int bt_uart_send(struct bt_buf *buf)
+DECL|bt_uart_setup|function|static void bt_uart_setup(int uart, struct uart_init_info *info)
+DECL|drv|variable|drv
