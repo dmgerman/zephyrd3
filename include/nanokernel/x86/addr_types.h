92a2945ed4a96676cb47bb500303db63a3060623
Peter Mitsis <peter.mitsis@windriver.com>
2015-04-13 16:38:26 -0400

    Remove PHYS_ADDR_xxx macros
    The PHYS_ADDR_xxx macros are no longer used.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 3f44105695af1e1c75bc3547f9a5b55274542c23

--- include/nanokernel/x86/addr_types.h
+++ include/nanokernel/x86/addr_types.h
@@ -1,5 +1,3 @@
-DECL|PHYS_ADDR_ANY|macro|PHYS_ADDR_ANY
-DECL|PHYS_ADDR_NONE|macro|PHYS_ADDR_NONE
 DECL|PHYS_ADDR|typedef|typedef unsigned int PHYS_ADDR;
 DECL|VIRT_ADDR|typedef|typedef unsigned int VIRT_ADDR;
 DECL|_x86_addr_types__h_|macro|_x86_addr_types__h_
