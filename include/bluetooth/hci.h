7d62c7df6c6a84231cd318782165228a4571dd3c
Johan Hedberg <johan.hedberg@intel.com>
2015-04-14 14:46:56 +0300

    Bluetooth: Add basic HCI definitions
    For performing a basic HCI initialization procedure we need
    definitions for the related HCI commands and events. This patch adds
    these into separate hci.h header file. More content will be added to
    this file as corresponding functionality gets added to other areas of
    the stack.
    
    Change-Id: Ida7555cb0a3d30b8a44324cb14699eba8a1bcdee
    Co-authored-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 63e948039901e3748a3381486824410af31791e9

---
+++ include/bluetooth/hci.h
@@ -0,0 +1,80 @@
+DECL|BT_HCI_EVT_CMD_COMPLETE|macro|BT_HCI_EVT_CMD_COMPLETE
+DECL|BT_HCI_EVT_CMD_STATUS|macro|BT_HCI_EVT_CMD_STATUS
+DECL|BT_HCI_LE_ENCRYPTION|macro|BT_HCI_LE_ENCRYPTION
+DECL|BT_HCI_OP_LE_READ_BUFFER_SIZE|macro|BT_HCI_OP_LE_READ_BUFFER_SIZE
+DECL|BT_HCI_OP_LE_READ_LOCAL_FEATURES|macro|BT_HCI_OP_LE_READ_LOCAL_FEATURES
+DECL|BT_HCI_OP_LE_WRITE_LE_HOST_SUPP|macro|BT_HCI_OP_LE_WRITE_LE_HOST_SUPP
+DECL|BT_HCI_OP_READ_BD_ADDR|macro|BT_HCI_OP_READ_BD_ADDR
+DECL|BT_HCI_OP_READ_BUFFER_SIZE|macro|BT_HCI_OP_READ_BUFFER_SIZE
+DECL|BT_HCI_OP_READ_LOCAL_FEATURES|macro|BT_HCI_OP_READ_LOCAL_FEATURES
+DECL|BT_HCI_OP_READ_LOCAL_VERSION_INFO|macro|BT_HCI_OP_READ_LOCAL_VERSION_INFO
+DECL|BT_HCI_OP_RESET|macro|BT_HCI_OP_RESET
+DECL|BT_HCI_OP_SET_EVENT_MASK|macro|BT_HCI_OP_SET_EVENT_MASK
+DECL|BT_LMP_LE|macro|BT_LMP_LE
+DECL|BT_LMP_NO_BREDR|macro|BT_LMP_NO_BREDR
+DECL|BT_OGF_BASEBAND|macro|BT_OGF_BASEBAND
+DECL|BT_OGF_INFO|macro|BT_OGF_INFO
+DECL|BT_OGF_LE|macro|BT_OGF_LE
+DECL|BT_OP|macro|BT_OP
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|__BT_HCI_H|macro|__BT_HCI_H
+DECL|acl_max_len|member|uint16_t acl_max_len;
+DECL|acl_max_num|member|uint16_t acl_max_num;
+DECL|bdaddr|member|uint8_t bdaddr[6];
+DECL|bt_acl_handle|macro|bt_acl_handle
+DECL|bt_hci_acl_hdr|struct|struct bt_hci_acl_hdr {
+DECL|bt_hci_cmd_hdr|struct|struct bt_hci_cmd_hdr {
+DECL|bt_hci_cp_set_event_mask|struct|struct bt_hci_cp_set_event_mask {
+DECL|bt_hci_cp_write_le_host_supp|struct|struct bt_hci_cp_write_le_host_supp {
+DECL|bt_hci_evt_cmd_status|struct|struct bt_hci_evt_cmd_status {
+DECL|bt_hci_evt_hdr|struct|struct bt_hci_evt_hdr {
+DECL|bt_hci_rp_le_read_buffer_size|struct|struct bt_hci_rp_le_read_buffer_size {
+DECL|bt_hci_rp_le_read_local_features|struct|struct bt_hci_rp_le_read_local_features {
+DECL|bt_hci_rp_read_bd_addr|struct|struct bt_hci_rp_read_bd_addr {
+DECL|bt_hci_rp_read_buffer_size|struct|struct bt_hci_rp_read_buffer_size {
+DECL|bt_hci_rp_read_local_features|struct|struct bt_hci_rp_read_local_features {
+DECL|bt_hci_rp_read_local_version_info|struct|struct bt_hci_rp_read_local_version_info {
+DECL|events|member|uint8_t events[8];
+DECL|evt|member|uint8_t evt;
+DECL|features|member|uint8_t features[8];
+DECL|features|member|uint8_t features[8];
+DECL|handle|member|uint16_t handle;
+DECL|hci_evt_cmd_complete|struct|struct hci_evt_cmd_complete {
+DECL|hci_revision|member|uint16_t hci_revision;
+DECL|hci_version|member|uint8_t hci_version;
+DECL|le_max_len|member|uint16_t le_max_len;
+DECL|le_max_num|member|uint8_t le_max_num;
+DECL|len|member|uint16_t len;
+DECL|len|member|uint8_t len;
+DECL|le|member|uint8_t le;
+DECL|lmp_subversion|member|uint16_t lmp_subversion;
+DECL|lmp_version|member|uint8_t lmp_version;
+DECL|manufacturer|member|uint16_t manufacturer;
+DECL|ncmd|member|uint8_t ncmd;
+DECL|ncmd|member|uint8_t ncmd;
+DECL|opcode|member|uint16_t opcode;
+DECL|opcode|member|uint16_t opcode;
+DECL|opcode|member|uint16_t opcode;
+DECL|param_len|member|uint8_t param_len;
+DECL|sco_max_len|member|uint8_t sco_max_len;
+DECL|sco_max_num|member|uint16_t sco_max_num;
+DECL|simul|member|uint8_t simul;
+DECL|status|member|uint8_t status;
+DECL|status|member|uint8_t status;
+DECL|status|member|uint8_t status;
+DECL|status|member|uint8_t status;
+DECL|status|member|uint8_t status;
+DECL|status|member|uint8_t status;
+DECL|status|member|uint8_t status;
