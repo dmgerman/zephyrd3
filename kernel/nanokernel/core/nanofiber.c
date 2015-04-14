fab9fc872740cb058429fff9509b3aba2e69181b
Allan Stephens <allan.stephens@windriver.com>
2015-04-14 14:46:26 -0400

    Eliminate _nano_start()
    The helper function _nano_start() is no longer required (due to the
    recent relocation of boot banner and kernel build timestamp code),
    so the kernel's initialization code calls _nano_fiber_swap() directly.
    
    Change-Id: I06b9f473715afbfbdd4105b0def3445f1b2e3583
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 2801be129a2bb5887a7a5bc0864c6410dbcd8ecf

--- kernel/nanokernel/core/nanofiber.c
+++ kernel/nanokernel/core/nanofiber.c
@@ -2,7 +2,6 @@ DECL|_context_essential_check|function|int _context_essential_check(tCCS *pCtx /
 DECL|_fiber_start|function|void _fiber_start(char *pStack, unsigned stackSize, /* stack size in bytes */ nano_fiber_entry_t pEntry, int parameter1, int parameter2,
 DECL|_insert_ccs|function|void _insert_ccs(tCCS **queue, tCCS *ccs)
 DECL|_nano_fiber_swap|function|FUNC_NORETURN void _nano_fiber_swap(void)
-DECL|_nano_start|function|void _nano_start(void)
 DECL|context_self_get|function|nano_context_id_t context_self_get(void)
 DECL|context_type_get|function|nano_context_type_t context_type_get(void)
 DECL|fiber_abort|function|FUNC_NORETURN void fiber_abort(void)
