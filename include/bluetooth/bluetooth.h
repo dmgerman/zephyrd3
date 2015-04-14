d255b190cfa903da9dbdbe02cbbfb98d8e9e4cab
Johan Hedberg <johan.hedberg@intel.com>
2015-04-14 15:32:32 +0300

    Bluetooth: Add HCI driver API
    This patch adds a basic HCI driver registration API along with a fiber
    to process data from the driver. A FIFO is used for receiving data
    from the driver and waking up the respective fiber. To open and set up
    the HCI transport for operation there's an open() callback passed to
    the HCI core. This function will be called as soon as an application
    initializes Bluetooth functionality through bt_init().
    
    Change-Id: I780cca517a0dfc714f1ca35527e1c61e307345a0
    Co-authored-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 23a0df8b9b91ca7e8c637cb7e2f2bee9eb2e758f

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -10,9 +10,13 @@ DECL|__unused|member|int __unused;
 DECL|bt_buf_tail|macro|bt_buf_tail
 DECL|bt_buf_type|enum|enum bt_buf_type {
 DECL|bt_buf|struct|struct bt_buf {
+DECL|bt_driver|struct|struct bt_driver {
 DECL|buf|member|uint8_t buf[BT_BUF_MAX_DATA];
 DECL|data|member|uint8_t *data;
+DECL|head_reserve|member|size_t head_reserve;
 DECL|len|member|uint8_t len;
 DECL|opcode|member|uint16_t opcode;
+DECL|open|member|int (*open) (void);
+DECL|send|member|int (*send) (struct bt_buf *buf);
 DECL|sync|member|struct nano_sem *sync;
 DECL|type|member|uint8_t type;
--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1,4 +1,5 @@
 DECL|NUM_BUFS|macro|NUM_BUFS
+DECL|RX_STACK_SIZE|macro|RX_STACK_SIZE
 DECL|bt_buf_add|function|uint8_t *bt_buf_add(struct bt_buf *buf, size_t len)
 DECL|bt_buf_get_reserve|function|struct bt_buf *bt_buf_get_reserve(size_t reserve_head)
 DECL|bt_buf_get|function|struct bt_buf *bt_buf_get(void)
@@ -7,7 +8,18 @@ DECL|bt_buf_pull|function|uint8_t *bt_buf_pull(struct bt_buf *buf, size_t len)
 DECL|bt_buf_push|function|uint8_t *bt_buf_push(struct bt_buf *buf, size_t len)
 DECL|bt_buf_put|function|void bt_buf_put(struct bt_buf *buf)
 DECL|bt_buf_tailroom|function|size_t bt_buf_tailroom(struct bt_buf *buf)
+DECL|bt_dev|struct|static struct bt_dev {
+DECL|bt_driver_register|function|int bt_driver_register(struct bt_driver *drv)
+DECL|bt_driver_unregister|function|void bt_driver_unregister(struct bt_driver *drv)
 DECL|bt_init|function|int bt_init(void)
+DECL|bt_recv|function|void bt_recv(struct bt_buf *buf)
 DECL|buffers|variable|buffers
+DECL|dev|variable|dev
+DECL|drv|member|struct bt_driver *drv;
 DECL|free_bufs|variable|free_bufs
-DECL|init_free_queue|function|static void init_free_queue(void)
+DECL|free_queue_init|function|static void free_queue_init(void)
+DECL|hci_receive_packet|function|static void hci_receive_packet(struct bt_buf *buf)
+DECL|hci_rx_fiber|function|static void hci_rx_fiber(void)
+DECL|rx_fiber_stack|variable|rx_fiber_stack
+DECL|rx_queue_init|function|static void rx_queue_init(void)
+DECL|rx_queue|member|struct nano_fifo rx_queue;
