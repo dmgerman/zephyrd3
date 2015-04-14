aa7d48bbb33771ee4b069f049bd9d7ad5d4ea309
Johan Hedberg <johan.hedberg@intel.com>
2015-04-14 15:04:46 +0300

    Bluetooth: Add support for HCI data buffers
    In order to manage incoming and outgoing HCI data (events, commands &
    ACL data packets) we need to have some way of storing this into
    buffers. This patch implements a 'pool' of buffers with the help of
    the nano_fifo API. The pool is initially populated with all available
    buffers. After this code can on demand request buffers from the pool
    with the help of bt_buf_get() and return buffers back into the pool
    with bt_buf_put().
    
    Since we don't always know the execution context from where the API is
    operated on, this patch also adds convenience fifo_get/fifo_put
    wrappers that look up the exact context before calling the correct
    nano_fifo API.
    
    Change-Id: Ie7f6d450de865273171e21a000d5a14274d27d32
    Co-authored-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 64b117a9b1e944a5a5d66a73e8a3aa2c05269713

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,1 +1,18 @@
+DECL|BT_ACL|enumerator|BT_ACL,
+DECL|BT_BUF_MAX_DATA|macro|BT_BUF_MAX_DATA
+DECL|BT_CMD|enumerator|BT_CMD,
+DECL|BT_DBG|macro|BT_DBG
+DECL|BT_ERR|macro|BT_ERR
+DECL|BT_EVT|enumerator|BT_EVT,
+DECL|BT_INFO|macro|BT_INFO
 DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
+DECL|__unused|member|int __unused;
+DECL|bt_buf_tail|macro|bt_buf_tail
+DECL|bt_buf_type|enum|enum bt_buf_type {
+DECL|bt_buf|struct|struct bt_buf {
+DECL|buf|member|uint8_t buf[BT_BUF_MAX_DATA];
+DECL|data|member|uint8_t *data;
+DECL|len|member|uint8_t len;
+DECL|opcode|member|uint16_t opcode;
+DECL|sync|member|struct nano_sem *sync;
+DECL|type|member|uint8_t type;
