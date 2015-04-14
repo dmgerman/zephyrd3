c6a36da6c6980f6036b1749b513a6e0ce1d3451f
Allan Stephens <allan.stephens@windriver.com>
2015-04-14 14:32:39 -0400

    Relocate boot banner and kernel build timestamp handling
    Moves boot banner and kernel build timestamp code to the kernel's
    main initialization routine, since it doesn't really belong in a
    generic fiber-processing file.
    
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 5555bcff3148b37116297df5512513094983300a

--- kernel/nanokernel/core/nanofiber.c
+++ kernel/nanokernel/core/nanofiber.c
@@ -1,13 +1,8 @@
-DECL|BOOT_BANNER|macro|BOOT_BANNER
-DECL|BUILD_TIMESTAMP|macro|BUILD_TIMESTAMP
-DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
-DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
 DECL|_context_essential_check|function|int _context_essential_check(tCCS *pCtx /* pointer to context */ )
 DECL|_fiber_start|function|void _fiber_start(char *pStack, unsigned stackSize, /* stack size in bytes */ nano_fiber_entry_t pEntry, int parameter1, int parameter2,
 DECL|_insert_ccs|function|void _insert_ccs(tCCS **queue, tCCS *ccs)
 DECL|_nano_fiber_swap|function|FUNC_NORETURN void _nano_fiber_swap(void)
 DECL|_nano_start|function|void _nano_start(void)
-DECL|build_timestamp|variable|build_timestamp
 DECL|context_self_get|function|nano_context_id_t context_self_get(void)
 DECL|context_type_get|function|nano_context_type_t context_type_get(void)
 DECL|fiber_abort|function|FUNC_NORETURN void fiber_abort(void)
