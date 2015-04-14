201cc15743ee18282cac351eedcb441985aa80a0
Johan Hedberg <johan.hedberg@intel.com>
2015-04-14 15:44:48 +0300

    Bluetooth: Add full HCI initialization routine
    This patch adds a full HCI initialization routine to retrieve all
    relevant information from the controller.
    
    We also need to introduce a new blocking bt_hci_cmd_send_sync() API
    since some commands are conditional to the results of others. The API
    is implemented with the help of a semaphore that's part of the
    command's bt_buf struct. We wait on the semaphore and get it back once
    the command has completed (with the help of the hci_cmd_done
    function).
    
    The patch also adds variables for storing various controller specific
    parameters which will be needed later during the operation of the
    stack. These variables are part of 'struct bt_dev' and get updated
    through the respective command complete handlers.
    
    A new bt_hci_reset() API is added which allows an application to reset
    the controller state at any time by re-running the HCI init procedure.
    
    Change-Id: I5b1a38e910d79ad5fe806467bc51388eedc9c8f9
    Co-authored-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: d210d306d7979c8c03ff7130eb495f87c1f76826

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -1,6 +1,7 @@
 DECL|CMD_STACK_SIZE|macro|CMD_STACK_SIZE
 DECL|NUM_BUFS|macro|NUM_BUFS
 DECL|RX_STACK_SIZE|macro|RX_STACK_SIZE
+DECL|bdaddr|member|uint8_t bdaddr[6];
 DECL|bt_buf_add|function|uint8_t *bt_buf_add(struct bt_buf *buf, size_t len)
 DECL|bt_buf_get_reserve|function|struct bt_buf *bt_buf_get_reserve(size_t reserve_head)
 DECL|bt_buf_get|function|struct bt_buf *bt_buf_get(void)
@@ -13,7 +14,9 @@ DECL|bt_dev|struct|static struct bt_dev {
 DECL|bt_driver_register|function|int bt_driver_register(struct bt_driver *drv)
 DECL|bt_driver_unregister|function|void bt_driver_unregister(struct bt_driver *drv)
 DECL|bt_hci_cmd_create|function|static struct bt_buf *bt_hci_cmd_create(uint16_t opcode, uint8_t param_len)
+DECL|bt_hci_cmd_send_sync|function|static int bt_hci_cmd_send_sync(uint16_t opcode, struct bt_buf *buf)
 DECL|bt_hci_cmd_send|function|static int bt_hci_cmd_send(uint16_t opcode, struct bt_buf *buf)
+DECL|bt_hci_reset|function|int bt_hci_reset(void)
 DECL|bt_init|function|int bt_init(void)
 DECL|bt_recv|function|void bt_recv(struct bt_buf *buf)
 DECL|buffers|variable|buffers
@@ -22,6 +25,7 @@ DECL|cmd_queue_init|function|static void cmd_queue_init(void)
 DECL|cmd_queue|member|struct nano_fifo cmd_queue;
 DECL|dev|variable|dev
 DECL|drv|member|struct bt_driver *drv;
+DECL|features|member|uint8_t features[8];
 DECL|free_bufs|variable|free_bufs
 DECL|free_queue_init|function|static void free_queue_init(void)
 DECL|hci_acl|function|static void hci_acl(struct bt_buf *buf)
@@ -31,9 +35,22 @@ DECL|hci_cmd_fiber|function|static void hci_cmd_fiber(void)
 DECL|hci_cmd_status|function|static void hci_cmd_status(struct bt_buf *buf)
 DECL|hci_event|function|static void hci_event(struct bt_buf *buf)
 DECL|hci_init|function|static int hci_init(void)
-DECL|hci_receive_packet|function|static void hci_receive_packet(struct bt_buf *buf)
+DECL|hci_le_read_buffer_size_complete|function|static void hci_le_read_buffer_size_complete(struct bt_buf *buf)
+DECL|hci_le_read_features_complete|function|static void hci_le_read_features_complete(struct bt_buf *buf)
+DECL|hci_read_bdaddr_complete|function|static void hci_read_bdaddr_complete(struct bt_buf *buf)
+DECL|hci_read_buffer_size_complete|function|static void hci_read_buffer_size_complete(struct bt_buf *buf)
+DECL|hci_read_features_complete|function|static void hci_read_features_complete(struct bt_buf *buf)
+DECL|hci_read_local_ver_complete|function|static void hci_read_local_ver_complete(struct bt_buf *buf)
 DECL|hci_reset_complete|function|static void hci_reset_complete(struct bt_buf *buf)
+DECL|hci_revision|member|uint16_t hci_revision;
 DECL|hci_rx_fiber|function|static void hci_rx_fiber(void)
+DECL|hci_version|member|uint8_t hci_version;
+DECL|le_features|member|uint8_t le_features[8];
+DECL|le_mtu|member|uint16_t le_mtu;
+DECL|le_pkts|member|uint8_t le_pkts;
+DECL|lmp_bredr_capable|macro|lmp_bredr_capable
+DECL|lmp_le_capable|macro|lmp_le_capable
+DECL|manufacturer|member|uint16_t manufacturer;
 DECL|ncmd_sem|member|struct nano_sem ncmd_sem;
 DECL|ncmd|member|uint8_t ncmd;
 DECL|rx_fiber_stack|variable|rx_fiber_stack
