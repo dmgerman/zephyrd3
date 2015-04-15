1b3912467a6eca0e7222b47140fe3f6bb97376cd
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-15 13:20:08 -0500

    Fix spaces around parentheses
    This commit deletes spaces after open parentheses '(' and spaces before
    close paratheses ')'.
    
    This modification were done with the following script:
    
    checkpatch_script="$VXMICRO_BASE/scripts/checkpatch.pl --mailback --no-tree -f --emacs --summary-file --show-types --ignore BRACES,PRINTK_WITHOUT_KERN_LEVEL,SPLIT_STRING --max-line-length=100 "
    
    cd $VXMICRO_BASE;
    for file in $(find ./ -name "*.[ch]" ! -path "./scripts/*" ! -path "./host/src/wrsconfig/*");
    do
    	# fixing spaces after open parethesis
    	for line in $(eval $checkpatch_script $file | grep "ERROR:SPACING: space prohibited after that open parenthesis '('" | cut -d":" -f2)
    	do
    		sed -i -r -e ''$line' { s/\([ \t]*/\(/g }' $file;
    	done;
    
    	# fixing spaces before close parethesis
    	for line in $(eval $checkpatch_script $file | grep "ERROR:SPACING: space prohibited before that close parenthesis ')'" | cut -d":" -f2)
    	do
    		sed -i -r -e ''$line' { s/[ \t]*\)/\)/g }' $file;
    	done;
    done;
    
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    Change-Id: I4482fc50ec45542afa2e60c5f2fef4e75fb24dd3
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: f7df273f8c55202d46a8213ed906af7a1a3d696f

--- include/nanokernel/x86/k_mem-other.h
+++ include/nanokernel/x86/k_mem-other.h
@@ -1,2 +1,2 @@
 DECL|k_memcpy|function|__asm volatile void k_memcpy(void *_d, const void *_s, size_t_n)
-DECL|k_memset|function|__asm volatile void k_memset( void *_d, int _v, size_t _n)
+DECL|k_memset|function|__asm volatile void k_memset(void *_d, int _v, size_t _n)
