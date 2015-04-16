d060e38cdadfd565a2bc4d4d964a49599df7a699
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-16 12:25:11 -0700

    Renaming _StackPop _stack_pop
    Updating nano kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.kconf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 2d5fa0198f4bc3b8eef3d8498d9c4200894efa12

--- kernel/nanokernel/core/nano_stack.c
+++ kernel/nanokernel/core/nano_stack.c
@@ -1,4 +1,4 @@
-DECL|_StackPop|function|int _StackPop(struct nano_stack *chan, /* channel on which to interact */ uint32_t *pData /* container for data to pop */ )
+DECL|_stack_pop|function|int _stack_pop(struct nano_stack *chan, /* channel on which to interact */ uint32_t *pData /* container for data to pop */ )
 DECL|_stack_push|function|void _stack_push(struct nano_stack *chan, /* stack channel on which to interact */ uint32_t data /* data to push on stack */ )
 DECL|nano_fiber_stack_pop_wait|function|uint32_t nano_fiber_stack_pop_wait(struct nano_stack *chan /* channel on which to interact */ )
 DECL|nano_stack_init|function|void nano_stack_init(struct nano_stack *chan, /* stack to initialize */ uint32_t *data /* container for stack */ )
