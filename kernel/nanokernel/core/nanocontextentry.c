603457e6bb3eb7b39c9a45912503199452a95901
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-16 12:25:11 -0700

    Renaming _ContextEntryRtn to _context_entry
    Updating nano kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.kconf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 1cc59ccce90cd9601e7508aac127e29b4a23e8f9

--- kernel/nanokernel/core/nanocontextentry.c
+++ kernel/nanokernel/core/nanocontextentry.c
@@ -1,2 +1,2 @@
-DECL|_ContextEntryRtn|function|FUNC_NORETURN void _ContextEntryRtn(_ContextEntry pEntry, /* address of app entry point function */ _ContextArg parameter1, /* 1st arg to app entry point function */ _ContextArg parameter2, /* 2nd arg to app entry point function */ _ContextArg parameter3 /* 3rd arg to app entry point function */
+DECL|_context_entry|function|FUNC_NORETURN void _context_entry(_ContextEntry pEntry, /* address of app entry point function */ _ContextArg parameter1, /* 1st arg to app entry point function */ _ContextArg parameter2, /* 2nd arg to app entry point function */ _ContextArg parameter3 /* 3rd arg to app entry point function */
 DECL|_context_exit|function|void _context_exit(tCCS *pContext)
