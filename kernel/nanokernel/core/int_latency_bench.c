da861870002ee57ff81e8c2418d6e23e86534ba5
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-16 12:25:10 -0700

    Renamign intLatencyShow to int_latency_show
    Updating nano kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.conf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: ff0641fac6c85768241340ccded9b938d1fd51af

--- kernel/nanokernel/core/int_latency_bench.c
+++ kernel/nanokernel/core/int_latency_bench.c
@@ -4,11 +4,11 @@ DECL|_int_latency_start|function|void _int_latency_start(void)
 DECL|_int_latency_stop|function|void _int_latency_stop(void)
 DECL|initialStartDelay|variable|initialStartDelay
 DECL|intLatencyBenchRdy|variable|intLatencyBenchRdy
-DECL|intLatencyShow|function|void intLatencyShow(void)
 DECL|intLockUnlockNest|variable|intLockUnlockNest
 DECL|intLockedTimestamp|variable|intLockedTimestamp
 DECL|intLockingLatencyMax|variable|intLockingLatencyMax
 DECL|intLockingLatencyMin|variable|intLockingLatencyMin
 DECL|int_latency_init|function|void int_latency_init(void)
+DECL|int_latency_show|function|void int_latency_show(void)
 DECL|nestingDelay|variable|nestingDelay
 DECL|stopDelay|variable|stopDelay
