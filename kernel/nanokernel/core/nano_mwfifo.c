57f8239c34e0cc38a0f59fe6016a72b84c79c148
Johan Hedberg <johan.hedberg@intel.com>
2015-04-16 12:23:55 +0300

    nanokernel: Add scheduling context-independent APIs for nano_fifo
    When the exact scheduling context is not known it's convenient to have
    a wrapper API that uses context_type_get() to call the right function
    at runtime.
    
    Change-Id: Ie72a4b78d53f664575c1d9dfeace52b53d018850
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 68224c1c6989734ae062d95cfebc018c84a6c7cd

--- kernel/nanokernel/core/nano_mwfifo.c
+++ kernel/nanokernel/core/nano_mwfifo.c
@@ -1,6 +1,8 @@
 DECL|_fifo_get|function|void *_fifo_get(struct nano_fifo *chan /* channel on which to interact */ )
 DECL|_fifo_put|function|void _fifo_put(struct nano_fifo *chan, /* channel on which to interact */ void *data /* data to send */ )
 DECL|nano_fiber_fifo_get_wait|function|void *nano_fiber_fifo_get_wait(struct nano_fifo *chan /* channel on which to interact */ )
+DECL|nano_fifo_get_wait|function|void *nano_fifo_get_wait(struct nano_fifo *chan)
 DECL|nano_fifo_init|function|void nano_fifo_init(struct nano_fifo *chan /* channel to initialize */ )
+DECL|nano_fifo_put|function|void nano_fifo_put(struct nano_fifo *chan, void *data)
 DECL|nano_task_fifo_get_wait|function|void *nano_task_fifo_get_wait(struct nano_fifo *chan /* channel on which to interact */ )
 DECL|nano_task_fifo_put|function|void nano_task_fifo_put(struct nano_fifo *chan, /* channel on which to interact */ void *data /* data to send */ )
