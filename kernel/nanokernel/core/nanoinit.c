31795442afdfe947b05bff29e5f441b59495ccbf
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-16 12:25:11 -0700

    Replacing _NanoInit with _nano_init
    Updating nano kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.kconf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 31e11d63164bc55a03bafb0a25653b0199453828

--- kernel/nanokernel/core/nanoinit.c
+++ kernel/nanokernel/core/nanoinit.c
@@ -1,3 +1,3 @@
 DECL|_InterruptStack|variable|_InterruptStack
-DECL|_NanoInit|function|void _NanoInit(tCCS *dummyOutContext, int argc, char *argv[], char *envp[])
+DECL|_nano_init|function|void _nano_init(tCCS *dummyOutContext, int argc, char *argv[], char *envp[])
 DECL|mainStack|variable|mainStack
