ad91f08d3844809b5c44812e0b3bcc1e9cba7c15
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-16 12:25:11 -0700

    Rename _NanoFiberSwap to _nano_fiber_swap
    Updating nano kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.kconf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 18c8d41bcc60ffbeeec5255bb83658bde7682db3

--- kernel/nanokernel/core/nanofiber.c
+++ kernel/nanokernel/core/nanofiber.c
@@ -2,11 +2,11 @@ DECL|BOOT_BANNER|macro|BOOT_BANNER
 DECL|BUILD_TIMESTAMP|macro|BUILD_TIMESTAMP
 DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
 DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
-DECL|_NanoFiberSwap|function|FUNC_NORETURN void _NanoFiberSwap(void)
 DECL|_NanoStart|function|void _NanoStart(void)
 DECL|_context_essential_check|function|int _context_essential_check(tCCS *pCtx /* pointer to context */ )
 DECL|_fiber_start|function|void _fiber_start(char *pStack, unsigned stackSize, /* stack size in bytes */ nano_fiber_entry_t pEntry, int parameter1, int parameter2,
 DECL|_insert_ccs|function|void _insert_ccs(tCCS **queue, tCCS *ccs)
+DECL|_nano_fiber_swap|function|FUNC_NORETURN void _nano_fiber_swap(void)
 DECL|build_timestamp|variable|build_timestamp
 DECL|context_self_get|function|nano_context_id_t context_self_get(void)
 DECL|context_type_get|function|nano_context_type_t context_type_get(void)
