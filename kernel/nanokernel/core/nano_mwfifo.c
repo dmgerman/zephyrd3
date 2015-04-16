4e551b3b67f274fcc6369790d7d10451655ce378
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-16 12:25:11 -0700

    Renaming _FifoGet to _fifo_get
    Updating nano kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.kconf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 3acec988cb2b062c670954e3968c5ad93cc9cb21

--- kernel/nanokernel/core/nano_mwfifo.c
+++ kernel/nanokernel/core/nano_mwfifo.c
@@ -1,4 +1,4 @@
-DECL|_FifoGet|function|void *_FifoGet(struct nano_fifo *chan /* channel on which to interact */ )
+DECL|_fifo_get|function|void *_fifo_get(struct nano_fifo *chan /* channel on which to interact */ )
 DECL|_fifo_put|function|void _fifo_put(struct nano_fifo *chan, /* channel on which to interact */ void *data /* data to send */ )
 DECL|nano_fiber_fifo_get_wait|function|void *nano_fiber_fifo_get_wait(struct nano_fifo *chan /* channel on which to interact */ )
 DECL|nano_fifo_init|function|void nano_fifo_init(struct nano_fifo *chan /* channel to initialize */ )
