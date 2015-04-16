e1493d48dfc793aec8f5e137cefda27f8f0f3b56
Sarah Sharp <sarah.a.sharp@intel.com>
2015-04-16 13:36:13 -0700

    Clarify set_state_bit: rename variables, add comments.
    Rename local variables to provide more clarity and improve readability:
     - X -> task_ptr
     - f_old -> old_state_bits
     - f_new -> new_state_bits
     - H -> task_queue
     - Y -> cur_task
    
    Add comments to clarify what this function actually does without needing
    to open two additional files (include/microkernel/k_struct.h and
    kernel/microkernel/core/global.c).
    
    This function is called when a task needs to be marked as not runnable.
    A task can be not runnable for a variety of reasons, which are tracked
    in a bitmask defined in kernel/microkernel/include/minik.h.
    
    Tasks that are runnable are stored in a global array, K_PrioList, that
    contains a list for each task priority level.  Tasks that have
    transitioned to not being runnable will be removed from their priority
    list by this function.
    
    Change-Id: I07a1180e75e30cdef472c5f77638fc5971ddbaeb
    Signed-off-by: Sarah Sharp <sarah.a.sharp@intel.com>
    
    Former-commit-id: 030d59ce4b01348de82c1125160d3519c0fc48d2

--- kernel/microkernel/k_task.c
+++ kernel/microkernel/k_task.c
@@ -6,7 +6,7 @@ DECL|_task_group_ioctl|function|void _task_group_ioctl(ktask_group_t group, /* t
 DECL|_task_ioctl|function|void _task_ioctl(ktask_t task, /* task on which to operate */ int opt /* task operation */ )
 DECL|abort_task|function|void abort_task(struct k_proc *X)
 DECL|reset_state_bit|function|void reset_state_bit(struct k_proc *X, /* ptr to task */ uint32_t bits /* bitmask of TF_xxx bits to reset */ )
-DECL|set_state_bit|function|void set_state_bit(struct k_proc *X, /* ptr to task */ uint32_t bits /* bitmask of TF_xxx bits to set */ )
+DECL|set_state_bit|function|void set_state_bit(struct k_proc *task_ptr, uint32_t bits /* bitmask of TF_xxx bits to set */ )
 DECL|task_abort_handler_set|function|void task_abort_handler_set(void (*func)(void) /* abort handler */ )
 DECL|task_entry_set|function|void task_entry_set(ktask_t task, /* task */ void (*func)(void) /* entry point */ )
 DECL|task_priority_set|function|void task_priority_set(ktask_t task, /* task whose priority is to be set */ kpriority_t prio /* new priority */ )
