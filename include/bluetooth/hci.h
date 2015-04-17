85a7ab6f33070fe6188fad9fd342cddf5f0f2863
Johan Hedberg <johan.hedberg@intel.com>
2015-04-17 13:59:15 +0300

    Bluetooth: Add basic advertising definitions
    To manage LE advertising we need basic HCI definitions for the
    commands and events involved. This patch adds those to the hci.h
    header file.
    
    Change-Id: Ic6cf17369ad09d97ca5c098f25ba786d265d0bfd
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7bfbe7abf2446109a84635697650c9bbfb410a04

--- include/bluetooth/hci.h
+++ include/bluetooth/hci.h
@@ -1,8 +1,14 @@
+DECL|BT_EIR_FLAGS|macro|BT_EIR_FLAGS
+DECL|BT_EIR_NAME_COMPLETE|macro|BT_EIR_NAME_COMPLETE
 DECL|BT_HCI_EVT_CMD_COMPLETE|macro|BT_HCI_EVT_CMD_COMPLETE
 DECL|BT_HCI_EVT_CMD_STATUS|macro|BT_HCI_EVT_CMD_STATUS
 DECL|BT_HCI_LE_ENCRYPTION|macro|BT_HCI_LE_ENCRYPTION
 DECL|BT_HCI_OP_LE_READ_BUFFER_SIZE|macro|BT_HCI_OP_LE_READ_BUFFER_SIZE
 DECL|BT_HCI_OP_LE_READ_LOCAL_FEATURES|macro|BT_HCI_OP_LE_READ_LOCAL_FEATURES
+DECL|BT_HCI_OP_LE_SET_ADV_DATA|macro|BT_HCI_OP_LE_SET_ADV_DATA
+DECL|BT_HCI_OP_LE_SET_ADV_ENABLE|macro|BT_HCI_OP_LE_SET_ADV_ENABLE
+DECL|BT_HCI_OP_LE_SET_ADV_PARAMETERS|macro|BT_HCI_OP_LE_SET_ADV_PARAMETERS
+DECL|BT_HCI_OP_LE_SET_SCAN_RSP_DATA|macro|BT_HCI_OP_LE_SET_SCAN_RSP_DATA
 DECL|BT_HCI_OP_LE_WRITE_LE_HOST_SUPP|macro|BT_HCI_OP_LE_WRITE_LE_HOST_SUPP
 DECL|BT_HCI_OP_READ_BD_ADDR|macro|BT_HCI_OP_READ_BD_ADDR
 DECL|BT_HCI_OP_READ_BUFFER_SIZE|macro|BT_HCI_OP_READ_BUFFER_SIZE
@@ -10,12 +16,20 @@ DECL|BT_HCI_OP_READ_LOCAL_FEATURES|macro|BT_HCI_OP_READ_LOCAL_FEATURES
 DECL|BT_HCI_OP_READ_LOCAL_VERSION_INFO|macro|BT_HCI_OP_READ_LOCAL_VERSION_INFO
 DECL|BT_HCI_OP_RESET|macro|BT_HCI_OP_RESET
 DECL|BT_HCI_OP_SET_EVENT_MASK|macro|BT_HCI_OP_SET_EVENT_MASK
+DECL|BT_LE_ADV_IND|macro|BT_LE_ADV_IND
+DECL|BT_LE_ADV_NONCONN_IND|macro|BT_LE_ADV_NONCONN_IND
+DECL|BT_LE_AD_GENERAL|macro|BT_LE_AD_GENERAL
+DECL|BT_LE_AD_NO_BREDR|macro|BT_LE_AD_NO_BREDR
 DECL|BT_LMP_LE|macro|BT_LMP_LE
 DECL|BT_LMP_NO_BREDR|macro|BT_LMP_NO_BREDR
 DECL|BT_OGF_BASEBAND|macro|BT_OGF_BASEBAND
 DECL|BT_OGF_INFO|macro|BT_OGF_INFO
 DECL|BT_OGF_LE|macro|BT_OGF_LE
 DECL|BT_OP|macro|BT_OP
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
@@ -36,6 +50,10 @@ DECL|bdaddr|member|uint8_t bdaddr[6];
 DECL|bt_acl_handle|macro|bt_acl_handle
 DECL|bt_hci_acl_hdr|struct|struct bt_hci_acl_hdr {
 DECL|bt_hci_cmd_hdr|struct|struct bt_hci_cmd_hdr {
+DECL|bt_hci_cp_le_set_adv_data|struct|struct bt_hci_cp_le_set_adv_data {
+DECL|bt_hci_cp_le_set_adv_enable|struct|struct bt_hci_cp_le_set_adv_enable {
+DECL|bt_hci_cp_le_set_adv_parameters|struct|struct bt_hci_cp_le_set_adv_parameters {
+DECL|bt_hci_cp_le_set_scan_rsp_data|struct|struct bt_hci_cp_le_set_scan_rsp_data {
 DECL|bt_hci_cp_set_event_mask|struct|struct bt_hci_cp_set_event_mask {
 DECL|bt_hci_cp_write_le_host_supp|struct|struct bt_hci_cp_write_le_host_supp {
 DECL|bt_hci_evt_cmd_status|struct|struct bt_hci_evt_cmd_status {
@@ -46,10 +64,17 @@ DECL|bt_hci_rp_read_bd_addr|struct|struct bt_hci_rp_read_bd_addr {
 DECL|bt_hci_rp_read_buffer_size|struct|struct bt_hci_rp_read_buffer_size {
 DECL|bt_hci_rp_read_local_features|struct|struct bt_hci_rp_read_local_features {
 DECL|bt_hci_rp_read_local_version_info|struct|struct bt_hci_rp_read_local_version_info {
+DECL|channel_map|member|uint8_t channel_map;
+DECL|data|member|uint8_t data[31];
+DECL|data|member|uint8_t data[31];
+DECL|direct_addr_type|member|uint8_t direct_addr_type;
+DECL|direct_addr|member|uint8_t direct_addr[6];
+DECL|enable|member|uint8_t enable;
 DECL|events|member|uint8_t events[8];
 DECL|evt|member|uint8_t evt;
 DECL|features|member|uint8_t features[8];
 DECL|features|member|uint8_t features[8];
+DECL|filter_policy|member|uint8_t filter_policy;
 DECL|handle|member|uint16_t handle;
 DECL|hci_evt_cmd_complete|struct|struct hci_evt_cmd_complete {
 DECL|hci_revision|member|uint16_t hci_revision;
@@ -58,15 +83,20 @@ DECL|le_max_len|member|uint16_t le_max_len;
 DECL|le_max_num|member|uint8_t le_max_num;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint8_t len;
+DECL|len|member|uint8_t len;
+DECL|len|member|uint8_t len;
 DECL|le|member|uint8_t le;
 DECL|lmp_subversion|member|uint16_t lmp_subversion;
 DECL|lmp_version|member|uint8_t lmp_version;
 DECL|manufacturer|member|uint16_t manufacturer;
+DECL|max_interval|member|uint16_t max_interval;
+DECL|min_interval|member|uint16_t min_interval;
 DECL|ncmd|member|uint8_t ncmd;
 DECL|ncmd|member|uint8_t ncmd;
 DECL|opcode|member|uint16_t opcode;
 DECL|opcode|member|uint16_t opcode;
 DECL|opcode|member|uint16_t opcode;
+DECL|own_addr_type|member|uint8_t own_addr_type;
 DECL|param_len|member|uint8_t param_len;
 DECL|sco_max_len|member|uint8_t sco_max_len;
 DECL|sco_max_num|member|uint16_t sco_max_num;
@@ -78,3 +108,4 @@ DECL|status|member|uint8_t status;
 DECL|status|member|uint8_t status;
 DECL|status|member|uint8_t status;
 DECL|status|member|uint8_t status;
+DECL|type|member|uint8_t type;
