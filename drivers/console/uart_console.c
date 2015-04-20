c2b53dc1ce08fee11c5f87cb63af23bd080c5fc6
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-04-20 11:04:22 +0300

    Add serial console input handler
    Serial console handler when registered allows to interact with serial
    line. It can either execute callback from ISR or defer execution to
    fiber or task. This will be used in Bluetooth development for tests and
    simple interaction with Bluetooth stack.
    
    Change-Id: Ia960b456a75062d614baea324608058d979aa11b
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 139c856403e91ad9e2d61f9308f66ccc2fabdeb7

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -1,5 +1,15 @@
+DECL|MAX_LINE_LENGTH|macro|MAX_LINE_LENGTH
 DECL|UART|macro|UART
 DECL|__printk_hook_install|macro|__printk_hook_install
 DECL|__stdout_hook_install|macro|__stdout_hook_install
 DECL|consoleOut|function|static int consoleOut(int c /* character to output */)
+DECL|console_handler_init|function|static void console_handler_init(void)
+DECL|console_handler_init|macro|console_handler_init
+DECL|console_uart_isr|function|void console_uart_isr(void *unused)
+DECL|handler|variable|handler
+DECL|rcv_data|variable|rcv_data
+DECL|rcv_pos|variable|rcv_pos
+DECL|read_uart|function|static int read_uart(int uart, uint8_t *buf, unsigned int size)
 DECL|uartConsoleInit|function|void uartConsoleInit(void)
+DECL|uart_register_handler|function|void uart_register_handler(void (*cb) (const char *string))
+DECL|uart_register_handler|macro|uart_register_handler
