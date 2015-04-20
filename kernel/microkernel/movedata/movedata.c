a25162c3d0f3fb9a8ddeed802dc686edf4d9c672
Peter Mitsis <peter.mitsis@windriver.com>
2015-04-20 16:07:26 -0400

    Remove references to multi-node systems in movedata.c
    Multi-node systems are not supported.
    
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    Change-Id: If16ffde2bf18d8729bc7d16e558db146b042bb36
    
    Former-commit-id: 3b70db2da32952cb8a049a9126a2c35b9fce5452

--- kernel/microkernel/movedata/movedata.c
+++ kernel/microkernel/movedata/movedata.c
@@ -1,3 +1,3 @@
 DECL|K_mvdreq|function|void K_mvdreq(struct k_args *Req)
-DECL|mvdreq_LILC|function|static void mvdreq_LILC(struct moved_req *ReqArgs)
+DECL|mvdreq_copy|function|static void mvdreq_copy(struct moved_req *ReqArgs)
 DECL|mvdreq_docont|function|static void mvdreq_docont(struct k_args *Cont)
