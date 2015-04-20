e03e26d4293924047c3c9350c3e69f91cdcee899
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-04-20 11:27:27 -0400

    Remove PowerPC artefacts from test_fp_sharing test
    PowerPC is not supported platform. It's components are to be removed.
    
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 869582eb92b06b1dd78082aadb665f798b4551a5

--- samples/microkernel/test/test_fp_sharing/src/float_context.h
+++ samples/microkernel/test/test_fp_sharing/src/float_context.h
@@ -1,47 +1,23 @@
 DECL|FIBER_FLOAT_REG_CHECK_BYTE|macro|FIBER_FLOAT_REG_CHECK_BYTE
 DECL|FP_NONVOLATILE_REG_SET|typedef|} FP_NONVOLATILE_REG_SET;
-DECL|FP_NONVOLATILE_REG_SET|typedef|} FP_NONVOLATILE_REG_SET;
-DECL|FP_NONVOLATILE_REG_SET|typedef|} FP_NONVOLATILE_REG_SET;
-DECL|FP_OPTION|macro|FP_OPTION
-DECL|FP_OPTION|macro|FP_OPTION
 DECL|FP_OPTION|macro|FP_OPTION
 DECL|FP_REG_SET|typedef|} FP_REG_SET;
 DECL|FP_REG|typedef|} FP_REG;
-DECL|FP_REG|typedef|} FP_REG;
-DECL|FP_REG|typedef|} FP_REG;
-DECL|FP_VOLATILE_REG_SET|typedef|} FP_VOLATILE_REG_SET;
-DECL|FP_VOLATILE_REG_SET|typedef|} FP_VOLATILE_REG_SET;
 DECL|FP_VOLATILE_REG_SET|typedef|} FP_VOLATILE_REG_SET;
 DECL|MAIN_FLOAT_REG_CHECK_BYTE|macro|MAIN_FLOAT_REG_CHECK_BYTE
 DECL|SIZEOF_FP_NONVOLATILE_REG_SET|macro|SIZEOF_FP_NONVOLATILE_REG_SET
-DECL|SIZEOF_FP_NONVOLATILE_REG_SET|macro|SIZEOF_FP_NONVOLATILE_REG_SET
-DECL|SIZEOF_FP_NONVOLATILE_REG_SET|macro|SIZEOF_FP_NONVOLATILE_REG_SET
 DECL|SIZEOF_FP_REG_SET|macro|SIZEOF_FP_REG_SET
-DECL|SIZEOF_FP_VOLATILE_REG_SET|macro|SIZEOF_FP_VOLATILE_REG_SET
-DECL|SIZEOF_FP_VOLATILE_REG_SET|macro|SIZEOF_FP_VOLATILE_REG_SET
 DECL|SIZEOF_FP_VOLATILE_REG_SET|macro|SIZEOF_FP_VOLATILE_REG_SET
 DECL|XMM_REG|typedef|} XMM_REG;
 DECL|_FLOATCONTEXT_H|macro|_FLOATCONTEXT_H
 DECL|fpNonVolRegSet|member|FP_NONVOLATILE_REG_SET fpNonVolRegSet;
 DECL|fpNonVolatileRegSet|struct|typedef struct fpNonVolatileRegSet
-DECL|fpNonVolatileRegSet|struct|typedef struct fpNonVolatileRegSet
-DECL|fpNonVolatileRegSet|struct|typedef struct fpNonVolatileRegSet
 DECL|fpRegSet|struct|typedef struct fpRegSet
-DECL|fpRegs|member|FP_REG fpRegs[11]; /* r0, r3 -> r12 */
-DECL|fpRegs|member|FP_REG fpRegs[14]; /* f0 -> f13 */
-DECL|fpRegs|member|FP_REG fpRegs[18]; /* f14 -> f31 */
-DECL|fpRegs|member|FP_REG fpRegs[18]; /* r14 -> r31 */
 DECL|fpRegs|member|FP_REG fpRegs[8]; /* ST[0] -> ST[7] */
-DECL|fpReg|struct|typedef struct fpReg
-DECL|fpReg|struct|typedef struct fpReg
 DECL|fpReg|struct|typedef struct fpReg
 DECL|fpVolRegSet|member|FP_VOLATILE_REG_SET fpVolRegSet;
 DECL|fpVolatileRegSet|struct|typedef struct fpVolatileRegSet
-DECL|fpVolatileRegSet|struct|typedef struct fpVolatileRegSet
-DECL|fpVolatileRegSet|struct|typedef struct fpVolatileRegSet
 DECL|reg|member|unsigned char reg[10];
 DECL|reg|member|unsigned char reg[16];
-DECL|reg|member|unsigned char reg[4];
-DECL|reg|member|unsigned char reg[8];
 DECL|xmmRegs|member|XMM_REG xmmRegs[8]; /* XMM[0] -> XMM[7] */
 DECL|xmmReg|struct|typedef struct xmmReg
