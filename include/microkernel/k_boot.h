01d1b736391879e4ba84d942fff6871c3261eba4
Peter Mitsis <peter.mitsis@windriver.com>
2015-04-21 14:11:11 -0400

    Remove k_boot.h and flashboot.c
    The files k_boot.h and flashboot.c are obsolete.
    
    Change-Id: Ieab69e714462079c2a836dacb7c5bab53d2e3b2b
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: bd969c97ccfcd5621ef1adfd2c3407ea652573e5

--- include/microkernel/k_boot.h
+++
@@ -1,4 +0,0 @@
-DECL|__K_BOOT_H__|macro|__K_BOOT_H__
-DECL|boot_struct|struct|struct boot_struct {
-DECL|data|member|uint32_t data;
-DECL|reserved|member|uint32_t reserved;
--- kernel/microkernel/core/flashboot.c
+++
@@ -1,2 +0,0 @@
-DECL|PutWordToDevNull|function|int PutWordToDevNull(struct boot_struct *pBootData)
-DECL|ReadNextWordFromFlash|function|int ReadNextWordFromFlash(struct boot_struct *pBootData)
