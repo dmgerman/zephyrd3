c326a88b225c21af434fcf9c219bbfcb9936d587
Peter Mitsis <peter.mitsis@windriver.com>
2015-04-20 20:05:01 -0400

    Cosmetic: remove cruft from mycopypacket()
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    Change-Id: Ia95366325668476a222c85748f3141d09767c72a
    
    Former-commit-id: cfa74111217d0b71de2b564d35795466136d7f92

--- kernel/microkernel/channel/ch_addit.c
+++ kernel/microkernel/channel/ch_addit.c
@@ -13,5 +13,5 @@ DECL|ChxxxSetChOpt|function|void ChxxxSetChOpt(K_ARGS_ARGS *pChxxx, K_PIPE_OPTIO
 DECL|ChxxxSetReqType|function|void ChxxxSetReqType(K_ARGS_ARGS *pChxxx, REQ_TYPE ReqType)
 DECL|ChxxxSetTimeType|function|void ChxxxSetTimeType(K_ARGS_ARGS *pChxxx, TIME_TYPE TimeType)
 DECL|DeListWaiter|function|void DeListWaiter(struct k_args *pReqProc)
-DECL|mycopypacket|function|void mycopypacket(struct k_args **out,struct k_args *in) /* borrowed from mailbox implementation */ /* my version will ALWAYS make a copy = as the name implies it!! */
+DECL|mycopypacket|function|void mycopypacket(struct k_args **out, struct k_args *in)
 DECL|myfreetimer|function|void myfreetimer(K_TIMER * *ppTimer)
