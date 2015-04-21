1104858bc9ee64570da6dc42a239b9860e834544
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-04-21 12:06:23 +0300

    ti_lm3s6965: Add Bluetooth initialization to system.c
    Add Bluetooth initialization for target ti_lm3s6965.
    
    Change-Id: I7c7e392d2bb8671098e54b1305051a5882fd3bef
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: cc19fbb873a5d8cb303ece90618d180a9eff7732

--- arch/arm/ti_lm3s6965/system.c
+++ arch/arm/ti_lm3s6965/system.c
@@ -6,6 +6,8 @@ DECL|RCGC1_UART1_EN|macro|RCGC1_UART1_EN
 DECL|RCGC1_UART2_EN|macro|RCGC1_UART2_EN
 DECL|RCGC1|macro|RCGC1
 DECL|_InitHardware|function|void _InitHardware(void)
+DECL|bluetooth_init|function|static void bluetooth_init(void)
+DECL|bluetooth_init|macro|bluetooth_init
 DECL|consoleInit|function|static void consoleInit(void)
 DECL|consoleInit|macro|consoleInit
 DECL|uartGenericInfoInit|function|static inline void uartGenericInfoInit(struct uart_init_info *pInfo)
