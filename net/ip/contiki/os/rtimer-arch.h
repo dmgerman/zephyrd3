9027d9433ff2f0568269764dbb37c6dcc48f6080
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-04-22 09:56:49 +0300

    net: uip: Added missing function prototypes to rtimer
    Real timer implementation needs function prototypes.
    This is just to satisfy the compiler. Needs more TLC.
    
    Change-Id: I39baa7cfe69d28aa1f330089e22144df286c0a0d
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: bb69910e2a254ee0c825d0ea41cb953d72612287

--- net/ip/contiki/os/rtimer-arch.h
+++ net/ip/contiki/os/rtimer-arch.h
@@ -1,1 +1,4 @@
+DECL|RTIMER_ARCH_SECOND|macro|RTIMER_ARCH_SECOND
 DECL|__RTIMER_ARCH_H__|macro|__RTIMER_ARCH_H__
+DECL|rtimer_arch_init|function|static inline void rtimer_arch_init(void) {}
+DECL|rtimer_arch_now|function|static inline rtimer_clock_t rtimer_arch_now(void) { return 0; }
