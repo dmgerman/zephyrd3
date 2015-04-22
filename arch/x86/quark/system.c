8713bf9a314be7ca01cfe7507074041ebbf06776
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-04-22 15:52:21 +0300

    quark: Avoid duplicate definition
    No need to duplicate PCI class definition since it's alread defined in
    drivers/pci/pci_mgr.h.
    
    Change-Id: I834b99c300487d7c51e406555de87d2484222213
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: e2eff8fafcd933bab86271aa0da0511c08940bea

--- arch/x86/quark/system.c
+++ arch/x86/quark/system.c
@@ -1,5 +1,4 @@
 DECL|DO_CONSOLE_INIT|macro|DO_CONSOLE_INIT
-DECL|PCI_CLASS_COMM_CTLR|macro|PCI_CLASS_COMM_CTLR
 DECL|_InitHardware|function|void _InitHardware(void)
 DECL|_SysPciMap|function|uint32_t _SysPciMap(uint32_t addr, uint32_t size)
 DECL|consoleInit|function|static void consoleInit(void)
