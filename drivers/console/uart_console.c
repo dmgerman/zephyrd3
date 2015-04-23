0b0a65c1ae758887c97aa262e87c7c75b7cda5d7
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-04-23 10:20:32 +0300

    serial_console: Rename console_uart_isr to uart_console_isr
    Following comments received during review rename console_uart_isr to
    uart_console_isr for consistency reason.
    
    Change-Id: Ibee1dec40ee19a9095f78969a07ffb73518fffbb
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: dfa5f2371bbdc4d08479cb0504e2119e76195b76

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -5,11 +5,11 @@ DECL|__stdout_hook_install|macro|__stdout_hook_install
 DECL|consoleOut|function|static int consoleOut(int c /* character to output */)
 DECL|console_handler_init|function|static void console_handler_init(void)
 DECL|console_handler_init|macro|console_handler_init
-DECL|console_uart_isr|function|void console_uart_isr(void *unused)
 DECL|handler|variable|handler
 DECL|rcv_data|variable|rcv_data
 DECL|rcv_pos|variable|rcv_pos
 DECL|read_uart|function|static int read_uart(int uart, uint8_t *buf, unsigned int size)
 DECL|uartConsoleInit|function|void uartConsoleInit(void)
+DECL|uart_console_isr|function|void uart_console_isr(void *unused)
 DECL|uart_register_handler|function|void uart_register_handler(void (*cb) (const char *string))
 DECL|uart_register_handler|macro|uart_register_handler
