3f34597db8b6a3879381c71a3a9c16e4b2e6c877
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-04-23 12:35:20 +0300

    serial_console: Use static wrappers with right _IntExit()
    Instead of calling directly isr call static wrapper with right
    _IntExit() at the end.
    
    Change-Id: Ic5ca74200be6477837bf6a25803540990e9e8889
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: c6a72c2f8152df9af2ececc9898c0074974ca197

--- arch/arm/fsl_frdm_k64f/irq_vector_table.c
+++ arch/arm/fsl_frdm_k64f/irq_vector_table.c
@@ -1,3 +1,4 @@
 DECL|_IrqVectorTable|variable|_IrqVectorTable
 DECL|_IrqVectorTable|variable|_IrqVectorTable
+DECL|_uart_console_isr|function|static void _uart_console_isr(void)
 DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
