4b5fc59e36a4d1564093c0629d9b896495eb0cd5
Allan Stephens <allan.stephens@windriver.com>
2015-04-24 16:34:53 -0400

    Introduce NUM_TASK_PRIORITIES configuration option
    Allows the number of task priority levels supported by the micro-
    kernel to be configured, rather than fixing this value at 64.
    Setting NUM_TASK_PRIORITIES to N provides support for task
    priorities 0 (highest priority) through N-1 (lowest priority).
    The lowest priority is reserved for the kernel's idle task.
    
    This enhancement allows system designers to significantly reduce
    the size of the microkernel's task priority queues by eliminating
    unneeded queues. In systems requiring only a few task priorities
    over 500 bytes can be saved.
    
    Change-Id: I497aac608a3d548fb1b024068c08a5f494c3d524
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 1c32b5d72099ec01191dc94999c023c101e6d1b1

--- kernel/microkernel/core/global.c
+++ kernel/microkernel/core/global.c
@@ -1,5 +1,4 @@
 DECL|_k_command_stack|variable|_k_command_stack
 DECL|_k_debug_halt|variable|_k_debug_halt
 DECL|_k_server_command_stack_storage|variable|_k_server_command_stack_storage
-DECL|_k_task_priority_bitmap|variable|_k_task_priority_bitmap
 DECL|kernel_init|function|void kernel_init(void)
