f9b986953905bcb36489a7d01cc105ceed602786
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:23 -0500

    Rename mainStack to _k_init_and_idle_task_stack
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I3d7f9dfe1874d189fa613088efd8c056b974e01b
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: d57685c93e8c56196b9a693f6bc5bad085eacda3

--- kernel/nanokernel/core/nanoinit.c
+++ kernel/nanokernel/core/nanoinit.c
@@ -1,3 +1,3 @@
 DECL|_interrupt_stack|variable|_interrupt_stack
+DECL|_k_init_and_idle_task_stack|variable|_k_init_and_idle_task_stack
 DECL|_nano_init|function|void _nano_init(tCCS *dummyOutContext, int argc, char *argv[], char *envp[])
-DECL|mainStack|variable|mainStack
