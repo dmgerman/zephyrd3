8ff5d4e6f5511d3d9808cf5b1f5cfcf88d686055
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:39 -0500

    Rename TaskSwitchCallBack to _k_task_switch_callback
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I18b2fb3250db6a7a6717b8329702e2b76a7492f9
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 79dd1b48bddf7f932440a898ab2aeea8e7490b63

--- kernel/microkernel/task_monitor.c
+++ kernel/microkernel/task_monitor.c
@@ -3,8 +3,8 @@ DECL|K_monitor_args|function|void K_monitor_args(struct k_args *A)
 DECL|K_monitor_read|function|void K_monitor_read(struct k_args *A)
 DECL|K_monitor_task|function|void K_monitor_task(struct k_proc *X, uint32_t D)
 DECL|K_monitor_wind|variable|K_monitor_wind
-DECL|TaskSwitchCallBack|variable|TaskSwitchCallBack
 DECL|_k_monitor_mask|variable|_k_monitor_mask
+DECL|_k_task_switch_callback|variable|_k_task_switch_callback
 DECL|k_monitor_buff|variable|k_monitor_buff
 DECL|k_monitor_capacity|variable|k_monitor_capacity
 DECL|k_monitor_nrec|variable|k_monitor_nrec
