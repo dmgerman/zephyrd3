fa4995c664e321243390b9a65a3c46a7941c4086
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:36 -0500

    Rename SlicePrio to slice_prio
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: Iefa600d4b2c981a7146fc34c0fb5e491690a694c
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 14d19a7bc3e7a71eb94443602547f83799336ef1

--- kernel/microkernel/timer/ticker.c
+++ kernel/microkernel/timer/ticker.c
@@ -1,6 +1,5 @@
 DECL|K_DebugLowTime|variable|K_DebugLowTime
 DECL|K_ticker|function|int K_ticker(int event)
-DECL|SlicePrio|variable|SlicePrio
 DECL|_HandleExpiredTimers|function|static inline void _HandleExpiredTimers(int ticks)
 DECL|_SysIdleElapsedTicksGet|function|static inline int32_t _SysIdleElapsedTicksGet(void)
 DECL|_SysIdleElapsedTicks|variable|_SysIdleElapsedTicks
@@ -10,6 +9,7 @@ DECL|_TlDebugUpdate|macro|_TlDebugUpdate
 DECL|_WlMonitorUpdate|function|static inline void _WlMonitorUpdate(void)
 DECL|scheduler_time_slice_set|function|void scheduler_time_slice_set(int32_t t, /* time slice in ticks */kpriority_t p /* beginning priority level to which time slicing applies */ )
 DECL|slice_count|variable|slice_count
+DECL|slice_prio|variable|slice_prio
 DECL|slice_time|variable|slice_time
 DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
