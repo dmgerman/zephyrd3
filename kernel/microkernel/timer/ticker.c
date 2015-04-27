e85ed7a7583ba36f8ea609569941791fbd239d6b
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:39 -0500

    Rename K_DebugLowTime to _k_debug_sys_clock_tick_count
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: Id55e2394076ed44694e3f761f8a0e6e7bf0b59f9
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 8489e2410d1d175ee89ee5a857c289bf131e045d

--- kernel/microkernel/timer/ticker.c
+++ kernel/microkernel/timer/ticker.c
@@ -1,4 +1,3 @@
-DECL|K_DebugLowTime|variable|K_DebugLowTime
 DECL|K_ticker|function|int K_ticker(int event)
 DECL|_HandleExpiredTimers|function|static inline void _HandleExpiredTimers(int ticks)
 DECL|_SysIdleElapsedTicksGet|function|static inline int32_t _SysIdleElapsedTicksGet(void)
@@ -6,6 +5,7 @@ DECL|_TimeSliceUpdate|function|static inline void _TimeSliceUpdate(void)
 DECL|_TlDebugUpdate|function|static inline int _TlDebugUpdate(int32_t ticks)
 DECL|_TlDebugUpdate|macro|_TlDebugUpdate
 DECL|_WlMonitorUpdate|function|static inline void _WlMonitorUpdate(void)
+DECL|_k_debug_sys_clock_tick_count|variable|_k_debug_sys_clock_tick_count
 DECL|_sys_idle_elapsed_ticks|variable|_sys_idle_elapsed_ticks
 DECL|scheduler_time_slice_set|function|void scheduler_time_slice_set(int32_t t, /* time slice in ticks */kpriority_t p /* beginning priority level to which time slicing applies */ )
 DECL|slice_count|variable|slice_count
