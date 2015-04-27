0e17ebd3eb648e858c4fded3bc05a7429888d3cb
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:28 -0500

    Rename K_Ttail to _k_timer_list_tail
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I5e774c9b78ace93049a41824cee0305ceb69228d
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: b4db2ae7fc7d28e49a429118e601cee53396747a

--- kernel/microkernel/timer/ticks.c
+++ kernel/microkernel/timer/ticks.c
@@ -1,5 +1,5 @@
 DECL|K_LowTime|variable|K_LowTime
-DECL|K_Ttail|variable|K_Ttail
 DECL|_LowTimeGet|function|int64_t _LowTimeGet(void)
 DECL|_LowTimeInc|function|void _LowTimeInc(int inc)
 DECL|_k_timer_list_head|variable|_k_timer_list_head
+DECL|_k_timer_list_tail|variable|_k_timer_list_tail
