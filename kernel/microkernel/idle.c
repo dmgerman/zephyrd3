bb725346bde9ed039427bcbd77ce64b97b664d26
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:32 -0500

    Rename WldT1 to _k_workload_t1
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I0edd17bc4019ff3924ac4bca00baac4faf4d7d3f
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 1bd57ea4c633dce7c76dac79ba836c8a9a7ce76f

--- kernel/microkernel/idle.c
+++ kernel/microkernel/idle.c
@@ -3,7 +3,6 @@ DECL|K_workload|function|void K_workload(struct k_args *P)
 DECL|MSEC_PER_SEC|macro|MSEC_PER_SEC
 DECL|WldN0|variable|WldN0
 DECL|WldN1|variable|WldN1
-DECL|WldT1|variable|WldT1
 DECL|WldTDelta|variable|WldTDelta
 DECL|WldT_end|variable|WldT_end
 DECL|WldT_start|variable|WldT_start
@@ -15,6 +14,7 @@ DECL|_WlLoop|function|static void _WlLoop(void)
 DECL|_k_workload_ref_time|variable|_k_workload_ref_time
 DECL|_k_workload_slice|variable|_k_workload_slice
 DECL|_k_workload_t0|variable|_k_workload_t0
+DECL|_k_workload_t1|variable|_k_workload_t1
 DECL|_k_workload_ticks|variable|_k_workload_ticks
 DECL|kernel_idle|function|int kernel_idle(void)
 DECL|task_node_workload_get|function|int task_node_workload_get(void)
