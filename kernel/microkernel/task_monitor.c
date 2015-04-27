e22a016105a919325d0921a259be71987fc7b911
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:37 -0500

    Rename K_monitor_capacity to k_monitor_capacity
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I44e74e2edd4b76797aa5ac7d4f0f528ce1a3bfae
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: b1b4747e8f98e7bb0485e11c45786c25067b6808

--- kernel/microkernel/task_monitor.c
+++ kernel/microkernel/task_monitor.c
@@ -1,6 +1,5 @@
 DECL|KS_TaskSetSwitchCallBack|function|void KS_TaskSetSwitchCallBack(taskswitchcallbackfunc func)
 DECL|K_monitor_args|function|void K_monitor_args(struct k_args *A)
-DECL|K_monitor_capacity|variable|K_monitor_capacity
 DECL|K_monitor_mask|variable|K_monitor_mask
 DECL|K_monitor_nrec|variable|K_monitor_nrec
 DECL|K_monitor_read|function|void K_monitor_read(struct k_args *A)
@@ -9,3 +8,4 @@ DECL|K_monitor_wind|variable|K_monitor_wind
 DECL|K_monitor_wptr|variable|K_monitor_wptr
 DECL|TaskSwitchCallBack|variable|TaskSwitchCallBack
 DECL|k_monitor_buff|variable|k_monitor_buff
+DECL|k_monitor_capacity|variable|k_monitor_capacity
