3717a0901d11c43d75d2352d9cf8cdb9b2288614
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:31 -0500

    Rename WldRefT to _k_workload_ref_time
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I31baa614d4951cfcc6a56661cad6132514fb17a7
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: b11fa62c50dd300a82229466eba86c318ec9689e

--- kernel/microkernel/idle.c
+++ kernel/microkernel/idle.c
@@ -3,7 +3,6 @@ DECL|K_workload|function|void K_workload(struct k_args *P)
 DECL|MSEC_PER_SEC|macro|MSEC_PER_SEC
 DECL|WldN0|variable|WldN0
 DECL|WldN1|variable|WldN1
-DECL|WldRefT|variable|WldRefT
 DECL|WldT0|variable|WldT0
 DECL|WldT1|variable|WldT1
 DECL|WldTDelta|variable|WldTDelta
@@ -14,6 +13,7 @@ DECL|Wld_i|variable|Wld_i
 DECL|_GetNextTimerExpiry|function|static inline int32_t _GetNextTimerExpiry(void)
 DECL|_PowerSave|function|static void _PowerSave(void)
 DECL|_WlLoop|function|static void _WlLoop(void)
+DECL|_k_workload_ref_time|variable|_k_workload_ref_time
 DECL|_k_workload_slice|variable|_k_workload_slice
 DECL|_k_workload_ticks|variable|_k_workload_ticks
 DECL|kernel_idle|function|int kernel_idle(void)
