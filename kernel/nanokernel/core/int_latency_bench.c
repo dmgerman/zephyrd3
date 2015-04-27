0d4e9234ed824b4ef61a5065d85d572b28c8859a
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:17 -0500

    Rename initialStartDelay to initial_start_delay
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I95b2198b75ea30e3a6911623b304e4559befab77
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: bfa745b12823fe62d738006cdf828dea44546cdf

--- kernel/nanokernel/core/int_latency_bench.c
+++ kernel/nanokernel/core/int_latency_bench.c
@@ -2,7 +2,7 @@ DECL|NB_CACHE_WARMING_DRY_RUN|macro|NB_CACHE_WARMING_DRY_RUN
 DECL|_HwIntToCHandlerLatency|variable|_HwIntToCHandlerLatency
 DECL|_int_latency_start|function|void _int_latency_start(void)
 DECL|_int_latency_stop|function|void _int_latency_stop(void)
-DECL|initialStartDelay|variable|initialStartDelay
+DECL|initial_start_delay|variable|initial_start_delay
 DECL|intLatencyBenchRdy|variable|intLatencyBenchRdy
 DECL|intLockUnlockNest|variable|intLockUnlockNest
 DECL|intLockedTimestamp|variable|intLockedTimestamp
