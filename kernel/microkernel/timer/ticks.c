629074aa59eac9f4bee76aa1f4e9feb80f0eaf89
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:28 -0500

    Rename K_Thead to _k_timer_list_head
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I8ad0774ad26836e193ed94165491e0bb1e6e9a0c
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: fc6d85e56b63bc5c1a3b9c2f18de4e3c5e0a0208

--- kernel/microkernel/timer/ticks.c
+++ kernel/microkernel/timer/ticks.c
@@ -1,5 +1,5 @@
 DECL|K_LowTime|variable|K_LowTime
-DECL|K_Thead|variable|K_Thead
 DECL|K_Ttail|variable|K_Ttail
 DECL|_LowTimeGet|function|int64_t _LowTimeGet(void)
 DECL|_LowTimeInc|function|void _LowTimeInc(int inc)
+DECL|_k_timer_list_head|variable|_k_timer_list_head
