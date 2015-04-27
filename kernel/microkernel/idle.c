a1ac0d68271c0be7133d8f695a14b6f6e3206842
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-27 15:14:31 -0700

    Renaming _WlLoop to _workload_loop
    Updating micro kernel functions to follow a consistent naming
    convention.  Part of that process is the removal of camelCase
    naming conventions for the preferred_underscore_method.
    
    Change accomplished with the following script:
    
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: I26965c7640b6caeb73ac11b4f6d5fa793a0be65b
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 490c702343cd3b36af88b902244dc1d17693d835

--- kernel/microkernel/idle.c
+++ kernel/microkernel/idle.c
@@ -3,7 +3,6 @@ DECL|K_workload|function|void K_workload(struct k_args *P)
 DECL|MSEC_PER_SEC|macro|MSEC_PER_SEC
 DECL|_GetNextTimerExpiry|function|static inline int32_t _GetNextTimerExpiry(void)
 DECL|_PowerSave|function|static void _PowerSave(void)
-DECL|_WlLoop|function|static void _WlLoop(void)
 DECL|_k_workload_delta|variable|_k_workload_delta
 DECL|_k_workload_end_time|variable|_k_workload_end_time
 DECL|_k_workload_i0|variable|_k_workload_i0
@@ -16,6 +15,7 @@ DECL|_k_workload_start_time|variable|_k_workload_start_time
 DECL|_k_workload_t0|variable|_k_workload_t0
 DECL|_k_workload_t1|variable|_k_workload_t1
 DECL|_k_workload_ticks|variable|_k_workload_ticks
+DECL|_workload_loop|function|static void _workload_loop(void)
 DECL|kernel_idle|function|int kernel_idle(void)
 DECL|task_node_workload_get|function|int task_node_workload_get(void)
 DECL|wlMonitorCalibrate|function|void wlMonitorCalibrate(void)
