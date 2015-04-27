27960f6fc5285963d590d11f8e02ad323741ef8d
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:17 -0500

    Rename _minik_args to _k_server_command_stack_storage
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I84449466d14457b4d83657280124554bdca63054
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: b07ba0e433919521374adbafb7ec25b654949cee

--- kernel/microkernel/core/global.c
+++ kernel/microkernel/core/global.c
@@ -1,5 +1,5 @@
 DECL|K_Args|variable|K_Args
 DECL|K_DebugHalt|variable|K_DebugHalt
 DECL|K_PrioBitMap|variable|K_PrioBitMap
-DECL|_minik_args|variable|_minik_args
+DECL|_k_server_command_stack_storage|variable|_k_server_command_stack_storage
 DECL|kernel_init|function|void kernel_init(void)
