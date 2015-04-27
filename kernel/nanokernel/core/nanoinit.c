be81ad7f917dceebbeed0f4bd3c0af7961a7558c
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:19 -0500

    Rename _InterruptStack to _interrupt_stack
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I36709c9833279f05a186a5c00b996d242c843d4c
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: e7c1f2c20b5d8890c10f73ed2e806b6581e38c40

--- kernel/nanokernel/core/nanoinit.c
+++ kernel/nanokernel/core/nanoinit.c
@@ -1,3 +1,3 @@
-DECL|_InterruptStack|variable|_InterruptStack
+DECL|_interrupt_stack|variable|_interrupt_stack
 DECL|_nano_init|function|void _nano_init(tCCS *dummyOutContext, int argc, char *argv[], char *envp[])
 DECL|mainStack|variable|mainStack
