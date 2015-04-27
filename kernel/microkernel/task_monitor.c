26b1ce140d080eac4decc8de645b869d674a2187
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-04-27 10:28:38 -0500

    Rename K_monitor_wptr to k_monitor_wptr
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I3e2dec591ac41a01b61bb1822cc22f17c0cbc1f9
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: ef3a395c4945d60ba11d0b8b07feacc98b39bfe7

--- kernel/microkernel/task_monitor.c
+++ kernel/microkernel/task_monitor.c
@@ -4,8 +4,8 @@ DECL|K_monitor_nrec|variable|K_monitor_nrec
 DECL|K_monitor_read|function|void K_monitor_read(struct k_args *A)
 DECL|K_monitor_task|function|void K_monitor_task(struct k_proc *X, uint32_t D)
 DECL|K_monitor_wind|variable|K_monitor_wind
-DECL|K_monitor_wptr|variable|K_monitor_wptr
 DECL|TaskSwitchCallBack|variable|TaskSwitchCallBack
 DECL|_k_monitor_mask|variable|_k_monitor_mask
 DECL|k_monitor_buff|variable|k_monitor_buff
 DECL|k_monitor_capacity|variable|k_monitor_capacity
+DECL|k_monitor_wptr|variable|k_monitor_wptr
