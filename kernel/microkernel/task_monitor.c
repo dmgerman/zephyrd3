33b1c7e470c2a743776ddce4ae72bc8c67e32d2b
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:51 -0700

    Renaming K_monitor_args to _k_task_montior_args
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: I369f7dc7d78451d47e2c0788bd7fcd517298dc4a
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: b4bf63d1ff42f869c3177215905be407963f1e63

--- kernel/microkernel/task_monitor.c
+++ kernel/microkernel/task_monitor.c
@@ -1,8 +1,8 @@
 DECL|KS_TaskSetSwitchCallBack|function|void KS_TaskSetSwitchCallBack(taskswitchcallbackfunc func)
-DECL|K_monitor_args|function|void K_monitor_args(struct k_args *A)
 DECL|K_monitor_read|function|void K_monitor_read(struct k_args *A)
 DECL|K_monitor_wind|variable|K_monitor_wind
 DECL|_k_monitor_mask|variable|_k_monitor_mask
+DECL|_k_task_monitor_args|function|void _k_task_monitor_args(struct k_args *A)
 DECL|_k_task_monitor|function|void _k_task_monitor(struct k_proc *X, uint32_t D)
 DECL|_k_task_switch_callback|variable|_k_task_switch_callback
 DECL|k_monitor_buff|variable|k_monitor_buff
