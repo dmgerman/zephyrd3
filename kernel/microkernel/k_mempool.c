be857391b372f7e760905166ccb2eb0a49485813
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:50 -0700

    Renaming K_GetBlock _k_mem_pool_block_get
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: Ibbca87c801157ef48996e86f7b4b3a87eab5628a
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: b3b8e72dab3f2be6bebcaf5940c25865202e5090

--- kernel/microkernel/k_mempool.c
+++ kernel/microkernel/k_mempool.c
@@ -2,11 +2,11 @@ DECL|AD_AFTER_SEARCH4BIGGERBLOCK|macro|AD_AFTER_SEARCH4BIGGERBLOCK
 DECL|AD_BEFORE_SEARCH4BIGGERBLOCK|macro|AD_BEFORE_SEARCH4BIGGERBLOCK
 DECL|AD_NONE|macro|AD_NONE
 DECL|AUTODEFRAG|macro|AUTODEFRAG
-DECL|K_GetBlock|function|void K_GetBlock(struct k_args *A)
 DECL|K_RelBlock|function|void K_RelBlock(struct k_args *A)
-DECL|_k_block_get_timeout_handle|function|void _k_block_get_timeout_handle(struct k_args *A)
 DECL|_k_block_waiters_get|function|void _k_block_waiters_get(struct k_args *A)
 DECL|_k_defrag|function|void _k_defrag(struct k_args *A)
+DECL|_k_mem_pool_block_get_timeout_handle|function|void _k_mem_pool_block_get_timeout_handle(struct k_args *A)
+DECL|_k_mem_pool_block_get|function|void _k_mem_pool_block_get(struct k_args *A)
 DECL|_mem_pools_init|function|void _mem_pools_init(void)
 DECL|_task_mem_pool_alloc|function|int _task_mem_pool_alloc(struct k_block *blockptr, /* ptr to requested block */ kmemory_pool_t poolid, /* pool from which to get block */ int reqsize, /* requested block size */ int32_t time /* maximum number of ticks to wait */ )
 DECL|defrag|function|static void defrag(struct pool_struct *P, int ifraglevel_start, int ifraglevel_stop)
