7100c324f24910f9d1a4722a75896d26f7c0701f
Johan Hedberg <johan.hedberg@intel.com>
2015-04-28 10:44:37 +0300

    Bluetooth: Add skeleton for Attribute Protocol handling
    This patch adds support for the ATT fixed channel. A dedicated att.c
    file is added to handle the ATT protocol handling.
    
    Change-Id: I2e481f56168b6f1b4c564f10d25dee0a85fb968f
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 33c50f332b4211a333236f839df076986ec74de0

---
+++ net/bluetooth/att.c
@@ -0,0 +1,2 @@
+DECL|bt_att_recv|function|void bt_att_recv(struct bt_conn *conn, struct bt_buf *buf)
+DECL|send_err_rsp|function|static void send_err_rsp(struct bt_conn *conn, uint8_t req, uint16_t handle, uint8_t err)
---
+++ net/bluetooth/att.h
@@ -0,0 +1,18 @@
+DECL|BT_ATT_ERROR_RSP|macro|BT_ATT_ERROR_RSP
+DECL|BT_ATT_ERR_NOT_SUPPORTED|macro|BT_ATT_ERR_NOT_SUPPORTED
+DECL|BT_L2CAP_ATT_EXCHANGE_MTU_REQ|macro|BT_L2CAP_ATT_EXCHANGE_MTU_REQ
+DECL|BT_L2CAP_ATT_EXCHANGE_MTU_RSP|macro|BT_L2CAP_ATT_EXCHANGE_MTU_RSP
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|bt_att_error_rsp|struct|struct bt_att_error_rsp {
+DECL|bt_att_exchange_mtu_req|struct|struct bt_att_exchange_mtu_req {
+DECL|bt_att_exchange_mtu_rsp|struct|struct bt_att_exchange_mtu_rsp {
+DECL|bt_att_hdr|struct|struct bt_att_hdr {
+DECL|code|member|uint8_t code;
+DECL|error|member|uint8_t error;
+DECL|handle|member|uint16_t handle;
+DECL|mtu|member|uint16_t mtu;
+DECL|mtu|member|uint16_t mtu;
+DECL|request|member|uint8_t request;
