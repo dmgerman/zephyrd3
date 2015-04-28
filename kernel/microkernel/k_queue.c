181c954e9b6228d53ed2a52500b752e7ccc411f4
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:50 -0700

    Renaming K_enqrpl to _k_fifo_enque_reply
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: I3d006b76386a4ac8fc82192bf83c028bc66455ed
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: e0d338a7e93a6f4f4e3493a82b937c59f03b4174

--- kernel/microkernel/k_queue.c
+++ kernel/microkernel/k_queue.c
@@ -1,8 +1,8 @@
 DECL|K_deqreq|function|void K_deqreq(struct k_args *A)
 DECL|K_deqrpl|function|void K_deqrpl(struct k_args *A)
 DECL|K_enqreq|function|void K_enqreq(struct k_args *A)
-DECL|K_enqrpl|function|void K_enqrpl(struct k_args *A)
 DECL|K_queue|function|void K_queue(struct k_args *A)
+DECL|_k_fifo_enque_reply|function|void _k_fifo_enque_reply(struct k_args *A)
 DECL|_task_fifo_get|function|int _task_fifo_get(kfifo_t queue, /* FIFO queue */void *data, /* where to store FIFO entry */ int32_t time /* maximum number of ticks to wait */ )
 DECL|_task_fifo_ioctl|function|int _task_fifo_ioctl(kfifo_t queue, /* FIFO queue */ int op/* 0: status query
 DECL|_task_fifo_put|function|int _task_fifo_put(kfifo_t queue, /* FIFO queue */void *data, /* ptr to data to add to queue */ int32_t time /* maximum number of ticks to wait */ )
