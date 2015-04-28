7c3e650d18230578c9ee25fba195a8649f5dc341
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-04-28 16:44:56 +0300

    net: Add preliminary UDP support
    Use the Contiki simple UDP API when sending and receiving the
    application data.
    
    Change-Id: I39a379f30c8f7febbea223fcf8f8702faf82e39e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 0344c11ad7978170ad795ebc6938fffab34e385f

--- net/ip/net_init.c
+++ net/ip/net_init.c
@@ -1,12 +1,15 @@
 DECL|DEBUG|macro|DEBUG
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|check_and_send_packet|function|static int check_and_send_packet(struct net_buf *buf)
 DECL|drv|member|struct net_driver *drv;
 DECL|init_rx_queue|function|static void init_rx_queue(void)
 DECL|init_tx_queue|function|static void init_tx_queue(void)
 DECL|net_dev|struct|static struct net_dev {
 DECL|net_init|function|int net_init(void)
+DECL|net_receive|function|struct net_buf *net_receive(struct net_context *context)
 DECL|net_register_driver|function|int net_register_driver(struct net_driver *drv)
 DECL|net_rx_fiber|function|static void net_rx_fiber(void)
+DECL|net_send|function|int net_send(struct net_buf *buf)
 DECL|net_set_mac|function|int net_set_mac(uint8_t *mac, uint8_t len)
 DECL|net_tx_fiber|function|static void net_tx_fiber(void)
 DECL|net_unregister_driver|function|void net_unregister_driver(struct net_driver *drv)
@@ -16,3 +19,4 @@ DECL|rx_fiber_stack|variable|rx_fiber_stack
 DECL|rx_queue|member|struct nano_fifo rx_queue;
 DECL|tx_fiber_stack|variable|tx_fiber_stack
 DECL|tx_queue|member|struct nano_fifo tx_queue;
+DECL|udp_packet_recv|function|static void udp_packet_recv(struct simple_udp_connection *c, const uip_ipaddr_t *source_addr, uint16_t source_port, const uip_ipaddr_t *dest_addr, uint16_t dest_port,
