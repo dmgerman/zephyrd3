8c1393251ed1819c57dea717ea92ea0bc3d6f31d
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:50 -0700

    Renaming InitPools to _mem_pools_init
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    find . -type f \( -iname \*.c -o -iname \*.h -o -iname \*.s -o -iname \*.kconf \) \
           -not \( -path host/src/genIdt -prune \) \   \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: I9f3fc202c580d158a18737ff0f16a25952902ae4
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: a536a186932e226be4e96391342cc3d0a25910a2

--- kernel/microkernel/k_mempool.c
+++ kernel/microkernel/k_mempool.c
@@ -2,12 +2,12 @@ DECL|AD_AFTER_SEARCH4BIGGERBLOCK|macro|AD_AFTER_SEARCH4BIGGERBLOCK
 DECL|AD_BEFORE_SEARCH4BIGGERBLOCK|macro|AD_BEFORE_SEARCH4BIGGERBLOCK
 DECL|AD_NONE|macro|AD_NONE
 DECL|AUTODEFRAG|macro|AUTODEFRAG
-DECL|InitPools|function|void InitPools(void)
 DECL|K_Defrag|function|void K_Defrag(struct k_args *A)
 DECL|K_GetBlock_Waiters|function|void K_GetBlock_Waiters(struct k_args *A)
 DECL|K_GetBlock|function|void K_GetBlock(struct k_args *A)
 DECL|K_RelBlock|function|void K_RelBlock(struct k_args *A)
 DECL|K_gtbltmo|function|void K_gtbltmo(struct k_args *A)
+DECL|_mem_pools_init|function|void _mem_pools_init(void)
 DECL|_task_mem_pool_alloc|function|int _task_mem_pool_alloc(struct k_block *blockptr, /* ptr to requested block */ kmemory_pool_t poolid, /* pool from which to get block */ int reqsize, /* requested block size */ int32_t time /* maximum number of ticks to wait */ )
 DECL|defrag|function|static void defrag(struct pool_struct *P, int ifraglevel_start, int ifraglevel_stop)
 DECL|rgetblock|function|static char *rgetblock(struct pool_struct *P, int index, int startindex)
