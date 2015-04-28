3fa957aa9e2d05e97118fc15466078d71f8ca090
Johan Hedberg <johan.hedberg@intel.com>
2015-04-28 21:58:52 +0300

    Bluetooth: Add BT_WARN convenience logging macro
    Some logs indicate potentially problematic situations but are not
    necessarily errors. Since we do want to log these regardless of the
    debug setting it's better to have a separate BT_WARN macro for them.
    
    Change-Id: Iad13fc4b58329113458c97b02855033836618f5b
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 50a25dcfd3f93c9de46fa15ab4bd5402382f3a36

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,6 +1,7 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_ERR|macro|BT_ERR
 DECL|BT_INFO|macro|BT_INFO
+DECL|BT_WARN|macro|BT_WARN
 DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
 DECL|bt_driver|struct|struct bt_driver {
 DECL|head_reserve|member|size_t head_reserve;
