c57b4b50fc289cd43caf17a6dae42025a76de8fc
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:52 -0700

    Renaming K_offload to _k_offload_to_fiber
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: I0e29bab5dcb7e00a394304a23e4b91e3e7b4cf75
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 24ad23d7fb97efff028aa898365b2dceef08e432

--- kernel/microkernel/k_offload.c
+++ kernel/microkernel/k_offload.c
@@ -1,2 +1,2 @@
-DECL|K_offload|function|void K_offload(struct k_args *A)
+DECL|_k_offload_to_fiber|function|void _k_offload_to_fiber(struct k_args *A)
 DECL|task_offload_to_fiber|function|int task_offload_to_fiber(int (*func)(), void *argp)
