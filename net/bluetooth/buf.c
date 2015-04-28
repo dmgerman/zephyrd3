cba5f1959a732138842aad731bf3a967d1a8ddf3
Johan Hedberg <johan.hedberg@intel.com>
2015-04-28 11:19:26 +0300

    Bluetooth: Consolidate bt_buf_get* functions
    We may soon want to have a _wait() variant of bt_buf_get, so to avoid
    the number of 'get' function growing too large consolidate the
    existing get() and get_reserve() functions into a single one. The new
    consolidated function also takes the type as input parameter so that
    we know this from the very start and thereby plan for the split into
    multiple buffer pools.
    
    Change-Id: Ia09448565349def2be9bc08d9510fedd029480b4
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: d829fe9755020a8664364cff5890756a1735aa7c

--- net/bluetooth/buf.c
+++ net/bluetooth/buf.c
@@ -1,7 +1,6 @@
 DECL|NUM_BUFS|macro|NUM_BUFS
 DECL|bt_buf_add|function|uint8_t *bt_buf_add(struct bt_buf *buf, size_t len)
-DECL|bt_buf_get_reserve|function|struct bt_buf *bt_buf_get_reserve(size_t reserve_head)
-DECL|bt_buf_get|function|struct bt_buf *bt_buf_get(void)
+DECL|bt_buf_get|function|struct bt_buf *bt_buf_get(enum bt_buf_type type, size_t reserve_head)
 DECL|bt_buf_headroom|function|size_t bt_buf_headroom(struct bt_buf *buf)
 DECL|bt_buf_init|function|void bt_buf_init(void)
 DECL|bt_buf_pull|function|uint8_t *bt_buf_pull(struct bt_buf *buf, size_t len)
