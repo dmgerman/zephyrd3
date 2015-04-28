1759f66d3ec9e581757ccc3845455eae97218c53
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:51 -0700

    Renaming K_unlock to _k_mutex_unlock
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: Iec1871232b52bcc39bfc37d8f02bb4ce74f98ad3
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: fee5db12598111a14e125fb5523a986bebe5c8a7

--- kernel/microkernel/k_mutex.c
+++ kernel/microkernel/k_mutex.c
@@ -1,5 +1,5 @@
-DECL|K_unlock|function|void K_unlock(struct k_args *A /* pointer to mutex unlock request arguments */ )
 DECL|_k_mutex_lock_reply|function|void _k_mutex_lock_reply(struct k_args *A /* pointer to mutex lock reply request arguments */ )
 DECL|_k_mutex_lock_request|function|void _k_mutex_lock_request(struct k_args *A /* pointer to mutex lock request arguments */ )
+DECL|_k_mutex_unlock|function|void _k_mutex_unlock(struct k_args *A /* pointer to mutex unlock request arguments */ )
 DECL|_task_mutex_lock|function|int _task_mutex_lock(kmutex_t mutex, /* mutex to lock */ int32_t time /* max # of ticks to wait for mutex */ )
 DECL|_task_mutex_unlock|function|void _task_mutex_unlock(kmutex_t mutex /* mutex to unlock */ )
