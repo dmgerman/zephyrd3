eeb0bb84c7df62fd770be18867e0d78fb1090fec
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-04-28 11:35:52 -0700

    Renaming K_mvdreq to _k_movedata_request
    Updating micro kernel functions to follow a consistent naming convention.
    Part of that process is the removal of camelCase naming conventions for the
    preferred_underscore_method.
    
    Change accomplished with the following script:
    
    #!/bin/bash
    echo "Searching for ${1} to replace with ${2}"
    
    echo "Checking C, CPP, H, HPP, and s files..."
    find . -type f \( -iname \*.[ch] -o -iname \*.[ch]pp -o -iname \*.s \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    echo "Checking KCONF, LST, and PY files..."
    find . -type f \( -iname \*.lst -o -iname \*.kconf -o -iname \*.py \) \
           -not \( -path host/src/genIdt -prune \) \
           -not \( -path host/src/gen_tables -prune \) \
           -print | xargs sed -i "s/"${1}"/"${2}"/g"
    
    Change-Id: I32e0105c90ef1a7f7326dd43c39c0eb85e173ee3
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: a6f9d1a7b8763f56dec3a1eabc7b5e9406812e76

--- kernel/microkernel/movedata/movedata.c
+++ kernel/microkernel/movedata/movedata.c
@@ -1,3 +1,3 @@
-DECL|K_mvdreq|function|void K_mvdreq(struct k_args *Req)
+DECL|_k_movedata_request|function|void _k_movedata_request(struct k_args *Req)
 DECL|mvdreq_copy|function|static void mvdreq_copy(struct moved_req *ReqArgs)
 DECL|mvdreq_docont|function|static void mvdreq_docont(struct k_args *Cont)
