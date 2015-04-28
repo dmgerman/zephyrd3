6c4ff5ee2d400f83e571f612957ed7ca7220b11e
Johan Hedberg <johan.hedberg@intel.com>
2015-04-28 10:43:14 +0300

    Bluetooth: Add basic L2CAP support
    This patch adds basic L2CAP functions, including support for the LE
    signaling channel as well as performing a connection parameter update
    in slave (peripheral) role.
    
    Change-Id: Ie1f9751475adcf26576e9587bf6d68d15d6a2569
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: c31237aa82d4996e9d39c86a1d1ab0344ac14617

--- net/bluetooth/l2cap.h
+++ net/bluetooth/l2cap.h
@@ -1,11 +1,30 @@
 DECL|BT_L2CAP_CID_ATT|macro|BT_L2CAP_CID_ATT
 DECL|BT_L2CAP_CID_LE_SIG|macro|BT_L2CAP_CID_LE_SIG
+DECL|BT_L2CAP_CMD_REJECT|macro|BT_L2CAP_CMD_REJECT
+DECL|BT_L2CAP_CONN_PARAM_REQ|macro|BT_L2CAP_CONN_PARAM_REQ
+DECL|BT_L2CAP_CONN_PARAM_RSP|macro|BT_L2CAP_CONN_PARAM_RSP
+DECL|BT_L2CAP_REJ_INVALID_CID|macro|BT_L2CAP_REJ_INVALID_CID
+DECL|BT_L2CAP_REJ_MTU_EXCEEDED|macro|BT_L2CAP_REJ_MTU_EXCEEDED
+DECL|BT_L2CAP_REJ_NOT_UNDERSTOOD|macro|BT_L2CAP_REJ_NOT_UNDERSTOOD
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|bt_l2cap_cmd_reject|struct|struct bt_l2cap_cmd_reject {
+DECL|bt_l2cap_conn_param_req|struct|struct bt_l2cap_conn_param_req {
+DECL|bt_l2cap_conn_param_rsp|struct|struct bt_l2cap_conn_param_rsp {
 DECL|bt_l2cap_hdr|struct|struct bt_l2cap_hdr {
 DECL|bt_l2cap_sig_hdr|struct|struct bt_l2cap_sig_hdr {
 DECL|cid|member|uint16_t cid;
 DECL|code|member|uint8_t code;
+DECL|data|member|uint8_t data[0];
 DECL|ident|member|uint8_t ident;
+DECL|latency|member|uint16_t latency;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
+DECL|max_interval|member|uint16_t max_interval;
+DECL|min_interval|member|uint16_t min_interval;
+DECL|reason|member|uint16_t reason;
+DECL|result|member|uint16_t result;
+DECL|timeout|member|uint16_t timeout;
