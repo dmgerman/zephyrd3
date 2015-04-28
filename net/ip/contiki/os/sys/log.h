dd3c8a92f1cedc85a5756cf020f1e8b92baa015f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-04-28 16:23:43 +0300

    net: Enable logging in uIP stack
    Allow the uIP stack to print information about its internal
    status. By default the logging is disabled.
    
    Change-Id: I55a03051d3f541b5f6ea950fdd6406814f937197
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7d7d4e41615bc1ab6397e96c3fb73301799522dc

---
+++ net/ip/contiki/os/sys/log.h
@@ -0,0 +1,2 @@
+DECL|LOG_H_|macro|LOG_H_
+DECL|log_message|macro|log_message
---
+++ net/ip/contiki/uip-log.c
@@ -0,0 +1,4 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|log_message|function|void log_message(const char *part1, const char *part2)
+DECL|uip_log|function|void uip_log(char *message)
