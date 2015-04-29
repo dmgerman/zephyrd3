021aa6e2cc9f772ecf913ac7d502eb157ffe4191
Johan Hedberg <johan.hedberg@intel.com>
2015-04-29 13:36:41 +0300

    Bluetooth: Fix UART driver robustness when out of buffers
    If we're out of buffers the UART driver would previously loose sync of
    where the packet boundaries go, because it was using the buffer itself
    to store and parse the HCI packet headers. This patch modifies the
    UART driver to use stack variable for initial header parsing, thereby
    being able to always determine how many remaining bytes there are and
    cleanly discard the bytes until the beginning of the next packet.
    
    Change-Id: I278f3fc0c983e6a2a6904356ef1af1d25c9f06e4
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 935198e7154733e2698376b5acd196b32b26da5a

--- drivers/bluetooth/uart.c
+++ drivers/bluetooth/uart.c
@@ -4,8 +4,9 @@ DECL|H4_EVT|macro|H4_EVT
 DECL|H4_HEADER_SIZE|macro|H4_HEADER_SIZE
 DECL|H4_SCO|macro|H4_SCO
 DECL|UART|macro|UART
-DECL|bt_uart_acl_recv|function|static int bt_uart_acl_recv(struct bt_buf *buf)
-DECL|bt_uart_evt_recv|function|static int bt_uart_evt_recv(struct bt_buf *buf)
+DECL|bt_uart_acl_recv|function|static struct bt_buf *bt_uart_acl_recv(int *remaining)
+DECL|bt_uart_discard|function|static size_t bt_uart_discard(int uart, size_t len)
+DECL|bt_uart_evt_recv|function|static struct bt_buf *bt_uart_evt_recv(int *remaining)
 DECL|bt_uart_init|function|void bt_uart_init(void)
 DECL|bt_uart_isr|function|void bt_uart_isr(void *unused)
 DECL|bt_uart_open|function|static int bt_uart_open()
