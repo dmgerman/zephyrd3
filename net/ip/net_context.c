10d676b7858d190a417bb7ebe6d9ee83fd4e2ef2
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-05-04 14:54:55 +0300

    net: context: Add helper function for checking socket registration
    We need to know if we have registered a socket listener, otherwise
    we try to register UDP handler too many times.
    
    Change-Id: I060150d01eb149f2a2a6a332d18e0142e5cb7a5e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 844a888e99d276d7cc9082782cce7734e3fd328e

--- net/ip/net_context.c
+++ net/ip/net_context.c
@@ -3,12 +3,15 @@ DECL|context_sem_give|function|static void context_sem_give(struct nano_sem *cha
 DECL|contexts_lock|variable|contexts_lock
 DECL|contexts|variable|contexts
 DECL|net_context_get_queue|function|struct nano_fifo *net_context_get_queue(struct net_context *context)
+DECL|net_context_get_receiver_registered|function|int net_context_get_receiver_registered(struct net_context *context)
 DECL|net_context_get_tuple|function|struct net_tuple *net_context_get_tuple(struct net_context *context)
 DECL|net_context_get_udp_connection|function|net_context_get_udp_connection(struct net_context *context)
 DECL|net_context_get|function|struct net_context *net_context_get(enum ip_protocol ip_proto,const struct net_addr *remote_addr, uint16_t remote_port, const struct net_addr *local_addr, uint16_t local_port)
 DECL|net_context_init|function|void net_context_init(void)
 DECL|net_context_put|function|void net_context_put(struct net_context *context)
+DECL|net_context_set_receiver_registered|function|void net_context_set_receiver_registered(struct net_context *context)
 DECL|net_context|struct|struct net_context {
+DECL|receiver_registered|member|bool receiver_registered;
 DECL|rx_queue|member|struct nano_fifo rx_queue;
 DECL|tuple|member|struct net_tuple tuple;
 DECL|udp|member|struct simple_udp_connection udp;
