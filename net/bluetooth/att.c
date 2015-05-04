b866b635f4b04dcb235c1a3a5592d58ff8407464
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-04 17:22:10 +0300

    Bluetooth: Add code to handle BT_ATT_OP_MTU_REQ
    Add code to respond to BT_ATT_OP_MTU_REQ with BT_ATT_OP_MTU_RSP
    selecting the MTU based on the amount of room bt_buf has:
    
    < ACL Data RX: Handle 39 flags 0x02 dlen 7
          ATT: Exchange MTU Response (0x03) len 2
            Server RX MTU: 65
    
    Change-Id: I47b8179fcee0d6adff5761abc37cfcb1ed636d65
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 028edd59baf6d74462ade1775ad93243452f85d3

--- net/bluetooth/att.c
+++ net/bluetooth/att.c
@@ -1,3 +1,4 @@
+DECL|att_mtu_req|function|static void att_mtu_req(struct bt_conn *conn, struct bt_buf *data)
 DECL|bt_att_create_pdu|function|struct bt_buf *bt_att_create_pdu(struct bt_conn *conn, uint8_t op, size_t len)
 DECL|bt_att_recv|function|void bt_att_recv(struct bt_conn *conn, struct bt_buf *buf)
 DECL|send_err_rsp|function|static void send_err_rsp(struct bt_conn *conn, uint8_t req, uint16_t handle, uint8_t err)
