752205957aa6ed36f2d9039ee8d44c61ffdf50a4
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-05-04 16:54:41 -0400

    nano_stack: clean up code style and comments
    Change-Id: If4518b3883eb0950398dd9bcd4b850154d325ac3
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: a24d2a5a9d344abd7964419412d06fd7b1b8459d

--- kernel/nanokernel/nano_stack.c
+++ kernel/nanokernel/nano_stack.c
@@ -1,6 +1,6 @@
-DECL|_stack_pop|function|int _stack_pop(struct nano_stack *chan, /* channel on which to interact */ uint32_t *pData /* container for data to pop */ )
+DECL|_stack_pop|function|int _stack_pop(struct nano_stack *chan,/* channel on which to interact */ uint32_t *pData/* container for data to pop */ )
 DECL|_stack_push|function|void _stack_push(struct nano_stack *chan, /* stack channel on which to interact */ uint32_t data /* data to push on stack */ )
 DECL|nano_fiber_stack_pop_wait|function|uint32_t nano_fiber_stack_pop_wait(struct nano_stack *chan /* channel on which to interact */ )
-DECL|nano_stack_init|function|void nano_stack_init(struct nano_stack *chan, /* stack to initialize */ uint32_t *data /* container for stack */ )
+DECL|nano_stack_init|function|void nano_stack_init(struct nano_stack *chan,/* stack to initialize */ uint32_t *data/* container for stack */ )
 DECL|nano_task_stack_pop_wait|function|uint32_t nano_task_stack_pop_wait(struct nano_stack *chan /* channel on which to interact */ )
 DECL|nano_task_stack_push|function|void nano_task_stack_push(struct nano_stack *chan, /* stack channel on which to interact */ uint32_t data /* data to push on stack */ )
