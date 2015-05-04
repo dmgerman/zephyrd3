6841baa02d9809c4d48203e306300224dc501be0
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-05-04 13:21:06 -0700

    Fixing the test_libs functions
    After the tree re-structuring, several of the files that the test_libs
    functions use are no longer valid, from headers to functions.  Updating
    to run on the current lay of the land.
    
    Change-Id: I3359a782f5ae8faa7909a13a4cca47f82c22797f
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: d6f5fa36ae0b23630413283cc1190938257e3942

--- samples/microkernel/test/test_libs/src/libraries.c
+++ samples/microkernel/test/test_libs/src/libraries.c
@@ -1,15 +1,10 @@
 DECL|BUFSIZE|macro|BUFSIZE
 DECL|RegressionTask|function|int RegressionTask(void)
 DECL|buffer|variable|buffer
-DECL|ctypeTest|function|int ctypeTest(void)
-DECL|d|variable|d
-DECL|inttypesTest|function|int inttypesTest(void)
-DECL|iso646Test|function|int iso646Test(void)
 DECL|limitsTest|function|int limitsTest(void)
 DECL|longMax|variable|longMax
 DECL|longOne|variable|longOne
 DECL|longVariable|variable|longVariable
-DECL|n|variable|n
 DECL|sizeOfLongVariable|variable|sizeOfLongVariable
 DECL|stdboolTest|function|int stdboolTest(void)
 DECL|stddefTest|function|int stddefTest(void)
@@ -17,5 +12,3 @@ DECL|stdintTest|function|int stdintTest(void)
 DECL|stringTest|function|int stringTest(void)
 DECL|unsignedByte|variable|unsignedByte
 DECL|unsignedInt|variable|unsignedInt
-DECL|x|variable|x
-DECL|y|variable|y
