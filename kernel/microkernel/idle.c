71e8d654561e662418d747dd3703c49dee5f0300
Allan Stephens <allan.stephens@windriver.com>
2015-05-04 11:29:44 -0400

    Renaming task_node_workload_get to task_workload_get
    Eliminates unnecessary "node_" portion, as there is only a single
    source for which workload information is collected.
    
    Change-Id: Id7c03e2149d1b7979a6921e99e1db0288da09c0c
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: c535392d8e3f9bd1f70c3b6535edaebcdcc4a630

--- kernel/microkernel/idle.c
+++ kernel/microkernel/idle.c
@@ -17,6 +17,6 @@ DECL|_k_workload_ticks|variable|_k_workload_ticks
 DECL|_power_save|function|static void _power_save(void)
 DECL|_workload_loop|function|static void _workload_loop(void)
 DECL|kernel_idle|function|int kernel_idle(void)
-DECL|task_node_workload_get|function|int task_node_workload_get(void)
+DECL|task_workload_get|function|int task_workload_get(void)
 DECL|workload_monitor_calibrate|function|void workload_monitor_calibrate(void)
 DECL|workload_time_slice_set|function|void workload_time_slice_set(int32_t t)
