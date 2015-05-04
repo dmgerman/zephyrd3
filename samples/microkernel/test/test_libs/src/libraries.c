fb8a43458c8071fc8a293904d04919aa923e79af
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-05-04 13:21:06 -0700

    Adding in test for strcmp
    Change-Id: I07c8a8477fb7bca0005744376bea6c21dd8f0c7f
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: a437761843bab7cf943eb0122f356c05450fc506

--- samples/microkernel/test/test_libs/src/libraries.c
+++ samples/microkernel/test/test_libs/src/libraries.c
@@ -10,6 +10,7 @@ DECL|sizeOfLongVariable|variable|sizeOfLongVariable
 DECL|stdboolTest|function|int stdboolTest(void)
 DECL|stddefTest|function|int stddefTest(void)
 DECL|stdintTest|function|int stdintTest(void)
+DECL|strcmp_test|function|int strcmp_test (void)
 DECL|stringTest|function|int stringTest(void)
 DECL|strlen_test|function|int strlen_test(void)
 DECL|unsignedByte|variable|unsignedByte
