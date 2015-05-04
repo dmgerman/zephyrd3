c071dc25729f5c28a46f86c0ad0544df96da2341
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-05-04 14:43:36 +0300

    console: Refactor serial console interface
    Make serial console interface more robust. Enables interrupts only
    when registering input, all memory is managed inside application.
    Interface to application is changed to have two fifo queues. One
    queue is a free line slots and another queue is keeping entered
    lines. This way memory for lines is managed inside application which
    provides free lines queue. It is also simpler to manage entered
    lines by sleeping on fifo_get on app layer.
    
    Change-Id: I4776c03eddd1e7d880df3b902bd48f5f2c901cad
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 879541a9189129256e5e838d72bcb1efc9109aec

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -1,15 +1,14 @@
-DECL|MAX_LINE_LENGTH|macro|MAX_LINE_LENGTH
 DECL|UART|macro|UART
 DECL|__printk_hook_install|macro|__printk_hook_install
 DECL|__stdout_hook_install|macro|__stdout_hook_install
+DECL|avail_queue|variable|avail_queue
 DECL|consoleOut|function|static int consoleOut(int c /* character to output */)
-DECL|console_handler_init|function|static void console_handler_init(void)
-DECL|console_handler_init|macro|console_handler_init
-DECL|handler|variable|handler
-DECL|rcv_data|variable|rcv_data
-DECL|rcv_pos|variable|rcv_pos
+DECL|console_input_init|function|static void console_input_init(void)
+DECL|console_input_init|macro|console_input_init
+DECL|lines_queue|variable|lines_queue
+DECL|pos|variable|pos
 DECL|read_uart|function|static int read_uart(int uart, uint8_t *buf, unsigned int size)
 DECL|uart_console_init|function|void uart_console_init(void)
 DECL|uart_console_isr|function|void uart_console_isr(void *unused)
-DECL|uart_register_handler|function|void uart_register_handler(void (*cb) (const char *string))
-DECL|uart_register_handler|macro|uart_register_handler
+DECL|uart_register_input|function|void uart_register_input(struct nano_fifo *avail, struct nano_fifo *lines)
+DECL|uart_register_input|macro|uart_register_input
