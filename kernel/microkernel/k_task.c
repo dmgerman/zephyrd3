203c6b8bd6b4ac586833f91df184678209f57de2
Allan Stephens <allan.stephens@windriver.com>
2015-05-04 12:10:53 -0400

    Eliminate nanokernel/task sub-directory
    Relocates start_task() so that it appears in the main microkernel
    directory, alongside its complementary routine abort_task();
    this corrects a long-standing anomaly in which this microkernel-
    specific routine appeared in the nanokernel portion of the tree.
    
    With this move, the start_task.c file and its parent nanokernel/task
    sub-directory no longer serve any purpose and are removed. (Note that
    no changes are required for ARC architecture support, which does not
    support microkernel capabilities and doesn't use this sub-directory.)
    
    Change-Id: I973e1c32c9a8ddcacdc08159069ae7cdfea0f107
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 3c8fb4ac7a1ba3307a4aeca74ce8c816e8915d4d

--- kernel/microkernel/k_task.c
+++ kernel/microkernel/k_task.c
@@ -7,6 +7,7 @@ DECL|_task_ioctl|function|void _task_ioctl(ktask_t task, /* task on which to ope
 DECL|abort_task|function|void abort_task(struct k_proc *X)
 DECL|reset_state_bit|function|void reset_state_bit(struct k_proc *X, /* ptr to task */ uint32_t bits /* bitmask of TF_xxx bits to reset */ )
 DECL|set_state_bit|function|void set_state_bit(struct k_proc *task_ptr, uint32_t bits /* bitmask of TF_xxx bits to set */ )
+DECL|start_task|function|void start_task(struct k_proc *X, /* ptr to task control block */ void (*func)(void) /* entry point for task */ )
 DECL|task_abort_handler_set|function|void task_abort_handler_set(void (*func)(void) /* abort handler */ )
 DECL|task_entry_set|function|void task_entry_set(ktask_t task, /* task */ void (*func)(void) /* entry point */ )
 DECL|task_priority_set|function|void task_priority_set(ktask_t task, /* task whose priority is to be set */ kpriority_t prio /* new priority */ )
