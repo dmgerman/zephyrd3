43e6159261ad93055656d7184644d158737ecbdc
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-04 19:12:51 +0300

    Bluetooth: Add bt_att_create_pdu
    Add bt_att_create_pdu which works similarly to bt_l2cap_create_pdu
    eliminating some duplicated when generating ATT PDUs.
    
    Change-Id: I30b89effbae887f4032ba1d1e380a763c3556869
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 7cbb4e6f4b5ef10c060b8ad36254f28bd103315c

--- net/bluetooth/att.c
+++ net/bluetooth/att.c
@@ -1,2 +1,3 @@
+DECL|bt_att_create_pdu|function|struct bt_buf *bt_att_create_pdu(struct bt_conn *conn, uint8_t op, size_t len)
 DECL|bt_att_recv|function|void bt_att_recv(struct bt_conn *conn, struct bt_buf *buf)
 DECL|send_err_rsp|function|static void send_err_rsp(struct bt_conn *conn, uint8_t req, uint16_t handle, uint8_t err)
