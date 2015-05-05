bc70cb9f5ff46b0a833b8ff9d3e9818cef9d456a
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-05-05 18:44:21 -0400

    nano_fifo: abstract data enqueue/dequeue in functions
    Removes code duplication.
    
    Change-Id: I7c06f29fca5d81319a2ab945d4e391a3c9a73523
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: a221bfcb639079147fe953e8d251d8bf479feb68

--- kernel/nanokernel/nano_mwfifo.c
+++ kernel/nanokernel/nano_mwfifo.c
@@ -1,5 +1,7 @@
 DECL|_fifo_get|function|void *_fifo_get(struct nano_fifo *fifo /* fifo on which to interact */ )
 DECL|_fifo_put|function|void _fifo_put(struct nano_fifo *fifo,/* fifo on which to interact */ void *data/* data to send */ )
+DECL|dequeue_data|function|static inline void *dequeue_data(struct nano_fifo *fifo)
+DECL|enqueue_data|function|static inline void enqueue_data(struct nano_fifo *fifo, void *data)
 DECL|nano_fiber_fifo_get_wait|function|void *nano_fiber_fifo_get_wait(struct nano_fifo *fifo /* fifo on which to interact */ )
 DECL|nano_fifo_get_wait|function|void *nano_fifo_get_wait(struct nano_fifo *fifo)
 DECL|nano_fifo_init|function|void nano_fifo_init(struct nano_fifo *fifo /* fifo to initialize */ )
