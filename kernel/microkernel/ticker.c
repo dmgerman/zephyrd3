a687105a9917da59f2ba88a7d3ead98e7df9b7d7
Allan Stephens <allan.stephens@windriver.com>
2015-05-06 10:34:06 -0400

    Relocate misplaced system clock routines
    Moves routines that have nothing to do with microkernel timer objects
    from the time support file to the file containing system clock support.
    
    Change-Id: I7ab3e579877d689dc9487ca3330fe25e2e4afdd0
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: b33dc88eb5321523522180ec20e80855f2eee4dd

--- kernel/microkernel/ticker.c
+++ kernel/microkernel/ticker.c
@@ -8,6 +8,7 @@ DECL|_TlDebugUpdate|macro|_TlDebugUpdate
 DECL|_WlMonitorUpdate|function|static inline void _WlMonitorUpdate(void)
 DECL|_k_debug_sys_clock_tick_count|variable|_k_debug_sys_clock_tick_count
 DECL|_k_sys_clock_tick_count|variable|_k_sys_clock_tick_count
+DECL|_k_time_elapse|function|void _k_time_elapse(struct k_args *P)
 DECL|_sys_idle_elapsed_ticks|variable|_sys_idle_elapsed_ticks
 DECL|scheduler_time_slice_set|function|void scheduler_time_slice_set(int32_t t, /* time slice in ticks */kpriority_t p /* beginning priority level to which time slicing applies */ )
 DECL|slice_count|variable|slice_count
@@ -18,3 +19,7 @@ DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_increment|function|static void sys_clock_increment(int inc)
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
+DECL|task_cycle_get_32|function|uint32_t task_cycle_get_32(void)
+DECL|task_tick_delta|function|int32_t task_tick_delta(int64_t *reftime /* pointer to reference time */ )
+DECL|task_tick_get_32|function|int32_t task_tick_get_32(void)
+DECL|task_tick_get|function|int64_t task_tick_get(void)
