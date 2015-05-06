db041ecdbed5dac89e4f567517b8ba6bcd324547
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-06 15:58:46 +0300

    Bluetooth: Add ATT Read by Type Request skeleton
    This adds the defines for Read by Type Value PDUs along with initial
    code to parse them:
    
    > ACL Data RX: Handle 3585 flags 0x02 dlen 11
          ATT: Read By Type Request (0x08) len 6
            Handle range: 0x0001-0xffff
            Attribute type: Characteristic (0x2803)
    < ACL Data TX: Handle 3585 flags 0x00 dlen 9
          ATT: Error Response (0x01) len 4
            Read By Type Request (0x08)
            Handle: 0x0001
            Error: Attribute Not Found (0x0a)
    
    Change-Id: I0b4371b8decebf33710a949552a11c80c544b5f8
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 0d853d64c289f45a20666100da5688b7f3c6dbf7

--- net/bluetooth/att.h
+++ net/bluetooth/att.h
@@ -26,6 +26,10 @@ DECL|BT_ATT_OP_FIND_TYPE_REQ|macro|BT_ATT_OP_FIND_TYPE_REQ
 DECL|BT_ATT_OP_FIND_TYPE_RSP|macro|BT_ATT_OP_FIND_TYPE_RSP
 DECL|BT_ATT_OP_MTU_REQ|macro|BT_ATT_OP_MTU_REQ
 DECL|BT_ATT_OP_MTU_RSP|macro|BT_ATT_OP_MTU_RSP
+DECL|BT_ATT_OP_READ_TYPE_REQ|macro|BT_ATT_OP_READ_TYPE_REQ
+DECL|BT_ATT_OP_READ_TYPE_RSP|macro|BT_ATT_OP_READ_TYPE_RSP
+DECL|BT_UUID_128|macro|BT_UUID_128
+DECL|BT_UUID_16|macro|BT_UUID_16
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
@@ -37,6 +41,10 @@ DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|PACK_STRUCT|variable|PACK_STRUCT
+DECL|bt_att_data|struct|struct bt_att_data {
 DECL|bt_att_error_rsp|struct|struct bt_att_error_rsp {
 DECL|bt_att_exchange_mtu_req|struct|struct bt_att_exchange_mtu_req {
 DECL|bt_att_exchange_mtu_rsp|struct|struct bt_att_exchange_mtu_rsp {
@@ -48,7 +56,12 @@ DECL|bt_att_handle_group|struct|struct bt_att_handle_group {
 DECL|bt_att_hdr|struct|struct bt_att_hdr {
 DECL|bt_att_info_1|struct|struct bt_att_info_1 {
 DECL|bt_att_info_2|struct|struct bt_att_info_2 {
+DECL|bt_att_read_type_req|struct|struct bt_att_read_type_req {
+DECL|bt_att_read_type_rsp|struct|struct bt_att_read_type_rsp {
+DECL|bt_uuid|struct|struct bt_uuid {
 DECL|code|member|uint8_t code;
+DECL|data|member|struct bt_att_data data[0];
+DECL|end_handle|member|uint16_t end_handle;
 DECL|end_handle|member|uint16_t end_handle;
 DECL|end_handle|member|uint16_t end_handle;
 DECL|end_handle|member|uint16_t end_handle;
@@ -57,7 +70,9 @@ DECL|format|member|uint8_t format;
 DECL|handle|member|uint16_t handle;
 DECL|handle|member|uint16_t handle;
 DECL|handle|member|uint16_t handle;
+DECL|handle|member|uint16_t handle;
 DECL|info|member|uint8_t info[0];
+DECL|len|member|uint8_t len;
 DECL|list|member|struct bt_att_handle_group list[0];
 DECL|mtu|member|uint16_t mtu;
 DECL|mtu|member|uint16_t mtu;
@@ -65,7 +80,13 @@ DECL|request|member|uint8_t request;
 DECL|start_handle|member|uint16_t start_handle;
 DECL|start_handle|member|uint16_t start_handle;
 DECL|start_handle|member|uint16_t start_handle;
+DECL|start_handle|member|uint16_t start_handle;
 DECL|type|member|uint16_t type;
+DECL|type|member|uint8_t type;
+DECL|u128|member|uint8_t u128[16];
+DECL|u16|member|uint16_t u16;
 DECL|uuid|member|uint16_t uuid;
+DECL|uuid|member|uint8_t uuid[0];
 DECL|uuid|member|uint8_t uuid[16];
+DECL|value|member|uint8_t value[0];
 DECL|value|member|uint8_t value[0];
