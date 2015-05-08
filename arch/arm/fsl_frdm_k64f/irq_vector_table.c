751d54e736ef4923924e019a50fa06fbabdedf70
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-05-08 17:12:46 -0500

    Rename _IrqVectorTable to _irq_vector_table
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" -o -name "*.arch" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: Id3f9096f28a1bf98035c6a531cd8d2dc66e6448a
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 817e2b275fe33b9fc0afaf2ac7f0bbc54d68b69b

--- arch/arm/fsl_frdm_k64f/irq_vector_table.c
+++ arch/arm/fsl_frdm_k64f/irq_vector_table.c
@@ -1,5 +1,5 @@
-DECL|_IrqVectorTable|variable|_IrqVectorTable
-DECL|_IrqVectorTable|variable|_IrqVectorTable
 DECL|_bt_uart_isr|function|static void _bt_uart_isr(void)
+DECL|_irq_vector_table|variable|_irq_vector_table
+DECL|_irq_vector_table|variable|_irq_vector_table
 DECL|_uart_console_isr|function|static void _uart_console_isr(void)
 DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
--- samples/nanokernel/test/test_arm_m3_irq_vector_table/src/main.c
+++ samples/nanokernel/test/test_arm_m3_irq_vector_table/src/main.c
@@ -1,4 +1,4 @@
-DECL|_IrqVectorTable|variable|_IrqVectorTable
+DECL|_irq_vector_table|variable|_irq_vector_table
 DECL|isr0|function|void isr0(void)
 DECL|isr1|function|void isr1(void)
 DECL|isr2|function|void isr2(void)
