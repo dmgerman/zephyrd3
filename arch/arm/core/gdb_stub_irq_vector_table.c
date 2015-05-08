5827affb5c79744df6d9d381de2a055fe827a7cb
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-05-08 17:12:50 -0500

    Rename _GdbStubIrqVectorTable to _irq_vector_table_with_gdb_stub
    Updating global variable's name to follow a consistent naming convention.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" -o -name "*.arch" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I42d80495508656c8476b5538fbd4af7a7569e8d6
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 2fae92685c70b3d9e057df9f95d05ad27c0784fd

--- arch/arm/core/gdb_stub_irq_vector_table.c
+++ arch/arm/core/gdb_stub_irq_vector_table.c
@@ -1,2 +1,2 @@
-DECL|_GdbStubIrqVectorTable|variable|_GdbStubIrqVectorTable
+DECL|_irq_vector_table_with_gdb_stub|variable|_irq_vector_table_with_gdb_stub
 DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
