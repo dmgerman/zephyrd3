897ff57b3c18652cfcca2626e8f2cb59482b4e11
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-05-08 17:12:54 -0500

    Rename _IdleOrigTicks to idle_original_ticks
    Updating local variable's name to follow a consistent naming convention.
    The prefix underscore is removed due to it's a local variable.
    
    Change accomplished with the following script:
    
       #!/bin/bash
       echo "Searching for ${1} to replace with ${2}"
       find ./ \( -name "*.[chs]" -o -name "sysgen.py" -o -name "*.kconf" -o -name "*.arch" \) \
                ! -path "./host/src/genIdt/*" \
                ! -path "*/outdir/*" | xargs sed -i 's/\b'${1}'\b/'${2}'/g';
    
    Change-Id: I28cbf0d02600d31548393870e712e6da062048da
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 5d861d51bb89f0fde992515bdce8f9a92e6e7220

--- drivers/timer/i8253.c
+++ drivers/timer/i8253.c
@@ -6,7 +6,6 @@ DECL|PIT_CNT2|macro|PIT_CNT2
 DECL|TIMER_MODE_PERIODIC_ENT|macro|TIMER_MODE_PERIODIC_ENT
 DECL|TIMER_MODE_PERIODIC|macro|TIMER_MODE_PERIODIC
 DECL|TIMER_SUPPORTS_TICKLESS|macro|TIMER_SUPPORTS_TICKLESS
-DECL|_IdleOrigTicks|variable|_IdleOrigTicks
 DECL|_MaxLoadValue|variable|_MaxLoadValue
 DECL|_TimerIdleSkew|variable|_TimerIdleSkew
 DECL|_TimerMode|variable|_TimerMode
@@ -25,6 +24,7 @@ DECL|_timer_int_handler|function|void _timer_int_handler(void *unusedArg /* not 
 DECL|clock_accumulated_count|variable|clock_accumulated_count
 DECL|counterLoadVal|variable|counterLoadVal
 DECL|idle_original_count|variable|idle_original_count
+DECL|idle_original_ticks|variable|idle_original_ticks
 DECL|max_system_ticks|variable|max_system_ticks
 DECL|oldAcc|variable|oldAcc
 DECL|oldCount|variable|oldCount
