91a2b23fc3ecfd3886df90d6e6585ed190278424
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-12 17:30:52 +0300

    Bluetooth: Add bt_buf_pull_le16 helper
    bt_buf_pull_le16 convert the initial 16 bits to little endian.
    
    Change-Id: I6f6e9d63fc19b0579d9f445e15e975929fc63ba9
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: d1b61254f394e848146df2d157146c78ab949694

--- net/bluetooth/buf.c
+++ net/bluetooth/buf.c
@@ -9,6 +9,7 @@ DECL|bt_buf_get|function|struct bt_buf *bt_buf_get(enum bt_buf_type type, size_t
 DECL|bt_buf_headroom|function|size_t bt_buf_headroom(struct bt_buf *buf)
 DECL|bt_buf_hold|function|struct bt_buf *bt_buf_hold(struct bt_buf *buf)
 DECL|bt_buf_init|function|int bt_buf_init(int acl_in, int acl_out)
+DECL|bt_buf_pull_le16|function|uint16_t bt_buf_pull_le16(struct bt_buf *buf)
 DECL|bt_buf_pull|function|uint8_t *bt_buf_pull(struct bt_buf *buf, size_t len)
 DECL|bt_buf_push|function|uint8_t *bt_buf_push(struct bt_buf *buf, size_t len)
 DECL|bt_buf_put|function|void bt_buf_put(struct bt_buf *buf)
