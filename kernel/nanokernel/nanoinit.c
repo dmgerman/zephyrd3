b0198b24e56b7faf573e756a5ed6f2438628e83b
Allan Stephens <allan.stephens@windriver.com>
2015-05-12 16:15:58 -0400

    Consolidate nanokernel initialization code
    Moves all nanokernel initialization code into a single file, since
    they are all conceptually related.
    
    Change-Id: Icd4e8572756fdcef7d0b965224ae00824e748a0a
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 9597404727f5030a57680a97d66090c84afbb1f2

--- kernel/nanokernel/nanoinit.c
+++ kernel/nanokernel/nanoinit.c
@@ -1,3 +1,14 @@
+DECL|BOOT_BANNER|macro|BOOT_BANNER
+DECL|BUILD_TIMESTAMP|macro|BUILD_TIMESTAMP
+DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
+DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
+DECL|PRINT_BOOT_BANNER|macro|PRINT_BOOT_BANNER
+DECL|STACK_CANARY_INIT|macro|STACK_CANARY_INIT
+DECL|STACK_CANARY_INIT|macro|STACK_CANARY_INIT
+DECL|_Cstart|function|FUNC_NORETURN void _Cstart(void)
+DECL|_MOVE_INSTR|macro|_MOVE_INSTR
+DECL|_MOVE_INSTR|macro|_MOVE_INSTR
 DECL|_interrupt_stack|variable|_interrupt_stack
 DECL|_k_init_and_idle_task_stack|variable|_k_init_and_idle_task_stack
 DECL|_nano_init|function|void _nano_init(tCCS *dummyOutContext, int argc, char *argv[], char *envp[])
+DECL|build_timestamp|variable|build_timestamp
