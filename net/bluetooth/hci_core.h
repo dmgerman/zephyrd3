09ed3590751fac87f705d90361cabc644986f56d
Johan Hedberg <johan.hedberg@intel.com>
2015-05-12 14:57:38 +0300

    Bluetooth: Reintroduce ACL buffers semaphore
    Since nano_sema is after all able to handle multiple waiters we can
    have a cleaner and more efficient implementation by using a single
    semaphore for all of the connection tx fibers.
    
    Change-Id: Iafb595ae556a2dd826502cf282b88548cb34f336
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 1f5b1b83e0ac661b695c17e335ec086d60d062f3

--- net/bluetooth/hci_core.h
+++ net/bluetooth/hci_core.h
@@ -9,6 +9,7 @@ DECL|hci_revision|member|uint16_t hci_revision;
 DECL|hci_version|member|uint8_t hci_version;
 DECL|le_features|member|uint8_t le_features[8];
 DECL|le_mtu|member|uint16_t le_mtu;
+DECL|le_pkts_sem|member|struct nano_sem le_pkts_sem;
 DECL|le_pkts|member|uint8_t le_pkts;
 DECL|lmp_bredr_capable|macro|lmp_bredr_capable
 DECL|lmp_le_capable|macro|lmp_le_capable
