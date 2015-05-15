8610e64cb3b5c179687215a5a89c5580f93c224b
Allan Stephens <allan.stephens@windriver.com>
2015-05-15 13:51:24 -0400

    Eliminate exposure of non-public task IRQ type definition
    Removes non-public type definition used to implement the microkernel's
    task IRQ support from the public API.
    
    Change-Id: I42c22f0fa323c5b9abbc7d3d29a89a9b2b903a36
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: cff0fc32099fa33214f87eb00e9f15e02e601c53

--- kernel/microkernel/k_irq.c
+++ kernel/microkernel/k_irq.c
@@ -6,13 +6,18 @@ DECL|RELEASE_VECTOR|macro|RELEASE_VECTOR
 DECL|RELEASE_VECTOR|macro|RELEASE_VECTOR
 DECL|_k_task_irq_alloc|function|static int _k_task_irq_alloc(void *arg /* ptr to registration request arguments */ )
 DECL|_task_irq_test|function|int _task_irq_test(kirq_t irq_obj, /* IRQ object identifier */ int32_t time /* time to wait (in ticks) */ )
+DECL|event|member|kevent_t event; /* event number assigned to task IRQ object */
 DECL|irq_obj_mem_stub|variable|irq_obj_mem_stub
 DECL|irq_obj_reg_arg|struct|struct irq_obj_reg_arg {
 DECL|irq_obj|member|kirq_t irq_obj; /* IRQ object identifier */
 DECL|irq|member|uint32_t irq; /* IRQ of device */
+DECL|irq|member|uint32_t irq; /* IRQ used by task IRQ object */
 DECL|taskId|member|ktask_t taskId; /* requesting task */
+DECL|taskId|member|ktask_t taskId; /* task ID of task IRQ object's owner */
 DECL|task_irq_ack|function|void task_irq_ack(kirq_t irq_obj /* IRQ object identifier */ )
 DECL|task_irq_alloc|function|uint32_t task_irq_alloc(kirq_t irq_obj, /* IRQ object identifier */ uint32_t irq, /* requested IRQ */ uint32_t priority /* requested interrupt priority */ )
 DECL|task_irq_free|function|void task_irq_free(kirq_t irq_obj /* IRQ object identifier */ )
+DECL|task_irq_info|struct|struct task_irq_info {
 DECL|task_irq_int_handler|function|static void task_irq_int_handler(void *parameter /* ptr to task IRQ object */ )
 DECL|task_irq_object|variable|task_irq_object
+DECL|vector|member|uint32_t vector; /* interrupt vector assigned to task IRQ object */
