d528972f6fadf22e11f7fd02192419db2281b007
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-15 13:37:53 +0300

    Bluetooth: Add GAP primary service to peripheral sample
    This adds attributes for Generic Access Profile.
    
    Change-Id: I1fe08429f80341e7ba9fc06ef35a6df9b5abbf84
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 0d505e20455c026b680ecef479a3b1d78f1173cd

--- samples/bluetooth/peripheral/src/main.c
+++ samples/bluetooth/peripheral/src/main.c
@@ -1,3 +1,13 @@
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|HEART_RATE_APPEARANCE|macro|HEART_RATE_APPEARANCE
 DECL|ad|variable|ad
+DECL|appearance_chrc|variable|appearance_chrc
+DECL|appeareance_uuid|variable|appeareance_uuid
+DECL|attrs|variable|attrs
+DECL|device_name_uuid|variable|device_name_uuid
+DECL|gap_uuid|variable|gap_uuid
 DECL|mainloop|function|void mainloop(void)
+DECL|name_chrc|variable|name_chrc
+DECL|read_appearance|function|static int read_appearance(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
+DECL|read_name|function|static int read_name(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|sd|variable|sd
