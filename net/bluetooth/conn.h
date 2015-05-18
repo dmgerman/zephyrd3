93f538e564293c37fa626230b3d2d83b6e557163
Johan Hedberg <johan.hedberg@intel.com>
2015-05-18 13:44:59 +0300

    Bluetooth: Add contexts for SMP & ATT specific data
    All fixed channels will need per-connection contexts. Right now in
    addition to the L2CAP (LE) signaling channel the other two fixed ones
    are ATT and SMP.
    
    Change-Id: I56dda6b903ef703e453438e03a99c704aa77899a
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 4dae513d4b080565e4f76cceafbbfce2c26e02f1

--- net/bluetooth/conn.h
+++ net/bluetooth/conn.h
@@ -2,7 +2,10 @@ DECL|BT_CONN_CONNECTED|enumerator|BT_CONN_CONNECTED,
 DECL|BT_CONN_DISCONNECTED|enumerator|BT_CONN_DISCONNECTED,
 DECL|BT_CONN_RX_STACK_SIZE|macro|BT_CONN_RX_STACK_SIZE
 DECL|BT_CONN_TX_STACK_SIZE|macro|BT_CONN_TX_STACK_SIZE
+DECL|att|member|struct bt_conn_att att;
+DECL|bt_conn_att|struct|struct bt_conn_att {
 DECL|bt_conn_l2cap|struct|struct bt_conn_l2cap {
+DECL|bt_conn_smp|struct|struct bt_conn_smp {
 DECL|bt_conn|struct|struct bt_conn {
 DECL|dev|member|struct bt_dev *dev;
 DECL|handle|member|uint16_t handle;
@@ -14,6 +17,7 @@ DECL|rx_len|member|uint16_t rx_len;
 DECL|rx_queue|member|struct nano_fifo rx_queue;
 DECL|rx_stack|member|char rx_stack[BT_CONN_RX_STACK_SIZE];
 DECL|rx|member|struct bt_buf *rx;
+DECL|smp|member|struct bt_conn_smp smp;
 DECL|state|member|uint8_t state;
 DECL|tx_queue|member|struct nano_fifo tx_queue;
 DECL|tx_stack|member|char tx_stack[BT_CONN_TX_STACK_SIZE];
