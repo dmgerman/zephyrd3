ad2d931007d27cb41e96e36838f19d758e104bed
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-18 17:21:08 +0300

    Bluetooth: Add enum for UUID types
    UUID types don't come from any spec so it is safe to have a enum which
    can be changed whenever we want.
    
    Change-Id: I7549f3a487191eeb847a239936f882ff88f4d345
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 0b21762db202a60fb0537669f67dba5f28babeb5

--- include/bluetooth/uuid.h
+++ include/bluetooth/uuid.h
@@ -1,6 +1,7 @@
-DECL|BT_UUID_128|macro|BT_UUID_128
-DECL|BT_UUID_16|macro|BT_UUID_16
+DECL|BT_UUID_128|enumerator|BT_UUID_128,
+DECL|BT_UUID_16|enumerator|BT_UUID_16,
 DECL|__BT_UUID_H|macro|__BT_UUID_H
+DECL|bt_uuid_type|enum|enum bt_uuid_type {
 DECL|bt_uuid|struct|struct bt_uuid {
 DECL|type|member|uint8_t type;
 DECL|u128|member|uint8_t u128[16];
