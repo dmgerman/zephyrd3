60f433e3f9c09ab8205a6414b73db824df67c32a
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-20 13:39:16 +0300

    Bluetooth: Add implementation of ATT Read by Group Type Request
    Read attributes matching UUID and handle range generating a proper
    response:
    
    < ACL Data TX: Handle 3585 flags 0x00 dlen 24
          ATT: Read By Group Type Response (0x11) len 19
            Attribute data length: 6
            Attribute group list: 3 entries
            Handle range: 0x0001-0x0005
            UUID: Generic Access Profile (0x1800)
            Handle range: 0x0006-0x000d
            UUID: Heart Rate (0x180d)
            Handle range: 0x000e-0x0011
            UUID: Battery Service (0x180f)
    
    Change-Id: I1ef2d5b1b0237fc0f89221239686777169cc8883
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 7c14248061b3809e57641394ecc2acf8f64b74a0

--- net/bluetooth/att.c
+++ net/bluetooth/att.c
@@ -7,6 +7,7 @@ DECL|att_mtu_req|function|static void att_mtu_req(struct bt_conn *conn, struct b
 DECL|att_prepare_write_req|function|static void att_prepare_write_req(struct bt_conn *conn, struct bt_buf *data)
 DECL|att_read_blob_req|function|static void att_read_blob_req(struct bt_conn *conn, struct bt_buf *data)
 DECL|att_read_group_req|function|static void att_read_group_req(struct bt_conn *conn, struct bt_buf *data)
+DECL|att_read_group_rsp|function|static void att_read_group_rsp(struct bt_conn *conn, struct bt_uuid *uuid, uint16_t start_handle, uint16_t end_handle)
 DECL|att_read_mult_req|function|static void att_read_mult_req(struct bt_conn *conn, struct bt_buf *data)
 DECL|att_read_req|function|static void att_read_req(struct bt_conn *conn, struct bt_buf *data)
 DECL|att_read_type_req|function|static void att_read_type_req(struct bt_conn *conn, struct bt_buf *data)
@@ -20,10 +21,17 @@ DECL|bt_att_init|function|void bt_att_init(void)
 DECL|bt_att_pool|variable|bt_att_pool
 DECL|bt_att_recv|function|static void bt_att_recv(struct bt_conn *conn, struct bt_buf *buf)
 DECL|bt_att|struct|struct bt_att {
+DECL|buf|member|struct bt_buf *buf;
 DECL|conn|member|struct bt_conn *conn;
+DECL|conn|member|struct bt_conn *conn;
+DECL|group|member|struct bt_att_group_data *group;
 DECL|mtu|member|uint16_t mtu;
 DECL|primary_uuid|variable|primary_uuid
 DECL|range_is_valid|function|static bool range_is_valid(uint16_t start, uint16_t end, uint16_t *err)
+DECL|read_group_cb|function|static uint8_t read_group_cb(const struct bt_gatt_attr *attr, void *user_data)
+DECL|read_group_data|struct|struct read_group_data {
+DECL|rsp|member|struct bt_att_read_group_rsp *rsp;
 DECL|secondary_uuid|variable|secondary_uuid
 DECL|send_err_rsp|function|static void send_err_rsp(struct bt_conn *conn, uint8_t req, uint16_t handle, uint8_t err)
 DECL|uuid_create|function|static bool uuid_create(struct bt_uuid *uuid, struct bt_buf *data)
+DECL|uuid|member|struct bt_uuid *uuid;
