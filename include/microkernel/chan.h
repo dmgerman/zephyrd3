463555cd68f19dcc3ad8ca5660dd90fdd31fb3e7
Allan Stephens <allan.stephens@windriver.com>
2015-05-20 10:15:03 -0400

    Streamline public pipe APIs
    1) Now invokes pipe routines directly, without the use of function
       pointers.
    2) Eliminates unreferenced _DEVICE_CHANNEL symbol.
    
    Change-Id: I6e26476265ed8f33febb5a06c9d03daf7147ee5e
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 267753d9676168f1bd378fcf5e4d57c778eb979b

--- include/microkernel/chan.h
+++ include/microkernel/chan.h
@@ -1,7 +1,8 @@
 DECL|CHAN_H|macro|CHAN_H
-DECL|KS__ChannelGetWT|macro|KS__ChannelGetWT
-DECL|KS__ChannelPutWT|macro|KS__ChannelPutWT
-DECL|PFN_CHANNEL_RWT|typedef|typedef int (*PFN_CHANNEL_RWT)(kpipe_t,
-DECL|PFN_CHANNEL_RW|typedef|typedef int (*PFN_CHANNEL_RW)(kpipe_t, void *, int, int *, K_PIPE_OPTION);
-DECL|_DEVICE_CHANNEL|macro|_DEVICE_CHANNEL
+DECL|task_pipe_get_wait_timeout|macro|task_pipe_get_wait_timeout
+DECL|task_pipe_get_wait|macro|task_pipe_get_wait
+DECL|task_pipe_get|macro|task_pipe_get
 DECL|task_pipe_put_async|macro|task_pipe_put_async
+DECL|task_pipe_put_wait_timeout|macro|task_pipe_put_wait_timeout
+DECL|task_pipe_put_wait|macro|task_pipe_put_wait
+DECL|task_pipe_put|macro|task_pipe_put
