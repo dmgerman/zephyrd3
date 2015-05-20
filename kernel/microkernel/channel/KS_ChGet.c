010113bb0b4e1fdb37b5b29069a0b69e6ee25872
Allan Stephens <allan.stephens@windriver.com>
2015-05-20 14:05:38 -0400

    Cosmetic cleanup of pipe-related code
    1) Eliminates non-functional (i.e. commented out) code.
    2) Improves adherence to coding conventions.
    
    Change-Id: I78ad1dd173a49f36c126e1c9f349bace62200174
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 4b3e0c99458aed30c3b6e7572f9e1602a0f856ca

--- kernel/microkernel/channel/KS_ChGet.c
+++ kernel/microkernel/channel/KS_ChGet.c
@@ -1,1 +1,1 @@
-DECL|_task_pipe_get|function|int _task_pipe_get(kpipe_t Id,void *pBuffer, int iNbrBytesToRead, int *piNbrBytesRead, K_PIPE_OPTION Option,
+DECL|_task_pipe_get|function|int _task_pipe_get(kpipe_t Id, void *pBuffer, int iNbrBytesToRead, int *piNbrBytesRead, K_PIPE_OPTION Option, int32_t TimeOut)
--- kernel/microkernel/channel/KS_ChPtA.c
+++ kernel/microkernel/channel/KS_ChPtA.c
@@ -1,1 +1,1 @@
-DECL|_task_pipe_put_async|function|int _task_pipe_put_async(kpipe_t Id, struct k_block Block, int iReqSize2Xfer, ksem_t Sema
+DECL|_task_pipe_put_async|function|int _task_pipe_put_async(kpipe_t Id, struct k_block Block, int iReqSize2Xfer, ksem_t Sema)
