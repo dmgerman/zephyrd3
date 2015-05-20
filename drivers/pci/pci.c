559013351446ef00b62f08d8226396a2edaf11ec
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-05-20 14:29:45 +0300

    pci: Apply code style changes
    - always add {} on any sub-statements
    - proper indentation
    - 80 chars limit
    
    Change-Id: I869239bf2e309a6e73fb180cf29acf66c019489a
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 1425fd844915081a6777f70250ba56511c08ea91

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -13,10 +13,10 @@ DECL|LSPCI_MAX_REG|macro|LSPCI_MAX_REG
 DECL|MAX_BARS|macro|MAX_BARS
 DECL|dev_info_index|variable|dev_info_index
 DECL|dev_info|variable|dev_info
-DECL|pci_bar_config_get|function|static int pci_bar_config_get(uint32_t bus, uint32_t dev, uint32_t func, uint32_t bar, uint32_t *config)
-DECL|pci_bar_params_get|function|static inline int pci_bar_params_get(uint32_t bus, uint32_t dev, uint32_t func, uint32_t bar, struct pci_dev_info *dev_info)
-DECL|pci_bus_scan|function|void pci_bus_scan(uint32_t class_mask /* bitmask, bits set for each needed class */)
+DECL|pci_bar_config_get|function|static int pci_bar_config_get(uint32_t bus,uint32_t dev, uint32_t func, uint32_t bar, uint32_t *config)
+DECL|pci_bar_params_get|function|static inline int pci_bar_params_get(uint32_t bus,uint32_t dev, uint32_t func, uint32_t bar, struct pci_dev_info *dev_info)
+DECL|pci_bus_scan|function|void pci_bus_scan(uint32_t class_mask)
 DECL|pci_dev_find|function|int pci_dev_find(int class, int idx, uint32_t *addr, uint32_t *size, int *irq)
-DECL|pci_dev_scan|function|static void pci_dev_scan(uint32_t bus, uint32_t dev, uint32_t class_mask /* bitmask, bits set for each needed class */ )
+DECL|pci_dev_scan|function|static void pci_dev_scan(uint32_t bus,uint32_t dev, uint32_t class_mask)
 DECL|pci_info_get|function|struct pci_dev_info *pci_info_get(void)
 DECL|pci_show|function|void pci_show(void)
