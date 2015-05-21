be052e8749ef35c5571cc780b9a4418c976b9e66
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-21 15:29:10 +0300

    Bluetooth: Add Battery Service to peripheral sample
    Add Battery Service along with its characteristics and descriptors
    to the database table.
    
    Change-Id: Ifb83ebcb4d6cdafa5df84d643b2e2a69e5a6c1e3
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: a8f34a1c1ec72b234baff11f0176194be982a8a9

--- samples/bluetooth/peripheral/src/main.c
+++ samples/bluetooth/peripheral/src/main.c
@@ -4,6 +4,9 @@ DECL|ad|variable|ad
 DECL|appearance_chrc|variable|appearance_chrc
 DECL|appeareance_uuid|variable|appeareance_uuid
 DECL|attrs|variable|attrs
+DECL|bas_uuid|variable|bas_uuid
+DECL|blvl_chrc|variable|blvl_chrc
+DECL|blvl_uuid|variable|blvl_uuid
 DECL|bslc_chrc|variable|bslc_chrc
 DECL|bslc_uuid|variable|bslc_uuid
 DECL|ccc_uuid|variable|ccc_uuid
@@ -18,6 +21,7 @@ DECL|mainloop|function|void mainloop(void)
 DECL|name_chrc|variable|name_chrc
 DECL|read_appearance|function|static int read_appearance(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_blsc|function|static int read_blsc(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
+DECL|read_blvl|function|static int read_blvl(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_ccc|function|static int read_ccc(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_name|function|static int read_name(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|sd|variable|sd
