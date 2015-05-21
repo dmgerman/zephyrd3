c4091db9986a574ada495c0e24bde627a8787830
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-21 13:12:28 +0300

    Bluetooth: Add Heart Rate Service to peripheral sample
    Add Heart Rate Service along with its characteristics and descriptors
    to the database table.
    
    Change-Id: Ibff52da7352883edccb7c5b903538a81aad2ba55
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 1b793139dbf3e2b69c062965504467c6fc306dc4

--- samples/bluetooth/peripheral/src/main.c
+++ samples/bluetooth/peripheral/src/main.c
@@ -4,10 +4,20 @@ DECL|ad|variable|ad
 DECL|appearance_chrc|variable|appearance_chrc
 DECL|appeareance_uuid|variable|appeareance_uuid
 DECL|attrs|variable|attrs
+DECL|bslc_chrc|variable|bslc_chrc
+DECL|bslc_uuid|variable|bslc_uuid
+DECL|ccc_uuid|variable|ccc_uuid
 DECL|device_name_uuid|variable|device_name_uuid
 DECL|gap_uuid|variable|gap_uuid
+DECL|hrcpc_chrc|variable|hrcpc_chrc
+DECL|hrcpc_uuid|variable|hrcpc_uuid
+DECL|hrmc_chrc|variable|hrmc_chrc
+DECL|hrmc_uuid|variable|hrmc_uuid
+DECL|hrs_uuid|variable|hrs_uuid
 DECL|mainloop|function|void mainloop(void)
 DECL|name_chrc|variable|name_chrc
 DECL|read_appearance|function|static int read_appearance(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
+DECL|read_blsc|function|static int read_blsc(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
+DECL|read_ccc|function|static int read_ccc(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_name|function|static int read_name(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|sd|variable|sd
