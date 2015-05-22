f6d9854667e33ee1ce4b1258ffe6244d7d3564c9
Allan Stephens <allan.stephens@windriver.com>
2015-05-22 15:19:28 -0400

    Eliminate secure string library routines
    These routines are either unused, or are always used in manner
    that does not require their added security checking.
    
    Change-Id: I6f484924ebc3d395a20879445a2fcabebf6c5014
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: cb2d18cc83b70c0aa83be83de9b03595ddafc1c8

--- kernel/nanokernel/string_s.c
+++
@@ -1,3 +0,0 @@
-DECL|__k_memcpy_s|function|errno_t __k_memcpy_s(void *dest, size_t nDestElem, const void *src, size_t nElem)
-DECL|__strcpy_s|function|errno_t __strcpy_s(char *dest, size_t nDestElem, const char *src)
-DECL|__strlen_s|function|size_t __strlen_s(const char *str, size_t maxElem)
