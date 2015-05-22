3919df91817b3fd309fe23d9b09bdd3a40ae1ebc
Patrik Flykt <patrik.flykt@linux.intel.com>
2015-05-22 10:46:24 +0300

    net: Add loopback driver
    Change-Id: If8ee848ed8519719b4ec3ad2e74167e0558709ef
    Signed-off-by: Patrik Flykt <patrik.flykt@linux.intel.com>
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: e26167cd4a9369ec213872bb160516904d30445c

---
+++ net/ip/net_driver_loopback.c
@@ -0,0 +1,8 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|in6addr_any|variable|in6addr_any
+DECL|in6addr_loopback|variable|in6addr_loopback
+DECL|net_driver_loopback_init|function|int net_driver_loopback_init(void)
+DECL|net_driver_loopback_open|function|static int net_driver_loopback_open(void)
+DECL|net_driver_loopback_send|function|static int net_driver_loopback_send(struct net_buf *buf)
+DECL|net_driver_loopback|variable|net_driver_loopback
