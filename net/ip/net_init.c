9e755ebc45edd756f5d4ebd7563a18b2d1491c84
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-05-22 12:40:38 +0300

    net: Add helper function to set the link local address
    Network driver should call this when it knows the device MAC or
    EUI-64 address.
    
    Change-Id: Ie03a80fcdb9a10cd1cb15f12037b3802da13bca9
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 6b3c739d7de1f33ad6a521c163608b8c690a8645

--- net/ip/net_init.c
+++ net/ip/net_init.c
@@ -7,6 +7,7 @@ DECL|net_dev|struct|static struct net_dev {
 DECL|net_init|function|int net_init(void)
 DECL|net_register_driver|function|int net_register_driver(struct net_driver *drv)
 DECL|net_rx_fiber|function|static void net_rx_fiber(void)
+DECL|net_set_mac|function|int net_set_mac(uint8_t *mac, uint8_t len)
 DECL|net_tx_fiber|function|static void net_tx_fiber(void)
 DECL|net_unregister_driver|function|void net_unregister_driver(struct net_driver *drv)
 DECL|netdev|variable|netdev
