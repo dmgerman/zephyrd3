a465efd7fe8060943d7b932e1d3bd543f2cd4795
Johan Hedberg <johan.hedberg@intel.com>
2015-05-22 13:54:41 +0300

    Bluetooth: Store full LTK information
    We also need to store the EDiv & Rand values to be able to do correct
    look-up of LTKs.
    
    Change-Id: If6dbfc7569d18a4c8597a401deccad3ef8cb51ad
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6140bd04fea0bb3f06a7ade66432c7b8f10bfdd5

--- net/bluetooth/hci_core.h
+++ net/bluetooth/hci_core.h
@@ -4,8 +4,10 @@ DECL|bdaddr|member|uint8_t bdaddr[6];
 DECL|bdaddr|member|uint8_t bdaddr[6];
 DECL|bt_dev|struct|struct bt_dev {
 DECL|bt_keys|struct|struct bt_keys {
+DECL|bt_ltk|struct|struct bt_ltk {
 DECL|cmd_queue|member|struct nano_fifo cmd_queue;
 DECL|drv|member|struct bt_driver *drv;
+DECL|ediv|member|uint16_t ediv;
 DECL|features|member|uint8_t features[8];
 DECL|hci_revision|member|uint16_t hci_revision;
 DECL|hci_version|member|uint8_t hci_version;
@@ -18,7 +20,9 @@ DECL|lmp_le_capable|macro|lmp_le_capable
 DECL|manufacturer|member|uint16_t manufacturer;
 DECL|ncmd_sem|member|struct nano_sem ncmd_sem;
 DECL|ncmd|member|uint8_t ncmd;
+DECL|rand|member|uint64_t rand;
 DECL|rx_queue|member|struct nano_fifo rx_queue;
 DECL|scan_enable|member|uint8_t scan_enable;
 DECL|sent_cmd|member|struct bt_buf *sent_cmd;
-DECL|slave_ltk|member|uint8_t slave_ltk[16];
+DECL|slave_ltk|member|struct bt_ltk slave_ltk;
+DECL|val|member|uint8_t val[16];
