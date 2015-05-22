ddac4c108e2d6791a81af9364948e67455ee6169
Allan Stephens <allan.stephens@windriver.com>
2015-05-22 15:11:36 -0400

    Eliminate testing of secure string library code
    Test project is no longer needed, since the associated library
    is now scheduled for removal.
    
    Change-Id: I843018e647c4bdc82c785ae21dbfa9de608c0d50
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 4b105f459d336612950852aa6e436f0c06c37d05

--- samples/microkernel/test/test_secure_string_api/src/secure.c
+++
@@ -1,5 +0,0 @@
-DECL|BUFSIZE|macro|BUFSIZE
-DECL|MainTask|function|void MainTask(void)
-DECL|MemcpyTask|function|void MemcpyTask(void)
-DECL|StrcpyTask|function|void StrcpyTask(void)
-DECL|WAIT_TOUT|macro|WAIT_TOUT
