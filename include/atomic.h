8c28bb333585202276c09961c0dfb1a90b0ac71a
Allan Stephens <allan.stephens@windriver.com>
2015-05-22 16:25:54 -0400

    Split off atomic APIs from main nanokernel include file
    The atomic APIs are not really tied to nanokernel-specific
    capabilities, and can be used by anyone at anytime -- even
    during system initialization!
    
    Change-Id: I427954029b4b465127cb5b4b642c2f9968c17d5a
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: a5e40e7247ef9d8dc5da5c0f8164e1f88d10d06a

---
+++ include/atomic.h
@@ -0,0 +1,4 @@
+DECL|ATOMIC_INIT|macro|ATOMIC_INIT
+DECL|__ATOMIC_H__|macro|__ATOMIC_H__
+DECL|atomic_t|typedef|typedef int atomic_t;
+DECL|atomic_val_t|typedef|typedef atomic_t atomic_val_t;
--- include/nanokernel.h
+++ include/nanokernel.h
@@ -1,11 +1,8 @@
-DECL|ATOMIC_INIT|macro|ATOMIC_INIT
 DECL|NANO_CTX_FIBER|macro|NANO_CTX_FIBER
 DECL|NANO_CTX_ISR|macro|NANO_CTX_ISR
 DECL|NANO_CTX_TASK|macro|NANO_CTX_TASK
 DECL|SYS_PREKERNEL_INIT|macro|SYS_PREKERNEL_INIT
 DECL|__NANOKERNEL_H__|macro|__NANOKERNEL_H__
-DECL|atomic_t|typedef|typedef int atomic_t;
-DECL|atomic_val_t|typedef|typedef atomic_t atomic_val_t;
 DECL|nano_context_id_t|typedef|typedef struct s_CCS *nano_context_id_t;
 DECL|nano_context_type_t|typedef|typedef int nano_context_type_t;
 DECL|nano_fiber_entry_t|typedef|typedef void (*nano_fiber_entry_t)(int i1, int i2);
