3032742f3b844318b86db3a50e2568e7cd7ab156
Peter Mitsis <peter.mitsis@windriver.com>
2015-05-25 10:21:57 -0400

    Optimizations to _NewContext() and _NewContextInternal()
    Since the address of the new context is known before _NewContext() is invoked
    (due to it being passed a properly aligned stack), there is no longer any
    need for _NewContext to return the pointer to the context.
    
    Furthermore, as a direct result of the properly aligned stack, the pointer to
    the new context does not need to be passed as a separate parameter since it
    will always match the passed stack pointer.
    
    Change-Id: Ie57a9c4ad17f6f13e8b3f659cd701d4f8950ea97
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 40b5200c7322b30169e07add9c175290274a8aea

--- arch/arm/core/nanocontext.c
+++ arch/arm/core/nanocontext.c
@@ -1,5 +1,5 @@
 DECL|CONTEXT_MONITOR_INIT|macro|CONTEXT_MONITOR_INIT
 DECL|CONTEXT_MONITOR_INIT|macro|CONTEXT_MONITOR_INIT
-DECL|_NewContext|function|void *_NewContext(char *pStackMem, /* aligned stack memory */ unsigned stackSize, /* stack size in bytes */ _ContextEntry pEntry, /* entry point */ void *parameter1, /* entry point first param */
+DECL|_NewContext|function|void _NewContext(char *pStackMem, /* aligned stack memory */ unsigned stackSize, /* stack size in bytes */ _ContextEntry pEntry, /* entry point */ void *parameter1, /* entry point first param */
 DECL|_context_monitor_init|function|static ALWAYS_INLINE void _context_monitor_init(struct s_CCS *pCcs /* context */ )
 DECL|_nanokernel|variable|_nanokernel
--- arch/x86/core/nanocontext.c
+++ arch/x86/core/nanocontext.c
@@ -1,3 +1,3 @@
-DECL|_NewContextInternal|function|static void _NewContextInternal(tCCS *ccs, /* pointer to the new task's ccs */ char *pStackMem, /* pointer to context stack memory */ unsigned stackSize, /* size of stack in bytes */ int priority, /* context priority */
-DECL|_NewContext|function|void *_NewContext(char *pStackMem, /* pointer to aligned stack memory */ unsigned stackSize, /* size of stack in bytes */ _ContextEntry pEntry, /* context entry point function */ void *parameter1, /* first parameter to context entry point function */
+DECL|_NewContextInternal|function|static void _NewContextInternal(char *pStackMem, /* pointer to context stack memory */ unsigned stackSize, /* size of stack in bytes */ int priority, /* context priority */ unsigned options /* context options: USE_FP, USE_SSE */
+DECL|_NewContext|function|void _NewContext(char *pStackMem, /* pointer to aligned stack memory */ unsigned stackSize, /* size of stack in bytes */ _ContextEntry pEntry, /* context entry point function */ void *parameter1, /* first parameter to context entry point function */
 DECL|_nanokernel|variable|_nanokernel
