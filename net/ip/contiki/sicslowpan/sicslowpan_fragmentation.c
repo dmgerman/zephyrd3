efca5d7ab7c36e05f90329dcc849622fd66b5fc0
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2015-05-25 15:07:21 +0300

    net: Add separated compression and fragmentation files.
    Change-Id: If639164d32dbebb0e23075b433739c56bf70163c
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9431109cdec57a25b70b08523a794830525f976e

---
+++ net/ip/contiki/sicslowpan/sicslowpan_fragmentation.c
@@ -0,0 +1,42 @@
+DECL|DEBUG|macro|DEBUG
+DECL|GET16|macro|GET16
+DECL|MAC_MAX_PAYLOAD|macro|MAC_MAX_PAYLOAD
+DECL|MAC_MAX_PAYLOAD|macro|MAC_MAX_PAYLOAD
+DECL|PACKETBUF_FRAG_DISPATCH_SIZE|macro|PACKETBUF_FRAG_DISPATCH_SIZE
+DECL|PACKETBUF_FRAG_OFFSET|macro|PACKETBUF_FRAG_OFFSET
+DECL|PACKETBUF_FRAG_TAG|macro|PACKETBUF_FRAG_TAG
+DECL|PRINTFI|macro|PRINTFI
+DECL|PRINTFI|macro|PRINTFI
+DECL|PRINTFO|macro|PRINTFO
+DECL|PRINTFO|macro|PRINTFO
+DECL|PRINTPACKETBUF|macro|PRINTPACKETBUF
+DECL|PRINTPACKETBUF|macro|PRINTPACKETBUF
+DECL|PRINTSICSLOWPANBUF|macro|PRINTSICSLOWPANBUF
+DECL|PRINTSICSLOWPANBUF|macro|PRINTSICSLOWPANBUF
+DECL|PRINTUIPBUF|macro|PRINTUIPBUF
+DECL|PRINTUIPBUF|macro|PRINTUIPBUF
+DECL|PRIORITIZE_NEW_PACKETS|macro|PRIORITIZE_NEW_PACKETS
+DECL|SET16|macro|SET16
+DECL|SICSLOWPAN_IP_BUF|macro|SICSLOWPAN_IP_BUF
+DECL|SICSLOWPAN_UDP_BUF|macro|SICSLOWPAN_UDP_BUF
+DECL|UIP_ICMP_BUF|macro|UIP_ICMP_BUF
+DECL|UIP_IP_BUF|macro|UIP_IP_BUF
+DECL|UIP_LOG|macro|UIP_LOG
+DECL|UIP_LOG|macro|UIP_LOG
+DECL|UIP_TCP_BUF|macro|UIP_TCP_BUF
+DECL|UIP_UDP_BUF|macro|UIP_UDP_BUF
+DECL|USE_FRAMER_HDRLEN|macro|USE_FRAMER_HDRLEN
+DECL|frag_sender|variable|frag_sender
+DECL|fragment|function|static int fragment(struct net_buf *buf, const uip_lladdr_t *localdest, void *ptr)
+DECL|my_tag|variable|my_tag
+DECL|packet_sent|function|packet_sent(struct net_mbuf *buf, void *ptr, int status, int transmissions)
+DECL|processed_ip_in_len|variable|processed_ip_in_len
+DECL|p|variable|p
+DECL|reass_tag|variable|reass_tag
+DECL|reassemble|function|static int reassemble(struct net_mbuf *mbuf)
+DECL|send_packet|function|send_packet(struct net_mbuf *buf, linkaddr_t *dest, void *ptr)
+DECL|sicslowpan_buf|macro|sicslowpan_buf
+DECL|sicslowpan_buf|macro|sicslowpan_buf
+DECL|sicslowpan_fragmentation|variable|sicslowpan_fragmentation
+DECL|sicslowpan_len|macro|sicslowpan_len
+DECL|sicslowpan_len|macro|sicslowpan_len
