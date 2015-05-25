ba726db20cacf15bb2d7c51bf4d338841206db62
Allan Stephens <allan.stephens@windriver.com>
2015-05-25 13:49:15 -0400

    Eliminate definitions of k_memset() and k_memcpy()
    These APIs are no longer referenced anywhere.
    
    Change-Id: I56e3410b9fb6bc4eb72bc9299b33f75227916434
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 3c0b6f0379cc91e26271131ccdd0efa8ed3543cc

--- include/arch/arm/ARMprc.h
+++ include/arch/arm/ARMprc.h
@@ -1,5 +1,3 @@
 DECL|OCTET_TO_SIZEOFUNIT|macro|OCTET_TO_SIZEOFUNIT
 DECL|SIZEOFUNIT_TO_OCTET|macro|SIZEOFUNIT_TO_OCTET
 DECL|_ARM_CPU__H_|macro|_ARM_CPU__H_
-DECL|k_memcpy|macro|k_memcpy
-DECL|k_memset|macro|k_memset
