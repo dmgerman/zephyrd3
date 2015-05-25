1d17daf8e1c55d5cd7704b87968a6e760ae10aa5
Allan Stephens <allan.stephens@windriver.com>
2015-05-25 13:50:16 -0400

    Optimize performance of memset() and memcpy()
    Builds upon the approach taken by the x86 version of k_memset()
    and k_memcpy() APIs, which uses word-sized operations for greater
    efficiency when manipulating large buffers. The algorithms used
    here are architecture-independent.
    
    Change-Id: I01ea8b22c8e6028f881e9b61ccc3a01e8ba4c02b
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: b52a09f536c7dd993d233bdb700ac4848c307d8f

--- lib/libc/minimal/source/string/string.c
+++ lib/libc/minimal/source/string/string.c
@@ -1,7 +1,7 @@
 DECL|memcmp|function|int memcmp(const void *m1, const void *m2, size_t n)
 DECL|memcpy|function|void *memcpy(void *restrict d, const void *restrict s, size_t n)
 DECL|memmove|function|void *memmove(void *d, const void *s, size_t n)
-DECL|memset|function|void *memset(void *s, int c, size_t n)
+DECL|memset|function|void *memset(void *buf, int c, size_t n)
 DECL|strchr|function|char *strchr(const char *s, int c)
 DECL|strcmp|function|int strcmp(const char *s1, const char *s2)
 DECL|strcpy|function|char *strcpy(char *restrict d, const char *restrict s)
