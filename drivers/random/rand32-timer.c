b24fba65e6d7a0773d76a3864f1f0eeff47965df
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-05-25 11:07:09 -0400

    Algorithm improvement for timer based random number generator
    The timer based pseudo random number generator returns the
    number of system clock ticks. If the random number function is
    called subsequently in a time less than a system clock tick, it
    leads to getting same number.
    
    To prevent this the counter is introduced. Counter gets changed
    each call, so it ensures the caller gets different numbers each time
    the random number function gets called.
    
    Change-Id: Ibe57e1e7a23909b185623af8dbb7e80647c7ee08
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: e2fa00be6ebb2543550853e778e3ce7eb46af57a

--- drivers/random/rand32-timer.c
+++ drivers/random/rand32-timer.c
@@ -1,2 +1,4 @@
+DECL|_RAND32_INC|macro|_RAND32_INC
 DECL|_Rand32Get|function|uint32_t _Rand32Get(void)
 DECL|_Rand32Init|function|void _Rand32Init(void)
+DECL|_rand32_counter|variable|_rand32_counter
