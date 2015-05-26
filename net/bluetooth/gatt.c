b95c72e86ebcdd9ee51c51c1aafc46ae897f7223
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-26 14:29:30 +0300

    Bluetooth: Add bt_gatt_notify
    bt_gatt_notify can be used to send ATT notifications:
    
    < ACL Data TX: Handle 3585 flags 0x00 dlen 8
          ATT: Handle Value Notification (0x1b) len 3
            Handle: 0x0010
              Data: 64
    
    Change-Id: I908f10f9e60dc7cef221e4b27c3b6e49e7b0cfc9
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: e4e6f4dbe9f60794ed21e316f2de296143ec5442

--- net/bluetooth/gatt.c
+++ net/bluetooth/gatt.c
@@ -10,12 +10,18 @@ DECL|bt_gatt_attr_read_service|function|int bt_gatt_attr_read_service(const bt_a
 DECL|bt_gatt_attr_read|function|int bt_gatt_attr_read(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, void *buf, uint8_t buf_len, uint16_t offset, const void *value, uint8_t value_len)
 DECL|bt_gatt_attr_write_ccc|function|int bt_gatt_attr_write_ccc(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, const void *buf, uint8_t len, uint16_t offset)
 DECL|bt_gatt_foreach_attr|function|void bt_gatt_foreach_attr(uint16_t start_handle, uint16_t end_handle, bt_gatt_attr_func_t func, void *user_data)
+DECL|bt_gatt_notify|function|void bt_gatt_notify(uint16_t handle, const void *data, size_t len)
 DECL|bt_gatt_register|function|void bt_gatt_register(const struct bt_gatt_attr *attrs, size_t count)
+DECL|data|member|const void *data;
 DECL|db|variable|db
 DECL|end_handle|member|uint16_t end_handle;
 DECL|gatt_ccc_changed|function|static void gatt_ccc_changed(struct _bt_gatt_ccc *ccc)
 DECL|gatt_chrc|struct|struct gatt_chrc {
 DECL|gatt_incl|struct|struct gatt_incl {
+DECL|handle|member|uint8_t handle;
+DECL|len|member|size_t len;
+DECL|notify_cb|function|static uint8_t notify_cb(const struct bt_gatt_attr *attr, void *user_data)
+DECL|notify_data|struct|struct notify_data {
 DECL|properties|member|uint8_t properties;
 DECL|start_handle|member|uint16_t start_handle;
 DECL|uuid16|member|uint16_t uuid16;
