2b678d8386c35f5246581a6a973a1ce7539d0a66
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-05-26 09:17:03 +0300

    Bluetooth: Add Current Time Service to peripheral app
    Add Current Time Service currently providing hardcoded time since
    there is no way of getting current time in the OS.
    
    Change-Id: I9954d51f77905230a6dd180f37d3a48f379c69ec
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: fae27b95879b2fc8ea2d8030d0b2b8b02b0aaf21

--- samples/bluetooth/peripheral/src/main.c
+++ samples/bluetooth/peripheral/src/main.c
@@ -10,8 +10,12 @@ DECL|blvl_uuid|variable|blvl_uuid
 DECL|bslc_chrc|variable|bslc_chrc
 DECL|bslc_uuid|variable|bslc_uuid
 DECL|ccc_uuid|variable|ccc_uuid
+DECL|ct_chrc|variable|ct_chrc
+DECL|ct_uuid|variable|ct_uuid
+DECL|cts_uuid|variable|cts_uuid
 DECL|device_name_uuid|variable|device_name_uuid
 DECL|gap_uuid|variable|gap_uuid
+DECL|generate_current_time|function|static void generate_current_time(uint8_t *buf)
 DECL|hrcpc_chrc|variable|hrcpc_chrc
 DECL|hrcpc_uuid|variable|hrcpc_uuid
 DECL|hrmc_chrc|variable|hrmc_chrc
@@ -23,5 +27,6 @@ DECL|read_appearance|function|static int read_appearance(const struct bt_gatt_at
 DECL|read_blsc|function|static int read_blsc(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_blvl|function|static int read_blvl(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_ccc|function|static int read_ccc(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
+DECL|read_ct|function|static int read_ct(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|read_name|function|static int read_name(const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|sd|variable|sd
