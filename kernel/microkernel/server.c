2443a3493d80f07943e51b85fcf825f73bfb0160
Peter Mitsis <peter.mitsis@windriver.com>
2015-05-27 14:16:09 -0400

    Refactor _Cget() to invoke _NanoFatalErrorHandler()
    It is a fatal error if the microkernel is configured with either too few
    command packets or too few timer packets.
    
    NOTE: During this refactoring of _Cget(), not only is it renamed to
    _nano_fiber_lifo_get_panic(), but it is moved into "nano_lifo.c".
    
    Change-Id: I1d866cda1b444da04877f7eda03762b6e83c9a6f
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: f8619b41811e7f709074eba4c371c6446587493b

--- kernel/microkernel/server.c
+++ kernel/microkernel/server.c
@@ -1,3 +1,2 @@
 DECL|K_swapper|function|FUNC_NORETURN void K_swapper(int parameter1, /* not used */ int parameter2 /* not used */ )
-DECL|_Cget|function|void *_Cget(struct nano_lifo *chan)
 DECL|next_task_select|function|static struct k_proc *next_task_select(void)
