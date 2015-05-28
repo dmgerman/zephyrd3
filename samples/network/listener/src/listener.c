36c1c9de8ff447cdec58d444fcdb9ca187118cf1
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-05-28 12:50:12 +0300

    net: apps: Simple listener application that just receives packets
    Change-Id: I91457d364742928743be9040c0db64a1f052c093
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 0b54f984b8a456041d458b5b2b004af86d152161

---
+++ samples/network/listener/src/listener.c
@@ -0,0 +1,22 @@
+DECL|MYADDR|macro|MYADDR
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|TASK_IPADDR|macro|TASK_IPADDR
+DECL|any_addr|variable|any_addr
+DECL|fiberEntry|function|void fiberEntry(void)
+DECL|fiberStack|variable|fiberStack
+DECL|get_context|function|static struct net_context *get_context(const struct net_addr *addr)
+DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|main|function|void main(void)
+DECL|my_addr|variable|my_addr
+DECL|nanoSemFiber|variable|nanoSemFiber
+DECL|nanoSemTask|variable|nanoSemTask
+DECL|taskA|function|void taskA(void)
+DECL|taskB|function|void taskB(void)
