779ab1cd035c3f1aed1db1361b1d482963c48fcf
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-05-29 13:03:57 +0300

    Bluetooth: Add BT_GATT_CCC macro
    BT_GATT_CCC uses _bt_gatt_ccc internal struct to handle peer
    configurations and special helpers to read and write to minimize the
    code necessary to handle CCC in the task.
    
    Note this not yet include handling of connection and disconnection but
    the idea is that the core would automatically disable the configurations
    while disconnected and re-enable once peer connect back.
    
    Change-Id: I89325b8a074766b9fd2423085565df669f7275e1
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: e8e0fbe63ac48f683c08eed39b116103c3cd2439

--- include/bluetooth/gatt.h
+++ include/bluetooth/gatt.h
@@ -1,5 +1,6 @@
 DECL|BT_GATT_CCC_INDICATE|macro|BT_GATT_CCC_INDICATE
 DECL|BT_GATT_CCC_NOTIFY|macro|BT_GATT_CCC_NOTIFY
+DECL|BT_GATT_CCC|macro|BT_GATT_CCC
 DECL|BT_GATT_CEP_RELIABLE_WRITE|macro|BT_GATT_CEP_RELIABLE_WRITE
 DECL|BT_GATT_CEP_WRITABLE_AUX|macro|BT_GATT_CEP_WRITABLE_AUX
 DECL|BT_GATT_CHARACTERISTIC|macro|BT_GATT_CHARACTERISTIC
@@ -19,17 +20,23 @@ DECL|BT_GATT_PRIMARY_SERVICE|macro|BT_GATT_PRIMARY_SERVICE
 DECL|BT_GATT_SECONDARY_SERVICE|macro|BT_GATT_SECONDARY_SERVICE
 DECL|BT_GATT_SERVICE|macro|BT_GATT_SERVICE
 DECL|__BT_GATT_H|macro|__BT_GATT_H
+DECL|_bt_gatt_ccc|struct|struct _bt_gatt_ccc {
 DECL|bt_gatt_attr_func_t|typedef|typedef uint8_t (*bt_gatt_attr_func_t)(const struct bt_gatt_attr *attr,
 DECL|bt_gatt_attr|struct|struct bt_gatt_attr {
+DECL|bt_gatt_ccc_cfg|struct|struct bt_gatt_ccc_cfg {
 DECL|bt_gatt_ccc|struct|struct bt_gatt_ccc {
 DECL|bt_gatt_cep|struct|struct bt_gatt_cep {
 DECL|bt_gatt_chrc|struct|struct bt_gatt_chrc {
 DECL|bt_gatt_cud|struct|struct bt_gatt_cud {
 DECL|bt_gatt_include|struct|struct bt_gatt_include {
 DECL|bt_gatt_service|struct|struct bt_gatt_service {
+DECL|cfg_changed|member|void (*cfg_changed)(uint16_t value);
+DECL|cfg_len|member|size_t cfg_len;
+DECL|cfg|member|struct bt_gatt_ccc_cfg *cfg;
 DECL|end_handle|member|uint16_t end_handle;
 DECL|flags|member|uint16_t flags;
 DECL|handle|member|uint16_t handle;
+DECL|peer|member|bt_addr_le_t peer;
 DECL|properties|member|uint8_t properties;
 DECL|properties|member|uint8_t properties;
 DECL|read|member|int (*read)(const bt_addr_le_t *peer,
@@ -41,4 +48,7 @@ DECL|uuid|member|const struct bt_uuid *uuid;
 DECL|uuid|member|const struct bt_uuid *uuid;
 DECL|uuid|member|const struct bt_uuid *uuid;
 DECL|value_handle|member|uint16_t value_handle;
+DECL|value_handle|member|uint16_t value_handle;
+DECL|value|member|uint16_t value;
+DECL|value|member|uint16_t value;
 DECL|write|member|int (*write)(const bt_addr_le_t *peer,
--- net/bluetooth/gatt.c
+++ net/bluetooth/gatt.c
@@ -3,14 +3,17 @@ DECL|BT_DBG|macro|BT_DBG
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|PACK_STRUCT|variable|PACK_STRUCT
 DECL|attr_count|variable|attr_count
+DECL|bt_gatt_attr_read_ccc|function|int bt_gatt_attr_read_ccc(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|bt_gatt_attr_read_chrc|function|int bt_gatt_attr_read_chrc(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|bt_gatt_attr_read_include|function|int bt_gatt_attr_read_include(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|bt_gatt_attr_read_service|function|int bt_gatt_attr_read_service(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
 DECL|bt_gatt_attr_read|function|int bt_gatt_attr_read(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, void *buf, uint8_t buf_len, uint16_t offset, const void *value, uint8_t value_len)
+DECL|bt_gatt_attr_write_ccc|function|int bt_gatt_attr_write_ccc(const bt_addr_le_t *peer, const struct bt_gatt_attr *attr, const void *buf, uint8_t len, uint16_t offset)
 DECL|bt_gatt_foreach_attr|function|void bt_gatt_foreach_attr(uint16_t start_handle, uint16_t end_handle, bt_gatt_attr_func_t func, void *user_data)
 DECL|bt_gatt_register|function|void bt_gatt_register(const struct bt_gatt_attr *attrs, size_t count)
 DECL|db|variable|db
 DECL|end_handle|member|uint16_t end_handle;
+DECL|gatt_ccc_changed|function|static void gatt_ccc_changed(struct _bt_gatt_ccc *ccc)
 DECL|gatt_chrc|struct|struct gatt_chrc {
 DECL|gatt_incl|struct|struct gatt_incl {
 DECL|properties|member|uint8_t properties;
