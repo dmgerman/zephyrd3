8f2a6859bd4ba14804f78157472af1c40c2cc894
Johan Hedberg <johan.hedberg@intel.com>
2015-05-31 22:36:44 +0700

    Bluetooth: Add IRK storage
    Add support to store IRKs through the bt_keys API.
    
    Change-Id: I01f713f8ad3657605a8bc2738db349d25bb89649
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 40e6a1288c6931c82a6088c60c0c14d147d2a16c

--- net/bluetooth/keys.c
+++ net/bluetooth/keys.c
@@ -3,5 +3,6 @@ DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(int type, const bt_addr_
 DECL|bt_keys_foreach|macro|bt_keys_foreach
 DECL|bt_keys_get_addr|function|struct bt_keys *bt_keys_get_addr(const bt_addr_le_t *addr)
 DECL|bt_keys_get_type|function|struct bt_keys *bt_keys_get_type(int type, const bt_addr_le_t *addr)
+DECL|irks|variable|irks
 DECL|key_pool|variable|key_pool
 DECL|slave_ltks|variable|slave_ltks
--- net/bluetooth/keys.h
+++ net/bluetooth/keys.h
@@ -1,11 +1,17 @@
-DECL|BT_KEYS_ALL|enumerator|BT_KEYS_ALL = (BT_KEYS_SLAVE_LTK),
+DECL|BT_KEYS_ALL|enumerator|BT_KEYS_ALL = (BT_KEYS_SLAVE_LTK | BT_KEYS_IRK),
+DECL|BT_KEYS_IRK|enumerator|BT_KEYS_IRK = (1 << 1),
 DECL|BT_KEYS_SLAVE_LTK|enumerator|BT_KEYS_SLAVE_LTK = (1 << 0),
 DECL|addr|member|bt_addr_le_t addr;
+DECL|bt_irk|struct|struct bt_irk {
 DECL|bt_keys|struct|struct bt_keys {
 DECL|bt_ltk|struct|struct bt_ltk {
 DECL|ediv|member|uint16_t ediv;
+DECL|irk|member|struct bt_irk irk;
 DECL|keys|member|int keys;
 DECL|next|member|struct bt_keys *next;
+DECL|next|member|struct bt_keys *next;
 DECL|rand|member|uint64_t rand;
+DECL|rpa|member|bt_addr_t rpa;
 DECL|slave_ltk|member|struct bt_ltk slave_ltk;
+DECL|val|member|uint8_t val[16];
 DECL|val|member|uint8_t val[16];
