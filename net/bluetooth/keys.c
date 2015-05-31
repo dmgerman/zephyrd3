547c18b1d1dfe253bcf34811ea1f2d7dec84911e
Johan Hedberg <johan.hedberg@intel.com>
2015-05-31 22:17:19 +0700

    Bluetooth: Refactor key handling to prepare for more key types
    We'll need to store many other keys besides slave LTKs. Refactor the
    key handling code so that new types can be easily added.
    
    Change-Id: I768db9cb29e8e73e87713e59f7b80af80399f20d
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 275c0316ff066d37745065fc6da8e5c7eb9d9406

--- net/bluetooth/keys.c
+++ net/bluetooth/keys.c
@@ -1,4 +1,7 @@
-DECL|bt_keys_clear|function|void bt_keys_clear(struct bt_keys *keys)
-DECL|bt_keys_create|function|struct bt_keys *bt_keys_create(const bt_addr_le_t *addr)
-DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(const bt_addr_le_t *addr)
-DECL|key_list|variable|key_list
+DECL|bt_keys_clear|function|void bt_keys_clear(struct bt_keys *keys, int type)
+DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(int type, const bt_addr_le_t *addr)
+DECL|bt_keys_foreach|macro|bt_keys_foreach
+DECL|bt_keys_get_addr|function|struct bt_keys *bt_keys_get_addr(const bt_addr_le_t *addr)
+DECL|bt_keys_get_type|function|struct bt_keys *bt_keys_get_type(int type, const bt_addr_le_t *addr)
+DECL|key_pool|variable|key_pool
+DECL|slave_ltks|variable|slave_ltks
--- net/bluetooth/keys.h
+++ net/bluetooth/keys.h
@@ -1,7 +1,11 @@
+DECL|BT_KEYS_ALL|enumerator|BT_KEYS_ALL = (BT_KEYS_SLAVE_LTK),
+DECL|BT_KEYS_SLAVE_LTK|enumerator|BT_KEYS_SLAVE_LTK = (1 << 0),
 DECL|addr|member|bt_addr_le_t addr;
 DECL|bt_keys|struct|struct bt_keys {
 DECL|bt_ltk|struct|struct bt_ltk {
 DECL|ediv|member|uint16_t ediv;
+DECL|keys|member|int keys;
+DECL|next|member|struct bt_keys *next;
 DECL|rand|member|uint64_t rand;
 DECL|slave_ltk|member|struct bt_ltk slave_ltk;
 DECL|val|member|uint8_t val[16];
