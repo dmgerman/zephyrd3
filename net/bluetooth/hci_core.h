44c741c4a1e47fe76ef449678ccfa6298a620812
Johan Hedberg <johan.hedberg@intel.com>
2015-06-01 00:13:34 +0700

    Bluetooth: Rename bt_addr_is_rpa to bt_addr_le_is_rpa
    The input paramter is bt_addr_le_t so the function should be named
    accordingly.
    
    Change-Id: I7b796020d6e43ae1aa76a261cbfbaf7cba163029
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 2dc3554e1cab443c83f37a685d95763ead26a4fd

--- net/bluetooth/hci_core.h
+++ net/bluetooth/hci_core.h
@@ -7,10 +7,10 @@ DECL|adv_enable|member|uint8_t adv_enable;
 DECL|bdaddr|member|bt_addr_t bdaddr;
 DECL|bt_addr_cmp|function|static inline int bt_addr_cmp(const bt_addr_t *a, const bt_addr_t *b)
 DECL|bt_addr_copy|function|static inline void bt_addr_copy(bt_addr_t *dst, const bt_addr_t *src)
-DECL|bt_addr_is_rpa|function|static inline bool bt_addr_is_rpa(const bt_addr_le_t *addr)
 DECL|bt_addr_le_cmp|function|static inline int bt_addr_le_cmp(const bt_addr_le_t *a, const bt_addr_le_t *b)
 DECL|bt_addr_le_copy|function|static inline void bt_addr_le_copy(bt_addr_le_t *dst, const bt_addr_le_t *src)
 DECL|bt_addr_le_is_identity|function|static inline bool bt_addr_le_is_identity(const bt_addr_le_t *addr)
+DECL|bt_addr_le_is_rpa|function|static inline bool bt_addr_le_is_rpa(const bt_addr_le_t *addr)
 DECL|bt_dev|struct|struct bt_dev {
 DECL|bt_keys|struct|struct bt_keys {
 DECL|bt_ltk|struct|struct bt_ltk {
