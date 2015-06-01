878ef90fefe196997f629fb23b3858de49679363
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-06-01 16:52:11 -0400

    irq_connect for x86 uses static stub array
    In order to provide the same irq_connect() on all platforms
    on x86, irq_connect() now uses a static array of interrupt
    stubs. Device driver does not need to provide interrupt stub
    to irq_connect() function.
    
    Add NUM_DYNAMIC_STUBS configuration parameter, the number
    of interrupt stubs used for dynamic interrupt registration.
    
    Modify tests for unified interrupt register API
    
    Tests that deal with interrupts are modified to work
    with the new interrupt registration API.
    
    Add CONFIG_NUM_DYNAMIC_STUBS option to dynamic interrupt projects
    
    Projects that use dynamic interrupt handler registration on x86
    have to include CONFIG_NUM_DYNAMIC_STUBS parameter in the
    configuration.
    
    Change-Id: Ic90c726485521a57cf695fd3edc8cac85d0b827d
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 660878e4e207c205467c74dd993e02b14d9998f9

--- arch/x86/core/intconnect.c
+++ arch/x86/core/intconnect.c
@@ -1,12 +1,17 @@
+DECL|ALL_DYNAMIC_STUBS|macro|ALL_DYNAMIC_STUBS
+DECL|ALL_DYNAMIC_STUBS|macro|ALL_DYNAMIC_STUBS
 DECL|STUB_PTR|macro|STUB_PTR
 DECL|VEC_ALLOC_NUM_INTS|macro|VEC_ALLOC_NUM_INTS
 DECL|_IntVecAlloc|function|int _IntVecAlloc(unsigned int priority)
 DECL|_IntVecMarkAllocated|function|void _IntVecMarkAllocated(unsigned int vector)
 DECL|_IntVecMarkFree|function|void _IntVecMarkFree(unsigned int vector)
 DECL|_IntVecSet|function|void _IntVecSet(unsigned int vector, /* interrupt vector: 0 to 255 on IA-32 */ void (*routine)(void *), unsigned int dpl /* priv level for interrupt-gate descriptor */ )
+DECL|_STUB_AVAIL|macro|_STUB_AVAIL
 DECL|_SpuriousIntHandler|variable|_SpuriousIntHandler
 DECL|_SpuriousIntNoErrCodeHandler|variable|_SpuriousIntNoErrCodeHandler
 DECL|_dummy_exception_vector_stub|variable|_dummy_exception_vector_stub
 DECL|_dummy_spurious_interrupt|variable|_dummy_spurious_interrupt
+DECL|_int_stub_alloc|function|static int _int_stub_alloc(void)
+DECL|dynamic_stubs|variable|dynamic_stubs
 DECL|interrupt_vectors_allocated|variable|interrupt_vectors_allocated
-DECL|irq_connect|function|int irq_connect(unsigned int irq, /* virtualized IRQ to connect to */ unsigned int priority, /* requested priority of interrupt */ void (*routine)(void *parameter), /* C interrupt handler */ void *parameter, /* parameter passed to C routine */
+DECL|irq_connect|function|int irq_connect(unsigned int irq, /* virtualized IRQ to connect to */ unsigned int priority, /* requested priority of interrupt */ void (*routine)(void *parameter), /* C interrupt handler */ void *parameter /* parameter passed to C routine */
