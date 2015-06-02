1f7d187e2eb6cd6ed9bb9128c47a922fdbf09c22
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2015-06-02 17:28:00 +0200

    Bluetooth: Introduce LE scan callback
    Enables sending LE discovery results to user app.
    Updates central apps to use new scan API.
    
    Change-Id: Id319c96e15554f2eed4cf507cddd114ca74aa8e5
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: d81689e915fee24a837d0a3e3d0ca763fd198511

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,6 +1,7 @@
 DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
 DECL|__packed|variable|__packed
 DECL|bt_eir|struct|struct bt_eir {
+DECL|bt_le_scan_cb_t|typedef|typedef void bt_le_scan_cb_t(const bt_addr_le_t *addr, int8_t rssi,
 DECL|data|member|uint8_t data[29];
 DECL|len|member|uint8_t len;
 DECL|type|member|uint8_t type;
--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -22,7 +22,7 @@ DECL|bt_hci_cmd_send|function|int bt_hci_cmd_send(uint16_t opcode, struct bt_buf
 DECL|bt_init|function|int bt_init(void)
 DECL|bt_recv|function|void bt_recv(struct bt_buf *buf)
 DECL|bt_start_advertising|function|int bt_start_advertising(uint8_t type, const struct bt_eir *ad, const struct bt_eir *sd)
-DECL|bt_start_scanning|function|int bt_start_scanning(uint8_t scan_type, uint8_t scan_filter)
+DECL|bt_start_scanning|function|int bt_start_scanning(uint8_t scan_filter, bt_le_scan_cb_t cb)
 DECL|bt_stop_scanning|function|int bt_stop_scanning(void)
 DECL|callback_list|variable|callback_list
 DECL|cmd_queue_init|function|static void cmd_queue_init(void)
@@ -55,3 +55,4 @@ DECL|read_local_ver_complete|function|static void read_local_ver_complete(struct
 DECL|rx_prio_fiber_id|variable|rx_prio_fiber_id
 DECL|rx_prio_fiber|function|static void rx_prio_fiber(void)
 DECL|rx_queue_init|function|static void rx_queue_init(void)
+DECL|scan_dev_found_cb|variable|scan_dev_found_cb
