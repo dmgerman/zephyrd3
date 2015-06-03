9d56023cd1eed63303a9dac9cdd3f93c608a6905
Johan Hedberg <johan.hedberg@intel.com>
2015-06-03 09:44:51 +0700

    atomic: Add bitfield API for atomic_t
    Add operations to manipulate individual bits in an atomic_t variable
    or array of atomic_t variables.
    
    Change-Id: I40133e203af36ebadbef93cfa1a9df04b62a94c9
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 2628a471fdaff47cf6ad57b8b1558bc5c36b1789

--- include/atomic.h
+++ include/atomic.h
@@ -1,4 +1,12 @@
+DECL|ATOMIC_BITS|macro|ATOMIC_BITS
+DECL|ATOMIC_ELEM|macro|ATOMIC_ELEM
 DECL|ATOMIC_INIT|macro|ATOMIC_INIT
+DECL|ATOMIC_MASK|macro|ATOMIC_MASK
 DECL|__ATOMIC_H__|macro|__ATOMIC_H__
+DECL|atomic_clear_bit|function|static inline void atomic_clear_bit(atomic_t *addr, int bit)
+DECL|atomic_set_bit|function|static inline void atomic_set_bit(atomic_t *addr, int bit)
+DECL|atomic_test_and_clear_bit|function|static inline int atomic_test_and_clear_bit(atomic_t *addr, int bit)
+DECL|atomic_test_and_set_bit|function|static inline int atomic_test_and_set_bit(atomic_t *addr, int bit)
+DECL|atomic_test_bit|function|static inline int atomic_test_bit(const atomic_t *addr, int bit)
 DECL|atomic_t|typedef|typedef int atomic_t;
 DECL|atomic_val_t|typedef|typedef atomic_t atomic_val_t;
