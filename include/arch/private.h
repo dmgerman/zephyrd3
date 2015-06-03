096ee88599b77efcea1b164236c420e35e3f48fe
Allan Stephens <allan.stephens@windriver.com>
2015-06-03 14:15:29 -0400

    Relocate declarations for public nanokernel object types
    These declarations are now co-located with the declarations
    for the routines that utilize them.
    
    Change-Id: I70940923d9e424345aeac60cb5ddd7f7a2a54734
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 24dd3044ca77584577417a7626eaa3c7130ab831

--- include/arch/private.h
+++ include/arch/private.h
@@ -3,25 +3,6 @@ DECL|_ContextEntry|typedef|typedef void (*_ContextEntry)(_ContextArg arg1,
 DECL|__NANOPRIVATE_H__|macro|__NANOPRIVATE_H__
 DECL|_context_exit|macro|_context_exit
 DECL|_nano_queue|struct|struct _nano_queue {
-DECL|base|member|uint32_t *base;
-DECL|data_q|member|struct _nano_queue data_q;
-DECL|fiber|member|tCCS *fiber;
 DECL|head|member|void *head;
-DECL|lifo|member|struct nano_lifo lifo;
-DECL|link|member|struct nano_timer *link;
-DECL|list|member|void *list;
-DECL|nano_fifo|struct|struct nano_fifo {
-DECL|nano_lifo|struct|struct nano_lifo {
-DECL|nano_sem|struct|struct nano_sem {
-DECL|nano_stack|struct|struct nano_stack {
-DECL|nano_timer|struct|struct nano_timer {
-DECL|next|member|uint32_t *next;
-DECL|nsig|member|int nsig;
-DECL|stat|member|int stat;
 DECL|tCCS|typedef|typedef struct s_CCS tCCS;
 DECL|tail|member|void *tail;
-DECL|ticks|member|uint32_t ticks;
-DECL|userData|member|void *userData;
-DECL|wait_q|member|struct _nano_queue wait_q;
-DECL|wait_q|member|struct _nano_queue wait_q;
-DECL|wait_q|member|struct _nano_queue wait_q;
--- include/nanokernel.h
+++ include/nanokernel.h
@@ -3,6 +3,25 @@ DECL|NANO_CTX_ISR|macro|NANO_CTX_ISR
 DECL|NANO_CTX_TASK|macro|NANO_CTX_TASK
 DECL|SYS_PREKERNEL_INIT|macro|SYS_PREKERNEL_INIT
 DECL|__NANOKERNEL_H__|macro|__NANOKERNEL_H__
+DECL|base|member|uint32_t *base;
+DECL|data_q|member|struct _nano_queue data_q;
+DECL|fiber|member|tCCS *fiber;
+DECL|lifo|member|struct nano_lifo lifo;
+DECL|link|member|struct nano_timer *link;
+DECL|list|member|void *list;
 DECL|nano_context_id_t|typedef|typedef struct s_CCS *nano_context_id_t;
 DECL|nano_context_type_t|typedef|typedef int nano_context_type_t;
 DECL|nano_fiber_entry_t|typedef|typedef void (*nano_fiber_entry_t)(int i1, int i2);
+DECL|nano_fifo|struct|struct nano_fifo {
+DECL|nano_lifo|struct|struct nano_lifo {
+DECL|nano_sem|struct|struct nano_sem {
+DECL|nano_stack|struct|struct nano_stack {
+DECL|nano_timer|struct|struct nano_timer {
+DECL|next|member|uint32_t *next;
+DECL|nsig|member|int nsig;
+DECL|stat|member|int stat;
+DECL|ticks|member|uint32_t ticks;
+DECL|userData|member|void *userData;
+DECL|wait_q|member|struct _nano_queue wait_q;
+DECL|wait_q|member|struct _nano_queue wait_q;
+DECL|wait_q|member|struct _nano_queue wait_q;
