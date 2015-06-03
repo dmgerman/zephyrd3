7e813ef23d3c5934426762338ac1a97d989c748a
Johan Hedberg <johan.hedberg@intel.com>
2015-06-03 09:44:08 +0700

    atomic: Add const declaration to atomic_get
    The atomic_get() function is a read-only API which doesn't modify the
    value given to it. The input parameter should therefore be declared as
    const.
    
    Change-Id: I084b7beb623610412f9237fc9dae6139580e2636
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 24c44026db0e590424060c809f343792c89a3055

--- arch/x86/core/nanoatomic_nolock.c
+++ arch/x86/core/nanoatomic_nolock.c
@@ -3,7 +3,7 @@ DECL|atomic_and|function|atomic_val_t atomic_and(atomic_t *target, /* memory loc
 DECL|atomic_cas|function|int atomic_cas(atomic_t *target, /* address to be tested */ atomic_val_t oldValue, /* value to compare against */ atomic_val_t newValue /* value to set to */ )
 DECL|atomic_clear|function|atomic_val_t atomic_clear(atomic_t *target /* memory location to write to */ )
 DECL|atomic_dec|function|atomic_val_t atomic_dec(atomic_t *target /* memory location to decrement */ )
-DECL|atomic_get|function|atomic_val_t atomic_get(atomic_t *target /* memory location to read from */ )
+DECL|atomic_get|function|atomic_val_t atomic_get(const atomic_t *target /* memory location to read from */ )
 DECL|atomic_inc|function|atomic_val_t atomic_inc(atomic_t *target /* memory location to increment */ )
 DECL|atomic_nand|function|atomic_val_t atomic_nand(atomic_t *target, /* memory location to be modified */ atomic_val_t value /* value to NAND */ )
 DECL|atomic_or|function|atomic_val_t atomic_or(atomic_t *target, /* memory location to be modified */ atomic_val_t value /* value to OR */ )
