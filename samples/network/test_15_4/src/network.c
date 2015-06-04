1d7a1d98c93329c8e0839057999cd5feaa6ab790
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-06-04 12:56:48 +0300

    net: apps: Add support for microkernel in 802.15.4 test app
    The test_15_4 test application now works with microkernel also.
    
    Change-Id: I041a917be599936089af9ffaba9c27b9aeb5a481
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 914bb3f3f5aa9e0d07064561705f8400453461ff

--- samples/network/test_15_4/src/network.c
+++ samples/network/test_15_4/src/network.c
@@ -1,15 +1,28 @@
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|TASK_IPADDR|macro|TASK_IPADDR
 DECL|any_addr|variable|any_addr
 DECL|fiberEntry|function|void fiberEntry(void)
 DECL|fiberStack|variable|fiberStack
+DECL|get_context|function|static struct net_context *get_context(const struct net_addr *remote, uint16_t remote_port, const struct net_addr *local, uint16_t local_port)
 DECL|in6addr_any|variable|in6addr_any
 DECL|in6addr_loopback|variable|in6addr_loopback
+DECL|listen|function|static void listen(const char *taskname, ksem_t mySem, ksem_t otherSem, struct net_context *ctx)
 DECL|loopback_addr|variable|loopback_addr
 DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
 DECL|nanoSemFiber|variable|nanoSemFiber
 DECL|nanoSemTask|variable|nanoSemTask
+DECL|receive_data|function|static void receive_data(const char *taskname, struct net_context *ctx)
+DECL|send_data|function|static void send_data(const char *taskname, struct net_context *ctx)
+DECL|send|function|static void send(const char *taskname, ksem_t mySem, ksem_t otherSem, struct net_context *ctx)
+DECL|set_routes|function|static void set_routes()
+DECL|taskA|function|void taskA(void)
+DECL|taskB|function|void taskB(void)
