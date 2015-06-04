c52481f8e38b4f303bfa3d5eb39a2f5d79349814
Allan Stephens <allan.stephens@windriver.com>
2015-06-04 09:02:25 -0400

    Begin consolidation of arch-independent private nanokernel APIs
    Introduces nano_internal.h, which will declare all architecture-
    independent non-public nanokernel APIs. This file is automatically
    incorporated by the various architecture-specific include files
    for non-public nanokernel APIs, and will not normally be included
    directly by any other files.
    
    Change-Id: I9f3de812a5747cc720fa0ff739007315e8d07dd9
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: bff7fc17fe44e19b3876f12398c2e6b14f7766fd

--- include/arch/private.h
+++ include/arch/private.h
@@ -1,7 +1,4 @@
-DECL|_ContextArg|typedef|typedef void *_ContextArg;
-DECL|_ContextEntry|typedef|typedef void (*_ContextEntry)(_ContextArg arg1,
 DECL|__NANOPRIVATE_H__|macro|__NANOPRIVATE_H__
-DECL|_context_exit|macro|_context_exit
 DECL|_nano_queue|struct|struct _nano_queue {
 DECL|head|member|void *head;
 DECL|tCCS|typedef|typedef struct s_CCS tCCS;
---
+++ kernel/nanokernel/include/nano_internal.h
@@ -0,0 +1,4 @@
+DECL|_ContextArg|typedef|typedef void *_ContextArg;
+DECL|_ContextEntry|typedef|typedef void (*_ContextEntry)(_ContextArg arg1,
+DECL|_NANO_INTERNAL__H_|macro|_NANO_INTERNAL__H_
+DECL|_context_exit|macro|_context_exit
