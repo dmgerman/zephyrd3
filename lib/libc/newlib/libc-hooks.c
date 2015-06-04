dd3a48cbd10a5e8a3e451ee8c134f0c48c68a064
Anas Nashif <anas.nashif@intel.com>
2015-06-04 11:24:35 -0400

    newlib: Fix coding style and checkpatch warnings/errors
    Change-Id: I96d2fc5232b5babdb5d849b603dcd2fa44c50de1
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 987d64c9baf82721da3d6768ee820d6c4ef96a94

--- lib/libc/newlib/libc-hooks.c
+++ lib/libc/newlib/libc-hooks.c
@@ -1,20 +1,20 @@
 DECL|HEAP_SIZE|macro|HEAP_SIZE
-DECL|__stdin_hook_install|function|void __stdin_hook_install(unsigned char (*hook)(void)) {
-DECL|__stdout_hook_install|function|void __stdout_hook_install(int (*hook)(int)) {
-DECL|_fstat|function|int _fstat(int file, struct stat *st) {
-DECL|_stdin_hook_default|function|static unsigned char _stdin_hook_default(void) {
+DECL|__stdin_hook_install|function|void __stdin_hook_install(unsigned char (*hook)(void))
+DECL|__stdout_hook_install|function|void __stdout_hook_install(int (*hook)(int))
+DECL|_fstat|function|int _fstat(int file, struct stat *st)
+DECL|_stdin_hook_default|function|static unsigned char _stdin_hook_default(void)
 DECL|_stdin_hook|variable|_stdin_hook
-DECL|_stdout_hook_default|function|static int _stdout_hook_default(int c) {
+DECL|_stdout_hook_default|function|static int _stdout_hook_default(int c)
 DECL|_stdout_hook|variable|_stdout_hook
-DECL|close|function|int close(int file) {
-DECL|exit|function|void exit(int status) {
-DECL|fstat|function|int fstat( int fd, struct stat *buf) {
-DECL|getpid|function|int getpid() {
+DECL|close|function|int close(int file)
+DECL|exit|function|void exit(int status)
+DECL|fstat|function|int fstat(int fd, struct stat *buf)
+DECL|getpid|function|int getpid(void)
 DECL|heap_sz|variable|heap_sz
 DECL|heap|variable|heap
-DECL|isatty|function|int isatty(int file) {
-DECL|kill|function|int kill(int i, int j) {
-DECL|lseek|function|int lseek(int file, int ptr, int dir) {
-DECL|read|function|int read( int fd, char *buf, int nbytes) {
-DECL|sbrk|function|void *sbrk(int count) {
-DECL|write|function|int write(int fd, char *buf, int nbytes) {
+DECL|isatty|function|int isatty(int file)
+DECL|kill|function|int kill(int i, int j)
+DECL|lseek|function|int lseek(int file, int ptr, int dir)
+DECL|read|function|int read(int fd, char *buf, int nbytes)
+DECL|sbrk|function|void *sbrk(int count)
+DECL|write|function|int write(int fd, char *buf, int nbytes)
