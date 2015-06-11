81a52a80f20eb55d0c06e287fbfe177d71660d0e
Allan Stephens <allan.stephens@windriver.com>
2015-06-11 09:18:10 -0400

    Rename scheduler_time_slice_set() to sys_scheduler_time_slice_set()
    This API now uses an approved Zephyr OS prefix.
    
    Change-Id: I1041b982492ea7b76213e3b57cf28a9f17e7fd9d
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 6c2da862814346919e948ab4267980f9653f61c5

--- kernel/microkernel/ticker.c
+++ kernel/microkernel/ticker.c
@@ -7,7 +7,6 @@ DECL|_k_sys_clock_tick_count|variable|_k_sys_clock_tick_count
 DECL|_k_ticker|function|int _k_ticker(int event)
 DECL|_k_time_elapse|function|void _k_time_elapse(struct k_args *P)
 DECL|_sys_idle_elapsed_ticks|variable|_sys_idle_elapsed_ticks
-DECL|scheduler_time_slice_set|function|void scheduler_time_slice_set(int32_t t, /* time slice in ticks */kpriority_t p /* beginning priority level to which time slicing applies */ )
 DECL|slice_count|variable|slice_count
 DECL|slice_prio|variable|slice_prio
 DECL|slice_time|variable|slice_time
@@ -16,6 +15,7 @@ DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_increment|function|static void sys_clock_increment(int inc)
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
+DECL|sys_scheduler_time_slice_set|function|void sys_scheduler_time_slice_set(int32_t t, kpriority_t p)
 DECL|task_cycle_get_32|function|uint32_t task_cycle_get_32(void)
 DECL|task_tick_delta|function|int64_t task_tick_delta(int64_t *reftime /* pointer to reference time */)
 DECL|task_tick_get_32|function|int32_t task_tick_get_32(void)
