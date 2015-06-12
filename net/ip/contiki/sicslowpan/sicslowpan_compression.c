be9cd3cfccfe8607f425bedadd549acc3d6cd39c
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2015-06-12 17:00:09 +0300

    net: 6lowpan: Adapt compression functions to net_mbuf buffers
    6lowpan HC06 and HC01 compression/uncompression functions are
    adapted to net_mbuf buffers. HC06 compression and uncompression
    works properly. HC01 adapted to net_mbuf but functionality is
    failing due to inproper IPv6 link local addresses. buf pointers
    from net_mbuf and couple of net_mbuf calls are removed (which
    are not used at the moment and saves memory from net_mbuf).
    
    Change-Id: I2711b71b1b8df2eaac67d69275baf4b62b03f9f6
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 89c83d0162163040a564dc45f3a50a9fce3ff16c

--- net/ip/contiki/sicslowpan/sicslowpan_compression.c
+++ net/ip/contiki/sicslowpan/sicslowpan_compression.c
@@ -4,10 +4,6 @@ DECL|DEBUG|macro|DEBUG
 DECL|GET16|macro|GET16
 DECL|MAC_MAX_PAYLOAD|macro|MAC_MAX_PAYLOAD
 DECL|MAC_MAX_PAYLOAD|macro|MAC_MAX_PAYLOAD
-DECL|PACKETBUF_FRAG_DISPATCH_SIZE|macro|PACKETBUF_FRAG_DISPATCH_SIZE
-DECL|PACKETBUF_FRAG_OFFSET|macro|PACKETBUF_FRAG_OFFSET
-DECL|PACKETBUF_FRAG_PTR|macro|PACKETBUF_FRAG_PTR
-DECL|PACKETBUF_FRAG_TAG|macro|PACKETBUF_FRAG_TAG
 DECL|PACKETBUF_HC1_DISPATCH|macro|PACKETBUF_HC1_DISPATCH
 DECL|PACKETBUF_HC1_ENCODING|macro|PACKETBUF_HC1_ENCODING
 DECL|PACKETBUF_HC1_HC_UDP_CHKSUM|macro|PACKETBUF_HC1_HC_UDP_CHKSUM
@@ -34,12 +30,7 @@ DECL|SET16|macro|SET16
 DECL|SICSLOWPAN_COMPRESSION|macro|SICSLOWPAN_COMPRESSION
 DECL|SICSLOWPAN_COMPRESSION|macro|SICSLOWPAN_COMPRESSION
 DECL|SICSLOWPAN_IP_BUF|macro|SICSLOWPAN_IP_BUF
-DECL|SICSLOWPAN_MAX_MAC_TRANSMISSIONS|macro|SICSLOWPAN_MAX_MAC_TRANSMISSIONS
-DECL|SICSLOWPAN_MAX_MAC_TRANSMISSIONS|macro|SICSLOWPAN_MAX_MAC_TRANSMISSIONS
 DECL|SICSLOWPAN_UDP_BUF|macro|SICSLOWPAN_UDP_BUF
-DECL|TCP_ACK|macro|TCP_ACK
-DECL|TCP_CTL|macro|TCP_CTL
-DECL|TCP_FIN|macro|TCP_FIN
 DECL|UIP_ICMP_BUF|macro|UIP_ICMP_BUF
 DECL|UIP_IP_BUF|macro|UIP_IP_BUF
 DECL|UIP_LOG|macro|UIP_LOG
@@ -50,9 +41,9 @@ DECL|addr_context_lookup_by_number|function|addr_context_lookup_by_number(uint8_
 DECL|addr_context_lookup_by_prefix|function|addr_context_lookup_by_prefix(uip_ipaddr_t *ipaddr)
 DECL|addr_contexts|variable|addr_contexts
 DECL|compress_addr_64|function|compress_addr_64(uint8_t bitpos, uip_ipaddr_t *ipaddr, uip_lladdr_t *lladdr)
-DECL|compress_hdr_hc06|function|compress_hdr_hc06(struct net_buf *mbuf, struct net_buf *buf, linkaddr_t *link_destaddr)
+DECL|compress_hdr_hc06|function|compress_hdr_hc06(struct net_mbuf *mbuf, struct net_buf *buf, linkaddr_t *link_destaddr)
 DECL|compress_hdr_hc1|function|compress_hdr_hc1(struct net_mbuf *mbuf, struct net_buf *buf, linkaddr_t *link_destaddr)
-DECL|compress_hdr_ipv6|function|compress_hdr_ipv6(struct net_mbuf *mbuf, struct net_buf *buf, linkaddr_t *link_destaddr)
+DECL|compress_hdr_ipv6|function|compress_hdr_ipv6(struct net_buf *buf)
 DECL|compress|function|static int compress(struct net_buf *buf)
 DECL|context|variable|context
 DECL|hc06_ptr|variable|hc06_ptr
@@ -67,6 +58,7 @@ DECL|unc_ctxconf|variable|unc_ctxconf
 DECL|unc_llconf|variable|unc_llconf
 DECL|unc_mxconf|variable|unc_mxconf
 DECL|uncompress_addr|function|uncompress_addr(uip_ipaddr_t *ipaddr, uint8_t const prefix[], uint8_t pref_post_count, uip_lladdr_t *lladdr)
-DECL|uncompress_hdr_hc06|function|uncompress_hdr_hc06(struct net_buf *buf, uint16_t ip_len)
-DECL|uncompress_hdr_hc1|function|uncompress_hdr_hc1(struct net_buf *buf, uint16_t ip_len)
+DECL|uncompress_hdr_hc06|function|uncompress_hdr_hc06(struct net_mbuf *mbuf, struct net_buf *ibuf)
+DECL|uncompress_hdr_hc1|function|uncompress_hdr_hc1(struct net_mbuf *mbuf, struct net_buf *ibuf)
+DECL|uncompress_hdr_ipv6|function|uncompress_hdr_ipv6(struct net_buf *buf)
 DECL|uncompress|function|static int uncompress(struct net_buf *buf)
