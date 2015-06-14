10a99fb208c860f491a47bb68a311af302df4fd0
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-06-14 14:23:49 -0400

    test_timer: test timeouts/timers in microkernel
    Simple tests to verify whether the microkernel timer system works
    alongside the nanokernel timeouts and timers system.
    
    Once the timer test is started, the nanokernel fifo timeout test is
    started in parallel: each other's timeout times should not be affected
    by the other. Also, a fiber is started, pending on a nanokernel timer,
    and awakens in the middle of the microkernel timers expiring.
    
    Change-Id: I05b307e58ac942961a4163cbe845a9bb732ecfb7
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: ebe62ab63df96415bdc364ef2f9ba1410c0ac56b

--- samples/microkernel/test/test_timer/src/timer.c
+++ samples/microkernel/test/test_timer/src/timer.c
@@ -1,5 +1,6 @@
 DECL|NTIMERS|macro|NTIMERS
 DECL|RegressionTaskEntry|function|void RegressionTaskEntry(void)
+DECL|TEST_NANO_TIMERS_DELAY|macro|TEST_NANO_TIMERS_DELAY
 DECL|WITHIN_ERROR|macro|WITHIN_ERROR
 DECL|pTimer|variable|pTimer
 DECL|testLowTimerDoesNotStart|function|int testLowTimerDoesNotStart(void)
@@ -7,3 +8,7 @@ DECL|testLowTimerGet|function|int testLowTimerGet(void)
 DECL|testLowTimerOneShot|function|int testLowTimerOneShot(void)
 DECL|testLowTimerPeriodicity|function|int testLowTimerPeriodicity(void)
 DECL|testLowTimerStop|function|int testLowTimerStop(void)
+DECL|test_nano_timeouts|function|void test_nano_timeouts(void)
+DECL|test_nano_timers_sem|variable|test_nano_timers_sem
+DECL|test_nano_timers_stack|variable|test_nano_timers_stack
+DECL|test_nano_timers|function|static void test_nano_timers(int unused1, int unused2)
