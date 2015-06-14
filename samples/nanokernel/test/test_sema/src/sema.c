fa1bef74d6d264c2eb67241ea342d2474681f6f4
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-06-14 13:44:34 -0400

    test_sema: test timeout APIs
    Test the nano_xxx_sem_wait_timeout() APIs.
    
    First, the task waits with a timeout and times out. Then it wait with a
    timeout, but gets the semaphore in time.
    
    Then, multiple timeout tests are done for the fibers, to test the
    ordering of queueing/dequeueing when timeout occurs, first on one
    semaphore, then on multiple semaphores.
    
    Finally, multiple fibers pend on one semaphore, and they all get the
    semaphore in time, except the last one: this tests that the timeout is
    recomputed correctly when timeouts are aborted.
    
    Change-Id: I73c174e6f15f38377a14573cb7f531e5e6ca4c07
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 76103ecc2b858b705b0e74fc575c341251c49fd2

--- samples/nanokernel/test/test_sema/src/sema.c
+++ samples/nanokernel/test/test_sema/src/sema.c
@@ -8,6 +8,7 @@ DECL|STS_FIBER_WOKE_TASK|enumerator|STS_FIBER_WOKE_TASK,
 DECL|STS_INIT|enumerator|STS_INIT = -1,
 DECL|STS_ISR_WOKE_TASK|enumerator|STS_ISR_WOKE_TASK
 DECL|STS_TASK_WOKE_FIBER|enumerator|STS_TASK_WOKE_FIBER,
+DECL|TIMEOUT_ORDER_NUM_FIBERS|macro|TIMEOUT_ORDER_NUM_FIBERS
 DECL|_trigger_nano_isr_sem_give|variable|_trigger_nano_isr_sem_give
 DECL|_trigger_nano_isr_sem_take|variable|_trigger_nano_isr_sem_take
 DECL|data|member|int data; /* data */
@@ -21,16 +22,37 @@ DECL|initNanoObjects|function|void initNanoObjects(void)
 DECL|isrSemInfo|variable|isrSemInfo
 DECL|isr_sem_give|function|void isr_sem_give(void *data)
 DECL|isr_sem_take|function|void isr_sem_take(void *data)
+DECL|link_in_fifo|member|void *link_in_fifo;
+DECL|link_in_fifo|member|void *link_in_fifo;
 DECL|main|function|void main(void)
 DECL|multi_waiters|variable|multi_waiters
+DECL|q_order|member|int q_order;
 DECL|reply_multi_waiters|variable|reply_multi_waiters
+DECL|reply_packet|struct|struct reply_packet {
+DECL|reply|member|int reply;
 DECL|semTestState|variable|semTestState
+DECL|sem_timeout|variable|sem_timeout
+DECL|sem|member|struct nano_sem *sem;
 DECL|sem|member|struct nano_sem *sem; /* ptr to semaphore */
 DECL|testSemFiberNoWait|function|int testSemFiberNoWait(void)
 DECL|testSemIsrNoWait|function|int testSemIsrNoWait(void)
 DECL|testSemTaskNoWait|function|int testSemTaskNoWait(void)
 DECL|testSemWait|function|int testSemWait(void)
 DECL|testSem|variable|testSem
+DECL|test_fiber_give_timeout|function|static void test_fiber_give_timeout(int sem, int timeout)
+DECL|test_fiber_pend_and_get_sem|function|static void test_fiber_pend_and_get_sem(int data, int unused)
+DECL|test_fiber_pend_and_timeout|function|static void test_fiber_pend_and_timeout(int data, int unused)
+DECL|test_fiber_ticks_special_values|function|static void test_fiber_ticks_special_values(int packet, int special_value)
+DECL|test_multiple_fibers_get_sem|function|static int test_multiple_fibers_get_sem(struct timeout_order_data *test_data,int test_data_size)
+DECL|test_multiple_fibers_pending|function|static int test_multiple_fibers_pending(struct timeout_order_data *test_data,int test_data_size)
 DECL|test_multiple_waiters|function|static int test_multiple_waiters(void)
+DECL|test_timeout|function|static int test_timeout(void)
+DECL|timeout_order_data_mult_sem|variable|timeout_order_data_mult_sem
+DECL|timeout_order_data|struct|struct timeout_order_data {
+DECL|timeout_order_data|variable|timeout_order_data
+DECL|timeout_order_fifo|variable|timeout_order_fifo
+DECL|timeout_order|member|int timeout_order;
+DECL|timeout_stacks|variable|timeout_stacks
+DECL|timeout|member|int32_t timeout;
 DECL|timerData|variable|timerData
 DECL|timer|variable|timer
