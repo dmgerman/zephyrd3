1fddfb77eee22d32597b3ff8c0b8241c44e7ec4c
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-06-14 12:15:05 -0400

    dlist: enhance library and make all functions inline
    The old doubly-linked list implementation had only support for very
    basic operations, i.e. add at head and remove.
    
    The new implementation is highly enhanced, allowing these operations:
    
    - initialize list
    - check if a node is the list's head
    - check if a node is the list's tail
    - check if the list is empty
    - get a reference to the head item in the list
    - get a reference to the next item in the list
    - add node to tail of list
    - add node to head of list
    - insert node after a node
    - insert node before a node
    - insert node at position based on condition
    - remove a specific node from a list
    - get the first node in a list
    
    The implementation is completely inline, since the operations are rather
    simple and do not warrant a function call.
    
    Change-Id: Id85c04e67f6a88b005390172f1276daa12bcf8ce
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: ca51a72133ed8b8ecfbfc69730752bab4e572829

--- include/misc/dlist.h
+++ include/misc/dlist.h
@@ -1,8 +1,21 @@
-DECL|_DLIST_H|macro|_DLIST_H
-DECL|dlist_t|typedef|} dlist_t, dnode_t;
-DECL|dnode_s|struct|typedef struct dnode_s {
-DECL|dnode_t|typedef|} dlist_t, dnode_t;
-DECL|head|member|struct dnode_s *head; /* ptr to head of list (dlist_t) */
-DECL|next|member|struct dnode_s *next; /* ptr to next node (dnode_t) */
-DECL|prev|member|struct dnode_s *prev; /* ptr to previous node (dnode_t) */
-DECL|tail|member|struct dnode_s *tail; /* ptr to tail of list (dlist_t) */
+DECL|_dnode|struct|struct _dnode {
+DECL|_misc_dlist__h_|macro|_misc_dlist__h_
+DECL|head|member|struct _dnode *head; /* ptr to head of list (sys_dlist_t) */
+DECL|next|member|struct _dnode *next; /* ptr to next node (sys_dnode_t) */
+DECL|prev|member|struct _dnode *prev; /* ptr to previous node (sys_dnode_t) */
+DECL|sys_dlist_append|function|static inline void sys_dlist_append(sys_dlist_t *list, sys_dnode_t *node)
+DECL|sys_dlist_get|function|static inline sys_dnode_t *sys_dlist_get(sys_dlist_t *list)
+DECL|sys_dlist_init|function|static inline void sys_dlist_init(sys_dlist_t *list)
+DECL|sys_dlist_insert_after|function|static inline void sys_dlist_insert_after(sys_dlist_t *list,sys_dnode_t *insert_point, sys_dnode_t *node)
+DECL|sys_dlist_insert_at|function|static inline void sys_dlist_insert_at(sys_dlist_t *list, sys_dnode_t *node,int (*cond)(sys_dnode_t *, void *), void *data)
+DECL|sys_dlist_insert_before|function|static inline void sys_dlist_insert_before(sys_dlist_t *list,sys_dnode_t *insert_point, sys_dnode_t *node)
+DECL|sys_dlist_is_empty|function|static inline int sys_dlist_is_empty(sys_dlist_t *list)
+DECL|sys_dlist_is_head|function|static inline int sys_dlist_is_head(sys_dlist_t *list, sys_dnode_t *node)
+DECL|sys_dlist_is_tail|function|static inline int sys_dlist_is_tail(sys_dlist_t *list, sys_dnode_t *node)
+DECL|sys_dlist_peek_head|function|static inline sys_dnode_t *sys_dlist_peek_head(sys_dlist_t *list)
+DECL|sys_dlist_peek_next|function|static inline sys_dnode_t *sys_dlist_peek_next(sys_dlist_t *list,sys_dnode_t *node)
+DECL|sys_dlist_prepend|function|static inline void sys_dlist_prepend(sys_dlist_t *list, sys_dnode_t *node)
+DECL|sys_dlist_remove|function|static inline void sys_dlist_remove(sys_dnode_t *node)
+DECL|sys_dlist_t|typedef|typedef struct _dnode sys_dlist_t;
+DECL|sys_dnode_t|typedef|typedef struct _dnode sys_dnode_t;
+DECL|tail|member|struct _dnode *tail; /* ptr to tail of list (sys_dlist_t) */
