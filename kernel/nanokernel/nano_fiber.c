d611273c7115f52e6ce4bf32ebf1a0e5a1909d75
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-06-14 14:34:26 -0400

    nano_fiber: add fiber delayed start/cancel and sleep
    This new API allows starting a fiber with a delay. It returns a handle
    to allow cancelling it before the fiber starts, via the cancel API.
    
    It also adds an API allowing a fiber to sleep for a given amount of
    ticks.
    
    NOTE: CONFIG_NANO_TIMEOUTS must be enabled.
    
    Change-Id: I608dc47b5f08321cfd0c1dd9bb18d1d77eab87e3
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: d131bae6763b4eb565e713c3a443489cecff4625

--- kernel/nanokernel/nano_fiber.c
+++ kernel/nanokernel/nano_fiber.c
@@ -2,4 +2,7 @@ DECL|_fiber_start|function|void _fiber_start(char *pStack, unsigned stackSize, /
 DECL|_nano_fiber_schedule|function|void _nano_fiber_schedule(tCCS *ccs)
 DECL|_nano_fiber_swap|function|FUNC_NORETURN void _nano_fiber_swap(void)
 DECL|fiber_abort|function|FUNC_NORETURN void fiber_abort(void)
+DECL|fiber_delayed_start_cancel|function|void fiber_delayed_start_cancel(void *handle)
+DECL|fiber_delayed_start|function|void *fiber_delayed_start(char *stack, unsigned int stack_size_in_bytes,nano_fiber_entry_t entry_point, int param1, int param2, unsigned int priority, unsigned int options, int32_t timeout_in_ticks)
+DECL|fiber_sleep|function|void fiber_sleep(int32_t timeout_in_ticks)
 DECL|fiber_yield|function|void fiber_yield(void)
