307af1b9a04dc638c42ccee566f1ef97ebf49c04
Johan Hedberg <johan.hedberg@intel.com>
2015-06-16 17:31:09 +0300

    Bluetooth: Move HCI driver API to dedicated header file
    This makes it clearer that this is a specialized API not intended for
    general app usage (unlike most other things in bluetooth.h).
    
    Change-Id: I0ce1d5903610a9b3a99cf0be9f0e1462b04bf45a
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 9bea2c3cfc0c4ebd70e787d6c564a2238c173dd1

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,10 +1,6 @@
 DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
 DECL|__packed|variable|__packed
-DECL|bt_driver|struct|struct bt_driver {
 DECL|bt_eir|struct|struct bt_eir {
 DECL|data|member|uint8_t data[29];
-DECL|head_reserve|member|size_t head_reserve;
 DECL|len|member|uint8_t len;
-DECL|open|member|int (*open)(void);
-DECL|send|member|int (*send)(struct bt_buf *buf);
 DECL|type|member|uint8_t type;
---
+++ include/bluetooth/driver.h
@@ -0,0 +1,5 @@
+DECL|__BT_DRIVER_H|macro|__BT_DRIVER_H
+DECL|bt_driver|struct|struct bt_driver {
+DECL|head_reserve|member|size_t head_reserve;
+DECL|open|member|int (*open)(void);
+DECL|send|member|int (*send)(struct bt_buf *buf);
