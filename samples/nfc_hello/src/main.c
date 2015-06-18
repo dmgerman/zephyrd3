da0fd60badd033d09e8881968ef536d018b7b8ff
Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
2015-06-18 09:49:38 +0300

    nfc: Dummy app for signal-through on UART1 for x86
    Change-Id: I88227932302c747f837cf391b937ef9f6da14f4e
    Signed-off-by: Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
    
    Former-commit-id: 0582d704f9298e4f1e38caf0d1d776af4da4168e

---
+++ samples/nfc_hello/src/main.c
@@ -0,0 +1,13 @@
+DECL|BUF_MAXSIZE|macro|BUF_MAXSIZE
+DECL|D|macro|D
+DECL|MSEC|macro|MSEC
+DECL|UART1_INT_PRI|macro|UART1_INT_PRI
+DECL|UART1_IRQ|macro|UART1_IRQ
+DECL|UART1|macro|UART1
+DECL|buf|variable|buf
+DECL|htonl|function|static inline const uint32_t htonl(uint32_t x)
+DECL|main|function|void main(void)
+DECL|msg_dump|function|static void msg_dump(const char *s, uint8_t *data, unsigned len)
+DECL|nci_reset|variable|nci_reset
+DECL|uart1_init|function|static void uart1_init(void)
+DECL|uart1_isr|function|static void uart1_isr(void *x)
