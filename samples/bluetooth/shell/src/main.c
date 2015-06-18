6264743a8aa5f9a2da57318435ae929f8ad1aeae
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2015-06-18 10:57:14 +0200

    Bluetooth: Enable basic interaction with LE scan
    Adds new commands to shell app allowing turn on/off active LE scan.
    App uses scan results callback to get feedback when found any
    advertisers around.
    The dummy app's callback prints out info about findings.
    
    Change-Id: Ifffe77b804230c19bbbcbc4f38e47210c835106d
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: b40b3363e42dda9f82cb8cc98d6ac51b553f9cd8

--- samples/bluetooth/shell/src/main.c
+++ samples/bluetooth/shell/src/main.c
@@ -1,9 +1,13 @@
+DECL|cmd_active_scan_on|function|static void cmd_active_scan_on()
 DECL|cmd_connect_le|function|static void cmd_connect_le(int argc, char *argv[])
 DECL|cmd_disconnect|function|static void cmd_disconnect(int argc, char *argv[])
 DECL|cmd_init|function|static void cmd_init(int argc, char *argv[])
+DECL|cmd_scan_off|function|static void cmd_scan_off()
+DECL|cmd_scan|function|static void cmd_scan(int argc, char *argv[])
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
 DECL|conns|variable|conns
+DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t evtype, const uint8_t *ad, uint8_t len)
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
 DECL|mainloop|function|void mainloop(void)
 DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
