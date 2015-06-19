b7311c2a392c33f773e904bad5990f5aa1fb237e
Allan Stephens <allan.stephens@windriver.com>
2015-06-19 10:09:45 -0400

    Rename microkernel/cmdPkt.h to command_packet.h
    Now spells out the name of the associated microkernel object in full.
    
    Change-Id: I608404a762cf9d1100d58fe80c82c0e40ada2ff1
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: bba56b1c8a2c621faf50381f527577259ef4b538

--- include/microkernel/command_packet.h
+++ include/microkernel/command_packet.h
@@ -1,7 +1,7 @@
 DECL|CMD_PKT_SET_INSTANCE|macro|CMD_PKT_SET_INSTANCE
 DECL|CMD_PKT_SET|macro|CMD_PKT_SET
 DECL|CMD_PKT_SIZE_IN_WORDS|macro|CMD_PKT_SIZE_IN_WORDS
-DECL|__INCcmdPkth|macro|__INCcmdPkth
+DECL|_COMMAND_PACKET_H|macro|_COMMAND_PACKET_H
 DECL|cmdPkt_t|typedef|typedef uint32_t cmdPkt_t[CMD_PKT_SIZE_IN_WORDS];
 DECL|cmdPkt|member|cmdPkt_t cmdPkt[]; /* array of command packets */
 DECL|cmd_pkt_set|struct|struct cmd_pkt_set {
