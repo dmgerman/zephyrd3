ef5efae8e0ed578e093543da375ee3f57061133e
Allan Stephens <allan.stephens@windriver.com>
2015-06-19 09:59:15 -0400

    Rename microkernel/mail.h to mailbox.h
    Now spells out the name of the associated microkernel object in full.
    
    Change-Id: Iee532e9fbbcafbf9ba44c91de0894b7181285e8e
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 4f4a8170ee0328cf55a634d344001cbe55cdfc2e

--- include/microkernel/mailbox.h
+++ include/microkernel/mailbox.h
@@ -1,4 +1,4 @@
-DECL|MAIL_H|macro|MAIL_H
+DECL|_MAILBOX_H|macro|_MAILBOX_H
 DECL|task_mbox_data_get_async_block_wait_timeout|macro|task_mbox_data_get_async_block_wait_timeout
 DECL|task_mbox_data_get_async_block_wait|macro|task_mbox_data_get_async_block_wait
 DECL|task_mbox_data_get_async_block|macro|task_mbox_data_get_async_block
