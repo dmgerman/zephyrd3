63b04d775d4f165da82b41dc64f32f4e8e34fd0b
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-06-24 08:22:56 -0700

    init: Add init system calls to micro and nano kernel init
    Add the call sites for the various init levels defined by the init
    system.
    
    These call sites are noops if there is no init proceedure registered
    at a given init level.
    
    pre_app_init has been renamed to app_early_init and late_initconfig to
    app_late_init to better reflect the ordering and intended use of these
    init levels
    
    Change-Id: I71e38d936a97da8fe163f4b7cf0ce6725f1c903e
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Former-commit-id: ac3fdf0be1187a707436a7231d8d8587b1a6b373

--- kernel/nanokernel/nano_init.c
+++ kernel/nanokernel/nano_init.c
@@ -14,6 +14,8 @@ DECL|__idle_tsc|variable|__idle_tsc
 DECL|__main_tsc|variable|__main_tsc
 DECL|__start_tsc|variable|__start_tsc
 DECL|_interrupt_stack|variable|_interrupt_stack
+DECL|_main|function|static void _main(void)
+DECL|_main|function|static void _main(void)
 DECL|build_timestamp|variable|build_timestamp
 DECL|initialize_nano_timeouts|macro|initialize_nano_timeouts
 DECL|initialize_nano_timeouts|macro|initialize_nano_timeouts
