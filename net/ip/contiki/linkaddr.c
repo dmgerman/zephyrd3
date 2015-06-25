a100df007d5fc7b105125827224898e5470a077d
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-06-25 12:35:25 +0300

    net: Allow 6 byte link addresses
    Need to define link address length for ethernet MAC addresses.
    This is needed when connecting to host from qemu using slip
    and when bluetooth connectivity will be used (in the future).
    
    Change-Id: I3881e970cec52017fd4120a48ff7a99cc5559a0a
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 62852f0ea7b4da8086fb5cda9184516939bd0395

--- net/ip/contiki/linkaddr.c
+++ net/ip/contiki/linkaddr.c
@@ -3,4 +3,5 @@ DECL|linkaddr_copy|function|linkaddr_copy(linkaddr_t *dest, const linkaddr_t *sr
 DECL|linkaddr_node_addr|variable|linkaddr_node_addr
 DECL|linkaddr_null|variable|linkaddr_null
 DECL|linkaddr_null|variable|linkaddr_null
+DECL|linkaddr_null|variable|linkaddr_null
 DECL|linkaddr_set_node_addr|function|linkaddr_set_node_addr(linkaddr_t *t)
