6c8732006fcbafd95a08d06d239dac7b4e3bc6d0
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-06-25 16:38:48 +0300

    net: apps: Simple echo server that sends back the data
    The echo server is listening on port 4242 and when it receives
    UDP packet, it will reverse the data and send it back to the
    caller. This can be used to verify that the IP stack is doing
    something useful.
    
    Change-Id: Ibbc48c21f2513d59480c5a3bb34c775eb3df8170
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c16d29ec0f122b134bdb77c2df16f07e48795208

---
+++ samples/network/echo_server/src/echo-server.c
@@ -0,0 +1,25 @@
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_PORT|macro|MY_PORT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|fiberEntry|function|void fiberEntry(void)
+DECL|fiberStack|variable|fiberStack
+DECL|get_context|function|static inline struct net_context *get_context(const struct net_addr *addr)
+DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|init_server|function|static inline void init_server()
+DECL|main|function|void main(void)
+DECL|my_mac|variable|my_mac
+DECL|nanoSemFiber|variable|nanoSemFiber
+DECL|nanoSemTask|variable|nanoSemTask
+DECL|peer_mac|variable|peer_mac
+DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *ctx)
+DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
+DECL|taskA|function|void taskA(void)
+DECL|taskB|function|void taskB(void)
