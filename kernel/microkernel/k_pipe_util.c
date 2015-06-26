0331246ae4987f37a7616da0ff271336cfdfdafd
Peter Mitsis <peter.mitsis@windriver.com>
2015-06-26 14:45:39 -0400

    Rename ChxxxGetTimeType() to _k_pipe_time_type_get()
    This private API now follows Zephyr OS naming conventions.
    
    Change-Id: I3cf5042acff7b299ffee4189dde1a3ef403b0d84
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 999e13ca074b06f623afc2a31f8a6fe534381bf3

--- kernel/microkernel/k_pipe_util.c
+++ kernel/microkernel/k_pipe_util.c
@@ -1,6 +1,5 @@
 DECL|CalcAvailWriterData|function|int CalcAvailWriterData(struct k_args *pWriterList)
 DECL|CalcFreeReaderSpace|function|int CalcFreeReaderSpace(struct k_args *pReaderList)
-DECL|ChxxxGetTimeType|function|TIME_TYPE ChxxxGetTimeType(K_ARGS_ARGS *pChxxx)
 DECL|ChxxxSetTimeType|function|void ChxxxSetTimeType(K_ARGS_ARGS *pChxxx, TIME_TYPE TimeType)
 DECL|DeListWaiter|function|void DeListWaiter(struct k_args *pReqProc)
 DECL|_ALL_OPT|macro|_ALL_OPT
@@ -9,5 +8,6 @@ DECL|_k_pipe_option_set|function|void _k_pipe_option_set(K_ARGS_ARGS *pChxxx, K_
 DECL|_k_pipe_request_status_set|function|void _k_pipe_request_status_set(struct k_chproc *pChProc, CHREQ_STATUS Status)
 DECL|_k_pipe_request_type_get|function|REQ_TYPE _k_pipe_request_type_get(K_ARGS_ARGS *pChxxx)
 DECL|_k_pipe_request_type_set|function|void _k_pipe_request_type_set(K_ARGS_ARGS *pChxxx, REQ_TYPE ReqType)
+DECL|_k_pipe_time_type_get|function|TIME_TYPE _k_pipe_time_type_get(K_ARGS_ARGS *pChxxx)
 DECL|mycopypacket|function|void mycopypacket(struct k_args **out, struct k_args *in)
 DECL|myfreetimer|function|void myfreetimer(struct k_timer **ppTimer)
