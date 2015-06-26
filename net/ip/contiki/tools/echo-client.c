63c5295f36f05f15ee19fac229384fa4f7152b83
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-06-26 12:23:34 +0300

    net: tools: Simple echo client that sends data to echo server
    This client sends pre-defined UDP data to server, reads back
    the reply and verifies that the data is the same.
    This test application is only usable when running in Linux
    host and it is not meant to be run inside Zephyr.
    
    Change-Id: I6df83f3a5b1ab92c724b4d3c245738eb5c56a91f
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: a64f6995bb910e4daa2c25f53f42c8a555f19a49

---
+++ net/ip/contiki/tools/echo-client.c
@@ -0,0 +1,20 @@
+DECL|A|variable|A
+DECL|CLIENT_PORT|macro|CLIENT_PORT
+DECL|ENTRY_FAIL|macro|ENTRY_FAIL
+DECL|ENTRY_OK|macro|ENTRY_OK
+DECL|ENTRY|macro|ENTRY
+DECL|MAX_BUF_SIZE|macro|MAX_BUF_SIZE
+DECL|MAX_TIMEOUT|macro|MAX_TIMEOUT
+DECL|SERVER_PORT|macro|SERVER_PORT
+DECL|array_1280|variable|array_1280
+DECL|array_256|variable|array_256
+DECL|buf|member|const unsigned char *buf;
+DECL|data|variable|data
+DECL|expecting_reply|member|bool expecting_reply;
+DECL|foobar|variable|foobar
+DECL|len|member|int len;
+DECL|lorem_ipsum|variable|lorem_ipsum
+DECL|main|function|int main(int argc, char**argv)
+DECL|null_byte|variable|null_byte
+DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
+DECL|small_binary|variable|small_binary
