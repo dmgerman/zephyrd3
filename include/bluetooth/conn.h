0e56304033d9a5a5c162e73f5783605c06653094
Szymon Janc <szymon.janc@tieto.com>
2015-06-26 19:06:21 +0200

    Bluetooth: Remove 'none' security level
    This was same as low security and is not needed. This also match
    security levels with LE Mode 1 security level from Core Specification.
    
    Change-Id: Ia82e1f4d94c281d7e7f88f03a16a1e34eb49f710
    Signed-off-by: Szymon Janc <szymon.janc@tieto.com>
    
    Former-commit-id: 3e1023cdcd24c5ae904576fb9ccdfbecfae82e98

--- include/bluetooth/conn.h
+++ include/bluetooth/conn.h
@@ -2,7 +2,6 @@ DECL|BT_CONN_SEC_FIPS|enumerator|BT_CONN_SEC_FIPS,
 DECL|BT_CONN_SEC_HIGH|enumerator|BT_CONN_SEC_HIGH,
 DECL|BT_CONN_SEC_LOW|enumerator|BT_CONN_SEC_LOW,
 DECL|BT_CONN_SEC_MEDIUM|enumerator|BT_CONN_SEC_MEDIUM,
-DECL|BT_CONN_SEC_NONE|enumerator|BT_CONN_SEC_NONE,
 DECL|__BT_CONN_H|macro|__BT_CONN_H
 DECL|_next|member|struct bt_conn_cb *_next;
 DECL|bt_conn_cb|struct|struct bt_conn_cb {
