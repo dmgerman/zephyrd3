143a2bebdb404a2275862b3bc7f838281302563a
Johan Hedberg <johan.hedberg@intel.com>
2015-06-30 10:47:53 +0300

    Bluetooth: Add config option for security keys handling
    This makes sure that keys.c doesn't always have its BT_DBG logs
    enabled.
    
    Change-Id: Ia97149f08a448f55a15c3282f72afb23c495b715
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: a2241a55fd3850daf6679ee1fc58c8292b17c4aa

--- net/bluetooth/keys.c
+++ net/bluetooth/keys.c
@@ -1,3 +1,5 @@
+DECL|BT_DBG|macro|BT_DBG
+DECL|BT_DBG|macro|BT_DBG
 DECL|bt_keys_clear|function|void bt_keys_clear(struct bt_keys *keys, int type)
 DECL|bt_keys_find_irk|function|struct bt_keys *bt_keys_find_irk(const bt_addr_le_t *addr)
 DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(int type, const bt_addr_le_t *addr)
