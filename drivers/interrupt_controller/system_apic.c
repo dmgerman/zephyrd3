19d2f4f530b4bc41ac1daee0b62d935168a5d2e4
Anas Nashif <anas.nashif@intel.com>
2015-07-02 18:59:02 -0400

    MVIC: handle boards with fixed IRQ to vector mapping
    Change-Id: I319bc2a18310a6a6808fced3e142640e225236d2
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 7625b098b95864e850be3a1e5b7317075764a95e

--- drivers/interrupt_controller/system_apic.c
+++ drivers/interrupt_controller/system_apic.c
@@ -1,4 +1,13 @@
+DECL|HARDWARE_IRQ_LIMIT|macro|HARDWARE_IRQ_LIMIT
+DECL|HARDWARE_IRQ_LIMIT|macro|HARDWARE_IRQ_LIMIT
+DECL|IS_IOAPIC_IRQ|macro|IS_IOAPIC_IRQ
+DECL|IS_IOAPIC_IRQ|macro|IS_IOAPIC_IRQ
+DECL|LOAPIC_IRQ_BASE|macro|LOAPIC_IRQ_BASE
+DECL|LOAPIC_IRQ_COUNT|macro|LOAPIC_IRQ_COUNT
 DECL|_SysIntVecAlloc|function|int _SysIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority, /* get vector from <priority> group */ uint32_t flags /* interrupt flags */ )
 DECL|_SysIntVecProgram|function|void _SysIntVecProgram(unsigned int vector, unsigned int irq, uint32_t flags)
+DECL|__LocalIntVecAlloc|function|static int __LocalIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority/* get vector from <priority> group */ )
+DECL|handle_fixed_mapping|function|static inline int handle_fixed_mapping(int irq, int *vector)
+DECL|handle_fixed_mapping|function|static inline int handle_fixed_mapping(int irq, int *vector)
 DECL|irq_disable|function|void irq_disable(unsigned int irq)
 DECL|irq_enable|function|void irq_enable(unsigned int irq)
