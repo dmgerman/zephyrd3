d30276196c158f7b5a878982f69902b23d6bc91b
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2015-07-02 17:06:33 +0300

    net: tests: Update 15.4 test with proper link local addresses
    6lowpan HC1 compression technique expecting proper link local
    addresses for compression, otherwise compression fails.
    
    Change-Id: I083b749a7b50d93d65ea56b80b9ed6245b571200
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 5e571ec7fb92dc1dc832c69166cc6e6d62828544

--- samples/network/test_15_4/src/network.c
+++ samples/network/test_15_4/src/network.c
@@ -1,3 +1,4 @@
+DECL|DEST_PORT|macro|DEST_PORT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
@@ -6,25 +7,26 @@ DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SRC_PORT|macro|SRC_PORT
 DECL|STACKSIZE|macro|STACKSIZE
-DECL|TASK_IPADDR|macro|TASK_IPADDR
 DECL|any_addr|variable|any_addr
+DECL|dest_mac|variable|dest_mac
 DECL|fiberEntry|function|void fiberEntry(void)
 DECL|fiberStack|variable|fiberStack
 DECL|get_context|function|static struct net_context *get_context(const struct net_addr *remote, uint16_t remote_port, const struct net_addr *local, uint16_t local_port)
-DECL|in6addr_any|variable|in6addr_any
-DECL|in6addr_loopback|variable|in6addr_loopback
+DECL|in6addr_dest|variable|in6addr_dest
+DECL|in6addr_src|variable|in6addr_src
 DECL|init_test|function|static inline void init_test()
 DECL|listen|function|static void listen(const char *taskname, ksem_t mySem, ksem_t otherSem, struct net_context *ctx)
 DECL|loopback_addr|variable|loopback_addr
 DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
-DECL|my_mac|variable|my_mac
 DECL|nanoSemFiber|variable|nanoSemFiber
 DECL|nanoSemTask|variable|nanoSemTask
 DECL|receive_data|function|static void receive_data(const char *taskname, struct net_context *ctx)
 DECL|send_data|function|static void send_data(const char *taskname, struct net_context *ctx)
 DECL|send|function|static void send(const char *taskname, ksem_t mySem, ksem_t otherSem, struct net_context *ctx)
 DECL|set_routes|function|static void set_routes()
+DECL|src_mac|variable|src_mac
 DECL|taskA|function|void taskA(void)
 DECL|taskB|function|void taskB(void)
