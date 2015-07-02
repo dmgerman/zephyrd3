476a556080d4bf191a5fe544dd070f30da2d2279
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-07-02 11:39:45 +0300

    net: apps: Add IPv4 support for listener application
    Change-Id: Ifa5378463dde1bcfc4c701dc4b881175395cd9fd
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7ef1a726d424ffce0c3b533c287a97b94dcddab0

--- samples/network/listener/src/listener.c
+++ samples/network/listener/src/listener.c
@@ -1,4 +1,5 @@
 DECL|MYADDR|macro|MYADDR
+DECL|MYADDR|macro|MYADDR
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
@@ -8,14 +9,11 @@ DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
-DECL|TASK_IPADDR|macro|TASK_IPADDR
-DECL|any_addr|variable|any_addr
 DECL|fiberEntry|function|void fiberEntry(void)
 DECL|fiberStack|variable|fiberStack
-DECL|get_context|function|static struct net_context *get_context(const struct net_addr *addr)
+DECL|get_context|function|static struct net_context *get_context(void)
 DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
 DECL|main|function|void main(void)
-DECL|my_addr|variable|my_addr
 DECL|nanoSemFiber|variable|nanoSemFiber
 DECL|nanoSemTask|variable|nanoSemTask
 DECL|set_mac|function|static void set_mac(void)
