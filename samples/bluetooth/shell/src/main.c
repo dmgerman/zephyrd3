de8cc8a85bda8e316cc7258932e5e165e4005acb
Szymon Janc <szymon.janc@tieto.com>
2015-07-03 11:42:50 +0200

    Bluetooth: Add simple advertise command to shell app
    This allows to enable advertising with some fixed Advertising Data
    and Scan Reponse.
    
    Change-Id: Ie3a33a49fe8517c9e931b7805694bd9c7b05fcdb
    Signed-off-by: Szymon Janc <szymon.janc@tieto.com>
    
    Former-commit-id: d0aa85096c7c89f60555e3f265aac81d27821383

--- samples/bluetooth/shell/src/main.c
+++ samples/bluetooth/shell/src/main.c
@@ -1,4 +1,6 @@
+DECL|ad|variable|ad
 DECL|cmd_active_scan_on|function|static void cmd_active_scan_on(void)
+DECL|cmd_advertise|function|static void cmd_advertise(int argc, char *argv[])
 DECL|cmd_connect_le|function|static void cmd_connect_le(int argc, char *argv[])
 DECL|cmd_disconnect|function|static void cmd_disconnect(int argc, char *argv[])
 DECL|cmd_gatt_discover|function|static void cmd_gatt_discover(int argc, char *argv[])
@@ -16,6 +18,7 @@ DECL|discover_func|function|static uint8_t discover_func(const struct bt_gatt_at
 DECL|discover_params|variable|discover_params
 DECL|exchange_rsp|function|static void exchange_rsp(struct bt_conn *conn, uint8_t err)
 DECL|mainloop|function|void mainloop(void)
+DECL|sd|variable|sd
 DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
 DECL|uuid|variable|uuid
 DECL|xtoi|function|static int xtoi(const char *str)
