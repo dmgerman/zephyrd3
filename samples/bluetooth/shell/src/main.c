988d383da36714b829c0915ae2ee76cb29aa6fec
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-03 15:00:06 +0300

    Bluetooth: Add gatt-discover command to btshell
    This adds gatt-discover which works as follow:
    
    btshell> gatt-discover <bdaddr> <bdaddr_type> <uuid> [start_handle] [end_handle]
    bt: bt_gatt_discover (0x0010cb10): uuid 0x180d start_handle 0x0001 end_handle 0xffff
    Discover pending
    btshell> bt: bt_att_recv (0x0010ed0c): Received ATT code 0x07 len 5
    bt: att_handle_find_type_rsp (0x0010ed0c):
    bt: att_find_type_rsp (0x0010ed0c): err 0x00
    bt: att_find_type_rsp (0x0010ed0c): start_handle 0x000a end_handle 0x0011
    Discover found handle 10
    bt: bt_gatt_discover (0x0010ed0c): uuid 0x180d start_handle 0x0011 end_handle 0xffff
    bt: bt_att_recv (0x0010ed0c): Received ATT code 0x01 len 5
    bt: att_error_rsp (0x0010ed0c): request 0x06 handle 0x0011 error 0x0a
    bt: att_find_type_rsp (0x0010ed0c): err 0x0a
    Discover destroy
    
    Change-Id: Ic0b28f19e4932e26b2eae2d19f62f6be6e1efa9c
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: a52d54cc3ebb5c7823a36fc5f3c4beaa32d322db

--- samples/bluetooth/shell/src/main.c
+++ samples/bluetooth/shell/src/main.c
@@ -1,6 +1,7 @@
 DECL|cmd_active_scan_on|function|static void cmd_active_scan_on(void)
 DECL|cmd_connect_le|function|static void cmd_connect_le(int argc, char *argv[])
 DECL|cmd_disconnect|function|static void cmd_disconnect(int argc, char *argv[])
+DECL|cmd_gatt_discover|function|static void cmd_gatt_discover(int argc, char *argv[])
 DECL|cmd_gatt_exchange_mtu|function|static void cmd_gatt_exchange_mtu(int argc, char *argv[])
 DECL|cmd_init|function|static void cmd_init(int argc, char *argv[])
 DECL|cmd_scan_off|function|static void cmd_scan_off(void)
@@ -10,6 +11,11 @@ DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
 DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t evtype, const uint8_t *ad, uint8_t len)
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
+DECL|discover_destroy|function|static void discover_destroy(void *user_data)
+DECL|discover_func|function|static uint8_t discover_func(const struct bt_gatt_attr *attr, void *user_data)
+DECL|discover_params|variable|discover_params
 DECL|exchange_rsp|function|static void exchange_rsp(struct bt_conn *conn, uint8_t err)
 DECL|mainloop|function|void mainloop(void)
 DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
+DECL|uuid|variable|uuid
+DECL|xtoi|function|static int xtoi(const char *str)
