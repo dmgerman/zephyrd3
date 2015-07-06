a15d4cfb223643bdf57e42a6ddf489c9d057242f
Peter Mitsis <peter.mitsis@windriver.com>
2015-07-06 16:20:19 -0400

    Nanokernel timer uses new driver initialization
    Update nanokernel systems to use the new driver initialization model on the
    timer driver.
    
    Change-Id: I22d8619f56052f094482d73ab34c9d610492d8c0
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 27e01a060a1c4591ff830523b7642037ffa233d8

--- kernel/nanokernel/nano_sys_clock.c
+++ kernel/nanokernel/nano_sys_clock.c
@@ -14,7 +14,6 @@ DECL|nano_tick_delta_32|function|uint32_t nano_tick_delta_32(int64_t *reftime)
 DECL|nano_tick_delta|function|int64_t nano_tick_delta(int64_t *reftime)
 DECL|nano_tick_get_32|function|uint32_t nano_tick_get_32(void)
 DECL|nano_tick_get|function|int64_t nano_tick_get(void)
-DECL|nano_time_init|function|void nano_time_init(void)
 DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
