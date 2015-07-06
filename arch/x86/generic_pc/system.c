32e8d0983d0d70eefc51a9c76b8ca9d40e2c0f6f
Anas Nashif <anas.nashif@intel.com>
2015-07-06 11:26:31 -0400

    Do not check for DO_CONSOLE_INIT, use Kconfig variables instead
    This commit removes 2 levels of checking and checks directly against
    define Kconfig variables. Also reduce the code to one block and check
    only once instead of twice.
    
    This is a cosmetic change.
    
    Change-Id: Ia427d9f15a9d09ea59d0230913168060d752ae99
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: db404bbdf97cea98df4ae83f0fcb4681d5a4c820

--- arch/x86/generic_pc/system.c
+++ arch/x86/generic_pc/system.c
@@ -1,4 +1,3 @@
-DECL|DO_CONSOLE_INIT|macro|DO_CONSOLE_INIT
 DECL|_InitHardware|function|void _InitHardware(void)
 DECL|bluetooth_init|function|static void bluetooth_init(void)
 DECL|bluetooth_init|macro|bluetooth_init
--- arch/x86/quark/system.c
+++ arch/x86/quark/system.c
@@ -1,4 +1,3 @@
-DECL|DO_CONSOLE_INIT|macro|DO_CONSOLE_INIT
 DECL|_InitHardware|function|void _InitHardware(void)
 DECL|consoleInit|function|static void consoleInit(void)
 DECL|consoleInit|macro|consoleInit
