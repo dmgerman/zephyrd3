ed00c02fe4f3512200fa0739fd6636d0cfaacbc7
Peter Mitsis <peter.mitsis@windriver.com>
2015-07-06 22:26:49 -0400

    Remove obsolete SYS_PREKERNEL_INIT() macro
    The SYS_PREKERNEL_INIT() macro was a temporary item until the new driver
    initialization model was implemented.
    
    Change-Id: I052db0c9f41f54bfd0b54b50aa0b18f018be640c
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: b99194ee9869a76400b385226a773e72bba6c7ad

--- include/toolchain/gcc.h
+++ include/toolchain/gcc.h
@@ -36,7 +36,6 @@ DECL|_NODATA_SECTION|macro|_NODATA_SECTION
 DECL|__aligned|macro|__aligned
 DECL|__may_alias|macro|__may_alias
 DECL|__packed|macro|__packed
-DECL|__prekernel_init_level|macro|__prekernel_init_level
 DECL|likely|macro|likely
 DECL|subsection|variable|subsection
 DECL|unlikely|macro|unlikely
