4911d723e0693f484a69d3778c3443f259fa40d8
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2015-07-10 14:04:35 +0300

    net: 15.4: Add support for data Tx/Rx between two qemus
    Add support for 802.15.4 dummy radio driver to send frames
    through qemu uart and and receive frames through uart. So
    two qemus can send and receive 802.15.4 frames.
    
    Change-Id: I325281d04234f7ec07c66bf31691c18a9d7c4d93
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: de67ecb58b4555683598ccf65597b726e1ccd140

--- net/ip/dummy_15_4_radio.c
+++ net/ip/dummy_15_4_radio.c
@@ -1,5 +1,6 @@
 DECL|DEBUG|macro|DEBUG
 DECL|DEBUG|macro|DEBUG
+DECL|DUMMY_RADIO_15_4_FRAME_TYPE|macro|DUMMY_RADIO_15_4_FRAME_TYPE
 DECL|FOOTER_LEN|macro|FOOTER_LEN
 DECL|NETWORK_TEST_MAX_PACKET_LEN|macro|NETWORK_TEST_MAX_PACKET_LEN
 DECL|UIP_LOG|macro|UIP_LOG
@@ -9,6 +10,9 @@ DECL|dummy154radio_driver|variable|dummy154radio_driver
 DECL|get_object|function|get_object(radio_param_t param, void *dest, size_t size)
 DECL|get_value|function|get_value(radio_param_t param, radio_value_t *value)
 DECL|init|function|init(void)
+DECL|input_len|variable|input_len
+DECL|input_offset|variable|input_offset
+DECL|input|variable|input
 DECL|last_packet_timestamp|variable|last_packet_timestamp
 DECL|loopback|variable|loopback
 DECL|off|function|off(void)
@@ -17,8 +21,10 @@ DECL|pending_packet|function|pending_packet(void)
 DECL|prepare|function|prepare(const void *payload, unsigned short payload_len)
 DECL|radio_read|function|radio_read(void *buf, unsigned short buf_len)
 DECL|receiving_packet|function|receiving_packet(void)
+DECL|recv_cb|function|static uint8_t *recv_cb(uint8_t *buf, size_t *off)
 DECL|route_buf|function|static void route_buf(struct net_mbuf *buf)
 DECL|send|function|send(struct net_mbuf *buf, const void *payload, unsigned short payload_len)
 DECL|set_object|function|set_object(radio_param_t param, const void *src, size_t size)
 DECL|set_value|function|set_value(radio_param_t param, radio_value_t value)
 DECL|transmit|function|transmit(struct net_mbuf *buf, unsigned short transmit_len)
+DECL|uart_send|function|static int uart_send(unsigned char c)
