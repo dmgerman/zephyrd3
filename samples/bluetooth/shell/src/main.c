50d63450b334506cb1646e0aef522c18592f9f33
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-13 15:22:03 +0300

    Bluetooth: Add gatt-read command to btshell
    This adds gatt-read which works as follow:
    
    gatt-read <bdaddr> <bdaddr_type> <handle> [offset]
    bt: bt_gatt_read (0x0010dca0): handle 0x0010
    Read pending
    btshell> bt: bt_att_recv (0x0010fe9c): Received ATT code 0x0b len 6
    bt: att_handle_read_rsp (0x0010fe9c):
    bt: att_read_rsp (0x0010fe9c): err 0x00
    Read complete: err 0 length 5
    
    Change-Id: I38686f0e592b38bf22a7bf76fab93676b375104d
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 7ea33a436b5d2d06f3455156ce175399fc80832a

--- samples/bluetooth/shell/src/main.c
+++ samples/bluetooth/shell/src/main.c
@@ -5,6 +5,7 @@ DECL|cmd_connect_le|function|static void cmd_connect_le(int argc, char *argv[])
 DECL|cmd_disconnect|function|static void cmd_disconnect(int argc, char *argv[])
 DECL|cmd_gatt_discover|function|static void cmd_gatt_discover(int argc, char *argv[])
 DECL|cmd_gatt_exchange_mtu|function|static void cmd_gatt_exchange_mtu(int argc, char *argv[])
+DECL|cmd_gatt_read|function|static void cmd_gatt_read(int argc, char *argv[])
 DECL|cmd_init|function|static void cmd_init(int argc, char *argv[])
 DECL|cmd_scan_off|function|static void cmd_scan_off(void)
 DECL|cmd_scan|function|static void cmd_scan(int argc, char *argv[])
@@ -18,6 +19,7 @@ DECL|discover_func|function|static uint8_t discover_func(const struct bt_gatt_at
 DECL|discover_params|variable|discover_params
 DECL|exchange_rsp|function|static void exchange_rsp(struct bt_conn *conn, uint8_t err)
 DECL|mainloop|function|void mainloop(void)
+DECL|read_func|function|static void read_func(struct bt_conn *conn, int err, const void *data, uint16_t length)
 DECL|sd|variable|sd
 DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
 DECL|uuid|variable|uuid
