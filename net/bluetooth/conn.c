c392de4042f204eefd13f5a7baec7c425c24072e
Johan Hedberg <johan.hedberg@intel.com>
2015-07-15 12:31:08 +0300

    Bluetooth: Move bt_disconnect to conn.[ch] and rename it appropriately
    The bt_disconnect deals with the bt_conn object and should therefore
    reside in conn.c and be exported through conn.h. It should also have
    the appropriate bt_conn_* prefix.
    
    Change-Id: I75eb648fd9aa8ced9a991d472c319d25f4b772af
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 824135122f1f34cfb814749aa391aac8e13b61e5

--- net/bluetooth/conn.c
+++ net/bluetooth/conn.c
@@ -1,6 +1,7 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
 DECL|bt_conn_add|function|struct bt_conn *bt_conn_add(struct bt_dev *dev, const bt_addr_le_t *peer, uint8_t role)
+DECL|bt_conn_disconnect|function|int bt_conn_disconnect(struct bt_conn *conn, uint8_t reason)
 DECL|bt_conn_get_dst|function|const bt_addr_le_t *bt_conn_get_dst(const struct bt_conn *conn)
 DECL|bt_conn_get|function|struct bt_conn *bt_conn_get(struct bt_conn *conn)
 DECL|bt_conn_lookup_addr_le|function|struct bt_conn *bt_conn_lookup_addr_le(const bt_addr_le_t *peer)
@@ -12,6 +13,8 @@ DECL|bt_conn_reset_rx_state|function|static void bt_conn_reset_rx_state(struct b
 DECL|bt_conn_send|function|void bt_conn_send(struct bt_conn *conn, struct bt_buf *buf)
 DECL|bt_conn_set_auto_conn|function|void bt_conn_set_auto_conn(struct bt_conn *conn, bool auto_conn)
 DECL|bt_conn_set_state|function|void bt_conn_set_state(struct bt_conn *conn, bt_conn_state_t state)
+DECL|bt_hci_connect_le_cancel|function|static int bt_hci_connect_le_cancel(struct bt_conn *conn)
+DECL|bt_hci_disconnect|function|static int bt_hci_disconnect(struct bt_conn *conn, uint8_t reason)
 DECL|bt_security|function|int bt_security(struct bt_conn *conn, bt_security_t sec)
 DECL|conn_tx_fiber|function|static void conn_tx_fiber(int arg1, int arg2)
 DECL|conns|variable|conns
