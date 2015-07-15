df1227b4ce5db77e89c98444b6d8543b6997b49a
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-07-15 16:05:10 +0300

    Bluetooth: Add local and remote CSRK keys
    Add local and remote Connection Signature Resolving Keys and helper
    functions.
    
    Change-Id: I63af2e566dccc6ffb5397d28bde6f04bc78b93b1
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: ef703e1317f3e99db76efee6efcf0cec3562d79d

--- net/bluetooth/keys.c
+++ net/bluetooth/keys.c
@@ -9,5 +9,7 @@ DECL|bt_keys_get_addr|function|struct bt_keys *bt_keys_get_addr(const bt_addr_le
 DECL|bt_keys_get_type|function|struct bt_keys *bt_keys_get_type(int type, const bt_addr_le_t *addr)
 DECL|irks|variable|irks
 DECL|key_pool|variable|key_pool
+DECL|local_csrks|variable|local_csrks
 DECL|ltks|variable|ltks
+DECL|remote_csrks|variable|remote_csrks
 DECL|slave_ltks|variable|slave_ltks
--- net/bluetooth/keys.h
+++ net/bluetooth/keys.h
@@ -1,19 +1,27 @@
 DECL|BT_KEYS_ALL|enumerator|BT_KEYS_ALL = (BT_KEYS_SLAVE_LTK | BT_KEYS_IRK | \
 DECL|BT_KEYS_IRK|enumerator|BT_KEYS_IRK = (1 << 1),
+DECL|BT_KEYS_LOCAL_CSRK|enumerator|BT_KEYS_LOCAL_CSRK = (1 << 3),
 DECL|BT_KEYS_LTK|enumerator|BT_KEYS_LTK = (1 << 2),
+DECL|BT_KEYS_REMOTE_CSRK|enumerator|BT_KEYS_REMOTE_CSRK = (1 << 4),
 DECL|BT_KEYS_SLAVE_LTK|enumerator|BT_KEYS_SLAVE_LTK = (1 << 0),
 DECL|addr|member|bt_addr_le_t addr;
+DECL|bt_csrk|struct|struct bt_csrk {
 DECL|bt_irk|struct|struct bt_irk {
 DECL|bt_keys|struct|struct bt_keys {
 DECL|bt_ltk|struct|struct bt_ltk {
+DECL|cnt|member|uint32_t cnt;
 DECL|ediv|member|uint16_t ediv;
 DECL|irk|member|struct bt_irk irk;
 DECL|keys|member|int keys;
+DECL|local_csrk|member|struct bt_csrk local_csrk;
 DECL|ltk|member|struct bt_ltk ltk;
 DECL|next|member|struct bt_keys *next;
 DECL|next|member|struct bt_keys *next;
+DECL|next|member|struct bt_keys *next;
 DECL|rand|member|uint64_t rand;
+DECL|remote_csrk|member|struct bt_csrk remote_csrk;
 DECL|rpa|member|bt_addr_t rpa;
 DECL|slave_ltk|member|struct bt_ltk slave_ltk;
+DECL|val|member|uint8_t val[16];
 DECL|val|member|uint8_t val[16];
 DECL|val|member|uint8_t val[16];
