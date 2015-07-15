d4d709d4a1c00c16dc2b7df39146ea12577233f1
Johan Hedberg <johan.hedberg@intel.com>
2015-07-15 14:21:37 +0300

    Bluetooth: Move bt_security to conn.h and rename it appropriately
    Change-Id: Ibea6e4ef08c68e9debf0c53036dd2e4f75ea9a72
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: fe41cec88549c90508d3c723812c707e6526ad74

--- net/bluetooth/conn.c
+++ net/bluetooth/conn.c
@@ -13,12 +13,12 @@ DECL|bt_conn_lookup_state|function|struct bt_conn *bt_conn_lookup_state(const bt
 DECL|bt_conn_put|function|void bt_conn_put(struct bt_conn *conn)
 DECL|bt_conn_recv|function|void bt_conn_recv(struct bt_conn *conn, struct bt_buf *buf, uint8_t flags)
 DECL|bt_conn_reset_rx_state|function|static void bt_conn_reset_rx_state(struct bt_conn *conn)
+DECL|bt_conn_security|function|int bt_conn_security(struct bt_conn *conn, bt_security_t sec)
 DECL|bt_conn_send|function|void bt_conn_send(struct bt_conn *conn, struct bt_buf *buf)
 DECL|bt_conn_set_auto_conn|function|void bt_conn_set_auto_conn(struct bt_conn *conn, bool auto_conn)
 DECL|bt_conn_set_state|function|void bt_conn_set_state(struct bt_conn *conn, bt_conn_state_t state)
 DECL|bt_hci_connect_le_cancel|function|static int bt_hci_connect_le_cancel(struct bt_conn *conn)
 DECL|bt_hci_disconnect|function|static int bt_hci_disconnect(struct bt_conn *conn, uint8_t reason)
-DECL|bt_security|function|int bt_security(struct bt_conn *conn, bt_security_t sec)
 DECL|conn_tx_fiber|function|static void conn_tx_fiber(int arg1, int arg2)
 DECL|conns|variable|conns
 DECL|state2str|function|static const char *state2str(bt_conn_state_t state)
