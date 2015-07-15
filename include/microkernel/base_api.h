eb564603e90bf9bdbf7534210ec596cf1536c3d6
Anas Nashif <anas.nashif@intel.com>
2015-07-15 17:10:49 -0400

    doxygen: base_api.h comments cleanup
    Change-Id: Id9197853064afa082d4c4aa50f2a3ca47f96694b
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 3d07b2d1ba7aa0e2dc26920cbeb614a43143e63c

--- include/microkernel/base_api.h
+++ include/microkernel/base_api.h
@@ -12,7 +12,7 @@ DECL|_ALL_N|enumerator|_ALL_N = 0x00000004
 DECL|_BASE_API_H|macro|_BASE_API_H
 DECL|address_in_pool|member|void *address_in_pool;
 DECL|extra|member|} extra;
-DECL|info|member|uint32_t info; /* information field, free for user */
+DECL|info|member|uint32_t info;
 DECL|k_block|struct|struct k_block {
 DECL|k_msg|struct|struct k_msg {
 DECL|kevent_handler_t|typedef|typedef int (*kevent_handler_t)(int event);
@@ -30,15 +30,15 @@ DECL|ksemg_t|typedef|typedef ksem_t *ksemg_t;
 DECL|ktask_group_t|typedef|typedef uint32_t ktask_group_t;
 DECL|ktask_t|typedef|typedef int32_t ktask_t;
 DECL|ktimer_t|typedef|typedef uint32_t ktimer_t;
-DECL|mailbox|member|kmbox_t mailbox; /* Mailbox ID */
+DECL|mailbox|member|kmbox_t mailbox;
 DECL|pointer_to_data|member|void *pointer_to_data;
 DECL|poolid|member|kmemory_pool_t poolid;
 DECL|req_size|member|uint32_t req_size;
-DECL|rx_data|member|void *rx_data; /* pointer to message data at receiver */
-DECL|rx_task|member|ktask_t rx_task; /* receiving task */
-DECL|sema|member|ksem_t sema; /* semaphore to signal when asynchr. call */
-DECL|size|member|uint32_t size; /* size of message (bytes) */
-DECL|transfer|member|struct k_args *transfer; /* for 2-steps data transfer operation
-DECL|tx_block|member|struct k_block tx_block; /* for async message posting */
-DECL|tx_data|member|void *tx_data; /* pointer to message data at sender side */
-DECL|tx_task|member|ktask_t tx_task; /* sending task */
+DECL|rx_data|member|void *rx_data;
+DECL|rx_task|member|ktask_t rx_task;
+DECL|sema|member|ksem_t sema;
+DECL|size|member|uint32_t size;
+DECL|transfer|member|struct k_args *transfer;
+DECL|tx_block|member|struct k_block tx_block;
+DECL|tx_data|member|void *tx_data;
+DECL|tx_task|member|ktask_t tx_task;
