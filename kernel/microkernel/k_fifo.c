4673390fa116f6555b325946d37199cb4dde960e
Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>
2015-07-15 17:32:10 -0500

    Doxygen: Move the microkernel FIFO APIs comments to the .h file.
    Moves the comments from the k_fifo.c to the fifo.h file. Only the
    external API information was moved. Minor edits were performed to adapt
    the comments to different parameters and aliases.
    
    Change-Id: I69d2d009f7ec6390e0a2fc9d6a346ddc0111f9f1
    Signed-off-by: Rodrigo Caballero <rodrigo.caballero.abraham@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: b7def368bae8b54595df54600471109efa138dd8

--- kernel/microkernel/k_fifo.c
+++ kernel/microkernel/k_fifo.c
@@ -3,6 +3,6 @@ DECL|_k_fifo_deque_request|function|void _k_fifo_deque_request(struct k_args *A)
 DECL|_k_fifo_enque_reply|function|void _k_fifo_enque_reply(struct k_args *A)
 DECL|_k_fifo_enque_request|function|void _k_fifo_enque_request(struct k_args *A)
 DECL|_k_fifo_ioctl|function|void _k_fifo_ioctl(struct k_args *A)
-DECL|_task_fifo_get|function|int _task_fifo_get(kfifo_t queue, /* FIFO queue */void *data, /* where to store FIFO entry */ int32_t time /* maximum number of ticks to wait */ )
-DECL|_task_fifo_ioctl|function|int _task_fifo_ioctl(kfifo_t queue, /* FIFO queue */ int op/* 0: status query
+DECL|_task_fifo_get|function|int _task_fifo_get(kfifo_t queue, void *data, int32_t time)
+DECL|_task_fifo_ioctl|function|int _task_fifo_ioctl(kfifo_t queue, int op)
 DECL|_task_fifo_put|function|int _task_fifo_put(kfifo_t queue, /* FIFO queue */void *data, /* ptr to data to add to queue */ int32_t time /* maximum number of ticks to wait */ )
