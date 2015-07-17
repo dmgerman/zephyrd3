f1682b0bc9dcdf9822f627b522e01f7cfe2e49a6
Daniel Leung <daniel.leung@intel.com>
2015-07-17 13:00:06 -0700

    microkernel: introduce support for private mailboxes
    This enable defining mailboxes in source code in addition to
    defining in MDEF files. This introduces the macro
    DEFINE_MAILBOX(mailbox_name). The mailboxes created this
    way are the same, in functionality, as those defined in MDEF
    files. They can be manipulated by the standard microkernel
    mailbox APIs.
    
    Define the mailbox using:
    
      DEFINE_MAILBOX(mailbox1);
    
    and "mailbox1" can be used, for example:
    
      task_mbox_put(mailbox1, &msg);
    
    or,
    
      task_mbox_get(mailbox1, &msg);
    
    etc.
    
    To use the mailbox defined in another source file, simply add:
    
    extern const kmox_t mailbox1;
    
    to the desired C or header file.
    
    Change-Id: I32d4194af740f96fc8df8c6cd3144a0c9accb4ec
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 39887c4358b7ff189dcc4dcf194c5d4862b958e1

--- include/microkernel/mailbox.h
+++ include/microkernel/mailbox.h
@@ -1,4 +1,6 @@
+DECL|DEFINE_MAILBOX|macro|DEFINE_MAILBOX
 DECL|_MAILBOX_H|macro|_MAILBOX_H
+DECL|__K_MAILBOX_DEFAULT|macro|__K_MAILBOX_DEFAULT
 DECL|task_mbox_data_get_async_block_wait_timeout|macro|task_mbox_data_get_async_block_wait_timeout
 DECL|task_mbox_data_get_async_block_wait|macro|task_mbox_data_get_async_block_wait
 DECL|task_mbox_data_get_async_block|macro|task_mbox_data_get_async_block
