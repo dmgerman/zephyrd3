21c361f96a08f72584feb15963e00b42eb2ffe7f
Daniel Leung <daniel.leung@intel.com>
2015-07-17 13:43:58 -0700

    samples/microkernel: add test for private mailboxes
    This adds unit test for microkernel private mailboxes.
    The code piggybacks to the public mailbox test (by
    including the same source files), so any updates to
    the test will be applied to both.
    
    Note that the prj.mdef are different for both tests, since
    the private mailboxes test move the mailboxes inside source
    code. So, both mdef files will need to be updated at
    the same time.
    
    Change-Id: I52ccc5931b1abfd3ae3d654f888d4a019268bba0
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: f6369abf4d1c8b0475cec8ac0d3c09bc1b816c6b

--- samples/microkernel/test/test_mail/src/mail.c
+++ samples/microkernel/test/test_mail/src/mail.c
@@ -8,6 +8,8 @@ DECL|myData1|variable|myData1
 DECL|myData2|variable|myData2
 DECL|myData3|variable|myData3
 DECL|myData4|variable|myData4
+DECL|myMbox|variable|myMbox
+DECL|noRcvrMbox|variable|noRcvrMbox
 DECL|setMsg_Receiver|function|static void setMsg_Receiver(struct k_msg *inMsg, kmbox_t inMbox, ktask_t senderTask,void *inBuffer, uint32_t inBufferSize)
 DECL|setMsg_RecvBuf|function|static void setMsg_RecvBuf(struct k_msg *inMsg, char *inBuffer, uint32_t inBufferSize)
 DECL|setMsg_Sender|function|static void setMsg_Sender(struct k_msg *inMsg, kmbox_t inMbox, ktask_t receiverTask, void *dataArea, uint32_t dataSize, uint32_t info)
