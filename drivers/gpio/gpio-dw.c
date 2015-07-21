452eaaa2ec71556be687ae548b84ee1137f81fa1
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-07-21 08:12:17 -0700

    gpio: Add driver for Synopsys DesignWare GPIO devices
    [DL: captilize commit message title; fixed some whitespaces;
         changed the __initconfig_gpio_* from level 0 to level 1,
         which is the level of pure_init; and added include path
         to board.h; ]
    
    Change-Id: I7eea6a6ca9e4b7cf8d1ccabb57f07f786da93ef0
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 77a78f08274add6efdbfdab810b6ec7141db0772

---
+++ drivers/gpio/gpio-dw.c
@@ -0,0 +1,39 @@
+DECL|BIT|macro|BIT
+DECL|EXT_PORTA|macro|EXT_PORTA
+DECL|EXT_PORTB|macro|EXT_PORTB
+DECL|EXT_PORTC|macro|EXT_PORTC
+DECL|EXT_PORTD|macro|EXT_PORTD
+DECL|INTEN|macro|INTEN
+DECL|INTMASK|macro|INTMASK
+DECL|INTSTATUS|macro|INTSTATUS
+DECL|INTTYPE_LEVEL|macro|INTTYPE_LEVEL
+DECL|INT_BOTHEDGE|macro|INT_BOTHEDGE
+DECL|INT_CLOCK_SYNC|macro|INT_CLOCK_SYNC
+DECL|INT_POLARITY|macro|INT_POLARITY
+DECL|PORTA_DEBOUNCE|macro|PORTA_DEBOUNCE
+DECL|PORTA_EOI|macro|PORTA_EOI
+DECL|SWPORTA_DDR|macro|SWPORTA_DDR
+DECL|SWPORTA_DR|macro|SWPORTA_DR
+DECL|SWPORTB_DDR|macro|SWPORTB_DDR
+DECL|SWPORTB_DR|macro|SWPORTB_DR
+DECL|SWPORTC_DDR|macro|SWPORTC_DDR
+DECL|SWPORTC_DR|macro|SWPORTC_DR
+DECL|SWPORTD_DDR|macro|SWPORTD_DDR
+DECL|SWPORTD_DR|macro|SWPORTD_DR
+DECL|api_funcs|variable|api_funcs
+DECL|dw_interrupt_config|function|static inline void dw_interrupt_config(struct device *port, int access_op, uint32_t pin, int flags)
+DECL|dw_pin_config|function|static inline void dw_pin_config(struct device *port, uint32_t pin, int flags)
+DECL|dw_port_config|function|static inline void dw_port_config(struct device *port, int flags)
+DECL|dw_read|function|static inline uint32_t dw_read(uint32_t base_addr, uint32_t offest)
+DECL|dw_set_bit|function|static void dw_set_bit(uint32_t base_addr, uint32_t offset, uint32_t bit, uint8_t value)
+DECL|dw_write|function|static inline void dw_write(uint32_t base_addr, uint32_t offest, uint32_t val)
+DECL|gpio_config_dw|function|static inline int gpio_config_dw(struct device *port, int access_op, uint32_t pin, int flags)
+DECL|gpio_disable_callback_dw|function|static inline int gpio_disable_callback_dw(struct device *port, int access_op, uint32_t pin)
+DECL|gpio_dw_isr|function|void gpio_dw_isr(struct device *port)
+DECL|gpio_enable_callback_dw|function|static inline int gpio_enable_callback_dw(struct device *port, int access_op, uint32_t pin)
+DECL|gpio_initialize_dw|function|int gpio_initialize_dw(struct device *port)
+DECL|gpio_read_dw|function|static inline int gpio_read_dw(struct device *port, int access_op, uint32_t pin, uint32_t *value)
+DECL|gpio_resume_port_dw|function|static inline int gpio_resume_port_dw(struct device *port)
+DECL|gpio_set_callback_dw|function|static inline int gpio_set_callback_dw(struct device *port, gpio_callback_t callback)
+DECL|gpio_suspend_port_dw|function|static inline int gpio_suspend_port_dw(struct device *port)
+DECL|gpio_write_dw|function|static inline int gpio_write_dw(struct device *port, int access_op,uint32_t pin, uint32_t value)
---
+++ drivers/gpio/gpio-dw.h
@@ -0,0 +1,11 @@
+DECL|CONFIG_GPIO_DW_BITS|macro|CONFIG_GPIO_DW_BITS
+DECL|base_addr|member|uint32_t base_addr;
+DECL|bits|member|uint32_t bits;
+DECL|callback|member|gpio_callback_t callback;
+DECL|config_func|member|gpio_config_irq_t config_func;
+DECL|enabled_callbacks|member|uint32_t enabled_callbacks;
+DECL|gpio_config_dw|struct|struct gpio_config_dw {
+DECL|gpio_config_irq_t|typedef|typedef void (*gpio_config_irq_t)(struct device *port);
+DECL|gpio_runtime_dw|struct|struct gpio_runtime_dw {
+DECL|irq_num|member|uint32_t irq_num;
+DECL|port_callback|member|uint8_t port_callback;
