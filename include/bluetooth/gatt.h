953618d1ef35bd877504abda69a1f5dde5354692
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-22 10:21:47 +0300

    Bluetooth: GATT: Add bt_gatt_subscribe
    This adds bt_gatt_subscribe which can used to subscribe to attribute
    value notification using CCC handle.
    
    Change-Id: I0983843836b0c2253f750b34b7765dd880cb10a0
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: fac5df2bae2efbce0adb708b69f2aa4f13f52215

--- include/bluetooth/gatt.h
+++ include/bluetooth/gatt.h
@@ -32,6 +32,8 @@ DECL|BT_GATT_SECONDARY_SERVICE|macro|BT_GATT_SECONDARY_SERVICE
 DECL|BT_GATT_SERVICE|macro|BT_GATT_SERVICE
 DECL|__BT_GATT_H|macro|__BT_GATT_H
 DECL|_bt_gatt_ccc|struct|struct _bt_gatt_ccc {
+DECL|_next|member|struct bt_gatt_subscribe_params *_next;
+DECL|_peer|member|bt_addr_le_t _peer;
 DECL|bt_gatt_attr_func_t|typedef|typedef uint8_t (*bt_gatt_attr_func_t)(const struct bt_gatt_attr *attr,
 DECL|bt_gatt_attr|struct|struct bt_gatt_attr {
 DECL|bt_gatt_ccc_cfg|struct|struct bt_gatt_ccc_cfg {
@@ -44,15 +46,18 @@ DECL|bt_gatt_include|struct|struct bt_gatt_include {
 DECL|bt_gatt_read_func_t|typedef|typedef void (*bt_gatt_read_func_t)(struct bt_conn *conn, int err,
 DECL|bt_gatt_rsp_func_t|typedef|typedef void (*bt_gatt_rsp_func_t)(struct bt_conn *conn, uint8_t err);
 DECL|bt_gatt_service|struct|struct bt_gatt_service {
+DECL|bt_gatt_subscribe_params|struct|struct bt_gatt_subscribe_params {
 DECL|cfg_changed|member|void (*cfg_changed)(uint16_t value);
 DECL|cfg_len|member|size_t cfg_len;
 DECL|cfg|member|struct bt_gatt_ccc_cfg *cfg;
+DECL|destroy|member|void (*destroy)(void *user_data);
 DECL|destroy|member|void (*destroy)(void *user_data);
 DECL|end_handle|member|uint16_t end_handle;
 DECL|end_handle|member|uint16_t end_handle;
 DECL|flags|member|uint16_t flags;
 DECL|flush|member|int (*flush)(struct bt_conn *conn,
 DECL|func|member|bt_gatt_attr_func_t func;
+DECL|func|member|bt_gatt_read_func_t func;
 DECL|handle|member|uint16_t handle;
 DECL|peer|member|bt_addr_le_t peer;
 DECL|perm|member|uint8_t perm;
@@ -69,6 +74,7 @@ DECL|uuid|member|const struct bt_uuid *uuid;
 DECL|uuid|member|const struct bt_uuid *uuid;
 DECL|uuid|member|struct bt_uuid *uuid;
 DECL|valid|member|uint8_t valid;
+DECL|value_handle|member|uint16_t value_handle;
 DECL|value_handle|member|uint16_t value_handle;
 DECL|value_handle|member|uint16_t value_handle;
 DECL|value|member|uint16_t value;
--- net/bluetooth/gatt.c
+++ net/bluetooth/gatt.c
@@ -6,6 +6,7 @@ DECL|att_find_info_rsp|function|static void att_find_info_rsp(struct bt_conn *co
 DECL|att_find_type_rsp|function|static void att_find_type_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|att_read_rsp|function|static void att_read_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|att_read_type_rsp|function|static void att_read_type_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
+DECL|att_write_ccc_rsp|function|static void att_write_ccc_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|att_write_rsp|function|static void att_write_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|attr_count|variable|attr_count
 DECL|bt_gatt_attr_read_ccc|function|int bt_gatt_attr_read_ccc(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint8_t len, uint16_t offset)
@@ -23,10 +24,12 @@ DECL|bt_gatt_discover_descriptor|function|int bt_gatt_discover_descriptor(struct
 DECL|bt_gatt_discover|function|int bt_gatt_discover(struct bt_conn *conn, struct bt_gatt_discover_params *params)
 DECL|bt_gatt_exchange_mtu|function|int bt_gatt_exchange_mtu(struct bt_conn *conn, bt_gatt_rsp_func_t func)
 DECL|bt_gatt_foreach_attr|function|void bt_gatt_foreach_attr(uint16_t start_handle, uint16_t end_handle, bt_gatt_attr_func_t func, void *user_data)
+DECL|bt_gatt_notification|function|void bt_gatt_notification(struct bt_conn *conn, uint16_t handle, const void *data, uint16_t length)
 DECL|bt_gatt_notify|function|void bt_gatt_notify(uint16_t handle, const void *data, size_t len)
 DECL|bt_gatt_read_multiple|function|int bt_gatt_read_multiple(struct bt_conn *conn, const uint16_t *handles, size_t count, bt_gatt_read_func_t func)
 DECL|bt_gatt_read|function|int bt_gatt_read(struct bt_conn *conn, uint16_t handle, uint16_t offset, bt_gatt_read_func_t func)
 DECL|bt_gatt_register|function|void bt_gatt_register(const struct bt_gatt_attr *attrs, size_t count)
+DECL|bt_gatt_subscribe|function|int bt_gatt_subscribe(struct bt_conn *conn, uint16_t handle, struct bt_gatt_subscribe_params *params)
 DECL|bt_gatt_write|function|int bt_gatt_write(struct bt_conn *conn, uint16_t handle, const void *data, uint16_t length, bt_gatt_rsp_func_t func)
 DECL|connected_cb|function|static uint8_t connected_cb(const struct bt_gatt_attr *attr, void *user_data)
 DECL|data|member|const void *data;
@@ -39,6 +42,8 @@ DECL|gatt_incl|struct|struct gatt_incl {
 DECL|gatt_mtu_rsp|function|static void gatt_mtu_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|gatt_read_blob|function|static int gatt_read_blob(struct bt_conn *conn, uint16_t handle, uint16_t offset, bt_gatt_read_func_t func)
 DECL|gatt_send|function|static int gatt_send(struct bt_conn *conn, struct bt_buf *buf, bt_att_func_t func, void *user_data, bt_att_destroy_t destroy)
+DECL|gatt_subscription_add|function|static void gatt_subscription_add(struct bt_conn *conn, struct bt_gatt_subscribe_params *params)
+DECL|gatt_write_ccc|function|static int gatt_write_ccc(struct bt_conn *conn, uint16_t handle, uint16_t value, bt_att_func_t func, struct bt_gatt_subscribe_params *params)
 DECL|gatt_write_cmd|function|static int gatt_write_cmd(struct bt_conn *conn, uint16_t handle, const void *data, uint16_t length)
 DECL|handle|member|uint8_t handle;
 DECL|len|member|size_t len;
@@ -46,6 +51,7 @@ DECL|notify_cb|function|static uint8_t notify_cb(const struct bt_gatt_attr *attr
 DECL|notify_data|struct|struct notify_data {
 DECL|properties|member|uint8_t properties;
 DECL|start_handle|member|uint16_t start_handle;
+DECL|subscriptions|variable|subscriptions
 DECL|uuid16|member|uint16_t uuid16;
 DECL|uuid16|member|uint16_t uuid16;
 DECL|uuid|member|uint8_t uuid[16];
