8344d769ae9ce1bdda974c6142f05f75883e877c
Mariusz Skamra <mariusz.skamra@tieto.com>
2015-07-23 11:35:59 +0200

    Bluetooth: Start using atomic flags to indicate bt_dev state
    With this patch advertising state and scanning state are stored as flags.
    
    Change-Id: I5d2dc37972620ee89aaf55d45e294e8be82863d3
    Signed-off-by: Mariusz Skamra <mariusz.skamra@tieto.com>
    
    Former-commit-id: 6b4f7025f283c127ba393d4c892827cbed638dc4

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,5 +1,7 @@
 DECL|BT_ADDR_LE_STR_LEN|macro|BT_ADDR_LE_STR_LEN
 DECL|BT_ADDR_STR_LEN|macro|BT_ADDR_STR_LEN
+DECL|BT_SCAN_FILTER_DUP_DISABLE|enumerator|BT_SCAN_FILTER_DUP_DISABLE,
+DECL|BT_SCAN_FILTER_DUP_ENABLE|enumerator|BT_SCAN_FILTER_DUP_ENABLE,
 DECL|BT_SECURITY_FIPS|enumerator|BT_SECURITY_FIPS, /** Authenticated LE Secure Connections and
 DECL|BT_SECURITY_HIGH|enumerator|BT_SECURITY_HIGH, /** encryption and authentication (MITM). */
 DECL|BT_SECURITY_LOW|enumerator|BT_SECURITY_LOW, /** No encryption and no authentication. */
@@ -10,6 +12,7 @@ DECL|bt_addr_le_to_str|function|static inline int bt_addr_le_to_str(const bt_add
 DECL|bt_addr_to_str|function|static inline int bt_addr_to_str(const bt_addr_t *addr, char *str, size_t len)
 DECL|bt_eir|struct|struct bt_eir {
 DECL|bt_le_scan_cb_t|typedef|typedef void bt_le_scan_cb_t(const bt_addr_le_t *addr, int8_t rssi,
+DECL|bt_scan_filter_dup_t|typedef|} bt_scan_filter_dup_t;
 DECL|bt_security_t|typedef|} bt_security_t;
 DECL|data|member|uint8_t data[29];
 DECL|len|member|uint8_t len;
--- net/bluetooth/hci_core.h
+++ net/bluetooth/hci_core.h
@@ -1,8 +1,10 @@
+DECL|BT_DEV_ADVERTISING|enumerator|BT_DEV_ADVERTISING,
+DECL|BT_DEV_SCANNING|enumerator|BT_DEV_SCANNING,
+DECL|BT_DEV_SCAN_FILTER_DUP|enumerator|BT_DEV_SCAN_FILTER_DUP,
 DECL|BT_STACK_DEBUG_EXTRA|macro|BT_STACK_DEBUG_EXTRA
 DECL|BT_STACK_DEBUG_EXTRA|macro|BT_STACK_DEBUG_EXTRA
 DECL|BT_STACK_NOINIT|macro|BT_STACK_NOINIT
 DECL|BT_STACK|macro|BT_STACK
-DECL|adv_enable|member|uint8_t adv_enable;
 DECL|bdaddr|member|bt_addr_t bdaddr;
 DECL|bt_addr_cmp|function|static inline int bt_addr_cmp(const bt_addr_t *a, const bt_addr_t *b)
 DECL|bt_addr_copy|function|static inline void bt_addr_copy(bt_addr_t *dst, const bt_addr_t *src)
@@ -14,6 +16,7 @@ DECL|bt_dev|struct|struct bt_dev {
 DECL|cmd_tx_queue|member|struct nano_fifo cmd_tx_queue;
 DECL|drv|member|struct bt_driver *drv;
 DECL|features|member|uint8_t features[8];
+DECL|flags|member|atomic_t flags[1];
 DECL|hci_revision|member|uint16_t hci_revision;
 DECL|hci_version|member|uint8_t hci_version;
 DECL|le_features|member|uint8_t le_features[8];
@@ -27,6 +30,4 @@ DECL|ncmd_sem|member|struct nano_sem ncmd_sem;
 DECL|ncmd|member|uint8_t ncmd;
 DECL|rx_prio_queue|member|struct nano_fifo rx_prio_queue;
 DECL|rx_queue|member|struct nano_fifo rx_queue;
-DECL|scan_enable|member|uint8_t scan_enable;
-DECL|scan_filter|member|uint8_t scan_filter;
 DECL|sent_cmd|member|struct bt_buf *sent_cmd;
