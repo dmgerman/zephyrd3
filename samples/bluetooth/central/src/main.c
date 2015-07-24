659d52b61cf3e18a16d8e6c48ab6752bffda6d60
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-24 15:54:05 +0300

    Bluetooth: Make central sample discover HRS over GATT
    This makes the central sample discover the attributes and subscribe
    to receive notifications of HeartRate Measurement Characteristic.
    
    Change-Id: Icedbdd759638be0b8fa8515112208aa61ad5eb90
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 369ff3bb90d12758a0facdba64f61f9f38227ced

--- samples/bluetooth/central/src/main.c
+++ samples/bluetooth/central/src/main.c
@@ -1,10 +1,17 @@
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|ad_parse|function|static void ad_parse(const uint8_t *data, uint8_t len, bool (*func)(const struct bt_eir *eir, void *user_data), void *user_data)
+DECL|ccc|variable|ccc
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
 DECL|default_conn|variable|default_conn
 DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type, const uint8_t *ad, uint8_t len)
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
+DECL|discover_func|function|static uint8_t discover_func(const struct bt_gatt_attr *attr, void *user_data)
+DECL|discover_params|variable|discover_params
 DECL|eir_found|function|static bool eir_found(const struct bt_eir *eir, void *user_data)
+DECL|hrm|variable|hrm
+DECL|hrs|variable|hrs
 DECL|mainloop|function|void mainloop(void)
+DECL|subscribe_func|function|static void subscribe_func(struct bt_conn *conn, int err, const void *data, uint16_t length)
+DECL|subscribe_params|variable|subscribe_params
