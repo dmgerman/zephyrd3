0c10e15e7c7715c69ca2477f84a79ebbe14c7b01
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-07-24 13:46:21 -0400

    Fix PIC shutoff.
    Disable the PIC upon initialization to prevent it from
    generating spurious interrupts.
    
    Change-Id: I08ba1d00e269e1fd1d32960dcb844e9fa1ccc195
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 63fe84bdaf221e3310b40bccbaef5d25ee1a78dc

--- drivers/interrupt_controller/i8259.c
+++ drivers/interrupt_controller/i8259.c
@@ -2,9 +2,11 @@ DECL|OCW3_DEF|macro|OCW3_DEF
 DECL|OCW3_IRR|macro|OCW3_IRR
 DECL|OCW3_ISR|macro|OCW3_ISR
 DECL|OCW3_PCB|macro|OCW3_PCB
+DECL|PIC_DISABLE|macro|PIC_DISABLE
 DECL|__I8259IntEnable|function|static void __I8259IntEnable(unsigned int irq, /* IRQ number to enable */ unsigned char enable /* 0 = disable, otherwise enable */ )
 DECL|_i8259_eoi_master|function|void _i8259_eoi_master(unsigned int irq /* IRQ number to send EOI: unused */ )
 DECL|_i8259_eoi_slave|function|void _i8259_eoi_slave(unsigned int irq /* IRQ number to send EOI: unused */ )
+DECL|_i8259_init|function|void _i8259_init(void)
 DECL|_i8259_init|function|void _i8259_init(void)
 DECL|_i8259_irq_disable|function|void _i8259_irq_disable(unsigned int irq /* IRQ number to disable */ )
 DECL|_i8259_irq_enable|function|void _i8259_irq_enable(unsigned int irq /* IRQ number to enable */)
