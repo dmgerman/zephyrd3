b1eb502b4a781f730ba755a2c4daab2244779ba9
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-24 13:17:33 +0300

    Bluetooth: Make central sample connect to devices supporting HRS
    This makes central sample to parse the Advertising Data and in case the
    device supports HeartRate Service stop scanning and connect to it, if the
    device disconnects then start scanning again.
    
    Change-Id: I49962eadb5c82c7a910185212063cd576cadf62e
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: f581894abfb671ec44dbbec2f3fd0596a9d86538

--- samples/bluetooth/central/src/main.c
+++ samples/bluetooth/central/src/main.c
@@ -1,6 +1,10 @@
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|ad_parse|function|static void ad_parse(const uint8_t *data, uint8_t len, bool (*func)(const struct bt_eir *eir, void *user_data), void *user_data)
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
+DECL|default_conn|variable|default_conn
+DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type, const uint8_t *ad, uint8_t len)
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
+DECL|eir_found|function|static bool eir_found(const struct bt_eir *eir, void *user_data)
 DECL|mainloop|function|void mainloop(void)
