bb886325342ca34f211e60eddd2632d44b84eb29
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-07-24 10:46:00 +0300

    Bluetooth: Add connection callbacks to central sample
    This will be needed to monitor connections with peripherals.
    
    Change-Id: Iccea2696af72b0b8b3f40aac180738dc6b80abc8
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 7942266868e0c8af097e7a26aa43cc4779c92749

--- samples/bluetooth/central/src/main.c
+++ samples/bluetooth/central/src/main.c
@@ -1,3 +1,6 @@
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|conn_callbacks|variable|conn_callbacks
+DECL|connected|function|static void connected(struct bt_conn *conn)
+DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
 DECL|mainloop|function|void mainloop(void)
