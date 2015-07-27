d90f0984b3c37ce1032c57ecef234386d15ef607
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-07-27 17:23:25 +0300

    net: apps: Add multicast support to echo-server
    echo-server can now receive both unicast and multicast UDP
    messages.
    
    Change-Id: I3837b8e7126584fa7c87958e68a8439824abd5ab
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: e0b6702d71fd6212898bf890558839d15f2b2510

--- samples/network/echo_server/src/echo-server.c
+++ samples/network/echo_server/src/echo-server.c
@@ -1,3 +1,5 @@
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
@@ -12,7 +14,7 @@ DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|fiberEntry|function|void fiberEntry(void)
 DECL|fiberStack|variable|fiberStack
-DECL|get_context|function|static inline struct net_context *get_context(void)
+DECL|get_context|function|static inline bool get_context(struct net_context **recv, struct net_context **mcast_recv)
 DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
 DECL|init_server|function|static inline void init_server()
 DECL|main|function|void main(void)
@@ -20,7 +22,8 @@ DECL|my_mac|variable|my_mac
 DECL|nanoSemFiber|variable|nanoSemFiber
 DECL|nanoSemTask|variable|nanoSemTask
 DECL|peer_mac|variable|peer_mac
-DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *ctx)
+DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const char *name, const char *type, struct net_buf *buf)
+DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *recv, struct net_context *mcast_recv)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
 DECL|taskA|function|void taskA(void)
 DECL|taskB|function|void taskB(void)
