9e68dd98ee4d08d62398788bba348c417eee7868
Johan Hedberg <johan.hedberg@intel.com>
2015-07-28 18:31:48 +0300

    Bluetooth: Use async callback for enabling Bluetooth
    In the peripheral sample app use the async feature of bt_enable() to
    verify that it works as expected.
    
    Change-Id: I15f7758944a8c34d1aaef84f0ba77a6b5bbb4695
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: e899c811e63573f0accee894a3010fd229729b4e

--- samples/bluetooth/peripheral/src/main.c
+++ samples/bluetooth/peripheral/src/main.c
@@ -12,6 +12,7 @@ DECL|blvl_chrc|variable|blvl_chrc
 DECL|blvl_uuid|variable|blvl_uuid
 DECL|bslc_chrc|variable|bslc_chrc
 DECL|bslc_uuid|variable|bslc_uuid
+DECL|bt_ready|function|static void bt_ready(int err)
 DECL|buf|member|uint8_t buf[BT_BUF_MAX_DATA];
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
