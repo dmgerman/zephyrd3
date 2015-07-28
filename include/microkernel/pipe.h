703d047fc41c0b4914e66283071204250b4cee7b
Daniel Leung <daniel.leung@intel.com>
2015-07-28 11:18:35 -0700

    microkernel: introduce support for private pipes
    This enable defining pipes in source code in addition to
    defining in MDEF files. This introduces the macro
    DEFINE_PIPE(pipe_name, ...). The pipes created this
    way are the same, in functionality, as those defined in MDEF
    files. They can be manipulated by the standard microkernel
    pipe APIs.
    
    Define the pipe using:
    
      DEFINE_PIPE(pipe1, size);
    
    and "pipe1" can be used, for example:
    
      task_pipe_put(pipe1, ...);
    
    or,
    
      task_pipe_get(pipe1, ...);
    
    etc.
    
    To use the pipe defined in another source file, simply add:
    
    extern const kpipe_t pipe1;
    
    to the desired C or header file.
    
    Change-Id: Iae8e04706359bc18aae51acc75df3e3d26388882
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: b5517ab61c96be584145e5b6c9aa92d11dc12792

--- include/microkernel/pipe.h
+++ include/microkernel/pipe.h
@@ -1,4 +1,6 @@
+DECL|DEFINE_PIPE|macro|DEFINE_PIPE
 DECL|PIPE_H|macro|PIPE_H
+DECL|__K_PIPE_INITIALIZER|macro|__K_PIPE_INITIALIZER
 DECL|task_pipe_get_wait_timeout|macro|task_pipe_get_wait_timeout
 DECL|task_pipe_get_wait|macro|task_pipe_get_wait
 DECL|task_pipe_get|macro|task_pipe_get
