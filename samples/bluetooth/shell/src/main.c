807933d893fb49df5db1ed4531532d1bdc03bc8f
Szymon Janc <szymon.janc@tieto.com>
2015-07-29 09:46:40 +0200

    Bluetooth: Register simple GAP service in btshell app
    Some devices (eg. Android KitKat) are confused if remote database
    is empty.
    
    Change-Id: Ie27920b6f64b12903917068101d5abb171ab42c0
    Signed-off-by: Szymon Janc <szymon.janc@tieto.com>
    
    Former-commit-id: dabedeb449dec1a597e819104f8f364986a61a28

--- samples/bluetooth/shell/src/main.c
+++ samples/bluetooth/shell/src/main.c
@@ -1,4 +1,6 @@
+DECL|DEVICE_NAME|macro|DEVICE_NAME
 DECL|ad|variable|ad
+DECL|attrs|variable|attrs
 DECL|cmd_active_scan_on|function|static void cmd_active_scan_on(void)
 DECL|cmd_advertise|function|static void cmd_advertise(int argc, char *argv[])
 DECL|cmd_connect_le|function|static void cmd_connect_le(int argc, char *argv[])
@@ -18,13 +20,17 @@ DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
 DECL|default_conn|variable|default_conn
 DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t evtype, const uint8_t *ad, uint8_t len)
+DECL|device_name_uuid|variable|device_name_uuid
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
 DECL|discover_destroy|function|static void discover_destroy(void *user_data)
 DECL|discover_func|function|static uint8_t discover_func(const struct bt_gatt_attr *attr, void *user_data)
 DECL|discover_params|variable|discover_params
 DECL|exchange_rsp|function|static void exchange_rsp(struct bt_conn *conn, uint8_t err)
+DECL|gap_uuid|variable|gap_uuid
 DECL|mainloop|function|void mainloop(void)
+DECL|name_chrc|variable|name_chrc
 DECL|read_func|function|static void read_func(struct bt_conn *conn, int err, const void *data, uint16_t length)
+DECL|read_name|function|static int read_name(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|sd|variable|sd
 DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
 DECL|subscribe_destroy|function|static void subscribe_destroy(void *user_data)
