8083dc8003f259a5b62ca7e0e1a195bf7840b295
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-07-30 16:20:58 +0300

    net: apps: echo-client test application
    The echo-client app will run in Zephyr and will send network
    packets to host process (echo-server) via SLIP.
    
    Change-Id: Icaf941ce883aec82136ef909db67e43ab6b981f7
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c0f04fd599e58f00c4d2f183a023d2b696ced2af

---
+++ samples/network/echo_client/src/echo-client.c
@@ -0,0 +1,39 @@
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_PORT|macro|MY_PORT
+DECL|PEER_IPADDR|macro|PEER_IPADDR
+DECL|PEER_IPADDR|macro|PEER_IPADDR
+DECL|PEER_PORT|macro|PEER_PORT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|expecting|variable|expecting
+DECL|fiberStack_receiving|variable|fiberStack_receiving
+DECL|fiberStack_sending|variable|fiberStack_sending
+DECL|fiber_receiving|function|void fiber_receiving(void)
+DECL|fiber_sending|function|void fiber_sending(void)
+DECL|flag|variable|flag
+DECL|get_context|function|static inline bool get_context(struct net_context **unicast, struct net_context **multicast)
+DECL|in6addr_my|variable|in6addr_my
+DECL|in6addr_peer|variable|in6addr_peer
+DECL|init_server|function|static inline void init_server()
+DECL|ipsum_len|variable|ipsum_len
+DECL|lorem_ipsum|variable|lorem_ipsum
+DECL|main|function|void main(void)
+DECL|multicast|variable|multicast
+DECL|my_mac|variable|my_mac
+DECL|peer_mac|variable|peer_mac
+DECL|receiving_loop|function|void receiving_loop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
+DECL|send_packet|function|static inline bool send_packet(const char *name, struct net_context *ctx, int ipsum_len, int pos)
+DECL|sending_loop|function|void sending_loop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|taskA|function|void taskA(void)
+DECL|taskB|function|void taskB(void)
+DECL|unicast|variable|unicast
+DECL|wait_reply|function|static inline bool wait_reply(const char *name, struct net_context *ctx, int ipsum_len, int pos)
