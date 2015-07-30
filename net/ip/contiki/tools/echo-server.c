05d34c75f986647748b23c28ef2fe42232f1697a
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-07-30 15:58:46 +0300

    net: tools: Add echo-server test application
    This echo-server app is run in the host side. It will
    connect to Zephyr via UART using SLIP. The app will
    listen data in socket 4242 and will reverse the returned
    bytes and send them back to the sender. The application
    supports both IPv4 and IPv6 protocols and it will also
    listen multicast packets sent to it.
    
    Change-Id: Icf0805794a16842e70fbb5e8a3f843a3782e1968
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 0ee55d73afdff3818d45a67807ec939ab6b4434b

---
+++ net/ip/contiki/tools/echo-server.c
@@ -0,0 +1,18 @@
+DECL|CLIENT_PORT|macro|CLIENT_PORT
+DECL|MAX_BUF_SIZE|macro|MAX_BUF_SIZE
+DECL|MAX_TIMEOUT|macro|MAX_TIMEOUT
+DECL|MAX|macro|MAX
+DECL|MY_MCAST_ADDR4|macro|MY_MCAST_ADDR4
+DECL|MY_MCAST_ADDR6|macro|MY_MCAST_ADDR6
+DECL|SERVER_PORT|macro|SERVER_PORT
+DECL|bind_device|function|static int bind_device(int fd, const char *interface, void *addr, int len)
+DECL|family_to_level|function|int family_to_level(int family)
+DECL|get_address|function|static int get_address(int ifindex, int family, void *address)
+DECL|get_ifindex|function|static int get_ifindex(const char *name)
+DECL|get_socket|function|static int get_socket(int family)
+DECL|join_mc_group|function|static int join_mc_group(int sock, int ifindex, int family, void *addr, int addr_len)
+DECL|main|function|int main(int argc, char**argv)
+DECL|receive_and_reply|function|static int receive_and_reply(fd_set *rfds, int fd_recv, int fd_send, unsigned char *buf, int buflen)
+DECL|receive|function|static int receive(int fd, unsigned char *buf, int buflen, struct sockaddr *addr, socklen_t *addrlen)
+DECL|reply|function|static int reply(int fd, unsigned char *buf, int buflen, struct sockaddr *addr, socklen_t addrlen)
+DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
