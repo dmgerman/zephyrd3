bf28c14170863e3f2e7a0ce38e25edb341016a79
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-07-31 15:58:09 -0500

    Add profiler method.
    Add a standardized mechanism to add profiling points to the Kernel
    with a single interface for the user to collect the profiling data.
    
    Change-Id: I4fa34ac1b42f73a73ba1fd805e755ee2fd00dff7
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: efd8e246ccd4554a772d2eda5415d6b2ffda14b4

---
+++ include/misc/profiler.h
@@ -0,0 +1,8 @@
+DECL|__PROFILE_H__|macro|__PROFILE_H__
+DECL|sys_profiler_get_wait_timeout|macro|sys_profiler_get_wait_timeout
+DECL|sys_profiler_get_wait|macro|sys_profiler_get_wait
+DECL|sys_profiler_get|macro|sys_profiler_get
+DECL|sys_profiler_logger|variable|sys_profiler_logger
+DECL|sys_profiler_put_timed|function|static inline void sys_profiler_put_timed(uint16_t event_id) {};
+DECL|sys_profiler_put|function|static inline void sys_profiler_put(uint16_t event_id, uint32_t *event_data,uint8_t data_size)
+DECL|sys_profiler_put|macro|sys_profiler_put
---
+++ kernel/nanokernel/profiler.c
@@ -0,0 +1,3 @@
+DECL|_sys_profiler_buffer|variable|_sys_profiler_buffer
+DECL|_sys_profiler_init|function|static int _sys_profiler_init(struct device *arg)
+DECL|sys_profiler_put_timed|function|void sys_profiler_put_timed(uint16_t event_id)
