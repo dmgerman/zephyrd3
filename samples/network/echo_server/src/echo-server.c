2701638ea5a3298b2d2b42023138cd259eb41794
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-08-03 13:57:06 +0300

    net: Refactor echo-server test application
    Now uses net_receive() with a proper timeout. Removed also
    extra waits when sending data. Removed extra semaphores
    and streamlined the code to act as fast as possible.
    
    Change-Id: I577d4d5ff85bf13400fb6e157bc0023267658ba4
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 29ee1cceef0d8d7177885f5453cd7a0f02bc89d6

--- samples/network/echo_server/src/echo-server.c
+++ samples/network/echo_server/src/echo-server.c
@@ -5,25 +5,17 @@ DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
-DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
-DECL|fiberEntry|function|void fiberEntry(void)
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|fiberStack|variable|fiberStack
+DECL|fiber_receive|function|void fiber_receive(void)
 DECL|get_context|function|static inline bool get_context(struct net_context **recv, struct net_context **mcast_recv)
-DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
 DECL|init_server|function|static inline void init_server()
 DECL|main|function|void main(void)
 DECL|my_mac|variable|my_mac
-DECL|nanoSemFiber|variable|nanoSemFiber
-DECL|nanoSemTask|variable|nanoSemTask
 DECL|peer_mac|variable|peer_mac
 DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const char *name, const char *type, struct net_buf *buf)
 DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *recv, struct net_context *mcast_recv)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
-DECL|taskA|function|void taskA(void)
-DECL|taskB|function|void taskB(void)
+DECL|task_receive|function|void task_receive(void)
