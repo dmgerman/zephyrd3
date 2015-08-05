4ac8391cedb843cde4c30c553f98e5f40149dac7
Peter Mitsis <peter.mitsis@windriver.com>
2015-08-05 16:39:27 -0400

    Remove PIC code, but not PIC disabling code
    Removes the non-PIC disabling PIC code as the PIC is not a supported interrupt
    controller.  The PIC disabling code remains as it is needed to prevent the
    generation of spurious interrupts from the PIC (see CONFIG_SHUTOFF_PIC).
    
    Change-Id: Ic59aa17ab96f34685a5d7b5f24cab391de47edca
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: c963861d47c76c538eca72b8df03b5505e5eec42

--- drivers/interrupt_controller/system_pic.c
+++
@@ -1,2 +0,0 @@
-DECL|_SysIntVecAlloc|function|int _SysIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority, /* get vector from <priority> group */ NANO_EOI_GET_FUNC * boiRtn, /* ptr to BOI routine
-DECL|_SysIntVecProgram|function|void _SysIntVecProgram(unsigned int vector, /* vector number */ unsigned int irq /* virtualized IRQ */ )
