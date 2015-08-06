a4cb8bcb77bb9fced7a9dce67493b71b5e757d22
Peter Mitsis <peter.mitsis@windriver.com>
2015-08-06 11:20:08 -0400

    cosmetic: Fix camel case in ia32 and ia32_pci platforms
    Converts camel case routine names to lower-case with underscores to comply with
    coding practices.
    
    Change-Id: I9fac0d3858389039c00882a7d2e97fcd4e4c812b
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 9216a2cbb970adfad9306381c35a890b60a0f4c9

--- arch/x86/platforms/ia32/system.c
+++ arch/x86/platforms/ia32/system.c
@@ -1,14 +1,14 @@
 DECL|bluetooth_init|function|static void bluetooth_init(void)
 DECL|bluetooth_init|macro|bluetooth_init
-DECL|consoleInit|function|static void consoleInit(void)
-DECL|consoleInit|macro|consoleInit
+DECL|console_init|function|static void console_init(void)
+DECL|console_init|macro|console_init
 DECL|ia32_init|function|static int ia32_init(struct device *arg)
-DECL|ioapicInit|function|static inline void ioapicInit(void)
-DECL|ioapicInit|macro|ioapicInit
-DECL|loapicInit|function|static inline void loapicInit(void)
-DECL|loapicInit|macro|loapicInit
-DECL|picInit|macro|picInit
-DECL|picInit|macro|picInit
+DECL|ioapic_init|function|static inline void ioapic_init(void)
+DECL|ioapic_init|macro|ioapic_init
+DECL|loapic_init|function|static inline void loapic_init(void)
+DECL|loapic_init|macro|loapic_init
+DECL|pic_init|macro|pic_init
+DECL|pic_init|macro|pic_init
 DECL|uartIrqProg|macro|uartIrqProg
 DECL|uartIrqProg|macro|uartIrqProg
 DECL|uart_generic_info_init|function|void uart_generic_info_init(struct uart_init_info *p_info)
--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -1,4 +1,4 @@
-DECL|consoleInit|function|static void consoleInit(void)
-DECL|consoleInit|macro|consoleInit
+DECL|console_init|function|static void console_init(void)
+DECL|console_init|macro|console_init
 DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
 DECL|uart_generic_info_init|function|void uart_generic_info_init(struct uart_init_info *p_info)
