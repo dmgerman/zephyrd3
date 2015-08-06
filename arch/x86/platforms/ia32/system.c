b52feef3978cd336057beffd80d6b24c4294a481
Peter Mitsis <peter.mitsis@windriver.com>
2015-08-06 11:17:09 -0400

    x86: Rename pc_init() to ia32_init()
    The new name corresponds to the platform (ia32), whereas the old name referred
    to the old platform/bsp name.
    
    Change-Id: Ia8fe5d2b35bceb7e04dd8ac9c41b9de7947ed1f9
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 0ea6e6003555539e9f260c557ef9001c67116bd0

--- arch/x86/platforms/ia32/system.c
+++ arch/x86/platforms/ia32/system.c
@@ -2,11 +2,11 @@ DECL|bluetooth_init|function|static void bluetooth_init(void)
 DECL|bluetooth_init|macro|bluetooth_init
 DECL|consoleInit|function|static void consoleInit(void)
 DECL|consoleInit|macro|consoleInit
+DECL|ia32_init|function|static int ia32_init(struct device *arg)
 DECL|ioapicInit|function|static inline void ioapicInit(void)
 DECL|ioapicInit|macro|ioapicInit
 DECL|loapicInit|function|static inline void loapicInit(void)
 DECL|loapicInit|macro|loapicInit
-DECL|pc_init|function|static int pc_init(struct device *arg)
 DECL|picInit|macro|picInit
 DECL|picInit|macro|picInit
 DECL|uartIrqProg|macro|uartIrqProg
