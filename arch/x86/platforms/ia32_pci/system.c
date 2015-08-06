cf8f75165895ff78bdac79e4f879bf49d7ee4843
Peter Mitsis <peter.mitsis@windriver.com>
2015-08-06 11:23:41 -0400

    x86: Rename quark_init() to ia32_pci_init()
    The new name corresponds to the platform (ia32_pci), whereas the old name
    referred to the old platform/bsp name.
    
    Change-Id: I1b6f3416db146af4fdbadf70d0ab153741c781d5
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 3ecf65e5ef77ca5780419de9bc4dce8fba969a17

--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -1,4 +1,4 @@
 DECL|consoleInit|function|static void consoleInit(void)
 DECL|consoleInit|macro|consoleInit
-DECL|quark_init|function|static int quark_init(struct device *arg)
+DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
 DECL|uart_generic_info_init|function|void uart_generic_info_init(struct uart_init_info *p_info)
