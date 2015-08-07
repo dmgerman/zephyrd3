69f1251e4ab1d07d37e309eadef19f785d355e72
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2015-08-07 16:40:04 +0300

    net: contiki: Add option to set/get PAN ID and get next framer seqno
    Importing patch b541cd24725591f586162497b493f37902120217 from Contiki.
    Original commit by Niclas Finne <nfi@sics.se>
    
    Change-Id: Id8a287c2cd21966039397ab7a596d978a14941f2
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: e6e55bc943b3178bc1bdd3f61cbee1858494c6fc

--- net/ip/contiki/mac/framer-802154.c
+++ net/ip/contiki/mac/framer-802154.c
@@ -1,15 +1,12 @@
 DECL|DEBUG|macro|DEBUG
-DECL|PRINTADDR|macro|PRINTADDR
-DECL|PRINTADDR|macro|PRINTADDR
-DECL|PRINTF|macro|PRINTF
-DECL|PRINTF|macro|PRINTF
 DECL|create_frame|function|create_frame(struct net_mbuf *buf, int do_create)
 DECL|create|function|create(struct net_mbuf *buf)
+DECL|framer_802154_get_pan_id|function|framer_802154_get_pan_id(void)
+DECL|framer_802154_next_seqno|function|framer_802154_next_seqno(void)
+DECL|framer_802154_set_pan_id|function|framer_802154_set_pan_id(uint16_t pan)
 DECL|framer_802154|variable|framer_802154
 DECL|hdr_length|function|hdr_length(struct net_mbuf *buf)
-DECL|initialized|variable|initialized
 DECL|is_broadcast_addr|function|is_broadcast_addr(uint8_t mode, uint8_t *addr)
-DECL|mac_dsn|variable|mac_dsn
 DECL|mac_dst_pan_id|variable|mac_dst_pan_id
 DECL|mac_src_pan_id|variable|mac_src_pan_id
 DECL|parse|function|parse(struct net_mbuf *buf)
