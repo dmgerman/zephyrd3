d64f46d2382f03c1526d688ac8721709825497b0
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-10 17:37:25 -0400

    pipes/mailboxes: rename functions using mem blocks
      task_pipe_put_async -> task_pipe_block_put
    
      task_mbox_put_async -> task_mbox_block_put
    
      task_mbox_data_get_async_block -> task_mbox_data_block_get
      task_mbox_data_get_async_block_wait -> task_mbox_data_block_get_wait
      task_mbox_data_get_async_block_wait_timeout ->
           task_mbox_data_block_get_wait_timeout
    
    Previous names, focusing on 'async', were misleading, because:
    
    - some of those APIs can be used synchronously as well
    - other APIs can also do asynchronous transfer, and don't have 'async'
      in their names
    - the key concept of these APIs is that they use memory pool blocks
      rather than raw data buffers.
    
    Change-Id: I0c08a6cf950ab23bb4172ce25eb6f9886b037649
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 87564a4602743df5f49d0e5cfa9b9bc9ed23cf75

--- include/microkernel/pipe.h
+++ include/microkernel/pipe.h
@@ -1,10 +1,10 @@
 DECL|DEFINE_PIPE|macro|DEFINE_PIPE
 DECL|PIPE_H|macro|PIPE_H
 DECL|__K_PIPE_INITIALIZER|macro|__K_PIPE_INITIALIZER
+DECL|task_pipe_block_put|macro|task_pipe_block_put
 DECL|task_pipe_get_wait_timeout|macro|task_pipe_get_wait_timeout
 DECL|task_pipe_get_wait|macro|task_pipe_get_wait
 DECL|task_pipe_get|macro|task_pipe_get
-DECL|task_pipe_put_async|macro|task_pipe_put_async
 DECL|task_pipe_put_wait_timeout|macro|task_pipe_put_wait_timeout
 DECL|task_pipe_put_wait|macro|task_pipe_put_wait
 DECL|task_pipe_put|macro|task_pipe_put
