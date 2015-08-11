5b321597ae83e8bef94fd017d4be0d995a2c0e09
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-08-11 13:33:26 -0500

    Add context switch profile point.
    Add the context switch profile point for x86 and ARM arquitectures.
    
    Change-Id: Ib7205059104ed47b96ba75b8cfefec3ff35f6813
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 520542bef2a1f712765f7cd24d3188bfb86cc73f

--- kernel/nanokernel/profiler.c
+++ kernel/nanokernel/profiler.c
@@ -1,3 +1,6 @@
+DECL|_collector_context|variable|_collector_context
 DECL|_sys_profiler_buffer|variable|_sys_profiler_buffer
+DECL|_sys_profiler_context_switch|function|void _sys_profiler_context_switch(void)
 DECL|_sys_profiler_init|function|static int _sys_profiler_init(struct device *arg)
 DECL|sys_profiler_put_timed|function|void sys_profiler_put_timed(uint16_t event_id)
+DECL|sys_profiler_register_as_collector|function|void sys_profiler_register_as_collector(void)
