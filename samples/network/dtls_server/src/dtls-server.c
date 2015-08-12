8793a6a391288bedc5fdf8a58ad1d7d17deb44ea
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-08-12 16:35:41 +0300

    net: apps: Add dtls-server test application
    The DTLS server is running in Zephyr and is listening on port 4242
    and when it receives DTLS UDP packet, it will reverse the data and
    send it back to the caller.
    
    Use the dtls-client application running in host to connect to
    the dtls-server.
    
    Change-Id: I3306cb242498019ffc9e0519b06061c36db96f78
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 29faed4aa8c7d970fd7e17a3bd3b82b621f6f328

---
+++ samples/network/dtls_server/src/dtls-server.c
@@ -0,0 +1,30 @@
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_PORT|macro|MY_PORT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|ecdsa_priv_key|variable|ecdsa_priv_key
+DECL|ecdsa_pub_key_x|variable|ecdsa_pub_key_x
+DECL|ecdsa_pub_key_y|variable|ecdsa_pub_key_y
+DECL|fiberStack|variable|fiberStack
+DECL|get_context|function|static inline struct net_context *get_context(void)
+DECL|get_ecdsa_key|function|static int get_ecdsa_key(struct dtls_context_t *ctx, const session_t *session, const dtls_ecdsa_key_t **result)
+DECL|get_psk_info|function|static int get_psk_info(struct dtls_context_t *ctx,const session_t *session, dtls_credentials_type_t type, const unsigned char *id, size_t id_len, unsigned char *result, size_t result_length)
+DECL|handle_event|function|static int handle_event(struct dtls_context_t *ctx, session_t *session,dtls_alert_level_t level, unsigned short code)
+DECL|in4addr_any|variable|in4addr_any
+DECL|in4addr_my|variable|in4addr_my
+DECL|in6addr_any|variable|in6addr_any
+DECL|in6addr_my|variable|in6addr_my
+DECL|init_dtls|function|static void init_dtls(struct net_context *recv, dtls_context_t **dtls)
+DECL|init_server|function|static inline void init_server()
+DECL|main|function|void main(void)
+DECL|my_mac|variable|my_mac
+DECL|read_from_peer|function|static int read_from_peer(struct dtls_context_t *ctx, session_t *session, uint8 *data, size_t len)
+DECL|receive_message|function|static inline void receive_message(const char *name, struct net_context *recv, dtls_context_t *dtls)
+DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
+DECL|send_to_peer|function|static int send_to_peer(struct dtls_context_t *ctx,session_t *session, uint8 *data, size_t len)
+DECL|startup|function|void startup(void)
+DECL|verify_ecdsa_key|function|static int verify_ecdsa_key(struct dtls_context_t *ctx, const session_t *session, const unsigned char *other_pub_x, const unsigned char *other_pub_y, size_t key_size)
