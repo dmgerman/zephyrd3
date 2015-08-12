b377c0c58bbef7c86c718c7cfdf1ba7072b96dc9
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-12 15:37:58 -0400

    microkernel: rename K_swapper to _k_server
    Align with the newer terminology used for microkernel internal symbols.
    
    Change-Id: I623b383f90d9e37a49429a79774c7f7a4953bd5f
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: aa78793e401637af11d80049ce4052c59b25398c

--- kernel/microkernel/k_server.c
+++ kernel/microkernel/k_server.c
@@ -1,2 +1,2 @@
-DECL|K_swapper|function|FUNC_NORETURN void K_swapper(int parameter1, /* not used */ int parameter2 /* not used */ )
+DECL|_k_server|function|FUNC_NORETURN void _k_server(int parameter1, /* not used */ int parameter2 /* not used */ )
 DECL|next_task_select|function|static struct k_proc *next_task_select(void)
