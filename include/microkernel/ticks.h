dda710f1ab4a6cc1e5ceaebdd93d7e7df764a51e
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-12 16:42:37 -0400

    timers: consolidate task_timer_start/restart()
    They use the same logic and _k_server handler, so only have one
    implementation of the user-facing API.
    
    Instead of using ENDLIST, create a new internal _USE_CURRENT_SEM symbol
    that reflects what the implementation is doing.
    
    Change-Id: I5c50efd15f4e97b778b4b5efd5ec931384a8631f
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: b8693ad72b7eb94b54f14b9b706e879a45b394fa

--- include/microkernel/ticks.h
+++ include/microkernel/ticks.h
@@ -3,3 +3,4 @@ DECL|isr_cycle_get_32|macro|isr_cycle_get_32
 DECL|isr_tick_get_32|macro|isr_tick_get_32
 DECL|isr_tick_get|macro|isr_tick_get
 DECL|task_tick_delta_32|function|static inline int32_t task_tick_delta_32(int64_t *reftime)
+DECL|task_timer_restart|function|static inline void task_timer_restart(ktimer_t timer, int32_t duration,int32_t period)
