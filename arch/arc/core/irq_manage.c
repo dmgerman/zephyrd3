a7a66603e1fc23b5428827cd0717dd5b1fff47a1
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-12 15:15:48 -0400

    irq: make utility functions private
    irq_handler_set, irq_priority_set and irq_disconnect have been made
    private by prepending an underscore to their names:
    
    	irq_handler_set -> irq_handler_set
    	irq_priority_set -> irq_priority_set
    	irq_disconnect -> irq_disconnect
    
    The prototypes have been removed from header files when possible, and
    extern statements used in C code where they were called.
    
    _irq_priority_set() for ARM is still in the header file because
    IRQ_CONFIG() relies on it.
    
    Change-Id: I2ad585f8156ff80250f6d9eeca4a249a4477fd9d
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: c1f9fd888da79cd6510b5405131dca54b0072a7b

--- arch/arc/core/irq_manage.c
+++ arch/arc/core/irq_manage.c
@@ -1,7 +1,7 @@
+DECL|_irq_disconnect|function|void _irq_disconnect(unsigned int irq)
+DECL|_irq_handler_set|function|void _irq_handler_set(unsigned int irq, void (*old)(void *arg), void (*new)(void *arg), void *arg
+DECL|_irq_priority_set|function|void _irq_priority_set(unsigned int irq, unsigned int prio )
 DECL|_irq_spurious|function|void _irq_spurious(void *unused)
 DECL|irq_connect|function|int irq_connect(unsigned int irq, unsigned int prio, void (*isr)(void *arg), void *arg
 DECL|irq_disable|function|void irq_disable(unsigned int irq)
-DECL|irq_disconnect|function|void irq_disconnect(unsigned int irq)
 DECL|irq_enable|function|void irq_enable(unsigned int irq)
-DECL|irq_handler_set|function|void irq_handler_set(unsigned int irq, void (*old)(void *arg), void (*new)(void *arg), void *arg
-DECL|irq_priority_set|function|void irq_priority_set(unsigned int irq, unsigned int prio )
--- arch/x86/core/inthndlset.c
+++ arch/x86/core/inthndlset.c
@@ -1,2 +1,2 @@
 DECL|FIRST_OPT_OPCODE_OFF|macro|FIRST_OPT_OPCODE_OFF
-DECL|irq_handler_set|function|void irq_handler_set(unsigned int vector, void (*oldRoutine)(void *parameter), void (*newRoutine)(void *parameter), void *parameter)
+DECL|_irq_handler_set|function|void _irq_handler_set(unsigned int vector, void (*oldRoutine)(void *parameter), void (*newRoutine)(void *parameter), void *parameter)
