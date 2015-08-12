3dadc85aa842e7af897ea720a186e1731681024f
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-12 15:41:41 -0400

    microkernel: rename _k_server parameters
    Reflect the fact that they are not used.
    
    Change-Id: I6ea83cb2c7532e10988cbf4350edccb78f444328
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: a5a6ba8e4e38c2f48d08e0478bc49537a147117f

--- kernel/microkernel/k_server.c
+++ kernel/microkernel/k_server.c
@@ -1,2 +1,2 @@
-DECL|_k_server|function|FUNC_NORETURN void _k_server(int parameter1, /* not used */ int parameter2 /* not used */ )
+DECL|_k_server|function|FUNC_NORETURN void _k_server(int unused1, int unused2)
 DECL|next_task_select|function|static struct k_proc *next_task_select(void)
