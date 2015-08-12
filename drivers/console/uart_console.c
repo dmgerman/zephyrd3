ef066d4b141e798ca5b4f9cc503662c70ef81701
Daniel Leung <daniel.leung@intel.com>
2015-08-12 10:17:35 -0700

    console: uart_console: decouple uart console init from UART init
    With this, the UART console has its own initialization which is not
    dependent on UART init routines.
    
    This will allow the app to utilize all UARTs for other purposes,
    instead of always reserving one for console. Future patch
    will enable this choice.
    
    Note that the console init is effectively demoted to pure_init
    from pure_early_init. The UART console depends on UART being
    initialized, but there is no deferred init at this point.
    So this initializes the console a bit later than UARTs,
    simply to make sure the UART is initialized before using it.
    Once there is a mechanism for some type of deferred initialization,
    the UART console init can be promoted back to pure_early.
    
    Change-Id: Iba95197b13384cb1a46e34c78638348b7a0bec8c
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: ad2d29689e223747bf8a48b618325adddcb9b4cd

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -7,7 +7,8 @@ DECL|console_input_init|macro|console_input_init
 DECL|lines_queue|variable|lines_queue
 DECL|pos|variable|pos
 DECL|read_uart|function|static int read_uart(struct device *uart, uint8_t *buf, unsigned int size)
-DECL|uart_console_init|function|void uart_console_init(void)
+DECL|uart_console_hook_install|function|void uart_console_hook_install(void)
+DECL|uart_console_init|function|static int uart_console_init(struct device *arg)
 DECL|uart_console_isr|function|void uart_console_isr(void *unused)
 DECL|uart_register_input|function|void uart_register_input(struct nano_fifo *avail, struct nano_fifo *lines)
 DECL|uart_register_input|macro|uart_register_input
