d9274f2ec6e6f0ae0519f0c8b29c698e32976373
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-14 17:22:50 -0400

    ffs: change find_[lsb|msb]_set parameter type to uint32_t
    Highlight the fact that find_[lsb|msb]_set operate on a 32-bit word.
    
    Change-Id: I24cee7709ea6497508dbc7f96a7b4d74fa4bc257
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 7710d0e083cc097bad52fbe5645ce81a4ab67fa0

--- include/arch/arc/v2/ffs.h
+++ include/arch/arc/v2/ffs.h
@@ -1,3 +1,3 @@
 DECL|_ARCH_ARC_V2_FFS_H_|macro|_ARCH_ARC_V2_FFS_H_
-DECL|find_lsb_set|function|static ALWAYS_INLINE unsigned int find_lsb_set(unsigned int op)
-DECL|find_msb_set|function|static ALWAYS_INLINE unsigned int find_msb_set(unsigned int op)
+DECL|find_lsb_set|function|static ALWAYS_INLINE unsigned int find_lsb_set(uint32_t op)
+DECL|find_msb_set|function|static ALWAYS_INLINE unsigned int find_msb_set(uint32_t op)
--- include/arch/arm/CortexM/asm_inline_gcc.h
+++ include/arch/arm/CortexM/asm_inline_gcc.h
@@ -4,7 +4,7 @@ DECL|_SCS_ICSR_PENDSV|macro|_SCS_ICSR_PENDSV
 DECL|_SCS_ICSR_RETTOBASE|macro|_SCS_ICSR_RETTOBASE
 DECL|_SCS_ICSR_UNPENDSV|macro|_SCS_ICSR_UNPENDSV
 DECL|_SCS_ICSR|macro|_SCS_ICSR
-DECL|find_lsb_set|function|static ALWAYS_INLINE unsigned int find_lsb_set(unsigned int op)
-DECL|find_msb_set|function|static ALWAYS_INLINE unsigned int find_msb_set(unsigned int op)
+DECL|find_lsb_set|function|static ALWAYS_INLINE unsigned int find_lsb_set(uint32_t op)
+DECL|find_msb_set|function|static ALWAYS_INLINE unsigned int find_msb_set(uint32_t op)
 DECL|irq_lock|function|static ALWAYS_INLINE unsigned int irq_lock(void)
 DECL|irq_unlock|function|static ALWAYS_INLINE void irq_unlock(unsigned int key)
