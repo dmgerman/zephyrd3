d0633fa3386759da7c3c30b7f03a8dd8e5c243fe
Anas Nashif <anas.nashif@intel.com>
2015-08-19 12:10:36 -0400

    doc: document pipe API in header file
    Change-Id: Ibac21c7fef5ee3a7313025aa91980cad170e5a0f
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: d62d1ac256b611e986625fcda62053cdadd7e861

--- kernel/microkernel/k_pipe.c
+++ kernel/microkernel/k_pipe.c
@@ -1,4 +1,4 @@
 DECL|_k_pipe_init|function|void _k_pipe_init(void)
-DECL|_task_pipe_block_put|function|int _task_pipe_block_put(kpipe_t Id, struct k_block Block, int iReqSize2Xfer, ksem_t sema)
-DECL|_task_pipe_get|function|int _task_pipe_get(kpipe_t Id, void *pBuffer, int iNbrBytesToRead, int *piNbrBytesRead, K_PIPE_OPTION Option, int32_t TimeOut)
-DECL|_task_pipe_put|function|int _task_pipe_put(kpipe_t Id, void *pBuffer, int iNbrBytesToWrite, int *piNbrBytesWritten, K_PIPE_OPTION Option, int32_t TimeOut)
+DECL|_task_pipe_block_put|function|int _task_pipe_block_put(kpipe_t Id, struct k_block Block,int iReqSize2Xfer, ksem_t sema)
+DECL|_task_pipe_get|function|int _task_pipe_get(kpipe_t Id, void *pBuffer,int iNbrBytesToRead, int *piNbrBytesRead, K_PIPE_OPTION Option, int32_t TimeOut)
+DECL|_task_pipe_put|function|int _task_pipe_put(kpipe_t Id, void *pBuffer,int iNbrBytesToWrite, int *piNbrBytesWritten, K_PIPE_OPTION Option, int32_t TimeOut)
