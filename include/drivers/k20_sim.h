8e83e69aa019810ad8b7cf489a711ba9f61a0413
Daniel Leung <daniel.leung@intel.com>
2015-08-20 16:34:19 -0700

    k20: sim: clean up camel case and doxygen for file description
    This converts away from camel case according to the coding style.
    Also, the file description has been made into doxygen style.
    
    Change-Id: Ib4dd85a4a96e4e87279c92fa243d3f860ebe63f2
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 8729b3c802012d5ba078f187163f51378a5bd6eb

--- include/drivers/k20_sim.h
+++ include/drivers/k20_sim.h
@@ -27,13 +27,13 @@ DECL|SIM_SOPT4_t|typedef|} SIM_SOPT4_t; /* 0x100C */
 DECL|SIM_SOPT5_t|typedef|} SIM_SOPT5_t; /* 0x1010 */
 DECL|SIM_UART_CLK_ENABLE|macro|SIM_UART_CLK_ENABLE
 DECL|_K20SIM_H_|macro|_K20SIM_H_
-DECL|_k20_sim_uart_clk_enable|function|static ALWAYS_INLINE void _k20_sim_uart_clk_enable(K20_SIM_t *sim_p, uint8_t which)
-DECL|clkOutSel|member|uint32_t clkOutSel : 3 __packed;
+DECL|_k20_sim_uart_clk_enable|function|static ALWAYS_INLINE void _k20_sim_uart_clk_enable(K20_SIM_t *sim, uint8_t port)
+DECL|clk_out_sel|member|uint32_t clk_out_sel : 3 __packed;
 DECL|clkdiv1|member|SIM_CLKDIV1_t clkdiv1; /* 0x1044 */
 DECL|clkdiv2|member|uint32_t clkdiv2; /* 0x1048 */
-DECL|cmpClkEn|member|uint32_t cmpClkEn : 1 __packed;
-DECL|cmtClkEn_0|member|uint32_t cmtClkEn_0 : 1 __packed;
-DECL|ewmClkEn_0|member|uint32_t ewmClkEn_0 : 1 __packed;
+DECL|cmp_clk_en|member|uint32_t cmp_clk_en : 1 __packed;
+DECL|cmt_clk_en_0|member|uint32_t cmt_clk_en_0 : 1 __packed;
+DECL|ewm_clk_en_0|member|uint32_t ewm_clk_en_0 : 1 __packed;
 DECL|field|member|} field;
 DECL|field|member|} field;
 DECL|field|member|} field;
@@ -43,35 +43,35 @@ DECL|field|member|} field;
 DECL|field|member|} field;
 DECL|field|member|} field;
 DECL|field|member|} field;
-DECL|flexBusSL|member|uint32_t flexBusSL : 2 __packed;
-DECL|fllPllClkSel|member|uint32_t fllPllClkSel : 1 __packed;
-DECL|ftm0ClkSel|member|uint32_t ftm0ClkSel : 1 __packed;
-DECL|ftm0Flt0|member|uint32_t ftm0Flt0 : 1 __packed;
-DECL|ftm0Flt1|member|uint32_t ftm0Flt1 : 1 __packed;
-DECL|ftm0Flt2|member|uint32_t ftm0Flt2 : 1 __packed;
-DECL|ftm0Trg0Src|member|uint32_t ftm0Trg0Src : 1 __packed;
-DECL|ftm0Trg1Src|member|uint32_t ftm0Trg1Src : 1 __packed;
-DECL|ftm1Ch0Src|member|uint32_t ftm1Ch0Src : 2 __packed;
-DECL|ftm1ClkSel|member|uint32_t ftm1ClkSel : 1 __packed;
-DECL|ftm1Flt0|member|uint32_t ftm1Flt0 : 1 __packed;
-DECL|ftm2Ch0Src|member|uint32_t ftm2Ch0Src : 2 __packed;
-DECL|ftm2ClkSel|member|uint32_t ftm2ClkSel : 1 __packed;
-DECL|ftm2Flt0|member|uint32_t ftm2Flt0 : 1 __packed;
-DECL|i2c0ClkEn|member|uint32_t i2c0ClkEn : 1 __packed;
-DECL|i2c1ClkEn|member|uint32_t i2c1ClkEn : 1 __packed;
+DECL|flex_bus_sl|member|uint32_t flex_bus_sl : 2 __packed;
+DECL|fll_pll_clk_sel|member|uint32_t fll_pll_clk_sel : 1 __packed;
+DECL|ftm0_clk_sel|member|uint32_t ftm0_clk_sel : 1 __packed;
+DECL|ftm0_flt0|member|uint32_t ftm0_flt0 : 1 __packed;
+DECL|ftm0_flt1|member|uint32_t ftm0_flt1 : 1 __packed;
+DECL|ftm0_flt2|member|uint32_t ftm0_flt2 : 1 __packed;
+DECL|ftm0_trg0_src|member|uint32_t ftm0_trg0_src : 1 __packed;
+DECL|ftm0_trg1_src|member|uint32_t ftm0_trg1_src : 1 __packed;
+DECL|ftm1_ch0_src|member|uint32_t ftm1_ch0_src : 2 __packed;
+DECL|ftm1_clk_sel|member|uint32_t ftm1_clk_sel : 1 __packed;
+DECL|ftm1_flt0|member|uint32_t ftm1_flt0 : 1 __packed;
+DECL|ftm2_ch0_src|member|uint32_t ftm2_ch0_src : 2 __packed;
+DECL|ftm2_clk_sel|member|uint32_t ftm2_clk_sel : 1 __packed;
+DECL|ftm2_flt0|member|uint32_t ftm2_flt0 : 1 __packed;
+DECL|i2c0_clk_en|member|uint32_t i2c0_clk_en : 1 __packed;
+DECL|i2c1_clk_en|member|uint32_t i2c1_clk_en : 1 __packed;
 DECL|lptimer|member|uint32_t lptimer : 1 __packed;
-DECL|osc32kSel|member|uint32_t osc32kSel : 2 __packed;
+DECL|osc32k_sel|member|uint32_t osc32k_sel : 2 __packed;
 DECL|outdiv1|member|uint32_t outdiv1 : 4 __packed;
 DECL|outdiv2|member|uint32_t outdiv2 : 4 __packed;
 DECL|outdiv3|member|uint32_t outdiv3 : 4 __packed;
 DECL|outdiv4|member|uint32_t outdiv4 : 4 __packed;
-DECL|portA_ClkEn|member|uint32_t portA_ClkEn : 1 __packed;
-DECL|portB_ClkEn|member|uint32_t portB_ClkEn : 1 __packed;
-DECL|portC_ClkEn|member|uint32_t portC_ClkEn : 1 __packed;
-DECL|portD_ClkEn|member|uint32_t portD_ClkEn : 1 __packed;
-DECL|portE_ClkEn|member|uint32_t portE_ClkEn : 1 __packed;
-DECL|ptd7Pad|member|uint32_t ptd7Pad : 1 __packed;
-DECL|ramSize|member|uint32_t ramSize : 4 __packed;
+DECL|port_a_clk_en|member|uint32_t port_a_clk_en : 1 __packed;
+DECL|port_b_clk_en|member|uint32_t port_b_clk_en : 1 __packed;
+DECL|port_c_clk_en|member|uint32_t port_c_clk_en : 1 __packed;
+DECL|port_d_clk_en|member|uint32_t port_d_clk_en : 1 __packed;
+DECL|port_e_clk_en|member|uint32_t port_e_clk_en : 1 __packed;
+DECL|ptd7pad|member|uint32_t ptd7pad : 1 __packed;
+DECL|ram_size|member|uint32_t ram_size : 4 __packed;
 DECL|res0008_1003|member|uint8_t res0008_1003[0x1003 - 0x8]; /* 0x0008-0x1003 Reserved */
 DECL|res1008|member|uint32_t res1008; /* 0x1008 Reserved */
 DECL|res1014|member|uint32_t res1014; /* 0x1014 Reserved */
@@ -104,7 +104,7 @@ DECL|res_8_31|member|uint32_t res_8_31 : 24 __packed;
 DECL|res_8_31|member|uint32_t res_8_31 : 24 __packed;
 DECL|res_8_9|member|uint32_t res_8_9 : 2 __packed;
 DECL|res_9_17|member|uint32_t res_9_17 : 9 __packed;
-DECL|rtcClkOutSel|member|uint32_t rtcClkOutSel : 1 __packed;
+DECL|rtc_clk_out_sel|member|uint32_t rtc_clk_out_sel : 1 __packed;
 DECL|scgc1|member|SIM_SCGC1_t scgc1; /* 0x1028 */
 DECL|scgc2|member|uint32_t scgc2; /* 0x102C */
 DECL|scgc3|member|uint32_t scgc3; /* 0x1030 */
@@ -112,34 +112,34 @@ DECL|scgc4|member|SIM_SCGC4_t scgc4; /* 0x1034 */
 DECL|scgc5|member|SIM_SCGC5_t scgc5; /* 0x1038 */
 DECL|scgc6|member|uint32_t scgc6; /* 0x103C */
 DECL|scgc7|member|uint32_t scgc7; /* 0x1040 */
-DECL|sopt1Cfg|member|SIM_SOPT1CFG_t sopt1Cfg; /* 0x0004 */
+DECL|sopt1_cfg|member|SIM_SOPT1CFG_t sopt1_cfg; /* 0x0004 */
 DECL|sopt1|member|SIM_SOPT1_t sopt1; /* 0x0000 */
 DECL|sopt2|member|SIM_SOPT2_t sopt2; /* 0x1004 */
 DECL|sopt4|member|SIM_SOPT4_t sopt4; /* 0x100C */
 DECL|sopt5|member|SIM_SOPT5_t sopt5; /* 0x1010 */
 DECL|sopt7|member|uint32_t sopt7; /* 0x1018 */
-DECL|traceClkSel|member|uint32_t traceClkSel : 1 __packed;
+DECL|trace_clk_sel|member|uint32_t trace_clk_sel : 1 __packed;
 DECL|tsi|member|uint32_t tsi : 1 __packed;
-DECL|uart0ClkEn|member|uint32_t uart0ClkEn : 1 __packed;
-DECL|uart0RxSrc|member|uint32_t uart0RxSrc : 2 __packed;
-DECL|uart0RxSrc|member|uint32_t uart0RxSrc : 2 __packed;
-DECL|uart0TxSrc|member|uint32_t uart0TxSrc : 2 __packed;
-DECL|uart0TxSrc|member|uint32_t uart0TxSrc : 2 __packed;
-DECL|uart1ClkEn|member|uint32_t uart1ClkEn : 1 __packed;
-DECL|uart1RxSrc|member|uint32_t uart1RxSrc : 2 __packed;
-DECL|uart1RxSrc|member|uint32_t uart1RxSrc : 2 __packed;
-DECL|uart1TxSrc|member|uint32_t uart1TxSrc : 2 __packed;
-DECL|uart1TxSrc|member|uint32_t uart1TxSrc : 2 __packed;
-DECL|uart2ClkEn|member|uint32_t uart2ClkEn : 1 __packed;
-DECL|uart3ClkEn|member|uint32_t uart3ClkEn : 1 __packed;
-DECL|usbClkEn|member|uint32_t usbClkEn : 1 __packed;
-DECL|usbRegEn|member|uint32_t usbRegEn : 1 __packed;
-DECL|usbRegWriteEn|member|uint32_t usbRegWriteEn : 1 __packed;
-DECL|usbSrc|member|uint32_t usbSrc : 1 __packed;
-DECL|usbStopStby|member|uint32_t usbStopStby : 1 __packed;
-DECL|usbStopWriteEn|member|uint32_t usbStopWriteEn : 1 __packed;
-DECL|usbVoltStby|member|uint32_t usbVoltStby : 1 __packed;
-DECL|usbVoltWriteEn|member|uint32_t usbVoltWriteEn : 1 __packed;
+DECL|uart0_clk_en|member|uint32_t uart0_clk_en : 1 __packed;
+DECL|uart0_rx_src|member|uint32_t uart0_rx_src : 2 __packed;
+DECL|uart0_rx_src|member|uint32_t uart0_rx_src : 2 __packed;
+DECL|uart0_tx_src|member|uint32_t uart0_tx_src : 2 __packed;
+DECL|uart0_tx_src|member|uint32_t uart0_tx_src : 2 __packed;
+DECL|uart1_clk_en|member|uint32_t uart1_clk_en : 1 __packed;
+DECL|uart1_rx_src|member|uint32_t uart1_rx_src : 2 __packed;
+DECL|uart1_rx_src|member|uint32_t uart1_rx_src : 2 __packed;
+DECL|uart1_tx_src|member|uint32_t uart1_tx_src : 2 __packed;
+DECL|uart1_tx_src|member|uint32_t uart1_tx_src : 2 __packed;
+DECL|uart2_clk_en|member|uint32_t uart2_clk_en : 1 __packed;
+DECL|uart3_clk_en|member|uint32_t uart3_clk_en : 1 __packed;
+DECL|usb_clk_en|member|uint32_t usb_clk_en : 1 __packed;
+DECL|usb_reg_en|member|uint32_t usb_reg_en : 1 __packed;
+DECL|usb_reg_write_en|member|uint32_t usb_reg_write_en : 1 __packed;
+DECL|usb_src|member|uint32_t usb_src : 1 __packed;
+DECL|usb_stop_stby|member|uint32_t usb_stop_stby : 1 __packed;
+DECL|usb_stop_write_en|member|uint32_t usb_stop_write_en : 1 __packed;
+DECL|usb_volt_stby|member|uint32_t usb_volt_stby : 1 __packed;
+DECL|usb_volt_write_en|member|uint32_t usb_volt_write_en : 1 __packed;
 DECL|value|member|uint32_t value;
 DECL|value|member|uint32_t value;
 DECL|value|member|uint32_t value;
@@ -149,4 +149,4 @@ DECL|value|member|uint32_t value;
 DECL|value|member|uint32_t value; /* reset 0 */
 DECL|value|member|uint32_t value; /* reset 0x0001 0000 */
 DECL|value|member|uint32_t value; /* reset = 0x8000 F03F */
-DECL|vrefClkEn|member|uint32_t vrefClkEn : 1 __packed;
+DECL|vref_clk_en|member|uint32_t vref_clk_en : 1 __packed;
