8fc0117b7e5f54569de62957ac43ae097766cd9a
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-08-20 13:56:10 -0400

    util: make is_power_of_two() a public utility
    Somewhat useful utility that should only have one implementation, so it
    can impede on the normally-available application namespace.
    
    Change-Id: I085850177c231fdf58634f97e897c4d2e1a5cffb
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 4dca2dfad34422fbbb46f964c3b9d4d5ca9b7ecf

--- include/misc/util.h
+++ include/misc/util.h
@@ -9,7 +9,7 @@ DECL|MB|macro|MB
 DECL|MHZ|macro|MHZ
 DECL|ROUND_DOWN|macro|ROUND_DOWN
 DECL|ROUND_UP|macro|ROUND_UP
-DECL|_IsPowerOfTwo|function|static inline int _IsPowerOfTwo(unsigned int x)
 DECL|_UTIL__H_|macro|_UTIL__H_
+DECL|is_power_of_two|function|static inline int is_power_of_two(unsigned int x)
 DECL|max|macro|max
 DECL|min|macro|min
