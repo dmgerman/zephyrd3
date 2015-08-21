f4f2fa261a9dcda9435b3401034566c3b47b4ec2
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-08-21 12:52:25 +0300

    pci: Add support for filtering by function
    This will be useful for PCI based drivers to integrate fully within
    the device driver model, where they will have to provide the function
    they are found.
    
    Change-Id: I7d64a4c6727cee52cbcb743c859cda43ac1a853b
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: ff39559146fc2f0a2a63dbcb4db74cb2eae60e03

--- include/drivers/pci/pci.h
+++ include/drivers/pci/pci.h
@@ -1,16 +1,17 @@
 DECL|BAR_SPACE_IO|macro|BAR_SPACE_IO
 DECL|BAR_SPACE_MEM|macro|BAR_SPACE_MEM
 DECL|PCI_BAR_ANY|macro|PCI_BAR_ANY
+DECL|PCI_FUNCTION_ANY|macro|PCI_FUNCTION_ANY
 DECL|PCI_MAX_BARS|macro|PCI_MAX_BARS
+DECL|PCI_MAX_FUNCTIONS|macro|PCI_MAX_FUNCTIONS
 DECL|_PCI_H_|macro|_PCI_H_
 DECL|addr|member|uint32_t addr; /* I/O or memory region address */
 DECL|bar|member|uint16_t bar:3;
 DECL|class|member|uint16_t class:8;
 DECL|device_id|member|uint16_t device_id;
-DECL|function|member|uint16_t function:3;
+DECL|function|member|uint16_t function:4;
 DECL|irq|member|int irq;
 DECL|mem_type|member|uint16_t mem_type:1; /* memory type: BAR_SPACE_MEM/BAR_SPACE_IO */
 DECL|pci_dev_info|struct|struct pci_dev_info {
 DECL|size|member|uint32_t size; /* memory region size */
-DECL|unused|member|uint16_t unused:1;
 DECL|vendor_id|member|uint16_t vendor_id;
