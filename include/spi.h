051564e38b8bc935b36c86e0f9c8bf5d2a9928e9
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-08-21 12:48:54 +0300

    spi: Fix the generic API
    - Add a spi_slave_select function for controllers supporting per-slave
      addressing
    - Adding one full-duplex r/w function
    - All r/w function go through one unique driver call
    - Only one callback for all
    
    Change-Id: Ie97e6a6119b2b34093598a8eb5dbfe0665bf08f3
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: dd12326e862726aa1eaaf6430e7ccb633fc58bab

--- include/spi.h
+++ include/spi.h
@@ -1,3 +1,6 @@
+DECL|SPI_CB_READ|enumerator|SPI_CB_READ = 2,
+DECL|SPI_CB_TRANSCEIVE|enumerator|SPI_CB_TRANSCEIVE = 3
+DECL|SPI_CB_WRITE|enumerator|SPI_CB_WRITE = 1,
 DECL|SPI_MODE_CPHA|macro|SPI_MODE_CPHA
 DECL|SPI_MODE_CPOL|macro|SPI_MODE_CPOL
 DECL|SPI_MODE_LOOP|macro|SPI_MODE_LOOP
@@ -9,23 +12,26 @@ DECL|SPI_TRANSFER_MSB|macro|SPI_TRANSFER_MSB
 DECL|SPI_WORD_SIZE_GET|macro|SPI_WORD_SIZE_GET
 DECL|SPI_WORD_SIZE_MASK|macro|SPI_WORD_SIZE_MASK
 DECL|__SPI_H__|macro|__SPI_H__
-DECL|cb_receive|member|spi_callback cb_receive;
-DECL|cb_transmit|member|spi_callback cb_transmit;
+DECL|callback|member|spi_callback callback;
 DECL|configure|member|spi_api_configure configure;
 DECL|config|member|uint32_t config;
 DECL|max_sys_freq|member|uint32_t max_sys_freq;
-DECL|read|member|spi_api_io read;
 DECL|resume|member|spi_api_control resume;
+DECL|slave_select|member|spi_api_slave_select slave_select;
 DECL|spi_api_configure|typedef|typedef int (*spi_api_configure)(struct device *dev, struct spi_config *config);
 DECL|spi_api_control|typedef|typedef int (*spi_api_control)(struct device *dev);
-DECL|spi_api_io|typedef|typedef int (*spi_api_io)(struct device *dev, uint8_t *buf, uint32_t len);
-DECL|spi_callback|typedef|typedef void (*spi_callback)(struct device *dev);
+DECL|spi_api_io|typedef|typedef int (*spi_api_io)(struct device *dev,
+DECL|spi_api_slave_select|typedef|typedef int (*spi_api_slave_select)(struct device *dev, uint32_t slave);
+DECL|spi_callback|typedef|typedef void (*spi_callback)(struct device *dev, enum spi_cb_type cb_type);
+DECL|spi_cb_type|enum|enum spi_cb_type {
 DECL|spi_configure|function|inline int spi_configure(struct device *dev, struct spi_config *config)
 DECL|spi_config|struct|struct spi_config {
 DECL|spi_driver_api|struct|struct spi_driver_api {
 DECL|spi_read|function|inline int spi_read(struct device *dev, uint8_t *buf, uint32_t len)
 DECL|spi_resume|function|inline int spi_resume(struct device *dev)
+DECL|spi_slave_select|function|inline int spi_slave_select(struct device *dev, uint32_t slave)
 DECL|spi_suspend|function|inline int spi_suspend(struct device *dev)
+DECL|spi_transceive|function|inline int spi_transceive(struct device *dev, uint8_t *tx_buf, uint32_t tx_buf_len, uint8_t *rx_buf, uint32_t rx_buf_len)
 DECL|spi_write|function|inline int spi_write(struct device *dev, uint8_t *buf, uint32_t len)
 DECL|suspend|member|spi_api_control suspend;
-DECL|write|member|spi_api_io write;
+DECL|transceive|member|spi_api_io transceive;
