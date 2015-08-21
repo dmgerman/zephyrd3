e04e4b79d817eb384a0dfbd75dc08c49e5baf725
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-08-21 12:50:57 +0300

    sys_io: Add memory bit manipulation functions
    This will be helpful also in drivers mostly, where non-atomic bit
    setting could be unnecessary.
    
    Change-Id: I10c069387d1045f14337b3ac8acfc7b6c1f106c3
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 507decb731e0f81d68a7da15762d9964388cd6f4

--- include/sys_io.h
+++ include/sys_io.h
@@ -1,3 +1,4 @@
 DECL|__SYS_IO_H__|macro|__SYS_IO_H__
 DECL|io_port_t|typedef|typedef uint32_t io_port_t;
+DECL|mem_addr_t|typedef|typedef uint32_t mem_addr_t;
 DECL|mm_reg_t|typedef|typedef uint32_t mm_reg_t;
