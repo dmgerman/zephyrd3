dded35d1f998b4a4517016213b51f9d3fb324d2d
Szymon Janc <ext.szymon.janc@tieto.com>
2015-08-21 13:28:56 +0200

    Bluetooth: Refactor tester application
    This big patch refactors tester application and put it in par with
    updated tester protocol.
    
    Change-Id: Ib016fdd0aac58a32bcdc67774843cf65d619947d
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: bece111ec9b840586abbf6681bdd783151d35924

--- samples/bluetooth/tester/src/bttester.h
+++ samples/bluetooth/tester/src/bttester.h
@@ -1,37 +1,130 @@
-DECL|IPC_MTU|macro|IPC_MTU
-DECL|OP_GAP_EV_CONNECTED|macro|OP_GAP_EV_CONNECTED
-DECL|OP_GAP_EV_DISCONNECTED|macro|OP_GAP_EV_DISCONNECTED
-DECL|OP_GAP_START_ADV|macro|OP_GAP_START_ADV
-DECL|OP_REGISTER_SERVICE|macro|OP_REGISTER_SERVICE
-DECL|OP_STATUS|macro|OP_STATUS
-DECL|SERVICE_ID_CORE|macro|SERVICE_ID_CORE
-DECL|SERVICE_ID_GAP|macro|SERVICE_ID_GAP
-DECL|STATUS_FAILED|macro|STATUS_FAILED
-DECL|STATUS_NOT_READY|macro|STATUS_NOT_READY
-DECL|STATUS_SUCCESS|macro|STATUS_SUCCESS
-DECL|STATUS_UNKNOWN_CMD|macro|STATUS_UNKNOWN_CMD
-DECL|T_BDADDR_LE_PUBLIC|macro|T_BDADDR_LE_PUBLIC
-DECL|T_BDADDR_LE_RANDOM|macro|T_BDADDR_LE_RANDOM
+DECL|BTP_BDADDR_BREDR|macro|BTP_BDADDR_BREDR
+DECL|BTP_BDADDR_LE_PUBLIC|macro|BTP_BDADDR_LE_PUBLIC
+DECL|BTP_BDADDR_LE_RANDOM|macro|BTP_BDADDR_LE_RANDOM
+DECL|BTP_INDEX_NONE|macro|BTP_INDEX_NONE
+DECL|BTP_MTU|macro|BTP_MTU
+DECL|BTP_SERVICE_ID_CORE|macro|BTP_SERVICE_ID_CORE
+DECL|BTP_SERVICE_ID_GAP|macro|BTP_SERVICE_ID_GAP
+DECL|BTP_STATUS_FAILED|macro|BTP_STATUS_FAILED
+DECL|BTP_STATUS_NOT_READY|macro|BTP_STATUS_NOT_READY
+DECL|BTP_STATUS_SUCCESS|macro|BTP_STATUS_SUCCESS
+DECL|BTP_STATUS_UNKNOWN_CMD|macro|BTP_STATUS_UNKNOWN_CMD
+DECL|BTP_STATUS|macro|BTP_STATUS
+DECL|CORE_READ_SUPPORTED_COMMANDS|macro|CORE_READ_SUPPORTED_COMMANDS
+DECL|CORE_READ_SUPPORTED_SERVICES|macro|CORE_READ_SUPPORTED_SERVICES
+DECL|CORE_REGISTER_SERVICE|macro|CORE_REGISTER_SERVICE
+DECL|GAP_DEVICE_FOUND_FLAG_AD|macro|GAP_DEVICE_FOUND_FLAG_AD
+DECL|GAP_DEVICE_FOUND_FLAG_RSSI|macro|GAP_DEVICE_FOUND_FLAG_RSSI
+DECL|GAP_DEVICE_FOUND_FLAG_SD|macro|GAP_DEVICE_FOUND_FLAG_SD
+DECL|GAP_DISCOVERY_FLAG_BREDR|macro|GAP_DISCOVERY_FLAG_BREDR
+DECL|GAP_DISCOVERY_FLAG_LE|macro|GAP_DISCOVERY_FLAG_LE
+DECL|GAP_DISCOVERY_FLAG_LIMITED|macro|GAP_DISCOVERY_FLAG_LIMITED
+DECL|GAP_EV_DEVICE_CONNECTED|macro|GAP_EV_DEVICE_CONNECTED
+DECL|GAP_EV_DEVICE_DISCONNECTED|macro|GAP_EV_DEVICE_DISCONNECTED
+DECL|GAP_EV_DEVICE_FOUND|macro|GAP_EV_DEVICE_FOUND
+DECL|GAP_EV_NEW_SETTINGS|macro|GAP_EV_NEW_SETTINGS
+DECL|GAP_READ_CONTROLLER_INDEX_LIST|macro|GAP_READ_CONTROLLER_INDEX_LIST
+DECL|GAP_READ_CONTROLLER_INFO|macro|GAP_READ_CONTROLLER_INFO
+DECL|GAP_READ_SUPPORTED_COMMANDS|macro|GAP_READ_SUPPORTED_COMMANDS
+DECL|GAP_RESET|macro|GAP_RESET
+DECL|GAP_SETTINGS_ADVERTISING|macro|GAP_SETTINGS_ADVERTISING
+DECL|GAP_SETTINGS_BONDABLE|macro|GAP_SETTINGS_BONDABLE
+DECL|GAP_SETTINGS_BREDR|macro|GAP_SETTINGS_BREDR
+DECL|GAP_SETTINGS_CONNECTABLE|macro|GAP_SETTINGS_CONNECTABLE
+DECL|GAP_SETTINGS_DEBUG_KEYS|macro|GAP_SETTINGS_DEBUG_KEYS
+DECL|GAP_SETTINGS_FAST_CONNECTABLE|macro|GAP_SETTINGS_FAST_CONNECTABLE
+DECL|GAP_SETTINGS_HS|macro|GAP_SETTINGS_HS
+DECL|GAP_SETTINGS_LE|macro|GAP_SETTINGS_LE
+DECL|GAP_SETTINGS_LINK_SEC_3|macro|GAP_SETTINGS_LINK_SEC_3
+DECL|GAP_SETTINGS_POWERED|macro|GAP_SETTINGS_POWERED
+DECL|GAP_SETTINGS_PRIVACY|macro|GAP_SETTINGS_PRIVACY
+DECL|GAP_SETTINGS_SC|macro|GAP_SETTINGS_SC
+DECL|GAP_SETTINGS_SSP|macro|GAP_SETTINGS_SSP
+DECL|GAP_SET_BONDABLE|macro|GAP_SET_BONDABLE
+DECL|GAP_SET_CONNECTABLE|macro|GAP_SET_CONNECTABLE
+DECL|GAP_SET_DISCOVERABLE|macro|GAP_SET_DISCOVERABLE
+DECL|GAP_SET_FAST_CONNECTABLE|macro|GAP_SET_FAST_CONNECTABLE
+DECL|GAP_SET_POWERED|macro|GAP_SET_POWERED
+DECL|GAP_START_ADVERTISING|macro|GAP_START_ADVERTISING
+DECL|GAP_START_DISCOVERY|macro|GAP_START_DISCOVERY
+DECL|GAP_STOP_ADVERTISING|macro|GAP_STOP_ADVERTISING
+DECL|GAP_STOP_DISCOVERY|macro|GAP_STOP_DISCOVERY
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
-DECL|__packed|variable|__packed
-DECL|addr_type|member|uint8_t addr_type;
-DECL|addr_type|member|uint8_t addr_type;
-DECL|addr|member|uint8_t addr[6];
-DECL|addr|member|uint8_t addr[6];
-DECL|adv_type|member|uint8_t adv_type;
-DECL|cmd_register_service|struct|struct cmd_register_service {
-DECL|cmd_start_advertising|struct|struct cmd_start_advertising {
+DECL|address_type|member|uint8_t address_type;
+DECL|address_type|member|uint8_t address_type;
+DECL|address_type|member|uint8_t address_type;
+DECL|address|member|uint8_t address[6];
+DECL|address|member|uint8_t address[6];
+DECL|address|member|uint8_t address[6];
+DECL|address|member|uint8_t address[6];
+DECL|adv_data_len|member|uint8_t adv_data_len;
+DECL|adv_data|member|uint8_t adv_data[0];
+DECL|btp_hdr|struct|struct btp_hdr {
+DECL|btp_status|struct|struct btp_status {
 DECL|code|member|uint8_t code;
+DECL|cod|member|uint8_t cod[3];
+DECL|connectable|member|uint8_t connectable;
+DECL|core_read_supported_commands_rp|struct|struct core_read_supported_commands_rp {
+DECL|core_read_supported_services_rp|struct|struct core_read_supported_services_rp {
+DECL|core_register_service_cmd|struct|struct core_register_service_cmd {
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
+DECL|current_settings|member|uint32_t current_settings;
 DECL|data|member|uint8_t data[0];
-DECL|ev_connected|struct|struct ev_connected {
-DECL|ev_disconnected|struct|struct ev_disconnected {
+DECL|data|member|uint8_t data[0];
+DECL|data|member|uint8_t data[0];
+DECL|data|member|uint8_t data[0];
+DECL|discoverable|member|uint8_t discoverable;
+DECL|eir_data_len|member|uint16_t eir_data_len;
+DECL|eir_data|member|uint8_t eir_data[0];
+DECL|fast_connectable|member|uint8_t fast_connectable;
+DECL|flags|member|uint8_t flags;
+DECL|flags|member|uint8_t flags;
+DECL|gap_device_connected_ev|struct|struct gap_device_connected_ev {
+DECL|gap_device_disconnected_ev|struct|struct gap_device_disconnected_ev {
+DECL|gap_device_found_ev|struct|struct gap_device_found_ev {
+DECL|gap_new_settings_ev|struct|struct gap_new_settings_ev {
+DECL|gap_read_controller_index_list_rp|struct|struct gap_read_controller_index_list_rp {
+DECL|gap_read_controller_info_rp|struct|struct gap_read_controller_info_rp {
+DECL|gap_read_supported_commands_rp|struct|struct gap_read_supported_commands_rp {
+DECL|gap_reset_rp|struct|struct gap_reset_rp {
+DECL|gap_set_bondable_cmd|member|uint8_t gap_set_bondable_cmd;
+DECL|gap_set_bondable_cmd|struct|struct gap_set_bondable_cmd {
+DECL|gap_set_bondable_rp|struct|struct gap_set_bondable_rp {
+DECL|gap_set_connectable_cmd|struct|struct gap_set_connectable_cmd {
+DECL|gap_set_connectable_rp|struct|struct gap_set_connectable_rp {
+DECL|gap_set_discoverable_cmd|struct|struct gap_set_discoverable_cmd {
+DECL|gap_set_discoverable_rp|struct|struct gap_set_discoverable_rp {
+DECL|gap_set_fast_connectable_cmd|struct|struct gap_set_fast_connectable_cmd {
+DECL|gap_set_fast_connectable_rp|struct|struct gap_set_fast_connectable_rp {
+DECL|gap_set_powered_cmd|struct|struct gap_set_powered_cmd {
+DECL|gap_set_powered_rp|struct|struct gap_set_powered_rp {
+DECL|gap_start_advertising_cmd|struct|struct gap_start_advertising_cmd {
+DECL|gap_start_advertising_rp|struct|struct gap_start_advertising_rp {
+DECL|gap_start_discovery_cmd|struct|struct gap_start_discovery_cmd {
+DECL|gap_stop_advertising_rp|struct|struct gap_stop_advertising_rp {
 DECL|id|member|uint8_t id;
-DECL|ipc_hdr|struct|struct ipc_hdr {
-DECL|ipc_status|struct|struct ipc_status {
+DECL|index|member|uint8_t index;
+DECL|index|member|uint8_t index[0];
 DECL|len|member|uint16_t len;
+DECL|name|member|uint8_t name[249];
+DECL|num|member|uint8_t num;
 DECL|opcode|member|uint8_t opcode;
+DECL|powered|member|uint8_t powered;
+DECL|rssi|member|int8_t rssi;
+DECL|scan_rsp_len|member|uint8_t scan_rsp_len;
+DECL|scan_rsp|member|uint8_t scan_rsp[0];
 DECL|service|member|uint8_t service;
+DECL|short_name|member|uint8_t short_name[11];
+DECL|supported_settings|member|uint32_t supported_settings;
--- samples/bluetooth/tester/src/gap.c
+++ samples/bluetooth/tester/src/gap.c
@@ -1,6 +1,16 @@
+DECL|CONTROLLER_ADDR|macro|CONTROLLER_ADDR
+DECL|CONTROLLER_INDEX|macro|CONTROLLER_INDEX
+DECL|CONTROLLER_NAME|macro|CONTROLLER_NAME
 DECL|conn_callbacks|variable|conn_callbacks
+DECL|controller_index_list|function|static void controller_index_list(uint8_t *data, uint16_t len)
+DECL|controller_info|function|static void controller_info(uint8_t *data, uint16_t len)
+DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t evtype, const uint8_t *ad, uint8_t len)
 DECL|le_connected|function|static void le_connected(struct bt_conn *conn)
 DECL|le_disconnected|function|static void le_disconnected(struct bt_conn *conn)
-DECL|start_advertising|function|static uint8_t start_advertising(uint8_t *data, uint16_t len)
-DECL|tester_handle_gap|function|void tester_handle_gap(uint8_t opcode, uint8_t *data, uint16_t len)
+DECL|start_advertising|function|static void start_advertising(const uint8_t *data, uint16_t len)
+DECL|start_discovery|function|static void start_discovery(const uint8_t *data, uint16_t len)
+DECL|stop_advertising|function|static void stop_advertising(const uint8_t *data, uint16_t len)
+DECL|stop_discovery|function|static void stop_discovery(const uint8_t *data, uint16_t len)
+DECL|supported_commands|function|static void supported_commands(uint8_t *data, uint16_t len)
+DECL|tester_handle_gap|function|void tester_handle_gap(uint8_t opcode, uint8_t index, uint8_t *data, uint16_t len)
 DECL|tester_init_gap|function|uint8_t tester_init_gap(void)
