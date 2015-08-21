b7c61bc5b6647869737d078c626ab93d7d642a36
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-08-21 18:17:56 -0500

    Improve profiler sample.
    Add interrupt and sleep event data to the profiler sample.
    
    Change-Id: Ie5d3c4201475824eaf833bef506140279a6c606d
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: c38acaacf0afa8783c7450839bb3e72c398842c5

--- samples/microkernel/apps/profiler_sample/src/profiler_collector_sample.c
+++ samples/microkernel/apps/profiler_sample/src/profiler_collector_sample.c
@@ -2,11 +2,19 @@ DECL|MAX_BUFFER_CONTEXT_DATA|macro|MAX_BUFFER_CONTEXT_DATA
 DECL|RANDOM|macro|RANDOM
 DECL|STSIZE|macro|STSIZE
 DECL|TEST_EVENT_ID|macro|TEST_EVENT_ID
+DECL|awake_cause|member|uint32_t awake_cause;
+DECL|busy_task_entry|function|void busy_task_entry(void)
 DECL|context_switch_data_t|struct|struct context_switch_data_t {
 DECL|context_switch_summary_data|variable|context_switch_summary_data
 DECL|count|member|uint32_t count;
+DECL|fork_manager_entry|function|void fork_manager_entry(void)
+DECL|forks_available|variable|forks_available
 DECL|forks|variable|forks
+DECL|interrupt_counters|variable|interrupt_counters
+DECL|is_busy_task_awake|variable|is_busy_task_awake
+DECL|last_duration|member|uint32_t last_duration;
 DECL|last_time_executed|member|uint32_t last_time_executed;
+DECL|last_time_slept|member|uint32_t last_time_slept;
 DECL|main|function|int main(void)
 DECL|philStack|variable|philStack
 DECL|print_context_data|function|void print_context_data(uint32_t thread_id, uint32_t count,uint32_t last_time_executed, int indice)
@@ -15,7 +23,10 @@ DECL|profiler_fiber_start|function|void profiler_fiber_start(void)
 DECL|profiler_stack|variable|profiler_stack
 DECL|profiling_data_collector|function|void profiling_data_collector(void)
 DECL|register_context_switch_data|function|void register_context_switch_data(uint32_t timestamp, uint32_t thread_id)
+DECL|register_interrupt_event_data|function|void register_interrupt_event_data(uint32_t timestamp, uint32_t irq)
+DECL|register_sleep_event_data|function|void register_sleep_event_data(uint32_t time_start, uint32_t duration,uint32_t cause)
+DECL|sleep_data_t|struct|struct sleep_data_t {
+DECL|sleep_event_data|variable|sleep_event_data
 DECL|summary_data_printer|function|void summary_data_printer(void)
-DECL|sys_profiler|variable|sys_profiler
 DECL|thread_id|member|uint32_t thread_id;
 DECL|total_dropped_counter|variable|total_dropped_counter
