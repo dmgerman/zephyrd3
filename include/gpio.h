e4d1b8533f4d5014ca1a07031ab3dfe02aa46df1
Daniel Leung <daniel.leung@intel.com>
2015-08-24 09:58:58 -0700

    gpio: change inline functions to static inline
    When compiling with optimization level 0 (-O0), the linker complains
    about missing references to uart functions. This is due to compiler
    treating this functions as extern, since -O0 disables function inlining,
    as described in https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49653
    
    So change the declaration to static inline.
    
    Change-Id: Ifd835bf0e667afb1cbf2e76bb490664af87c382d
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 03066f26d825bd588540b3bb23e6b7d6a7c849fb

--- include/gpio.h
+++ include/gpio.h
@@ -20,23 +20,23 @@ DECL|gpio_config_t|typedef|typedef int (*gpio_config_t)(struct device *port, int
 DECL|gpio_disable_callback_t|typedef|typedef int (*gpio_disable_callback_t)(struct device *port,
 DECL|gpio_driver_api|struct|struct gpio_driver_api {
 DECL|gpio_enable_callback_t|typedef|typedef int (*gpio_enable_callback_t)(struct device *port,
-DECL|gpio_pin_configure|function|inline int gpio_pin_configure(struct device *port, uint8_t pin, int flags)
-DECL|gpio_pin_disable_callback|function|inline int gpio_pin_disable_callback(struct device *port, uint32_t pin)
-DECL|gpio_pin_enable_callback|function|inline int gpio_pin_enable_callback(struct device *port, uint32_t pin)
-DECL|gpio_pin_read|function|inline int gpio_pin_read(struct device *port, uint32_t pin, uint32_t *value)
-DECL|gpio_pin_write|function|inline int gpio_pin_write(struct device *port, uint32_t pin, uint32_t value)
-DECL|gpio_port_configure|function|inline int gpio_port_configure(struct device *port, int flags)
-DECL|gpio_port_disable_callback|function|inline int gpio_port_disable_callback(struct device *port)
-DECL|gpio_port_enable_callback|function|inline int gpio_port_enable_callback(struct device *port)
-DECL|gpio_port_read|function|inline int gpio_port_read(struct device *port, uint32_t *value)
-DECL|gpio_port_write|function|inline int gpio_port_write(struct device *port, uint32_t value)
+DECL|gpio_pin_configure|function|static inline int gpio_pin_configure(struct device *port, uint8_t pin, int flags)
+DECL|gpio_pin_disable_callback|function|static inline int gpio_pin_disable_callback(struct device *port, uint32_t pin)
+DECL|gpio_pin_enable_callback|function|static inline int gpio_pin_enable_callback(struct device *port, uint32_t pin)
+DECL|gpio_pin_read|function|static inline int gpio_pin_read(struct device *port, uint32_t pin,uint32_t *value)
+DECL|gpio_pin_write|function|static inline int gpio_pin_write(struct device *port, uint32_t pin, uint32_t value)
+DECL|gpio_port_configure|function|static inline int gpio_port_configure(struct device *port, int flags)
+DECL|gpio_port_disable_callback|function|static inline int gpio_port_disable_callback(struct device *port)
+DECL|gpio_port_enable_callback|function|static inline int gpio_port_enable_callback(struct device *port)
+DECL|gpio_port_read|function|static inline int gpio_port_read(struct device *port, uint32_t *value)
+DECL|gpio_port_write|function|static inline int gpio_port_write(struct device *port, uint32_t value)
 DECL|gpio_read_t|typedef|typedef int (*gpio_read_t)(struct device *port, int access_op,
 DECL|gpio_resume_port_t|typedef|typedef int (*gpio_resume_port_t)(struct device *port);
-DECL|gpio_resume|function|inline int gpio_resume(struct device *port)
+DECL|gpio_resume|function|static inline int gpio_resume(struct device *port)
 DECL|gpio_set_callback_t|typedef|typedef int (*gpio_set_callback_t)(struct device *port,
-DECL|gpio_set_callback|function|inline int gpio_set_callback(struct device *port, gpio_callback_t callback)
+DECL|gpio_set_callback|function|static inline int gpio_set_callback(struct device *port, gpio_callback_t callback)
 DECL|gpio_suspend_port_t|typedef|typedef int (*gpio_suspend_port_t)(struct device *port);
-DECL|gpio_suspend|function|inline int gpio_suspend(struct device *port)
+DECL|gpio_suspend|function|static inline int gpio_suspend(struct device *port)
 DECL|gpio_write_t|typedef|typedef int (*gpio_write_t)(struct device *port, int access_op,
 DECL|read|member|gpio_read_t read;
 DECL|resume|member|gpio_resume_port_t resume;
