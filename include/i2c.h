6c6761e69a0c7c3b5cef1dc4a637360de9f8220a
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-08-24 11:30:06 -0700

    i2c: Cleaning up public interface
    The public interface has been found to need a few additional
    changes, and a few clean ups to functionally work with an IP block.
    
    - Removing the faulty (0 << x) defines.
    - Updating the write/read functions to take the destination address
    - Adding in a union and struct for the dev_config access
    
    Change-Id: I4820aa64a96180ca46daac94113b04a4ca1bc84a
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 9c2156731ad927a41608a9751acbae3bdb9e63fd

--- include/i2c.h
+++ include/i2c.h
@@ -1,36 +1,33 @@
 DECL|I2C_ADDR_10_BITS|macro|I2C_ADDR_10_BITS
-DECL|I2C_ADDR_7_BITS|macro|I2C_ADDR_7_BITS
-DECL|I2C_ADDR_GET|macro|I2C_ADDR_GET
-DECL|I2C_ADDR_MASK|macro|I2C_ADDR_MASK
-DECL|I2C_FAST_PLUS|macro|I2C_FAST_PLUS
-DECL|I2C_FAST|macro|I2C_FAST
 DECL|I2C_MODE_MASTER|macro|I2C_MODE_MASTER
-DECL|I2C_MODE_SLAVE|macro|I2C_MODE_SLAVE
+DECL|I2C_MODE_SLAVE_READ|macro|I2C_MODE_SLAVE_READ
+DECL|I2C_SPEED_FAST_PLUS|macro|I2C_SPEED_FAST_PLUS
+DECL|I2C_SPEED_FAST|macro|I2C_SPEED_FAST
 DECL|I2C_SPEED_HIGH|macro|I2C_SPEED_HIGH
+DECL|I2C_SPEED_MASK|macro|I2C_SPEED_MASK
+DECL|I2C_SPEED_STANDARD|macro|I2C_SPEED_STANDARD
 DECL|I2C_SPEED_ULTRA|macro|I2C_SPEED_ULTRA
-DECL|I2C_STANDARD|macro|I2C_STANDARD
 DECL|__DRIVERS_I2C_H|macro|__DRIVERS_I2C_H
-DECL|base_address|member|uint32_t base_address;
-DECL|cb_receive|member|i2c_callback cb_receive;
-DECL|cb_transmit|member|i2c_callback cb_transmit;
-DECL|configure|member|i2c_api_configure configure;
-DECL|dev_config|member|uint32_t dev_config;
-DECL|i2c_api_configure|typedef|typedef int (*i2c_api_configure)(struct device *dev, struct i2c_config *config);
-DECL|i2c_api_io|typedef|typedef int (*i2c_api_io)(struct device *dev, unsigned char *buf, uint32_t len);
-DECL|i2c_api_resume|typedef|typedef int (*i2c_api_resume)(struct device *dev);
-DECL|i2c_api_suspend|typedef|typedef int (*i2c_api_suspend)(struct device *dev);
-DECL|i2c_callback|typedef|typedef void (*i2c_callback)(struct device *dev, uint32_t context);
-DECL|i2c_configure|function|static inline int i2c_configure(struct device *dev, struct i2c_config *config)
-DECL|i2c_config|struct|struct i2c_config {
+DECL|bits|member|} bits;
+DECL|configure|member|i2c_api_configure_t configure;
+DECL|dev_config|union|union dev_config {
+DECL|i2c_api_configure_t|typedef|typedef int (*i2c_api_configure_t)(struct device *dev,
+DECL|i2c_api_io_t|typedef|typedef int (*i2c_api_io_t)(struct device *dev,
+DECL|i2c_api_resume_t|typedef|typedef int (*i2c_api_resume_t)(struct device *dev);
+DECL|i2c_api_suspend_t|typedef|typedef int (*i2c_api_suspend_t)(struct device *dev);
+DECL|i2c_configure|function|static inline int i2c_configure(struct device *dev, uint32_t dev_config)
 DECL|i2c_driver_api|struct|struct i2c_driver_api {
-DECL|i2c_read|function|static inline int i2c_read(struct device *dev, unsigned char *buf, uint32_t len)
+DECL|i2c_read|function|static inline int i2c_read(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|i2c_resume|function|static inline int i2c_resume(struct device *dev)
-DECL|i2c_rom_config|struct|struct i2c_rom_config {
 DECL|i2c_suspend|function|static inline int i2c_suspend(struct device *dev)
-DECL|i2c_write|function|static inline int i2c_write(struct device *dev, unsigned char *buf, uint32_t len)
-DECL|interrupt_mask|member|uint32_t interrupt_mask;
-DECL|interrupt_vector|member|uint32_t interrupt_vector;
-DECL|read|member|i2c_api_io read;
-DECL|resume|member|i2c_api_resume resume;
-DECL|suspend|member|i2c_api_suspend suspend;
-DECL|write|member|i2c_api_io write;
+DECL|i2c_write|function|static inline int i2c_write(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
+DECL|is_master_device|member|uint32_t is_master_device : 1;
+DECL|is_slave_read|member|uint32_t is_slave_read : 1;
+DECL|raw|member|uint32_t raw;
+DECL|read|member|i2c_api_io_t read;
+DECL|reserved|member|uint32_t reserved : 26;
+DECL|resume|member|i2c_api_resume_t resume;
+DECL|speed|member|uint32_t speed : 3;
+DECL|suspend|member|i2c_api_suspend_t suspend;
+DECL|use_10_bit_addr|member|uint32_t use_10_bit_addr : 1;
+DECL|write|member|i2c_api_io_t write;
