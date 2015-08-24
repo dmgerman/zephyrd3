8117ad00f1dc03e4a96fda2b87b13586b37f8aa3
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-08-24 15:30:57 -0500

    Rename microkernel struct field 'cmdPkt' to 'command_packet'.
    Rename field 'cmdPkt' of the struct:
    - 'cmd_pkt_set' in the file Include/microkernel/command_packet.h
    
    Change-Id: I60b58f58d44c300387722f0a9f46fc0c83f4cf69
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 995ef816a02ae4a879fda3763c6887ab606ac3b5

--- include/microkernel/command_packet.h
+++ include/microkernel/command_packet.h
@@ -3,7 +3,7 @@ DECL|CMD_PKT_SET|macro|CMD_PKT_SET
 DECL|CMD_PKT_SIZE_IN_WORDS|macro|CMD_PKT_SIZE_IN_WORDS
 DECL|_COMMAND_PACKET_H|macro|_COMMAND_PACKET_H
 DECL|cmdPkt_t|typedef|typedef uint32_t cmdPkt_t[CMD_PKT_SIZE_IN_WORDS];
-DECL|cmdPkt|member|cmdPkt_t cmdPkt[]; /* array of command packets */
 DECL|cmd_pkt_set|struct|struct cmd_pkt_set {
+DECL|command_packet|member|cmdPkt_t command_packet[]; /* array of command packets */
 DECL|index|member|uint32_t index; /* index into command packet array */
 DECL|num_packets|member|uint32_t num_packets; /* number of command packets in set */
