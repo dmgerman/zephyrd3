4fffdad35ff11a9ebf2f67d31424f812ed1a5ebd
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-08-24 12:49:07 -0500

    Rename microkernel struct field 'iFreeSpaceCont' to 'free_space_count'.
    Rename field 'iFreeSpaceCont' of the struct:
    - '_k_pipe_desc' in the file include/microkernel/base_api.h
    
    Change-Id: Icc9da1162f594349799e3503eafad29d8a2e52a2
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 57bc635397bc3f59d45bcaf04946504b29bad1fe

--- include/microkernel/base_api.h
+++ include/microkernel/base_api.h
@@ -58,6 +58,7 @@ DECL|extra|member|} extra;
 DECL|first_marker|member|int first_marker;
 DECL|fn_abort|member|void (*fn_abort)(void);
 DECL|fn_start|member|void (*fn_start)(void);
+DECL|free_space_count|member|int free_space_count;
 DECL|group|member|uint32_t group;
 DECL|high_watermark|member|int high_watermark;
 DECL|high_watermark|member|int high_watermark;
@@ -65,7 +66,6 @@ DECL|iAvailDataAWA|member|int iAvailDataAWA; /* AWA == After Wrap Around */
 DECL|iAvailDataCont|member|int iAvailDataCont;
 DECL|iBufferSize|member|int iBufferSize; /* size in bytes, must be first for sysgen */
 DECL|iFreeSpaceAWA|member|int iFreeSpaceAWA;
-DECL|iFreeSpaceCont|member|int iFreeSpaceCont;
 DECL|iNbrPendingReads|member|int iNbrPendingReads;
 DECL|iNbrPendingWrites|member|int iNbrPendingWrites;
 DECL|id|member|ktask_t id;
