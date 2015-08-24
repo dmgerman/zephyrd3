3cdb2cb0b2967adb9c46b5db28ef831e5c5c974d
Daniel Leung <daniel.leung@intel.com>
2015-08-24 10:00:05 -0700

    spi: change inline functions to static inline
    When compiling with optimization level 0 (-O0), the linker complains
    about missing references to uart functions. This is due to compiler
    treating this functions as extern, since -O0 disables function inlining,
    as described in https://gcc.gnu.org/bugzilla/show_bug.cgi?id=49653
    
    So change the declaration to static inline.
    
    Change-Id: I2b75e686961878f299bb951b5a420f9738594795
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 5f353e0e948e73d45d6f7c1c94f6bceaa1a0eaaa

--- include/spi.h
+++ include/spi.h
@@ -24,14 +24,14 @@ DECL|spi_api_io|typedef|typedef int (*spi_api_io)(struct device *dev,
 DECL|spi_api_slave_select|typedef|typedef int (*spi_api_slave_select)(struct device *dev, uint32_t slave);
 DECL|spi_callback|typedef|typedef void (*spi_callback)(struct device *dev, enum spi_cb_type cb_type);
 DECL|spi_cb_type|enum|enum spi_cb_type {
-DECL|spi_configure|function|inline int spi_configure(struct device *dev, struct spi_config *config)
+DECL|spi_configure|function|static inline int spi_configure(struct device *dev, struct spi_config *config)
 DECL|spi_config|struct|struct spi_config {
 DECL|spi_driver_api|struct|struct spi_driver_api {
-DECL|spi_read|function|inline int spi_read(struct device *dev, uint8_t *buf, uint32_t len)
-DECL|spi_resume|function|inline int spi_resume(struct device *dev)
+DECL|spi_read|function|static inline int spi_read(struct device *dev, uint8_t *buf, uint32_t len)
+DECL|spi_resume|function|static inline int spi_resume(struct device *dev)
 DECL|spi_slave_select|function|inline int spi_slave_select(struct device *dev, uint32_t slave)
-DECL|spi_suspend|function|inline int spi_suspend(struct device *dev)
+DECL|spi_suspend|function|static inline int spi_suspend(struct device *dev)
 DECL|spi_transceive|function|inline int spi_transceive(struct device *dev, uint8_t *tx_buf, uint32_t tx_buf_len, uint8_t *rx_buf, uint32_t rx_buf_len)
-DECL|spi_write|function|inline int spi_write(struct device *dev, uint8_t *buf, uint32_t len)
+DECL|spi_write|function|static inline int spi_write(struct device *dev, uint8_t *buf, uint32_t len)
 DECL|suspend|member|spi_api_control suspend;
 DECL|transceive|member|spi_api_io transceive;
