2896d6e8fc9819bd711f4466c98eae59faf91426
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-08-25 15:00:17 -0500

    Rename microkernel struct field 'ContRcv' to 'continuation_receive'.
    Rename field 'ContRcv' of the struct:
    - 'moved_req_args_setup' in the file kernel/microkernel/include/micro_private_types.h
    
    Change-Id: Ic8984aa2ed7e62c2ed7a25e825e32b2c55bb7e5d
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: 0af44eb7c33076906c69dd25160d664b9d06adfd

--- kernel/microkernel/include/micro_private_types.h
+++ kernel/microkernel/include/micro_private_types.h
@@ -1,6 +1,5 @@
 DECL|Action|member|MovedAction Action;
 DECL|Comm|member|int Comm;
-DECL|ContRcv|member|struct k_args *ContRcv;
 DECL|Ctxt|member|K_CREF Ctxt;
 DECL|Extra|member|} Extra;
 DECL|K_ARGS_ARGS|typedef|typedef union k_args_args K_ARGS_ARGS;
@@ -133,6 +132,7 @@ DECL|blocktable|member|struct block_stat *blocktable;
 DECL|block|member|struct k_block block;
 DECL|bufblock|member|char *bufblock;
 DECL|c1|member|struct _c1arg c1;
+DECL|continuation_receive|member|struct k_args *continuation_receive;
 DECL|continuation_send|member|struct k_args *continuation_send;
 DECL|count|member|int count;
 DECL|count|member|int count;
