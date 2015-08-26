77bd1a525a9f38866acceb3bd80be18474d16211
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-08-26 17:28:44 +0300

    net: coap: Use CoAP context also in non-DTLS case
    The use of CoAP context is needed as we do not have
    global buffers any longer. This commit adds support
    for CoAP context when user does not have DTLS enabled.
    
    Change-Id: Ief716e2a3290bd1f7e5a3286a20deba690b3c1c5
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 8848f330304b4d208ff29c25da50c876b32042df

--- net/ip/er-coap/er-coap-context.c
+++ net/ip/er-coap/er-coap-context.c
@@ -6,6 +6,7 @@ DECL|STATUS_ALERT|macro|STATUS_ALERT
 DECL|STATUS_CONNECTED|macro|STATUS_CONNECTED
 DECL|STATUS_CONNECTING|macro|STATUS_CONNECTING
 DECL|coap_context_close|function|coap_context_close(coap_context_t *coap_ctx)
+DECL|coap_context_close|function|coap_context_close(coap_context_t *coap_ctx)
 DECL|coap_context_connect|function|coap_context_connect(coap_context_t *coap_ctx, uip_ipaddr_t *addr, uint16_t port)
 DECL|coap_context_event|variable|coap_context_event
 DECL|coap_context_has_errors|function|coap_context_has_errors(const coap_context_t *coap_ctx)
@@ -13,9 +14,14 @@ DECL|coap_context_init|function|coap_context_init(void)
 DECL|coap_context_is_connected|function|coap_context_is_connected(const coap_context_t *coap_ctx)
 DECL|coap_context_is_connecting|function|coap_context_is_connecting(const coap_context_t *coap_ctx)
 DECL|coap_context_is_secure|function|coap_context_is_secure(const coap_context_t *coap_ctx)
+DECL|coap_context_listen|function|int coap_context_listen(coap_context_t *coap_ctx, uip_ipaddr_t *peer_addr,uint16_t peer_port)
 DECL|coap_context_new|function|coap_context_new(uip_ipaddr_t *my_addr, uint16_t port)
+DECL|coap_context_new|function|coap_context_new(uip_ipaddr_t *my_addr, uint16_t port)
+DECL|coap_context_reply|function|int coap_context_reply(coap_context_t *ctx, struct net_buf *buf)
+DECL|coap_context_send_message|function|coap_context_send_message(coap_context_t *coap_ctx, uip_ipaddr_t *addr, uint16_t port, const uint8_t *data, uint16_t length)
 DECL|coap_context_send_message|function|coap_context_send_message(coap_context_t *coap_ctx, uip_ipaddr_t *addr, uint16_t port, const uint8_t *data, uint16_t length)
 DECL|coap_context_set_key_handlers|function|coap_context_set_key_handlers(coap_context_t *ctx, dtls_get_psk_info_t get_psk_info, dtls_get_ecdsa_key_t get_ecdsa_key, dtls_verify_ecdsa_key_t verify_ecdsa_key)
+DECL|coap_context_wait_data|function|int coap_context_wait_data(coap_context_t *coap_ctx, int32_t ticks)
 DECL|coap_context_wait_data|function|int coap_context_wait_data(coap_context_t *coap_ctx, int32_t ticks)
 DECL|coap_contexts|variable|coap_contexts
 DECL|event|function|event(struct dtls_context_t *ctx, session_t *session, dtls_alert_level_t level, unsigned short code)
--- net/ip/er-coap/er-coap-context.h
+++ net/ip/er-coap/er-coap-context.h
@@ -7,7 +7,6 @@ DECL|WITH_DTLS|macro|WITH_DTLS
 DECL|WITH_DTLS|macro|WITH_DTLS
 DECL|addr|member|struct net_addr addr;
 DECL|buf|member|struct net_buf *buf;
-DECL|coap_context_close|function|coap_context_close(coap_context_t *coap_ctx)
 DECL|coap_context_connect|function|coap_context_connect(coap_context_t *coap_ctx, uip_ipaddr_t *addr, uint16_t port)
 DECL|coap_context_event|macro|coap_context_event
 DECL|coap_context_has_errors|function|coap_context_has_errors(const coap_context_t *coap_ctx)
@@ -15,8 +14,7 @@ DECL|coap_context_init|function|coap_context_init(void)
 DECL|coap_context_is_connected|function|coap_context_is_connected(const coap_context_t *coap_ctx)
 DECL|coap_context_is_connecting|function|coap_context_is_connecting(const coap_context_t *coap_ctx)
 DECL|coap_context_is_secure|function|coap_context_is_secure(const coap_context_t *coap_ctx)
-DECL|coap_context_new|function|coap_context_new(uint16_t port)
-DECL|coap_context_send_message|function|coap_context_send_message(coap_context_t *coap_ctx, uip_ipaddr_t *addr, uint16_t port, const uint8_t *data, uint16_t length)
+DECL|coap_context_set_key_handlers|macro|coap_context_set_key_handlers
 DECL|coap_context_t|typedef|} coap_context_t;
 DECL|coap_context|struct|typedef struct coap_context {
 DECL|dtls_context|member|struct dtls_context_t *dtls_context;
