eab71370e36c3c1aea2de7b4702f62369ac7289e
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-08-27 16:58:28 +0300

    uart: Configure ioapic for UART interrupts
    Follow similar configuration for other platforms.
    
    Change-Id: I1478d10799cbb375c715bed4d30d1bcf223d282f
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 69871aaecc1d1d95f4144f0f1db728d4b0f8410d

--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -1,3 +1,5 @@
+DECL|console_irq_set|function|static inline void console_irq_set(void)
+DECL|console_irq_set|macro|console_irq_set
 DECL|hpet_irq_set|function|static inline void hpet_irq_set(void)
 DECL|hpet_irq_set|macro|hpet_irq_set
 DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
