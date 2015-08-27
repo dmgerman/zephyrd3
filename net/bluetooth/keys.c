9f5881521cc6036e14832d7b2251182f0ad18e90
Szymon Janc <ext.szymon.janc@tieto.com>
2015-08-27 16:14:41 +0200

    Bluetooth: Simplify keys management code
    There is no need for separate lists of each key type as this
    lookup optimization is of little benefit. Removing those results in
    much simpler and smaller code. Using array over list also improves
    data locality and thus cache utilization.
    
    This patch results in following code size reduction:
    keys.o from 14208 to 10028 bytes
    microkernel.elf from 599417 to 597091 bytes
    
    Change-Id: Ia8fe21ca3d79f668d8550c4376fa403e8778861e
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: 90121208ab76e3fd5316d8e1934b8d144759e063

--- net/bluetooth/keys.c
+++ net/bluetooth/keys.c
@@ -5,12 +5,6 @@ DECL|bt_keys_clear|function|void bt_keys_clear(struct bt_keys *keys, int type)
 DECL|bt_keys_find_addr|function|struct bt_keys *bt_keys_find_addr(const bt_addr_le_t *addr)
 DECL|bt_keys_find_irk|function|struct bt_keys *bt_keys_find_irk(const bt_addr_le_t *addr)
 DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(int type, const bt_addr_le_t *addr)
-DECL|bt_keys_foreach|macro|bt_keys_foreach
 DECL|bt_keys_get_addr|function|struct bt_keys *bt_keys_get_addr(const bt_addr_le_t *addr)
 DECL|bt_keys_get_type|function|struct bt_keys *bt_keys_get_type(int type, const bt_addr_le_t *addr)
-DECL|irks|variable|irks
 DECL|key_pool|variable|key_pool
-DECL|local_csrks|variable|local_csrks
-DECL|ltks|variable|ltks
-DECL|remote_csrks|variable|remote_csrks
-DECL|slave_ltks|variable|slave_ltks
