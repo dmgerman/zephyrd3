51dbf3e20372feb15a301bf16829815629949718
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-08-28 08:58:44 +0300

    net: Get debug info about available net buffers
    Print additional information about how many net buffers
    are available when allocating (get) or de-allocating (put)
    them. The prints are only available if Kconfig option
    CONFIG_NETWORKING_WITH_LOGGING is set.
    
    Change-Id: I85e6a5813356d8fe00f0ba633c3ebf5379e56bff
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 48186c99f584fde070352aadf2e4b13c3ad3cd47

--- net/ip/net_buf.c
+++ net/ip/net_buf.c
@@ -3,9 +3,18 @@ DECL|NET_BUF_RX_SIZE|macro|NET_BUF_RX_SIZE
 DECL|NET_BUF_TX_SIZE|macro|NET_BUF_TX_SIZE
 DECL|NET_BUF_TX_SIZE|macro|NET_BUF_TX_SIZE
 DECL|NET_NUM_MAC_BUFS|macro|NET_NUM_MAC_BUFS
+DECL|dec_free_rx_bufs|function|static inline void dec_free_rx_bufs(struct net_buf *buf)
+DECL|dec_free_rx_bufs|macro|dec_free_rx_bufs
+DECL|dec_free_tx_bufs|function|static inline void dec_free_tx_bufs(struct net_buf *buf)
+DECL|dec_free_tx_bufs|macro|dec_free_tx_bufs
 DECL|free_mbufs|variable|free_mbufs
 DECL|free_rx_bufs|variable|free_rx_bufs
 DECL|free_tx_bufs|variable|free_tx_bufs
+DECL|get_frees|function|static inline int get_frees(enum net_buf_type type)
+DECL|inc_free_rx_bufs|function|static inline void inc_free_rx_bufs(struct net_buf *buf)
+DECL|inc_free_rx_bufs|macro|inc_free_rx_bufs
+DECL|inc_free_tx_bufs|function|static inline void inc_free_tx_bufs(struct net_buf *buf)
+DECL|inc_free_tx_bufs|macro|inc_free_tx_bufs
 DECL|mac_buffers|variable|mac_buffers
 DECL|net_buf_add|function|uint8_t *net_buf_add(struct net_buf *buf, uint16_t len)
 DECL|net_buf_get_debug|function|static struct net_buf *net_buf_get_debug(enum net_buf_type type, struct net_context *context, const char *caller, int line) #else static struct net_buf *net_buf_get(enum net_buf_type type,
@@ -21,6 +30,8 @@ DECL|net_buf_put_debug|function|void net_buf_put_debug(struct net_buf *buf, cons
 DECL|net_mbuf_get_reserve_debug|function|struct net_mbuf *net_mbuf_get_reserve_debug(uint16_t reserve_head, const char *caller, int line)
 DECL|net_mbuf_init|function|static void net_mbuf_init(void)
 DECL|net_mbuf_put_debug|function|void net_mbuf_put_debug(struct net_mbuf *buf, const char *caller, int line)
+DECL|num_free_rx_bufs|variable|num_free_rx_bufs
+DECL|num_free_tx_bufs|variable|num_free_tx_bufs
 DECL|rx_buffers|variable|rx_buffers
 DECL|tx_buffers|variable|tx_buffers
 DECL|type2str|function|static inline const char *type2str(enum net_buf_type type)
