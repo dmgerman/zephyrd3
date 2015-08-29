bc73e3336d95a559d60b49a3b292aaa011250147
Anas Nashif <anas.nashif@intel.com>
2015-08-29 12:57:26 -0400

    doxygen: move command_packet documentation to header
    Change-Id: I27631d742127d0ea04a04330c155fd2b1e196556
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 387ec49c1e9352c6df83b40c651c7f144c243e7f

--- kernel/microkernel/k_command_packet.c
+++ kernel/microkernel/k_command_packet.c
@@ -1,3 +1,3 @@
-DECL|_cmd_pkt_get|function|cmdPkt_t *_cmd_pkt_get(struct cmd_pkt_set *pSet /* ptr to set of command packets */ )
+DECL|_cmd_pkt_get|function|cmdPkt_t *_cmd_pkt_get(struct cmd_pkt_set *pSet)
 DECL|_k_task_call|function|void _k_task_call(struct k_args *cmd_packet)
 DECL|_k_test_cmd_pkt_size|variable|_k_test_cmd_pkt_size
