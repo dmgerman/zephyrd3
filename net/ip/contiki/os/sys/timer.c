5aa0b29c19d669035a41cd509c743420c0137354
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-08-31 17:36:25 +0300

    net: contiki: Use Zephyr provided timer API
    Refactor etimer and timer Contiki APIs to use the native
    Zephyr timers. The Contiki way did not work well anyway
    as we do not really have the process concept in Zephyr.
    
    Change-Id: I14743707f3cf43eddfe914717b069204cde4bd65
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c796f3cfccc4acf31708d0f363411473b43a343f

--- net/ip/contiki/os/sys/timer.c
+++ net/ip/contiki/os/sys/timer.c
@@ -1,5 +1,8 @@
+DECL|DEBUG|macro|DEBUG
+DECL|do_init|function|static inline void do_init(struct timer *t)
 DECL|timer_expired|function|timer_expired(struct timer *t)
 DECL|timer_remaining|function|timer_remaining(struct timer *t)
 DECL|timer_reset|function|timer_reset(struct timer *t)
 DECL|timer_restart|function|timer_restart(struct timer *t)
 DECL|timer_set|function|timer_set(struct timer *t, clock_time_t interval)
+DECL|timer_stop|function|void timer_stop(struct timer *t)
--- net/ip/contiki/os/sys/timer.h
+++ net/ip/contiki/os/sys/timer.h
@@ -1,4 +1,7 @@
 DECL|TIMER_H_|macro|TIMER_H_
+DECL|init_done|member|int init_done;
 DECL|interval|member|clock_time_t interval;
+DECL|nano_timer|member|struct nano_timer nano_timer;
+DECL|started|member|int started;
 DECL|start|member|clock_time_t start;
 DECL|timer|struct|struct timer {
