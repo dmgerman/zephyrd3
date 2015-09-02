93935269da766d1eb833b73b36ddffb1249c03f0
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-09-02 13:20:34 +0300

    net: apps: Add coap observe client
    Add sample CoAP application for observe client functionality.
    
    Change-Id: Ie2ee5d191bcc68ec634367c9b36e46ea4261d6b5
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 23a0543aeebc81b6482a456f92f4c44ebe32daad

---
+++ samples/network/coap_observe_client/src/coap-observe-client.c
@@ -0,0 +1,38 @@
+DECL|DEBUG|macro|DEBUG
+DECL|DEBUG|macro|DEBUG
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_PORT|macro|MY_PORT
+DECL|OBS_RESOURCE_URI|macro|OBS_RESOURCE_URI
+DECL|PEER_IPADDR|macro|PEER_IPADDR
+DECL|PEER_IPADDR|macro|PEER_IPADDR
+DECL|PEER_PORT|macro|PEER_PORT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|TOGGLE_INTERVAL|macro|TOGGLE_INTERVAL
+DECL|WAIT_PEER|macro|WAIT_PEER
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|ecdsa_priv_key|variable|ecdsa_priv_key
+DECL|ecdsa_pub_key_x|variable|ecdsa_pub_key_x
+DECL|ecdsa_pub_key_y|variable|ecdsa_pub_key_y
+DECL|fiberStack|variable|fiberStack
+DECL|get_ecdsa_key|function|static int get_ecdsa_key(struct dtls_context_t *ctx, const session_t *session, const dtls_ecdsa_key_t **result)
+DECL|get_ecdsa_key|macro|get_ecdsa_key
+DECL|get_psk_info|function|static int get_psk_info(struct dtls_context_t *ctx,const session_t *session, dtls_credentials_type_t type, const unsigned char *id, size_t id_len, unsigned char *result, size_t result_length)
+DECL|get_psk_info|macro|get_psk_info
+DECL|in4addr_my|variable|in4addr_my
+DECL|in4addr_peer|variable|in4addr_peer
+DECL|in6addr_my|variable|in6addr_my
+DECL|in6addr_peer|variable|in6addr_peer
+DECL|init_server|function|static inline void init_server()
+DECL|main|function|void main(void)
+DECL|my_mac|variable|my_mac
+DECL|notification_callback|function|static void notification_callback(coap_observee_t *obs, void *notification, coap_notification_flag_t flag)
+DECL|obs|variable|obs
+DECL|peer_mac|variable|peer_mac
+DECL|startup|function|void startup(void)
+DECL|toggle_observation|function|void toggle_observation(coap_context_t *coap_ctx)
+DECL|verify_ecdsa_key|function|static int verify_ecdsa_key(struct dtls_context_t *ctx, const session_t *session, const unsigned char *other_pub_x, const unsigned char *other_pub_y, size_t key_size)
+DECL|verify_ecdsa_key|macro|verify_ecdsa_key
