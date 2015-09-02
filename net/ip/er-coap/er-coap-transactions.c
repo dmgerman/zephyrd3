60bc803a24b365c04c7cd8fdd72db39498d4f0fa
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-09-02 13:14:52 +0300

    net: coap: Retransmit needs to allocate net_buf
    When a transaction is retransmitted, we need to allocate a
    net_buf that will hold the actual UDP packet to be sent.
    
    Change-Id: Ie0d51694b5531938cd1da5c330a308e3e9fd1764
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 73e861d351ba7b8cb94822b467f7bff9de613285

--- net/ip/er-coap/er-coap-transactions.c
+++ net/ip/er-coap/er-coap-transactions.c
@@ -5,5 +5,6 @@ DECL|coap_get_transaction_by_mid|function|coap_get_transaction_by_mid(uint16_t m
 DECL|coap_new_transaction|function|coap_new_transaction(uint16_t mid, coap_context_t *coap_ctx, uip_ipaddr_t *addr, uint16_t port)
 DECL|coap_register_as_transaction_handler|function|coap_register_as_transaction_handler()
 DECL|coap_send_transaction|function|coap_send_transaction(coap_transaction_t *t)
+DECL|get_retransmit_buf|function|static inline struct net_buf *get_retransmit_buf(coap_transaction_t *t)
 DECL|transaction_handler_process|variable|transaction_handler_process
 DECL|transactions_list|variable|transactions_list
