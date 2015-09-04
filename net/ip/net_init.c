f8628cf55b03a99fd2474dba8c170eb057d7f216
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-09-04 16:44:38 +0300

    net: Create a separate fiber that handles Contiki timers
    The Contiki etimers need polling which is handled by this fiber.
    This fiber sleeps until next etimer is to be triggered. If the wakeup
    time is long, we wait max 60 sec so that new timers can be triggered
    also. This 60 sec timeout might be too long and require tweaking when
    we know how the system behaves.
    If there are no etimer event in the system, then the timer fiber
    will only sleep 2 seconds before checking the timers again. This
    interval might need tweaking also.
    
    Change-Id: If34b97073882d8b273941cfb0be36df6aac97cf8
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 88fcbd52d0bc01814e4ab99a1be5eaf707086071

--- net/ip/net_init.c
+++ net/ip/net_init.c
@@ -1,8 +1,11 @@
 DECL|DEBUG|macro|DEBUG
+DECL|DEFAULT_TIMER_WAKEUP|macro|DEFAULT_TIMER_WAKEUP
+DECL|MAX_TIMER_WAKEUP|macro|MAX_TIMER_WAKEUP
 DECL|STACKSIZE_UNIT|macro|STACKSIZE_UNIT
 DECL|check_and_send_packet|function|static int check_and_send_packet(struct net_buf *buf)
 DECL|drv|member|struct net_driver *drv;
 DECL|init_rx_queue|function|static void init_rx_queue(void)
+DECL|init_timer_fiber|function|static void init_timer_fiber(void)
 DECL|init_tx_queue|function|static void init_tx_queue(void)
 DECL|net_dev|struct|static struct net_dev {
 DECL|net_init|function|int net_init(void)
@@ -14,12 +17,14 @@ DECL|net_rx_fiber|function|static void net_rx_fiber(void)
 DECL|net_send|function|int net_send(struct net_buf *buf)
 DECL|net_set_mac|function|int net_set_mac(uint8_t *mac, uint8_t len)
 DECL|net_tcpip_output|function|static uint8_t net_tcpip_output(struct net_buf *buf, const uip_lladdr_t *lladdr)
+DECL|net_timer_fiber|function|static void net_timer_fiber(void)
 DECL|net_tx_fiber|function|static void net_tx_fiber(void)
 DECL|net_unregister_driver|function|void net_unregister_driver(struct net_driver *drv)
 DECL|netdev|variable|netdev
 DECL|network_initialization|function|static int network_initialization(void)
 DECL|rx_fiber_stack|variable|rx_fiber_stack
 DECL|rx_queue|member|struct nano_fifo rx_queue;
+DECL|timer_fiber_stack|variable|timer_fiber_stack
 DECL|tx_fiber_stack|variable|tx_fiber_stack
 DECL|tx_queue|member|struct nano_fifo tx_queue;
 DECL|udp_packet_receive|function|static void udp_packet_receive(struct simple_udp_connection *c, const uip_ipaddr_t *source_addr, uint16_t source_port, const uip_ipaddr_t *dest_addr, uint16_t dest_port,
