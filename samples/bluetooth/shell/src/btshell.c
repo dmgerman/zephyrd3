448ad2a9117f2e36265871e1dd479b76958b6bfd
Szymon Janc <ext.szymon.janc@tieto.com>
2015-09-04 14:14:37 +0200

    Bluetooth: Refactor command parameters handling in shell app
    Reduce usage of static variables making code easier to follow.
    Also improve robustness in case of too many parameters provided.
    
    Change-Id: I6a3f0162a4fb301479ed36c54ceca2967bb7e389
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: ea80649a838aab0ae5b2394ee51dccfc1ae67a35

--- samples/bluetooth/shell/src/btshell.c
+++ samples/bluetooth/shell/src/btshell.c
@@ -1,14 +1,12 @@
 DECL|ARGC_MAX|macro|ARGC_MAX
 DECL|MAX_CMD_QUEUED|macro|MAX_CMD_QUEUED
 DECL|STACKSIZE|macro|STACKSIZE
-DECL|argc|variable|argc
-DECL|argv|variable|argv
 DECL|avail_queue|variable|avail_queue
 DECL|buf|variable|buf
 DECL|cmds_queue|variable|cmds_queue
 DECL|commands|variable|commands
 DECL|get_cb|function|static cmd_function_t get_cb(const char *string)
-DECL|line2argv|function|static void line2argv(char *str)
+DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t size)
 DECL|line_queue_init|function|static void line_queue_init(void)
 DECL|prompt|variable|prompt
 DECL|shell_init|function|void shell_init(const char *str, struct shell_cmd *cmds)
