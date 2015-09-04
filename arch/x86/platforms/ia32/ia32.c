e879145d9ab23f71259c614c36038056f92ff8cd
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-09-04 14:55:57 +0300

    Bluetooth: Configure Bluetooth IRQ for ia32 platform
    Configure IOAPIC for Bluetooth interrupts connected to UART.
    
    Change-Id: Ia93f71a8e8435c523dd8ee232f9ce4cf9266b978
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 3195016822a811089d023259e271eaa4d9358695

--- arch/x86/platforms/ia32/ia32.c
+++ arch/x86/platforms/ia32/ia32.c
@@ -1,3 +1,4 @@
+DECL|bluetooth_irq_set|function|static int bluetooth_irq_set(struct device *unused)
 DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|ia32_init|function|static int ia32_init(struct device *arg)
