57b23b798577adcfd99e57cf213772c55204bfc9
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-09-07 12:29:05 +0300

    net: contiki: Add utility function to get the MAC address
    Add function that returns the link address that the system
    is using at the moment.
    
    Change-Id: Id09aed3f6cbc4cd3ee694e18441f7443f0b7c3bf
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7d3e5f847fc78eefd1650ca9c8171b34e7f80033

--- net/ip/contiki/linkaddr.c
+++ net/ip/contiki/linkaddr.c
@@ -1,5 +1,6 @@
 DECL|linkaddr_cmp|function|linkaddr_cmp(const linkaddr_t *addr1, const linkaddr_t *addr2)
 DECL|linkaddr_copy|function|linkaddr_copy(linkaddr_t *dest, const linkaddr_t *src)
+DECL|linkaddr_get_node_addr|function|linkaddr_t *linkaddr_get_node_addr(int *addr_len)
 DECL|linkaddr_node_addr|variable|linkaddr_node_addr
 DECL|linkaddr_null|variable|linkaddr_null
 DECL|linkaddr_null|variable|linkaddr_null
