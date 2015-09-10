212cbf241c32ad1116c1f21dd5e663988d7ce4e8
Daniel Leung <daniel.leung@intel.com>
2015-09-10 14:25:30 -0700

    x86: ia32_pci: add options to allow I2C IRQ trigger
    Add Kconfig option to specify how interrupt is triggered for I2C.
    
    Change-Id: I5f5afdf6bfc2adbfc2f66bdc1f58bd3a8fc60ecf
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: e495f95a43768ee97a86f49a88e4664215e1fdd0

--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -1,5 +1,7 @@
 DECL|console_irq_set|function|static inline void console_irq_set(void)
 DECL|console_irq_set|macro|console_irq_set
+DECL|dw_i2c0_irq_set|function|static inline void dw_i2c0_irq_set(void)
+DECL|dw_i2c0_irq_set|macro|dw_i2c0_irq_set
 DECL|hpet_irq_set|function|static inline void hpet_irq_set(void)
 DECL|hpet_irq_set|macro|hpet_irq_set
 DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
