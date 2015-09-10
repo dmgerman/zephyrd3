21151afe4afa8494171654ed5ee89f83cfc1c312
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-09-10 10:54:09 -0700

    arc: add support for generic
    Add support for a "generic" ARC implementation. This is a misnomer
    since ARC platforms are synthesized by the SOC vendor and are specifc
    to a given vendors implementation/configuration.  This platform
    support if for an SOC where we are allowed to let the code to support
    the SOC into the wild but not allowed to name it in public.
    
    This change allows to work on support for the SOC in the open and let
    the rest of the community see the evolution of of the platform.
    
    Change-Id: Idbb52fc80464ec04e91665871951f6ea79e5299d
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Former-commit-id: 8570e3cac2da4d46e86150fed7752f411b6e6c11

---
+++ arch/arc/platforms/generic/sw_isr_table.c
@@ -0,0 +1,2 @@
+DECL|_IsrTable|variable|_IsrTable
+DECL|_sw_isr_table|variable|_sw_isr_table
---
+++ arch/arc/platforms/generic/system.c
@@ -0,0 +1,3 @@
+DECL|consoleInit|function|static void consoleInit(void)
+DECL|consoleInit|macro|consoleInit
+DECL|generic_init|function|static int generic_init(struct device *arg)
