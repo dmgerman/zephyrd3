b6ee32b492230b7e95eccfc0decd3abae58ab576
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-09-11 14:47:11 +0300

    gpio: ia32_pci: Add options to allow GPIO IRQ trigger
    Add Kconfig option to specify how interrupt is triggered for GPIO.
    
    Change-Id: I8db988efb0c081a18e585ccc6dd121bff61d6ccc
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 608fc5071b150519b44943e5d96bd28cdc91f908

--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -2,6 +2,8 @@ DECL|console_irq_set|function|static inline void console_irq_set(void)
 DECL|console_irq_set|macro|console_irq_set
 DECL|dw_i2c0_irq_set|function|static inline void dw_i2c0_irq_set(void)
 DECL|dw_i2c0_irq_set|macro|dw_i2c0_irq_set
+DECL|gpio_irq_set|function|static inline void gpio_irq_set(void) {
+DECL|gpio_irq_set|macro|gpio_irq_set
 DECL|hpet_irq_set|function|static inline void hpet_irq_set(void)
 DECL|hpet_irq_set|macro|hpet_irq_set
 DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
