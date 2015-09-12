f830858c0c8d92f8225311274c6b2835c94183a0
Anas Nashif <anas.nashif@intel.com>
2015-09-12 18:49:33 -0400

    arc: rename generic platform to generic_arc
    The generic name is too generic and when it appears on its
    own, for example nano_generic_defconfig, it is not
    visible what the file is about, so call it generic_arc the same
    way the Kconfig platform is called.
    
    Change-Id: Icf737f305d84142dad4f52fd26a5c7481f9fc5be
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 8976762df8ef042cd0967ca254f1fe632364087a

--- arch/arc/platforms/generic/system.c
+++
@@ -1,3 +0,0 @@
-DECL|consoleInit|function|static void consoleInit(void)
-DECL|consoleInit|macro|consoleInit
-DECL|generic_init|function|static int generic_init(struct device *arg)
---
+++ arch/arc/platforms/generic_arc/system.c
@@ -0,0 +1,1 @@
+DECL|generic_arc_init|function|static int generic_arc_init(struct device *arg)
