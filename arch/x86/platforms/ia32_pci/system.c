7863dd41129579cb7382964561ba1ffce0338a4d
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-09-15 09:17:38 -0700

    x86: Change GPIO IRQ configuration function to init system routine
    Change-Id: If511362ec3f2dba5010052863fc350407f87aedc
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Former-commit-id: c52e6481fe83a2bca59e8a829688168b6cf5735c

--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -1,7 +1,6 @@
 DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
 DECL|dw_i2c0_irq_set|function|static inline void dw_i2c0_irq_set(void)
 DECL|dw_i2c0_irq_set|macro|dw_i2c0_irq_set
-DECL|gpio_irq_set|function|static inline void gpio_irq_set(void) {
-DECL|gpio_irq_set|macro|gpio_irq_set
+DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused) {
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
