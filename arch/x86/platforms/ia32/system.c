8039bf60ae51bb759559ef4cd1ef66cb90fc2168
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-09-15 09:17:38 -0700

    x86: Change HPET IRQ configuration function to init system routine
    Change-Id: I8adc0fe190bd253202145418ab7898bb95b7d7ec
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    Former-commit-id: 1ac4e50ed84ae6d558def7cf56d20c677ae0478f

--- arch/x86/platforms/ia32/system.c
+++ arch/x86/platforms/ia32/system.c
@@ -1,5 +1,4 @@
 DECL|console_irq_set|function|static inline void console_irq_set(void)
 DECL|console_irq_set|macro|console_irq_set
-DECL|hpet_irq_set|function|static inline void hpet_irq_set(void)
-DECL|hpet_irq_set|macro|hpet_irq_set
+DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|ia32_init|function|static int ia32_init(struct device *arg)
--- arch/x86/platforms/ia32_pci/system.c
+++ arch/x86/platforms/ia32_pci/system.c
@@ -4,6 +4,5 @@ DECL|dw_i2c0_irq_set|function|static inline void dw_i2c0_irq_set(void)
 DECL|dw_i2c0_irq_set|macro|dw_i2c0_irq_set
 DECL|gpio_irq_set|function|static inline void gpio_irq_set(void) {
 DECL|gpio_irq_set|macro|gpio_irq_set
-DECL|hpet_irq_set|function|static inline void hpet_irq_set(void)
-DECL|hpet_irq_set|macro|hpet_irq_set
+DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
