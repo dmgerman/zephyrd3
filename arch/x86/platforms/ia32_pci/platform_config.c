5f5b62f1602d85235ba3392e3f3ba8401b1b88e5
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-09-17 06:32:31 -0700

    x86: i2c: Move system binding code to dw_i2c.c
    The code fragment to bind the instances of the I2C driver is not
    platform specific but is driver specific.
    
    All the information required to bind a driver instance comes from
    CONFIG_ variables.  Having the binding code with the driver code
    avoids duplicating the code fragments in each platform where the
    driver may be used.
    
    Change-Id: I9ea132c9340437ccb292bb8f3fa3d3a2b2a794db
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    
    Former-commit-id: 6865fb658f3177a64c665d1226f41c10e14455d8

--- arch/x86/platforms/ia32_pci/platform_config.c
+++ arch/x86/platforms/ia32_pci/platform_config.c
@@ -1,7 +1,3 @@
-DECL|dw_i2c_isr_0|function|void dw_i2c_isr_0(void *unused)
-DECL|i2c_0_runtime|variable|i2c_0_runtime
-DECL|i2c_config_0_irq|function|void i2c_config_0_irq(struct device *port)
-DECL|i2c_config_dw_0|variable|i2c_config_dw_0
 DECL|ns16550_uart_dev_cfg|variable|ns16550_uart_dev_cfg
 DECL|ns16550_uart_dev_data|variable|ns16550_uart_dev_data
 DECL|ns16550_uart_init|function|static int ns16550_uart_init(struct device *dev)
--- drivers/i2c/dw_i2c.c
+++ drivers/i2c/dw_i2c.c
@@ -4,6 +4,7 @@ DECL|DBG|macro|DBG
 DECL|dw_i2c_data_read|function|static void dw_i2c_data_read(struct device *dev)
 DECL|dw_i2c_data_send|function|static void dw_i2c_data_send(struct device *dev)
 DECL|dw_i2c_initialize|function|int dw_i2c_initialize(struct device *port)
+DECL|dw_i2c_isr_0|function|void dw_i2c_isr_0(void *unused)
 DECL|dw_i2c_isr|function|void dw_i2c_isr(struct device *port)
 DECL|dw_i2c_memory_read|function|static inline uint32_t dw_i2c_memory_read(uint32_t base_addr, uint32_t offset)
 DECL|dw_i2c_memory_write|function|static inline void dw_i2c_memory_write(uint32_t base_addr, uint32_t offset, uint32_t val)
@@ -17,3 +18,6 @@ DECL|dw_i2c_suspend|function|static int dw_i2c_suspend(struct device *dev)
 DECL|dw_i2c_transfer|function|static int dw_i2c_transfer(struct device *dev, uint8_t *write_buf, uint32_t write_len, uint8_t *read_buf, uint32_t read_len, uint16_t slave_address)
 DECL|dw_i2c_write|function|static int dw_i2c_write(struct device *dev, uint8_t *buf,uint32_t len, uint16_t slave_addr)
 DECL|funcs|variable|funcs
+DECL|i2c_0_runtime|variable|i2c_0_runtime
+DECL|i2c_config_0_irq|function|void i2c_config_0_irq(struct device *port)
+DECL|i2c_config_dw_0|variable|i2c_config_dw_0
