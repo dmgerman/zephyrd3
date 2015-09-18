fc0da94b06ef35da01e78aa26d59a86787413384
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-09-18 15:14:25 -0400

    test: add nanokernel mem_safe test
    This test can only be executed on x86.
    
    Change-Id: I4fe60dbee9dcb11f983855b507f53c2b9fbb54d0
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: c433fc7f8ac7b8877560f50e18f7eab610b6ec6b

---
+++ samples/nanokernel/test/test_mem_safe/src/main.c
@@ -0,0 +1,26 @@
+DECL|INVALID|macro|INVALID
+DECL|MY_DATA_SIZE|macro|MY_DATA_SIZE
+DECL|RAM_END|macro|RAM_END
+DECL|RAM_START|macro|RAM_START
+DECL|ROM_END|macro|ROM_END
+DECL|ROM_START|macro|ROM_START
+DECL|RO|macro|RO
+DECL|RW|macro|RW
+DECL|access_func|typedef|typedef int (*access_func)(void *, char *, size_t, int);
+DECL|foo|variable|foo
+DECL|main|function|void main(void)
+DECL|mem_range_check|function|static int mem_range_check(const void *p)
+DECL|p_image_ram_end|variable|p_image_ram_end
+DECL|p_image_ram_start|variable|p_image_ram_start
+DECL|p_image_rom_end|variable|p_image_rom_end
+DECL|p_image_rom_start|variable|p_image_rom_start
+DECL|ro_data_after_image_end|variable|ro_data_after_image_end
+DECL|ro_data_after_image|variable|ro_data_after_image
+DECL|rodata|variable|rodata
+DECL|rw_data_after_image_end|variable|rw_data_after_image_end
+DECL|rw_data_after_image|variable|rw_data_after_image
+DECL|test_mem_safe_access|function|static int test_mem_safe_access(void *p, char *buf, int size,int width, int perm)
+DECL|test_width_read|function|static int test_width_read(char *mem, int width, int expected)
+DECL|test_width_write|function|static int test_width_write(char *mem, int width, int expected)
+DECL|test_width|function|static int test_width(char *mem, int perm, int width, int expected)
+DECL|update_rv|function|static void update_rv(int *rv, int last_result)
