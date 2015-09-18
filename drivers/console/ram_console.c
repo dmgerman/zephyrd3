44a2b2f11385ae7bd13f10597c91124ce29bad0f
Andrew Boie <andrew.p.boie@intel.com>
2015-09-18 09:57:13 -0700

    ram_console: driver for emitting console messages to a buffer
    This buffer can be examined at runtime with a debugger to see what
    is going on. Helpful if there are no other working console drivers
    on the system.
    
    Change-Id: I759467a57d16204b4b316c4f6fa8b7d5e1d1d236
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: d5feec5c64c2c9f668628399d77dba05417f11c3

---
+++ drivers/console/ram_console.c
@@ -0,0 +1,4 @@
+DECL|pos|variable|pos
+DECL|ram_console_init|function|static int ram_console_init(struct device *d)
+DECL|ram_console_out|function|static int ram_console_out(int character)
+DECL|ram_console|variable|ram_console
