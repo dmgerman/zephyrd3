e852e5d742a15b962e9d1822fc68788c1b28b24e
Daniel Leung <daniel.leung@intel.com>
2015-09-18 16:53:26 -0700

    gpio: add driver for MMIO-based GPIO ports
    This adds a simple driver for MMIO-based GPIO ports.
    It does not trigger any interrupts due to pin level at this moment.
    
    Change-Id: I0c439f221988817e4be72653c68257ef6ace5bde
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 721bb7afb387b40a9061de3f05714ebd18cca650

---
+++ drivers/gpio/gpio-mmio.c
@@ -0,0 +1,13 @@
+DECL|_set_bit|function|static void _set_bit(uint32_t addr, uint32_t bit, uint8_t value)
+DECL|gpio_mmio_0_cfg|variable|gpio_mmio_0_cfg
+DECL|gpio_mmio_1_cfg|variable|gpio_mmio_1_cfg
+DECL|gpio_mmio_config|function|static int gpio_mmio_config(struct device *dev, int access_op, uint32_t pin, int flags)
+DECL|gpio_mmio_disable_callback|function|static int gpio_mmio_disable_callback(struct device *dev, int access_op, uint32_t pin)
+DECL|gpio_mmio_drv_api_funcs|variable|gpio_mmio_drv_api_funcs
+DECL|gpio_mmio_enable_callback|function|static int gpio_mmio_enable_callback(struct device *dev, int access_op, uint32_t pin)
+DECL|gpio_mmio_init|function|int gpio_mmio_init(struct device *dev)
+DECL|gpio_mmio_read|function|static int gpio_mmio_read(struct device *dev, int access_op, uint32_t pin, uint32_t *value)
+DECL|gpio_mmio_resume_port|function|static int gpio_mmio_resume_port(struct device *dev)
+DECL|gpio_mmio_set_callback|function|static int gpio_mmio_set_callback(struct device *dev, gpio_callback_t callback)
+DECL|gpio_mmio_suspend_port|function|static int gpio_mmio_suspend_port(struct device *dev)
+DECL|gpio_mmio_write|function|static int gpio_mmio_write(struct device *dev, int access_op, uint32_t pin, uint32_t value)
---
+++ drivers/gpio/gpio-mmio.h
@@ -0,0 +1,14 @@
+DECL|GPIO_MMIO_CFG_DIR_INV|macro|GPIO_MMIO_CFG_DIR_INV
+DECL|GPIO_MMIO_CFG_DIR_MASK|macro|GPIO_MMIO_CFG_DIR_MASK
+DECL|GPIO_MMIO_CFG_DIR_NORMAL|macro|GPIO_MMIO_CFG_DIR_NORMAL
+DECL|GPIO_MMIO_CFG_EN_INV|macro|GPIO_MMIO_CFG_EN_INV
+DECL|GPIO_MMIO_CFG_EN_MASK|macro|GPIO_MMIO_CFG_EN_MASK
+DECL|GPIO_MMIO_CFG_EN_NORMAL|macro|GPIO_MMIO_CFG_EN_NORMAL
+DECL|_GPIO_MMIO_H_|macro|_GPIO_MMIO_H_
+DECL|cfg_flags|member|uint32_t cfg_flags;
+DECL|dir|member|uint32_t dir;
+DECL|en|member|uint32_t en;
+DECL|gpio_mmio_config|struct|struct gpio_mmio_config {
+DECL|input|member|uint32_t input;
+DECL|output|member|uint32_t output;
+DECL|reg|member|} reg;
