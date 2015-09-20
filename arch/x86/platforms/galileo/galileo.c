c236839379e6afa728ba802d7ded06e207e9f3cd
Anas Nashif <anas.nashif@intel.com>
2015-09-19 23:49:34 -0400

    galileo: add as a standalone platform
    Change-Id: I8ca4856d9e6a9461208044242950fdde710c4ffd
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: c842bdd76dd56848bf3533f1ddd829d83fdee6e6

---
+++ arch/x86/platforms/galileo/galileo.c
@@ -0,0 +1,5 @@
+DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
+DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
+DECL|galileo_init|function|static int galileo_init(struct device *arg)
+DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused) {
+DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
---
+++ arch/x86/platforms/galileo/galileo_config.c
@@ -0,0 +1,5 @@
+DECL|ns16550_uart_dev_cfg|variable|ns16550_uart_dev_cfg
+DECL|ns16550_uart_dev_data|variable|ns16550_uart_dev_data
+DECL|ns16550_uart_init|function|static int ns16550_uart_init(struct device *dev)
+DECL|ns16550_uart_init|function|static int ns16550_uart_init(struct device *dev)
+DECL|uart_devs|variable|uart_devs
