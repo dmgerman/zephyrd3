86f0fcf8bfb20413daad54a5b12e6eb3d1e91bad
Daniel Leung <daniel.leung@intel.com>
2015-09-21 16:28:58 -0700

    pwm: add driver for PCA9685 PWM chip
    This adds the driver for PCA9685 I2C-based PWM chip, and provides
    basic PWM functionality.
    
    Change-Id: I8c711d9b703fdaf5ccb23657e07495a95cbe16ee
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: cb1c7816f89a74965a9722e1fa2340748fe7460c

---
+++ drivers/pwm/pwm-pca9685.c
@@ -0,0 +1,23 @@
+DECL|MAX_PWM_OUT|macro|MAX_PWM_OUT
+DECL|PWM_ONE_PERIOD_TICKS|macro|PWM_ONE_PERIOD_TICKS
+DECL|REG_ALL_LED_OFF_H|macro|REG_ALL_LED_OFF_H
+DECL|REG_ALL_LED_OFF_L|macro|REG_ALL_LED_OFF_L
+DECL|REG_ALL_LED_ON_H|macro|REG_ALL_LED_ON_H
+DECL|REG_ALL_LED_ON_L|macro|REG_ALL_LED_ON_L
+DECL|REG_LED_OFF_H|macro|REG_LED_OFF_H
+DECL|REG_LED_OFF_L|macro|REG_LED_OFF_L
+DECL|REG_LED_ON_H|macro|REG_LED_ON_H
+DECL|REG_LED_ON_L|macro|REG_LED_ON_L
+DECL|REG_MODE1|macro|REG_MODE1
+DECL|REG_MODE2|macro|REG_MODE2
+DECL|REG_PRE_SCALE|macro|REG_PRE_SCALE
+DECL|_has_i2c_master|function|static inline int _has_i2c_master(struct device *dev)
+DECL|pwm_pca9685_0_cfg|variable|pwm_pca9685_0_cfg
+DECL|pwm_pca9685_0_drvdata|variable|pwm_pca9685_0_drvdata
+DECL|pwm_pca9685_configure|function|static int pwm_pca9685_configure(struct device *dev, int access_op, uint32_t pwm, int flags)
+DECL|pwm_pca9685_drv_api_funcs|variable|pwm_pca9685_drv_api_funcs
+DECL|pwm_pca9685_init|function|int pwm_pca9685_init(struct device *dev)
+DECL|pwm_pca9685_resume|function|static int pwm_pca9685_resume(struct device *dev)
+DECL|pwm_pca9685_set_duty_cycle|function|static int pwm_pca9685_set_duty_cycle(struct device *dev, int access_op, uint32_t pwm, uint8_t duty)
+DECL|pwm_pca9685_set_values|function|static int pwm_pca9685_set_values(struct device *dev, int access_op, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|pwm_pca9685_suspend|function|static int pwm_pca9685_suspend(struct device *dev)
---
+++ drivers/pwm/pwm-pca9685.h
@@ -0,0 +1,6 @@
+DECL|__PWM_PCA9685_H__|macro|__PWM_PCA9685_H__
+DECL|i2c_master_dev_name|member|const char * const i2c_master_dev_name;
+DECL|i2c_master|member|struct device *i2c_master;
+DECL|i2c_slave_addr|member|uint16_t i2c_slave_addr;
+DECL|pwm_pca9685_config|struct|struct pwm_pca9685_config {
+DECL|pwm_pca9685_drv_data|struct|struct pwm_pca9685_drv_data {
