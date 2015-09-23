e36114a6b9227596a8365c6926a052c8675bde1e
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-09-23 17:03:52 -0400

    Add PCI legacy bridge to Galileo configuration
    Galileo uses PCI legacy bridge to set up the following
    PCI interrupt pins to IRQ mapping:
    INTA -> IRQ16
    INTB -> IRQ17
    INTC -> IRQ18
    INTD -> IRQ19
    
    Change-Id: I8113ee16c6712f3166340d5eb0f2e0f440a37636
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 45a0e81871b1124539c17e1d12e8d4aaba36d070

--- arch/x86/platforms/galileo/galileo.c
+++ arch/x86/platforms/galileo/galileo.c
@@ -10,6 +10,7 @@ DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
 DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
 DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused) {
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
+DECL|pci_legacy_bridge_irq_config|function|static int pci_legacy_bridge_irq_config(struct device *unused)
 DECL|shared_irq_config|function|static int shared_irq_config(struct device *unused)
 DECL|spi_irq_set_0|function|static int spi_irq_set_0(struct device *unused)
 DECL|spi_irq_set_1|function|static int spi_irq_set_1(struct device *unused)
