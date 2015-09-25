c7e057a3b6d6ecfc1f18f26875cc3a8acf3635c6
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-09-25 13:02:12 -0700

    init: rename pure_early_init to pre_kernel_early_init
    Change-Id: Id52cd7a5c1a715a5c609f88f940ec2e27341d81e
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    
    Former-commit-id: f96f61d2f02a980c0b7237a194dad696944cb1e7

--- include/init.h
+++ include/init.h
@@ -4,9 +4,9 @@ DECL|MICRO_EARLY|macro|MICRO_EARLY
 DECL|MICRO_LATE|macro|MICRO_LATE
 DECL|NANO_EARLY|macro|NANO_EARLY
 DECL|NANO_LATE|macro|NANO_LATE
+DECL|PRE_KERNEL_EARLY|macro|PRE_KERNEL_EARLY
 DECL|PRE_KERNEL_LATE|macro|PRE_KERNEL_LATE
 DECL|PURE_CORE|macro|PURE_CORE
-DECL|PURE_EARLY|macro|PURE_EARLY
 DECL|_INIT_H_|macro|_INIT_H_
 DECL|__define_initconfig|macro|__define_initconfig
 DECL|app_early_init|macro|app_early_init
@@ -15,6 +15,6 @@ DECL|micro_early_init|macro|micro_early_init
 DECL|micro_late_init|macro|micro_late_init
 DECL|nano_early_init|macro|nano_early_init
 DECL|nano_late_init|macro|nano_late_init
+DECL|pre_kernel_early_init|macro|pre_kernel_early_init
 DECL|pre_kernel_late_init|macro|pre_kernel_late_init
 DECL|pure_core_init|macro|pure_core_init
-DECL|pure_early_init|macro|pure_early_init
