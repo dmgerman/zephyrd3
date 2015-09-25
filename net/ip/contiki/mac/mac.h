083e26cac3f466d89df27ca134961f164063fdd0
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-09-25 16:50:04 +0300

    net: Gather statistics about amount of sent L2 bytes
    This collects byte counts from layer 2 (MAC).
    
    Change-Id: I16c25f7ae8b9aa485e3c5f1573154cd2270cff93
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: f84e0c1b9b6b8d000202437007b7dc7fb7f228e1

--- net/ip/contiki/mac/mac.h
+++ net/ip/contiki/mac/mac.h
@@ -5,12 +5,16 @@ DECL|MAC_TX_ERR_FATAL|enumerator|MAC_TX_ERR_FATAL,
 DECL|MAC_TX_ERR|enumerator|MAC_TX_ERR,
 DECL|MAC_TX_NOACK|enumerator|MAC_TX_NOACK,
 DECL|MAC_TX_OK|enumerator|MAC_TX_OK,
+DECL|bytes_received|member|uint32_t bytes_received;
+DECL|bytes_sent|member|uint32_t bytes_sent;
 DECL|channel_check_interval|member|unsigned short (* channel_check_interval)(void);
 DECL|init|member|void (* init)(void);
 DECL|input|member|uint8_t (* input)(struct net_mbuf *buf);
 DECL|mac_callback_t|typedef|typedef void (* mac_callback_t)(struct net_mbuf *buf, void *ptr, int status, int transmissions);
 DECL|mac_driver|struct|struct mac_driver {
 DECL|name|member|char *name;
+DECL|net_mac_stats_t|typedef|} net_mac_stats_t;
+DECL|net_mac_stats|struct|typedef struct net_mac_stats {
 DECL|off|member|int (* off)(int keep_radio_on);
 DECL|on|member|int (* on)(void);
 DECL|send|member|uint8_t (* send)(struct net_mbuf *buf, mac_callback_t sent_callback,
