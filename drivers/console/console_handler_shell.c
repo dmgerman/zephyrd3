96cac3b4ebd8cc2e8755bf1215852b297ff2ca61
Thomas Heeley <thomas.heeley@intel.com>
2015-09-28 15:49:14 +0100

    misc: add shell support.
    Move btshell.h out of samples into include/misc and rename to shell.h
    
    Move btshell.c into driver/console and rename to console_handler_shell.c
    as an shell implementation based on new config
    CONFIG_CONSOLE_HANDLER_SHELL.
    
    Add shell_register_app_cmd_handler for an to app to optionally call so
    that it can receive cmdlines not handled by the cmds registered with
    shell_init
    
    Change-Id: I5c1585e62ff7a0ee923c6c92833cc762cf912bad
    Signed-off-by: Thomas Heeley <thomas.heeley@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: f85566a0b62dfc48b7bfa257ee3d35d0b9624371

--- drivers/console/console_handler_shell.c
+++ drivers/console/console_handler_shell.c
@@ -1,6 +1,7 @@
 DECL|ARGC_MAX|macro|ARGC_MAX
 DECL|MAX_CMD_QUEUED|macro|MAX_CMD_QUEUED
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|app_cmd_handler|variable|app_cmd_handler
 DECL|avail_queue|variable|avail_queue
 DECL|buf|variable|buf
 DECL|cmds_queue|variable|cmds_queue
@@ -10,6 +11,7 @@ DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t 
 DECL|line_queue_init|function|static void line_queue_init(void)
 DECL|prompt|variable|prompt
 DECL|shell_init|function|void shell_init(const char *str, struct shell_cmd *cmds)
+DECL|shell_register_app_cmd_handler|function|void shell_register_app_cmd_handler(cmd_function_t handler)
 DECL|shell|function|static void shell(int arg1, int arg2)
 DECL|show_help|function|static void show_help(int argc, char *argv[])
 DECL|stack|variable|stack
