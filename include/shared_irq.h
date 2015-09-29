9c84603b6360fab252678ee2566e5834c73ad86a
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-09-29 08:42:22 -0700

    drivers: add shared interrupt driver
    This driver allows multiple drivers to share a common interrupt
    line. This functionality is required on system that conform to the PC
    interrupt structure.  In the context of Zephyr this is needed for
    SOC's that have their I/O IP blocks behind a PCI interface. Due to the
    limited number of interrupt lines provided by the PCI interface
    multiple IP blocks may be configured to share an interrupt line.
    
    Drivers that share interrupts  must be modified to *not* register their
    own interrupt service routine as part of their configuration/initialization
    but instead bind to the correct instance of this driver by name, then
    register their interrupt service routine with this driver.
    
    Change-Id: I57b517b97ebeabce484ba53c8f940da993cb391d
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    
    Former-commit-id: 002c53b2251c48dd0e542e536dd4841b5cca0802

---
+++ include/shared_irq.h
@@ -0,0 +1,23 @@
+DECL|_SHIRQ_H_|macro|_SHIRQ_H_
+DECL|client_count|member|uint32_t client_count;
+DECL|client|member|struct shared_irq_client client[CONFIG_SHARED_IRQ_NUM_CLIENTS];
+DECL|config|member|shared_irq_config_irq_t config;
+DECL|disable|member|shared_irq_disable_t disable;
+DECL|enabled|member|uint32_t enabled;
+DECL|enable|member|shared_irq_enable_t enable;
+DECL|irq_num|member|uint32_t irq_num;
+DECL|isr_dev|member|struct device *isr_dev;
+DECL|isr_func|member|isr_t isr_func;
+DECL|isr_register|member|shared_irq_register_t isr_register;
+DECL|isr_t|typedef|typedef int (*isr_t)(struct device *dev);
+DECL|shared_irq_client|struct|struct shared_irq_client {
+DECL|shared_irq_config_irq_t|typedef|typedef void (*shared_irq_config_irq_t)(struct device *port);
+DECL|shared_irq_config|struct|struct shared_irq_config {
+DECL|shared_irq_disable_t|typedef|typedef int (*shared_irq_disable_t)(struct device *dev, struct device *isr_dev);
+DECL|shared_irq_disable|function|static inline int shared_irq_disable(struct device *dev, struct device *isr_dev)
+DECL|shared_irq_driver_api|struct|struct shared_irq_driver_api {
+DECL|shared_irq_enable_t|typedef|typedef int (*shared_irq_enable_t)(struct device *dev, struct device *isr_dev);
+DECL|shared_irq_enable|function|static inline int shared_irq_enable(struct device *dev, struct device *isr_dev)
+DECL|shared_irq_isr_register|function|static inline int shared_irq_isr_register(struct device *dev, isr_t isr_func, struct device *isr_dev)
+DECL|shared_irq_register_t|typedef|typedef int (*shared_irq_register_t)(struct device *dev,
+DECL|shared_irq_runtime|struct|struct shared_irq_runtime {
