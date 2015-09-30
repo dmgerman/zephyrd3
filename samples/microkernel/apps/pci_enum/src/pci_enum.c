0ea8094a79cbe42969320fb5a0915d1326db195e
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-09-30 15:52:56 +0300

    samples: Add an application to print out PCI enumeration
    This application can be used to list the PCI devices through the
    console.
    
    Change-Id: I3409ab9025b1f18a42b4cdd594fb095ebeac96f3
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: c55e8861ab72170d21be5882753c86ec0dfbad92

---
+++ samples/microkernel/apps/pci_enum/src/pci_enum.c
@@ -0,0 +1,6 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|done|variable|done
+DECL|main|function|void main(void)
+DECL|pci_enumerate|function|void pci_enumerate(void)
+DECL|task_enum_pci|function|void task_enum_pci(void)
