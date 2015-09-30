54c1b4e56b1280cd8ff77d0c7e9708a7d656457e
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-09-30 16:05:41 +0300

    net: apps: test_15_4 fixes when running in one qemu
    Made the test_15_4 application to run much faster when it is
    running inside qemu (started with make qemu0).
    
    Change-Id: I4455eca4f0426e8acc8b87d93eb80f4d179a1b48
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 05d8a096e107cc70e905a6cc97c6fb6a46a47481

--- samples/network/test_15_4/src/network.c
+++ samples/network/test_15_4/src/network.c
@@ -7,12 +7,15 @@ DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SRC_PORT|macro|SRC_PORT
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|any_addr|variable|any_addr
 DECL|dest_mac|variable|dest_mac
-DECL|fiberEntry|function|void fiberEntry(void)
-DECL|fiberStack|variable|fiberStack
+DECL|fiberStack_receiving|variable|fiberStack_receiving
+DECL|fiberStack_sending|variable|fiberStack_sending
+DECL|fiber_receiving|function|void fiber_receiving(void)
+DECL|fiber_sending|function|void fiber_sending(void)
 DECL|get_context|function|static struct net_context *get_context(const struct net_addr *remote, uint16_t remote_port, const struct net_addr *local, uint16_t local_port)
 DECL|in6addr_dest|variable|in6addr_dest
 DECL|in6addr_src|variable|in6addr_src
@@ -21,8 +24,8 @@ DECL|listen|function|static void listen(const char *taskname, ksem_t mySem, ksem
 DECL|loopback_addr|variable|loopback_addr
 DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
-DECL|nanoSemFiber|variable|nanoSemFiber
-DECL|nanoSemTask|variable|nanoSemTask
+DECL|nanoSemFiberRecv|variable|nanoSemFiberRecv
+DECL|nanoSemFiberSent|variable|nanoSemFiberSent
 DECL|receive_data|function|static void receive_data(const char *taskname, struct net_context *ctx)
 DECL|send_data|function|static void send_data(const char *taskname, struct net_context *ctx)
 DECL|send|function|static void send(const char *taskname, ksem_t mySem, ksem_t otherSem, struct net_context *ctx)
