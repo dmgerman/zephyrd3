9ef04430bf540f93f8336c319de13b7950380639
Daniel Leung <daniel.leung@intel.com>
2015-09-30 09:58:29 -0700

    x86: galileo: setup shared IRQ triggering condition
    This adds the code to setup the IO-APIC for shared IRQ.
    The code to enable shared IRQ with GPIO and I2C will follow.
    
    Change-Id: I6e7de69f83bf7f1dd0da0571dbcb417beb2c232b
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 0b45e8b0c2a44204279ad79c658c79297f262811

--- arch/x86/platforms/galileo/galileo.c
+++ arch/x86/platforms/galileo/galileo.c
@@ -1,5 +1,14 @@
+DECL|SHARED_IRQ_0_IOAPIC_FLAGS|macro|SHARED_IRQ_0_IOAPIC_FLAGS
+DECL|SHARED_IRQ_0_IOAPIC_FLAGS|macro|SHARED_IRQ_0_IOAPIC_FLAGS
+DECL|SHARED_IRQ_0_IOAPIC_FLAGS|macro|SHARED_IRQ_0_IOAPIC_FLAGS
+DECL|SHARED_IRQ_0_IOAPIC_FLAGS|macro|SHARED_IRQ_0_IOAPIC_FLAGS
+DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
+DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
+DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
+DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
 DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
 DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
 DECL|galileo_init|function|static int galileo_init(struct device *arg)
 DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused) {
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
+DECL|shared_irq_config|function|static int shared_irq_config(struct device *unused)
