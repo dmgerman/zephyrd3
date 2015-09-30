94086a71acbb05cedc8760ad4284af42a6498a82
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-09-30 13:33:07 +0300

    pci: Removing pci enumeration debug info
    Since the console is not yet setup, there will be nothing to be printed
    out. Also, it adds a dedicated init function which is only useful for
    it. Instead, debugging PCI enumeration can be simply done in application
    side.
    
    Change-Id: Ia8384caa97d43f0bc4300ecf36bc11d1b8bd5581
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: a15c4a6be0f898850248e8ce618cd0429ec0fc39

--- arch/x86/platforms/ia32_pci/ia32_pci.c
+++ arch/x86/platforms/ia32_pci/ia32_pci.c
@@ -2,6 +2,5 @@ DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
 DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
 DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused) {
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
-DECL|ia32_pci_init|function|static int ia32_pci_init(struct device *arg)
 DECL|spi_irq_set_0|function|static int spi_irq_set_0(struct device *unused)
 DECL|spi_irq_set_1|function|static int spi_irq_set_1(struct device *unused)
