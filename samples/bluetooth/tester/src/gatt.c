dcb69622f4295576a1a6af84738df4bfb355634c
Mariusz Skamra <mariusz.skamra@tieto.com>
2015-10-01 09:46:19 +0200

    Bluetooth: Add support for writting characterisitc value
    This adds write and flush attribute callbacks to be used for writting
    attribute values.
    With this patch lower tester can test if IUT can properly handle
    write request to the characteristic values.
    
    > ACL Data RX: Handle 64 flags 0x02 dlen 7  [hci0] 1151923.440823
          ATT: Read Request (0x0a) len 2
            Handle: 0x0003
    < ACL Data TX: Handle 64 flags 0x00 dlen 7  [hci0] 1151923.456050
          ATT: Read Response (0x0b) len 2
            Value: bbbb
    > ACL Data RX: Handle 64 flags 0x02 dlen 9  [hci0] 1151928.435525
          ATT: Write Request (0x12) len 4
            Handle: 0x0003
              Data: ffff
    < ACL Data TX: Handle 64 flags 0x00 dlen 5  [hci0] 1151928.456865
          ATT: Write Response (0x13) len 0
    > ACL Data RX: Handle 64 flags 0x02 dlen 7  [hci0] 1151929.380796
          ATT: Read Request (0x0a) len 2
            Handle: 0x0003
    < ACL Data TX: Handle 64 flags 0x00 dlen 7  [hci0] 1151929.394636
          ATT: Read Response (0x0b) len 2
            Value: ffff
    
    Change-Id: Iea1d39e1a52496692141411251fd4ac2f9ae0b97
    Signed-off-by: Mariusz Skamra <mariusz.skamra@tieto.com>
    
    Former-commit-id: 13e0178c559cb2fd26d337e28727cceef95b839b

--- samples/bluetooth/tester/src/gatt.c
+++ samples/bluetooth/tester/src/gatt.c
@@ -7,7 +7,9 @@ DECL|buf|member|uint8_t buf[MAX_BUFFER_SIZE];
 DECL|chr_val|variable|chr_val
 DECL|chr|variable|chr
 DECL|data|member|uint8_t *data;
+DECL|flush_value|function|static int flush_value(struct bt_conn *conn, const struct bt_gatt_attr *attr, uint8_t flags)
 DECL|gatt_buf_add|function|static void *gatt_buf_add(const void *data, size_t len)
+DECL|gatt_buf_reserve|function|static void *gatt_buf_reserve(size_t len)
 DECL|gatt_buf|variable|gatt_buf
 DECL|gatt_db_add|function|static struct bt_gatt_attr *gatt_db_add(const struct bt_gatt_attr *pattern)
 DECL|gatt_db_lookup_id|function|static struct bt_gatt_attr *gatt_db_lookup_id(uint16_t attr_id)
@@ -15,6 +17,7 @@ DECL|gatt_db|variable|gatt_db
 DECL|gatt_value|struct|struct gatt_value {
 DECL|len|member|uint16_t len;
 DECL|len|member|uint8_t len;
+DECL|prep_data|member|uint8_t *prep_data;
 DECL|read_value|function|static int read_value(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|set_value|function|static void set_value(uint8_t *data, uint16_t len)
 DECL|start_server|function|static void start_server(uint8_t *data, uint16_t len)
@@ -23,3 +26,4 @@ DECL|svc_pri|variable|svc_pri
 DECL|svc_sec|variable|svc_sec
 DECL|tester_handle_gatt|function|void tester_handle_gatt(uint8_t opcode, uint8_t index, uint8_t *data, uint16_t len)
 DECL|tester_init_gatt|function|uint8_t tester_init_gatt(void)
+DECL|write_value|function|static int write_value(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *buf, uint16_t len, uint16_t offset)
