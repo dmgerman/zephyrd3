20af3f4aaf8d694d81f8d066aca8bf3cf3fa5673
Szymon Janc <ext.szymon.janc@tieto.com>
2015-10-01 10:39:01 +0200

    Bluetooth: Add new code for stack usage analysis
    This introduces simple API for analysing stack usage. Stack growth
    direction is determined on build instead of runtime.
    
    Change-Id: Iacb160d088cc0af57e2e9dedc72cbc5e31fd22f4
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: edf9cc3065dac7a522286b73e675d6ab387a834a

---
+++ net/bluetooth/stack.h
@@ -0,0 +1,2 @@
+DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned size)
+DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned size)
