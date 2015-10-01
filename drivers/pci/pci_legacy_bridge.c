b74a50e38f6b347f7cd5bbc4cdb22b799480d2df
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-10-01 14:40:23 -0400

    Add PCI legacy bridge device driver
    PCI legacy bridge is a device connected to PCI bus and
    allowing to program the routes from interrupt pins to IRQ
    numbers.
    
    Change-Id: I129719d71f958bbf5ad8c5c9949dcea93d94b89d
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 13b3e2a127823ba1b78d9b377dcabe81c02a7812

---
+++ drivers/pci/pci_legacy_bridge.c
@@ -0,0 +1,8 @@
+DECL|PCI_ADDR_IRQAGENT0|macro|PCI_ADDR_IRQAGENT0
+DECL|PCI_ADDR_IRQAGENT1|macro|PCI_ADDR_IRQAGENT1
+DECL|PCI_ADDR_IRQAGENT2|macro|PCI_ADDR_IRQAGENT2
+DECL|PCI_ADDR_IRQAGENT3|macro|PCI_ADDR_IRQAGENT3
+DECL|PCI_LEGACY_BRIDGE_REG|macro|PCI_LEGACY_BRIDGE_REG
+DECL|pci_legacy_bridge_configure|function|void pci_legacy_bridge_configure(struct pci_dev_info *dev_info, int io_block_num, int pci_interrupt_pin, int irq_number)
+DECL|pci_legacy_bridge_detect|function|int pci_legacy_bridge_detect(struct pci_dev_info *dev_info)
+DECL|pci_rcba_mask_get|function|static int pci_rcba_mask_get(union pci_addr_reg pci_ctrl_addr, uint32_t *mask)
