59a7c5d54772562fc6568a662c681bbac41db165
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-10-01 14:16:47 -0700

    i2c: Support the generic API callback in designware's controller driver
    Call the callback when relevant:
    - When STOP_DET if received
    - On error
    
    Change-Id: Id60190c6ac5c0b15a0a6cd3fb30b9cc41f2333e8
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: ddb35ccfa63487a0692cd9310a41def09977d974

--- drivers/i2c/i2c-dw.c
+++ drivers/i2c/i2c-dw.c
@@ -5,6 +5,7 @@ DECL|POLLING_TIMEOUT|macro|POLLING_TIMEOUT
 DECL|_i2c_dw_data_read|function|static void _i2c_dw_data_read(struct device *dev)
 DECL|_i2c_dw_data_send|function|static void _i2c_dw_data_send(struct device *dev)
 DECL|_i2c_dw_setup|function|static int _i2c_dw_setup(struct device *dev)
+DECL|_i2c_dw_transfer_complete|function|static inline void _i2c_dw_transfer_complete(struct device *dev)
 DECL|_i2c_dw_transfer|function|static int _i2c_dw_transfer(struct device *dev, uint8_t *write_buf, uint32_t write_len, uint8_t *read_buf, uint32_t read_len, uint16_t slave_address)
 DECL|funcs|variable|funcs
 DECL|i2c_0_runtime|variable|i2c_0_runtime
@@ -21,5 +22,6 @@ DECL|i2c_dw_polling_write|function|static int i2c_dw_polling_write(struct device
 DECL|i2c_dw_read|function|static int i2c_dw_read(struct device *dev, uint8_t *buf,uint32_t len, uint16_t slave_addr)
 DECL|i2c_dw_resume|function|static int i2c_dw_resume(struct device *dev)
 DECL|i2c_dw_runtime_configure|function|static int i2c_dw_runtime_configure(struct device *dev, uint32_t config)
+DECL|i2c_dw_set_callback|function|static int i2c_dw_set_callback(struct device *dev, i2c_callback cb)
 DECL|i2c_dw_suspend|function|static int i2c_dw_suspend(struct device *dev)
 DECL|i2c_dw_write|function|static int i2c_dw_write(struct device *dev, uint8_t *buf,uint32_t len, uint16_t slave_addr)
