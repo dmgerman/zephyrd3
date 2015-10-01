1e4f516050e121fefd9a4521386246abf3fab8fe
Daniel Leung <daniel.leung@intel.com>
2015-10-01 10:59:35 -0700

    x86: galileo: expand pinmux to setup GPIOs on SoC
    This expands the Galileo pinmux driver to configure the GPIOs
    on the DesignWare IP block, and the core/resume wells on
    the legacy bridge.
    
    Change-Id: Ia1df4b6fd3b104f08563fe9eab93f01efbb53b66
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 9299ef07cc3e0a132c079ce3ccfa992ee53cfea1

--- arch/x86/platforms/galileo/galileo_pinmux.c
+++ arch/x86/platforms/galileo/galileo_pinmux.c
@@ -2,6 +2,9 @@ DECL|DONT_CARE|enumerator|DONT_CARE = 0xFF,
 DECL|EXP0|enumerator|EXP0,
 DECL|EXP1|enumerator|EXP1,
 DECL|EXP2|enumerator|EXP2,
+DECL|G_CW|enumerator|G_CW,
+DECL|G_DW|enumerator|G_DW,
+DECL|G_RW|enumerator|G_RW,
 DECL|NONE|enumerator|NONE,
 DECL|NUM_PIN_FUNCS|macro|NUM_PIN_FUNCS
 DECL|PIN_HIGH|enumerator|PIN_HIGH = 0x01,
@@ -21,13 +24,16 @@ DECL|galileo_dev_set|function|static uint32_t galileo_dev_set(struct device *dev
 DECL|galileo_pinmux_driver|variable|galileo_pinmux_driver
 DECL|galileo_pmux|variable|galileo_pmux
 DECL|gpio_chip|enum|enum gpio_chip {
+DECL|gpio_core|member|struct device *gpio_core;
+DECL|gpio_dw|member|struct device *gpio_dw;
+DECL|gpio_resume|member|struct device *gpio_resume;
 DECL|io_pin|member|uint8_t io_pin;
 DECL|level|member|enum pin_level level;
 DECL|mux_config|variable|mux_config
 DECL|mux_path|struct|struct mux_path {
 DECL|mux_pin|struct|struct mux_pin {
 DECL|mux|member|enum gpio_chip mux;
-DECL|path|member|struct mux_pin path[4];
+DECL|path|member|struct mux_pin path[5];
 DECL|pin_level|enum|enum pin_level {
 DECL|pinmux_galileo_initialize|function|int pinmux_galileo_initialize(struct device *port)
 DECL|pin|member|uint8_t pin;
