53631b520e317c83926e611802534a2becf180d0
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-10-01 14:16:47 -0700

    i2c: Add a callback to the generic API
    Thus the caller will be noticed once its read and/or write calls have
    gone through.
    Taking the opportunity to fix the documentation about return code.
    
    Change-Id: Id3f588fab978cad4393583f26509a9e91dcc8be2
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: d3d14846cc075da340042b3b2491fde647f76771

--- include/i2c.h
+++ include/i2c.h
@@ -1,4 +1,7 @@
 DECL|I2C_ADDR_10_BITS|macro|I2C_ADDR_10_BITS
+DECL|I2C_CB_ERROR|enumerator|I2C_CB_ERROR = 3,
+DECL|I2C_CB_READ|enumerator|I2C_CB_READ = 2,
+DECL|I2C_CB_WRITE|enumerator|I2C_CB_WRITE = 1,
 DECL|I2C_MODE_MASTER|macro|I2C_MODE_MASTER
 DECL|I2C_MODE_SLAVE_READ|macro|I2C_MODE_SLAVE_READ
 DECL|I2C_SPEED_FAST_PLUS|macro|I2C_SPEED_FAST_PLUS
@@ -14,12 +17,16 @@ DECL|dev_config|union|union dev_config {
 DECL|i2c_api_configure_t|typedef|typedef int (*i2c_api_configure_t)(struct device *dev,
 DECL|i2c_api_io_t|typedef|typedef int (*i2c_api_io_t)(struct device *dev,
 DECL|i2c_api_resume_t|typedef|typedef int (*i2c_api_resume_t)(struct device *dev);
+DECL|i2c_api_set_callback_t|typedef|typedef int (*i2c_api_set_callback_t)(struct device *dev,
 DECL|i2c_api_suspend_t|typedef|typedef int (*i2c_api_suspend_t)(struct device *dev);
+DECL|i2c_callback|typedef|typedef void (*i2c_callback)(struct device *dev,
+DECL|i2c_cb_type|enum|enum i2c_cb_type {
 DECL|i2c_configure|function|static inline int i2c_configure(struct device *dev, uint32_t dev_config)
 DECL|i2c_driver_api|struct|struct i2c_driver_api {
 DECL|i2c_polling_write|function|static inline int i2c_polling_write(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|i2c_read|function|static inline int i2c_read(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|i2c_resume|function|static inline int i2c_resume(struct device *dev)
+DECL|i2c_set_callback|function|static inline int i2c_set_callback(struct device *dev, i2c_callback cb)
 DECL|i2c_suspend|function|static inline int i2c_suspend(struct device *dev)
 DECL|i2c_write|function|static inline int i2c_write(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|is_master_device|member|uint32_t is_master_device : 1;
@@ -29,6 +36,7 @@ DECL|raw|member|uint32_t raw;
 DECL|read|member|i2c_api_io_t read;
 DECL|reserved|member|uint32_t reserved : 26;
 DECL|resume|member|i2c_api_resume_t resume;
+DECL|set_callback|member|i2c_api_set_callback_t set_callback;
 DECL|speed|member|uint32_t speed : 3;
 DECL|suspend|member|i2c_api_suspend_t suspend;
 DECL|use_10_bit_addr|member|uint32_t use_10_bit_addr : 1;
