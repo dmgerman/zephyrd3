1316884bd6141cea062dbd4df34438cdcf7688c1
Szymon Janc <ext.szymon.janc@tieto.com>
2015-10-05 15:10:59 +0200

    Bluetooth: Don't delay bt_conn connected callback
    Delaying only connected callback resulted in races where security
    or pairing callback could be called before bt_conn connected.
    
    Currently only procedure that relies on read features is connection
    parameters update and that should not conflict with other traffic.
    
    Change-Id: I835535abbff21ea8d86b728f5681f90ef9fd337c
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: 89f8453ebe0897e32993ee95a8b2aeac79a3d0cb

--- net/bluetooth/conn.c
+++ net/bluetooth/conn.c
@@ -3,7 +3,7 @@ DECL|BT_DBG|macro|BT_DBG
 DECL|CONN_TIMEOUT|macro|CONN_TIMEOUT
 DECL|bt_conn_add|function|struct bt_conn *bt_conn_add(const bt_addr_le_t *peer)
 DECL|bt_conn_cb_register|function|void bt_conn_cb_register(struct bt_conn_cb *cb)
-DECL|bt_conn_connected|function|void bt_conn_connected(struct bt_conn *conn)
+DECL|bt_conn_connected|function|static void bt_conn_connected(struct bt_conn *conn)
 DECL|bt_conn_create_le|function|struct bt_conn *bt_conn_create_le(const bt_addr_le_t *peer)
 DECL|bt_conn_disconnected|function|static void bt_conn_disconnected(struct bt_conn *conn)
 DECL|bt_conn_disconnect|function|int bt_conn_disconnect(struct bt_conn *conn, uint8_t reason)
