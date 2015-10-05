299320b1519dcfd8f3d4890c7923d295c9ad27b0
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-10-05 18:41:18 +0300

    adc: Adding driver for TI's adc108s102 chip
    It's an SPI driven chip and proposes up to 8 channels for input
    conversion. It's found on Galileo board.
    
    Change-Id: I916367e9be0849812c6a509082a501730a01ce60
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 5bcdfbe255bece0c427bc358a593257732d98a8a

---
+++ drivers/adc/adc-ti-adc108s102.c
@@ -0,0 +1,18 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|_ti_adc108s102_completed|function|static void _ti_adc108s102_completed(struct device *dev,enum adc_callback_type cb_type)
+DECL|_ti_adc108s102_handle_result|function|static inline void _ti_adc108s102_handle_result(struct device *dev)
+DECL|_ti_adc108s102_prepare|function|static int32_t _ti_adc108s102_prepare(struct device *dev)
+DECL|_ti_adc108s102_run_with_delay|function|static inline void _ti_adc108s102_run_with_delay(struct device *dev,int32_t delay)
+DECL|_ti_adc108s102_sampling|function|static void _ti_adc108s102_sampling(int data, int unused)
+DECL|_ti_adc108s102_spi_cb|function|static void _ti_adc108s102_spi_cb(struct device *spi_dev,enum spi_cb_type cb_type, void *user_data)
+DECL|_verify_entries|function|static inline int _verify_entries(struct adc_seq_table *seq_table)
+DECL|adc108s102_0_config|variable|adc108s102_0_config
+DECL|adc108s102_0_data|variable|adc108s102_0_data
+DECL|ti_adc108s102_api|variable|ti_adc108s102_api
+DECL|ti_adc108s102_disable|function|static void ti_adc108s102_disable(struct device *dev)
+DECL|ti_adc108s102_enable|function|static void ti_adc108s102_enable(struct device *dev)
+DECL|ti_adc108s102_init|function|int ti_adc108s102_init(struct device *dev)
+DECL|ti_adc108s102_read|function|static int ti_adc108s102_read(struct device *dev,struct adc_seq_table *seq_table)
+DECL|ti_adc108s102_set_callback|function|static void ti_adc108s102_set_callback(struct device *dev, adc_callback_t cb)
---
+++ drivers/adc/adc-ti-adc108s102.h
@@ -0,0 +1,24 @@
+DECL|ADC108S102_CHANNELS_SIZE|macro|ADC108S102_CHANNELS_SIZE
+DECL|ADC108S102_CHANNELS|macro|ADC108S102_CHANNELS
+DECL|ADC108S102_CHANNEL_CMD|macro|ADC108S102_CHANNEL_CMD
+DECL|ADC108S102_CMD_BUFFER_SIZE|macro|ADC108S102_CMD_BUFFER_SIZE
+DECL|ADC108S102_SAMPLING_BUFFER_SIZE|macro|ADC108S102_SAMPLING_BUFFER_SIZE
+DECL|ADC108S102_SAMPLING_STACK_SIZE|macro|ADC108S102_SAMPLING_STACK_SIZE
+DECL|__ADC108S102_PRIV_H__|macro|__ADC108S102_PRIV_H__
+DECL|buf_idx|member|uint32_t buf_idx;
+DECL|cb|member|adc_callback_t cb;
+DECL|chans|member|struct ti_adc108s102_chan chans[ADC108S102_CHANNELS];
+DECL|cmd_buf_len|member|uint8_t cmd_buf_len;
+DECL|cmd_buffer|member|uint16_t cmd_buffer[ADC108S102_CMD_BUFFER_SIZE];
+DECL|sampling_buf_len|member|uint8_t sampling_buf_len;
+DECL|sampling_buffer|member|uint16_t sampling_buffer[ADC108S102_SAMPLING_BUFFER_SIZE];
+DECL|sampling_stack|member|char __stack sampling_stack[ADC108S102_SAMPLING_STACK_SIZE];
+DECL|seq_table|member|struct adc_seq_table *seq_table;
+DECL|spi_config_flags|member|uint32_t spi_config_flags;
+DECL|spi_freq|member|uint32_t spi_freq;
+DECL|spi_port|member|const char *spi_port;
+DECL|spi_slave|member|uint32_t spi_slave;
+DECL|spi|member|struct device *spi;
+DECL|ti_adc108s102_chan|struct|struct ti_adc108s102_chan {
+DECL|ti_adc108s102_config|struct|struct ti_adc108s102_config {
+DECL|ti_adc108s102_data|struct|struct ti_adc108s102_data {
