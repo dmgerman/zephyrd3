aa2d86d48547d4c75f2344e30257cf243d6e9155
Peter Mitsis <peter.mitsis@windriver.com>
2015-10-05 07:07:20 -0700

    arc: add nanokernel tickless idle support in timer0 driver
    Change-Id: Id2ee328458c5a1be944e90c34bbc2158464be325
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 9cfd0de4100b24e30175df19cbb5005851ed554b

--- drivers/timer/arcv2_timer0.c
+++ drivers/timer/arcv2_timer0.c
@@ -3,10 +3,23 @@ DECL|_ARC_V2_TMR_CTRL_IP|macro|_ARC_V2_TMR_CTRL_IP
 DECL|_ARC_V2_TMR_CTRL_NH|macro|_ARC_V2_TMR_CTRL_NH
 DECL|_ARC_V2_TMR_CTRL_W|macro|_ARC_V2_TMR_CTRL_W
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
+DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t ticks)
+DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
 DECL|_timer_int_handler|function|void _timer_int_handler(void *unused)
-DECL|clock_accumulated_count|variable|clock_accumulated_count
-DECL|count_get|function|static ALWAYS_INLINE uint32_t count_get(void)
-DECL|enable|function|static ALWAYS_INLINE void enable(uint32_t count /* interrupt triggers when up-counter reaches this value */ )
-DECL|limit_get|function|static ALWAYS_INLINE uint32_t limit_get(void)
+DECL|accumulated_cycle_count|variable|accumulated_cycle_count
+DECL|cycles_per_tick|variable|cycles_per_tick
+DECL|max_system_ticks|variable|max_system_ticks
+DECL|programmed_limit|variable|programmed_limit
+DECL|programmed_ticks|variable|programmed_ticks
+DECL|straddled_tick_on_idle_enter|variable|straddled_tick_on_idle_enter
 DECL|sys_clock_disable|function|void sys_clock_disable(void)
 DECL|sys_cycle_get_32|function|uint32_t sys_cycle_get_32(void)
+DECL|tickless_idle_init|function|static void tickless_idle_init(void)
+DECL|tickless_idle_init|function|static void tickless_idle_init(void) {}
+DECL|timer0_control_register_get|function|static ALWAYS_INLINE uint32_t timer0_control_register_get(void)
+DECL|timer0_control_register_set|function|static ALWAYS_INLINE void timer0_control_register_set(uint32_t value)
+DECL|timer0_count_register_get|function|static ALWAYS_INLINE uint32_t timer0_count_register_get(void)
+DECL|timer0_count_register_set|function|static ALWAYS_INLINE void timer0_count_register_set(uint32_t value)
+DECL|timer0_limit_register_set|function|static ALWAYS_INLINE void timer0_limit_register_set(uint32_t count)
+DECL|update_accumulated_count|function|static ALWAYS_INLINE void update_accumulated_count(void)
+DECL|update_accumulated_count|function|static ALWAYS_INLINE void update_accumulated_count(void)
