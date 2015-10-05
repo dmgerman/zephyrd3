a039d7f4a58ce4a2f1da24c6f98f1f588adc47a0
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-10-05 13:53:03 +0300

    Bluetooth: L2CAP: Add initial public API
    This split L2CAP API so that server API is available to applications
    while the rest of the API is keep internal to the stack.
    
    Change-Id: I031926ff906ce100684fba0947b2e9eb2c8fcaeb
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 30b6a9fe7b027fab97659fc9a6e1fd24f007d672

---
+++ include/bluetooth/l2cap.h
@@ -0,0 +1,21 @@
+DECL|__BT_L2CAP_H|macro|__BT_L2CAP_H
+DECL|_next|member|struct bt_l2cap_chan *_next;
+DECL|_next|member|struct bt_l2cap_server *_next;
+DECL|accept|member|int (*accept)(struct bt_conn *conn, struct bt_l2cap_chan **chan);
+DECL|bt_l2cap_chan_ops|struct|struct bt_l2cap_chan_ops {
+DECL|bt_l2cap_chan|struct|struct bt_l2cap_chan {
+DECL|bt_l2cap_endpoint|struct|struct bt_l2cap_endpoint {
+DECL|bt_l2cap_server|struct|struct bt_l2cap_server {
+DECL|cid|member|uint16_t cid;
+DECL|connected|member|void (*connected)(struct bt_l2cap_chan *chan);
+DECL|conn|member|struct bt_conn *conn;
+DECL|credits|member|uint16_t credits;
+DECL|disconnected|member|void (*disconnected)(struct bt_l2cap_chan *chan);
+DECL|encrypt_change|member|void (*encrypt_change)(struct bt_l2cap_chan *chan);
+DECL|mps|member|uint16_t mps;
+DECL|mtu|member|uint16_t mtu;
+DECL|ops|member|struct bt_l2cap_chan_ops *ops;
+DECL|psm|member|uint16_t psm;
+DECL|recv|member|void (*recv)(struct bt_l2cap_chan *chan,
+DECL|rx|member|struct bt_l2cap_endpoint rx;
+DECL|tx|member|struct bt_l2cap_endpoint tx;
