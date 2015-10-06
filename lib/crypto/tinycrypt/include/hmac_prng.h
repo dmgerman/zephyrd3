62fccaa4c1c33ee1b96c1c88f6b3df132f31f632
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-10-06 15:35:12 -0700

    crypto: comply with checkpatch file length
    Fixes the tinycrypt library to use Zephyr checkpath acceptable line
    lengths.
    
    Change-Id: Ia7bef46a39fa029cda826ec5b2d7d42036d9bdfe
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 53dbe1f1ec67158b4943ba2d095598606e7cac8b

--- lib/crypto/tinycrypt/include/hmac_prng.h
+++ lib/crypto/tinycrypt/include/hmac_prng.h
@@ -1,7 +1,7 @@
 DECL|TCHmacPrng_t|typedef|typedef struct tc_hmac_prng_struct *TCHmacPrng_t;
 DECL|__TC_HMAC_PRNG_H__|macro|__TC_HMAC_PRNG_H__
-DECL|countdown|member|uint32_t countdown; /* calls to tc_hmac_prng_generate left before re-seed */
-DECL|h|member|struct tc_hmac_state_struct h; /* the HMAC instance for this PRNG */
-DECL|key|member|uint8_t key[TC_SHA256_DIGEST_SIZE]; /* the PRNG key */
+DECL|countdown|member|uint32_t countdown;
+DECL|h|member|struct tc_hmac_state_struct h;
+DECL|key|member|uint8_t key[TC_SHA256_DIGEST_SIZE];
 DECL|tc_hmac_prng_struct|struct|struct tc_hmac_prng_struct {
-DECL|v|member|uint8_t v[TC_SHA256_DIGEST_SIZE]; /* PRNG state */
+DECL|v|member|uint8_t v[TC_SHA256_DIGEST_SIZE];
