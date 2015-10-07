6a5c0e4ccb93e0838ffdad286d3b89ee2bc3b226
Daniel Leung <daniel.leung@intel.com>
2015-10-07 15:08:17 -0700

    i2c: add API for generic transfer
    Add an API to I2C for generic transfer. This is for those data
    transactions which are more complex than simple read or write.
    
    One example use of this is to read register data from a slave
    device. The master has to write the address of the register
    to slave, then start reading from slave for register data.
    In this situation, simple read or write is no longer
    sufficient, as for some slave devices, they require RESTART
    when data transfer direction changes. Doing a simple write via
    i2c_write() results in STOP at end of transfer, which results
    in the slave not responding to read request as it is not
    expected to send anything back.
    
    Change-Id: I22f9c5ba2f7a7c48a33b9cb1aaf559e596a85d39
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: ac0beb703437161687a061e409525207d3ddad0d

--- include/i2c.h
+++ include/i2c.h
@@ -15,6 +15,7 @@ DECL|bits|member|} bits;
 DECL|configure|member|i2c_api_configure_t configure;
 DECL|dev_config|union|union dev_config {
 DECL|i2c_api_configure_t|typedef|typedef int (*i2c_api_configure_t)(struct device *dev,
+DECL|i2c_api_full_io_t|typedef|typedef int (*i2c_api_full_io_t)(struct device *dev,
 DECL|i2c_api_io_t|typedef|typedef int (*i2c_api_io_t)(struct device *dev,
 DECL|i2c_api_resume_t|typedef|typedef int (*i2c_api_resume_t)(struct device *dev);
 DECL|i2c_api_set_callback_t|typedef|typedef int (*i2c_api_set_callback_t)(struct device *dev,
@@ -23,14 +24,17 @@ DECL|i2c_callback|typedef|typedef void (*i2c_callback)(struct device *dev,
 DECL|i2c_cb_type|enum|enum i2c_cb_type {
 DECL|i2c_configure|function|static inline int i2c_configure(struct device *dev, uint32_t dev_config)
 DECL|i2c_driver_api|struct|struct i2c_driver_api {
+DECL|i2c_poll_transfer|function|static inline int i2c_poll_transfer(struct device *dev, uint8_t *tx_buf, uint32_t tx_len, uint8_t *rx_buf, uint32_t rx_len, uint16_t addr, uint32_t ctrl_flags)
 DECL|i2c_polling_write|function|static inline int i2c_polling_write(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|i2c_read|function|static inline int i2c_read(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|i2c_resume|function|static inline int i2c_resume(struct device *dev)
 DECL|i2c_set_callback|function|static inline int i2c_set_callback(struct device *dev, i2c_callback cb)
 DECL|i2c_suspend|function|static inline int i2c_suspend(struct device *dev)
+DECL|i2c_transfer|function|static inline int i2c_transfer(struct device *dev, uint8_t *tx_buf, uint32_t tx_len, uint8_t *rx_buf, uint32_t rx_len, uint16_t addr, uint32_t ctrl_flags)
 DECL|i2c_write|function|static inline int i2c_write(struct device *dev, uint8_t *buf, uint32_t len, uint16_t addr)
 DECL|is_master_device|member|uint32_t is_master_device : 1;
 DECL|is_slave_read|member|uint32_t is_slave_read : 1;
+DECL|poll_transfer|member|i2c_api_full_io_t poll_transfer;
 DECL|polling_write|member|i2c_api_io_t polling_write;
 DECL|raw|member|uint32_t raw;
 DECL|read|member|i2c_api_io_t read;
@@ -39,5 +43,6 @@ DECL|resume|member|i2c_api_resume_t resume;
 DECL|set_callback|member|i2c_api_set_callback_t set_callback;
 DECL|speed|member|uint32_t speed : 3;
 DECL|suspend|member|i2c_api_suspend_t suspend;
+DECL|transfer|member|i2c_api_full_io_t transfer;
 DECL|use_10_bit_addr|member|uint32_t use_10_bit_addr : 1;
 DECL|write|member|i2c_api_io_t write;
