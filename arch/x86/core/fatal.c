75db565b625896a7af027730314299a81a97255c
Andrew Boie <andrew.p.boie@intel.com>
2015-10-08 12:42:11 -0700

    x86: Improve exception/trap debugging
    Currently when an unhandled exception or trap occurs, we don't know
    specifically which one happened. The CONFIG_EXCEPTION_DEBUG option
    installs handlers for various exceptions and prints out details
    when they happen.
    
    Change-Id: I4d3fe4544c3e2583c4f83b306b86ac0595d9ce0e
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: be3e3b0f8c3cb77f08e0e1e7d4dbad37f29a4d24

--- arch/x86/core/fatal.c
+++ arch/x86/core/fatal.c
@@ -1,2 +1,4 @@
+DECL|EXC_FUNC|macro|EXC_FUNC
 DECL|_NanoFatalErrorHandler|function|FUNC_NORETURN void _NanoFatalErrorHandler(unsigned int reason, /* reason that handler was called */ const NANO_ESF *pEsf /* pointer to exception stack frame */ )
 DECL|_default_esf|variable|_default_esf
+DECL|generic_exc_handle|function|static FUNC_NORETURN void generic_exc_handle(char *description, unsigned int vector, const NANO_ESF *pEsf)
