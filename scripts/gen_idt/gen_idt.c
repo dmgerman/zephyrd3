42e8a0a6bf1d3a88f89a44177f09261712d5ea60
Peter Mitsis <peter.mitsis@windriver.com>
2015-10-08 11:34:06 -0400

    Refactor gen_idt
    Refactor gen_idt tool to detect more errors as well as be both more clear and
    easily expanded.
    
    Change-Id: I035a0666871733cc670f64302a1e85be455550ee
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 3dc247825c23ccd1a665c5bcb520be8ae8fadb85

--- scripts/gen_idt/gen_idt.c
+++ scripts/gen_idt/gen_idt.c
@@ -1,7 +1,8 @@
 DECL|EXECFILE|enumerator|EXECFILE = NUSERFILES, /* for name of executable */
 DECL|IFILE|enumerator|IFILE = 0, /* input file */
 DECL|LONG_USAGE|enumerator|enum { SHORT_USAGE, LONG_USAGE };
-DECL|NFILES|enumerator|NFILES /* total number of files open */
+DECL|MAX_NUM_VECTORS|macro|MAX_NUM_VECTORS
+DECL|NFILES|enumerator|NFILES /* total number of file names */
 DECL|NUSERFILES|enumerator|NUSERFILES, /* number of user-provided file names */
 DECL|OFILE|enumerator|OFILE, /* output file */
 DECL|O_BINARY|macro|O_BINARY
@@ -14,14 +15,26 @@ DECL|close_files|function|static void close_files(void)
 DECL|dpl|member|unsigned int dpl;
 DECL|fds|variable|fds
 DECL|filenames|variable|filenames
-DECL|fnc|member|void *fnc;
-DECL|genIdt|function|static void genIdt(void)
+DECL|generate_idt|function|static void generate_idt(void)
+DECL|generated_entry|variable|generated_entry
+DECL|genidt_entry_s|struct|struct genidt_entry_s {
+DECL|genidt_header_s|struct|struct genidt_header_s {
+DECL|genidt_header|variable|genidt_header
 DECL|get_exec_name|function|static void get_exec_name(char *pathname)
 DECL|get_options|function|static void get_options(int argc, char *argv[])
-DECL|idt|variable|idt
+DECL|isr|member|void *isr;
 DECL|main|function|int main(int argc, char *argv[])
-DECL|numVecs|variable|numVecs
+DECL|num_entries|member|unsigned int num_entries;
+DECL|num_vectors|variable|num_vectors
 DECL|open_files|function|static void open_files(void)
-DECL|s_isrList|struct|struct s_isrList {
+DECL|read_input_file|function|static void read_input_file(void)
+DECL|show_entry|function|static void show_entry(struct genidt_entry_s *entry)
+DECL|spurious_addr|member|void *spurious_addr;
+DECL|spurious_no_error_addr|member|void *spurious_no_error_addr;
+DECL|supplied_entry|variable|supplied_entry
 DECL|usage|function|static void usage(int len)
+DECL|validate_dpl|function|static void validate_dpl(void)
+DECL|validate_input_file|function|static void validate_input_file(void)
+DECL|validate_vector_id|function|static void validate_vector_id(void)
+DECL|vector_id|member|unsigned int vector_id;
 DECL|version|variable|version
