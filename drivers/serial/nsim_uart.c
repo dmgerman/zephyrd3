ddf49dccba0a86c88471479dc1f859f01e303af3
Daniel Leung <daniel.leung@intel.com>
2015-10-08 11:08:16 -0700

    serial: nsim: add dummy poll_in function
    Both poll_in and poll_out are supposed to be implemented
    in each driver, as these are basic function.
    
    Change-Id: If1b6b11834f57934fe7e776bfcf818e088abdd0a
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: c615ae56cbf3b726ec86eec581f8399f159eb66f

--- drivers/serial/nsim_uart.c
+++ drivers/serial/nsim_uart.c
@@ -6,5 +6,6 @@ DECL|STATUS_REG|macro|STATUS_REG
 DECL|TXEMPTY|macro|TXEMPTY
 DECL|nsim_uart_driver_api|variable|nsim_uart_driver_api
 DECL|nsim_uart_driver_api|variable|nsim_uart_driver_api
+DECL|nsim_uart_poll_in|function|static int nsim_uart_poll_in(struct device *dev, unsigned char *c)
 DECL|nsim_uart_poll_out|function|unsigned char nsim_uart_poll_out(struct device *dev, unsigned char c)
 DECL|nsim_uart_port_init|function|void nsim_uart_port_init(struct device *dev, const struct uart_init_info * const init_info)
