03b6dde3a82424515bfe857d5785568e3fde62c8
Daniel Leung <daniel.leung@intel.com>
2015-10-07 17:03:08 -0700

    i2c: dw: implement generic transfer functions
    () For interrupt driven transfer, the driver already has
       a generic transfer function, so this simply exposes it
       for public consumption.
    () The polling write is extended to support generic
       transfer.
    
    Change-Id: I657db8a6376282c723728b7a875813d389cf7d27
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 01796ecf5c155cbf7daf5d89c3b938bb740630f7

--- drivers/i2c/i2c-dw.c
+++ drivers/i2c/i2c-dw.c
@@ -8,7 +8,6 @@ DECL|_i2c_dw_data_send|function|static void _i2c_dw_data_send(struct device *dev
 DECL|_i2c_dw_setup|function|static int _i2c_dw_setup(struct device *dev)
 DECL|_i2c_dw_transfer_complete|function|static inline void _i2c_dw_transfer_complete(struct device *dev)
 DECL|_i2c_dw_transfer_init|function|static int _i2c_dw_transfer_init(struct device *dev, uint8_t *write_buf, uint32_t write_len, uint8_t *read_buf, uint32_t read_len, uint16_t slave_address)
-DECL|_i2c_dw_transfer_start|function|static int _i2c_dw_transfer_start(struct device *dev, uint8_t *write_buf, uint32_t write_len, uint8_t *read_buf, uint32_t read_len, uint16_t slave_address)
 DECL|funcs|variable|funcs
 DECL|i2c_0_runtime|variable|i2c_0_runtime
 DECL|i2c_config_0|function|void i2c_config_0(struct device *port)
@@ -20,10 +19,12 @@ DECL|i2c_dw_memory_read|function|static inline uint32_t i2c_dw_memory_read(uint3
 DECL|i2c_dw_memory_write|function|static inline void i2c_dw_memory_write(uint32_t base_addr, uint32_t offset, uint32_t val)
 DECL|i2c_dw_pci_setup|function|static inline int i2c_dw_pci_setup(struct device *dev)
 DECL|i2c_dw_pci_setup|macro|i2c_dw_pci_setup
-DECL|i2c_dw_polling_write|function|static int i2c_dw_polling_write(struct device *dev,uint8_t *write_buf, uint32_t write_len, uint16_t slave_address)
+DECL|i2c_dw_poll_transfer|function|static int i2c_dw_poll_transfer(struct device *dev,uint8_t *write_buf, uint32_t write_len, uint8_t *read_buf, uint32_t read_len, uint16_t slave_address, uint32_t flags)
+DECL|i2c_dw_polling_write|function|static int i2c_dw_polling_write(struct device *dev, uint8_t *write_buf, uint32_t write_len, uint16_t slave_address)
 DECL|i2c_dw_read|function|static int i2c_dw_read(struct device *dev, uint8_t *buf,uint32_t len, uint16_t slave_addr)
 DECL|i2c_dw_resume|function|static int i2c_dw_resume(struct device *dev)
 DECL|i2c_dw_runtime_configure|function|static int i2c_dw_runtime_configure(struct device *dev, uint32_t config)
 DECL|i2c_dw_set_callback|function|static int i2c_dw_set_callback(struct device *dev, i2c_callback cb)
 DECL|i2c_dw_suspend|function|static int i2c_dw_suspend(struct device *dev)
+DECL|i2c_dw_transfer|function|static int i2c_dw_transfer(struct device *dev, uint8_t *write_buf, uint32_t write_len, uint8_t *read_buf, uint32_t read_len, uint16_t slave_address, uint32_t flags)
 DECL|i2c_dw_write|function|static int i2c_dw_write(struct device *dev, uint8_t *buf,uint32_t len, uint16_t slave_addr)
