0ed0bf4d2477d094de40892a58e84770579d914a
Anas Nashif <anas.nashif@intel.com>
2015-10-08 10:21:57 -0400

    toolchain: change section  macro to support external c libraries
    rename the sections macro to avoid conflicts with the same
    macro being defined in sys/cdefs.h of an external libc.
    
    Change-Id: I4d9e060eeff788ca4112c0ad3e98f0bea135f145
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: db1399874160ef8671da09eed53f3c2d6056283d

--- include/section_tags.h
+++ include/section_tags.h
@@ -1,4 +1,5 @@
 DECL|__gdb_stub_irq_vector_table|macro|__gdb_stub_irq_vector_table
+DECL|__in_section|macro|__in_section
 DECL|__irq_vector_table|macro|__irq_vector_table
 DECL|__irq_vector_table|macro|__irq_vector_table
 DECL|__isr_table_section|macro|__isr_table_section
@@ -6,6 +7,5 @@ DECL|__isr_table_section|macro|__isr_table_section
 DECL|__noinit|macro|__noinit
 DECL|__scp_section|macro|__scp_section
 DECL|__scs_section|macro|__scs_section
-DECL|__section|macro|__section
 DECL|__security_frdm_k64f_section|macro|__security_frdm_k64f_section
 DECL|_section_tags__h_|macro|_section_tags__h_
