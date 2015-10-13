5f730c552b09e726c0483241c4460e399c03fdbf
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-10-13 10:46:12 +0300

    Bluetooth: Configure Bluetooth for galileo platform
    Configure IOAPIC for Bluetooth interrupts connected to UART.
    
    Change-Id: Iad58f52e95c05245d38dd0f4cb10494b53bad8e0
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 1e1bb51da2c7f7b3752ea49f7f3e885b3f5093cc

--- arch/x86/platforms/galileo/galileo.c
+++ arch/x86/platforms/galileo/galileo.c
@@ -6,6 +6,7 @@ DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
 DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
 DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
 DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
+DECL|bluetooth_irq_set|function|static int bluetooth_irq_set(struct device *unused)
 DECL|console_irq_set|function|static int console_irq_set(struct device *unused)
 DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
 DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused)
