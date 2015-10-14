2978aec1bf53aa18d84631dd9264bcd412a2f38a
Allan Stephens <allan.stephens@windriver.com>
2015-10-14 16:02:04 -0400

    microkernel: Don't initialize packets in a command packet set
    Reworks the internal design of a command packet set so that
    the command packet array is split out into a separate variable
    lying in uninitialized memory. This shrinks the command packet's
    data section footprint to almost nothing.
    
    Note: A side effect of this change is that it is no longer possible
    to define a command packet set as a "static" variable since the
    CMD_PKT_SET_INSTANCE() macro now generates two variables.
    
    Change-Id: I9c7ebe637edf879758589ff4a26ace1303790bf7
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: f330d5adfed93b5641eb659accfc80515ca4a39a

--- include/microkernel/command_packet.h
+++ include/microkernel/command_packet.h
@@ -1,9 +1,8 @@
 DECL|CMD_PKT_SET_INSTANCE|macro|CMD_PKT_SET_INSTANCE
-DECL|CMD_PKT_SET|macro|CMD_PKT_SET
 DECL|CMD_PKT_SIZE_IN_WORDS|macro|CMD_PKT_SIZE_IN_WORDS
 DECL|_COMMAND_PACKET_H|macro|_COMMAND_PACKET_H
 DECL|cmdPkt_t|typedef|typedef uint32_t cmdPkt_t[CMD_PKT_SIZE_IN_WORDS];
 DECL|cmd_pkt_set|struct|struct cmd_pkt_set {
-DECL|command_packet|member|cmdPkt_t command_packet[]; /* array of command packets */
+DECL|command_packet|member|cmdPkt_t *command_packet; /* pointer to array of command packets */
 DECL|index|member|uint32_t index; /* index into command packet array */
 DECL|num_packets|member|uint32_t num_packets; /* number of command packets in set */
