204dbb541940d59c5a9290d591b4da19f8b80683
Allan Stephens <allan.stephens@windriver.com>
2015-10-14 13:39:06 -0400

    init: Optimize device_get_binding()
    Improves the efficiency of the search by doing a single pass through
    all device objects, rather than searching one level at a time.
    Also enhances the comments for this routine.
    
    Change-Id: I6786f24381f03d74d8c559ccbd7aa6a37683a8b7
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 83946282a94efb7bc9243b172665c1924aca782a

--- kernel/nanokernel/device.c
+++ kernel/nanokernel/device.c
@@ -1,3 +1,3 @@
 DECL|_sys_device_do_config_level|function|void _sys_device_do_config_level(int level)
 DECL|config_levels|variable|config_levels
-DECL|device_get_binding|function|struct device* device_get_binding(char *name)
+DECL|device_get_binding|function|struct device *device_get_binding(char *name)
