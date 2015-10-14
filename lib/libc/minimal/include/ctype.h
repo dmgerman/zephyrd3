334c8e026ddae794fd64c8bb429597d6306a2d20
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-10-14 14:31:32 -0700

    checkpatch: error - open_brace
    Change-Id: I434037ce969bcd1fc08bd4b407f9508773e64b1e
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: a60c866c70d027b7eec7af18a43fa2aab3554b6f

--- lib/libc/minimal/include/ctype.h
+++ lib/libc/minimal/include/ctype.h
@@ -1,6 +1,6 @@
 DECL|__INC_ctype_h__|macro|__INC_ctype_h__
 DECL|isalpha|function|static inline int isalpha(int c)
-DECL|isdigit|function|static inline int isdigit(int a) {
-DECL|isspace|function|static inline int isspace(int c) {
+DECL|isdigit|function|static inline int isdigit(int a)
+DECL|isspace|function|static inline int isspace(int c)
 DECL|isupper|function|static inline int isupper(int a)
 DECL|tolower|function|static inline int tolower(int chr)
