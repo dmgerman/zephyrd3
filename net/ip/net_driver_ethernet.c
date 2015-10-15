2fe8cb3154063f4e80e59dc5a3df10841bf562a7
Michael LeMay <michael.lemay@intel.com>
2015-10-14 21:52:49 -0700

    net: Add Ethernet support to the network stack
    This patch adds support for Ethernet to the network stack, which
    entails relying on the Ethernet device driver to set the MAC address,
    invoking ARP routines at the appropriate points, configuring the
    Contiki network stack to use the appropriate link-level header length,
    etc.
    
    Change-Id: I153a6f4c83c28ca7129ef5affa1e9a18b0f92913
    Signed-off-by: Michael LeMay <michael.lemay@intel.com>
    
    Former-commit-id: 0927378a59500c048b76798f42864ddaaa3e7471

---
+++ net/ip/net_driver_ethernet.c
@@ -0,0 +1,11 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|net_driver_ethernet_init|function|int net_driver_ethernet_init(void)
+DECL|net_driver_ethernet_is_opened|function|bool net_driver_ethernet_is_opened(void)
+DECL|net_driver_ethernet_open|function|static int net_driver_ethernet_open(void)
+DECL|net_driver_ethernet_recv|function|void net_driver_ethernet_recv(struct net_buf *buf)
+DECL|net_driver_ethernet_register_tx|function|void net_driver_ethernet_register_tx(ethernet_tx_callback cb)
+DECL|net_driver_ethernet_send|function|static int net_driver_ethernet_send(struct net_buf *buf)
+DECL|net_driver_ethernet|variable|net_driver_ethernet
+DECL|opened|variable|opened
+DECL|tx_cb|variable|tx_cb
---
+++ net/ip/net_driver_ethernet.h
@@ -0,0 +1,2 @@
+DECL|ethernet_tx_callback|typedef|typedef int (*ethernet_tx_callback)(struct net_buf *buf);
+DECL|net_driver_ethernet_init|macro|net_driver_ethernet_init
