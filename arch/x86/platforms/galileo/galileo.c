2ecbb6f2df5c58f54678dadefba6ba3eb01695a8
Michael LeMay <michael.lemay@intel.com>
2015-10-14 21:49:56 -0700

    eth: dw: galileo: Provide pre-set MMIO base address and IRQ
    Provide a preconfigured base address for MMIO and a preconfigured IRQ
    pin identifier for the first PCI Ethernet MAC in the Intel Quark X1000
    SoC.
    
    Change-Id: I1b527df6c3b1b65da9f0233464e54157029f04f5
    Signed-off-by: Michael LeMay <michael.lemay@intel.com>
    
    Former-commit-id: ef2a2254e6ae6c156998b86ee07d65fa5fba3383

--- arch/x86/platforms/galileo/galileo.c
+++ arch/x86/platforms/galileo/galileo.c
@@ -9,6 +9,7 @@ DECL|SHARED_IRQ_1_IOAPIC_FLAGS|macro|SHARED_IRQ_1_IOAPIC_FLAGS
 DECL|bluetooth_irq_set|function|static int bluetooth_irq_set(struct device *unused)
 DECL|console_irq_set|function|static int console_irq_set(struct device *unused)
 DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
+DECL|eth_irq_set_0|function|static int eth_irq_set_0(struct device *unused)
 DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused)
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|pci_legacy_bridge_irq_config|function|static int pci_legacy_bridge_irq_config(struct device *unused)
--- arch/x86/platforms/ia32_pci/ia32_pci.c
+++ arch/x86/platforms/ia32_pci/ia32_pci.c
@@ -1,6 +1,7 @@
 DECL|bluetooth_irq_set|function|static int bluetooth_irq_set(struct device *unused)
 DECL|console_irq_set|function|static int console_irq_set(struct device *unsued)
 DECL|dw_i2c0_irq_set|function|static int dw_i2c0_irq_set(struct device *unused)
+DECL|eth_irq_set_0|function|static int eth_irq_set_0(struct device *unused)
 DECL|gpio_irq_set_0|function|static int gpio_irq_set_0(struct device *unused)
 DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|spi_irq_set_0|function|static int spi_irq_set_0(struct device *unused)
