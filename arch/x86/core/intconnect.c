b75e174e3523dfb20e2d318929519b3d7100c899
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-10-20 09:42:34 -0700

    checkpatch: error - spacing
    Change-Id: Ie051000e3d3f0f5bdc330d0265010c37acb873bd
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: d81d8bcbfad43c02652ea2f4c8066773376faa07

--- arch/x86/core/intconnect.c
+++ arch/x86/core/intconnect.c
@@ -4,7 +4,7 @@ DECL|STUB_PTR|macro|STUB_PTR
 DECL|_IntVecAlloc|function|int _IntVecAlloc(unsigned int priority)
 DECL|_IntVecMarkAllocated|function|void _IntVecMarkAllocated(unsigned int vector)
 DECL|_IntVecMarkFree|function|void _IntVecMarkFree(unsigned int vector)
-DECL|_IntVecSet|function|void _IntVecSet( unsigned int vector, void (*routine)(void *), unsigned int dpl)
+DECL|_IntVecSet|function|void _IntVecSet(unsigned int vector, void (*routine)(void *), unsigned int dpl)
 DECL|_STUB_AVAIL|macro|_STUB_AVAIL
 DECL|_SpuriousIntHandler|variable|_SpuriousIntHandler
 DECL|_SpuriousIntNoErrCodeHandler|variable|_SpuriousIntNoErrCodeHandler
@@ -12,4 +12,4 @@ DECL|_dummy_exception_vector_stub|variable|_dummy_exception_vector_stub
 DECL|_dummy_spurious_interrupt|variable|_dummy_spurious_interrupt
 DECL|_int_stub_alloc|function|static int _int_stub_alloc(void)
 DECL|dynamic_stubs|variable|dynamic_stubs
-DECL|irq_connect|function|int irq_connect( unsigned int irq, unsigned int priority,void (*routine)(void *parameter), void *parameter)
+DECL|irq_connect|function|int irq_connect(unsigned int irq, unsigned int priority,void (*routine)(void *parameter), void *parameter)
--- arch/x86/core/strtask.c
+++ arch/x86/core/strtask.c
@@ -1,2 +1,2 @@
 DECL|SSE_GROUP|macro|SSE_GROUP
-DECL|_StartTaskArch|function|void _StartTaskArch( struct k_task *X, unsigned int *pOpt)
+DECL|_StartTaskArch|function|void _StartTaskArch(struct k_task *X, unsigned int *pOpt)
