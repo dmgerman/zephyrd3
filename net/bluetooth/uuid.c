9adf96a46be362fc0d3eb20011d6d8bd7022c8f1
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-10-21 13:34:21 +0300

    Bluetooth: Make use of bt_uuid_to_str
    This make use of bt_uuid_to_str to when printing UUID values, to make it
    simpler when it is just going to print so the patch introduces a new
    function that does the conversion in place using a static variable.
    
    Change-Id: Idfedf05a5ad201653fff2e01387f046cd5647c83
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 6cb42e1e20eb6af6a00f86294955ac73990e64ce

--- net/bluetooth/uuid.c
+++ net/bluetooth/uuid.c
@@ -1,4 +1,5 @@
 DECL|bt_uuid_cmp|function|int bt_uuid_cmp(const struct bt_uuid *u1, const struct bt_uuid *u2)
+DECL|bt_uuid_str|function|const char *bt_uuid_str(struct bt_uuid *uuid)
 DECL|bt_uuid_to_str|function|void bt_uuid_to_str(const struct bt_uuid *uuid, char *str, size_t len)
 DECL|uuid128_base|variable|uuid128_base
 DECL|uuid128_cmp|function|static int uuid128_cmp(const struct bt_uuid *u1, const struct bt_uuid *u2)
