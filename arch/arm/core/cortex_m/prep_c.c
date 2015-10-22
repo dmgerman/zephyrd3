d6ddf1b169733a0c6c41e41efe0d3cfea6b15f08
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-10-22 18:39:48 -0400

    arm/reset: relocate the vector table when a non-XIP image
    The processor is made aware that the vector table built in the image is
    located at the start of SRAM in the case of a non-XIP image, rather than
    at 0 in as is the case in XIP images.
    
    Change-Id: I40b28ca32daf3e8930f103224766ed4e0ccc88e0
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 4465cf47e9cdccd7772b8a1e16a2b10ceba989ab

--- arch/arm/core/cortex_m/prep_c.c
+++ arch/arm/core/cortex_m/prep_c.c
@@ -2,3 +2,5 @@ DECL|_PrepC|function|void _PrepC(void)
 DECL|bssZero|function|static void bssZero(void)
 DECL|dataCopy|function|static void dataCopy(void)
 DECL|dataCopy|function|static void dataCopy(void)
+DECL|relocate_vector_table|function|static inline void relocate_vector_table(void)
+DECL|relocate_vector_table|function|static inline void relocate_vector_table(void) { /* do nothing */ }
