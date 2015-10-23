6dd1ff056bc99b05b1a4e3c8dd707eafe241e006
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-10-23 16:23:53 -0400

    arm: allow accessing GPRs in ESF by their real name
    The ESF was built using the 'alias' names of the GPRs (a1, lr, pc, etc)
    rather than their 'real' name (rN).
    
    Change-Id: I49cae5e94869a79a3165dc7f2347d8cec39dbf67
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 2ace19ede8e65714e39e9742ed622993ae89e7a6

--- include/arch/arm/cortex_m/exc.h
+++ include/arch/arm/cortex_m/exc.h
@@ -2,11 +2,4 @@ DECL|NANO_ESF|typedef|typedef struct __esf NANO_ESF;
 DECL|_ARCH_ARM_CORTEXM_EXC_H_|macro|_ARCH_ARM_CORTEXM_EXC_H_
 DECL|_ExcExit|variable|_ExcExit
 DECL|__esf|struct|struct __esf {
-DECL|a1|member|uint32_t a1; /* r0 */
-DECL|a2|member|uint32_t a2; /* r1 */
-DECL|a3|member|uint32_t a3; /* r2 */
-DECL|a4|member|uint32_t a4; /* r3 */
-DECL|ip|member|uint32_t ip; /* r12 */
-DECL|lr|member|uint32_t lr; /* r14 */
-DECL|pc|member|uint32_t pc; /* r15 */
 DECL|xpsr|member|uint32_t xpsr;
