52d5c6536905017cc254654c124d345759755f7c
Anas Nashif <anas.nashif@intel.com>
2015-10-27 08:23:31 -0700

    Remove obsolete header files
    lists.h and lists_c.h are not being used anywhere.
    
    Change-Id: I1434c2b5bf4ddda9166069515a7390a179f418a5
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 60052529fe983f173daa58c958e13bf555972272

--- include/misc/lists.h
+++
@@ -1,9 +0,0 @@
-DECL|Elem|struct|struct Elem {
-DECL|List|struct|struct List {
-DECL|TailPrev|member|struct list_elem *TailPrev;
-DECL|_LISTS_H|macro|_LISTS_H
-DECL|head|member|struct list_elem *head;
-DECL|next|member|struct Elem *next;
-DECL|prev|member|struct Elem *prev;
-DECL|priority|member|unsigned int priority;
-DECL|tail|member|struct list_elem *tail;
