b262182e21148c3397f1945a5dc810de33f630b7
Johan Hedberg <johan.hedberg@intel.com>
2015-10-28 10:26:44 +0200

    Bluetooth: uart: Convert to net_buf API
    Convert the code to use the net_buf API instead of the soon to be
    removed bt_buf API.
    
    Change-Id: I8df37d214e1f93ee472cde82c4198d667709ff5a
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 1f01f1da16c3d0c38bd097900f9a19bd2aa2d111

--- drivers/bluetooth/uart.c
+++ drivers/bluetooth/uart.c
@@ -5,14 +5,14 @@ DECL|H4_CMD|macro|H4_CMD
 DECL|H4_EVT|macro|H4_EVT
 DECL|H4_HEADER_SIZE|macro|H4_HEADER_SIZE
 DECL|H4_SCO|macro|H4_SCO
-DECL|bt_uart_acl_recv|function|static struct bt_buf *bt_uart_acl_recv(int *remaining)
+DECL|bt_uart_acl_recv|function|static struct net_buf *bt_uart_acl_recv(int *remaining)
 DECL|bt_uart_discard|function|static size_t bt_uart_discard(struct device *uart, size_t len)
-DECL|bt_uart_evt_recv|function|static struct bt_buf *bt_uart_evt_recv(int *remaining)
+DECL|bt_uart_evt_recv|function|static struct net_buf *bt_uart_evt_recv(int *remaining)
 DECL|bt_uart_init|function|void bt_uart_init(void)
 DECL|bt_uart_isr|function|void bt_uart_isr(void *unused)
 DECL|bt_uart_open|function|static int bt_uart_open(void)
 DECL|bt_uart_read|function|static int bt_uart_read(struct device *uart, uint8_t *buf,size_t len, size_t min)
-DECL|bt_uart_send|function|static int bt_uart_send(struct bt_buf *buf)
+DECL|bt_uart_send|function|static int bt_uart_send(struct net_buf *buf)
 DECL|bt_uart_setup|function|static void bt_uart_setup(struct device *uart, struct uart_init_info *info)
 DECL|drv|variable|drv
 DECL|uart_out|function|static void uart_out(struct device *dev, const uint8_t *data, int size)
