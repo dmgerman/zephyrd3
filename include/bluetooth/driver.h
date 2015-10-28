787d0460ab77a3f3cec1207b2685877725517a59
Johan Hedberg <johan.hedberg@intel.com>
2015-10-28 14:55:53 +0200

    Bluetooth: Split HCI driver ACL/CMD send callback
    This is a necessary step in preparation for removing the buffer type
    information from the buffers.
    
    Change-Id: I29d8fae32aa660416a1c12e87840499c711e659f
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7ff1cbd976a07a3f4390dfac88557c4389573403

--- include/bluetooth/driver.h
+++ include/bluetooth/driver.h
@@ -2,4 +2,5 @@ DECL|__BT_DRIVER_H|macro|__BT_DRIVER_H
 DECL|bt_driver|struct|struct bt_driver {
 DECL|head_reserve|member|size_t head_reserve;
 DECL|open|member|int (*open)(void);
-DECL|send|member|int (*send)(struct net_buf *buf);
+DECL|send_acl|member|int (*send_acl)(struct net_buf *buf);
+DECL|send_cmd|member|int (*send_cmd)(struct net_buf *buf);
