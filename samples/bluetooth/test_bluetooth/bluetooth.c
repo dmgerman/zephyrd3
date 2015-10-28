9d65ad625e260f2a84901a209c121bb3e303a2e6
Johan Hedberg <johan.hedberg@intel.com>
2015-10-28 11:00:40 +0200

    Bluetooth: samples/test_bluetooth: Convert to net_buf API
    Convert the code to use the net_buf API instead of the soon to be
    removed bt_buf API.
    
    Change-Id: I98a0720483d3f364b77fe6dc030c9169aa4ade67
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 48fde83fe5230a6dc5882ddeb2747606697a883c

--- samples/bluetooth/test_bluetooth/bluetooth.c
+++ samples/bluetooth/test_bluetooth/bluetooth.c
@@ -1,6 +1,6 @@
 DECL|EXPECTED_ERROR|macro|EXPECTED_ERROR
 DECL|driver_init|function|static void driver_init(void)
 DECL|driver_open|function|static int driver_open(void)
-DECL|driver_send|function|static int driver_send(struct bt_buf *buf)
+DECL|driver_send|function|static int driver_send(struct net_buf *buf)
 DECL|drv|variable|drv
 DECL|mainloop|function|void mainloop(void)
