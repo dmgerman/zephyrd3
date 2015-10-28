c1dab057b38e21f89f19019754ddc6b8fe8ae0ca
Johan Hedberg <johan.hedberg@intel.com>
2015-10-28 20:24:16 +0200

    Bluetooth: Merge send_acl/cmd back to a single callback
    Now that we have the buffer type enum as part of the HCI driver API we
    can take advantage of it to pass the buffer type information and not
    have to have two separate callbacks.
    
    Change-Id: Ib2ee5b1540e532c9b27903e97660a276c1293fbc
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 5f4daa155128b7f8e595cc0b99b11ae9c9a0c4d1

--- include/bluetooth/driver.h
+++ include/bluetooth/driver.h
@@ -7,5 +7,4 @@ DECL|bt_buf_type|enum|enum bt_buf_type {
 DECL|bt_driver|struct|struct bt_driver {
 DECL|head_reserve|member|size_t head_reserve;
 DECL|open|member|int (*open)(void);
-DECL|send_acl|member|int (*send_acl)(struct net_buf *buf);
-DECL|send_cmd|member|int (*send_cmd)(struct net_buf *buf);
+DECL|send|member|int (*send)(enum bt_buf_type type, struct net_buf *buf);
