796eeb5f209f19aa1bc6e45e9eabe49e49b6f4c8
Johan Hedberg <johan.hedberg@intel.com>
2015-10-28 10:41:10 +0200

    Bluetooth: l2cap: Convert to net_buf API
    Convert the code to use the net_buf API instead of the soon to be
    removed bt_buf API.
    
    Change-Id: I5f376caeb861ac8b815a0a2e235bd188b9e8185b
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 529a12338b93f316a181ec4a858aa1851ef9cb27

--- net/bluetooth/l2cap.c
+++ net/bluetooth/l2cap.c
@@ -5,7 +5,7 @@ DECL|L2CAP_LE_CID_END|macro|L2CAP_LE_CID_END
 DECL|L2CAP_LE_MAX_CREDITS|macro|L2CAP_LE_MAX_CREDITS
 DECL|L2CAP_LE_MIN_MTU|macro|L2CAP_LE_MIN_MTU
 DECL|bt_l2cap_connected|function|void bt_l2cap_connected(struct bt_conn *conn)
-DECL|bt_l2cap_create_pdu|function|struct bt_buf *bt_l2cap_create_pdu(struct bt_conn *conn)
+DECL|bt_l2cap_create_pdu|function|struct net_buf *bt_l2cap_create_pdu(struct bt_conn *conn)
 DECL|bt_l2cap_disconnected|function|void bt_l2cap_disconnected(struct bt_conn *conn)
 DECL|bt_l2cap_encrypt_change|function|void bt_l2cap_encrypt_change(struct bt_conn *conn)
 DECL|bt_l2cap_fixed_chan_register|function|void bt_l2cap_fixed_chan_register(struct bt_l2cap_fixed_chan *chan)
@@ -13,8 +13,8 @@ DECL|bt_l2cap_init|function|int bt_l2cap_init(void)
 DECL|bt_l2cap_lookup_rx_cid|function|struct bt_l2cap_chan *bt_l2cap_lookup_rx_cid(struct bt_conn *conn, uint16_t cid)
 DECL|bt_l2cap_lookup_tx_cid|function|struct bt_l2cap_chan *bt_l2cap_lookup_tx_cid(struct bt_conn *conn, uint16_t cid)
 DECL|bt_l2cap_pool|variable|bt_l2cap_pool
-DECL|bt_l2cap_recv|function|void bt_l2cap_recv(struct bt_conn *conn, struct bt_buf *buf)
-DECL|bt_l2cap_send|function|void bt_l2cap_send(struct bt_conn *conn, uint16_t cid, struct bt_buf *buf)
+DECL|bt_l2cap_recv|function|void bt_l2cap_recv(struct bt_conn *conn, struct net_buf *buf)
+DECL|bt_l2cap_send|function|void bt_l2cap_send(struct bt_conn *conn, uint16_t cid, struct net_buf *buf)
 DECL|bt_l2cap_server_register|function|int bt_l2cap_server_register(struct bt_l2cap_server *server)
 DECL|bt_l2cap_update_conn_param|function|int bt_l2cap_update_conn_param(struct bt_conn *conn)
 DECL|bt_l2cap|struct|struct bt_l2cap {
@@ -28,10 +28,10 @@ DECL|l2cap_chan_alloc_cid|function|static void l2cap_chan_alloc_cid(struct bt_co
 DECL|l2cap_chan_get|function|static struct bt_l2cap *l2cap_chan_get(struct bt_conn *conn)
 DECL|l2cap_connected|function|static void l2cap_connected(struct bt_l2cap_chan *chan)
 DECL|l2cap_disconnected|function|static void l2cap_disconnected(struct bt_l2cap_chan *chan)
-DECL|l2cap_recv|function|static void l2cap_recv(struct bt_l2cap_chan *chan, struct bt_buf *buf)
+DECL|l2cap_recv|function|static void l2cap_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|l2cap_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_server_lookup_psm(uint16_t psm)
-DECL|le_conn_param_rsp|function|static void le_conn_param_rsp(struct bt_l2cap *l2cap, struct bt_buf *buf)
-DECL|le_conn_param_update_req|function|static void le_conn_param_update_req(struct bt_l2cap *l2cap, uint8_t ident, struct bt_buf *buf)
-DECL|le_conn_req|function|static void le_conn_req(struct bt_l2cap *l2cap, uint8_t ident,struct bt_buf *buf)
+DECL|le_conn_param_rsp|function|static void le_conn_param_rsp(struct bt_l2cap *l2cap, struct net_buf *buf)
+DECL|le_conn_param_update_req|function|static void le_conn_param_update_req(struct bt_l2cap *l2cap, uint8_t ident, struct net_buf *buf)
+DECL|le_conn_req|function|static void le_conn_req(struct bt_l2cap *l2cap, uint8_t ident,struct net_buf *buf)
 DECL|rej_not_understood|function|static void rej_not_understood(struct bt_conn *conn, uint8_t ident)
 DECL|servers|variable|servers
