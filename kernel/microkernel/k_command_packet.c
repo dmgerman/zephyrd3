c8c2bd8d0d75b3bed54257a6ddca0e7c4b7d3d31
Allan Stephens <allan.stephens@windriver.com>
2015-10-29 08:47:46 -0400

    kernel: Eliminate the need for command packet sets
    Revises microkernel semaphore sub-system to allow ISRs and fibers
    to give a semaphore without having to define a command packet set.
    The microkernel server now supports a 3rd command type on its
    command stack, allowing a semaphore to be given in a similar manner
    to the one used for the existing "give event" command type.
    
    Change-Id: Ibd7fb1a77949792f72acd20a9ee304d6eabd62f7
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 7c4b9ce1f75a6d4cd17383f051ee0156ee6a8ff6

--- kernel/microkernel/k_command_packet.c
+++ kernel/microkernel/k_command_packet.c
@@ -1,3 +1,2 @@
-DECL|_cmd_pkt_get|function|struct k_args *_cmd_pkt_get(struct cmd_pkt_set *pSet)
 DECL|_k_task_call|function|void _k_task_call(struct k_args *cmd_packet)
 DECL|_k_test_cmd_pkt_size|variable|_k_test_cmd_pkt_size
