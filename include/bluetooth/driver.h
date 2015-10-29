81bdf3de0c36c76898d97f2055b8ca90a9cba77e
Johan Hedberg <johan.hedberg@intel.com>
2015-10-29 10:18:44 +0200

    Bluetooth: Differentiate send/recv of needed driver headroom
    The space that the HCI driver needs to reserve for its own use is not
    necessarily symmetrical. E.g. even with the current H4 driver we only
    need to reserve space for sending data but not for receiving it.
    Keeping track of these values independently enables more efficient use
    of the buffers.
    
    Change-Id: I64917b545c5cd77356ed038d09afe76422334661
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 4b433d4fa656a1f90aa99a9a58f54062f8851ac5

--- include/bluetooth/driver.h
+++ include/bluetooth/driver.h
@@ -5,6 +5,7 @@ DECL|BT_EVT|enumerator|BT_EVT, /** HCI event */
 DECL|__BT_DRIVER_H|macro|__BT_DRIVER_H
 DECL|bt_buf_type|enum|enum bt_buf_type {
 DECL|bt_driver|struct|struct bt_driver {
-DECL|head_reserve|member|size_t head_reserve;
 DECL|open|member|int (*open)(void);
+DECL|recv_reserve|member|size_t recv_reserve;
+DECL|send_reserve|member|size_t send_reserve;
 DECL|send|member|int (*send)(enum bt_buf_type type, struct net_buf *buf);
