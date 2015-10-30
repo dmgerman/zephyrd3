3aaa282797e795a729dd610c9f337b016c42f266
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-10-30 12:38:24 +0200

    net: contiki: Add debug function to print buffer content
    Change-Id: I04c76798394651f9026d869e3a9491c4e452b396
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 0b9736dce4d077ffc89cdd4cf0034d47e6cffb79

--- net/ip/contiki/ip/uip-debug.c
+++ net/ip/contiki/ip/uip-debug.c
@@ -1,2 +1,3 @@
+DECL|uip_debug_hex_dump|function|uip_debug_hex_dump(const unsigned char *buffer, int len)
 DECL|uip_debug_ipaddr_print|function|uip_debug_ipaddr_print(const uip_ipaddr_t *addr)
 DECL|uip_debug_lladdr_print|function|uip_debug_lladdr_print(const uip_lladdr_t *addr)
