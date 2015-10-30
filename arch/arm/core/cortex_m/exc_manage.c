889dd3e4d6b9620f057844042813204b6df8fec6
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-10-30 14:29:49 -0400

    arm: add connecting exceptions at runtime
    Add sys_exc_connect() (and its x86-compatible alias nanoCpuExcConnect())
    that allows connecting an exception handler at runtime.
    
    The current implementation is a bit of a bastard, to avoid disturbing
    the current implementation of the exception handlers. Instead of hooking
    _exc_wrapper() in all vectors and adapting the exception handlers, the
    current exception handlers are still hooked directly in the vectors.
    When an exception is hooked at runtime, _exc_wrapper() gets installed in
    the vector and the real handler gets inserted in _sw_exc_table; this
    means that the scheme only works with non-XIP kernels.
    
    This should be enhanced so that _exc_wrapper() is hooked in all vectors,
    and that current exception handlers (for faults mostly) are reworked to
    be inserted in the _sw_exc_table and wrapped in _exc_wrapper().
    
    Change-Id: Icaa14f4835b57873d2905b7fbcbb94eeb3b247d1
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: bd94d9825540a67555691ae7fc7d0741ea127050

---
+++ arch/arm/core/cortex_m/exc_manage.c
@@ -0,0 +1,4 @@
+DECL|__noinit|variable|__noinit
+DECL|exc_can_be_connected|function|static inline int exc_can_be_connected(int num)
+DECL|sys_exc_connect|function|void sys_exc_connect(unsigned int num, sys_exc_handler_t *handler, void *unused)
+DECL|sys_exc_esf_dump|function|void sys_exc_esf_dump(NANO_ESF *esf)
--- include/arch/arm/cortex_m/exc.h
+++ include/arch/arm/cortex_m/exc.h
@@ -2,4 +2,6 @@ DECL|NANO_ESF|typedef|typedef struct __esf NANO_ESF;
 DECL|_ARCH_ARM_CORTEXM_EXC_H_|macro|_ARCH_ARM_CORTEXM_EXC_H_
 DECL|_ExcExit|variable|_ExcExit
 DECL|__esf|struct|struct __esf {
+DECL|sys_exc_handler_sig|macro|sys_exc_handler_sig
+DECL|sys_exc_handler_t|typedef|typedef sys_exc_handler_sig(sys_exc_handler_t);
 DECL|xpsr|member|uint32_t xpsr;
