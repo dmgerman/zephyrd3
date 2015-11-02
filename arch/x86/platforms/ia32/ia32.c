73ece5a5d19265d3db2d3561c9ac6b72e1dd470c
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2015-11-02 18:06:08 -0500

    irq: Add flags to IRQ_CONNECT_STATIC() macro and irq_connect() function
    Flags allow passing IRQ triggering option for x86 architecture.
    Each platform defines flags for a particular device and then
    device driver uses them when registers the interrupt handler.
    
    The change in API means that device drivers and sample
    applications need to use the new API.
    
    IRQ triggering configuration is now handled by device drivers
    by using flags passed to interrupt registering API:
    IRQ_CONNECT_STATIC() or irq_connect()
    
    Change-Id: Ibc4312ea2b4032a2efc5b913c6389f780a2a11d1
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: f1420515a787bd1448ae536dd091ce0b862268e9

--- arch/x86/platforms/ia32/ia32.c
+++ arch/x86/platforms/ia32/ia32.c
@@ -1,4 +1,1 @@
-DECL|bluetooth_irq_set|function|static int bluetooth_irq_set(struct device *unused)
-DECL|console_irq_set|function|static int console_irq_set(struct device *unused)
-DECL|hpet_irq_set|function|static int hpet_irq_set(struct device *unused)
 DECL|ia32_init|function|static int ia32_init(struct device *arg)
--- drivers/interrupt_controller/system_apic.c
+++ drivers/interrupt_controller/system_apic.c
@@ -1,4 +1,4 @@
-DECL|_SysIntVecAlloc|function|int _SysIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority /* get vector from <priority> group */ )
-DECL|_SysIntVecProgram|function|void _SysIntVecProgram(unsigned int vector, unsigned int irq)
+DECL|_SysIntVecAlloc|function|int _SysIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority, /* get vector from <priority> group */ uint32_t flags /* interrupt flags */ )
+DECL|_SysIntVecProgram|function|void _SysIntVecProgram(unsigned int vector, unsigned int irq, uint32_t flags)
 DECL|irq_disable|function|void irq_disable(unsigned int irq)
 DECL|irq_enable|function|void irq_enable(unsigned int irq)
