49717e79a3cf7a94a77438cba5593784c3e9a4eb
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2015-11-03 12:19:01 +0100

    Bluetooth: Refactor bt_conn struct
    Isolates existing LE connection specific attributes and
    prepares for now empty BR/EDR specific connection internals.
    
    Change-Id: Ib423306fbf60e8451998e3933aee00cb672a1106
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 555d0d2594804edb832c4f0dbbc2f119dddc77b1

--- net/bluetooth/conn_internal.h
+++ net/bluetooth/conn_internal.h
@@ -4,17 +4,21 @@ DECL|BT_CONN_CONNECT_SCAN|enumerator|BT_CONN_CONNECT_SCAN,
 DECL|BT_CONN_CONNECT|enumerator|BT_CONN_CONNECT,
 DECL|BT_CONN_DISCONNECTED|enumerator|BT_CONN_DISCONNECTED,
 DECL|BT_CONN_DISCONNECT|enumerator|BT_CONN_DISCONNECT,
+DECL|br|member|struct bt_conn_br br;
+DECL|bt_conn_br|struct|struct bt_conn_br {
+DECL|bt_conn_le|struct|struct bt_conn_le {
 DECL|bt_conn_state_t|typedef|} bt_conn_state_t;
 DECL|bt_conn|struct|struct bt_conn {
 DECL|channels|member|void *channels;
+DECL|conn_interval|member|uint8_t conn_interval;
 DECL|dst|member|bt_addr_le_t dst;
 DECL|encrypt|member|uint8_t encrypt;
+DECL|features|member|uint8_t features[8];
 DECL|flags|member|atomic_t flags[1];
 DECL|handle|member|uint16_t handle;
 DECL|init_addr|member|bt_addr_le_t init_addr;
 DECL|keys|member|struct bt_keys *keys;
-DECL|le_conn_interval|member|uint8_t le_conn_interval;
-DECL|le_features|member|uint8_t le_features[8];
+DECL|le|member|struct bt_conn_le le;
 DECL|pending_pkts|member|uint8_t pending_pkts;
 DECL|ref|member|atomic_t ref;
 DECL|required_sec_level|member|bt_security_t required_sec_level;
