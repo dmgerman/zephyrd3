c8665b6911e009dd92d27d351444c31ead0a0e12
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2015-11-03 11:19:19 +0100

    Bluetooth: Refactor bt_dev struct
    Groups LE stack specific values.
    
    Change-Id: Ia947d25c105f518287293b4f870466daa416385c
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 99bd06deaaccd764b68d119f5c35ae77427f03b9

--- net/bluetooth/hci_core.h
+++ net/bluetooth/hci_core.h
@@ -16,23 +16,25 @@ DECL|bt_addr_le_cmp|function|static inline int bt_addr_le_cmp(const bt_addr_le_t
 DECL|bt_addr_le_copy|function|static inline void bt_addr_le_copy(bt_addr_le_t *dst, const bt_addr_le_t *src)
 DECL|bt_addr_le_is_identity|function|static inline bool bt_addr_le_is_identity(const bt_addr_le_t *addr)
 DECL|bt_addr_le_is_rpa|function|static inline bool bt_addr_le_is_rpa(const bt_addr_le_t *addr)
+DECL|bt_dev_le|struct|struct bt_dev_le {
 DECL|bt_dev|struct|struct bt_dev {
 DECL|bt_le_conn_params_valid|function|static inline bool bt_le_conn_params_valid(uint16_t min, uint16_t max,uint16_t latency, uint16_t timeout)
 DECL|cmd_tx_queue|member|struct nano_fifo cmd_tx_queue;
 DECL|drv|member|struct bt_driver *drv;
 DECL|features|member|uint8_t features[8];
+DECL|features|member|uint8_t features[8];
 DECL|flags|member|atomic_t flags[1];
 DECL|hci_revision|member|uint16_t hci_revision;
 DECL|hci_version|member|uint8_t hci_version;
-DECL|le_features|member|uint8_t le_features[8];
-DECL|le_mtu|member|uint16_t le_mtu;
-DECL|le_pkts_sem|member|struct nano_sem le_pkts_sem;
-DECL|le_pkts|member|uint8_t le_pkts;
+DECL|le|member|struct bt_dev_le le;
 DECL|lmp_bredr_capable|macro|lmp_bredr_capable
 DECL|lmp_le_capable|macro|lmp_le_capable
 DECL|manufacturer|member|uint16_t manufacturer;
+DECL|mtu|member|uint16_t mtu;
 DECL|ncmd_sem|member|struct nano_sem ncmd_sem;
 DECL|ncmd|member|uint8_t ncmd;
+DECL|pkts_sem|member|struct nano_sem pkts_sem;
+DECL|pkts|member|uint8_t pkts;
 DECL|rx_prio_queue|member|struct nano_fifo rx_prio_queue;
 DECL|rx_queue|member|struct nano_fifo rx_queue;
 DECL|sent_cmd|member|struct net_buf *sent_cmd;
