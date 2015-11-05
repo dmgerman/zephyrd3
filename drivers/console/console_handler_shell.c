861cc45af80dde4db666304ade71411f9bf2c6ba
Anas Nashif <anas.nashif@intel.com>
2015-11-05 08:03:24 -0500

    shell: prefix types with shell_
    To be consistent across the API.
    
    Change-Id: I13081ac7d67a7bd2095925239f4431db96cd6242
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 48fad1d2cd4f4d565945de953b7e59b434f84a8f

--- drivers/console/console_handler_shell.c
+++ drivers/console/console_handler_shell.c
@@ -6,12 +6,12 @@ DECL|avail_queue|variable|avail_queue
 DECL|buf|variable|buf
 DECL|cmds_queue|variable|cmds_queue
 DECL|commands|variable|commands
-DECL|get_cb|function|static cmd_function_t get_cb(const char *string)
+DECL|get_cb|function|static shell_cmd_function_t get_cb(const char *string)
 DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t size)
 DECL|line_queue_init|function|static void line_queue_init(void)
 DECL|prompt|variable|prompt
 DECL|shell_init|function|void shell_init(const char *str, struct shell_cmd *cmds)
-DECL|shell_register_app_cmd_handler|function|void shell_register_app_cmd_handler(cmd_function_t handler)
+DECL|shell_register_app_cmd_handler|function|void shell_register_app_cmd_handler(shell_cmd_function_t handler)
 DECL|shell|function|static void shell(int arg1, int arg2)
 DECL|show_help|function|static void show_help(int argc, char *argv[])
 DECL|stack|variable|stack
--- include/misc/shell.h
+++ include/misc/shell.h
@@ -1,4 +1,4 @@
-DECL|cb|member|cmd_function_t cb;
-DECL|cmd_function_t|typedef|typedef void (*cmd_function_t)(int argc, char *argv[]);
+DECL|cb|member|shell_cmd_function_t cb;
 DECL|cmd_name|member|const char *cmd_name;
+DECL|shell_cmd_function_t|typedef|typedef void (*shell_cmd_function_t)(int argc, char *argv[]);
 DECL|shell_cmd|struct|struct shell_cmd {
