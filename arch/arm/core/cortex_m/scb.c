fca8672edbfb87a3c97146b7ba7f0035ff960864
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-11-05 18:26:01 -0500

    arm: add software reboot for QEMU
    This simulates a reboot by jumping back to the address stored in the
    reset vector in vector table found at address 0. It is supported from
    interrupt/exception level, which means that sys_arch_reboot() in this
    case finds out if it is called from thread mode or handler mode, and in
    the latter case, it unwinds the nested exception stack as needed.
    
    Change-Id: Ib67f850f8411f1ee8fc592a5f31f2f70d0af14a4
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: c5bb958db4665e9ec724a226bb0f13991a5d4e93

--- arch/arm/core/cortex_m/scb.c
+++ arch/arm/core/cortex_m/scb.c
@@ -1,3 +1,7 @@
+DECL|DO_REBOOT|macro|DO_REBOOT
+DECL|DO_REBOOT|macro|DO_REBOOT
 DECL|SCB_AIRCR_VECTKEY_EN_W|macro|SCB_AIRCR_VECTKEY_EN_W
 DECL|_ScbNumPriGroupSet|function|void _ScbNumPriGroupSet(unsigned int n)
+DECL|reboot_through_sysresetreq|function|static void reboot_through_sysresetreq(void)
+DECL|software_reboot|function|static void software_reboot(void)
 DECL|sys_arch_reboot|function|void sys_arch_reboot(int type)
