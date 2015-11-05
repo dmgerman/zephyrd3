929218aec25c22d219af881c084d6bb435a1441f
Anas Nashif <anas.nashif@intel.com>
2015-11-04 22:45:09 -0500

    shell: add sample application
    [QEMU] CPU: qemu32
    shell> ping
    pong
    shell> ticks
    ticks: 481
    shell> highticks
    highticks: 750736288
    shell> help
    Available commands:
    help
    ping
    ticks
    highticks
    
    Change-Id: Ie8e9d27d83bf944a8b8800f7391c6246be769875
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: a297b2f61c867f0b9971317b86ead0f83b2ae47b

---
+++ samples/nanokernel/apps/shell/src/main.c
@@ -0,0 +1,6 @@
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|commands|variable|commands
+DECL|main|function|void main(void)
+DECL|shell_cmd_highticks|function|static void shell_cmd_highticks(int argc, char *argv[])
+DECL|shell_cmd_ping|function|static void shell_cmd_ping(int argc, char *argv[])
+DECL|shell_cmd_ticks|function|static void shell_cmd_ticks(int argc, char *argv[])
