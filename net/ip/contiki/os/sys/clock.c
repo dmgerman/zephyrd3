ed81d5583f335b083901fcce86b874d86633bbe0
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-11-09 12:09:25 +0200

    net: contiki: Add clock function to busy wait a timeout
    Add a function that allows the code to busy wait usec timeout.
    
    Change-Id: I6c0bbe33a98d45c58667e1400e0ebcdc59d28bad
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b148130cdb88de607b14aeca54ca2559474204ae

--- net/ip/contiki/os/sys/clock.c
+++ net/ip/contiki/os/sys/clock.c
@@ -1,6 +1,8 @@
 DECL|DEBUG|macro|DEBUG
 DECL|UIP_LOG|macro|UIP_LOG
 DECL|UIP_LOG|macro|UIP_LOG
+DECL|USEC_TO_CYCLES|macro|USEC_TO_CYCLES
+DECL|clock_delay_usec_busywait|function|void clock_delay_usec_busywait(uint32_t dt)
 DECL|clock_delay|function|void clock_delay(unsigned int d)
 DECL|clock_init|function|void clock_init(void)
 DECL|clock_seconds|function|unsigned long clock_seconds(void)
--- net/ip/contiki/os/sys/clock.h
+++ net/ip/contiki/os/sys/clock.h
@@ -1,3 +1,6 @@
+DECL|CLOCK_CYCLE_LT|macro|CLOCK_CYCLE_LT
 DECL|CLOCK_H_|macro|CLOCK_H_
+DECL|CLOCK_MSEC_TO_CYCLES|macro|CLOCK_MSEC_TO_CYCLES
 DECL|CLOCK_SECOND|macro|CLOCK_SECOND
 DECL|CLOCK_SECOND|macro|CLOCK_SECOND
+DECL|clock_get_cycle|function|static inline uint32_t clock_get_cycle() { return nano_cycle_get_32(); }
