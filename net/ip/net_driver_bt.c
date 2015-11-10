3934b6bf5bb4c8c68fe48cb95aad744e18c396bf
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-11-10 09:52:32 +0200

    net: Network driver for Bluetooth 6LoWPAN connections
    This driver handles IPv6 traffic over Bluetooth LE connection.
    The packet fragmentation is done inside BT stack, so this
    driver only calls compression function.
    
    Change-Id: Ib7f2356a1e88e78502239e54ef6e41f1f24ccd67
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 64662703af7aa7cd40603c3985b0166d05b421e3

---
+++ net/ip/net_driver_bt.c
@@ -0,0 +1,19 @@
+DECL|L2CAP_IPSP_MTU|macro|L2CAP_IPSP_MTU
+DECL|L2CAP_IPSP_PSM|macro|L2CAP_IPSP_PSM
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|attrs|variable|attrs
+DECL|ipsp_accept|function|static int ipsp_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
+DECL|ipsp_alloc_buf|function|static struct net_buf *ipsp_alloc_buf(struct bt_l2cap_chan *chan)
+DECL|ipsp_chan|variable|ipsp_chan
+DECL|ipsp_connected|function|static void ipsp_connected(struct bt_l2cap_chan *chan)
+DECL|ipsp_disconnected|function|static void ipsp_disconnected(struct bt_l2cap_chan *chan)
+DECL|ipsp_ops|variable|ipsp_ops
+DECL|ipsp_recv|function|static void ipsp_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
+DECL|ipss_uuid|variable|ipss_uuid
+DECL|memswap|function|static inline void memswap(void *dst, const void *src, int len)
+DECL|net_driver_bt_init|function|int net_driver_bt_init(void)
+DECL|net_driver_bt_open|function|static int net_driver_bt_open(void)
+DECL|net_driver_bt_send|function|static int net_driver_bt_send(struct net_buf *buf)
+DECL|net_driver_bt|variable|net_driver_bt
+DECL|server|variable|server
