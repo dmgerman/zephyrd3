cfe6bc59a53b65aae38f0bbd37d0612263e9e68b
Andrew Boie <andrew.p.boie@intel.com>
2015-11-12 14:43:58 -0800

    x86: reduce footprint of exception debugging
    We're smart, we can look up the vector IDs in a book if we
    don't already know what they are.
    
    Change-Id: Iaff3986d7c96dea597be4b2a5b13721ab57980fa
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: c065ed5d78a689d4f4273dcc668f3ed05e67b23d

--- arch/x86/core/fatal.c
+++ arch/x86/core/fatal.c
@@ -1,4 +1,19 @@
 DECL|EXC_FUNC|macro|EXC_FUNC
+DECL|IV_ALIGNMENT_CHECK|variable|IV_ALIGNMENT_CHECK
+DECL|IV_BOUND_RANGE|variable|IV_BOUND_RANGE
+DECL|IV_DEVICE_NOT_AVAILABLE|variable|IV_DEVICE_NOT_AVAILABLE
+DECL|IV_DIVIDE_ERROR|variable|IV_DIVIDE_ERROR
+DECL|IV_DOUBLE_FAULT|variable|IV_DOUBLE_FAULT
+DECL|IV_GENERAL_PROTECTION|variable|IV_GENERAL_PROTECTION
+DECL|IV_INVALID_OPCODE|variable|IV_INVALID_OPCODE
+DECL|IV_INVALID_TSS|variable|IV_INVALID_TSS
+DECL|IV_MACHINE_CHECK|variable|IV_MACHINE_CHECK
+DECL|IV_NON_MASKABLE_INTERRUPT|variable|IV_NON_MASKABLE_INTERRUPT
+DECL|IV_OVERFLOW|variable|IV_OVERFLOW
+DECL|IV_PAGE_FAULT|variable|IV_PAGE_FAULT
+DECL|IV_SEGMENT_NOT_PRESENT|variable|IV_SEGMENT_NOT_PRESENT
+DECL|IV_STACK_FAULT|variable|IV_STACK_FAULT
+DECL|IV_X87_FPU_FP_ERROR|variable|IV_X87_FPU_FP_ERROR
 DECL|_NanoFatalErrorHandler|function|FUNC_NORETURN void _NanoFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|_default_esf|variable|_default_esf
-DECL|generic_exc_handle|function|static FUNC_NORETURN void generic_exc_handle(char *description, unsigned int vector, const NANO_ESF *pEsf)
+DECL|generic_exc_handle|function|static FUNC_NORETURN void generic_exc_handle(unsigned int vector, const NANO_ESF *pEsf)
