cbb4dce9f421f2bfbceb9ad517d6bb3b16e398d5
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2015-11-16 10:20:07 +0200

    Bluetooth: Rename __unused to compile with newlib
    __unused is define in sys/cdefs.h when building against newlib.
    
    Change-Id: I0fad81dc91e1eb91368b5b3370e3fbb0fb7ba4a9
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 71a19117d684daca89270fc8184f4671dfcdc039

--- include/drivers/console/uart_console.h
+++ include/drivers/console/uart_console.h
@@ -1,5 +1,5 @@
 DECL|MAX_LINE_LEN|macro|MAX_LINE_LEN
 DECL|_UART_CONSOLE__H_|macro|_UART_CONSOLE__H_
-DECL|__unused|member|int __unused;
+DECL|_unused|member|int _unused;
 DECL|line|member|char line[MAX_LINE_LEN];
 DECL|uart_console_input|struct|struct uart_console_input {
