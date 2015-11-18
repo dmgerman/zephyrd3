c9ebd146b7eee7ce0a78f02df1b973102473ecc3
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2015-11-18 12:31:10 +0200

    net: contiki: export ctimer_process so it can be called
    We need to export ctimer_process so that net_core.c can
    call it and let it set the corresponding event timers.
    Without this the timers would not be called properly.
    
    Change-Id: I623b60420eacfe326f6b18d89f3e008cc56379fc
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9e675cf7c62f89403a7a68fcdc0048fbd6794873

--- net/ip/contiki/os/sys/ctimer.h
+++ net/ip/contiki/os/sys/ctimer.h
@@ -1,5 +1,6 @@
 DECL|CTIMER_H_|macro|CTIMER_H_
 DECL|buf|member|struct net_buf *buf;
+DECL|ctimer_process|variable|ctimer_process
 DECL|ctimer|struct|struct ctimer {
 DECL|etimer|member|struct etimer etimer;
 DECL|f|member|void (*f)(struct net_buf *, void *);
