4172f386a249c290c4bee8f5b71e4fbf585fae86
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2015-11-21 21:28:21 -0500

    spi: Add support for Designware SPI controller
    Such controller is found on Quark SE Lakemont and ARC cores. This
    driver currently supports the Lakemont core (x86).
    
    Change-Id: Iefebd6ce9dbe81aa3902e7c2d801b07c027c548a
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: f7a6c5c263f94c5b452b92fc5de75f8bffabdf6c

---
+++ drivers/spi/dw_spi.c
@@ -0,0 +1,31 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DEFINE_CLEAR_BIT_OP|macro|DEFINE_CLEAR_BIT_OP
+DECL|DEFINE_MM_REG_READ|macro|DEFINE_MM_REG_READ
+DECL|DEFINE_MM_REG_WRITE|macro|DEFINE_MM_REG_WRITE
+DECL|DEFINE_SET_BIT_OP|macro|DEFINE_SET_BIT_OP
+DECL|DEFINE_TEST_BIT_OP|macro|DEFINE_TEST_BIT_OP
+DECL|_clock_config|function|static inline void _clock_config(struct device *dev)
+DECL|_clock_off|function|static inline void _clock_off(struct device *dev)
+DECL|_clock_on|function|static inline void _clock_on(struct device *dev)
+DECL|completed|function|static void completed(struct device *dev, int error)
+DECL|dw_spi_api|variable|dw_spi_api
+DECL|int_unmask|macro|int_unmask
+DECL|pull_data|function|static void pull_data(struct device *dev)
+DECL|push_data|function|static void push_data(struct device *dev)
+DECL|spi_config_0_irq|function|void spi_config_0_irq(struct device *dev)
+DECL|spi_config_1_irq|function|void spi_config_1_irq(struct device *dev)
+DECL|spi_dw_config_0|variable|spi_dw_config_0
+DECL|spi_dw_config_1|variable|spi_dw_config_1
+DECL|spi_dw_configure|function|static int spi_dw_configure(struct device *dev,struct spi_config *config, void *user_data)
+DECL|spi_dw_data_port_0|variable|spi_dw_data_port_0
+DECL|spi_dw_data_port_1|variable|spi_dw_data_port_1
+DECL|spi_dw_init|function|int spi_dw_init(struct device *dev)
+DECL|spi_dw_isr_port_0|variable|spi_dw_isr_port_0
+DECL|spi_dw_isr_port_1|variable|spi_dw_isr_port_1
+DECL|spi_dw_isr|function|void spi_dw_isr(void *arg)
+DECL|spi_dw_resume|function|static int spi_dw_resume(struct device *dev)
+DECL|spi_dw_slave_select|function|static int spi_dw_slave_select(struct device *dev, uint32_t slave)
+DECL|spi_dw_suspend|function|static int spi_dw_suspend(struct device *dev)
+DECL|spi_dw_transceive|function|static int spi_dw_transceive(struct device *dev, uint8_t *tx_buf, uint32_t tx_buf_len, uint8_t *rx_buf, uint32_t rx_buf_len)
