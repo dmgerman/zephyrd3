dcfd205f83aa66805144875caead976df31de6c6
Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
2015-11-24 17:02:50 -0600

    arc: static IRQ handling
    Adds support for static IRQ handler initialization.
    
    Currently, IRQ_CONNECT and IRQ_CONFIG macros are emulating static
    behavior through dynamic initialization.
    This commit updates the macros to get real static initialization.
    IRQ handlers must be assigned at build time.
    
    Change-Id: Ia07fb25a5e4dae489f84ffcedb28007ee18a3b82
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
    
    Former-commit-id: a894799c94b8dac562b6d9465ddb39c27a3f3563

--- include/arch/arc/arch.h
+++ include/arch/arc/arch.h
@@ -4,8 +4,3 @@ DECL|OCTET_TO_SIZEOFUNIT|macro|OCTET_TO_SIZEOFUNIT
 DECL|SIZEOFUNIT_TO_OCTET|macro|SIZEOFUNIT_TO_OCTET
 DECL|STACK_ALIGN|macro|STACK_ALIGN
 DECL|_ARC_ARCH__H_|macro|_ARC_ARCH__H_
-DECL|irq_desc|struct|struct irq_desc {
-DECL|irq|member|unsigned int irq;
-DECL|isr|member|void (*isr)(void *);
-DECL|parameter|member|void *parameter;
-DECL|priority|member|unsigned int priority;
