0f102a743b08c3ce161cbfa62c2a3bce0ef77a58
Peter Mitsis <peter.mitsis@windriver.com>
2015-11-24 15:11:35 -0500

    nanokernel: Rename nano_tick_xxx APIs
    Renames the following nanokernel tick APIs.
    	nano_tick_get()      -> sys_tick_get()
    	nano_tick_get_32()   -> sys_tick_get_32()
    	nano_tick_delta()    -> sys_tick_delta()
    	nano_tick_delta_32() -> sys_tick_delta_32()
    
    Change-Id: Ie969545335d76df94b4e2d200fef86a93596f5e8
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: c40e84c57a1d86b6d061b7ea275e295280ae0c42

--- kernel/nanokernel/nano_sys_clock.c
+++ kernel/nanokernel/nano_sys_clock.c
@@ -9,13 +9,13 @@ DECL|handle_expired_nano_timeouts|function|static inline void handle_expired_nan
 DECL|handle_expired_nano_timeouts|macro|handle_expired_nano_timeouts
 DECL|handle_expired_nano_timers|function|static inline void handle_expired_nano_timers(int ticks)
 DECL|handle_expired_nano_timers|macro|handle_expired_nano_timers
-DECL|nano_tick_delta_32|function|uint32_t nano_tick_delta_32(int64_t *reftime)
-DECL|nano_tick_delta|function|int64_t nano_tick_delta(int64_t *reftime)
-DECL|nano_tick_get_32|function|uint32_t nano_tick_get_32(void)
-DECL|nano_tick_get|function|int64_t nano_tick_get(void)
 DECL|sys_clock_hw_cycles_per_sec|variable|sys_clock_hw_cycles_per_sec
 DECL|sys_clock_hw_cycles_per_sec|variable|sys_clock_hw_cycles_per_sec
 DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
+DECL|sys_tick_delta_32|function|uint32_t sys_tick_delta_32(int64_t *reftime)
+DECL|sys_tick_delta|function|int64_t sys_tick_delta(int64_t *reftime)
+DECL|sys_tick_get_32|function|uint32_t sys_tick_get_32(void)
+DECL|sys_tick_get|function|int64_t sys_tick_get(void)
