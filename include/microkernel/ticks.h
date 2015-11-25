73288325158cdc4bc498dc0b9fe9537c4130eee8
Peter Mitsis <peter.mitsis@windriver.com>
2015-11-25 15:43:03 -0500

    timer: Remove deprecated routines
    Removes the following deprecated routines:
    	nano_cycle_get_32()
    	task_cycle_get_32()
    	fiber_cycle_get_32()
    	isr_cycle_get_32()
    
    Those routines have all been replaced by sys_cycle_get_32().
    
    Change-Id: I8709952633bb87c8963e88caffe1036fb9add527
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 53af19050dcf9022f7ea75de7c6634964fe9ad18

--- include/microkernel/ticks.h
+++ include/microkernel/ticks.h
@@ -1,4 +1,2 @@
 DECL|TICKS_H|macro|TICKS_H
-DECL|fiber_cycle_get_32|macro|fiber_cycle_get_32
-DECL|isr_cycle_get_32|macro|isr_cycle_get_32
 DECL|task_timer_restart|function|static inline void task_timer_restart(ktimer_t timer, int32_t duration,int32_t period)
