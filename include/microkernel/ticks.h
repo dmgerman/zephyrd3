ef0e62272bf4b34595376ec62b8aa5652457b636
Peter Mitsis <peter.mitsis@windriver.com>
2015-11-25 11:08:50 -0500

    Remove fiber/isr_tick_get[_32]() APIs
    Removes the following context based APIs:
    	isr_tick_get_()
    	isr_tick_get_32()
    	fiber_tick_get()
    	fiber_tick_get_32()
    The routines sys_tick_get() and sys_tick_get_32() are to be used instead.
    
    Change-Id: I3fa1338f174b10e783bfb00941edadfff16eeaba
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 18cc029e3bf73b610c296f644ee55765144a1f70

--- include/microkernel/ticks.h
+++ include/microkernel/ticks.h
@@ -1,8 +1,4 @@
 DECL|TICKS_H|macro|TICKS_H
 DECL|fiber_cycle_get_32|macro|fiber_cycle_get_32
-DECL|fiber_tick_get_32|macro|fiber_tick_get_32
-DECL|fiber_tick_get|macro|fiber_tick_get
 DECL|isr_cycle_get_32|macro|isr_cycle_get_32
-DECL|isr_tick_get_32|macro|isr_tick_get_32
-DECL|isr_tick_get|macro|isr_tick_get
 DECL|task_timer_restart|function|static inline void task_timer_restart(ktimer_t timer, int32_t duration,int32_t period)
