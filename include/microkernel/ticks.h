7e1da17c46c8ef5eb8d840122bfc44e49745c402
Peter Mitsis <peter.mitsis@windriver.com>
2015-11-25 09:56:44 -0500

    microkernel: Elminate task_tick_xxx() APIs
    Eliminates task_tick_xxx APIs in favor of sys_tick_xxx APIs.
    	task_tick_get()    -> sys_tick_get()
    	task_tick_get_32() -> sys_tick_get_32()
    	task_tick_delta    -> sys_tick_delta()
    	task_tick_delta_32 -> sys_tick_delta_32()
    
    Change-Id: Ie8c2bc114a08f091997faaf68f6fc5536b2ba25d
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: cd9893d563620e8655122c4865c61bd98cc516b8

--- include/microkernel/ticks.h
+++ include/microkernel/ticks.h
@@ -5,5 +5,4 @@ DECL|fiber_tick_get|macro|fiber_tick_get
 DECL|isr_cycle_get_32|macro|isr_cycle_get_32
 DECL|isr_tick_get_32|macro|isr_tick_get_32
 DECL|isr_tick_get|macro|isr_tick_get
-DECL|task_tick_delta_32|function|static inline int32_t task_tick_delta_32(int64_t *reftime)
 DECL|task_timer_restart|function|static inline void task_timer_restart(ktimer_t timer, int32_t duration,int32_t period)
