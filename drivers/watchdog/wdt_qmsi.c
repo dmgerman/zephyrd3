398d00bff43900f1844199387fa5dc6068a5ad79
Andre Guedes <andre.guedes@intel.com>
2015-11-27 08:18:37 -0500

    watchdog: Introduce QMSI watchdog driver
    This patch introduces the QMSI watchdog driver which is simply a shim
    driver based on watchdog driver provided by QMSI BSP. This driver has
    been tested with the latest version of QMSI BSP (1.0.0).
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_WATCHDOG=y
    CONFIG_WDT_QMSI=y
    
    Change-Id: Id910fa7f8a0559bcd4746747d8ce4588a0c48589
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 5c2dfcd53a594889c721780834c6adde2c2f993f

---
+++ drivers/watchdog/wdt_qmsi.c
@@ -0,0 +1,11 @@
+DECL|api|variable|api
+DECL|disable|function|static void disable(struct device *dev)
+DECL|enable|function|static void enable(struct device *dev)
+DECL|get_config|function|static void get_config(struct device *dev, struct wdt_config *cfg)
+DECL|init|function|static int init(struct device *dev)
+DECL|interrupt_callback|function|static void interrupt_callback(void)
+DECL|reload|function|static void reload(struct device *dev)
+DECL|set_config|function|static int set_config(struct device *dev, struct wdt_config *cfg)
+DECL|user_cb|variable|user_cb
+DECL|wdt_qmsi_isr_dev|variable|wdt_qmsi_isr_dev
+DECL|wdt_qmsi_isr|function|void wdt_qmsi_isr(void *arg)
