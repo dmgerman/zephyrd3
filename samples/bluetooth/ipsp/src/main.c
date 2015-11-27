f75eda1766b581addc116c1055db4b4ed8185eb4
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-11-27 14:54:44 +0200

    Bluetooth: Add IPSP sample application
    This application uses Bluetooth IP driver to listen to UPD port and
    echo back the data received.
    
    Change-Id: Id8e58f5f8bf58ff5947441cbcb3131c0da27081c
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 6be8a35d054fea4c68ff98fb4ed8935686b94734

---
+++ samples/bluetooth/ipsp/src/main.c
@@ -0,0 +1,28 @@
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|UDP_PORT|macro|UDP_PORT
+DECL|UNKNOWN_APPEARANCE|macro|UNKNOWN_APPEARANCE
+DECL|ad|variable|ad
+DECL|appeareance_uuid|variable|appeareance_uuid
+DECL|attrs|variable|attrs
+DECL|bt_ready|function|static void bt_ready(int err)
+DECL|conn_callbacks|variable|conn_callbacks
+DECL|connected|function|static void connected(struct bt_conn *conn)
+DECL|device_name_uuid|variable|device_name_uuid
+DECL|dis_uuid|variable|dis_uuid
+DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
+DECL|gap_uuid|variable|gap_uuid
+DECL|get_context|function|static inline bool get_context(struct net_context **recv, struct net_context **mcast_recv)
+DECL|listen|function|static void listen(void)
+DECL|mainloop|function|void mainloop(void)
+DECL|manuf_uuid|variable|manuf_uuid
+DECL|model_uuid|variable|model_uuid
+DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const char *type, struct net_buf *buf)
+DECL|read_appearance|function|static int read_appearance(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_manuf|function|static int read_manuf(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_model|function|static int read_model(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_name|function|static int read_name(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|receive_and_reply|function|static inline void receive_and_reply(struct net_context *recv, struct net_context *mcast_recv)
+DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
+DECL|sd|variable|sd
