07080583f03165fc9abd2af113bfd857912ea568
Anas Nashif <anas.nashif@intel.com>
2015-11-30 13:17:54 -0500

    samples: add a sample application using RTC
    This sample creates an alarm and repeats the alarm on every
    interrupt.
    
    Change-Id: I97583e3c5af826c51536f8d244c016e2c628e88a
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: a039c486cb1dd2ab757275ed3af8f3a7ce2bfa87

---
+++ samples/nanokernel/apps/rtc/main.c
@@ -0,0 +1,5 @@
+DECL|ALARM|macro|ALARM
+DECL|RTC_DRIVER|macro|RTC_DRIVER
+DECL|main|function|void main(void)
+DECL|rtc_dev|variable|rtc_dev
+DECL|test_rtc_interrupt_fn|function|void test_rtc_interrupt_fn(void)
