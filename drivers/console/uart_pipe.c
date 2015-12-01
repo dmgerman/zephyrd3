8af16ad88645952c252412814815444fcfdb60ef
Daniel Leung <daniel.leung@intel.com>
2015-12-01 08:42:19 -0800

    uart_pipe: let UART driver do the initialization
    Remove the call to uart_init(), and let the UART drivers take care of
    the port initialization.
    
    Change-Id: Id3e46135ab993cb6596b1fb5339ab1664c65ab40
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    Former-commit-id: 5faca5067acf2493404e10daeea01c9c57478228

--- drivers/console/uart_pipe.c
+++ drivers/console/uart_pipe.c
@@ -6,4 +6,4 @@ DECL|recv_off|variable|recv_off
 DECL|uart_pipe_isr|function|void uart_pipe_isr(void *unused)
 DECL|uart_pipe_register|function|void uart_pipe_register(uint8_t *buf, size_t len, uart_pipe_recv_cb cb)
 DECL|uart_pipe_send|function|int uart_pipe_send(const uint8_t *data, int len)
-DECL|uart_pipe_setup|function|static void uart_pipe_setup(struct device *uart, struct uart_init_info *info)
+DECL|uart_pipe_setup|function|static void uart_pipe_setup(struct device *uart)
