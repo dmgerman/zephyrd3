8481aad3e84f135a08bb97d43ce26200848e4055
Daniel Leung <daniel.leung@intel.com>
2015-12-01 08:42:19 -0800

    serial/stellaris: refactor (again) to conform to driver model
    () Renames stellarisUartDrv.c to uart_stellaris.c. This is to follow
       the driver naming convention.
    () Renames functions stellaris_uart_*() to uart_stellaris_*(),
       following driver naming convention.
    () Renames CONFIG_STELLARIS_UART* to CONFIG_UART_STELLARIS*
    () UART ports initialization is moved into the driver itself.
       All the init code in platform config files is removed.
    () Adds (many) Kconfig options. These don't have to be defined
       in each platform's board.h anymore.
    
    Change-Id: I0eadc3878d69ff24d1637f8df5220fd2f161d24f
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: b541dbdf7797199c18463a4a984e24b56d81a0b1

--- arch/arm/platforms/ti_lm3s6965/platform_config.c
+++ arch/arm/platforms/ti_lm3s6965/platform_config.c
@@ -2,10 +2,4 @@ DECL|RCGC1_UART0_EN|macro|RCGC1_UART0_EN
 DECL|RCGC1_UART1_EN|macro|RCGC1_UART1_EN
 DECL|RCGC1_UART2_EN|macro|RCGC1_UART2_EN
 DECL|RCGC1|macro|RCGC1
-DECL|stellaris_uart0_init|function|static int stellaris_uart0_init(struct device *dev)
-DECL|stellaris_uart1_init|function|static int stellaris_uart1_init(struct device *dev)
-DECL|stellaris_uart2_init|function|static int stellaris_uart2_init(struct device *dev)
-DECL|stellaris_uart_console_init|function|int stellaris_uart_console_init(struct device *dev)
-DECL|stellaris_uart_console_init|function|static int stellaris_uart_console_init(struct device *dev)
-DECL|stellaris_uart_dev_cfg|variable|stellaris_uart_dev_cfg
-DECL|uart_devs|variable|uart_devs
+DECL|uart_stellaris_init|function|static int uart_stellaris_init(struct device *dev)
--- drivers/serial/uart_stellaris.c
+++ drivers/serial/uart_stellaris.c
@@ -84,22 +84,27 @@ DECL|peripd_id7|member|uint32_t peripd_id7;
 DECL|poll_tx_ready|function|static int poll_tx_ready(struct device *dev)
 DECL|ris|member|uint32_t ris;
 DECL|sr|macro|sr
-DECL|stellaris_uart_driver_api|variable|stellaris_uart_driver_api
-DECL|stellaris_uart_driver_api|variable|stellaris_uart_driver_api
-DECL|stellaris_uart_fifo_fill|function|static int stellaris_uart_fifo_fill(struct device *dev, const uint8_t *tx_data, int len)
-DECL|stellaris_uart_fifo_read|function|static int stellaris_uart_fifo_read(struct device *dev, uint8_t *rx_data, const int size)
-DECL|stellaris_uart_irq_err_disable|function|static void stellaris_uart_irq_err_disable(struct device *dev)
-DECL|stellaris_uart_irq_err_enable|function|static void stellaris_uart_irq_err_enable(struct device *dev)
-DECL|stellaris_uart_irq_get|function|static unsigned int stellaris_uart_irq_get(struct device *dev)
-DECL|stellaris_uart_irq_is_pending|function|static int stellaris_uart_irq_is_pending(struct device *dev)
-DECL|stellaris_uart_irq_rx_disable|function|static void stellaris_uart_irq_rx_disable(struct device *dev)
-DECL|stellaris_uart_irq_rx_enable|function|static void stellaris_uart_irq_rx_enable(struct device *dev)
-DECL|stellaris_uart_irq_rx_ready|function|static int stellaris_uart_irq_rx_ready(struct device *dev)
-DECL|stellaris_uart_irq_tx_disable|function|static void stellaris_uart_irq_tx_disable(struct device *dev)
-DECL|stellaris_uart_irq_tx_enable|function|static void stellaris_uart_irq_tx_enable(struct device *dev)
-DECL|stellaris_uart_irq_tx_ready|function|static int stellaris_uart_irq_tx_ready(struct device *dev)
-DECL|stellaris_uart_irq_update|function|static int stellaris_uart_irq_update(struct device *dev)
-DECL|stellaris_uart_poll_in|function|static int stellaris_uart_poll_in(struct device *dev, unsigned char *c)
-DECL|stellaris_uart_poll_out|function|static unsigned char stellaris_uart_poll_out(struct device *dev, unsigned char c)
-DECL|stellaris_uart_port_init|function|void stellaris_uart_port_init(struct device *dev, const struct uart_init_info * const init_info)
 DECL|u1|member|} u1;
+DECL|uart_devs|variable|uart_devs
+DECL|uart_stellaris_dev_cfg_0|variable|uart_stellaris_dev_cfg_0
+DECL|uart_stellaris_dev_cfg_1|variable|uart_stellaris_dev_cfg_1
+DECL|uart_stellaris_dev_cfg_2|variable|uart_stellaris_dev_cfg_2
+DECL|uart_stellaris_driver_api|variable|uart_stellaris_driver_api
+DECL|uart_stellaris_driver_api|variable|uart_stellaris_driver_api
+DECL|uart_stellaris_fifo_fill|function|static int uart_stellaris_fifo_fill(struct device *dev, const uint8_t *tx_data, int len)
+DECL|uart_stellaris_fifo_read|function|static int uart_stellaris_fifo_read(struct device *dev, uint8_t *rx_data, const int size)
+DECL|uart_stellaris_init|function|static int uart_stellaris_init(struct device *dev)
+DECL|uart_stellaris_irq_err_disable|function|static void uart_stellaris_irq_err_disable(struct device *dev)
+DECL|uart_stellaris_irq_err_enable|function|static void uart_stellaris_irq_err_enable(struct device *dev)
+DECL|uart_stellaris_irq_get|function|static unsigned int uart_stellaris_irq_get(struct device *dev)
+DECL|uart_stellaris_irq_is_pending|function|static int uart_stellaris_irq_is_pending(struct device *dev)
+DECL|uart_stellaris_irq_rx_disable|function|static void uart_stellaris_irq_rx_disable(struct device *dev)
+DECL|uart_stellaris_irq_rx_enable|function|static void uart_stellaris_irq_rx_enable(struct device *dev)
+DECL|uart_stellaris_irq_rx_ready|function|static int uart_stellaris_irq_rx_ready(struct device *dev)
+DECL|uart_stellaris_irq_tx_disable|function|static void uart_stellaris_irq_tx_disable(struct device *dev)
+DECL|uart_stellaris_irq_tx_enable|function|static void uart_stellaris_irq_tx_enable(struct device *dev)
+DECL|uart_stellaris_irq_tx_ready|function|static int uart_stellaris_irq_tx_ready(struct device *dev)
+DECL|uart_stellaris_irq_update|function|static int uart_stellaris_irq_update(struct device *dev)
+DECL|uart_stellaris_poll_in|function|static int uart_stellaris_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_stellaris_poll_out|function|static unsigned char uart_stellaris_poll_out(struct device *dev, unsigned char c)
+DECL|uart_stellaris_port_init|function|void uart_stellaris_port_init(struct device *dev, const struct uart_init_info * const init_info)
