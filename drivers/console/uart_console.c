752dcb41a95295ccc6acd87c371b0332b87d9006
Daniel Leung <daniel.leung@intel.com>
2015-12-01 08:42:20 -0800

    console/uart: use device name instead of index
    Use device name to find the UART device for console usage, instead of
    relying on an arbitrary index.
    
    Change-Id: Iebe01c9bf392dfee6d8284367f67647f7d47561a
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 08b4fd431b415ea97ad99cc54fbece4eb1317545

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -1,12 +1,13 @@
 DECL|__printk_hook_install|macro|__printk_hook_install
 DECL|__stdout_hook_install|macro|__stdout_hook_install
 DECL|avail_queue|variable|avail_queue
-DECL|consoleOut|function|static int consoleOut(int c /* character to output */)
 DECL|console_input_init|function|static void console_input_init(void)
 DECL|console_input_init|macro|console_input_init
+DECL|console_out|function|static int console_out(int c)
 DECL|lines_queue|variable|lines_queue
 DECL|pos|variable|pos
 DECL|read_uart|function|static int read_uart(struct device *uart, uint8_t *buf, unsigned int size)
+DECL|uart_console_dev|variable|uart_console_dev
 DECL|uart_console_hook_install|function|void uart_console_hook_install(void)
 DECL|uart_console_init|function|static int uart_console_init(struct device *arg)
 DECL|uart_console_isr|function|void uart_console_isr(void *unused)
