b18c5d05f13866335442fbb1599c79ca3d514c81
Daniel Leung <daniel.leung@intel.com>
2015-12-01 08:42:20 -0800

    uart_pipe: use device name instead of index
    Use device name to find the UART device for uart_pipe usage,
    instead of relying on an arbitrary index.
    
    Change-Id: I36aaa4ed8f0b4905e4e741ca1464947e59f30869
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    Former-commit-id: 5cdd4584fead396ba6abdb46575a00c43258cbb7

--- drivers/console/uart_pipe.c
+++ drivers/console/uart_pipe.c
@@ -1,8 +1,8 @@
-DECL|UART|macro|UART
 DECL|app_cb|variable|app_cb
 DECL|recv_buf_len|variable|recv_buf_len
 DECL|recv_buf|variable|recv_buf
 DECL|recv_off|variable|recv_off
+DECL|uart_pipe_dev|variable|uart_pipe_dev
 DECL|uart_pipe_isr|function|void uart_pipe_isr(void *unused)
 DECL|uart_pipe_register|function|void uart_pipe_register(uint8_t *buf, size_t len, uart_pipe_recv_cb cb)
 DECL|uart_pipe_send|function|int uart_pipe_send(const uint8_t *data, int len)
