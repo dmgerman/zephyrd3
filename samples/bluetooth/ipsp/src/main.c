b1bd4604d95ed48982101cadd9ebc23c9ba6bb2e
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2015-12-01 15:16:02 +0200

    Bluetooth: Make IPSP Application use blocking version of bt_enable
    For test purpose pass NULL to bt_enable so it blocks until init
    sequence is complete.
    
    Change-Id: I875ff9a59bb3d1d2e799db6fc967da3aee3ac77a
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: c3fde0020d865887cbe98ba45ad3dceff921f79c

--- samples/bluetooth/ipsp/src/main.c
+++ samples/bluetooth/ipsp/src/main.c
@@ -6,7 +6,6 @@ DECL|UNKNOWN_APPEARANCE|macro|UNKNOWN_APPEARANCE
 DECL|ad|variable|ad
 DECL|appeareance_uuid|variable|appeareance_uuid
 DECL|attrs|variable|attrs
-DECL|bt_ready|function|static void bt_ready(int err)
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn)
 DECL|device_name_uuid|variable|device_name_uuid
