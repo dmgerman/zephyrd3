143aa53d2b4d0af843b57bd6330e303dd0b2e24a
Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
2015-12-01 16:29:23 -0600

    Add _loapic_isr_vector_get to mvic
    The kernel event logger requires this function to obtain
    the irq that is being processed, but this was not implemented
    for mvic. This patch add this function to mvic.
    
    Change-Id: I5e1b4a6e2758b69ec713c18ac9f78aebad18a93f
    Signed-off-by: Yonattan Louise <yonattan.a.louise.mendoza@intel.com>
    
    Former-commit-id: dc12b5d06a804d221de98fdda64aeefddeb7a191

--- drivers/interrupt_controller/mvic.c
+++ drivers/interrupt_controller/mvic.c
@@ -1,6 +1,7 @@
 DECL|IOAPIC_DATA|macro|IOAPIC_DATA
 DECL|IOAPIC_IND|macro|IOAPIC_IND
 DECL|LOAPIC_ENABLE|macro|LOAPIC_ENABLE
+DECL|LOAPIC_MVIC_ISR|macro|LOAPIC_MVIC_ISR
 DECL|LOAPIC_VECTOR|macro|LOAPIC_VECTOR
 DECL|MVIC_HIGH_NIBBLE_MASK|macro|MVIC_HIGH_NIBBLE_MASK
 DECL|MVIC_LOW_NIBBLE_MASK|macro|MVIC_LOW_NIBBLE_MASK
@@ -15,6 +16,7 @@ DECL|_loapic_enable|function|void _loapic_enable(void)
 DECL|_loapic_int_vec_set|function|void _loapic_int_vec_set(unsigned int irq, unsigned int vector)
 DECL|_loapic_irq_disable|function|void _loapic_irq_disable(unsigned int irq)
 DECL|_loapic_irq_enable|function|void _loapic_irq_enable(unsigned int irq)
+DECL|_loapic_isr_vector_get|function|int _loapic_isr_vector_get(void)
 DECL|_mvic_init|function|int _mvic_init(struct device *unused)
 DECL|_mvic_rte_get|function|static uint32_t _mvic_rte_get(unsigned int irq)
 DECL|_mvic_rte_set|function|static void _mvic_rte_set(unsigned int irq, uint32_t value)
