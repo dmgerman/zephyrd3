a258fad4a0b5085165d97be00443f7d865d1a71f
Daniel Leung <daniel.leung@intel.com>
2015-12-01 08:42:19 -0800

    serial/nsim: refactor (again) to conform to driver model
    () Rename nsim_uart.c to uart_nsim.c. This is to follow
       the driver naming convention.
    () Rename functions nsim_uart_*() to uart_nsim_*(),
       following driver naming convention.
    () UART ports initialization is moved into the driver itself.
       All the init code in platform config files is removed.
    () Adds (many) Kconfig options. These don't have to be defined
       in each platform's board.h anymore.
    
    Change-Id: If015f39a6f6b4fcc65625e6e5f973b4469202f54
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 91cea2c9a63c7b8d251baf094292cf29837fe646

---
+++ drivers/serial/uart_nsim.c
@@ -0,0 +1,12 @@
+DECL|DATA_REG|macro|DATA_REG
+DECL|DEV_CFG|macro|DEV_CFG
+DECL|NSIM_UART_DATA|macro|NSIM_UART_DATA
+DECL|NSIM_UART_STATUS|macro|NSIM_UART_STATUS
+DECL|STATUS_REG|macro|STATUS_REG
+DECL|TXEMPTY|macro|TXEMPTY
+DECL|uart_nsim_dev_cfg_0|variable|uart_nsim_dev_cfg_0
+DECL|uart_nsim_driver_api|variable|uart_nsim_driver_api
+DECL|uart_nsim_driver_api|variable|uart_nsim_driver_api
+DECL|uart_nsim_init|function|static int uart_nsim_init(struct device *dev)
+DECL|uart_nsim_poll_in|function|static int uart_nsim_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_nsim_poll_out|function|unsigned char uart_nsim_poll_out(struct device *dev, unsigned char c)
