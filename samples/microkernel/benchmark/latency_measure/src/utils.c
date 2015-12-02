5735121cf40a44ecf8e053badc44bf92e10ffcfb
Andrew Boie <andrew.p.boie@intel.com>
2015-12-02 10:29:39 -0800

    tests: latency_measure: use irq_offload()
    Change-Id: I4f109652a4d778b641c7646a48309c552eb865dc
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 4307552dab757375cf8e777f382a6eb83d98c214

--- samples/microkernel/benchmark/latency_measure/src/utils.c
+++ samples/microkernel/benchmark/latency_measure/src/utils.c
@@ -1,5 +1,1 @@
-DECL|initSwInterrupt|function|int initSwInterrupt(ptestIsr pIsrHdlr)
-DECL|raiseIntFunc|function|void raiseIntFunc(void)
-DECL|setSwInterrupt|function|void setSwInterrupt(ptestIsr pIsrHdlr)
 DECL|tmpString|variable|tmpString
-DECL|vector|variable|vector
