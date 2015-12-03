f53db4304b46a0f1b8ed4949478081e45be6689c
Anas Nashif <anas.nashif@intel.com>
2015-12-03 11:49:28 -0500

    watchdog: add sample application
    This application creates a watchdog with 4.194s timout and reloads
    every time it kicks (and does not force a reboot).
    
    Change-Id: Ib128e2f7f91d05081d1bfb8cc24258b69bd3cb6e
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: e5c41f07202224c3f445c1e54714ec901f5db9fd

---
+++ samples/nanokernel/apps/watchdog/src/main.c
@@ -0,0 +1,5 @@
+DECL|WDT_DRIVER|macro|WDT_DRIVER
+DECL|main|function|void main(void)
+DECL|wdt_dev|variable|wdt_dev
+DECL|wdt_example_cb|function|void wdt_example_cb(void)
+DECL|wdt_fired|variable|wdt_fired
