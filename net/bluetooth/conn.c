7b531f965772c007e90d9593ad7692b77cdc7377
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2015-12-03 17:05:51 +0100

    Bluetooth: BR/EDR: Look up conn by BT address helper
    Defines helper routine to look up conn pool object for existance
    of instance matching requested bluetooth address.
    
    Change-Id: I31e05afb2346c7dab14e7ba1bd2f4e1378876ae0
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 7abddd2bdf67f005f5888f716c9d016d8e8f7f1e

--- net/bluetooth/conn.c
+++ net/bluetooth/conn.c
@@ -15,6 +15,7 @@ DECL|bt_conn_identity_resolved|function|void bt_conn_identity_resolved(struct bt
 DECL|bt_conn_init|function|int bt_conn_init(void)
 DECL|bt_conn_le_conn_update|function|int bt_conn_le_conn_update(struct bt_conn *conn, uint16_t min, uint16_t max, uint16_t latency, uint16_t timeout)
 DECL|bt_conn_le_start_encryption|function|int bt_conn_le_start_encryption(struct bt_conn *conn, uint64_t rand,uint16_t ediv, const uint8_t *ltk, size_t len)
+DECL|bt_conn_lookup_addr_br|function|struct bt_conn *bt_conn_lookup_addr_br(const bt_addr_t *peer)
 DECL|bt_conn_lookup_addr_le|function|struct bt_conn *bt_conn_lookup_addr_le(const bt_addr_le_t *peer)
 DECL|bt_conn_lookup_handle|function|struct bt_conn *bt_conn_lookup_handle(uint16_t handle)
 DECL|bt_conn_lookup_state_le|function|struct bt_conn *bt_conn_lookup_state_le(const bt_addr_le_t *peer,const bt_conn_state_t state)
