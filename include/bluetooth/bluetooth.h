e155851ef10c8124bc47f8d0d9ace886e845b351
Johan Hedberg <johan.hedberg@intel.com>
2015-12-03 15:17:10 +0200

    Bluetooth: Add ability to specify dedicated LE scan parameters
    We'll need to have a much more flexible LE scanning API. This is a
    first step towards it. The existing BT_LE_SCAN_FILTER_DUP_* enum
    values are converted to macros to avoid having to update application
    code at this point.
    
    Change-Id: I0c8d29dc156bad67cddc9401c0d72aee8ec6d8de
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6f470dcbce7b834bf0e3027e898b65bba3e2aa00

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,7 +1,7 @@
 DECL|BT_ADDR_LE_STR_LEN|macro|BT_ADDR_LE_STR_LEN
 DECL|BT_ADDR_STR_LEN|macro|BT_ADDR_STR_LEN
-DECL|BT_LE_SCAN_FILTER_DUP_DISABLE|enumerator|BT_LE_SCAN_FILTER_DUP_DISABLE,
-DECL|BT_LE_SCAN_FILTER_DUP_ENABLE|enumerator|BT_LE_SCAN_FILTER_DUP_ENABLE,
+DECL|BT_LE_SCAN_FILTER_DUP_DISABLE|macro|BT_LE_SCAN_FILTER_DUP_DISABLE
+DECL|BT_LE_SCAN_FILTER_DUP_ENABLE|macro|BT_LE_SCAN_FILTER_DUP_ENABLE
 DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
 DECL|__packed|variable|__packed
 DECL|bt_addr_le_to_str|function|static inline int bt_addr_le_to_str(const bt_addr_le_t *addr, char *str, size_t len)
@@ -9,10 +9,11 @@ DECL|bt_addr_to_str|function|static inline int bt_addr_to_str(const bt_addr_t *a
 DECL|bt_auth_cb|struct|struct bt_auth_cb {
 DECL|bt_eir|struct|struct bt_eir {
 DECL|bt_le_scan_cb_t|typedef|typedef void bt_le_scan_cb_t(const bt_addr_le_t *addr, int8_t rssi,
-DECL|bt_le_scan_filter_dup_t|typedef|} bt_le_scan_filter_dup_t;
+DECL|bt_le_scan_param|struct|struct bt_le_scan_param {
 DECL|bt_ready_cb_t|typedef|typedef void (*bt_ready_cb_t)(int err);
 DECL|cancel|member|void (*cancel)(struct bt_conn *conn);
 DECL|data|member|uint8_t data[29];
+DECL|filter_dup|member|uint8_t filter_dup;
 DECL|len|member|uint8_t len;
 DECL|passkey_confirm|member|void (*passkey_confirm)(struct bt_conn *conn, unsigned int passkey);
 DECL|passkey_display|member|void (*passkey_display)(struct bt_conn *conn, unsigned int passkey);
