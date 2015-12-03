24f6671066e1df72ddfbe6514a90f3d425c4b98f
Johan Hedberg <johan.hedberg@intel.com>
2015-12-03 10:19:38 +0200

    Bluetooth: Rename LE scan & advertising APIs for consistency
    To have a clear split between LE & BR/EDR rename these APIs with the
    appropriate prefixes.
    
    Change-Id: I983df2b5880947d96f0ad289d12f3383f44894be
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 5619d1114ba8c9f80c1c4840dea37b668b4ce9bd

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -1,7 +1,7 @@
 DECL|BT_ADDR_LE_STR_LEN|macro|BT_ADDR_LE_STR_LEN
 DECL|BT_ADDR_STR_LEN|macro|BT_ADDR_STR_LEN
-DECL|BT_SCAN_FILTER_DUP_DISABLE|enumerator|BT_SCAN_FILTER_DUP_DISABLE,
-DECL|BT_SCAN_FILTER_DUP_ENABLE|enumerator|BT_SCAN_FILTER_DUP_ENABLE,
+DECL|BT_LE_SCAN_FILTER_DUP_DISABLE|enumerator|BT_LE_SCAN_FILTER_DUP_DISABLE,
+DECL|BT_LE_SCAN_FILTER_DUP_ENABLE|enumerator|BT_LE_SCAN_FILTER_DUP_ENABLE,
 DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
 DECL|__packed|variable|__packed
 DECL|bt_addr_le_to_str|function|static inline int bt_addr_le_to_str(const bt_addr_le_t *addr, char *str, size_t len)
@@ -9,8 +9,8 @@ DECL|bt_addr_to_str|function|static inline int bt_addr_to_str(const bt_addr_t *a
 DECL|bt_auth_cb|struct|struct bt_auth_cb {
 DECL|bt_eir|struct|struct bt_eir {
 DECL|bt_le_scan_cb_t|typedef|typedef void bt_le_scan_cb_t(const bt_addr_le_t *addr, int8_t rssi,
+DECL|bt_le_scan_filter_dup_t|typedef|} bt_le_scan_filter_dup_t;
 DECL|bt_ready_cb_t|typedef|typedef void (*bt_ready_cb_t)(int err);
-DECL|bt_scan_filter_dup_t|typedef|} bt_scan_filter_dup_t;
 DECL|cancel|member|void (*cancel)(struct bt_conn *conn);
 DECL|data|member|uint8_t data[29];
 DECL|len|member|uint8_t len;
