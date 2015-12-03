12bc8ebb7bf9600b03538720647d959d025a8480
Peter Mitsis <peter.mitsis@windriver.com>
2015-12-03 09:56:31 -0500

    mailbox: Simplify task_mbox_put() API family
    Changes the mailbox API so that the timeout parameter must be specified
    when invoking task_mbox_put() thereby obsoleting the following APIs:
    	task_mbox_put_wait()
    	task_mbox_put_wait_timeout()
    	_task_mbox_put()
    
    Change-Id: I174857bdf32fe7e59b79838185666cd557312814
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 96116cadbb51513049def794e47bc60a204815f8

--- include/microkernel/mailbox.h
+++ include/microkernel/mailbox.h
@@ -9,6 +9,3 @@ DECL|task_mbox_data_get|macro|task_mbox_data_get
 DECL|task_mbox_get_wait_timeout|macro|task_mbox_get_wait_timeout
 DECL|task_mbox_get_wait|macro|task_mbox_get_wait
 DECL|task_mbox_get|macro|task_mbox_get
-DECL|task_mbox_put_wait_timeout|macro|task_mbox_put_wait_timeout
-DECL|task_mbox_put_wait|macro|task_mbox_put_wait
-DECL|task_mbox_put|macro|task_mbox_put
