fce4991d434fcba082334868c8dd3108343fd414
Daniel Leung <daniel.leung@intel.com>
2015-12-04 14:47:49 -0800

    quark_se: need some extra love for interrupt driver UARTs
    () The SCSS has some additional registers to mask interrupts
       from peripherals. This patch unmasks the interrupts for
       the two UARTs on board, so that interrupts can be delivered.
    () The UART interrupts are edge-triggerd, so set the config
       options accordingly.
    
    Change-Id: I39a1edccd830a8a19288982efa2e9d0faaaff8ba
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 3c9592ec594a441d06470af0003009e057ba97cc

--- arch/x86/platforms/quark_se/system.c
+++ arch/x86/platforms/quark_se/system.c
@@ -2,3 +2,4 @@ DECL|SCSS_REG_VAL|macro|SCSS_REG_VAL
 DECL|arc_init_debug|macro|arc_init_debug
 DECL|arc_init_debug|macro|arc_init_debug
 DECL|arc_init|function|static int arc_init(struct device *arg)
+DECL|platform_uart_init|function|static int platform_uart_init(struct device *arg)
