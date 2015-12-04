5ee663c2a458bef219f4daeaf06a5f7cced3aad6
Johan Hedberg <johan.hedberg@intel.com>
2015-12-04 13:10:36 +0200

    drivers/console: shell: Add custom prompt callback support
    Shell applications may want to have a dynamic prompt rather than a
    fixed string. Add a new API to let the application specify a custom
    callback instead of using the fixed string given to shell_init().
    
    Change-Id: I9844481057fc8e164530a677e7cb1bfb6d02cfc0
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 2893def459bd59381bcc875f8bc4c6b830f16b40

--- drivers/console/console_handler_shell.c
+++ drivers/console/console_handler_shell.c
@@ -2,16 +2,19 @@ DECL|ARGC_MAX|macro|ARGC_MAX
 DECL|MAX_CMD_QUEUED|macro|MAX_CMD_QUEUED
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|app_cmd_handler|variable|app_cmd_handler
+DECL|app_prompt_handler|variable|app_prompt_handler
 DECL|avail_queue|variable|avail_queue
 DECL|buf|variable|buf
 DECL|cmds_queue|variable|cmds_queue
 DECL|commands|variable|commands
 DECL|get_cb|function|static shell_cmd_function_t get_cb(const char *string)
+DECL|get_prompt|function|static const char *get_prompt(void)
 DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t size)
 DECL|line_queue_init|function|static void line_queue_init(void)
 DECL|prompt|variable|prompt
 DECL|shell_init|function|void shell_init(const char *str, struct shell_cmd *cmds)
 DECL|shell_register_app_cmd_handler|function|void shell_register_app_cmd_handler(shell_cmd_function_t handler)
+DECL|shell_register_prompt_handler|function|void shell_register_prompt_handler(shell_prompt_function_t handler)
 DECL|shell|function|static void shell(int arg1, int arg2)
 DECL|show_help|function|static void show_help(int argc, char *argv[])
 DECL|stack|variable|stack
--- include/misc/shell.h
+++ include/misc/shell.h
@@ -2,3 +2,4 @@ DECL|cb|member|shell_cmd_function_t cb;
 DECL|cmd_name|member|const char *cmd_name;
 DECL|shell_cmd_function_t|typedef|typedef void (*shell_cmd_function_t)(int argc, char *argv[]);
 DECL|shell_cmd|struct|struct shell_cmd {
+DECL|shell_prompt_function_t|typedef|typedef const char *(*shell_prompt_function_t)(void);
