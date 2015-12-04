f2399c7487fdd9591ce43d93254176bc1186dc5c
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-12-04 15:05:23 -0800

    pinmux : Quark_SE : add pullup, input enable regs
    Adding support for the pinmux pins to set the pullup and input enable
    registers.  On the Arduino_101 board some pullup pins specifically can
    be used to cause a reset for the Arduino sketch being run.  Its use on
    other pins is currently being investigated.
    
    Change-Id: Id3293a4da84ea5bf553bf62ccb12782cb88503a5
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: f6694ed31f6c87b513439b6596d1c0de523cbe06

--- arch/x86/platforms/quark_se/pinmux.c
+++ arch/x86/platforms/quark_se/pinmux.c
@@ -1,12 +1,22 @@
+DECL|PINMUX_INPUT_OFFSET|macro|PINMUX_INPUT_OFFSET
+DECL|PINMUX_PULLUP_OFFSET|macro|PINMUX_PULLUP_OFFSET
+DECL|PINMUX_SELECT_OFFSET|macro|PINMUX_SELECT_OFFSET
+DECL|PINMUX_SLEW_OFFSET|macro|PINMUX_SLEW_OFFSET
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
+DECL|_pinmux_pullups|function|static inline void _pinmux_pullups(uint32_t base_address)
+DECL|_pinmux_pullups|function|static inline void _pinmux_pullups(uint32_t base_address) { };
+DECL|_quark_se_input|function|static uint32_t _quark_se_input(uint32_t base, uint32_t pin, uint8_t func)
+DECL|_quark_se_pullup|function|static uint32_t _quark_se_pullup(uint32_t base, uint32_t pin, uint8_t func)
 DECL|_quark_se_select_get|function|static uint32_t _quark_se_select_get(uint32_t base, uint32_t pin)
-DECL|_quark_se_select_set|function|static void _quark_se_select_set(uint32_t base, uint32_t pin, uint32_t mode)
+DECL|_quark_se_select|function|static void _quark_se_select(uint32_t base, uint32_t pin, uint32_t mode)
 DECL|api_funcs|variable|api_funcs
 DECL|board_pmux|variable|board_pmux
 DECL|pinmux_dev_get|function|static uint32_t pinmux_dev_get(struct device *dev, uint32_t pin, uint8_t *func)
 DECL|pinmux_dev_get|function|static uint32_t pinmux_dev_get(struct device *dev, uint32_t pin, uint8_t *func)
+DECL|pinmux_dev_input|function|static uint32_t pinmux_dev_input(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_dev_pullup|function|static uint32_t pinmux_dev_pullup(struct device *dev, uint32_t pin, uint8_t func)
 DECL|pinmux_dev_set|function|static uint32_t pinmux_dev_set(struct device *dev, uint32_t pin, uint8_t func)
 DECL|pinmux_dev_set|function|static uint32_t pinmux_dev_set(struct device *dev, uint32_t pin, uint8_t func)
-DECL|pinmux_initialize|function|int pinmux_initialize(struct device *dev)
+DECL|pinmux_initialize|function|int pinmux_initialize(struct device *port)
