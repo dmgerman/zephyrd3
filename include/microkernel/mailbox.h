d0edb5951c729eb6bc7acc5bb194ff990e3b66a9
Peter Mitsis <peter.mitsis@windriver.com>
2015-12-04 11:02:13 -0500

    mailbox: Simplify task_mbox_data_block_get() API family
    Changes the mailbox API so that the timeout parameter must be specified
    when invoking task_mbox_data_block_get() thereby obsoleting the
    following APIs:
        task_mbox_data_block_get_wait()
        task_mbox_data_block_get_wait_timeout()
        _task_mbox_data_block_get()
    
    Change-Id: I284be505e6de792ba5483611d1299063162550e1
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: d3e4b72da27ba42713e82629364930b263a9e4cc

--- include/microkernel/mailbox.h
+++ include/microkernel/mailbox.h
@@ -2,7 +2,4 @@ DECL|DEFINE_MAILBOX|macro|DEFINE_MAILBOX
 DECL|_MAILBOX_H|macro|_MAILBOX_H
 DECL|__K_MAILBOX_DEFAULT|macro|__K_MAILBOX_DEFAULT
 DECL|task_mbox_block_put|macro|task_mbox_block_put
-DECL|task_mbox_data_block_get_wait_timeout|macro|task_mbox_data_block_get_wait_timeout
-DECL|task_mbox_data_block_get_wait|macro|task_mbox_data_block_get_wait
-DECL|task_mbox_data_block_get|macro|task_mbox_data_block_get
 DECL|task_mbox_data_get|macro|task_mbox_data_get
