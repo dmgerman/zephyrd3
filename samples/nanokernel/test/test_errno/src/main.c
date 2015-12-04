5259a92f11eabd867c58ee0aabe18a3ba25aa346
Benjamin Walsh <benjamin.walsh@windriver.com>
2015-12-04 15:30:41 -0500

    test: add nanokernel errno test
    Tests errno is retrieved per-thread with fibers and one task.
    
    Change-Id: I0b0890f250a80a30806dc1294944aa42f4209ace
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 355a00e6f0224bb76d160e9073c4a954592425f6

---
+++ samples/nanokernel/test/test_errno/src/main.c
@@ -0,0 +1,11 @@
+DECL|N_FIBERS|macro|N_FIBERS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|errno_fiber|function|static void errno_fiber(int n, int my_errno)
+DECL|errno_values|variable|errno_values
+DECL|fifo|variable|fifo
+DECL|main|function|void main(void)
+DECL|pass|member|int pass;
+DECL|q|member|void *q;
+DECL|result|struct|struct result {
+DECL|result|variable|result
+DECL|stacks|variable|stacks
