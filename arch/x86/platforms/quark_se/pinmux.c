1f635d8ad83e37ea19bdecf0295f39c9623f457e
Dan Kalowsky <daniel.kalowsky@intel.com>
2015-12-04 15:05:23 -0800

    quark_se : pinmux : fixing the naming convention
    Naming convention should follow the IP_noun_verb concept.  In this case,
    renaming several functions from (for example) pinmux_set_pin to
    pinmux_pin_set, while also correctly naming the function to their
    specific tasks.
    
    This is being done to make way for the ability to change the input
    value, pullup value, and the slew as well.
    
    Change-Id: Iec6f1723a48f80b66f3cea44df9bb6925972f6af
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 14b5d5c79d46918916786b534f9e3fbc5c5fbc35

--- arch/x86/platforms/quark_se/pinmux.c
+++ arch/x86/platforms/quark_se/pinmux.c
@@ -1,8 +1,8 @@
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
-DECL|_pinmux_get|function|static uint32_t _pinmux_get(uint32_t base, uint32_t pin)
-DECL|_pinmux_set|function|static void _pinmux_set(uint32_t base, uint32_t pin, uint32_t mode)
+DECL|_quark_se_select_get|function|static uint32_t _quark_se_select_get(uint32_t base, uint32_t pin)
+DECL|_quark_se_select_set|function|static void _quark_se_select_set(uint32_t base, uint32_t pin, uint32_t mode)
 DECL|api_funcs|variable|api_funcs
 DECL|board_pmux|variable|board_pmux
 DECL|pinmux_dev_get|function|static uint32_t pinmux_dev_get(struct device *dev, uint32_t pin, uint8_t *func)
