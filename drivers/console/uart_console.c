fbacd0a0b8ada8d3d32008fce53e718162a96c7b
Johan Hedberg <johan.hedberg@intel.com>
2015-12-08 10:35:04 +0200

    drivers/console: Handle basic control characters cleanly
    By default, since we don't have special support for control characters
    we should simply ignore them. This includes both pure (single) control
    characters as well as ANSI escape sequences. The most important
    control character to support for basic editing is Backspace.
    
    The terminal creates a Delete (DEL) character by default when
    Backspace is pressed. It also treats the Backspace character ('\b') as
    a non-destructive backspace. To create the effect of destructive
    backspace we have to send the sequence "\b \b".
    
    Change-Id: Idc942d09be2a84b8dd0a60ace8429102c6c7e355
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: f88cccd2b5713e554d1a3d5b266061c711217056

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -1,3 +1,14 @@
+DECL|ANSI_BACKWARD_STR|macro|ANSI_BACKWARD_STR
+DECL|ANSI_BACKWARD|macro|ANSI_BACKWARD
+DECL|ANSI_DOWN_STR|macro|ANSI_DOWN_STR
+DECL|ANSI_DOWN|macro|ANSI_DOWN
+DECL|ANSI_ESC|macro|ANSI_ESC
+DECL|ANSI_FORWARD_STR|macro|ANSI_FORWARD_STR
+DECL|ANSI_FORWARD|macro|ANSI_FORWARD
+DECL|ANSI_UP_STR|macro|ANSI_UP_STR
+DECL|ANSI_UP|macro|ANSI_UP
+DECL|DEL|macro|DEL
+DECL|ESC|macro|ESC
 DECL|__printk_hook_install|macro|__printk_hook_install
 DECL|__stdout_hook_install|macro|__stdout_hook_install
 DECL|avail_queue|variable|avail_queue
@@ -12,3 +23,4 @@ DECL|uart_console_init|function|static int uart_console_init(struct device *arg)
 DECL|uart_console_isr|function|void uart_console_isr(void *unused)
 DECL|uart_register_input|function|void uart_register_input(struct nano_fifo *avail, struct nano_fifo *lines)
 DECL|uart_register_input|macro|uart_register_input
+DECL|write_uart|function|static void write_uart(struct device *uart, const char *str)
