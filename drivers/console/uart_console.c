4a28b6a376bde898b586dbdcbdd885670f4e0eca
Johan Hedberg <johan.hedberg@intel.com>
2015-12-08 22:45:12 +0200

    drivers/console: Add support for editing the line internally
    Add support for forward/backward ANSI sequences and the ability to
    enter new text while the cursor is somewhere in the middle of the line
    rather than at the end of it. printk is introduced into the game
    rather than the old write_uart() since we need to make use of the
    %u format string support for creating the ANSI sequence to move
    backward/forward the right number of characters.
    
    Change-Id: I41f880fc5de773573147c5a291e2cbe94d5370a4
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: a9f6f89e840222cd1acd5a9f63f4bdfc9eef8207

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -1,11 +1,7 @@
-DECL|ANSI_BACKWARD_STR|macro|ANSI_BACKWARD_STR
 DECL|ANSI_BACKWARD|macro|ANSI_BACKWARD
-DECL|ANSI_DOWN_STR|macro|ANSI_DOWN_STR
 DECL|ANSI_DOWN|macro|ANSI_DOWN
 DECL|ANSI_ESC|macro|ANSI_ESC
-DECL|ANSI_FORWARD_STR|macro|ANSI_FORWARD_STR
 DECL|ANSI_FORWARD|macro|ANSI_FORWARD
-DECL|ANSI_UP_STR|macro|ANSI_UP_STR
 DECL|ANSI_UP|macro|ANSI_UP
 DECL|DEL|macro|DEL
 DECL|ESC|macro|ESC
@@ -15,6 +11,10 @@ DECL|avail_queue|variable|avail_queue
 DECL|console_input_init|function|static void console_input_init(void)
 DECL|console_input_init|macro|console_input_init
 DECL|console_out|function|static int console_out(int c)
+DECL|cursor_backward|function|static inline void cursor_backward(unsigned int count)
+DECL|cursor_forward|function|static inline void cursor_forward(unsigned int count)
+DECL|del_char|function|static void del_char(char *pos, uint8_t end)
+DECL|insert_char|function|static void insert_char(char *pos, char c, uint8_t end)
 DECL|lines_queue|variable|lines_queue
 DECL|read_uart|function|static int read_uart(struct device *uart, uint8_t *buf, unsigned int size)
 DECL|uart_console_dev|variable|uart_console_dev
@@ -23,4 +23,3 @@ DECL|uart_console_init|function|static int uart_console_init(struct device *arg)
 DECL|uart_console_isr|function|void uart_console_isr(void *unused)
 DECL|uart_register_input|function|void uart_register_input(struct nano_fifo *avail, struct nano_fifo *lines)
 DECL|uart_register_input|macro|uart_register_input
-DECL|write_uart|function|static void write_uart(struct device *uart, const char *str)
