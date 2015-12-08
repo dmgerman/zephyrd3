c1d670e91a88a865f7839c23ab56781846e6d4ff
Dirk Brandewie <dirk.j.brandewie@intel.com>
2015-12-08 09:26:53 -0800

    kernel: remove CONFIG_INT_LATENCY_BENCHMARK fences from code path
    Add null definitions for the interrupt latency measurement API so we
    can remove compile fences in C code.
    
    Change-Id: If86eedf79afcb49002108814dd4fb864956eb667
    Signed-off-by: Dirk Brandewie <dirk.j.brandewie@intel.com>
    
    Former-commit-id: 88ce5bc8f532894dfbc4b9b80ce97566f70f168e

--- kernel/nanokernel/int_latency_bench.c
+++ kernel/nanokernel/int_latency_bench.c
@@ -1,10 +1,13 @@
 DECL|NB_CACHE_WARMING_DRY_RUN|macro|NB_CACHE_WARMING_DRY_RUN
 DECL|_hw_irq_to_c_handler_latency|variable|_hw_irq_to_c_handler_latency
 DECL|_int_latency_start|function|void _int_latency_start(void)
+DECL|_int_latency_start|function|void _int_latency_start(void) {};
 DECL|_int_latency_stop|function|void _int_latency_stop(void)
+DECL|_int_latency_stop|function|void _int_latency_stop(void) {};
 DECL|initial_start_delay|variable|initial_start_delay
 DECL|int_latency_bench_ready|variable|int_latency_bench_ready
 DECL|int_latency_init|function|void int_latency_init(void)
+DECL|int_latency_init|function|void int_latency_init(void) {};
 DECL|int_latency_show|function|void int_latency_show(void)
 DECL|int_lock_unlock_nest|variable|int_lock_unlock_nest
 DECL|int_locked_latency_max|variable|int_locked_latency_max
