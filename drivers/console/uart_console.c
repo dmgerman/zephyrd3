9dd6182dab3a7e64240bb7ebe7d22c290b2e9327
Johan Hedberg <johan.hedberg@intel.com>
2015-12-09 11:36:25 +0200

    drivers/console: Add support for ANSI sequences with values
    ANSI sequences may have a numbered prefix which means that the action
    needs to be taken the number amount of times, e.g. "Esc[ValueA" (move
    cursor forward Value number of characters). Some ANSI commands also
    have two preceding values, such as Esc[Line;ColumnH (move cursor to
    Line & Column).
    
    To support these ANSI sequences we need to track a bit of extra state,
    most conveniently done using a bit field. Since the ANSI parsing logic
    gets a bit long at this point it's refactored into its own handle_ansi()
    function.
    
    Change-Id: I0711f869d5324f9f827915fde468fe470e7689ed
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 8683dc4836e78fd233ef55db157e4d3f29be6f68

--- drivers/console/uart_console.c
+++ drivers/console/uart_console.c
@@ -4,9 +4,16 @@ DECL|ANSI_ESC|macro|ANSI_ESC
 DECL|ANSI_FORWARD|macro|ANSI_FORWARD
 DECL|ANSI_UP|macro|ANSI_UP
 DECL|DEL|macro|DEL
+DECL|ESC_ANSI_FIRST|enumerator|ESC_ANSI_FIRST,
+DECL|ESC_ANSI_VAL_2|enumerator|ESC_ANSI_VAL_2
+DECL|ESC_ANSI_VAL|enumerator|ESC_ANSI_VAL,
+DECL|ESC_ANSI|enumerator|ESC_ANSI,
+DECL|ESC_ESC|enumerator|ESC_ESC,
 DECL|ESC|macro|ESC
 DECL|__printk_hook_install|macro|__printk_hook_install
 DECL|__stdout_hook_install|macro|__stdout_hook_install
+DECL|ansi_val_2|variable|ansi_val_2
+DECL|ansi_val|variable|ansi_val
 DECL|avail_queue|variable|avail_queue
 DECL|console_input_init|function|static void console_input_init(void)
 DECL|console_input_init|macro|console_input_init
@@ -15,7 +22,11 @@ DECL|cursor_backward|function|static inline void cursor_backward(unsigned int co
 DECL|cursor_forward|function|static inline void cursor_forward(unsigned int count)
 DECL|cursor_restore|function|static inline void cursor_restore(void)
 DECL|cursor_save|function|static inline void cursor_save(void)
+DECL|cur|variable|cur
 DECL|del_char|function|static void del_char(char *pos, uint8_t end)
+DECL|end|variable|end
+DECL|esc_state|variable|esc_state
+DECL|handle_ansi|function|static void handle_ansi(uint8_t byte)
 DECL|insert_char|function|static void insert_char(char *pos, char c, uint8_t end)
 DECL|lines_queue|variable|lines_queue
 DECL|read_uart|function|static int read_uart(struct device *uart, uint8_t *buf, unsigned int size)
