94ed5216f425020caf2a0471ff4941cd2c1285fe
Andrew Boie <andrew.p.boie@intel.com>
2015-12-09 10:18:30 -0800

    x86: irq_offload: don't use the last vector
    The APIC by default uses vector 0xFF for the spurious interrupt
    vector as described in the x86 CPU manual volume 3 section 10.9.
    Make this its own config option defaulting to 32.
    
    MVIC doesn't have a spurious interrupt vector, and due to the
    fixed IRQ-to-vector mapping for this controller, continue
    to use the last available entry.
    
    Change-Id: I29bd09df700629dc0d15b30a6ae590b0df1ef890
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 407d9e050b572ee7d36c5dca96f9a2818a0e73e3

--- arch/x86/core/irq_offload.c
+++ arch/x86/core/irq_offload.c
@@ -1,4 +1,3 @@
-DECL|SW_IRQ_VECTOR|macro|SW_IRQ_VECTOR
 DECL|_irq_do_offload|function|void _irq_do_offload(void)
 DECL|irq_offload|function|void irq_offload(irq_offload_routine_t routine, void *parameter)
 DECL|offload_param|variable|offload_param
