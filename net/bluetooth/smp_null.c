1211bf613cfdc20d3eb11e4eb2a7c5bcaf077f91
Szymon Janc <ext.szymon.janc@tieto.com>
2015-12-15 10:18:48 +0100

    Bluetooth: SMP: Fix NULL backend compilation
    This was not building due to bt_l2cap_create_pdu API change.
    
      CC      net/bluetooth/smp_null.o
    net/bluetooth/smp_null.c: In function 'bt_smp_recv':
    net/bluetooth/smp_null.c:59:2: warning: passing argument 1 of 'bt_l2cap_create_pdu' from incompatible pointer type [enabled by default]
      buf = bt_l2cap_create_pdu(conn);
      ^
    In file included from net/bluetooth/smp_null.c:32:0:
    net/bluetooth/l2cap_internal.h:136:17: note: expected 'struct nano_fifo *' but argument is of type 'struct bt_conn *'
     struct net_buf *bt_l2cap_create_pdu(struct nano_fifo *fifo);
    
    Change-Id: I5fd8ce63da09af1c123b1092624d48edc009756b
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: 74333b74afac9203e63f553ef0e08cd45be6efb9

--- net/bluetooth/smp_null.c
+++ net/bluetooth/smp_null.c
@@ -3,3 +3,4 @@ DECL|bt_smp_init|function|int bt_smp_init(void)
 DECL|bt_smp_pool|variable|bt_smp_pool
 DECL|bt_smp_recv|function|static void bt_smp_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|bt_smp_sign_verify|function|int bt_smp_sign_verify(struct bt_conn *conn, struct net_buf *buf)
+DECL|smp_buf|variable|smp_buf
