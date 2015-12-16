042f02f29ae4ea880747d212f2d06908770bc693
Daniel Leung <daniel.leung@intel.com>
2015-12-15 16:13:47 -0800

    samples: using I2C FRAM via Arduino 101
    This adds a sample app to utilize the Fujitsu
    MB85RC256V FRAM via I2C on Arduino 101. It does
    simple read and write on the chip.
    
    Change-Id: Ie20470f3701acb0e4fa9bc3d7b3676625839cf06
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: d9e930bc093e46e24fee018d066c759d158fab87

---
+++ samples/nanokernel/apps/i2c_fujitsu_fram/src/main.c
@@ -0,0 +1,4 @@
+DECL|FRAM_I2C_ADDR|macro|FRAM_I2C_ADDR
+DECL|main|function|void main(void)
+DECL|read_byte|function|int read_byte(struct device *i2c_dev, uint16_t addr, uint8_t *data)
+DECL|write_byte|function|int write_byte(struct device *i2c_dev, uint16_t addr, uint8_t data)
