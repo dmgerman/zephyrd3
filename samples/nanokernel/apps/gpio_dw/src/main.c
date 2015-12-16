9baba7ee9014a805e809e1b61aef743e9a3ff21c
Daniel Leung <daniel.leung@intel.com>
2015-12-16 11:08:52 -0800

    samples: add sample app for GPIO
    This adds a sample app to utilize GPIOs on x86 side of
    Arduino 101. Refer to the header of source file for
    more information.
    
    Change-Id: Ie547618b2c4fb483d85d6458bd68dd2849e1d19d
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 649267298f16257170b2cfdd8fefa202aa8d8418

---
+++ samples/nanokernel/apps/gpio_dw/src/main.c
@@ -0,0 +1,5 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|gpio_callback|function|void gpio_callback(struct device *port, uint32_t pin)
+DECL|main|function|void main(void)
