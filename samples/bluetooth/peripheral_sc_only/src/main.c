6d6acddb5c2c5f22233663b48a010b6dd7c72fb0
Szymon Janc <ext.szymon.janc@tieto.com>
2015-12-18 15:18:24 +0100

    Bluetooth: Add LE SC Only mode sample
    This adds peripheral sample application that is configured
    in LE SC Only Mode. It tries to set security level to FIPS right
    after connection is created. Two paired devices are supported.
    
    Change-Id: I78f3e3eeaf552c52f982035168e967016858a00b
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: 6edc0edd8a3f8c43edb9d2a2a8c8e3060f4be268

---
+++ samples/bluetooth/peripheral_sc_only/src/main.c
@@ -0,0 +1,16 @@
+DECL|APPEARANCE|macro|APPEARANCE
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|ad|variable|ad
+DECL|attrs|variable|attrs
+DECL|auth_cancel|function|static void auth_cancel(struct bt_conn *conn)
+DECL|auth_cb_display|variable|auth_cb_display
+DECL|auth_passkey_display|function|static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
+DECL|conn_callbacks|variable|conn_callbacks
+DECL|connected|function|static void connected(struct bt_conn *conn)
+DECL|disconnected|function|static void disconnected(struct bt_conn *conn)
+DECL|identity_resolved|function|static void identity_resolved(struct bt_conn *conn, const bt_addr_le_t *rpa, const bt_addr_le_t *identity)
+DECL|mainloop|function|void mainloop(void)
+DECL|read_appearance|function|static int read_appearance(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_string|function|static int read_string(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|sd|variable|sd
+DECL|security_changed|function|static void security_changed(struct bt_conn *conn, bt_security_t level)
