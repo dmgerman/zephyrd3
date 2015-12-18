bd88ce9053067c499591b63e77cca2cb5c6427cb
Anas Nashif <anas.nashif@intel.com>
2015-12-18 09:38:05 -0500

    samples: rename hello_world -> synchronisation
    This sample is more than just a hello world, it demonstrates basic
    kernel functionality that deserve to be its own sample.
    
    A simplified Hello World sample will replace this one.
    
    Change-Id: I9120daa1923f99def994e484783abe04db5b14eb
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: ba860ba8e937b065798b1cf219b1e5c37ecad1ee

---
+++ samples/microkernel/apps/synchronization/src/main.c
@@ -0,0 +1,7 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|taskA|function|void taskA(void)
+DECL|taskB|function|void taskB(void)
