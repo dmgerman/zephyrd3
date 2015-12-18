cb55f4fc5c79fa014ab9fa11eb5ea031fb0bd085
Daniel Leung <daniel.leung@intel.com>
2015-12-18 11:04:54 -0800

    quark_se_ss: enable UART
    On Arduino 101, the sensor subsystem also has access to the two UARTs
    on board. So this adds the necessary code to enable them to be used.
    However, one needs to make sure only one core has access to one UART
    at the same time.
    
    Change-Id: I9f6c203916164d1b48559a9752fb1e4d879d7fa4
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: cbd7dc65c4108bcd0e4963406bb36b3dbefde472

--- arch/arc/platforms/quark_se_ss/platform_config.c
+++ arch/arc/platforms/quark_se_ss/platform_config.c
@@ -2,3 +2,4 @@ DECL|arc_quark_se_ipm_init|function|static int arc_quark_se_ipm_init(void)
 DECL|gpio_dw_ss_init|function|static int gpio_dw_ss_init(struct device *dev)
 DECL|ipm_controller_config|variable|ipm_controller_config
 DECL|quark_se_ipm_sender_config|variable|quark_se_ipm_sender_config
+DECL|uart_ns16550_init|function|static int uart_ns16550_init(struct device *dev)
