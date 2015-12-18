83426e088b54edce9136bb2a68013c2782832bb2
Peter Mitsis <peter.mitsis@windriver.com>
2015-12-18 15:36:34 -0500

    nano_sema: Simplify nano_xxx_sem_take() API family
    Changes the nanokernel semaphore API so that the timeout parameter must be
    specified when invoking nano_isr_sem_take(), nano_fiber_sem_take(),
    nano_task_sem_take() and nano_sem_take().
    
    This obsoletes the following APIs:
    	nano_fiber_sem_take_wait()
    	nano_fiber_sem_take_wait_timeout()
    	nano_task_sem_take_wait()
    	nano_task_sem_take_wait_timeout()
    	nano_sem_take_wait()
    	nano_sem_take_wait_timeout()
    
    Change-Id: If7a4bce1bd8ec8d6410d04f3c16ff1922ff0910e
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 54b782a88bd0ddb4ff062fcb86bb99e4f97ad951

--- kernel/nanokernel/nano_sema.c
+++ kernel/nanokernel/nano_sema.c
@@ -1,13 +1,9 @@
 DECL|_sem_give_non_preemptible|function|void _sem_give_non_preemptible(struct nano_sem *sem)
-DECL|_sem_take|function|int _sem_take(struct nano_sem *sem )
-DECL|nano_fiber_sem_take_wait_timeout|function|int nano_fiber_sem_take_wait_timeout(struct nano_sem *sem, int32_t timeout_in_ticks)
-DECL|nano_fiber_sem_take_wait|function|void nano_fiber_sem_take_wait(struct nano_sem *sem)
+DECL|_sem_take|function|int _sem_take(struct nano_sem *sem, int32_t timeout_in_ticks)
 DECL|nano_sem_give|function|void nano_sem_give(struct nano_sem *sem)
 DECL|nano_sem_init|function|void nano_sem_init(struct nano_sem *sem)
-DECL|nano_sem_take_wait_timeout|function|void nano_sem_take_wait_timeout(struct nano_sem *sem, int32_t timeout)
-DECL|nano_sem_take_wait|function|void nano_sem_take_wait(struct nano_sem *sem)
+DECL|nano_sem_take|function|int nano_sem_take(struct nano_sem *sem, int32_t timeout)
 DECL|nano_task_sem_give|function|void nano_task_sem_give(struct nano_sem *sem)
-DECL|nano_task_sem_take_wait_timeout|function|int nano_task_sem_take_wait_timeout(struct nano_sem *sem, int32_t timeout_in_ticks)
-DECL|nano_task_sem_take_wait|function|void nano_task_sem_take_wait(struct nano_sem *sem)
+DECL|nano_task_sem_take|function|int nano_task_sem_take(struct nano_sem *sem, int32_t timeout_in_ticks)
 DECL|set_sem_available|macro|set_sem_available
 DECL|set_sem_available|macro|set_sem_available
