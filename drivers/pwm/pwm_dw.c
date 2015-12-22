b7a0438846e136bb6ca6a8bab0e14f9d2a2e7876
Daniel Leung <daniel.leung@intel.com>
2015-12-22 12:34:27 -0800

    drivers: add new DesignWare PWM driver
    This adds a driver to utilize the DesignWare timer IP block
    to do PWM.
    
    Change-Id: I11ba689350b664209ad2440e3001e57eb38fd982
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 6604381c115cf537b0a9a8a3b10a9acaedb5dead

---
+++ drivers/pwm/pwm_dw.c
@@ -0,0 +1,21 @@
+DECL|REG_COMP_VER|macro|REG_COMP_VER
+DECL|REG_OFFSET_LOAD_CNT2|macro|REG_OFFSET_LOAD_CNT2
+DECL|REG_OFFSET|macro|REG_OFFSET
+DECL|REG_TMR_CTRL|macro|REG_TMR_CTRL
+DECL|REG_TMR_LOAD_CNT2|macro|REG_TMR_LOAD_CNT2
+DECL|REG_TMR_LOAD_CNT|macro|REG_TMR_LOAD_CNT
+DECL|TIMER_INIT_CTRL|macro|TIMER_INIT_CTRL
+DECL|__set_one_port|function|static int __set_one_port(struct device *dev, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|addr|member|uint32_t addr;
+DECL|num_ports|member|uint32_t num_ports;
+DECL|pwm_dw_cfg|variable|pwm_dw_cfg
+DECL|pwm_dw_configure|function|static int pwm_dw_configure(struct device *dev, int access_op, uint32_t pwm, int flags)
+DECL|pwm_dw_config|struct|struct pwm_dw_config {
+DECL|pwm_dw_drv_api_funcs|variable|pwm_dw_drv_api_funcs
+DECL|pwm_dw_init|function|int pwm_dw_init(struct device *dev)
+DECL|pwm_dw_resume|function|static int pwm_dw_resume(struct device *dev)
+DECL|pwm_dw_set_duty_cycle|function|static int pwm_dw_set_duty_cycle(struct device *dev, int access_op, uint32_t pwm, uint8_t duty)
+DECL|pwm_dw_set_values|function|static int pwm_dw_set_values(struct device *dev, int access_op, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|pwm_dw_suspend|function|static int pwm_dw_suspend(struct device *dev)
+DECL|pwm_dw_timer_base_addr|function|static inline int pwm_dw_timer_base_addr(struct device *dev, uint32_t timer)
+DECL|pwm_dw_timer_ldcnt2_addr|function|static inline int pwm_dw_timer_ldcnt2_addr(struct device *dev, uint32_t timer)
