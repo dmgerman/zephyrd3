0a48eb1e87834508715800820a588703d9d5a109
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2015-12-28 16:39:57 +0100

    Bluetooth: BR/EDR: Update keys management
    Updates keys interfaces allowing to store and locate legacy link key
    in keys database.
    
    Change-Id: Ibd50edff890e998e3c6f80ee7f1442dff7a7c91f
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 53de0818f94149e5bf69d71f6d76a9106ed94e9f

--- net/bluetooth/keys.c
+++ net/bluetooth/keys.c
@@ -4,7 +4,10 @@ DECL|bt_keys_add_type|function|void bt_keys_add_type(struct bt_keys *keys, int t
 DECL|bt_keys_clear|function|void bt_keys_clear(struct bt_keys *keys, int type)
 DECL|bt_keys_find_addr|function|struct bt_keys *bt_keys_find_addr(const bt_addr_le_t *addr)
 DECL|bt_keys_find_irk|function|struct bt_keys *bt_keys_find_irk(const bt_addr_le_t *addr)
+DECL|bt_keys_find_link_key|function|struct bt_keys *bt_keys_find_link_key(const bt_addr_t *addr)
 DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(int type, const bt_addr_le_t *addr)
+DECL|bt_keys_get_addr_br|function|static struct bt_keys *bt_keys_get_addr_br(const bt_addr_t *addr)
 DECL|bt_keys_get_addr|function|struct bt_keys *bt_keys_get_addr(const bt_addr_le_t *addr)
+DECL|bt_keys_get_link_key|function|struct bt_keys *bt_keys_get_link_key(const bt_addr_t *addr)
 DECL|bt_keys_get_type|function|struct bt_keys *bt_keys_get_type(int type, const bt_addr_le_t *addr)
 DECL|key_pool|variable|key_pool
--- net/bluetooth/keys.h
+++ net/bluetooth/keys.h
@@ -1,6 +1,7 @@
 DECL|BT_KEYS_ALL|enumerator|BT_KEYS_ALL = (BT_KEYS_SLAVE_LTK | BT_KEYS_IRK | \
 DECL|BT_KEYS_AUTHENTICATED|enumerator|BT_KEYS_AUTHENTICATED,
 DECL|BT_KEYS_IRK|enumerator|BT_KEYS_IRK = (1 << 1),
+DECL|BT_KEYS_LINK_KEY|enumerator|BT_KEYS_LINK_KEY = (1 << 6),
 DECL|BT_KEYS_LOCAL_CSRK|enumerator|BT_KEYS_LOCAL_CSRK = (1 << 3),
 DECL|BT_KEYS_LTK_P256|enumerator|BT_KEYS_LTK_P256 = (1 << 5),
 DECL|BT_KEYS_LTK|enumerator|BT_KEYS_LTK = (1 << 2),
@@ -11,6 +12,7 @@ DECL|bt_csrk|struct|struct bt_csrk {
 DECL|bt_irk|struct|struct bt_irk {
 DECL|bt_keys_find_addr|function|static inline struct bt_keys *bt_keys_find_addr(const bt_addr_le_t *addr)
 DECL|bt_keys|struct|struct bt_keys {
+DECL|bt_link_key|struct|struct bt_link_key {
 DECL|bt_ltk|struct|struct bt_ltk {
 DECL|cnt|member|uint32_t cnt;
 DECL|ediv|member|uint16_t ediv;
@@ -18,12 +20,14 @@ DECL|enc_size|member|uint8_t enc_size;
 DECL|flags|member|atomic_t flags;
 DECL|irk|member|struct bt_irk irk;
 DECL|keys|member|uint16_t keys;
+DECL|link_key|member|struct bt_link_key link_key;
 DECL|local_csrk|member|struct bt_csrk local_csrk;
 DECL|ltk|member|struct bt_ltk ltk;
 DECL|rand|member|uint64_t rand;
 DECL|remote_csrk|member|struct bt_csrk remote_csrk;
 DECL|rpa|member|bt_addr_t rpa;
 DECL|slave_ltk|member|struct bt_ltk slave_ltk;
+DECL|val|member|uint8_t val[16];
 DECL|val|member|uint8_t val[16];
 DECL|val|member|uint8_t val[16];
 DECL|val|member|uint8_t val[16];
