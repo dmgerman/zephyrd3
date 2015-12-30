e2c105381aa07e13d80d8c9236ecff8c5ad2f079
Javier B Perez Hernandez <javier.b.perez.hernandez@linux.intel.com>
2015-12-30 13:41:56 -0600

    sample: nano: benchmark object: update BOARD to arduino_101
    Updated board to arduino_101 and updated nanokernel_objects.c
    NANO_CPU_INT_REGISTER macro args.
    
    Change-Id: I5281a84138ef031ae82cdd80ab617f94b3f712af
    Signed-off-by: Javier B Perez Hernandez <javier.b.perez.hernandez@linux.intel.com>
    Former-commit-id: 243c36360a620adf1cb5861e7329307c2e12f2b7

--- samples/nanokernel/benchmark/object_footprint/src/nanokernel_objects.c
+++ samples/nanokernel/benchmark/object_footprint/src/nanokernel_objects.c
@@ -1,6 +1,7 @@
 DECL|FIBER_STACK_SIZE|macro|FIBER_STACK_SIZE
 DECL|IRQ_PRIORITY|macro|IRQ_PRIORITY
 DECL|MESSAGE|macro|MESSAGE
+DECL|TEST_IRQ_OFFLOAD_VECTOR|macro|TEST_IRQ_OFFLOAD_VECTOR
 DECL|TEST_SOFT_INT|macro|TEST_SOFT_INT
 DECL|dummyIsr|function|void dummyIsr(void *unused)
 DECL|fiberEntry|function|static void fiberEntry(int message, int arg1)
