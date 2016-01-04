2e184e8917397865ceb9b947eeaff299111c2437
Anas Nashif <anas.nashif@intel.com>
2016-01-04 15:43:32 -0500

    arc: fixed missed rename of _IrqVectorTable
    _IrqVectorTable was renamed to _irq_vector_table
    
    Change-Id: I1488bebc7d8174c08f3ce2dc8bcace6ef567aad6
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 8c31ff783ad7ee84c530834d9feb1243b8ae2641

--- arch/arc/soc/quark_se_ss/irq_vector_table.c
+++ arch/arc/soc/quark_se_ss/irq_vector_table.c
@@ -1,3 +1,3 @@
-DECL|_IrqVectorTable|variable|_IrqVectorTable
-DECL|_IrqVectorTable|variable|_IrqVectorTable
+DECL|_irq_vector_table|variable|_irq_vector_table
+DECL|_irq_vector_table|variable|_irq_vector_table
 DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
