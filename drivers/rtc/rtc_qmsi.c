c82f61015acfaa5ce16d0c8e7157c9a0432e85c6
Andre Guedes <andre.guedes@intel.com>
2016-01-06 16:59:18 -0200

    rtc: Introduce QMSI RTC device driver
    This patch introduces the 'QMSI RTC device driver' which is simply a shim
    driver based on RTC driver provided by QMSI BSP.
    
    Some config options are independent of the driver implementation used,
    so use a consistent name for them. In this case RTC Interrupt number and
    Priority use the same config options for both the QMSI and DesignWare
    drivers.
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_RTC=y
    CONFIG_RTC_QMSI=y
    
    Change-Id: I48292406e5472e5786f3b9abbeb71016a273bfec
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 7b0076a7778099f00b249ff0278350d0368deece

---
+++ drivers/rtc/rtc_qmsi.c
@@ -0,0 +1,11 @@
+DECL|api|variable|api
+DECL|rtc_callback|function|static void rtc_callback(void)
+DECL|rtc_qmsi_dev|variable|rtc_qmsi_dev
+DECL|rtc_qmsi_dev|variable|rtc_qmsi_dev
+DECL|rtc_qmsi_disable|function|static void rtc_qmsi_disable(struct device *dev)
+DECL|rtc_qmsi_enable|function|static void rtc_qmsi_enable(struct device *dev)
+DECL|rtc_qmsi_init|function|static int rtc_qmsi_init(struct device *dev)
+DECL|rtc_qmsi_read|function|static uint32_t rtc_qmsi_read(struct device *dev)
+DECL|rtc_qmsi_set_alarm|function|static int rtc_qmsi_set_alarm(struct device *dev, const uint32_t alarm_val)
+DECL|rtc_qmsi_set_config|function|static int rtc_qmsi_set_config(struct device *dev, struct rtc_config *cfg)
+DECL|user_callback|variable|user_callback
--- samples/nanokernel/apps/rtc/src/main.c
+++ samples/nanokernel/apps/rtc/src/main.c
@@ -1,4 +1,3 @@
 DECL|ALARM|macro|ALARM
-DECL|RTC_DRIVER|macro|RTC_DRIVER
 DECL|main|function|void main(void)
 DECL|test_rtc_interrupt_fn|function|void test_rtc_interrupt_fn(struct device *rtc_dev)
