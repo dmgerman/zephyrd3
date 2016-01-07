85fcf381283d7fa33cf8ddb9af1c304f1648b7a3
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-01-07 13:34:09 +0100

    contiki: clock: Tiny cleanup
    Use existing macros from Zephyr and apply proper code style.
    
    Change-Id: Ia3d572054aa702ce33ceae04eff931cfb900c45c
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: fa79a5df21057d405d5a287aa3ca88afd378006b

--- net/ip/contiki/os/sys/clock.h
+++ net/ip/contiki/os/sys/clock.h
@@ -3,4 +3,4 @@ DECL|CLOCK_H_|macro|CLOCK_H_
 DECL|CLOCK_MSEC_TO_CYCLES|macro|CLOCK_MSEC_TO_CYCLES
 DECL|CLOCK_SECOND|macro|CLOCK_SECOND
 DECL|CLOCK_SECOND|macro|CLOCK_SECOND
-DECL|clock_get_cycle|function|static inline uint32_t clock_get_cycle(void) { return sys_cycle_get_32(); }
+DECL|clock_get_cycle|function|static inline uint32_t clock_get_cycle(void)
