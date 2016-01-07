5aee38aed234ddab1bcc00b7206472f91274aeaf
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-01-07 22:51:27 +0100

    Bluetooth: BR/EDR: Define HCI Link Key request
    Enable HCI protocol for handling Link Key request event and
    responses.
    
    Change-Id: Ib3e24782257a87028b63198d210203b442883211
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 9b0d42930c68a822bbb0f961d8123611b9bfa791

--- include/bluetooth/hci.h
+++ include/bluetooth/hci.h
@@ -66,6 +66,7 @@ DECL|BT_HCI_EVT_LE_LTK_REQUEST|macro|BT_HCI_EVT_LE_LTK_REQUEST
 DECL|BT_HCI_EVT_LE_META_EVENT|macro|BT_HCI_EVT_LE_META_EVENT
 DECL|BT_HCI_EVT_LE_P256_PUBLIC_KEY_COMPLETE|macro|BT_HCI_EVT_LE_P256_PUBLIC_KEY_COMPLETE
 DECL|BT_HCI_EVT_LINK_KEY_NOTIFY|macro|BT_HCI_EVT_LINK_KEY_NOTIFY
+DECL|BT_HCI_EVT_LINK_KEY_REQ|macro|BT_HCI_EVT_LINK_KEY_REQ
 DECL|BT_HCI_EVT_NUM_COMPLETED_PACKETS|macro|BT_HCI_EVT_NUM_COMPLETED_PACKETS
 DECL|BT_HCI_EVT_PIN_CODE_REQ|macro|BT_HCI_EVT_PIN_CODE_REQ
 DECL|BT_HCI_EV_LE_REMOTE_FEAT_COMPLETE|macro|BT_HCI_EV_LE_REMOTE_FEAT_COMPLETE
@@ -106,6 +107,8 @@ DECL|BT_HCI_OP_LE_SET_SCAN_PARAMS|macro|BT_HCI_OP_LE_SET_SCAN_PARAMS
 DECL|BT_HCI_OP_LE_SET_SCAN_RSP_DATA|macro|BT_HCI_OP_LE_SET_SCAN_RSP_DATA
 DECL|BT_HCI_OP_LE_START_ENCRYPTION|macro|BT_HCI_OP_LE_START_ENCRYPTION
 DECL|BT_HCI_OP_LE_WRITE_LE_HOST_SUPP|macro|BT_HCI_OP_LE_WRITE_LE_HOST_SUPP
+DECL|BT_HCI_OP_LINK_KEY_NEG_REPLY|macro|BT_HCI_OP_LINK_KEY_NEG_REPLY
+DECL|BT_HCI_OP_LINK_KEY_REPLY|macro|BT_HCI_OP_LINK_KEY_REPLY
 DECL|BT_HCI_OP_PIN_CODE_NEG_REPLY|macro|BT_HCI_OP_PIN_CODE_NEG_REPLY
 DECL|BT_HCI_OP_PIN_CODE_REPLY|macro|BT_HCI_OP_PIN_CODE_REPLY
 DECL|BT_HCI_OP_READ_BD_ADDR|macro|BT_HCI_OP_READ_BD_ADDR
@@ -208,12 +211,18 @@ DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
 DECL|acl_max_len|member|uint16_t acl_max_len;
 DECL|acl_max_num|member|uint16_t acl_max_num;
 DECL|acl_mtu|member|uint16_t acl_mtu;
 DECL|acl_pkts|member|uint16_t acl_pkts;
 DECL|addr_type|member|uint8_t addr_type;
 DECL|addr|member|bt_addr_le_t addr;
+DECL|bdaddr|member|bt_addr_t bdaddr;
+DECL|bdaddr|member|bt_addr_t bdaddr;
+DECL|bdaddr|member|bt_addr_t bdaddr;
 DECL|bdaddr|member|bt_addr_t bdaddr;
 DECL|bdaddr|member|bt_addr_t bdaddr;
 DECL|bdaddr|member|bt_addr_t bdaddr;
@@ -252,6 +261,8 @@ DECL|bt_hci_cp_le_set_scan_enable|struct|struct bt_hci_cp_le_set_scan_enable {
 DECL|bt_hci_cp_le_set_scan_params|struct|struct bt_hci_cp_le_set_scan_params {
 DECL|bt_hci_cp_le_set_scan_rsp_data|struct|struct bt_hci_cp_le_set_scan_rsp_data {
 DECL|bt_hci_cp_le_start_encryption|struct|struct bt_hci_cp_le_start_encryption {
+DECL|bt_hci_cp_link_key_neg_reply|struct|struct bt_hci_cp_link_key_neg_reply {
+DECL|bt_hci_cp_link_key_reply|struct|struct bt_hci_cp_link_key_reply {
 DECL|bt_hci_cp_pin_code_neg_reply|struct|struct bt_hci_cp_pin_code_neg_reply {
 DECL|bt_hci_cp_pin_code_reply|struct|struct bt_hci_cp_pin_code_reply {
 DECL|bt_hci_cp_reject_conn_req|struct|struct bt_hci_cp_reject_conn_req {
@@ -274,6 +285,7 @@ DECL|bt_hci_evt_le_generate_dhkey_complete|struct|struct bt_hci_evt_le_generate_
 DECL|bt_hci_evt_le_ltk_request|struct|struct bt_hci_evt_le_ltk_request {
 DECL|bt_hci_evt_le_meta_event|struct|struct bt_hci_evt_le_meta_event {
 DECL|bt_hci_evt_le_p256_public_key_complete|struct|struct bt_hci_evt_le_p256_public_key_complete {
+DECL|bt_hci_evt_link_key_req|struct|struct bt_hci_evt_link_key_req {
 DECL|bt_hci_evt_num_completed_packets|struct|struct bt_hci_evt_num_completed_packets {
 DECL|bt_hci_evt_pin_code_req|struct|struct bt_hci_evt_pin_code_req {
 DECL|bt_hci_handle_count|struct|struct bt_hci_handle_count {
@@ -371,6 +383,7 @@ DECL|len|member|uint8_t len;
 DECL|len|member|uint8_t len;
 DECL|len|member|uint8_t len;
 DECL|le|member|uint8_t le;
+DECL|link_key|member|uint8_t link_key[16];
 DECL|link_key|member|uint8_t link_key[16];
 DECL|link_type|member|uint8_t link_type;
 DECL|link_type|member|uint8_t link_type;
