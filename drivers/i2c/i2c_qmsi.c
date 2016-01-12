221ee43fb6fd56be0ae7cea72e3558997857e486
Andre Guedes <andre.guedes@intel.com>
2016-01-12 11:56:02 -0200

    i2c: Introduce QMSI I2C driver
    This patch introduces the QMSI I2C driver which is simply a shim driver
    based on I2C driver provided by QMSI BSP.
    
    This initial version supports only I2C_0 controller in 'master' mode.
    Due to some issues with qm_i2c_master_irq_transfer API, the driver uses
    the polling APIs from QMSI. This means that the current thread doesn't
    sleep (i.e. is not scheduled out) while the I2C transfer is carried out.
    The qm_i2c_master_irq_transfer() issue is under investigation.
    
    Below follows the missing features which will be addressed by a new
    version of this patch or by upcoming patches.
      * Block the current thread until the I2C operation has actually
        completed.
      * Add support for multiple controllers.
      * Add support for slave mode.
      * Add support for suspend() and resume() APIs.
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_I2C=y
    CONFIG_I2C_QMSI=y
    
    Finally, due to the lack of proper support for Quark SE in QMSI 1.0.0,
    this driver requires QMSI version greater than 1.0.0.
    
    This driver has been tested only with Quark SE based boards.
    
    Change-Id: I369992c7b5fe37f7e0b45be7f19e667ad1fca9ac
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: 959632684c613ed1e5afc523827c8251e2972aa3

---
+++ drivers/i2c/i2c_qmsi.c
@@ -0,0 +1,6 @@
+DECL|api|variable|api
+DECL|i2c_qmsi_configure|function|static int i2c_qmsi_configure(struct device *dev, uint32_t config)
+DECL|i2c_qmsi_init|function|static int i2c_qmsi_init(struct device *dev)
+DECL|i2c_qmsi_resume|function|static int i2c_qmsi_resume(struct device *dev)
+DECL|i2c_qmsi_suspend|function|static int i2c_qmsi_suspend(struct device *dev)
+DECL|i2c_qmsi_transfer|function|static int i2c_qmsi_transfer(struct device *dev, struct i2c_msg *msgs, uint8_t num_msgs, uint16_t addr)
