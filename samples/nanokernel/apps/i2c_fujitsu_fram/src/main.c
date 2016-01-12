338c491767b87a9bfe36b599240c7982389c0661
Daniel Leung <daniel.leung@intel.com>
2016-01-12 11:17:12 -0800

    samples/i2c_fujitsu_fram: do some multi-bytes read/write
    This extends the i2c_fujitsu_fram sample app to do multi-bytes.
    
    Change-Id: I20ea0e08c340cf94158ad521c27d5315e522b6de
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 9b8acb7b2623490b1511bc464952807972ffab23

--- samples/nanokernel/apps/i2c_fujitsu_fram/src/main.c
+++ samples/nanokernel/apps/i2c_fujitsu_fram/src/main.c
@@ -1,4 +1,4 @@
 DECL|FRAM_I2C_ADDR|macro|FRAM_I2C_ADDR
 DECL|main|function|void main(void)
-DECL|read_byte|function|int read_byte(struct device *i2c_dev, uint16_t addr, uint8_t *data)
-DECL|write_byte|function|int write_byte(struct device *i2c_dev, uint16_t addr, uint8_t data)
+DECL|read_bytes|function|int read_bytes(struct device *i2c_dev, uint16_t addr, uint8_t *data, uint32_t num_bytes)
+DECL|write_bytes|function|int write_bytes(struct device *i2c_dev, uint16_t addr,uint8_t *data, uint32_t num_bytes)
