62288c4b75112162248cb4570370a352e8666ee3
Johan Hedberg <johan.hedberg@intel.com>
2016-01-14 18:16:51 +0200

    drivers/nble: Move conn.h functions from gap.c to conn.c
    Change-Id: I123e51f02e474ba1b0b7ac195a0da5900b523a53
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: d565ac5e1f96fff20064357772c26891610b9b74

--- drivers/nble/conn.c
+++ drivers/nble/conn.c
@@ -1,3 +1,7 @@
+DECL|bt_auth_cancel|function|int bt_auth_cancel(struct bt_conn *conn)
+DECL|bt_auth_cb_register|function|int bt_auth_cb_register(const struct bt_auth_cb *cb)
+DECL|bt_auth_passkey_confirm|function|int bt_auth_passkey_confirm(struct bt_conn *conn, bool match)
+DECL|bt_auth_passkey_entry|function|int bt_auth_passkey_entry(struct bt_conn *conn, unsigned int passkey)
 DECL|bt_conn_cb_register|function|void bt_conn_cb_register(struct bt_conn_cb *cb)
 DECL|bt_conn_create_le|function|struct bt_conn *bt_conn_create_le(const bt_addr_le_t *peer, const struct bt_le_conn_param *param)
 DECL|bt_conn_disconnect|function|int bt_conn_disconnect(struct bt_conn *conn, uint8_t reason)
@@ -8,3 +12,4 @@ DECL|bt_conn_lookup_addr_le|function|struct bt_conn *bt_conn_lookup_addr_le(cons
 DECL|bt_conn_ref|function|struct bt_conn *bt_conn_ref(struct bt_conn *conn)
 DECL|bt_conn_security|function|int bt_conn_security(struct bt_conn *conn, bt_security_t sec)
 DECL|bt_conn_unref|function|void bt_conn_unref(struct bt_conn *conn)
+DECL|bt_le_set_auto_conn|function|int bt_le_set_auto_conn(bt_addr_le_t *addr,const struct bt_le_conn_param *param)
