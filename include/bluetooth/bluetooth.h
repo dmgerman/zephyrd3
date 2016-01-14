41a82d827ed8a1e728b55d2326693b2b7ef4a914
Johan Hedberg <johan.hedberg@intel.com>
2016-01-14 15:02:54 +0200

    Bluetooth: Move all bt_conn related APIs to conn.h
    To have some proper split of conn.h and bluetooth.h APIs make sure all
    bt_conn related ones are in conn.h. This also helps avoid forward
    declarations for some upcoming patches where we need bluetooth.h
    defines from within conn.h.
    
    Change-Id: Ief3d32118a6749fb5785dab6cb3fee4ebb86ddb4
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 25f6579016f3520ad3f4d3f3ac6063114dfd0919

--- include/bluetooth/bluetooth.h
+++ include/bluetooth/bluetooth.h
@@ -13,23 +13,17 @@ DECL|__BT_BLUETOOTH_H|macro|__BT_BLUETOOTH_H
 DECL|addr_type|member|uint8_t addr_type;
 DECL|bt_addr_le_to_str|function|static inline int bt_addr_le_to_str(const bt_addr_le_t *addr, char *str, size_t len)
 DECL|bt_addr_to_str|function|static inline int bt_addr_to_str(const bt_addr_t *addr, char *str, size_t len)
-DECL|bt_auth_cb|struct|struct bt_auth_cb {
 DECL|bt_data|struct|struct bt_data {
 DECL|bt_le_adv_param|struct|struct bt_le_adv_param {
 DECL|bt_le_scan_cb_t|typedef|typedef void bt_le_scan_cb_t(const bt_addr_le_t *addr, int8_t rssi,
 DECL|bt_le_scan_param|struct|struct bt_le_scan_param {
 DECL|bt_ready_cb_t|typedef|typedef void (*bt_ready_cb_t)(int err);
-DECL|cancel|member|void (*cancel)(struct bt_conn *conn);
 DECL|data_len|member|uint8_t data_len;
 DECL|data|member|const uint8_t *data;
 DECL|filter_dup|member|uint8_t filter_dup;
 DECL|interval_max|member|uint16_t interval_max;
 DECL|interval_min|member|uint16_t interval_min;
 DECL|interval|member|uint16_t interval;
-DECL|passkey_confirm|member|void (*passkey_confirm)(struct bt_conn *conn, unsigned int passkey);
-DECL|passkey_display|member|void (*passkey_display)(struct bt_conn *conn, unsigned int passkey);
-DECL|passkey_entry|member|void (*passkey_entry)(struct bt_conn *conn);
-DECL|pincode_entry|member|void (*pincode_entry)(struct bt_conn *conn, bool highsec);
 DECL|type|member|uint8_t type;
 DECL|type|member|uint8_t type;
 DECL|type|member|uint8_t type;
--- include/bluetooth/conn.h
+++ include/bluetooth/conn.h
@@ -10,12 +10,14 @@ DECL|__BT_CONN_H|macro|__BT_CONN_H
 DECL|__packed|enum|typedef enum __packed {
 DECL|_next|member|struct bt_conn_cb *_next;
 DECL|br|member|struct bt_conn_br_info br;
+DECL|bt_auth_cb|struct|struct bt_auth_cb {
 DECL|bt_conn_br_info|struct|struct bt_conn_br_info {
 DECL|bt_conn_cb|struct|struct bt_conn_cb {
 DECL|bt_conn_info|struct|struct bt_conn_info {
 DECL|bt_conn_le_info|struct|struct bt_conn_le_info {
 DECL|bt_le_conn_param|struct|struct bt_le_conn_param {
 DECL|bt_security_t|typedef|} bt_security_t;
+DECL|cancel|member|void (*cancel)(struct bt_conn *conn);
 DECL|connected|member|void (*connected)(struct bt_conn *conn, uint8_t err);
 DECL|disconnected|member|void (*disconnected)(struct bt_conn *conn, uint8_t reason);
 DECL|dst|member|const bt_addr_le_t *dst; /** Destination Address */
@@ -25,6 +27,10 @@ DECL|interval_max|member|uint16_t interval_max;
 DECL|interval_min|member|uint16_t interval_min;
 DECL|latency|member|uint16_t latency;
 DECL|le|member|struct bt_conn_le_info le;
+DECL|passkey_confirm|member|void (*passkey_confirm)(struct bt_conn *conn, unsigned int passkey);
+DECL|passkey_display|member|void (*passkey_display)(struct bt_conn *conn, unsigned int passkey);
+DECL|passkey_entry|member|void (*passkey_entry)(struct bt_conn *conn);
+DECL|pincode_entry|member|void (*pincode_entry)(struct bt_conn *conn, bool highsec);
 DECL|security_changed|member|void (*security_changed)(struct bt_conn *conn, bt_security_t level);
 DECL|src|member|const bt_addr_le_t *src; /** Source Address */
 DECL|timeout|member|uint16_t timeout;
