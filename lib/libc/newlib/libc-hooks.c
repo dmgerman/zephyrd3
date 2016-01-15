86917ab0ec9a2adb46a7755d22107b7041617a9d
Andre Guedes <andre.guedes@intel.com>
2016-01-15 19:29:21 -0200

    newlib: Rename exit to _exit
    This patch renames the 'exit' function from newlib/libc-hooks.c to
    '_exit' since this is the correct name of the newlib system call.
    
    This patch fixes some linking errors that occur when an applications
    uses abort() or assert().
    
    Change-Id: I593e5ec5dc0f84dbbebe4aceb38703256e140914
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: aa47bd8d6c6edd4a70c9f6f0e793e60b147b4021

--- lib/libc/newlib/libc-hooks.c
+++ lib/libc/newlib/libc-hooks.c
@@ -1,13 +1,13 @@
 DECL|HEAP_SIZE|macro|HEAP_SIZE
 DECL|__stdin_hook_install|function|void __stdin_hook_install(unsigned char (*hook)(void))
 DECL|__stdout_hook_install|function|void __stdout_hook_install(int (*hook)(int))
+DECL|_exit|function|void _exit(int status)
 DECL|_fstat|function|int _fstat(int file, struct stat *st)
 DECL|_stdin_hook_default|function|static unsigned char _stdin_hook_default(void)
 DECL|_stdin_hook|variable|_stdin_hook
 DECL|_stdout_hook_default|function|static int _stdout_hook_default(int c)
 DECL|_stdout_hook|variable|_stdout_hook
 DECL|close|function|int close(int file)
-DECL|exit|function|void exit(int status)
 DECL|fstat|function|int fstat(int fd, struct stat *buf)
 DECL|getpid|function|int getpid(void)
 DECL|heap_sz|variable|heap_sz
