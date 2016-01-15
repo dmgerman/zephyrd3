6f9604e86035a46328e26094830e677b2bc0fc38
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-01-15 08:33:13 -0300

    Bluetooth: GATT: Add bt_gatt_attr_next function
    This add bt_gatt_attr_next function that can be used to iterate to next
    attribute which is convenient if original attribute is known and using
    bt_gatt_foreach_attr would require a lookup and another function to pass
    as callback.
    
    Change-Id: I1bd522fd4ae784e08aa375b35320191cbfc03a54
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: ea1534f1009fa5193df2b3e8a7dbee44eb8dc6fa

--- net/bluetooth/gatt.c
+++ net/bluetooth/gatt.c
@@ -1,7 +1,5 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
-DECL|BT_GATT_ATTR_NEXT|macro|BT_GATT_ATTR_NEXT
-DECL|BT_GATT_ATTR_NEXT|macro|BT_GATT_ATTR_NEXT
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|att_find_info_rsp|function|static void att_find_info_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
@@ -17,6 +15,7 @@ DECL|att_read_type|function|static int att_read_type(struct bt_conn *conn, struc
 DECL|att_write_ccc_rsp|function|static void att_write_ccc_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|att_write_rsp|function|static void att_write_rsp(struct bt_conn *conn, uint8_t err, const void *pdu, uint16_t length, void *user_data)
 DECL|attr_count|variable|attr_count
+DECL|bt_gatt_attr_next|function|struct bt_gatt_attr *bt_gatt_attr_next(const struct bt_gatt_attr *attr)
 DECL|bt_gatt_attr_read_ccc|function|int bt_gatt_attr_read_ccc(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|bt_gatt_attr_read_cep|function|int bt_gatt_attr_read_cep(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|bt_gatt_attr_read_chrc|function|int bt_gatt_attr_read_chrc(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
