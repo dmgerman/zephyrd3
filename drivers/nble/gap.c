06c167d1333a91b2bc03fa56b757f95312ea17fc
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-01-15 15:45:13 +0200

    Bluetooth: nble: Implement bt_enable
    Resets Nordic BLE chip and opens communication channel to UART.
    
    Change-Id: Ie516869a4d978fc1622361ebc42894430fdf9ea6
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 124795afed703a7a1adbab21b0fc5d7b0afebfe3

--- drivers/nble/gap.c
+++ drivers/nble/gap.c
@@ -1,5 +1,10 @@
+DECL|NBLE_CHANNEL|macro|NBLE_CHANNEL
+DECL|NBLE_RESET_PIN|macro|NBLE_RESET_PIN
+DECL|NBLE_SWDIO_PIN|macro|NBLE_SWDIO_PIN
 DECL|bt_enable|function|int bt_enable(bt_ready_cb_t cb)
 DECL|bt_le_adv_start|function|int bt_le_adv_start(const struct bt_le_adv_param *param, const struct bt_data *ad, size_t ad_len, const struct bt_data *sd, size_t sd_len)
 DECL|bt_le_adv_stop|function|int bt_le_adv_stop(void)
 DECL|bt_le_scan_start|function|int bt_le_scan_start(const struct bt_le_scan_param *param, bt_le_scan_cb_t cb)
 DECL|bt_le_scan_stop|function|int bt_le_scan_stop(void)
+DECL|channel|variable|channel
+DECL|recv_cb|function|static int recv_cb(int channel, int request, int len, void *p_data)
