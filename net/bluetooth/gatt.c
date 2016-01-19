441ad66c86c735659c6a0e4556332b5f1a34ec13
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-01-19 13:18:21 +0200

    Bluetooth: GATT: Replace handle with attribute object in bt_gatt_notify
    This make more sense since the handles are normally self allocated by
    the stack.
    
    Change-Id: I198dd9c3ef6259cff8a0e528514918ec18990dea
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: d65dcc5e4b5d541ffc67b1348aa6f15dd6a4e975

--- net/bluetooth/gatt.c
+++ net/bluetooth/gatt.c
@@ -32,7 +32,7 @@ DECL|bt_gatt_discover|function|int bt_gatt_discover(struct bt_conn *conn, struct
 DECL|bt_gatt_exchange_mtu|function|int bt_gatt_exchange_mtu(struct bt_conn *conn, bt_gatt_rsp_func_t func)
 DECL|bt_gatt_foreach_attr|function|void bt_gatt_foreach_attr(uint16_t start_handle, uint16_t end_handle, bt_gatt_attr_func_t func, void *user_data)
 DECL|bt_gatt_notification|function|void bt_gatt_notification(struct bt_conn *conn, uint16_t handle, const void *data, uint16_t length)
-DECL|bt_gatt_notify|function|int bt_gatt_notify(struct bt_conn *conn, uint16_t handle, const void *data, uint16_t len)
+DECL|bt_gatt_notify|function|int bt_gatt_notify(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *data, uint16_t len)
 DECL|bt_gatt_read_multiple|function|int bt_gatt_read_multiple(struct bt_conn *conn, const uint16_t *handles, size_t count, bt_gatt_read_func_t func)
 DECL|bt_gatt_read|function|int bt_gatt_read(struct bt_conn *conn, struct bt_gatt_read_params *params)
 DECL|bt_gatt_register|function|int bt_gatt_register(struct bt_gatt_attr *attrs, size_t count)
