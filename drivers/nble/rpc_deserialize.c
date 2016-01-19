06ec0c0e8d0f87175b06875e6632d8b3b019fd12
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-01-19 14:13:44 +0200

    Bluetooth: nble: Add RPC deserialization functions
    Port RPC deserialization to Zephyr upstream.
    
    Change-Id: Icc9e055565185e907f51832443386f929a752586
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 14613089e5ab3627144c61a50560d5c2c4423727

---
+++ drivers/nble/rpc_deserialize.c
@@ -0,0 +1,99 @@
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|LIST_FN_SIG_B_B_P|enumerator|enum { LIST_FN_SIG_B_B_P fn_b_b_p_index_max };
+DECL|LIST_FN_SIG_NONE|enumerator|enum { LIST_FN_SIG_NONE fn_none_index_max };
+DECL|LIST_FN_SIG_P|enumerator|enum { LIST_FN_SIG_P fn_p_index_max };
+DECL|LIST_FN_SIG_S_B_B_P|enumerator|enum { LIST_FN_SIG_S_B_B_P fn_s_b_b_p_index_max };
+DECL|LIST_FN_SIG_S_B_P|enumerator|enum { LIST_FN_SIG_S_B_P fn_s_b_p_index_max };
+DECL|LIST_FN_SIG_S_B|enumerator|enum { LIST_FN_SIG_S_B fn_s_b_index_max };
+DECL|LIST_FN_SIG_S_P|enumerator|enum { LIST_FN_SIG_S_P fn_s_p_index_max };
+DECL|LIST_FN_SIG_S|enumerator|enum { LIST_FN_SIG_S fn_s_index_max };
+DECL|PRINTK|macro|PRINTK
+DECL|PRINTK|macro|PRINTK
+DECL|deserialize_b_b_p|function|static void deserialize_b_b_p(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|deserialize_buf|function|static const uint8_t *deserialize_buf(const uint8_t *p, const uint8_t **pp_buf, uint16_t *p_buflen)
+DECL|deserialize_none|function|static void deserialize_none(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|deserialize_p|function|static void deserialize_p(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|deserialize_s_b_b_p|function|static void deserialize_s_b_b_p(uint8_t fn_index, const uint8_t *buf,uint16_t length)
+DECL|deserialize_s_b_p|function|static void deserialize_s_b_p(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|deserialize_s_b|function|static void deserialize_s_b(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|deserialize_s_p|function|static void deserialize_s_p(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|deserialize_struct|function|static const uint8_t *deserialize_struct(const uint8_t *p, const uint8_t **pp_struct, uint8_t *p_struct_length)
+DECL|deserialize_s|function|static void deserialize_s(uint8_t fn_index, const uint8_t *buf, uint16_t length)
+DECL|fn_b_b_p_index_max|enumerator|enum { LIST_FN_SIG_B_B_P fn_b_b_p_index_max };
+DECL|fn_none_index_max|enumerator|enum { LIST_FN_SIG_NONE fn_none_index_max };
+DECL|fn_p_index_max|enumerator|enum { LIST_FN_SIG_P fn_p_index_max };
+DECL|fn_s_b_b_p_index_max|enumerator|enum { LIST_FN_SIG_S_B_B_P fn_s_b_b_p_index_max };
+DECL|fn_s_b_index_max|enumerator|enum { LIST_FN_SIG_S_B fn_s_b_index_max };
+DECL|fn_s_b_p_index_max|enumerator|enum { LIST_FN_SIG_S_B_P fn_s_b_p_index_max };
+DECL|fn_s_index_max|enumerator|enum { LIST_FN_SIG_S fn_s_index_max };
+DECL|fn_s_p_index_max|enumerator|enum { LIST_FN_SIG_S_P fn_s_p_index_max };
+DECL|m_fct_b_b_p|variable|m_fct_b_b_p
+DECL|m_fct_none|variable|m_fct_none
+DECL|m_fct_p|variable|m_fct_p
+DECL|m_fct_s_b_b_p|variable|m_fct_s_b_b_p
+DECL|m_fct_s_b_p|variable|m_fct_s_b_p
+DECL|m_fct_s_b|variable|m_fct_s_b
+DECL|m_fct_s_p|variable|m_fct_s_p
+DECL|m_fct_s|variable|m_fct_s
+DECL|m_size_s_b_b_p|variable|m_size_s_b_b_p
+DECL|m_size_s_b_p|variable|m_size_s_b_p
+DECL|m_size_s_b|variable|m_size_s_b
+DECL|m_size_s_p|variable|m_size_s_p
+DECL|m_size_s|variable|m_size_s
+DECL|panic|function|static void panic(int err)
+DECL|rpc_deserialize|function|void rpc_deserialize(const uint8_t *buf, uint16_t length)
