f07cbea8b50cf0d04b25983320b15ce0eb403a87
Andrew Boie <andrew.p.boie@intel.com>
2016-01-20 14:32:45 -0800

    x86/iamcu: improve _execute_handler() readability, small optimization
    It wasn't immediately clear in _execute_handler() that
    enable_interrupts() didn't actually do anything if
    CONFIG_NESTED_INTERRUPTS wasn't enabled.
    
    If we are not using nested interrupts, perform small optimization
    1) The call to disable interrupts isn't necessary as interrupts
    are already disasbled
    2) The check for !_nanokernel.nested in a couple places always
    evaluates to true if nested interrupts aren't used
    
    Change-Id: I7e66cee0466d03bc1172a572e19389accf6e0e62
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 123c33a8368eb7ea888cbc61c9b53c9a91c1bc51

--- arch/x86/core/iamcu_abi/intstub.c
+++ arch/x86/core/iamcu_abi/intstub.c
@@ -1,7 +1,8 @@
 DECL|_SpuriousIntHandler|function|void _SpuriousIntHandler(void)
 DECL|_SpuriousIntNoErrCodeHandler|function|void _SpuriousIntNoErrCodeHandler(void)
 DECL|_execute_handler|function|void _execute_handler(int_handler_t function, int context)
-DECL|disable_interrupts|function|static inline void disable_interrupts(void)
-DECL|enable_interrupts|function|static inline void enable_interrupts(void)
-DECL|enable_interrupts|function|static inline void enable_interrupts(void){};
+DECL|disable_nested_interrupts|function|static inline void disable_nested_interrupts(void)
+DECL|disable_nested_interrupts|function|static inline void disable_nested_interrupts(void){};
+DECL|enable_nested_interrupts|function|static inline void enable_nested_interrupts(void)
+DECL|enable_nested_interrupts|function|static inline void enable_nested_interrupts(void){};
 DECL|int_handler_t|typedef|typedef void (*int_handler_t) (int context);
