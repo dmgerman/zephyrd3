7620752390dd3ec9c502f7de244266dab61406f5
Daniel Leung <daniel.leung@intel.com>
2016-01-21 15:02:06 -0800

    samples: add a sample app using TI INA219 power monitor
    This is a sample app to interface with TI INA219 power monitor.
    The values used in the app are for use on Adafruit's breakout board
    (https://www.adafruit.com/products/904).
    
    This assumes the slave address is 0x40, where A0 and A1 are all
    tied to ground.
    
    Change-Id: I4116629171a98be6f2b9aa422dd3eb386b4d59c2
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 11f97de957695d4f7a812ab559f73e538ae6be33

---
+++ samples/nanokernel/apps/current_sensing/src/main.c
@@ -0,0 +1,7 @@
+DECL|CAL_VAL|macro|CAL_VAL
+DECL|CUR_LSB|macro|CUR_LSB
+DECL|I2C_SLV_ADDR|macro|I2C_SLV_ADDR
+DECL|PWR_LSB|macro|PWR_LSB
+DECL|main|function|void main(void)
+DECL|read_reg16|function|int read_reg16(struct device *i2c_dev, uint8_t reg_addr, uint8_t *data)
+DECL|write_reg16|function|int write_reg16(struct device *i2c_dev, uint8_t reg_addr,uint8_t *data)
