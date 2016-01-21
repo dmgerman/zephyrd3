60aaf23328be0ce42c4be3eadc6bc108c628e59a
Andrew Boie <andrew.p.boie@intel.com>
2016-01-21 10:59:46 -0800

    microkernel: remove task_irq_free() API
    This is being removed for a few reasons:
    
    - AFAICT this is the only API in Zephyr that follows an allocate/free
    model.
    
    - There are no public APIs in the interrupt subsystem for releasing
    or reconfiguring an interrupt. This code was relying on arch-specific
    private APIs. If we really want to keep this capability we should
    make these APIs public and consistent across arches.
    
    - The use-case for this API is not clear, as Zephyr is not intended
    for hot-pluggable peripherals. Built-in hardware tends to need its
    interrupt for its entire life cycle.
    
    - The current implementation of dynamic interrupts on x86 does not
    support freeing a dynamic IRQ that was reserved with
    irq_connect_dynamic(), causing this code not to work. To add this
    would require reimplementing _get_dynamic_stub() to use a bitfield
    or set of bitfields to track unused stubs rather than the simple
    counter it uses now.
    
    Change-Id: I7a03c134fb3498b91a1816318a88b293e26b846c
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 583126adf92fdb4f436f6c9a6c58bee9afa322d8

--- kernel/microkernel/k_irq.c
+++ kernel/microkernel/k_irq.c
@@ -1,7 +1,5 @@
 DECL|INVALID_TASK|macro|INVALID_TASK
 DECL|MAX_TASK_IRQS|macro|MAX_TASK_IRQS
-DECL|RELEASE_VECTOR|macro|RELEASE_VECTOR
-DECL|RELEASE_VECTOR|macro|RELEASE_VECTOR
 DECL|_k_task_irq_alloc|function|static int _k_task_irq_alloc(void *arg)
 DECL|event|member|kevent_t event; /* event number assigned to task IRQ object */
 DECL|irq_obj_reg_arg|struct|struct irq_obj_reg_arg {
@@ -12,7 +10,6 @@ DECL|task_id|member|ktask_t task_id; /* requesting task */
 DECL|task_id|member|ktask_t task_id; /* task ID of task IRQ object's owner */
 DECL|task_irq_ack|function|void task_irq_ack(kirq_t irq_obj)
 DECL|task_irq_alloc|function|uint32_t task_irq_alloc(kirq_t irq_obj, uint32_t irq, uint32_t priority,uint32_t flags)
-DECL|task_irq_free|function|void task_irq_free(kirq_t irq_obj)
 DECL|task_irq_info|struct|struct task_irq_info {
 DECL|task_irq_int_handler|function|static void task_irq_int_handler(void *parameter)
 DECL|task_irq_object|variable|task_irq_object
