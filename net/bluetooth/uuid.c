75682afc46b3aaaf8c67a7480e1e5d308546c9c9
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-01-22 14:19:12 +0200

    Bluetooth: UUID: Fix conversion to 128 bit format
    The current format used is a swap of RFC 4122 thus the 16 bit values do
    come at the end not at the beggining and data need to be swapped.
    
    Change-Id: I38a27cdf82a28d28e47675f719772278d85deaaa
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 16f398ddf7760b8b8bc8b9863d7b497d04a80626

--- net/bluetooth/uuid.c
+++ net/bluetooth/uuid.c
@@ -1,7 +1,9 @@
+DECL|UUID_16_BASE_OFFSET|macro|UUID_16_BASE_OFFSET
 DECL|bt_uuid_cmp|function|int bt_uuid_cmp(const struct bt_uuid *u1, const struct bt_uuid *u2)
 DECL|bt_uuid_copy|function|void bt_uuid_copy(struct bt_uuid *dst, const struct bt_uuid *src)
 DECL|bt_uuid_str|function|const char *bt_uuid_str(const struct bt_uuid *uuid)
 DECL|bt_uuid_to_str|function|void bt_uuid_to_str(const struct bt_uuid *uuid, char *str, size_t len)
+DECL|cpu_to_uuid128|function|static inline void cpu_to_uuid128(void *dst, const void *src, int len)
 DECL|uuid128_base|variable|uuid128_base
 DECL|uuid128_cmp|function|static int uuid128_cmp(const struct bt_uuid *u1, const struct bt_uuid *u2)
 DECL|uuid_to_uuid128|function|static void uuid_to_uuid128(const struct bt_uuid *src, struct bt_uuid *dst)
