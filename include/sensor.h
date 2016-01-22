722e60837590b220c747875dbcb5c803b13a63a5
Vlad Dogaru <vlad.dogaru@intel.com>
2016-01-22 14:59:10 +0200

    Introduce new sensor API
    Sensor drivers expose one or more channels, corresponding to each
    individual quantity they can measure.  Such quantities may be different
    altogether (e.g. temperature and pressure) or different axes for the
    same unit (e.g. three axes of acceleration).  Before reading channels, a
    driver must be explicitly instructed to obtain a sample from the device.
    This helps accommodate sensors which can only read all channels at once,
    and also helps ensure coherence of measurements and optimize I2C/SPI
    traffic.
    
    Channels can be read as floating point values or struct sensor_value.
    The latter consists of a pair of integers and a type field which
    dictates how to interpret said integers.  The most common type is INT
    (where the second value is ignored) or INT_PLUS_MICRO, which means the
    second value should be multiplied by 1.0e-6 and added to the first.
    
    A sensor driver may support one or more triggers, corresponding to
    interrupts or timers.  Registering for a trigger involves supplying the
    driver with a callback which is called when a condition is reached.
    Examples of trigger types are: data ready, timer expiration, any-motion,
    near/far.
    
    Finally, sensors support attributes such as sample frequency,
    measurement accuracy or threshold values for triggers.  However, runtime
    configuration is discouraged, in the interest of keeping code simple.
    
    Origin: Original
    Change-Id: Id290fe544b6f7eccc4b109f3912fca1692e55623
    Signed-off-by: Vlad Dogaru <vlad.dogaru@intel.com>
    
    Former-commit-id: d1c7c7b7efe54beb2bc269a2c3442ce52cc5a608

---
+++ include/sensor.h
@@ -0,0 +1,57 @@
+DECL|SENSOR_ATTR_LOWER_THRESH|enumerator|SENSOR_ATTR_LOWER_THRESH,
+DECL|SENSOR_ATTR_SAMPLING_FREQUENCY|enumerator|SENSOR_ATTR_SAMPLING_FREQUENCY,
+DECL|SENSOR_ATTR_UPPER_THRESH|enumerator|SENSOR_ATTR_UPPER_THRESH,
+DECL|SENSOR_CHAN_ACCEL_ANY|enumerator|SENSOR_CHAN_ACCEL_ANY,
+DECL|SENSOR_CHAN_ACCEL_X|enumerator|SENSOR_CHAN_ACCEL_X,
+DECL|SENSOR_CHAN_ACCEL_Y|enumerator|SENSOR_CHAN_ACCEL_Y,
+DECL|SENSOR_CHAN_ACCEL_Z|enumerator|SENSOR_CHAN_ACCEL_Z,
+DECL|SENSOR_CHAN_GYRO_ANY|enumerator|SENSOR_CHAN_GYRO_ANY,
+DECL|SENSOR_CHAN_GYRO_X|enumerator|SENSOR_CHAN_GYRO_X,
+DECL|SENSOR_CHAN_GYRO_Y|enumerator|SENSOR_CHAN_GYRO_Y,
+DECL|SENSOR_CHAN_GYRO_Z|enumerator|SENSOR_CHAN_GYRO_Z,
+DECL|SENSOR_CHAN_HUMIDITY|enumerator|SENSOR_CHAN_HUMIDITY,
+DECL|SENSOR_CHAN_IR|enumerator|SENSOR_CHAN_IR,
+DECL|SENSOR_CHAN_LIGHT|enumerator|SENSOR_CHAN_LIGHT,
+DECL|SENSOR_CHAN_PRESS|enumerator|SENSOR_CHAN_PRESS,
+DECL|SENSOR_CHAN_PROX|enumerator|SENSOR_CHAN_PROX,
+DECL|SENSOR_CHAN_TEMP|enumerator|SENSOR_CHAN_TEMP,
+DECL|SENSOR_TRIG_DATA_READY|enumerator|SENSOR_TRIG_DATA_READY,
+DECL|SENSOR_TRIG_DELTA|enumerator|SENSOR_TRIG_DELTA,
+DECL|SENSOR_TRIG_NEAR_FAR|enumerator|SENSOR_TRIG_NEAR_FAR,
+DECL|SENSOR_TRIG_THRESHOLD|enumerator|SENSOR_TRIG_THRESHOLD,
+DECL|SENSOR_TRIG_TIMER|enumerator|SENSOR_TRIG_TIMER,
+DECL|SENSOR_TYPE_DOUBLE|enumerator|SENSOR_TYPE_DOUBLE,
+DECL|SENSOR_TYPE_INT_PLUS_MICRO|enumerator|SENSOR_TYPE_INT_PLUS_MICRO,
+DECL|SENSOR_TYPE_INT|enumerator|SENSOR_TYPE_INT,
+DECL|SENSOR_TYPE_Q16_16|enumerator|SENSOR_TYPE_Q16_16,
+DECL|__SENSOR_H__|macro|__SENSOR_H__
+DECL|arg|member|void *arg;
+DECL|attr_set|member|sensor_attr_set_t attr_set;
+DECL|channel_get|member|sensor_channel_get_t channel_get;
+DECL|chan|member|enum sensor_channel chan;
+DECL|dval|member|double dval;
+DECL|handler|member|sensor_work_handler_t handler;
+DECL|sample_fetch|member|sensor_sample_fetch_t sample_fetch;
+DECL|sensor_attr_set_t|typedef|typedef int (*sensor_attr_set_t)(struct device *dev,
+DECL|sensor_attr_set|function|static inline int sensor_attr_set(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|sensor_attribute|enum|enum sensor_attribute {
+DECL|sensor_channel_get_t|typedef|typedef int (*sensor_channel_get_t)(struct device *dev,
+DECL|sensor_channel_get|function|static inline int sensor_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|sensor_channel|enum|enum sensor_channel {
+DECL|sensor_driver_api|struct|struct sensor_driver_api {
+DECL|sensor_sample_fetch_t|typedef|typedef int (*sensor_sample_fetch_t)(struct device *dev);
+DECL|sensor_sample_fetch|function|static inline int sensor_sample_fetch(struct device *dev)
+DECL|sensor_trigger_handler_t|typedef|typedef void (*sensor_trigger_handler_t)(struct device *dev,
+DECL|sensor_trigger_set_t|typedef|typedef int (*sensor_trigger_set_t)(struct device *dev,
+DECL|sensor_trigger_set|function|static inline int sensor_trigger_set(struct device *dev, struct sensor_trigger *trig, sensor_trigger_handler_t handler)
+DECL|sensor_trigger_type|enum|enum sensor_trigger_type {
+DECL|sensor_trigger|struct|struct sensor_trigger {
+DECL|sensor_value_type|enum|enum sensor_value_type {
+DECL|sensor_value|struct|struct sensor_value {
+DECL|sensor_work_handler_t|typedef|typedef void (*sensor_work_handler_t)(void *arg);
+DECL|sensor_work|struct|struct sensor_work {
+DECL|trigger_set|member|sensor_trigger_set_t trigger_set;
+DECL|type|member|enum sensor_trigger_type type;
+DECL|type|member|enum sensor_value_type type;
+DECL|val1|member|int32_t val1;
+DECL|val2|member|int32_t val2;
