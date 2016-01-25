579e8dc89810978de79dde033ff536f549fbb1ca
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-01-25 10:21:51 +0100

    samples: Add a q&d test application for SPI
    A dummy test to verify SPI driver and API build properly.
    
    Change-Id: Icb1ab282b4a0fa02ec949c96becdfdc16f20bc2d
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: a150c1650ef3823e7e3bdfc8f281f90dc3471a74

---
+++ samples/nanokernel/apps/spi_test/src/spi.c
@@ -0,0 +1,13 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SPI_DRV_NAME|macro|SPI_DRV_NAME
+DECL|SPI_DRV_NAME|macro|SPI_DRV_NAME
+DECL|SPI_MAX_CLK_FREQ_250KHZ|macro|SPI_MAX_CLK_FREQ_250KHZ
+DECL|SPI_SLAVE|macro|SPI_SLAVE
+DECL|SPI_SLAVE|macro|SPI_SLAVE
+DECL|_spi_show|function|static void _spi_show(struct spi_config *spi_conf)
+DECL|main|function|void main(void)
+DECL|print_buf_hex|function|static void print_buf_hex(unsigned char *b, uint32_t len)
+DECL|rbuf|variable|rbuf
+DECL|spi_conf|variable|spi_conf
+DECL|wbuf|variable|wbuf
