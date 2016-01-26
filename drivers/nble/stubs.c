e1554780b2074ad5db822d8869386867c5e0697c
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-01-26 12:05:18 +0200

    Bluetooth: nble: Use weak functions printing debug info
    Since the headers defining RPC with Nordic chip changes frequently
    define weak functions printing BT_WARN("Not implemented") if
    function is not defined.
    Weak function symbol is marked with "W" as viewed by nm.
    
    Change-Id: Ia39b64c6e89fdddc65683711fd192a235fe754d4
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 9c3de9ebcaf4991214d90cf62f276d6c375fccc9

---
+++ drivers/nble/stubs.c
@@ -0,0 +1,9 @@
+DECL|FN_SIG_B_B_P|macro|FN_SIG_B_B_P
+DECL|FN_SIG_NONE|macro|FN_SIG_NONE
+DECL|FN_SIG_P|macro|FN_SIG_P
+DECL|FN_SIG_S_B_B_P|macro|FN_SIG_S_B_B_P
+DECL|FN_SIG_S_B_P|macro|FN_SIG_S_B_P
+DECL|FN_SIG_S_B|macro|FN_SIG_S_B
+DECL|FN_SIG_S_P|macro|FN_SIG_S_P
+DECL|FN_SIG_S|macro|FN_SIG_S
+DECL|__weak|macro|__weak
