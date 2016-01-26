949094a7fbdd367eae306339a1c7d6ed4c137be5
Andre Guedes <andre.guedes@intel.com>
2016-01-26 19:01:24 -0200

    i2c: Add multi controller support for QMSI shim driver
    Currently, the I2C QMSI shim driver supports only the "I2C 0"
    controller instance. This patch extends the driver so it supports
    the "I2C 1" instance as well.
    
    The controller instances can be enabled independently.
    
    Change-Id: Ieb64df46816026f3f4fb262a1682c98ee36bedd5
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: 4c3a1afbac742c34431e127ac717848fc7a2b61f

--- drivers/i2c/i2c_qmsi.c
+++ drivers/i2c/i2c_qmsi.c
@@ -1,6 +1,11 @@
+DECL|GET_CONTROLLER_INSTANCE|macro|GET_CONTROLLER_INSTANCE
 DECL|api|variable|api
+DECL|config_info_0|variable|config_info_0
+DECL|config_info_1|variable|config_info_1
+DECL|i2c_qmsi_config_info|struct|struct i2c_qmsi_config_info {
 DECL|i2c_qmsi_configure|function|static int i2c_qmsi_configure(struct device *dev, uint32_t config)
 DECL|i2c_qmsi_init|function|static int i2c_qmsi_init(struct device *dev)
 DECL|i2c_qmsi_resume|function|static int i2c_qmsi_resume(struct device *dev)
 DECL|i2c_qmsi_suspend|function|static int i2c_qmsi_suspend(struct device *dev)
 DECL|i2c_qmsi_transfer|function|static int i2c_qmsi_transfer(struct device *dev, struct i2c_msg *msgs, uint8_t num_msgs, uint16_t addr)
+DECL|instance|member|qm_i2c_t instance; /* Controller instance. */
