90cafa79e9bc9fbad948d3341456b2f6a62ca4a9
Andrew Boie <andrew.p.boie@intel.com>
2016-01-27 14:12:11 -0800

    x86: fix IDT entry definition
    It was, in a nutshell, wrong. Fortunately, the incorrectly
    specified fields weren't being used by anything.
    
    Change-Id: I0fa63fa16a267502744a7a2c82865c7de8b5446e
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 313ed3125302b0b1ed916801fd1da92258eae6af

--- arch/x86/include/idtEnt.h
+++ arch/x86/include/idtEnt.h
@@ -2,11 +2,17 @@ DECL|IDT_ENTRY|typedef|} __packed IDT_ENTRY;
 DECL|_EXC_ERROR_CODE_FAULTS|macro|_EXC_ERROR_CODE_FAULTS
 DECL|_IDTENT_H|macro|_IDTENT_H
 DECL|_IdtEntCreate|function|static inline void _IdtEntCreate(unsigned long long *pIdtEntry, void (*routine)(void *), unsigned int dpl)
-DECL|descPrivLevel|member|unsigned short descPrivLevel:2;
-DECL|hiOffset|member|unsigned short hiOffset;
-DECL|idtEntry|struct|typedef struct idtEntry {
-DECL|intGateIndicator|member|unsigned short intGateIndicator:8;
-DECL|lowOffset|member|unsigned short lowOffset;
-DECL|present|member|unsigned short present:1;
-DECL|reserved|member|unsigned short reserved:5;
-DECL|segmentSelector|member|unsigned short segmentSelector;
+DECL|always_0_0|member|uint8_t always_0_0:3;
+DECL|always_0_1|member|uint8_t always_0_1:1;
+DECL|dpl|member|uint8_t dpl:2; /* Descriptor privilege level */
+DECL|gate_size|member|uint8_t gate_size:1; /* size of gate, 1: 32-bit, 0:16-bit */
+DECL|idt_entry|struct|typedef struct idt_entry {
+DECL|offset_high|member|uint16_t offset_high;
+DECL|offset_low|member|uint16_t offset_low;
+DECL|present|member|uint8_t present:1; /* present yes/no */
+DECL|reserved_task_gate_0|member|uint16_t reserved_task_gate_0;
+DECL|reserved_task_gate_1|member|uint8_t reserved_task_gate_1;
+DECL|reserved_task_gate_2|member|uint16_t reserved_task_gate_2;
+DECL|reserved|member|uint8_t reserved:5;
+DECL|segment_selector|member|uint16_t segment_selector;
+DECL|type|member|uint8_t type:3; /* task:101, irq:110, trap:111 */
