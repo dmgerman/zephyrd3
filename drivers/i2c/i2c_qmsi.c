1570361c9c9653c2ae3b5d35a63a03f026408021
Andre Guedes <andre.guedes@intel.com>
2016-01-28 15:16:53 -0200

    i2c: Enable interrupts for QMSI shim driver
    Due to an issue with the interrupt-based transfer API from QMSI driver,
    the 'transfer' callback from the shim driver (i2c_qmsi_transfer) is
    implemented with polling APIs. This is not ideal because we are not able
    to sleep the current thread (so another task can be scheduled in) while
    the i2c operation is carried out.
    
    The interrupt issue with the QMSI driver has been solved then this patch
    fixes the shim driver so it uses the interrupt-based API and adds extra
    code to handle the thread synchronization.
    
    Finally, this patch also moves all 'struct device' related definitions
    from the bottom to the top of the i2c_qmsi.c file so the DEVICE_GET
    macro can be used in transfer_complete() and removes the init.h include
    since it is not needed anymore.
    
    Change-Id: I7ef7ce4cea6fcc939e310e5fe12c406645f6a16e
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: dacf54de686ef624366f94793cde12cd8acf4d1b

--- drivers/i2c/i2c_qmsi.c
+++ drivers/i2c/i2c_qmsi.c
@@ -1,11 +1,20 @@
 DECL|GET_CONTROLLER_INSTANCE|macro|GET_CONTROLLER_INSTANCE
+DECL|GET_DRIVER_DATA|macro|GET_DRIVER_DATA
 DECL|api|variable|api
+DECL|complete_cb|function|static void complete_cb(uint32_t id, uint32_t len)
 DECL|config_info_0|variable|config_info_0
 DECL|config_info_1|variable|config_info_1
+DECL|driver_data_0|variable|driver_data_0
+DECL|driver_data_1|variable|driver_data_1
+DECL|err_cb|function|static void err_cb(uint32_t id, qm_i2c_status_t status)
 DECL|i2c_qmsi_config_info|struct|struct i2c_qmsi_config_info {
 DECL|i2c_qmsi_configure|function|static int i2c_qmsi_configure(struct device *dev, uint32_t config)
+DECL|i2c_qmsi_driver_data|struct|struct i2c_qmsi_driver_data {
 DECL|i2c_qmsi_init|function|static int i2c_qmsi_init(struct device *dev)
 DECL|i2c_qmsi_resume|function|static int i2c_qmsi_resume(struct device *dev)
 DECL|i2c_qmsi_suspend|function|static int i2c_qmsi_suspend(struct device *dev)
 DECL|i2c_qmsi_transfer|function|static int i2c_qmsi_transfer(struct device *dev, struct i2c_msg *msgs, uint8_t num_msgs, uint16_t addr)
 DECL|instance|member|qm_i2c_t instance; /* Controller instance. */
+DECL|sync|member|device_sync_call_t sync;
+DECL|transfer_complete|function|static void transfer_complete(uint32_t id, qm_rc_t status)
+DECL|transfer_status|member|qm_rc_t transfer_status;
