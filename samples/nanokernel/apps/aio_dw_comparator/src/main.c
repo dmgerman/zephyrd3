4217ad56c460bd3b9c49e84139f28f8007fd135e
Daniel Leung <daniel.leung@intel.com>
2016-01-28 09:24:50 -0800

    samples: adds a sample for using aio_dw_comparator driver
    Change-Id: Idcfe266db237a0f2aaa6376562cf92bb49e626cc
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 631c3b1242c727aa7131ad53d03f389ba7099d11

---
+++ samples/nanokernel/apps/aio_dw_comparator/src/main.c
@@ -0,0 +1,11 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|ain_idx|member|uint8_t ain_idx;
+DECL|cb_data_t|struct|struct cb_data_t {
+DECL|cb_data|variable|cb_data
+DECL|cb|function|void cb(void *param)
+DECL|main|function|void main(void)
+DECL|name|member|char name[50];
+DECL|pol|member|enum aio_cmp_polarity pol;
+DECL|ref|member|enum aio_cmp_ref ref;
