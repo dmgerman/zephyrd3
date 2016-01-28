dbd9fd92c1a226e03ab4929bc925057381f3df0a
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-01-28 10:43:18 -0200

    samples: Add a sample SPI application
    This application reads the 'WHO AM I' register from a LSM9DS0
    sensor connected over SPI.
    
    Change-Id: I7b0631e9bc783aea60fdb489f40058e0c15275fb
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 9442446944b5f594075ae69269dcf91646a52a22

---
+++ samples/nanokernel/apps/spi_lsm9ds0/src/main.c
@@ -0,0 +1,8 @@
+DECL|LSM9DS0_READ_MASK|macro|LSM9DS0_READ_MASK
+DECL|LSM9DS0_WHOAMI_G|macro|LSM9DS0_WHOAMI_G
+DECL|SPI_PORT_0|macro|SPI_PORT_0
+DECL|SPI_PORT_0|macro|SPI_PORT_0
+DECL|lsm9ds0_read_whoami_g|function|static uint8_t lsm9ds0_read_whoami_g(struct device *dev)
+DECL|main|function|void main(void)
+DECL|rx_buffer|variable|rx_buffer
+DECL|tx_buffer|variable|tx_buffer
