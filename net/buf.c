681a75be9fee4ce2dc29c1fefc65ba98c37b3341
Johan Hedberg <johan.hedberg@intel.com>
2016-01-28 16:39:29 -0500

    net: buf: Add net_buf_push_le16 helper function
    Change-Id: Icce051392d1523df8ebfeccf76477a6264d3d216
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 8d98247c8f38fadb3c7c338103b53272cf5ab907

--- net/buf.c
+++ net/buf.c
@@ -15,6 +15,7 @@ DECL|net_buf_get|function|struct net_buf *net_buf_get(struct nano_fifo *fifo, si
 DECL|net_buf_headroom|function|size_t net_buf_headroom(struct net_buf *buf)
 DECL|net_buf_pull_le16|function|uint16_t net_buf_pull_le16(struct net_buf *buf)
 DECL|net_buf_pull|function|void *net_buf_pull(struct net_buf *buf, size_t len)
+DECL|net_buf_push_le16|function|void net_buf_push_le16(struct net_buf *buf, uint16_t value)
 DECL|net_buf_push|function|void *net_buf_push(struct net_buf *buf, size_t len)
 DECL|net_buf_ref|function|struct net_buf *net_buf_ref(struct net_buf *buf)
 DECL|net_buf_tailroom|function|size_t net_buf_tailroom(struct net_buf *buf)
