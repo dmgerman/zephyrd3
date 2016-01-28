e5d747479f326b9448f88a7b781e6586a03aad68
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-01-28 16:55:57 -0200

    samples: Add a sample SPI application for W25Q80BL
    W25Q80BL is the SPI flash chip soldered on the Quark SE development
    board, for communicating with it, having support for using GPIO as Chip
    Select is necessary, so it will be useful when testing that feature.
    
    Change-Id: Idbf571cdb3b1ae8704d7a932bc0ac56dc53f8ff1
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 42a8032fd9d878a3ce112ae8a0781867fa00785d

---
+++ samples/nanokernel/apps/w25q80bl/src/main.c
@@ -0,0 +1,7 @@
+DECL|SPI_PORT_0|macro|SPI_PORT_0
+DECL|SPI_PORT_0|macro|SPI_PORT_0
+DECL|W25Q80BL_MANUFACTURER_ID|macro|W25Q80BL_MANUFACTURER_ID
+DECL|main|function|int main(void)
+DECL|rx_buffer|variable|rx_buffer
+DECL|tx_buffer|variable|tx_buffer
+DECL|w25q80bl_read_id|function|int w25q80bl_read_id(struct device *dev, uint8_t *manufacturer, uint8_t *deviceid)
