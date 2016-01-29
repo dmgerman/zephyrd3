89da0bafc7fc4e9d0374e3d5202c2a427f4a4095
Daniel Leung <daniel.leung@intel.com>
2016-01-29 13:51:57 -0800

    boards/arduino_due: add pinmux driver
    This adds the pinmux driver for use with Arduino Due.
    The default for pinmux (mostly) reflects the pin layout
    on the board.
    
    Change-Id: I80827ee8bc507567e0cc04b3c8c48580aadf2b3f
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 9c9ad646e12d40a7a3919bc69f61be03486ccf61

---
+++ boards/arduino_due/pinmux_due.c
@@ -0,0 +1,17 @@
+DECL|N_PIOA|macro|N_PIOA
+DECL|N_PIOB|macro|N_PIOB
+DECL|N_PIOC|macro|N_PIOC
+DECL|N_PIOD|macro|N_PIOD
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|__pinmux_defaults|function|static void __pinmux_defaults(void)
+DECL|_get_port|function|static volatile struct __pio *_get_port(uint32_t pin)
+DECL|api_funcs|variable|api_funcs
+DECL|pinmux_get|function|static uint32_t pinmux_get(struct device *dev, uint32_t pin, uint8_t *func)
+DECL|pinmux_get|function|static uint32_t pinmux_get(struct device *dev, uint32_t pin, uint8_t *func)
+DECL|pinmux_init|function|int pinmux_init(struct device *port)
+DECL|pinmux_input|function|static uint32_t pinmux_input(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_pullup|function|static uint32_t pinmux_pullup(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_set|function|static uint32_t pinmux_set(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_set|function|static uint32_t pinmux_set(struct device *dev, uint32_t pin, uint8_t func)
