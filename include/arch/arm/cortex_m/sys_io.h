f9889fe31f708cb1fc7fbede92d9b3a875a4fa7e
Jeff Blais <jeff.blais@windriver.com>
2016-01-29 16:23:38 -0500

    arm: add generic memory-mapped I/O routines for Cortex-M
    Add sys_read32(), sys_write32 and sys_set_bit() routines.
    
    Change-Id: I4c624f30cda743d575afdef7f7790d6cc260e01d
    Signed-off-by: Jeff Blais <jeff.blais@windriver.com>
    
    Former-commit-id: 6896a7c2b413dd69a4434463d1be62eb5bde169b

---
+++ include/arch/arm/cortex_m/sys_io.h
@@ -0,0 +1,5 @@
+DECL|_CORTEX_M_SYS_IO_H_|macro|_CORTEX_M_SYS_IO_H_
+DECL|sys_clear_bit|function|static inline void sys_clear_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_read32|function|static inline uint32_t sys_read32(mem_addr_t addr)
+DECL|sys_set_bit|function|static inline void sys_set_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_write32|function|static inline void sys_write32(uint32_t data, mem_addr_t addr)
