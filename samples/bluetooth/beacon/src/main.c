377d1e98be140958d284f08a981b1df04915fa10
Johan Hedberg <johan.hedberg@intel.com>
2016-01-30 13:36:02 +0200

    Bluetooth: samples: Use sizeof() to evaluate fixed name length
    This way we avoid bugs coming from wrongly (human) counted length.
    
    Change-Id: I95eb87a78fc12d4c5e083ce1c65d8b690ad8db78
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: bd0065b3bd18f5c0a002e8917e82850ec2db2fed

--- samples/bluetooth/beacon/src/main.c
+++ samples/bluetooth/beacon/src/main.c
@@ -1,3 +1,5 @@
+DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
+DECL|DEVICE_NAME|macro|DEVICE_NAME
 DECL|ad|variable|ad
 DECL|bt_ready|function|static void bt_ready(int err)
 DECL|mainloop|function|void mainloop(void)
