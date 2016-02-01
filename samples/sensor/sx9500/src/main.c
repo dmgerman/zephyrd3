96d1df7c6acc0cd6ae74d3f29b2a48c11e6496ae
Vlad Dogaru <vlad.dogaru@intel.com>
2016-02-01 15:32:34 +0200

    samples: Add sample app for sx9500 sensor driver
    This illustrates the usage of the sensor API, using either triggers or
    periodic reads from the driver.
    
    Origin: Original
    Change-Id: Ief7451fc4771b9459a2b3b7ed0a33341a7710cc3
    Signed-off-by: Vlad Dogaru <vlad.dogaru@intel.com>
    
    Former-commit-id: e479839fa14b1b89a627803396b03544251c4a3e

---
+++ samples/sensor/sx9500/src/main.c
@@ -0,0 +1,5 @@
+DECL|do_main|function|static void do_main(struct device *dev)
+DECL|do_main|function|void do_main(struct device *dev)
+DECL|main|function|void main(void)
+DECL|sensor_trigger_handler|function|static void sensor_trigger_handler(struct device *dev, struct sensor_trigger *trig)
+DECL|setup_trigger|function|static void setup_trigger(struct device *dev)
