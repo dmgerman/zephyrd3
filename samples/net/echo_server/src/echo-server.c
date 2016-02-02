d3bc200d6c4b75602d23c1bca629b6f5c3d6b6ea
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-02-02 17:54:54 +0200

    net: apps: echo-server refactoring to work in microkernel
    The echo-server uses only one code base for both microkernel
    and nanokernel.
    
    Change-Id: I7abdf76dc13c31e4f91d0fd9d3b870ffe20ac71b
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9e79aa6f9d6e808c60aa201d0c25fc18057bb5c1

--- samples/net/echo_server/src/echo-server.c
+++ samples/net/echo_server/src/echo-server.c
@@ -3,18 +3,20 @@ DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
+DECL|PEER_IPADDR|macro|PEER_IPADDR
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|fiberStack|variable|fiberStack
-DECL|fiber_receive|function|void fiber_receive(void)
 DECL|get_context|function|static inline bool get_context(struct net_context **recv, struct net_context **mcast_recv)
+DECL|in6addr_my|variable|in6addr_my
+DECL|in6addr_peer|variable|in6addr_peer
 DECL|init_server|function|static inline void init_server()
 DECL|main|function|void main(void)
 DECL|my_mac|variable|my_mac
 DECL|peer_mac|variable|peer_mac
 DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const char *name, const char *type, struct net_buf *buf)
 DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *recv, struct net_context *mcast_recv)
+DECL|receive|function|void receive(void)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
-DECL|task_receive|function|void task_receive(void)
