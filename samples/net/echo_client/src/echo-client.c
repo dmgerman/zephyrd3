1b4caa40d966ceefaebc4f9b8aec06ce845b0630
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-02-02 17:29:01 +0200

    net: apps: echo-client refactoring to work in microkernel
    The echo-client uses only one code base for both microkernel
    and nanokernel.
    
    Change-Id: Ieaf9a969319537286181ba333e3a05bfa0e9363a
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 40bef01a5fb5d7bb89d71f36dba7980971d1fd45

--- samples/net/echo_client/src/echo-client.c
+++ samples/net/echo_client/src/echo-client.c
@@ -8,14 +8,9 @@ DECL|PEER_IPADDR|macro|PEER_IPADDR
 DECL|PEER_PORT|macro|PEER_PORT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
-DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|expecting|variable|expecting
-DECL|fiberStack_receiving|variable|fiberStack_receiving
-DECL|fiber_receiving|function|void fiber_receiving(void)
-DECL|fiber_sending|function|void fiber_sending(void)
 DECL|get_context|function|static inline bool get_context(struct net_context **unicast, struct net_context **multicast)
 DECL|in6addr_my|variable|in6addr_my
 DECL|in6addr_peer|variable|in6addr_peer
@@ -26,11 +21,10 @@ DECL|main|function|void main(void)
 DECL|multicast|variable|multicast
 DECL|my_mac|variable|my_mac
 DECL|peer_mac|variable|peer_mac
-DECL|receiving_loop|function|void receiving_loop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|receiving|function|void receiving(void)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
 DECL|send_packet|function|static inline bool send_packet(const char *name, struct net_context *ctx, int ipsum_len, int pos)
-DECL|sending_loop|function|void sending_loop(const char *taskname, ksem_t mySem, ksem_t otherSem)
-DECL|taskA|function|void taskA(void)
-DECL|taskB|function|void taskB(void)
+DECL|sending|function|void sending(void)
+DECL|stack_receiving|variable|stack_receiving
 DECL|unicast|variable|unicast
 DECL|wait_reply|function|static inline bool wait_reply(const char *name, struct net_context *ctx, int ipsum_len, int pos)
