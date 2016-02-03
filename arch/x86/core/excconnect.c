43bb0934e7be08925f89cebf5a0d9fee35608eee
Andrew Boie <andrew.p.boie@intel.com>
2016-02-02 21:02:36 -0800

    iamcu: fix dynamic exceptions
    The argument to _common_dynamic_exc_handler() in the C domain
    was still being set up the Sys V way. Arguments are popped off the
    stack in reverse order, so _ExcEnt pushes *pEsf for arg 2, and then
    the stub pushes the stub_num for Arg 1.
    
    For IAMCU case, *pEsf is placed in EAX which is where argument 1
    lives; static exception handlers take just one argument. However
    since we're calling _common_dynamic_exc_handler we also need to
    supply the stub_id, which we put in EDX (where arg 2 goes).
    
    Rather than swapping things around at runtime the prototype for
    _common_dynamic_exc_handler() is adjusted to fit the calling
    convention in use.
    
    Change-Id: Id43cbc3b86d90f941cea771678b2796ae5f1358d
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 6e83effea8461fc103a99527f82a86c3561e3d2c

--- arch/x86/core/excconnect.c
+++ arch/x86/core/excconnect.c
@@ -1,5 +1,5 @@
 DECL|_NanoCpuExcConnectAtDpl|function|void _NanoCpuExcConnectAtDpl(unsigned int vector, /* interrupt vector: 0 to 255 on IA-32 */ void (*routine)(NANO_ESF * pEsf), unsigned int dpl /* priv level for interrupt-gate descriptor */ )
-DECL|_common_dynamic_exc_handler|function|void _common_dynamic_exc_handler(uint32_t stub_idx, NANO_ESF *pEsf)
+DECL|_common_dynamic_exc_handler|function|void _common_dynamic_exc_handler(NANO_ESF *pEsf, uint32_t stub_idx)
 DECL|exc_handlers|variable|exc_handlers
 DECL|nanoCpuExcConnect|function|void nanoCpuExcConnect(unsigned int vector, /* interrupt vector: 0 to 255 on * IA-32 */ void (*routine)(NANO_ESF * pEsf))
 DECL|next_exc_noerr_stub|variable|next_exc_noerr_stub
