dca6819a248f30be8e7d3fe9e17bc8035b9a9798
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-02-04 16:43:16 +0200

    drivers/nble: Style fixes
    Fixes some styles and removes p_buf from debug log.
    
    Change-Id: I861c2c250d8478353f8272e9c59c06f9fc211036
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: d4bb288f7b579fa688ca0565a9abafdf664e2a97

--- drivers/nble/uart.c
+++ drivers/nble/uart.c
@@ -10,7 +10,7 @@ DECL|nble_open|function|int nble_open(void)
 DECL|nble_read|function|static int nble_read(struct device *uart, uint8_t *buf, size_t len, size_t min)
 DECL|poll_out|function|static void poll_out(const void *buf, size_t length)
 DECL|rpc_alloc_cb|function|uint8_t *rpc_alloc_cb(uint16_t length)
-DECL|rpc_transmit_cb|function|void rpc_transmit_cb(uint8_t *p_buf, uint16_t length)
+DECL|rpc_transmit_cb|function|void rpc_transmit_cb(uint8_t *data, uint16_t length)
 DECL|rx_fiber|function|static void rx_fiber(void)
 DECL|rx_queue|variable|rx_queue
 DECL|rx|variable|rx
