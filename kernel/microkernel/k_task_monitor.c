e2fe1e5fad1def72ba75ccde0c889f945e90ded2
Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
2016-02-05 15:12:01 -0600

    logger: include task monitor in kernel event logger
    Zephyr includes a Task Monitor feature that allows to
    track events on the microkernel server scheduler.
    Task monitor is integrated as a profiler point for the
    Kernel Event Logger feature.
    
    Change-Id: I7b8be5872439a333f976eada1aa3511d93b46388
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
    
    Former-commit-id: 65ec185f7962f1da1c48c0e82bbe153d0a3566d0

--- kernel/microkernel/k_task_monitor.c
+++ kernel/microkernel/k_task_monitor.c
@@ -1,11 +1,5 @@
-DECL|K_monitor_wind|variable|K_monitor_wind
 DECL|_k_monitor_mask|variable|_k_monitor_mask
 DECL|_k_task_monitor_args|function|void _k_task_monitor_args(struct k_args *A)
-DECL|_k_task_monitor_read|function|void _k_task_monitor_read(struct k_args *A)
 DECL|_k_task_monitor|function|void _k_task_monitor(struct k_task *X, uint32_t D)
 DECL|_k_task_switch_callback|variable|_k_task_switch_callback
-DECL|k_monitor_buff|variable|k_monitor_buff
-DECL|k_monitor_capacity|variable|k_monitor_capacity
-DECL|k_monitor_nrec|variable|k_monitor_nrec
-DECL|k_monitor_wptr|variable|k_monitor_wptr
 DECL|task_monitor_hook_set|function|void task_monitor_hook_set(k_task_monitor_hook_t func)
