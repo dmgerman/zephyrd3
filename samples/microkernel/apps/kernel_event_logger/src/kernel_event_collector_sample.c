07d5b8803fe0fd2edb28fe61323cfe2ba17f83c3
Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
2016-02-05 15:22:22 -0600

    logger: updates logger sample to include task monitor events
    Change-Id: I8fad34518d66194b349a6cac82e6a03332344132
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@linux.intel.com>
    
    Former-commit-id: 4601ccdbdf8964efc2103a72e9446ca82f738f78

--- samples/microkernel/apps/kernel_event_logger/src/kernel_event_collector_sample.c
+++ samples/microkernel/apps/kernel_event_logger/src/kernel_event_collector_sample.c
@@ -13,6 +13,8 @@ DECL|busy_task_entry|function|void busy_task_entry(void)
 DECL|context_switch_data_t|struct|struct context_switch_data_t {
 DECL|context_switch_summary_data|variable|context_switch_summary_data
 DECL|count|member|uint32_t count;
+DECL|data|member|uint32_t data;
+DECL|event_type|member|uint32_t event_type;
 DECL|fork_manager_entry|function|void fork_manager_entry(void)
 DECL|forks_available|variable|forks_available
 DECL|forks|variable|forks
@@ -27,12 +29,19 @@ DECL|last_time_slept|member|uint32_t last_time_slept;
 DECL|main|function|int main(void)
 DECL|philStack|variable|philStack
 DECL|print_context_data|function|void print_context_data(uint32_t thread_id, uint32_t count,uint32_t last_time_executed, int indice)
+DECL|print_tmon_status_data|function|void print_tmon_status_data(int index)
 DECL|profiling_data_collector|function|void profiling_data_collector(void)
 DECL|register_context_switch_data|function|void register_context_switch_data(uint32_t timestamp, uint32_t thread_id)
 DECL|register_interrupt_event_data|function|void register_interrupt_event_data(uint32_t timestamp, uint32_t irq)
 DECL|register_sleep_event_data|function|void register_sleep_event_data(uint32_t time_start, uint32_t duration,uint32_t cause)
+DECL|register_tmon_data|function|void register_tmon_data(uint32_t event_type, uint32_t timestamp,uint32_t task_id, uint32_t data)
 DECL|sleep_data_t|struct|struct sleep_data_t {
 DECL|sleep_event_data|variable|sleep_event_data
 DECL|summary_data_printer|function|void summary_data_printer(void)
+DECL|task_id|member|uint32_t task_id;
 DECL|thread_id|member|uint32_t thread_id;
+DECL|timestamp|member|uint32_t timestamp;
+DECL|tmon_data_t|struct|struct tmon_data_t {
+DECL|tmon_index|variable|tmon_index
+DECL|tmon_summary_data|variable|tmon_summary_data
 DECL|total_dropped_counter|variable|total_dropped_counter
