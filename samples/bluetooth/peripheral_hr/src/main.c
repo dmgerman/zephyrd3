2422332da1552a320b196270120cc525dde2eb87
Johan Hedberg <johan.hedberg@intel.com>
2016-02-05 14:24:57 +0200

    Bluetooth: samples: Add dedicated Heartrate peripheral sample app
    This can be used to focus on demonstrating a simple heartrate service.
    
    Change-Id: Ib59c653ebe356784592c8468767b45c5240ec67a
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 2b7a5229f2fa2d7403e1be38edde461b3eab507c

---
+++ samples/bluetooth/peripheral_hr/src/main.c
@@ -0,0 +1,29 @@
+DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|HEART_RATE_APPEARANCE|macro|HEART_RATE_APPEARANCE
+DECL|ad|variable|ad
+DECL|auth_cancel|function|static void auth_cancel(struct bt_conn *conn)
+DECL|auth_cb_display|variable|auth_cb_display
+DECL|bas_attrs|variable|bas_attrs
+DECL|battery|variable|battery
+DECL|blvl_ccc_cfg_changed|function|static void blvl_ccc_cfg_changed(uint16_t value)
+DECL|blvl_ccc_cfg|variable|blvl_ccc_cfg
+DECL|bt_ready|function|static void bt_ready(int err)
+DECL|conn_callbacks|variable|conn_callbacks
+DECL|connected|function|static void connected(struct bt_conn *conn, uint8_t err)
+DECL|dis_attrs|variable|dis_attrs
+DECL|disconnected|function|static void disconnected(struct bt_conn *conn, uint8_t reason)
+DECL|gap_attrs|variable|gap_attrs
+DECL|hrmc_ccc_cfg_changed|function|static void hrmc_ccc_cfg_changed(uint16_t value)
+DECL|hrmc_ccc_cfg|variable|hrmc_ccc_cfg
+DECL|hrs_attrs|variable|hrs_attrs
+DECL|mainloop|function|void mainloop(void)
+DECL|read_appearance|function|static int read_appearance(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_blsc|function|static int read_blsc(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_blvl|function|static int read_blvl(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_manuf|function|static int read_manuf(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_model|function|static int read_model(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|read_name|function|static int read_name(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|sd|variable|sd
+DECL|simulate_blvl|variable|simulate_blvl
+DECL|simulate_hrm|variable|simulate_hrm
