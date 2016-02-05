aa4955eac687d84eb0487ddd05a7dd611adcad6d
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-02-05 11:51:55 -0500

    device: fix race condition in device_sync if ISR runs before thread waits
    If an ISR was enabled and ran before the thread that wants to wait for
    completion actually does wait, the type of thread would not be
    registered in the object. The ISR would thus not know what type of
    semaphore to release. This caused the ISR to release the object for the
    previous caller of device_sync_call_wait().
    
    Instead, the ISR now looks if there is already a waiter: if not, it
    marks the device as being ready in the object, which allows the thread
    to skip taking the semaphore; if there is a waiter, the correct
    semaphore type is released, as before.
    
    Change-Id: Ib97480db8ba3e895812cf4bc209d9794639af325
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 48e4f82f3c9353fdf4c70b50164700f78b77b5c7

--- include/device.h
+++ include/device.h
@@ -2,6 +2,9 @@ DECL|DEVICE_DECLARE|macro|DEVICE_DECLARE
 DECL|DEVICE_GET|macro|DEVICE_GET
 DECL|DEVICE_INIT|macro|DEVICE_INIT
 DECL|DEVICE_NAME_GET|macro|DEVICE_NAME_GET
+DECL|DEVICE_SYNC_WAITER_FIBER|enumerator|DEVICE_SYNC_WAITER_FIBER,
+DECL|DEVICE_SYNC_WAITER_NONE|enumerator|DEVICE_SYNC_WAITER_NONE,
+DECL|DEVICE_SYNC_WAITER_TASK|enumerator|DEVICE_SYNC_WAITER_TASK,
 DECL|DEV_FAIL|macro|DEV_FAIL
 DECL|DEV_INVALID_CONF|macro|DEV_INVALID_CONF
 DECL|DEV_INVALID_OP|macro|DEV_INVALID_OP
@@ -11,16 +14,19 @@ DECL|DEV_NO_SUPPORT|macro|DEV_NO_SUPPORT
 DECL|DEV_OK|macro|DEV_OK
 DECL|DEV_USED|macro|DEV_USED
 DECL|_DEVICE_H_|macro|_DEVICE_H_
+DECL|_is_blocking_task|function|static inline bool _is_blocking_task(void)
 DECL|_t_sem|member|struct _k_sem_struct _t_sem;
 DECL|config_info|member|void *config_info;
 DECL|config|member|struct device_config *config;
 DECL|device_config|struct|struct device_config {
+DECL|device_ready|member|bool device_ready;
 DECL|device_sync_call_complete|function|static inline void device_sync_call_complete(device_sync_call_t *sync)
 DECL|device_sync_call_complete|function|static inline void device_sync_call_complete(device_sync_call_t *sync)
 DECL|device_sync_call_init|function|static inline void device_sync_call_init(device_sync_call_t *sync)
 DECL|device_sync_call_t|typedef|} device_sync_call_t;
 DECL|device_sync_call_wait|function|static inline void device_sync_call_wait(device_sync_call_t *sync)
 DECL|device_sync_call_wait|function|static inline void device_sync_call_wait(device_sync_call_t *sync)
+DECL|device_sync_waiter|enum|enum device_sync_waiter {
 DECL|device|struct|struct device {
 DECL|driver_api|member|void *driver_api;
 DECL|driver_data|member|void *driver_data;
@@ -28,4 +34,4 @@ DECL|f_sem|member|struct nano_sem f_sem;
 DECL|init|member|int (*init)(struct device *device);
 DECL|name|member|char *name;
 DECL|t_sem|member|ksem_t t_sem;
-DECL|waiter_is_task|member|bool waiter_is_task;
+DECL|waiter|member|enum device_sync_waiter waiter;
