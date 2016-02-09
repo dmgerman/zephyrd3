e1784769b47cc2584a477262c884dc94dbab8f88
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-02-09 12:22:21 +0200

    drivers/nble: Add basic connection handling support
    Add connection handling skeleton keeping interfaces similar to
    net/bluetooth and putting internals to private conn_internal.h header.
    
    ...
    bt: rpc_deserialize (0xa8008924): on_ble_gap_connect_evt
    bt: on_ble_gap_connect_evt (0xa8008924): handle 3
    ...
    bt: rpc_deserialize (0xa8008924): on_ble_gap_conn_update_evt
    bt: bt_conn_ref (0xa8008924): handle 3 ref 2
    bt: bt_conn_unref (0xa8008924): handle 3 ref 1
    ...
    bt: rpc_deserialize (0xa8008924): on_ble_gap_disconnect_evt
    bt: bt_conn_ref (0xa8008924): handle 3 ref 2
    bt: on_ble_gap_disconnect_evt (0xa8008924): conn 0xa80080c4 handle 3
    bt: bt_conn_unref (0xa8008924): handle 3 ref 1
    bt: bt_conn_unref (0xa8008924): handle 3 ref 0
    ...
    
    Change-Id: I1885ecad5c57323b7d5a68be0c63d98dc65350b3
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 48b10035c50ff1161fbd4a3a0c0ee04d1ee9198f

--- drivers/nble/conn.c
+++ drivers/nble/conn.c
@@ -1,3 +1,4 @@
+DECL|bt_conn_add_le|function|static struct bt_conn *bt_conn_add_le(const bt_addr_le_t *peer)
 DECL|bt_conn_auth_cancel|function|int bt_conn_auth_cancel(struct bt_conn *conn)
 DECL|bt_conn_auth_cb_register|function|int bt_conn_auth_cb_register(const struct bt_conn_auth_cb *cb)
 DECL|bt_conn_auth_passkey_confirm|function|int bt_conn_auth_passkey_confirm(struct bt_conn *conn, bool match)
@@ -11,7 +12,13 @@ DECL|bt_conn_get_dst|function|const bt_addr_le_t *bt_conn_get_dst(const struct b
 DECL|bt_conn_get_info|function|int bt_conn_get_info(const struct bt_conn *conn, struct bt_conn_info *info)
 DECL|bt_conn_le_param_update|function|int bt_conn_le_param_update(struct bt_conn *conn, const struct bt_le_conn_param *param)
 DECL|bt_conn_lookup_addr_le|function|struct bt_conn *bt_conn_lookup_addr_le(const bt_addr_le_t *peer)
+DECL|bt_conn_lookup_handle|function|struct bt_conn *bt_conn_lookup_handle(uint16_t handle)
 DECL|bt_conn_ref|function|struct bt_conn *bt_conn_ref(struct bt_conn *conn)
 DECL|bt_conn_security|function|int bt_conn_security(struct bt_conn *conn, bt_security_t sec)
 DECL|bt_conn_unref|function|void bt_conn_unref(struct bt_conn *conn)
 DECL|bt_le_set_auto_conn|function|int bt_le_set_auto_conn(bt_addr_le_t *addr,const struct bt_le_conn_param *param)
+DECL|conn_new|function|static struct bt_conn *conn_new(void)
+DECL|conns|variable|conns
+DECL|on_ble_gap_conn_update_evt|function|void on_ble_gap_conn_update_evt(const struct ble_gap_conn_update_evt *ev)
+DECL|on_ble_gap_connect_evt|function|void on_ble_gap_connect_evt(const struct ble_gap_connect_evt *ev)
+DECL|on_ble_gap_disconnect_evt|function|void on_ble_gap_disconnect_evt(const struct ble_gap_disconnect_evt *ev)
---
+++ drivers/nble/conn_internal.h
@@ -0,0 +1,3 @@
+DECL|bt_conn|struct|struct bt_conn {
+DECL|handle|member|uint16_t handle;
+DECL|ref|member|atomic_t ref;
