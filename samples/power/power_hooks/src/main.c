04d1908945b333226eb29b2f2a87abf668e8697d
Ramesh Thomas <ramesh.thomas@intel.com>
2016-02-09 17:38:23 -0800

    adv_power:Power management hooks template app
    A template app that defines the power management hooks and
    enables the power management related CONFIG flags.  This
    app will enable build testing of power management code inside
    the CONFIG flags.
    
    Change-Id: If26904f69333ff05a0711596218b51b828669de1
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: d230fc36532e75838935f821ce1aaef6f962793e

---
+++ samples/power/power_hooks/src/main.c
@@ -0,0 +1,5 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
+DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
+DECL|main|function|void main(void)
