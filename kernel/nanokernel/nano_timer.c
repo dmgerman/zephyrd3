90078af37c13277447b97dd009bb184ced52f06d
Dan Kalowsky <daniel.kalowsky@intel.com>
2016-02-10 16:38:50 -0800

    nanokernel : clean up comments
    Moving comments back to the 70 character limit.  Also moving some of the
    comments up to be included in the doxygen headers instead of inline.
    
    Change-Id: I56a6015e5fd6da81e9a06701217e62e899b6aa62
    Signed-off-by: Dan Kalowsky <daniel.kalowsky@intel.com>
    
    Former-commit-id: 21a99e639dc3fdea891f8c8ee0147669923b6136

--- kernel/nanokernel/nano_timer.c
+++ kernel/nanokernel/nano_timer.c
@@ -1,5 +1,5 @@
 DECL|_nano_timer_list|variable|_nano_timer_list
-DECL|_timer_start|function|void _timer_start(struct nano_timer *timer, /* timer to start */ int ticks /* number of system ticks * before expiry */ )
+DECL|_timer_start|function|void _timer_start(struct nano_timer *timer, int ticks)
 DECL|_timer_stop_non_preemptible|function|void _timer_stop_non_preemptible(struct nano_timer *timer)
 DECL|_timer_stop|function|static void _timer_stop(struct nano_timer *timer)
 DECL|nano_task_timer_stop|function|void nano_task_timer_stop(struct nano_timer *timer)
