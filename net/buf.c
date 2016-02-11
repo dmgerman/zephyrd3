06c0fee17f48fab64864f52764abdb6a60386801
Johan Hedberg <johan.hedberg@intel.com>
2016-02-11 11:05:56 +0200

    net: buf: Add convenience API for adding 8-bit integers
    There are many situations when encoding data when we need to insert
    single bytes to the buffer. With this helper the encoding code stays a
    bit more readable.
    
    Change-Id: Ibc0ce43af5ae25a1baa0f1adbc5816ae7c04e3bb
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 754248aedc7322147eae06a49665a0995c7fb18a

--- net/buf.c
+++ net/buf.c
@@ -9,6 +9,7 @@ DECL|NET_BUF_INFO|macro|NET_BUF_INFO
 DECL|NET_BUF_WARN|macro|NET_BUF_WARN
 DECL|NET_BUF_WARN|macro|NET_BUF_WARN
 DECL|net_buf_add_le16|function|void net_buf_add_le16(struct net_buf *buf, uint16_t value)
+DECL|net_buf_add_u8|function|uint8_t *net_buf_add_u8(struct net_buf *buf, uint8_t value)
 DECL|net_buf_add|function|void *net_buf_add(struct net_buf *buf, size_t len)
 DECL|net_buf_clone|function|struct net_buf *net_buf_clone(struct net_buf *buf)
 DECL|net_buf_get|function|struct net_buf *net_buf_get(struct nano_fifo *fifo, size_t reserve_head)
