7e71844868bc6b3cc6c27f1eec2e751db27838e7
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-02-11 15:56:48 -0500

    sys_clock/microkernel: do not announce ticks until microkernel is up
    This is a prologue to reverting:
    
    	commit 3c66686
    	Author: Benjamin Walsh <benjamin.walsh@windriver.com>
    	Date:   Tue Feb 9 17:34:02 2016 -0500
    
    		sys_clock: start the microkernel ticker in the MICROKERNEL init level
    
    to allow the devices initializing in pre-MICROKERNEL init levels to poll
    the hi-res clock (sys_cycle_get_32()), which relies on the system clock
    having been started.
    
    This change allows starting the system clock in the NANOKERNEL init
    level by delaying announcing the ticks until the MICROKERNEL init level.
    
    Change-Id: I43d54bb5e2f182d4edd880da0124a0817f911943
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 038790a9457e946489054aa3b86a49c6c7258a75

--- kernel/microkernel/k_ticker.c
+++ kernel/microkernel/k_ticker.c
@@ -2,8 +2,11 @@ DECL|_SysIdleElapsedTicksGet|function|static inline int32_t _SysIdleElapsedTicks
 DECL|_TimeSliceUpdate|function|static inline void _TimeSliceUpdate(void)
 DECL|_TlDebugUpdate|function|static inline int _TlDebugUpdate(int32_t ticks)
 DECL|_TlDebugUpdate|macro|_TlDebugUpdate
+DECL|_do_sys_clock_tick_announce|variable|_do_sys_clock_tick_announce
 DECL|_k_debug_sys_clock_tick_count|variable|_k_debug_sys_clock_tick_count
 DECL|_k_ticker|function|int _k_ticker(int event)
+DECL|_sys_clock_tick_announce_install|function|static int _sys_clock_tick_announce_install(struct device *dev)
+DECL|_sys_clock_tick_announce_pre_micro_nop|function|static void _sys_clock_tick_announce_pre_micro_nop(kevent_t e)
 DECL|_sys_idle_elapsed_ticks|variable|_sys_idle_elapsed_ticks
 DECL|slice_count|variable|slice_count
 DECL|slice_prio|variable|slice_prio
