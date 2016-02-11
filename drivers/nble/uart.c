ffe04071c5e3a8c89d24c28f0c4a78962b01c847
Johan Hedberg <johan.hedberg@intel.com>
2016-02-11 11:35:03 +0200

    drivers/nble: Clean up & simplify IPC UART encoding
    We can take advantage of the net_buf headroom. At the same time move
    the necessary definition straight into uart.c and remove any unused
    definitions.
    
    Change-Id: I932bba2cfb11808aabce7bad09e41f94d073bb5b
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 9632d46ae793765ea948061b93945da5fa3d89db

--- drivers/nble/uart.c
+++ drivers/nble/uart.c
@@ -3,16 +3,20 @@ DECL|NBLE_RX_BUF_COUNT|macro|NBLE_RX_BUF_COUNT
 DECL|NBLE_TX_BUF_COUNT|macro|NBLE_TX_BUF_COUNT
 DECL|NET_BUF_POOL|variable|NET_BUF_POOL
 DECL|NET_BUF_POOL|variable|NET_BUF_POOL
+DECL|__packed|variable|__packed
 DECL|_bt_nble_init|function|static int _bt_nble_init(struct device *unused)
 DECL|bt_uart_isr|function|void bt_uart_isr(void *unused)
+DECL|channel|member|uint8_t channel; /**< Channel number of IPC message. */
+DECL|ipc_uart_header|struct|struct ipc_uart_header {
+DECL|len|member|uint16_t len; /**< Length of IPC message. */
 DECL|nble_dev|variable|nble_dev
 DECL|nble_discard|function|static size_t nble_discard(struct device *uart, size_t len)
 DECL|nble_open|function|int nble_open(void)
 DECL|nble_read|function|static int nble_read(struct device *uart, uint8_t *buf, size_t len, size_t min)
-DECL|poll_out|function|static void poll_out(const void *buf, size_t length)
 DECL|rpc_alloc_cb|function|struct net_buf *rpc_alloc_cb(uint16_t length)
 DECL|rpc_transmit_cb|function|void rpc_transmit_cb(struct net_buf *buf)
 DECL|rx_fiber|function|static void rx_fiber(void)
 DECL|rx_queue|variable|rx_queue
 DECL|rx|variable|rx
+DECL|src_cpu_id|member|uint8_t src_cpu_id; /**< CPU id of IPC sender. */
 DECL|tx|variable|tx
