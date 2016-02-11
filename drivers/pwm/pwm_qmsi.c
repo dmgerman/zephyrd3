4984bb4b6b1cb2c2d4b9677a8a2d0ffd2ca6d90b
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-02-11 13:18:25 -0800

    pwm:  QMSI PWM driver
    This creates the QMSI PWM driver which is simply a shim driver based
    on the PWM driver provided by QMSI BSP.
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_PWM=y
    CONFIG_PWM_QMSI=y
    
    Optionally this can be set:
    CONFIG_PWM_QMSI_NUM_PORTS=Number of ports available, default is 1
    
    Origin of the file: This file has been created from scratch
    
    Change-Id: Icac90154d020babca1c11147056a3438b84b0d3f
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: d8910a419b7bb3fa2136ddc31bb5cee3f6018bd5

---
+++ drivers/pwm/pwm_qmsi.c
@@ -0,0 +1,8 @@
+DECL|__set_one_port|function|static int __set_one_port(qm_pwm_t id, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|pwm_qmsi_configure|function|static int pwm_qmsi_configure(struct device *dev, int access_op, uint32_t pwm, int flags)
+DECL|pwm_qmsi_drv_api_funcs|variable|pwm_qmsi_drv_api_funcs
+DECL|pwm_qmsi_init|function|static int pwm_qmsi_init(struct device *dev)
+DECL|pwm_qmsi_resume|function|static int pwm_qmsi_resume(struct device *dev)
+DECL|pwm_qmsi_set_duty_cycle|function|static int pwm_qmsi_set_duty_cycle(struct device *dev, int access_op, uint32_t pwm, uint8_t duty)
+DECL|pwm_qmsi_set_values|function|static int pwm_qmsi_set_values(struct device *dev, int access_op, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|pwm_qmsi_suspend|function|static int pwm_qmsi_suspend(struct device *dev)
