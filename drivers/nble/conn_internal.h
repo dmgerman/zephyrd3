ffce2aa61c933dcbabd0d0e67d89eaec3ed1cf56
Johan Hedberg <johan.hedberg@intel.com>
2016-02-15 12:39:29 +0200

    drivers/nble: Track all connection parameters
    The bt_conn_get_info() API requires that we track the role and the
    various connection parameters.
    
    Change-Id: I732eace1e45173f94962df3f11dbe5ad520a75cf
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: f0315be5775b95c43db6948057ca167fde55b23b

--- drivers/nble/conn_internal.h
+++ drivers/nble/conn_internal.h
@@ -1,4 +1,8 @@
 DECL|bt_conn|struct|struct bt_conn {
 DECL|dst|member|bt_addr_le_t dst;
 DECL|handle|member|uint16_t handle;
+DECL|interval|member|uint16_t interval;
+DECL|latency|member|uint16_t latency;
 DECL|ref|member|atomic_t ref;
+DECL|role|member|uint8_t role;
+DECL|timeout|member|uint16_t timeout;
