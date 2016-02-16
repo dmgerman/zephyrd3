4d843f720a4960a588b155f361c2ad836b47a262
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-02-16 11:59:16 +0200

    drivers/nble: Implement client scanning
    Implement Bluetooth LE scan start/stop API for Nordic.
    
    Change-Id: I3dc153346d0135501091a4b952a3c60c081802db
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 68fcd4e04beb2fd6d71e542f5f153acc85ef5a97

--- drivers/nble/gap.c
+++ drivers/nble/gap.c
@@ -10,9 +10,13 @@ DECL|bt_le_scan_stop|function|int bt_le_scan_stop(void)
 DECL|bt_ready_cb|variable|bt_ready_cb
 DECL|nble_bdaddr|variable|nble_bdaddr
 DECL|nble_log|function|void nble_log(const struct nble_log_s *param, char *format, uint8_t len)
+DECL|on_nble_gap_adv_report_evt|function|void on_nble_gap_adv_report_evt(const struct nble_gap_adv_report_evt *evt,const uint8_t *buf, uint8_t len)
 DECL|on_nble_gap_read_bda_rsp|function|void on_nble_gap_read_bda_rsp(const struct nble_service_read_bda_response *rsp)
+DECL|on_nble_gap_scan_start_stop_rsp|function|void on_nble_gap_scan_start_stop_rsp(const struct nble_response *rsp)
 DECL|on_nble_gap_sm_config_rsp|function|void on_nble_gap_sm_config_rsp(struct nble_gap_sm_config_rsp *rsp)
 DECL|on_nble_get_version_rsp|function|void on_nble_get_version_rsp(const struct nble_version_response *rsp)
 DECL|on_nble_up|function|void on_nble_up(void)
+DECL|scan_dev_found_cb|variable|scan_dev_found_cb
 DECL|send_dm_config|function|static void send_dm_config(void)
 DECL|valid_adv_param|function|static bool valid_adv_param(const struct bt_le_adv_param *param)
+DECL|valid_le_scan_param|function|static bool valid_le_scan_param(const struct bt_le_scan_param *param)
