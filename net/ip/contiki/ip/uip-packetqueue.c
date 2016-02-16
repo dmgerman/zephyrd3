e1e0e081ceceddfff33b8e558e7c6f18c6ebdd0c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-02-16 17:57:47 +0200

    net: contiki: Enable packet queueing
    Currently the packet is dropped if some other packet needs to
    be sent instead. This does not make much sense in Zephyr so
    activate packet queueing where the saved packet is sent later.
    The queueing can happen e.g., with neighbor discovery where
    the packet to be sent requires ND to be done before the actual
    application packet is being sent.
    
    Change-Id: Ia321d641eec5acfbc9f8f581de712801a483e32d
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 3c44f7e0bcd7b5213c8f8fa10fba46f99340383f

--- net/ip/contiki/ip/uip-packetqueue.c
+++ net/ip/contiki/ip/uip-packetqueue.c
@@ -1,7 +1,5 @@
 DECL|DEBUG|macro|DEBUG
 DECL|MAX_NUM_QUEUED_PACKETS|macro|MAX_NUM_QUEUED_PACKETS
-DECL|PRINTF|macro|PRINTF
-DECL|PRINTF|macro|PRINTF
 DECL|packet_timedout|function|packet_timedout(struct net_buf *buf, void *ptr)
 DECL|uip_packetqueue_alloc|function|uip_packetqueue_alloc(struct net_buf *buf, struct uip_packetqueue_handle *handle, clock_time_t lifetime)
 DECL|uip_packetqueue_buflen|function|uip_packetqueue_buflen(struct uip_packetqueue_handle *h)
