1e60b143cb3b6ffe672adfe705e2bf284f4788d6
Andre Guedes <andre.guedes@intel.com>
2016-02-17 10:58:00 -0200

    samples: Add i2c_lsm9ds0 application
    This patch adds a new sample application to demonstrate how to use
    I2C APIs. The application is very simple. It simply reads the 'WHO
    AM I' register from the accelerometer in the LSM9DS0 chip and check
    if it matches with the value described in the datasheet.
    
    It also adds a README file which provides the wiring information
    required to get the sample application working on Quark SE DevBoard.
    
    Change-Id: I9162c030874c2718506b76519b255c9c11631802
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: 0c5b7f7d22c7be5e0f513b3e7edc47d456aeafeb

---
+++ samples/drivers/i2c_lsm9ds0/src/main.c
@@ -0,0 +1,5 @@
+DECL|GYRO_I2C_ADDR|macro|GYRO_I2C_ADDR
+DECL|WHO_AM_I_REG|macro|WHO_AM_I_REG
+DECL|main|function|void main(void)
+DECL|read_who_am_i_by_transfer|function|static void read_who_am_i_by_transfer(struct device *dev)
+DECL|read_who_am_i|function|static void read_who_am_i(struct device *dev)
