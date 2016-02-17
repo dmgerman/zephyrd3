4635cd16419497d014c4f664406a8355de3861d3
Ramesh Thomas <ramesh.thomas@intel.com>
2016-02-16 17:53:22 -0800

    power_mgmt:sample:Power Manager application
    Sample implementation of a power manager app that uses the zephyr
    power management infrastructure. This app demonstrates use of the
    hooks to implement Low Power State and Tickless idle power saving
    policies.
    
    Tested on quark_se.
    
    Origin: Original
    Change-Id: I10207014f2a844374b4c7a1c0ed50f2fb5c3c440
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 58d71d4efb1eb95082b2e1f8d367f2c3511006e4

---
+++ samples/power/power_mgr/src/main.c
@@ -0,0 +1,17 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|P_LVL2|macro|P_LVL2
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
+DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
+DECL|check_pm_policy|function|static int check_pm_policy(int32_t ticks)
+DECL|do_low_power|function|static int do_low_power(int32_t ticks)
+DECL|do_tickless_idle|function|static int do_tickless_idle(int32_t ticks)
+DECL|end_time|variable|end_time
+DECL|low_power_resume|function|static void low_power_resume(void)
+DECL|main|function|void main(void)
+DECL|pm_state|variable|pm_state
+DECL|quark_low_power|function|static void quark_low_power(void)
+DECL|rtc_dev|variable|rtc_dev
+DECL|start_time|variable|start_time
+DECL|tickless_idle_resume|function|static void tickless_idle_resume(void)
