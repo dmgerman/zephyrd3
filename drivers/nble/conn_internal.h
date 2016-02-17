83170b5f80a843de9364da2d939ff79bd3bc8119
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-02-17 11:32:11 +0200

    drivers/nble: Add connection states
    Connection state helps to keep make right choice when connecting /
    disconnecting.
    
    Change-Id: Ifea620c05f869a633f578bf5d5c8ba603a58a46a
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: b125e38f9c91b81d70c46b5054620881ab49b276

--- drivers/nble/conn_internal.h
+++ drivers/nble/conn_internal.h
@@ -1,3 +1,7 @@
+DECL|BT_CONN_CONNECTED|enumerator|BT_CONN_CONNECTED,
+DECL|BT_CONN_CONNECT|enumerator|BT_CONN_CONNECT,
+DECL|BT_CONN_DISCONNECTED|enumerator|BT_CONN_DISCONNECTED,
+DECL|BT_CONN_DISCONNECT|enumerator|BT_CONN_DISCONNECT,
 DECL|bt_conn|struct|struct bt_conn {
 DECL|dst|member|bt_addr_le_t dst;
 DECL|handle|member|uint16_t handle;
@@ -5,4 +9,5 @@ DECL|interval|member|uint16_t interval;
 DECL|latency|member|uint16_t latency;
 DECL|ref|member|atomic_t ref;
 DECL|role|member|uint8_t role;
+DECL|state|member|} state;
 DECL|timeout|member|uint16_t timeout;
