b60f3c902b6a3bc68c241cf0a8ce54f952c7efd6
Baohong Liu <baohong.liu@intel.com>
2016-02-18 10:52:19 -0800

    drivers: framework for a generic flash driver
    The framework for a generic flash driver.
    
    Change-Id: I030eb6d88338f4d6611fc6e5cae42ac586cfdb12
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 29c8f95ec4a60e572dd40d8ceeed4606e269af11

---
+++ include/flash.h
@@ -0,0 +1,14 @@
+DECL|__FLASH_H__|macro|__FLASH_H__
+DECL|erase|member|flash_api_erase erase;
+DECL|flash_api_erase|typedef|typedef int (*flash_api_erase)(struct device *dev, uint32_t offset,
+DECL|flash_api_read|typedef|typedef int (*flash_api_read)(struct device *dev, uint32_t offset,
+DECL|flash_api_write_protection|typedef|typedef int (*flash_api_write_protection)(struct device *dev, bool enable);
+DECL|flash_api_write|typedef|typedef int (*flash_api_write)(struct device *dev, uint32_t offset,
+DECL|flash_driver_api|struct|struct flash_driver_api {
+DECL|flash_erase|function|static inline int flash_erase(struct device *dev, uint32_t offset, uint32_t size)
+DECL|flash_read|function|static inline int flash_read(struct device *dev, uint32_t offset, unsigned int len, uint8_t *data)
+DECL|flash_write_protection_set|function|static inline int flash_write_protection_set(struct device *dev, bool enable)
+DECL|flash_write|function|static inline int flash_write(struct device *dev, uint32_t offset, unsigned int len, uint8_t *data)
+DECL|read|member|flash_api_read read;
+DECL|write_protection|member|flash_api_write_protection write_protection;
+DECL|write|member|flash_api_write write;
