6ba016db3131de85fa11db957de3b75d54eb3236
Peter Mitsis <peter.mitsis@windriver.com>
2016-02-19 13:26:42 -0500

    nanokernel: Add routines for waking a fiber
    Adds the following routines for waking a fiber that was previously
    put to sleep using fiber_sleep().
        isr_fiber_wakeup()
        fiber_fiber_wakeup()
        task_fiber_wakeup()
    
    Change-Id: I7d78ee6997163d71b92f388a7b4c484f2e97862b
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: b4313cef6f66e1aa979136fd58bfaf00117787a0

--- kernel/nanokernel/nano_sleep.c
+++ kernel/nanokernel/nano_sleep.c
@@ -1,2 +1,4 @@
+DECL|_fiber_wakeup|function|void _fiber_wakeup(nano_thread_id_t fiber)
 DECL|fiber_sleep|function|void fiber_sleep(int32_t timeout_in_ticks)
+DECL|task_fiber_wakeup|function|void task_fiber_wakeup(nano_thread_id_t fiber)
 DECL|task_sleep|function|void task_sleep(int32_t timeout_in_ticks)
