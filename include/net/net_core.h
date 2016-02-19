62559d3902856e89a5e591101ac29fdd3b142e63
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-02-19 10:04:07 +0100

    net: ip: Make sure CONFIG_NETWORKING_WITH_LOGGING is properly handled
    If the user unsets CONFIG_NETWORKING_WITH_LOGGING, it will mean he
    really doesn't want to get any network stack logging output.
    
    Change-Id: I0629a045adbf8916327a8a9ba69b6e6e9ee2e7d9
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 050ec1355604014125f740546777f86990d3d272

--- include/net/net_core.h
+++ include/net/net_core.h
@@ -1,6 +1,10 @@
 DECL|NET_DBG|macro|NET_DBG
+DECL|NET_DBG|macro|NET_DBG
+DECL|NET_ERR|macro|NET_ERR
 DECL|NET_ERR|macro|NET_ERR
 DECL|NET_INFO|macro|NET_INFO
+DECL|NET_INFO|macro|NET_INFO
+DECL|NET_PRINT|macro|NET_PRINT
 DECL|NET_PRINT|macro|NET_PRINT
 DECL|__NET_CORE_H|macro|__NET_CORE_H
 DECL|head_reserve|member|size_t head_reserve;
