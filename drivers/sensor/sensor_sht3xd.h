b24c3f3a72bb5bd247cc19255bb9a3162c6283b4
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-02-22 16:22:16 +0200

    sensor: add driver for SHT3xD sensors
    Add device driver for the SHT3xD temperature and humidity sensors.
    
    Datasheets:
    	https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/Humidity_Sensors/Sensirion_Humidity_Sensors_SHT3x_Datasheet_digital.pdf
    	https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/Humidity_Sensors/Sensirion_Humidity_Sensors_SHT3x_Application_Note_Alert_Mode_DIS_V1.pdf
    
    Origin: Original
    Change-Id: I9cda2f1bc7172e93ce34fde63c6febf0b8b28289
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: 653874c73f1c14654e9eb48455290ad089a08fd8

---
+++ drivers/sensor/sensor_sht3xd.h
@@ -0,0 +1,38 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|SHT3XD_CLEAR_STATUS_WAIT_USEC|macro|SHT3XD_CLEAR_STATUS_WAIT_USEC
+DECL|SHT3XD_CMD_ART|macro|SHT3XD_CMD_ART
+DECL|SHT3XD_CMD_CLEAR_STATUS|macro|SHT3XD_CMD_CLEAR_STATUS
+DECL|SHT3XD_CMD_FETCH|macro|SHT3XD_CMD_FETCH
+DECL|SHT3XD_CMD_READ_STATUS|macro|SHT3XD_CMD_READ_STATUS
+DECL|SHT3XD_CMD_WRITE_TH_HIGH_CLEAR|macro|SHT3XD_CMD_WRITE_TH_HIGH_CLEAR
+DECL|SHT3XD_CMD_WRITE_TH_HIGH_SET|macro|SHT3XD_CMD_WRITE_TH_HIGH_SET
+DECL|SHT3XD_CMD_WRITE_TH_LOW_CLEAR|macro|SHT3XD_CMD_WRITE_TH_LOW_CLEAR
+DECL|SHT3XD_CMD_WRITE_TH_LOW_SET|macro|SHT3XD_CMD_WRITE_TH_LOW_SET
+DECL|SHT3XD_I2C_ADDRESS|macro|SHT3XD_I2C_ADDRESS
+DECL|SHT3XD_I2C_ADDRESS|macro|SHT3XD_I2C_ADDRESS
+DECL|SHT3XD_MPS_IDX|macro|SHT3XD_MPS_IDX
+DECL|SHT3XD_MPS_IDX|macro|SHT3XD_MPS_IDX
+DECL|SHT3XD_MPS_IDX|macro|SHT3XD_MPS_IDX
+DECL|SHT3XD_MPS_IDX|macro|SHT3XD_MPS_IDX
+DECL|SHT3XD_MPS_IDX|macro|SHT3XD_MPS_IDX
+DECL|SHT3XD_REPEATABILITY_IDX|macro|SHT3XD_REPEATABILITY_IDX
+DECL|SHT3XD_REPEATABILITY_IDX|macro|SHT3XD_REPEATABILITY_IDX
+DECL|SHT3XD_REPEATABILITY_IDX|macro|SHT3XD_REPEATABILITY_IDX
+DECL|_SENSOR_SHT3XD_|macro|_SENSOR_SHT3XD_
+DECL|fiber_stack|member|char __stack fiber_stack[CONFIG_SHT3XD_FIBER_STACK_SIZE];
+DECL|gpio_sem|member|struct nano_sem gpio_sem;
+DECL|gpio|member|struct device *gpio;
+DECL|handler|member|sensor_trigger_handler_t handler;
+DECL|i2c|member|struct device *i2c;
+DECL|rh_high|member|uint16_t rh_high;
+DECL|rh_low|member|uint16_t rh_low;
+DECL|rh_sample|member|uint16_t rh_sample;
+DECL|sht3xd_data|struct|struct sht3xd_data {
+DECL|sht3xd_measure_cmd|variable|sht3xd_measure_cmd
+DECL|sht3xd_measure_wait|variable|sht3xd_measure_wait
+DECL|t_high|member|uint16_t t_high;
+DECL|t_low|member|uint16_t t_low;
+DECL|t_sample|member|uint16_t t_sample;
+DECL|trigger|member|struct sensor_trigger trigger;
+DECL|work|member|struct sensor_work work;
