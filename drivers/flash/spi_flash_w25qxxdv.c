495f6562e9fe29042ead781c1f223f8a08985c80
Baohong Liu <baohong.liu@intel.com>
2016-02-22 10:48:08 -0800

    drivers: WinBond SPI flash support
    SPI flash sub-driver to support WinBond SPI flash.
    It is based on the generic flash driver framework.
    
    Change-Id: I58202e38445a052fa0556b03f854e75ef836e2dd
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: f3da05f4cb7a1c1619d22c86ec2d045e6066ec81

---
+++ drivers/flash/spi_flash_w25qxxdv.c
@@ -0,0 +1,13 @@
+DECL|spi_flash_api|variable|spi_flash_api
+DECL|spi_flash_init|function|static int spi_flash_init(struct device *dev)
+DECL|spi_flash_memory_data|variable|spi_flash_memory_data
+DECL|spi_flash_wb_config|function|static int spi_flash_wb_config(struct device *dev)
+DECL|spi_flash_wb_erase_internal|function|static inline int spi_flash_wb_erase_internal(struct device *dev, uint32_t offset, uint32_t size)
+DECL|spi_flash_wb_erase|function|static int spi_flash_wb_erase(struct device *dev, uint32_t offset, uint32_t size)
+DECL|spi_flash_wb_id|function|static inline int spi_flash_wb_id(struct device *dev)
+DECL|spi_flash_wb_read|function|static int spi_flash_wb_read(struct device *dev, uint32_t offset, unsigned int len, uint8_t *data)
+DECL|spi_flash_wb_reg_read|function|static int spi_flash_wb_reg_read(struct device *dev, uint8_t *data)
+DECL|spi_flash_wb_reg_write|function|static int spi_flash_wb_reg_write(struct device *dev, uint8_t *data)
+DECL|spi_flash_wb_write_protection_set|function|static int spi_flash_wb_write_protection_set(struct device *dev, bool enable)
+DECL|spi_flash_wb_write|function|static int spi_flash_wb_write(struct device *dev, uint32_t offset, unsigned int len, uint8_t *data)
+DECL|wait_for_flash_idle|function|static inline void wait_for_flash_idle(struct device *dev)
