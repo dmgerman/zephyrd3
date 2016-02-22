cb23eb148054071185c66655614e462d1cf4e645
Peter Mitsis <peter.mitsis@windriver.com>
2016-02-22 11:05:52 -0500

    test: Add nanokernel test_sleep project
    The 'test_sleep' nanokernel project tests the following functionality:
       1. Normal expiration of fiber_sleep()
       2. Waking a sleeping fiber via fiber_fiber_wakeup()
       3. Waking a sleeping fiber via isr_fiber_wakeup()
       4. Waking a sleeping fiber via task_fiber_wakeup()
       5. Normal expiration of task_sleep()
    
    Change-Id: Ie51997ace9a4413f62d77daacd6dff97b6b3a4dd
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: e0f8162a8bc1e664f62a25a109285bbd29b6ef8a

---
+++ tests/kernel/test_sleep/src/sleep.c
@@ -0,0 +1,18 @@
+DECL|FIBER_STACKSIZE|macro|FIBER_STACKSIZE
+DECL|HELPER_FIBER_PRIORITY|macro|HELPER_FIBER_PRIORITY
+DECL|ONE_SECOND|macro|ONE_SECOND
+DECL|TEST_FIBER_PRIORITY|macro|TEST_FIBER_PRIORITY
+DECL|align_to_tick_boundary|function|static void align_to_tick_boundary(void)
+DECL|helper_fiber_id|variable|helper_fiber_id
+DECL|helper_fiber_sem|variable|helper_fiber_sem
+DECL|helper_fiber_stack|variable|helper_fiber_stack
+DECL|helper_fiber|function|static void helper_fiber(int arg1, int arg2)
+DECL|irq_offload_isr|function|static void irq_offload_isr(void *arg)
+DECL|main|function|void main(void)
+DECL|task_sem|variable|task_sem
+DECL|test_failure|variable|test_failure
+DECL|test_fiber_id|variable|test_fiber_id
+DECL|test_fiber_sem|variable|test_fiber_sem
+DECL|test_fiber_stack|variable|test_fiber_stack
+DECL|test_fiber|function|static void test_fiber(int arg1, int arg2)
+DECL|test_objects_init|function|static void test_objects_init(void)
