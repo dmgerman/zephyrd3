1aac7532fb2d0d21a39ac01cf4b2130bef33fad6
Mariusz Skamra <mariusz.skamra@tieto.com>
2016-02-23 17:01:03 +0100

    Bluetooth: tester: Return BTP error if requested unknown attr ID
    With this patch BTP error will be returned if attribute with
    requested ID don't exist in GATT attribute database.
    
    Change-Id: Ic6125c6359e75a80fb9a51b6a527ee0a98628e6d
    Signed-off-by: Mariusz Skamra <mariusz.skamra@tieto.com>
    
    Former-commit-id: f84bf33acad7f1ceef419ba5096dfbb643d446d0

--- tests/bluetooth/tester/src/gatt.c
+++ tests/bluetooth/tester/src/gatt.c
@@ -7,20 +7,26 @@ DECL|add_ccc|function|static struct bt_gatt_attr *add_ccc(const struct bt_gatt_a
 DECL|add_cep|function|static struct bt_gatt_attr *add_cep(const struct bt_gatt_attr *attr_chrc)
 DECL|add_characteristic_cb|function|static uint8_t add_characteristic_cb(const struct bt_gatt_attr *attr, void *user_data)
 DECL|add_characteristic|function|static void add_characteristic(uint8_t *data, uint16_t len)
+DECL|add_characteristic|struct|struct add_characteristic {
 DECL|add_descriptor_cb|function|static uint8_t add_descriptor_cb(const struct bt_gatt_attr *attr, void *user_data)
 DECL|add_descriptor|function|static void add_descriptor(uint8_t *data, uint16_t len)
+DECL|add_descriptor|struct|struct add_descriptor {
 DECL|add_included_cb|function|static uint8_t add_included_cb(const struct bt_gatt_attr *attr, void *user_data)
 DECL|add_included|function|static void add_included(uint8_t *data, uint16_t len)
 DECL|add_service|function|static void add_service(uint8_t *data, uint16_t len)
 DECL|btp2bt_uuid|function|static uint8_t btp2bt_uuid(const uint8_t *uuid, uint8_t len, struct bt_uuid *bt_uuid)
 DECL|btp_opcode|variable|btp_opcode
+DECL|btp_status|member|uint8_t btp_status;
+DECL|btp_status|member|uint8_t btp_status;
 DECL|buf|member|uint8_t buf[MAX_BUFFER_SIZE];
 DECL|ccc_added|variable|ccc_added
 DECL|ccc_cfg_changed|function|static void ccc_cfg_changed(uint16_t value)
 DECL|ccc_cfg|variable|ccc_cfg
 DECL|ccc|variable|ccc
+DECL|char_id|member|uint16_t char_id;
 DECL|config_subscription|function|static void config_subscription(uint8_t *data, uint16_t len, uint16_t op)
 DECL|data|member|uint8_t *data;
+DECL|desc_id|member|uint16_t desc_id;
 DECL|disable_subscription|function|static int disable_subscription(struct bt_conn *conn, uint16_t ccc_handle)
 DECL|disc_all_chrc|function|static void disc_all_chrc(uint8_t *data, uint16_t len)
 DECL|disc_all_desc_cb|function|static uint8_t disc_all_desc_cb(struct bt_conn *conn,const struct bt_gatt_attr *attr, struct bt_gatt_discover_params *params)
@@ -50,10 +56,15 @@ DECL|gatt_db|variable|gatt_db
 DECL|gatt_value|struct|struct gatt_value {
 DECL|get_service_handles|function|static uint8_t get_service_handles(const struct bt_gatt_attr *attr, void *user_data)
 DECL|has_ccc|member|bool has_ccc;
+DECL|key_size|member|uint8_t key_size;
+DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
 DECL|notify_func|function|static uint8_t notify_func(struct bt_conn *conn, struct bt_gatt_subscribe_params *params, const void *data, uint16_t length)
+DECL|permissions|member|uint8_t permissions;
+DECL|permissions|member|uint8_t permissions;
 DECL|prep_data|member|uint8_t *prep_data;
+DECL|properties|member|uint8_t properties;
 DECL|read_cb|function|static uint8_t read_cb(struct bt_conn *conn, int err, struct bt_gatt_read_params *params, const void *data, uint16_t length)
 DECL|read_destroy|function|static void read_destroy(struct bt_gatt_read_params *params)
 DECL|read_long|function|static void read_long(uint8_t *data, uint16_t len)
@@ -64,8 +75,10 @@ DECL|read|function|static void read(uint8_t *data, uint16_t len)
 DECL|set_cep_value|function|static uint8_t set_cep_value(struct bt_gatt_attr *attr, const void *value, const uint16_t len)
 DECL|set_enc_key_size_cb|function|static uint8_t set_enc_key_size_cb(const struct bt_gatt_attr *attr, void *user_data)
 DECL|set_enc_key_size|function|static void set_enc_key_size(uint8_t *data, uint16_t len)
+DECL|set_enc_key_size|struct|struct set_enc_key_size {
 DECL|set_value_cb|function|static uint8_t set_value_cb(struct bt_gatt_attr *attr, void *user_data)
 DECL|set_value|function|static void set_value(uint8_t *data, uint16_t len)
+DECL|set_value|struct|struct set_value {
 DECL|start_server|function|static void start_server(uint8_t *data, uint16_t len)
 DECL|subscribe_params|variable|subscribe_params
 DECL|supported_commands|function|static void supported_commands(uint8_t *data, uint16_t len)
@@ -73,9 +86,12 @@ DECL|tester_handle_gatt|function|void tester_handle_gatt(uint8_t opcode, uint8_t
 DECL|tester_init_gatt|function|uint8_t tester_init_gatt(void)
 DECL|u128|member|struct bt_uuid_128 u128;
 DECL|u16|member|struct bt_uuid_16 u16;
+DECL|uuid|member|const struct bt_uuid *uuid;
+DECL|uuid|member|const struct bt_uuid *uuid;
 DECL|uuid|member|struct bt_uuid uuid;
 DECL|uuid|union|union uuid {
 DECL|uuid|variable|uuid
+DECL|value|member|const uint8_t *value;
 DECL|write_long_rsp|function|static void write_long_rsp(struct bt_conn *conn, uint8_t err)
 DECL|write_long|function|static void write_long(uint8_t *data, uint16_t len)
 DECL|write_rsp|function|static void write_rsp(struct bt_conn *conn, uint8_t err)
