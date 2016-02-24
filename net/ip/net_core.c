c5ca7283fd1ff40bdeb98960eb3500ecf4f5cf16
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-02-24 17:47:55 +0200

    net: Timer fiber now sleeps until next event
    The network timers will now use the fiber wakeup to let the
    network timer fiber to get the next event. This enables the
    timer fiber to sleep much longer and only wakeup when there
    is something to do.
    
    Change-Id: Ia69d92392c02b884c036793de41b38b6489c4d2e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1aeca8d569f9e77b0a0df2c780f3d8ab0f97be85

--- net/ip/net_core.c
+++ net/ip/net_core.c
@@ -2,7 +2,6 @@ DECL|CONFIG_IP_RX_STACK_SIZE|macro|CONFIG_IP_RX_STACK_SIZE
 DECL|CONFIG_IP_TIMER_STACK_SIZE|macro|CONFIG_IP_TIMER_STACK_SIZE
 DECL|CONFIG_IP_TX_STACK_SIZE|macro|CONFIG_IP_TX_STACK_SIZE
 DECL|DEBUG|macro|DEBUG
-DECL|DEFAULT_TIMER_WAKEUP|macro|DEFAULT_TIMER_WAKEUP
 DECL|IEEE802154_STAT|macro|IEEE802154_STAT
 DECL|MAC_STAT|macro|MAC_STAT
 DECL|MAX_TIMER_WAKEUP|macro|MAX_TIMER_WAKEUP
@@ -29,6 +28,7 @@ DECL|net_rx_fiber|function|static void net_rx_fiber(void)
 DECL|net_send|function|int net_send(struct net_buf *buf)
 DECL|net_set_mac|function|int net_set_mac(uint8_t *mac, uint8_t len)
 DECL|net_tcpip_output|function|static uint8_t net_tcpip_output(struct net_buf *buf, const uip_lladdr_t *lladdr)
+DECL|net_timer_check|function|void net_timer_check(void)
 DECL|net_timer_fiber|function|static void net_timer_fiber(void)
 DECL|net_tx_fiber|function|static void net_tx_fiber(void)
 DECL|net_unregister_driver|function|void net_unregister_driver(struct net_driver *drv)
@@ -37,6 +37,7 @@ DECL|network_initialization|function|static int network_initialization(void)
 DECL|rx_fiber_stack|variable|rx_fiber_stack
 DECL|rx_queue|member|struct nano_fifo rx_queue;
 DECL|stats|function|static void stats(void)
+DECL|timer_fiber_id|variable|timer_fiber_id
 DECL|timer_fiber_stack|variable|timer_fiber_stack
 DECL|tx_fiber_stack|variable|tx_fiber_stack
 DECL|tx_queue|member|struct nano_fifo tx_queue;
