65a9518867f5ea4c9739df57c11f74b29b408b9f
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-02-25 17:24:16 -0800

    adc:  QMSI ADC driver
    This creates the QMSI ADC driver which is simply a shim driver based
    on the ADC driver provided by QMSI BSP.
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_ADC=y
    CONFIG_ADC_QMSI=y
    
    Optionally this can be set:
    CONFIG_ADC_IRQ
    CONFIG_ADC_PRI
    CONFIG_ADC_QMSI_POLL
    CONFIG_ADC_QMSI_INTERRUPT
    CONFIG_ADC_QMSI_CALIBRATION
    CONFIG_ADC_QMSI_CLOCK_RATIO
    CONFIG_ADC_QMSI_SERIAL_DELAY
    CONFIG_ADC_QMSI_SAMPLE_WIDTH
    
    Origin of the file: This file has been created from scratch
    
    Change-Id: Ie04776ac2ed88e56852409070edec568974f8e0d
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: b47df6e800e7820f3dc7dc14330c16c44f2506e4

---
+++ drivers/adc/adc_qmsi.c
@@ -0,0 +1,22 @@
+DECL|ADC_STATE_BUSY|enumerator|ADC_STATE_BUSY,
+DECL|ADC_STATE_ERROR|enumerator|ADC_STATE_ERROR
+DECL|ADC_STATE_IDLE|enumerator|ADC_STATE_IDLE,
+DECL|adc_config_irq|function|static void adc_config_irq(void)
+DECL|adc_context|variable|adc_context
+DECL|adc_info_dev|variable|adc_info_dev
+DECL|adc_info|struct|struct adc_info {
+DECL|adc_lock|function|static void adc_lock(struct adc_info *data)
+DECL|adc_qmsi_disable|function|static void adc_qmsi_disable(struct device *dev)
+DECL|adc_qmsi_enable|function|static void adc_qmsi_enable(struct device *dev)
+DECL|adc_qmsi_enable|function|static void adc_qmsi_enable(struct device *dev)
+DECL|adc_qmsi_init|function|int adc_qmsi_init(struct device *dev)
+DECL|adc_qmsi_isr|function|void adc_qmsi_isr(void *arg)
+DECL|adc_qmsi_read|function|static int adc_qmsi_read(struct device *dev, struct adc_seq_table *seq_tbl)
+DECL|adc_qmsi_read|function|static int adc_qmsi_read(struct device *dev, struct adc_seq_table *seq_tbl)
+DECL|adc_unlock|function|static void adc_unlock(struct adc_info *data)
+DECL|api_funcs|variable|api_funcs
+DECL|complete_callback|function|static void complete_callback(void)
+DECL|error_callback|function|static void error_callback(void)
+DECL|sem|member|struct nano_sem sem;
+DECL|state|member|atomic_t state;
+DECL|sync|member|device_sync_call_t sync;
