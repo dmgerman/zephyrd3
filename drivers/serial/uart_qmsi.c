1577c5c3e031a759b4f075d3c0bd31e61d0204aa
Andre Guedes <andre.guedes@intel.com>
2016-02-26 14:51:28 -0300

    uart: Introduce QMSI shim driver
    This patch introduces the QMSI UART driver which is simply a shim driver
    based on UART driver provided by QMSI BSP.
    
    This initial version implements only the mandatory APIs 'poll_in',
    'poll_out' and 'err_check' which are required by trivial sample apps and
    by output functionality from the console driver. The remaining APIs will
    be implemented by up coming patches. The driver supports only 115200 baud
    rate at the moment.
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_SERIAL=y
    CONFIG_UART_QMSI=y
    
    This driver has been tested with Quark SE Devboard so this patch also
    adds its platform-specific default configuration options to 'arch/x86/
    soc/quark_se/Kconfig'.
    
    Change-Id: Ibde1825d4b0349a376a8e7d91cc9de306946b62f
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: d95d6c63a1c15712c1201a29a45bd72fe4abfdf9

---
+++ drivers/serial/uart_qmsi.c
@@ -0,0 +1,11 @@
+DECL|GET_CONTROLLER_INSTANCE|macro|GET_CONTROLLER_INSTANCE
+DECL|api|variable|api
+DECL|clock_gate|member|clk_periph_t clock_gate;
+DECL|config_info_0|variable|config_info_0
+DECL|config_info_1|variable|config_info_1
+DECL|instance|member|qm_uart_t instance;
+DECL|uart_qmsi_config_info|struct|struct uart_qmsi_config_info {
+DECL|uart_qmsi_err_check|function|static int uart_qmsi_err_check(struct device *dev)
+DECL|uart_qmsi_init|function|static int uart_qmsi_init(struct device *dev)
+DECL|uart_qmsi_poll_in|function|static int uart_qmsi_poll_in(struct device *dev, unsigned char *data)
+DECL|uart_qmsi_poll_out|function|static unsigned char uart_qmsi_poll_out(struct device *dev,unsigned char data)
