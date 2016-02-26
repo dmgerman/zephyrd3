d85a8233d731ff60ba92ce94a727ea43b3faaf3f
Jeff Blais <jeff.blais@windriver.com>
2016-02-26 15:27:38 -0500

    arm: K64F Pulse Width Modulation (PWM) support
    PWM support using the Freescale K64 FlexTimer Module (FTM)
    
    Change-Id: Iaad429c01bd877babba04e84d6a4679bd7e38120
    Work-by: Mike Hirst <michael.hirst@windriver.com>
    Signed-off-by: Jeff Blais <jeff.blais@windriver.com>
    
    Former-commit-id: 1f90470a276101b20f49e3f40ced1accf2a4a540

---
+++ drivers/pwm/pwm_k64_ftm.c
@@ -0,0 +1,22 @@
+DECL|COMBINE_MODE_SUPPORT|macro|COMBINE_MODE_SUPPORT
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|MAX_PWM_OUT|macro|MAX_PWM_OUT
+DECL|pwm_ftm_0_cfg|variable|pwm_ftm_0_cfg
+DECL|pwm_ftm_0_drvdata|variable|pwm_ftm_0_drvdata
+DECL|pwm_ftm_1_cfg|variable|pwm_ftm_1_cfg
+DECL|pwm_ftm_1_drvdata|variable|pwm_ftm_1_drvdata
+DECL|pwm_ftm_2_cfg|variable|pwm_ftm_2_cfg
+DECL|pwm_ftm_2_drvdata|variable|pwm_ftm_2_drvdata
+DECL|pwm_ftm_3_cfg|variable|pwm_ftm_3_cfg
+DECL|pwm_ftm_3_drvdata|variable|pwm_ftm_3_drvdata
+DECL|pwm_ftm_clk_enable|function|static int pwm_ftm_clk_enable(uint8_t ftm_num)
+DECL|pwm_ftm_configure|function|static int pwm_ftm_configure(struct device *dev, int access_op, uint32_t channel, int flags)
+DECL|pwm_ftm_drv_api_funcs|variable|pwm_ftm_drv_api_funcs
+DECL|pwm_ftm_init|function|int pwm_ftm_init(struct device *dev)
+DECL|pwm_ftm_resume|function|static int pwm_ftm_resume(struct device *dev)
+DECL|pwm_ftm_set_duty_cycle|function|static int pwm_ftm_set_duty_cycle(struct device *dev, int access_op, uint32_t channel, uint8_t duty)
+DECL|pwm_ftm_set_phase|function|static int pwm_ftm_set_phase(struct device *dev, int access_op, uint32_t channel, uint8_t phase)
+DECL|pwm_ftm_set_values|function|static int pwm_ftm_set_values(struct device *dev, int access_op, uint32_t channel, uint32_t on, uint32_t off)
+DECL|pwm_ftm_suspend|function|static int pwm_ftm_suspend(struct device *dev)
