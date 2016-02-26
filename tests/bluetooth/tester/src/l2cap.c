ae7d5a39a7841cb33bc8a88496b4ded87082547e
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
2016-02-26 14:28:05 +0100

    Bluetooth: tester: Add L2CAP init method
    This adds method which initialize data pool for L2CAP data.
    
    Change-Id: I2b170489aa4b2afc3c1f962591f377e23d147161
    Signed-off-by: Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@tieto.com>
    Signed-off-by: Mariusz Skamra <mariusz.skamra@tieto.com>
    
    Former-commit-id: 81e10314efe4a4f8a22905cb5118ce5f38497f9f

--- tests/bluetooth/tester/src/l2cap.c
+++ tests/bluetooth/tester/src/l2cap.c
@@ -1,3 +1,6 @@
 DECL|CONTROLLER_INDEX|macro|CONTROLLER_INDEX
+DECL|DATA_MTU|macro|DATA_MTU
+DECL|data_fifo|variable|data_fifo
 DECL|supported_commands|function|static void supported_commands(uint8_t *data, uint16_t len)
 DECL|tester_handle_l2cap|function|void tester_handle_l2cap(uint8_t opcode, uint8_t index, uint8_t *data, uint16_t len)
+DECL|tester_init_l2cap|function|uint8_t tester_init_l2cap(void)
