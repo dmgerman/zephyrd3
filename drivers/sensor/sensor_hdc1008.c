995a9a6302e09f360c78f5a58dafe5bc72fc0234
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-02-26 14:40:25 +0200

    sensor: add driver for HDC1008 temperature and humidity sensor
    Add driver for the HDC1008 temperature and relative humidity
    sensor.
    
    Datasheet:
    	http://www.ti.com/lit/ds/symlink/hdc1008.pdf
    
    Origin: Original
    Change-Id: I63cb4cdd94120b80d0d6f3205da073f0817c4f17
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: ef65f155c21536b3ca21450f2814ed281edc360d

---
+++ drivers/sensor/sensor_hdc1008.c
@@ -0,0 +1,8 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|hdc1008_channel_get|function|static int hdc1008_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|hdc1008_driver_api|variable|hdc1008_driver_api
+DECL|hdc1008_driver|variable|hdc1008_driver
+DECL|hdc1008_gpio_callback|function|static void hdc1008_gpio_callback(struct device *dev, uint32_t pin)
+DECL|hdc1008_init|function|int hdc1008_init(struct device *dev)
+DECL|hdc1008_sample_fetch|function|static int hdc1008_sample_fetch(struct device *dev)
---
+++ drivers/sensor/sensor_hdc1008.h
@@ -0,0 +1,13 @@
+DECL|HDC1008_I2C_ADDRESS|macro|HDC1008_I2C_ADDRESS
+DECL|HDC1008_I2C_ADDRESS|macro|HDC1008_I2C_ADDRESS
+DECL|HDC1008_I2C_ADDRESS|macro|HDC1008_I2C_ADDRESS
+DECL|HDC1008_I2C_ADDRESS|macro|HDC1008_I2C_ADDRESS
+DECL|HDC1008_REG_HUMIDITY|macro|HDC1008_REG_HUMIDITY
+DECL|HDC1008_REG_TEMP|macro|HDC1008_REG_TEMP
+DECL|_SENSOR_HDC1008|macro|_SENSOR_HDC1008
+DECL|data_sem|member|struct nano_sem data_sem;
+DECL|gpio|member|struct device *gpio;
+DECL|hdc1008_data|struct|struct hdc1008_data {
+DECL|i2c|member|struct device *i2c;
+DECL|rh_sample|member|uint16_t rh_sample;
+DECL|t_sample|member|uint16_t t_sample;
