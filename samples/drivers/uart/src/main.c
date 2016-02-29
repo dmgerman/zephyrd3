996aad38b2873b91a1ff2b5ad91293d4d5674456
Andre Guedes <andre.guedes@intel.com>
2016-02-29 17:23:09 -0300

    samples: Add UART application
    This patch adds a very simple UART sample application which demonstrates
    how to use the UART APIs. This sample is also useful to quickly verify
    if a given UART driver is working properly. For now, the application
    tests uart_poll_in() and uart_poll_out() APIs only, but new APIs should
    be added in future.
    
    Change-Id: If815f358f11efb058e947291b234d3d4130581d8
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: 59912cf13f7763331f818520c3692883933e085b

---
+++ samples/drivers/uart/src/main.c
@@ -0,0 +1,7 @@
+DECL|UART_DEVICE|macro|UART_DEVICE
+DECL|UART_DEVICE|macro|UART_DEVICE
+DECL|UART_DEVICE|macro|UART_DEVICE
+DECL|banner1|variable|banner1
+DECL|banner2|variable|banner2
+DECL|main|function|void main(void)
+DECL|write_string|function|static void write_string(struct device *dev, const char *str, int len)
