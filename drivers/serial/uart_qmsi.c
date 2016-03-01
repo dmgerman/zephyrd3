5b3fe1942d06f04bd440b22c9f8157b6b54032cc
Andre Guedes <andre.guedes@intel.com>
2016-03-01 16:25:03 -0300

    uart: qmsi: Support for line control and driver command
    This patch extends the UART QMSI driver so it supports line control and
    driver command APIs.
    
    The line control function supports the baud rate option only (LINE_CTRL_
    BAUD_RATE) and, at the moment, the QMSI driver doesn't have any custom
    command.
    
    Change-Id: Icc10cb39b4077ed4ccfefb1f0feecec75b79d67c
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: 1de58be8664cbdbaf5036f2c49862a9f9b779925

--- drivers/serial/uart_qmsi.c
+++ drivers/serial/uart_qmsi.c
@@ -8,7 +8,9 @@ DECL|config_info_0|variable|config_info_0
 DECL|config_info_1|variable|config_info_1
 DECL|instance|member|qm_uart_t instance;
 DECL|uart_qmsi_config_info|struct|struct uart_qmsi_config_info {
+DECL|uart_qmsi_drv_cmd|function|static int uart_qmsi_drv_cmd(struct device *dev, uint32_t cmd, uint32_t p)
 DECL|uart_qmsi_err_check|function|static int uart_qmsi_err_check(struct device *dev)
 DECL|uart_qmsi_init|function|static int uart_qmsi_init(struct device *dev)
+DECL|uart_qmsi_line_ctrl_set|function|static int uart_qmsi_line_ctrl_set(struct device *dev, uint32_t ctrl, uint32_t val)
 DECL|uart_qmsi_poll_in|function|static int uart_qmsi_poll_in(struct device *dev, unsigned char *data)
 DECL|uart_qmsi_poll_out|function|static unsigned char uart_qmsi_poll_out(struct device *dev,unsigned char data)
