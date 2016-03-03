ef15d680aa9463077ba0333c3c77a58bf426de13
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-03-03 10:14:12 +0200

    drivers/nble: Implement GATT write request
    Implement GATT write and handle response.
    
    Change-Id: I5f1dd48e12149534d1c7bb0096a5bddc68fd8a0d
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: a8612d5aa848effc48bb3ec92c7ce9b35e4d1f22

--- drivers/nble/gatt.c
+++ drivers/nble/gatt.c
@@ -34,6 +34,7 @@ DECL|nble_gatt_service|struct|struct nble_gatt_service {
 DECL|on_nble_gatt_register_rsp|function|void on_nble_gatt_register_rsp(const struct nble_gatt_register_rsp *rsp, const struct nble_gatt_attr_handles *handles, uint8_t len)
 DECL|on_nble_gattc_discover_rsp|function|void on_nble_gattc_discover_rsp(const struct nble_gattc_discover_rsp *rsp,const uint8_t *data, uint8_t data_len)
 DECL|on_nble_gattc_read_rsp|function|void on_nble_gattc_read_rsp(const struct nble_gattc_read_rsp *rsp, uint8_t *data, uint8_t len, void *user_data)
+DECL|on_nble_gattc_write_rsp|function|void on_nble_gattc_write_rsp(const struct nble_gattc_write_rsp *rsp, void *user_data)
 DECL|on_nble_gatts_read_evt|function|void on_nble_gatts_read_evt(const struct nble_gatt_rd_evt *evt)
 DECL|on_nble_gatts_write_evt|function|void on_nble_gatts_write_evt(const struct nble_gatt_wr_evt *evt, const uint8_t *buf, uint8_t buflen)
 DECL|parse_characteristic|function|static uint16_t parse_characteristic(struct bt_conn *conn, const uint8_t *data, uint8_t len)
