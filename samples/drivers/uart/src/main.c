568efc11f102be68f104e1969ddd5cc170b8dbf0
Andre Guedes <andre.guedes@intel.com>
2016-03-04 20:30:12 -0300

    samples: uart: Test irq-based APIs
    This patch extends the uart sample so the same functionality (write
    buffer, read char then write buffer again) is implemented using
    irq-based APIs.
    
    Change-Id: Iefb14dae2b253f90da64ccef8c123619ed494aca
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: eeec6088aaf36c4933a7845fcf2808bd5d34bf7e

--- samples/drivers/uart/src/main.c
+++ samples/drivers/uart/src/main.c
@@ -3,5 +3,13 @@ DECL|UART_DEVICE|macro|UART_DEVICE
 DECL|UART_DEVICE|macro|UART_DEVICE
 DECL|banner1|variable|banner1
 DECL|banner2|variable|banner2
+DECL|data_arrived|variable|data_arrived
+DECL|data_transmitted|variable|data_transmitted
+DECL|interrupt_handler|function|static void interrupt_handler(struct device *dev)
 DECL|main|function|void main(void)
+DECL|new_data|variable|new_data
+DECL|read_char_irq|function|static void read_char_irq(struct device *dev, char *data)
+DECL|test_by_irq|function|static void test_by_irq(struct device *dev)
+DECL|test_by_polling|function|static void test_by_polling(struct device *dev)
+DECL|write_buf_irq|function|static void write_buf_irq(struct device *dev, const char *buf, int len)
 DECL|write_string|function|static void write_string(struct device *dev, const char *str, int len)
