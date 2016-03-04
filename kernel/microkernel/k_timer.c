c85a75156bceba3bf8eba4a0b885696224d7dd1f
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2016-03-04 12:21:54 -0500

    microkernel: Add support for *_sleep() during initialization
    Add support for task_sleep() and fiber_sleep() during the
    system initialization. When CONFIG_NANO_TIMEOUTS defined,
    before the k_server() starts, kernel uses nanokernel
    system clock announce and task sleep functionality.
    
    To give device drivers early sleep functionality, the system
    clock has to start on SECONDARY initialization level, same
    as most of the drivers.
    
    Change-Id: I5b3cf3da4c8d8398a966e901ab211f2fcee18dd6
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 0d50329105b74b361d04237d44823bf55b0dc9b3

--- kernel/microkernel/k_timer.c
+++ kernel/microkernel/k_timer.c
@@ -1,3 +1,5 @@
+DECL|_do_task_sleep|variable|_do_task_sleep
+DECL|_do_task_sleep|variable|_do_task_sleep
 DECL|_k_task_sleep|function|void _k_task_sleep(struct k_args *P)
 DECL|_k_task_wakeup|function|void _k_task_wakeup(struct k_args *P)
 DECL|_k_timeout_alloc|function|void _k_timeout_alloc(struct k_args *P)
@@ -12,7 +14,7 @@ DECL|_k_timer_list_tail|variable|_k_timer_list_tail
 DECL|_k_timer_list_update|function|void _k_timer_list_update(int ticks)
 DECL|_k_timer_start|function|void _k_timer_start(struct k_args *P)
 DECL|_k_timer_stop|function|void _k_timer_stop(struct k_args *P)
-DECL|task_sleep|function|void task_sleep(int32_t ticks)
+DECL|_micro_task_sleep|function|void _micro_task_sleep(int32_t ticks)
 DECL|task_timer_alloc|function|ktimer_t task_timer_alloc(void)
 DECL|task_timer_free|function|void task_timer_free(ktimer_t timer)
 DECL|task_timer_start|function|void task_timer_start(ktimer_t timer, int32_t duration, int32_t period, ksem_t sema)
