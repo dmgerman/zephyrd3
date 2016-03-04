4d2b954b243a57e8e4f551d65e77ac00cab88c07
Jithu Joseph <jithu.joseph@intel.com>
2016-03-04 14:44:25 -0800

    memory_pool_heap: malloc/free access over a heap memory pool
    Specifying  HEAP_SIZE keyword in an app's MDEF file, results in
    creating a new memory pool, which can be accessed using the
    task_malloc() and task_free() APIs, which have the usual malloc/free
    like semantics.
    
    Expected format in MDEF file
    HEAP_SIZE    <value>
    
    Change-Id: I0569cffeecf8a2c23c20c7b359256123ece91982
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 9e45411a59282d95d13ec8bd23766a2740daad15

--- kernel/microkernel/k_memory_pool.c
+++ kernel/microkernel/k_memory_pool.c
@@ -2,6 +2,7 @@ DECL|AD_AFTER_SEARCH4BIGGERBLOCK|macro|AD_AFTER_SEARCH4BIGGERBLOCK
 DECL|AD_BEFORE_SEARCH4BIGGERBLOCK|macro|AD_BEFORE_SEARCH4BIGGERBLOCK
 DECL|AD_NONE|macro|AD_NONE
 DECL|AUTODEFRAG|macro|AUTODEFRAG
+DECL|MALLOC_ALIGN|macro|MALLOC_ALIGN
 DECL|_k_block_waiters_get|function|void _k_block_waiters_get(struct k_args *A)
 DECL|_k_defrag|function|void _k_defrag(struct k_args *A)
 DECL|_k_mem_pool_block_get_timeout_handle|function|void _k_mem_pool_block_get_timeout_handle(struct k_args *A)
@@ -12,6 +13,8 @@ DECL|defrag|function|static void defrag(struct pool_struct *P, int ifraglevel_st
 DECL|get_block_recusive|function|static char *get_block_recusive(struct pool_struct *P, int index, int startindex)
 DECL|search_block_on_frag_level|function|static char *search_block_on_frag_level(struct pool_block *pfraglevelinfo, int *piblockindex)
 DECL|search_bp|function|static void search_bp(char *ptr, struct pool_struct *P, int index)
+DECL|task_free|function|void task_free(void *ptr)
+DECL|task_malloc|function|void *task_malloc(uint32_t size)
 DECL|task_mem_pool_alloc|function|int task_mem_pool_alloc(struct k_block *blockptr, kmemory_pool_t pool_id, int reqsize, int32_t timeout)
 DECL|task_mem_pool_defragment|function|void task_mem_pool_defragment(kmemory_pool_t Pid)
 DECL|task_mem_pool_free|function|void task_mem_pool_free(struct k_block *blockptr)
