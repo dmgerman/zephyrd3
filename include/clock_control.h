aaa12cf4bbda75781c660edd0fe3c462259dbb74
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-05 15:08:51 +0100

    clock_control: extend API with clock rate query operation
    Extend the public API of clock_control driver with a function for
    querying of clock rate driving given peripheral. This is useful when the
    driver must calculate or configurae values that depend on the rate of
    clock driving this subsystem.
    
    A baud rate setting of UART port is an example of such operation. To
    configure a particular baud rate the driver may need to set the internal
    counters of UART port, where the actual value will depend on the clock
    rate for this peripheral.
    
    The implementation returns DEV_NO_SUPPORT for clock_control drivers that
    do not implement such functionality.
    
    Change-Id: I4e7ec96cd3678a4bb1ff289b0247488289310f2d
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    Former-commit-id: 14482f09402435dbf087b17522c4e28f8e1ad5fb

--- include/clock_control.h
+++ include/clock_control.h
@@ -1,9 +1,12 @@
 DECL|CLOCK_CONTROL_SUBSYS_ALL|macro|CLOCK_CONTROL_SUBSYS_ALL
 DECL|__CLOCK_CONTROL_H__|macro|__CLOCK_CONTROL_H__
 DECL|clock_control_driver_api|struct|struct clock_control_driver_api {
+DECL|clock_control_get_rate|function|static inline int clock_control_get_rate(struct device *dev, clock_control_subsys_t sys, uint32_t *rate)
+DECL|clock_control_get|typedef|typedef int (*clock_control_get)(struct device *dev,
 DECL|clock_control_off|function|static inline int clock_control_off(struct device *dev, clock_control_subsys_t sys)
 DECL|clock_control_on|function|static inline int clock_control_on(struct device *dev, clock_control_subsys_t sys)
 DECL|clock_control_subsys_t|typedef|typedef void *clock_control_subsys_t;
 DECL|clock_control|typedef|typedef int (*clock_control)(struct device *dev, clock_control_subsys_t sys);
+DECL|get_rate|member|clock_control_get get_rate;
 DECL|off|member|clock_control off;
 DECL|on|member|clock_control on;
