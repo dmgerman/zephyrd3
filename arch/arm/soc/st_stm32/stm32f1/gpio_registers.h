8e611252733a521888e9b118b9bb07abb1404c30
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-06 13:50:48 +0100

    soc/stm32f1/gpio: implement GPIO support
    Add functions required for integration with common STM32 GPIO driver.
    
    Change-Id: Ic6637e991f5c0cf659f3b927ed47ef482b13b64f
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    Former-commit-id: 3642f9d6a3d423f1de0edc38f068d73c936443f4

---
+++ arch/arm/soc/st_stm32/stm32f1/gpio_registers.h
@@ -0,0 +1,9 @@
+DECL|_STM32F10X_GPIO_REGISTERS_H_|macro|_STM32F10X_GPIO_REGISTERS_H_
+DECL|brr|member|uint32_t brr;
+DECL|bsrr|member|uint32_t bsrr;
+DECL|crh|member|uint32_t crh;
+DECL|crl|member|uint32_t crl;
+DECL|idr|member|uint32_t idr;
+DECL|lckr|member|uint32_t lckr;
+DECL|odr|member|uint32_t odr;
+DECL|stm32f10x_gpio|struct|struct stm32f10x_gpio {
