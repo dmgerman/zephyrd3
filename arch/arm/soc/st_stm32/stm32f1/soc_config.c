78414a05c4b3c361a4bfab53845260aec361190d
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-06 13:53:00 +0100

    soc/stm32f1/pinmux: implement STM32 pinmux integration
    Add necessary integration code enabling use of common STM32 pinmux
    driver.
    
    The alternate function listing currently consists of USART1 pins
    only. The listing should be updated when support for more devices is
    added.
    
    Change-Id: Ic65aeea9df9aaea7636ecdd6996f56e6ef59dc2f
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    Former-commit-id: a8700114b3d2262f04c8216ff29eefca4073491a

---
+++ arch/arm/soc/st_stm32/stm32f1/soc_config.c
@@ -0,0 +1,9 @@
+DECL|pin_pa10_funcs|variable|pin_pa10_funcs
+DECL|pin_pa2_funcs|variable|pin_pa2_funcs
+DECL|pin_pa3_funcs|variable|pin_pa3_funcs
+DECL|pin_pa9_funcs|variable|pin_pa9_funcs
+DECL|pin_pb10_funcs|variable|pin_pb10_funcs
+DECL|pin_pb11_funcs|variable|pin_pb11_funcs
+DECL|pins|variable|pins
+DECL|stm32_get_pin_config|function|int stm32_get_pin_config(int pin, int func)
+DECL|stm32_get_port_clock|function|clock_control_subsys_t stm32_get_port_clock(int port)
---
+++ arch/arm/soc/st_stm32/stm32f1/soc_pinmux.c
@@ -0,0 +1,1 @@
+DECL|stm32_pin_configure|function|int stm32_pin_configure(int pin, int func)
