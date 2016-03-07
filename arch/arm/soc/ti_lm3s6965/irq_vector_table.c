4c16c927dd2a09a51c4adcbbd9755b2ac986071e
Andrew Boie <andrew.p.boie@intel.com>
2016-03-07 13:31:50 -0800

    arm: move irq_vector_table to common location
    We don't support hard-coding vectors in this table anymore.
    If someone really wants to do this, they can set
    IRQ_VECTOR_TABLE_CUSTOM and define their own.
    
    Change-Id: I45f49782ba5fefb0a02eab02ec96efd0019bc6d5
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 5415d945f8f2e206622d174f504e49daeaa35b86

--- arch/arm/soc/ti_lm3s6965/irq_vector_table.c
+++
@@ -1,6 +0,0 @@
-DECL|_irq_vector_table|variable|_irq_vector_table
-DECL|_irq_vector_table|variable|_irq_vector_table
-DECL|_uart_stellaris_port_0_isr|function|static void _uart_stellaris_port_0_isr(void)
-DECL|_uart_stellaris_port_1_isr|function|static void _uart_stellaris_port_1_isr(void)
-DECL|_uart_stellaris_port_2_isr|function|static void _uart_stellaris_port_2_isr(void)
-DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
