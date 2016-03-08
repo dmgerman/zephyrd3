76485d02d8cc61c99246080ce957b0fe6d4d7708
Andrew Boie <andrew.p.boie@intel.com>
2016-03-08 10:29:56 -0800

    ipm: add demo code for inter-processor mailboxes
    Migrated from internal repository.
    Run "make flash" for each on the same Arduino 101 device.
    
    Origin: Original code, Zephyr "hello world" used as a starting point
    Change-Id: I3fdeed6b7e85ad703983c2674e265c85a365ce5b
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 8554331b94b60d98fba98cb887694c57253211a1

---
+++ samples/ipm/ipm_demo_arc/src/hello.c
@@ -0,0 +1,12 @@
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|counters|variable|counters
+DECL|dat1|variable|dat1
+DECL|dat2|variable|dat2
+DECL|fiberStack|variable|fiberStack
+DECL|main|function|void main(void)
+DECL|message_ipm_callback|function|void message_ipm_callback(void *context, uint32_t id, volatile void *data)
+DECL|nanoSemFiber|variable|nanoSemFiber
+DECL|nanoSemTask|variable|nanoSemTask
+DECL|ping_ipm_callback|function|void ping_ipm_callback(void *context, uint32_t id, volatile void *data)
---
+++ samples/ipm/ipm_demo_lmt/src/hello.c
@@ -0,0 +1,20 @@
+DECL|MAIN_FIBER_PRI|macro|MAIN_FIBER_PRI
+DECL|MSG_FIBER_PRI|macro|MSG_FIBER_PRI
+DECL|PING_FIBER_PRI|macro|PING_FIBER_PRI
+DECL|PING_TICKS|macro|PING_TICKS
+DECL|SCSS_REGISTER_BASE|macro|SCSS_REGISTER_BASE
+DECL|SCSS_SS_STS|macro|SCSS_SS_STS
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|dat1|variable|dat1
+DECL|dat2|variable|dat2
+DECL|fiber_stacks|variable|fiber_stacks
+DECL|main_fiber|function|void main_fiber(int arg1, int arg2)
+DECL|main_task|function|void main_task(void)
+DECL|message_source_task_0|function|void message_source_task_0(void)
+DECL|message_source_task_1|function|void message_source_task_1(void)
+DECL|message_source_task_2|function|void message_source_task_2(void)
+DECL|message_source|function|void message_source(struct device *ipm)
+DECL|ping_source_fiber|function|void ping_source_fiber(int arg1, int arg2)
+DECL|scss_reg|function|uint32_t scss_reg(uint32_t offset)
