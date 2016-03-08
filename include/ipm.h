3f7b9535fecb78beeb5a01845b76a388d057cc29
Andrew Boie <andrew.p.boie@intel.com>
2016-03-08 10:53:45 -0800

    ipm: correct prototype for ipm_send
    ipm_send_t properly had 'const void *' for the data payload, but
    not the ipm_send() API.
    
    Change-Id: I62a424d37bc17f5bc3646c71d3ff3b029f52cbcc
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 53e6275b3ab169ea70bcd50783871cfa97daa15e

--- include/ipm.h
+++ include/ipm.h
@@ -8,7 +8,7 @@ DECL|ipm_max_id_val_get|function|static inline uint32_t ipm_max_id_val_get(struc
 DECL|ipm_register_callback_t|typedef|typedef void (*ipm_register_callback_t)(struct device *port, ipm_callback_t cb,
 DECL|ipm_register_callback|function|static inline void ipm_register_callback(struct device *ipmdev, ipm_callback_t cb, void *context)
 DECL|ipm_send_t|typedef|typedef int (*ipm_send_t)(struct device *ipmdev, int wait, uint32_t id,
-DECL|ipm_send|function|static inline int ipm_send(struct device *ipmdev, int wait, uint32_t id, void *data, int size)
+DECL|ipm_send|function|static inline int ipm_send(struct device *ipmdev, int wait, uint32_t id, const void *data, int size)
 DECL|ipm_set_enabled_t|typedef|typedef int (*ipm_set_enabled_t)(struct device *ipmdev, int enable);
 DECL|ipm_set_enabled|function|static inline int ipm_set_enabled(struct device *ipmdev, int enable)
 DECL|max_data_size_get|member|ipm_max_data_size_get_t max_data_size_get;
