a06cca4ec18a0a5540304bb12f76c8967eb6d7ac
Johan Hedberg <johan.hedberg@intel.com>
2016-03-09 21:45:46 +0200

    libc: Add off_t definition
    Add definition for the POSIX standard off_t type.
    
    Change-Id: I1142428a3d226d641a8628cbba71cb3ea341ef92
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 41f8a3355504d91e542b09808673d8a9e1aa591f

--- lib/libc/minimal/include/sys/types.h
+++ lib/libc/minimal/include/sys/types.h
@@ -1,5 +1,9 @@
 DECL|__INC_sys_types_h__|macro|__INC_sys_types_h__
+DECL|__off_t_defined|macro|__off_t_defined
 DECL|__ssize_t_defined|macro|__ssize_t_defined
+DECL|off_t|typedef|typedef int off_t;
+DECL|off_t|typedef|typedef int off_t;
+DECL|off_t|typedef|typedef long int off_t;
 DECL|ssize_t|typedef|typedef int ssize_t;
 DECL|ssize_t|typedef|typedef int ssize_t;
 DECL|ssize_t|typedef|typedef long int ssize_t;
