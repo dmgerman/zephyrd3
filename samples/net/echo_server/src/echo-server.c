9cf3423705184b4f6d35ee0087bdad70416a6f7e
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-10 00:11:28 +0200

    net: apps: Change echo-server to use common testing header file
    Move network testing setup from echo-server to common net_testing.h
    file which makes the application much simpler.
    
    Change-Id: I6f68826916c870ad7db597d30f20245bada70091
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: ed4f794ac8d93292942a34745073cfe7c0d85159

--- samples/net/echo_server/src/echo-server.c
+++ samples/net/echo_server/src/echo-server.c
@@ -1,21 +1,14 @@
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
-DECL|MY_IPADDR|macro|MY_IPADDR
-DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
-DECL|PEER_IPADDR|macro|PEER_IPADDR
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|fiberStack|variable|fiberStack
 DECL|get_context|function|static inline bool get_context(struct net_context **recv, struct net_context **mcast_recv)
-DECL|in6addr_my|variable|in6addr_my
-DECL|in6addr_peer|variable|in6addr_peer
 DECL|init_app|function|static inline void init_app(void)
 DECL|main|function|void main(void)
-DECL|my_mac|variable|my_mac
-DECL|peer_mac|variable|peer_mac
 DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const char *name, const char *type, struct net_buf *buf)
 DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *recv, struct net_context *mcast_recv)
 DECL|receive|function|void receive(void)
