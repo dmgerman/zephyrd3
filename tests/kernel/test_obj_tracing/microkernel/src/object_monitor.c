9b96fbd9a24f867ac78b00c51a2d79a28fd7855d
Juan Manuel Cruz <juan.m.cruz.alcaraz@intel.com>
2016-03-09 15:57:41 -0600

    debug: object tracing sanity test includes thread monitor.
    Change-Id: Ic57ed69374b2075bcdf5cd5d72b83b03b59fbac5
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@intel.com>
    
    Former-commit-id: 5f566034e5ccacfc0fa387ed72fc469a242a9314

--- tests/kernel/test_obj_tracing/microkernel/src/object_monitor.c
+++ tests/kernel/test_obj_tracing/microkernel/src/object_monitor.c
@@ -1,5 +1,8 @@
+DECL|DELTA_THREADS|macro|DELTA_THREADS
+DECL|DELTA_THREADS|macro|DELTA_THREADS
 DECL|OBJ_LIST_NAME|macro|OBJ_LIST_NAME
 DECL|OBJ_LIST_NAME|macro|OBJ_LIST_NAME
 DECL|OBJ_LIST_TYPE|macro|OBJ_LIST_TYPE
 DECL|OBJ_LIST_TYPE|macro|OBJ_LIST_TYPE
 DECL|object_monitor|function|void object_monitor(void)
+DECL|test_thread_monitor|function|static inline int test_thread_monitor(void)
