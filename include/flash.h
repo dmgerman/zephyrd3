d969776160e9e2309c940e80bbc7becdf135146b
Johan Hedberg <johan.hedberg@intel.com>
2016-03-09 20:11:26 +0200

    flash: Reorder data & len parameters
    This makes the ordering consistent e.g. with SPI, Bluetooth and
    Networking APIs. This also follows the order of parameters in the
    POSIX read/write APIs.
    
    Change-Id: I4f11c8c90ccadf176d79f6a7bbd98aac61c26cf5
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: a82ed2d9057044f6140ee34e4c100c03d7029fdc

--- include/flash.h
+++ include/flash.h
@@ -6,9 +6,9 @@ DECL|flash_api_write_protection|typedef|typedef int (*flash_api_write_protection
 DECL|flash_api_write|typedef|typedef int (*flash_api_write)(struct device *dev, size_t offset,
 DECL|flash_driver_api|struct|struct flash_driver_api {
 DECL|flash_erase|function|static inline int flash_erase(struct device *dev, size_t offset, size_t size)
-DECL|flash_read|function|static inline int flash_read(struct device *dev, size_t offset, size_t len, void *data)
+DECL|flash_read|function|static inline int flash_read(struct device *dev, size_t offset, void *data, size_t len)
 DECL|flash_write_protection_set|function|static inline int flash_write_protection_set(struct device *dev, bool enable)
-DECL|flash_write|function|static inline int flash_write(struct device *dev, size_t offset, size_t len, const void *data)
+DECL|flash_write|function|static inline int flash_write(struct device *dev, size_t offset, const void *data, size_t len)
 DECL|read|member|flash_api_read read;
 DECL|write_protection|member|flash_api_write_protection write_protection;
 DECL|write|member|flash_api_write write;
