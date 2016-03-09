19ae65fc31ac4f41dc24ecf8daee24ee0ec02026
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-10 00:11:29 +0200

    net: apps: Change echo-client to use common testing header file
    Move network testing setup from echo-client to common net_testing.h
    file which makes the application much simpler.
    
    Change-Id: Ib1a90b9d87014b7f9adc5f6865ce0fc9faa13422
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: efe0d36c7d09c7cd68e8ec2a54bb02044970b866

--- samples/net/echo_client/src/echo-client.c
+++ samples/net/echo_client/src/echo-client.c
@@ -1,7 +1,5 @@
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
-DECL|MY_IPADDR|macro|MY_IPADDR
-DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
 DECL|PEER_IPADDR|macro|PEER_IPADDR
 DECL|PEER_IPADDR|macro|PEER_IPADDR
@@ -14,15 +12,11 @@ DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|expecting|variable|expecting
 DECL|get_context|function|static inline bool get_context(struct net_context **unicast, struct net_context **multicast)
-DECL|in6addr_my|variable|in6addr_my
-DECL|in6addr_peer|variable|in6addr_peer
 DECL|init_app|function|static inline void init_app(void)
 DECL|ipsum_len|variable|ipsum_len
 DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
 DECL|multicast|variable|multicast
-DECL|my_mac|variable|my_mac
-DECL|peer_mac|variable|peer_mac
 DECL|receiving|function|void receiving(void)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
 DECL|send_packet|function|static inline bool send_packet(const char *name, struct net_context *ctx, int ipsum_len, int pos)
