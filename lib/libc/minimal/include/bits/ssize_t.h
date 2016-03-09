6f3011dbbb9552d180ed6da79b77917bb7bde6e4
Johan Hedberg <johan.hedberg@intel.com>
2016-03-09 21:42:01 +0200

    libc: Move ssize_t definition to sys/types.h
    There's no need to have a separate file for this, and we'll be adding
    other types like off_t here soon as well.
    
    Change-Id: I40629a5a0fba7af44828eaeead294e4e55844bb0
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6692e982988ebd83742a0cb4ba09380290420719

--- lib/libc/minimal/include/bits/ssize_t.h
+++
@@ -1,4 +0,0 @@
-DECL|__ssize_t_defined|macro|__ssize_t_defined
-DECL|ssize_t|typedef|typedef int ssize_t;
-DECL|ssize_t|typedef|typedef int ssize_t;
-DECL|ssize_t|typedef|typedef long int ssize_t;
--- lib/libc/minimal/include/sys/types.h
+++ lib/libc/minimal/include/sys/types.h
@@ -1,1 +1,5 @@
 DECL|__INC_sys_types_h__|macro|__INC_sys_types_h__
+DECL|__ssize_t_defined|macro|__ssize_t_defined
+DECL|ssize_t|typedef|typedef int ssize_t;
+DECL|ssize_t|typedef|typedef int ssize_t;
+DECL|ssize_t|typedef|typedef long int ssize_t;
