0ab1f584cb840cb3e2677e1b6526552d1d02e002
Andre Guedes <andre.guedes@intel.com>
2016-03-10 15:41:30 -0300

    samples: Add STTS751 sample application
    This patch introduces the i2c_stts751 sample application which
    illustrates how to use the I2C APIs from Zephyr. The slave device
    is the STTS751 sensor which is present in Quark SE devboard. So
    no wiring instructions are required in order to get this sample
    working.
    
    The application reads the temperature register from STTS751 sensor
    and prints the temperature value. This sample is specific to Quark
    SE Devboard, but it can be easily extended to support any other
    board with the STTS751 sensor.
    
    Change-Id: I9b56aa73645d4741942fc793f1450c6a1c7beb29
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    
    Former-commit-id: 9a8c79f08aa644b767b32e07b553820ba07ec900

---
+++ samples/drivers/i2c_stts751/src/main.c
@@ -0,0 +1,5 @@
+DECL|STTS751_I2C_ADDR|macro|STTS751_I2C_ADDR
+DECL|TEMP_VAL_HIGH_REG_ADDR|macro|TEMP_VAL_HIGH_REG_ADDR
+DECL|main|function|void main(void)
+DECL|read_temperature_by_transfer|function|static void read_temperature_by_transfer(struct device *dev)
+DECL|read_temperature|function|static void read_temperature(struct device *dev)
