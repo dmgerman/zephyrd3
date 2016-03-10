949ce662a4001e82fd9d08945307f231e2abec1f
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-03-09 18:46:15 -0800

    aio:  QMSI analog comparator driver
    This creates the QMSI comparator driver which is simply a shim driver
    based on the comparator driver provided by QMSI BSP.
    
    In order to enable this driver, the following options should be set:
    CONFIG_QMSI_DRIVERS=y
    CONFIG_QMSI_INSTALL_PATH="/path/to/libqmsi/directory"
    CONFIG_AIO_COMPARATOR=y
    CONFIG_AIO_QMSI_COMPARATOR=y
    
    Origin of the file: Original
    
    Change-Id: Iad01cb80f7bb1eff1710cd76cd0afeb70c311e04
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 07da8afd4d6caa19ae216cb0780ff6b5cca4bbbc

---
+++ drivers/aio/aio_qmsi_comparator.c
@@ -0,0 +1,15 @@
+DECL|AIO_QMSI_CMP_COUNT|macro|AIO_QMSI_CMP_COUNT
+DECL|INT_COMPARATORS_MASK|macro|INT_COMPARATORS_MASK
+DECL|aio_cmp_config|function|static int aio_cmp_config(struct device *dev)
+DECL|aio_cmp_funcs|variable|aio_cmp_funcs
+DECL|aio_qmsi_cmp_cb|struct|struct aio_qmsi_cmp_cb {
+DECL|aio_qmsi_cmp_configure|function|static int aio_qmsi_cmp_configure(struct device *dev, uint8_t index, enum aio_cmp_polarity polarity, enum aio_cmp_ref refsel, aio_cmp_cb cb, void *param)
+DECL|aio_qmsi_cmp_dev_data_t|struct|struct aio_qmsi_cmp_dev_data_t {
+DECL|aio_qmsi_cmp_dev_data|variable|aio_qmsi_cmp_dev_data
+DECL|aio_qmsi_cmp_disable|function|static int aio_qmsi_cmp_disable(struct device *dev, uint8_t index)
+DECL|aio_qmsi_cmp_init|function|int aio_qmsi_cmp_init(struct device *dev)
+DECL|aio_qmsi_cmp_isr|function|void aio_qmsi_cmp_isr(struct device *dev)
+DECL|cb|member|aio_cmp_cb cb;
+DECL|cb|member|struct aio_qmsi_cmp_cb cb[AIO_QMSI_CMP_COUNT];
+DECL|num_cmp|member|uint8_t num_cmp;
+DECL|param|member|void *param;
