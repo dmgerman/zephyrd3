97e310ecaa1c479cfb43bf6319787d7b2a1bba99
Andrew Boie <andrew.p.boie@intel.com>
2016-03-10 13:55:56 -0800

    iamcu: fix -fstack-protector
    One of the tricks that GCC's stack protector does is to stick a
    sentinel value on the stack at the beginning of the function, and
    check if it is still there when the function is about to return.
    However, since this function switches stacks that fails and we get
    a stack protector exception before main() even starts.
    
    Change-Id: I2acba8b8c822d7447d8e371bb72603f36e87f54b
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 97594df3214c9ab1ed4e9a74cd469c54491846dc

--- arch/x86/core/iamcu_abi/swap.c
+++ arch/x86/core/iamcu_abi/swap.c
@@ -1,2 +1,2 @@
-DECL|_Swap|function|unsigned int _Swap(unsigned int eflags)
+DECL|_Swap|function|_Swap(unsigned int eflags)
 DECL|_sys_k_event_logger_context_switch|macro|_sys_k_event_logger_context_switch
