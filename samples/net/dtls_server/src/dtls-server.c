79bd9d06c43daf59a1c85ee44afb84b5fee78a3e
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-10 20:51:18 +0200

    net: apps: Change dtls-server to use common testing header file
    Move network testing setup from dtls-server to common net_testing.h
    file which makes the application much simpler.
    
    Change-Id: Iacd99917cda3efc4cd92d0f48a8cbbc259ef7a84
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 8399fc4b3d6a35dea88f0c91ca4af39ed6a961d3

--- samples/net/dtls_server/src/dtls-server.c
+++ samples/net/dtls_server/src/dtls-server.c
@@ -1,5 +1,3 @@
-DECL|MY_IPADDR|macro|MY_IPADDR
-DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
@@ -13,14 +11,11 @@ DECL|get_context|function|static inline struct net_context *get_context(void)
 DECL|get_ecdsa_key|function|static int get_ecdsa_key(struct dtls_context_t *ctx, const session_t *session, const dtls_ecdsa_key_t **result)
 DECL|get_psk_info|function|static int get_psk_info(struct dtls_context_t *ctx,const session_t *session, dtls_credentials_type_t type, const unsigned char *id, size_t id_len, unsigned char *result, size_t result_length)
 DECL|handle_event|function|static int handle_event(struct dtls_context_t *ctx, session_t *session,dtls_alert_level_t level, unsigned short code)
-DECL|in4addr_any|variable|in4addr_any
 DECL|in4addr_my|variable|in4addr_my
-DECL|in6addr_any|variable|in6addr_any
 DECL|in6addr_my|variable|in6addr_my
 DECL|init_app|function|static inline void init_app(void)
 DECL|init_dtls|function|static void init_dtls(struct net_context *recv, dtls_context_t **dtls)
 DECL|main|function|void main(void)
-DECL|my_mac|variable|my_mac
 DECL|read_from_peer|function|static int read_from_peer(struct dtls_context_t *ctx, session_t *session, uint8 *data, size_t len)
 DECL|receive_message|function|static inline void receive_message(const char *name, struct net_context *recv, dtls_context_t *dtls)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
