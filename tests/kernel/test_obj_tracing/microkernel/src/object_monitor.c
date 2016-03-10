40054198ad4ddaef597bbc1a67c9819cfcafa282
Juan Manuel Cruz <juan.m.cruz.alcaraz@intel.com>
2016-03-09 19:55:17 -0600

    debug: add task tracing to sanity test
    Change-Id: I2d9691d094a764b9e884ef5f7a2700bedcf71745
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@intel.com>
    
    Former-commit-id: 6d040a89ff69f0acdbb11afef725f122a24db3ab

--- tests/kernel/test_obj_tracing/microkernel/src/object_monitor.c
+++ tests/kernel/test_obj_tracing/microkernel/src/object_monitor.c
@@ -4,5 +4,8 @@ DECL|OBJ_LIST_NAME|macro|OBJ_LIST_NAME
 DECL|OBJ_LIST_NAME|macro|OBJ_LIST_NAME
 DECL|OBJ_LIST_TYPE|macro|OBJ_LIST_TYPE
 DECL|OBJ_LIST_TYPE|macro|OBJ_LIST_TYPE
+DECL|TOTAL_TEST_NUMBER|macro|TOTAL_TEST_NUMBER
 DECL|object_monitor|function|void object_monitor(void)
+DECL|test_task_tracing|function|static inline int test_task_tracing(void)
+DECL|test_task_tracing|function|static inline int test_task_tracing(void)
 DECL|test_thread_monitor|function|static inline int test_thread_monitor(void)
