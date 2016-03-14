8f1a8057afc8d64c50bcd5ab81d982d9e4c9d461
Baohong Liu <baohong.liu@intel.com>
2016-03-14 15:01:42 -0700

    samples: A test app for quark Always-on counter and timer
    Test application to test quark Always-on counter and timer.
    
    This app requires QMSI lib which is not in the repo yet.
    See the instruction in readme.txt.
    
    Origin: Original
    
    Change-Id: If75b2714f9b609abc6f7b51c1c0d99d15152399f
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 61064f0c5e58cf5b9aaf609365db5a8f3335779d

---
+++ samples/drivers/aon_counter/src/main.c
@@ -0,0 +1,4 @@
+DECL|aonpt_example_callback|function|static void aonpt_example_callback(struct device *dev, void *user_data)
+DECL|free_running_counter_example|function|static void free_running_counter_example(void)
+DECL|main|function|void main(void)
+DECL|periodic_timer_example|function|static void periodic_timer_example(void)
