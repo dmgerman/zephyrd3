d4b15c1250a435df38cbb12ff793d0f5640cafbd
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-14 17:29:10 +0100

    samples/button: button input example
    An example of button driver.
    
    Change-Id: I8af860058166d7ca5653f50ff585bc9784008f78
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    Former-commit-id: 47d88b25ecb223c9141aa513142d9abd988037da

---
+++ samples/drivers/button/src/main.c
@@ -0,0 +1,8 @@
+DECL|EDGE|macro|EDGE
+DECL|PIN|macro|PIN
+DECL|PORT|macro|PORT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|PULL_UP|macro|PULL_UP
+DECL|button_pressed|function|void button_pressed(struct device *gpiob, uint32_t pin)
+DECL|main|function|void main(void)
