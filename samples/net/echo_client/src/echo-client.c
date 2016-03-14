5888008a730c73bed7e99f9c03de03132c049dca
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-14 15:42:49 +0200

    net: apps: Fix connectivity between echo-server and client
    If the echo-server and echo-client are run in real hw, then
    we need to add the used IPv6 prefix to the system. Otherwise
    the client is not able to send anything because there is no
    route to 2001:db8::/64 network that is used in these examples.
    
    Change-Id: I3a00b77acf1329b3e448bd62ec38cc408c0bd086
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d8c495f24b7fa66b08406a2c0a04a41d1ded224e

--- samples/net/echo_client/src/echo-client.c
+++ samples/net/echo_client/src/echo-client.c
@@ -1,6 +1,8 @@
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
+DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
 DECL|PEER_IPADDR|macro|PEER_IPADDR
 DECL|PEER_IPADDR|macro|PEER_IPADDR
 DECL|PEER_PORT|macro|PEER_PORT
@@ -12,6 +14,8 @@ DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|expecting|variable|expecting
 DECL|get_context|function|static inline bool get_context(struct net_context **unicast, struct net_context **multicast)
+DECL|in6addr_my|variable|in6addr_my
+DECL|in6addr_peer|variable|in6addr_peer
 DECL|init_app|function|static inline void init_app(void)
 DECL|ipsum_len|variable|ipsum_len
 DECL|lorem_ipsum|variable|lorem_ipsum
--- samples/net/echo_server/src/echo-server.c
+++ samples/net/echo_server/src/echo-server.c
@@ -1,12 +1,16 @@
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
+DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|fiberStack|variable|fiberStack
 DECL|get_context|function|static inline bool get_context(struct net_context **recv, struct net_context **mcast_recv)
+DECL|in6addr_my|variable|in6addr_my
+DECL|in6addr_my|variable|in6addr_my
 DECL|init_app|function|static inline void init_app(void)
 DECL|main|function|void main(void)
 DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const char *name, const char *type, struct net_buf *buf)
