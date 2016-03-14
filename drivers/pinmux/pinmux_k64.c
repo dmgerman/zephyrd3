4c0fa82e34903adfd845d2701c8645b7d95e96bb
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-14 18:04:50 -0300

    k64f: Fix mixing GPIO and pinmux concepts
    Setting the direction of a GPIO pin is not the responsibility of the
    pinmux "board" initialisation. This should be left for the GPIO
    utilising application.
    
    Some macros that were only used when setting the pin direction are
    removed.
    
    Change-Id: I5b63d52446a27fe539c89f0639a8dcadf5ea9f80
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 649253c8a873bd2524cfb2fdfa15427837c74237

--- drivers/pinmux/pinmux_k64.c
+++ drivers/pinmux/pinmux_k64.c
@@ -1,5 +1,4 @@
 DECL|PIN_FROM_ID|macro|PIN_FROM_ID
-DECL|_fsl_k64_get_gpio_dev|function|static int _fsl_k64_get_gpio_dev(struct device *dev, mem_addr_t port_base_addr, struct device **gpio_dev_ptr)
 DECL|_fsl_k64_get_pin|function|static int _fsl_k64_get_pin(struct device *dev, uint32_t pin_id, uint32_t *func)
 DECL|_fsl_k64_get_port_addr|function|static int _fsl_k64_get_port_addr(uint8_t pin_id, mem_addr_t *port_addr_ptr)
 DECL|_fsl_k64_set_pin|function|static int _fsl_k64_set_pin(struct device *dev, uint32_t pin_id, uint32_t func)
@@ -14,14 +13,7 @@ DECL|config_port_d|function|static inline int config_port_d(mem_addr_t *addr)
 DECL|config_port_d|macro|config_port_d
 DECL|config_port_e|function|static inline int config_port_e(mem_addr_t *addr)
 DECL|config_port_e|macro|config_port_e
-DECL|fsl_k64_data|struct|struct fsl_k64_data {
 DECL|fsl_k64_dev_get|function|static int fsl_k64_dev_get(struct device *dev, uint32_t pin, uint32_t *func)
 DECL|fsl_k64_dev_set|function|static int fsl_k64_dev_set(struct device *dev, uint32_t pin, uint32_t func)
-DECL|fsl_k64_pinmux_driver|variable|fsl_k64_pinmux_driver
 DECL|fsl_k64_pmux|variable|fsl_k64_pmux
-DECL|gpio_a|member|struct device *gpio_a; /* port A */
-DECL|gpio_b|member|struct device *gpio_b; /* port B */
-DECL|gpio_c|member|struct device *gpio_c; /* port C */
-DECL|gpio_d|member|struct device *gpio_d; /* port D */
-DECL|gpio_e|member|struct device *gpio_e; /* port E */
 DECL|pinmux_fsl_k64_initialize|function|int pinmux_fsl_k64_initialize(struct device *port)
