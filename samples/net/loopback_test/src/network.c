d7de544027484187b80db113cff820d44f11369f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-03-14 12:55:34 +0200

    net: apps: Fix the loopback test application
    The loopback test application works now and it verifies that
    the data sent is the same as data received. This fixes the
    issue described in bug ZEP-101.
    
    Change-Id: I1babfab4db9d4df0e769711aa41983366a8b5065
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b8f28079ecfa05e437b3868bc797d05da478efbf

--- samples/net/loopback_test/src/network.c
+++ samples/net/loopback_test/src/network.c
@@ -1,17 +1,21 @@
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|any_addr|variable|any_addr
-DECL|fiberEntry|function|void fiberEntry(void)
-DECL|fiberStack|variable|fiberStack
+DECL|appdatalen|variable|appdatalen
+DECL|failure|variable|failure
+DECL|fiberReceiverStack|variable|fiberReceiverStack
+DECL|fiberSenderStack|variable|fiberSenderStack
+DECL|fiber_receiver|function|void fiber_receiver(void)
+DECL|fiber_sender|function|void fiber_sender(void)
 DECL|in6addr_any|variable|in6addr_any
 DECL|in6addr_loopback|variable|in6addr_loopback
 DECL|loopback_addr|variable|loopback_addr
 DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
-DECL|nanoSemFiber|variable|nanoSemFiber
-DECL|nanoSemTask|variable|nanoSemTask
+DECL|received|variable|received
+DECL|receiver_id|variable|receiver_id
+DECL|sender_id|variable|sender_id
+DECL|sent|variable|sent
