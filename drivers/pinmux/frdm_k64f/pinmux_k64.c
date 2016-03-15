d8c191bb44758f97a43fe402c836e7eb847070a6
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:28:30 -0300

    pinmux: Move the Freescale FRDM K64F board to the pinmux model
    Change-Id: I50cf5fcf00481a3ebac07ced3aa6f7783765934f
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: a12a25ad00a43a8d93ebe01dfb7f2bf0d7b3d71d

--- drivers/pinmux/frdm_k64f/pinmux_k64.c
+++ drivers/pinmux/frdm_k64f/pinmux_k64.c
@@ -1,8 +1,7 @@
 DECL|PIN_FROM_ID|macro|PIN_FROM_ID
-DECL|_fsl_k64_get_pin|function|static int _fsl_k64_get_pin(struct device *dev, uint32_t pin_id, uint32_t *func)
+DECL|_fsl_k64_get_pin|function|int _fsl_k64_get_pin(uint32_t pin_id, uint32_t *func)
 DECL|_fsl_k64_get_port_addr|function|static int _fsl_k64_get_port_addr(uint8_t pin_id, mem_addr_t *port_addr_ptr)
-DECL|_fsl_k64_set_pin|function|static int _fsl_k64_set_pin(struct device *dev, uint32_t pin_id, uint32_t func)
-DECL|api_funcs|variable|api_funcs
+DECL|_fsl_k64_set_pin|function|int _fsl_k64_set_pin(uint32_t pin_id, uint32_t func)
 DECL|config_port_a|function|static inline int config_port_a(mem_addr_t *addr)
 DECL|config_port_a|macro|config_port_a
 DECL|config_port_b|function|static inline int config_port_b(mem_addr_t *addr)
@@ -13,7 +12,3 @@ DECL|config_port_d|function|static inline int config_port_d(mem_addr_t *addr)
 DECL|config_port_d|macro|config_port_d
 DECL|config_port_e|function|static inline int config_port_e(mem_addr_t *addr)
 DECL|config_port_e|macro|config_port_e
-DECL|fsl_k64_dev_get|function|static int fsl_k64_dev_get(struct device *dev, uint32_t pin, uint32_t *func)
-DECL|fsl_k64_dev_set|function|static int fsl_k64_dev_set(struct device *dev, uint32_t pin, uint32_t func)
-DECL|fsl_k64_pmux|variable|fsl_k64_pmux
-DECL|pinmux_fsl_k64_initialize|function|int pinmux_fsl_k64_initialize(struct device *port)
