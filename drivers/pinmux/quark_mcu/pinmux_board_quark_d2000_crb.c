22404d416fde0ad990a77b6577c623d4d60967e3
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:29:38 -0300

    pinmux: Move the Quark D2000 board to the pinmux model
    Change-Id: If82e7323ffa8547bb6b090872948b5e69715b19b
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 3f24658f29f8f1f360891c9f1e0c477c227b762b

---
+++ drivers/pinmux/quark_mcu/pinmux_board_quark_d2000_crb.c
@@ -0,0 +1,3 @@
+DECL|PINMUX_MAX_REGISTERS|macro|PINMUX_MAX_REGISTERS
+DECL|_pinmux_defaults|function|static void _pinmux_defaults(uint32_t base)
+DECL|pinmux_initialize|function|static int pinmux_initialize(struct device *port)
