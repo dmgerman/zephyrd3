89f3d2d6e131fd4bf8ac218e20b859309d43c6d0
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:24:45 -0300

    pinmux: Move the Arduino Due board to the pinmux model
    Change-Id: I53fb54c0d11bcdbc42191dd4a26787d9beebbf58
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: f25c5ca06117cbc5be672720887196a3af9b20a1

---
+++ drivers/pinmux/sam3x/pinmux_board_arduino_due.c
@@ -0,0 +1,6 @@
+DECL|N_PIOA|macro|N_PIOA
+DECL|N_PIOB|macro|N_PIOB
+DECL|N_PIOC|macro|N_PIOC
+DECL|N_PIOD|macro|N_PIOD
+DECL|__pinmux_defaults|function|static void __pinmux_defaults(void)
+DECL|pinmux_init|function|static int pinmux_init(struct device *port)
