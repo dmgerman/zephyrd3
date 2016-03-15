a3ba42e6628ba43dd4570d5cd5d7ea7f15891e73
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:45:04 -0300

    pinmux_dev: Add driver for Freescale FRDM K64F
    Change-Id: I89fcb707f70e385354f099d27895ac8b30bbdb03
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 3a9a77e77cc40f847dbf9afceadc5fc86f560244

---
+++ drivers/pinmux/dev/pinmux_dev_frdm_k64f.c
@@ -0,0 +1,4 @@
+DECL|api_funcs|variable|api_funcs
+DECL|fsl_k64_dev_get|function|static int fsl_k64_dev_get(struct device *dev, uint32_t pin,uint32_t *func)
+DECL|fsl_k64_dev_set|function|static int fsl_k64_dev_set(struct device *dev, uint32_t pin,uint32_t func)
+DECL|pinmux_fsl_k64_initialize|function|int pinmux_fsl_k64_initialize(struct device *port)
