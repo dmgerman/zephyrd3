4246adeb69738a21ce8e6143a910afd6930ea896
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:43:38 -0300

    pinmux_dev: Add the pinmux_dev driver for Atmel SAM3X
    This introduces the pinmux_dev driver for the Atmel SAM3X.
    
    This driver implements what used to be the pinmux driver API, which
    applications could use to modify the function of pins during runtime.
    
    That functionality is now protected under the CONFIG_PINMUX_DEV option,
    which should only be set during the early enabling of a new board, as
    there is risk of damage to the board when misused.
    
    Change-Id: I3aa00505d2771b53c41fe687c3e5230e804756be
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 05cbf0b20b67a2d203375ae40fc4d6b4c04505bf

---
+++ drivers/pinmux/dev/pinmux_dev_atmel_sam3x.c
@@ -0,0 +1,7 @@
+DECL|_get_port|function|static volatile struct __pio *_get_port(uint32_t pin)
+DECL|api_funcs|variable|api_funcs
+DECL|pinmux_dev_init|function|static int pinmux_dev_init(struct device *port)
+DECL|pinmux_get|function|static int pinmux_get(struct device *dev, uint32_t pin, uint32_t *func)
+DECL|pinmux_input|function|static int pinmux_input(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_pullup|function|static int pinmux_pullup(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_set|function|static int pinmux_set(struct device *dev, uint32_t pin, uint32_t func)
