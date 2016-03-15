a7fc1ed101d8b61d83b50e55eba42c78d8ac27bb
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:47:55 -0300

    pinmux_dev: Add driver using QMSI library
    Change-Id: I81e3b60ac6c4d57e1978a64fdeb651ae52b23b77
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 56bebf9206af8487e672523a7ccff780b1d82d9c

---
+++ drivers/pinmux/dev/pinmux_dev_qmsi.c
@@ -0,0 +1,6 @@
+DECL|api_funcs|variable|api_funcs
+DECL|pinmux_dev_get|function|static int pinmux_dev_get(struct device *dev, uint32_t pin, uint32_t *func)
+DECL|pinmux_dev_initialize|function|static int pinmux_dev_initialize(struct device *port)
+DECL|pinmux_dev_input|function|static int pinmux_dev_input(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_dev_pullup|function|static int pinmux_dev_pullup(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_dev_set|function|static int pinmux_dev_set(struct device *dev, uint32_t pin, uint32_t func)
