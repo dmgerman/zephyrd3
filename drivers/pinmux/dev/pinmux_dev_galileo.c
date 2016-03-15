61356fc08c811d2e8610dc1a9b83756b6a6a9a4d
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-03-15 15:46:08 -0300

    pinmux_dev: Add driver for Galileo board
    Change-Id: I27d3ffc6dca56fa8704a269fee64b51ad1d18f1e
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 95b4be946163e4659d84723c515722df58ae38dd

---
+++ drivers/pinmux/dev/pinmux_dev_galileo.c
@@ -0,0 +1,6 @@
+DECL|api_funcs|variable|api_funcs
+DECL|galileo_dev_get|function|static int galileo_dev_get(struct device *dev,uint32_t pin, uint32_t *func)
+DECL|galileo_dev_input_enable|function|static int galileo_dev_input_enable(struct device *dev, uint32_t pin, uint8_t func)
+DECL|galileo_dev_pullup|function|static int galileo_dev_pullup(struct device *dev, uint32_t pin, uint8_t func)
+DECL|galileo_dev_set|function|static int galileo_dev_set(struct device *dev,uint32_t pin, uint32_t func)
+DECL|pinmux_dev_galileo_initialize|function|static int pinmux_dev_galileo_initialize(struct device *port)
