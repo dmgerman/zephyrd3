e2e91095d5ea77350fdff4b912b5e44b0c74a2e6
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-03-15 14:44:37 +0100

    gpio/stm32: GPIO input with interrupts
    Add support for interrupts for input GPIO pins on STM32 MCUs. The SoC
    support code is expected to provide an implementation of
    stm32_gpio_enable_int() call.
    
    Change-Id: I61bae54b8e044be4d7d8eb60c0c67b71f8c59553
    Origin: Original
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    Former-commit-id: 1524e713c5478a93ee04741b555a7892ec0847d6

--- drivers/gpio/gpio_stm32.c
+++ drivers/gpio/gpio_stm32.c
@@ -4,6 +4,7 @@ DECL|gpio_stm32_disable_callback|function|static int gpio_stm32_disable_callback
 DECL|gpio_stm32_driver|variable|gpio_stm32_driver
 DECL|gpio_stm32_enable_callback|function|static int gpio_stm32_enable_callback(struct device *dev, int access_op, uint32_t pin)
 DECL|gpio_stm32_init|function|static int gpio_stm32_init(struct device *device)
+DECL|gpio_stm32_isr|function|static void gpio_stm32_isr(int line, void *arg)
 DECL|gpio_stm32_read|function|static int gpio_stm32_read(struct device *dev, int access_op, uint32_t pin, uint32_t *value)
 DECL|gpio_stm32_resume_port|function|static int gpio_stm32_resume_port(struct device *dev)
 DECL|gpio_stm32_set_callback|function|static int gpio_stm32_set_callback(struct device *dev, gpio_callback_t callback)
--- drivers/gpio/gpio_stm32.h
+++ drivers/gpio/gpio_stm32.h
@@ -1,5 +1,8 @@
 DECL|_STM32_GPIO_H_|macro|_STM32_GPIO_H_
 DECL|base|member|uint32_t *base;
+DECL|cb|member|gpio_callback_t cb;
 DECL|clock_subsys|member|clock_control_subsys_t clock_subsys;
+DECL|enabled_mask|member|uint32_t enabled_mask;
 DECL|gpio_stm32_config|struct|struct gpio_stm32_config {
+DECL|gpio_stm32_data|struct|struct gpio_stm32_data {
 DECL|port|member|enum stm32_pin_port port;
