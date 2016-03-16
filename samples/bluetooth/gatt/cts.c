2b4a4218d9adc8dc808ec751b2446078f5e5da06
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-03-16 17:44:47 +0200

    Bluetooth: CTS: Add service sample
    This adds a CTS service sample which can be shared with application
    samples.
    
    Change-Id: I38889f71f6ce8fc7b784d0d04af6c97408e2d662
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 7a585ee1372ccabf722cafc930ffdc98ecf7bf3b

---
+++ samples/bluetooth/gatt/cts.c
@@ -0,0 +1,10 @@
+DECL|attrs|variable|attrs
+DECL|ct_ccc_cfg_changed|function|static void ct_ccc_cfg_changed(uint16_t value)
+DECL|ct_ccc_cfg|variable|ct_ccc_cfg
+DECL|ct_update|variable|ct_update
+DECL|cts_init|function|void cts_init(void)
+DECL|cts_notify|function|void cts_notify(void)
+DECL|ct|variable|ct
+DECL|generate_current_time|function|static void generate_current_time(uint8_t *buf)
+DECL|read_ct|function|static ssize_t read_ct(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
+DECL|write_ct|function|static ssize_t write_ct(struct bt_conn *conn, const struct bt_gatt_attr *attr,const void *buf, uint16_t len, uint16_t offset)
--- samples/bluetooth/peripheral/src/main.c
+++ samples/bluetooth/peripheral/src/main.c
@@ -10,20 +10,13 @@ DECL|bt_ready|function|static void bt_ready(int err)
 DECL|buf|member|uint8_t buf[MAX_DATA];
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn, uint8_t err)
-DECL|ct_ccc_cfg_changed|function|static void ct_ccc_cfg_changed(uint16_t value)
-DECL|ct_ccc_cfg|variable|ct_ccc_cfg
-DECL|ct_update|variable|ct_update
-DECL|cts_attrs|variable|cts_attrs
-DECL|ct|variable|ct
 DECL|data|member|uint8_t data[MAX_DATA];
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn, uint8_t reason)
 DECL|flush_long_vnd|function|static ssize_t flush_long_vnd(struct bt_conn *conn, const struct bt_gatt_attr *attr, uint8_t flags)
-DECL|generate_current_time|function|static void generate_current_time(uint8_t *buf)
 DECL|ind_params|variable|ind_params
 DECL|indicate_cb|function|static void indicate_cb(struct bt_conn *conn, const struct bt_gatt_attr *attr,int err)
 DECL|indicating|variable|indicating
 DECL|mainloop|function|void mainloop(void)
-DECL|read_ct|function|static ssize_t read_ct(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|read_long_vnd|function|static ssize_t read_long_vnd(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|read_signed|function|static ssize_t read_signed(struct bt_conn *conn, const struct bt_gatt_attr *attr, void *buf, uint16_t len, uint16_t offset)
 DECL|read_vnd|function|static ssize_t read_vnd(struct bt_conn *conn, const struct bt_gatt_attr *attr,void *buf, uint16_t len, uint16_t offset)
@@ -42,7 +35,6 @@ DECL|vnd_long_value|variable|vnd_long_value
 DECL|vnd_signed_uuid|variable|vnd_signed_uuid
 DECL|vnd_uuid|variable|vnd_uuid
 DECL|vnd_value|variable|vnd_value
-DECL|write_ct|function|static ssize_t write_ct(struct bt_conn *conn, const struct bt_gatt_attr *attr,const void *buf, uint16_t len, uint16_t offset)
 DECL|write_long_vnd|function|static ssize_t write_long_vnd(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *buf, uint16_t len, uint16_t offset)
 DECL|write_signed|function|static ssize_t write_signed(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *buf, uint16_t len, uint16_t offset)
 DECL|write_vnd|function|static ssize_t write_vnd(struct bt_conn *conn, const struct bt_gatt_attr *attr, const void *buf, uint16_t len, uint16_t offset)
