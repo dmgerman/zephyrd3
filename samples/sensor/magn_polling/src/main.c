363ccd91fdbd863e6711f10dbfcbd928ebb66ea5
Murtaza Alexandru <alexandru.murtaza@intel.com>
2016-03-17 16:18:05 +0200

    sensor: add magnetometer generic polling sample
    Add generic sample that continuosly read magnetometer data from
    the first device that finds available within a predefined list.
    
    Change-Id: Ia9b5fd434f462fb4704896852415f90eb633e1eb
    Signed-off-by: Murtaza Alexandru <alexandru.murtaza@intel.com>
    
    Former-commit-id: 9d4c6507e4b38b5aa26f4688219ebf36875560ee

---
+++ samples/sensor/magn_polling/src/main.c
@@ -0,0 +1,3 @@
+DECL|do_main|function|static void do_main(struct device *dev)
+DECL|main|function|void main(void)
+DECL|sensor_search_for_magnetometer|function|struct device *sensor_search_for_magnetometer()
