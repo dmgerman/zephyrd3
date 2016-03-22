493081d207a142072feff87dfa14f62cf7cf44cb
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-03-22 12:51:52 +0100

    include: misc: Add a generic single linked list API
    As for double linked list provided through misc/dlist.h, this provides a
    generic API for single linked list in misc/slist.h. It follows the same
    naming rule as in dlist.h
    
    Change-Id: I955bd16a201bc9987c29f5a9e3e3d8447682a71e
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 6c1f11ec97a9b93cee987d28038974d220e4a028

---
+++ include/misc/slist.h
@@ -0,0 +1,19 @@
+DECL|SYS_SLIST_FOR_EACH_NODE_SAFE|macro|SYS_SLIST_FOR_EACH_NODE_SAFE
+DECL|SYS_SLIST_FOR_EACH_NODE|macro|SYS_SLIST_FOR_EACH_NODE
+DECL|__SLIST_H__|macro|__SLIST_H__
+DECL|_slist|struct|struct _slist {
+DECL|_snode|struct|struct _snode {
+DECL|head|member|sys_snode_t *head;
+DECL|next|member|struct _snode *next;
+DECL|sys_slist_append|function|static inline void sys_slist_append(sys_slist_t *list, sys_snode_t *node)
+DECL|sys_slist_find_and_remove|function|static inline void sys_slist_find_and_remove(sys_slist_t *list, sys_snode_t *node)
+DECL|sys_slist_init|function|static inline void sys_slist_init(sys_slist_t *list)
+DECL|sys_slist_is_empty|function|static inline bool sys_slist_is_empty(sys_slist_t *list)
+DECL|sys_slist_peek_head|function|static inline sys_snode_t *sys_slist_peek_head(sys_slist_t *list)
+DECL|sys_slist_peek_next|function|static inline sys_snode_t *sys_slist_peek_next(sys_snode_t *node)
+DECL|sys_slist_peek_tail|function|static inline sys_snode_t *sys_slist_peek_tail(sys_slist_t *list)
+DECL|sys_slist_prepend|function|static inline void sys_slist_prepend(sys_slist_t *list, sys_snode_t *node)
+DECL|sys_slist_remove|function|static inline void sys_slist_remove(sys_slist_t *list, sys_snode_t *prev_node, sys_snode_t *node)
+DECL|sys_slist_t|typedef|typedef struct _slist sys_slist_t;
+DECL|sys_snode_t|typedef|typedef struct _snode sys_snode_t;
+DECL|tail|member|sys_snode_t *tail;
