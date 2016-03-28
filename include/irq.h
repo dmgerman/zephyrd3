a604a84b13c6b46cddfe2a79ebd13d0499748518
Andrew Boie <andrew.p.boie@intel.com>
2016-03-28 14:49:37 -0700

    nanokernel: deprecate dynamic IRQs
    We have not found any use-cases for dynamic IRQs where a static
    IRQ did not also suffice. Deprecate so that we can eventually
    remove from Zephyr and nontrivially decrease the complexity of
    the kernel.
    
    Change-Id: I509655371773aeaca7d01134dd850eb4cd95f387
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 15800ce78a5e881250ab1d9caa522d3f6d1f9eb5

--- include/irq.h
+++ include/irq.h
@@ -1,6 +1,6 @@
 DECL|IRQ_CONNECT|macro|IRQ_CONNECT
 DECL|_IRQ_H_|macro|_IRQ_H_
-DECL|irq_connect_dynamic|macro|irq_connect_dynamic
+DECL|irq_connect_dynamic|function|irq_connect_dynamic(unsigned int irq, unsigned int priority, void (*routine)(void *parameter), void *parameter, uint32_t flags)
 DECL|irq_disable|macro|irq_disable
 DECL|irq_enable|macro|irq_enable
 DECL|irq_lock|macro|irq_lock
