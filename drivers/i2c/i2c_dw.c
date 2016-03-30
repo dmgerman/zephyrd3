9ea00db2427cb824a21009ce616d5df543122524
Daniel Leung <daniel.leung@intel.com>
2016-03-30 16:00:22 -0700

    i2c/dw: remove kconfigs that are SoC specific
    Remove those kconfig options that are SoC specific, and should not be
    configurable via kconfig.
    
    Change-Id: Ia62888838877da4627419bd36c261d5254761acd
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: cb83db8c746757e8abe8f80383d55bd433992b48

--- drivers/i2c/i2c_dw.c
+++ drivers/i2c/i2c_dw.c
@@ -1,11 +1,6 @@
 DECL|DBG|macro|DBG
 DECL|DBG|macro|DBG
 DECL|DBG|macro|DBG
-DECL|I2C_DW_IRQ_FLAGS|macro|I2C_DW_IRQ_FLAGS
-DECL|I2C_DW_IRQ_FLAGS|macro|I2C_DW_IRQ_FLAGS
-DECL|I2C_DW_IRQ_FLAGS|macro|I2C_DW_IRQ_FLAGS
-DECL|I2C_DW_IRQ_FLAGS|macro|I2C_DW_IRQ_FLAGS
-DECL|I2C_DW_IRQ_FLAGS|macro|I2C_DW_IRQ_FLAGS
 DECL|_i2c_dw_data_ask|function|static inline void _i2c_dw_data_ask(struct device *dev)
 DECL|_i2c_dw_data_read|function|static void _i2c_dw_data_read(struct device *dev)
 DECL|_i2c_dw_data_send|function|static int _i2c_dw_data_send(struct device *dev)
