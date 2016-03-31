88e26cda223d7f1f0f0a45dfea0f6eaa3edd43b3
Laurentiu Palcu <laurentiu.palcu@intel.com>
2016-03-31 15:06:46 +0300

    bmi160: Add sample application
    Origin: Original
    Change-Id: I8bd3fa30f3794de0fa22ffff8e3eaeb213612741
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@intel.com>
    
    Former-commit-id: dbe2c5fafff9c37a1f2ffc13205c71abffbfce89

---
+++ samples/sensor/bmi160/src/arc_bmi160.c
@@ -0,0 +1,9 @@
+DECL|MAX_TEST_TIME|macro|MAX_TEST_TIME
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|acc_calib|variable|acc_calib
+DECL|accel_offsets|variable|accel_offsets
+DECL|auto_calibration|function|static int auto_calibration(struct device *bmi160)
+DECL|gyro_offsets|variable|gyro_offsets
+DECL|main|function|void main(void)
+DECL|manual_calibration|function|static int manual_calibration(struct device *bmi160)
+DECL|test_polling_mode|function|static void test_polling_mode(struct device *bmi160)
