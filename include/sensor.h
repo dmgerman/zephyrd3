72441fba5187cd10edb3b1839d57c9fe6bedd067
Laurentiu Palcu <laurentiu.palcu@intel.com>
2016-03-31 15:01:47 +0300

    sensor.h: Add helper functions for unit conversions
    Since sensors' API uses SI units, all attributes and channel reading use
    SI units. Hence, conversions from other units to SI are necessary.
    
    This patch adds helper for converting:
     * m/s^2 to Gs (and vice versa);
     * degrees to radians (and vice versa);
    
    Change-Id: I49f8763bed253ff6bde4c97618766b05f97da699
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@intel.com>
    
    Former-commit-id: d43e152845ccbfdea00e0aa5494fafe6d34df022

--- include/sensor.h
+++ include/sensor.h
@@ -25,6 +25,8 @@ DECL|SENSOR_CHAN_MAGN_Z|enumerator|SENSOR_CHAN_MAGN_Z,
 DECL|SENSOR_CHAN_PRESS|enumerator|SENSOR_CHAN_PRESS,
 DECL|SENSOR_CHAN_PROX|enumerator|SENSOR_CHAN_PROX,
 DECL|SENSOR_CHAN_TEMP|enumerator|SENSOR_CHAN_TEMP,
+DECL|SENSOR_G|macro|SENSOR_G
+DECL|SENSOR_PI|macro|SENSOR_PI
 DECL|SENSOR_TRIG_DATA_READY|enumerator|SENSOR_TRIG_DATA_READY,
 DECL|SENSOR_TRIG_DELTA|enumerator|SENSOR_TRIG_DELTA,
 DECL|SENSOR_TRIG_NEAR_FAR|enumerator|SENSOR_TRIG_NEAR_FAR,
@@ -48,7 +50,11 @@ DECL|sensor_attribute|enum|enum sensor_attribute {
 DECL|sensor_channel_get_t|typedef|typedef int (*sensor_channel_get_t)(struct device *dev,
 DECL|sensor_channel_get|function|static inline int sensor_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
 DECL|sensor_channel|enum|enum sensor_channel {
+DECL|sensor_degrees_to_rad|function|static inline void sensor_degrees_to_rad(int32_t d, struct sensor_value *rad)
 DECL|sensor_driver_api|struct|struct sensor_driver_api {
+DECL|sensor_g_to_ms2|function|static inline void sensor_g_to_ms2(int32_t g, struct sensor_value *ms2)
+DECL|sensor_ms2_to_g|function|static inline int32_t sensor_ms2_to_g(const struct sensor_value *ms2)
+DECL|sensor_rad_to_degrees|function|static inline int32_t sensor_rad_to_degrees(const struct sensor_value *rad)
 DECL|sensor_sample_fetch_t|typedef|typedef int (*sensor_sample_fetch_t)(struct device *dev);
 DECL|sensor_sample_fetch|function|static inline int sensor_sample_fetch(struct device *dev)
 DECL|sensor_trigger_handler_t|typedef|typedef void (*sensor_trigger_handler_t)(struct device *dev,
