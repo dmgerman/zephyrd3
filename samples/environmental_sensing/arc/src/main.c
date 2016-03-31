158cbba4c1ea14eca031796b1bf5abfc8be0f609
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-03-31 18:56:07 +0300

    samples: add environmental sensing sample for Arduino 101
    The ARC application collects temperature, humidity and pressure data from
    a set of sensors connected to the Arduino 101 and sends it to the x86 core
    through IPM. The collected data is also displayed on a Grove LCD.
    
    The x86 application exposes the received sensor data as a simple Bluetooth
    Environmental Sensing Service.
    
    This version of the sample does not offer notifications when the sensor data
    values change.
    
    Change-Id: Iee456d5d2455c7ccb7c5923ef3c94ecb20b5cecb
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: 9773fdaadef7f05accdd6859a7412e3b545fac2d

---
+++ samples/environmental_sensing/arc/src/main.c
@@ -0,0 +1,5 @@
+DECL|channel_info|struct|struct channel_info {
+DECL|chan|member|int chan;
+DECL|dev_name|member|char *dev_name;
+DECL|info|variable|info
+DECL|main|function|void main(void)
---
+++ samples/environmental_sensing/x86/src/main.c
@@ -0,0 +1,16 @@
+DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|HUMIDITY_CUD|macro|HUMIDITY_CUD
+DECL|PRESSURE_CUD|macro|PRESSURE_CUD
+DECL|TEMPERATURE_CUD|macro|TEMPERATURE_CUD
+DECL|ad|variable|ad
+DECL|attrs|variable|attrs
+DECL|bt_ready|function|static void bt_ready(int err)
+DECL|humidity_value|variable|humidity_value
+DECL|main|function|void main(void)
+DECL|pressure_value|variable|pressure_value
+DECL|read_u16|function|static ssize_t read_u16(struct bt_conn *conn, const struct bt_gatt_attr *attr,void *buf, uint16_t len, uint16_t offset)
+DECL|read_u32|function|static ssize_t read_u32(struct bt_conn *conn, const struct bt_gatt_attr *attr,void *buf, uint16_t len, uint16_t offset)
+DECL|sd|variable|sd
+DECL|sensor_ipm_callback|function|static void sensor_ipm_callback(void *context, uint32_t id, volatile void *data)
+DECL|temp_value|variable|temp_value
