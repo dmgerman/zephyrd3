db404ab41b2557c56243cd497604120fd294d529
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-03-31 13:43:13 +0200

    tests: Add a test suite for the single linked list generic
    It will extensively test all provided functions within a normal usage of
    the API.
    
    Change-Id: I723203a29c3f3416b464030a7fe34eac5fff6095
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 71e26d0d03a17084e175fab9fd53d98837886717

---
+++ tests/kernel/test_slist/src/slist.c
@@ -0,0 +1,8 @@
+DECL|main|function|void main(void)
+DECL|test_list|variable|test_list
+DECL|test_node_1|variable|test_node_1
+DECL|test_node_2|variable|test_node_2
+DECL|test_node_3|variable|test_node_3
+DECL|verify_content_amount|function|static inline bool verify_content_amount(sys_slist_t *list, int amount)
+DECL|verify_emptyness|function|static inline bool verify_emptyness(sys_slist_t *list)
+DECL|verify_tail_head|function|static inline bool verify_tail_head(sys_slist_t *list, sys_snode_t *head, sys_snode_t *tail, bool same)
