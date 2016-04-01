449496c893403be1e69083f928f1222d28f48ed0
Maciek Borzecki <maciek.borzecki@gmail.com>
2016-04-01 10:22:41 +0200

    console: shell: expect const commands array
    The commands array is not expected to change and can be declared
    const. This allows callers to cleanly declare their commands lists as
    const, effectively moving the structures from SRAM to code section.
    
    Change-Id: Ie1710622b8cfa609e129eb79712f910f1d1aace3
    Signed-off-by: Maciej Borzecki <maciek.borzecki@gmail.com>
    
    Former-commit-id: 5eb51feeb0392614cb7cb827d70ebb5fb636a3ed

--- drivers/console/console_handler_shell.c
+++ drivers/console/console_handler_shell.c
@@ -12,7 +12,7 @@ DECL|get_prompt|function|static const char *get_prompt(void)
 DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t size)
 DECL|line_queue_init|function|static void line_queue_init(void)
 DECL|prompt|variable|prompt
-DECL|shell_init|function|void shell_init(const char *str, struct shell_cmd *cmds)
+DECL|shell_init|function|void shell_init(const char *str, const struct shell_cmd *cmds)
 DECL|shell_register_app_cmd_handler|function|void shell_register_app_cmd_handler(shell_cmd_function_t handler)
 DECL|shell_register_prompt_handler|function|void shell_register_prompt_handler(shell_prompt_function_t handler)
 DECL|shell|function|static void shell(int arg1, int arg2)
