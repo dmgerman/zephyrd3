5644dbb3e0ce3d9531123bf656b17d104ed5df0d
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-04-05 14:51:34 +0300

    drivers/nble: Refactor PM code to make it reusable
    Refactoring Power Management related code to special file nrf51_pm
    making it possible to reuse the functions for H:4 UART driver when
    Nordic BLE is flashed with HCI firmware.
    
    Change-Id: If389c1f4af13fa786e5866129624527cec0928e0
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 3252f6cf1a25b991c6cb67343542ce2289ae4a3a

--- drivers/bluetooth/nble/uart.c
+++ drivers/bluetooth/nble/uart.c
@@ -1,10 +1,7 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
-DECL|NBLE_BTWAKE_PIN|macro|NBLE_BTWAKE_PIN
 DECL|NBLE_BUF_SIZE|macro|NBLE_BUF_SIZE
-DECL|NBLE_RESET_PIN|macro|NBLE_RESET_PIN
 DECL|NBLE_RX_BUF_COUNT|macro|NBLE_RX_BUF_COUNT
-DECL|NBLE_SWDIO_PIN|macro|NBLE_SWDIO_PIN
 DECL|NBLE_TX_BUF_COUNT|macro|NBLE_TX_BUF_COUNT
 DECL|NET_BUF_POOL|variable|NET_BUF_POOL
 DECL|NET_BUF_POOL|variable|NET_BUF_POOL
@@ -15,10 +12,10 @@ DECL|channel|member|uint8_t channel; /**< Channel number of IPC message. */
 DECL|ipc_uart_header|struct|struct ipc_uart_header {
 DECL|len|member|uint16_t len; /**< Length of IPC message. */
 DECL|nble_dev|variable|nble_dev
-DECL|nble_disable|function|static int nble_disable(void)
 DECL|nble_discard|function|static size_t nble_discard(struct device *uart, size_t len)
-DECL|nble_enable|function|int nble_enable(void)
 DECL|nble_open|function|int nble_open(void)
+DECL|post_init|function|static int post_init(void)
+DECL|pre_init|function|static int pre_init(void)
 DECL|rpc_alloc_cb|function|struct net_buf *rpc_alloc_cb(uint16_t length)
 DECL|rpc_transmit_cb|function|void rpc_transmit_cb(struct net_buf *buf)
 DECL|rx_fiber|function|static void rx_fiber(void)
---
+++ drivers/bluetooth/nrf51_pm.c
@@ -0,0 +1,5 @@
+DECL|NBLE_BTWAKE_PIN|macro|NBLE_BTWAKE_PIN
+DECL|NBLE_RESET_PIN|macro|NBLE_RESET_PIN
+DECL|NBLE_SWDIO_PIN|macro|NBLE_SWDIO_PIN
+DECL|nrf51_disable|function|int nrf51_disable(void)
+DECL|nrf51_enable|function|int nrf51_enable(void)
