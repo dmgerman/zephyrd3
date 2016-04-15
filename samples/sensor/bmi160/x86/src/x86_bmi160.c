62eefbcb5ffe761378d902bfcbb09f7037b58772
Laurentiu Palcu <laurentiu.palcu@intel.com>
2016-04-15 18:08:35 +0300

    sensor: bmi160: add x86 app for Arduino101
    This app is needed for Arduino101 only, or boards using the Curie
    module.
    
    BMI160 interrupt pin is connected to AON GPIO controller. The driver
    handling the AON GPIO controller runs on x86 core. Even though, the AON
    GPIO controller is accesible to ARC core too, the current implementation
    for Designware controller driver does not allow it. Hence, this app will
    register a gpio callback and signal the ARC core through IPM.
    
    Other boards that use a gpio pin on the regular GPIO controllers don't
    need this app.
    
    Origin: Original
    Change-Id: I225ca60735e39c7a47693dc53237f48cd9ac2680
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@intel.com>
    
    Former-commit-id: b70278f113578935ce8f9b30c44f16942683d5f8

---
+++ samples/sensor/bmi160/x86/src/x86_bmi160.c
@@ -0,0 +1,5 @@
+DECL|BMI160_INTERRUPT_PIN|macro|BMI160_INTERRUPT_PIN
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|aon_gpio_callback|function|static void aon_gpio_callback(struct device *port, uint32_t pin)
+DECL|ipm|variable|ipm
+DECL|main|function|void main(void)
