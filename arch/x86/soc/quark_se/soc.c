5a2a2b394da65ea4e900d82f33e90ddeebfe2e81
Anas Nashif <anas.nashif@intel.com>
2016-04-17 09:02:30 +0800

    qmsi: uart: use built-in qmsi driver
    Change-Id: Ie19aee8dad8ba082891f92a2d42dc71f4ac34dbe
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 1797c8f1081a4d12cb434f40203791ff0cd2ab42

--- arch/x86/soc/quark_se/soc.c
+++ arch/x86/soc/quark_se/soc.c
@@ -2,4 +2,3 @@ DECL|SCSS_REG_VAL|macro|SCSS_REG_VAL
 DECL|arc_init_debug|macro|arc_init_debug
 DECL|arc_init_debug|macro|arc_init_debug
 DECL|arc_init|function|static int arc_init(struct device *arg)
-DECL|platform_uart_init|function|static int platform_uart_init(struct device *arg)
