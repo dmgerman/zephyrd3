420e0333e25f2ae457fc3fde3a4049dcf12c6872
Peter Mitsis <peter.mitsis@windriver.com>
2016-04-18 15:49:49 -0400

    device: simplify synchronization
    Takes advantage of the fact that microkernel tasks can now wait on
    nanokernel objects to simplify the device synchronization code.
    
    Change-Id: I5b8d21eaccde9db8b63dd906ef982494a6170271
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 542c37fe9c1e0e6a349260c7fccc909680e5846d

--- include/device.h
+++ include/device.h
@@ -7,27 +7,18 @@ DECL|DEVICE_INIT_PM|macro|DEVICE_INIT_PM
 DECL|DEVICE_INIT|macro|DEVICE_INIT
 DECL|DEVICE_INIT|macro|DEVICE_INIT
 DECL|DEVICE_NAME_GET|macro|DEVICE_NAME_GET
-DECL|DEVICE_SYNC_WAITER_FIBER|enumerator|DEVICE_SYNC_WAITER_FIBER,
-DECL|DEVICE_SYNC_WAITER_NONE|enumerator|DEVICE_SYNC_WAITER_NONE,
-DECL|DEVICE_SYNC_WAITER_TASK|enumerator|DEVICE_SYNC_WAITER_TASK,
 DECL|_DEVICE_H_|macro|_DEVICE_H_
-DECL|_is_blocking_task|function|static inline bool _is_blocking_task(void)
-DECL|_t_sem|member|struct _k_sem_struct _t_sem;
 DECL|config_info|member|void *config_info;
 DECL|config|member|struct device_config *config;
 DECL|dev_pm_ops|member|struct device_pm_ops *dev_pm_ops;
 DECL|device_config|struct|struct device_config {
 DECL|device_pm_ops|struct|struct device_pm_ops {
-DECL|device_ready|member|bool device_ready;
 DECL|device_resume|function|static inline int device_resume(struct device *device, int pm_policy)
 DECL|device_suspend|function|static inline int device_suspend(struct device *device, int pm_policy)
-DECL|device_sync_call_complete|function|static inline void device_sync_call_complete(device_sync_call_t *sync)
 DECL|device_sync_call_complete|function|static inline void device_sync_call_complete(device_sync_call_t *sync)
 DECL|device_sync_call_init|function|static inline void device_sync_call_init(device_sync_call_t *sync)
 DECL|device_sync_call_t|typedef|} device_sync_call_t;
 DECL|device_sync_call_wait|function|static inline void device_sync_call_wait(device_sync_call_t *sync)
-DECL|device_sync_call_wait|function|static inline void device_sync_call_wait(device_sync_call_t *sync)
-DECL|device_sync_waiter|enum|enum device_sync_waiter {
 DECL|device|struct|struct device {
 DECL|driver_api|member|void *driver_api;
 DECL|driver_data|member|void *driver_data;
@@ -36,5 +27,3 @@ DECL|init|member|int (*init)(struct device *device);
 DECL|name|member|char *name;
 DECL|resume|member|int (*resume)(struct device *device, int pm_policy);
 DECL|suspend|member|int (*suspend)(struct device *device, int pm_policy);
-DECL|t_sem|member|ksem_t t_sem;
-DECL|waiter|member|enum device_sync_waiter waiter;
