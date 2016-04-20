c2120c447ad7d1ca6dcbc25cddd6b4902927716f
Johan Hedberg <johan.hedberg@intel.com>
2016-04-20 17:54:07 +0300

    Bluetooth: Add custom "monitor" protocol logging over UART
    With most boards there's no simple way to get access to the HCI
    traffic. Simultaneously these boards only have one external UART for
    the console. This patch introduces a protocol which combines both
    normal logs and HCI logs over a single binary protocol sent over the
    console UART.
    
    The protocol is modeled based on the btsnoop/monitor protocols used by
    BlueZ, and the first tool that's able to decode this is btmon from
    BlueZ ("btmon --tty <tty>").
    
    For platforms with two or more external UARTs it is still possible to
    use CONFIG_UART_CONSOLE as long as the UART devices used are
    different, however on platforms with a single external UART
    UART_CONSOLE should be disabled if BLUETOOTH_DEBUG_MONITOR is enabled
    (in this case printk/printf get encoded to the monitor protocol).
    
    Origin: Original
    Change-Id: I9d3997c7a06fe48e7decb212b2ac9bd8b8f9b74c
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 264c207f4fafafa236b445f4cfb38f887b339011

---
+++ net/bluetooth/monitor.c
@@ -0,0 +1,9 @@
+DECL|bt_log|function|void bt_log(int prio, const char *fmt, ...)
+DECL|bt_monitor_init|function|static int bt_monitor_init(struct device *d)
+DECL|bt_monitor_new_index|function|void bt_monitor_new_index(uint8_t type, uint8_t bus, bt_addr_t *addr, const char *name)
+DECL|bt_monitor_send|function|void bt_monitor_send(uint16_t opcode, const void *data, size_t len)
+DECL|log_out|function|static int log_out(int c, void *unused)
+DECL|monitor_console_out|function|static int monitor_console_out(int c)
+DECL|monitor_dev|variable|monitor_dev
+DECL|monitor_send|function|static void monitor_send(const void *data, size_t len)
+DECL|send_hdr|function|static void send_hdr(uint16_t opcode, uint16_t index, uint16_t len)
---
+++ net/bluetooth/monitor.h
@@ -0,0 +1,42 @@
+DECL|BT_MONITOR_ACL_RX_PKT|macro|BT_MONITOR_ACL_RX_PKT
+DECL|BT_MONITOR_ACL_TX_PKT|macro|BT_MONITOR_ACL_TX_PKT
+DECL|BT_MONITOR_BUS_PCCARD|macro|BT_MONITOR_BUS_PCCARD
+DECL|BT_MONITOR_BUS_PCI|macro|BT_MONITOR_BUS_PCI
+DECL|BT_MONITOR_BUS_RS232|macro|BT_MONITOR_BUS_RS232
+DECL|BT_MONITOR_BUS_SDIO|macro|BT_MONITOR_BUS_SDIO
+DECL|BT_MONITOR_BUS_UART|macro|BT_MONITOR_BUS_UART
+DECL|BT_MONITOR_BUS_USB|macro|BT_MONITOR_BUS_USB
+DECL|BT_MONITOR_BUS_VIRTUAL|macro|BT_MONITOR_BUS_VIRTUAL
+DECL|BT_MONITOR_CLOSE_INDEX|macro|BT_MONITOR_CLOSE_INDEX
+DECL|BT_MONITOR_COMMAND_PKT|macro|BT_MONITOR_COMMAND_PKT
+DECL|BT_MONITOR_DEL_INDEX|macro|BT_MONITOR_DEL_INDEX
+DECL|BT_MONITOR_EVENT_PKT|macro|BT_MONITOR_EVENT_PKT
+DECL|BT_MONITOR_INDEX_INFO|macro|BT_MONITOR_INDEX_INFO
+DECL|BT_MONITOR_NEW_INDEX|macro|BT_MONITOR_NEW_INDEX
+DECL|BT_MONITOR_NOP|macro|BT_MONITOR_NOP
+DECL|BT_MONITOR_OPEN_INDEX|macro|BT_MONITOR_OPEN_INDEX
+DECL|BT_MONITOR_SCO_RX_PKT|macro|BT_MONITOR_SCO_RX_PKT
+DECL|BT_MONITOR_SCO_TX_PKT|macro|BT_MONITOR_SCO_TX_PKT
+DECL|BT_MONITOR_SYSTEM_NOTE|macro|BT_MONITOR_SYSTEM_NOTE
+DECL|BT_MONITOR_TYPE_AMP|macro|BT_MONITOR_TYPE_AMP
+DECL|BT_MONITOR_TYPE_PRIMARY|macro|BT_MONITOR_TYPE_PRIMARY
+DECL|BT_MONITOR_USER_LOGGING|macro|BT_MONITOR_USER_LOGGING
+DECL|BT_MONITOR_VENDOR_DIAG|macro|BT_MONITOR_VENDOR_DIAG
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|bdaddr|member|uint8_t bdaddr[6];
+DECL|bt_monitor_hdr|struct|struct bt_monitor_hdr {
+DECL|bt_monitor_new_index|macro|bt_monitor_new_index
+DECL|bt_monitor_new_index|struct|struct bt_monitor_new_index {
+DECL|bt_monitor_opcode|function|static inline uint8_t bt_monitor_opcode(struct net_buf *buf)
+DECL|bt_monitor_send|macro|bt_monitor_send
+DECL|bt_monitor_user_logging|struct|struct bt_monitor_user_logging {
+DECL|bus|member|uint8_t bus;
+DECL|ident_len|member|uint8_t ident_len;
+DECL|index|member|uint16_t index;
+DECL|len|member|uint16_t len;
+DECL|name|member|char name[8];
+DECL|opcode|member|uint16_t opcode;
+DECL|priority|member|uint8_t priority;
+DECL|type|member|uint8_t type;
