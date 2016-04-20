a2157ed200a431073b8b958abb309c6673dde9a2
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-04-20 12:36:49 +0300

    net: apps: Fix IPv4 support in echo-server
    The IPv4 address is set properly for testing purposes.
    
    Change-Id: I996e7435276f65dbb642aa9ab99c37cd842d4b39
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7e91a42cede766253dd73b9766da9754d82d3ef6

--- samples/net/echo_server/src/echo-server.c
+++ samples/net/echo_server/src/echo-server.c
@@ -1,11 +1,20 @@
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
 DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|UIP_DRIPADDR0|macro|UIP_DRIPADDR0
+DECL|UIP_DRIPADDR1|macro|UIP_DRIPADDR1
+DECL|UIP_DRIPADDR2|macro|UIP_DRIPADDR2
+DECL|UIP_DRIPADDR3|macro|UIP_DRIPADDR3
+DECL|UIP_IPADDR0|macro|UIP_IPADDR0
+DECL|UIP_IPADDR1|macro|UIP_IPADDR1
+DECL|UIP_IPADDR2|macro|UIP_IPADDR2
+DECL|UIP_IPADDR3|macro|UIP_IPADDR3
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TIME|macro|WAIT_TIME
@@ -19,3 +28,6 @@ DECL|prepare_reply|function|static inline struct net_buf *prepare_reply(const ch
 DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *udp_recv, struct net_context *tcp_recv, struct net_context *mcast_recv)
 DECL|receive|function|void receive(void)
 DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
+DECL|uip_draddr|variable|uip_draddr
+DECL|uip_hostaddr|variable|uip_hostaddr
+DECL|uip_netmask|variable|uip_netmask
