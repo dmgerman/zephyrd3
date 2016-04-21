6f37bec5fdb41b2f4f96ef40291f4c021da45019
Murtaza Alexandru <alexandru.murtaza@intel.com>
2016-04-21 15:05:22 +0300

    sensor: split lsm9ds0_gyro driver
    Split driver for LSM9DS0 gyroscope in core functionalities and trigger.
    
    Change-Id: I19736b65f45a6c4fc79a1f16423097b89accff24
    Signed-off-by: Murtaza Alexandru <alexandru.murtaza@intel.com>
    
    Former-commit-id: 52ccdada9c1bc0a5b1c1e0c29efbd1e631adaf8f

---
+++ drivers/sensor/sensor_lsm9ds0_gyro_trigger.c
@@ -0,0 +1,4 @@
+DECL|lsm9ds0_gyro_fiber_main|function|static void lsm9ds0_gyro_fiber_main(int arg1, int gpio_pin)
+DECL|lsm9ds0_gyro_gpio_drdy_callback|function|static void lsm9ds0_gyro_gpio_drdy_callback(struct device *dev, struct gpio_callback *cb, uint32_t pins)
+DECL|lsm9ds0_gyro_init_interrupt|function|int lsm9ds0_gyro_init_interrupt(struct device *dev)
+DECL|lsm9ds0_gyro_trigger_set|function|int lsm9ds0_gyro_trigger_set(struct device *dev, const struct sensor_trigger *trig, sensor_trigger_handler_t handler)
