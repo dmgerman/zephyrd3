66812c82828fcb3eef1ed05c00035a492acacb6e
Johan Hedberg <johan.hedberg@intel.com>
2016-04-22 15:13:09 +0300

    Bluetooth: Add name member to HCI driver struct
    This is more of a convenience to let the driver identify itself in
    debug logs and the monitor protocol.
    
    Change-Id: I73351477e98d45d6344c180b8088bde29df6f7d9
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: e3e91d328016553fb26dbd35c4152b10e6ca4691

--- include/bluetooth/driver.h
+++ include/bluetooth/driver.h
@@ -9,5 +9,6 @@ DECL|__BT_DRIVER_H|macro|__BT_DRIVER_H
 DECL|bt_driver_bus|enum|enum bt_driver_bus {
 DECL|bt_driver|struct|struct bt_driver {
 DECL|bus|member|enum bt_driver_bus bus;
+DECL|name|member|const char *name;
 DECL|open|member|int (*open)(void);
 DECL|send|member|int (*send)(struct net_buf *buf);
