5895eedf9e71dfea27dd25bf362a771e9e192a12
Johan Hedberg <johan.hedberg@intel.com>
2016-04-22 14:42:59 +0300

    Bluetooth: Move HCI device bus information to the driver
    The bus that we notify over the monitor protocol is really driver
    specific, so let each driver specify their bus type.
    
    Change-Id: Ic3a086fcc06352dbf051e52cef5bf6b8696349ae
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 4488fc6cabb989ec081198389706657b6b15fe5c

--- include/bluetooth/driver.h
+++ include/bluetooth/driver.h
@@ -1,4 +1,13 @@
+DECL|BT_DRIVER_BUS_PCCARD|enumerator|BT_DRIVER_BUS_PCCARD = 2,
+DECL|BT_DRIVER_BUS_PCI|enumerator|BT_DRIVER_BUS_PCI = 5,
+DECL|BT_DRIVER_BUS_RS232|enumerator|BT_DRIVER_BUS_RS232 = 4,
+DECL|BT_DRIVER_BUS_SDIO|enumerator|BT_DRIVER_BUS_SDIO = 6,
+DECL|BT_DRIVER_BUS_UART|enumerator|BT_DRIVER_BUS_UART = 3,
+DECL|BT_DRIVER_BUS_USB|enumerator|BT_DRIVER_BUS_USB = 1,
+DECL|BT_DRIVER_BUS_VIRTUAL|enumerator|BT_DRIVER_BUS_VIRTUAL = 0,
 DECL|__BT_DRIVER_H|macro|__BT_DRIVER_H
+DECL|bt_driver_bus|enum|enum bt_driver_bus {
 DECL|bt_driver|struct|struct bt_driver {
+DECL|bus|member|enum bt_driver_bus bus;
 DECL|open|member|int (*open)(void);
 DECL|send|member|int (*send)(struct net_buf *buf);
