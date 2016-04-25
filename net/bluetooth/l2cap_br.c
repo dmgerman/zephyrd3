1745b40b661657fabce011d284c156d53dcf1c61
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-04-25 15:14:24 +0200

    Bluetooth: L2CAP: Add API to register PSM server
    Enables API to register PSM server on BREDR transport.
    Adds l2cap_br.c file responsible for BREDR specific functionality to
    build path based on Kconfig selection.
    
    Change-Id: I92823b0207ab0da96bfb813353de9f95fa5dd0f4
    Origin: Original
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 8e34b5869b81a04621e4be007ccc66cf85f79d0f

---
+++ net/bluetooth/l2cap_br.c
@@ -0,0 +1,7 @@
+DECL|BT_DBG|macro|BT_DBG
+DECL|BT_DBG|macro|BT_DBG
+DECL|L2CAP_BR_PSM_END|macro|L2CAP_BR_PSM_END
+DECL|L2CAP_BR_PSM_START|macro|L2CAP_BR_PSM_START
+DECL|br_servers|variable|br_servers
+DECL|bt_l2cap_br_server_register|function|int bt_l2cap_br_server_register(struct bt_l2cap_server *server)
+DECL|l2cap_br_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_br_server_lookup_psm(uint16_t psm)
