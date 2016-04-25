aae6198f018938373aa3cd998de1e305e9f695e1
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-04-25 16:39:04 +0300

    net: buf: Add fragmentation API tests
    Add tests for checking that the net_buf fragmentation works ok.
    
    Change-Id: I778f79bae971314b6f19c1bf00ed42e1c2128e99
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7d6197cf941e7c3950d54d280353c771424d6eb9

--- tests/net/buf/src/main.c
+++ tests/net/buf/src/main.c
@@ -6,6 +6,9 @@ DECL|bt_data|struct|struct bt_data {
 DECL|buf_destroy|function|static void buf_destroy(struct net_buf *buf)
 DECL|bufs_fifo|variable|bufs_fifo
 DECL|destroy_called|variable|destroy_called
+DECL|frag_destroy_called|variable|frag_destroy_called
+DECL|frag_destroy|function|static void frag_destroy(struct net_buf *buf)
+DECL|frags_fifo|variable|frags_fifo
 DECL|func|member|bool (*func)(void);
 DECL|hci_opcode|member|uint16_t hci_opcode;
 DECL|hci_sync|member|void *hci_sync;
@@ -14,6 +17,8 @@ DECL|name|member|const char *name;
 DECL|net_buf_test_1|function|static bool net_buf_test_1(void)
 DECL|net_buf_test_2|function|static bool net_buf_test_2(void)
 DECL|net_buf_test_3|function|static bool net_buf_test_3(void)
+DECL|net_buf_test_4|function|static bool net_buf_test_4(void)
+DECL|no_data_buf_fifo|variable|no_data_buf_fifo
 DECL|test_3_fiber|function|static void test_3_fiber(int arg1, int arg2)
 DECL|tests|variable|tests
 DECL|type|member|uint8_t type;
