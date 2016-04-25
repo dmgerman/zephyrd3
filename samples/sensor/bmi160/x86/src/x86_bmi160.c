5b627750f2aaab02b13d74fff262b0af1e2ed51b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-04-25 10:26:05 +0200

    samples: sensor: Use new GPIO callback API for bmi160 sample
    gpio_set_callback is meant to be deprecated and thus modifying the
    sample to use the new GPIO callback API.
    
    Change-Id: I822cd1c81575834d40926c56cd61b87884230594
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: cc53ac79187bcee454fb0e9673395b7df0a86b68

--- samples/sensor/bmi160/x86/src/x86_bmi160.c
+++ samples/sensor/bmi160/x86/src/x86_bmi160.c
@@ -1,5 +1,6 @@
 DECL|BMI160_INTERRUPT_PIN|macro|BMI160_INTERRUPT_PIN
 DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|aon_gpio_callback|function|static void aon_gpio_callback(struct device *port, uint32_t pin)
+DECL|aon_gpio_callback|function|static void aon_gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins)
+DECL|cb|variable|cb
 DECL|ipm|variable|ipm
 DECL|main|function|void main(void)
