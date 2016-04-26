49b6f688341c774be3ce569afee60f71502e5b69
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-04-26 15:09:30 +0300

    tests: test_slist: Add test for sys_slist_insert
    Change-Id: I7b478c78050027013f386146bae6198a554f2005
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: e2c1b2b7be2c6de08cae8c88ed6dff58e8e7b51d

--- tests/kernel/test_slist/src/slist.c
+++ tests/kernel/test_slist/src/slist.c
@@ -3,6 +3,7 @@ DECL|test_list|variable|test_list
 DECL|test_node_1|variable|test_node_1
 DECL|test_node_2|variable|test_node_2
 DECL|test_node_3|variable|test_node_3
+DECL|test_node_4|variable|test_node_4
 DECL|verify_content_amount|function|static inline bool verify_content_amount(sys_slist_t *list, int amount)
 DECL|verify_emptyness|function|static inline bool verify_emptyness(sys_slist_t *list)
 DECL|verify_tail_head|function|static inline bool verify_tail_head(sys_slist_t *list, sys_snode_t *head, sys_snode_t *tail, bool same)
