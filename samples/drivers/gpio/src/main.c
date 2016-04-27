34325d4dedd67b73cec3a7061a0e1cd465d44afa
Anas Nashif <anas.nashif@intel.com>
2016-04-26 21:43:55 -0400

    samples: gpio: sanitize test and remove obsolete options
    - CONFIG_GPIO_DW_0_BITS is not longer available
    - Use default printk
    - Do not enable debug
    - remove useless whitelist in testcase.ini, we already whitelist
      boards
    
    Change-Id: If21795d544e268c26a757b697ce3c9f2e56db65f
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 19542114def07c52a47b805d9d8a21ab4d83fa25

--- samples/drivers/gpio/src/main.c
+++ samples/drivers/gpio/src/main.c
@@ -13,8 +13,6 @@ DECL|GPIO_OUT_PIN|macro|GPIO_OUT_PIN
 DECL|GPIO_OUT_PIN|macro|GPIO_OUT_PIN
 DECL|GPIO_OUT_PIN|macro|GPIO_OUT_PIN
 DECL|GPIO_OUT_PIN|macro|GPIO_OUT_PIN
-DECL|PRINT|macro|PRINT
-DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|gpio_callback|function|void gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins)
 DECL|gpio_cb|variable|gpio_cb
