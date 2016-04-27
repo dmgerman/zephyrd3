d6e3521c40dfd42f449b89cc1091576226366379
Laurentiu Palcu <laurentiu.palcu@intel.com>
2016-04-27 17:35:50 +0300

    sensor: bmg160: add sample application
    The application tests polling mode and trigger mode (both data ready and
    anymotion triggers).
    
    Origin: Original
    Change-Id: Ic0f9400758aaf56ef95f0f3c68c872d1bfd51176
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@intel.com>
    
    Former-commit-id: 5841aeae14fdb0680157cffd9cca7381ce46bb66

---
+++ samples/sensor/bmg160/src/arc_bmg160.c
@@ -0,0 +1,8 @@
+DECL|MAX_TEST_TIME|macro|MAX_TEST_TIME
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|main|function|void main(void)
+DECL|print_gyro_data|function|static void print_gyro_data(struct device *bmg160)
+DECL|print_temp_data|function|static void print_temp_data(struct device *bmg160)
+DECL|test_polling_mode|function|static void test_polling_mode(struct device *bmg160)
+DECL|test_trigger_mode|function|static void test_trigger_mode(struct device *bmg160)
+DECL|trigger_handler|function|static void trigger_handler(struct device *bmg160, struct sensor_trigger *trigger)
