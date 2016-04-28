23134e97b57882d7a77fe6c8451456c11dc12967
Andrew Boie <andrew.p.boie@intel.com>
2016-04-28 14:30:07 -0700

    nios2: add prep_c to zero BSS and copy XIP data
    Copied verbatim from arch/arc/core
    
    Change-Id: I00c920064f75455010806951ad052289d0290654
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: f6cf65d57c1aa52bddb777654877210c5b2b4c27

---
+++ arch/nios2/core/prep_c.c
@@ -0,0 +1,4 @@
+DECL|_PrepC|function|void _PrepC(void)
+DECL|bssZero|function|static void bssZero(void)
+DECL|dataCopy|function|static void dataCopy(void)
+DECL|dataCopy|function|static void dataCopy(void)
