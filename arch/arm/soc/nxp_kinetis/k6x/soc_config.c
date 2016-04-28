56dbb4cdc9240d40d25ae869d1cb300cde5a0c61
Daniel Leung <daniel.leung@intel.com>
2016-04-28 15:14:38 -0700

    arm/nxp_kinetis/k6x: simplify uart init
    The UART console init should be done after UART init. So it can be
    folded into general UART init, thus one fewer SYS_INIT().
    
    Change-Id: Ifb18c5308dfef790325e6bff5bc7b5976fcd421e
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 8f0ab3038cb48a1175ba0c6b630fe30ada1c7d9c

--- arch/arm/soc/nxp_kinetis/k6x/soc_config.c
+++ arch/arm/soc/nxp_kinetis/k6x/soc_config.c
@@ -1,2 +1,3 @@
-DECL|uart_k20_console_init|function|static int uart_k20_console_init(struct device *dev)
+DECL|uart_k20_console_init|function|static ALWAYS_INLINE int uart_k20_console_init(void)
+DECL|uart_k20_console_init|macro|uart_k20_console_init
 DECL|uart_k20_init|function|static int uart_k20_init(struct device *dev)
