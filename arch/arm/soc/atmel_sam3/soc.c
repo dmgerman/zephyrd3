1ae65c6e5ddd60443de54b7f3719445a7b0eae12
Daniel Leung <daniel.leung@intel.com>
2016-04-28 14:59:05 -0700

    arm/atmel_sam3: always inline clock init function
    The clock_init() is always called so this can be inlined.
    This saves 32 bytes of ROM space compared to non-inlined version.
    
    Change-Id: I1b95142da4c22f4b7fc7f7f6210503d90cf65cc9
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: b6f56d478a1b18f8d0acb044d07144754a40d343

--- arch/arm/soc/atmel_sam3/soc.c
+++ arch/arm/soc/atmel_sam3/soc.c
@@ -1,2 +1,2 @@
 DECL|atmel_sam3_init|function|static int atmel_sam3_init(struct device *arg)
-DECL|clock_init|function|static void clock_init(void)
+DECL|clock_init|function|static ALWAYS_INLINE void clock_init(void)
