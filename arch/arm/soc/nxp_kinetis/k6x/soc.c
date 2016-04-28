e978560a7a11cbcc68c3170d9f51acce3ceef7c2
Daniel Leung <daniel.leung@intel.com>
2016-04-28 15:08:11 -0700

    arm/nxp_kinetis/k6x: always inline clock init function
    The clkInit() is always called so this can be inlined.
    This saves 32 bytes of ROM space compared to non-inlined version.
    
    Change-Id: I2cb6d02c0996acad16029be243f6059183938089
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: ad6ed299b73125a42b22225fc73186fc40b75448

--- arch/arm/soc/nxp_kinetis/k6x/soc.c
+++ arch/arm/soc/nxp_kinetis/k6x/soc.c
@@ -1,5 +1,5 @@
 DECL|FRDM_K64F_PLL_DIV_20|macro|FRDM_K64F_PLL_DIV_20
 DECL|FRDM_K64F_PLL_MULT_48|macro|FRDM_K64F_PLL_MULT_48
 DECL|__security_frdm_k64f|variable|__security_frdm_k64f
-DECL|clkInit|function|static void clkInit(void)
+DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
 DECL|fsl_frdm_k64f_init|function|static int fsl_frdm_k64f_init(struct device *arg)
