f624dd07d7546efd0f491fab222c5ae0185c4424
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-04-29 15:52:09 +0300

    net: Initial commit for the new IP stack
    This is a placeholder that compiles but does nothing else.
    
    Change-Id: I9689fa26eb13bc23d29940938f7b3c11f32b2ff1
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: a3b88f53c4896e0167e3bcc3f3830e9838f1c08b

---
+++ net/yaip/net_core.c
@@ -0,0 +1,8 @@
+DECL|CONFIG_NET_RX_STACK_SIZE|macro|CONFIG_NET_RX_STACK_SIZE
+DECL|DEBUG|macro|DEBUG
+DECL|init_rx_queue|function|static void init_rx_queue(void)
+DECL|net_init|function|int net_init(void)
+DECL|net_rx_fiber|function|static void net_rx_fiber(int unused1, int unused2)
+DECL|network_initialization|function|static int network_initialization(void)
+DECL|rx_fiber_stack|variable|rx_fiber_stack
+DECL|rx_queue|variable|rx_queue
