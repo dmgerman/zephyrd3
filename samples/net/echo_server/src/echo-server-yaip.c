2e1a0a3c89c1621f8f41beb5fb875d89582dea86
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-02 10:52:55 +0300

    net: apps: Create a skeleton echo-server for new IP stack
    This echo-server does not yet do anything useful.
    
    Change-Id: I317051b3a2bfb0a80584ad385c2f308978a243cd
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: e6d92ecdcfeede3994733867439037b1b06d63f2

---
+++ samples/net/echo_server/src/echo-server-yaip.c
@@ -0,0 +1,18 @@
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MCAST_IPADDR|macro|MCAST_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_PORT|macro|MY_PORT
+DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|fiberStack|variable|fiberStack
+DECL|get_context|function|static inline bool get_context(struct net_context **udp_recv4, struct net_context **udp_recv6, struct net_context **tcp_recv4, struct net_context **tcp_recv6, struct net_context **mcast_recv4,
+DECL|in6addr_my|variable|in6addr_my
+DECL|in6addr_my|variable|in6addr_my
+DECL|init_app|function|static inline void init_app(void)
+DECL|main|function|void main(void)
+DECL|receive|function|void receive(void)
