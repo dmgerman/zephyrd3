8336f74bea8943718c17c1aaa27ee6fa4d3d36e6
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-05-02 09:02:04 +0200

    net: Add generic network interface header
    This will be used by the new network stack to relate a device to actual
    network context, and used in the different layers (mac, ip ...).
    
    Change-Id: I30c08fa975314544c36b71636fd9653d562891b3
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 2fa38c18f0ef889c8c5bdd0ac0ed1e0606785a98

--- include/net/net_if.h
+++ include/net/net_if.h
@@ -1,1 +1,14 @@
-
+DECL|NET_DEVICE_INIT|macro|NET_DEVICE_INIT
+DECL|NET_IF_INIT|macro|NET_IF_INIT
+DECL|NET_IF_INIT|macro|NET_IF_INIT
+DECL|__NET_IF_H__|macro|__NET_IF_H__
+DECL|dev|member|struct device *dev;
+DECL|init|member|void (*init)(struct net_if *iface);
+DECL|link_addr|member|struct net_linkaddr link_addr;
+DECL|mtu|member|uint16_t mtu;
+DECL|net_if_api|struct|struct net_if_api {
+DECL|net_if_get_device|function|static inline struct device *net_if_get_device(struct net_if *iface)
+DECL|net_if_get_link_addr|function|static inline struct net_linkaddr *net_if_get_link_addr(struct net_if *iface)
+DECL|net_if_get_mtu|function|static inline uint16_t net_if_get_mtu(struct net_if *iface)
+DECL|net_if_set_link_addr|function|static inline void net_if_set_link_addr(struct net_if *iface,uint8_t *addr, uint8_t len)
+DECL|net_if|struct|struct net_if {
---
+++ net/yaip/net_if.c
@@ -0,0 +1,1 @@
+DECL|net_if_init|function|static int net_if_init(struct device *unused)
