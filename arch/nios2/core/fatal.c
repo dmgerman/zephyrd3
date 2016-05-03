2c95518135a820ae63c22d7635b68b3c1525ed7d
Andrew Boie <andrew.p.boie@intel.com>
2016-05-03 11:49:50 -0700

    nios2: fatal: add _SysFatalErrorHandler and _Fault stub
    ZEP-252 will handle implementation of the code here.
    
    Change-Id: I3e9a6c7cdf2d5a3b0240317b772628fead528095
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 7f7337a0fe82979b04c6a2b3db3a8c4ebf32663d

--- arch/nios2/core/fatal.c
+++ arch/nios2/core/fatal.c
@@ -1,2 +1,4 @@
+DECL|_Fault|function|FUNC_NORETURN void _Fault(void)
 DECL|_NanoFatalErrorHandler|function|FUNC_NORETURN void _NanoFatalErrorHandler(unsigned int reason, const NANO_ESF *esf)
+DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *esf)
 DECL|_default_esf|variable|_default_esf
