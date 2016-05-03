9627e3869871419e78d59b22231aae9e70a41b56
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-03 08:35:41 +0300

    net: yaip: Add IPv6 prefixes to network interface
    Change-Id: I34c75eec6068abbd6af1eac2e178e42ce382b128
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 61aae6ab27b1c9024b5a7bd7b5c3fc6950357b71

--- include/net/yaip/net_if.h
+++ include/net/yaip/net_if.h
@@ -2,6 +2,7 @@ DECL|NET_DEVICE_INIT|macro|NET_DEVICE_INIT
 DECL|NET_IF_INIT|macro|NET_IF_INIT
 DECL|NET_IF_MAX_IPV6_ADDR|macro|NET_IF_MAX_IPV6_ADDR
 DECL|NET_IF_MAX_IPV6_MADDR|macro|NET_IF_MAX_IPV6_MADDR
+DECL|NET_IF_MAX_IPV6_PREFIX|macro|NET_IF_MAX_IPV6_PREFIX
 DECL|__NET_IF_H__|macro|__NET_IF_H__
 DECL|addr_state|member|enum net_addr_state addr_state;
 DECL|addr_type|member|enum net_addr_type addr_type;
@@ -14,8 +15,12 @@ DECL|hop_limit|member|uint8_t hop_limit;
 DECL|init|member|void (*init)(struct net_if *iface);
 DECL|ipv6|member|} ipv6;
 DECL|is_infinite|member|bool is_infinite;
+DECL|is_infinite|member|bool is_infinite;
 DECL|is_used|member|bool is_used;
 DECL|is_used|member|bool is_used;
+DECL|is_used|member|bool is_used;
+DECL|len|member|uint8_t len;
+DECL|lifetime|member|struct nano_timer lifetime;
 DECL|lifetime|member|struct nano_timer lifetime;
 DECL|link_addr|member|struct net_linkaddr link_addr;
 DECL|mcast|member|struct net_if_mcast_addr mcast[NET_IF_MAX_IPV6_MADDR];
@@ -25,7 +30,10 @@ DECL|net_if_api|struct|struct net_if_api {
 DECL|net_if_get_device|function|static inline struct device *net_if_get_device(struct net_if *iface)
 DECL|net_if_get_link_addr|function|static inline struct net_linkaddr *net_if_get_link_addr(struct net_if *iface)
 DECL|net_if_get_mtu|function|static inline uint16_t net_if_get_mtu(struct net_if *iface)
+DECL|net_if_ipv6_prefix|struct|struct net_if_ipv6_prefix {
 DECL|net_if_mcast_addr|struct|struct net_if_mcast_addr {
 DECL|net_if_set_link_addr|function|static inline void net_if_set_link_addr(struct net_if *iface,uint8_t *addr, uint8_t len)
 DECL|net_if|struct|struct net_if {
+DECL|prefix|member|struct in6_addr prefix;
+DECL|prefix|member|struct net_if_ipv6_prefix prefix[NET_IF_MAX_IPV6_PREFIX];
 DECL|unicast|member|struct net_if_addr unicast[NET_IF_MAX_IPV6_ADDR];
