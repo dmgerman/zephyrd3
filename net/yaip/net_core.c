392ce5817cdb7d1248b53aaf4248b3e4bcf58038
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-03 10:41:25 +0300

    net: Add function that feeds data to RX fifo
    Network drivers should call this when new data has been
    received from network.
    
    Change-Id: Ife78fa0683b8c410c38358300a6a18e9325f0ef8
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 920a5f5e3825edc67e0ffacb72dba6d8be568fbd

--- net/yaip/net_core.c
+++ net/yaip/net_core.c
@@ -2,6 +2,7 @@ DECL|CONFIG_NET_RX_STACK_SIZE|macro|CONFIG_NET_RX_STACK_SIZE
 DECL|DEBUG|macro|DEBUG
 DECL|init_rx_queue|function|static void init_rx_queue(void)
 DECL|net_init|function|int net_init(void)
+DECL|net_recv|function|int net_recv(struct net_if *iface, struct net_buf *buf)
 DECL|net_rx_fiber|function|static void net_rx_fiber(int unused1, int unused2)
 DECL|network_initialization|function|static int network_initialization(void)
 DECL|rx_fiber_stack|variable|rx_fiber_stack
