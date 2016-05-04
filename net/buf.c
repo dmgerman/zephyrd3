1d2d8499600ae5aa201be97f8b5920794d86c6ad
Johan Hedberg <johan.hedberg@intel.com>
2016-05-04 20:27:48 +0300

    net: Add net_buf_get_timeout() API
    With this API it's possible for the caller to force specific behavior
    when it comes to waiting (or not waiting) on the FIFO.
    
    Change-Id: Ib66e2f767c26c82abf1ba3b80bd15aec2383542e
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6e4d2a9e0ed5a185d23cdd2114f644f9fc9431d9

--- net/buf.c
+++ net/buf.c
@@ -13,6 +13,7 @@ DECL|net_buf_add_le32|function|void net_buf_add_le32(struct net_buf *buf, uint32
 DECL|net_buf_add_u8|function|uint8_t *net_buf_add_u8(struct net_buf *buf, uint8_t value)
 DECL|net_buf_add|function|void *net_buf_add(struct net_buf *buf, size_t len)
 DECL|net_buf_clone|function|struct net_buf *net_buf_clone(struct net_buf *buf)
+DECL|net_buf_get_timeout|function|struct net_buf *net_buf_get_timeout(struct nano_fifo *fifo, size_t reserve_head, int32_t timeout)
 DECL|net_buf_get|function|struct net_buf *net_buf_get(struct nano_fifo *fifo, size_t reserve_head)
 DECL|net_buf_headroom|function|size_t net_buf_headroom(struct net_buf *buf)
 DECL|net_buf_pull_le16|function|uint16_t net_buf_pull_le16(struct net_buf *buf)
