e73e28e9c6559c53ddc0e233a1755c8e14e840c6
Johan Hedberg <johan.hedberg@intel.com>
2016-05-04 15:02:40 +0300

    Bluetooth: Fix draining nRF51 FIFO during init
    It's possible that there's a queued event from the nRF51 at the point
    where we start resetting it, resulting in the stack receiving two NOP
    events. To prevent this, make sure to drain the UART while the
    controller is in a powered off state.
    
    Change-Id: Ic009e11c11ac750fc76c881c0ffe9bd12d38da0a
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: caf50a52ea513cec5e871fdd5de1ae69b7681e00

--- drivers/bluetooth/nrf51_pm.c
+++ drivers/bluetooth/nrf51_pm.c
@@ -4,5 +4,5 @@ DECL|NBLE_SWDIO_PIN|macro|NBLE_SWDIO_PIN
 DECL|nrf51_disable|function|int nrf51_disable(void)
 DECL|nrf51_enable|function|int nrf51_enable(void)
 DECL|nrf51_gpio|variable|nrf51_gpio
-DECL|nrf51_init|function|int nrf51_init(void)
+DECL|nrf51_init|function|int nrf51_init(struct device *dev)
 DECL|sleep_ms|function|static inline void sleep_ms(unsigned int ms)
