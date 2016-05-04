01c3709ce062b65dcee206ba4d97abe3778519de
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-04 15:25:15 +0300

    net: buf: Add tests for net_buf fragmentation
    More realistic IPv6 packet creation tests created.
    One test creates one big IPv6 packet and manipulates that.
    Second test creates a list of IPv6 header + data fragments
    and sets some user data in those fragment buffers.
    
    Change-Id: Ic8309ff115e1d735e98c30b3c8292b0d6ffc5f42
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 95f5cd2b9a04e386e225cc5243eddfa59d19a852

--- tests/net/buf/src/main.c
+++ tests/net/buf/src/main.c
@@ -1,24 +1,45 @@
 DECL|DBG|macro|DBG
 DECL|DBG|macro|DBG
+DECL|LL_HEADERS|macro|LL_HEADERS
+DECL|PROTO_HEADERS|macro|PROTO_HEADERS
 DECL|TEST_TIMEOUT|macro|TEST_TIMEOUT
 DECL|acl_handle|member|uint16_t acl_handle;
+DECL|big_frags_fifo|variable|big_frags_fifo
 DECL|bt_data|struct|struct bt_data {
 DECL|buf_destroy|function|static void buf_destroy(struct net_buf *buf)
 DECL|bufs_fifo|variable|bufs_fifo
+DECL|chksum|member|uint16_t chksum;
 DECL|destroy_called|variable|destroy_called
+DECL|dst_port|member|uint16_t dst_port;
+DECL|dst|member|struct in6_addr dst;
+DECL|example_data|variable|example_data
+DECL|flow|member|uint16_t flow;
+DECL|frag_destroy_big|function|static void frag_destroy_big(struct net_buf *buf)
 DECL|frag_destroy_called|variable|frag_destroy_called
 DECL|frag_destroy|function|static void frag_destroy(struct net_buf *buf)
 DECL|frags_fifo|variable|frags_fifo
 DECL|func|member|bool (*func)(void);
 DECL|hci_opcode|member|uint16_t hci_opcode;
 DECL|hci_sync|member|void *hci_sync;
+DECL|hop_limit|member|uint8_t hop_limit;
+DECL|ipv6_hdr|struct|struct ipv6_hdr {
+DECL|len|member|uint16_t len;
+DECL|len|member|uint8_t len[2];
 DECL|main|function|void main(void)
 DECL|name|member|const char *name;
 DECL|net_buf_test_1|function|static bool net_buf_test_1(void)
 DECL|net_buf_test_2|function|static bool net_buf_test_2(void)
 DECL|net_buf_test_3|function|static bool net_buf_test_3(void)
 DECL|net_buf_test_4|function|static bool net_buf_test_4(void)
+DECL|net_buf_test_big_buf|function|static bool net_buf_test_big_buf(void)
+DECL|net_buf_test_multi_frags|function|static bool net_buf_test_multi_frags(void)
+DECL|nexthdr|member|uint8_t nexthdr;
 DECL|no_data_buf_fifo|variable|no_data_buf_fifo
+DECL|src_port|member|uint16_t src_port;
+DECL|src|member|struct in6_addr src;
+DECL|tcflow|member|uint8_t tcflow;
 DECL|test_3_fiber|function|static void test_3_fiber(int arg1, int arg2)
 DECL|tests|variable|tests
 DECL|type|member|uint8_t type;
+DECL|udp_hdr|struct|struct udp_hdr {
+DECL|vtc|member|uint8_t vtc;
