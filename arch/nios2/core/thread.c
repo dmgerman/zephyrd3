abbfac5ada0a851b987aa96b4f2ddfab61af4b10
Andrew Boie <andrew.p.boie@intel.com>
2016-05-05 10:30:14 -0700

    nios2: update _new_thread prototype to latest changes
    Now matches kernel/nanokernel/include/nano_internal.h.
    
    Change-Id: I4dbbf50aa05c55de42100a6896fe0fa3b26955ec
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: e91f7101c6b07f98b3d39643e055561b1cea8847

--- arch/nios2/core/thread.c
+++ arch/nios2/core/thread.c
@@ -1,2 +1,2 @@
 DECL|_nanokernel|variable|_nanokernel
-DECL|_new_thread|function|void _new_thread(char *stack_memory, unsigned stack_size, _thread_entry_t thread_func, void *parameter1, void *parameter2, void *parameter3, int priority, unsigned options)
+DECL|_new_thread|function|void _new_thread(char *stack_memory, unsigned stack_size, void *uk_task_ptr, _thread_entry_t thread_func, _thread_arg_t arg1, _thread_arg_t arg2, _thread_arg_t arg3, int prio, unsigned options)
