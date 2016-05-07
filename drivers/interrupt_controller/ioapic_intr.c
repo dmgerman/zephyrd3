79542d642a6fadf8ce55fb9b5df7f5d1f2723542
Jithu Joseph <jithu.joseph@intel.com>
2016-05-06 21:45:04 -0700

    pm/ioapic: Add suspend/resume support for IOAPIC
    Save and restore IOAPIC flags and irq to vector translation
    info for supporting deep sleep.
    
    Jira: ZEP-223
    Change-Id: Ifc50a5a72699ff6782ad194d8e96b18fac34da18
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: b1625df53489c6e0ffbf7f5314dfd8e196e3e239

--- drivers/interrupt_controller/ioapic_intr.c
+++ drivers/interrupt_controller/ioapic_intr.c
@@ -1,3 +1,9 @@
+DECL|BITS_PER_IRQ|macro|BITS_PER_IRQ
+DECL|BIT_POS_FOR_IRQ_OPTION|macro|BIT_POS_FOR_IRQ_OPTION
+DECL|IOAPIC_BITFIELD_ENBL_DSBL|macro|IOAPIC_BITFIELD_ENBL_DSBL
+DECL|IOAPIC_BITFIELD_HI_LO|macro|IOAPIC_BITFIELD_HI_LO
+DECL|IOAPIC_BITFIELD_LVL_EDGE|macro|IOAPIC_BITFIELD_LVL_EDGE
+DECL|SUSPEND_BITS_REQD|macro|SUSPEND_BITS_REQD
 DECL|_IoApicRedUpdateLo|function|static void _IoApicRedUpdateLo(unsigned int irq,uint32_t value, uint32_t mask)
 DECL|__IoApicGet|function|static uint32_t __IoApicGet(int32_t offset)
 DECL|__IoApicSet|function|static void __IoApicSet(int32_t offset, uint32_t value)
@@ -9,3 +15,9 @@ DECL|_ioapic_irq_set|function|void _ioapic_irq_set(unsigned int irq, unsigned in
 DECL|ioApicRedGetLo|function|static uint32_t ioApicRedGetLo(unsigned int irq)
 DECL|ioApicRedSetHi|function|static void ioApicRedSetHi(unsigned int irq, uint32_t upper32)
 DECL|ioApicRedSetLo|function|static void ioApicRedSetLo(unsigned int irq, uint32_t lower32)
+DECL|ioapic_pm_ops|variable|ioapic_pm_ops
+DECL|ioapic_resume|function|int ioapic_resume(struct device *port, int pm_policy)
+DECL|ioapic_suspend_buf|variable|ioapic_suspend_buf
+DECL|ioapic_suspend|function|int ioapic_suspend(struct device *port, int pm_policy)
+DECL|restore_flags|function|uint32_t restore_flags(unsigned int irq)
+DECL|store_flags|function|void store_flags(unsigned int irq, uint32_t flags)
