0e0fa511c97b0e642ae7301c8625778f607cac73
Carles Cufi <carles.cufi@nordicsemi.no>
2016-05-07 06:05:38 -0700

    arm: Add support for Nordic Semiconductor's nRF52 series of ICs
    Add SoC support for the Nordic Semiconductor nRF52 series.
    
    JIRA: ZEP-377
    Change-Id: Idb476f43d7ca35f87c73910dd9bd1c50032ccdbe
    Signed-off-by: Glenn Ruben Bakke <glenn.ruben.bakke@nordicsemi.no>
    Signed-off-by: Vinayak Kariappa Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    Signed-off-by: Håkon Alseth <haakon.alseth@nordicsemi.no>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 893fbd5460a77ef36a0d11515c6df8255ffafbcc

---
+++ arch/arm/soc/nordic_nrf5/nrf52/soc.c
@@ -0,0 +1,9 @@
+DECL|NMI_INIT|macro|NMI_INIT
+DECL|NMI_INIT|macro|NMI_INIT
+DECL|__SYSTEM_CLOCK_64M|macro|__SYSTEM_CLOCK_64M
+DECL|__used|variable|__used
+DECL|clock_init|function|static void clock_init(void)
+DECL|ftpan_32|function|static bool ftpan_32(void)
+DECL|ftpan_36|function|static bool ftpan_36(void)
+DECL|ftpan_37|function|static bool ftpan_37(void)
+DECL|nordicsemi_nrf52_init|function|static int nordicsemi_nrf52_init(struct device *arg)
