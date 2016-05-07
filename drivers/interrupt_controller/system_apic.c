ba49351d33ef58cd9e93cb434b2eeb3dbed1e64e
Jithu Joseph <jithu.joseph@intel.com>
2016-05-06 20:56:24 -0700

    apic : Refactor some macros into a header
    Moving few macros into a header file.
    
    Change-Id: I975e19ba518bd6184038d6c1715224be7190b3ad
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 7c29fefd7dd6c2cf943cf572fcadbe07c7a9d4fe

--- drivers/interrupt_controller/system_apic.c
+++ drivers/interrupt_controller/system_apic.c
@@ -1,9 +1,3 @@
-DECL|HARDWARE_IRQ_LIMIT|macro|HARDWARE_IRQ_LIMIT
-DECL|HARDWARE_IRQ_LIMIT|macro|HARDWARE_IRQ_LIMIT
-DECL|IS_IOAPIC_IRQ|macro|IS_IOAPIC_IRQ
-DECL|IS_IOAPIC_IRQ|macro|IS_IOAPIC_IRQ
-DECL|LOAPIC_IRQ_BASE|macro|LOAPIC_IRQ_BASE
-DECL|LOAPIC_IRQ_COUNT|macro|LOAPIC_IRQ_COUNT
 DECL|_SysIntVecAlloc|function|int _SysIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority, /* get vector from <priority> group */ uint32_t flags /* interrupt flags */ )
 DECL|_SysIntVecProgram|function|void _SysIntVecProgram(unsigned int vector, unsigned int irq, uint32_t flags)
 DECL|__LocalIntVecAlloc|function|static int __LocalIntVecAlloc(unsigned int irq, /* virtualized IRQ */ unsigned int priority/* get vector from <priority> group */ )
