a0d3de4c2f47c83f55baa5c62a33f7244d668eca
Chuck Jordan <cjordan@synopsys.com>
2016-05-09 14:26:28 -0700

    arc: early init should invalidate d-cache and set vector table
    Some ARC targets can have a data-cache. Although there is no special
    instruction to clear exceptions during early init, it is necessary to
    invalidate the d-cache BEFORE any data is fetched. The ARC on arduino 101
    doesn't have d-cache, and will thus skip this d-cache invalidate.
    
    Also, it is important to set the vector table base register to point to
    the interrupt vector table EARLY, so that if an exception is encountered,
    the correct vector table is found. Set this base only if it is found to be
    different from the one compiled in to the code.
    These initialization steps assure that proper exception handling
    is in place during early init.
    
    Change-Id: Ie8b5928e5813e104680a6d6510c85d32dc8ed8f3
    Signed-off-by: Chuck Jordan <cjordan@synopsys.com>
    
    Former-commit-id: 4ad41d8d7fc9c22eb51c40ae614738be92262580

--- arch/arc/core/prep_c.c
+++ arch/arc/core/prep_c.c
@@ -1,4 +1,6 @@
 DECL|_PrepC|function|void _PrepC(void)
+DECL|adjust_vector_table_base|function|static void adjust_vector_table_base(void)
 DECL|bssZero|function|static void bssZero(void)
 DECL|dataCopy|function|static void dataCopy(void)
 DECL|dataCopy|function|static void dataCopy(void)
+DECL|invalidate_dcache|function|static void invalidate_dcache(void)
