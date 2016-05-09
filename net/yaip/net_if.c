0c8beffc23f74888066995a9682ef4e82284db7d
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 15:43:06 +0300

    net: yaip: Refactor debug printing in net_if
    There is now Kconfig option to enable net_if debugging.
    Also use NET_DBG() instead of directly calling syslog macros
    so that we can print fiber id automatically.
    
    Change-Id: I150d363006e4be358c38308a93771ba444b0835e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d7cf956c0704fca119f04e50280718d02e87a5eb

--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -1,4 +1,6 @@
 DECL|CONFIG_NET_TX_STACK_SIZE|macro|CONFIG_NET_TX_STACK_SIZE
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|init_tx_queue|function|static inline void init_tx_queue(struct net_if *iface)
 DECL|net_if_get_by_link_addr|function|struct net_if *net_if_get_by_link_addr(struct net_linkaddr *ll_addr)
 DECL|net_if_init|function|int net_if_init(void)
