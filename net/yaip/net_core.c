bab90a042780959ad6dae5c27bb79df19d30efa3
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 17:39:09 +0300

    net: Add statistics gathering support
    Collect network statistics if CONFIG_NET_STATISTICS is
    defined.
    
    Change-Id: Id217daa3e19142c95396dc391ba2dc4123b18b22
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 875ea297d4dabde3622f545a44d416b2bee1b933

--- net/yaip/net_core.c
+++ net/yaip/net_core.c
@@ -1,12 +1,19 @@
 DECL|CONFIG_NET_RX_STACK_SIZE|macro|CONFIG_NET_RX_STACK_SIZE
+DECL|GET_STAT|macro|GET_STAT
 DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|PRINT_STATISTICS_INTERVAL|macro|PRINT_STATISTICS_INTERVAL
+DECL|PRINT_STAT|macro|PRINT_STAT
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|init_rx_queue|function|static void init_rx_queue(void)
 DECL|net_init|function|static int net_init(struct device *unused)
+DECL|net_print_statistics|macro|net_print_statistics
+DECL|net_print_statistics|macro|net_print_statistics
 DECL|net_recv|function|int net_recv(struct net_if *iface, struct net_buf *buf)
 DECL|net_rx_fiber|function|static void net_rx_fiber(int unused1, int unused2)
+DECL|net_stats|variable|net_stats
 DECL|network_initialization|function|static int network_initialization(void)
 DECL|process_data|function|static inline enum net_verdict process_data(struct net_buf *buf)
 DECL|rx_fiber_id|variable|rx_fiber_id
 DECL|rx_fiber_stack|variable|rx_fiber_stack
 DECL|rx_queue|variable|rx_queue
+DECL|stats|function|static inline void stats(void)
