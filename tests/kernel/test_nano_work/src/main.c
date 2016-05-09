8bf756e94fe8a92235341d53a08527b2fd1054a6
Vlad Dogaru <vlad.dogaru@intel.com>
2016-05-09 09:10:11 +0300

    tests: Add nano_work test
    Change-Id: I78359bdd75aa2aa5e2f4b34347d838747d60015f
    Signed-off-by: Vlad Dogaru <vlad.dogaru@intel.com>
    
    Former-commit-id: efa3b6fa99f244e88d3ac4fdbe8f1259a8d53215

---
+++ tests/kernel/test_nano_work/src/main.c
@@ -0,0 +1,21 @@
+DECL|FIBER_STACK_SIZE|macro|FIBER_STACK_SIZE
+DECL|NUM_TEST_ITEMS|macro|NUM_TEST_ITEMS
+DECL|SUBMIT_WAIT|macro|SUBMIT_WAIT
+DECL|WORK_ITEM_WAIT|macro|WORK_ITEM_WAIT
+DECL|check_results|function|static int check_results(void)
+DECL|fiber_main|function|static void fiber_main(int arg1, int arg2)
+DECL|fiber_stack|variable|fiber_stack
+DECL|key|member|int key;
+DECL|main|function|void main(void)
+DECL|num_results|variable|num_results
+DECL|reset_results|function|static void reset_results(void)
+DECL|resubmit_work_handler|function|static void resubmit_work_handler(struct nano_work *work)
+DECL|results|variable|results
+DECL|test_items_init|function|static void test_items_init(void)
+DECL|test_items_submit|function|static void test_items_submit(void)
+DECL|test_item|struct|struct test_item {
+DECL|test_resubmit|function|static int test_resubmit(void)
+DECL|test_sequence|function|static int test_sequence(void)
+DECL|tests|variable|tests
+DECL|work_handler|function|static void work_handler(struct nano_work *work)
+DECL|work|member|struct nano_work work;
