9540d0c9d231d9e4d897f60debb3503880b4520f
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-05-09 12:01:04 +0300

    grove: add temperature sensor
    Add driver for the Grove Temperature Sensor which measures ambient
    temperature.
    
    Sensor reference page:
    	http://www.seeedstudio.com/wiki/Grove_-_Temperature_Sensor
    
    Change-Id: I4f56224be5fa7a968749d5d4df8cb99a462c3c21
    Origin: original
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: 2064f4a9cc0a418737ae0aa5e9e8cd451293f2e5

---
+++ drivers/grove/temperature_sensor.c
@@ -0,0 +1,14 @@
+DECL|B_CONST|macro|B_CONST
+DECL|B_CONST|macro|B_CONST
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|adc_buffer|member|uint8_t adc_buffer[4];
+DECL|adc_table|member|struct adc_seq_table adc_table;
+DECL|adc|member|struct device *adc;
+DECL|gts_api|variable|gts_api
+DECL|gts_channel_get|function|static int gts_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|gts_data|struct|struct gts_data {
+DECL|gts_data|variable|gts_data
+DECL|gts_init|function|static int gts_init(struct device *dev)
+DECL|gts_sample_fetch|function|static int gts_sample_fetch(struct device *dev, enum sensor_channel chan)
+DECL|sample|member|struct adc_seq_entry sample;
