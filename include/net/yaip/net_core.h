be79d5711f4af5f06c602369290153bb166f9ed4
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 15:36:46 +0300

    net: Start to use logging macros from sys_log.h
    Instead of directly calling printk/printf, the network printing
    macros will use syslog macros defined in sys_log.h
    
    Change-Id: I3f12f81557f50b24ca47a43f345162f9ffbd574c
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: f75d8a86bb27e7c9395b4085c533b1aab54b36e9

--- include/net/yaip/net_core.h
+++ include/net/yaip/net_core.h
@@ -4,8 +4,11 @@ DECL|NET_ERR|macro|NET_ERR
 DECL|NET_ERR|macro|NET_ERR
 DECL|NET_INFO|macro|NET_INFO
 DECL|NET_INFO|macro|NET_INFO
-DECL|NET_PRINT|macro|NET_PRINT
-DECL|NET_PRINT|macro|NET_PRINT
+DECL|NET_WARN|macro|NET_WARN
+DECL|NET_WARN|macro|NET_WARN
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|__NET_CORE_H|macro|__NET_CORE_H
 DECL|net_analyze_stack|function|static inline void net_analyze_stack(const char *name, unsigned char *stack, size_t size)
 DECL|net_analyze_stack|macro|net_analyze_stack
