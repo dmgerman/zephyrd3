e1b62c14fdd70341d0ea5b5f06a5ce9b4ef853a2
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 16:09:36 +0300

    net: yaip: Start to receive network packets
    Currently all received packets are dropped as this is just
    the initial patch enabling the receive support.
    
    Change-Id: Ib06735f498a80edb8c9f7c5ec6f536a5ac1d362a
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 5a3834915d6bfe47b16e1fde81d5a04c255e642b

--- include/net/yaip/net_core.h
+++ include/net/yaip/net_core.h
@@ -1,9 +1,11 @@
 DECL|NET_DBG|macro|NET_DBG
 DECL|NET_DBG|macro|NET_DBG
+DECL|NET_DROP|enumerator|NET_DROP,
 DECL|NET_ERR|macro|NET_ERR
 DECL|NET_ERR|macro|NET_ERR
 DECL|NET_INFO|macro|NET_INFO
 DECL|NET_INFO|macro|NET_INFO
+DECL|NET_OK|enumerator|NET_OK,
 DECL|NET_WARN|macro|NET_WARN
 DECL|NET_WARN|macro|NET_WARN
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
@@ -12,3 +14,4 @@ DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|__NET_CORE_H|macro|__NET_CORE_H
 DECL|net_analyze_stack|function|static inline void net_analyze_stack(const char *name, unsigned char *stack, size_t size)
 DECL|net_analyze_stack|macro|net_analyze_stack
+DECL|net_verdict|enum|enum net_verdict {
