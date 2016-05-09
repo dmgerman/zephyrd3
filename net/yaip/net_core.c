9ef1a7680afb1a2a950e880de2831ff1a909b635
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 16:09:36 +0300

    net: Start to receive network packets
    Currently all received packets are dropped as this is just
    the initial patch enabling the receive support.
    
    Change-Id: Ib06735f498a80edb8c9f7c5ec6f536a5ac1d362a
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 8c8052497dc31f6a6a5e4c76ec568eefd4fbc6f2

--- net/yaip/net_core.c
+++ net/yaip/net_core.c
@@ -6,5 +6,7 @@ DECL|net_init|function|static int net_init(struct device *unused)
 DECL|net_recv|function|int net_recv(struct net_if *iface, struct net_buf *buf)
 DECL|net_rx_fiber|function|static void net_rx_fiber(int unused1, int unused2)
 DECL|network_initialization|function|static int network_initialization(void)
+DECL|process_data|function|static inline enum net_verdict process_data(struct net_buf *buf)
+DECL|rx_fiber_id|variable|rx_fiber_id
 DECL|rx_fiber_stack|variable|rx_fiber_stack
 DECL|rx_queue|variable|rx_queue
