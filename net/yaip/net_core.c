1989c24597b5a8b592561671c7abd32951d9c4bc
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 14:31:12 +0300

    net: yaip: Execute net_init() automatically
    User does not need to call net_init() as it is automatically
    called when system is brought up.
    
    Change-Id: I2cb10f4402088a0ca4feff226b65341f337194ea
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9291f82cd9d088ddb8cdb331067526090253bc50

--- net/yaip/net_core.c
+++ net/yaip/net_core.c
@@ -1,7 +1,7 @@
 DECL|CONFIG_NET_RX_STACK_SIZE|macro|CONFIG_NET_RX_STACK_SIZE
 DECL|DEBUG|macro|DEBUG
 DECL|init_rx_queue|function|static void init_rx_queue(void)
-DECL|net_init|function|int net_init(void)
+DECL|net_init|function|static int net_init(struct device *unused)
 DECL|net_recv|function|int net_recv(struct net_if *iface, struct net_buf *buf)
 DECL|net_rx_fiber|function|static void net_rx_fiber(int unused1, int unused2)
 DECL|network_initialization|function|static int network_initialization(void)
--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -1,5 +1,5 @@
 DECL|CONFIG_NET_TX_STACK_SIZE|macro|CONFIG_NET_TX_STACK_SIZE
 DECL|init_tx_queue|function|static inline void init_tx_queue(struct net_if *iface)
-DECL|net_if_init|function|static int net_if_init(struct device *unused)
+DECL|net_if_init|function|int net_if_init(void)
 DECL|net_if_tx_fiber|function|static void net_if_tx_fiber(struct net_if *iface)
 DECL|tx_fiber_stack|variable|tx_fiber_stack
