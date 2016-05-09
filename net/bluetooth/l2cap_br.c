61b945b76dbaf5e43a6d3ab37396c07053372986
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-05-09 22:23:10 +0200

    Bluetooth: L2CAP: Move init BR/EDR pool signal buffers to l2cap_br.c
    Moves initialization of signal buffer for BR/EDR transport to
    l2cap_br.c unit and removes existing references to signal buffer in
    l2cap.c file to prevent compiler complains.
    
    Change-Id: I784b926df814e632944f3a12b0b6d03d144d2438
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: bc1423f441a491cf1341392d58f16c06d16ef8cf

--- net/bluetooth/l2cap_br.c
+++ net/bluetooth/l2cap_br.c
@@ -1,7 +1,10 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
+DECL|L2CAP_BR_MIN_MTU|macro|L2CAP_BR_MIN_MTU
 DECL|L2CAP_BR_PSM_END|macro|L2CAP_BR_PSM_END
 DECL|L2CAP_BR_PSM_START|macro|L2CAP_BR_PSM_START
 DECL|br_servers|variable|br_servers
+DECL|br_sig|variable|br_sig
+DECL|bt_l2cap_br_init|function|void bt_l2cap_br_init(void)
 DECL|bt_l2cap_br_server_register|function|int bt_l2cap_br_server_register(struct bt_l2cap_server *server)
 DECL|l2cap_br_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_br_server_lookup_psm(uint16_t psm)
