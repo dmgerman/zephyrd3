f8f13278dc468933eb526c35e12764502f72f4cf
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 15:00:30 +0300

    net: Add net_if_get_by_link_addr() util function
    This will return the network interface that has some specific
    link address.
    
    Change-Id: Iaebcf6e769d4f91f3cda6d3a0779324f89603b54
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b33d8d6591f2adf8e5ff265ffcc72848ff83ef6f

--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -1,5 +1,6 @@
 DECL|CONFIG_NET_TX_STACK_SIZE|macro|CONFIG_NET_TX_STACK_SIZE
 DECL|init_tx_queue|function|static inline void init_tx_queue(struct net_if *iface)
+DECL|net_if_get_by_link_addr|function|struct net_if *net_if_get_by_link_addr(struct net_linkaddr *ll_addr)
 DECL|net_if_init|function|int net_if_init(void)
 DECL|net_if_tx_fiber|function|static void net_if_tx_fiber(struct net_if *iface)
 DECL|tx_fiber_stack|variable|tx_fiber_stack
