05f0f77235877a9bf75b23fd6948cd105d4c9f59
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-09 17:39:09 +0300

    net: yaip: Add statistics gathering support
    Collect network statistics if CONFIG_NET_STATISTICS is
    defined.
    
    Change-Id: Id217daa3e19142c95396dc391ba2dc4123b18b22
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 27dcfc3efdb4be24167bf4179c4cf4b8be69ac3b

---
+++ include/net/net_stats.h
@@ -0,0 +1,45 @@
+DECL|NET_STATS|macro|NET_STATS
+DECL|NET_STATS|macro|NET_STATS
+DECL|__NET_STATS_H|macro|__NET_STATS_H
+DECL|ackerr|member|net_stats_t ackerr;
+DECL|chkerr|member|net_stats_t chkerr;
+DECL|chkerr|member|net_stats_t chkerr;
+DECL|chkerr|member|net_stats_t chkerr;
+DECL|chkerr|member|net_stats_t chkerr;
+DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|forwarded|member|net_stats_t forwarded;
+DECL|fragerr|member|net_stats_t fragerr;
+DECL|hblenerr|member|net_stats_t hblenerr;
+DECL|icmp|member|struct net_stats_icmp icmp;
+DECL|ip_errors|member|struct net_stats_ip_errors ip_errors;
+DECL|ipv4|member|struct net_stats_ip ipv4;
+DECL|ipv6|member|struct net_stats_ip ipv6;
+DECL|lblenerr|member|net_stats_t lblenerr;
+DECL|net_stats_icmp|struct|struct net_stats_icmp {
+DECL|net_stats_ip_errors|struct|struct net_stats_ip_errors {
+DECL|net_stats_ip|struct|struct net_stats_ip {
+DECL|net_stats_tcp|struct|struct net_stats_tcp {
+DECL|net_stats_t|typedef|typedef uint32_t net_stats_t;
+DECL|net_stats_udp|struct|struct net_stats_udp {
+DECL|net_stats|struct|struct net_stats {
+DECL|processing_error|member|net_stats_t processing_error;
+DECL|protoerr|member|net_stats_t protoerr;
+DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|rexmit|member|net_stats_t rexmit;
+DECL|rst|member|net_stats_t rst;
+DECL|sent|member|net_stats_t sent;
+DECL|sent|member|net_stats_t sent;
+DECL|sent|member|net_stats_t sent;
+DECL|sent|member|net_stats_t sent;
+DECL|syndrop|member|net_stats_t syndrop;
+DECL|synrst|member|net_stats_t synrst;
+DECL|tcp|member|struct net_stats_tcp tcp;
+DECL|typeerr|member|net_stats_t typeerr;
+DECL|udp|member|struct net_stats_udp udp;
+DECL|vhlerr|member|net_stats_t vhlerr;
