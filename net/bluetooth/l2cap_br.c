c6b35cfd07844496ef09b4618bb8334fd053963b
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-05-10 18:18:40 +0200

    Bluetooth: L2CAP: Implement bt_l2cap_br_connected() notifier
    Adds implementation of the BR/EDR connection L2CAP notifier. It makes,
    for each registered BR/EDR fixed channel, basic validation and channel
    initialization using two local helper functions.
    
    Change-Id: I6393a43b25ff0d9f23deaf93562add08861ea1d2
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: bc6a2cd7d6261d65a777939ab01801daa2829831

--- net/bluetooth/l2cap_br.c
+++ net/bluetooth/l2cap_br.c
@@ -1,5 +1,7 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
+DECL|L2CAP_BR_DYN_CID_END|macro|L2CAP_BR_DYN_CID_END
+DECL|L2CAP_BR_DYN_CID_START|macro|L2CAP_BR_DYN_CID_START
 DECL|L2CAP_BR_MIN_MTU|macro|L2CAP_BR_MIN_MTU
 DECL|L2CAP_BR_PSM_END|macro|L2CAP_BR_PSM_END
 DECL|L2CAP_BR_PSM_START|macro|L2CAP_BR_PSM_START
@@ -10,4 +12,6 @@ DECL|bt_l2cap_br_connected|function|void bt_l2cap_br_connected(struct bt_conn *c
 DECL|bt_l2cap_br_fixed_chan_register|function|static void bt_l2cap_br_fixed_chan_register(struct bt_l2cap_fixed_chan *chan)
 DECL|bt_l2cap_br_init|function|void bt_l2cap_br_init(void)
 DECL|bt_l2cap_br_server_register|function|int bt_l2cap_br_server_register(struct bt_l2cap_server *server)
+DECL|l2cap_br_chan_add|function|static int l2cap_br_chan_add(struct bt_conn *conn, struct bt_l2cap_chan *chan)
+DECL|l2cap_br_chan_alloc_cid|function|static void l2cap_br_chan_alloc_cid(struct bt_conn *conn, struct bt_l2cap_chan *chan)
 DECL|l2cap_br_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_br_server_lookup_psm(uint16_t psm)
