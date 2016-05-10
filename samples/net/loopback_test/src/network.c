9f30eae97760f2fda1b2a1fee14e51c2f56ecc79
Flavio Santes <flavio.santes@intel.com>
2016-05-10 17:41:18 -0500

    samples/net/loopback: Add helper functions to handle app data
    Add two helper functions to simplify the use case.
    Add comments to clarify the use of the networking API.
    
    Change-Id: I60c58296b77d724b7094b90e153b50dfc0b58135
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 7e32716d25289c01bc76c1b32f5ea6ace24ac055

--- samples/net/loopback_test/src/network.c
+++ samples/net/loopback_test/src/network.c
@@ -6,19 +6,19 @@ DECL|STACKSIZE|macro|STACKSIZE
 DECL|TEST_COUNT|macro|TEST_COUNT
 DECL|TEST_COUNT|macro|TEST_COUNT
 DECL|any_addr|variable|any_addr
-DECL|appdatalen|variable|appdatalen
 DECL|count|variable|count
+DECL|data_len|variable|data_len
+DECL|eval_rcvd_data|function|int eval_rcvd_data(char *rcvd_buf, int rcvd_len)
 DECL|failure|variable|failure
-DECL|fiberReceiverStack|variable|fiberReceiverStack
-DECL|fiberSenderStack|variable|fiberSenderStack
 DECL|fiber_receiver|function|void fiber_receiver(void)
 DECL|fiber_sender|function|void fiber_sender(void)
-DECL|in6addr_any|variable|in6addr_any
-DECL|in6addr_loopback|variable|in6addr_loopback
 DECL|loopback_addr|variable|loopback_addr
-DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
+DECL|prepare_to_send|function|void prepare_to_send(struct net_buf *buf, size_t *len)
 DECL|received|variable|received
 DECL|receiver_id|variable|receiver_id
+DECL|receiver_stack|variable|receiver_stack
 DECL|sender_id|variable|sender_id
+DECL|sender_stack|variable|sender_stack
 DECL|sent|variable|sent
+DECL|text|variable|text
