f1b6a566397184617ab1ec121efe89fc8ec0d1f0
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-05-10 12:09:28 +0300

    sensors: add driver for DHTxx sensors
    Add driver for the DHT temperature and relative himidity sensor family.
    The sensor has a single signal pin (in addition to GND and VDD) over
    which communication is done in a half-duplex manner (see datasheet for
    more details).
    
    Datasheets:
    	http://www.micropik.com/PDF/dht11.pdf
    	https://www.sparkfun.com/datasheets/Sensors/Temperature/DHT22.pdf
    	http://akizukidenshi.com/download/ds/aosong/AM2302.pdf
    
    Origin: Original
    Change-Id: If8c8be73d7fdfef2ab2091bf9b8fe72bcd700ed1
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: c9caf1806a0fa22c3d001cc807a9ec94984f3d2c

---
+++ drivers/sensor/sensor_dht.c
@@ -0,0 +1,6 @@
+DECL|dht_api|variable|dht_api
+DECL|dht_channel_get|function|static int dht_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|dht_data|variable|dht_data
+DECL|dht_init|function|static int dht_init(struct device *dev)
+DECL|dht_measure_signal_duration|function|static int8_t dht_measure_signal_duration(struct dht_data *drv_data, uint32_t signal_val)
+DECL|dht_sample_fetch|function|static int dht_sample_fetch(struct device *dev, enum sensor_channel chan)
