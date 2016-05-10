73a2a2ad7cec508664f49024599af9a3bc5a9234
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-10 09:30:10 +0300

    net: yaip: Add debug function to print MAC address
    Change-Id: I32ca7a4f005d73be7c8d4d28a7a6fbd19c01dd2e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: abda6faa6515b3c91ea275776ecdc71d5cc490db

--- net/yaip/net_private.h
+++ net/yaip/net_private.h
@@ -1,1 +1,4 @@
-
+DECL|net_byte_to_hex|function|static inline char *net_byte_to_hex(uint8_t *ptr, uint8_t byte, char base, bool pad)
+DECL|net_sprint_ll_addr_buf|function|static inline char *net_sprint_ll_addr_buf(uint8_t *ll, uint8_t ll_len, char *buf, int buflen)
+DECL|net_sprint_ll_addr|function|static inline char *net_sprint_ll_addr(uint8_t *ll, uint8_t ll_len)
+DECL|net_sprint_ll_addr|function|static inline char *net_sprint_ll_addr(uint8_t *ll, uint8_t ll_len)
