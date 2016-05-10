aba570585c9606cd1844c5b994ffdaedf5781f28
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-05-10 18:41:21 +0200

    Bluetooth: L2CAP: Enable handle BR/EDR signal channel traffic
    Separates LE and BR/EDR L2CAP signal channel context and starts accept
    incoming PDU traffic on BR/EDR signal channel. For now all valid L2CAP
    commands are send back as rejected on BR/EDR signal channel.
    
    Change-Id: If4392b2d3ccbe157cb7eeafcaa0804e90f44e2be
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 742c71a186f7affe352b9d945e3de2c0a1a89c41

--- net/bluetooth/l2cap_br.c
+++ net/bluetooth/l2cap_br.c
@@ -11,7 +11,16 @@ DECL|br_sig|variable|br_sig
 DECL|bt_l2cap_br_connected|function|void bt_l2cap_br_connected(struct bt_conn *conn)
 DECL|bt_l2cap_br_fixed_chan_register|function|static void bt_l2cap_br_fixed_chan_register(struct bt_l2cap_fixed_chan *chan)
 DECL|bt_l2cap_br_init|function|void bt_l2cap_br_init(void)
+DECL|bt_l2cap_br_pool|variable|bt_l2cap_br_pool
 DECL|bt_l2cap_br_server_register|function|int bt_l2cap_br_server_register(struct bt_l2cap_server *server)
+DECL|bt_l2cap|struct|struct bt_l2cap {
+DECL|chan|member|struct bt_l2cap_chan chan;
+DECL|ident|member|uint8_t ident;
+DECL|l2cap_br_accept|function|static int l2cap_br_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
 DECL|l2cap_br_chan_add|function|static int l2cap_br_chan_add(struct bt_conn *conn, struct bt_l2cap_chan *chan)
 DECL|l2cap_br_chan_alloc_cid|function|static void l2cap_br_chan_alloc_cid(struct bt_conn *conn, struct bt_l2cap_chan *chan)
+DECL|l2cap_br_connected|function|static void l2cap_br_connected(struct bt_l2cap_chan *chan)
+DECL|l2cap_br_disconnected|function|static void l2cap_br_disconnected(struct bt_l2cap_chan *chan)
+DECL|l2cap_br_recv|function|static void l2cap_br_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
+DECL|l2cap_br_send_reject|function|static void l2cap_br_send_reject(struct bt_conn *conn, uint8_t ident, uint16_t reason)
 DECL|l2cap_br_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_br_server_lookup_psm(uint16_t psm)
