04b617a4cf2a763aa19b3c362b52c5ab0883e5c5
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-05-10 17:26:21 +0200

    Bluetooth: L2CAP: Introduce internal BR/EDR connection L2CAP notifier API
    Adds dedicated function informing L2CAP layer about established BR/EDR
    connection. Existing 'bt_l2cap_connected()' connection notifier is
    refactored to use new API 'bt_l2cap_br_connected()' which is implemented
    in l2cap_br.c file.
    Currently the new handler is just stub since proper internals of
    BR/EDR L2CAP signal channel management are not yet implemented
    in l2cap_br.c file.
    
    Change-Id: Idef9be63338c6bb4b3315ec0452d84f2431fbaa2
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 51044bf5f899d458b95597bcb82a970b96e8bf95

--- net/bluetooth/l2cap_br.c
+++ net/bluetooth/l2cap_br.c
@@ -5,6 +5,7 @@ DECL|L2CAP_BR_PSM_END|macro|L2CAP_BR_PSM_END
 DECL|L2CAP_BR_PSM_START|macro|L2CAP_BR_PSM_START
 DECL|br_servers|variable|br_servers
 DECL|br_sig|variable|br_sig
+DECL|bt_l2cap_br_connected|function|void bt_l2cap_br_connected(struct bt_conn *conn)
 DECL|bt_l2cap_br_init|function|void bt_l2cap_br_init(void)
 DECL|bt_l2cap_br_server_register|function|int bt_l2cap_br_server_register(struct bt_l2cap_server *server)
 DECL|l2cap_br_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_br_server_lookup_psm(uint16_t psm)
