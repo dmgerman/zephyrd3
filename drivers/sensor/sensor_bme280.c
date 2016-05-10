3242d78c619b3cae62badbb6bf8dae2d50e6cd53
Murtaza Alexandru <alexandru.murtaza@intel.com>
2016-05-10 15:30:06 +0300

    sensor: add support for BME280
    * Rename BMP280 to BME280 which is a superset for the former.
    * Autodetect device based on chip ID.
    * Implement humidity fetching and reading for BME280
    * Rename sample BMP280 to BME280
    
    Change-Id: I565c769a7011e7496c9f1cad861d5ee311839b4f
    Signed-off-by: Murtaza Alexandru <alexandru.murtaza@intel.com>
    
    Former-commit-id: 310ed1e2e946eae7d5d7396078bedaec360ac6c9

---
+++ drivers/sensor/sensor_bme280.c
@@ -0,0 +1,10 @@
+DECL|bme280_api_funcs|variable|bme280_api_funcs
+DECL|bme280_channel_get|function|static int bme280_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|bme280_chip_init|function|static int bme280_chip_init(struct device *dev)
+DECL|bme280_compensate_humidity|function|static void bme280_compensate_humidity(struct bme280_data *data, int32_t adc_humidity)
+DECL|bme280_compensate_press|function|static void bme280_compensate_press(struct bme280_data *data, int32_t adc_press)
+DECL|bme280_compensate_temp|function|static void bme280_compensate_temp(struct bme280_data *data, int32_t adc_temp)
+DECL|bme280_data|variable|bme280_data
+DECL|bme280_init|function|int bme280_init(struct device *dev)
+DECL|bme280_read_compensation|function|static void bme280_read_compensation(struct bme280_data *data)
+DECL|bme280_sample_fetch|function|static int bme280_sample_fetch(struct device *dev, enum sensor_channel chan)
--- drivers/sensor/sensor_bmp280.c
+++
@@ -1,9 +0,0 @@
-DECL|bmp280_api_funcs|variable|bmp280_api_funcs
-DECL|bmp280_channel_get|function|static int bmp280_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
-DECL|bmp280_chip_init|function|static int bmp280_chip_init(struct device *dev)
-DECL|bmp280_compensate_press|function|static void bmp280_compensate_press(struct bmp280_data *data, int32_t adc_press)
-DECL|bmp280_compensate_temp|function|static void bmp280_compensate_temp(struct bmp280_data *data, int32_t adc_temp)
-DECL|bmp280_data|variable|bmp280_data
-DECL|bmp280_init|function|int bmp280_init(struct device *dev)
-DECL|bmp280_read_compensation|function|static void bmp280_read_compensation(struct bmp280_data *data)
-DECL|bmp280_sample_fetch|function|static int bmp280_sample_fetch(struct device *dev, enum sensor_channel chan)
