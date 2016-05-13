e56526c187055a8850d697947ad1354cda7f053f
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-05-13 15:13:37 +0300

    nano_work: Add nano_work_init
    Change-Id: I2e54e3e6ad048ff0d85cbef83e415ad436ecf720
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 21fabb3d39a5a2d4aee0e5b274e45e9a1a998145

--- include/misc/nano_work.h
+++ include/misc/nano_work.h
@@ -2,6 +2,7 @@ DECL|_misc_nano_work__h_|macro|_misc_nano_work__h_
 DECL|_reserved|member|void *_reserved; /* Used by nano_fifo implementation. */
 DECL|fifo|member|struct nano_fifo fifo;
 DECL|handler|member|work_handler_t handler;
+DECL|nano_work_init|function|static inline void nano_work_init(struct nano_work *work, work_handler_t handler)
 DECL|nano_work_submit_to_queue|function|static inline void nano_work_submit_to_queue(struct nano_workqueue *wq, struct nano_work *work)
 DECL|nano_work_submit|function|static inline void nano_work_submit(struct nano_work *work)
 DECL|nano_workqueue|struct|struct nano_workqueue {
