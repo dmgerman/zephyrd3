6c2d18021216a8e8c3a11a7a72e9cfbdb14401e6
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-05-16 10:52:54 +0300

    tests: Add tests for delayed workqueue
    Change-Id: If62f44a989012047008c0d782aef2aa7b6016190
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 8b1afeaaa393582ae201920099dacf7f005f6fb2

--- tests/kernel/test_nano_work/src/main.c
+++ tests/kernel/test_nano_work/src/main.c
@@ -2,15 +2,26 @@ DECL|FIBER_STACK_SIZE|macro|FIBER_STACK_SIZE
 DECL|NUM_TEST_ITEMS|macro|NUM_TEST_ITEMS
 DECL|SUBMIT_WAIT|macro|SUBMIT_WAIT
 DECL|WORK_ITEM_WAIT|macro|WORK_ITEM_WAIT
-DECL|check_results|function|static int check_results(void)
-DECL|fiber_main|function|static void fiber_main(int arg1, int arg2)
+DECL|check_results|function|static int check_results(int num_tests)
+DECL|delayed_resubmit_work_handler|function|static void delayed_resubmit_work_handler(struct nano_work *work)
+DECL|delayed_work_handler|function|static void delayed_work_handler(struct nano_work *work)
+DECL|fiber_delayed_work_cancel_main|function|static void fiber_delayed_work_cancel_main(int arg1, int arg2)
+DECL|fiber_delayed_work_main|function|static void fiber_delayed_work_main(int arg1, int arg2)
+DECL|fiber_delayed_work_resubmit|function|static void fiber_delayed_work_resubmit(int arg1, int arg2)
 DECL|fiber_stack|variable|fiber_stack
+DECL|fiber_work_main|function|static void fiber_work_main(int arg1, int arg2)
 DECL|key|member|int key;
 DECL|main|function|void main(void)
 DECL|num_results|variable|num_results
 DECL|reset_results|function|static void reset_results(void)
 DECL|resubmit_work_handler|function|static void resubmit_work_handler(struct nano_work *work)
 DECL|results|variable|results
+DECL|test_delayed_cancel|function|static int test_delayed_cancel(void)
+DECL|test_delayed_init|function|static void test_delayed_init(void)
+DECL|test_delayed_resubmit_fiber|function|static int test_delayed_resubmit_fiber(void)
+DECL|test_delayed_resubmit|function|static int test_delayed_resubmit(void)
+DECL|test_delayed_submit|function|static int test_delayed_submit(void)
+DECL|test_delayed|function|static int test_delayed(void)
 DECL|test_items_init|function|static void test_items_init(void)
 DECL|test_items_submit|function|static void test_items_submit(void)
 DECL|test_item|struct|struct test_item {
@@ -18,4 +29,4 @@ DECL|test_resubmit|function|static int test_resubmit(void)
 DECL|test_sequence|function|static int test_sequence(void)
 DECL|tests|variable|tests
 DECL|work_handler|function|static void work_handler(struct nano_work *work)
-DECL|work|member|struct nano_work work;
+DECL|work|member|struct nano_delayed_work work;
