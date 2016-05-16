c7cd4d2cc831950abda1ed6e9b20371e8cf0a598
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-05-16 10:53:53 +0300

    nano_work: Add delayed version
    This adds a delayed version of nano_work API which is useful when
    handling timeouts since the same stack/workqueue can be shared.
    
    Change-Id: Iac43796fe96deb0a9c8976c91a65104b57779b00
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: b559c1bca21c495807fe58e8dee5b05bb3659cac

--- include/misc/nano_work.h
+++ include/misc/nano_work.h
@@ -1,10 +1,17 @@
+DECL|NANO_WORK_STATE_IDLE|enumerator|NANO_WORK_STATE_IDLE, /* Work item idle state */
 DECL|_misc_nano_work__h_|macro|_misc_nano_work__h_
 DECL|_reserved|member|void *_reserved; /* Used by nano_fifo implementation. */
 DECL|fifo|member|struct nano_fifo fifo;
+DECL|flags|member|atomic_t flags[1];
 DECL|handler|member|work_handler_t handler;
+DECL|nano_delayed_work_submit|function|static inline int nano_delayed_work_submit(struct nano_delayed_work *work, int ticks)
+DECL|nano_delayed_work|struct|struct nano_delayed_work {
 DECL|nano_work_init|function|static inline void nano_work_init(struct nano_work *work, work_handler_t handler)
 DECL|nano_work_submit_to_queue|function|static inline void nano_work_submit_to_queue(struct nano_workqueue *wq, struct nano_work *work)
 DECL|nano_work_submit|function|static inline void nano_work_submit(struct nano_work *work)
 DECL|nano_workqueue|struct|struct nano_workqueue {
 DECL|nano_work|struct|struct nano_work {
+DECL|timeout|member|struct _nano_timeout timeout;
 DECL|work_handler_t|typedef|typedef void (*work_handler_t)(struct nano_work *);
+DECL|work|member|struct nano_work work;
+DECL|wq|member|struct nano_workqueue *wq;
--- kernel/nanokernel/nano_work.c
+++ kernel/nanokernel/nano_work.c
@@ -1,3 +1,6 @@
+DECL|nano_delayed_work_cancel|function|int nano_delayed_work_cancel(struct nano_delayed_work *work)
+DECL|nano_delayed_work_init|function|void nano_delayed_work_init(struct nano_delayed_work *work, work_handler_t handler)
+DECL|nano_delayed_work_submit_to_queue|function|int nano_delayed_work_submit_to_queue(struct nano_workqueue *wq, struct nano_delayed_work *work, int ticks)
 DECL|nano_fiber_workqueue_start|function|void nano_fiber_workqueue_start(struct nano_workqueue *wq,const struct fiber_config *config)
 DECL|nano_task_workqueue_start|function|void nano_task_workqueue_start(struct nano_workqueue *wq, const struct fiber_config *config)
 DECL|nano_workqueue_start|function|void nano_workqueue_start(struct nano_workqueue *wq, const struct fiber_config *config)
@@ -5,4 +8,5 @@ DECL|sys_workqueue_init|function|static int sys_workqueue_init(struct device *de
 DECL|sys_workqueue|variable|sys_workqueue
 DECL|sys_wq_config|variable|sys_wq_config
 DECL|sys_wq_stack|variable|sys_wq_stack
+DECL|work_timeout|function|static void work_timeout(struct _nano_timeout *t)
 DECL|workqueue_fiber_main|function|static void workqueue_fiber_main(int arg1, int arg2)
