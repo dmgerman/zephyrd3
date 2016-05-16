0c9a2d53a543308b25ae7e826bee55bd1fa04025
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-16 10:45:22 +0300

    net: apps: Add IPv4 client support to echo-client
    Change-Id: Ic879b64f22e0a2a1edbeaa1a6315a605993a09a4
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1f10410076ea87dcefd1abbcbd40e13ca878f176

--- samples/net/echo_client/src/echo-client.c
+++ samples/net/echo_client/src/echo-client.c
@@ -1,6 +1,7 @@
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MCAST_IPADDR|macro|MCAST_IPADDR
 DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_IPADDR|macro|MY_IPADDR
 DECL|MY_PORT|macro|MY_PORT
 DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
 DECL|ONE_SEC|macro|ONE_SEC
@@ -10,11 +11,21 @@ DECL|PEER_PORT|macro|PEER_PORT
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|UIP_DRIPADDR0|macro|UIP_DRIPADDR0
+DECL|UIP_DRIPADDR1|macro|UIP_DRIPADDR1
+DECL|UIP_DRIPADDR2|macro|UIP_DRIPADDR2
+DECL|UIP_DRIPADDR3|macro|UIP_DRIPADDR3
+DECL|UIP_IPADDR0|macro|UIP_IPADDR0
+DECL|UIP_IPADDR1|macro|UIP_IPADDR1
+DECL|UIP_IPADDR2|macro|UIP_IPADDR2
+DECL|UIP_IPADDR3_PEER|macro|UIP_IPADDR3_PEER
+DECL|UIP_IPADDR3|macro|UIP_IPADDR3
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|expecting|variable|expecting
 DECL|get_context|function|static inline bool get_context(struct net_context **unicast, struct net_context **multicast)
+DECL|in4addr_my|variable|in4addr_my
 DECL|in6addr_my|variable|in6addr_my
 DECL|in6addr_peer|variable|in6addr_peer
 DECL|init_app|function|static inline void init_app(void)
@@ -27,5 +38,8 @@ DECL|reverse|function|static inline void reverse(unsigned char *buf, int len)
 DECL|send_packet|function|static inline int send_packet(const char *name, struct net_context *ctx, int ipsum_len, int pos)
 DECL|sending|function|static bool sending(int resend)
 DECL|stack_receiving|variable|stack_receiving
+DECL|uip_draddr|variable|uip_draddr
+DECL|uip_hostaddr|variable|uip_hostaddr
+DECL|uip_netmask|variable|uip_netmask
 DECL|unicast|variable|unicast
 DECL|wait_reply|function|static inline bool wait_reply(const char *name, struct net_context *ctx, int ipsum_len, int pos)
