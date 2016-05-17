f3434dce85ae882d770efc0d0bacb5689fb9a6fa
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-17 12:33:45 +0300

    net: yaip: Utilities to set and lookup interface IPv6 addresses
    The caller is able to add and lookup network interface IPv6
    addresses.
    
    Change-Id: I7f43e18bd1fa69c7dcd2f6b46ad33043c28b92e9
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 208d558f68d720b570b7234fdc1961bbe53a6f68

--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -4,5 +4,9 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|init_tx_queue|function|static inline void init_tx_queue(struct net_if *iface)
 DECL|net_if_get_by_link_addr|function|struct net_if *net_if_get_by_link_addr(struct net_linkaddr *ll_addr)
 DECL|net_if_init|function|int net_if_init(void)
+DECL|net_if_ipv6_addr_add|function|struct net_if_addr *net_if_ipv6_addr_add(struct net_if *iface, struct in6_addr *addr, enum net_addr_type addr_type, uint32_t vlifetime)
+DECL|net_if_ipv6_addr_lookup|function|struct net_if_addr *net_if_ipv6_addr_lookup(struct in6_addr *addr)
+DECL|net_if_ipv6_maddr_add|function|struct net_if_mcast_addr *net_if_ipv6_maddr_add(struct net_if *iface,struct in6_addr *addr)
+DECL|net_if_ipv6_maddr_lookup|function|struct net_if_mcast_addr *net_if_ipv6_maddr_lookup(struct in6_addr *maddr)
 DECL|net_if_tx_fiber|function|static void net_if_tx_fiber(struct net_if *iface)
 DECL|tx_fiber_stack|variable|tx_fiber_stack
