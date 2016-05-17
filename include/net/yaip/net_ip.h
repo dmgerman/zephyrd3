2fea3d1483295ec8763c0aaeba0de9cc643fc2ba
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-17 13:03:02 +0300

    net: yaip: Receive IPv6 packet
    Check if we have received IPv6 packet and call a handler
    function to process it.
    
    Change-Id: I1226729685ae2a805128b587e2a37e0016d53ecc
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d9c3552e00307d674e9ebdc37a63fa5264ce6bd0

--- include/net/yaip/net_ip.h
+++ include/net/yaip/net_ip.h
@@ -33,7 +33,10 @@ DECL|PF_INET6|macro|PF_INET6
 DECL|PF_INET|macro|PF_INET
 DECL|PF_UNSPEC|macro|PF_UNSPEC
 DECL|__NET_IP_H|macro|__NET_IP_H
+DECL|dst|member|struct in6_addr dst;
 DECL|family|member|sa_family_t family;
+DECL|flow|member|uint16_t flow;
+DECL|hop_limit|member|uint8_t hop_limit;
 DECL|htonl|macro|htonl
 DECL|htons|macro|htons
 DECL|in4_u|member|} in4_u;
@@ -44,12 +47,15 @@ DECL|in_addr|member|struct in_addr in_addr;
 DECL|in_addr|struct|struct in_addr {
 DECL|ip_protocol|enum|enum ip_protocol {
 DECL|ip_proto|member|enum ip_protocol ip_proto;
+DECL|len|member|uint8_t len[2];
 DECL|local_addr|member|struct net_addr *local_addr;
 DECL|local_port|member|uint16_t local_port;
 DECL|net_addr_state|enum|enum net_addr_state {
 DECL|net_addr_type|enum|enum net_addr_type {
 DECL|net_addr|struct|struct net_addr {
+DECL|net_ipv6_hdr|struct|struct net_ipv6_hdr {
 DECL|net_tuple|struct|struct net_tuple {
+DECL|nexthdr|member|uint8_t nexthdr;
 DECL|ntohl|macro|ntohl
 DECL|ntohs|macro|ntohs
 DECL|remote_addr|member|struct net_addr *remote_addr;
@@ -62,9 +68,12 @@ DECL|s6_addr32|macro|s6_addr32
 DECL|s6_addr|macro|s6_addr
 DECL|s_addr|macro|s_addr
 DECL|sa_family_t|typedef|typedef unsigned short int sa_family_t;
+DECL|src|member|struct in6_addr src;
+DECL|tcflow|member|uint8_t tcflow;
 DECL|u4_addr16|member|uint16_t u4_addr16[2]; /* In big endian */
 DECL|u4_addr32|member|uint32_t u4_addr32[1]; /* In big endian */
 DECL|u4_addr8|member|uint8_t u4_addr8[4];
 DECL|u6_addr16|member|uint16_t u6_addr16[8]; /* In big endian */
 DECL|u6_addr32|member|uint32_t u6_addr32[4]; /* In big endian */
 DECL|u6_addr8|member|uint8_t u6_addr8[16];
+DECL|vtc|member|uint8_t vtc;
