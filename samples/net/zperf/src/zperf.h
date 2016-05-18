86b3257bc4f377bb209fd1abbcd72b891ed8e819
Jeremie GARCIA <jeremie.garcia@intel.com>
2016-05-18 19:36:55 +0200

    net: apps: zperf - add TCP client
    Change-Id: I292e03c28169ca05f6b982a36be2ed618f8455bb
    Signed-off-by: Jeremie GARCIA <jeremie.garcia@intel.com>
    
    Former-commit-id: 9891d1e12b7aef26da5e6762a8d4a2ad201cfd73

--- samples/net/zperf/src/zperf.h
+++ samples/net/zperf/src/zperf.h
@@ -1,3 +1,10 @@
+DECL|CMD_STR_CONNECTAP|macro|CMD_STR_CONNECTAP
+DECL|CMD_STR_SETIP|macro|CMD_STR_SETIP
+DECL|CMD_STR_TCP_DOWNLOAD|macro|CMD_STR_TCP_DOWNLOAD
+DECL|CMD_STR_TCP_UPLOAD|macro|CMD_STR_TCP_UPLOAD
+DECL|CMD_STR_UDP_DOWNLOAD|macro|CMD_STR_UDP_DOWNLOAD
+DECL|CMD_STR_UDP_UPLOAD|macro|CMD_STR_UDP_UPLOAD
+DECL|CMD_STR_VERSION|macro|CMD_STR_VERSION
 DECL|IPV4_STR_LEN_MAX|macro|IPV4_STR_LEN_MAX
 DECL|IPV4_STR_LEN_MIN|macro|IPV4_STR_LEN_MIN
 DECL|VERSION|macro|VERSION
@@ -5,10 +12,12 @@ DECL|__ZPERF_H|macro|__ZPERF_H
 DECL|client_time_in_us|member|uint32_t client_time_in_us;
 DECL|jitter_in_us|member|uint32_t jitter_in_us;
 DECL|nb_bytes_sent|member|uint32_t nb_bytes_sent;
+DECL|nb_packets_errors|member|uint32_t nb_packets_errors;
 DECL|nb_packets_lost|member|uint32_t nb_packets_lost;
 DECL|nb_packets_outorder|member|uint32_t nb_packets_outorder;
 DECL|nb_packets_rcvd|member|uint32_t nb_packets_rcvd;
 DECL|nb_packets_sent|member|uint32_t nb_packets_sent;
+DECL|packet_size|member|uint32_t packet_size;
 DECL|time_in_us|member|uint32_t time_in_us;
 DECL|zperf_callback|typedef|typedef void (*zperf_callback)(int status, zperf_results*);
 DECL|zperf_results|struct|typedef struct zperf_results {
---
+++ samples/net/zperf/src/zperf_tcp_uploader.c
@@ -0,0 +1,2 @@
+DECL|TAG|macro|TAG
+DECL|zperf_tcp_upload|function|void zperf_tcp_upload(struct net_context *net_context,unsigned int duration_in_ms, unsigned int packet_size, zperf_results *results)
