e6e7de686731d904a7818d7dcad5794be2fa28ed
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-18 12:50:48 +0300

    net: yaip: Move IP address print funcs to separate file
    Making all big IP address print functions inline will increase
    the code space too much. So moving the big functions to utils.c
    and just leaving the stubs to be inline. These functions are
    available only when debugging is activated.
    
    Change-Id: Ib76846d282a821e99885ec1801df4302bb0c9b42
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 6b2a002d06fd3c49b98b3c400547960e587a20cd

---
+++ net/yaip/utils.c
@@ -0,0 +1,4 @@
+DECL|net_byte_to_hex|function|char *net_byte_to_hex(uint8_t *ptr, uint8_t byte, char base, bool pad)
+DECL|net_sprint_ip_addr_buf|function|char *net_sprint_ip_addr_buf(uint8_t *ip, int ip_len, char *buf, int buflen)
+DECL|net_sprint_ll_addr_buf|function|char *net_sprint_ll_addr_buf(uint8_t *ll, uint8_t ll_len, char *buf, int buflen)
+DECL|net_value_to_udec|function|static int net_value_to_udec(char *buf, uint32_t value, int precision)
