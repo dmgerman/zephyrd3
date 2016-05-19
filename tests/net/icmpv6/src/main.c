2ba07b32f58e0946caeb187c3334b0914fc5cc23
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-19 12:46:06 +0300

    net: Add unit tests for ICMPv6 handler
    Change-Id: Ibdbe82ed56f110e2e0c6443700863632ee1ef56b
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 5580cdcc540606e8d8e586bdfacd2d5301e34a95

---
+++ tests/net/icmpv6/src/main.c
@@ -0,0 +1,11 @@
+DECL|TEST_MSG|macro|TEST_MSG
+DECL|buf_status|macro|buf_status
+DECL|bufs_fifo|variable|bufs_fifo
+DECL|data_fifo|variable|data_fifo
+DECL|handle_test_msg|function|static enum net_verdict handle_test_msg(struct net_buf *buf)
+DECL|handler_called|variable|handler_called
+DECL|header|struct|struct header {
+DECL|main|function|void main(void)
+DECL|status|member|int status;
+DECL|test_handler1|variable|test_handler1
+DECL|test_handler2|variable|test_handler2
