fcc864115617bb1a40e7352f706025ab56e56b6f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-19 16:19:59 +0300

    net: Process received ICMPv4 messages
    Call ICMPv4 handler when receiving ICMP v4 packet.
    Currently only echo-request msg is handled.
    
    Change-Id: Ib59c65b38f13c484f1842485118dad32fb6a6f36
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b6a7648569bed6f23d81fc90b29db05d01a0ea44

--- net/yaip/net_private.h
+++ net/yaip/net_private.h
@@ -1,3 +1,4 @@
+DECL|net_calc_chksum_icmpv4|function|static inline uint16_t net_calc_chksum_icmpv4(struct net_buf *buf)
 DECL|net_calc_chksum_icmpv6|function|static inline uint16_t net_calc_chksum_icmpv6(struct net_buf *buf)
 DECL|net_hexdump|function|static inline void net_hexdump(const char *str, const uint8_t *packet, size_t length)
 DECL|net_hexdump|macro|net_hexdump
