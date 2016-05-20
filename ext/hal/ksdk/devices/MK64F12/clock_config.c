7815b5f048fb161f7966499ab2d6a7a69db23109
Maureen Helm <maureen.helm@nxp.com>
2016-05-20 15:06:07 -0500

    ksdk: Import Kinetis SDK device support for K64F
    Import device-specific header files and clock configuration for K64F.
    
    Origin: NXP KSDK 2.0
    URL: kex.nxp.com
    Maintained-by: External
    
    Change-Id: I8b4cc0fc0a832c736067459afec57939643181a7
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: d6f950c918715fe8b4de499b2ffc772b2d524f54

---
+++ ext/hal/ksdk/devices/MK64F12/clock_config.c
@@ -0,0 +1,10 @@
+DECL|BOARD_BootClockRUN|function|void BOARD_BootClockRUN(void)
+DECL|BOARD_BootClockVLPR|function|void BOARD_BootClockVLPR(void)
+DECL|_clock_config|struct|typedef struct _clock_config
+DECL|clock_config_t|typedef|} clock_config_t;
+DECL|coreClock|member|uint32_t coreClock; /*!< core clock frequency. */
+DECL|g_defaultClockConfigRun|variable|g_defaultClockConfigRun
+DECL|g_defaultClockConfigVlpr|variable|g_defaultClockConfigVlpr
+DECL|mcgConfig|member|mcg_config_t mcgConfig; /*!< MCG configuration. */
+DECL|oscConfig|member|osc_config_t oscConfig; /*!< OSC configuration. */
+DECL|simConfig|member|sim_clock_config_t simConfig; /*!< SIM configuration. */
