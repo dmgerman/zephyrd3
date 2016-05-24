90327f82c119860be5e756e4fa7d9f7b2eb5c2c8
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-05-24 13:26:26 +0300

    net: apps: Add DHCP client sample application
    Change-Id: I9c3f317fb13bce56ee93d66d692618145087a6a9
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 6a9817a091ec6bddefd3abb60ad34a176c09363d

---
+++ samples/net/dhcp_client/src/dhcp-client.c
@@ -0,0 +1,5 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|dhcpc_configured_cb|function|static void dhcpc_configured_cb(void)
+DECL|dhcpc_unconfigured_cb|function|static void dhcpc_unconfigured_cb(void)
+DECL|main|function|void main(void)
