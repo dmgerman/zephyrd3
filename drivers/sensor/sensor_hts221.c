ff7e8ad123e14ab0ee224414b95e21f62022ec13
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-05-24 12:09:22 +0300

    sensor: add driver for HTS221 sensor
    Add driver for HTS221 temeprature and relative humidity sensor.
    
    Datasheet:
    	http://www.st.com/resource/en/datasheet/hts221.pdf
    
    Interpreting humidity and temperature readings document:
    	http://www.st.com/resource/en/technical_note/dm00208001.pdf
    
    Change-Id: I1fee9d8372ad71b22bc85a60939c82f81569e135
    Origin: Original
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: 34f63de9011b80b298fa20e0e1859be30f4f99f4

---
+++ drivers/sensor/sensor_hts221.c
@@ -0,0 +1,6 @@
+DECL|hts221_channel_get|function|static int hts221_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|hts221_driver_api|variable|hts221_driver_api
+DECL|hts221_driver|variable|hts221_driver
+DECL|hts221_init|function|int hts221_init(struct device *dev)
+DECL|hts221_read_conversion_data|function|static int hts221_read_conversion_data(struct hts221_data *drv_data)
+DECL|hts221_sample_fetch|function|static int hts221_sample_fetch(struct device *dev, enum sensor_channel chan)
---
+++ drivers/sensor/sensor_hts221.h
@@ -0,0 +1,36 @@
+DECL|HTS221_AUTOINCREMENT_ADDR|macro|HTS221_AUTOINCREMENT_ADDR
+DECL|HTS221_BDU_BIT|macro|HTS221_BDU_BIT
+DECL|HTS221_CHIP_ID|macro|HTS221_CHIP_ID
+DECL|HTS221_DRDY_EN|macro|HTS221_DRDY_EN
+DECL|HTS221_I2C_ADDR|macro|HTS221_I2C_ADDR
+DECL|HTS221_ODR_SHIFT|macro|HTS221_ODR_SHIFT
+DECL|HTS221_PD_BIT|macro|HTS221_PD_BIT
+DECL|HTS221_REG_CONVERSION_START|macro|HTS221_REG_CONVERSION_START
+DECL|HTS221_REG_CTRL1|macro|HTS221_REG_CTRL1
+DECL|HTS221_REG_CTRL3|macro|HTS221_REG_CTRL3
+DECL|HTS221_REG_DATA_START|macro|HTS221_REG_DATA_START
+DECL|HTS221_REG_WHO_AM_I|macro|HTS221_REG_WHO_AM_I
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|__SENSOR_HTS221_H__|macro|__SENSOR_HTS221_H__
+DECL|data_ready_handler|member|sensor_trigger_handler_t data_ready_handler;
+DECL|data_ready_trigger|member|struct sensor_trigger data_ready_trigger;
+DECL|dev|member|struct device *dev;
+DECL|fiber_stack|member|char __stack fiber_stack[CONFIG_HTS221_FIBER_STACK_SIZE];
+DECL|gpio_cb|member|struct gpio_callback gpio_cb;
+DECL|gpio_sem|member|struct nano_sem gpio_sem;
+DECL|gpio|member|struct device *gpio;
+DECL|h0_rh_x2|member|uint8_t h0_rh_x2;
+DECL|h0_t0_out|member|int16_t h0_t0_out;
+DECL|h1_rh_x2|member|uint8_t h1_rh_x2;
+DECL|h1_t0_out|member|int16_t h1_t0_out;
+DECL|hts221_data|struct|struct hts221_data {
+DECL|hts221_odr_strings|variable|hts221_odr_strings
+DECL|i2c|member|struct device *i2c;
+DECL|rh_sample|member|int16_t rh_sample;
+DECL|t0_degc_x8|member|uint16_t t0_degc_x8;
+DECL|t0_out|member|int16_t t0_out;
+DECL|t1_degc_x8|member|uint16_t t1_degc_x8;
+DECL|t1_out|member|int16_t t1_out;
+DECL|t_sample|member|int16_t t_sample;
+DECL|work|member|struct nano_work work;
