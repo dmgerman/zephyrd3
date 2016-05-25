e441e1f29264b5d4326f7b517bc356ba844f2eca
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-25 10:23:28 +0300

    net: yaip: Setting static IP addresses for echo-server
    Makes it possible to test things between qemu and host when
    using slip.
    
    Change-Id: I27e17de9f0d8c2c8406f0afdd66c295e827f87a0
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1145bd43a1e8c1998951e9067702dcaa02bd2c8b

--- samples/net/echo_server/src/echo-server-yaip.c
+++ samples/net/echo_server/src/echo-server-yaip.c
@@ -10,8 +10,10 @@ DECL|STACKSIZE|macro|STACKSIZE
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|addr6|variable|addr6
 DECL|fiberStack|variable|fiberStack
 DECL|get_context|function|static inline bool get_context(struct net_context **udp_recv4, struct net_context **udp_recv6, struct net_context **tcp_recv4, struct net_context **tcp_recv6, struct net_context **mcast_recv4,
+DECL|in4addr_my|variable|in4addr_my
 DECL|in6addr_my|variable|in6addr_my
 DECL|in6addr_my|variable|in6addr_my
 DECL|init_app|function|static inline void init_app(void)
