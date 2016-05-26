2fd4c3aae93af1edf3dca30bd50c98763ec00fa5
Chuck Jordan <cjordan@synopsys.com>
2016-05-26 11:05:59 -0700

    test: timestamp_serialize() empty macro for ARCv2
    If the CPU is ARC, define timestamp_serialize() to be an
    empty macro so that this test builds properly.
    
    Change-Id: I36f00dd429972b610c327cbe59bedb24b33553ed
    Signed-off-by: Chuck Jordan <cjordan@synopsys.com>
    
    Former-commit-id: bc5cc9896648e91955b20a5bf90ab34735819099

--- tests/benchmark/latency_measure/microkernel/src/test_asm_inline_gcc.h
+++ tests/benchmark/latency_measure/microkernel/src/test_asm_inline_gcc.h
@@ -1,3 +1,4 @@
 DECL|_TEST_ASM_INLINE_GCC_H|macro|_TEST_ASM_INLINE_GCC_H
 DECL|timestamp_serialize|function|static inline void timestamp_serialize(void)
 DECL|timestamp_serialize|function|static inline void timestamp_serialize(void)
+DECL|timestamp_serialize|macro|timestamp_serialize
