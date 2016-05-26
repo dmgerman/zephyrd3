165b8ee6d60f08c6072d330eebe01feb8824228f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-05-26 10:58:09 +0300

    net: 802.15.4: Removed the TX fiber and its stack
    This saves little over 4kb of memory.
    
    Change-Id: I4dc3812b11de2736e2bdc17042e47fcdf5cf5491
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 81671f793d097133c248e2ed5cf2218cce76cd56

--- net/ip/net_driver_15_4.c
+++ net/ip/net_driver_15_4.c
@@ -1,12 +1,10 @@
 DECL|CONFIG_15_4_RX_STACK_SIZE|macro|CONFIG_15_4_RX_STACK_SIZE
-DECL|CONFIG_15_4_TX_STACK_SIZE|macro|CONFIG_15_4_TX_STACK_SIZE
 DECL|NET_DBG|macro|NET_DBG
 DECL|NET_DBG|macro|NET_DBG
 DECL|PRINT|macro|PRINT
 DECL|PRINT|macro|PRINT
 DECL|STACKSIZE_UNIT|macro|STACKSIZE_UNIT
 DECL|init_rx_queue|function|static void init_rx_queue(void)
-DECL|init_tx_queue|function|static void init_tx_queue(void)
 DECL|net_driver_15_4_init|function|int net_driver_15_4_init(void)
 DECL|net_driver_15_4_open|function|static int net_driver_15_4_open(void)
 DECL|net_driver_15_4_recv_from_hw|function|int net_driver_15_4_recv_from_hw(struct net_buf *buf)
@@ -14,8 +12,5 @@ DECL|net_driver_15_4_recv|function|int net_driver_15_4_recv(struct net_buf *buf)
 DECL|net_driver_15_4_send|function|static int net_driver_15_4_send(struct net_buf *buf)
 DECL|net_driver_15_4|variable|net_driver_15_4
 DECL|net_rx_15_4_fiber|function|static void net_rx_15_4_fiber(void)
-DECL|net_tx_15_4_fiber|function|static void net_tx_15_4_fiber(void)
 DECL|rx_fiber_stack|variable|rx_fiber_stack
 DECL|rx_queue|variable|rx_queue
-DECL|tx_fiber_stack|variable|tx_fiber_stack
-DECL|tx_queue|variable|tx_queue
