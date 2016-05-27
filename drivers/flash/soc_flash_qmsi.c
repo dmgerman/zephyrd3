18588ba7175c69b9f7f058386c70a967fb7274b4
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2016-05-27 16:09:22 -0700

    qmsi: flash: Improved reentrancy of the soc flash driver
    Calls to flash write or erase operations can get preempted
    in the middle of the API call if a concurrent call (from
    other fiber or task) to the same API is made. The patch ensures
    concurrent API calls from other fiber/task is put on hold until
    the current operation is completed.
    
    The mechanism is by default not activated. To enable
    it, the following flag needs to be defined:
    
    CONFIG_SOC_FLASH_QMSI_API_REENTRANCY
    
    Jira: ZEP-414
    
    Change-Id: I39429e40cb6ed446123dd1a1d7c7acc1b12417aa
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: 2f4975b625278c7436d8d830057890151f06a730

--- drivers/flash/soc_flash_qmsi.c
+++ drivers/flash/soc_flash_qmsi.c
@@ -1,9 +1,19 @@
+DECL|FLASH_CONTEXT|macro|FLASH_CONTEXT
+DECL|FLASH_CONTEXT|macro|FLASH_CONTEXT
+DECL|flash_critical_region_end|function|static void flash_critical_region_end(struct device *dev)
+DECL|flash_critical_region_start|function|static void flash_critical_region_start(struct device *dev)
 DECL|flash_qmsi_api|variable|flash_qmsi_api
 DECL|flash_qmsi_erase|function|static int flash_qmsi_erase(struct device *dev, off_t addr, size_t size)
 DECL|flash_qmsi_read|function|static int flash_qmsi_read(struct device *dev, off_t addr, void *data, size_t len)
 DECL|flash_qmsi_write_protection|function|static int flash_qmsi_write_protection(struct device *dev, bool enable)
 DECL|flash_qmsi_write|function|static int flash_qmsi_write(struct device *dev, off_t addr, const void *data, size_t len)
+DECL|flash_reentrancy_init|function|static void flash_reentrancy_init(struct device *dev)
 DECL|flash_region|function|static qm_flash_region_t flash_region(uint32_t addr)
 DECL|get_page_num|function|static uint32_t get_page_num(uint32_t addr)
 DECL|is_aligned_32|function|static inline bool is_aligned_32(uint32_t data)
 DECL|quark_flash_init|function|static int quark_flash_init(struct device *dev)
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|sem|member|struct nano_sem sem;
+DECL|soc_flash_context|variable|soc_flash_context
+DECL|soc_flash_data|struct|struct soc_flash_data {
