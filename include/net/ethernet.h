72bcf0ab5db756bfd2ec47da7ce761278dd029ea
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-05-30 13:54:04 +0300

    net: yaip: Provide separate header file for ethernet
    Change-Id: I2f3bba1014cf598cdd7606d5d43b3653052b1f7f
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 3ef38a6e05fdf316c60c39882aa88a8ffec36bcb

---
+++ include/net/ethernet.h
@@ -0,0 +1,11 @@
+DECL|NET_ETH_PTYPE_ARP|macro|NET_ETH_PTYPE_ARP
+DECL|NET_ETH_PTYPE_IPV6|macro|NET_ETH_PTYPE_IPV6
+DECL|NET_ETH_PTYPE_IP|macro|NET_ETH_PTYPE_IP
+DECL|__ETHERNET_H|macro|__ETHERNET_H
+DECL|__packed|variable|__packed
+DECL|addr|member|uint8_t addr[6];
+DECL|dst|member|struct net_eth_addr dst;
+DECL|net_eth_addr|struct|struct net_eth_addr {
+DECL|net_eth_hdr|struct|struct net_eth_hdr {
+DECL|src|member|struct net_eth_addr src;
+DECL|type|member|uint16_t type;
