73a469ce95eefecc648723a0505dea29ff6e9081
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-05-30 14:09:30 +0200

    net: Use generic wrapper for semaphore give operation
    No need to recreate an convenient context wise wrapper since one already
    exists.
    
    Change-Id: Ie1bfc0e203281485acb36e0e844b2bad384c2af6
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 309ff1b62f9381d43b7679c055ea70bfa228b3aa

--- net/yaip/net_context.c
+++ net/yaip/net_context.c
@@ -1,7 +1,6 @@
 DECL|NET_MAX_CONTEXT|macro|NET_MAX_CONTEXT
 DECL|NET_MAX_CONTEXT|macro|NET_MAX_CONTEXT
 DECL|context_port_used|function|static int context_port_used(enum ip_protocol ip_proto, uint16_t local_port, const struct net_addr *local_addr)
-DECL|context_sem_give|function|static void context_sem_give(struct nano_sem *chan)
 DECL|contexts_lock|variable|contexts_lock
 DECL|contexts|variable|contexts
 DECL|net_context_get|function|struct net_context *net_context_get(enum ip_protocol ip_proto, const struct net_addr *remote_addr, uint16_t remote_port, struct net_addr *local_addr, uint16_t local_port)
