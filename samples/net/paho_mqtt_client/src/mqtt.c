6c226982967d174651171f7655a571dc19503dd9
Flavio Santes <flavio.santes@intel.com>
2016-05-30 15:42:56 -0500

    samples/net/mqtt: Reduce tx/rx buffer size and update mqtt_publish_read
    Set CONFIG_IP_BUF_RX_SIZE and CONFIG_IP_BUF_TX_SIZE to 4.
    Modify mqtt_publish_read to retrieve topic name and message as strings.
    Change printf for printk.
    
    Change-Id: I6746846cb3359096d87dfbb3134d6a3a95bf087e
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 4a320be8a2ab81e0dfa4d355995e1b44dec26851

--- samples/net/paho_mqtt_client/src/mqtt.c
+++ samples/net/paho_mqtt_client/src/mqtt.c
@@ -3,6 +3,6 @@ DECL|mqtt_buffer|variable|mqtt_buffer
 DECL|mqtt_connect|function|int mqtt_connect(struct net_context *ctx, char *client_name)
 DECL|mqtt_disconnect|function|int mqtt_disconnect(struct net_context *ctx)
 DECL|mqtt_pingreq|function|int mqtt_pingreq(struct net_context *ctx)
-DECL|mqtt_publish_read|function|int mqtt_publish_read(struct net_context *ctx)
+DECL|mqtt_publish_read|function|int mqtt_publish_read(struct net_context *ctx, char *topic_str, char *msg_str)
 DECL|mqtt_publish|function|int mqtt_publish(struct net_context *ctx, char *topic, char *msg)
 DECL|mqtt_subscribe|function|int mqtt_subscribe(struct net_context *ctx, char *topic)
