17db489c43a550f9afc561f9ef8720c578772a78
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2016-05-31 17:15:33 -0700

    qmsi: aon counter: Use locking mechanism to guard critical regions of API call
    This will guard critical regions in the driver API so that the API
    operation called from one thread (task or fiber) can complete
    without the context being over-written by any simultaneous call
    of the API from other thread (task or fiber).
    
    The locking mechanism is by default disabled in the build. To enable it,
    the following flag needs to be defined:
    
    CONFIG_AON_API_REENTRANCY
    
    Jira: ZEP-424
    Change-Id: I1b0eae70a4ab97229931033153662874d7c4a68e
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    
    Former-commit-id: aebe38465f3888589135fc9903dc7b460f39c231

--- drivers/counter/counter_qmsi_aonpt.c
+++ drivers/counter/counter_qmsi_aonpt.c
@@ -1,8 +1,18 @@
+DECL|AONPT_CONTEXT|macro|AONPT_CONTEXT
+DECL|AONPT_CONTEXT|macro|AONPT_CONTEXT
+DECL|aon_critical_region_end|function|static void aon_critical_region_end(struct device *dev)
+DECL|aon_critical_region_start|function|static void aon_critical_region_start(struct device *dev)
+DECL|aon_data|struct|struct aon_data {
+DECL|aon_reentrancy_init|function|static void aon_reentrancy_init(struct device *dev)
 DECL|aon_timer_init|function|static int aon_timer_init(struct device *dev)
 DECL|aon_timer_qmsi_api|variable|aon_timer_qmsi_api
 DECL|aon_timer_qmsi_read|function|static uint32_t aon_timer_qmsi_read(void)
 DECL|aon_timer_qmsi_set_alarm|function|static int aon_timer_qmsi_set_alarm(struct device *dev, counter_callback_t callback, uint32_t count, void *user_data)
 DECL|aon_timer_qmsi_start|function|static int aon_timer_qmsi_start(struct device *dev)
 DECL|aon_timer_qmsi_stop|function|static int aon_timer_qmsi_stop(struct device *dev)
+DECL|aonpt_context|variable|aonpt_context
 DECL|aonpt_int_callback|function|static void aonpt_int_callback(void *user_data)
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|sem|member|struct nano_sem sem;
 DECL|user_cb|variable|user_cb
