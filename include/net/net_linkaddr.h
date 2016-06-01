3658d50a53351e348e3a4439eb7ef1d68b9eedc4
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-01 10:42:21 +0300

    net: Add struct to store link layer address
    Adding a variant for net_linkaddr that can actually store
    a link layer address. The normal net_linkaddr struct just
    uses pointer to a ll address. The storage version is used
    when we have a lladdr cache for neighbors.
    
    Change-Id: I57421047f66090caf09595981c81d386144edce3
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 301feea5b48427384d45348beadc5995d6937eac

--- include/net/net_linkaddr.h
+++ include/net/net_linkaddr.h
@@ -1,4 +1,8 @@
 DECL|__NET_LINKADDR_H__|macro|__NET_LINKADDR_H__
 DECL|addr|member|uint8_t *addr;
+DECL|addr|member|uint8_t addr[0];
 DECL|len|member|uint8_t len;
+DECL|len|member|uint8_t len;
+DECL|net_linkaddr_storage|struct|struct net_linkaddr_storage {
 DECL|net_linkaddr|struct|struct net_linkaddr {
+DECL|storage|member|uint32_t storage[2];
