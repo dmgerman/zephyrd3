7911653ca704e8e563566ecef1e0d83e5fdfc0c6
Adrian Bradianu <adrian.bradianu@windriver.com>
2016-06-01 14:20:02 +0300

    usb: Add USB device core layer
    USB Device core layer is a hardware independent interface between USB
    device controller driver and USB device class drivers or customer
    applications. It's a port of the LPCUSB device stack.
    
    Change-Id: I9371ffab7034d20953fec0525e72fbe9e094c931
    Signed-off-by: Adrian Bradianu <adrian.bradianu@windriver.com>
    Signed-off-by: Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 7a16ade6d12ab7db8137ab762caf5bcfa06228c1

---
+++ usb/include/usb_device.h
@@ -0,0 +1,24 @@
+DECL|MAX_PACKET_SIZE0|macro|MAX_PACKET_SIZE0
+DECL|USB_DEVICE_H_|macro|USB_DEVICE_H_
+DECL|bRequest|member|uint8_t bRequest; /**< specific request */
+DECL|bmRequestType|member|uint8_t bmRequestType; /**< characteristics of the specific request */
+DECL|cb_usb_status|member|usb_status_callback cb_usb_status;
+DECL|class_handler|member|usb_request_handler class_handler;
+DECL|custom_handler|member|usb_request_handler custom_handler;
+DECL|endpoint|member|struct usb_ep_cfg_data *endpoint;
+DECL|ep_addr|member|uint8_t ep_addr;
+DECL|ep_cb|member|usb_ep_callback ep_cb;
+DECL|interface|member|struct usb_interface_cfg_data interface;
+DECL|num_endpoints|member|uint8_t num_endpoints;
+DECL|payload_data|member|uint8_t *payload_data;
+DECL|usb_cfg_data|struct|struct usb_cfg_data {
+DECL|usb_device_description|member|const uint8_t *usb_device_description;
+DECL|usb_ep_callback|typedef|typedef void (*usb_ep_callback)(uint8_t ep,
+DECL|usb_ep_cfg_data|struct|struct usb_ep_cfg_data {
+DECL|usb_interface_cfg_data|struct|struct usb_interface_cfg_data {
+DECL|usb_request_handler|typedef|typedef int (*usb_request_handler) (struct usb_setup_packet *detup,
+DECL|usb_setup_packet|struct|struct usb_setup_packet {
+DECL|usb_status_callback|typedef|typedef void (*usb_status_callback)(enum usb_dc_status_code status_code);
+DECL|wIndex|member|uint16_t wIndex; /**< request specific parameter */
+DECL|wLength|member|uint16_t wLength; /**< length of data transferred in data phase */
+DECL|wValue|member|uint16_t wValue; /**< request specific parameter */
---
+++ usb/usb_device.c
@@ -0,0 +1,52 @@
+DECL|CONF_DESC_bConfigurationValue|macro|CONF_DESC_bConfigurationValue
+DECL|CONF_DESC_bmAttributes|macro|CONF_DESC_bmAttributes
+DECL|CONF_DESC_wTotalLength|macro|CONF_DESC_wTotalLength
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DESC_bDescriptorType|macro|DESC_bDescriptorType
+DECL|DESC_bLength|macro|DESC_bLength
+DECL|ENDP_DESC_bEndpointAddress|macro|ENDP_DESC_bEndpointAddress
+DECL|ENDP_DESC_bmAttributes|macro|ENDP_DESC_bmAttributes
+DECL|ENDP_DESC_wMaxPacketSize|macro|ENDP_DESC_wMaxPacketSize
+DECL|INTF_DESC_bAlternateSetting|macro|INTF_DESC_bAlternateSetting
+DECL|MAX_DESC_HANDLERS|macro|MAX_DESC_HANDLERS
+DECL|MAX_NUM_REQ_HANDLERS|macro|MAX_NUM_REQ_HANDLERS
+DECL|MAX_STD_REQ_MSG_SIZE|macro|MAX_STD_REQ_MSG_SIZE
+DECL|USB_CONTROL_IN_EP0|macro|USB_CONTROL_IN_EP0
+DECL|USB_CONTROL_OUT_EP0|macro|USB_CONTROL_OUT_EP0
+DECL|configuration|member|uint8_t configuration;
+DECL|custom_req_handler|member|usb_request_handler custom_req_handler;
+DECL|data_buf_len|member|int32_t data_buf_len;
+DECL|data_buf_residue|member|int32_t data_buf_residue;
+DECL|data_buf|member|uint8_t *data_buf;
+DECL|data_store|member|uint8_t *data_store[MAX_NUM_REQ_HANDLERS];
+DECL|descriptors|member|const uint8_t *descriptors;
+DECL|enabled|member|bool enabled;
+DECL|req_handlers|member|usb_request_handler req_handlers[MAX_NUM_REQ_HANDLERS];
+DECL|setup|member|struct usb_setup_packet setup;
+DECL|status_callback|member|usb_status_callback status_callback;
+DECL|std_req_data|member|uint8_t std_req_data[MAX_STD_REQ_MSG_SIZE];
+DECL|usb_data_to_host|function|static void usb_data_to_host(void)
+DECL|usb_deconfig|function|int usb_deconfig(void)
+DECL|usb_dev_priv|struct|static struct usb_dev_priv {
+DECL|usb_dev|variable|usb_dev
+DECL|usb_disable|function|int usb_disable(void)
+DECL|usb_enable|function|int usb_enable(struct usb_cfg_data *config)
+DECL|usb_get_descriptor|function|static bool usb_get_descriptor(uint16_t type_index, uint16_t lang_id,int32_t *len, uint8_t **data)
+DECL|usb_handle_control_transfer|function|static void usb_handle_control_transfer(uint8_t ep,enum usb_dc_ep_cb_status_code ep_status)
+DECL|usb_handle_request|function|static bool usb_handle_request(struct usb_setup_packet *setup,int32_t *len, uint8_t **data)
+DECL|usb_handle_standard_request|function|static int usb_handle_standard_request(struct usb_setup_packet *setup,int32_t *len, uint8_t **data_buf)
+DECL|usb_handle_std_device_req|function|static bool usb_handle_std_device_req(struct usb_setup_packet *setup,int32_t *len, uint8_t **data_buf)
+DECL|usb_handle_std_endpoint_req|function|static bool usb_handle_std_endpoint_req(struct usb_setup_packet *setup,int32_t *len, uint8_t **data_buf)
+DECL|usb_handle_std_interface_req|function|static bool usb_handle_std_interface_req(struct usb_setup_packet *setup,int32_t *len, uint8_t **data_buf)
+DECL|usb_print_setup|function|static void usb_print_setup(struct usb_setup_packet *setup)
+DECL|usb_read|function|int usb_read(uint8_t ep, uint8_t *data, uint32_t max_data_len,uint32_t *ret_bytes)
+DECL|usb_register_custom_req_handler|function|static void usb_register_custom_req_handler(usb_request_handler handler)
+DECL|usb_register_descriptors|function|static void usb_register_descriptors(const uint8_t *usb_descriptors)
+DECL|usb_register_request_handler|function|static void usb_register_request_handler(int32_t type,usb_request_handler handler, uint8_t *data_store)
+DECL|usb_register_status_callback|function|static void usb_register_status_callback(usb_status_callback cb)
+DECL|usb_set_configuration|function|static bool usb_set_configuration(uint8_t config_index, uint8_t alt_setting)
+DECL|usb_set_config|function|int usb_set_config(struct usb_cfg_data *config)
+DECL|usb_vbus_set|function|static int usb_vbus_set(bool on)
+DECL|usb_write|function|int usb_write(uint8_t ep, const uint8_t *data, uint32_t data_len,uint32_t *bytes_ret)
