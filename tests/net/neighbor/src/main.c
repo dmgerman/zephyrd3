f423cc0273abae6efafdf7d3bc5cf5f2567914d9
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-01 15:27:09 +0300

    net: tests: Add unit tests for neighbor cache handling
    Change-Id: I1518c102232a607a9c0f91bf5fd71e4b3183a419
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 4695e533914431e184a015c39598c55d500cbcd4

---
+++ tests/net/neighbor/src/main.c
@@ -0,0 +1,12 @@
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|add_count|variable|add_count
+DECL|clear_called|variable|clear_called
+DECL|hwaddr1|variable|hwaddr1
+DECL|hwaddr2|variable|hwaddr2
+DECL|hwaddr3|variable|hwaddr3
+DECL|hwaddr4|variable|hwaddr4
+DECL|hwaddr5|variable|hwaddr5
+DECL|mainloop|function|void mainloop(void)
+DECL|net_neighbor_data_remove|function|static void net_neighbor_data_remove(struct net_nbr *nbr)
+DECL|net_neighbor_table_clear|function|static void net_neighbor_table_clear(struct net_nbr_table *table)
+DECL|remove_count|variable|remove_count
