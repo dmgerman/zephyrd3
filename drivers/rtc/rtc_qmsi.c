70d9e0810e2dcd8b410d3dd93a0ecadd562ef372
Jithu Joseph <jithu.joseph@intel.com>
2016-06-01 20:13:47 -0700

    qmsi: rtc: Use locking to guard critical section
    Guard critical section of the driver API, so that
    multiple simultaneous calls from tasks/fibers wont
    corrupt the driver state.
    
    The locking mechanism is by default disabled in the
    build. To enable it, the following flag needs to be defined:
    
    CONFIG_RTC_QMSI_API_REENTRANCY
    
    Jira: ZEP-412
    
    Change-Id: I430ebc07bf3277e5a5961cb6278c5500cd1e139b
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: f271d7dd3e8e392b1b52f8a439b966e980acddab

--- drivers/rtc/rtc_qmsi.c
+++ drivers/rtc/rtc_qmsi.c
@@ -1,10 +1,20 @@
+DECL|RTC_CONTEXT|macro|RTC_CONTEXT
+DECL|RTC_CONTEXT|macro|RTC_CONTEXT
 DECL|api|variable|api
 DECL|int_rtc_mask_save|variable|int_rtc_mask_save
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|rtc_context|variable|rtc_context
+DECL|rtc_critical_region_end|function|static void rtc_critical_region_end(struct device *dev)
+DECL|rtc_critical_region_start|function|static void rtc_critical_region_start(struct device *dev)
+DECL|rtc_data|struct|struct rtc_data {
 DECL|rtc_qmsi_disable|function|static void rtc_qmsi_disable(struct device *dev)
 DECL|rtc_qmsi_enable|function|static void rtc_qmsi_enable(struct device *dev)
 DECL|rtc_qmsi_init|function|static int rtc_qmsi_init(struct device *dev)
 DECL|rtc_qmsi_read|function|static uint32_t rtc_qmsi_read(struct device *dev)
 DECL|rtc_qmsi_set_alarm|function|static int rtc_qmsi_set_alarm(struct device *dev, const uint32_t alarm_val)
 DECL|rtc_qmsi_set_config|function|static int rtc_qmsi_set_config(struct device *dev, struct rtc_config *cfg)
+DECL|rtc_reentrancy_init|function|static void rtc_reentrancy_init(struct device *dev)
 DECL|rtc_resume_device|function|static int rtc_resume_device(struct device *dev, int pm_policy)
 DECL|rtc_suspend_device|function|static int rtc_suspend_device(struct device *dev, int pm_policy)
+DECL|sem|member|struct nano_sem sem;
