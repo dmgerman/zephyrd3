a3c1e246b12796959e3c2e225bd5058989efc00d
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-06-01 20:57:59 -0700

    qmsi: gpio: Use locking mechanism to guard critical regions.
    This will guard the driver API to be safe for concurrent
    invocation from fibers or tasks.
    
    The machanism is by default disabled. To enable it, the
    following flag needs to be defined:
    
    CONFIG_GPIO_QMSI_API_REENTRANCY
    
    Jira: ZEP-441
    
    Change-Id: Ia3ee738aff2f8e70e4f9a7ec76346138ff6f5031
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: b4d87a575a58119fda861d40d0d9bfa2cecd1ae3

--- drivers/gpio/gpio_qmsi.c
+++ drivers/gpio/gpio_qmsi.c
@@ -1,3 +1,5 @@
+DECL|RP_GET|macro|RP_GET
+DECL|RP_GET|macro|RP_GET
 DECL|api_funcs|variable|api_funcs
 DECL|callbacks|member|sys_slist_t callbacks;
 DECL|gpio_0_config|variable|gpio_0_config
@@ -6,6 +8,8 @@ DECL|gpio_0|variable|gpio_0
 DECL|gpio_aon_config|variable|gpio_aon_config
 DECL|gpio_aon_runtime|variable|gpio_aon_runtime
 DECL|gpio_aon|variable|gpio_aon
+DECL|gpio_critical_region_end|function|static void gpio_critical_region_end(struct device *dev)
+DECL|gpio_critical_region_start|function|static void gpio_critical_region_start(struct device *dev)
 DECL|gpio_qmsi_0_int_callback|function|static void gpio_qmsi_0_int_callback(void *data, uint32_t status)
 DECL|gpio_qmsi_aon_int_callback|function|static void gpio_qmsi_aon_int_callback(void *data, uint32_t status)
 DECL|gpio_qmsi_callback|function|static void gpio_qmsi_callback(struct device *port, uint32_t status)
@@ -18,9 +22,13 @@ DECL|gpio_qmsi_manage_callback|function|static inline int gpio_qmsi_manage_callb
 DECL|gpio_qmsi_read|function|static inline int gpio_qmsi_read(struct device *port, int access_op, uint32_t pin, uint32_t *value)
 DECL|gpio_qmsi_runtime|struct|struct gpio_qmsi_runtime {
 DECL|gpio_qmsi_write|function|static inline int gpio_qmsi_write(struct device *port, int access_op, uint32_t pin, uint32_t value)
+DECL|gpio_reentrancy_init|function|static void gpio_reentrancy_init(struct device *dev)
 DECL|gpio|member|qm_gpio_t gpio;
 DECL|num_pins|member|uint8_t num_pins;
 DECL|pin_callbacks|member|uint32_t pin_callbacks;
 DECL|qmsi_pin_config|function|static inline void qmsi_pin_config(struct device *port, uint32_t pin, int flags)
 DECL|qmsi_port_config|function|static inline void qmsi_port_config(struct device *port, int flags)
 DECL|qmsi_write_bit|function|static void qmsi_write_bit(uint32_t *target, uint8_t bit, uint8_t value)
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|sem|member|struct nano_sem sem;
