22697aa99096b95ac7bd1b44462124a67428f5f8
Jithu Joseph <jithu.joseph@intel.com>
2016-06-03 19:33:39 -0700

    qmsi: wdt: improved reentrancy of the wdt driver
    Uses locking in an API and removes a global variable
    for improved safety/correctness in the face of concurrent
    threads and fibers.
    
    The locking mechanism is by default disabled in the
    build. To enable it, the following flag needs to be defined:
    
    CONFIG_WDT_QMSI_API_REENTRANCY
    
    Jira: ZEP-440
    Change-Id: Ibcd1501a4af628017b20d7e7ce20b988c8e4e4e2
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: 3dd008e0c26dce2dda6767ad42e56e2040d667f0

--- drivers/watchdog/wdt_qmsi.c
+++ drivers/watchdog/wdt_qmsi.c
@@ -1,9 +1,18 @@
+DECL|WDT_CONTEXT|macro|WDT_CONTEXT
+DECL|WDT_CONTEXT|macro|WDT_CONTEXT
 DECL|api|variable|api
 DECL|disable|function|static void disable(struct device *dev)
 DECL|enable|function|static void enable(struct device *dev)
 DECL|get_config|function|static void get_config(struct device *dev, struct wdt_config *cfg)
 DECL|init|function|static int init(struct device *dev)
-DECL|qm_cfg|variable|qm_cfg
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|reentrancy_protection|variable|reentrancy_protection
 DECL|reload|function|static void reload(struct device *dev)
+DECL|sem|member|struct nano_sem sem;
 DECL|set_config|function|static int set_config(struct device *dev, struct wdt_config *cfg)
 DECL|user_cb|variable|user_cb
+DECL|wdt_context|variable|wdt_context
+DECL|wdt_critical_region_end|function|static void wdt_critical_region_end(struct device *dev)
+DECL|wdt_critical_region_start|function|static void wdt_critical_region_start(struct device *dev)
+DECL|wdt_data|struct|struct wdt_data {
+DECL|wdt_reentrancy_init|function|static void wdt_reentrancy_init(struct device *dev)
