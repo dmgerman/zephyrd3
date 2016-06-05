1a384176ae0218d46a1babcfd61db72768bbd037
Johan Hedberg <johan.hedberg@intel.com>
2016-06-05 16:55:47 +0300

    net: buf: Introduce support for fragmentation
    Introduce support buffer fragment chains that are linked together.
    This is done with the help of a flag while the buffer is inside a FIFO
    (indicating that the next fragment follows it in the same FIFO) and
    with the help of a "next" pointer while the buffer is outside of a
    FIFO.
    
    In order to do proper "marshaling" a new net_buf_put() API needs to be
    always used when inserting a buffer into a FIFO. Respectively, the
    net_buf_get() and net_buf_get_timeout() functions are extended to
    support getting buffers from arbitrary FIFOs and reassemble the
    fragment chain based on the flags that the received buffers contain.
    
    The insertion of a fragment chain using net_buf_put() into a FIFO is
    done atomically with the help of irq_lock/unlock since FIFOs support
    multiple writers, however since there's ever only a single reader per
    FIFO similar locking is not necessary there.
    
    Change-Id: I0ec579f63ea8d063f50e3f1f4c2e80ec399622d7
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7720f6ed1c962094a69121065c95dc993c435caa

--- include/net/buf.h
+++ include/net/buf.h
@@ -1,3 +1,4 @@
+DECL|NET_BUF_FRAGS|macro|NET_BUF_FRAGS
 DECL|NET_BUF_POOL|macro|NET_BUF_POOL
 DECL|__NET_BUF_H|macro|__NET_BUF_H
 DECL|__net_buf_align|macro|__net_buf_align
@@ -5,6 +6,8 @@ DECL|__net_buf_align|member|uint8_t __buf[0] __net_buf_align;
 DECL|_unused|member|int _unused;
 DECL|data|member|uint8_t *data;
 DECL|destroy|member|void (*const destroy)(struct net_buf *buf);
+DECL|flags|member|uint8_t flags;
+DECL|frags|member|struct net_buf *frags;
 DECL|free|member|struct nano_fifo * const free;
 DECL|len|member|uint16_t len;
 DECL|net_buf_pool_init|macro|net_buf_pool_init
