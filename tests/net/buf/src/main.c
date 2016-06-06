e5c5b678ad437d88957088e42a0d38e43e9b5947
Johan Hedberg <johan.hedberg@intel.com>
2016-06-06 11:21:29 +0300

    net: tests: Refactor net_buf tests for easier extensibility
    Change-Id: I23380b1c78b5a9c8814c037a2d48b3505c99f6ea
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6d93cbb627574bfda3e570bedc1c0795f2a9eaa1

--- tests/net/buf/src/main.c
+++ tests/net/buf/src/main.c
@@ -3,7 +3,12 @@ DECL|bt_data|struct|struct bt_data {
 DECL|buf_destroy|function|static void buf_destroy(struct net_buf *buf)
 DECL|bufs_fifo|variable|bufs_fifo
 DECL|destroy_called|variable|destroy_called
+DECL|func|member|bool (*func)(void);
 DECL|hci_opcode|member|uint16_t hci_opcode;
 DECL|hci_sync|member|void *hci_sync;
 DECL|main|function|void main(void)
+DECL|name|member|const char *name;
+DECL|net_buf_test_1|function|static bool net_buf_test_1(void)
+DECL|net_buf_test_2|function|static bool net_buf_test_2(void)
+DECL|tests|variable|tests
 DECL|type|member|uint8_t type;
