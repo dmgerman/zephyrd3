b5f7bbdb90e4b1dc36f9706aa28605ad14aa748d
Kumar Gala <kumar.gala@linaro.org>
2016-06-07 14:15:23 -0500

    newlib: Support both namespace protected & nonprotected stubs
    Some variants of newlib build and expect namespace protected stubs
    (typically having an underscore prefix).  To support such newlib variants
    change all the stubs to the namespace protect version and use function
    aliases to support the nonprotected namespace version.
    
    Change-Id: I6a4162eca949afec96b152ffe6f60b87c4496c4d
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 7fdd4f758df224fad83573768b1cd34b645b870a

--- lib/libc/newlib/libc-hooks.c
+++ lib/libc/newlib/libc-hooks.c
@@ -5,20 +5,20 @@ DECL|USED_RAM_SIZE|macro|USED_RAM_SIZE
 DECL|USED_RAM_SIZE|macro|USED_RAM_SIZE
 DECL|__stdin_hook_install|function|void __stdin_hook_install(unsigned char (*hook)(void))
 DECL|__stdout_hook_install|function|void __stdout_hook_install(int (*hook)(int))
+DECL|_close|function|int _close(int file)
 DECL|_exit|function|void _exit(int status)
+DECL|_fstat|function|int _fstat(int file, struct stat *st)
+DECL|_getpid|function|int _getpid(void)
+DECL|_isatty|function|int _isatty(int file)
+DECL|_kill|function|int _kill(int i, int j)
+DECL|_lseek|function|int _lseek(int file, int ptr, int dir)
+DECL|_open|function|int _open(const char *name, int mode)
+DECL|_read|function|int _read(int fd, char *buf, int nbytes)
+DECL|_sbrk|function|void *_sbrk(int count)
 DECL|_stdin_hook_default|function|static unsigned char _stdin_hook_default(void)
 DECL|_stdin_hook|variable|_stdin_hook
 DECL|_stdout_hook_default|function|static int _stdout_hook_default(int c)
 DECL|_stdout_hook|variable|_stdout_hook
-DECL|close|function|int close(int file)
-DECL|fstat|function|int fstat(int file, struct stat *st)
-DECL|getpid|function|int getpid(void)
+DECL|_write|function|int _write(int fd, char *buf, int nbytes)
 DECL|heap_base|variable|heap_base
 DECL|heap_sz|variable|heap_sz
-DECL|isatty|function|int isatty(int file)
-DECL|kill|function|int kill(int i, int j)
-DECL|lseek|function|int lseek(int file, int ptr, int dir)
-DECL|open|function|int open(const char *name, int mode)
-DECL|read|function|int read(int fd, char *buf, int nbytes)
-DECL|sbrk|function|void *sbrk(int count)
-DECL|write|function|int write(int fd, char *buf, int nbytes)
