5cf49271efa3c47b2ec2157dbe138775052b6fc0
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-06-08 14:30:58 -0700

    pwm: Implement set period and duty cycle API
    This allow the PWM QMSI shim driver to implement the set duty cycle
    API, and correct the behavior of Set Value
    
    Jira: ZEP-69 ZEP-156 ZEP-158
    
    Change-Id: I47744958fed889116fbb5024343ea00f76ed7706
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 9852ee55732972a4c5fa03e2243e6defba7186be

--- drivers/pwm/pwm_qmsi.c
+++ drivers/pwm/pwm_qmsi.c
@@ -1,8 +1,17 @@
+DECL|DEFAULT_PERIOD|macro|DEFAULT_PERIOD
+DECL|HW_CLOCK_CYCLES_PER_USEC|macro|HW_CLOCK_CYCLES_PER_USEC
+DECL|MAX_HIGH_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_HIGH_PERIOD_IN_HW_CLOCK_CYCLES
+DECL|MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES
+DECL|MAX_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_PERIOD_IN_HW_CLOCK_CYCLES
+DECL|MAX_PERIOD|macro|MAX_PERIOD
+DECL|MIN_PERIOD|macro|MIN_PERIOD
 DECL|__set_one_port|function|static int __set_one_port(qm_pwm_t id, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|pwm_channel_period|variable|pwm_channel_period
 DECL|pwm_qmsi_configure|function|static int pwm_qmsi_configure(struct device *dev, int access_op, uint32_t pwm, int flags)
 DECL|pwm_qmsi_drv_api_funcs|variable|pwm_qmsi_drv_api_funcs
 DECL|pwm_qmsi_init|function|static int pwm_qmsi_init(struct device *dev)
 DECL|pwm_qmsi_resume|function|static int pwm_qmsi_resume(struct device *dev)
 DECL|pwm_qmsi_set_duty_cycle|function|static int pwm_qmsi_set_duty_cycle(struct device *dev, int access_op, uint32_t pwm, uint8_t duty)
+DECL|pwm_qmsi_set_period|function|static int pwm_qmsi_set_period(struct device *dev, int access_op, uint32_t pwm, uint32_t period)
 DECL|pwm_qmsi_set_values|function|static int pwm_qmsi_set_values(struct device *dev, int access_op, uint32_t pwm, uint32_t on, uint32_t off)
 DECL|pwm_qmsi_suspend|function|static int pwm_qmsi_suspend(struct device *dev)
