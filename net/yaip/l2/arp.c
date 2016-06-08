81eecba7f84d52f017bd49c5ebc84f0109d1f21d
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-08 14:13:27 +0300

    net: Add utility func to return eth broadcast addr
    As the broadcast address is needed in both arp.c and ethernet.c
    it is useful to create a utility function for returning that.
    
    Change-Id: I26e75e325f7ee6dcd703a92b3349bd8d388e157e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b4b296b8882d28ac834ef8efba5bb1efb542fb9d

--- net/yaip/l2/arp.c
+++ net/yaip/l2/arp.c
@@ -3,7 +3,6 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|arp_entry|struct|struct arp_entry {
 DECL|arp_table|variable|arp_table
 DECL|arp_update|function|static inline void arp_update(struct net_if *iface, struct in_addr *src, struct net_eth_addr *hwaddr)
-DECL|broadcast_eth_addr|variable|broadcast_eth_addr
 DECL|eth|member|struct net_eth_addr eth;
 DECL|find_entry|function|static inline struct arp_entry *find_entry(struct net_if *iface, struct in_addr *dst, struct arp_entry **free_entry, struct arp_entry **non_pending)
 DECL|if_get_addr|function|static inline struct in_addr *if_get_addr(struct net_if *iface)
--- net/yaip/l2/ethernet.c
+++ net/yaip/l2/ethernet.c
@@ -1,7 +1,9 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|broadcast_eth_addr|variable|broadcast_eth_addr
 DECL|ethernet_recv|function|static enum net_verdict ethernet_recv(struct net_if *iface, struct net_buf *buf)
 DECL|ethernet_send|function|static enum net_verdict ethernet_send(struct net_if *iface, struct net_buf *buf)
 DECL|get_reserve|function|static inline uint16_t get_reserve(struct net_if *iface)
+DECL|net_eth_broadcast_addr|function|const struct net_eth_addr *net_eth_broadcast_addr(void)
 DECL|print_ll_addrs|macro|print_ll_addrs
 DECL|print_ll_addrs|macro|print_ll_addrs
