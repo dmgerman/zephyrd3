b2720cf7b4dd09bafb9b5d2ddabc94f2ba2ca140
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-08 08:39:46 +0300

    net: Add debug support to ethernet L2 driver
    Change-Id: I53f35dcb1be25aed1a15ebf9322938bd75263047
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7bb273f8f27a076f87bd4064faeab31471ffe6bd

--- net/yaip/l2/ethernet.c
+++ net/yaip/l2/ethernet.c
@@ -1,3 +1,7 @@
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|ethernet_recv|function|static enum net_verdict ethernet_recv(struct net_if *iface, struct net_buf *buf)
 DECL|ethernet_send|function|static enum net_verdict ethernet_send(struct net_if *iface, struct net_buf *buf)
 DECL|get_reserve|function|static inline uint16_t get_reserve(struct net_if *iface)
+DECL|print_ll_addrs|macro|print_ll_addrs
+DECL|print_ll_addrs|macro|print_ll_addrs
