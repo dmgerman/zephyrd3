a99d829a805b3274a8fc6dca8d3d5523cca92838
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-06-09 12:17:07 +0200

    net: l2 layer reserve size might need extra parameter
    The dst_ip6 is a parameter, required for future 802.15.4 L2 layer.
    At some point such parameter could be changed to a void pointer,
    and a utility function would populate it depending on L2 layer,
    if such parameter needs someday to be variable and complex (some
    specific struct or else).
    
    Also make sure we use ARG_UNUSED() relevantly, and using proper prefix
    to functions.
    
    Change-Id: I43297bb4fb48a8f1bb5075c216342db16261cbb8
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: f7a9b07a84c48cf2fecc0167152d1eb7e6d4e87f

--- net/yaip/l2/ethernet.c
+++ net/yaip/l2/ethernet.c
@@ -2,8 +2,8 @@ DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|broadcast_eth_addr|variable|broadcast_eth_addr
 DECL|ethernet_recv|function|static enum net_verdict ethernet_recv(struct net_if *iface, struct net_buf *buf)
+DECL|ethernet_reserve|function|static inline uint16_t ethernet_reserve(struct net_if *iface, void *unused)
 DECL|ethernet_send|function|static enum net_verdict ethernet_send(struct net_if *iface, struct net_buf *buf)
-DECL|get_reserve|function|static inline uint16_t get_reserve(struct net_if *iface)
 DECL|multicast_eth_addr|variable|multicast_eth_addr
 DECL|net_eth_broadcast_addr|function|const struct net_eth_addr *net_eth_broadcast_addr(void)
 DECL|print_ll_addrs|macro|print_ll_addrs
