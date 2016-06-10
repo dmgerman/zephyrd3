74fc813628926eda94de09c4a864ae1c66fdb42d
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-06-10 15:49:22 -0400

    tests: test nano_fifo_put_list and nano_fifo_put_slist.
    Change-Id: I35ab267070126dcf7c8649ed79a7a4a5479b1f55
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 53c3106520e0ae21d34997813987ac66d6652075

--- tests/kernel/test_fifo/nanokernel/src/fifo.c
+++ tests/kernel/test_fifo/nanokernel/src/fifo.c
@@ -13,12 +13,16 @@ DECL|fiber3|function|void fiber3(void)
 DECL|fiberStack1|variable|fiberStack1
 DECL|fiberStack2|variable|fiberStack2
 DECL|fiberStack3|variable|fiberStack3
+DECL|fiber_list_0|function|void fiber_list_0(int a, int b)
+DECL|fiber_list_1|function|static void fiber_list_1(int a, int b)
+DECL|fifo_list|variable|fifo_list
 DECL|fifo_ptr|member|struct nano_fifo *fifo_ptr; /* FIFO */
 DECL|initNanoObjects|function|void initNanoObjects(void)
 DECL|isrFifoInfo|variable|isrFifoInfo
 DECL|isr_fifo_get|function|void isr_fifo_get(void *parameter)
 DECL|isr_fifo_info|struct|struct isr_fifo_info {
 DECL|isr_fifo_put|function|void isr_fifo_put(void *parameter)
+DECL|list|variable|list
 DECL|main|function|void main(void)
 DECL|myFifoData1|variable|myFifoData1
 DECL|myFifoData2|variable|myFifoData2
@@ -30,16 +34,23 @@ DECL|nanoSemObj1|variable|nanoSemObj1
 DECL|nanoSemObj2|variable|nanoSemObj2
 DECL|nanoSemObj3|variable|nanoSemObj3
 DECL|nanoSemObjTask|variable|nanoSemObjTask
+DECL|next|member|void *next;
+DECL|n|member|int n;
 DECL|pMyFifoData1|variable|pMyFifoData1
 DECL|pMyFifoData2|variable|pMyFifoData2
 DECL|pMyFifoData3|variable|pMyFifoData3
 DECL|pMyFifoData4|variable|pMyFifoData4
 DECL|pPutList1|variable|pPutList1
 DECL|pPutList2|variable|pPutList2
+DECL|packet_list|struct|struct packet_list {
+DECL|packets|variable|packets
 DECL|retCode|variable|retCode
+DECL|sem_list|variable|sem_list
+DECL|stacks_list|variable|stacks_list
 DECL|testFiberFifoGetW|function|void testFiberFifoGetW(void)
 DECL|testIsrFifoFromFiber|function|void testIsrFifoFromFiber(void)
 DECL|testIsrFifoFromTask|function|void testIsrFifoFromTask(void)
 DECL|testTaskFifoGetW|function|void testTaskFifoGetW(void)
+DECL|test_fifo_put_list|function|static void test_fifo_put_list(void)
 DECL|timerData|variable|timerData
 DECL|timer|variable|timer
