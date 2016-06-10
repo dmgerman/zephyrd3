e5ee86c41fd4e4bdf23d1acd76abb899c7723c0c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-10 09:08:29 +0300

    net: Set multicast dst address in ethernet if missing
    If ethernet destination address is not set, then check if
    IPv6 packet is multicast one and set the ethernet destination
    address using multicast MAC address prefix.
    
    Change-Id: Iade43e9ebcd0939346d89422f74ee547d6908866
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 976fb9d9884eb4d4858f5795f1ec67036985fefb

--- net/yaip/l2/ethernet.c
+++ net/yaip/l2/ethernet.c
@@ -4,6 +4,7 @@ DECL|broadcast_eth_addr|variable|broadcast_eth_addr
 DECL|ethernet_recv|function|static enum net_verdict ethernet_recv(struct net_if *iface, struct net_buf *buf)
 DECL|ethernet_send|function|static enum net_verdict ethernet_send(struct net_if *iface, struct net_buf *buf)
 DECL|get_reserve|function|static inline uint16_t get_reserve(struct net_if *iface)
+DECL|multicast_eth_addr|variable|multicast_eth_addr
 DECL|net_eth_broadcast_addr|function|const struct net_eth_addr *net_eth_broadcast_addr(void)
 DECL|print_ll_addrs|macro|print_ll_addrs
 DECL|print_ll_addrs|macro|print_ll_addrs
