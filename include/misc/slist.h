0182109b2baf392df78b88f1621ce0a4522c19b5
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-06-11 18:56:07 -0400

    slist: add sys_slist_get() to fetch and remove the head
    More straightforward than doing sys_slist_peek_head() followed by
    sys_slist_remove().
    
    Also add a version that does not check if the list is non-empty to be
    used when the list is known bo be non-empty.
    
    Change-Id: I8fd10e20e2c84c7d8972c9207f3d4917884808cb
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: e69982b8b5e8df2ef99b90ffc23e4ec90d9ac0cd

--- include/misc/slist.h
+++ include/misc/slist.h
@@ -7,6 +7,8 @@ DECL|head|member|sys_snode_t *head;
 DECL|next|member|struct _snode *next;
 DECL|sys_slist_append|function|static inline void sys_slist_append(sys_slist_t *list, sys_snode_t *node)
 DECL|sys_slist_find_and_remove|function|static inline void sys_slist_find_and_remove(sys_slist_t *list, sys_snode_t *node)
+DECL|sys_slist_get_not_empty|function|static inline sys_snode_t *sys_slist_get_not_empty(sys_slist_t *list)
+DECL|sys_slist_get|function|static inline sys_snode_t *sys_slist_get(sys_slist_t *list)
 DECL|sys_slist_init|function|static inline void sys_slist_init(sys_slist_t *list)
 DECL|sys_slist_insert|function|static inline void sys_slist_insert(sys_slist_t *list, sys_snode_t *prev, sys_snode_t *node)
 DECL|sys_slist_is_empty|function|static inline bool sys_slist_is_empty(sys_slist_t *list)
