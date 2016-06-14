ff045e4f9b0ebc715bed89fe858a49daa93b3610
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-14 09:42:01 +0300

    net: Add support for ICMPv6 error message
    Provide an API to send ICMPv6 error message. This can be used
    for example in UDP and TCP to inform peer that there is no one
    listening a certain port.
    
    Change-Id: Ied682901f7fc406ba383293cb7c338ea21114b0d
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9612f97e9488ee46aa6363d0f4b1f9da5b224afd

--- net/yaip/icmpv6.c
+++ net/yaip/icmpv6.c
@@ -1,4 +1,5 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|NET_ICMPV6_UNUSED_LEN|macro|NET_ICMPV6_UNUSED_LEN
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|echo_request_handler|variable|echo_request_handler
 DECL|handle_echo_request|function|static enum net_verdict handle_echo_request(struct net_buf *buf)
@@ -6,3 +7,5 @@ DECL|handlers|variable|handlers
 DECL|net_icmpv6_init|function|void net_icmpv6_init(void)
 DECL|net_icmpv6_input|function|enum net_verdict net_icmpv6_input(struct net_buf *buf, uint16_t len, uint8_t type, uint8_t code)
 DECL|net_icmpv6_register_handler|function|void net_icmpv6_register_handler(struct net_icmpv6_handler *handler)
+DECL|net_icmpv6_send_error|function|int net_icmpv6_send_error(struct net_buf *orig, uint8_t type, uint8_t code)
+DECL|setup_ipv6_header|function|static inline void setup_ipv6_header(struct net_buf *buf, uint8_t extra_len, uint8_t hop_limit, uint8_t icmp_type, uint8_t icmp_code)
