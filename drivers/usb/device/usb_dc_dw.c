def307d726ae5634f0933ef72b5cc74ff8b736ab
Adrian Bradianu <adrian.bradianu@windriver.com>
2016-06-14 13:23:39 +0100

    usb: dw: Add Quark SE USB device controller driver
    Quark SE USB device (Designware IP) controller driver implements
    the low level control routines to deal directly with the hardware.
    Only FIFO mode supported.
    
    Change-Id: I086186df017734579f0363ed79effc1481ff32c2
    Signed-off-by: Adrian Bradianu <adrian.bradianu@windriver.com>
    Signed-off-by: Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: ff712114efaf7ae05f3aa60828b673d8c83e94f6

---
+++ drivers/usb/device/usb_dc_dw.c
@@ -0,0 +1,50 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|USB_DW_EP_ADDR2DIR|macro|USB_DW_EP_ADDR2DIR
+DECL|USB_DW_EP_ADDR2IDX|macro|USB_DW_EP_ADDR2IDX
+DECL|USB_DW_EP_IDX2ADDR|macro|USB_DW_EP_IDX2ADDR
+DECL|USB_DW_SUP_CNT|macro|USB_DW_SUP_CNT
+DECL|_usb_dw_int_unmask|function|static inline void _usb_dw_int_unmask(void)
+DECL|attached|member|uint8_t attached;
+DECL|cb|member|usb_dc_ep_callback cb;/* Endpoint callback function */
+DECL|data_len|member|uint32_t data_len;
+DECL|ep_ena|member|uint8_t ep_ena;
+DECL|in_ep_ctrl|member|struct usb_ep_ctrl_prv in_ep_ctrl[USB_DW_IN_EP_NUM];
+DECL|mps|member|uint16_t mps; /* Max ep pkt size */
+DECL|out_ep_ctrl|member|struct usb_ep_ctrl_prv out_ep_ctrl[USB_DW_OUT_EP_NUM];
+DECL|status_cb|member|usb_dc_status_callback status_cb;
+DECL|usb_dc_attach|function|int usb_dc_attach(void)
+DECL|usb_dc_detach|function|int usb_dc_detach(void)
+DECL|usb_dc_ep_clear_stall|function|int usb_dc_ep_clear_stall(const uint8_t ep)
+DECL|usb_dc_ep_configure|function|int usb_dc_ep_configure(const struct usb_dc_ep_cfg_data * const ep_cfg)
+DECL|usb_dc_ep_disable|function|int usb_dc_ep_disable(const uint8_t ep)
+DECL|usb_dc_ep_enable|function|int usb_dc_ep_enable(const uint8_t ep)
+DECL|usb_dc_ep_flush|function|int usb_dc_ep_flush(const uint8_t ep)
+DECL|usb_dc_ep_halt|function|int usb_dc_ep_halt(const uint8_t ep)
+DECL|usb_dc_ep_is_stalled|function|int usb_dc_ep_is_stalled(const uint8_t ep, uint8_t *const stalled)
+DECL|usb_dc_ep_read|function|int usb_dc_ep_read(const uint8_t ep, uint8_t *const data,const uint32_t max_data_len, uint32_t * const read_bytes)
+DECL|usb_dc_ep_set_callback|function|int usb_dc_ep_set_callback(const uint8_t ep, const usb_dc_ep_callback cb)
+DECL|usb_dc_ep_set_stall|function|int usb_dc_ep_set_stall(const uint8_t ep)
+DECL|usb_dc_ep_write|function|int usb_dc_ep_write(const uint8_t ep, const uint8_t *const data,const uint32_t data_len, uint32_t * const ret_bytes)
+DECL|usb_dc_reset|function|int usb_dc_reset(void)
+DECL|usb_dc_set_address|function|int usb_dc_set_address(const uint8_t addr)
+DECL|usb_dc_set_status_callback|function|int usb_dc_set_status_callback(const usb_dc_status_callback cb)
+DECL|usb_dw_clock_disable|function|static void usb_dw_clock_disable(void)
+DECL|usb_dw_clock_enable|function|static int usb_dw_clock_enable(void)
+DECL|usb_dw_ctrl_prv|struct|struct usb_dw_ctrl_prv {
+DECL|usb_dw_ctrl|variable|usb_dw_ctrl
+DECL|usb_dw_ep_is_enabled|function|static uint8_t usb_dw_ep_is_enabled(uint8_t ep)
+DECL|usb_dw_ep_is_valid|function|static uint8_t usb_dw_ep_is_valid(uint8_t ep)
+DECL|usb_dw_ep_set|function|static int usb_dw_ep_set(uint8_t ep,uint32_t ep_mps, enum usb_dc_ep_type ep_type)
+DECL|usb_dw_handle_enum_done|function|static void usb_dw_handle_enum_done(void)
+DECL|usb_dw_handle_reset|function|static void usb_dw_handle_reset(void)
+DECL|usb_dw_init|function|static int usb_dw_init(void)
+DECL|usb_dw_isr_handler|function|static void usb_dw_isr_handler(void)
+DECL|usb_dw_prep_rx|function|static void usb_dw_prep_rx(const uint8_t ep, uint8_t setup)
+DECL|usb_dw_reg_dump|function|static void usb_dw_reg_dump(void)
+DECL|usb_dw_reg_dump|macro|usb_dw_reg_dump
+DECL|usb_dw_reset|function|static int usb_dw_reset(void)
+DECL|usb_dw_tx|function|static int usb_dw_tx(uint8_t ep, const uint8_t *const data,uint32_t data_len)
+DECL|usb_dw_udelay|function|static inline void usb_dw_udelay(uint32_t us)
+DECL|usb_ep_ctrl_prv|struct|struct usb_ep_ctrl_prv {
---
+++ drivers/usb/device/usb_dw_registers.h
@@ -0,0 +1,155 @@
+DECL|USB_DW_0|enumerator|enum USB_DW { USB_DW_0 = 0, USB_DW_NUM };
+DECL|USB_DW_BASE|macro|USB_DW_BASE
+DECL|USB_DW_CORE_RST_TIMEOUT_US|macro|USB_DW_CORE_RST_TIMEOUT_US
+DECL|USB_DW_DAINT_IN_EP_INT|macro|USB_DW_DAINT_IN_EP_INT
+DECL|USB_DW_DAINT_OUT_EP_INT|macro|USB_DW_DAINT_OUT_EP_INT
+DECL|USB_DW_DCFG_DEV_ADDR_MASK|macro|USB_DW_DCFG_DEV_ADDR_MASK
+DECL|USB_DW_DCFG_DEV_ADDR_OFFSET|macro|USB_DW_DCFG_DEV_ADDR_OFFSET
+DECL|USB_DW_DCFG_DEV_SPD_FS|macro|USB_DW_DCFG_DEV_SPD_FS
+DECL|USB_DW_DCFG_DEV_SPD_LS|macro|USB_DW_DCFG_DEV_SPD_LS
+DECL|USB_DW_DCTL_SFT_DISCON|macro|USB_DW_DCTL_SFT_DISCON
+DECL|USB_DW_DEPCTL0_MSP_16|macro|USB_DW_DEPCTL0_MSP_16
+DECL|USB_DW_DEPCTL0_MSP_32|macro|USB_DW_DEPCTL0_MSP_32
+DECL|USB_DW_DEPCTL0_MSP_64|macro|USB_DW_DEPCTL0_MSP_64
+DECL|USB_DW_DEPCTL0_MSP_8|macro|USB_DW_DEPCTL0_MSP_8
+DECL|USB_DW_DEPCTL0_MSP_MASK|macro|USB_DW_DEPCTL0_MSP_MASK
+DECL|USB_DW_DEPCTL_CNAK|macro|USB_DW_DEPCTL_CNAK
+DECL|USB_DW_DEPCTL_EP_DIS|macro|USB_DW_DEPCTL_EP_DIS
+DECL|USB_DW_DEPCTL_EP_ENA|macro|USB_DW_DEPCTL_EP_ENA
+DECL|USB_DW_DEPCTL_EP_TYPE_BULK|macro|USB_DW_DEPCTL_EP_TYPE_BULK
+DECL|USB_DW_DEPCTL_EP_TYPE_CONTROL|macro|USB_DW_DEPCTL_EP_TYPE_CONTROL
+DECL|USB_DW_DEPCTL_EP_TYPE_INTERRUPT|macro|USB_DW_DEPCTL_EP_TYPE_INTERRUPT
+DECL|USB_DW_DEPCTL_EP_TYPE_ISO|macro|USB_DW_DEPCTL_EP_TYPE_ISO
+DECL|USB_DW_DEPCTL_EP_TYPE_MASK|macro|USB_DW_DEPCTL_EP_TYPE_MASK
+DECL|USB_DW_DEPCTL_EP_TYPE_OFFSET|macro|USB_DW_DEPCTL_EP_TYPE_OFFSET
+DECL|USB_DW_DEPCTL_MSP_OFFSET|macro|USB_DW_DEPCTL_MSP_OFFSET
+DECL|USB_DW_DEPCTL_SETDOPID|macro|USB_DW_DEPCTL_SETDOPID
+DECL|USB_DW_DEPCTL_SNAK|macro|USB_DW_DEPCTL_SNAK
+DECL|USB_DW_DEPCTL_STALL|macro|USB_DW_DEPCTL_STALL
+DECL|USB_DW_DEPCTL_USB_ACT_EP|macro|USB_DW_DEPCTL_USB_ACT_EP
+DECL|USB_DW_DEPCTLn_MSP_MASK|macro|USB_DW_DEPCTLn_MSP_MASK
+DECL|USB_DW_DEPTSIZ0_XFER_SIZE_MASK|macro|USB_DW_DEPTSIZ0_XFER_SIZE_MASK
+DECL|USB_DW_DEPTSIZ_PKT_CNT_OFFSET|macro|USB_DW_DEPTSIZ_PKT_CNT_OFFSET
+DECL|USB_DW_DEPTSIZ_XFER_SIZE_OFFSET|macro|USB_DW_DEPTSIZ_XFER_SIZE_OFFSET
+DECL|USB_DW_DEPTSIZn_XFER_SIZE_MASK|macro|USB_DW_DEPTSIZn_XFER_SIZE_MASK
+DECL|USB_DW_DIEPINT_TX_FEMP|macro|USB_DW_DIEPINT_TX_FEMP
+DECL|USB_DW_DIEPINT_XFER_COMPL|macro|USB_DW_DIEPINT_XFER_COMPL
+DECL|USB_DW_DIEPINT_XFER_COMPL|macro|USB_DW_DIEPINT_XFER_COMPL
+DECL|USB_DW_DIEPTSIZ0_PKT_CNT_MASK|macro|USB_DW_DIEPTSIZ0_PKT_CNT_MASK
+DECL|USB_DW_DIEPTSIZn_PKT_CNT_MASK|macro|USB_DW_DIEPTSIZn_PKT_CNT_MASK
+DECL|USB_DW_DOEPINT_SET_UP|macro|USB_DW_DOEPINT_SET_UP
+DECL|USB_DW_DOEPINT_XFER_COMPL|macro|USB_DW_DOEPINT_XFER_COMPL
+DECL|USB_DW_DOEPTSIZ0_PKT_CNT_MASK|macro|USB_DW_DOEPTSIZ0_PKT_CNT_MASK
+DECL|USB_DW_DOEPTSIZ_SUP_CNT_MASK|macro|USB_DW_DOEPTSIZ_SUP_CNT_MASK
+DECL|USB_DW_DOEPTSIZ_SUP_CNT_OFFSET|macro|USB_DW_DOEPTSIZ_SUP_CNT_OFFSET
+DECL|USB_DW_DOEPTSIZn_PKT_CNT_MASK|macro|USB_DW_DOEPTSIZn_PKT_CNT_MASK
+DECL|USB_DW_DSTS_ENUM_FS|macro|USB_DW_DSTS_ENUM_FS
+DECL|USB_DW_DSTS_ENUM_LS|macro|USB_DW_DSTS_ENUM_LS
+DECL|USB_DW_DSTS_ENUM_SPD_MASK|macro|USB_DW_DSTS_ENUM_SPD_MASK
+DECL|USB_DW_DSTS_ENUM_SPD_OFFSET|macro|USB_DW_DSTS_ENUM_SPD_OFFSET
+DECL|USB_DW_DTXFSTS_TXF_SPC_AVAIL_MASK|macro|USB_DW_DTXFSTS_TXF_SPC_AVAIL_MASK
+DECL|USB_DW_EP_FIFO|macro|USB_DW_EP_FIFO
+DECL|USB_DW_GAHBCFG_DMA_EN|macro|USB_DW_GAHBCFG_DMA_EN
+DECL|USB_DW_GAHBCFG_GLB_INTR_MASK|macro|USB_DW_GAHBCFG_GLB_INTR_MASK
+DECL|USB_DW_GINTSTS_ENUM_DONE|macro|USB_DW_GINTSTS_ENUM_DONE
+DECL|USB_DW_GINTSTS_IEP_INT|macro|USB_DW_GINTSTS_IEP_INT
+DECL|USB_DW_GINTSTS_OEP_INT|macro|USB_DW_GINTSTS_OEP_INT
+DECL|USB_DW_GINTSTS_OTG_INT|macro|USB_DW_GINTSTS_OTG_INT
+DECL|USB_DW_GINTSTS_RX_FLVL|macro|USB_DW_GINTSTS_RX_FLVL
+DECL|USB_DW_GINTSTS_USB_RST|macro|USB_DW_GINTSTS_USB_RST
+DECL|USB_DW_GINTSTS_USB_SUSP|macro|USB_DW_GINTSTS_USB_SUSP
+DECL|USB_DW_GINTSTS_WK_UP_INT|macro|USB_DW_GINTSTS_WK_UP_INT
+DECL|USB_DW_GRSTCTL_AHB_IDLE|macro|USB_DW_GRSTCTL_AHB_IDLE
+DECL|USB_DW_GRSTCTL_C_SFT_RST|macro|USB_DW_GRSTCTL_C_SFT_RST
+DECL|USB_DW_GRSTCTL_TX_FFLSH|macro|USB_DW_GRSTCTL_TX_FFLSH
+DECL|USB_DW_GRSTCTL_TX_FNUM_OFFSET|macro|USB_DW_GRSTCTL_TX_FNUM_OFFSET
+DECL|USB_DW_GRXSTSR_EP_NUM_MASK|macro|USB_DW_GRXSTSR_EP_NUM_MASK
+DECL|USB_DW_GRXSTSR_PKT_CNT_MASK|macro|USB_DW_GRXSTSR_PKT_CNT_MASK
+DECL|USB_DW_GRXSTSR_PKT_CNT_OFFSET|macro|USB_DW_GRXSTSR_PKT_CNT_OFFSET
+DECL|USB_DW_GRXSTSR_PKT_STS_MASK|macro|USB_DW_GRXSTSR_PKT_STS_MASK
+DECL|USB_DW_GRXSTSR_PKT_STS_OFFSET|macro|USB_DW_GRXSTSR_PKT_STS_OFFSET
+DECL|USB_DW_GRXSTSR_PKT_STS_OUT_DATA_DONE|macro|USB_DW_GRXSTSR_PKT_STS_OUT_DATA_DONE
+DECL|USB_DW_GRXSTSR_PKT_STS_OUT_DATA|macro|USB_DW_GRXSTSR_PKT_STS_OUT_DATA
+DECL|USB_DW_GRXSTSR_PKT_STS_SETUP_DONE|macro|USB_DW_GRXSTSR_PKT_STS_SETUP_DONE
+DECL|USB_DW_GRXSTSR_PKT_STS_SETUP|macro|USB_DW_GRXSTSR_PKT_STS_SETUP
+DECL|USB_DW_IN_EP_0|enumerator|USB_DW_IN_EP_0 = 0,
+DECL|USB_DW_IN_EP_1|enumerator|USB_DW_IN_EP_1,
+DECL|USB_DW_IN_EP_2|enumerator|USB_DW_IN_EP_2,
+DECL|USB_DW_IN_EP_3|enumerator|USB_DW_IN_EP_3,
+DECL|USB_DW_IN_EP_4|enumerator|USB_DW_IN_EP_4,
+DECL|USB_DW_IN_EP_5|enumerator|USB_DW_IN_EP_5,
+DECL|USB_DW_IN_EP_NUM|enumerator|USB_DW_IN_EP_NUM
+DECL|USB_DW_IRQ|macro|USB_DW_IRQ
+DECL|USB_DW_NUM|enumerator|enum USB_DW { USB_DW_0 = 0, USB_DW_NUM };
+DECL|USB_DW_OUT_EP_0|enumerator|USB_DW_OUT_EP_0 = 0,
+DECL|USB_DW_OUT_EP_1|enumerator|USB_DW_OUT_EP_1,
+DECL|USB_DW_OUT_EP_2|enumerator|USB_DW_OUT_EP_2,
+DECL|USB_DW_OUT_EP_3|enumerator|USB_DW_OUT_EP_3,
+DECL|USB_DW_OUT_EP_NUM|enumerator|USB_DW_OUT_EP_NUM
+DECL|USB_DW_PLL_TIMEOUT_US|macro|USB_DW_PLL_TIMEOUT_US
+DECL|USB_DW|enum|enum USB_DW { USB_DW_0 = 0, USB_DW_NUM };
+DECL|USB_DW|macro|USB_DW
+DECL|__USB_DW_REGISTERS_H__|macro|__USB_DW_REGISTERS_H__
+DECL|daintmsk|member|volatile uint32_t daintmsk;
+DECL|daint|member|volatile uint32_t daint;
+DECL|dcfg|member|volatile uint32_t dcfg;
+DECL|dctl|member|volatile uint32_t dctl;
+DECL|diepctl|member|volatile uint32_t diepctl;
+DECL|diepdma|member|volatile uint32_t diepdma;
+DECL|diepempmsk|member|volatile uint32_t diepempmsk;
+DECL|diepint|member|volatile uint32_t diepint;
+DECL|diepmsk|member|volatile uint32_t diepmsk;
+DECL|dieptsiz|member|volatile uint32_t dieptsiz;
+DECL|dieptxf1|member|volatile uint32_t dieptxf1;
+DECL|dieptxf2|member|volatile uint32_t dieptxf2;
+DECL|dieptxf3|member|volatile uint32_t dieptxf3;
+DECL|dieptxf4|member|volatile uint32_t dieptxf4;
+DECL|dieptxf5|member|volatile uint32_t dieptxf5;
+DECL|doepctl|member|volatile uint32_t doepctl;
+DECL|doepdma|member|volatile uint32_t doepdma;
+DECL|doepint|member|volatile uint32_t doepint;
+DECL|doepmsk|member|volatile uint32_t doepmsk;
+DECL|doeptsiz|member|volatile uint32_t doeptsiz;
+DECL|dsts|member|volatile uint32_t dsts;
+DECL|dthrctl|member|volatile uint32_t dthrctl;
+DECL|dtxfsts|member|volatile uint32_t dtxfsts;
+DECL|dvbusdis|member|volatile uint32_t dvbusdis;
+DECL|dvbuspulse|member|volatile uint32_t dvbuspulse;
+DECL|gahbcfg|member|volatile uint32_t gahbcfg;
+DECL|gdfifocfg|member|volatile uint32_t gdfifocfg;
+DECL|ghwcfg1|member|volatile uint32_t ghwcfg1;
+DECL|ghwcfg2|member|volatile uint32_t ghwcfg2;
+DECL|ghwcfg3|member|volatile uint32_t ghwcfg3;
+DECL|ghwcfg4|member|volatile uint32_t ghwcfg4;
+DECL|gintmsk|member|volatile uint32_t gintmsk;
+DECL|gintsts|member|volatile uint32_t gintsts;
+DECL|gnptxfsiz|member|volatile uint32_t gnptxfsiz;
+DECL|gotgctl|member|volatile uint32_t gotgctl;
+DECL|gotgint|member|volatile uint32_t gotgint;
+DECL|grstctl|member|volatile uint32_t grstctl;
+DECL|grxfsiz|member|volatile uint32_t grxfsiz;
+DECL|grxstsp|member|volatile uint32_t grxstsp;
+DECL|grxstsr|member|volatile uint32_t grxstsr;
+DECL|gsnpsid|member|volatile uint32_t gsnpsid;
+DECL|gusbcfg|member|volatile uint32_t gusbcfg;
+DECL|in_ep_reg|member|struct usb_dw_in_ep_reg in_ep_reg[USB_DW_IN_EP_NUM];
+DECL|out_ep_reg|member|struct usb_dw_out_ep_reg out_ep_reg[USB_DW_OUT_EP_NUM];
+DECL|reserved1|member|uint32_t reserved1;
+DECL|reserved1|member|uint32_t reserved1;
+DECL|reserved1|member|uint32_t reserved1[43];
+DECL|reserved2|member|uint32_t reserved2;
+DECL|reserved2|member|uint32_t reserved2;
+DECL|reserved2|member|uint32_t reserved2[442];
+DECL|reserved3|member|uint32_t reserved3;
+DECL|reserved3|member|uint32_t reserved3;
+DECL|reserved4|member|uint32_t reserved4[2];
+DECL|reserved5|member|uint32_t reserved5[50];
+DECL|reserved6|member|uint32_t reserved6[80];
+DECL|reserved|member|uint32_t reserved;
+DECL|reserved|member|uint32_t reserved;
+DECL|reserved|member|uint32_t reserved[5];
+DECL|usb_dw_in_ep_idx|enum|enum usb_dw_in_ep_idx {
+DECL|usb_dw_in_ep_reg|struct|struct usb_dw_in_ep_reg {
+DECL|usb_dw_out_ep_idx|enum|enum usb_dw_out_ep_idx {
+DECL|usb_dw_out_ep_reg|struct|struct usb_dw_out_ep_reg {
+DECL|usb_dw_reg|struct|struct usb_dw_reg {
