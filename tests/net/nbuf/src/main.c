59c957090a74d11ba2809f14d65bc7b8eec87b48
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-14 09:29:23 +0300

    net: tests: Add unit tests for net_nbuf_copy()
    The tests will make sure net_nbuf_copy() works as expected.
    
    Change-Id: I48dc3b794c70ec4d6436feb76508952c88bbf09b
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: f923adb03811ed1a208d562237721c188ff0b94a

--- tests/net/nbuf/src/main.c
+++ tests/net/nbuf/src/main.c
@@ -1,18 +1,29 @@
 DECL|LL_RESERVE|macro|LL_RESERVE
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|buf_copy|variable|buf_copy
+DECL|buf_orig|variable|buf_orig
 DECL|chksum|member|uint16_t chksum;
+DECL|chksum|member|uint16_t chksum;
+DECL|code|member|uint8_t code;
 DECL|dst_port|member|uint16_t dst_port;
 DECL|dst|member|struct in6_addr dst;
 DECL|example_data|variable|example_data
 DECL|flow|member|uint16_t flow;
 DECL|hop_limit|member|uint8_t hop_limit;
+DECL|icmp_hdr|struct|struct icmp_hdr {
 DECL|ipv6_hdr|struct|struct ipv6_hdr {
 DECL|len|member|uint16_t len;
 DECL|len|member|uint8_t len[2];
+DECL|linearize|function|static void linearize(struct net_buf *buf, char *buffer, int len)
 DECL|mainloop|function|void mainloop(void)
 DECL|nexthdr|member|uint8_t nexthdr;
 DECL|src_port|member|uint16_t src_port;
 DECL|src|member|struct in6_addr src;
 DECL|tcflow|member|uint8_t tcflow;
+DECL|test_fragment_copy|function|static int test_fragment_copy(void)
 DECL|test_ipv6_multi_frags|function|static int test_ipv6_multi_frags(void)
+DECL|type|member|uint8_t type;
 DECL|udp_hdr|struct|struct udp_hdr {
 DECL|vtc|member|uint8_t vtc;
