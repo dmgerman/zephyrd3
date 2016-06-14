20c4dd4a8f6a11e1d9cf1a15687c07692a8dc0f3
Baohong Liu <baohong.liu@intel.com>
2016-06-14 13:37:02 -0700

    samples: Add DMA memory to memory transfer sample app
    Add a sample app to show and test how the DMA memory
    to memory transfer work.
    
    Origin: Original
    
    Change-Id: I0ff6790d9ba3e188cd2ad93d4c9a9c2405a84ac8
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 0096a18d5e37a719052056e30bccffd2b8f50f97

---
+++ samples/drivers/dma/src/dma.c
@@ -0,0 +1,13 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|DMA_DEVICE_NAME|macro|DMA_DEVICE_NAME
+DECL|RX_BUFF_SIZE|macro|RX_BUFF_SIZE
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|TRANSFER_LOOPS|macro|TRANSFER_LOOPS
+DECL|main|function|void main(void)
+DECL|rx_data|variable|rx_data
+DECL|test_error|function|static void test_error(struct device *dev, void *data)
+DECL|test_transfer|function|static void test_transfer(struct device *dev, void *data)
+DECL|transfer_count|variable|transfer_count
+DECL|tx_data|variable|tx_data
