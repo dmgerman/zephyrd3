4674012b518784478c661fd682ed83e817c4a557
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-06-14 10:52:57 +0200

    net: Add new IEEE 802.15.4 Radio API for device drivers
    This new radio API will be provided by 802.15.4 device drivers in order
    to fit in the new IEEE 802.15.4 stack within native IP stack.
    
    Change-Id: Ib28e8dd3f25e5c802284568b88c2ea6d5cf58f89
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: c3ce028d3b4b5be6c0224e68ebd67d143ceb837e

---
+++ include/net/yaip/ieee802154_radio.h
@@ -0,0 +1,13 @@
+DECL|__IEEE802154_RADIO_H__|macro|__IEEE802154_RADIO_H__
+DECL|__packed|variable|__packed
+DECL|cca|member|int (*cca)(struct device *dev);
+DECL|ieee802154_radio_api|struct|struct ieee802154_radio_api {
+DECL|iface_api|member|struct net_if_api iface_api;
+DECL|set_channel|member|int (*set_channel)(struct device *dev, uint16_t channel);
+DECL|set_ieee_addr|member|int (*set_ieee_addr)(struct device *dev, const uint8_t *ieee_addr);
+DECL|set_pan_id|member|int (*set_pan_id)(struct device *dev, uint16_t pan_id);
+DECL|set_short_addr|member|int (*set_short_addr)(struct device *dev, uint16_t short_addr);
+DECL|set_txpower|member|int (*set_txpower)(struct device *dev, int16_t dbm);
+DECL|start|member|int (*start)(struct device *dev);
+DECL|stop|member|int (*stop)(struct device *dev);
+DECL|tx|member|int (*tx)(struct device *dev, struct net_buf *buf);
