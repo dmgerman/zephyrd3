2e0fadaa4de7def2390c7102d48ad3f63c7e7467
Ramesh Thomas <ramesh.thomas@intel.com>
2016-06-14 14:01:59 -0700

    fs: Add a sample app to demo Zephyr file system API
    Demonstrates the Zephyr file system APIs. This currently
    runs on FAT FS using RAM emulation of disk.
    
    Origin: Original
    Jira: ZEP-285 ZEP-228 ZEP-481 ZEP-446
    Change-Id: If459b8c2f819a2a38c8c4d805ec3f689dfcdef17
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 939460521116984c67c0cce2168817ddbd08e2f5

---
+++ samples/fs/fat_fs/src/main.c
@@ -0,0 +1,15 @@
+DECL|TEST_FILE|macro|TEST_FILE
+DECL|TEST_SUB_DIR|macro|TEST_SUB_DIR
+DECL|check_file_dir_exists|function|static int check_file_dir_exists(const char *path)
+DECL|close_file|function|static int close_file(ZFILE *fp, const char *path)
+DECL|create_dir|function|static int create_dir(const char *path)
+DECL|delete_test|function|static int delete_test(const char *path)
+DECL|dir_tests|function|static void dir_tests(void)
+DECL|file_tests|function|static void file_tests(void)
+DECL|list_dir|function|static int list_dir(const char *path)
+DECL|main|function|void main(void)
+DECL|open_file|function|static int open_file(ZFILE *fp, const char *path)
+DECL|read_test|function|static int read_test(ZFILE *fp, off_t ofs, size_t sz, char *read_buff)
+DECL|remove_dir|function|static int remove_dir(const char *path)
+DECL|test_str|variable|test_str
+DECL|write_test|function|static int write_test(ZFILE *fp, off_t ofs, const char *str)
