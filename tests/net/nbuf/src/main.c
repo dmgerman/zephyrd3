15fd0128b524842316e522525ec6c0be2b685f42
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-15 14:39:14 +0300

    net: tests: Unit tests for net_nbuf_push()
    These tests will verify that we can successfully insert
    some space in front of the net_buf fragment chain.
    
    Change-Id: Idf52ed56c52d66268783fb4fecd44b70fc852734
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: f42c8126ebf350968d084aaef49d56d6befed5c9

--- tests/net/nbuf/src/main.c
+++ tests/net/nbuf/src/main.c
@@ -1,3 +1,4 @@
+DECL|FRAG_COUNT|macro|FRAG_COUNT
 DECL|LL_RESERVE|macro|LL_RESERVE
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
@@ -9,8 +10,10 @@ DECL|chksum|member|uint16_t chksum;
 DECL|code|member|uint8_t code;
 DECL|dst_port|member|uint16_t dst_port;
 DECL|dst|member|struct in6_addr dst;
+DECL|empty_data|variable|empty_data
 DECL|example_data|variable|example_data
 DECL|flow|member|uint16_t flow;
+DECL|hexdump|function|static void hexdump(const char *str, const uint8_t *packet, size_t length)
 DECL|hop_limit|member|uint8_t hop_limit;
 DECL|icmp_hdr|struct|struct icmp_hdr {
 DECL|ipv6_hdr|struct|struct ipv6_hdr {
@@ -22,7 +25,9 @@ DECL|nexthdr|member|uint8_t nexthdr;
 DECL|src_port|member|uint16_t src_port;
 DECL|src|member|struct in6_addr src;
 DECL|tcflow|member|uint8_t tcflow;
+DECL|test_data|variable|test_data
 DECL|test_fragment_copy|function|static int test_fragment_copy(void)
+DECL|test_fragment_push|function|static int test_fragment_push(void)
 DECL|test_ipv6_multi_frags|function|static int test_ipv6_multi_frags(void)
 DECL|type|member|uint8_t type;
 DECL|udp_hdr|struct|struct udp_hdr {
