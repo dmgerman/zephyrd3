c4a53164fd49f8b55efa431c6f8f417b4e6d8090
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-06-16 18:04:55 +0200

    net: Adding ALOHA radio protocol to IEEE 802.15.4 L2 driver
    This is the simplest - if not the dumbest - radio protocol we can use
    for IEEE 802.15.4 stack. It just transmsits right away (with optional
    retry) without taking care of doing any CCA (Clear Channel Assesment).
    
    See IEEE 802.15.4 specification document, part 4.5.4.2
    
    Change-Id: I341ad197aa221bc8f9ec67d0fd523da294a0351b
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 0624199e1f3c0c70a43566376e9289841baccedb

--- include/net/yaip/ieee802154.h
+++ include/net/yaip/ieee802154.h
@@ -1,5 +1,7 @@
 DECL|__IEEE802154_H__|macro|__IEEE802154_H__
 DECL|__packed|variable|__packed
+DECL|ack_lock|member|struct nano_sem ack_lock;
+DECL|ack_received|member|bool ack_received;
 DECL|channel|member|uint16_t channel;
 DECL|ieee802154_context|struct|struct ieee802154_context {
 DECL|pan_id|member|uint16_t pan_id;
