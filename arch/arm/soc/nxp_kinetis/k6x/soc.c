af963d5d3b170591e7cc56057221b0b06b466c22
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-06-16 17:33:47 +0100

    eth: Add KSDK ENET driver.
    Provide a network driver wrapped around the KSDK ENET and PHY
    drivers.
    
    The driver performs one shot PHY setup.  There is no support for PHY
    disconnect, reconnect or configuration change.  The PHY setup,
    implement via KSDK contains polled code that can block the
    initialization thread for a few seconds.
    
    There is no statistics collection for either normal operation or error
    behaviour.
    
    Origin: Original
    
    Change-Id: Ia0f2e89a61348ed949976070353e823c178fcb24
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 87077727bf88351f4edf43d4007340012c2ebd61

--- arch/arm/soc/nxp_kinetis/k6x/soc.c
+++ arch/arm/soc/nxp_kinetis/k6x/soc.c
@@ -4,6 +4,7 @@ DECL|ER32KSEL_RTC|macro|ER32KSEL_RTC
 DECL|PLLFLLSEL_IRC48MHZ|macro|PLLFLLSEL_IRC48MHZ
 DECL|PLLFLLSEL_MCGFLLCLK|macro|PLLFLLSEL_MCGFLLCLK
 DECL|PLLFLLSEL_MCGPLLCLK|macro|PLLFLLSEL_MCGPLLCLK
+DECL|TIMESRC_OSCERCLK|macro|TIMESRC_OSCERCLK
 DECL|__security_frdm_k64f|variable|__security_frdm_k64f
 DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
 DECL|fsl_frdm_k64f_init|function|static int fsl_frdm_k64f_init(struct device *arg)
---
+++ drivers/ethernet/eth_ksdk.c
@@ -0,0 +1,18 @@
+DECL|ETH_KSDK_BUFFER_SIZE|macro|ETH_KSDK_BUFFER_SIZE
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|enet_handle|member|enet_handle_t enet_handle;
+DECL|eth_0_config_func|function|static void eth_0_config_func(void)
+DECL|eth_0_context|variable|eth_0_context
+DECL|eth_0_init|function|static int eth_0_init(struct device *dev)
+DECL|eth_callback|function|void eth_callback(ENET_Type *base, enet_handle_t *handle, enet_event_t event, void *param)
+DECL|eth_context|struct|struct eth_context {
+DECL|eth_ksdk_error_isr|function|static void eth_ksdk_error_isr(void *p)
+DECL|eth_ksdk_rx_isr|function|static void eth_ksdk_rx_isr(void *p)
+DECL|eth_ksdk_tx_isr|function|static void eth_ksdk_tx_isr(void *p)
+DECL|eth_net_tx|function|static int eth_net_tx(struct net_buf *buf)
+DECL|eth_rx|function|static void eth_rx(struct device *iface)
+DECL|eth_tx|function|static int eth_tx(struct device *iface, struct net_buf *buf)
+DECL|generate_mac|function|static void generate_mac(uint8_t *mac_addr)
+DECL|mac_addr|member|uint8_t mac_addr[6];
+DECL|tx_buf_sem|member|struct nano_sem tx_buf_sem;
