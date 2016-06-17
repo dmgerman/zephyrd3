6f67e314035654a19fceb33d300055e96f15fb6c
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-06-17 19:22:15 -0300

    samples/net: Add a sample for a CoAP server
    Change-Id: Ibca646d9c9f85a14939e1cf488eee7a41f688fd2
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 97ea9b19ea992a5a745abd9e6009a3afeb05ed73

---
+++ samples/net/zoap_server/src/zoap-server.c
@@ -0,0 +1,10 @@
+DECL|ALL_NODES_LOCAL_COAP_MCAST|macro|ALL_NODES_LOCAL_COAP_MCAST
+DECL|MY_COAP_PORT|macro|MY_COAP_PORT
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|context|variable|context
+DECL|fiberStack|variable|fiberStack
+DECL|main|function|void main(void)
+DECL|resources|variable|resources
+DECL|test_get|function|static int test_get(struct zoap_resource *resource, struct zoap_packet *request, const void *from)
+DECL|test_path|variable|test_path
+DECL|udp_receive|function|static void udp_receive(void)
