5c3f3931a434fa96fac89d003ce0b000821f4ba9
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-06-17 19:20:42 -0300

    tests: Add simple CoAP tests
    Change-Id: I3d7b11ac5a623eab8e07d484b60c24c8a9a824f1
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 3a53a73d8fb49a726c4c76ae1d5d1792fa5141a7

---
+++ tests/net/zoap/src/main.c
@@ -0,0 +1,13 @@
+DECL|ZOAP_BUF_SIZE|macro|ZOAP_BUF_SIZE
+DECL|ZOAP_LIMITED_BUF_SIZE|macro|ZOAP_LIMITED_BUF_SIZE
+DECL|func|member|int (*func)(void);
+DECL|main|function|int main(int argc, char *argv[])
+DECL|name|member|const char *name;
+DECL|test_build_empty_pdu|function|static int test_build_empty_pdu(void)
+DECL|test_build_no_size_for_options|function|static int test_build_no_size_for_options(void)
+DECL|test_build_simple_pdu|function|static int test_build_simple_pdu(void)
+DECL|test_parse_empty_pdu|function|static int test_parse_empty_pdu(void)
+DECL|test_parse_simple_pdu|function|static int test_parse_simple_pdu(void)
+DECL|tests|variable|tests
+DECL|zoap_fifo|variable|zoap_fifo
+DECL|zoap_limited_fifo|variable|zoap_limited_fifo
