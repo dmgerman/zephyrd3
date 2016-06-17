0e023b4999330abb461d35a6d53bcb440884544d
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-06-17 19:21:33 -0300

    samples/net: Add a sample for a CoAP client
    Change-Id: I17d88081eb719e55d97a5a034f6861e01ebb6a52
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 8dad0093d32941a1ef225cbd0395c615c1961974

---
+++ samples/net/zoap_client/src/zoap-client.c
@@ -0,0 +1,17 @@
+DECL|ALL_NODES_LOCAL_COAP_MCAST|macro|ALL_NODES_LOCAL_COAP_MCAST
+DECL|MY_COAP_PORT|macro|MY_COAP_PORT
+DECL|NUM_PENDINGS|macro|NUM_PENDINGS
+DECL|NUM_REPLIES|macro|NUM_REPLIES
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|a_light_path|variable|a_light_path
+DECL|fiberStack|variable|fiberStack
+DECL|main|function|void main(void)
+DECL|msg_dump|function|static void msg_dump(const char *s, uint8_t *data, unsigned len)
+DECL|pendings|variable|pendings
+DECL|receive_context|variable|receive_context
+DECL|replies|variable|replies
+DECL|resource_reply_cb|function|static int resource_reply_cb(const struct zoap_packet *response, struct zoap_reply *reply, const void *from)
+DECL|retransmit_request|function|static void retransmit_request(struct nano_work *work)
+DECL|retransmit_work|variable|retransmit_work
+DECL|send_context|variable|send_context
+DECL|udp_receive|function|static void udp_receive(void)
