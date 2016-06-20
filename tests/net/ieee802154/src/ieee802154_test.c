2955958d4a6d251931e280af41684b1aa502a121
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-06-20 12:15:29 +0200

    tests: Add grounds for IEEE 802.15.4 stack tests.
    First test is a unit test for frame parsing
    
    Change-Id: I080d64a71fe4b6ff00e071a0cdee97ab9cd922d5
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 11c9b8dac7286b9f495327049b0a9a707a025270

---
+++ tests/net/ieee802154/src/ieee802154_test.c
@@ -0,0 +1,21 @@
+DECL|current_buf|variable|current_buf
+DECL|driver_lock|variable|driver_lock
+DECL|dst_addr|member|struct ieee802154_address_field *dst_addr;
+DECL|dst|member|struct in6_addr dst;
+DECL|fc_seq|member|struct ieee802154_fcf_seq *fc_seq;
+DECL|ieee802154_pkt_test|struct|struct ieee802154_pkt_test {
+DECL|ieee_addr_hexdump|function|static void ieee_addr_hexdump(uint8_t *addr, uint8_t length)
+DECL|iface|variable|iface
+DECL|initialize_test_environment|function|static inline int initialize_test_environment(void)
+DECL|length|member|uint8_t length;
+DECL|main|function|void main(void)
+DECL|mhr_check|member|} mhr_check;
+DECL|name|member|char *name;
+DECL|ns_pkt|variable|ns_pkt
+DECL|pkt_hexdump|function|static void pkt_hexdump(uint8_t *pkt, uint8_t length)
+DECL|pkt|member|uint8_t *pkt;
+DECL|src_addr|member|struct ieee802154_address_field *src_addr;
+DECL|src|member|struct in6_addr src;
+DECL|test_ns_pkt|variable|test_ns_pkt
+DECL|test_ns_sending|function|static inline int test_ns_sending(struct ieee802154_pkt_test *t)
+DECL|test_packet_parsing|function|static inline int test_packet_parsing(struct ieee802154_pkt_test *t)
