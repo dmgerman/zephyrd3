ce19d6da1299cd2d3c82e0d5f3cf5e5e7a8138fb
Andrew Boie <andrew.p.boie@intel.com>
2016-06-21 12:15:33 -0700

    nios2: add base exception handling code
    Change-Id: I56b0ec1a3576a77ca7bd6f2c0217de8053406927
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: e2ff2fdd91d3f9ab43d777a8f78882e2bbeb02d0

--- arch/nios2/core/fatal.c
+++ arch/nios2/core/fatal.c
@@ -1,4 +1,4 @@
-DECL|_Fault|function|FUNC_NORETURN void _Fault(void)
+DECL|_Fault|function|FUNC_NORETURN void _Fault(const NANO_ESF *esf)
 DECL|_NanoFatalErrorHandler|function|FUNC_NORETURN void _NanoFatalErrorHandler(unsigned int reason, const NANO_ESF *esf)
-DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *esf)
+DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|_default_esf|variable|_default_esf
