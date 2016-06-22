62b0ef6ca25f013e5ef4213fd581efe302ecb960
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-22 16:35:55 +0300

    net: Add net_conn pointer to callback
    When a UDP packet has been received and if a watcher match
    is found, then a callback is called. It is useful to have
    a pointer to net_conn when calling the callback.
    
    Change-Id: Ifa97b37c22b1fc9cac7eadfba23aa68dfd473fda
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9331b2bc7bdf64b04c0b1142dce346352a7b3a3a

--- net/yaip/connection.h
+++ net/yaip/connection.h
@@ -2,7 +2,7 @@ DECL|__CONNECTION_H|macro|__CONNECTION_H
 DECL|cb|member|net_conn_cb_t cb;
 DECL|flags|member|uint8_t flags;
 DECL|local_addr|member|struct sockaddr local_addr;
-DECL|net_conn_cb_t|typedef|typedef enum net_verdict (*net_conn_cb_t)(struct net_buf *buf,
+DECL|net_conn_cb_t|typedef|typedef enum net_verdict (*net_conn_cb_t)(struct net_conn *conn,
 DECL|net_conn_input|function|static inline enum net_verdict net_conn_input(enum ip_protocol proto, struct net_buf *buf)
 DECL|net_conn|struct|struct net_conn {
 DECL|proto|member|uint8_t proto;
--- tests/net/udp/src/main.c
+++ tests/net/udp/src/main.c
@@ -36,8 +36,8 @@ DECL|send_status|variable|send_status
 DECL|set_port|function|static void set_port(sa_family_t family, struct sockaddr *raddr, struct sockaddr *laddr, uint16_t rport, uint16_t lport)
 DECL|setup_ipv4_udp|function|static void setup_ipv4_udp(struct net_buf *buf, struct in_addr *remote_addr, struct in_addr *local_addr, uint16_t remote_port, uint16_t local_port)
 DECL|setup_ipv6_udp|function|static void setup_ipv6_udp(struct net_buf *buf, struct in6_addr *remote_addr, struct in6_addr *local_addr, uint16_t remote_port, uint16_t local_port)
-DECL|test_fail|function|static enum net_verdict test_fail(struct net_buf *buf, void *user_data)
-DECL|test_ok|function|static enum net_verdict test_ok(struct net_buf *buf, void *user_data)
+DECL|test_fail|function|static enum net_verdict test_fail(struct net_conn *conn, struct net_buf *buf, void *user_data)
+DECL|test_ok|function|static enum net_verdict test_ok(struct net_conn *conn,struct net_buf *buf, void *user_data)
 DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_buf *buf)
 DECL|test|member|char *test;
 DECL|ud|struct|struct ud {
