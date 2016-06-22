61c2142d10beb66f97e0cc82c83336657bb9b91a
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-22 15:42:55 +0300

    net: Utility to get net_if according to index value
    Using network interface index instead of net_if pointer can
    save 3 bytes of memory. So these utilities help to convert
    pointer to index and vice versa.
    
    Change-Id: I8743a06e0935d48798b19526154058be385550af
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 20167bd472ded239682ba5dea447a0df9eaa440a

--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -12,6 +12,8 @@ DECL|debug_check_packet|macro|debug_check_packet
 DECL|get_length|function|static inline uint8_t get_length(struct in6_addr *src, struct in6_addr *dst)
 DECL|init_tx_queue|function|static inline void init_tx_queue(struct net_if *iface)
 DECL|is_proper_ipv6_address|function|static inline bool is_proper_ipv6_address(struct net_if_addr *addr)
+DECL|net_if_get_by_iface|function|uint8_t net_if_get_by_iface(struct net_if *iface)
+DECL|net_if_get_by_index|function|struct net_if *net_if_get_by_index(uint8_t index)
 DECL|net_if_get_by_link_addr|function|struct net_if *net_if_get_by_link_addr(struct net_linkaddr *ll_addr)
 DECL|net_if_get_default|function|struct net_if *net_if_get_default(void)
 DECL|net_if_init|function|void net_if_init(void)
