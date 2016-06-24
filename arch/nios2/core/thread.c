e79526ecde627f5f43ec815f15f51797cc57622b
Andrew Boie <andrew.p.boie@intel.com>
2016-06-24 14:39:08 -0700

    nios2: implement thread monitor
    Pulled from the ARC implementation. Tested via
    test_obj_tracing.
    
    Change-Id: I858e89cc9187f99539b362ade8098b3606d31464
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: a0464b802bb686cb128a00b1017c20aed71fd2b7

--- arch/nios2/core/thread.c
+++ arch/nios2/core/thread.c
@@ -1,3 +1,5 @@
+DECL|THREAD_MONITOR_INIT|macro|THREAD_MONITOR_INIT
+DECL|THREAD_MONITOR_INIT|macro|THREAD_MONITOR_INIT
 DECL|_nanokernel|variable|_nanokernel
 DECL|_new_thread|function|void _new_thread(char *stack_memory, unsigned stack_size, void *uk_task_ptr, _thread_entry_t thread_func, _thread_arg_t arg1, _thread_arg_t arg2, _thread_arg_t arg3, int priority, unsigned options)
 DECL|arg1|member|_thread_arg_t arg1;
@@ -5,3 +7,4 @@ DECL|arg2|member|_thread_arg_t arg2;
 DECL|arg3|member|_thread_arg_t arg3;
 DECL|entry_point|member|_thread_entry_t entry_point;
 DECL|init_stack_frame|struct|struct init_stack_frame {
+DECL|thread_monitor_init|function|static ALWAYS_INLINE void thread_monitor_init(struct tcs *tcs)
