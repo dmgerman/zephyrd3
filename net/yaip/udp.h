9d5cc7cfe86076eb67d3dbcb8f7b28ac1a96bb8c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-27 12:26:39 +0300

    net: Utility function to append UDP packet into net_buf
    Change-Id: I7008b7cddb08525c2f549715a46cae608bb865d4
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b481908c71ba7b0a58ab820661f183ef94df75dd

--- net/yaip/udp.h
+++ net/yaip/udp.h
@@ -1,4 +1,6 @@
 DECL|__UDP_H|macro|__UDP_H
+DECL|net_udp_append|function|static inline struct net_buf *net_udp_append(struct net_context *context, struct net_buf *buf, uint16_t port)
+DECL|net_udp_append|macro|net_udp_append
 DECL|net_udp_init|function|static inline void net_udp_init(void)
 DECL|net_udp_init|macro|net_udp_init
 DECL|net_udp_register|function|static inline int net_udp_register(const struct sockaddr *remote_addr, const struct sockaddr *local_addr, uint16_t remote_port, uint16_t local_port, net_conn_cb_t cb,
