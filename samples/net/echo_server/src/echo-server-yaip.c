80c27ed16a7cc39ec74d49d165ca82a4f986ff14
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-28 15:04:30 +0300

    net: apps: Fix echo-server to use the new user API
    Echo-server no longer reverses the data as doing it
    when we have a fragment chain is quite challenging.
    
    Change-Id: I298669f3ddba67000672320e7b3b14f72e9f12d4
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 84edd8f2f1dd96decce14a377598e9c30cbe276a

--- samples/net/echo_server/src/echo-server-yaip.c
+++ samples/net/echo_server/src/echo-server-yaip.c
@@ -1,21 +1,29 @@
+DECL|MAX_DBG_PRINT|macro|MAX_DBG_PRINT
 DECL|MCAST_IP4ADDR|macro|MCAST_IP4ADDR
 DECL|MCAST_IP6ADDR|macro|MCAST_IP6ADDR
 DECL|MY_IP4ADDR|macro|MY_IP4ADDR
 DECL|MY_IP6ADDR|macro|MY_IP6ADDR
 DECL|MY_PORT|macro|MY_PORT
 DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
-DECL|PRINT|macro|PRINT
-DECL|PRINT|macro|PRINT
+DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|WAIT_TICKS|macro|WAIT_TICKS
 DECL|WAIT_TICKS|macro|WAIT_TICKS
-DECL|WAIT_TIME|macro|WAIT_TIME
-DECL|addr6|variable|addr6
 DECL|fiberStack|variable|fiberStack
-DECL|get_context|function|static inline bool get_context(struct net_context **udp_recv4, struct net_context **udp_recv6, struct net_context **tcp_recv4, struct net_context **tcp_recv6, struct net_context **mcast_recv4,
+DECL|get_context|function|static inline bool get_context(struct net_context **udp_recv4, struct net_context **udp_recv6, struct net_context **tcp_recv4, struct net_context **tcp_recv6, struct net_context **mcast_recv6)
 DECL|in4addr_my|variable|in4addr_my
+DECL|in6addr_mcast|variable|in6addr_mcast
 DECL|in6addr_my|variable|in6addr_my
 DECL|in6addr_my|variable|in6addr_my
 DECL|init_app|function|static inline void init_app(void)
 DECL|main|function|void main(void)
+DECL|quit_lock|variable|quit_lock
+DECL|quit|function|static inline void quit(void)
 DECL|receive|function|void receive(void)
+DECL|set_dst_addr|function|static inline void set_dst_addr(sa_family_t family,struct net_buf *buf, struct sockaddr *dst_addr)
+DECL|setup_udp_recv|function|static void setup_udp_recv(struct net_context *udp_recv4, struct net_context *udp_recv6)
+DECL|udp_received|function|static void udp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
+DECL|udp_recv|function|static struct net_buf *udp_recv(const char *name,struct net_context *context, struct net_buf *buf)
+DECL|udp_sent|function|static inline void udp_sent(struct net_context *context, int status, void *token, void *user_data)
