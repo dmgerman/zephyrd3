df490b2b438ba58dc4caa71c0ca01af0c24f4e3f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-06-28 17:30:59 +0300

    net: Do IPv6 ND if LL address is not known when sending
    Resolve the ll address so that IPv6 packet can be sent.
    This means that the IPv6 neighbor discovery needs to be done.
    The original packet will be sent after neighbor advertisement
    is received.
    
    Change-Id: If54cdbd1468ca802f59e206996ea3addab16d5b8
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 14dc08583027cb9a82c9f621c4d0354ed6e2695d

--- net/yaip/ipv6.h
+++ net/yaip/ipv6.h
@@ -4,3 +4,4 @@ DECL|NET_IPV6_ND_INFINITE_LIFETIME|macro|NET_IPV6_ND_INFINITE_LIFETIME
 DECL|NET_MAX_RS_COUNT|macro|NET_MAX_RS_COUNT
 DECL|__IPV6_H|macro|__IPV6_H
 DECL|net_ipv6_init|macro|net_ipv6_init
+DECL|net_ipv6_prepare_for_send|function|static inline struct net_buf *net_ipv6_prepare_for_send(struct net_buf *buf)
