ef50e404f16dc3e4989e161b28e8c7371c7fdd16
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-06-28 15:05:07 +0200

    net: ieee802154: Support ACK replies
    If a data packet is received with AR flag set, it will reply
    accordingly. This is meaningful when running the stack above hardware
    that cannot reply automatically to ACK request.
    
    Change-Id: I7e5be94c4a5b1c889adb67ceb6f44dadd36db3ce
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: d82d96a93d9dc18e44c03e82e55dfa9f867122d1

--- net/yaip/l2/ieee802154/ieee802154.c
+++ net/yaip/l2/ieee802154/ieee802154.c
@@ -1,5 +1,7 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|ieee802154_acknowledge|function|static inline void ieee802154_acknowledge(struct net_if *iface, struct ieee802154_mpdu *mpdu)
+DECL|ieee802154_acknowledge|macro|ieee802154_acknowledge
 DECL|ieee802154_init|function|void ieee802154_init(struct net_if *iface)
 DECL|ieee802154_recv|function|static enum net_verdict ieee802154_recv(struct net_if *iface,struct net_buf *buf)
 DECL|ieee802154_send|function|static enum net_verdict ieee802154_send(struct net_if *iface,struct net_buf *buf)
