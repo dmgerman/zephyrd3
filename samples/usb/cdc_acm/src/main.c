3a1880a915239610e9c93682808d98ab0671ade8
Adrian Bradianu <adrian.bradianu@windriver.com>
2016-06-28 17:46:10 +0300

    usb: cdc acm: Read all the received data
    Change CDC example to read and echo all the received data
    before waiting for a new read interrupt.
    
    Change-Id: I21e1646fb20a2c4f7aea2b396b633a4e0ae429fc
    Signed-off-by: Adrian Bradianu <adrian.bradianu@windriver.com>
    
    Former-commit-id: b6fb004354aecb3c530739539f38ad9ec1dce7d9

--- samples/usb/cdc_acm/src/main.c
+++ samples/usb/cdc_acm/src/main.c
@@ -7,5 +7,5 @@ DECL|data_buf|variable|data_buf
 DECL|data_transmitted|variable|data_transmitted
 DECL|interrupt_handler|function|static void interrupt_handler(struct device *dev)
 DECL|main|function|void main(void)
-DECL|read_data|function|static void read_data(struct device *dev, int *bytes_read)
+DECL|read_and_echo_data|function|static void read_and_echo_data(struct device *dev, int *bytes_read)
 DECL|write_data|function|static void write_data(struct device *dev, const char *buf, int len)
