d1520f706e90c0ba66ccdbf51471e129794ee9fa
Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
2016-06-29 15:19:49 +0300

    samples: bmi160: add sensor_value_sprintf helper function
    Add helper function for printing sensor_value to char buffer.
    
    Change-Id: I46329675d9a69376ea74c4f7dfcb270ca2078c72
    Signed-off-by: Bogdan Davidoaia <bogdan.m.davidoaia@intel.com>
    
    Former-commit-id: a30a3ac949177a5ddc84507b919b168a06c1c19e

--- samples/sensor/bmi160/arc/src/arc_bmi160.c
+++ samples/sensor/bmi160/arc/src/arc_bmi160.c
@@ -9,6 +9,7 @@ DECL|manual_calibration|function|static int manual_calibration(struct device *bm
 DECL|print_accel_data|function|static void print_accel_data(struct device *bmi160)
 DECL|print_gyro_data|function|static void print_gyro_data(struct device *bmi160)
 DECL|print_temp_data|function|static void print_temp_data(struct device *bmi160)
+DECL|sensor_value_snprintf|function|static inline int sensor_value_snprintf(char *buf, size_t len,const struct sensor_value *val)
 DECL|test_anymotion_trigger|function|static void test_anymotion_trigger(struct device *bmi160)
 DECL|test_data_ready_trigger|function|static void test_data_ready_trigger(struct device *bmi160)
 DECL|test_polling_mode|function|static void test_polling_mode(struct device *bmi160)
