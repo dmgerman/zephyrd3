a3d7913662292a79e9061b3e5c021599027dface
Mariusz Ryndzionek <mariusz.ryndzionek@firmwave.com>
2016-06-30 07:57:53 +0200

    sensor: tmp112: use arithmetic_shift_right helper function
    Use the function provided in misc/util.h to do the sample
    conversion.
    
    Change-Id: I21179982b6001bf79448e4eb289e9cffaf97f3fb
    Signed-off-by: Mariusz Ryndzionek <mariusz.ryndzionek@firmwave.com>
    
    Former-commit-id: 57afc5f85174bc88470f6f2730876c27a8639193

--- drivers/sensor/sensor_tmp112.c
+++ drivers/sensor/sensor_tmp112.c
@@ -3,7 +3,6 @@ DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|TMP112_CR0_BIT|macro|TMP112_CR0_BIT
 DECL|TMP112_CR1_BIT|macro|TMP112_CR1_BIT
 DECL|TMP112_D0_BIT|macro|TMP112_D0_BIT
-DECL|TMP112_D15_BIT|macro|TMP112_D15_BIT
 DECL|TMP112_EM_BIT|macro|TMP112_EM_BIT
 DECL|TMP112_I2C_ADDRESS|macro|TMP112_I2C_ADDRESS
 DECL|TMP112_I2C_ADDRESS|macro|TMP112_I2C_ADDRESS
@@ -13,7 +12,7 @@ DECL|TMP112_REG_CONFIG|macro|TMP112_REG_CONFIG
 DECL|TMP112_REG_TEMPERATURE|macro|TMP112_REG_TEMPERATURE
 DECL|TMP112_TEMP_SCALE|macro|TMP112_TEMP_SCALE
 DECL|i2c|member|struct device *i2c;
-DECL|sample|member|uint16_t sample;
+DECL|sample|member|int16_t sample;
 DECL|tmp112_attr_set|function|static int tmp112_attr_set(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
 DECL|tmp112_channel_get|function|static int tmp112_channel_get(struct device *dev,enum sensor_channel chan, struct sensor_value *val)
 DECL|tmp112_data|struct|struct tmp112_data {
