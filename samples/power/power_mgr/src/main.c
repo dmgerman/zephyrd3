eb222938c575c4dbcfb6122bafbc52431557b6fd
Ramesh Thomas <ramesh.thomas@intel.com>
2016-06-30 19:26:45 -0700

    power_mgmt: Add Deep Sleep support in sample PM app
    Added changes to enhance device suspend/resume involving
    core devices used by OS and app. Updated with Deep Sleep
    power policy usage example. Moved the arch/soc dependent
    code to soc/arch dependent areas in Zephyr in a separate
    patch.  This app now uses api that abstracts the soc/arch
    details in handling power states.
    
    Jira: ZEP-511
    Change-Id: I3b4eb0be973027dbf9bd046e16836f7863eee46e
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 1af3a40b9e74521bdd93fadcaa154da1de343729

--- samples/power/power_mgr/src/main.c
+++ samples/power/power_mgr/src/main.c
@@ -1,25 +1,31 @@
+DECL|ALARM|macro|ALARM
 DECL|DEVICE_POLICY_MAX|macro|DEVICE_POLICY_MAX
-DECL|PRINT|macro|PRINT
-DECL|PRINT|macro|PRINT
-DECL|P_LVL2|macro|P_LVL2
+DECL|GPIO_IN_PIN|macro|GPIO_IN_PIN
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
 DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
 DECL|check_pm_policy|function|static int check_pm_policy(int32_t ticks)
 DECL|create_device_list|function|static void create_device_list(void)
+DECL|deep_sleep_entry|function|static int deep_sleep_entry(int32_t ticks)
+DECL|deep_sleep_exit|function|static void deep_sleep_exit(void)
 DECL|device_count|variable|device_count
 DECL|device_list|variable|device_list
 DECL|device_policy_list|variable|device_policy_list
 DECL|device_retval|variable|device_retval
 DECL|device_suspend_only_entry|function|static int device_suspend_only_entry(int32_t ticks)
 DECL|device_suspend_only_exit|function|static void device_suspend_only_exit(void)
+DECL|enable_wake_event|function|static void enable_wake_event(void)
 DECL|end_time|variable|end_time
+DECL|gpio_dev|variable|gpio_dev
 DECL|low_power_state_entry|function|static int low_power_state_entry(int32_t ticks)
 DECL|low_power_state_exit|function|static void low_power_state_exit(void)
 DECL|main|function|void main(void)
-DECL|pm_state|variable|pm_state
-DECL|quark_low_power|function|static void quark_low_power(void)
 DECL|resume_devices|function|static void resume_devices(int pm_policy)
 DECL|rtc_dev|variable|rtc_dev
+DECL|rtc_interrupt_fn|function|void rtc_interrupt_fn(struct device *rtc_dev)
+DECL|setup_gpio|function|static int setup_gpio(void)
+DECL|setup_rtc|function|static void setup_rtc(void)
 DECL|start_time|variable|start_time
 DECL|suspend_devices|function|static void suspend_devices(int pm_policy)
+DECL|test_started|variable|test_started
+DECL|wait_gpio_low|function|static int wait_gpio_low(void)
