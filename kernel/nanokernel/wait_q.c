71a89e589423d3e58ef7a7cf6c6921897c932765
tulasinagraj <tulasi.r@tcs.com>
2016-07-05 17:07:56 +0530

    nanokernel: remove duplicate symbols in image file
    Jira ZEP-68,zephyr.elf shows duplicate routines which increases the foot print.
    Current fix removes duplicate routines and reduces foot print of the image
    
    Change-Id: I01a2e5a8a02481ab33a2bb09e9c545d6879c1b81
    Signed-off-by: tulasinagraj <tulasi.r@tcs.com>
    
    Former-commit-id: ce2574a30beed18a0a137f5d33d88c42b63781ee

---
+++ kernel/nanokernel/wait_q.c
@@ -0,0 +1,2 @@
+DECL|_nano_wait_q_remove_no_check|function|static struct tcs *_nano_wait_q_remove_no_check(struct _nano_queue *wait_q)
+DECL|_nano_wait_q_remove|function|struct tcs *_nano_wait_q_remove(struct _nano_queue *wait_q)
