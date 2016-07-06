d13c5e94217a92eb167ee1d9381f9bee9bae6a61
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-07-06 22:20:39 +0300

    net: Add neighbor free function to IPv6 cache
    Add utility function that cleanups the neighbor cache.
    
    Change-Id: I77363566469c4f9d0410c08b5a3009bbe8d1b96e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7bc11597aabc776a0f205618fa2c352fa44e884c

--- net/yaip/ipv6.c
+++ net/yaip/ipv6.c
@@ -43,6 +43,7 @@ DECL|link_metric|member|uint16_t link_metric;
 DECL|na_input_handler|variable|na_input_handler
 DECL|nbr_add|function|static struct net_nbr *nbr_add(struct net_buf *buf, struct in6_addr *addr, struct net_linkaddr *lladdr, bool is_router, enum net_nbr_state state)
 DECL|nbr_clear_ns_pending|function|static inline void nbr_clear_ns_pending(struct net_nbr_data *data)
+DECL|nbr_free|function|static inline void nbr_free(struct net_nbr *nbr)
 DECL|nbr_lookup|function|static struct net_nbr *nbr_lookup(struct net_nbr_table *table, struct net_if *iface, struct in6_addr *addr)
 DECL|nbr_new|function|static struct net_nbr *nbr_new(struct net_if *iface, struct in6_addr *addr, enum net_nbr_state state)
 DECL|nbr_print|function|void nbr_print(void)
