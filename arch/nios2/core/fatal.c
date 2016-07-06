2e716f8fb11c3f6e43de49305bf014a95ee15596
Andrew Boie <andrew.p.boie@intel.com>
2016-07-06 14:35:26 -0700

    nios2: optionally print cause code reason
    This is at the expense of code size. The QEMU and MAX10
    targets have plenty of space so enable it for these boards,
    but leave off by default for others.
    
    Change-Id: I93fdb7db14232727e9953b22490d8869ff3b60e7
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 2eb86db52995a26641f584c89ead1998039ab808

--- arch/nios2/core/fatal.c
+++ arch/nios2/core/fatal.c
@@ -2,3 +2,4 @@ DECL|_Fault|function|FUNC_NORETURN void _Fault(const NANO_ESF *esf)
 DECL|_NanoFatalErrorHandler|function|FUNC_NORETURN void _NanoFatalErrorHandler(unsigned int reason, const NANO_ESF *esf)
 DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|_default_esf|variable|_default_esf
+DECL|cause_str|function|static char *cause_str(uint32_t cause_code)
