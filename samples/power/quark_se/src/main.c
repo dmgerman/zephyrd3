379cbc4f50e1430bf4162a35bdfb19f4f4f606bf
Andre Guedes <andre.guedes@intel.com>
2016-07-07 08:46:32 -0300

    samples: Add Quark SE power management application
    This patch adds a reference Power Management Application (PMA) for
    Quark SE based platforms.
    
    This application demostrates how to interact with the PM subsystem
    and how to put the SoC in different power states. The application
    "modus operandi" is very simple. The application keeps transiting
    between all power states supported by Quark SE SoC. For now, C1,
    C2 and C2LP CPU states are implemented. Upcoming patches will
    add support for the remaining states (LPS and Sleep states).
    
    Change-Id: Id81aadb8bf170fbb2ecfd9483ae250c786619e43
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>
    Signed-off-by: Iván Briano <ivan.briano@intel.com>
    
    Former-commit-id: 02dc270d5e6de173a8023382b1a1053f63676309

---
+++ samples/power/quark_se/src/main.c
@@ -0,0 +1,14 @@
+DECL|POWER_STATE_CPU_C1|enumerator|POWER_STATE_CPU_C1,
+DECL|POWER_STATE_CPU_C2LP|enumerator|POWER_STATE_CPU_C2LP,
+DECL|POWER_STATE_CPU_C2|enumerator|POWER_STATE_CPU_C2,
+DECL|POWER_STATE_MAX|enumerator|POWER_STATE_MAX
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
+DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
+DECL|get_next_state|function|static enum power_states get_next_state(void)
+DECL|last_state|variable|last_state
+DECL|main|function|void main(void)
+DECL|power_states|enum|enum power_states {
+DECL|rtc_dev|variable|rtc_dev
+DECL|set_rtc_alarm|function|static void set_rtc_alarm(void)
+DECL|state_to_string|function|static const char *state_to_string(int state)
