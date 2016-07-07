a3d8db11aa225d4a148008c5860db1ca540fcb7e
Genaro Saucedo Tejada <genaro.saucedo.tejada@intel.com>
2016-07-07 12:31:12 -0500

    sys_log: removed unused macro at pci_config.c
    Debug macro definition at this file was repetitive and has been
    superseded by new system logging API. Furthermore it was not being
    used on this .c file.
    JIRA: ZEP-311
    
    Change-Id: Id5828d1144fa03cc24f100ae94ecc596502c1131
    Signed-off-by: Genaro Saucedo Tejada <genaro.saucedo.tejada@intel.com>
    
    Former-commit-id: 9f48993d039ac49a3cd8c6dc73a77f67e600697c

--- drivers/pci/pci_config.c
+++ drivers/pci/pci_config.c
@@ -1,5 +1,3 @@
-DECL|_DEBUG_PRINT|macro|_DEBUG_PRINT
-DECL|_DEBUG_PRINT|macro|_DEBUG_PRINT
 DECL|pci_config_ext_cap_ptr_find|function|int pci_config_ext_cap_ptr_find(uint8_t ext_cap_find_id, uint32_t bus,uint32_t device, uint32_t function, uint8_t *p_offset)
 DECL|pci_config_in_byte|function|void pci_config_in_byte(uint32_t bus_no, uint32_t device_no, uint32_t func_no,uint32_t offset, uint8_t *data)
 DECL|pci_config_in_long|function|void pci_config_in_long(uint32_t bus_no, uint32_t device_no, uint32_t func_no,uint32_t offset, uint32_t *data)
