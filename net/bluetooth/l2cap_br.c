5d3698ed64e1ca6c94a359961ec7d163901d23a1
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-07-08 16:30:31 +0200

    Bluetooth: L2CAP: Introduce security requirements on CoC
    Applies on L2CAP channel object security requirement member and
    implements on BR/EDR transport basic security cases during incoming
    CoC connection request. Since channel security requirements can involve
    sending 2 separate connection responses with proper results depending on
    context path, there's a need to store L2CAP signaling identifier set in
    original connection request to be restored later to help match proper
    response context.
    
    Change-Id: Ibac9a5d2443f2975637e1bd15f61afcad53f843b
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 96e68fe65430fa93addbdb94304f806ab5574848

--- net/bluetooth/l2cap_br.c
+++ net/bluetooth/l2cap_br.c
@@ -50,6 +50,7 @@ DECL|l2cap_br_info_req|function|static int l2cap_br_info_req(struct bt_l2cap_br 
 DECL|l2cap_br_info_rsp|function|static int l2cap_br_info_rsp(struct bt_l2cap_br *l2cap, uint8_t ident, struct net_buf *buf)
 DECL|l2cap_br_recv|function|static void l2cap_br_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|l2cap_br_remove_tx_cid|function|static struct bt_l2cap_br_chan *l2cap_br_remove_tx_cid(struct bt_conn *conn, uint16_t cid)
+DECL|l2cap_br_security_check|function|static bool l2cap_br_security_check(struct bt_l2cap_chan *chan, const uint16_t psm)
 DECL|l2cap_br_send_reject|function|static void l2cap_br_send_reject(struct bt_conn *conn, uint8_t ident, uint16_t reason, void *data, uint8_t data_len)
 DECL|l2cap_br_server_lookup_psm|function|static struct bt_l2cap_server *l2cap_br_server_lookup_psm(uint16_t psm)
 DECL|l2cap_br_state_set|function|static void l2cap_br_state_set(struct bt_l2cap_chan *ch, bt_l2cap_chan_state_t state)
