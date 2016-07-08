94da869622c2be504ccccd26e86f9792aa85c8de
Johan Hedberg <johan.hedberg@intel.com>
2016-07-08 10:47:35 +0300

    net: buf: Add net_buf_reserve() API
    This API allows initializing the buffer with a given headroom. This
    prepares the way for eventually removing the 'reserve_head' parameter
    from the net_buf_get APIs, but can already now be useful in some
    scenarios where the headroom is desired to be different than some
    higher level API sets it to be.
    
    Change-Id: Iffbe5761fdf3d2ad8cb4b8437b1074cf42ea9c6c
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 727a2459b18c529938df970073a68d0bc985a700

--- net/buf.c
+++ net/buf.c
@@ -24,5 +24,6 @@ DECL|net_buf_push_le16|function|void net_buf_push_le16(struct net_buf *buf, uint
 DECL|net_buf_push|function|void *net_buf_push(struct net_buf *buf, size_t len)
 DECL|net_buf_put|function|void net_buf_put(struct nano_fifo *fifo, struct net_buf *buf)
 DECL|net_buf_ref|function|struct net_buf *net_buf_ref(struct net_buf *buf)
+DECL|net_buf_reserve|function|void net_buf_reserve(struct net_buf *buf, size_t reserve)
 DECL|net_buf_tailroom|function|size_t net_buf_tailroom(struct net_buf *buf)
 DECL|net_buf_unref|function|void net_buf_unref(struct net_buf *buf)
