f137601a6b74061c41a2f8ad97f7379b62518190
Iván Briano <ivan.briano@intel.com>
2016-07-11 18:04:39 -0300

    samples: Add suspend/resume of devices to Quark SE power sample
    Change-Id: I6d0878b7e38114e2e5523a0970d193b42c507f8f
    Signed-off-by: Iván Briano <ivan.briano@intel.com>
    
    Former-commit-id: c829fbebdc2bf512585b45cc044644b8d22dfcff

--- samples/power/quark_se/src/main.c
+++ samples/power/quark_se/src/main.c
@@ -1,3 +1,4 @@
+DECL|MAX_SUSPEND_DEVICE_COUNT|macro|MAX_SUSPEND_DEVICE_COUNT
 DECL|POWER_STATE_CPU_C1|enumerator|POWER_STATE_CPU_C1,
 DECL|POWER_STATE_CPU_C2LP|enumerator|POWER_STATE_CPU_C2LP,
 DECL|POWER_STATE_CPU_C2|enumerator|POWER_STATE_CPU_C2,
@@ -7,6 +8,7 @@ DECL|POWER_STATE_SOC_SLEEP|enumerator|POWER_STATE_SOC_SLEEP,
 DECL|TIMEOUT|macro|TIMEOUT
 DECL|__do_soc_sleep|function|static void __do_soc_sleep(int deep)
 DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
+DECL|build_suspend_device_list|function|static void build_suspend_device_list(void)
 DECL|do_soc_sleep|function|static void do_soc_sleep(int deep)
 DECL|get_next_state|function|static enum power_states get_next_state(void)
 DECL|last_state|variable|last_state
@@ -15,3 +17,5 @@ DECL|power_states|enum|enum power_states {
 DECL|rtc_dev|variable|rtc_dev
 DECL|set_rtc_alarm|function|static void set_rtc_alarm(void)
 DECL|state_to_string|function|static const char *state_to_string(int state)
+DECL|suspend_device_count|variable|suspend_device_count
+DECL|suspend_devices|variable|suspend_devices
