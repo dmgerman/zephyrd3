4afd6f824271706698ec508ef442826dc17b9107
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-07-11 11:03:30 +0300

    net: Add 6lowpan without compression header support
    Add support for IPv6 header dispatch (without IPv6 compression
    and uncompression). Only IPv6 dispatch header will be added at the
    beginning of the buffer.
    
    Change-Id: I7400a4dd115ac3b4b3d9deecd3cdb9ea292573fb
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 1725ac66d731ff81f8a27a4995a7a6db55780057

--- net/yaip/6lo.c
+++ net/yaip/6lo.c
@@ -1,11 +1,13 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|compress_IPHC_header|function|static inline bool compress_IPHC_header(struct net_buf *buf)
+DECL|compress_ipv6_header|function|static inline bool compress_ipv6_header(struct net_buf *buf)
 DECL|net_6lo_addr_16_bit_compressible|function|static inline bool net_6lo_addr_16_bit_compressible(struct in6_addr *addr)
-DECL|net_6lo_compress|function|bool net_6lo_compress(struct net_buf *buf)
+DECL|net_6lo_compress|function|bool net_6lo_compress(struct net_buf *buf, bool iphc)
 DECL|net_6lo_ll_prefix_padded_with_zeros|function|static inline bool net_6lo_ll_prefix_padded_with_zeros(struct in6_addr *addr)
 DECL|net_6lo_maddr_32_bit_compressible|function|static inline bool net_6lo_maddr_32_bit_compressible(struct in6_addr *addr)
 DECL|net_6lo_maddr_48_bit_compressible|function|static inline bool net_6lo_maddr_48_bit_compressible(struct in6_addr *addr)
 DECL|net_6lo_maddr_8_bit_compressible|function|static inline bool net_6lo_maddr_8_bit_compressible(struct in6_addr *addr)
 DECL|net_6lo_uncompress|function|bool net_6lo_uncompress(struct net_buf *buf)
 DECL|uncompress_IPHC_header|function|static inline bool uncompress_IPHC_header(struct net_buf *buf)
+DECL|uncompress_ipv6_header|function|static inline bool uncompress_ipv6_header(struct net_buf *buf)
