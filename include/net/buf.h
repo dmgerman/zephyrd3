c223f10e7b647d893c7dc1f0e4c389502433576e
Johan Hedberg <johan.hedberg@intel.com>
2016-07-12 10:25:27 +0300

    net: buf: Introduce net_buf_simple object
    The object has the same layout as the last fields of the existing
    net_buf. To maintain backwards compatibility net_buf receives a union
    so that the old data, len and size members can be accessed as before,
    however they are located in the same memory as the actual
    net_buf_simple members.
    
    Change-Id: I22d208faba30fe41c5ada17e6d13e641f528729a
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 2d5bfe2f1f8bbb16ccfb210e6ae078a9b4051e73

--- include/net/buf.h
+++ include/net/buf.h
@@ -3,19 +3,25 @@ DECL|NET_BUF_POOL|macro|NET_BUF_POOL
 DECL|__NET_BUF_H|macro|__NET_BUF_H
 DECL|__net_buf_align|macro|__net_buf_align
 DECL|__net_buf_align|member|uint8_t __buf[0] __net_buf_align;
+DECL|__net_buf_align|member|uint8_t __buf[0] __net_buf_align;
 DECL|_unused|member|int _unused;
+DECL|b|member|struct net_buf_simple b;
+DECL|data|member|uint8_t *data;
 DECL|data|member|uint8_t *data;
 DECL|destroy|member|void (*const destroy)(struct net_buf *buf);
 DECL|flags|member|uint8_t flags;
 DECL|frags|member|struct net_buf *frags;
 DECL|free|member|struct nano_fifo * const free;
 DECL|len|member|uint16_t len;
+DECL|len|member|uint16_t len;
 DECL|net_buf_frag_add|macro|net_buf_frag_add
 DECL|net_buf_frags_len|function|static inline size_t net_buf_frags_len(struct net_buf *buf)
 DECL|net_buf_pool_init|macro|net_buf_pool_init
+DECL|net_buf_simple|struct|struct net_buf_simple {
 DECL|net_buf_tail|function|static inline uint8_t *net_buf_tail(struct net_buf *buf)
 DECL|net_buf_user_data|function|static inline void *net_buf_user_data(struct net_buf *buf)
 DECL|net_buf|struct|struct net_buf {
 DECL|ref|member|uint8_t ref;
+DECL|size|member|const uint16_t size;
 DECL|size|member|const uint16_t size;
 DECL|user_data_size|member|const uint16_t user_data_size;
