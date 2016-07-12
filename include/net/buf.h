0e6d3b8406d93623052a88a57333e9fe45474305
Johan Hedberg <johan.hedberg@intel.com>
2016-07-12 14:10:32 +0300

    net: buf: Add complete net_buf_simple implementation
    This just renames the existing net_buf helper APIs to corresponding
    net_buf_simple APIs and then defines the old net_buf API names as
    macros mapping to the new net_buf_simple helpers.
    
    Change-Id: I3c2b35683c0fde66f5f697be2c34c01172af8e31
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 497268503e735ccb1a18ac59ec86c9706a8c3f83

--- include/net/buf.h
+++ include/net/buf.h
@@ -14,11 +14,27 @@ DECL|frags|member|struct net_buf *frags;
 DECL|free|member|struct nano_fifo * const free;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
+DECL|net_buf_add_be16|macro|net_buf_add_be16
+DECL|net_buf_add_le16|macro|net_buf_add_le16
+DECL|net_buf_add_le32|macro|net_buf_add_le32
+DECL|net_buf_add_u8|macro|net_buf_add_u8
+DECL|net_buf_add|macro|net_buf_add
 DECL|net_buf_frag_add|macro|net_buf_frag_add
 DECL|net_buf_frags_len|function|static inline size_t net_buf_frags_len(struct net_buf *buf)
+DECL|net_buf_headroom|macro|net_buf_headroom
 DECL|net_buf_pool_init|macro|net_buf_pool_init
+DECL|net_buf_pull_le16|macro|net_buf_pull_le16
+DECL|net_buf_pull_le32|macro|net_buf_pull_le32
+DECL|net_buf_pull_u8|macro|net_buf_pull_u8
+DECL|net_buf_pull|macro|net_buf_pull
+DECL|net_buf_push_be16|macro|net_buf_push_be16
+DECL|net_buf_push_le16|macro|net_buf_push_le16
+DECL|net_buf_push_u8|macro|net_buf_push_u8
+DECL|net_buf_push|macro|net_buf_push
+DECL|net_buf_simple_tail|function|static inline uint8_t *net_buf_simple_tail(struct net_buf_simple *buf)
 DECL|net_buf_simple|struct|struct net_buf_simple {
-DECL|net_buf_tail|function|static inline uint8_t *net_buf_tail(struct net_buf *buf)
+DECL|net_buf_tailroom|macro|net_buf_tailroom
+DECL|net_buf_tail|macro|net_buf_tail
 DECL|net_buf_user_data|function|static inline void *net_buf_user_data(struct net_buf *buf)
 DECL|net_buf|struct|struct net_buf {
 DECL|ref|member|uint8_t ref;
