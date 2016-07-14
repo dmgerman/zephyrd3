b8422fc34290b680c65313def8d597a852d43a30
Ramesh Thomas <ramesh.thomas@intel.com>
2016-07-14 11:42:27 -0700

    rtc: qmsi: Adds device suspend/resume to rtc_qmsi device
    This adds some basic suspend/resume support in the rtc qmsi
    driver. This is part of a change to enable device suspend/resume
    and Deep Sleep support in sample application.
    
    Jira: ZEP-512
    Change-Id: I79ccfb56de5b3cbfb5bc91e9cacca68554aa46ee
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 117134803845b7e7a159affac5e4f90d9e52adaa

--- drivers/rtc/rtc_qmsi.c
+++ drivers/rtc/rtc_qmsi.c
@@ -1,7 +1,10 @@
 DECL|api|variable|api
+DECL|int_rtc_mask_save|variable|int_rtc_mask_save
 DECL|rtc_qmsi_disable|function|static void rtc_qmsi_disable(struct device *dev)
 DECL|rtc_qmsi_enable|function|static void rtc_qmsi_enable(struct device *dev)
 DECL|rtc_qmsi_init|function|static int rtc_qmsi_init(struct device *dev)
 DECL|rtc_qmsi_read|function|static uint32_t rtc_qmsi_read(struct device *dev)
 DECL|rtc_qmsi_set_alarm|function|static int rtc_qmsi_set_alarm(struct device *dev, const uint32_t alarm_val)
 DECL|rtc_qmsi_set_config|function|static int rtc_qmsi_set_config(struct device *dev, struct rtc_config *cfg)
+DECL|rtc_resume_device|function|static int rtc_resume_device(struct device *dev, int pm_policy)
+DECL|rtc_suspend_device|function|static int rtc_suspend_device(struct device *dev, int pm_policy)
