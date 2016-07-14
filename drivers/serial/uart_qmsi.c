2e425cd3e2b9c00cf2a2e1afe35e8002c9e97a7c
Ramesh Thomas <ramesh.thomas@intel.com>
2016-07-14 11:48:21 -0700

    uart: qmsi: Adds device suspend/resume to uart_qmsi device
    This adds some basic suspend/resume support in the uart_qmsi
    driver. This is part of a change to enable device suspend/resume
    and deep sleep support in sample PM application.
    
    Jira: ZEP-512
    Change-Id: I38cfbfb352d0027b7beaadebedbc10002dade50a
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 53bc73a438bba4d7cd708c1bc72e471ee591eaeb

--- drivers/serial/uart_qmsi.c
+++ drivers/serial/uart_qmsi.c
@@ -4,12 +4,14 @@ DECL|GET_CONTROLLER_INSTANCE|macro|GET_CONTROLLER_INSTANCE
 DECL|IIR_IID_NO_INTERRUPT_PENDING|macro|IIR_IID_NO_INTERRUPT_PENDING
 DECL|api|variable|api
 DECL|baud_divisor|member|uint32_t baud_divisor;
+DECL|cfg_save|member|qm_uart_config_t cfg_save;
 DECL|clock_gate|member|clk_periph_t clock_gate;
 DECL|config_info_0|variable|config_info_0
 DECL|config_info_1|variable|config_info_1
 DECL|drv_data_0|variable|drv_data_0
 DECL|drv_data_1|variable|drv_data_1
 DECL|hw_fc|member|bool hw_fc;
+DECL|iir_cache|member|uint8_t iir_cache;
 DECL|iir_cache|member|uint8_t iir_cache;
 DECL|instance|member|qm_uart_t instance;
 DECL|irq_config_func_0|function|static void irq_config_func_0(struct device *dev)
@@ -19,6 +21,7 @@ DECL|is_data_ready|function|static bool is_data_ready(qm_uart_t instance)
 DECL|is_tx_fifo_full|function|static bool is_tx_fifo_full(qm_uart_t instance)
 DECL|uart_qmsi_config_info|struct|struct uart_qmsi_config_info {
 DECL|uart_qmsi_drv_cmd|function|static int uart_qmsi_drv_cmd(struct device *dev, uint32_t cmd, uint32_t p)
+DECL|uart_qmsi_drv_data|struct|struct uart_qmsi_drv_data {
 DECL|uart_qmsi_drv_data|struct|struct uart_qmsi_drv_data {
 DECL|uart_qmsi_err_check|function|static int uart_qmsi_err_check(struct device *dev)
 DECL|uart_qmsi_fifo_fill|function|static int uart_qmsi_fifo_fill(struct device *dev, const uint8_t *tx_data, int size)
@@ -38,6 +41,10 @@ DECL|uart_qmsi_irq_tx_ready|function|static int uart_qmsi_irq_tx_ready(struct de
 DECL|uart_qmsi_irq_update|function|static int uart_qmsi_irq_update(struct device *dev)
 DECL|uart_qmsi_isr|function|static void uart_qmsi_isr(void *arg)
 DECL|uart_qmsi_line_ctrl_set|function|static int uart_qmsi_line_ctrl_set(struct device *dev, uint32_t ctrl, uint32_t val)
+DECL|uart_qmsi_pm_save_config|function|void uart_qmsi_pm_save_config(struct device *dev, qm_uart_config_t *cfg)
+DECL|uart_qmsi_pm_save_config|macro|uart_qmsi_pm_save_config
 DECL|uart_qmsi_poll_in|function|static int uart_qmsi_poll_in(struct device *dev, unsigned char *data)
 DECL|uart_qmsi_poll_out|function|static unsigned char uart_qmsi_poll_out(struct device *dev,unsigned char data)
+DECL|uart_resume_device|function|static int uart_resume_device(struct device *dev, int pm_policy)
+DECL|user_cb|member|uart_irq_callback_t user_cb;
 DECL|user_cb|member|uart_irq_callback_t user_cb;
