e558ba187519a070a5a12af9e569dd03e2e7489c
Andrew Boie <andrew.p.boie@intel.com>
2016-07-15 13:15:00 -0700

    nanokernel: support GCC compiler atomic builtins
    Arches now select whether they want to use the GCC built-ins,
    their own assembly implementation, or the generic C code.
    
    At the moment, the SDK compilers only support builtins for ARM
    and X86. ZEP-557 opened to investigate further.
    
    Change-Id: I53e411b4967d87f737338379bd482bd653f19422
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 6a1474e75b86d65e9bf2ddf7d61c076079fe89a9

--- include/atomic.h
+++ include/atomic.h
@@ -3,10 +3,22 @@ DECL|ATOMIC_ELEM|macro|ATOMIC_ELEM
 DECL|ATOMIC_INIT|macro|ATOMIC_INIT
 DECL|ATOMIC_MASK|macro|ATOMIC_MASK
 DECL|__ATOMIC_H__|macro|__ATOMIC_H__
+DECL|atomic_add|function|static inline atomic_val_t atomic_add(atomic_t *target, atomic_val_t value)
+DECL|atomic_and|function|static inline atomic_val_t atomic_and(atomic_t *target, atomic_val_t value)
+DECL|atomic_cas|function|static inline int atomic_cas(atomic_t *target, atomic_val_t old_value, atomic_val_t new_value)
 DECL|atomic_clear_bit|function|static inline void atomic_clear_bit(atomic_t *addr, int bit)
+DECL|atomic_clear|function|static inline atomic_val_t atomic_clear(atomic_t *target)
+DECL|atomic_dec|function|static inline atomic_val_t atomic_dec(atomic_t *target)
+DECL|atomic_get|function|static inline atomic_val_t atomic_get(const atomic_t *target)
+DECL|atomic_inc|function|static inline atomic_val_t atomic_inc(atomic_t *target)
+DECL|atomic_nand|function|static inline atomic_val_t atomic_nand(atomic_t *target, atomic_val_t value)
+DECL|atomic_or|function|static inline atomic_val_t atomic_or(atomic_t *target, atomic_val_t value)
 DECL|atomic_set_bit|function|static inline void atomic_set_bit(atomic_t *addr, int bit)
+DECL|atomic_set|function|static inline atomic_val_t atomic_set(atomic_t *target, atomic_val_t value)
+DECL|atomic_sub|function|static inline atomic_val_t atomic_sub(atomic_t *target, atomic_val_t value)
 DECL|atomic_test_and_clear_bit|function|static inline int atomic_test_and_clear_bit(atomic_t *addr, int bit)
 DECL|atomic_test_and_set_bit|function|static inline int atomic_test_and_set_bit(atomic_t *addr, int bit)
 DECL|atomic_test_bit|function|static inline int atomic_test_bit(const atomic_t *addr, int bit)
 DECL|atomic_t|typedef|typedef int atomic_t;
 DECL|atomic_val_t|typedef|typedef atomic_t atomic_val_t;
+DECL|atomic_xor|function|static inline atomic_val_t atomic_xor(atomic_t *target, atomic_val_t value)
