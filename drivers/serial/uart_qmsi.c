98a2828eb64e4cbdcf600cd21a1cb21339f55532
Julien Delayen <julien.delayen@intel.com>
2016-07-15 10:52:49 +0100

    uart_qmsi: Implement suspend and resume functions
    In order to restore uart context after entering
    SYS_PM_DEEP_SLEEP, suspend and resume functions
    are called.
    
    The following parameters are restored:
     - All registers for the UART device.
     - UART clocks.
    
    The FIFO control register cannot be read back and is
    not stored in the device configuration.
    The default parameters are applied for them.
    
    The suspend/resume functionality is implemented in
    the QMSI shim layer as a fast and temporary solution,
    it will be removed and migrated to QMSI later.
    
    Change-Id: I4be9246f6aa5a6e0d91df54c1c69574060136607
    Signed-off-by: Julien Delayen <julien.delayen@intel.com>
    
    Former-commit-id: bc60506d3e5d1cb2c46fda3bebe35467d084699b

--- drivers/serial/uart_qmsi.c
+++ drivers/serial/uart_qmsi.c
@@ -4,21 +4,31 @@ DECL|GET_CONTROLLER_INSTANCE|macro|GET_CONTROLLER_INSTANCE
 DECL|IIR_IID_NO_INTERRUPT_PENDING|macro|IIR_IID_NO_INTERRUPT_PENDING
 DECL|api|variable|api
 DECL|baud_divisor|member|uint32_t baud_divisor;
-DECL|cfg_save|member|qm_uart_config_t cfg_save;
 DECL|clock_gate|member|clk_periph_t clock_gate;
 DECL|config_info_0|variable|config_info_0
 DECL|config_info_1|variable|config_info_1
+DECL|ctx_save|member|struct uart_context_t ctx_save;
+DECL|dlf|member|uint32_t dlf; /**< Divisor Latch Fraction. */
+DECL|dlh|member|uint32_t dlh; /**< Divisor Latch High. */
+DECL|dll|member|uint32_t dll; /**< Divisor Latch Low. */
 DECL|drv_data_0|variable|drv_data_0
 DECL|drv_data_1|variable|drv_data_1
+DECL|htx|member|uint32_t htx; /**< Halt Transmission. */
 DECL|hw_fc|member|bool hw_fc;
+DECL|ier|member|uint32_t ier; /**< Interrupt Enable Register. */
 DECL|iir_cache|member|uint8_t iir_cache;
 DECL|iir_cache|member|uint8_t iir_cache;
 DECL|instance|member|qm_uart_t instance;
+DECL|int_uart_mask|member|uint32_t int_uart_mask; /**< Interrupt Mask. */
 DECL|irq_config_func_0|function|static void irq_config_func_0(struct device *dev)
 DECL|irq_config_func_1|function|static void irq_config_func_1(struct device *dev)
 DECL|irq_config_func|member|uart_irq_config_func_t irq_config_func;
 DECL|is_data_ready|function|static bool is_data_ready(qm_uart_t instance)
 DECL|is_tx_fifo_full|function|static bool is_tx_fifo_full(qm_uart_t instance)
+DECL|lcr|member|uint32_t lcr; /**< Line Control. */
+DECL|mcr|member|uint32_t mcr; /**< Modem Control. */
+DECL|scr|member|uint32_t scr; /**< Scratchpad. */
+DECL|uart_context_t|struct|struct uart_context_t {
 DECL|uart_qmsi_config_info|struct|struct uart_qmsi_config_info {
 DECL|uart_qmsi_drv_cmd|function|static int uart_qmsi_drv_cmd(struct device *dev, uint32_t cmd, uint32_t p)
 DECL|uart_qmsi_drv_data|struct|struct uart_qmsi_drv_data {
@@ -41,10 +51,9 @@ DECL|uart_qmsi_irq_tx_ready|function|static int uart_qmsi_irq_tx_ready(struct de
 DECL|uart_qmsi_irq_update|function|static int uart_qmsi_irq_update(struct device *dev)
 DECL|uart_qmsi_isr|function|static void uart_qmsi_isr(void *arg)
 DECL|uart_qmsi_line_ctrl_set|function|static int uart_qmsi_line_ctrl_set(struct device *dev, uint32_t ctrl, uint32_t val)
-DECL|uart_qmsi_pm_save_config|function|void uart_qmsi_pm_save_config(struct device *dev, qm_uart_config_t *cfg)
-DECL|uart_qmsi_pm_save_config|macro|uart_qmsi_pm_save_config
 DECL|uart_qmsi_poll_in|function|static int uart_qmsi_poll_in(struct device *dev, unsigned char *data)
 DECL|uart_qmsi_poll_out|function|static unsigned char uart_qmsi_poll_out(struct device *dev,unsigned char data)
 DECL|uart_resume_device|function|static int uart_resume_device(struct device *dev, int pm_policy)
+DECL|uart_suspend_device|function|static int uart_suspend_device(struct device *dev, int pm_policy)
 DECL|user_cb|member|uart_irq_callback_t user_cb;
 DECL|user_cb|member|uart_irq_callback_t user_cb;
