cdb81ef464c2ac25b6e3d1e3704c8991aa7f9347
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2016-07-18 11:32:42 +0200

    Bluetooth: doc: Fix displacement of storage keys description
    Improves placement of well known storage keys type values description
    in Bluetooth API documentation by moving the comment to separate line
    above the item it describes.
    
    Change-Id: I6a9182d135983c5037cf44a4fda4305abfa4387f
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: cc1d2f6ff719e84594da189d97c816389fdf7faa

--- include/bluetooth/storage.h
+++ include/bluetooth/storage.h
@@ -1,5 +1,5 @@
-DECL|BT_STORAGE_ID_ADDR|enumerator|BT_STORAGE_ID_ADDR, /** Identity Address */
-DECL|BT_STORAGE_LOCAL_IRK|enumerator|BT_STORAGE_LOCAL_IRK, /** Local Identity Resolving Key */
+DECL|BT_STORAGE_ID_ADDR|enumerator|BT_STORAGE_ID_ADDR,
+DECL|BT_STORAGE_LOCAL_IRK|enumerator|BT_STORAGE_LOCAL_IRK,
 DECL|__BT_STORAGE_H|macro|__BT_STORAGE_H
 DECL|bt_storage|struct|struct bt_storage {
 DECL|clear|member|int (*clear)(const bt_addr_le_t *addr);
