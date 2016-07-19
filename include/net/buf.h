597f976730a40798667fb63eaa28c31e311668fb
Johan Hedberg <johan.hedberg@intel.com>
2016-07-19 17:43:44 +0300

    net: buf: Add API for saving & restoring parsing state
    Occasionally it may be useful to pass a buffer to a subroutine and
    have the routine do parsing of the buffer. However, since there isn't
    necessarily a guarantee of how the subroutine performs the parsing it
    may be necessary to restore to a well known state after the routine
    returns.
    
    This patch adds a simple struct for storing the parsing state as well
    as two new functions to save and restore the state.
    
    Change-Id: If9153ff9997021c76243ea9ebff13dfe94c45faa
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: e088b06e44313917f79699665117a13d6e7f748c

--- include/net/buf.h
+++ include/net/buf.h
@@ -15,6 +15,7 @@ DECL|frags|member|struct net_buf *frags;
 DECL|free|member|struct nano_fifo * const free;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
+DECL|len|member|uint16_t len;
 DECL|net_buf_add_be16|macro|net_buf_add_be16
 DECL|net_buf_add_be32|macro|net_buf_add_be32
 DECL|net_buf_add_le16|macro|net_buf_add_le16
@@ -36,12 +37,16 @@ DECL|net_buf_push_le16|macro|net_buf_push_le16
 DECL|net_buf_push_u8|macro|net_buf_push_u8
 DECL|net_buf_push|macro|net_buf_push
 DECL|net_buf_simple_init|function|static inline void net_buf_simple_init(struct net_buf_simple *buf, size_t reserve_head)
+DECL|net_buf_simple_restore|function|static inline void net_buf_simple_restore(struct net_buf_simple *buf, struct net_buf_simple_state *state)
+DECL|net_buf_simple_save|function|static inline void net_buf_simple_save(struct net_buf_simple *buf, struct net_buf_simple_state *state)
+DECL|net_buf_simple_state|struct|struct net_buf_simple_state {
 DECL|net_buf_simple_tail|function|static inline uint8_t *net_buf_simple_tail(struct net_buf_simple *buf)
 DECL|net_buf_simple|struct|struct net_buf_simple {
 DECL|net_buf_tailroom|macro|net_buf_tailroom
 DECL|net_buf_tail|macro|net_buf_tail
 DECL|net_buf_user_data|function|static inline void *net_buf_user_data(struct net_buf *buf)
 DECL|net_buf|struct|struct net_buf {
+DECL|offset|member|uint16_t offset;
 DECL|ref|member|uint8_t ref;
 DECL|size|member|const uint16_t size;
 DECL|size|member|const uint16_t size;
