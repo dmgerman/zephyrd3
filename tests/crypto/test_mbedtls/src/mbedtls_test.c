e660b68e4ac4d9fd59ec97e16c40a82d118dfa0c
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-07-19 13:35:25 -0700

    samples/net : Adding mbedTLS self test routine
    This contain the run of the self test for the ciphers, crypto
    algorithms and utilities of mbedTLS.
    
    Jira: ZEP-327
    Jira: ZEP-340
    Jira: ZEP-495
    
    Origin: https://tls.mbed.org/download/start/mbedtls-2.3.0-apache.tgz
    
    Change-Id: Ic1bb30b7ed691f17421510cd914ec5096e4e70ea
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: 950f45b5321332b15c0f26a7ddfed567c9416f29

---
+++ tests/crypto/test_mbedtls/src/mbedtls_test.c
@@ -0,0 +1,13 @@
+DECL|MBEDTLS_EXIT_FAILURE|macro|MBEDTLS_EXIT_FAILURE
+DECL|MBEDTLS_EXIT_SUCCESS|macro|MBEDTLS_EXIT_SUCCESS
+DECL|MBEDTLS_PRINT|macro|MBEDTLS_PRINT
+DECL|MBEDTLS_PRINT|macro|MBEDTLS_PRINT
+DECL|buf|variable|buf
+DECL|create_entropy_seed_file|function|static void create_entropy_seed_file(void)
+DECL|main|function|int main(void)
+DECL|mbedtls_exit|macro|mbedtls_exit
+DECL|mbedtls_printf|macro|mbedtls_printf
+DECL|mbedtls_snprintf|macro|mbedtls_snprintf
+DECL|rand|function|int rand(void)
+DECL|run_test_snprintf|function|static int run_test_snprintf(void)
+DECL|test_snprintf|function|static int test_snprintf(size_t n, const char ref_buf[10], int ref_ret)
