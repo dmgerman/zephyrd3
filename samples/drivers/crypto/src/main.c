bff30a70d6c10a121e8d4bbe966fa897563f4565
Jithu Joseph <jithu.joseph@intel.com>
2016-07-19 15:00:38 -0700

    samples/drivers/crypto: crypto sample app
    A sample application  to illustrate the usage of crypto APIs.
    This shows the usage of AES - CTR, CBC and CCM  based encryption
    and decryption.
    
    origin: original
    
    Change-Id: I17e4d966a70169b71a754c9cdc3f713a5d0c3ac0
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 38aaa7c95574696e42692ece03cccb63c71af686

---
+++ samples/drivers/crypto/src/main.c
@@ -0,0 +1,17 @@
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|cap_flags|variable|cap_flags
+DECL|cbc_mode|function|void cbc_mode(void)
+DECL|ccm_data|variable|ccm_data
+DECL|ccm_expected|variable|ccm_expected
+DECL|ccm_hdr|variable|ccm_hdr
+DECL|ccm_key|variable|ccm_key
+DECL|ccm_mode|function|void ccm_mode(void)
+DECL|ccm_nonce|variable|ccm_nonce
+DECL|ciphertext|variable|ciphertext
+DECL|ctr_ciphertext|variable|ctr_ciphertext
+DECL|ctr_mode|function|void ctr_mode(void)
+DECL|iv|variable|iv
+DECL|key|variable|key
+DECL|main|function|void main(void)
+DECL|plaintext|variable|plaintext
+DECL|validate_hw_compatibility|function|int validate_hw_compatibility(struct device *dev)
