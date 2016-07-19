75f506f119ee0574d8b01cd8fdbe105e8b9791a6
Jithu Joseph <jithu.joseph@intel.com>
2016-07-19 14:20:25 -0700

    drivers/crypto: TinyCrypt shim driver
    Shim layer interfacing between crypto APIs and TinyCrypt
    library. Currently facilitates only a subset of TinyCrypt
    features/algorithms.
    
    Jira: ZEP-509
    
    Change-Id: I7fe6b9d86df016d92d717378d08a1ab09caafb31
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: de106b026a5a540dda40df30478b8d80b3f92a02

---
+++ drivers/crypto/tc_shim.c
@@ -0,0 +1,15 @@
+DECL|CRYPTO_MAX_SESSION|macro|CRYPTO_MAX_SESSION
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|SYS_LOG_NO_NEWLINE|macro|SYS_LOG_NO_NEWLINE
+DECL|crypto_enc_funcs|variable|crypto_enc_funcs
+DECL|do_cbc_decrypt|function|static int do_cbc_decrypt(struct cipher_ctx *ctx, struct cipher_pkt *op, uint8_t *iv)
+DECL|do_cbc_encrypt|function|static int do_cbc_encrypt(struct cipher_ctx *ctx, struct cipher_pkt *op, uint8_t *iv)
+DECL|do_ccm_decrypt_auth|function|static int do_ccm_decrypt_auth(struct cipher_ctx *ctx, struct cipher_aead_pkt *aead_op, uint8_t *nonce)
+DECL|do_ccm_encrypt_mac|function|static int do_ccm_encrypt_mac(struct cipher_ctx *ctx, struct cipher_aead_pkt *aead_op, uint8_t *nonce)
+DECL|do_ctr_op|function|static int do_ctr_op(struct cipher_ctx *ctx, struct cipher_pkt *op, uint8_t *iv)
+DECL|get_unused_session|function|static int get_unused_session(void)
+DECL|tc_driver_state|variable|tc_driver_state
+DECL|tc_query_caps|function|int tc_query_caps(struct device *dev)
+DECL|tc_session_free|function|int tc_session_free(struct device *dev, struct cipher_ctx *sessn)
+DECL|tc_session_setup|function|int tc_session_setup(struct device *dev, struct cipher_ctx *ctx, enum cipher_algo algo, enum cipher_mode mode, enum cipher_op op_type)
+DECL|tc_shim_init|function|static int tc_shim_init(struct device *dev)
---
+++ drivers/crypto/tc_shim_priv.h
@@ -0,0 +1,4 @@
+DECL|__TC_SHIM_PRIV_H__|macro|__TC_SHIM_PRIV_H__
+DECL|in_use|member|int in_use;
+DECL|session_key|member|struct tc_aes_key_sched_struct session_key;
+DECL|tc_shim_drv_state|struct|struct tc_shim_drv_state {
