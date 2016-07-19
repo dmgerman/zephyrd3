ba61caed53d9e149284b5208ffb6b3d6f2684cb5
Jithu Joseph <jithu.joseph@intel.com>
2016-07-19 08:15:52 -0700

    include/crypto: Crypto abstraction header
    Adds crypto API interface for interaction between applications
    and crypto drivers. Encryption/Decryption APIs are defined
    here.
    
    Jira: ZEP-328
    
    Change-Id: I1a534ae2a69c7e1c416fa78a2822c37040b225f6
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 417c3410111766f5d103b7720d7a1026a2488072

---
+++ include/crypto/cipher.h
@@ -0,0 +1,14 @@
+DECL|__CRYPTO_CIPHER_H__|macro|__CRYPTO_CIPHER_H__
+DECL|begin_session|member|int (*begin_session)(struct device *dev, struct cipher_ctx *ctx,
+DECL|cipher_begin_session|function|static inline int cipher_begin_session(struct device *dev, struct cipher_ctx *ctx, enum cipher_algo algo, enum cipher_mode mode, enum cipher_op optype)
+DECL|cipher_block_op|function|static inline int cipher_block_op(struct cipher_ctx *ctx, struct cipher_pkt *pkt)
+DECL|cipher_callback_set|function|static inline int cipher_callback_set(struct device *dev, crypto_completion_cb cb)
+DECL|cipher_cbc_op|function|static inline int cipher_cbc_op(struct cipher_ctx *ctx,struct cipher_pkt *pkt, uint8_t *iv)
+DECL|cipher_ccm_op|function|static inline int cipher_ccm_op(struct cipher_ctx *ctx,struct cipher_aead_pkt *pkt, uint8_t *nonce)
+DECL|cipher_ctr_op|function|static inline int cipher_ctr_op(struct cipher_ctx *ctx,struct cipher_pkt *pkt, uint8_t *iv)
+DECL|cipher_free_session|function|static inline int cipher_free_session(struct device *dev, struct cipher_ctx *ctx)
+DECL|cipher_query_hwcaps|function|static inline int cipher_query_hwcaps(struct device *dev)
+DECL|crypto_async_callback_set|member|int (*crypto_async_callback_set)(struct device *dev,
+DECL|crypto_driver_api|struct|struct crypto_driver_api {
+DECL|free_session|member|int (*free_session)(struct device *dev, struct cipher_ctx *ctx);
+DECL|query_hw_caps|member|int (*query_hw_caps)(struct device *dev);
---
+++ include/crypto/cipher_structs.h
@@ -0,0 +1,61 @@
+DECL|CAP_ASYNC_OPS|macro|CAP_ASYNC_OPS
+DECL|CAP_AUTONONCE|macro|CAP_AUTONONCE
+DECL|CAP_INPLACE_OPS|macro|CAP_INPLACE_OPS
+DECL|CAP_KEY_LOADING_API|macro|CAP_KEY_LOADING_API
+DECL|CAP_OPAQUE_KEY_HNDL|macro|CAP_OPAQUE_KEY_HNDL
+DECL|CAP_RAW_KEY|macro|CAP_RAW_KEY
+DECL|CAP_SEPARATE_IO_BUFS|macro|CAP_SEPARATE_IO_BUFS
+DECL|CAP_SYNC_OPS|macro|CAP_SYNC_OPS
+DECL|CRYPTO_CIPHER_ALGO_AES|enumerator|CRYPTO_CIPHER_ALGO_AES = 1,
+DECL|CRYPTO_CIPHER_MODE_CBC|enumerator|CRYPTO_CIPHER_MODE_CBC = 2,
+DECL|CRYPTO_CIPHER_MODE_CCM|enumerator|CRYPTO_CIPHER_MODE_CCM = 4,
+DECL|CRYPTO_CIPHER_MODE_CTR|enumerator|CRYPTO_CIPHER_MODE_CTR = 3,
+DECL|CRYPTO_CIPHER_MODE_ECB|enumerator|CRYPTO_CIPHER_MODE_ECB = 1,
+DECL|CRYPTO_CIPHER_OP_DECRYPT|enumerator|CRYPTO_CIPHER_OP_DECRYPT = 0,
+DECL|CRYPTO_CIPHER_OP_ENCRYPT|enumerator|CRYPTO_CIPHER_OP_ENCRYPT = 1,
+DECL|__CRYPTO_CIPHER_STRUCTS_H__|macro|__CRYPTO_CIPHER_STRUCTS_H__
+DECL|ad_len|member|uint32_t ad_len;
+DECL|ad|member|uint8_t *ad;
+DECL|app_sessn_state|member|void *app_sessn_state;
+DECL|bit_stream|member|uint8_t *bit_stream;
+DECL|block_crypt_hndlr|member|block_op_t block_crypt_hndlr;
+DECL|block_op_t|typedef|typedef int (*block_op_t)(struct cipher_ctx *ctx, struct cipher_pkt *pkt);
+DECL|cbc_crypt_hndlr|member|cbc_op_t cbc_crypt_hndlr;
+DECL|cbc_op_t|typedef|typedef int (*cbc_op_t)(struct cipher_ctx *ctx, struct cipher_pkt *pkt,
+DECL|ccm_crypt_hndlr|member|ccm_op_t ccm_crypt_hndlr;
+DECL|ccm_info|member|struct ccm_params ccm_info;
+DECL|ccm_op_t|typedef|typedef int (*ccm_op_t)(struct cipher_ctx *ctx, struct cipher_aead_pkt *pkt,
+DECL|ccm_params|struct|struct ccm_params {
+DECL|cipher_aead_pkt|struct|struct cipher_aead_pkt {
+DECL|cipher_algo|enum|enum cipher_algo {
+DECL|cipher_ctx|struct|struct cipher_ctx {
+DECL|cipher_mode|enum|enum cipher_mode {
+DECL|cipher_mode|member|enum cipher_mode cipher_mode;
+DECL|cipher_ops|struct|struct cipher_ops {
+DECL|cipher_op|enum|enum cipher_op {
+DECL|cipher_pkt|struct|struct cipher_pkt {
+DECL|crypto_completion_cb|typedef|typedef void (*crypto_completion_cb)(struct cipher_pkt *completed, int status);
+DECL|ctr_crypt_hndlr|member|ctr_op_t ctr_crypt_hndlr;
+DECL|ctr_info|member|struct ctr_params ctr_info;
+DECL|ctr_len|member|uint32_t ctr_len;
+DECL|ctr_op_t|typedef|typedef int (*ctr_op_t)(struct cipher_ctx *ctx, struct cipher_pkt *pkt,
+DECL|ctr_params|struct|struct ctr_params {
+DECL|ctx|member|struct cipher_ctx *ctx;
+DECL|device|member|struct device *device;
+DECL|drv_sessn_state|member|void *drv_sessn_state;
+DECL|flags|member|uint16_t flags;
+DECL|handle|member|void *handle;
+DECL|in_buf|member|uint8_t *in_buf;
+DECL|in_len|member|int in_len;
+DECL|keylen|member|uint16_t keylen;
+DECL|key|member|} key;
+DECL|mode_params|member|} mode_params;
+DECL|nonce_len|member|uint16_t nonce_len;
+DECL|ops|member|struct cipher_ops ops;
+DECL|out_buf_max|member|int out_buf_max;
+DECL|out_buf|member|uint8_t *out_buf;
+DECL|out_len|member|int out_len;
+DECL|pkt|member|struct cipher_pkt *pkt;
+DECL|status|member|uint8_t status;
+DECL|tag_len|member|uint16_t tag_len;
+DECL|tag|member|uint8_t *tag;
