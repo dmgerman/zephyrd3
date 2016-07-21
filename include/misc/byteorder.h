493d08ec237a2fd5c51adcc1bd9ca70ded30d4cd
Johan Hedberg <johan.hedberg@intel.com>
2016-07-21 08:18:56 +0300

    misc/byteorder.h: Add helpers to get & put unaligned integers
    These helpers provide a way to read and write data buffers containing
    multi-byte integers with a specific endianness. They are particularly
    convenient for network protocol encoding and decoding.
    
    Change-Id: I09ad125a1bbc4510b97a1d75c83cb70d893e22e8
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: cac17e858ff9de473e8079e9c50906ba05b791c1

--- include/misc/byteorder.h
+++ include/misc/byteorder.h
@@ -12,7 +12,15 @@ DECL|sys_cpu_to_le16|macro|sys_cpu_to_le16
 DECL|sys_cpu_to_le16|macro|sys_cpu_to_le16
 DECL|sys_cpu_to_le32|macro|sys_cpu_to_le32
 DECL|sys_cpu_to_le32|macro|sys_cpu_to_le32
+DECL|sys_get_be16|function|static inline uint16_t sys_get_be16(const uint8_t src[2])
+DECL|sys_get_be32|function|static inline uint32_t sys_get_be32(const uint8_t src[4])
+DECL|sys_get_le16|function|static inline uint16_t sys_get_le16(const uint8_t src[2])
+DECL|sys_get_le32|function|static inline uint32_t sys_get_le32(const uint8_t src[4])
 DECL|sys_le16_to_cpu|macro|sys_le16_to_cpu
 DECL|sys_le16_to_cpu|macro|sys_le16_to_cpu
 DECL|sys_le32_to_cpu|macro|sys_le32_to_cpu
 DECL|sys_le32_to_cpu|macro|sys_le32_to_cpu
+DECL|sys_put_be16|function|static inline void sys_put_be16(uint16_t val, uint8_t dst[2])
+DECL|sys_put_be32|function|static inline void sys_put_be32(uint32_t val, uint8_t dst[4])
+DECL|sys_put_le16|function|static inline void sys_put_le16(uint16_t val, uint8_t dst[2])
+DECL|sys_put_le32|function|static inline void sys_put_le32(uint32_t val, uint8_t dst[4])
