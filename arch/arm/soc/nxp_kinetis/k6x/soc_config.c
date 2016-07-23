3dcb6cb252d1644debfcc03969cdca670641caa4
Maureen Helm <maureen.helm@nxp.com>
2016-07-23 10:13:01 -0500

    nxp_kinetis: Refactor K64F init to use CMSIS register accesses
    The K64F SoC initialization previously used macros and structs
    custom-defined in Zephyr in order to access peripheral registers.
    Refactored it to use CMSIS-style register accesses from the ksdk
    instead.
    
    Change-Id: I80975c62de07ec95cf830e99cd5b0abb9623acd0
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: dc631750fa79e95a73924bf214841a2df78cdb47

--- arch/arm/soc/nxp_kinetis/k6x/soc_config.c
+++ arch/arm/soc/nxp_kinetis/k6x/soc_config.c
@@ -1,3 +1,4 @@
+DECL|ports|variable|ports
 DECL|uart_k20_console_init|function|static ALWAYS_INLINE int uart_k20_console_init(void)
 DECL|uart_k20_console_init|macro|uart_k20_console_init
 DECL|uart_k20_init|function|static int uart_k20_init(struct device *dev)
