abb7c2d9e351c71a272f293875cee0697b74f9b2
Ramesh Thomas <ramesh.thomas@intel.com>
2016-07-22 17:48:59 -0700

    fs: Add Zephyr File System API
    Adds the header files containing Zephyr File System API
    descriptions and interfaces.
    
    Origin: Original
    Jira: ZEP-228 ZEP-480
    Change-Id: I76a000187a133df7a82329af78aec9ea6511915c
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: c9ec4ee452007294392543d6e916ef7954abb8f7

---
+++ include/fs.h
@@ -0,0 +1,13 @@
+DECL|DIR_ENTRY_DIR|enumerator|DIR_ENTRY_DIR
+DECL|DIR_ENTRY_FILE|enumerator|DIR_ENTRY_FILE,
+DECL|SEEK_CUR|macro|SEEK_CUR
+DECL|SEEK_END|macro|SEEK_END
+DECL|SEEK_SET|macro|SEEK_SET
+DECL|ZDIR|typedef|typedef struct _zdir_object ZDIR;
+DECL|ZFILE|typedef|typedef struct _zfile_object ZFILE;
+DECL|_FS_H_|macro|_FS_H_
+DECL|dir_entry_type|enum|enum dir_entry_type {
+DECL|name|member|char name[MAX_FILE_NAME + 1]; /* Name of directory or file */
+DECL|size|member|size_t size; /* Size of file. 0 if directory */
+DECL|type|member|enum dir_entry_type type; /* Whether file or directory */
+DECL|zfs_dirent|struct|struct zfs_dirent {
