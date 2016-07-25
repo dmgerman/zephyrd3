962d30ceb32adfbf5bc84028a32c16f85b3dfa90
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2016-07-25 15:52:24 -0700

    qmsi: pwm: Use locking mechanism to guard critical regions
    Guard critical section of the driver API, so that multiple
    simultaneous calls from tasks/fibers won't corrupt the driver
    state.
    
    The locking mechanism is by default disabled. To enable it,
    the following flag needs to be defined:
    
    CONFIG_PWM_QMSI_API_REENTRANCY
    
    Jira: ZEP-430
    
    Change-Id: Ia3eb2a962f4176a3ac94163b9843ab068abe4b4e
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    
    Former-commit-id: 6911589c13e00eafc7670a8722fdf70b99055c32

--- drivers/pwm/pwm_qmsi.c
+++ drivers/pwm/pwm_qmsi.c
@@ -5,8 +5,14 @@ DECL|MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES
 DECL|MAX_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_PERIOD_IN_HW_CLOCK_CYCLES
 DECL|MAX_PERIOD|macro|MAX_PERIOD
 DECL|MIN_PERIOD|macro|MIN_PERIOD
-DECL|__set_one_port|function|static int __set_one_port(qm_pwm_t id, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|PWM_CONTEXT|macro|PWM_CONTEXT
+DECL|PWM_CONTEXT|macro|PWM_CONTEXT
+DECL|__set_one_port|function|static int __set_one_port(struct device *dev, qm_pwm_t id, uint32_t pwm,uint32_t on, uint32_t off)
 DECL|pwm_channel_period|variable|pwm_channel_period
+DECL|pwm_context|variable|pwm_context
+DECL|pwm_critical_region_end|function|static void pwm_critical_region_end(struct device *dev)
+DECL|pwm_critical_region_start|function|static void pwm_critical_region_start(struct device *dev)
+DECL|pwm_data|struct|struct pwm_data {
 DECL|pwm_qmsi_configure|function|static int pwm_qmsi_configure(struct device *dev, int access_op, uint32_t pwm, int flags)
 DECL|pwm_qmsi_drv_api_funcs|variable|pwm_qmsi_drv_api_funcs
 DECL|pwm_qmsi_init|function|static int pwm_qmsi_init(struct device *dev)
@@ -15,3 +21,7 @@ DECL|pwm_qmsi_set_duty_cycle|function|static int pwm_qmsi_set_duty_cycle(struct 
 DECL|pwm_qmsi_set_period|function|static int pwm_qmsi_set_period(struct device *dev, int access_op, uint32_t pwm, uint32_t period)
 DECL|pwm_qmsi_set_values|function|static int pwm_qmsi_set_values(struct device *dev, int access_op, uint32_t pwm, uint32_t on, uint32_t off)
 DECL|pwm_qmsi_suspend|function|static int pwm_qmsi_suspend(struct device *dev)
+DECL|pwm_reentrancy_init|function|static void pwm_reentrancy_init(struct device *dev)
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|reentrancy_protection|variable|reentrancy_protection
+DECL|sem|member|struct nano_sem sem;
