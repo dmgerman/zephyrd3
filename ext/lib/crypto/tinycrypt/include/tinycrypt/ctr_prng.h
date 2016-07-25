3687bf85bc027fcd1cec30f294deddfb7a7dc831
Flavio Santes <flavio.santes@intel.com>
2016-07-24 23:52:50 -0500

    crypto: Update tinycrypt source files
    Update Zephyr's tinycrypt to version 2.0. This new version adds support
    for ECC_DH, ECC_DSA, CMAC, & CTR_PRNG.
    
    The following doxygen documentation typos were detected and fixed:
    
    - ctr_prng.h:84 change plen by pLen,
    - ctr_prng.h:109 change entropylen by entropyLen,
    - sha256.h:110 change Sha256 by s.
    
    ecc_dh.h is also modified to fix the discrepancy of ecc_make_key
    definition and declaration.
    See https://gerrit.zephyrproject.org/r/#/c/1982/
    
    TC_FAIL and TC_SUCCESS defines are renamed in this new version of
    tinycrypt, so net/bluetooth/hci_core.c, net/bluetooth/hci_ecc.c and
    net/bluetooth/smp.c are also updated to reflect those changes.
    
    Origin: https://github.com/01org/tinycrypt/archive/v0.2.0.tar.gz
    
    Jira: ZEP-590
    
    Change-Id: I85f4f0ab61d9b0be6a60897e2b96f245dd8c51a8
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: e6ffc0ed289acf8aab11bc7b9aa5803d378e5159

---
+++ ext/lib/crypto/tinycrypt/include/tinycrypt/ctr_prng.h
@@ -0,0 +1,6 @@
+DECL|TCCtrPrng_t|typedef|} TCCtrPrng_t;
+DECL|TC_CTR_PRNG_RESEED_REQ|macro|TC_CTR_PRNG_RESEED_REQ
+DECL|V|member|uint8_t V[TC_AES_BLOCK_SIZE];
+DECL|__TC_CTR_PRNG_H__|macro|__TC_CTR_PRNG_H__
+DECL|key|member|struct tc_aes_key_sched_struct key;
+DECL|reseedCount|member|uint64_t reseedCount;
