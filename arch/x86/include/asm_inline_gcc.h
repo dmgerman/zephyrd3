ce7b3479e10d1bb1aeb0960c6e6a9b0d5fa9f345
Andrew Boie <andrew.p.boie@intel.com>
2016-07-26 14:55:41 -0700

    x86: improve exception APIs
    Previously, exception stubs had to be declared in assembly
    language files. Now we have two new APIs to regsiter exception
    handlers at C toplevel:
    
     _EXCEPTION_CONNECT_CODE(handler, vector)
     _EXCEPTION_CONNECT_NOCODE(handler, vector)
    
    For x86 exceptions that do and do not push error codes onto
    the stack respectively.
    
    In addition, it's now no longer necessary to #define around
    exception registration. We now use .gnu.linkonce magic such that
    the first _EXCEPTION_CONNECT_*() that the linker finds is used
    for the specified vector. Applications are free to install their
    own exception handlers which will take precedence over default
    handlers such as installed by arch/x86/core/fatal.c
    
    Some Makefiles have been adjusted so that the default exception
    handlers in arch/x86/core/fatal.c are linked last. The code has
    been tested that the right order of precedence is taken for
    exceptions overridden in the floating point, gdb debug, or
    application code. The asm SYS_NANO_CPU_EXC_CONNECT API has been
    removed; it was ill- conceived as it only worked for exceptions
    that didn't push error codes. All the asm NANO_CPU_EXC_CONNECT_*
    APIs are gone as well in favor of the new _EXCEPTION_CONNNECT_*()
    APIs.
    
    CONFIG_EXCEPTION_DEBUG no longer needs to be disabled for test
    cases that define their own exception handlers.
    
    Issue: ZEP-203
    Change-Id: I782e0143fba832d18cdf4daaa7e47820595fe041
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 17c0b372a2f8a8e9cc5967a49e37b43419dccb48

--- arch/x86/include/asm_inline_gcc.h
+++ arch/x86/include/asm_inline_gcc.h
@@ -1,5 +1,4 @@
 DECL|EflagsGet|function|static inline unsigned int EflagsGet(void)
-DECL|SYS_NANO_CPU_EXC_CONNECT|macro|SYS_NANO_CPU_EXC_CONNECT
 DECL|_ASM_INLINE_GCC_H|macro|_ASM_INLINE_GCC_H
 DECL|_FpAccessDisable|function|static inline void _FpAccessDisable(void)
 DECL|_do_fp_ctx_init|function|static inline void _do_fp_ctx_init(int flags)
