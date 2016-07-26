974ec7b739f96471cc2dccf5f9a93122e0623f05
Flavio Santes <flavio.santes@intel.com>
2016-07-26 14:55:24 -0500

    samples/net: MQTT subscriber with QoS
    This sample demostrates how to write a subscriber application that is
    able to receive messages with different Quality of Service values.
    
    The code is self-documented. Read the README file for more information
    about the network setup.
    
    This sample code uses the Network for Zephyr API (netz).
    
    More details about MQTT can be found in the official documentation:
    http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/mqtt-v3.1.1.html
    
    Jira: ZEP-416
    Jira: ZEP-568
    Jira: ZEP-573
    Jira: ZEP-594
    
    Change-Id: Ia660313439c0c61c2210abbf8ce09ff278d11a1a
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: bbd4d4cb1f0082215c176d5a8fbd3b86ba900254

---
+++ samples/net/paho_mqtt_clients/subscriber/src/main.c
@@ -0,0 +1,10 @@
+DECL|BUF_SIZE|macro|BUF_SIZE
+DECL|RC_STR|macro|RC_STR
+DECL|SLEEP_TIME|macro|SLEEP_TIME
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|cb_publish|function|int cb_publish(struct mqtt_msg_t *msg)
+DECL|fiber|function|void fiber(void)
+DECL|main|function|void main(void)
+DECL|rx_raw_buf|variable|rx_raw_buf
+DECL|stack|variable|stack
+DECL|tx_raw_buf|variable|tx_raw_buf
