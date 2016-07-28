840cd113cb5bfdd6d9aa9065593945ef9aeaeb4b
Andrew Boie <andrew.p.boie@intel.com>
2016-07-28 07:33:00 -0700

    Revert "samples/net: Add netz_client sample code"
    This reverts commit cb91e1932236ff7829ed84f67957a49faea5691a [formerly a3c0b2c00ff82c12ee4f3bbbab8c0315ec4ec204].
    The status of the 'netz' library is still under
    consideration.
    
    Change-Id: Ifde93465da306d86aec8a71d02a16653eb7cef9c
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 18aac630ef2e6731c3d47407cb17151bb3a872c2

--- samples/net/netz_client/src/main.c
+++
@@ -1,8 +0,0 @@
-DECL|BUF_SIZE|macro|BUF_SIZE
-DECL|STACK_SIZE|macro|STACK_SIZE
-DECL|fiber|function|void fiber(void)
-DECL|main|function|void main(void)
-DECL|print_buf|function|void print_buf(struct app_buf_t *buf)
-DECL|rx_raw_buf|variable|rx_raw_buf
-DECL|stack|variable|stack
-DECL|tx_raw_buf|variable|tx_raw_buf
