46d2a0ef8774cb9b4640e81c3d9a270085f9cc3f
Ramesh Thomas <ramesh.thomas@intel.com>
2016-07-29 02:24:37 -0700

    fs: Adds the glue layer for the Fat FS module
    Adds the glue layer to interface the Fat FS module with Zephyr
    file system API.
    
    Origin: Original
    Jira: ZEP-228 ZEP-483
    Change-Id: I878fdad8ecaf46992d6163e24a9b0b633f49154f
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: e89c739e6ff06c756b6ea246b8b4f13b5dfcff95

---
+++ include/fs/fat_fs.h
@@ -0,0 +1,5 @@
+DECL|MAX_FILE_NAME|macro|MAX_FILE_NAME
+DECL|_FAT_FS_H_|macro|_FAT_FS_H_
+DECL|dp|variable|dp
+DECL|fp|variable|fp
+DECL|fs_tell|function|static inline off_t fs_tell(struct _zfile_object *zfp)
