9c52c1fbeabd585f07fe95f8d89ae6386cc58e9a
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-07-28 21:31:53 -0700

    samples/net : Adding mbedTLS sample client
    This is a client that uses shared keys to execute a TLS handshake and read
    info from the server, information on how to run the test is in the README
    file.
    
    Jira: ZEP-327
    Jira: ZEP-340
    Jira: ZEP-495
    
    Origin: https://tls.mbed.org/download/start/mbedtls-2.3.0-apache.tgz
    
    Change-Id: I10f31f3635f346936807b7c8470b3d6ffb3af283
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 259c24397def2443bde55f9d9a9c8062f2290a38

---
+++ samples/net/mbedtls_sslclient/src/tcp.h
@@ -0,0 +1,5 @@
+DECL|_TCP_H_|macro|_TCP_H_
+DECL|net_ctx|member|struct net_context *net_ctx;
+DECL|remaining|member|int remaining;
+DECL|rx_nbuf|member|struct net_buf *rx_nbuf;
+DECL|tcp_context|struct|struct tcp_context {
