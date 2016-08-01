c12240d63572127a6842cef0a02900cf4087e1ff
Andrew Boie <andrew.p.boie@intel.com>
2016-08-01 16:53:32 -0700

    arc: remove deprecated dynamic interrupt implementation
    Change-Id: I7a81123f72458f43d3e8e1ca04a921ae6c5f1f6e
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 3a1cf9af2ca20efff4beb50844f8c28a667aee6f

--- arch/arc/core/irq_manage.c
+++ arch/arc/core/irq_manage.c
@@ -1,7 +1,4 @@
-DECL|_arch_irq_connect_dynamic|function|int _arch_irq_connect_dynamic(unsigned int irq, unsigned int prio, void (*isr)(void *arg), void *arg,
 DECL|_arch_irq_disable|function|void _arch_irq_disable(unsigned int irq)
 DECL|_arch_irq_enable|function|void _arch_irq_enable(unsigned int irq)
-DECL|_irq_disconnect|function|void _irq_disconnect(unsigned int irq)
-DECL|_irq_handler_set|function|void _irq_handler_set(unsigned int irq, void (*new)(void *arg), void *arg )
 DECL|_irq_priority_set|function|void _irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
 DECL|_irq_spurious|function|void _irq_spurious(void *unused)
