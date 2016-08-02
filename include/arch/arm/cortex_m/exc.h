fb5e78e40238b6b6e1b9def9fc8bdf6bc75ed2e4
Andrew Boie <andrew.p.boie@intel.com>
2016-08-01 17:15:05 -0700

    arm: remove dynamic IRQs and exceptions
    Change-Id: I8fea235aff6b7cb7da07b491ba39ea383709b57f
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 844e21269c8db1007849329e52fffc3ea597e9fd

--- include/arch/arm/cortex_m/exc.h
+++ include/arch/arm/cortex_m/exc.h
@@ -3,8 +3,6 @@ DECL|_ARCH_ARM_CORTEXM_EXC_H_|macro|_ARCH_ARM_CORTEXM_EXC_H_
 DECL|_ExcExit|variable|_ExcExit
 DECL|__esf|struct|struct __esf {
 DECL|fpscr|member|uint32_t fpscr;
-DECL|sys_exc_handler_sig|macro|sys_exc_handler_sig
-DECL|sys_exc_handler_t|typedef|typedef sys_exc_handler_sig(sys_exc_handler_t);
 DECL|s|member|float s[16];
 DECL|undefined|member|uint32_t undefined;
 DECL|xpsr|member|uint32_t xpsr;
