2741fbffcbaad2cc11e30a3049aacb56954ee897
Juan Manuel Cruz Alcaraz <juan.m.cruz.alcaraz@intel.com>
2016-08-02 22:08:03 -0500

    misc: Remove generic PRINT macros from pci samples
    Remove generic PRINT macros from pci samples and replace
    with SYS_LOG  module.
    
    Jira: ZEP-240
    
    Change-Id: I5a556637c441b1a4117eb75463d66f7127355a59
    Signed-off-by: Juan Manuel Cruz Alcaraz <juan.m.cruz.alcaraz@intel.com>
    
    Former-commit-id: a0007012b7864e9f90991d6f70f049fa9dbc048d

--- samples/drivers/pci_enum/src/pci_enum.c
+++ samples/drivers/pci_enum/src/pci_enum.c
@@ -1,5 +1,4 @@
-DECL|PRINT|macro|PRINT
-DECL|PRINT|macro|PRINT
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|done|variable|done
 DECL|main|function|void main(void)
 DECL|pci_enumerate|function|void pci_enumerate(void)
