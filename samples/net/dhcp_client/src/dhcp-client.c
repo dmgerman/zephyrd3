289d58cbe2891f6bd0ba7db02722eb2f1b811118
Juan Manuel Cruz Alcaraz <juan.m.cruz.alcaraz@intel.com>
2016-08-02 22:00:29 -0500

    misc: Remove generic PRINT macros from net samples
    Remove generic PRINT macros from net samples and replace
    with SYS_LOG  module.
    
    Jira: ZEP-240
    
    Change-Id: I4bec179c99056cce41e1f3495c162f25c3cd2364
    Signed-off-by: Juan Manuel Cruz Alcaraz <juan.m.cruz.alcaraz@intel.com>
    
    Former-commit-id: 4dc93fed2f80d21adbfec0f4523fcab30059edc9

--- samples/net/dhcp_client/src/dhcp-client.c
+++ samples/net/dhcp_client/src/dhcp-client.c
@@ -1,5 +1,4 @@
-DECL|PRINT|macro|PRINT
-DECL|PRINT|macro|PRINT
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|dhcpc_configured_cb|function|static void dhcpc_configured_cb(void)
 DECL|dhcpc_unconfigured_cb|function|static void dhcpc_unconfigured_cb(void)
 DECL|main|function|void main(void)
