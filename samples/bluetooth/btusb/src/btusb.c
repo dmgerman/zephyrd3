90309d5d0186a52487a00f8e186b980c5dced79f
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-08-08 10:43:36 +0300

    Bluetooth: Export USB HCI controller using RAW HCI channel
    The application exports Bluetooth controller over USB as USB HCI
    dongle using RAW HCI channel introduced in the previous patch.
    
    When connected to Linux is should be handled by btusb driver.
    
    usb-devices output:
    
    T:  Bus=05 Lev=03 Prnt=121 Port=00 Cnt=01 Dev#= 22 Spd=12  MxCh= 0
    D:  Ver= 1.10 Cls=e0(wlcon) Sub=01 Prot=01 MxPS=64 #Cfgs=  1
    P:  Vendor=8086 ProdID=ff02 Rev=01.00
    S:  Manufacturer=Intel
    S:  Product=BTUSB12
    S:  SerialNumber=00.01
    C:  #Ifs= 2 Cfg#= 1 Atr=c0 MxPwr=100mA
    I:  If#= 0 Alt= 0 #EPs= 3 Cls=e0(wlcon) Sub=01 Prot=01 Driver=btusb
    I:  If#= 1 Alt= 0 #EPs= 2 Cls=e0(wlcon) Sub=01 Prot=01 Driver=btusb
    
    Change-Id: I8bc362876353b6c97eb7051f4cdff3d8a9f55abd
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: e8abb72a6a82793cd9dc3f32129e8c4c6896ebee

---
+++ samples/bluetooth/btusb/src/btusb.c
@@ -0,0 +1,55 @@
+DECL|BTUSB_BUFFER_SIZE|macro|BTUSB_BUFFER_SIZE
+DECL|BTUSB_BULK_EP_MPS|macro|BTUSB_BULK_EP_MPS
+DECL|BTUSB_CLASS_MAX_DATA_SIZE|macro|BTUSB_CLASS_MAX_DATA_SIZE
+DECL|BTUSB_CONF_SIZE|macro|BTUSB_CONF_SIZE
+DECL|BTUSB_ENDP_BULK_IN|macro|BTUSB_ENDP_BULK_IN
+DECL|BTUSB_ENDP_BULK_OUT|macro|BTUSB_ENDP_BULK_OUT
+DECL|BTUSB_ENDP_INT|macro|BTUSB_ENDP_INT
+DECL|BTUSB_ENDP_ISO_IN|macro|BTUSB_ENDP_ISO_IN
+DECL|BTUSB_ENDP_ISO_OUT|macro|BTUSB_ENDP_ISO_OUT
+DECL|BTUSB_IF1_NUM_EP|macro|BTUSB_IF1_NUM_EP
+DECL|BTUSB_IF2_NUM_EP|macro|BTUSB_IF2_NUM_EP
+DECL|BTUSB_INTERRUPT_EP_MPS|macro|BTUSB_INTERRUPT_EP_MPS
+DECL|BTUSB_ISO1_EP_MPS|macro|BTUSB_ISO1_EP_MPS
+DECL|BTUSB_ISO2_EP_MPS|macro|BTUSB_ISO2_EP_MPS
+DECL|BTUSB_ISO3_EP_MPS|macro|BTUSB_ISO3_EP_MPS
+DECL|BTUSB_ISO4_EP_MPS|macro|BTUSB_ISO4_EP_MPS
+DECL|BTUSB_ISO5_EP_MPS|macro|BTUSB_ISO5_EP_MPS
+DECL|BTUSB_ISO6_EP_MPS|macro|BTUSB_ISO6_EP_MPS
+DECL|BTUSB_NUM_CONF|macro|BTUSB_NUM_CONF
+DECL|BTUSB_NUM_EP|macro|BTUSB_NUM_EP
+DECL|BTUSB_NUM_ITF_TOTAL|macro|BTUSB_NUM_ITF_TOTAL
+DECL|BTUSB_NUM_ITF|macro|BTUSB_NUM_ITF
+DECL|BT_BUF_ACL_SIZE|macro|BT_BUF_ACL_SIZE
+DECL|BT_L2CAP_MTU|macro|BT_L2CAP_MTU
+DECL|CMD_BUF_SIZE|macro|CMD_BUF_SIZE
+DECL|DEVICE_RELNUM|macro|DEVICE_RELNUM
+DECL|DEV_DATA|macro|DEV_DATA
+DECL|HIGH_BYTE|macro|HIGH_BYTE
+DECL|INTEL_VENDOR_ID|macro|INTEL_VENDOR_ID
+DECL|LOW_BYTE|macro|LOW_BYTE
+DECL|NET_BUF_POOL|variable|NET_BUF_POOL
+DECL|PRODUCT_ID|macro|PRODUCT_ID
+DECL|avail_acl_tx|variable|avail_acl_tx
+DECL|avail_tx|variable|avail_tx
+DECL|btusb_bulk_in|function|static void btusb_bulk_in(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
+DECL|btusb_bulk_out|function|static void btusb_bulk_out(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
+DECL|btusb_class_handler|function|static int btusb_class_handler(struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
+DECL|btusb_config|variable|btusb_config
+DECL|btusb_desc|variable|btusb_desc
+DECL|btusb_dev_data_t|struct|struct btusb_dev_data_t {
+DECL|btusb_dev_data|variable|btusb_dev_data
+DECL|btusb_dev|variable|btusb_dev
+DECL|btusb_ep|variable|btusb_ep
+DECL|btusb_init|function|static int btusb_init(struct device *dev)
+DECL|btusb_int_in|function|static void btusb_int_in(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
+DECL|btusb_iso_in|function|static void btusb_iso_in(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
+DECL|btusb_iso_out|function|static void btusb_iso_out(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
+DECL|btusb_status_cb|function|static void btusb_status_cb(enum usb_dc_status_code status)
+DECL|hexdump|function|static void hexdump(const char *str, const uint8_t *packet, size_t length)
+DECL|interface_data|member|uint8_t interface_data[BTUSB_CLASS_MAX_DATA_SIZE];
+DECL|main|function|void main(void)
+DECL|notification_sent|member|uint8_t notification_sent;
+DECL|rx_queue|variable|rx_queue
+DECL|try_write|function|static int try_write(uint8_t ep, struct net_buf *buf)
+DECL|usb_status|member|enum usb_dc_status_code usb_status;
