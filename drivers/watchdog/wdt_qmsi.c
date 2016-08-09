3249ca0ec6134f2267ad785d4e103e348d042bc1
Julien Delayen <julien.delayen@intel.com>
2016-08-09 09:02:59 +0100

    wdt_qmsi: Implement suspend and resume functions
    In order to restore watchdog context after entering
    SYS_PM_DEEP_SLEEP, suspend and resume functions
    are called.
    
    The following parameters are restored:
     - All registers for the WDT device.
     - Watchdog interrupt mask.
    
    The suspend/resume functionality is implemented in
    the QMSI shim layer as a fast and temporary solution,
    it will be removed and migrated to QMSI later.
    
    Change-Id: I6e4e30a797b0ca38ce727d50fa51d1b2f8330405
    Signed-off-by: Julien Delayen <julien.delayen@intel.com>
    
    Former-commit-id: 0392d798458533bc5e980fbf6a55627b8c4c53aa

--- drivers/watchdog/wdt_qmsi.c
+++ drivers/watchdog/wdt_qmsi.c
@@ -5,6 +5,7 @@ DECL|disable|function|static void disable(struct device *dev)
 DECL|enable|function|static void enable(struct device *dev)
 DECL|get_config|function|static void get_config(struct device *dev, struct wdt_config *cfg)
 DECL|init|function|static int init(struct device *dev)
+DECL|int_watchdog_mask|member|uint32_t int_watchdog_mask;
 DECL|reentrancy_protection|variable|reentrancy_protection
 DECL|reentrancy_protection|variable|reentrancy_protection
 DECL|reload|function|static void reload(struct device *dev)
@@ -14,5 +15,11 @@ DECL|user_cb|variable|user_cb
 DECL|wdt_context|variable|wdt_context
 DECL|wdt_critical_region_end|function|static void wdt_critical_region_end(struct device *dev)
 DECL|wdt_critical_region_start|function|static void wdt_critical_region_start(struct device *dev)
+DECL|wdt_cr|member|uint32_t wdt_cr;
+DECL|wdt_ctx_save|variable|wdt_ctx_save
+DECL|wdt_ctx|struct|struct wdt_ctx {
 DECL|wdt_data|struct|struct wdt_data {
 DECL|wdt_reentrancy_init|function|static void wdt_reentrancy_init(struct device *dev)
+DECL|wdt_resume_device|function|static int wdt_resume_device(struct device *dev, int pm_policy)
+DECL|wdt_suspend_device|function|static int wdt_suspend_device(struct device *dev, int pm_policy)
+DECL|wdt_torr|member|uint32_t wdt_torr;
