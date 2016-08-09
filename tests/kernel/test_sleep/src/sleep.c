75024cc7f9259ebbacebd48f0896a2fe56ed3f65
Andy Ross <andrew.j.ross@intel.com>
2016-08-09 13:52:33 -0700

    tests/kernel/{test_task,test_sleep}: Less aggressive timer testing
    On some hardware (Qemu) the timer guarantees aren't honored as well as
    we like, and these tests are observed to spuriously fail in practice
    (e.g. CI testing).  Allow for one tick of slop when testing sleep
    durations.
    
    Change-Id: I4b694c0a9ddfc1ee48510fa5deda2bb31499debf
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 5a8add7821562fa57a46414b446fb60b6a15ce52

--- tests/kernel/test_sleep/src/sleep.c
+++ tests/kernel/test_sleep/src/sleep.c
@@ -9,6 +9,7 @@ DECL|helper_fiber_stack|variable|helper_fiber_stack
 DECL|helper_fiber|function|static void helper_fiber(int arg1, int arg2)
 DECL|irq_offload_isr|function|static void irq_offload_isr(void *arg)
 DECL|main|function|void main(void)
+DECL|sleep_time_valid|function|static int sleep_time_valid(uint32_t start, uint32_t end, uint32_t dur)
 DECL|task_sem|variable|task_sem
 DECL|test_failure|variable|test_failure
 DECL|test_fiber_id|variable|test_fiber_id
