e8cd1fefae640ea38774a0abb8207a9591643020
Ricardo Salveti <ricardo.salveti@linaro.org>
2016-08-10 18:28:32 -0300

    pinmux: stm32: add support for STM32F4
    STM32F401 allows for upto 16 alternate functions on each pin.
    
    Change-Id: Ib1c14fd31abaa2b05a5ab0f7bd1b4a4748f10f84
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>
    Signed-off-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 1c1bd9a01889c2a798669b08a61b259a80704083

---
+++ arch/arm/soc/st_stm32/stm32f4/soc_pinmux.c
@@ -0,0 +1,6 @@
+DECL|pin_pa2_funcs|variable|pin_pa2_funcs
+DECL|pin_pa3_funcs|variable|pin_pa3_funcs
+DECL|pin_pb6_funcs|variable|pin_pb6_funcs
+DECL|pin_pb7_funcs|variable|pin_pb7_funcs
+DECL|pins|variable|pins
+DECL|stm32_get_pin_config|function|int stm32_get_pin_config(int pin, int func)
