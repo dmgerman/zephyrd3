a7b5fd82165168d6793fbd993d2190b736f35b2f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-08-10 16:38:57 +0300

    net: apps: Example app for Trickle algorithm
    This is only meant for Contiki based IP stack.
    
    Change-Id: I53cbcb7da0bd13ad87243fd70974cec0bd31072e
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 4a088a1259ff69f96d8ec6c6f5cb2e1c337a723e

---
+++ samples/net/trickle-legacy/src/main.c
@@ -0,0 +1,24 @@
+DECL|IMAX|macro|IMAX
+DECL|IMIN|macro|IMIN
+DECL|MY_IPADDR|macro|MY_IPADDR
+DECL|MY_PORT|macro|MY_PORT
+DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
+DECL|REDUNDANCY_CONST|macro|REDUNDANCY_CONST
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|WAIT_TICKS|macro|WAIT_TICKS
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|fiberStack|variable|fiberStack
+DECL|get_context|function|static inline bool get_context(struct net_context **udp_recv)
+DECL|in6addr_my|variable|in6addr_my
+DECL|init_app|function|static inline void init_app(void)
+DECL|main|function|void main(void)
+DECL|receive_and_reply|function|static inline void receive_and_reply(const char *name, struct net_context *udp_recv)
+DECL|receive|function|void receive(void)
+DECL|timeout|variable|timeout
+DECL|token_expired|function|static inline void token_expired(struct nano_work *work)
+DECL|token_lifetime|variable|token_lifetime
+DECL|token|variable|token
+DECL|trickle_tx|function|static void trickle_tx(void *user_data, uint8_t suppress)
+DECL|tt|variable|tt
