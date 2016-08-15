2f849a6bfe93ed5eb1cac66bd441add2eeeacbaa
Andy Ross <andrew.j.ross@intel.com>
2016-08-15 12:50:45 -0700

    libc/printf: Use compiler-provided 64 bit math, phase 2
    Remove the function wrappers around the 64 bit math and just use C
    syntax natively, combining ops where appropriate (e.g. there was a
    sequence implementing "(x<<2+x)<<2" to do "multiply by 10").  The
    _ldiv5 and _rlrshift routines are non-standard ops that provide useful
    abstraction, so they remain as separate functions.
    
    Change-Id: I4d83847348fdd7be09887b833c8ccbd2aa1e4182
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 5729235b57ba8f2cef6f4ae815c00455285ed3fb

--- lib/libc/minimal/source/stdout/prf.c
+++ lib/libc/minimal/source/stdout/prf.c
@@ -1,17 +1,15 @@
 DECL|EOF|macro|EOF
+DECL|HIGHBIT64|macro|HIGHBIT64
 DECL|MAXFLD|macro|MAXFLD
 DECL|MAXFP1|macro|MAXFP1
-DECL|MAXFP2|macro|MAXFP2
 DECL|_atoi|function|static int _atoi(char **sptr)
-DECL|_get_digit|function|static char _get_digit(uint32_t fract[], int *digit_count)
-DECL|_ladd|function|static void __attribute__((noinline)) _ladd(uint32_t *result, uint32_t *value)
-DECL|_ldiv5|function|static void _ldiv5(uint32_t value[])
-DECL|_llshift|function|static void _llshift(uint32_t value[])
+DECL|_get_digit|function|static char _get_digit(uint64_t *fr, int *digit_count)
+DECL|_ldiv5|function|static void _ldiv5(uint64_t *v)
 DECL|_prf|function|int _prf(int (*func)(), void *dest, char *format, va_list vargs)
 DECL|_reverse_and_pad|function|static int _reverse_and_pad(char *start, char *end, int minlen)
-DECL|_rlrshift|function|static void _rlrshift(uint32_t value[])
+DECL|_rlrshift|function|static void _rlrshift(uint64_t *v)
 DECL|_to_dec|function|static int _to_dec(char *buf, int32_t value, int fplus, int fspace, int precision)
-DECL|_to_float|function|static int _to_float(char *buf, uint32_t double_temp[], int c, int falt, int fplus, int fspace, int precision)
+DECL|_to_float|function|static int _to_float(char *buf, uint64_t double_temp, int c, int falt, int fplus, int fspace, int precision)
 DECL|_to_hex|function|static int _to_hex(char *buf, uint32_t value, int alt_form, int precision, int prefix)
 DECL|_to_octal|function|static int _to_octal(char *buf, uint32_t value, int alt_form, int precision)
 DECL|_to_udec|function|static int _to_udec(char *buf, uint32_t value, int precision)
