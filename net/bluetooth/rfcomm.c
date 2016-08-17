2f09aa8fc3c806ba7ccf88e659e085402505b8de
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-08-17 23:25:09 +0530

    Bluetooth: RFCOMM: Initialize and register to L2CAP
    Initialize RFCOMM layer and register PSM to L2CAP. RFCOMM session
    which represents the signalling context is also defined. Only one
    signalling context will be there per ACL connection and hence l2cap
    chan object is embedded into session.
    
    Change-Id: I9b0931b51753571f5da40c76f4b8a7d5f93546fb
    Origin: Original
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: b4aa810f0c21d9764b8d23ad05332ed2c22c6a27

---
+++ net/bluetooth/rfcomm.c
@@ -0,0 +1,10 @@
+DECL|BT_DBG|macro|BT_DBG
+DECL|BT_DBG|macro|BT_DBG
+DECL|RFCOMM_DEFAULT_MTU|macro|RFCOMM_DEFAULT_MTU
+DECL|RFCOMM_SESSION|macro|RFCOMM_SESSION
+DECL|bt_rfcomm_init|function|void bt_rfcomm_init(void)
+DECL|bt_rfcomm_pool|variable|bt_rfcomm_pool
+DECL|rfcomm_accept|function|static int rfcomm_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
+DECL|rfcomm_connected|function|static void rfcomm_connected(struct bt_l2cap_chan *chan)
+DECL|rfcomm_disconnected|function|static void rfcomm_disconnected(struct bt_l2cap_chan *chan)
+DECL|rfcomm_recv|function|static void rfcomm_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
---
+++ net/bluetooth/rfcomm_internal.h
@@ -0,0 +1,5 @@
+DECL|BT_RFCOMM_STATE_IDLE|enumerator|BT_RFCOMM_STATE_IDLE,
+DECL|BT_RFCOMM_STATE_INIT|enumerator|BT_RFCOMM_STATE_INIT,
+DECL|br_chan|member|struct bt_l2cap_br_chan br_chan;
+DECL|bt_rfcomm_session|struct|struct bt_rfcomm_session {
+DECL|state|member|uint8_t state;
