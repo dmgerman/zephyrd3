cfa4c28a58fc74e687be9c8a7f4d910359a8e855
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-08-18 16:48:05 +0530

    Bluetooth: RFCOMM: Handle signalling connection request
    Handles the SABM request to dlci 0 and sends UA response which makes
    session state as CONNECTED. Session mtu is set as min of l2cap tx and
    rx mtu since in rfcomm mtu is symmetric (unlike L2CAP). This means that
    mtu of all the dlcs should be less than or equal to its session mtu so
    that each rfcomm frame can be contained in single l2cap pdu.
    
    > ACL Data RX: Handle 256 flags 0x02 dlen 8                                                                                                                                        [hci0] 210.078442
          Channel: 64 len 4 [PSM 3 mode 0] {chan 0}
          RFCOMM: Set Async Balance Mode (SABM) (0x2f)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0x3f poll/final 1
             Length: 0
             FCS: 0x1c
    < ACL Data TX: Handle 256 flags 0x00 dlen 8                                                                                                                                        [hci0] 210.080586
          Channel: 64 len 4 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Ack (UA) (0x63)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0x73 poll/final 1
             Length: 0
             FCS: 0xd7
    
    This also inntroduces table and functions to calculate and
    check FCS. These are taken from GSM 07.10 TS 101 369 V6.5.0
    
    Change-Id: Ief5822b7f9350f50e700ff4f460c818a5a1068b7
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: 7eec8a4442eb5f48ff7bc54096ead2e868aadc5e

--- net/bluetooth/rfcomm.c
+++ net/bluetooth/rfcomm.c
@@ -9,9 +9,14 @@ DECL|bt_rfcomm_init|function|void bt_rfcomm_init(void)
 DECL|bt_rfcomm_pool|variable|bt_rfcomm_pool
 DECL|bt_rfcomm_server_register|function|int bt_rfcomm_server_register(struct bt_rfcomm_server *server)
 DECL|rfcomm_accept|function|static int rfcomm_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
+DECL|rfcomm_calc_fcs|function|static uint8_t rfcomm_calc_fcs(uint16_t len, const uint8_t *data)
+DECL|rfcomm_check_fcs|function|static bool rfcomm_check_fcs(uint16_t len, const uint8_t *data, uint8_t recvd_fcs)
 DECL|rfcomm_connected|function|static void rfcomm_connected(struct bt_l2cap_chan *chan)
+DECL|rfcomm_crc_table|variable|rfcomm_crc_table
 DECL|rfcomm_disconnected|function|static void rfcomm_disconnected(struct bt_l2cap_chan *chan)
+DECL|rfcomm_handle_sabm|function|static void rfcomm_handle_sabm(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_recv|function|static void rfcomm_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
+DECL|rfcomm_send_ua|function|static int rfcomm_send_ua(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_server_lookup_channel|function|static struct bt_rfcomm_server *rfcomm_server_lookup_channel(uint8_t channel)
 DECL|rfcomm_session|variable|rfcomm_session
 DECL|servers|variable|servers
--- net/bluetooth/rfcomm_internal.h
+++ net/bluetooth/rfcomm_internal.h
@@ -1,13 +1,32 @@
 DECL|BT_RFCOMM_BUF_SIZE|macro|BT_RFCOMM_BUF_SIZE
+DECL|BT_RFCOMM_FCS_LEN_NON_UIH|macro|BT_RFCOMM_FCS_LEN_NON_UIH
+DECL|BT_RFCOMM_FCS_LEN_UIH|macro|BT_RFCOMM_FCS_LEN_UIH
+DECL|BT_RFCOMM_FCS_SIZE|macro|BT_RFCOMM_FCS_SIZE
+DECL|BT_RFCOMM_GET_DLCI|macro|BT_RFCOMM_GET_DLCI
+DECL|BT_RFCOMM_GET_FRAME_TYPE|macro|BT_RFCOMM_GET_FRAME_TYPE
+DECL|BT_RFCOMM_HDR_SIZE|macro|BT_RFCOMM_HDR_SIZE
+DECL|BT_RFCOMM_SABM|macro|BT_RFCOMM_SABM
+DECL|BT_RFCOMM_SET_ADDR|macro|BT_RFCOMM_SET_ADDR
+DECL|BT_RFCOMM_SET_CTRL|macro|BT_RFCOMM_SET_CTRL
+DECL|BT_RFCOMM_SET_LEN_8|macro|BT_RFCOMM_SET_LEN_8
 DECL|BT_RFCOMM_SIG_MIN_MTU|macro|BT_RFCOMM_SIG_MIN_MTU
+DECL|BT_RFCOMM_STATE_CONNECTED|enumerator|BT_RFCOMM_STATE_CONNECTED,
 DECL|BT_RFCOMM_STATE_IDLE|enumerator|BT_RFCOMM_STATE_IDLE,
 DECL|BT_RFCOMM_STATE_INIT|enumerator|BT_RFCOMM_STATE_INIT,
+DECL|BT_RFCOMM_UA|macro|BT_RFCOMM_UA
+DECL|BT_RFCOMM_UIH|macro|BT_RFCOMM_UIH
+DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|address|member|uint8_t address;
 DECL|br_chan|member|struct bt_l2cap_br_chan br_chan;
 DECL|bt_rfcomm_hdr|struct|struct bt_rfcomm_hdr {
+DECL|bt_rfcomm_msg_hdr|struct|struct bt_rfcomm_msg_hdr {
 DECL|bt_rfcomm_session|struct|struct bt_rfcomm_session {
 DECL|control|member|uint8_t control;
 DECL|dlcs|member|struct bt_rfcomm_dlc *dlcs;
+DECL|initiator|member|bool initiator;
 DECL|length|member|uint8_t length;
+DECL|len|member|uint8_t len;
+DECL|mtu|member|uint16_t mtu;
 DECL|state|member|uint8_t state;
+DECL|type|member|uint8_t type;
