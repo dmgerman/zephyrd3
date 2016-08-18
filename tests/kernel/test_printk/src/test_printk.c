db794a0f2869832a1a0ce3f0c9a7164d614c4912
Andrew Boie <andrew.p.boie@intel.com>
2016-08-18 13:00:42 -0700

    tests: test_printk: crude printk test case
    Print some stuff, and verify that the output is as expected.
    Not comprehensive (yet).
    
    Change-Id: Ib1ce8dff8165d8ee6b02ff6272513fd76a7be842
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 0ef0136a044649a52aa66985e79d0ea1507ad18a

---
+++ tests/kernel/test_printk/src/test_printk.c
@@ -0,0 +1,20 @@
+DECL|BUF_SZ|macro|BUF_SZ
+DECL|_old_char_out|variable|_old_char_out
+DECL|c|variable|c
+DECL|expected|variable|expected
+DECL|hex|variable|hex
+DECL|main|function|void main(void)
+DECL|pos|variable|pos
+DECL|ptr|variable|ptr
+DECL|ram_console_out|function|static int ram_console_out(int character)
+DECL|ram_console|variable|ram_console
+DECL|si|variable|si
+DECL|sll|variable|sll
+DECL|sl|variable|sl
+DECL|ssi|variable|ssi
+DECL|stv|variable|stv
+DECL|uc|variable|uc
+DECL|ui|variable|ui
+DECL|ull|variable|ull
+DECL|ul|variable|ul
+DECL|usi|variable|usi
