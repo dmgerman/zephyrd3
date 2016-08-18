ca22a3d516445c66731e6b130eb81b0c0133cad9
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-08-18 16:17:18 +0530

    Bluetooth: RFCOMM: Init buffer for outgoing signalling packets
    Buffer size is currently set as rfcomm min mtu
    
    Change-Id: Ie8fd3f1cef9d6d9b62d5aca272f0030181175460
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: a81614268796aa663d17d4544eeb8ac0161b504f

--- net/bluetooth/rfcomm.c
+++ net/bluetooth/rfcomm.c
@@ -3,6 +3,7 @@ DECL|BT_DBG|macro|BT_DBG
 DECL|RFCOMM_CHANNEL_END|macro|RFCOMM_CHANNEL_END
 DECL|RFCOMM_CHANNEL_START|macro|RFCOMM_CHANNEL_START
 DECL|RFCOMM_DEFAULT_MTU|macro|RFCOMM_DEFAULT_MTU
+DECL|RFCOMM_MIN_MTU|macro|RFCOMM_MIN_MTU
 DECL|RFCOMM_SESSION|macro|RFCOMM_SESSION
 DECL|bt_rfcomm_init|function|void bt_rfcomm_init(void)
 DECL|bt_rfcomm_pool|variable|bt_rfcomm_pool
@@ -12,4 +13,5 @@ DECL|rfcomm_connected|function|static void rfcomm_connected(struct bt_l2cap_chan
 DECL|rfcomm_disconnected|function|static void rfcomm_disconnected(struct bt_l2cap_chan *chan)
 DECL|rfcomm_recv|function|static void rfcomm_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|rfcomm_server_lookup_channel|function|static struct bt_rfcomm_server *rfcomm_server_lookup_channel(uint8_t channel)
+DECL|rfcomm_session|variable|rfcomm_session
 DECL|servers|variable|servers
--- net/bluetooth/rfcomm_internal.h
+++ net/bluetooth/rfcomm_internal.h
@@ -1,6 +1,13 @@
+DECL|BT_RFCOMM_BUF_SIZE|macro|BT_RFCOMM_BUF_SIZE
+DECL|BT_RFCOMM_SIG_MIN_MTU|macro|BT_RFCOMM_SIG_MIN_MTU
 DECL|BT_RFCOMM_STATE_IDLE|enumerator|BT_RFCOMM_STATE_IDLE,
 DECL|BT_RFCOMM_STATE_INIT|enumerator|BT_RFCOMM_STATE_INIT,
+DECL|__packed|variable|__packed
+DECL|address|member|uint8_t address;
 DECL|br_chan|member|struct bt_l2cap_br_chan br_chan;
+DECL|bt_rfcomm_hdr|struct|struct bt_rfcomm_hdr {
 DECL|bt_rfcomm_session|struct|struct bt_rfcomm_session {
+DECL|control|member|uint8_t control;
 DECL|dlcs|member|struct bt_rfcomm_dlc *dlcs;
+DECL|length|member|uint8_t length;
 DECL|state|member|uint8_t state;
