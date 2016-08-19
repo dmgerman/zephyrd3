f5111de86580974ccfc0f3eb1935b439c0f633f7
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-08-19 23:54:21 +0530

    Bluetooth: RFCOMM: Handle PN request
    PN (Parameter negotiation) is used to negotiate parameters like mtu.
    Initial credit for flow control is also sent in PN. If the dlci to which
    PN requested is not found in the dlc list then it will be treated as
    incoming dlc request and accept callback will be called to profile.
    Dlc mtu has to be set by profile (before it returns dlc) which will be
    negotiated with remote. But the final mtu will be min of mtu provided
    by profile, mtu sent by remote, and session mtu
    
    > ACL Data RX: Handle 256 flags 0x02 dlen 18                                                                                                                                       [hci0] 210.108444
          Channel: 64 len 14 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0xef poll/final 0
             Length: 10
             FCS: 0x70
             MCC Message type: DLC Parameter Negotiation CMD (0x20)
               Length: 8
               dlci 2 frame_type 0 credit_flow 15 pri 7
               ack_timer 0 frame_size 122 max_retrans 0 credits 7
    < ACL Data TX: Handle 256 flags 0x00 dlen 18                                                                                                                                       [hci0] 210.111452
          Channel: 64 len 14 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x01 cr 0 dlci 0x00
             Control: 0xef poll/final 0
             Length: 10
             FCS: 0xaa
             MCC Message type: DLC Parameter Negotiation RSP (0x20)
               Length: 8
               dlci 2 frame_type 0 credit_flow 14 pri 0
               ack_timer 0 frame_size 30 max_retrans 0 credits 7
    
    Change-Id: Ifd466db6b3b868d04e38db02ebad6e47ab2da030
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: ab96eae52dd5ee649e4b471b9894ce3e474c6405

--- net/bluetooth/rfcomm.c
+++ net/bluetooth/rfcomm.c
@@ -2,6 +2,7 @@ DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
 DECL|RFCOMM_CHANNEL_END|macro|RFCOMM_CHANNEL_END
 DECL|RFCOMM_CHANNEL_START|macro|RFCOMM_CHANNEL_START
+DECL|RFCOMM_DEFAULT_CREDIT|macro|RFCOMM_DEFAULT_CREDIT
 DECL|RFCOMM_DEFAULT_MTU|macro|RFCOMM_DEFAULT_MTU
 DECL|RFCOMM_MIN_MTU|macro|RFCOMM_MIN_MTU
 DECL|RFCOMM_SESSION|macro|RFCOMM_SESSION
@@ -14,8 +15,14 @@ DECL|rfcomm_check_fcs|function|static bool rfcomm_check_fcs(uint16_t len, const 
 DECL|rfcomm_connected|function|static void rfcomm_connected(struct bt_l2cap_chan *chan)
 DECL|rfcomm_crc_table|variable|rfcomm_crc_table
 DECL|rfcomm_disconnected|function|static void rfcomm_disconnected(struct bt_l2cap_chan *chan)
+DECL|rfcomm_dlc_accept|function|static int rfcomm_dlc_accept(struct bt_rfcomm_session *session, uint8_t dlci, struct bt_rfcomm_dlc **dlc)
+DECL|rfcomm_dlcs_lookup_dlci|function|static struct bt_rfcomm_dlc *rfcomm_dlcs_lookup_dlci(struct bt_rfcomm_dlc *dlcs, uint8_t dlci)
+DECL|rfcomm_handle_msg|function|static void rfcomm_handle_msg(struct bt_rfcomm_session *session, struct net_buf *buf)
+DECL|rfcomm_handle_pn|function|static void rfcomm_handle_pn(struct bt_rfcomm_session *session, struct net_buf *buf, uint8_t cr)
 DECL|rfcomm_handle_sabm|function|static void rfcomm_handle_sabm(struct bt_rfcomm_session *session, uint8_t dlci)
+DECL|rfcomm_make_uih_msg|function|static struct net_buf *rfcomm_make_uih_msg(struct bt_rfcomm_dlc *dlc, uint8_t cr, uint8_t type, uint8_t len)
 DECL|rfcomm_recv|function|static void rfcomm_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
+DECL|rfcomm_send_pn|function|static int rfcomm_send_pn(struct bt_rfcomm_dlc *dlc, uint8_t cr)
 DECL|rfcomm_send_ua|function|static int rfcomm_send_ua(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_server_lookup_channel|function|static struct bt_rfcomm_server *rfcomm_server_lookup_channel(uint8_t channel)
 DECL|rfcomm_session|variable|rfcomm_session
--- net/bluetooth/rfcomm_internal.h
+++ net/bluetooth/rfcomm_internal.h
@@ -1,15 +1,27 @@
 DECL|BT_RFCOMM_BUF_SIZE|macro|BT_RFCOMM_BUF_SIZE
+DECL|BT_RFCOMM_CHECK_MTU|macro|BT_RFCOMM_CHECK_MTU
 DECL|BT_RFCOMM_FCS_LEN_NON_UIH|macro|BT_RFCOMM_FCS_LEN_NON_UIH
 DECL|BT_RFCOMM_FCS_LEN_UIH|macro|BT_RFCOMM_FCS_LEN_UIH
 DECL|BT_RFCOMM_FCS_SIZE|macro|BT_RFCOMM_FCS_SIZE
+DECL|BT_RFCOMM_GET_CHANNEL|macro|BT_RFCOMM_GET_CHANNEL
 DECL|BT_RFCOMM_GET_DLCI|macro|BT_RFCOMM_GET_DLCI
 DECL|BT_RFCOMM_GET_FRAME_TYPE|macro|BT_RFCOMM_GET_FRAME_TYPE
+DECL|BT_RFCOMM_GET_LEN|macro|BT_RFCOMM_GET_LEN
+DECL|BT_RFCOMM_GET_MSG_CR|macro|BT_RFCOMM_GET_MSG_CR
+DECL|BT_RFCOMM_GET_MSG_TYPE|macro|BT_RFCOMM_GET_MSG_TYPE
 DECL|BT_RFCOMM_HDR_SIZE|macro|BT_RFCOMM_HDR_SIZE
+DECL|BT_RFCOMM_LEN_EXTENDED|macro|BT_RFCOMM_LEN_EXTENDED
+DECL|BT_RFCOMM_MSG_CMD|macro|BT_RFCOMM_MSG_CMD
+DECL|BT_RFCOMM_MSG_RESP|macro|BT_RFCOMM_MSG_RESP
+DECL|BT_RFCOMM_PN|macro|BT_RFCOMM_PN
 DECL|BT_RFCOMM_SABM|macro|BT_RFCOMM_SABM
 DECL|BT_RFCOMM_SET_ADDR|macro|BT_RFCOMM_SET_ADDR
 DECL|BT_RFCOMM_SET_CTRL|macro|BT_RFCOMM_SET_CTRL
 DECL|BT_RFCOMM_SET_LEN_8|macro|BT_RFCOMM_SET_LEN_8
+DECL|BT_RFCOMM_SET_MSG_TYPE|macro|BT_RFCOMM_SET_MSG_TYPE
+DECL|BT_RFCOMM_SIG_MAX_MTU|macro|BT_RFCOMM_SIG_MAX_MTU
 DECL|BT_RFCOMM_SIG_MIN_MTU|macro|BT_RFCOMM_SIG_MIN_MTU
+DECL|BT_RFCOMM_STATE_CONFIG|enumerator|BT_RFCOMM_STATE_CONFIG,
 DECL|BT_RFCOMM_STATE_CONNECTED|enumerator|BT_RFCOMM_STATE_CONNECTED,
 DECL|BT_RFCOMM_STATE_IDLE|enumerator|BT_RFCOMM_STATE_IDLE,
 DECL|BT_RFCOMM_STATE_INIT|enumerator|BT_RFCOMM_STATE_INIT,
@@ -17,16 +29,25 @@ DECL|BT_RFCOMM_UA|macro|BT_RFCOMM_UA
 DECL|BT_RFCOMM_UIH|macro|BT_RFCOMM_UIH
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|ack_timer|member|uint8_t ack_timer;
 DECL|address|member|uint8_t address;
 DECL|br_chan|member|struct bt_l2cap_br_chan br_chan;
 DECL|bt_rfcomm_hdr|struct|struct bt_rfcomm_hdr {
 DECL|bt_rfcomm_msg_hdr|struct|struct bt_rfcomm_msg_hdr {
+DECL|bt_rfcomm_pn|struct|struct bt_rfcomm_pn {
 DECL|bt_rfcomm_session|struct|struct bt_rfcomm_session {
 DECL|control|member|uint8_t control;
+DECL|credits|member|uint8_t credits;
+DECL|dlci|member|uint8_t dlci;
 DECL|dlcs|member|struct bt_rfcomm_dlc *dlcs;
+DECL|flow_ctrl|member|uint8_t flow_ctrl;
 DECL|initiator|member|bool initiator;
 DECL|length|member|uint8_t length;
 DECL|len|member|uint8_t len;
+DECL|max_retrans|member|uint8_t max_retrans;
 DECL|mtu|member|uint16_t mtu;
+DECL|mtu|member|uint16_t mtu;
+DECL|priority|member|uint8_t priority;
 DECL|state|member|uint8_t state;
 DECL|type|member|uint8_t type;
