3fd71ba9f4c6f24a00324125071726f83111e440
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-08-22 18:17:29 +0530

    Bluetooth: RFCOMM: Perform MSC transaction after dlc
    MSC (Modem status command) is used to convey RS-232 control and break
    signals. This shall be sent prior to any user data after dlc. Remote
    also would have sent it which is handled in this patch
    
    This has meaning only if a port emulation entity is there on top (which
    is not there in zephyr right now). So currently v24 signal is hard
    coded like below.
    DV = 1 IC = 0 RTR = 1 RTC = 1 FC = 0 EXT = 0
    
    < ACL Data TX: Handle 256 flags 0x00 dlen 12
          Channel: 64 len 8 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x01 cr 0 dlci 0x00
             Control: 0xef poll/final 0
             Length: 4
             FCS: 0xaa
             MCC Message type: Modem Status Command CMD (0x38)
               Length: 2
               dlci 2
               fc 0 rtc 1 rtr 1 ic 0 dv 1
    > ACL Data RX: Handle 256 flags 0x02 dlen 12
          Channel: 64 len 8 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0xef poll/final 0
             Length: 4
             FCS: 0x70
             MCC Message type: Modem Status Command CMD (0x38)
               Length: 2
               dlci 2
               fc 0 rtc 1 rtr 1 ic 0 dv 1
    < ACL Data TX: Handle 256 flags 0x00 dlen 12
          Channel: 64 len 8 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x01 cr 0 dlci 0x00
             Control: 0xef poll/final 0
             Length: 4
             FCS: 0xaa
             MCC Message type: Modem Status Command RSP (0x38)
               Length: 2
               dlci 2
               fc 0 rtc 1 rtr 1 ic 0 dv 1
    > ACL Data RX: Handle 256 flags 0x02 dlen 12
          Channel: 64 len 8 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0xef poll/final 0
             Length: 4
             FCS: 0x70
             MCC Message type: Modem Status Command RSP (0x38)
               Length: 2
               dlci 2
               fc 0 rtc 1 rtr 1 ic 0 dv 1
    
    Change-Id: Iab06f12de2f2357485309eb622c8c5e13db7011c
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: d6af0ef0915ca8a933c852ccafc88ea7921b95e1

--- net/bluetooth/rfcomm_internal.h
+++ net/bluetooth/rfcomm_internal.h
@@ -1,5 +1,6 @@
 DECL|BT_RFCOMM_BUF_SIZE|macro|BT_RFCOMM_BUF_SIZE
 DECL|BT_RFCOMM_CHECK_MTU|macro|BT_RFCOMM_CHECK_MTU
+DECL|BT_RFCOMM_DEFAULT_V24_SIG|macro|BT_RFCOMM_DEFAULT_V24_SIG
 DECL|BT_RFCOMM_FCS_LEN_NON_UIH|macro|BT_RFCOMM_FCS_LEN_NON_UIH
 DECL|BT_RFCOMM_FCS_LEN_UIH|macro|BT_RFCOMM_FCS_LEN_UIH
 DECL|BT_RFCOMM_FCS_SIZE|macro|BT_RFCOMM_FCS_SIZE
@@ -11,6 +12,7 @@ DECL|BT_RFCOMM_GET_MSG_CR|macro|BT_RFCOMM_GET_MSG_CR
 DECL|BT_RFCOMM_GET_MSG_TYPE|macro|BT_RFCOMM_GET_MSG_TYPE
 DECL|BT_RFCOMM_HDR_SIZE|macro|BT_RFCOMM_HDR_SIZE
 DECL|BT_RFCOMM_LEN_EXTENDED|macro|BT_RFCOMM_LEN_EXTENDED
+DECL|BT_RFCOMM_MSC|macro|BT_RFCOMM_MSC
 DECL|BT_RFCOMM_MSG_CMD|macro|BT_RFCOMM_MSG_CMD
 DECL|BT_RFCOMM_MSG_RESP|macro|BT_RFCOMM_MSG_RESP
 DECL|BT_RFCOMM_PN|macro|BT_RFCOMM_PN
@@ -30,15 +32,18 @@ DECL|BT_RFCOMM_UIH|macro|BT_RFCOMM_UIH
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
 DECL|ack_timer|member|uint8_t ack_timer;
 DECL|address|member|uint8_t address;
 DECL|br_chan|member|struct bt_l2cap_br_chan br_chan;
 DECL|bt_rfcomm_hdr|struct|struct bt_rfcomm_hdr {
+DECL|bt_rfcomm_msc|struct|struct bt_rfcomm_msc {
 DECL|bt_rfcomm_msg_hdr|struct|struct bt_rfcomm_msg_hdr {
 DECL|bt_rfcomm_pn|struct|struct bt_rfcomm_pn {
 DECL|bt_rfcomm_session|struct|struct bt_rfcomm_session {
 DECL|control|member|uint8_t control;
 DECL|credits|member|uint8_t credits;
+DECL|dlci|member|uint8_t dlci;
 DECL|dlci|member|uint8_t dlci;
 DECL|dlcs|member|struct bt_rfcomm_dlc *dlcs;
 DECL|flow_ctrl|member|uint8_t flow_ctrl;
@@ -51,3 +56,4 @@ DECL|mtu|member|uint16_t mtu;
 DECL|priority|member|uint8_t priority;
 DECL|state|member|uint8_t state;
 DECL|type|member|uint8_t type;
+DECL|v24_signal|member|uint8_t v24_signal;
