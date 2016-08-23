40c95b5a5b287908c35234f4fb342d826cca9634
Andrew Boie <andrew.p.boie@intel.com>
2016-08-23 13:25:53 -0700

    Revert "sys_log: replace old debug macro on ADC driver sample."
    This reverts commit 24d4082da93cc77ecd882fae437341ec900c5360 [formerly eeb2b733e40891234f30199ad05ac591562fbee9].
    
    Change-Id: I4174d3c91b78856266fc457f8107c0043bba0416
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: c18ec01fdcb7873993f44495de0a488b66570f43

--- samples/drivers/adc/src/adc.c
+++ samples/drivers/adc/src/adc.c
@@ -1,9 +1,10 @@
 DECL|ADC_DEVICE_NAME|macro|ADC_DEVICE_NAME
 DECL|BUFFER_SIZE|macro|BUFFER_SIZE
 DECL|CHANNEL|macro|CHANNEL
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|SYS_LOG_NO_NEWLINE|macro|SYS_LOG_NO_NEWLINE
 DECL|_print_sample_in_hex|function|static void _print_sample_in_hex(uint8_t *buf, uint32_t length)
 DECL|main|function|void main(void)
 DECL|sample|variable|sample
