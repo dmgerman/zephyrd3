d48e7c07bc3723e5e78757abcb27dbd87fadf8f6
Andrew Boie <andrew.p.boie@intel.com>
2016-08-23 13:15:39 -0700

    Revert "misc: Remove generic PRINT macros from pci samples"
    This reverts commit 2741fbffcbaad2cc11e30a3049aacb56954ee897 [formerly a0007012b7864e9f90991d6f70f049fa9dbc048d].
    
    Change-Id: I72f4b8adfc33c833116fef9fc1cd8c542f3979b0
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 03c40a268539d05fd4185b26e8e67a50d3ac3337

--- samples/drivers/pci_enum/src/pci_enum.c
+++ samples/drivers/pci_enum/src/pci_enum.c
@@ -1,4 +1,5 @@
-DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
 DECL|done|variable|done
 DECL|main|function|void main(void)
 DECL|pci_enumerate|function|void pci_enumerate(void)
