d414eab697788ee68cfa66f3d65a485abe37baf9
Carles Cufi <carles.cufi@nordicsemi.no>
2016-08-23 09:41:17 -0700

    lib: Use offsetof() builtin with GCC
    The default offsetof() implementation generates a warning
    (variably modified <variable> at file scope) when used in
    to define the size of an array. By using this builtin with
    GCC we avoid the warning and make sure no variable-size
    arrays are used.
    
    Change-Id: Iae215f777241f7daaa061067230086dffaa8311d
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 1980b7bb0ffee0750ed832fc1b474c6ae6e85b00

--- lib/libc/minimal/include/stddef.h
+++ lib/libc/minimal/include/stddef.h
@@ -1,4 +1,5 @@
 DECL|__INC_stddef_h__|macro|__INC_stddef_h__
 DECL|__ptrdiff_t_defined|macro|__ptrdiff_t_defined
 DECL|offsetof|macro|offsetof
+DECL|offsetof|macro|offsetof
 DECL|ptrdiff_t|typedef|typedef int ptrdiff_t;
