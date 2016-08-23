ac421528e9f4ed089c9cd8de5215c1af13cd002b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-08-23 14:25:10 +0200

    net: ieee802154: Add CSMA-CA non slotted radio protocol support
    Implementing the primitives for non-slotted CSMA-CA radio protocol
    according to section 5.1.1.4 in the 2011 version of the specification.
    
    Slotted version will come when Beacon frame will be supported, thus
    getting PAN coordinator info from its superframe.
    
    Change-Id: I0545ed953a3c48e6b8e9cd4082db01ed66ad098b
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 7353cf4079f3e8ce6a8c8a403bd8e8a38973c728

---
+++ net/yaip/l2/ieee802154/ieee802154_radio_csma_ca.c
@@ -0,0 +1,2 @@
+DECL|csma_ca_radio_handle_ack|function|static enum net_verdict csma_ca_radio_handle_ack(struct net_if *iface, struct net_buf *buf)
+DECL|csma_ca_radio_send|function|static int csma_ca_radio_send(struct net_if *iface, struct net_buf *buf)
