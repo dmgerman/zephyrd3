312aa8307d27ba868331bf207b9a339e62e34153
Andrew Boie <andrew.p.boie@intel.com>
2016-08-23 13:16:09 -0700

    Revert "misc: Remove generic PRINT macros from button samples"
    This reverts commit bf5dad5b50c3a2c9a74defecd19bd698074a71b6 [formerly 2939752a3e57db7c3e7b78d888745820bf74fd4d].
    
    Change-Id: I36b8788938bcfa0db48ef618919a9821714096d0
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: a713d4be6f16a3c7790e235c8cf29fb41b43bb3c

--- samples/drivers/button/src/main.c
+++ samples/drivers/button/src/main.c
@@ -1,8 +1,9 @@
 DECL|EDGE|macro|EDGE
 DECL|PIN|macro|PIN
 DECL|PORT|macro|PORT
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
 DECL|PULL_UP|macro|PULL_UP
-DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|button_pressed|function|void button_pressed(struct device *gpiob, struct gpio_callback *cb, uint32_t pins)
 DECL|gpio_cb|variable|gpio_cb
 DECL|main|function|void main(void)
