a9fc5704cf400650b95ac30b1bf08974dfd8ed67
Andrew Boie <andrew.p.boie@intel.com>
2016-08-23 13:14:51 -0700

    Revert "misc: Remove generic PRINT macros from power samples"
    This reverts commit 2937196703541bc7df0aef1b32311365b0301542 [formerly 6f992e2119cb0ee23ef1caf6116f8c41cf746a18].
    
    Change-Id: I14767699b02d147a245db1cb8ac5cef430782232
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 7bc48f69c2aaaa70e600414098eaa92d6e8543e6

--- samples/power/power_hooks/src/main.c
+++ samples/power/power_hooks/src/main.c
@@ -1,4 +1,5 @@
-DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
 DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
 DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
 DECL|main|function|void main(void)
