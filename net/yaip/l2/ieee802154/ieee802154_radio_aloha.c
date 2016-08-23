76cebcf1834e38af569f4de64ee85559d3bca829
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-08-23 12:23:31 +0200

    net: Centralize generic IEEE 802.15.4 radio utility functions
    These will be used by other radio protocol, such as CSMA-CA.
    
    Change-Id: I8b308c0376e5e26ae35fbbb4a0a7f99228fa2529
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 06f10d43e197fe1d0115cbc39a5117d1d88da959

--- net/yaip/l2/ieee802154/ieee802154_radio_aloha.c
+++ net/yaip/l2/ieee802154/ieee802154_radio_aloha.c
@@ -1,4 +1,2 @@
 DECL|aloha_radio_handle_ack|function|static enum net_verdict aloha_radio_handle_ack(struct net_if *iface, struct net_buf *buf)
 DECL|aloha_radio_send|function|static int aloha_radio_send(struct net_if *iface, struct net_buf *buf)
-DECL|prepare_for_ack|function|static inline bool prepare_for_ack(struct ieee802154_context *ctx, struct net_buf *buf)
-DECL|wait_for_ack|function|static inline int wait_for_ack(struct ieee802154_context *ctx, bool ack_required)
---
+++ net/yaip/l2/ieee802154/ieee802154_radio_utils.h
@@ -0,0 +1,4 @@
+DECL|__IEEE802154_RADIO_UTILS_H__|macro|__IEEE802154_RADIO_UTILS_H__
+DECL|handle_ack|function|static inline int handle_ack(struct ieee802154_context *ctx, struct net_buf *buf)
+DECL|prepare_for_ack|function|static inline bool prepare_for_ack(struct ieee802154_context *ctx, struct net_buf *buf)
+DECL|wait_for_ack|function|static inline int wait_for_ack(struct ieee802154_context *ctx, bool ack_required)
