38f6968059d11d84f2f59e6225e193ef2c646987
Andrew Boie <andrew.p.boie@intel.com>
2016-08-23 13:14:16 -0700

    Revert "misc: Remove generic PRINT macros from synch samples"
    This reverts commit 08d7d6a70caf97562971df46bed8f281ecba7d6c [formerly f352ca1e05203bc271b1373929fb85931177fb5b].
    
    Change-Id: Iebefa150449785c00a1b2cc7e3446fba3495cd03
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: b8a27f30feb31274196faffb08470ec42efe3578

--- samples/synchronization/microkernel/src/main.c
+++ samples/synchronization/microkernel/src/main.c
@@ -1,6 +1,7 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
 DECL|taskA|function|void taskA(void)
 DECL|taskB|function|void taskB(void)
--- samples/synchronization/nanokernel/src/main.c
+++ samples/synchronization/nanokernel/src/main.c
@@ -1,7 +1,8 @@
+DECL|PRINT|macro|PRINT
+DECL|PRINT|macro|PRINT
 DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
-DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|fiberEntry|function|void fiberEntry(void)
 DECL|fiberStack|variable|fiberStack
 DECL|main|function|void main(void)
