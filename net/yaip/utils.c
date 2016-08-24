64cce4b9cf3c3e78e0a945a8bc90a49fe76678c6
Jaakko Hannikainen <jaakko.hannikainen@intel.com>
2016-08-24 14:57:55 +0300

    net: Add net_addr_pton()
    This function converts a string into an IP address.
    
    Change-Id: If57983a345fe4c18df01e58dbce7d9a50a27b82a
    Signed-off-by: Jaakko Hannikainen <jaakko.hannikainen@intel.com>
    
    Former-commit-id: f9f0d895ea34c89ed9fb9d5db514d7c38bd9ff70

--- net/yaip/utils.c
+++ net/yaip/utils.c
@@ -2,6 +2,7 @@ DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|calc_chksum_buf|function|static inline uint16_t calc_chksum_buf(uint16_t sum, struct net_buf *buf, uint16_t upper_layer_len)
 DECL|calc_chksum|function|static uint16_t calc_chksum(uint16_t sum, const uint8_t *ptr, uint16_t len)
+DECL|net_addr_pton|function|int net_addr_pton(sa_family_t family, const char *src, struct sockaddr *dst)
 DECL|net_byte_to_hex|function|char *net_byte_to_hex(uint8_t *ptr, uint8_t byte, char base, bool pad)
 DECL|net_calc_chksum_ipv4|function|uint16_t net_calc_chksum_ipv4(struct net_buf *buf)
 DECL|net_calc_chksum|function|uint16_t net_calc_chksum(struct net_buf *buf, uint8_t proto)
