ae8fe038fb6d19e27da6f51cc0dd7f4d244e54dd
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-08-24 10:53:08 +0300

    net: tests: Extented 6lo unit tests
    Earlier all unit tests are with IPv6 and UDP as next header.
    Modified tests with IPv6 only, IPv6 + UDP and IPv6 + IPHC next
    headers.
    
    Change-Id: Ie02cecb37e07ceae8a06a0653e2ccdfe0db91239
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 0d76826a0dbaef25d82dc743e3a45d59c5e7ac67

--- tests/net/6lo/src/main.c
+++ tests/net/6lo/src/main.c
@@ -15,6 +15,7 @@ DECL|dst_m1_dam01|macro|dst_m1_dam01
 DECL|dst_m1_dam10|macro|dst_m1_dam10
 DECL|dst_m1_dam11|macro|dst_m1_dam11
 DECL|fiberStack|variable|fiberStack
+DECL|icmp|member|struct net_icmp_hdr icmp;
 DECL|iphc|member|bool iphc;
 DECL|ipv6|member|struct net_ipv6_hdr ipv6;
 DECL|main_fiber|function|static void main_fiber(void)
@@ -24,12 +25,18 @@ DECL|net_6lo_data|struct|struct net_6lo_data {
 DECL|net_6lo_dev_init|function|int net_6lo_dev_init(struct device *dev)
 DECL|net_6lo_if_api|variable|net_6lo_if_api
 DECL|net_6lo_iface_init|function|static void net_6lo_iface_init(struct net_if *iface)
+DECL|nh_icmp|member|bool nh_icmp;
+DECL|nh_udp|member|bool nh_udp;
+DECL|nh|member|} nh;
 DECL|small|member|bool small;
 DECL|src_sac1_sam00|macro|src_sac1_sam00
 DECL|src_sam00|macro|src_sam00
 DECL|src_sam01|macro|src_sam01
 DECL|src_sam10|macro|src_sam10
 DECL|test_6lo|function|static int test_6lo(struct net_6lo_data *data)
+DECL|test_data_10|variable|test_data_10
+DECL|test_data_11|variable|test_data_11
+DECL|test_data_12|variable|test_data_12
 DECL|test_data_1|variable|test_data_1
 DECL|test_data_2|variable|test_data_2
 DECL|test_data_3|variable|test_data_3
@@ -38,6 +45,7 @@ DECL|test_data_5|variable|test_data_5
 DECL|test_data_6|variable|test_data_6
 DECL|test_data_7|variable|test_data_7
 DECL|test_data_8|variable|test_data_8
+DECL|test_data_9|variable|test_data_9
 DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_buf *buf)
 DECL|tests|variable|tests
 DECL|udp_dst_port_16bit|macro|udp_dst_port_16bit
