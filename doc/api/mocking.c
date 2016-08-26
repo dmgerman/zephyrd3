dad4fd6f342e56dafd84da43b3b5b2d49f54ca72
Jaakko Hannikainen <jaakko.hannikainen@intel.com>
2016-08-26 13:26:10 +0300

    ztest: Add documentation
    Origin: Original
    
    Change-Id: I0927c25fbbba5d4863f199d058d311c10d52d784
    Signed-off-by: Jaakko Hannikainen <jaakko.hannikainen@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 891c3698077b0a6628590e5d6b1583d00b0baaa7

---
+++ doc/api/mocking.c
@@ -0,0 +1,5 @@
+DECL|expect_two_parameters|function|static void expect_two_parameters(int a, int b)
+DECL|parameter_tests|function|static void parameter_tests(void)
+DECL|return_value_tests|function|static void return_value_tests(void)
+DECL|returns_int|function|static int returns_int(void)
+DECL|test_main|function|void test_main(void)
---
+++ samples/testing/integration/src/main.c
@@ -0,0 +1,2 @@
+DECL|assert_tests|function|static void assert_tests(void)
+DECL|test_main|function|void test_main(void)
