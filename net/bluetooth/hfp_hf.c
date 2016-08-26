dfa6975f027628753b3363be2d2ae67a97e907cb
Sathish Narasimman <sathish.narasimman@intel.com>
2016-08-26 14:47:27 +0530

    Bluetooth: HFP HF: Initialize Handsfree profile
    Initialize Handsfree profile for HF Role and register RFCOMM server
    channel number to the RFCOMM. And also exposes some basic callbacks
    which is required for the application.
    
    Change-Id: Ic79cbd66ef9529c4eb134cc21efcdbc388bb707d
    Signed-off-by: Sathish Narasimman <sathish.narasimman@intel.com>
    
    Former-commit-id: 9738e83285bbdcaff989929c0c59be1372ed66d4

---
+++ net/bluetooth/hfp_hf.c
@@ -0,0 +1,11 @@
+DECL|BT_DBG|macro|BT_DBG
+DECL|BT_DBG|macro|BT_DBG
+DECL|bt_hfp_hf_accept|function|static int bt_hfp_hf_accept(struct bt_conn *conn, struct bt_rfcomm_dlc **dlc)
+DECL|bt_hfp_hf_pool|variable|bt_hfp_hf_pool
+DECL|bt_hfp_hf_register|function|int bt_hfp_hf_register(struct bt_hfp_hf_cb *cb)
+DECL|bt_hf|variable|bt_hf
+DECL|hf_buf|variable|hf_buf
+DECL|hfp_hf_connected|function|static void hfp_hf_connected(struct bt_rfcomm_dlc *dlc)
+DECL|hfp_hf_disconnected|function|static void hfp_hf_disconnected(struct bt_rfcomm_dlc *dlc)
+DECL|hfp_hf_init|function|static void hfp_hf_init(void)
+DECL|hfp_hf_recv|function|static void hfp_hf_recv(struct bt_rfcomm_dlc *dlc, struct net_buf *buf)
