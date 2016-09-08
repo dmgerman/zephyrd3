24c9cb97b8357d2c488f7663c06944ef0c52660f
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-09-08 16:36:46 -0400

    unified/test_mutex: adapt to run on unified kernel
    - Use a more limited range of priorities, since the current
      implementation of the unified kernel only works with 32 priorities
      total. Instead of starting at 10 and going up by 5 up to  50, start
      at 5 and go up by 1 up to 12.
    
    - The definition of kmutex_t has changed from a uint32_t to a struct
      k_mutex *, causing this to not work anymore:
    
        const kmutex_t private_mutex;
    
      since this makes the object constant instead of the reference to it.
      Private object must be referenced like this instead:
    
        kmutex_t const private_mutex;
    
      since const is left-associative.
    
    Change-Id: I9d70bfa3944ea46033a6b49251a4993e9bd2b588
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 8011221712ea1bceae4bee3cc773a37d00c5173c

--- tests/kernel/test_mutex/src/mutex.c
+++ tests/kernel/test_mutex/src/mutex.c
@@ -3,11 +3,11 @@ DECL|HALF_SECOND|macro|HALF_SECOND
 DECL|ONE_SECOND|macro|ONE_SECOND
 DECL|RegressionTask|function|void RegressionTask(void)
 DECL|THIRD_SECOND|macro|THIRD_SECOND
-DECL|Task10|function|void Task10(void)
-DECL|Task15|function|void Task15(void)
-DECL|Task20|function|void Task20(void)
-DECL|Task25|function|void Task25(void)
-DECL|Task30|function|void Task30(void)
-DECL|Task45|function|void Task45(void)
+DECL|Task05|function|void Task05(void)
+DECL|Task06|function|void Task06(void)
+DECL|Task07|function|void Task07(void)
+DECL|Task08|function|void Task08(void)
+DECL|Task09|function|void Task09(void)
+DECL|Task11|function|void Task11(void)
 DECL|private_mutex|variable|private_mutex
 DECL|tcRC|variable|tcRC
