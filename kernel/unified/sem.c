e20919c313adfb9883b9e2bb3cc2fef927055c8c
Peter Mitsis <peter.mitsis@windriver.com>
2016-09-09 14:24:06 -0400

    unified: Add support for semaphore groups
    Semaphore groups are enabled by default. Disabling them will both
    decrease the footprint as well as improve the performance of the
    k_sem_give() routine.
    
    Change-Id: If6c1b0e2e1f71afd43e620f05f17068039d12b05
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 45403678aa65d031661f3e618a6cabedd6cd691b

--- kernel/unified/sem.c
+++ kernel/unified/sem.c
@@ -1,3 +1,16 @@
+DECL|_sem_desc|struct|struct _sem_desc {
+DECL|_sem_thread|struct|struct _sem_thread {
+DECL|desc|member|struct _sem_desc desc;
+DECL|dummy|member|struct tcs_base dummy;
+DECL|handle_sem_group|function|static int handle_sem_group(struct k_sem *sem, struct k_thread *thread)
+DECL|handle_sem_group|macro|handle_sem_group
 DECL|k_sem_give|function|void k_sem_give(struct k_sem *sem)
+DECL|k_sem_group_give|function|void k_sem_group_give(struct k_sem *sem_array[])
+DECL|k_sem_group_reset|function|void k_sem_group_reset(struct k_sem *sem_array[])
+DECL|k_sem_group_take|function|int k_sem_group_take(struct k_sem *sem_array[], struct k_sem **sem, int32_t timeout)
 DECL|k_sem_init|function|void k_sem_init(struct k_sem *sem, unsigned int initial_count,unsigned int limit)
 DECL|k_sem_take|function|int k_sem_take(struct k_sem *sem, int32_t timeout)
+DECL|sem_give_common|function|static bool sem_give_common(struct k_sem *sem)
+DECL|semg_node|member|sys_dnode_t semg_node; /* Node in list of semaphores */
+DECL|sem|member|struct k_sem *sem; /* Semaphore on which to wait */
+DECL|thread|member|struct k_thread *thread; /* Thread waiting for semaphores */
