0fd9d543427439aa056c00cc7b908d2794e9f894
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-09-10 12:38:36 +0200

    ARM: irq: Add _arch_irq_is_enabled external interrupt API
    Add _arch_irq_is_enabled external interrupt API to find out
    if an IRQ is enabled.
    
    Change-id: I8ccbaa6d4640c1ab8369d2d35c01a2cfbb02f6cd
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 79cdf33579802f252e8f5b9a64693bf75de25dd2

--- arch/arm/core/irq_manage.c
+++ arch/arm/core/irq_manage.c
@@ -1,5 +1,6 @@
 DECL|IRQ_PRIORITY_OFFSET|macro|IRQ_PRIORITY_OFFSET
 DECL|_arch_irq_disable|function|void _arch_irq_disable(unsigned int irq)
 DECL|_arch_irq_enable|function|void _arch_irq_enable(unsigned int irq)
+DECL|_arch_irq_is_enabled|function|int _arch_irq_is_enabled(unsigned int irq)
 DECL|_irq_priority_set|function|void _irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
 DECL|_irq_spurious|function|void _irq_spurious(void *unused)
