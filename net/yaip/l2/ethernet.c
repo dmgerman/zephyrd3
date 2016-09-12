4e5371a3398c168a79f361ac5e36b4ba1f736147
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-09-12 16:03:37 +0300

    net: Fix arp/ethernet broadcast and multcast addr scenario
    In case of broadcast or multicast destination address, ARP
    message is not required. But ethernet_send assumes net_arp_prepare()
    returns ARP buffer and doesn't fill any LL header information in
    all fragments.
    
    So checking whether destination address is broadcast or multicast
    in ethernet and fill LL headers according to the case. Lets assume
    if net_arp_prepare() returns success means that is ARP message.
    
    Change-Id: If5909d04d993b1690ae44cd9f5af43b8346d3a72
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: f5dec254665777bb388d61e6029745ab7dc0f715

--- net/yaip/l2/ethernet.c
+++ net/yaip/l2/ethernet.c
@@ -1,6 +1,7 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|broadcast_eth_addr|variable|broadcast_eth_addr
+DECL|check_if_dst_is_broadcast_or_mcast|function|static inline bool check_if_dst_is_broadcast_or_mcast(struct net_if *iface, struct net_buf *buf)
 DECL|ethernet_recv|function|static enum net_verdict ethernet_recv(struct net_if *iface, struct net_buf *buf)
 DECL|ethernet_reserve|function|static inline uint16_t ethernet_reserve(struct net_if *iface, void *unused)
 DECL|ethernet_send|function|static enum net_verdict ethernet_send(struct net_if *iface, struct net_buf *buf)
