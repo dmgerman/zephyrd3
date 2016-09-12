d26789cbb2a733ee9dd29578f81c596f7680f7e1
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-09-12 17:16:22 +0300

    net: apps: Add DHCPv4 client sample application
    Change-Id: Ia0366a24c2bc18cbd9278b7dc7d4a155df6521fb
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: a026c615ff8e5b9d7956a697e297cb3d42c47d3a

---
+++ samples/net/dhcpv4_client/src/main.c
@@ -0,0 +1,10 @@
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|fiberStack|variable|fiberStack
+DECL|init_app|function|static inline void init_app(void)
+DECL|main_fiber|function|void main_fiber(void)
+DECL|main|function|void main(void)
+DECL|quit_lock|variable|quit_lock
+DECL|quit|function|static inline void quit(void)
