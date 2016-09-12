f9c9f420ccbc2cac42ed09fe4b3f8f4abce20043
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-09-12 17:28:24 +0300

    nano_work: Add nano_work_pending
    This adds nano_work_pending which can be use to check if a nano_work
    is pending execution.
    
    Change-Id: Iae0492a750de93fcd7e89e3a2e74509ffce4983b
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: cd4501c4538b4899f4c75b688255c2e837b714bd

--- include/misc/nano_work.h
+++ include/misc/nano_work.h
@@ -1,5 +1,5 @@
 DECL|NANO_WORK_NUM_FLAGS|enumerator|NANO_WORK_NUM_FLAGS, /* Number of flags - must be last */
-DECL|NANO_WORK_STATE_IDLE|enumerator|NANO_WORK_STATE_IDLE, /* Work item idle state */
+DECL|NANO_WORK_STATE_PENDING|enumerator|NANO_WORK_STATE_PENDING, /* Work item pending state */
 DECL|_misc_nano_work__h_|macro|_misc_nano_work__h_
 DECL|_reserved|member|void *_reserved; /* Used by nano_fifo implementation. */
 DECL|fifo|member|struct nano_fifo fifo;
@@ -7,6 +7,7 @@ DECL|handler|member|work_handler_t handler;
 DECL|nano_delayed_work_submit|function|static inline int nano_delayed_work_submit(struct nano_delayed_work *work, int ticks)
 DECL|nano_delayed_work|struct|struct nano_delayed_work {
 DECL|nano_work_init|function|static inline void nano_work_init(struct nano_work *work, work_handler_t handler)
+DECL|nano_work_pending|function|static inline int nano_work_pending(struct nano_work *work)
 DECL|nano_work_submit_to_queue|function|static inline void nano_work_submit_to_queue(struct nano_workqueue *wq, struct nano_work *work)
 DECL|nano_work_submit|function|static inline void nano_work_submit(struct nano_work *work)
 DECL|nano_workqueue|struct|struct nano_workqueue {
