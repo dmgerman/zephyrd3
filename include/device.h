153a71246238c3fb2f68f8f160940471fdbc7d46
amirkapl <amir.kaplan@intel.com>
2016-09-12 14:35:13 +0300

    power_mgmt: Mark old device pm API functions as deprecated
    Mark old device power management API functions as deprecated:
    1. device_suspend(struct device *device,int pm_policy)
    2. device_resume(struct device *device, int pm_policy)
    In addition added deprecated comments for the macro related
    to the old API
    
    Jira: ZEP-954
    Change-Id: Ibfeeb88f4e6644409296b5f4e2ed02a149d911a2
    Signed-off-by: Amir Kaplan <amir.kaplan@intel.com>
    
    Former-commit-id: eb9d45e6cddfebd24e58ff0f5e8ef831939d31cc

--- include/device.h
+++ include/device.h
@@ -22,14 +22,14 @@ DECL|DEVICE_PM_SUSPEND_STATE|macro|DEVICE_PM_SUSPEND_STATE
 DECL|_DEVICE_H_|macro|_DEVICE_H_
 DECL|config_info|member|void *config_info;
 DECL|config|member|struct device_config *config;
-DECL|dev_pm_ops|member|struct device_pm_ops *dev_pm_ops;
+DECL|dev_pm_ops|member|struct device_pm_ops *dev_pm_ops; /* deprecated */
 DECL|device_config|struct|struct device_config {
 DECL|device_control|member|int (*device_control)(struct device *device, uint32_t command,
 DECL|device_get_power_state|function|static inline int device_get_power_state(struct device *device, uint32_t *device_power_state)
 DECL|device_pm_ops|struct|struct device_pm_ops {
-DECL|device_resume|function|static inline int device_resume(struct device *device, int pm_policy)
+DECL|device_resume|function|static inline int __deprecated device_resume(struct device *device,int pm_policy)
 DECL|device_set_power_state|function|static inline int device_set_power_state(struct device *device, uint32_t device_power_state)
-DECL|device_suspend|function|static inline int device_suspend(struct device *device, int pm_policy)
+DECL|device_suspend|function|static inline int __deprecated device_suspend(struct device *device,int pm_policy)
 DECL|device_sync_call_complete|function|static inline void device_sync_call_complete(device_sync_call_t *sync)
 DECL|device_sync_call_init|function|static inline void device_sync_call_init(device_sync_call_t *sync)
 DECL|device_sync_call_t|typedef|} device_sync_call_t;
