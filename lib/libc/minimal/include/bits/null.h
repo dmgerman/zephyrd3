de48385d7c1104358601b7b2f8b6419d6693f48c
Qiu Peiyang <peiyangx.qiu@intel.com>
2016-09-13 10:19:07 +0800

    libc: replace null.h and size_t.h with stddef.h
    According to commit 9895aec995 [formerly b71a8a4591] and Jira: ZEP-733
    (libc: remove stddef.h which is provided by the compiler),
    the stddef.h in zephyr code is removed and it's provided
    by the compiler. The original stddef.h includes two head
    files, null.h and size_t.h, which are also useless now.
    So remove these two deprecated files, in case conflicting
    definition.
    
    Change-Id: Ie7163fdbd23c32759425b50f3deff2a57cc051a9
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>
    
    Former-commit-id: c53fe299c0280d51e2aea0d8f37f55925bed433e

--- lib/libc/minimal/include/bits/null.h
+++
@@ -1,2 +0,0 @@
-DECL|NULL|macro|NULL
-DECL|NULL|macro|NULL
--- lib/libc/minimal/include/bits/size_t.h
+++
@@ -1,5 +0,0 @@
-DECL|__size_t_defined|macro|__size_t_defined
-DECL|size_t|typedef|typedef unsigned int size_t;
-DECL|size_t|typedef|typedef unsigned int size_t;
-DECL|size_t|typedef|typedef unsigned int size_t;
-DECL|size_t|typedef|typedef unsigned long int size_t;
