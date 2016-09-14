ae0a3ff441f0dd6239095156ec65f48594ed9f04
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-09-14 19:45:18 +0200

    drivers: clock_control: Add nRF5x Series SoC clock driver
    In the Nordic Semiconductor's nRF5x series SoC clock control
    system can source the system clocks from a range of internal or
    external high and low frequency oscillators and distribute them
    to modules based upon a module’s individual requirements.
    In this commit the implementation adds support for 16MHz
    peripheral clock, 32.768 kHz RC oscillator, and 32.768 kHz
    crystal oscillator clock sources.
    
    16MHz peripheral clock is required to use the 2.4GHz Radio
    peripheral. 32.768 kHz RC or crystal oscillator clock source
    is required to use the real time counter peripheral.
    
    Jira: ZEP-896
    
    Change-id: I1a1e22322a5a26b587f3e27bb979e6296987ac1c
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 5843e8df8985fecba584e57cf80e1ca79a085450

---
+++ drivers/clock_control/nrf5_power_clock.c
@@ -0,0 +1,9 @@
+DECL|_clock_control_init|function|static int _clock_control_init(struct device *dev)
+DECL|_k32src_clock_control_api|variable|_k32src_clock_control_api
+DECL|_k32src_start|function|static int _k32src_start(struct device *dev, clock_control_subsys_t sub_system)
+DECL|_m16src_clock_control_api|variable|_m16src_clock_control_api
+DECL|_m16src_start|function|static int _m16src_start(struct device *dev, clock_control_subsys_t sub_system)
+DECL|_m16src_stop|function|static int _m16src_stop(struct device *dev, clock_control_subsys_t sub_system)
+DECL|_power_clock_isr|function|static void _power_clock_isr(void *arg)
+DECL|m16src_grd|variable|m16src_grd
+DECL|m16src_ref|variable|m16src_ref
