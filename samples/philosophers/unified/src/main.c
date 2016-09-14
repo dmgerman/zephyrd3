5c1e7d2966a8202f660b506d2c5399ed33e9db19
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-09-14 12:28:27 -0400

    Dining philosophers demo for unified kernel.
    The demo can be configured to use different object types for its
    synchronization: SEMAPHORES, MUTEXES, STACKS, FIFOS and LIFOS. To
    configure a specific object, set the value of FORKS to one of these.
    
    By default, the demo uses MUTEXES.
    
    The demo can also be configured to work with static objects or dynamic
    objects. The behaviour will change depending if STATIC_OBJS is set to 0
    or 1.
    
    By default, the demo uses dynamic objects.
    
    The demo can be configured to work with threads of the same priority or
    not. If using different priorities, two threads will be cooperative
    threads, and the other four will be preemtible threads; if using one
    priority, there will be six preemtible threads of priority 0. This is
    changed via SAME_PRIO.
    
    By default, the demo uses different priorities.
    
    The number of threads is set via NUM_PHIL. The demo has only been tested
    with six threads. In theory it should work with less than six threads,
    but not with more without making changes to the forks[] array in the
    phil_obj_abstract.h header file.
    
    Change-Id: If7a0a34b216929a661245fd921a32ec413df8a4a
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: aabf74cda5e10eec04137222fcf045aa5ef3b32c

---
+++ samples/philosophers/unified/src/main.c
@@ -0,0 +1,27 @@
+DECL|DEBUG_PRINTF|macro|DEBUG_PRINTF
+DECL|DEMO_DESCRIPTION|macro|DEMO_DESCRIPTION
+DECL|FIFOS|macro|FIFOS
+DECL|FORKS|macro|FORKS
+DECL|LIFOS|macro|LIFOS
+DECL|MUTEXES|macro|MUTEXES
+DECL|NUM_PHIL|macro|NUM_PHIL
+DECL|PRINTF|macro|PRINTF
+DECL|PRINTF|macro|PRINTF
+DECL|PR_DEBUG|macro|PR_DEBUG
+DECL|PR_DEBUG|macro|PR_DEBUG
+DECL|SAME_PRIO|macro|SAME_PRIO
+DECL|SEMAPHORES|macro|SEMAPHORES
+DECL|STACKS|macro|STACKS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|STATIC_OBJS|macro|STATIC_OBJS
+DECL|display_demo_description|function|static void display_demo_description(void)
+DECL|fork|macro|fork
+DECL|get_random_delay|function|static int32_t get_random_delay(int id)
+DECL|init_objects|function|static void init_objects(void)
+DECL|is_last_philosopher|function|static inline int is_last_philosopher(int id)
+DECL|main|function|void main(void)
+DECL|new_prio|function|static int new_prio(int phil)
+DECL|philosopher|function|void philosopher(void *id, void *unused1, void *unused2)
+DECL|print_phil_state|function|static void print_phil_state(int id, const char *fmt, int32_t delay)
+DECL|set_phil_state_pos|function|static void set_phil_state_pos(int id)
+DECL|start_threads|function|static void start_threads(void)
---
+++ samples/philosophers/unified/src/phil_obj_abstract.h
@@ -0,0 +1,51 @@
+DECL|MAGIC|macro|MAGIC
+DECL|data|member|int data;
+DECL|data|member|struct packet data;
+DECL|data|member|struct packet data;
+DECL|drop|macro|drop
+DECL|drop|macro|drop
+DECL|drop|macro|drop
+DECL|drop|macro|drop
+DECL|drop|macro|drop
+DECL|fifo|member|struct k_fifo fifo;
+DECL|fork0|variable|fork0
+DECL|fork1|variable|fork1
+DECL|fork2|variable|fork2
+DECL|fork3|variable|fork3
+DECL|fork4|variable|fork4
+DECL|fork5|variable|fork5
+DECL|fork_init|macro|fork_init
+DECL|fork_init|macro|fork_init
+DECL|fork_init|macro|fork_init
+DECL|fork_init|macro|fork_init
+DECL|fork_init|macro|fork_init
+DECL|fork_obj_t|macro|fork_obj_t
+DECL|fork_obj_t|macro|fork_obj_t
+DECL|fork_obj_t|typedef|} fork_obj_t;
+DECL|fork_obj_t|typedef|} fork_obj_t;
+DECL|fork_obj_t|typedef|} fork_obj_t;
+DECL|fork_objs|variable|fork_objs
+DECL|fork_type_str|macro|fork_type_str
+DECL|fork_type_str|macro|fork_type_str
+DECL|fork_type_str|macro|fork_type_str
+DECL|fork_type_str|macro|fork_type_str
+DECL|fork_type_str|macro|fork_type_str
+DECL|fork_t|macro|fork_t
+DECL|fork_t|macro|fork_t
+DECL|fork_t|macro|fork_t
+DECL|fork_t|macro|fork_t
+DECL|fork_t|macro|fork_t
+DECL|forks|variable|forks
+DECL|lifo|member|struct k_lifo lifo;
+DECL|next|member|void *next;
+DECL|obj_init_type|macro|obj_init_type
+DECL|obj_init_type|macro|obj_init_type
+DECL|orig_packet|variable|orig_packet
+DECL|packet|struct|struct packet {
+DECL|phil_obj_abstract__h|macro|phil_obj_abstract__h
+DECL|stacks|variable|stacks
+DECL|take|macro|take
+DECL|take|macro|take
+DECL|take|macro|take
+DECL|take|macro|take
+DECL|take|macro|take
