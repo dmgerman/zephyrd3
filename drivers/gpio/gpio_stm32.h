ee7a5bc455e4f42da95b367fb629bc2f07d94e97
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-09-14 16:16:07 +0200

    gpio: stm32: Fix bug introduced by removing API 1.0 support
    Bug was introduced by dfb74252b09e2ef1bdb8cb9c419fc330e8938c2d [formerly a6cf6038e3b3a6b44bfec1573fb9b62878208ad7]
    
    Change-Id: I91ceab2ca20c4d2f70a953b56d895152a471125a
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 86d02e01b15fa6d464a1471c339b237377be1b5a

--- drivers/gpio/gpio_stm32.h
+++ drivers/gpio/gpio_stm32.h
@@ -1,5 +1,6 @@
 DECL|_STM32_GPIO_H_|macro|_STM32_GPIO_H_
 DECL|base|member|uint32_t *base;
+DECL|cb_pins|member|uint32_t cb_pins;
 DECL|cb|member|sys_slist_t cb;
 DECL|clock_subsys|member|clock_control_subsys_t clock_subsys;
 DECL|gpio_stm32_config|struct|struct gpio_stm32_config {
