8724bef24740a0ed843b848b2522c902130bb5d1
Itankar, Piyush T <piyush.t.itankar@intel.com>
2016-09-15 16:21:57 +0530

    Bluetooth: A2DP: Initialization of A2DP
    A2DP Initialization, registers callback functions with AVDTP.
    
    Change-Id: I6b24ca5e7906c08774a173f35da28b1711f34577
    Signed-off-by: Itankar, Piyush T <piyush.t.itankar@intel.com>
    
    Former-commit-id: acb8c75648fc57751c196bf47c4dcca80d1bddd1

---
+++ net/bluetooth/a2dp.c
@@ -0,0 +1,6 @@
+DECL|BT_DBG|macro|BT_DBG
+DECL|BT_DBG|macro|BT_DBG
+DECL|avdtp_cb|variable|avdtp_cb
+DECL|bt_a2dp_init|function|int bt_a2dp_init(void)
+DECL|cb_cfm|variable|cb_cfm
+DECL|cb_ind|variable|cb_ind
