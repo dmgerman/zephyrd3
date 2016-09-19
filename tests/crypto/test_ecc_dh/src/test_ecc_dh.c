090930bdc196620f82b6fe70cec5f976efbdde7f
Flavio Santes <flavio.santes@intel.com>
2016-09-18 22:35:11 -0500

    tinycrypt: Add test case for the ECC DH algorithm
    This commit adds the test-case for the ECC DH algorithm.
    
    Origin: https://github.com/01org/tinycrypt/tree/v0.2.0
    
    The code was refactored to eliminate function calls and checkpatch
    errors. Furthermore, the PRNG was replaced by sys_rand32_init.
     Refactoring history is available at:
    
    Repo:       https://gitlab.com/santes/tinycrypt/tree/refactoring
    Commit:     f71fc46403e90be0c54a77150e0539756d8314e7
    
    Jira: ZEP-614
    Jira: ZEP-861
    
    Change-Id: I884f26ef8e9ff8e79ed0858678c4f51a37796e02
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 19d22dc389b782c717aad8aa0d254002c75bff4a

---
+++ tests/crypto/test_ecc_dh/src/test_ecc_dh.c
@@ -0,0 +1,19 @@
+DECL|RC_STR|macro|RC_STR
+DECL|cavp_ecdh|function|int cavp_ecdh(int verbose)
+DECL|cavp_keygen|function|int cavp_keygen(int verbose)
+DECL|cavp_pkv|function|int cavp_pkv(int verbose)
+DECL|dh_d|variable|dh_d
+DECL|dh_x|variable|dh_x
+DECL|dh_y|variable|dh_y
+DECL|dh_z|variable|dh_z
+DECL|ecdh_vectors|function|int ecdh_vectors(char **qx_vec, char **qy_vec, char **d_vec, char **z_vec, int tests, int verbose)
+DECL|gen_d|variable|gen_d
+DECL|gen_x|variable|gen_x
+DECL|gen_y|variable|gen_y
+DECL|main|function|int main(void)
+DECL|montecarlo_ecdh|function|int montecarlo_ecdh(uint32_t num, int verbose)
+DECL|names|variable|names
+DECL|pkv_vectors|function|int pkv_vectors(char **qx_vec, char **qy_vec, char **res_vec, int *results,int tests, int verbose)
+DECL|results|variable|results
+DECL|verify_x|variable|verify_x
+DECL|verify_y|variable|verify_y
