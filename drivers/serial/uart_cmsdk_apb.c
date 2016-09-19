9f2308c1414d3348c95a584ed2d1218b8479b787
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2016-09-19 15:58:03 +0100

    serial: Add driver for CMSDK (Cortex-M System Design Kit) APB UART
    Add driver for CMSDK APB UART ports. The driver implements RX and TX
    functionalities. Data transmission is implemented using polling and
    interrupts. The driver default configuration for the port is to use
    8bit data transmission, 1 stop bit, no parity control.
    
    The driver exposes a public uart driver API and allows to register up
    to five UART devices: from 'UART_0' to 'UART_4'. The driver performs
    the required pinmux, the clock control configuration is left to the
    platform.
    
    The driver has been verified to work with the Hello World sample
    application on a Beetle Board.
    
    Jira: ZEP-1245
    Change-Id: I5baf78b7659aae2a574d8e66205e6fd5eb579133
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: bb1818a535e0254d93442b2cd315c9e4d99b7617

---
+++ drivers/serial/uart_cmsdk_apb.c
@@ -0,0 +1,69 @@
+DECL|DEV_CFG|macro|DEV_CFG
+DECL|DEV_DATA|macro|DEV_DATA
+DECL|UART_HS_TM_TX|macro|UART_HS_TM_TX
+DECL|UART_RX_BF|macro|UART_RX_BF
+DECL|UART_RX_B_OV|macro|UART_RX_B_OV
+DECL|UART_RX_EN|macro|UART_RX_EN
+DECL|UART_RX_IN_EN|macro|UART_RX_IN_EN
+DECL|UART_RX_IN|macro|UART_RX_IN
+DECL|UART_RX_OV_EN|macro|UART_RX_OV_EN
+DECL|UART_RX_OV_IN|macro|UART_RX_OV_IN
+DECL|UART_STRUCT|macro|UART_STRUCT
+DECL|UART_TX_BF|macro|UART_TX_BF
+DECL|UART_TX_B_OV|macro|UART_TX_B_OV
+DECL|UART_TX_EN|macro|UART_TX_EN
+DECL|UART_TX_IN_EN|macro|UART_TX_IN_EN
+DECL|UART_TX_IN|macro|UART_TX_IN
+DECL|UART_TX_OV_EN|macro|UART_TX_OV_EN
+DECL|UART_TX_OV_IN|macro|UART_TX_OV_IN
+DECL|baud_rate|member|uint32_t baud_rate; /* Baud rate */
+DECL|bauddiv|member|volatile uint32_t bauddiv;
+DECL|baudrate_set|function|static void baudrate_set(struct device *dev)
+DECL|ctrl|member|volatile uint32_t ctrl;
+DECL|data|member|volatile uint32_t data;
+DECL|intclear|member|volatile uint32_t intclear;
+DECL|intstatus|member|volatile uint32_t intstatus;
+DECL|irq_cb|member|uart_irq_callback_t irq_cb;
+DECL|state|member|volatile uint32_t state;
+DECL|uart_cmsdk_apb_dev_cfg_0|variable|uart_cmsdk_apb_dev_cfg_0
+DECL|uart_cmsdk_apb_dev_cfg_1|variable|uart_cmsdk_apb_dev_cfg_1
+DECL|uart_cmsdk_apb_dev_cfg_2|variable|uart_cmsdk_apb_dev_cfg_2
+DECL|uart_cmsdk_apb_dev_cfg_3|variable|uart_cmsdk_apb_dev_cfg_3
+DECL|uart_cmsdk_apb_dev_cfg_4|variable|uart_cmsdk_apb_dev_cfg_4
+DECL|uart_cmsdk_apb_dev_data_0|variable|uart_cmsdk_apb_dev_data_0
+DECL|uart_cmsdk_apb_dev_data_1|variable|uart_cmsdk_apb_dev_data_1
+DECL|uart_cmsdk_apb_dev_data_2|variable|uart_cmsdk_apb_dev_data_2
+DECL|uart_cmsdk_apb_dev_data_3|variable|uart_cmsdk_apb_dev_data_3
+DECL|uart_cmsdk_apb_dev_data_4|variable|uart_cmsdk_apb_dev_data_4
+DECL|uart_cmsdk_apb_dev_data|struct|struct uart_cmsdk_apb_dev_data {
+DECL|uart_cmsdk_apb_driver_api|variable|uart_cmsdk_apb_driver_api
+DECL|uart_cmsdk_apb_driver_api|variable|uart_cmsdk_apb_driver_api
+DECL|uart_cmsdk_apb_fifo_fill|function|static int uart_cmsdk_apb_fifo_fill(struct device *dev, const uint8_t *tx_data, int len)
+DECL|uart_cmsdk_apb_fifo_read|function|static int uart_cmsdk_apb_fifo_read(struct device *dev, uint8_t *rx_data, const int size)
+DECL|uart_cmsdk_apb_init|function|static int uart_cmsdk_apb_init(struct device *dev)
+DECL|uart_cmsdk_apb_irq_callback_set|function|static void uart_cmsdk_apb_irq_callback_set(struct device *dev, uart_irq_callback_t cb)
+DECL|uart_cmsdk_apb_irq_config_func_0|function|static void uart_cmsdk_apb_irq_config_func_0(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_0|function|static void uart_cmsdk_apb_irq_config_func_0(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_1|function|static void uart_cmsdk_apb_irq_config_func_1(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_1|function|static void uart_cmsdk_apb_irq_config_func_1(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_2|function|static void uart_cmsdk_apb_irq_config_func_2(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_2|function|static void uart_cmsdk_apb_irq_config_func_2(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_3|function|static void uart_cmsdk_apb_irq_config_func_3(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_3|function|static void uart_cmsdk_apb_irq_config_func_3(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_4|function|static void uart_cmsdk_apb_irq_config_func_4(struct device *dev)
+DECL|uart_cmsdk_apb_irq_config_func_4|function|static void uart_cmsdk_apb_irq_config_func_4(struct device *dev)
+DECL|uart_cmsdk_apb_irq_err_disable|function|static void uart_cmsdk_apb_irq_err_disable(struct device *dev)
+DECL|uart_cmsdk_apb_irq_err_enable|function|static void uart_cmsdk_apb_irq_err_enable(struct device *dev)
+DECL|uart_cmsdk_apb_irq_is_pending|function|static int uart_cmsdk_apb_irq_is_pending(struct device *dev)
+DECL|uart_cmsdk_apb_irq_rx_disable|function|static void uart_cmsdk_apb_irq_rx_disable(struct device *dev)
+DECL|uart_cmsdk_apb_irq_rx_enable|function|static void uart_cmsdk_apb_irq_rx_enable(struct device *dev)
+DECL|uart_cmsdk_apb_irq_rx_ready|function|static int uart_cmsdk_apb_irq_rx_ready(struct device *dev)
+DECL|uart_cmsdk_apb_irq_tx_disable|function|static void uart_cmsdk_apb_irq_tx_disable(struct device *dev)
+DECL|uart_cmsdk_apb_irq_tx_empty|function|static int uart_cmsdk_apb_irq_tx_empty(struct device *dev)
+DECL|uart_cmsdk_apb_irq_tx_enable|function|static void uart_cmsdk_apb_irq_tx_enable(struct device *dev)
+DECL|uart_cmsdk_apb_irq_tx_ready|function|static int uart_cmsdk_apb_irq_tx_ready(struct device *dev)
+DECL|uart_cmsdk_apb_irq_update|function|static int uart_cmsdk_apb_irq_update(struct device *dev)
+DECL|uart_cmsdk_apb_isr|function|void uart_cmsdk_apb_isr(void *arg)
+DECL|uart_cmsdk_apb_poll_in|function|static int uart_cmsdk_apb_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_cmsdk_apb_poll_out|function|static unsigned char uart_cmsdk_apb_poll_out(struct device *dev, unsigned char c)
+DECL|uart_cmsdk_apb|struct|struct uart_cmsdk_apb {
