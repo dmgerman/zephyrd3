ddda3584c4c07cc96dccc56e7d04860d0cf6d50b
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-09-19 15:12:52 +0300

    net: tests: Add tests for route management API
    Change-Id: Ic2f8d2d270f63baa0a16ae76f248485bc00eef08
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 66d3601dd515a067adcf2dcb8bee3351be64e5f5

---
+++ tests/net/route/src/main.c
@@ -0,0 +1,64 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|MAX_ROUTES|macro|MAX_ROUTES
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
+DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|data_failure|variable|data_failure
+DECL|dest_addresses|variable|dest_addresses
+DECL|dest_addr|variable|dest_addr
+DECL|entry|variable|entry
+DECL|feed_data|variable|feed_data
+DECL|fiberStack|variable|fiberStack
+DECL|func|member|bool (*func)(void);
+DECL|generic_addr|variable|generic_addr
+DECL|get_llao_len|function|static inline uint8_t get_llao_len(struct net_if *iface)
+DECL|in6addr_mcast|variable|in6addr_mcast
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|ll_addr|variable|ll_addr
+DECL|mac_addr|member|uint8_t mac_addr[sizeof(struct net_eth_addr)];
+DECL|main|function|void main(void)
+DECL|max_routes|variable|max_routes
+DECL|msg_sending|variable|msg_sending
+DECL|my_addr|variable|my_addr
+DECL|my_iface|variable|my_iface
+DECL|name|member|const char *name;
+DECL|net_ctx_create|function|static bool net_ctx_create(void)
+DECL|net_route_data_peer|variable|net_route_data_peer
+DECL|net_route_data|variable|net_route_data
+DECL|net_route_dev_init|function|int net_route_dev_init(struct device *dev)
+DECL|net_route_get_mac|function|static uint8_t *net_route_get_mac(struct device *dev)
+DECL|net_route_if_api_peer|variable|net_route_if_api_peer
+DECL|net_route_if_api|variable|net_route_if_api
+DECL|net_route_iface_init|function|static void net_route_iface_init(struct net_if *iface)
+DECL|net_route_test|struct|struct net_route_test {
+DECL|net_test_nbr_lookup_ok|function|static bool net_test_nbr_lookup_ok(struct net_if *iface, struct in6_addr *addr)
+DECL|net_test_send_na|function|static bool net_test_send_na(struct net_if *iface, struct in6_addr *addr, struct net_linkaddr *lladdr, struct in6_addr *dst)
+DECL|net_test_send_ns|function|static bool net_test_send_ns(struct net_if *iface, struct in6_addr *addr)
+DECL|peer_addr|variable|peer_addr
+DECL|peer_iface|variable|peer_iface
+DECL|populate_nbr_cache|function|static bool populate_nbr_cache(void)
+DECL|recipient|variable|recipient
+DECL|route_add_many|function|static bool route_add_many(void)
+DECL|route_add|function|static bool route_add(void)
+DECL|route_del_again|function|static bool route_del_again(void)
+DECL|route_del_many|function|static bool route_del_many(void)
+DECL|route_del_nexthop_again|function|static bool route_del_nexthop_again(void)
+DECL|route_del_nexthop|function|static bool route_del_nexthop(void)
+DECL|route_del|function|static bool route_del(void)
+DECL|route_get_nexthop|function|static bool route_get_nexthop(void)
+DECL|route_lookup_fail|function|static bool route_lookup_fail(void)
+DECL|route_lookup_ok|function|static bool route_lookup_ok(void)
+DECL|route_update|function|static bool route_update(void)
+DECL|set_llao|function|static inline void set_llao(struct net_linkaddr *lladdr, uint8_t *llao, uint8_t llao_len, uint8_t type)
+DECL|setup_icmpv6_hdr|function|static inline void setup_icmpv6_hdr(struct net_buf *buf, uint8_t type, uint8_t code)
+DECL|test_failed|variable|test_failed
+DECL|test_init|function|static bool test_init(void)
+DECL|test_routes|variable|test_routes
+DECL|tester_send_peer|function|static int tester_send_peer(struct net_if *iface, struct net_buf *buf)
+DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_buf *buf)
+DECL|tests|variable|tests
+DECL|udp_ctx|variable|udp_ctx
+DECL|wait_data|variable|wait_data
