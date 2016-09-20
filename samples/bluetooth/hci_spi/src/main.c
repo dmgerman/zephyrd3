e128ad3797f65fc571c1fc531c452ec7ec2cb121
Ricardo Salveti <ricardo.salveti@linaro.org>
2016-09-20 16:14:16 +0530

    samples: Bluetooth: add HCI SPI raw handler sample
    Add a sample application that allows a Zephyr-based Bluetooth
    controller to interface with an HCI driver via SPI. This sample
    implements the same BT SPI protocol already as Zephyr's HCI SPI
    driver.
    
    Currently, the sample only supports the legacy SPI API.
    
    Provide a single configuration file, avoiding board-specific
    files. Some board-specific configuration information must be provided
    via other means:
    
    - CONFIG_BT_CONTROLLER_TO_HOST_SPI_DEV_NAME
    - CONFIG_BT_CONTROLLER_TO_HOST_SPI_IRQ_DEV_NAME
    - CONFIG_BT_CONTROLLER_TO_HOST_SPI_IRQ_PIN
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Former-commit-id: 8dbb38800057234932d6bd71a4545c3a47fbee73

---
+++ samples/bluetooth/hci_spi/src/main.c
@@ -0,0 +1,31 @@
+DECL|BT_BUF_ACL_SIZE|macro|BT_BUF_ACL_SIZE
+DECL|BT_L2CAP_MTU|macro|BT_L2CAP_MTU
+DECL|BT_L2CAP_MTU|macro|BT_L2CAP_MTU
+DECL|CMD_BUF_SIZE|macro|CMD_BUF_SIZE
+DECL|EVT_BLUE_INITIALIZED|macro|EVT_BLUE_INITIALIZED
+DECL|GPIO_IRQ_DEV_NAME|macro|GPIO_IRQ_DEV_NAME
+DECL|GPIO_IRQ_PIN|macro|GPIO_IRQ_PIN
+DECL|HCI_ACL|macro|HCI_ACL
+DECL|HCI_CMD|macro|HCI_CMD
+DECL|HCI_EVT|macro|HCI_EVT
+DECL|HCI_SCO|macro|HCI_SCO
+DECL|PACKET_TYPE|macro|PACKET_TYPE
+DECL|READY_NOW|macro|READY_NOW
+DECL|SANITY_CHECK|macro|SANITY_CHECK
+DECL|SPI_HCI_DEV_NAME|macro|SPI_HCI_DEV_NAME
+DECL|SPI_MAX_MSG_LEN|macro|SPI_MAX_MSG_LEN
+DECL|SPI_READ|macro|SPI_READ
+DECL|SPI_WRITE|macro|SPI_WRITE
+DECL|STATUS_HEADER_READY|macro|STATUS_HEADER_READY
+DECL|STATUS_HEADER_TOREAD|macro|STATUS_HEADER_TOREAD
+DECL|TX_BUF_COUNT|macro|TX_BUF_COUNT
+DECL|TX_BUF_COUNT|macro|TX_BUF_COUNT
+DECL|bt_tx_thread_data|variable|bt_tx_thread_data
+DECL|bt_tx_thread|function|static void bt_tx_thread(void *p1, void *p2, void *p3)
+DECL|gpio_dev|variable|gpio_dev
+DECL|hci_spi_init|function|static int hci_spi_init(struct device *unused)
+DECL|main|function|void main(void)
+DECL|rxmsg|variable|rxmsg
+DECL|spi_hci_dev|variable|spi_hci_dev
+DECL|spi_send|function|static inline int spi_send(struct net_buf *buf)
+DECL|txmsg|variable|txmsg
