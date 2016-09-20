275d4c612a9bd0e9224993b0ae45e9b29dcc7688
Baohong Liu <baohong.liu@intel.com>
2016-09-20 11:57:50 -0700

    drivers: pwm: re-design pwm API interfaces
    Re-design the pwm API interfaces. A RFC was sent and discussed
    recently on the re-design of PWM APIs.
    
    APIs like pwm_pin_set_values, pwm_pin_set_period, pwm_pin
    _set_duty_cycle will be deprecated and replaced by the new
    APIs pwm_pin_set_cycles and pwm_pin_set_usec.
    
    A new API pwm_get_cycles_per_sec will be added to get the
    clock rate for pwm pins.
    
    The API pwm_pin_set_phase will be deprecated(it always sets
    the phase to zero).
    
    The API pwm_pin_configure will be deprecated since it is not
    doing anything for most of the platforms or it can be moved
    to device init function for the other platforms.
    
    All the pwm_all_xxx APIs will be deprecated. If necessary,
    user or an app can set the pins individually.
    
    Jira: ZEP-745
    
    Change-Id: Ie7095df2a67cff08873b59794812ac64ee00c1c2
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 56e0b53c6e6cb7eae1ea78789bf469dbbedafc67

--- include/pwm.h
+++ include/pwm.h
@@ -2,18 +2,25 @@ DECL|PWM_ACCESS_ALL|macro|PWM_ACCESS_ALL
 DECL|PWM_ACCESS_BY_PIN|macro|PWM_ACCESS_BY_PIN
 DECL|__PWM_H__|macro|__PWM_H__
 DECL|config|member|pwm_config_t config;
-DECL|pwm_all_configure|function|static inline int pwm_all_configure(struct device *dev, int flags)
-DECL|pwm_all_period|function|static inline int pwm_all_period(struct device *dev, uint32_t period)
-DECL|pwm_all_set_duty_cycle|function|static inline int pwm_all_set_duty_cycle(struct device *dev, uint8_t duty)
-DECL|pwm_all_set_phase|function|static inline int pwm_all_set_phase(struct device *dev, uint8_t phase)
-DECL|pwm_all_set_values|function|static inline int pwm_all_set_values(struct device *dev, uint32_t on, uint32_t off)
+DECL|get_cycles_per_sec|member|pwm_get_cycles_per_sec_t get_cycles_per_sec;
+DECL|pin_set|member|pwm_pin_set_t pin_set;
+DECL|pwm_all_configure|function|static inline int __deprecated pwm_all_configure(struct device *dev, int flags)
+DECL|pwm_all_period|function|static inline int __deprecated pwm_all_period(struct device *dev, uint32_t period)
+DECL|pwm_all_set_duty_cycle|function|static inline int __deprecated pwm_all_set_duty_cycle(struct device *dev, uint8_t duty)
+DECL|pwm_all_set_phase|function|static inline int __deprecated pwm_all_set_phase(struct device *dev, uint8_t phase)
+DECL|pwm_all_set_values|function|static inline int __deprecated pwm_all_set_values(struct device *dev, uint32_t on, uint32_t off)
 DECL|pwm_config_t|typedef|typedef int (*pwm_config_t)(struct device *dev, int access_op,
 DECL|pwm_driver_api|struct|struct pwm_driver_api {
-DECL|pwm_pin_configure|function|static inline int pwm_pin_configure(struct device *dev, uint8_t pwm, int flags)
-DECL|pwm_pin_set_duty_cycle|function|static inline int pwm_pin_set_duty_cycle(struct device *dev, uint32_t pwm, uint8_t duty)
-DECL|pwm_pin_set_period|function|static inline int pwm_pin_set_period(struct device *dev, uint32_t pwm, uint32_t period)
-DECL|pwm_pin_set_phase|function|static inline int pwm_pin_set_phase(struct device *dev, uint32_t pwm, uint8_t phase)
-DECL|pwm_pin_set_values|function|static inline int pwm_pin_set_values(struct device *dev, uint32_t pwm, uint32_t on, uint32_t off)
+DECL|pwm_get_cycles_per_sec_t|typedef|typedef int (*pwm_get_cycles_per_sec_t)(struct device *dev, uint32_t pwm,
+DECL|pwm_get_cycles_per_sec|function|static inline int pwm_get_cycles_per_sec(struct device *dev, uint32_t pwm, uint64_t *cycles)
+DECL|pwm_pin_configure|function|static inline int __deprecated pwm_pin_configure(struct device *dev, uint8_t pwm, int flags)
+DECL|pwm_pin_set_cycles|function|static inline int pwm_pin_set_cycles(struct device *dev, uint32_t pwm, uint32_t period, uint32_t pulse)
+DECL|pwm_pin_set_duty_cycle|function|static inline int __deprecated pwm_pin_set_duty_cycle(struct device *dev, uint32_t pwm, uint8_t duty)
+DECL|pwm_pin_set_period|function|static inline int __deprecated pwm_pin_set_period(struct device *dev, uint32_t pwm, uint32_t period)
+DECL|pwm_pin_set_phase|function|static inline int __deprecated pwm_pin_set_phase(struct device *dev, uint32_t pwm, uint8_t phase)
+DECL|pwm_pin_set_t|typedef|typedef int (*pwm_pin_set_t)(struct device *dev, uint32_t pwm,
+DECL|pwm_pin_set_usec|function|static inline int pwm_pin_set_usec(struct device *dev, uint32_t pwm, uint32_t period, uint32_t pulse)
+DECL|pwm_pin_set_values|function|static inline int __deprecated pwm_pin_set_values(struct device *dev, uint32_t pwm, uint32_t on, uint32_t off)
 DECL|pwm_set_duty_cycle_t|typedef|typedef int (*pwm_set_duty_cycle_t)(struct device *dev, int access_op,
 DECL|pwm_set_period_t|typedef|typedef int (*pwm_set_period_t)(struct device *dev, int access_op,
 DECL|pwm_set_phase_t|typedef|typedef int (*pwm_set_phase_t)(struct device *dev, int access_op,
