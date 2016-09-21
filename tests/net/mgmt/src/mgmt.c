9dc0b4726396db5a55259d9396195fb8138b2e93
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-09-21 15:44:03 +0200

    tests: net: Add net management event sender/receiver tests
    It mimics how event could be thrown through a dedicated thrower (a
    fiber). The amount and event it can throw is gived through parameters.
    
    Change-Id: I484bc72bdb730a77ce976b0b2998411d9918b7fc
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 8b1953f74003ecde6de88025ef8294e4d1c1f28c

--- tests/net/mgmt/src/mgmt.c
+++ tests/net/mgmt/src/mgmt.c
@@ -1,4 +1,17 @@
+DECL|TEST_MGMT_EVENT_UNHANDLED|macro|TEST_MGMT_EVENT_UNHANDLED
+DECL|TEST_MGMT_EVENT|macro|TEST_MGMT_EVENT
 DECL|TEST_MGMT_REQUEST|macro|TEST_MGMT_REQUEST
+DECL|event2throw|variable|event2throw
+DECL|initialize_event_tests|function|static void initialize_event_tests(void)
 DECL|main|function|void main(void)
+DECL|receiver_cb|function|static void receiver_cb(struct net_mgmt_event_cb *cb,uint32_t nm_event, struct net_if *iface)
+DECL|rx_calls|variable|rx_calls
+DECL|rx_cb|variable|rx_cb
+DECL|rx_event|variable|rx_event
 DECL|test_mgmt_request|function|int test_mgmt_request(uint32_t mgmt_request, struct net_if *iface, void *data, uint32_t len)
 DECL|test_requesting_nm|function|static inline int test_requesting_nm(void)
+DECL|test_sending_event|function|static inline int test_sending_event(uint32_t times, bool receiver)
+DECL|throw_times|variable|throw_times
+DECL|thrower_fiber|function|static void thrower_fiber(void)
+DECL|thrower_lock|variable|thrower_lock
+DECL|thrower_stack|variable|thrower_stack
