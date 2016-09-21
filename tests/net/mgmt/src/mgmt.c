0dbee1dd0da2632d744d56df60b9c0f6f6eb267a
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-09-21 12:41:16 +0200

    tests: net: Add a basic network management API test
    Testing if net_mng() works.
    
    Change-Id: I303cffa206859184a503ba8efe392b673b162e43
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: ec1d2c5a48571b48e49975725b2d804d037e174e

---
+++ tests/net/mgmt/src/mgmt.c
@@ -0,0 +1,4 @@
+DECL|TEST_MGMT_REQUEST|macro|TEST_MGMT_REQUEST
+DECL|main|function|void main(void)
+DECL|test_mgmt_request|function|int test_mgmt_request(uint32_t mgmt_request, struct net_if *iface, void *data, uint32_t len)
+DECL|test_requesting_nm|function|static inline int test_requesting_nm(void)
