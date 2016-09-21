8af6f32ae35b2347f1eb7636ab0f6bd0b5e40426
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-09-21 11:11:47 +0200

    net: Add support for event handling on network management API
    This adds the capability to raise an event, and for anybody to listen to
    it.
    
    Change-Id: I2287b43d678930c29391efa2ddf7b12652cc1bb6
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: cce3a7e3d52bbcab6288e5425b81d6f25c398040

--- include/net/yaip/net_mgmt.h
+++ include/net/yaip/net_mgmt.h
@@ -10,4 +10,12 @@ DECL|NET_MGMT_ON_IFACE_MASK|macro|NET_MGMT_ON_IFACE_MASK
 DECL|NET_MGMT_ON_IFACE|macro|NET_MGMT_ON_IFACE
 DECL|NET_MGMT_REGISTER_REQUEST_HANDLER|macro|NET_MGMT_REGISTER_REQUEST_HANDLER
 DECL|__NET_MGMT_H__|macro|__NET_MGMT_H__
+DECL|event_mask|member|uint32_t event_mask;
+DECL|handler|member|net_mgmt_callback_t handler;
+DECL|net_mgmt_callback_t|typedef|typedef void (*net_mgmt_callback_t)(struct net_mgmt_event_cb *cb,
+DECL|net_mgmt_event_cb|struct|struct net_mgmt_event_cb {
+DECL|net_mgmt_init_event_cb|function|static inline void net_mgmt_init_event_cb(struct net_mgmt_event_cb *callback, net_mgmt_callback_t handler, uint32_t mgmt_event_mask)
+DECL|net_mgmt_init|macro|net_mgmt_init
+DECL|net_mgmt_notify|macro|net_mgmt_notify
 DECL|net_mgmt|macro|net_mgmt
+DECL|node|member|sys_snode_t node;
---
+++ net/yaip/net_mgmt.c
@@ -0,0 +1,22 @@
+DECL|events|variable|events
+DECL|event|member|uint32_t event;
+DECL|global_event_mask|variable|global_event_mask
+DECL|iface|member|struct net_if *iface;
+DECL|in_event|variable|in_event
+DECL|mgmt_add_event_mask|function|static inline void mgmt_add_event_mask(uint32_t event_mask)
+DECL|mgmt_callbacks|variable|mgmt_callbacks
+DECL|mgmt_clean_event|function|static inline void mgmt_clean_event(struct mgmt_event_entry *mgmt_event)
+DECL|mgmt_event_entry|struct|struct mgmt_event_entry {
+DECL|mgmt_fiber_stack|variable|mgmt_fiber_stack
+DECL|mgmt_fiber|function|static void mgmt_fiber(void)
+DECL|mgmt_is_event_handled|function|static inline bool mgmt_is_event_handled(uint32_t mgmt_event)
+DECL|mgmt_pop_event|function|static inline struct mgmt_event_entry *mgmt_pop_event(void)
+DECL|mgmt_push_event|function|static inline void mgmt_push_event(uint32_t mgmt_event, struct net_if *iface)
+DECL|mgmt_rebuild_global_event_mask|function|static inline void mgmt_rebuild_global_event_mask(void)
+DECL|mgmt_run_callbacks|function|static inline void mgmt_run_callbacks(struct mgmt_event_entry *mgmt_event)
+DECL|net_mgmt_add_event_callback|function|void net_mgmt_add_event_callback(struct net_mgmt_event_cb *cb)
+DECL|net_mgmt_del_event_callback|function|void net_mgmt_del_event_callback(struct net_mgmt_event_cb *cb)
+DECL|net_mgmt_init|function|void net_mgmt_init(void)
+DECL|net_mgmt_notify|function|void net_mgmt_notify(uint32_t mgmt_event, struct net_if *iface)
+DECL|network_event|variable|network_event
+DECL|out_event|variable|out_event
