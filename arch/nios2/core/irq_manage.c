aa0abfa17de00b86f982e55ad942ced073bacaaf
Andrew Boie <andrew.p.boie@intel.com>
2016-09-22 11:20:26 -0700

    kernel: remove lingering irq_connect_dynamic() references
    This API no longer exists.
    
    Change-Id: I724bee8c0ebfbbe2d47c00c7645977d817bbcd36
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 325cae5c94863603ad98c80147a827fbd9488b87

--- arch/nios2/core/irq_manage.c
+++ arch/nios2/core/irq_manage.c
@@ -1,4 +1,3 @@
-DECL|_arch_irq_connect_dynamic|function|int _arch_irq_connect_dynamic(unsigned int irq, unsigned int priority, void (*routine)(void *parameter), void *parameter, uint32_t flags)
 DECL|_arch_irq_disable|function|void _arch_irq_disable(unsigned int irq)
 DECL|_arch_irq_enable|function|void _arch_irq_enable(unsigned int irq)
 DECL|_enter_irq|function|void _enter_irq(uint32_t ipending)
