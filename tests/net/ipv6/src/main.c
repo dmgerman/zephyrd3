158e9a1b63be922cf6fdf72dbc310de1d64babfc
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-09-22 15:57:42 +0300

    net: tests: Add initial set of IPv6 tests
    Change-Id: Ib68b38a809068ca14ee9307e9ea605f03611ebd3
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 338c2be888e0e9e8575aa4d5ce7085b45e3f86e7

---
+++ tests/net/ipv6/src/main.c
@@ -0,0 +1,38 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|MY_PORT|macro|MY_PORT
+DECL|PEER_PORT|macro|PEER_PORT
+DECL|SENDING|macro|SENDING
+DECL|WAIT_TIME_LONG|macro|WAIT_TIME_LONG
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
+DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|func|member|bool (*func)(void);
+DECL|icmpv6_ns_invalid|variable|icmpv6_ns_invalid
+DECL|icmpv6_ns_no_sllao|variable|icmpv6_ns_no_sllao
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|mac_addr|member|uint8_t mac_addr[sizeof(struct net_eth_addr)];
+DECL|main|function|void main(void)
+DECL|mcast_addr|variable|mcast_addr
+DECL|my_addr|variable|my_addr
+DECL|name|member|const char *name;
+DECL|net_test_data|variable|net_test_data
+DECL|net_test_dev_init|function|int net_test_dev_init(struct device *dev)
+DECL|net_test_get_mac|function|static uint8_t *net_test_get_mac(struct device *dev)
+DECL|net_test_if_api|variable|net_test_if_api
+DECL|net_test_iface_init|function|static void net_test_iface_init(struct net_if *iface)
+DECL|net_test_ipv6|struct|struct net_test_ipv6 {
+DECL|net_test_nbr_lookup_fail|function|static bool net_test_nbr_lookup_fail(void)
+DECL|net_test_nbr_lookup_ok|function|static bool net_test_nbr_lookup_ok(void)
+DECL|net_test_prefix_timeout_overflow|function|static bool net_test_prefix_timeout_overflow(void)
+DECL|net_test_prefix_timeout|function|static bool net_test_prefix_timeout(void)
+DECL|net_test_send_ns_extra_options|function|static bool net_test_send_ns_extra_options(void)
+DECL|net_test_send_ns_mcast|function|static bool net_test_send_ns_mcast(void)
+DECL|net_test_send_ns_no_options|function|static bool net_test_send_ns_no_options(void)
+DECL|net_test_send_ns|function|static bool net_test_send_ns(void)
+DECL|peer_addr|variable|peer_addr
+DECL|test_failed|variable|test_failed
+DECL|test_init|function|static bool test_init(void)
+DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_buf *buf)
+DECL|tests|variable|tests
+DECL|wait_data|variable|wait_data
