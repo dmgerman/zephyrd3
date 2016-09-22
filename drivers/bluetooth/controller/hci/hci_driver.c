24392f8aa8ad83d94d6142144421c35dc48d9d7e
Carles Cufi <carles.cufi@nordicsemi.no>
2016-09-22 16:46:21 +0200

    Bluetooth: Controller: Use net_buf for evt and ACL RX
    Use the net_buf structure directly when populating incoming
    ACL data or asynchronous events. This ensures that no additional
    memcpy() operations are required and removes completely the
    statically allocated buffers in hci.c.
    
    Jira: ZEP-726
    
    Change-Id: I6ac8bd0becb7037ce8ecfac109f44832d23fcfd2
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 2f33724017728ece52acec34910ffb55ab75ea41

--- drivers/bluetooth/controller/hci/hci_driver.c
+++ drivers/bluetooth/controller/hci/hci_driver.c
@@ -5,11 +5,8 @@ DECL|HCI_CMD|macro|HCI_CMD
 DECL|HCI_EVT|macro|HCI_EVT
 DECL|HCI_SCO|macro|HCI_SCO
 DECL|_hci_driver_init|function|static int _hci_driver_init(struct device *unused)
-DECL|acl_create|function|static struct net_buf *acl_create(uint8_t *remaining, uint8_t **in)
 DECL|cmd_handle|function|static int cmd_handle(struct net_buf *buf)
 DECL|drv|variable|drv
-DECL|evt_acl_create|function|static int evt_acl_create(uint8_t remaining, uint8_t *in)
-DECL|evt_create|function|static struct net_buf *evt_create(uint8_t *remaining, uint8_t **in)
 DECL|hci_driver_open|function|static int hci_driver_open(void)
 DECL|hci_driver_send|function|static int hci_driver_send(struct net_buf *buf)
 DECL|nano_sem_recv|variable|nano_sem_recv
