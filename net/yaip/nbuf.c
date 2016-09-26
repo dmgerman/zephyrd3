2cfca0904e3c91e2660bf076dc822c8299aef887
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-09-26 10:44:34 +0300

    net: Add be16 and be32 read helpers to nbuf.h
    Two utility functions net_nbuf_read_be16() and net_nbuf_read_be32()
    added which return either 16 or 32 bit big endian value.
    
    Change-Id: I3401dde75669b429160c602a820359c800671afb
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7a4cd852d48a2a89871a80756b8a2d3e3469b52d

--- net/yaip/nbuf.c
+++ net/yaip/nbuf.c
@@ -67,6 +67,8 @@ DECL|net_nbuf_print_frags|function|void net_nbuf_print_frags(struct net_buf *buf
 DECL|net_nbuf_print|function|void net_nbuf_print(void)
 DECL|net_nbuf_pull|function|struct net_buf *net_nbuf_pull(struct net_buf *buf, size_t amount)
 DECL|net_nbuf_push|function|struct net_buf *net_nbuf_push(struct net_buf *parent, struct net_buf *buf, size_t amount)
+DECL|net_nbuf_read_be16|function|struct net_buf *net_nbuf_read_be16(struct net_buf *buf, uint16_t offset, uint16_t *pos, uint16_t *value)
+DECL|net_nbuf_read_be32|function|struct net_buf *net_nbuf_read_be32(struct net_buf *buf, uint16_t offset, uint16_t *pos, uint32_t *value)
 DECL|net_nbuf_read_byte|function|static inline struct net_buf *net_nbuf_read_byte(struct net_buf *buf, uint16_t offset, uint16_t *pos, uint8_t *data)
 DECL|net_nbuf_read|function|struct net_buf *net_nbuf_read(struct net_buf *buf, uint16_t offset, uint16_t *pos, uint16_t len, uint8_t *data)
 DECL|net_nbuf_ref_debug|function|struct net_buf *net_nbuf_ref_debug(struct net_buf *buf, const char *caller, int line) #else struct net_buf *net_nbuf_ref(struct net_buf *buf) #endif
