fb6d635ed79e29cd8b9a172480aece26ac961bdc
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-09-26 18:04:54 +0300

    net: Adopt new nbuf API's to RA message handlers.
    Add multi fragment parsing support for Router Advertisement
    packet.
    
    Change-Id: Ie2730156c96759ee720f406f9d034b350643322e
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: c69b10a3a0e946961de5460eb3468a358cc03ba3

--- net/yaip/ipv6.c
+++ net/yaip/ipv6.c
@@ -38,8 +38,8 @@ DECL|handle_ns_neighbor|function|static inline void handle_ns_neighbor(struct ne
 DECL|handle_prefix_autonomous|function|static inline void handle_prefix_autonomous(struct net_buf *buf,struct net_icmpv6_nd_opt_prefix_info *prefix_info)
 DECL|handle_prefix_onlink|function|static inline void handle_prefix_onlink(struct net_buf *buf,struct net_icmpv6_nd_opt_prefix_info *prefix_info)
 DECL|handle_ra_input|function|static enum net_verdict handle_ra_input(struct net_buf *buf)
-DECL|handle_ra_neighbor|function|static inline struct net_nbr *handle_ra_neighbor(struct net_buf *buf,struct net_icmpv6_nd_opt_hdr *hdr)
-DECL|handle_ra_prefix|function|static inline void handle_ra_prefix(struct net_buf *buf, struct net_icmpv6_nd_opt_hdr *hdr)
+DECL|handle_ra_neighbor|function|static inline struct net_buf *handle_ra_neighbor(struct net_buf *buf, struct net_buf *frag, uint8_t len, uint16_t offset, uint16_t *pos, struct net_nbr **nbr)
+DECL|handle_ra_prefix|function|static inline struct net_buf *handle_ra_prefix(struct net_buf *buf, struct net_buf *frag, uint8_t len, uint16_t offset, uint16_t *pos)
 DECL|is_router|member|bool is_router;
 DECL|link_metric|member|uint16_t link_metric;
 DECL|na_input_handler|variable|na_input_handler
