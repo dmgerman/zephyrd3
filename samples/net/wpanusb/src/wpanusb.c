f5269fdf1360b5a22b13dc3b8fbb0799afe5771c
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-09-27 15:43:25 +0300

    wpanusb: Export ieee802.14.5 over USB
    Export ieee802154 radio over USB to be used in other Operation Systems
    like Linux. In this scenario Linux SoftMAC driver would be used
    implementing ieee802154 stack inside Linux.
    
    Currently tested to work with Raspberry Pi and addon ieee802154 board.
    
    Change-Id: Iabcbbfcaad8e3935a63faca50a19fb624e78a351
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 3bc58717a1ef593dd7c622c710c1b6ef2fc1a440

---
+++ samples/net/wpanusb/src/wpanusb.c
@@ -0,0 +1,59 @@
+DECL|ATUSB_BUFFER_SIZE|macro|ATUSB_BUFFER_SIZE
+DECL|ATUSB_BULK_EP_MPS|macro|ATUSB_BULK_EP_MPS
+DECL|ATUSB_CLASS_MAX_DATA_SIZE|macro|ATUSB_CLASS_MAX_DATA_SIZE
+DECL|ATUSB_CONF_SIZE|macro|ATUSB_CONF_SIZE
+DECL|ATUSB_ENDP_BULK_IN|macro|ATUSB_ENDP_BULK_IN
+DECL|ATUSB_IF1_NUM_EP|macro|ATUSB_IF1_NUM_EP
+DECL|ATUSB_INTERRUPT_EP_MPS|macro|ATUSB_INTERRUPT_EP_MPS
+DECL|ATUSB_NUM_CONF|macro|ATUSB_NUM_CONF
+DECL|ATUSB_NUM_EP|macro|ATUSB_NUM_EP
+DECL|ATUSB_NUM_ITF|macro|ATUSB_NUM_ITF
+DECL|ATUSB_PROTOCOL|macro|ATUSB_PROTOCOL
+DECL|ATUSB_SUBCLASS|macro|ATUSB_SUBCLASS
+DECL|DEVICE_RELNUM|macro|DEVICE_RELNUM
+DECL|DEV_DATA|macro|DEV_DATA
+DECL|HIGH_BYTE|macro|HIGH_BYTE
+DECL|LOW_BYTE|macro|LOW_BYTE
+DECL|PRODUCT_ID|macro|PRODUCT_ID
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|VENDOR_ID|macro|VENDOR_ID
+DECL|__dev|variable|__dev
+DECL|buffer|variable|buffer
+DECL|hexdump|function|static void hexdump(const char *str, const uint8_t *packet, size_t length)
+DECL|ieee802154_dev|variable|ieee802154_dev
+DECL|ieee802154_init|function|void ieee802154_init(struct net_if *iface)
+DECL|ieee802154_radio_handle_ack|function|extern enum net_verdict ieee802154_radio_handle_ack(struct net_if *iface, struct net_buf *buf)
+DECL|ieee802154_radio_send|function|int ieee802154_radio_send(struct net_if *iface, struct net_buf *buf)
+DECL|init_tx_queue|function|static void init_tx_queue(void)
+DECL|interface_data|member|uint8_t interface_data[ATUSB_CLASS_MAX_DATA_SIZE];
+DECL|main|function|void main(void)
+DECL|net_recv_data|function|int net_recv_data(struct net_if *iface, struct net_buf *buf)
+DECL|notification_sent|member|uint8_t notification_sent;
+DECL|radio_api|variable|radio_api
+DECL|set_channel|function|static int set_channel(void *data, int len)
+DECL|set_ieee_addr|function|static int set_ieee_addr(void *data, int len)
+DECL|set_pan_id|function|static int set_pan_id(void *data, int len)
+DECL|set_short_addr|function|static int set_short_addr(void *data, int len)
+DECL|start|function|static int start(void)
+DECL|stop|function|static int stop(void)
+DECL|try_write|function|static int try_write(uint8_t ep, uint8_t *data, uint16_t len)
+DECL|tx_fiber_id|variable|tx_fiber_id
+DECL|tx_fiber_stack|variable|tx_fiber_stack
+DECL|tx_fiber|function|static void tx_fiber(void)
+DECL|tx_queue|variable|tx_queue
+DECL|tx|function|static int tx(struct net_buf *pkt)
+DECL|usb_status|member|enum usb_dc_status_code usb_status;
+DECL|wpanusb_bulk_in|function|static void wpanusb_bulk_in(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
+DECL|wpanusb_class_handler|function|static int wpanusb_class_handler(struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
+DECL|wpanusb_config|variable|wpanusb_config
+DECL|wpanusb_custom_handler|function|static int wpanusb_custom_handler(struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
+DECL|wpanusb_desc|variable|wpanusb_desc
+DECL|wpanusb_dev_data_t|struct|struct wpanusb_dev_data_t {
+DECL|wpanusb_dev_data|variable|wpanusb_dev_data
+DECL|wpanusb_dev|variable|wpanusb_dev
+DECL|wpanusb_ep|variable|wpanusb_ep
+DECL|wpanusb_init|function|static int wpanusb_init(struct device *dev)
+DECL|wpanusb_start|function|static void wpanusb_start(struct device *dev)
+DECL|wpanusb_start|macro|wpanusb_start
+DECL|wpanusb_status_cb|function|static void wpanusb_status_cb(enum usb_dc_status_code status)
+DECL|wpanusb_vendor_handler|function|static int wpanusb_vendor_handler(struct usb_setup_packet *setup, int32_t *len, uint8_t **data)
