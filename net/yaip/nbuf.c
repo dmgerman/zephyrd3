b6a9bb50844ace8e58b41a1cf37a2f0fd85f1b31
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-09-27 10:46:26 +0300

    net: Optimize net_nbuf_write() when handling last frag
    Get the last fragment only once instead of searching it for
    every byte written.
    
    Change-Id: Iebeed21671b12bc081d7a1291f91d2c897a7a1d6
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 060362ff14fed4ab5bb7b75872bfb3931bd909c0

--- net/yaip/nbuf.c
+++ net/yaip/nbuf.c
@@ -73,7 +73,7 @@ DECL|net_nbuf_read_byte|function|static inline struct net_buf *net_nbuf_read_byt
 DECL|net_nbuf_read|function|struct net_buf *net_nbuf_read(struct net_buf *buf, uint16_t offset, uint16_t *pos, uint16_t len, uint8_t *data)
 DECL|net_nbuf_ref_debug|function|struct net_buf *net_nbuf_ref_debug(struct net_buf *buf, const char *caller, int line) #else struct net_buf *net_nbuf_ref(struct net_buf *buf) #endif
 DECL|net_nbuf_unref_debug|function|void net_nbuf_unref_debug(struct net_buf *buf, const char *caller, int line)
-DECL|net_nbuf_write_byte|function|static inline bool net_nbuf_write_byte(struct net_buf *buf, uint8_t value)
+DECL|net_nbuf_write_byte|function|static inline bool net_nbuf_write_byte(struct net_buf *buf, uint8_t value, struct net_buf **last)
 DECL|net_nbuf_write|function|bool net_nbuf_write(struct net_buf *buf, uint16_t len, uint8_t *data)
 DECL|num_free_data_bufs|variable|num_free_data_bufs
 DECL|num_free_rx_bufs|variable|num_free_rx_bufs
