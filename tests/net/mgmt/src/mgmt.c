0125182ac2997c5bd6ff365d5cc4cb2a30fa417d
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-09-29 19:07:00 +0200

    tests: Add network core event tests
    A fake network device and interface are setup. Test implies add and
    removing an IPv6 address which should trigger the related notification.
    
    Change-Id: I6035cfbfaf80c8f4352b5d31b47ca1445434d926
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 08b748432b6609e85c055ac899686e28c90a14d5

--- tests/net/mgmt/src/mgmt.c
+++ tests/net/mgmt/src/mgmt.c
@@ -1,14 +1,22 @@
 DECL|TEST_MGMT_EVENT_UNHANDLED|macro|TEST_MGMT_EVENT_UNHANDLED
 DECL|TEST_MGMT_EVENT|macro|TEST_MGMT_EVENT
 DECL|TEST_MGMT_REQUEST|macro|TEST_MGMT_REQUEST
+DECL|_iface_ip6_add|function|static bool _iface_ip6_add(void)
+DECL|_iface_ip6_del|function|static bool _iface_ip6_del(void)
+DECL|addr6|variable|addr6
 DECL|event2throw|variable|event2throw
+DECL|fake_dev_init|function|int fake_dev_init(struct device *dev)
+DECL|fake_iface_api|variable|fake_iface_api
+DECL|fake_iface_init|function|static void fake_iface_init(struct net_if *iface)
+DECL|fake_iface_send|function|static int fake_iface_send(struct net_if *iface, struct net_buf *buf)
 DECL|initialize_event_tests|function|static void initialize_event_tests(void)
 DECL|main|function|void main(void)
 DECL|receiver_cb|function|static void receiver_cb(struct net_mgmt_event_callback *cb,uint32_t nm_event, struct net_if *iface)
 DECL|rx_calls|variable|rx_calls
 DECL|rx_cb|variable|rx_cb
 DECL|rx_event|variable|rx_event
-DECL|test_mgmt_request|function|int test_mgmt_request(uint32_t mgmt_request, struct net_if *iface, void *data, uint32_t len)
+DECL|test_core_event|function|static int test_core_event(uint32_t event, bool (*func)(void))
+DECL|test_mgmt_request|function|static int test_mgmt_request(uint32_t mgmt_request, struct net_if *iface, void *data, uint32_t len)
 DECL|test_requesting_nm|function|static inline int test_requesting_nm(void)
 DECL|test_sending_event|function|static inline int test_sending_event(uint32_t times, bool receiver)
 DECL|throw_times|variable|throw_times
