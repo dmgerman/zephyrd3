a91c5caa6d11faa690b0da1385ea610010bfcff3
Johan Hedberg <johan.hedberg@intel.com>
2016-09-29 13:27:37 +0300

    net: buf: Allow head deletion with net_buf_frag_del()
    Extend the net_buf_frag_del() API to be usable for deleting the head
    of a fragment chain. This is useful when parsing a stream-based
    protocol encoded into a fragment chain, making code such as the
    following possible:
    
    	...parse data from 'head' buffer...
    
    	/* If current buffer is empty, move to the next one */
    	if (!head->len) {
    		head = net_buf_frag_del(NULL, head);
    	}
    
    Change-Id: I65794bd7fab4e6dadfd2d6b2fa367f9424fd1bde
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 8afb6fc229da871176d4a408dffbf60e642d7398

--- net/buf.c
+++ net/buf.c
@@ -9,7 +9,7 @@ DECL|NET_BUF_INFO|macro|NET_BUF_INFO
 DECL|NET_BUF_WARN|macro|NET_BUF_WARN
 DECL|NET_BUF_WARN|macro|NET_BUF_WARN
 DECL|net_buf_clone|function|struct net_buf *net_buf_clone(struct net_buf *buf)
-DECL|net_buf_frag_del|function|void net_buf_frag_del(struct net_buf *parent, struct net_buf *frag)
+DECL|net_buf_frag_del|function|struct net_buf *net_buf_frag_del(struct net_buf *parent, struct net_buf *frag)
 DECL|net_buf_frag_insert|function|void net_buf_frag_insert(struct net_buf *parent, struct net_buf *frag)
 DECL|net_buf_frag_last|function|struct net_buf *net_buf_frag_last(struct net_buf *buf)
 DECL|net_buf_get_timeout|function|struct net_buf *net_buf_get_timeout(struct nano_fifo *fifo, size_t reserve_head, int32_t timeout)
