35acc24a03ef0babcdfdeb54d0aea165e38ab9d3
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2016-09-30 11:02:37 -0400

    unified/arc: add unified kernel support for ARC arch
    - the interrupt (both regular and fast) now does not do rescheduling
      if the current thread is a coop thread or if the scheduler is not locked
    
    - the _nanokernel.flags cache of _current.flags is not used anymore
      (could be a source of bugs) and is not needed in the scheduling algo
    
    - there is no 'task' field in the _nanokernel anymore: scheduling routines
      call _get_next_ready_thread instead
    
    - the _nanokernel.fiber field is replaced by a more sophisticated
      ready_q, based on the microkernel's priority-bitmap-based one
    
    - thread initialization initializes new fields in the tcs, and does not
      initialize obsolete ones
    
    - nano_private includes nano_internal.h from the unified directory
    
    - The FIBER, TASK and PREEMPTIBLE flags do not exist anymore: the thread
      priority drives the behaviour
    
    - the tcs uses a dlist for queuing in both ready and wait queues instead
      of a custom singly-linked list
    
    - other new fields in the tcs include a schedule-lock count, a
      back-pointer to init data (when the task is static) and a pointer to
      swap data, needed when a thread pending on _Swap() must be passed more
      then just one value (e.g. k_stack_pop() needs an error code and data)
    
    - the 'fiber' and 'task' fields of _nanokernel are replaced with an O(1)
      ready queue (taken from the microkernel)
    
    - fiberRtnValueSet() is aliased to _set_thread_return_value since it
      also operates on preempt threads now
    
    - _set_thread_return_value_with_data() sets the swap_data field in
      addition to a return value from _Swap()
    
    - convenience aliases are created for shorter names:
    
      - _current is defined as _nanokernel.current
      - _ready_q is defined as _nanokernel.ready_q
    
    - _Swap() sets the threads's return code to -EAGAIN before swapping out
      to prevent timeouts to have to set it (solves hard issues in some
      kernel objects).
    
    Change-Id: Ib9690173cbc36c36a9ec67e65590b40d758673de
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: 909bfffda92bbe811177f9d9a052729611f1c913

--- arch/arc/include/nano_private.h
+++ arch/arc/include/nano_private.h
@@ -2,6 +2,15 @@ DECL|ESSENTIAL|macro|ESSENTIAL
 DECL|EXC_ACTIVE|macro|EXC_ACTIVE
 DECL|FIBER|macro|FIBER
 DECL|INT_ACTIVE|macro|INT_ACTIVE
+DECL|K_DEAD|macro|K_DEAD
+DECL|K_DUMMY|macro|K_DUMMY
+DECL|K_EXECUTION_MASK|macro|K_EXECUTION_MASK
+DECL|K_PENDING|macro|K_PENDING
+DECL|K_PRESTART|macro|K_PRESTART
+DECL|K_READY|macro|K_READY
+DECL|K_STATIC|macro|K_STATIC
+DECL|K_SUSPENDED|macro|K_SUSPENDED
+DECL|K_TIMING|macro|K_TIMING
 DECL|NO_METRICS|macro|NO_METRICS
 DECL|PREEMPTIBLE|macro|PREEMPTIBLE
 DECL|STACK_ALIGN_SIZE|macro|STACK_ALIGN_SIZE
@@ -13,12 +22,20 @@ DECL|_CAUSE_COOP|macro|_CAUSE_COOP
 DECL|_CAUSE_FIRQ|macro|_CAUSE_FIRQ
 DECL|_CAUSE_NONE|macro|_CAUSE_NONE
 DECL|_CAUSE_RIRQ|macro|_CAUSE_RIRQ
+DECL|_IDLE_THREAD_PRIO|macro|_IDLE_THREAD_PRIO
 DECL|_INTERRUPT_CAUSE|function|static ALWAYS_INLINE int _INTERRUPT_CAUSE(void)
-DECL|_IS_IN_ISR|function|static ALWAYS_INLINE int _IS_IN_ISR(void)
+DECL|_IS_IN_ISR|macro|_IS_IN_ISR
 DECL|_IntLibInit|function|static inline void _IntLibInit(void)
 DECL|_NANO_PRIVATE_H|macro|_NANO_PRIVATE_H
 DECL|__thread_entry|struct|struct __thread_entry {
+DECL|_current|macro|_current
+DECL|_is_in_isr|function|static ALWAYS_INLINE int _is_in_isr(void)
+DECL|_ready_q|macro|_ready_q
+DECL|_set_thread_return_value_with_data|function|_set_thread_return_value_with_data(struct k_thread *thread, unsigned int value, void *data)
+DECL|_set_thread_return_value|macro|_set_thread_return_value
+DECL|_timeout_q|macro|_timeout_q
 DECL|blink|member|uint32_t blink;
+DECL|cache|member|struct k_thread *cache;
 DECL|callee_saved|struct|struct callee_saved {
 DECL|coopReg|member|struct coop coopReg;
 DECL|coop|struct|struct coop {
@@ -30,14 +47,18 @@ DECL|entry|member|struct __thread_entry *entry; /* thread entry and parameters d
 DECL|errno_var|member|int errno_var;
 DECL|fiberRtnValueSet|function|static ALWAYS_INLINE void fiberRtnValueSet(struct tcs *fiber, unsigned int value)
 DECL|fiber|member|struct tcs *fiber; /* singly linked list of runnable fibers */
-DECL|flags|member|uint32_t flags; /* bitmask of flags above */
+DECL|flags|member|uint32_t flags;
+DECL|flags|member|uint32_t flags;
+DECL|fn_abort|member|void (*fn_abort)(void);
 DECL|fp|member|uint32_t fp; /* r27 */
 DECL|idle|member|int32_t idle; /* Number of ticks for kernel idling */
+DECL|init_data|member|void *init_data;
 DECL|intlock_key|member|uint32_t intlock_key; /* interrupt key when relinquishing control */
 DECL|irq_stack_frame|struct|struct irq_stack_frame {
 DECL|jli_base|member|uint32_t jli_base;
+DECL|k_q_node|member|sys_dnode_t k_q_node;
+DECL|k_q_node|member|sys_dnode_t k_q_node; /* node object in any kernel queue */
 DECL|ldi_base|member|uint32_t ldi_base;
-DECL|link|member|struct tcs *link; /* node in singly-linked list
 DECL|lp_count|member|uint32_t lp_count;
 DECL|lp_end|member|uint32_t lp_end;
 DECL|lp_start|member|uint32_t lp_start;
@@ -51,7 +72,10 @@ DECL|parameter3|member|void *parameter3;
 DECL|pc|member|uint32_t pc;
 DECL|preempReg|member|struct preempt preempReg;
 DECL|preempt|struct|struct preempt {
-DECL|prio|member|int prio; /* fiber priority, -1 for a task */
+DECL|prio_bmap|member|uint32_t prio_bmap[1];
+DECL|prio|member|int prio;
+DECL|prio|member|int prio;
+DECL|q|member|sys_dlist_t q[K_NUM_PRIORITIES];
 DECL|r0|member|uint32_t r0;
 DECL|r10|member|uint32_t r10;
 DECL|r11|member|uint32_t r11;
@@ -81,20 +105,28 @@ DECL|r6|member|uint32_t r6;
 DECL|r7|member|uint32_t r7;
 DECL|r8|member|uint32_t r8;
 DECL|r9|member|uint32_t r9;
+DECL|ready_q|member|struct ready_q ready_q;
+DECL|ready_q|struct|struct ready_q {
 DECL|relinquish_cause|member|int relinquish_cause; /* one of the _CAUSE_xxxx definitions above */
 DECL|return_value|member|unsigned int return_value;/* return value from _Swap */
 DECL|rirq_sp|member|char *rirq_sp; /* regular IRQ stack pointer base */
 DECL|s_NANO|struct|struct s_NANO {
+DECL|sched_locked|member|atomic_t sched_locked;
 DECL|sp|member|uint32_t sp; /* r28 */
 DECL|stack_top|member|uint32_t stack_top;
 DECL|status32|member|uint32_t status32;
+DECL|swap_data|member|void *swap_data;
+DECL|swap_data|member|void *swap_data;
 DECL|tCalleeSaved|typedef|typedef struct callee_saved tCalleeSaved;
 DECL|tISF|typedef|typedef struct irq_stack_frame tISF;
 DECL|tNANO|typedef|typedef struct s_NANO tNANO;
 DECL|tPreempt|typedef|typedef struct preempt tPreempt;
 DECL|task_timeout|member|int32_t task_timeout;
 DECL|task|member|struct tcs *task; /* current task the nanokernel knows about */
+DECL|tcs_base|struct|struct tcs_base {
 DECL|tcs|struct|struct tcs {
 DECL|threads|member|struct tcs *threads; /* singly linked list of ALL fiber+tasks */
 DECL|timeout_q|member|sys_dlist_t timeout_q;
+DECL|timeout|member|struct _timeout timeout;
+DECL|timeout|member|struct _timeout timeout;
 DECL|uk_task_ptr|member|void *uk_task_ptr;
