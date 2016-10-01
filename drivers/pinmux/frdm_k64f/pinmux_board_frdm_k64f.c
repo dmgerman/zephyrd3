476761097e4ed46be764f1da5c848db382c484ca
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-01 10:02:11 +0100

    k64f: Refactor pinmux configuration table.
    Statically compute the number of elements in the pinmux table rather
    than keep a manual entry in step with the table.
    
    Change-Id: I99634aa460a39bb8a9a10fe37d28aaae792aa641
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 9d67337c2d57978b336df7b9685e3eab41325d85

--- drivers/pinmux/frdm_k64f/pinmux_board_frdm_k64f.c
+++ drivers/pinmux/frdm_k64f/pinmux_board_frdm_k64f.c
@@ -1,4 +1,2 @@
-DECL|NUM_DFLT_PINS_SET|macro|NUM_DFLT_PINS_SET
-DECL|NUM_DFLT_PINS_SET|macro|NUM_DFLT_PINS_SET
 DECL|fsl_frdm_k64f_pin_init|function|int fsl_frdm_k64f_pin_init(struct device *arg)
 DECL|mux_config|variable|mux_config
