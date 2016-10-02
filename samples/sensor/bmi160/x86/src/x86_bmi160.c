f5e7831f7d439f15ebd583b09082458da87f3771
Anas Nashif <anas.nashif@intel.com>
2016-10-02 10:23:26 -0400

    gpio: reduce Kconfigs and use consistent name for GPIOs
    Using AON for GPIO kconfigs is very specifc to quark se, there
    is no need to make this special for this platform. Use the
    existing scheme instead.
    
    Change-Id: I946431490380dc0f537d6056277a94c9c9c80fed
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 84c9db39e772db5914d0bbf6cbf21e9733236914

--- samples/sensor/bmi160/x86/src/x86_bmi160.c
+++ samples/sensor/bmi160/x86/src/x86_bmi160.c
@@ -1,6 +1,6 @@
 DECL|BMI160_INTERRUPT_PIN|macro|BMI160_INTERRUPT_PIN
 DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|aon_gpio_callback|function|static void aon_gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins)
 DECL|cb|variable|cb
+DECL|gpio_callback|function|static void gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins)
 DECL|ipm|variable|ipm
 DECL|main|function|void main(void)
