4142a0da044aeb21db7e745a0fc0561c5783a91e
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-10-04 15:36:40 -0700

    samples/mbedtls_dtlsclient: mbedTLS sample DTLS client app.
    This is a sample app using ECJPAKE crypto algorithm on DTLS, using
    mbedTLS, and native IP stack
    
    Jira: ZEP-900
    Jira: ZEP-943
    
    Change-Id: Ica17b047aab11b989d3e8c8f6ac1b79e3041053a
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 05aded096e47deb043d0d5c3443943422511ef1c

---
+++ samples/net/mbedtls_dtlsclient/src/udp.c
@@ -0,0 +1,6 @@
+DECL|INET_FAMILY|macro|INET_FAMILY
+DECL|udp_init|function|int udp_init(struct udp_context *ctx)
+DECL|udp_rx|function|int udp_rx(void *context, unsigned char *buf, size_t size)
+DECL|udp_tx|function|int udp_tx(void *context, const unsigned char *buf, size_t size)
+DECL|uip_hostaddr|variable|uip_hostaddr
+DECL|uip_netmask|variable|uip_netmask
