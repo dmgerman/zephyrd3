435bcfb1d95f55cafd4ac3b50ea79b5838481314
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-10-04 11:25:51 +0300

    net: Add more inline helper functions in 6lowpan
    6lowpan IPHC compression and uncompression functions are very big and
    bit difficult to follow. Added more inline helper routines. No changes
    in functionality.
    
    Change-Id: I310e636e2a23fe5f19a254c158b65c6f4d6253b0
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 2bb8eed501f3f622bba942a69ef04c941ef850ac

--- net/yaip/6lo.c
+++ net/yaip/6lo.c
@@ -3,7 +3,13 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|__packed|variable|__packed
 DECL|cid|member|uint8_t cid : 4;
 DECL|compress_IPHC_header|function|static inline bool compress_IPHC_header(struct net_buf *buf,fragment_handler_t fragment)
+DECL|compress_da|function|static inline uint8_t compress_da(struct net_ipv6_hdr *ipv6, struct net_buf *buf, struct net_buf *frag, uint8_t offset)
+DECL|compress_hoplimit|function|static inline uint8_t compress_hoplimit(struct net_ipv6_hdr *ipv6,struct net_buf *frag, uint8_t offset)
 DECL|compress_ipv6_header|function|static inline bool compress_ipv6_header(struct net_buf *buf,fragment_handler_t fragment)
+DECL|compress_nh_udp|function|static inline uint8_t compress_nh_udp(struct net_udp_hdr *udp, struct net_buf *frag, uint8_t offset)
+DECL|compress_nh|function|static inline uint8_t compress_nh(struct net_ipv6_hdr *ipv6, struct net_buf *frag, uint8_t offset)
+DECL|compress_sa|function|static inline uint8_t compress_sa(struct net_ipv6_hdr *ipv6, struct net_buf *buf, struct net_buf *frag, uint8_t offset)
+DECL|compress_tfl|function|static inline uint8_t compress_tfl(struct net_ipv6_hdr *ipv6, struct net_buf *frag, uint8_t offset)
 DECL|compress|member|uint8_t compress : 1;
 DECL|ctx_6co|variable|ctx_6co
 DECL|get_6co_cid|function|static inline uint8_t get_6co_cid(struct net_icmpv6_nd_opt_6co *opt)
@@ -23,5 +29,10 @@ DECL|net_6lo_uncompress|function|bool net_6lo_uncompress(struct net_buf *buf)
 DECL|prefix|member|struct in6_addr prefix;
 DECL|set_6lo_context|function|static inline void set_6lo_context(struct net_if *iface, uint8_t index, struct net_icmpv6_nd_opt_6co *context)
 DECL|uncompress_IPHC_header|function|static inline bool uncompress_IPHC_header(struct net_buf *buf)
+DECL|uncompress_da|function|static inline uint8_t uncompress_da(struct net_buf *buf, struct net_ipv6_hdr *ipv6, uint8_t offset)
+DECL|uncompress_hoplimit|function|static inline uint8_t uncompress_hoplimit(struct net_buf *buf, struct net_ipv6_hdr *ipv6, uint8_t offset)
 DECL|uncompress_ipv6_header|function|static inline bool uncompress_ipv6_header(struct net_buf *buf)
+DECL|uncompress_nh_udp|function|static inline uint8_t uncompress_nh_udp(struct net_buf *buf,struct net_udp_hdr *udp, uint8_t offset)
+DECL|uncompress_sa|function|static inline uint8_t uncompress_sa(struct net_buf *buf, struct net_ipv6_hdr *ipv6, uint8_t offset)
+DECL|uncompress_tfl|function|static inline uint8_t uncompress_tfl(struct net_buf *buf, struct net_ipv6_hdr *ipv6, uint8_t offset)
 DECL|unused|member|uint8_t unused : 2;
