cb6d2ee56871f8f79330adfdada087906dfd1d9b
Allan Stephens <allan.stephens@windriver.com>
2016-10-06 10:19:38 -0500

    unified/test: Fix issues affecting stack canary applications
    The nanokernel version of this application now uses its own source
    file to eliminate its reliance on the build system setting the
    CONFIG_MICROKERNEL and CONFIG_NANOKERNEL options correctly
    (which the unified kernel build system doesn't do).
    
    Change-Id: Ife27f8172b2be33b95136ccdfa29522c8a6fba0b
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 98e1e0674cb40bffb341f621f4670bdc6ed8ba61

--- tests/kernel/test_stackprot/nanokernel/src/stackprot.c
+++ tests/kernel/test_stackprot/nanokernel/src/stackprot.c
@@ -1,6 +1,8 @@
-DECL|AlternateTask|function|void AlternateTask(void)
-DECL|RegressionTask|function|void RegressionTask(void)
+DECL|STACKSIZE|macro|STACKSIZE
 DECL|check_input|function|void check_input(const char *name, const char *input)
 DECL|count|variable|count
+DECL|fiber1|function|void fiber1(void)
+DECL|fiberStack|variable|fiberStack
+DECL|main|function|void main(void)
 DECL|printLoop|function|void printLoop(const char *name)
 DECL|tcRC|variable|tcRC
