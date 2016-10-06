61e00977e8e6cd9c4759658d864f619bc0525799
Peter Mitsis <peter.mitsis@windriver.com>
2016-10-06 11:36:59 -0400

    unified: Tweak msgq API parameters
     - Reorders parameters where necessary
     - Adds alignment parameter to K_MSGQ_DEFINE() for buffer alignment
     - Renames parameters where necessary so they are more intuitive
    
    Change-Id: I0b53105c04109127897bf4790e6908082f82da4e
    Signed-off-by: Peter Mitsis <peter.mitsis@windriver.com>
    
    Former-commit-id: 1da807e7a8bb61a8eea1052075037b402335674d

--- kernel/unified/msg_q.c
+++ kernel/unified/msg_q.c
@@ -1,4 +1,4 @@
 DECL|k_msgq_get|function|int k_msgq_get(struct k_msgq *q, void *data, int32_t timeout)
-DECL|k_msgq_init|function|void k_msgq_init(struct k_msgq *q, uint32_t msg_size, uint32_t max_msgs, char *buffer)
+DECL|k_msgq_init|function|void k_msgq_init(struct k_msgq *q, char *buffer, uint32_t msg_size, uint32_t max_msgs)
 DECL|k_msgq_purge|function|void k_msgq_purge(struct k_msgq *q)
 DECL|k_msgq_put|function|int k_msgq_put(struct k_msgq *q, void *data, int32_t timeout)
