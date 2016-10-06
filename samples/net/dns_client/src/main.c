6242ff65297f41fe46c7dd52d20965a3bd708cfe
Flavio Santes <flavio.santes@intel.com>
2016-10-06 00:27:01 -0500

    iot/dns: Add DNS Client sample app
    This commit adds the DNS Client sample app on top of the new native
    IP stack. This sample app supports IPv4 and IPv6. See README_IPv4 and
    README_IPv6 files.
    
    Origin: Original
    
    Jira: ZEP-793
    Jira: ZEP-855
    Jira: ZEP-975
    
    Change-Id: Id78bd634c44e216fd45b395c91d95b71ef68c976
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 552dfaeaa7ddf99162576a57e63c18feab302363

---
+++ samples/net/dns_client/src/main.c
@@ -0,0 +1,17 @@
+DECL|MAX_ADDRESSES|macro|MAX_ADDRESSES
+DECL|RC_STR|macro|RC_STR
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|addresses|variable|addresses
+DECL|addresses|variable|addresses
+DECL|ctx|variable|ctx
+DECL|domains|variable|domains
+DECL|local_addr|variable|local_addr
+DECL|local_addr|variable|local_addr
+DECL|local_sock|variable|local_sock
+DECL|local_sock|variable|local_sock
+DECL|main|function|void main(void)
+DECL|remote_sock|variable|remote_sock
+DECL|remote_sock|variable|remote_sock
+DECL|run_dns|function|void run_dns(void)
+DECL|stack|variable|stack
+DECL|str|variable|str
