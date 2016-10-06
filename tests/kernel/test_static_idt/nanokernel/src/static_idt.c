330feeab877f3e0e2dcc3573a32db422743898e0
Allan Stephens <allan.stephens@windriver.com>
2016-10-06 10:35:02 -0500

    unified/test: Fix issues affecting static idt application
    The nanokernel version of this application now uses its own source
    files to eliminate its reliance on the build system setting the
    CONFIG_MICROKERNEL and CONFIG_NANOKERNEL options correctly
    (which the unified kernel build system doesn't do).
    
    Change-Id: Ief1d90251df62b54a6814e82cb95730088d40d99
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: b5f9543535ba99f9bf3dd52c7737bc966b1d82b5

--- tests/kernel/test_static_idt/nanokernel/src/static_idt.c
+++ tests/kernel/test_static_idt/nanokernel/src/static_idt.c
@@ -2,9 +2,10 @@ DECL|TEST_SOFT_INT|macro|TEST_SOFT_INT
 DECL|TEST_SPUR_INT|macro|TEST_SPUR_INT
 DECL|excHandlerExecuted|variable|excHandlerExecuted
 DECL|exc_divide_error_handler|function|void exc_divide_error_handler(NANO_ESF *pEsf)
-DECL|idtSpurTask|function|void idtSpurTask(void)
-DECL|idtTestTask|function|void idtTestTask(void)
+DECL|fiberStack|variable|fiberStack
+DECL|idtSpurFiber|function|static void idtSpurFiber(int a1, int a2)
 DECL|intHandlerExecuted|variable|intHandlerExecuted
 DECL|isr_handler|function|void isr_handler(void)
+DECL|main|function|void main(void)
 DECL|nanoIdtStubTest|function|int nanoIdtStubTest(void)
 DECL|spurHandlerAbortedThread|variable|spurHandlerAbortedThread
