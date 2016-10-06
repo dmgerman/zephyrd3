820f62662480d3865f9afc11f21b4352f69538c4
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-10-06 14:47:29 -0400

    unified: remove unused references from nano_private.h
    Leftover from the nanokernel.
    
    Change-Id: I06f43f182ddbf40617b97aea424252e9c46a95e1
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 8ae55deaf92287697723ed9d574474e7ef2cecf1

--- kernel/unified/include/nano_internal.h
+++ kernel/unified/include/nano_internal.h
@@ -1,11 +1,5 @@
 DECL|K_NUM_PRIORITIES|macro|K_NUM_PRIORITIES
-DECL|_IS_MICROKERNEL_TASK|macro|_IS_MICROKERNEL_TASK
 DECL|_NANO_INTERNAL__H_|macro|_NANO_INTERNAL__H_
-DECL|_NANO_TASK_READY|macro|_NANO_TASK_READY
-DECL|_NANO_TIMER_TASK_READY|macro|_NANO_TIMER_TASK_READY
-DECL|_NANO_UNPEND_TASKS|macro|_NANO_UNPEND_TASKS
-DECL|_TASK_NANO_UNPEND_TASKS|macro|_TASK_NANO_UNPEND_TASKS
-DECL|_TASK_PENDQ_INIT|macro|_TASK_PENDQ_INIT
 DECL|_data_copy|function|static inline void _data_copy(void)
 DECL|_thread_entry_t|typedef|typedef void (*_thread_entry_t)(void *, void *, void *);
 DECL|_thread_exit|macro|_thread_exit
