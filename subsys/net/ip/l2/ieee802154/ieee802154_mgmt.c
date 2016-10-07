1dd82fbf99005f88439002a7775bc192248b5669
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-07 10:50:21 +0200

    net: ieee802154: Add grounds for passive scan
    Currently, as only CC2520 device is supported: it will loop over the 16
    channels present in 2.4Ghz spectrum. However, this will need refinement
    as soon as other spectrums will be in use: L2 layer could request
    capabilites from the device in order to know which range to use.
    
    Scan result is done one at a time for now. It's unlikely going to fit
    all production use case, so it's going to change in the future.
    
    Change-Id: I2fafec49eb5cbfca1a4f04107ba3b6511d7d956d
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 88d4f1f5431a9f746c5b8650439bd64ae49f4b95

--- subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
+++ subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
@@ -1,1 +1,4 @@
+DECL|ieee802154_cancel_scan|function|static int ieee802154_cancel_scan(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
+DECL|ieee802154_handle_beacon|function|enum net_verdict ieee802154_handle_beacon(struct net_if *iface, struct ieee802154_mpdu *mpdu)
+DECL|ieee802154_scan|function|static int ieee802154_scan(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|ieee802154_set_ack|function|static int ieee802154_set_ack(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
---
+++ subsys/net/ip/l2/ieee802154/ieee802154_mgmt.h
@@ -0,0 +1,6 @@
+DECL|__IEEE802154_MGMT_H__|macro|__IEEE802154_MGMT_H__
+DECL|ieee802154_handle_beacon|macro|ieee802154_handle_beacon
+DECL|ieee802154_is_scanning|function|static inline bool ieee802154_is_scanning(struct net_if *iface)
+DECL|ieee802154_is_scanning|macro|ieee802154_is_scanning
+DECL|ieee802154_mgmt_init|function|static inline void ieee802154_mgmt_init(struct net_if *iface)
+DECL|ieee802154_mgmt_init|macro|ieee802154_mgmt_init
