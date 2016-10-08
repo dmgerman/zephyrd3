fc065722b79baf38ec9849624e7006e1d817b654
Anas Nashif <anas.nashif@intel.com>
2016-10-08 07:32:44 -0400

    sensors: add TH02 temperature sensor (Grove)
    Support for the Temperature&Humidity Sensor (High-Accuracy &Mini)
    v1.0 sensor, see:
    
    http://wiki.seeed.cc/Grove-TemptureAndHumidity_Sensor-High-Accuracy_AndMini-v1.0/
    
    Change-Id: I439eafc863116f21e3b3385175d1935e60a5e9b1
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 915eac46254e24726a91c2aba15d0bc273dcff09

---
+++ drivers/sensor/th02/th02.c
@@ -0,0 +1,10 @@
+DECL|get_humi|function|uint16_t get_humi(struct device *dev)
+DECL|get_temp|function|uint16_t get_temp(struct device *dev)
+DECL|is_ready|function|int is_ready(struct device *dev)
+DECL|read16|function|uint16_t read16(struct device *dev, uint8_t d)
+DECL|read8|function|uint8_t read8(struct device *dev, uint8_t d)
+DECL|th02_channel_get|function|static int th02_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|th02_driver_api|variable|th02_driver_api
+DECL|th02_driver|variable|th02_driver
+DECL|th02_init|function|int th02_init(struct device *dev)
+DECL|th02_sample_fetch|function|static int th02_sample_fetch(struct device *dev, enum sensor_channel chan)
