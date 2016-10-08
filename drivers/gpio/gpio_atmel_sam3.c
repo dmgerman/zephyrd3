bae6a9cb98dda075a6cf3e8386ba9b29a587e29e
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-08 09:48:30 +0100

    gpio/sam3: Move RW data from driver config to runtime.
    The callback list within the driver config is updated dynamically
    hence the list itself cannot be const.  Create a runtime context
    driver data structure and relocate the callback list to the new
    structure.
    
    Change-Id: Ied20846e6a78e43967239afa7797456a9abd8548
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 2d1678c632b88a05392ef714ffe48bb3451885ff

--- drivers/gpio/gpio_atmel_sam3.c
+++ drivers/gpio/gpio_atmel_sam3.c
@@ -3,8 +3,11 @@ DECL|cb|member|sys_slist_t cb;
 DECL|config_func_t|typedef|typedef void (*config_func_t)(struct device *port);
 DECL|config_func|member|config_func_t config_func;
 DECL|gpio_sam3_a_cfg|variable|gpio_sam3_a_cfg
+DECL|gpio_sam3_a_runtime|variable|gpio_sam3_a_runtime
 DECL|gpio_sam3_b_cfg|variable|gpio_sam3_b_cfg
+DECL|gpio_sam3_b_runtime|variable|gpio_sam3_b_runtime
 DECL|gpio_sam3_c_cfg|variable|gpio_sam3_c_cfg
+DECL|gpio_sam3_c_runtime|variable|gpio_sam3_c_runtime
 DECL|gpio_sam3_config_a|function|void gpio_sam3_config_a(struct device *dev)
 DECL|gpio_sam3_config_b|function|void gpio_sam3_config_b(struct device *dev)
 DECL|gpio_sam3_config_c|function|void gpio_sam3_config_c(struct device *dev)
@@ -12,6 +15,7 @@ DECL|gpio_sam3_config_d|function|void gpio_sam3_config_d(struct device *dev)
 DECL|gpio_sam3_config|function|static int gpio_sam3_config(struct device *dev, int access_op, uint32_t pin, int flags)
 DECL|gpio_sam3_config|struct|struct gpio_sam3_config {
 DECL|gpio_sam3_d_cfg|variable|gpio_sam3_d_cfg
+DECL|gpio_sam3_d_runtime|variable|gpio_sam3_d_runtime
 DECL|gpio_sam3_disable_callback|function|static int gpio_sam3_disable_callback(struct device *dev, int access_op, uint32_t pin)
 DECL|gpio_sam3_drv_api_funcs|variable|gpio_sam3_drv_api_funcs
 DECL|gpio_sam3_enable_callback|function|static int gpio_sam3_enable_callback(struct device *dev, int access_op, uint32_t pin)
@@ -19,5 +23,6 @@ DECL|gpio_sam3_init|function|int gpio_sam3_init(struct device *dev)
 DECL|gpio_sam3_isr|function|static void gpio_sam3_isr(void *arg)
 DECL|gpio_sam3_manage_callback|function|static int gpio_sam3_manage_callback(struct device *dev, struct gpio_callback *callback, bool set)
 DECL|gpio_sam3_read|function|static int gpio_sam3_read(struct device *dev, int access_op, uint32_t pin, uint32_t *value)
+DECL|gpio_sam3_runtime|struct|struct gpio_sam3_runtime {
 DECL|gpio_sam3_write|function|static int gpio_sam3_write(struct device *dev, int access_op, uint32_t pin, uint32_t value)
 DECL|port|member|volatile struct __pio *port;
