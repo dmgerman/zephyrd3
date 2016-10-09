073db2ba2a554e55e13f01868d776723e54cd3cb
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-09 09:41:29 +0100

    pwm: qmsi: Remove RW data from driver config structure.
    Move the RW channel_period object from the driver config structure to
    the driver context.  Remove the now empty driver config structure.
    
    Change-Id: I38df7877f1bfb9bb55bcd61e7f35cebb6e4916bd
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 9f308df473b7ab114e85348fc99d5ce7948cc8bd

--- drivers/pwm/pwm_qmsi.c
+++ drivers/pwm/pwm_qmsi.c
@@ -5,12 +5,10 @@ DECL|MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_LOW_PERIOD_IN_HW_CLOCK_CYCLES
 DECL|MAX_PERIOD_IN_HW_CLOCK_CYCLES|macro|MAX_PERIOD_IN_HW_CLOCK_CYCLES
 DECL|MAX_PERIOD|macro|MAX_PERIOD
 DECL|MIN_PERIOD|macro|MIN_PERIOD
-DECL|PWM_CONTEXT|macro|PWM_CONTEXT
-DECL|PWM_CONTEXT|macro|PWM_CONTEXT
-DECL|PWM_HAS_CONTEXT_DATA|macro|PWM_HAS_CONTEXT_DATA
 DECL|RP_GET|macro|RP_GET
 DECL|RP_GET|macro|RP_GET
 DECL|__set_one_port|function|static int __set_one_port(struct device *dev, qm_pwm_t id, uint32_t pwm,uint32_t on, uint32_t off)
+DECL|channel_period|member|uint32_t channel_period[CONFIG_PWM_QMSI_NUM_PORTS];
 DECL|channels|member|struct pwm_channel_ctx channels[CONFIG_PWM_QMSI_NUM_PORTS];
 DECL|controlreg|member|uint32_t controlreg;
 DECL|device_power_state|member|uint32_t device_power_state;
@@ -18,7 +16,6 @@ DECL|int_pwm_timer_mask|member|uint32_t int_pwm_timer_mask;
 DECL|loadcount1|member|uint32_t loadcount1;
 DECL|loadcount2|member|uint32_t loadcount2;
 DECL|pwm_channel_ctx|struct|struct pwm_channel_ctx {
-DECL|pwm_channel_period|variable|pwm_channel_period
 DECL|pwm_context|variable|pwm_context
 DECL|pwm_critical_region_end|function|static void pwm_critical_region_end(struct device *dev)
 DECL|pwm_critical_region_start|function|static void pwm_critical_region_start(struct device *dev)
