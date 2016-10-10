53a753e69404ef1e6c27a17565e15619cab5ee45
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-10 13:00:58 +0200

    net: ieee802154: Add MAC command frames structures
    These descriptors will be used to parse and create IEEE 8022.15.4 MAC
    Command frames.
    
    Change-Id: Ic9eb17fe50ae1a9e8460ca62a05cea02cfb14f8a
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 34b789e5102dce1fea70af996814bf7d659a13c8

--- net/yaip/l2/ieee802154/ieee802154_frame.h
+++ net/yaip/l2/ieee802154/ieee802154_frame.h
@@ -4,6 +4,11 @@ DECL|IEEE802154_ADDR_MODE_NONE|enumerator|IEEE802154_ADDR_MODE_NONE = 0x0,
 DECL|IEEE802154_ADDR_MODE_RESERVED|macro|IEEE802154_ADDR_MODE_RESERVED
 DECL|IEEE802154_ADDR_MODE_SHORT|enumerator|IEEE802154_ADDR_MODE_SHORT = 0x2,
 DECL|IEEE802154_ADDR_MODE_SIMPLE|enumerator|IEEE802154_ADDR_MODE_SIMPLE = 0x1,
+DECL|IEEE802154_ASF_PAN_ACCESS_DENIED|enumerator|IEEE802154_ASF_PAN_ACCESS_DENIED = 0x02,
+DECL|IEEE802154_ASF_PAN_AT_CAPACITY|enumerator|IEEE802154_ASF_PAN_AT_CAPACITY = 0x01,
+DECL|IEEE802154_ASF_RESERVED_PRIMITIVES|enumerator|IEEE802154_ASF_RESERVED_PRIMITIVES = 0x80,
+DECL|IEEE802154_ASF_RESERVED|enumerator|IEEE802154_ASF_RESERVED = 0x03,
+DECL|IEEE802154_ASF_SUCCESSFUL|enumerator|IEEE802154_ASF_SUCCESSFUL = 0x00,
 DECL|IEEE802154_BEACON_GTS_DIR_SIZE|macro|IEEE802154_BEACON_GTS_DIR_SIZE
 DECL|IEEE802154_BEACON_GTS_IF_MIN_SIZE|macro|IEEE802154_BEACON_GTS_IF_MIN_SIZE
 DECL|IEEE802154_BEACON_GTS_RX|macro|IEEE802154_BEACON_GTS_RX
@@ -15,6 +20,22 @@ DECL|IEEE802154_BEACON_PAS_IF_MIN_SIZE|macro|IEEE802154_BEACON_PAS_IF_MIN_SIZE
 DECL|IEEE802154_BEACON_PAS_SPEC_SIZE|macro|IEEE802154_BEACON_PAS_SPEC_SIZE
 DECL|IEEE802154_BEACON_SF_SIZE|macro|IEEE802154_BEACON_SF_SIZE
 DECL|IEEE802154_BROADCAST_ADDRESS|macro|IEEE802154_BROADCAST_ADDRESS
+DECL|IEEE802154_CFI_ASSOCIATION_REQUEST|enumerator|IEEE802154_CFI_ASSOCIATION_REQUEST = 0x01,
+DECL|IEEE802154_CFI_ASSOCIATION_RESPONSE|enumerator|IEEE802154_CFI_ASSOCIATION_RESPONSE = 0x02,
+DECL|IEEE802154_CFI_BEACON_REQUEST|enumerator|IEEE802154_CFI_BEACON_REQUEST = 0x07,
+DECL|IEEE802154_CFI_COORDINATOR_REALIGNEMENT|enumerator|IEEE802154_CFI_COORDINATOR_REALIGNEMENT = 0x08,
+DECL|IEEE802154_CFI_DATA_REQUEST|enumerator|IEEE802154_CFI_DATA_REQUEST = 0x04,
+DECL|IEEE802154_CFI_DISASSOCIATION_NOTIFICATION|enumerator|IEEE802154_CFI_DISASSOCIATION_NOTIFICATION = 0x03,
+DECL|IEEE802154_CFI_GTS_REQUEST|enumerator|IEEE802154_CFI_GTS_REQUEST = 0x09,
+DECL|IEEE802154_CFI_ORPHAN_NOTIFICATION|enumerator|IEEE802154_CFI_ORPHAN_NOTIFICATION = 0x06,
+DECL|IEEE802154_CFI_PAN_ID_CONLICT_NOTIFICATION|enumerator|IEEE802154_CFI_PAN_ID_CONLICT_NOTIFICATION = 0x05,
+DECL|IEEE802154_CFI_RESERVED|enumerator|IEEE802154_CFI_RESERVED = 0x0a,
+DECL|IEEE802154_CFI_UNKNOWN|enumerator|IEEE802154_CFI_UNKNOWN = 0x00,
+DECL|IEEE802154_DRF_COORDINATOR_WISH|enumerator|IEEE802154_DRF_COORDINATOR_WISH = 0x01,
+DECL|IEEE802154_DRF_DEVICE_WISH|enumerator|IEEE802154_DRF_DEVICE_WISH = 0x02,
+DECL|IEEE802154_DRF_RESERVED_1|enumerator|IEEE802154_DRF_RESERVED_1 = 0x00,
+DECL|IEEE802154_DRF_RESERVED_2|enumerator|IEEE802154_DRF_RESERVED_2 = 0x03,
+DECL|IEEE802154_DRF_RESERVED_PRIMITIVES|enumerator|IEEE802154_DRF_RESERVED_PRIMITIVES = 0x80,
 DECL|IEEE802154_EXT_ADDR_LENGTH|macro|IEEE802154_EXT_ADDR_LENGTH
 DECL|IEEE802154_FRAME_TYPE_ACK|enumerator|IEEE802154_FRAME_TYPE_ACK = 0x2,
 DECL|IEEE802154_FRAME_TYPE_BEACON|enumerator|IEEE802154_FRAME_TYPE_BEACON = 0x0,
@@ -48,17 +69,36 @@ DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
 DECL|addr|member|struct ieee802154_address addr;
 DECL|addr|member|struct ieee802154_address addr;
+DECL|alloc_addr|member|uint8_t alloc_addr : 1;
 DECL|ar|member|uint16_t ar :1;
+DECL|assoc_req|member|struct ieee802154_cmd_assoc_req assoc_req;
+DECL|assoc_res|member|struct ieee802154_cmd_assoc_res assoc_res;
 DECL|association|member|uint16_t association : 1;
 DECL|bc_order|member|uint16_t bc_order : 4;
 DECL|beacon|member|struct ieee802154_beacon *beacon;
 DECL|ble|member|uint16_t ble : 1;
 DECL|cap_slot|member|uint16_t cap_slot : 4;
+DECL|cfi|member|uint8_t cfi;
+DECL|channel_page|member|uint8_t channel_page; /* Optional */
+DECL|channel|member|uint8_t channel;
+DECL|ci|member|} ci;
+DECL|command|member|struct ieee802154_command *command;
 DECL|comp|member|struct ieee802154_address_field_comp comp;
+DECL|coord_realign|member|struct ieee802154_cmd_coord_realign coord_realign;
+DECL|coordinator_short_addr|member|uint16_t coordinator_short_addr;
 DECL|coordinator|member|uint16_t coordinator : 1;
 DECL|desc_count|member|uint8_t desc_count : 3;
+DECL|dev_type|member|uint8_t dev_type : 1;
+DECL|direction|member|uint8_t direction : 1;
+DECL|disassoc_note|member|struct ieee802154_cmd_disassoc_note disassoc_note;
 DECL|dst_addr_mode|member|uint16_t dst_addr_mode :2;
 DECL|dst_addr|member|struct ieee802154_address_field *dst_addr;
 DECL|dst|member|} dst;
@@ -69,7 +109,9 @@ DECL|frame_pending|member|uint16_t frame_pending :1;
 DECL|frame_type|member|uint16_t frame_type :3;
 DECL|frame_version|member|uint16_t frame_version :2;
 DECL|fs|member|struct ieee802154_fcf_seq *fs;
+DECL|gts_request|member|struct ieee802154_gts_request gts_request;
 DECL|gts|member|struct ieee802154_gts_spec gts;
+DECL|gts|member|} gts;
 DECL|ie_list|member|uint16_t ie_list :1;
 DECL|ieee802154_ack_required|function|static inline bool ieee802154_ack_required(struct net_buf *buf)
 DECL|ieee802154_address_field_comp|struct|struct ieee802154_address_field_comp {
@@ -77,12 +119,21 @@ DECL|ieee802154_address_field_plain|struct|struct ieee802154_address_field_plain
 DECL|ieee802154_address_field|struct|struct ieee802154_address_field {
 DECL|ieee802154_addressing_mode|enum|enum ieee802154_addressing_mode {
 DECL|ieee802154_address|struct|struct ieee802154_address {
+DECL|ieee802154_association_status_field|enum|enum ieee802154_association_status_field {
 DECL|ieee802154_beacon_sf|struct|struct ieee802154_beacon_sf {
 DECL|ieee802154_beacon|struct|struct ieee802154_beacon {
+DECL|ieee802154_cfi|enum|enum ieee802154_cfi {
+DECL|ieee802154_cmd_assoc_req|struct|struct ieee802154_cmd_assoc_req {
+DECL|ieee802154_cmd_assoc_res|struct|struct ieee802154_cmd_assoc_res {
+DECL|ieee802154_cmd_coord_realign|struct|struct ieee802154_cmd_coord_realign {
+DECL|ieee802154_cmd_disassoc_note|struct|struct ieee802154_cmd_disassoc_note {
+DECL|ieee802154_command|struct|struct ieee802154_command {
+DECL|ieee802154_disassociation_reason_field|enum|enum ieee802154_disassociation_reason_field {
 DECL|ieee802154_fcf_seq|struct|struct ieee802154_fcf_seq {
 DECL|ieee802154_frame_params|struct|struct ieee802154_frame_params {
 DECL|ieee802154_frame_type|enum|enum ieee802154_frame_type {
 DECL|ieee802154_gts_dir|struct|struct ieee802154_gts_dir {
+DECL|ieee802154_gts_request|struct|struct ieee802154_gts_request {
 DECL|ieee802154_gts_spec|struct|struct ieee802154_gts_spec {
 DECL|ieee802154_gts|struct|struct ieee802154_gts {
 DECL|ieee802154_mfr|struct|struct ieee802154_mfr {
@@ -90,6 +141,7 @@ DECL|ieee802154_mhr|struct|struct ieee802154_mhr {
 DECL|ieee802154_mpdu|struct|struct ieee802154_mpdu {
 DECL|ieee802154_pas_spec|struct|struct ieee802154_pas_spec {
 DECL|ieee802154_version|enum|enum ieee802154_version {
+DECL|length|member|uint8_t length : 4;
 DECL|length|member|uint8_t length : 4;
 DECL|len|member|uint16_t len;
 DECL|mask|member|uint8_t mask : 7;
@@ -101,15 +153,23 @@ DECL|pan_id_comp|member|uint16_t pan_id_comp :1;
 DECL|pan_id|member|uint16_t pan_id;
 DECL|pan_id|member|uint16_t pan_id;
 DECL|pan_id|member|uint16_t pan_id;
+DECL|pan_id|member|uint16_t pan_id;
 DECL|payload|member|void *payload;
 DECL|permit|member|uint8_t permit : 1;
 DECL|plain|member|struct ieee802154_address_field_plain plain;
+DECL|power_src|member|uint8_t power_src : 1;
+DECL|reason|member|uint8_t reason;
+DECL|reserved_1|member|uint8_t reserved_1 : 1;
 DECL|reserved_1|member|uint8_t reserved_1 : 1;
 DECL|reserved_2|member|uint8_t reserved_2 : 1;
+DECL|reserved_2|member|uint8_t reserved_2 : 2;
 DECL|reserved|member|uint16_t reserved : 1;
 DECL|reserved|member|uint16_t reserved :1;
 DECL|reserved|member|uint8_t reserved : 1;
+DECL|reserved|member|uint8_t reserved : 2;
 DECL|reserved|member|uint8_t reserved : 4;
+DECL|rx_on|member|uint8_t rx_on : 1;
+DECL|sec_capability|member|uint8_t sec_capability : 1;
 DECL|security_enabled|member|uint16_t security_enabled :1;
 DECL|seq_num_suppr|member|uint16_t seq_num_suppr :1;
 DECL|sequence|member|uint8_t sequence;
@@ -119,7 +179,11 @@ DECL|short_address|member|uint16_t short_address;
 DECL|short_addr|member|uint16_t short_addr;
 DECL|short_addr|member|uint16_t short_addr;
 DECL|short_addr|member|uint16_t short_addr;
+DECL|short_addr|member|uint16_t short_addr;
+DECL|short_addr|member|uint16_t short_addr;
 DECL|simple_addr|member|uint8_t simple_addr;
 DECL|src_addr_mode|member|uint16_t src_addr_mode :2;
 DECL|src_addr|member|struct ieee802154_address_field *src_addr;
 DECL|starting_slot|member|uint8_t starting_slot : 4;
+DECL|status|member|uint8_t status;
+DECL|type|member|uint8_t type : 1;
