3259108ab4e75282817b8d0acea00fc9288c2452
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-10-11 18:09:34 -0300

    samples/zoap_server: Add preliminary support for validation
    Implement resources so they conform to what the ETSI plugtest suite
    expects.
    
    This allows the zoap-server sample to pass most of the tests of the
    CORE[1] group, only TD_COAP_CORE_09 is not implemented.
    
    Tests involving lossy networks weren't run as well.
    
    The tests were run against the libcoap[2] client.
    
    [1] ETSI CoAP test description
    http://www.etsi.org/plugtests/CoAP/Document/CoAP_TestDescriptions_v015.pdf
    
    [2] libcoap
    https://libcoap.net/
    
    Change-Id: Ifa3ed21a93052e02f47e99d7cb6d9d4b28e576d8
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: fcf98d1534054cfd3eaa0956f0a66277aae1ea7e

--- samples/net/zoap_server/src/zoap-server.c
+++ samples/net/zoap_server/src/zoap-server.c
@@ -4,7 +4,13 @@ DECL|STACKSIZE|macro|STACKSIZE
 DECL|context|variable|context
 DECL|fiberStack|variable|fiberStack
 DECL|main|function|void main(void)
+DECL|piggyback_get|function|static int piggyback_get(struct zoap_resource *resource, struct zoap_packet *request, const uip_ipaddr_t *addr, uint16_t port)
+DECL|query_get|function|static int query_get(struct zoap_resource *resource, struct zoap_packet *request, const uip_ipaddr_t *addr, uint16_t port)
+DECL|query_path|variable|query_path
 DECL|resources|variable|resources
-DECL|test_get|function|static int test_get(struct zoap_resource *resource, struct zoap_packet *request, const uip_ipaddr_t *addr, uint16_t port)
+DECL|segments_path|variable|segments_path
+DECL|test_del|function|static int test_del(struct zoap_resource *resource, struct zoap_packet *request, const uip_ipaddr_t *addr, uint16_t port)
 DECL|test_path|variable|test_path
+DECL|test_post|function|static int test_post(struct zoap_resource *resource, struct zoap_packet *request, const uip_ipaddr_t *addr, uint16_t port)
+DECL|test_put|function|static int test_put(struct zoap_resource *resource, struct zoap_packet *request, const uip_ipaddr_t *addr, uint16_t port)
 DECL|udp_receive|function|static void udp_receive(void)
