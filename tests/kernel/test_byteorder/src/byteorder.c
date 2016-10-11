8dc5e88803710357c2cdd8813aa6c6e55674894e
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-11 11:03:11 +0200

    tests: Add a unit test for the byteorder buffer swap utilities
    sys_memcpy_swap() and sys_mem_swap are tested.
    
    Change-Id: Ib7ee9bd5e58a17cb41960c1834510d6643dc8271
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: fb24857b5cb1a12c4660b94ce995d616b1d3b457

---
+++ tests/kernel/test_byteorder/src/byteorder.c
@@ -0,0 +1,3 @@
+DECL|byteorder_test_mem_swap|function|static void byteorder_test_mem_swap(void)
+DECL|byteorder_test_memcpy_swap|function|static void byteorder_test_memcpy_swap(void)
+DECL|test_main|function|void test_main(void)
