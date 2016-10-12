be5988506896fdf188bc2e3d9611fc2495bff103
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-12 14:30:20 +0200

    net: ieee802154: Optimize context structure usage
    For future purpose, let's not occupy a full byte for a unique boolean.
    
    Change-Id: I36f2824cca4806eb90640a71ed3cea37a353b276
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 7e4bba03b41063f5631442274c3ed21affc1328e

--- include/net/yaip/ieee802154.h
+++ include/net/yaip/ieee802154.h
@@ -1,7 +1,8 @@
 DECL|__IEEE802154_H__|macro|__IEEE802154_H__
 DECL|__packed|variable|__packed
+DECL|_unused|member|uint8_t _unused : 7;
 DECL|ack_lock|member|struct nano_sem ack_lock;
-DECL|ack_received|member|bool ack_received;
+DECL|ack_received|member|uint8_t ack_received : 1;
 DECL|channel|member|uint16_t channel;
 DECL|ieee802154_context|struct|struct ieee802154_context {
 DECL|pan_id|member|uint16_t pan_id;
