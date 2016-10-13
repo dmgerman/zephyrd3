b41fadbbb59de10237e2dd1e19beaa87db20dc96
Lee Jones <lee.jones@linaro.org>
2016-10-13 11:39:09 +0100

    dma: Introduce STM32F4x DMA driver
    This driver currently only handles memory-to-memory transfers.
    
    TODO: Add support for all types of transfers.
    
    Change-Id: Ic9787dcca919a58fb11b48c9f6b6b371db88c3ea
    Origin: Original
    Maintained-by: Zephyr
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    
    Former-commit-id: fe8d1bdedb6af4323b52cf8e48e28b07bae640d0

---
+++ drivers/dma/dma_stm32f4x.c
@@ -0,0 +1,127 @@
+DECL|BITS_PER_LONG|macro|BITS_PER_LONG
+DECL|DMA_STM32_1_BASE|macro|DMA_STM32_1_BASE
+DECL|DMA_STM32_1|macro|DMA_STM32_1
+DECL|DMA_STM32_2_BASE|macro|DMA_STM32_2_BASE
+DECL|DMA_STM32_2|macro|DMA_STM32_2
+DECL|DMA_STM32_DEV_TO_MEM|macro|DMA_STM32_DEV_TO_MEM
+DECL|DMA_STM32_DMEI|macro|DMA_STM32_DMEI
+DECL|DMA_STM32_FEI|macro|DMA_STM32_FEI
+DECL|DMA_STM32_FIFO_THRESHOLD_1QUARTERFULL|macro|DMA_STM32_FIFO_THRESHOLD_1QUARTERFULL
+DECL|DMA_STM32_FIFO_THRESHOLD_3QUARTERSFULL|macro|DMA_STM32_FIFO_THRESHOLD_3QUARTERSFULL
+DECL|DMA_STM32_FIFO_THRESHOLD_FULL|macro|DMA_STM32_FIFO_THRESHOLD_FULL
+DECL|DMA_STM32_FIFO_THRESHOLD_HALFFULL|macro|DMA_STM32_FIFO_THRESHOLD_HALFFULL
+DECL|DMA_STM32_HIFCR|macro|DMA_STM32_HIFCR
+DECL|DMA_STM32_HISR|macro|DMA_STM32_HISR
+DECL|DMA_STM32_HTI|macro|DMA_STM32_HTI
+DECL|DMA_STM32_IRQ_PRI|macro|DMA_STM32_IRQ_PRI
+DECL|DMA_STM32_LIFCR|macro|DMA_STM32_LIFCR
+DECL|DMA_STM32_LISR|macro|DMA_STM32_LISR
+DECL|DMA_STM32_MAX_CHANNELS|macro|DMA_STM32_MAX_CHANNELS
+DECL|DMA_STM32_MAX_DATA_ITEMS|macro|DMA_STM32_MAX_DATA_ITEMS
+DECL|DMA_STM32_MAX_DEVS|macro|DMA_STM32_MAX_DEVS
+DECL|DMA_STM32_MEM_TO_DEV|macro|DMA_STM32_MEM_TO_DEV
+DECL|DMA_STM32_MEM_TO_MEM|macro|DMA_STM32_MEM_TO_MEM
+DECL|DMA_STM32_PRIORITY_HIGH|macro|DMA_STM32_PRIORITY_HIGH
+DECL|DMA_STM32_PRIORITY_LOW|macro|DMA_STM32_PRIORITY_LOW
+DECL|DMA_STM32_PRIORITY_MEDIUM|macro|DMA_STM32_PRIORITY_MEDIUM
+DECL|DMA_STM32_PRIORITY_VERY_HIGH|macro|DMA_STM32_PRIORITY_VERY_HIGH
+DECL|DMA_STM32_SCR_CFG_MASK|macro|DMA_STM32_SCR_CFG_MASK
+DECL|DMA_STM32_SCR_CIRC|macro|DMA_STM32_SCR_CIRC
+DECL|DMA_STM32_SCR_CT|macro|DMA_STM32_SCR_CT
+DECL|DMA_STM32_SCR_DBM|macro|DMA_STM32_SCR_DBM
+DECL|DMA_STM32_SCR_DIR_MASK|macro|DMA_STM32_SCR_DIR_MASK
+DECL|DMA_STM32_SCR_DIR|macro|DMA_STM32_SCR_DIR
+DECL|DMA_STM32_SCR_DMEIE|macro|DMA_STM32_SCR_DMEIE
+DECL|DMA_STM32_SCR_EN|macro|DMA_STM32_SCR_EN
+DECL|DMA_STM32_SCR_HTIE|macro|DMA_STM32_SCR_HTIE
+DECL|DMA_STM32_SCR_IRQ_MASK|macro|DMA_STM32_SCR_IRQ_MASK
+DECL|DMA_STM32_SCR_MBURST_MASK|macro|DMA_STM32_SCR_MBURST_MASK
+DECL|DMA_STM32_SCR_MBURST|macro|DMA_STM32_SCR_MBURST
+DECL|DMA_STM32_SCR_MINC|macro|DMA_STM32_SCR_MINC
+DECL|DMA_STM32_SCR_MSIZE_MASK|macro|DMA_STM32_SCR_MSIZE_MASK
+DECL|DMA_STM32_SCR_MSIZE|macro|DMA_STM32_SCR_MSIZE
+DECL|DMA_STM32_SCR_PBURST_MASK|macro|DMA_STM32_SCR_PBURST_MASK
+DECL|DMA_STM32_SCR_PBURST|macro|DMA_STM32_SCR_PBURST
+DECL|DMA_STM32_SCR_PFCTRL|macro|DMA_STM32_SCR_PFCTRL
+DECL|DMA_STM32_SCR_PINCOS|macro|DMA_STM32_SCR_PINCOS
+DECL|DMA_STM32_SCR_PINC|macro|DMA_STM32_SCR_PINC
+DECL|DMA_STM32_SCR_PL_MASK|macro|DMA_STM32_SCR_PL_MASK
+DECL|DMA_STM32_SCR_PL|macro|DMA_STM32_SCR_PL
+DECL|DMA_STM32_SCR_PSIZE_GET|macro|DMA_STM32_SCR_PSIZE_GET
+DECL|DMA_STM32_SCR_PSIZE_MASK|macro|DMA_STM32_SCR_PSIZE_MASK
+DECL|DMA_STM32_SCR_PSIZE|macro|DMA_STM32_SCR_PSIZE
+DECL|DMA_STM32_SCR_REQ|macro|DMA_STM32_SCR_REQ
+DECL|DMA_STM32_SCR_TCIE|macro|DMA_STM32_SCR_TCIE
+DECL|DMA_STM32_SCR_TEIE|macro|DMA_STM32_SCR_TEIE
+DECL|DMA_STM32_SCR|macro|DMA_STM32_SCR
+DECL|DMA_STM32_SFCR_DMDIS|macro|DMA_STM32_SFCR_DMDIS
+DECL|DMA_STM32_SFCR_FEIE|macro|DMA_STM32_SFCR_FEIE
+DECL|DMA_STM32_SFCR_FTH_MASK|macro|DMA_STM32_SFCR_FTH_MASK
+DECL|DMA_STM32_SFCR_FTH|macro|DMA_STM32_SFCR_FTH
+DECL|DMA_STM32_SFCR_MASK|macro|DMA_STM32_SFCR_MASK
+DECL|DMA_STM32_SFCR_STAT_MASK|macro|DMA_STM32_SFCR_STAT_MASK
+DECL|DMA_STM32_SFCR|macro|DMA_STM32_SFCR
+DECL|DMA_STM32_SM0AR|macro|DMA_STM32_SM0AR
+DECL|DMA_STM32_SM1AR|macro|DMA_STM32_SM1AR
+DECL|DMA_STM32_SNDTR|macro|DMA_STM32_SNDTR
+DECL|DMA_STM32_SPAR|macro|DMA_STM32_SPAR
+DECL|DMA_STM32_TCI|macro|DMA_STM32_TCI
+DECL|DMA_STM32_TEI|macro|DMA_STM32_TEI
+DECL|GENMASK|macro|GENMASK
+DECL|RESERVED_1|macro|RESERVED_1
+DECL|RESERVED_6|macro|RESERVED_6
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|SYS_LOG_U32|macro|SYS_LOG_U32
+DECL|base|member|uint32_t base;
+DECL|busy|member|bool busy;
+DECL|callback_data|member|void *callback_data;
+DECL|chan|member|struct dma_stm32_chan chan[DMA_STM32_MAX_CHANNELS];
+DECL|clk|member|struct device *clk;
+DECL|config|member|void (*config)(struct dma_stm32_device *);
+DECL|ddata|variable|ddata
+DECL|dev|member|struct device *dev;
+DECL|direction|member|uint32_t direction;
+DECL|dma_error|member|void (*dma_error)(struct device *dev, void *data);
+DECL|dma_funcs|variable|dma_funcs
+DECL|dma_stm32_1_cdata|variable|dma_stm32_1_cdata
+DECL|dma_stm32_1_config|function|static void dma_stm32_1_config(struct dma_stm32_device *ddata)
+DECL|dma_stm32_2_cdata|variable|dma_stm32_2_cdata
+DECL|dma_stm32_2_config|function|static void dma_stm32_2_config(struct dma_stm32_device *ddata)
+DECL|dma_stm32_chan_reg|struct|struct dma_stm32_chan_reg {
+DECL|dma_stm32_channel_config|function|static int dma_stm32_channel_config(struct device *dev, uint32_t channel, struct dma_channel_config *config)
+DECL|dma_stm32_chan|struct|struct dma_stm32_chan {
+DECL|dma_stm32_config_memcpy|function|static int dma_stm32_config_memcpy(struct device *dev, uint32_t channel, struct dma_channel_config *config)
+DECL|dma_stm32_config|struct|struct dma_stm32_config {
+DECL|dma_stm32_device|struct|static struct dma_stm32_device {
+DECL|dma_stm32_disable_chan|function|static int dma_stm32_disable_chan(struct dma_stm32_device *ddata, uint32_t channel)
+DECL|dma_stm32_init|function|static int dma_stm32_init(struct device *dev)
+DECL|dma_stm32_irq_0|function|static void dma_stm32_irq_0(void *arg) { dma_stm32_irq_handler(arg, 0); }
+DECL|dma_stm32_irq_1|function|static void dma_stm32_irq_1(void *arg) { dma_stm32_irq_handler(arg, 1); }
+DECL|dma_stm32_irq_2|function|static void dma_stm32_irq_2(void *arg) { dma_stm32_irq_handler(arg, 2); }
+DECL|dma_stm32_irq_3|function|static void dma_stm32_irq_3(void *arg) { dma_stm32_irq_handler(arg, 3); }
+DECL|dma_stm32_irq_4|function|static void dma_stm32_irq_4(void *arg) { dma_stm32_irq_handler(arg, 4); }
+DECL|dma_stm32_irq_5|function|static void dma_stm32_irq_5(void *arg) { dma_stm32_irq_handler(arg, 5); }
+DECL|dma_stm32_irq_6|function|static void dma_stm32_irq_6(void *arg) { dma_stm32_irq_handler(arg, 6); }
+DECL|dma_stm32_irq_7|function|static void dma_stm32_irq_7(void *arg) { dma_stm32_irq_handler(arg, 7); }
+DECL|dma_stm32_irq_clear|function|static void dma_stm32_irq_clear(struct dma_stm32_device *ddata,uint32_t channel, uint32_t irqs)
+DECL|dma_stm32_irq_handler|function|static void dma_stm32_irq_handler(void *arg, uint32_t channel)
+DECL|dma_stm32_irq_status|function|static uint32_t dma_stm32_irq_status(struct dma_stm32_device *ddata, uint32_t channel)
+DECL|dma_stm32_read|function|static uint32_t dma_stm32_read(struct dma_stm32_device *ddata, uint32_t reg)
+DECL|dma_stm32_transfer_config|function|static int dma_stm32_transfer_config(struct device *dev, uint32_t channel, struct dma_transfer_config *config)
+DECL|dma_stm32_transfer_start|function|static int dma_stm32_transfer_start(struct device *dev, uint32_t channel)
+DECL|dma_stm32_write|function|static void dma_stm32_write(struct dma_stm32_device *ddata, uint32_t reg, uint32_t val)
+DECL|dma_transfer|member|void (*dma_transfer)(struct device *dev, void *data);
+DECL|hifcr|member|uint32_t hifcr;
+DECL|hisr|member|uint32_t hisr;
+DECL|id|member|uint32_t id;
+DECL|lifcr|member|uint32_t lifcr;
+DECL|lisr|member|uint32_t lisr;
+DECL|mem2mem|member|bool mem2mem;
+DECL|pclken|member|struct stm32f4x_pclken pclken;
+DECL|regs|member|struct dma_stm32_chan_reg regs;
+DECL|scr|member|uint32_t scr;
+DECL|sfcr|member|uint32_t sfcr;
+DECL|sm0ar|member|uint32_t sm0ar;
+DECL|sm1ar|member|uint32_t sm1ar;
+DECL|sndtr|member|uint32_t sndtr;
+DECL|spar|member|uint32_t spar;
