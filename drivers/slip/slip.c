e1269ea8a3b042824b60bbdec024f4903bb392d0
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2016-10-18 12:09:28 +0300

    net: Fix slip compilation issue with debug option enabled
    Slip context parameter is not used in hexdump and in few places
    it is not defined. So CONFIG_SLIP_DEBUG=y throws errors.
    
    Change-Id: I73ca26b7d5cd7c3cd532462b5300e25bbdac9511
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: c90e0ae58c9d7593aa32958c95cf52ac9dac5cbb

--- drivers/slip/slip.c
+++ drivers/slip/slip.c
@@ -21,7 +21,7 @@ DECL|_SLIP_MTU|macro|_SLIP_MTU
 DECL|_SLIP_MTU|macro|_SLIP_MTU
 DECL|buf|member|uint8_t buf[1]; /* SLIP data is read into this buf */
 DECL|garbage|member|uint16_t garbage;
-DECL|hexdump|function|static void hexdump(struct slip_context *slip, const char *str, const uint8_t *packet, size_t length, size_t ll_reserve)
+DECL|hexdump|function|static void hexdump(const char *str, const uint8_t *packet, size_t length, size_t ll_reserve)
 DECL|hexdump|macro|hexdump
 DECL|init_done|member|bool init_done;
 DECL|ip_drop|member|uint16_t ip_drop;
