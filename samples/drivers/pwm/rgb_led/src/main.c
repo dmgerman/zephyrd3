ecca0ea9de7bede3d8ed83e86d221adbba255ee3
Baohong Liu <baohong.liu@intel.com>
2016-10-18 15:35:20 -0700

    samples: pwm: use new API and add rgb led app
    Add a new pwm app (rgb led) using the new API.
    
    Following the recent pwm RFC, all existing APIs will
    be deprecated. Two new APIs will be added. The changes
    for the new APIs and the required driver function have
    been posted.
    
    To demonstrate the usage of the new PWM APIs, this app
    to drive a rgb led is added. It will use the new API
    pwm_pin_set_usec().
    
    This app will be located in a new sub directory.
    
    Jira: ZEP-745 ZEP-1029
    
    Origin: Original
    
    Change-Id: If8e5d8ab22dcf8b7cf3665e38949715c61ca9c03
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 98246089357f1a5acda543372d7f4e0d9b5228ce

---
+++ samples/drivers/pwm/rgb_led/src/main.c
@@ -0,0 +1,4 @@
+DECL|PERIOD|macro|PERIOD
+DECL|STEPSIZE|macro|STEPSIZE
+DECL|main|function|void main(void)
+DECL|write_pin|function|static int write_pin(struct device *pwm_dev, uint32_t pwm_pin, uint32_t pulse_width)
