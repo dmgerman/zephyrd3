eb2fa819b73e9c458f5ac8ffc339950cdfb321cc
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-20 09:44:42 +0200

    net: ieee802154: Add PAN disassociation request
    If associated, this will request to disassociate from the PAN. That's
    only client side, PAN coordinator can also disassociate a client, which
    case is not handled here.
    
    Change-Id: If308f51b62c5006b4a2db53d0c891e8192f3c198
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 820713239a37b2c1414773a4102cec416923185f

--- subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
+++ subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
@@ -2,6 +2,7 @@ DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|ieee802154_associate|function|static int ieee802154_associate(uint32_t mgmt_request, struct net_if *iface,void *data, size_t len)
 DECL|ieee802154_cancel_scan|function|static int ieee802154_cancel_scan(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
+DECL|ieee802154_disassociate|function|static int ieee802154_disassociate(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|ieee802154_handle_beacon|function|enum net_verdict ieee802154_handle_beacon(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_handle_mac_command|function|enum net_verdict ieee802154_handle_mac_command(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_scan|function|static int ieee802154_scan(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
