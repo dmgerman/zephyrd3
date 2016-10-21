63905ee6a41e74646a567e11b6ce676870a54566
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-10-21 20:14:21 +0530

    Bluetooth: RFCOMM: Initiate session connection
    This patch introduces the Connect API which initiates session
    connection first. If session is already there with the peer
    then it has to reuse it and initiate DLC (which will be done
    in the subsequent patch) since there can be only one session
    per device.
    
    < ACL Data TX: Handle 256 flags 0x00 dlen 8
          Channel: 64 len 4 [PSM 3 mode 0] {chan 0}
          RFCOMM: Set Async Balance Mode (SABM) (0x2f)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0x3f poll/final 1
             Length: 0
             FCS: 0x1c
    
    > ACL Data RX: Handle 256 flags 0x02 dlen 8
          Channel: 64 len 4 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Ack (UA) (0x63)
             Address: 0x03 cr 1 dlci 0x00
             Control: 0x73 poll/final 1
             Length: 0
             FCS: 0xd7
    
    Change-Id: I9828e0f3b3ea43bb17df95f0536e15df86f1b4be
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: 00457636f8a3742028cf3c26ea0b655b06cfa13a

--- subsys/bluetooth/host/rfcomm.c
+++ subsys/bluetooth/host/rfcomm.c
@@ -12,6 +12,7 @@ DECL|RFCOMM_SECURITY_PENDING|enumerator|RFCOMM_SECURITY_PENDING
 DECL|RFCOMM_SECURITY_REJECT|enumerator|RFCOMM_SECURITY_REJECT,
 DECL|RFCOMM_SESSION|macro|RFCOMM_SESSION
 DECL|bt_rfcomm_create_pdu|function|struct net_buf *bt_rfcomm_create_pdu(struct nano_fifo *fifo)
+DECL|bt_rfcomm_dlc_connect|function|int bt_rfcomm_dlc_connect(struct bt_conn *conn, struct bt_rfcomm_dlc *dlc, uint8_t channel)
 DECL|bt_rfcomm_dlc_send|function|int bt_rfcomm_dlc_send(struct bt_rfcomm_dlc *dlc, struct net_buf *buf)
 DECL|bt_rfcomm_init|function|void bt_rfcomm_init(void)
 DECL|bt_rfcomm_pool|variable|bt_rfcomm_pool
@@ -42,16 +43,19 @@ DECL|rfcomm_handle_msc|function|static void rfcomm_handle_msc(struct bt_rfcomm_s
 DECL|rfcomm_handle_msg|function|static void rfcomm_handle_msg(struct bt_rfcomm_session *session, struct net_buf *buf)
 DECL|rfcomm_handle_pn|function|static void rfcomm_handle_pn(struct bt_rfcomm_session *session, struct net_buf *buf, uint8_t cr)
 DECL|rfcomm_handle_sabm|function|static void rfcomm_handle_sabm(struct bt_rfcomm_session *session, uint8_t dlci)
+DECL|rfcomm_handle_ua|function|static void rfcomm_handle_ua(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_make_uih_msg|function|static struct net_buf *rfcomm_make_uih_msg(struct bt_rfcomm_dlc *dlc, uint8_t cr, uint8_t type, uint8_t len)
 DECL|rfcomm_recv|function|static void rfcomm_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|rfcomm_send_credit|function|static int rfcomm_send_credit(struct bt_rfcomm_dlc *dlc, uint8_t credits)
 DECL|rfcomm_send_dm|function|static int rfcomm_send_dm(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_send_msc|function|static int rfcomm_send_msc(struct bt_rfcomm_dlc *dlc, uint8_t cr)
 DECL|rfcomm_send_pn|function|static int rfcomm_send_pn(struct bt_rfcomm_dlc *dlc, uint8_t cr)
+DECL|rfcomm_send_sabm|function|static int rfcomm_send_sabm(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_send_ua|function|static int rfcomm_send_ua(struct bt_rfcomm_session *session, uint8_t dlci)
 DECL|rfcomm_server_lookup_channel|function|static struct bt_rfcomm_server *rfcomm_server_lookup_channel(uint8_t channel)
 DECL|rfcomm_session_disconnected|function|static void rfcomm_session_disconnected(struct bt_rfcomm_session *session)
 DECL|rfcomm_session_new|function|static struct bt_rfcomm_session *rfcomm_session_new(bt_rfcomm_role_t role)
+DECL|rfcomm_sessions_lookup_bt_conn|function|rfcomm_sessions_lookup_bt_conn(struct bt_conn *conn)
 DECL|rfcomm_session|variable|rfcomm_session
 DECL|security_result|enum|enum security_result {
 DECL|servers|variable|servers
