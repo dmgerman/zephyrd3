58d45410563fba1765988ee9d4c1dfe846c7aeb6
Flavio Santes <flavio.santes@intel.com>
2016-10-23 14:19:56 -0500

    test/context: Fix style issues
    Fix some style issues found at the 'test_context' source code.
    Issues detected:
    
    - Naming convention
    - Lines over 80 characters
    
    Change-Id: I2f121fce7b00aaf805da44a9aa33cd5aa8d3d6ac
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 73d492988613cda255f13b918b68c8ea2ff4ddee

--- tests/kernel/test_context/src/context.c
+++ tests/kernel/test_context/src/context.c
@@ -11,47 +11,46 @@ DECL|TICK_IRQ|macro|TICK_IRQ
 DECL|TICK_IRQ|macro|TICK_IRQ
 DECL|TICK_IRQ|macro|TICK_IRQ
 DECL|UNKNOWN_COMMAND|macro|UNKNOWN_COMMAND
-DECL|_trigger_isrHandler|function|static void _trigger_isrHandler(void)
 DECL|command|member|int command; /* command to process */
 DECL|data|member|void *data; /* pointer to data to use or return */
-DECL|delayed_fiber|function|void delayed_fiber(int num, int unused)
-DECL|disable_interrupt_func|typedef|typedef int (* disable_interrupt_func)(int);
-DECL|enable_interrupt_func|typedef|typedef void (* enable_interrupt_func)(int);
+DECL|delayed_fiber|function|static void delayed_fiber(int num, int unused)
+DECL|disable_int_func|typedef|typedef int (*disable_int_func)(int);
+DECL|enable_int_func|typedef|typedef void (*enable_int_func)(int);
 DECL|error|member|int error; /* error value (if any) */
-DECL|fiberDetectedError|variable|fiberDetectedError
-DECL|fiberEntry|function|static void fiberEntry(int task_thread_id, int arg1)
-DECL|fiberEvidence|variable|fiberEvidence
-DECL|fiberHelper|function|static void fiberHelper(int arg1, int arg2)
-DECL|fiberStack1|variable|fiberStack1
-DECL|fiberStack2|variable|fiberStack2
+DECL|fiber_detected_error|variable|fiber_detected_error
+DECL|fiber_entry|function|static void fiber_entry(int task_thread_id, int arg1)
+DECL|fiber_evidence|variable|fiber_evidence
+DECL|fiber_helper|function|static void fiber_helper(int arg1, int arg2)
 DECL|fiber_priority_set|macro|fiber_priority_set
 DECL|fiber_priority_set|macro|fiber_priority_set
-DECL|fiber_yieldTest|function|int fiber_yieldTest(void)
-DECL|initNanoObjects|function|int initNanoObjects(void)
-DECL|irq_disableWrapper|function|int irq_disableWrapper(int irq)
-DECL|irq_enableWrapper|function|void irq_enableWrapper(int irq)
-DECL|irq_lockWrapper|function|int irq_lockWrapper(int unused)
-DECL|irq_unlockWrapper|function|void irq_unlockWrapper(int imask)
-DECL|isrInfo|variable|isrInfo
-DECL|isr_handler|function|void isr_handler(void *data)
+DECL|fiber_stack1|variable|fiber_stack1
+DECL|fiber_stack2|variable|fiber_stack2
+DECL|irq_disable_wrapper|function|int irq_disable_wrapper(int irq)
+DECL|irq_enable_wrapper|function|void irq_enable_wrapper(int irq)
+DECL|irq_lock_wrapper|function|int irq_lock_wrapper(int unused)
+DECL|irq_unlock_wrapper|function|void irq_unlock_wrapper(int imask)
+DECL|isr_handler_trigger|function|static void isr_handler_trigger(void)
+DECL|isr_handler|function|static void isr_handler(void *data)
+DECL|isr_info|variable|isr_info
 DECL|link_in_fifo|member|void *link_in_fifo;
 DECL|main|function|void main(void)
-DECL|nanoCpuDisableInterruptsTest|function|int nanoCpuDisableInterruptsTest(disable_interrupt_func disableRtn, enable_interrupt_func enableRtn, int irq)
-DECL|nanoCtxFiberTest|function|int nanoCtxFiberTest(nano_thread_id_t task_thread_id)
-DECL|nanoCtxTaskTest|function|int nanoCtxTaskTest(void)
-DECL|nano_cpu_idleTest|function|int nano_cpu_idleTest(void)
+DECL|nano_init_objects|function|static int nano_init_objects(void)
 DECL|q_order|member|int q_order;
 DECL|reply_timeout|variable|reply_timeout
-DECL|test_fiber_busy_wait|function|static void test_fiber_busy_wait(int ticks, int unused)
-DECL|test_fiber_sleep|function|static void test_fiber_sleep(int timeout, int arg2)
+DECL|sem_fiber|variable|sem_fiber
+DECL|test_busy_wait|function|static void test_busy_wait(int ticks, int unused)
+DECL|test_fiber_sleep|function|static void test_fiber_sleep(int timeout, int unused)
+DECL|test_fiber_yield|function|static int test_fiber_yield(void)
+DECL|test_nano_cpu_idle|function|static int test_nano_cpu_idle(void)
+DECL|test_nano_ctx_task|function|static int test_nano_ctx_task(void)
+DECL|test_nano_fiber|function|static int test_nano_fiber(nano_thread_id_t task_thread_id)
+DECL|test_nano_interrupts|function|static int test_nano_interrupts(disable_int_func disable_int,enable_int_func enable_int, int irq)
 DECL|test_timeout|function|static int test_timeout(void)
-DECL|timeout_order_data|struct|struct timeout_order_data {
-DECL|timeout_order_data|variable|timeout_order_data
 DECL|timeout_order_fifo|variable|timeout_order_fifo
 DECL|timeout_order|member|int timeout_order;
+DECL|timeout_order|struct|struct timeout_order {
 DECL|timeout_stacks|variable|timeout_stacks
+DECL|timeouts|variable|timeouts
 DECL|timeout|member|int32_t timeout;
-DECL|timerData|variable|timerData
 DECL|timer|variable|timer
 DECL|value|member|int value; /* value to be passed or returned */
-DECL|wakeFiber|variable|wakeFiber
