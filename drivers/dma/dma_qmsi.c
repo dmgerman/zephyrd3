df212c006a19f576bc1abe8d1b85419851725632
Iván Briano <ivan.briano@intel.com>
2016-10-24 11:06:03 -0200

    dma qmsi: Add support for device power management
    Add the necessary infrastructure to support power management for the
    QMSI DMA driver. If deep sleep is supported, this driver supports
    saving its context to be restored on resume.
    
    Jira: ZEP-1005
    Change-Id: I49f1f985eb0f250c777c6950178715fb794db537
    Signed-off-by: Iván Briano <ivan.briano@intel.com>
    
    Former-commit-id: d53811ec732c2d461b694626ce0c1cb45c680d03

--- drivers/dma/dma_qmsi.c
+++ drivers/dma/dma_qmsi.c
@@ -1,4 +1,5 @@
 DECL|callback_data|member|void *callback_data[QM_DMA_CHANNEL_NUM];
+DECL|device_power_state|member|uint32_t device_power_state;
 DECL|dev|member|struct device *dev;
 DECL|dma_callback|function|static void dma_callback(void *callback_context, uint32_t len, int error_code)
 DECL|dma_context|variable|dma_context
@@ -9,12 +10,19 @@ DECL|dma_qmsi_config_info|struct|struct dma_qmsi_config_info {
 DECL|dma_qmsi_config|function|static void dma_qmsi_config(struct device *dev)
 DECL|dma_qmsi_context|struct|struct dma_qmsi_context {
 DECL|dma_qmsi_dev_data|variable|dma_qmsi_dev_data
+DECL|dma_qmsi_device_ctrl|function|static int dma_qmsi_device_ctrl(struct device *dev, uint32_t ctrl_command,void *context)
 DECL|dma_qmsi_driver_data|struct|struct dma_qmsi_driver_data {
+DECL|dma_qmsi_get_power_state|function|static uint32_t dma_qmsi_get_power_state(struct device *dev)
 DECL|dma_qmsi_init|function|int dma_qmsi_init(struct device *dev)
+DECL|dma_qmsi_set_power_state|function|static void dma_qmsi_set_power_state(struct device *dev, uint32_t power_state)
+DECL|dma_qmsi_set_power_state|macro|dma_qmsi_set_power_state
 DECL|dma_qmsi_transfer_config|function|static int dma_qmsi_transfer_config(struct device *dev, uint32_t channel, struct dma_transfer_config *config)
 DECL|dma_qmsi_transfer_start|function|static int dma_qmsi_transfer_start(struct device *dev, uint32_t channel)
 DECL|dma_qmsi_transfer_stop|function|static int dma_qmsi_transfer_stop(struct device *dev, uint32_t channel)
+DECL|dma_resume_device|function|static int dma_resume_device(struct device *dev)
+DECL|dma_suspend_device|function|static int dma_suspend_device(struct device *dev)
 DECL|error|member|void (*error[QM_DMA_CHANNEL_NUM])(struct device *dev, void *data);
 DECL|index|member|uint32_t index;
 DECL|instance|member|qm_dma_t instance; /* Controller instance. */
+DECL|saved_ctx|member|qm_dma_context_t saved_ctx;
 DECL|transfer|member|void (*transfer[QM_DMA_CHANNEL_NUM])(struct device *dev, void *data);
