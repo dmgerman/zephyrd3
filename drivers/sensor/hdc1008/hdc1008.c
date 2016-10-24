7caebf555122240681443049acfd1377c34d1487
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-24 10:17:49 +0100

    hdc1008: Limit name space pollution by using 'static'
    The read16() name collides with other definitions within zephyr.  Make
    it static and fix a few other unnecessary name exports while we are
    here.
    
    Change-Id: Id72d44a6b84b8d72c7ba2d0f34b29dbc1d02f2b6
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: abefffeda673877c4e6cfb01261d9fd7e6d99696

--- drivers/sensor/hdc1008/hdc1008.c
+++ drivers/sensor/hdc1008/hdc1008.c
@@ -2,6 +2,6 @@ DECL|hdc1008_channel_get|function|static int hdc1008_channel_get(struct device *
 DECL|hdc1008_data|variable|hdc1008_data
 DECL|hdc1008_driver_api|variable|hdc1008_driver_api
 DECL|hdc1008_gpio_callback|function|static void hdc1008_gpio_callback(struct device *dev, struct gpio_callback *cb, uint32_t pins)
-DECL|hdc1008_init|function|int hdc1008_init(struct device *dev)
+DECL|hdc1008_init|function|static int hdc1008_init(struct device *dev)
 DECL|hdc1008_sample_fetch|function|static int hdc1008_sample_fetch(struct device *dev, enum sensor_channel chan)
-DECL|read16|function|uint16_t read16(struct device *dev, uint8_t a, uint8_t d)
+DECL|read16|function|static uint16_t read16(struct device *dev, uint8_t a, uint8_t d)
