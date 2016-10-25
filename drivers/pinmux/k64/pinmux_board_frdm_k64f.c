e17516f837bb98a42abf600a0450407da61eb8ad
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-25 21:22:03 +0100

    pinmux/k64: Make mux_config const and limit name space with static
    Change-Id: I91b7ee866c8a66c368b316d1eb30fe760ac26dff
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: d149a7bf3406028b3394b443f7c22b68967deb53

--- drivers/pinmux/k64/pinmux_board_frdm_k64f.c
+++ drivers/pinmux/k64/pinmux_board_frdm_k64f.c
@@ -1,2 +1,2 @@
-DECL|fsl_frdm_k64f_pin_init|function|int fsl_frdm_k64f_pin_init(struct device *arg)
+DECL|fsl_frdm_k64f_pin_init|function|static int fsl_frdm_k64f_pin_init(struct device *arg)
 DECL|mux_config|variable|mux_config
