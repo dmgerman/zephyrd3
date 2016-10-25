bbf25cf75dc6f55e14e11a3f2afcf00c78cd5272
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-25 09:59:33 +0200

    net: ieee802154: Integrate 6lo fragmentation reassembly
    Relevantly call ieee802154_reassemble() on reception.
    
    Change-Id: I10ec027f5300e6e0a14e7483961b1ae2d6dfafe2
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 4266653c8c47acb7f33e2e91f8174ef4ba8bd323

--- net/yaip/l2/ieee802154/ieee802154.c
+++ net/yaip/l2/ieee802154/ieee802154.c
@@ -4,6 +4,8 @@ DECL|get_lqi|function|static inline uint8_t get_lqi(struct net_buf *buf)
 DECL|ieee802154_acknowledge|function|static inline void ieee802154_acknowledge(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_acknowledge|macro|ieee802154_acknowledge
 DECL|ieee802154_init|function|void ieee802154_init(struct net_if *iface)
+DECL|ieee802154_manage_recv_buffer|function|enum net_verdict ieee802154_manage_recv_buffer(struct net_if *iface, struct net_buf *buf)
+DECL|ieee802154_manage_recv_buffer|macro|ieee802154_manage_recv_buffer
 DECL|ieee802154_recv|function|static enum net_verdict ieee802154_recv(struct net_if *iface,struct net_buf *buf)
 DECL|ieee802154_send|function|static enum net_verdict ieee802154_send(struct net_if *iface,struct net_buf *buf)
 DECL|ieeee802154_reserve|function|static uint16_t ieeee802154_reserve(struct net_if *iface, void *data)
