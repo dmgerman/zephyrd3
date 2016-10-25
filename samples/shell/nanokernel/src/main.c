b9ec59fe4abf7b62df57f1628f959c2a2aa0f404
Anas Nashif <anas.nashif@intel.com>
2016-10-25 15:12:13 -0400

    samples: shell: unify sample and drop nanokernel part
    Changed command to reflect features of unified kernel:
    
    * ticks -> uptime
    * hightick -> cycles
    
    Change-Id: I2250e3785d71d0d6677f288d85ce0c23b447a93c
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: e66112dd071343defc71f07a3d68783a5b7e98dc

--- samples/shell/nanokernel/src/main.c
+++
@@ -1,6 +0,0 @@
-DECL|DEVICE_NAME|macro|DEVICE_NAME
-DECL|commands|variable|commands
-DECL|main|function|void main(void)
-DECL|shell_cmd_highticks|function|static int shell_cmd_highticks(int argc, char *argv[])
-DECL|shell_cmd_ping|function|static int shell_cmd_ping(int argc, char *argv[])
-DECL|shell_cmd_ticks|function|static int shell_cmd_ticks(int argc, char *argv[])
--- samples/shell/src/main.c
+++ samples/shell/src/main.c
@@ -1,6 +1,6 @@
 DECL|DEVICE_NAME|macro|DEVICE_NAME
 DECL|commands|variable|commands
 DECL|main|function|void main(void)
-DECL|shell_cmd_highticks|function|static int shell_cmd_highticks(int argc, char *argv[])
+DECL|shell_cmd_cycles|function|static int shell_cmd_cycles(int argc, char *argv[])
 DECL|shell_cmd_ping|function|static int shell_cmd_ping(int argc, char *argv[])
-DECL|shell_cmd_ticks|function|static int shell_cmd_ticks(int argc, char *argv[])
+DECL|shell_cmd_uptime|function|static int shell_cmd_uptime(int argc, char *argv[])
