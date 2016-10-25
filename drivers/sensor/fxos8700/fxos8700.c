88f77693dfb48f80d1d6e604975525fdade5732a
Maureen Helm <maureen.helm@nxp.com>
2016-10-24 23:45:12 -0500

    sensor: fxos8700: Add accelerometer/magnetometer driver
    Adds basic sensor driver support for the NXP FXOS8700 6-axis
    accelerometer/magnetometer. Currently this driver supports
    accelerometer-only, magnetometer-only, and hybrid (accelerometer +
    magnetometer) modes, as well as 2g, 4g, and 8g accelerometer full scale
    ranges.
    
    This driver does not yet support any sensor triggers such as the data
    ready trigger, or runtime changing of sensor attributes.
    
    Datasheet:
    http://cache.nxp.com/files/sensors/doc/data_sheet/FXOS8700CQ.pdf
    
    Origin: Original
    Jira: ZEP-721
    
    Change-Id: Iff0f751c737196f60d5c5d3448631b57093ece34
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 3bdc77ee094c0f4f88379dd3159cd10199dc3a61

---
+++ drivers/sensor/fxos8700/fxos8700.c
@@ -0,0 +1,8 @@
+DECL|fxos8700_accel_convert|function|static void fxos8700_accel_convert(struct sensor_value *val, int16_t raw, enum fxos8700_range range)
+DECL|fxos8700_channel_get|function|static int fxos8700_channel_get(struct device *dev, enum sensor_channel chan,struct sensor_value *val)
+DECL|fxos8700_config|variable|fxos8700_config
+DECL|fxos8700_data|variable|fxos8700_data
+DECL|fxos8700_driver_api|variable|fxos8700_driver_api
+DECL|fxos8700_init|function|static int fxos8700_init(struct device *dev)
+DECL|fxos8700_magn_convert|function|static void fxos8700_magn_convert(struct sensor_value *val, int16_t raw)
+DECL|fxos8700_sample_fetch|function|static int fxos8700_sample_fetch(struct device *dev, enum sensor_channel chan)
