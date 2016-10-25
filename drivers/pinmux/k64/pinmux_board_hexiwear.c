24572a1b0c43f4547ea1535e90b58348b64e0ca9
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-25 21:22:30 +0100

    pinmux/hexiwear: Make mux_config const and limit name space with static
    Change-Id: Iba171d2367e4ff5c4e36f9b876cb5fb11ebd923a
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: a7147f70488728f431d50283eb2b6a34c6c7bff7

--- drivers/pinmux/k64/pinmux_board_hexiwear.c
+++ drivers/pinmux/k64/pinmux_board_hexiwear.c
@@ -1,2 +1,2 @@
-DECL|hexiwear_pin_init|function|int hexiwear_pin_init(struct device *arg)
+DECL|hexiwear_pin_init|function|static int hexiwear_pin_init(struct device *arg)
 DECL|mux_config|variable|mux_config
