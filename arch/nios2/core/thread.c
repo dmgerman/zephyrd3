ae00aa8efe33f28d0dfe68ad8b92923916ab4b67
Andrew Boie <andrew.p.boie@intel.com>
2016-10-25 11:47:52 -0700

    nios2: port to unified kernel
    With this patch we introduce unified kernel support for NIOS II.
    Not all test cases have been ported, but the following command
    currently succeeds with 43/43 passing test cases:
    
     $ sanitycheck --arch=nios2 -xKERNEL_TYPE=unified \
             --tag=unified_capable
    
    Issue: ZEP-934
    Change-Id: Id8effa0369a6a22c4d0a789fa2a8e108af0e0786
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 431607c20a01d16011754b1f5af64d9f2d07423b

--- arch/nios2/core/thread.c
+++ arch/nios2/core/thread.c
@@ -1,8 +1,8 @@
 DECL|_nanokernel|variable|_nanokernel
-DECL|_new_thread|function|void _new_thread(char *stack_memory, unsigned stack_size, void *uk_task_ptr, _thread_entry_t thread_func, _thread_arg_t arg1, _thread_arg_t arg2, _thread_arg_t arg3, int priority, unsigned options)
-DECL|arg1|member|_thread_arg_t arg1;
-DECL|arg2|member|_thread_arg_t arg2;
-DECL|arg3|member|_thread_arg_t arg3;
+DECL|_new_thread|function|void _new_thread(char *stack_memory, unsigned stack_size, void *uk_task_ptr, _thread_entry_t thread_func, void *arg1, void *arg2, void *arg3, int priority, unsigned options)
+DECL|arg1|member|void *arg1;
+DECL|arg2|member|void *arg2;
+DECL|arg3|member|void *arg3;
 DECL|entry_point|member|_thread_entry_t entry_point;
 DECL|init_stack_frame|struct|struct init_stack_frame {
 DECL|thread_monitor_init|function|static ALWAYS_INLINE void thread_monitor_init(struct tcs *tcs)
