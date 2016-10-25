9cf026d73b88d4a07a6cc5b28a642f2f5111a591
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-25 22:39:39 +0100

    driver/eth_dw: Limit name space, add static.
    Change-Id: If9de0bec8bafb66119e158782cf9618143dc2a52
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: ab7a768a18d521fb59af22476a757de42a920743

--- drivers/ethernet/eth_dw.c
+++ drivers/ethernet/eth_dw.c
@@ -3,7 +3,7 @@ DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|eth_0_runtime|variable|eth_0_runtime
 DECL|eth_config_0_irq|function|static void eth_config_0_irq(struct device *port)
 DECL|eth_config_0|variable|eth_config_0
-DECL|eth_dw_isr|function|void eth_dw_isr(struct device *port)
+DECL|eth_dw_isr|function|static void eth_dw_isr(struct device *port)
 DECL|eth_initialize|function|static int eth_initialize(struct device *port)
 DECL|eth_net_tx|function|static int eth_net_tx(struct net_buf *buf)
 DECL|eth_read|function|static inline uint32_t eth_read(uint32_t base_addr, uint32_t offset)
