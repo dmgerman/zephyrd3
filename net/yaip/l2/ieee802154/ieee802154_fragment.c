5e694604ff6d6f91ea2aa273d178988e27f4d5fd
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-10-25 10:43:33 +0200

    net: ieee802154: Improve 6lo frag reassemble API
    No reassembly logic is changed besides removing the final reassembled
    pointer. Instead of requiring such pointer, let's just use the frag one.
    Such changes will ease the usage of ieee802154_reassemble():
    the given frag buffer, in case it finally got reassembled and thus
    provided a NET_CONTINUE verdict, will directly be handled by net_core.
    
    This makes things consistent as well with verdicts:
    - in case of NET_OK, buf has been consumed and thus unrefed
    - in case of NET_DROP, buf needs to be unrefed by caller
    - in case of NET_CONTINUE, buf is updated with reassembled and
      decompressed data packet, up to net_core to handel it accordingly.
    
    Change-Id: Ibd1588debd22a7f4571b36713dd46bf8b1d88329
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 831bfcb048d846e812a5502900f3b3a98807b0bb

--- net/yaip/l2/ieee802154/ieee802154_fragment.c
+++ net/yaip/l2/ieee802154/ieee802154_fragment.c
@@ -2,11 +2,11 @@ DECL|FRAG_REASSEMBLY_TIMEOUT|macro|FRAG_REASSEMBLY_TIMEOUT
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|REASS_CACHE_SIZE|macro|REASS_CACHE_SIZE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
-DECL|add_frag_to_cache|function|static inline enum net_verdict add_frag_to_cache(struct net_buf *frag, struct net_buf **buf, bool first)
+DECL|add_frag_to_cache|function|static inline enum net_verdict add_frag_to_cache(struct net_buf *buf, bool first)
 DECL|buf|member|struct net_buf *buf; /* Reassemble buffer */
 DECL|cache|variable|cache
 DECL|calc_max_payload|function|static inline uint8_t calc_max_payload(struct net_buf *buf, uint8_t offset)
-DECL|clear_reass_cache|function|static inline void clear_reass_cache(uint16_t size, uint16_t tag, bool fail)
+DECL|clear_reass_cache|function|static inline void clear_reass_cache(uint16_t size, uint16_t tag)
 DECL|compact_frag|function|static inline uint8_t compact_frag(struct net_buf *frag, uint8_t moved)
 DECL|datagram_tag|variable|datagram_tag
 DECL|frag_cache|struct|struct frag_cache {
@@ -14,7 +14,7 @@ DECL|get_datagram_size|function|static inline uint16_t get_datagram_size(uint8_t
 DECL|get_datagram_tag|function|static inline uint16_t get_datagram_tag(uint8_t *ptr)
 DECL|get_reass_cache|function|static inline struct frag_cache *get_reass_cache(struct net_buf *buf, uint16_t size, uint16_t tag)
 DECL|ieee802154_fragment|function|bool ieee802154_fragment(struct net_buf *buf, int hdr_diff)
-DECL|ieee802154_reassemble|function|enum net_verdict ieee802154_reassemble(struct net_buf *frag, struct net_buf **buf)
+DECL|ieee802154_reassemble|function|enum net_verdict ieee802154_reassemble(struct net_buf *buf)
 DECL|move_frag_data|function|static inline uint8_t move_frag_data(struct net_buf *frag, struct net_buf *next, uint8_t max, uint8_t data_len)
 DECL|reass_timeout|function|static void reass_timeout(struct nano_work *work)
 DECL|remove_frag_header|function|static inline void remove_frag_header(struct net_buf *frag, uint8_t hdr_len)
