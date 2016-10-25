ca237351321a03a54a4cfdc1e4566d0ffde0ecd8
Allan Stephens <allan.stephens@windriver.com>
2016-10-25 09:30:19 -0500

    kernel: Standardize thread monitoring initialization
    Gets rid of unnecessary THREAD_MONITOR_INIT() macro, to be
    consistent with the approach taken by _thread_monitor_exit().
    
    Aligns x86 code with the approach used on other architectures.
    
    Revises the associated comments and removes unnecessary
    doxygen tags.
    
    Change-Id: Ied1aebcd476afb82f61862b77264efb8a7dc66c9
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 2220f25f0a6271a0c51f30ef44d9d6ce74c77b9f

--- arch/arc/core/thread.c
+++ arch/arc/core/thread.c
@@ -1,5 +1,3 @@
-DECL|THREAD_MONITOR_INIT|macro|THREAD_MONITOR_INIT
-DECL|THREAD_MONITOR_INIT|macro|THREAD_MONITOR_INIT
 DECL|_nanokernel|variable|_nanokernel
 DECL|_new_thread|function|void _new_thread(char *pStackMem, unsigned stackSize, void *uk_task_ptr, _thread_entry_t pEntry, void *parameter1, void *parameter2, void *parameter3, int priority, unsigned options)
 DECL|init_stack_frame|struct|struct init_stack_frame {
@@ -10,3 +8,4 @@ DECL|r2|member|uint32_t r2;
 DECL|r3|member|uint32_t r3;
 DECL|status32|member|uint32_t status32;
 DECL|thread_monitor_init|function|static ALWAYS_INLINE void thread_monitor_init(struct tcs *tcs)
+DECL|thread_monitor_init|macro|thread_monitor_init
--- arch/nios2/core/thread.c
+++ arch/nios2/core/thread.c
@@ -1,5 +1,3 @@
-DECL|THREAD_MONITOR_INIT|macro|THREAD_MONITOR_INIT
-DECL|THREAD_MONITOR_INIT|macro|THREAD_MONITOR_INIT
 DECL|_nanokernel|variable|_nanokernel
 DECL|_new_thread|function|void _new_thread(char *stack_memory, unsigned stack_size, void *uk_task_ptr, _thread_entry_t thread_func, _thread_arg_t arg1, _thread_arg_t arg2, _thread_arg_t arg3, int priority, unsigned options)
 DECL|arg1|member|_thread_arg_t arg1;
@@ -8,3 +6,4 @@ DECL|arg3|member|_thread_arg_t arg3;
 DECL|entry_point|member|_thread_entry_t entry_point;
 DECL|init_stack_frame|struct|struct init_stack_frame {
 DECL|thread_monitor_init|function|static ALWAYS_INLINE void thread_monitor_init(struct tcs *tcs)
+DECL|thread_monitor_init|macro|thread_monitor_init
