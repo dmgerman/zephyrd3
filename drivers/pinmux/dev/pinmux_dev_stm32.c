bf22705f400fb466515cc7ec97134fc2ab6d89df
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-26 08:45:46 +0100

    pinmux/dev_stm32: Limit name space, add static.
    Change-Id: I023d921b35e5c3188b70bf3413c390f29628d380
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: cbc5a66505a3394f176fb7bfd372447328fa90cc

--- drivers/pinmux/dev/pinmux_dev_stm32.c
+++ drivers/pinmux/dev/pinmux_dev_stm32.c
@@ -1,6 +1,6 @@
 DECL|pinmux_stm32_api|variable|pinmux_stm32_api
 DECL|pinmux_stm32_get|function|static int pinmux_stm32_get(struct device *dev, uint32_t pin, uint32_t *func)
-DECL|pinmux_stm32_init|function|int pinmux_stm32_init(struct device *port)
+DECL|pinmux_stm32_init|function|static int pinmux_stm32_init(struct device *port)
 DECL|pinmux_stm32_input|function|static int pinmux_stm32_input(struct device *dev,uint32_t pin, uint8_t func)
 DECL|pinmux_stm32_pullup|function|static int pinmux_stm32_pullup(struct device *dev,uint32_t pin, uint8_t func)
 DECL|pinmux_stm32_set|function|static int pinmux_stm32_set(struct device *dev, uint32_t pin, uint32_t func)
