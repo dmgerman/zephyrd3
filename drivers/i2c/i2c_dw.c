8fc9e81765936c5fc164b7c0a9a523f2f4f9ffde
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-10-26 05:53:50 +0100

    grove/i2c/dw: Limit name space, add static.
    Change-Id: I949d07a962c9dc409de9c458a09a9d829f791496
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: c8a359c0396dbac33b9a05b87df2abdf9f9d2854

--- drivers/i2c/i2c_dw.c
+++ drivers/i2c/i2c_dw.c
@@ -7,12 +7,12 @@ DECL|_i2c_dw_transfer_complete|function|static inline void _i2c_dw_transfer_comp
 DECL|funcs|variable|funcs
 DECL|i2c_0_runtime|variable|i2c_0_runtime
 DECL|i2c_1_runtime|variable|i2c_1_runtime
-DECL|i2c_config_0|function|void i2c_config_0(struct device *port)
-DECL|i2c_config_1|function|void i2c_config_1(struct device *port)
+DECL|i2c_config_0|function|static void i2c_config_0(struct device *port)
+DECL|i2c_config_1|function|static void i2c_config_1(struct device *port)
 DECL|i2c_config_dw_0|variable|i2c_config_dw_0
 DECL|i2c_config_dw_1|variable|i2c_config_dw_1
-DECL|i2c_dw_initialize|function|int i2c_dw_initialize(struct device *port)
-DECL|i2c_dw_isr|function|void i2c_dw_isr(void *arg)
+DECL|i2c_dw_initialize|function|static int i2c_dw_initialize(struct device *port)
+DECL|i2c_dw_isr|function|static void i2c_dw_isr(void *arg)
 DECL|i2c_dw_pci_setup|function|static inline int i2c_dw_pci_setup(struct device *dev)
 DECL|i2c_dw_pci_setup|macro|i2c_dw_pci_setup
 DECL|i2c_dw_runtime_configure|function|static int i2c_dw_runtime_configure(struct device *dev, uint32_t config)
