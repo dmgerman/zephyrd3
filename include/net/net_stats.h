220d7178c72ac6eb4790efdf133e9eb7cf3fa9a3
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-10-26 13:57:30 +0300

    net: rpl: Initial commit
    RPL implementation taken from net/ip/contiki/rpl and
    ported to use the new stack.
    
    Origin: Contiki
    
    Change-Id: I479d9dd143b763f90cb7915806fd7e9faea0300c
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 98404b175ce9382b44170a3b71a3d8629f942259

--- include/net/net_stats.h
+++ include/net/net_stats.h
@@ -4,6 +4,10 @@ DECL|NET_STATS_IPV6_ND|macro|NET_STATS_IPV6_ND
 DECL|NET_STATS_IPV6_ND|macro|NET_STATS_IPV6_ND
 DECL|NET_STATS_IPV6|macro|NET_STATS_IPV6
 DECL|NET_STATS_IPV6|macro|NET_STATS_IPV6
+DECL|NET_STATS_RPL_DIS|macro|NET_STATS_RPL_DIS
+DECL|NET_STATS_RPL_DIS|macro|NET_STATS_RPL_DIS
+DECL|NET_STATS_RPL|macro|NET_STATS_RPL
+DECL|NET_STATS_RPL|macro|NET_STATS_RPL
 DECL|NET_STATS_TCP|macro|NET_STATS_TCP
 DECL|NET_STATS_TCP|macro|NET_STATS_TCP
 DECL|NET_STATS_UDP|macro|NET_STATS_UDP
@@ -16,28 +20,50 @@ DECL|chkerr|member|net_stats_t chkerr;
 DECL|chkerr|member|net_stats_t chkerr;
 DECL|chkerr|member|net_stats_t chkerr;
 DECL|chkerr|member|net_stats_t chkerr;
+DECL|dao_ack|member|struct net_stats_rpl_dao_ack dao_ack;
+DECL|dao|member|struct net_stats_rpl_dao dao;
+DECL|dio|member|struct net_stats_rpl_dio dio;
+DECL|dis|member|struct net_stats_rpl_dis dis;
 DECL|drop|member|net_stats_t drop;
 DECL|drop|member|net_stats_t drop;
 DECL|drop|member|net_stats_t drop;
 DECL|drop|member|net_stats_t drop;
 DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|drop|member|net_stats_t drop;
+DECL|forward_errors|member|uint16_t forward_errors;
+DECL|forwarded|member|net_stats_t forwarded;
 DECL|forwarded|member|net_stats_t forwarded;
 DECL|fragerr|member|net_stats_t fragerr;
+DECL|global_repairs|member|uint16_t global_repairs;
 DECL|hblenerr|member|net_stats_t hblenerr;
 DECL|icmp|member|struct net_stats_icmp icmp;
+DECL|interval|member|net_stats_t interval;
 DECL|ip_errors|member|struct net_stats_ip_errors ip_errors;
 DECL|ipv4|member|struct net_stats_ip ipv4;
 DECL|ipv6_nd|member|struct net_stats_ipv6_nd ipv6_nd;
 DECL|ipv6|member|struct net_stats_ip ipv6;
 DECL|lblenerr|member|net_stats_t lblenerr;
+DECL|local_repairs|member|uint16_t local_repairs;
+DECL|loop_errors|member|uint16_t loop_errors;
+DECL|loop_warnings|member|uint16_t loop_warnings;
+DECL|malformed_msgs|member|uint16_t malformed_msgs;
+DECL|mem_overflows|member|uint16_t mem_overflows;
 DECL|net_stats_icmp|struct|struct net_stats_icmp {
 DECL|net_stats_ip_errors|struct|struct net_stats_ip_errors {
 DECL|net_stats_ipv6_nd|struct|struct net_stats_ipv6_nd {
 DECL|net_stats_ip|struct|struct net_stats_ip {
+DECL|net_stats_rpl_dao_ack|struct|struct net_stats_rpl_dao_ack {
+DECL|net_stats_rpl_dao|struct|struct net_stats_rpl_dao {
+DECL|net_stats_rpl_dio|struct|struct net_stats_rpl_dio {
+DECL|net_stats_rpl_dis|struct|struct net_stats_rpl_dis {
 DECL|net_stats_tcp|struct|struct net_stats_tcp {
 DECL|net_stats_t|typedef|typedef uint32_t net_stats_t;
 DECL|net_stats_udp|struct|struct net_stats_udp {
 DECL|net_stats|struct|struct net_stats {
+DECL|parent_switch|member|uint16_t parent_switch;
 DECL|processing_error|member|net_stats_t processing_error;
 DECL|protoerr|member|net_stats_t protoerr;
 DECL|recv|member|net_stats_t recv;
@@ -45,8 +71,19 @@ DECL|recv|member|net_stats_t recv;
 DECL|recv|member|net_stats_t recv;
 DECL|recv|member|net_stats_t recv;
 DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|recv|member|net_stats_t recv;
+DECL|resets|member|uint16_t resets;
 DECL|rexmit|member|net_stats_t rexmit;
+DECL|root_repairs|member|uint16_t root_repairs;
+DECL|rpl|member|} rpl;
 DECL|rst|member|net_stats_t rst;
+DECL|sent|member|net_stats_t sent;
+DECL|sent|member|net_stats_t sent;
+DECL|sent|member|net_stats_t sent;
+DECL|sent|member|net_stats_t sent;
 DECL|sent|member|net_stats_t sent;
 DECL|sent|member|net_stats_t sent;
 DECL|sent|member|net_stats_t sent;
