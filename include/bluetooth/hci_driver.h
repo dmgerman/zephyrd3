467696ceb940e45d79d2c71e4eb922ff9c17ceb0
Johan Hedberg <johan.hedberg@intel.com>
2016-10-27 16:55:01 +0300

    Bluetooth: Rename bt_driver to bt_hci_driver
    The bt_driver API was created when Zephyr only had a Bluetooth host
    stack, but no controller-side functionality. The only "driver" that
    was needed for the host was the HCI driver, and hence "HCI" was
    omitted from the name.
    
    With support both for host and controller Zephyr will be getting more
    Bluetooth driver types, in particular radio drivers. To prepare for
    this, move all HCI drivers to drivers/bluetooth/hci/ and rename the
    bt_driver API bt_hci_driver.
    
    Change-Id: I82829da80aa61f26c2bb2005380f1e88d069ac7d
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6989bf88e1da18e02adb2f6a431a5a175f02bb00

---
+++ include/bluetooth/hci_driver.h
@@ -0,0 +1,16 @@
+DECL|BT_HCI_DRIVER_BUS_I2C|enumerator|BT_HCI_DRIVER_BUS_I2C = 8,
+DECL|BT_HCI_DRIVER_BUS_PCCARD|enumerator|BT_HCI_DRIVER_BUS_PCCARD = 2,
+DECL|BT_HCI_DRIVER_BUS_PCI|enumerator|BT_HCI_DRIVER_BUS_PCI = 5,
+DECL|BT_HCI_DRIVER_BUS_RS232|enumerator|BT_HCI_DRIVER_BUS_RS232 = 4,
+DECL|BT_HCI_DRIVER_BUS_SDIO|enumerator|BT_HCI_DRIVER_BUS_SDIO = 6,
+DECL|BT_HCI_DRIVER_BUS_SPI|enumerator|BT_HCI_DRIVER_BUS_SPI = 7,
+DECL|BT_HCI_DRIVER_BUS_UART|enumerator|BT_HCI_DRIVER_BUS_UART = 3,
+DECL|BT_HCI_DRIVER_BUS_USB|enumerator|BT_HCI_DRIVER_BUS_USB = 1,
+DECL|BT_HCI_DRIVER_BUS_VIRTUAL|enumerator|BT_HCI_DRIVER_BUS_VIRTUAL = 0,
+DECL|__BT_HCI_DRIVER_H|macro|__BT_HCI_DRIVER_H
+DECL|bt_hci_driver_bus|enum|enum bt_hci_driver_bus {
+DECL|bt_hci_driver|struct|struct bt_hci_driver {
+DECL|bus|member|enum bt_hci_driver_bus bus;
+DECL|name|member|const char *name;
+DECL|open|member|int (*open)(void);
+DECL|send|member|int (*send)(struct net_buf *buf);
