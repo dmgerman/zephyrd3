9c3d52735dcece1e4d5d344fdce85f8e675136d2
Szymon Janc <ext.szymon.janc@tieto.com>
2016-10-27 11:56:51 +0200

    Bluetooth: Controller: Use unified k_sem API for semaphore
    Change-Id: I02b4e5f4d0a2995cfae1b9a2edcbdf01d6ec631a
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: e18e3ed60ab6c5fdf5b6dc47d5dd2c105a784116

--- subsys/bluetooth/controller/hci/hci_driver.c
+++ subsys/bluetooth/controller/hci/hci_driver.c
@@ -9,12 +9,12 @@ DECL|cmd_handle|function|static int cmd_handle(struct net_buf *buf)
 DECL|drv|variable|drv
 DECL|hci_driver_open|function|static int hci_driver_open(void)
 DECL|hci_driver_send|function|static int hci_driver_send(struct net_buf *buf)
-DECL|nano_sem_recv|variable|nano_sem_recv
 DECL|radio_active_callback|function|void radio_active_callback(uint8_t active)
 DECL|radio_event_callback|function|void radio_event_callback(void)
 DECL|radio_nrf5_isr|function|static void radio_nrf5_isr(void *arg)
 DECL|recv_fiber|function|static void recv_fiber(int unused0, int unused1)
 DECL|rng_nrf5_isr|function|static void rng_nrf5_isr(void *arg)
 DECL|rtc0_nrf5_isr|function|static void rtc0_nrf5_isr(void *arg)
+DECL|sem_recv|variable|sem_recv
 DECL|swi4_nrf5_isr|function|static void swi4_nrf5_isr(void *arg)
 DECL|swi5_nrf5_isr|function|static void swi5_nrf5_isr(void *arg)
