c5dbf5a7e1979925a97fc85fee1c9b9ee14133bd
Ramesh Thomas <ramesh.thomas@intel.com>
2016-10-26 21:16:37 -0700

    power_mgmt: Update changes in k_idle.c missing in idle.c
    Some changes that went into k_idle.c were missing in idle.c
    causing errors while building power management code for
    unified kernel. Added the missing changes.
    
    Tested with power_mgr app built for unified kernel.
    
    Jira: ZEP-1139
    Change-Id: I9fe005544f7ee69d3cb3ff10c649be28037fcf15
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 3e0f20a7d507882719ac2b9137b865bfd4430a97

--- kernel/unified/idle.c
+++ kernel/unified/idle.c
@@ -1,6 +1,7 @@
 DECL|_sys_idle_threshold_ticks|variable|_sys_idle_threshold_ticks
 DECL|_sys_power_save_idle_exit|function|void _sys_power_save_idle_exit(int32_t ticks)
 DECL|_sys_power_save_idle|function|static void _sys_power_save_idle(int32_t ticks __unused)
+DECL|_sys_soc_notify_wake_event|variable|_sys_soc_notify_wake_event
 DECL|idle|function|void idle(void *unused1, void *unused2, void *unused3)
 DECL|set_kernel_idle_time_in_ticks|function|static void set_kernel_idle_time_in_ticks(int32_t ticks)
 DECL|set_kernel_idle_time_in_ticks|macro|set_kernel_idle_time_in_ticks
