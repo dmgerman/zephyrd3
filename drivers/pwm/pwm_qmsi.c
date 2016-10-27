277a07ca3779f0f37bdc303d8777386eef2ec35f
JuanX Solano Menacho <juanx.solano.menacho@intel.com>
2016-10-27 14:15:25 +0200

    pwm_qmsi: Use qm_pwm_save/restore_context APIs
    This commit updates the pwm_qmsi driver by removing the temporary Zephyr
    save/restore context implementation and using the new QMSI APIs.
    
    Jira: ZEP-1008
    Change-Id: Ic92feeb906539f506eb521b7302f8cf5af3edc64
    Signed-off-by: JuanX Solano Menacho <juanx.solano.menacho@intel.com>
    
    Former-commit-id: 5a4c3e3fbb54fa95444a800e632f370fc5de01ad

--- drivers/pwm/pwm_qmsi.c
+++ drivers/pwm/pwm_qmsi.c
@@ -9,18 +9,11 @@ DECL|RP_GET|macro|RP_GET
 DECL|RP_GET|macro|RP_GET
 DECL|__set_one_port|function|static int __set_one_port(struct device *dev, qm_pwm_t id, uint32_t pwm,uint32_t on, uint32_t off)
 DECL|channel_period|member|uint32_t channel_period[CONFIG_PWM_QMSI_NUM_PORTS];
-DECL|channels|member|struct pwm_channel_ctx channels[CONFIG_PWM_QMSI_NUM_PORTS];
-DECL|controlreg|member|uint32_t controlreg;
 DECL|device_power_state|member|uint32_t device_power_state;
-DECL|int_pwm_timer_mask|member|uint32_t int_pwm_timer_mask;
-DECL|loadcount1|member|uint32_t loadcount1;
-DECL|loadcount2|member|uint32_t loadcount2;
-DECL|pwm_channel_ctx|struct|struct pwm_channel_ctx {
 DECL|pwm_context|variable|pwm_context
 DECL|pwm_critical_region_end|function|static void pwm_critical_region_end(struct device *dev)
 DECL|pwm_critical_region_start|function|static void pwm_critical_region_start(struct device *dev)
-DECL|pwm_ctx_save|variable|pwm_ctx_save
-DECL|pwm_ctx|struct|struct pwm_ctx {
+DECL|pwm_ctx|variable|pwm_ctx
 DECL|pwm_data|struct|struct pwm_data {
 DECL|pwm_qmsi_configure|function|static int pwm_qmsi_configure(struct device *dev, int access_op, uint32_t pwm, int flags)
 DECL|pwm_qmsi_device_ctrl|function|static int pwm_qmsi_device_ctrl(struct device *dev, uint32_t ctrl_command,void *context)
