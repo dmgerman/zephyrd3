eb08ab997fd3ddca3872d185577e2d05d9323d25
Julien Delayen <julien.delayen@intel.com>
2016-10-27 14:31:19 +0100

    aio_cmp_qmsi: Add get_pending_int API
    For AON peripherals on Quark SE C1000, an API is needed
    to retrieve the interrupt status after wake up.
    This enables the application to know the wake source before
    enabling again the interrupts.
    Add this API to the aio comparator as this is a wake event
    on Quark SE C1000.
    
    Jira: ZEP-1188
    
    Change-Id: Ifa08353270910a363c15a4203770ff3e7857572b
    Signed-off-by: Julien Delayen <julien.delayen@intel.com>
    
    Former-commit-id: 14711488db17f2938fc659e9b6881f179de889df

--- drivers/aio/aio_comparator_qmsi.c
+++ drivers/aio/aio_comparator_qmsi.c
@@ -4,6 +4,7 @@ DECL|CMP_INTR_ROUTER|macro|CMP_INTR_ROUTER
 DECL|INT_COMPARATORS_MASK|macro|INT_COMPARATORS_MASK
 DECL|aio_cmp_config|function|static int aio_cmp_config(struct device *dev)
 DECL|aio_cmp_funcs|variable|aio_cmp_funcs
+DECL|aio_cmp_qmsi_get_pending_int|function|static uint32_t aio_cmp_qmsi_get_pending_int(struct device *dev)
 DECL|aio_qmsi_cmp_cb|struct|struct aio_qmsi_cmp_cb {
 DECL|aio_qmsi_cmp_configure|function|static int aio_qmsi_cmp_configure(struct device *dev, uint8_t index, enum aio_cmp_polarity polarity, enum aio_cmp_ref refsel, aio_cmp_cb cb, void *param)
 DECL|aio_qmsi_cmp_dev_data_t|struct|struct aio_qmsi_cmp_dev_data_t {
--- include/aio_comparator.h
+++ include/aio_comparator.h
@@ -5,11 +5,14 @@ DECL|AIO_CMP_REF_B|enumerator|AIO_CMP_REF_B, /**< Use reference B. */
 DECL|_AIO_COMPARATOR_H_|macro|_AIO_COMPARATOR_H_
 DECL|aio_cmp_api_configure|typedef|typedef int (*aio_cmp_api_configure)(struct device *dev, uint8_t index,
 DECL|aio_cmp_api_disable|typedef|typedef int (*aio_cmp_api_disable)(struct device *dev, uint8_t index);
+DECL|aio_cmp_api_get_pending_int|typedef|typedef uint32_t (*aio_cmp_api_get_pending_int)(struct device *dev);
 DECL|aio_cmp_cb|typedef|typedef void (*aio_cmp_cb)(void *);
 DECL|aio_cmp_configure|function|static inline int aio_cmp_configure(struct device *dev, uint8_t index, enum aio_cmp_polarity polarity, enum aio_cmp_ref refsel, aio_cmp_cb cb, void *param)
 DECL|aio_cmp_disable|function|static inline int aio_cmp_disable(struct device *dev, uint8_t index)
 DECL|aio_cmp_driver_api|struct|struct aio_cmp_driver_api {
+DECL|aio_cmp_get_pending_int|function|static inline int aio_cmp_get_pending_int(struct device *dev)
 DECL|aio_cmp_polarity|enum|enum aio_cmp_polarity {
 DECL|aio_cmp_ref|enum|enum aio_cmp_ref {
 DECL|configure|member|aio_cmp_api_configure configure;
 DECL|disable|member|aio_cmp_api_disable disable;
+DECL|get_pending_int|member|aio_cmp_api_get_pending_int get_pending_int;
