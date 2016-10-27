83e96b48d780cabe85643ea36f21b81f2e924d6f
Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
2016-10-27 21:08:45 +0530

    Bluetooth: RFCOMM: Define enum and macros for role and CR
    This replaces initiator flag of session and dlc to enum which has
    two values INITIATOR and ACCEPTOR.
    
    Also this defines macros for CRs in header. Basically there are
    three types fo CRs. Frame header CR has different meaning for
    UIH and non UIH packets. Also this renames the existing msg hdr
    CR to make it consistent.
    
    These changes are basically done to make it more readable
    
    Change-Id: Ic15e93465b0afbd19d8805f27d7a43f34ef38689
    Signed-off-by: Jaganath Kanakkassery <jaganathx.kanakkassery@intel.com>
    
    Former-commit-id: 2eba256fd2ad58961a65b362847dc19acb11e94d

--- include/bluetooth/rfcomm.h
+++ include/bluetooth/rfcomm.h
@@ -3,22 +3,26 @@ DECL|BT_RFCOMM_CHAN_HFP_HF|enumerator|BT_RFCOMM_CHAN_HFP_HF = 1,
 DECL|BT_RFCOMM_CHAN_HSP_AG|enumerator|BT_RFCOMM_CHAN_HSP_AG,
 DECL|BT_RFCOMM_CHAN_HSP_HS|enumerator|BT_RFCOMM_CHAN_HSP_HS,
 DECL|BT_RFCOMM_CHAN_SPP|enumerator|BT_RFCOMM_CHAN_SPP,
+DECL|BT_RFCOMM_ROLE_ACCEPTOR|enumerator|BT_RFCOMM_ROLE_ACCEPTOR,
+DECL|BT_RFCOMM_ROLE_INITIATOR|enumerator|BT_RFCOMM_ROLE_INITIATOR
 DECL|__BT_RFCOMM_H|macro|__BT_RFCOMM_H
 DECL|_next|member|struct bt_rfcomm_dlc *_next;
 DECL|_next|member|struct bt_rfcomm_server *_next;
 DECL|accept|member|int (*accept)(struct bt_conn *conn, struct bt_rfcomm_dlc **dlc);
 DECL|bt_rfcomm_dlc_ops|struct|struct bt_rfcomm_dlc_ops {
 DECL|bt_rfcomm_dlc|struct|struct bt_rfcomm_dlc {
+DECL|bt_rfcomm_role_t|typedef|} __packed bt_rfcomm_role_t;
+DECL|bt_rfcomm_role|enum|typedef enum bt_rfcomm_role {
 DECL|bt_rfcomm_server|struct|struct bt_rfcomm_server {
 DECL|channel|member|uint8_t channel;
 DECL|connected|member|void (*connected)(struct bt_rfcomm_dlc *dlc);
 DECL|disconnected|member|void (*disconnected)(struct bt_rfcomm_dlc *dlc);
 DECL|dlci|member|uint8_t dlci;
-DECL|initiator|member|bool initiator;
 DECL|mtu|member|uint16_t mtu;
 DECL|ops|member|struct bt_rfcomm_dlc_ops *ops;
 DECL|recv|member|void (*recv)(struct bt_rfcomm_dlc *dlc, struct net_buf *buf);
 DECL|required_sec_level|member|bt_security_t required_sec_level;
+DECL|role|member|bt_rfcomm_role_t role;
 DECL|rx_credit|member|uint8_t rx_credit;
 DECL|session|member|struct bt_rfcomm_session *session;
 DECL|state|member|uint8_t state;
