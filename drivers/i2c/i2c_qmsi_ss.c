cd46d0fbc8bb066a2f5420b8b63865ce12fcd91e
JuanX Solano Menacho <juanx.solano.menacho@intel.com>
2016-10-28 11:54:11 +0200

    i2c_qmsi_ss: Use qm_ss_i2c_save/restore_context APIs
    This commit updates the i2c_qmsi_ss driver by adding save/restore
    context functionality for power management, using the corresponsing
    QMSI APIs.
    
    Jira: ZEP-666
    
    Change-Id: I36fb18b52edd4dac4c4b6cb98162adbca74800cb
    Signed-off-by: JuanX Solano Menacho <juanx.solano.menacho@intel.com>
    
    Former-commit-id: b4b612f1e4b8c1cfb786c4201fb6d55120b6b8ea

--- drivers/i2c/i2c_qmsi_ss.c
+++ drivers/i2c/i2c_qmsi_ss.c
@@ -4,8 +4,10 @@ DECL|api|variable|api
 DECL|config_info_0|variable|config_info_0
 DECL|config_info_1|variable|config_info_1
 DECL|default_cfg|member|union dev_config default_cfg;
+DECL|device_power_state|member|uint32_t device_power_state;
 DECL|driver_data_0|variable|driver_data_0
 DECL|driver_data_1|variable|driver_data_1
+DECL|i2c_ctx|member|qm_ss_i2c_context_t i2c_ctx;
 DECL|i2c_qmsi_ss_config_info|struct|struct i2c_qmsi_ss_config_info {
 DECL|i2c_qmsi_ss_config_irq_0|function|static void i2c_qmsi_ss_config_irq_0(void)
 DECL|i2c_qmsi_ss_config_irq_1|function|static void i2c_qmsi_ss_config_irq_1(void)
@@ -17,6 +19,12 @@ DECL|i2c_qmsi_ss_transfer|function|static int i2c_qmsi_ss_transfer(struct device
 DECL|instance|member|qm_ss_i2c_t instance; /* Controller instance. */
 DECL|irq_cfg|member|void (*irq_cfg)(void);
 DECL|sem|member|struct k_sem sem;
+DECL|ss_i2c_device_ctrl|function|static int ss_i2c_device_ctrl(struct device *dev, uint32_t ctrl_command, void *context)
+DECL|ss_i2c_qmsi_get_power_state|function|static uint32_t ss_i2c_qmsi_get_power_state(struct device *dev)
+DECL|ss_i2c_qmsi_set_power_state|function|static void ss_i2c_qmsi_set_power_state(struct device *dev,uint32_t power_state)
+DECL|ss_i2c_qmsi_set_power_state|macro|ss_i2c_qmsi_set_power_state
+DECL|ss_i2c_resume_device_from_suspend|function|static int ss_i2c_resume_device_from_suspend(struct device *dev)
+DECL|ss_i2c_suspend_device|function|static int ss_i2c_suspend_device(struct device *dev)
 DECL|sync|member|device_sync_call_t sync;
 DECL|transfer_complete|function|static void transfer_complete(void *data, int rc, qm_ss_i2c_status_t status,uint32_t len)
 DECL|transfer_status|member|int transfer_status;
