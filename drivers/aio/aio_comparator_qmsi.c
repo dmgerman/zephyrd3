931f8fc508d878526dcaef8cbbe5fdaea8d88512
Iván Briano <ivan.briano@intel.com>
2016-10-28 14:56:01 -0200

    aio qmsi: Enable the driver to work on ARC
    Jira: ZEP-1030
    Change-Id: I614646053974d6a81579bbea9d969a10916f9cac
    Signed-off-by: Iván Briano <ivan.briano@intel.com>
    
    Former-commit-id: 244af4a2af21db941adcf117a252b8ceca1c95a1

--- drivers/aio/aio_comparator_qmsi.c
+++ drivers/aio/aio_comparator_qmsi.c
@@ -1,4 +1,6 @@
 DECL|AIO_QMSI_CMP_COUNT|macro|AIO_QMSI_CMP_COUNT
+DECL|CMP_INTR_ROUTER|macro|CMP_INTR_ROUTER
+DECL|CMP_INTR_ROUTER|macro|CMP_INTR_ROUTER
 DECL|INT_COMPARATORS_MASK|macro|INT_COMPARATORS_MASK
 DECL|aio_cmp_config|function|static int aio_cmp_config(struct device *dev)
 DECL|aio_cmp_funcs|variable|aio_cmp_funcs
@@ -8,7 +10,7 @@ DECL|aio_qmsi_cmp_dev_data_t|struct|struct aio_qmsi_cmp_dev_data_t {
 DECL|aio_qmsi_cmp_dev_data|variable|aio_qmsi_cmp_dev_data
 DECL|aio_qmsi_cmp_disable|function|static int aio_qmsi_cmp_disable(struct device *dev, uint8_t index)
 DECL|aio_qmsi_cmp_init|function|static int aio_qmsi_cmp_init(struct device *dev)
-DECL|aio_qmsi_cmp_isr|function|static void aio_qmsi_cmp_isr(struct device *dev)
+DECL|aio_qmsi_cmp_isr|function|static void aio_qmsi_cmp_isr(void *data)
 DECL|cb|member|aio_cmp_cb cb;
 DECL|cb|member|struct aio_qmsi_cmp_cb cb[AIO_QMSI_CMP_COUNT];
 DECL|config|variable|config
