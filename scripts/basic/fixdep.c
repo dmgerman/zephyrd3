d2d5a0242335bf8f4fcaf1f0686b3ec2038e2d06
Flavio Santes <flavio.santes@intel.com>
2016-10-30 20:20:53 -0600

    scripts: Update fixdep from kernel.org
    This commit updates fixdep.c from the Linux Kernel, stable v4.8.5.
    
    Origin: https://git.kernel.org/cgit/linux/kernel/git/stable/linux-stable.git/plain/scripts/basic/fixdep.c?id=refs/tags/v4.8.5
    
    Jira: ZEP-737
    
    Change-Id: If75d0abb22f98bae74a801aea53218039107dd65
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 70ff94c42a6a91a397a3583ad787f78a9b5be5b1

--- scripts/basic/fixdep.c
+++ scripts/basic/fixdep.c
@@ -8,13 +8,14 @@ DECL|INT_NFIG|macro|INT_NFIG
 DECL|INT_NFIG|macro|INT_NFIG
 DECL|INT_ONFI|macro|INT_ONFI
 DECL|INT_ONFI|macro|INT_ONFI
-DECL|clear_config|function|static void clear_config(void)
 DECL|cmdline|variable|cmdline
 DECL|define_config|function|static void define_config(const char *name, int len, unsigned int hash)
 DECL|depfile|variable|depfile
 DECL|do_config_file|function|static void do_config_file(const char *filename)
+DECL|do_extra_deps|function|static void do_extra_deps(void)
 DECL|hashtab|variable|hashtab
 DECL|hash|member|unsigned int hash;
+DECL|insert_extra_deps|variable|insert_extra_deps
 DECL|is_defined_config|function|static int is_defined_config(const char *name, int len, unsigned int hash)
 DECL|item|struct|struct item {
 DECL|len|member|unsigned int len;
@@ -24,10 +25,11 @@ DECL|next|member|struct item *next;
 DECL|parse_config_file|function|static void parse_config_file(const char *map, size_t len)
 DECL|parse_dep_file|function|static void parse_dep_file(void *map, size_t len)
 DECL|print_cmdline|function|static void print_cmdline(void)
+DECL|print_config|function|static void print_config(const char *m, int slen)
 DECL|print_deps|function|static void print_deps(void)
 DECL|read_file_in|function|static void *read_file_in(int fd, size_t size)
 DECL|strhash|function|static unsigned int strhash(const char *str, unsigned int sz)
-DECL|strrcmp|function|static int strrcmp(char *s, char *sub)
+DECL|strrcmp|function|static int strrcmp(const char *s, const char *sub)
 DECL|target|variable|target
 DECL|traps|function|static void traps(void)
 DECL|usage|function|static void usage(void)
