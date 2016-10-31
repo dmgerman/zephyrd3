c387d5fe7dbaf07def2899369af624d0e30e5a61
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-10-31 17:12:23 -0400

    unified/test Adapt sys_kernel test to unified kernel
    As unified kernel starts main() application function, remove test
    thread.
    
    Remove nanokernel part of the test since their code base is
    identical.
    
    Change-Id: I2afb4c0da2d139b159c89186166afdb5d9813d61
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 1952ac1611c9b2877daf1618f60145370d77b7b2

--- tests/benchmark/sys_kernel/src/syskernel.c
+++ tests/benchmark/sys_kernel/src/syskernel.c
@@ -1,17 +1,16 @@
 DECL|Msg|variable|Msg
-DECL|SysKernelBench|function|void SysKernelBench(void)
 DECL|begin_test|function|void begin_test(void)
 DECL|check_result|function|int check_result(int i, uint32_t t)
 DECL|fiber_stack1|variable|fiber_stack1
 DECL|fiber_stack2|variable|fiber_stack2
 DECL|init_output|function|void init_output(int *continuously)
 DECL|kbhit|function|int kbhit(void)
+DECL|main|function|void main(void)
 DECL|output_close|function|void output_close(void)
 DECL|output_file|variable|output_file
 DECL|sz_case_details_fmt|variable|sz_case_details_fmt
 DECL|sz_case_end_fmt|variable|sz_case_end_fmt
 DECL|sz_case_result_fmt|variable|sz_case_result_fmt
-DECL|sz_case_timing_fmt|variable|sz_case_timing_fmt
 DECL|sz_case_timing_fmt|variable|sz_case_timing_fmt
 DECL|sz_date_fmt|variable|sz_date_fmt
 DECL|sz_description|variable|sz_description
