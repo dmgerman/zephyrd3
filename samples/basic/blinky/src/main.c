d10a05012fbc04aac0dbcc9812417298f9eee939
Anas Nashif <anas.nashif@intel.com>
2016-11-01 07:33:08 -0400

    samples: convert to unified applications
    Change-Id: Ic65886b9144e4c8b738f94ab228541ef4a34dea3
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 0ad8e7c21af1e4c4b884622f06e7b1800d327544

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -1,3 +1,4 @@
 DECL|LED|macro|LED
 DECL|PORT|macro|PORT
+DECL|SLEEP_TIME|macro|SLEEP_TIME
 DECL|main|function|void main(void)
--- samples/basic/disco/src/main.c
+++ samples/basic/disco/src/main.c
@@ -1,4 +1,5 @@
 DECL|LED1|macro|LED1
 DECL|LED2|macro|LED2
 DECL|PORT|macro|PORT
+DECL|SLEEP_TIME|macro|SLEEP_TIME
 DECL|main|function|void main(void)
