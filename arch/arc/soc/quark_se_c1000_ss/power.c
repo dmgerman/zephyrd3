76f415a3476a865b4a265d5bd2fd5acd462f4290
Julien Delayen <julien.delayen@intel.com>
2016-11-01 13:07:34 +0000

    power: Add ARC power states and update sample
    Add Low Power States support to the power shim layer
    and show the usage in the quark_se sample.
    
    States are defined as follow:
    - SYS_POWER_STATE_CPU_LPS: SS2 with LPSS enabled
    - SYS_POWER_STATE_CPU_LPS_1: SS2 with LPSS disabled
    - SYS_POWER_STATE_CPU_LPS_2: SS1 with LPSS disabled
    
    Jira: ZEP-994
    
    Change-Id: Ie4b93f6e539cb53fc035be00280b66b2cb0d9fea
    Signed-off-by: Julien Delayen <julien.delayen@intel.com>
    
    Former-commit-id: f540cbc5c535ad218dced9087c4a92a16d035e02

---
+++ arch/arc/soc/quark_se_c1000_ss/power.c
@@ -0,0 +1,11 @@
+DECL|ARC_SS1|macro|ARC_SS1
+DECL|ARC_SS2|macro|ARC_SS2
+DECL|ENABLE_INTERRUPTS|macro|ENABLE_INTERRUPTS
+DECL|SLEEP_MODE_CORE_OFF|macro|SLEEP_MODE_CORE_OFF
+DECL|SLEEP_MODE_CORE_TIMERS_RTC_OFF|macro|SLEEP_MODE_CORE_TIMERS_RTC_OFF
+DECL|_sys_soc_power_state_post_ops|function|void _sys_soc_power_state_post_ops(enum power_states state)
+DECL|_sys_soc_set_power_state|function|void _sys_soc_set_power_state(enum power_states state)
+DECL|aonpt_trigger|variable|aonpt_trigger
+DECL|enter_arc_state|function|static void enter_arc_state(int mode)
+DECL|restore_interrupt_trigger|function|static void restore_interrupt_trigger(void)
+DECL|rtc_trigger|variable|rtc_trigger
--- tests/power/power_states/src/main.c
+++ tests/power/power_states/src/main.c
@@ -1,16 +1,19 @@
 DECL|CMP_INTERRUPT_PIN|macro|CMP_INTERRUPT_PIN
 DECL|GPIO_INTERRUPT_PIN|macro|GPIO_INTERRUPT_PIN
 DECL|MAX_SUSPEND_DEVICE_COUNT|macro|MAX_SUSPEND_DEVICE_COUNT
+DECL|NB_STATES|macro|NB_STATES
 DECL|TIMEOUT|macro|TIMEOUT
+DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
 DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
 DECL|build_suspend_device_list|function|static void build_suspend_device_list(void)
 DECL|cmp_dev|variable|cmp_dev
 DECL|counter_dev|variable|counter_dev
+DECL|current_state|variable|current_state
 DECL|do_soc_sleep|function|static void do_soc_sleep(enum power_states state)
 DECL|get_next_state|function|static enum power_states get_next_state(void)
 DECL|gpio_dev|variable|gpio_dev
-DECL|last_state|variable|last_state
 DECL|main|function|void main(void)
+DECL|post_ops_done|variable|post_ops_done
 DECL|rtc_dev|variable|rtc_dev
 DECL|set_counter_alarm|function|static void set_counter_alarm(void)
 DECL|set_rtc_alarm|function|static void set_rtc_alarm(void)
@@ -20,5 +23,6 @@ DECL|setup_counter|function|static void setup_counter(void)
 DECL|setup_rtc|function|static void setup_rtc(void)
 DECL|setup_wake_event|function|static void setup_wake_event(void)
 DECL|state_to_string|function|static const char *state_to_string(int state)
+DECL|states_list|variable|states_list
 DECL|suspend_device_count|variable|suspend_device_count
 DECL|suspend_devices|variable|suspend_devices
