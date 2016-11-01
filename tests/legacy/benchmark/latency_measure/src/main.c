2d17040a23f5f6d023a75b8433504f9ec991a590
Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
2016-11-01 17:05:18 -0400

    unified/tests: Remove nanokernel test part from latency_measure
    Nanokernel part of the latency_measure test is a subset of the
    microkekernel tests. For unified kernel there is no reason to
    have nanokernel test - for unified kernel we have to run whole
    the set of tests.
    
    Change-Id: Ief176fd9d25e7355e3d3697c3bbc1e953b16655c
    Signed-off-by: Dmitriy Korovkin <dmitriy.korovkin@windriver.com>
    
    Former-commit-id: b4db06615d1042a180e3a3b28117e85e5525db68

--- tests/legacy/benchmark/latency_measure/src/main.c
+++ tests/legacy/benchmark/latency_measure/src/main.c
@@ -1,5 +1,4 @@
 DECL|errorCount|variable|errorCount
-DECL|main|function|void main(void)
 DECL|microMain|function|void microMain(void)
 DECL|microTest|function|void microTest(void)
 DECL|nanoTest|function|void nanoTest(void)
