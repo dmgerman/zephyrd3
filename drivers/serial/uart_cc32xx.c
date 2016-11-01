b7e76216b9773354627e41d8d72b2ecd71c81952
Gil Pitney <gil.pitney@linaro.org>
2016-10-31 17:46:32 -0700

    cc3200: Add a UART driver (polled and interrupt modes)
    The pinmux configuration is done during board initialization.
    
    This was validated using the following Zephyr apps:
    - samples/hello_world
    - samples/philosophers
    - samples/drivers/uart
    - samples/shell
    
    UARTA0 is currently supported.
    
    Change-Id: I85727c622d4d42183cc9f2f8b43d653e245dd17e
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>
    
    Former-commit-id: e8b43cbc817dedb9d192d6964c3cd8e9e93789a6

---
+++ drivers/serial/uart_cc32xx.c
@@ -0,0 +1,27 @@
+DECL|DEV_CFG|macro|DEV_CFG
+DECL|DEV_DATA|macro|DEV_DATA
+DECL|EXCEPTION_UARTA0|macro|EXCEPTION_UARTA0
+DECL|cb|member|uart_irq_callback_t cb; /**< Callback function pointer */
+DECL|uart_cc32xx_dev_cfg_0|variable|uart_cc32xx_dev_cfg_0
+DECL|uart_cc32xx_dev_data_0|variable|uart_cc32xx_dev_data_0
+DECL|uart_cc32xx_dev_data_t|struct|struct uart_cc32xx_dev_data_t {
+DECL|uart_cc32xx_driver_api|variable|uart_cc32xx_driver_api
+DECL|uart_cc32xx_err_check|function|static int uart_cc32xx_err_check(struct device *dev)
+DECL|uart_cc32xx_fifo_fill|function|static int uart_cc32xx_fifo_fill(struct device *dev, const uint8_t *tx_data, int size)
+DECL|uart_cc32xx_fifo_read|function|static int uart_cc32xx_fifo_read(struct device *dev, uint8_t *rx_data, const int size)
+DECL|uart_cc32xx_init|function|static int uart_cc32xx_init(struct device *dev)
+DECL|uart_cc32xx_irq_callback_set|function|static void uart_cc32xx_irq_callback_set(struct device *dev, uart_irq_callback_t cb)
+DECL|uart_cc32xx_irq_err_disable|function|static void uart_cc32xx_irq_err_disable(struct device *dev)
+DECL|uart_cc32xx_irq_err_enable|function|static void uart_cc32xx_irq_err_enable(struct device *dev)
+DECL|uart_cc32xx_irq_is_pending|function|static int uart_cc32xx_irq_is_pending(struct device *dev)
+DECL|uart_cc32xx_irq_rx_disable|function|static void uart_cc32xx_irq_rx_disable(struct device *dev)
+DECL|uart_cc32xx_irq_rx_enable|function|static void uart_cc32xx_irq_rx_enable(struct device *dev)
+DECL|uart_cc32xx_irq_rx_ready|function|static int uart_cc32xx_irq_rx_ready(struct device *dev)
+DECL|uart_cc32xx_irq_tx_disable|function|static void uart_cc32xx_irq_tx_disable(struct device *dev)
+DECL|uart_cc32xx_irq_tx_empty|function|static int uart_cc32xx_irq_tx_empty(struct device *dev)
+DECL|uart_cc32xx_irq_tx_enable|function|static void uart_cc32xx_irq_tx_enable(struct device *dev)
+DECL|uart_cc32xx_irq_tx_ready|function|static int uart_cc32xx_irq_tx_ready(struct device *dev)
+DECL|uart_cc32xx_irq_update|function|static int uart_cc32xx_irq_update(struct device *dev)
+DECL|uart_cc32xx_isr|function|static void uart_cc32xx_isr(void *arg)
+DECL|uart_cc32xx_poll_in|function|static int uart_cc32xx_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_cc32xx_poll_out|function|static unsigned char uart_cc32xx_poll_out(struct device *dev, unsigned char c)
