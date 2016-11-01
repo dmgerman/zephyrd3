34fc92cb1150428804af61396c72fcf47a13aedb
Andrew Boie <andrew.p.boie@intel.com>
2016-11-01 13:37:45 -0700

    tests: benchmark: enable for V2 kernel
    Not completely ported to V2 APIs yet, but enough that we pull in
    all the kernel.h APIs for the microkernel test, bypassing the legacy
    layer.
    
    We pull in a lot more than the legacy test did, so results are not
    directly comparable.
    
    Change-Id: I35e74bfd1aa15ee59951f68fb7dd8d811619c3f8
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 25ce056279faf7db2a870e9045210f777d217efa

--- tests/legacy/benchmark/footprint/microkernel/src/microkernel_footprint.c
+++ tests/legacy/benchmark/footprint/microkernel/src/microkernel_footprint.c
@@ -6,5 +6,6 @@ DECL|TEST_SOFT_INT|macro|TEST_SOFT_INT
 DECL|dummyIsr|function|void dummyIsr(void *unused)
 DECL|fgTaskEntry|function|void fgTaskEntry(void)
 DECL|func_array|variable|func_array
+DECL|func_array|variable|func_array
 DECL|i|variable|i
 DECL|pfunc|typedef|typedef void* (*pfunc) (void*);
