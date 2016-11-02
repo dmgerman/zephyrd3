19b54e1c7b6f0be20900cde3f54d874665fc71e2
LIU, Sharron <sharron.liu@intel.com>
2016-11-02 11:21:07 +0800

    tests: kernel: added test_thread_init
    This test case is under test set "lifecycle" against
    https://www.zephyrproject.org/doc/kernel_v2/threads/lifecycle.html
    
    Change-Id: I0d43c1a1798411c786efc45d76e6fd7c024c47dd
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 638066757f6f79d1075fe823274215d8ea232107

---
+++ tests/kernel/threads_lifecycle/thread_init/src/test_thread_init.c
@@ -0,0 +1,30 @@
+DECL|INIT_COOP_DELAY|macro|INIT_COOP_DELAY
+DECL|INIT_COOP_OPTION|macro|INIT_COOP_OPTION
+DECL|INIT_COOP_P1|macro|INIT_COOP_P1
+DECL|INIT_COOP_P2|macro|INIT_COOP_P2
+DECL|INIT_COOP_P3|macro|INIT_COOP_P3
+DECL|INIT_COOP_PRIO|macro|INIT_COOP_PRIO
+DECL|INIT_COOP_STACK_SIZE|macro|INIT_COOP_STACK_SIZE
+DECL|INIT_PREEMPT_DELAY|macro|INIT_PREEMPT_DELAY
+DECL|INIT_PREEMPT_OPTION|macro|INIT_PREEMPT_OPTION
+DECL|INIT_PREEMPT_P1|macro|INIT_PREEMPT_P1
+DECL|INIT_PREEMPT_P2|macro|INIT_PREEMPT_P2
+DECL|INIT_PREEMPT_P3|macro|INIT_PREEMPT_P3
+DECL|INIT_PREEMPT_PRIO|macro|INIT_PREEMPT_PRIO
+DECL|INIT_PREEMPT_STACK_SIZE|macro|INIT_PREEMPT_STACK_SIZE
+DECL|expected|variable|expected
+DECL|init_delay|member|int32_t init_delay;
+DECL|init_p1|member|void *init_p1;
+DECL|init_p2|member|void *init_p2;
+DECL|init_p3|member|void *init_p3;
+DECL|init_prio|member|int init_prio;
+DECL|stack_coop|variable|stack_coop
+DECL|stack_preempt|variable|stack_preempt
+DECL|stack_size|variable|stack_size
+DECL|t_create|variable|t_create
+DECL|test_kdefine_coop_thread|function|void test_kdefine_coop_thread(void)
+DECL|test_kdefine_preempt_thread|function|void test_kdefine_preempt_thread(void)
+DECL|test_kinit_coop_thread|function|void test_kinit_coop_thread(void)
+DECL|test_kinit_preempt_thread|function|void test_kinit_preempt_thread(void)
+DECL|thread_data|struct|static struct thread_data{
+DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
