cbcea33be56ed684176381d9daca2b852e55574b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-11-02 15:05:57 +0100

    net: ieee802154: Add debug output to radio strategy drivers
    Change-Id: Iffd628c0ae8b472f93f2f24a187b3fc6e563c6e8
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: ab32634b513af2fcceed3e5b1e02d037e0df62ff

--- net/yaip/l2/ieee802154/ieee802154_radio_csma_ca.c
+++ net/yaip/l2/ieee802154/ieee802154_radio_csma_ca.c
@@ -1,3 +1,5 @@
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|csma_ca_radio_handle_ack|function|static enum net_verdict csma_ca_radio_handle_ack(struct net_if *iface, struct net_buf *buf)
 DECL|csma_ca_radio_send|function|static int csma_ca_radio_send(struct net_if *iface, struct net_buf *buf)
 DECL|csma_ca_tx_fragment|function|static inline int csma_ca_tx_fragment(struct net_if *iface, struct net_buf *buf)
