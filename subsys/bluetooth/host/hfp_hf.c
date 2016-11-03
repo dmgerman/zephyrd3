de2eb9936166cabd1d5cfd110d92a1a235dea9b8
Sathish Narasimman <sathish.narasimman@intel.com>
2016-11-03 13:07:20 +0530

    Bluetooth: HFP HF: SLC connection-Send/Parse BRSF
    Service Level Connection(SLC) Initialization part. Sending AT+BRSF
    and parsing its response.
    
    < ACL Data TX: Handle 256 flags 0x00 dlen 20
          Channel: 75 len 16 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x09 cr 0 dlci 0x02
             Control: 0xef poll/final 0
             Length: 12
             FCS: 0x40
            41 54 2b 42 52 53 46 3d 31 34 38 0d 40           AT+BRSF=20.
    
    .
    > ACL Data RX: Handle 256 flags 0x02 dlen 23
          Channel: 64 len 19 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x0b cr 1 dlci 0x02
             Control: 0xff poll/final 1
             Length: 14
             FCS: 0x86
             Credits: 4
            0d 0a 2b 42 52 53 46 3a 20 38 37 31 0d 0a 86     ..+BRSF: 871.
    > ACL Data RX: Handle 256 flags 0x02 dlen 14
          Channel: 64 len 10 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x0b cr 1 dlci 0x02
             Control: 0xef poll/final 0
             Length: 6
             FCS: 0x9a
            0d 0a 4f 4b 0d 0a 9a                             ..OK...
    
    Change-Id: I51581928d479ea4229b32a07cbea86c1f6fe09c8
    Signed-off-by: Sathish Narasimman <sathish.narasimman@intel.com>
    
    Former-commit-id: c719bfb6897b88220059f24d70eb7be5921e71af

--- subsys/bluetooth/host/hfp_hf.c
+++ subsys/bluetooth/host/hfp_hf.c
@@ -1,11 +1,17 @@
 DECL|BT_DBG|macro|BT_DBG
 DECL|BT_DBG|macro|BT_DBG
+DECL|brsf_finish|function|int brsf_finish(struct at_client *hf_at, struct net_buf *buf,enum at_result result)
+DECL|brsf_handle|function|int brsf_handle(struct at_client *hf_at)
+DECL|brsf_resp|function|int brsf_resp(struct at_client *hf_at, struct net_buf *buf)
 DECL|bt_hfp_hf_accept|function|static int bt_hfp_hf_accept(struct bt_conn *conn, struct bt_rfcomm_dlc **dlc)
 DECL|bt_hfp_hf_pool|variable|bt_hfp_hf_pool
 DECL|bt_hfp_hf_register|function|int bt_hfp_hf_register(struct bt_hfp_hf_cb *cb)
 DECL|bt_hf|variable|bt_hf
-DECL|hf_buf|variable|hf_buf
+DECL|hf_fifo|variable|hf_fifo
+DECL|hf_slc_error|function|void hf_slc_error(struct at_client *hf_at)
+DECL|hf_slc_establish|function|int hf_slc_establish(struct bt_hfp_hf *hf)
 DECL|hfp_hf_connected|function|static void hfp_hf_connected(struct bt_rfcomm_dlc *dlc)
 DECL|hfp_hf_disconnected|function|static void hfp_hf_disconnected(struct bt_rfcomm_dlc *dlc)
 DECL|hfp_hf_init|function|static void hfp_hf_init(void)
 DECL|hfp_hf_recv|function|static void hfp_hf_recv(struct bt_rfcomm_dlc *dlc, struct net_buf *buf)
+DECL|hfp_hf_send_cmd|function|int hfp_hf_send_cmd(struct bt_hfp_hf *hf, at_resp_cb_t resp, at_finish_cb_t finish, const char *format, ...)
