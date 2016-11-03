8a540fdb003d52431965b39754989c6d52e2a57e
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-03 14:44:44 +0200

    net: Add util to send ICMPv6 Echo-Request packet
    Add function to send IPv6 ping message to peer.
    
    Change-Id: If7517beefe109f5f35cba82bc175da5a22cdb1d1
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: f8a8cac3fe29ea85503a26bb4ff7cf5dc2fa0707

--- net/yaip/icmpv6.c
+++ net/yaip/icmpv6.c
@@ -6,5 +6,6 @@ DECL|handlers|variable|handlers
 DECL|net_icmpv6_init|function|void net_icmpv6_init(void)
 DECL|net_icmpv6_input|function|enum net_verdict net_icmpv6_input(struct net_buf *buf, uint16_t len, uint8_t type, uint8_t code)
 DECL|net_icmpv6_register_handler|function|void net_icmpv6_register_handler(struct net_icmpv6_handler *handler)
+DECL|net_icmpv6_send_echo_request|function|int net_icmpv6_send_echo_request(struct net_if *iface, struct in6_addr *dst, uint16_t identifier, uint16_t sequence)
 DECL|net_icmpv6_send_error|function|int net_icmpv6_send_error(struct net_buf *orig, uint8_t type, uint8_t code, uint32_t param)
 DECL|setup_ipv6_header|function|static inline void setup_ipv6_header(struct net_buf *buf, uint8_t extra_len, uint8_t hop_limit, uint8_t icmp_type, uint8_t icmp_code)
