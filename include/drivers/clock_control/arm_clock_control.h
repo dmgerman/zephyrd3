0ddb8f25f29b8fd8f1bffb9d462dc225e1f3c0b5
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2016-11-03 11:12:06 +0000

    clock_control: Add Beetle clock_control
    This patch adds the clock_control implementation for the ARM LTD
    Beetle platform.
    
    The main features enabled are:
    * Clock on and off in ACTIVE, SLEEP and DEEPSLEEP mode.
    * PLL support (freq: 12, 36, 48 Mhz).
    
    The integration with the existing drivers will be done in future
    patches.
    
    Jira: ZEP-1300
    Change-Id: I07cb2325275bd86a036e8e24aeb7bbf2c6176a93
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>
    
    Former-commit-id: 6be2a81ae48d6fbdce7c5e9a2a0343c9f3cc5652

---
+++ include/drivers/clock_control/arm_clock_control.h
@@ -0,0 +1,12 @@
+DECL|CMSDK_AHB|enumerator|CMSDK_AHB = 0,
+DECL|CMSDK_APB|enumerator|CMSDK_APB,
+DECL|SOC_ACTIVE|enumerator|SOC_ACTIVE = 0,
+DECL|SOC_DEEPSLEEP|enumerator|SOC_DEEPSLEEP,
+DECL|SOC_SLEEP|enumerator|SOC_SLEEP,
+DECL|_ARM_CLOCK_CONTROL_H_|macro|_ARM_CLOCK_CONTROL_H_
+DECL|arm_bus_type_t|enum|enum arm_bus_type_t {
+DECL|arm_clock_control_t|struct|struct arm_clock_control_t {
+DECL|arm_soc_state_t|enum|enum arm_soc_state_t {
+DECL|bus|member|enum arm_bus_type_t bus;
+DECL|device|member|uint32_t device;
+DECL|state|member|enum arm_soc_state_t state;
