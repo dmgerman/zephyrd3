0332a17fac96d2e68863c85d5ae39afc73b9bfc1
Allan Stephens <allan.stephens@windriver.com>
2016-11-03 13:54:53 -0500

    unified: Add missing arguments to K_TIMER_DEFINE()
    It is now possible to specify the expiry and stop functions
    of a statically-defined timer, just as can be done for a
    dynamically-defined timer.
    
    [Part of fix to ZEP-1186]
    
    Change-Id: Ibb9096f3fdafdc6c904184587f86ecd52accdd66
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 1342adbd636d9a3689b546f4ff41ad92402a1869

--- kernel/unified/timer.c
+++ kernel/unified/timer.c
@@ -1,3 +1,4 @@
+DECL|_timer_expiration_handler|function|void _timer_expiration_handler(struct _timeout *t)
 DECL|_trace_list_k_timer|variable|_trace_list_k_timer
 DECL|init_timer_module|function|static int init_timer_module(struct device *dev)
 DECL|k_timer_init|function|void k_timer_init(struct k_timer *timer, void (*expiry_fn)(struct k_timer *), void (*stop_fn)(struct k_timer *))
@@ -6,4 +7,3 @@ DECL|k_timer_start|function|void k_timer_start(struct k_timer *timer, int32_t du
 DECL|k_timer_status_get|function|uint32_t k_timer_status_get(struct k_timer *timer)
 DECL|k_timer_status_sync|function|uint32_t k_timer_status_sync(struct k_timer *timer)
 DECL|k_timer_stop|function|void k_timer_stop(struct k_timer *timer)
-DECL|timer_expiration_handler|function|static void timer_expiration_handler(struct _timeout *t)
