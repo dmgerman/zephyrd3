abfdaf08eef78cabcfb8a2eac62195bd59aa93e3
Anas Nashif <anas.nashif@intel.com>
2016-11-04 08:09:17 -0400

    shell: split kernel shell into separate module
    Add kernel functions to kernel module and make it part
    of the shell sample.
    
    Change-Id: If5e8ff8ce7b8edbbb8d62509964700b007eaf88b
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: b27d3a8a60c10460b818ff4f6e6509e35a9b28a8

--- drivers/console/shells/shell_service.c
+++ drivers/console/shells/shell_service.c
@@ -1,5 +1,2 @@
-DECL|SHELL_KERNEL|macro|SHELL_KERNEL
 DECL|SHELL_PROMPT|macro|SHELL_PROMPT
-DECL|kernel_commands|variable|kernel_commands
-DECL|shell_cmd_version|function|static int shell_cmd_version(int argc, char *argv[])
 DECL|shell_run|function|int shell_run(struct device *dev)
--- samples/shell/src/main.c
+++ samples/shell/src/main.c
@@ -3,7 +3,5 @@ DECL|DEVICE_NAME|macro|DEVICE_NAME
 DECL|MY_SHELL_MODULE|macro|MY_SHELL_MODULE
 DECL|commands|variable|commands
 DECL|main|function|void main(void)
-DECL|shell_cmd_cycles|function|static int shell_cmd_cycles(int argc, char *argv[])
 DECL|shell_cmd_params|function|static int shell_cmd_params(int argc, char *argv[])
 DECL|shell_cmd_ping|function|static int shell_cmd_ping(int argc, char *argv[])
-DECL|shell_cmd_uptime|function|static int shell_cmd_uptime(int argc, char *argv[])
