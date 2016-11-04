5cf67e28303d5262af0fba941ce28d0b3032ae84
Leandro Pereira <leandro.pereira@intel.com>
2016-11-04 14:28:21 -0700

    net: Allow changing the callback of a connection
    For certain scenarios, such as handling TCP active close, where the
    state group {FIN_WAIT_1, FIN_WAIT_2, CLOSING, TIME_WAIT} can be reached
    from more than one state (ESTABLISHED and SYN_RCVD respectively), it's
    cleaner to handle all the transitions inside a single callback
    function.
    
    Change-Id: I3d4d210dd667fef5cf81d5174d626adbf8675019
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 79b9e1413db8aac6d6adb5da469637ae1fb724b3

--- net/yaip/connection.c
+++ net/yaip/connection.c
@@ -29,6 +29,7 @@ DECL|get_conn|function|static inline int32_t get_conn(enum net_ip_protocol proto
 DECL|idx|member|int32_t idx;
 DECL|ipv4_to_hash|function|static inline uint16_t ipv4_to_hash(struct in_addr *addr)
 DECL|ipv6_to_hash|function|static inline uint16_t ipv6_to_hash(struct in6_addr *addr)
+DECL|net_conn_change_callback|function|int net_conn_change_callback(struct net_conn_handle *handle, net_conn_cb_t cb, void *user_data)
 DECL|net_conn_init|function|void net_conn_init(void)
 DECL|net_conn_input|function|enum net_verdict net_conn_input(enum net_ip_protocol proto, struct net_buf *buf)
 DECL|net_conn_register|function|int net_conn_register(enum net_ip_protocol proto, const struct sockaddr *remote_addr, const struct sockaddr *local_addr, uint16_t remote_port, uint16_t local_port,
