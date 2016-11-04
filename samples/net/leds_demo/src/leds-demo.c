550fcc3fa9792db123aa8988e2b6ff6c00d79cb0
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-11-04 18:38:40 -0200

    samples/net: Add a simple demo for the link-format feature
    This demo showcases how to expose resources that are exposed using the
    link-format feature.
    
    Change-Id: Iad9421082fae3212cae58e6d6e9b88fa170678b8
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 432c6b9997638e7243b73cabc0e4ade45c9619d7

---
+++ samples/net/leds_demo/src/leds-demo.c
@@ -0,0 +1,31 @@
+DECL|ALL_NODES_LOCAL_COAP_MCAST|macro|ALL_NODES_LOCAL_COAP_MCAST
+DECL|LED_GPIO_NAME|macro|LED_GPIO_NAME
+DECL|LED_GPIO_NAME|macro|LED_GPIO_NAME
+DECL|LED_PIN|macro|LED_PIN
+DECL|LED_PIN|macro|LED_PIN
+DECL|MY_COAP_PORT|macro|MY_COAP_PORT
+DECL|MY_IP6ADDR|macro|MY_IP6ADDR
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|context|variable|context
+DECL|dummy_attributes|variable|dummy_attributes
+DECL|dummy_get|function|static int dummy_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|dummy_path|variable|dummy_path
+DECL|fake_led|variable|fake_led
+DECL|join_coap_multicast_group|function|static bool join_coap_multicast_group(void)
+DECL|led0|variable|led0
+DECL|led_default_attributes|variable|led_default_attributes
+DECL|led_default_path|variable|led_default_path
+DECL|led_get|function|static int led_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|led_off|variable|led_off
+DECL|led_on|variable|led_on
+DECL|led_post|function|static int led_post(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|led_put|function|static int led_put(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|led_toggle_off|variable|led_toggle_off
+DECL|led_toggle_on|variable|led_toggle_on
+DECL|main|function|void main(void)
+DECL|read_led|function|static bool read_led(void)
+DECL|resources|variable|resources
+DECL|udp_receive|function|static void udp_receive(struct net_context *context,struct net_buf *buf, int status, void *user_data)
+DECL|write_led|function|static void write_led(bool led)
