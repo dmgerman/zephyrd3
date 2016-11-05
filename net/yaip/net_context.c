4f2b04fa848b96ab87688899fdf12e0cf81dec1c
Leandro Pereira <leandro.pereira@intel.com>
2016-11-04 17:01:07 -0700

    net: tcp: Reduce some of the boilerplate to send control segments
    Move most of the code to send a control segment to a single function to
    avoid copies of boilerplate code in a few functions.
    
    Change-Id: Id2d4770b6bec3f10b4eb786af3eb2a25482d953d
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 2bfc42610df9fd43832401012d1dce0b55726c8a

--- net/yaip/net_context.c
+++ net/yaip/net_context.c
@@ -25,6 +25,7 @@ DECL|net_tcp_print_send_info|macro|net_tcp_print_send_info
 DECL|net_tcp_print_send_info|macro|net_tcp_print_send_info
 DECL|packet_received|function|enum net_verdict packet_received(struct net_conn *conn, struct net_buf *buf, void *user_data)
 DECL|send_ack|function|static inline int send_ack(struct net_context *context, struct sockaddr *remote)
+DECL|send_control_segment|function|static inline int send_control_segment(struct net_context *context, const struct sockaddr *remote, int flags, const char *msg)
 DECL|send_data|function|static int send_data(struct net_context *context, struct net_buf *buf, net_context_send_cb_t cb, int32_t timeout, void *token,
 DECL|send_fin_ack|function|static inline int send_fin_ack(struct net_context *context, struct sockaddr *remote)
 DECL|send_fin_if_active_close|function|static bool send_fin_if_active_close(struct net_context *context)
