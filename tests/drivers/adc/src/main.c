fd331a28bfd3bb9447e0ba0847b98b3f019a83ef
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2016-11-04 23:20:53 -0700

    tests/drivers/adc: move to ztest to actually test
    This TC is only exercising the API, as we don't have a feedback loop
    mechanism to verify whichever values are fed to the ADC.
    
    Fixed the loop to complete after 10 runs; on each run, print the
    values and actually report the difference between them. With no inputs
    connected (aka: floating), they should be reporting noise relatively
    close to the previous reading, so we might want to use this delta as a
    testing pattern (assert if the delta is higher than some value, but
    I've seen variations as high as 40M units). For now, the test is just
    happy with being able to read them.
    
    The buffer has been re-typed to uint32_t so we can iterate over it
    without casting tricks -- it requires then only a single cast when
    initializing sample.buffer (which shall be a void* anyway).
    
    Duplicated the buffer, so we can flip/flop between two buffers to
    compare against the entries read in the previous run.
    
    v4: fixed missed warnings
    
    Change-Id: If6b48b92231007202d74f5c042f6d0cf3fdcb60a
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    
    Former-commit-id: 9e1df6f21f5e362513d7f3817965812c5d4c4f60

--- tests/drivers/adc/src/main.c
+++ tests/drivers/adc/src/main.c
@@ -2,8 +2,10 @@ DECL|ADC_DEVICE_NAME|macro|ADC_DEVICE_NAME
 DECL|BUFFER_SIZE|macro|BUFFER_SIZE
 DECL|CHANNEL|macro|CHANNEL
 DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|_print_sample_in_hex|function|static void _print_sample_in_hex(uint8_t *buf, uint32_t length)
-DECL|main|function|void main(void)
+DECL|_abs|function|static long _abs(long x)
+DECL|_print_sample_in_hex|function|static void _print_sample_in_hex(const uint32_t *buf, uint32_t length)
+DECL|adc_test|function|static void adc_test(void)
 DECL|sample|variable|sample
 DECL|seq_buffer|variable|seq_buffer
 DECL|table|variable|table
+DECL|test_main|function|void test_main(void)
