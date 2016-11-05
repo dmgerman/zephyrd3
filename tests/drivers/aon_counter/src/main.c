304058a4f9dbcb8ddafccf0129d65dd391bb8a0a
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2016-11-04 23:05:56 -0700

    tests/drivers/aon_counter: make a ztest testcase
    Reorganize the code tree so no forward declarations are needed
    (basically, move main() to the bottom).
    
    Retag as not build only so it can be run on Quark*
    
    Convert to actually verify counter readouts are increasing /
    decreasing over busy empty loops and the alarm is being called.
    
    Change-Id: I746efe595b8d1ac4471dccc2e87f8b36f5cc8ebe
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    
    Former-commit-id: 5b7bfdaf485260d0eef9252c1b3a92b85c40c49c

--- tests/drivers/aon_counter/src/main.c
+++ tests/drivers/aon_counter/src/main.c
@@ -1,4 +1,5 @@
+DECL|aonpt_example_callback_was_called|variable|aonpt_example_callback_was_called
 DECL|aonpt_example_callback|function|static void aonpt_example_callback(struct device *dev, void *user_data)
 DECL|free_running_counter_example|function|static void free_running_counter_example(void)
-DECL|main|function|void main(void)
 DECL|periodic_timer_example|function|static void periodic_timer_example(void)
+DECL|test_main|function|void test_main(void)
