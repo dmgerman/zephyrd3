1dde5f1bc5741dabaeec34c74926c5ade2f4741f
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2016-11-07 12:31:01 +0200

    wpanusb: Decrease amount of debugs
    Print packet hexdump only when VERBOSE_DEBUG is enabled and remove
    debug print in empty functions.
    
    Change-Id: Ifd3ae59001233f0237c4b7575bd92607c93e2ea0
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: c275f0756ea5e423502c5e53b7cc2cc197600d08

--- samples/net/wpanusb/src/wpanusb.c
+++ samples/net/wpanusb/src/wpanusb.c
@@ -21,6 +21,7 @@ DECL|WPANUSB_SUBCLASS|macro|WPANUSB_SUBCLASS
 DECL|__dev|variable|__dev
 DECL|buffer|variable|buffer
 DECL|hexdump|function|static void hexdump(const char *str, const uint8_t *packet, size_t length)
+DECL|hexdump|macro|hexdump
 DECL|ieee802154_dev|variable|ieee802154_dev
 DECL|ieee802154_init|function|void ieee802154_init(struct net_if *iface)
 DECL|ieee802154_radio_handle_ack|function|extern enum net_verdict ieee802154_radio_handle_ack(struct net_if *iface, struct net_buf *buf)
