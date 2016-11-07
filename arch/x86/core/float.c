ae0f698cae0dce5457ff0b2292c2f5830cc5d34d
Allan Stephens <allan.stephens@windriver.com>
2016-11-07 08:55:13 -0600

    kernel: Clean up of x86 floating point code
    Updates x86 floating point support to reflect changes that have
    been made in recent months.
    
    * Many, many, many cosmetic changes (mostly revisions to comments).
    
    * Elimination of unnecessary function aliases that were needed
      to support the task and fiber versions of certain APIs.
    
    * Elimination of run-time code to enable a thread's "FP regs"
      option bit if the "SSE regs" option bit was set. The kernel
      now recognizes that the thread is using the FPU as long as
      either option bit is set. (If the thread has both option bits
      enabled this is the same as if only the "SSE regs" bit is set.)
    
    Change-Id: Ic12abc54b6fa78921749b546d8debf23e7ad232d
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: bce8fbb61ef2c844c061f7bad3c41bfdf6479d4c

--- arch/x86/core/float.c
+++ arch/x86/core/float.c
@@ -1,5 +1,5 @@
 DECL|_FpCtxInit|function|static inline void _FpCtxInit(struct tcs *tcs)
 DECL|_FpCtxSave|function|static void _FpCtxSave(struct tcs *tcs)
-DECL|_FpDisable|function|void _FpDisable(struct tcs *tcs)
-DECL|_FpEnable|function|void _FpEnable(struct tcs *tcs, unsigned int options)
-DECL|_FpNotAvailableExcHandler|function|void _FpNotAvailableExcHandler(NANO_ESF * pEsf)
+DECL|_FpNotAvailableExcHandler|function|void _FpNotAvailableExcHandler(NANO_ESF *pEsf)
+DECL|k_float_disable|function|void k_float_disable(struct tcs *tcs)
+DECL|k_float_enable|function|void k_float_enable(struct tcs *tcs, unsigned int options)
--- arch/x86/include/asm_inline_gcc.h
+++ arch/x86/include/asm_inline_gcc.h
@@ -1,5 +1,7 @@
 DECL|EflagsGet|function|static inline unsigned int EflagsGet(void)
 DECL|_ASM_INLINE_GCC_H|macro|_ASM_INLINE_GCC_H
 DECL|_FpAccessDisable|function|static inline void _FpAccessDisable(void)
-DECL|_do_fp_ctx_init|function|static inline void _do_fp_ctx_init(int flags)
-DECL|_do_fp_ctx_save|function|static inline void _do_fp_ctx_save(int flags, void *preemp_float_reg)
+DECL|_do_fp_and_sse_regs_save|function|static inline void _do_fp_and_sse_regs_save(void *preemp_float_reg)
+DECL|_do_fp_regs_init|function|static inline void _do_fp_regs_init(void)
+DECL|_do_fp_regs_save|function|static inline void _do_fp_regs_save(void *preemp_float_reg)
+DECL|_do_sse_regs_init|function|static inline void _do_sse_regs_init(void)
