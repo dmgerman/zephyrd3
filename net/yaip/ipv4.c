c1ae6478ad5532e844e839d72b130f0a4245f2a4
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-07 13:37:33 +0200

    net: Add util to return IPv4 ANY address
    Add function that returns IPv4 any address (all bits are zero).
    
    Change-Id: I8816f7f264ad3a98fced7089f0b94220603c2291
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b30bb362160e41c4a2021bdbcf567c4458fc97ec

--- net/yaip/ipv4.c
+++ net/yaip/ipv4.c
@@ -2,3 +2,4 @@ DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|net_ipv4_create|function|struct net_buf *net_ipv4_create(struct net_context *context,struct net_buf *buf, const struct in_addr *addr)
 DECL|net_ipv4_finalize|function|struct net_buf *net_ipv4_finalize(struct net_context *context, struct net_buf *buf)
+DECL|net_ipv4_unspecified_address|function|const struct in_addr *net_ipv4_unspecified_address(void)
