bc5befb22f449f91150268d1bb92948a46c541db
Johan Hedberg <johan.hedberg@intel.com>
2016-11-08 10:21:37 +0200

    fs: Add simple shell support
    Add a simple shell that allows exploring the contents of the file
    system.
    
    Jira: ZEP-1235
    
    Change-Id: Iaa49f0be18980dd740e9552ddf4761196a818884
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 61cfaa1be1b01eeebcb83e12733c6f74afa5af38

---
+++ subsys/fs/shell.c
@@ -0,0 +1,6 @@
+DECL|MAX_PATH_LEN|macro|MAX_PATH_LEN
+DECL|cmd_cd|function|static int cmd_cd(int argc, char *argv[])
+DECL|cmd_ls|function|static int cmd_ls(int argc, char *argv[])
+DECL|cmd_pwd|function|static int cmd_pwd(int argc, char *argv[])
+DECL|cwd|variable|cwd
+DECL|fs_commands|variable|fs_commands
