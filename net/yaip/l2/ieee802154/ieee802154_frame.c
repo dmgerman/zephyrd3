dd14c0f2aff4a65ab89a2541dd34acba27121d44
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-11-08 19:27:04 +0100

    net: ieee802154: Create data frame after 6lo processing
    6lo compresses and fragments the result. Result is that we might end up
    with less fragments than earlier. So it's more logical to create the
    15.4 frame _after_ 6lo compression/fragmentation. However, since 6lo
    compresses the IPv6 header, it is not possible to use buffer's IPv6
    distant address directly. Instead, we copy it locally before 6lo, and
    use this copy to generate the 15.4 frames afterwards.
    
    Change-Id: I1c7b1a1702daffd6be4cacd15011c84f0beb4b2f
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 68bddda6de5ca79e37e575e9f49c3ee2e796933b

--- net/yaip/l2/ieee802154/ieee802154_frame.c
+++ net/yaip/l2/ieee802154/ieee802154_frame.c
@@ -1,15 +1,15 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|cfi_to_fs_settings|function|static inline bool cfi_to_fs_settings(enum ieee802154_cfi cfi, struct ieee802154_fcf_seq *fs, struct ieee802154_frame_params *params)
-DECL|data_addr_to_fs_settings|function|void data_addr_to_fs_settings(struct net_if *iface, struct net_buf *buf, struct ieee802154_fcf_seq *fs, struct ieee802154_frame_params *params)
+DECL|data_addr_to_fs_settings|function|void data_addr_to_fs_settings(struct net_if *iface, struct in6_addr *dst, struct ieee802154_fcf_seq *fs, struct ieee802154_frame_params *params)
 DECL|dbg_print_fs|macro|dbg_print_fs
 DECL|dbg_print_fs|macro|dbg_print_fs
 DECL|generate_addressing_fields|function|uint8_t *generate_addressing_fields(struct net_if *iface, struct ieee802154_fcf_seq *fs, struct ieee802154_frame_params *params, uint8_t *p_buf)
 DECL|generate_fcf_grounds|function|static inline struct ieee802154_fcf_seq *generate_fcf_grounds(uint8_t **p_buf, uint8_t ack)
-DECL|get_dst_addr_mode|function|get_dst_addr_mode(struct net_if *iface, struct net_buf *buf, struct net_nbr **nbr, bool *broadcast)
+DECL|get_dst_addr_mode|function|get_dst_addr_mode(struct net_if *iface, struct in6_addr *dst, struct net_nbr **nbr, bool *broadcast)
 DECL|ieee802154_compute_header_size|function|uint16_t ieee802154_compute_header_size(struct net_if *iface,struct in6_addr *dst)
 DECL|ieee802154_create_ack_frame|function|bool ieee802154_create_ack_frame(struct net_if *iface, struct net_buf *buf, uint8_t seq)
-DECL|ieee802154_create_data_frame|function|bool ieee802154_create_data_frame(struct net_if *iface, struct net_buf *buf, uint8_t *p_buf, uint8_t len)
+DECL|ieee802154_create_data_frame|function|bool ieee802154_create_data_frame(struct net_if *iface, struct in6_addr *dst, uint8_t *p_buf, uint8_t len)
 DECL|ieee802154_create_mac_cmd_frame|function|ieee802154_create_mac_cmd_frame(struct net_if *iface,enum ieee802154_cfi type, struct ieee802154_frame_params *params)
 DECL|ieee802154_validate_frame|function|bool ieee802154_validate_frame(uint8_t *buf, uint8_t length, struct ieee802154_mpdu *mpdu)
 DECL|mac_command_length|function|static inline uint8_t mac_command_length(enum ieee802154_cfi cfi)
