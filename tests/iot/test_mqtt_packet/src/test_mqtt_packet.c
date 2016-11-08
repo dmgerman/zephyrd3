6e31520d78974e3a3a26c7e96eeed0596e24a964
Flavio Santes <flavio.santes@intel.com>
2016-11-08 00:24:53 -0600

    iot/mqtt: Add test app for the MQTT packet handling routines
    The following routines are evaluated in this test case:
    
    - mqtt_pack_connect
    - mqtt_pack_disconnect
    - mqtt_pack_publish
    - mqtt_pack_subscribe
    - mqtt_pack_suback
    - mqtt_pack_pingreq
    - mqtt_pack_pingresp
    - mqtt_pack_puback
    - mqtt_pack_pubcomp
    - mqtt_pack_pubrec
    - mqtt_pack_pubrel
    - mqtt_pack_unsuback
    
    - mqtt_unpack_connect
    - mqtt_unpack_disconnect
    - mqtt_unpack_pingreq
    - mqtt_unpack_pingresp
    - mqtt_unpack_puback
    - mqtt_unpack_pubcomp
    - mqtt_unpack_pubrec
    - mqtt_unpack_pubrel
    - mqtt_unpack_unsuback
    
    Future patches will extend this test case to cover the whole MQTT
    packet library.
    
    Origin: Original
    
    Jira: ZEP-870
    
    Change-Id: I4fee403c24d1eb0428fb18b413763e28b6b726d1
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 88c160021cde5743124858766877c8b0838c7acb

---
+++ tests/iot/test_mqtt_packet/src/test_mqtt_packet.c
@@ -0,0 +1,88 @@
+DECL|BUF_SIZE|macro|BUF_SIZE
+DECL|MAX_TOPICS|macro|MAX_TOPICS
+DECL|RC_STR|macro|RC_STR
+DECL|buf_len|variable|buf_len
+DECL|buf|variable|buf
+DECL|connect1|variable|connect1
+DECL|connect2|variable|connect2
+DECL|connect3|variable|connect3
+DECL|connect4|variable|connect4
+DECL|connect5|variable|connect5
+DECL|connect6|variable|connect6
+DECL|connect7|variable|connect7
+DECL|disconnect1|variable|disconnect1
+DECL|elements|member|int elements;
+DECL|eval_buffers|function|int eval_buffers(uint8_t *buf, uint16_t buf_len, uint8_t *expected, uint16_t len)
+DECL|eval_fcn|member|int (*eval_fcn)(struct mqtt_test *);
+DECL|eval_msg_connect|function|static int eval_msg_connect(struct mqtt_test *mqtt_test)
+DECL|eval_msg_disconnect|function|static int eval_msg_disconnect(struct mqtt_test *mqtt_test)
+DECL|eval_msg_packet_id|function|int eval_msg_packet_id(struct mqtt_test *mqtt_test, enum mqtt_packet type)
+DECL|eval_msg_pingreq|function|static int eval_msg_pingreq(struct mqtt_test *mqtt_test)
+DECL|eval_msg_pingresp|function|static int eval_msg_pingresp(struct mqtt_test *mqtt_test)
+DECL|eval_msg_puback|function|static int eval_msg_puback(struct mqtt_test *mqtt_test)
+DECL|eval_msg_pubcomp|function|static int eval_msg_pubcomp(struct mqtt_test *mqtt_test)
+DECL|eval_msg_publish|function|static int eval_msg_publish(struct mqtt_test *mqtt_test)
+DECL|eval_msg_pubrec|function|static int eval_msg_pubrec(struct mqtt_test *mqtt_test)
+DECL|eval_msg_pubrel|function|static int eval_msg_pubrel(struct mqtt_test *mqtt_test)
+DECL|eval_msg_suback|function|static int eval_msg_suback(struct mqtt_test *mqtt_test)
+DECL|eval_msg_subscribe|function|static int eval_msg_subscribe(struct mqtt_test *mqtt_test)
+DECL|eval_msg_unsuback|function|static int eval_msg_unsuback(struct mqtt_test *mqtt_test)
+DECL|expected_len|member|uint16_t expected_len;
+DECL|expected|member|uint8_t *expected;
+DECL|items|member|int items;
+DECL|main|function|void main(void)
+DECL|mqtt_tests|variable|mqtt_tests
+DECL|mqtt_test|struct|struct mqtt_test {
+DECL|msg_connect1|variable|msg_connect1
+DECL|msg_connect2|variable|msg_connect2
+DECL|msg_connect3|variable|msg_connect3
+DECL|msg_connect4|variable|msg_connect4
+DECL|msg_connect5|variable|msg_connect5
+DECL|msg_connect6|variable|msg_connect6
+DECL|msg_connect7|variable|msg_connect7
+DECL|msg_pkt_id|struct|struct msg_pkt_id {
+DECL|msg_puback1|variable|msg_puback1
+DECL|msg_pubcomp1|variable|msg_pubcomp1
+DECL|msg_publish1|variable|msg_publish1
+DECL|msg_publish2|variable|msg_publish2
+DECL|msg_publish3|variable|msg_publish3
+DECL|msg_publish4|variable|msg_publish4
+DECL|msg_pubrec1|variable|msg_pubrec1
+DECL|msg_pubrel1|variable|msg_pubrel1
+DECL|msg_suback1|variable|msg_suback1
+DECL|msg_suback2|variable|msg_suback2
+DECL|msg_suback3|variable|msg_suback3
+DECL|msg_suback|struct|struct msg_suback {
+DECL|msg_subscribe1|variable|msg_subscribe1
+DECL|msg_subscribe2|variable|msg_subscribe2
+DECL|msg_subscribe3|variable|msg_subscribe3
+DECL|msg_subscribe|struct|struct msg_subscribe {
+DECL|msg_unsuback1|variable|msg_unsuback1
+DECL|msg|member|void *msg;
+DECL|pingreq1|variable|pingreq1
+DECL|pingresp1|variable|pingresp1
+DECL|pkt_id|member|uint16_t pkt_id;
+DECL|pkt_id|member|uint16_t pkt_id;
+DECL|pkt_id|member|uint16_t pkt_id;
+DECL|print_array|function|static void print_array(uint8_t *a, uint16_t size)
+DECL|puback1|variable|puback1
+DECL|pubcomp1|variable|pubcomp1
+DECL|publish1|variable|publish1
+DECL|publish2|variable|publish2
+DECL|publish3|variable|publish3
+DECL|publish4|variable|publish4
+DECL|pubrec1|variable|pubrec1
+DECL|pubrel1|variable|pubrel1
+DECL|qos|member|enum mqtt_qos qos[MAX_TOPICS];
+DECL|qos|member|enum mqtt_qos qos[MAX_TOPICS];
+DECL|run_tests|function|static int run_tests(void)
+DECL|suback1|variable|suback1
+DECL|suback2|variable|suback2
+DECL|suback3|variable|suback3
+DECL|subscribe1|variable|subscribe1
+DECL|subscribe2|variable|subscribe2
+DECL|subscribe3|variable|subscribe3
+DECL|test_name|member|const char *test_name;
+DECL|test_strlen|function|static size_t test_strlen(const char *str)
+DECL|topics|member|const char *topics[MAX_TOPICS];
+DECL|unsuback1|variable|unsuback1
