57f90d9a625b2e6f065a00b61d8f6dc14a34a497
Baohong Liu <baohong.liu@intel.com>
2016-11-08 14:58:07 -0800

    samples: spi_fram: add spi FRAM sample app
    Add a sample app to demonstrate the usage of Fujitsu SPI
    FRAM.
    
    It uses SPI 1 on quark spi controller and gpio 20 as CS.
    
    Change-Id: I30c83bdd8312ca0cab7a62e397c0cb4ade0821cb
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 933e34621b982fe9716ec46f0b66d528e587807e

---
+++ samples/drivers/spi_fujitsu_fram/src/main.c
@@ -0,0 +1,9 @@
+DECL|MAX_DATA_LENGTH|macro|MAX_DATA_LENGTH
+DECL|MB85RS64V_MANUFACTURER_ID|macro|MB85RS64V_MANUFACTURER_ID
+DECL|cmp_data|variable|cmp_data
+DECL|data|variable|data
+DECL|main|function|void main(void)
+DECL|mb85rs64v_read_id|function|int mb85rs64v_read_id(struct device *dev)
+DECL|read_bytes|function|int read_bytes(struct device *dev, uint16_t addr, uint8_t *data, uint32_t num_bytes)
+DECL|spi_buffer|variable|spi_buffer
+DECL|write_bytes|function|int write_bytes(struct device *dev, uint16_t addr,uint8_t *data, uint32_t num_bytes)
