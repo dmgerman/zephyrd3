dd9fb1802b1b2a0cf9c735bd66e2153343187134
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-11-09 17:16:31 +0100

    net: mgmt: Using unified kernel API
    Dropping legacy API.
    
    Change-Id: Ifcada6513cf57bf2b73809bd9ed886f2c623baab
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: bd293e4dd94933d90cd21f06cfc5d13bfa224749

--- net/yaip/net_mgmt.c
+++ net/yaip/net_mgmt.c
@@ -9,13 +9,13 @@ DECL|in_event|variable|in_event
 DECL|mgmt_add_event_mask|function|static inline void mgmt_add_event_mask(uint32_t event_mask)
 DECL|mgmt_clean_event|function|static inline void mgmt_clean_event(struct mgmt_event_entry *mgmt_event)
 DECL|mgmt_event_entry|struct|struct mgmt_event_entry {
-DECL|mgmt_fiber_stack|variable|mgmt_fiber_stack
-DECL|mgmt_fiber|function|static void mgmt_fiber(void)
 DECL|mgmt_is_event_handled|function|static inline bool mgmt_is_event_handled(uint32_t mgmt_event)
 DECL|mgmt_pop_event|function|static inline struct mgmt_event_entry *mgmt_pop_event(void)
 DECL|mgmt_push_event|function|static inline void mgmt_push_event(uint32_t mgmt_event, struct net_if *iface)
 DECL|mgmt_rebuild_global_event_mask|function|static inline void mgmt_rebuild_global_event_mask(void)
 DECL|mgmt_run_callbacks|function|static inline void mgmt_run_callbacks(struct mgmt_event_entry *mgmt_event)
+DECL|mgmt_stack|variable|mgmt_stack
+DECL|mgmt_thread|function|static void mgmt_thread(void)
 DECL|net_mgmt_add_event_callback|function|void net_mgmt_add_event_callback(struct net_mgmt_event_callback *cb)
 DECL|net_mgmt_del_event_callback|function|void net_mgmt_del_event_callback(struct net_mgmt_event_callback *cb)
 DECL|net_mgmt_event_init|function|void net_mgmt_event_init(void)
