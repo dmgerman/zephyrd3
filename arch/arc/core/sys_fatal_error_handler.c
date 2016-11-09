5c8454d575f0a940f07643ec84a7c870885d3c39
Andrew Boie <andrew.p.boie@intel.com>
2016-11-09 11:10:21 -0800

    arches: use new kernel APIs
    Change-Id: I4b6f5264d5295ebf4278991a1f4e2141bef6602f
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 56f561e15e7f2a2263582ebe82f06b5f8473364f

--- arch/arc/core/sys_fatal_error_handler.c
+++ arch/arc/core/sys_fatal_error_handler.c
@@ -1,3 +1,1 @@
-DECL|PRINTK|macro|PRINTK
-DECL|PRINTK|macro|PRINTK
-DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF * pEsf)
+DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
