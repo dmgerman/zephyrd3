b0a479d2b46df6340aa7134e730dc13d6fb5d72c
Baohong Liu <baohong.liu@intel.com>
2016-11-10 11:57:13 -0800

    tests: adc: update to unified kernel
    Use k_sleep instead of nano timer.
    
    Change-Id: Iba88b93c71dedd60bdbb842fd8e44a55593f54bf
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 5e2f899bada75aa8e6d28a049185eb8e8352d662

--- tests/drivers/adc/src/main.c
+++ tests/drivers/adc/src/main.c
@@ -1,7 +1,6 @@
 DECL|ADC_DEVICE_NAME|macro|ADC_DEVICE_NAME
 DECL|BUFFER_SIZE|macro|BUFFER_SIZE
 DECL|CHANNEL|macro|CHANNEL
-DECL|SLEEPTICKS|macro|SLEEPTICKS
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|_print_sample_in_hex|function|static void _print_sample_in_hex(uint8_t *buf, uint32_t length)
 DECL|main|function|void main(void)
