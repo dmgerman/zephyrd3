a8d0daa5bff593afff9e0afb9d7362b758faedd2
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2016-11-10 11:26:20 +0200

    Bluetooth: ATT: Fix using nano_delayed_work API
    This fixes warnings related to the use of nano_work and
    nano_delayed_work in ATT layer.
    
    Note that k_delayed_work takes a timeout in miliseconds rather than in
    ticks thus the timeouts values changes.
    
    Change-Id: I14d8438c1537febcb7768ef2934042ce38682739
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: ec57d7cdb21746b87a696c61c6f1fed8c462a389

--- subsys/bluetooth/host/att.c
+++ subsys/bluetooth/host/att.c
@@ -48,7 +48,7 @@ DECL|att_req_destroy|function|static void att_req_destroy(struct bt_att_req *req
 DECL|att_reset|function|static void att_reset(struct bt_att *att)
 DECL|att_send_req|function|static int att_send_req(struct bt_att *att, struct bt_att_req *req)
 DECL|att_signed_write_cmd|function|static uint8_t att_signed_write_cmd(struct bt_att *att, struct net_buf *buf)
-DECL|att_timeout|function|static void att_timeout(struct nano_work *work)
+DECL|att_timeout|function|static void att_timeout(struct k_work *work)
 DECL|att_write_cmd|function|static uint8_t att_write_cmd(struct bt_att *att, struct net_buf *buf)
 DECL|att_write_req|function|static uint8_t att_write_req(struct bt_att *att, struct net_buf *buf)
 DECL|att_write_rsp|function|static uint8_t att_write_rsp(struct bt_conn *conn, uint8_t op, uint8_t rsp, uint16_t handle, uint16_t offset, const void *value, uint8_t len)
@@ -129,7 +129,7 @@ DECL|rsp|member|struct bt_att_read_group_rsp *rsp;
 DECL|rsp|member|struct bt_att_read_rsp *rsp;
 DECL|rsp|member|struct bt_att_read_type_rsp *rsp;
 DECL|send_err_rsp|function|static void send_err_rsp(struct bt_conn *conn, uint8_t req, uint16_t handle, uint8_t err)
-DECL|timeout_work|member|struct nano_delayed_work timeout_work;
+DECL|timeout_work|member|struct k_delayed_work timeout_work;
 DECL|uuid_create|function|static bool uuid_create(struct bt_uuid *uuid, struct net_buf *buf)
 DECL|uuid|member|struct bt_uuid *uuid;
 DECL|uuid|member|struct bt_uuid *uuid;
