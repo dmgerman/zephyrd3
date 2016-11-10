92b0ce776f069b6334256525507a2d2c38c06eea
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2016-11-10 16:26:55 -0200

    samples/zoap_server: Add support for blockwise GET tests
    This adds support for TD_COAP_BLOCK_01 and TD_COAP_BLOCK_02 tests,
    which test that the CoAP server is able to handle GET requests with
    blockwise tranfers.
    
    Change-Id: Id0d1703adcf5d4e76dd1bc489c8bcc94a3fd90bc
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 6429b1bf2b8250c9206ac4e3142cad81089ca2be

--- samples/net/zoap_server/src/zoap-server.c
+++ samples/net/zoap_server/src/zoap-server.c
@@ -1,4 +1,5 @@
 DECL|ALL_NODES_LOCAL_COAP_MCAST|macro|ALL_NODES_LOCAL_COAP_MCAST
+DECL|LARGE_TRANSFER_SIZE|macro|LARGE_TRANSFER_SIZE
 DECL|MY_COAP_PORT|macro|MY_COAP_PORT
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|STACKSIZE|macro|STACKSIZE
@@ -6,6 +7,8 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|context|variable|context
 DECL|join_coap_multicast_group|function|static bool join_coap_multicast_group(void)
+DECL|large_get|function|static int large_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|large_path|variable|large_path
 DECL|main|function|void main(void)
 DECL|piggyback_get|function|static int piggyback_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
 DECL|query_get|function|static int query_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
