95ad66a470e8a860de7113999be0e3c049c64905
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-11-09 16:29:55 -0800

    samples/mbedtls_dtlsclient: Removing unnecessary files
    The DTLS client will not longer use the legacy IP stack.
    
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Change-Id: I7402a188fc7501873d98d3b6cb2f03ee51f3506b
    
    Former-commit-id: 67ce715d0d217b8031e07bc93e2c70bc395f87c3

--- samples/net/mbedtls_dtlsclient/src/udp.c
+++ samples/net/mbedtls_dtlsclient/src/udp.c
@@ -1,6 +1,6 @@
-DECL|INET_FAMILY|macro|INET_FAMILY
+DECL|client_addr|variable|client_addr
+DECL|server_addr|variable|server_addr
 DECL|udp_init|function|int udp_init(struct udp_context *ctx)
+DECL|udp_received|function|static void udp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
 DECL|udp_rx|function|int udp_rx(void *context, unsigned char *buf, size_t size)
 DECL|udp_tx|function|int udp_tx(void *context, const unsigned char *buf, size_t size)
-DECL|uip_hostaddr|variable|uip_hostaddr
-DECL|uip_netmask|variable|uip_netmask
--- samples/net/mbedtls_dtlsclient/src/udp_native.c
+++
@@ -1,7 +0,0 @@
-DECL|client_addr|variable|client_addr
-DECL|server_addr|variable|server_addr
-DECL|udp_init|function|int udp_init(struct udp_context *ctx)
-DECL|udp_received|function|static void udp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
-DECL|udp_rx|function|int udp_rx(void *context, unsigned char *buf, size_t size)
-DECL|udp_sent|function|static inline void udp_sent(struct net_context *context, int status, void *bytes_sent, void *user_data)
-DECL|udp_tx|function|int udp_tx(void *context, const unsigned char *buf, size_t size)
