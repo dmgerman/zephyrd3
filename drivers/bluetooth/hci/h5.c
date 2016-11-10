f735b1b4b6f2721e419bd3b28a4a01e5f59d72cc
Szymon Janc <ext.szymon.janc@tieto.com>
2016-11-10 10:50:35 +0100

    Bluetooth: drivers: Use unified k_fifo API for FIFOs
    Change-Id: Id2ef6f9a134e6471c59bf3baedccef219d7f12f6
    Signed-off-by: Szymon Janc <ext.szymon.janc@tieto.com>
    
    Former-commit-id: e2795499d8d07363db510381327b91412322326a

--- drivers/bluetooth/hci/h5.c
+++ drivers/bluetooth/hci/h5.c
@@ -69,14 +69,14 @@ DECL|retx_work|variable|retx_work
 DECL|rx_ack|member|uint8_t rx_ack;
 DECL|rx_buf|member|struct net_buf *rx_buf;
 DECL|rx_fiber|function|static void rx_fiber(void)
-DECL|rx_queue|member|struct nano_fifo rx_queue;
+DECL|rx_queue|member|struct k_fifo rx_queue;
 DECL|rx_state|member|} rx_state;
 DECL|sync_req|variable|sync_req
 DECL|sync_rsp|variable|sync_rsp
 DECL|tx_ack|member|uint8_t tx_ack;
 DECL|tx_fiber|function|static void tx_fiber(void)
-DECL|tx_queue|member|struct nano_fifo tx_queue;
+DECL|tx_queue|member|struct k_fifo tx_queue;
 DECL|tx_seq|member|uint8_t tx_seq;
 DECL|tx_win|member|uint8_t tx_win;
 DECL|unack_queue_len|variable|unack_queue_len
-DECL|unack_queue|member|struct nano_fifo unack_queue;
+DECL|unack_queue|member|struct k_fifo unack_queue;
