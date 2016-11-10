047f6a78705a036c77d5cdb390304b8d8b7f3e37
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-11-10 14:04:20 -0800

    usb: class: Using unified kernel thread spawn API
    Change-Id: I1e46a3414b71493875cf7f3f05af4ba4750df7c6
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 6044f05f8adde058b4cd5aee651c6dbc6dcf223c

--- subsys/usb/class/mass_storage.c
+++ subsys/usb/class/mass_storage.c
@@ -3,8 +3,8 @@ DECL|CBWDecode|function|static void CBWDecode(uint8_t *buf, uint16_t size)
 DECL|CSW_ERROR|enumerator|CSW_ERROR,
 DECL|CSW_FAILED|enumerator|CSW_FAILED,
 DECL|CSW_PASSED|enumerator|CSW_PASSED,
-DECL|DISK_FIBER_PRIO|macro|DISK_FIBER_PRIO
-DECL|DISK_FIBER_STACK_SZ|macro|DISK_FIBER_STACK_SZ
+DECL|DISK_THREAD_PRIO|macro|DISK_THREAD_PRIO
+DECL|DISK_THREAD_STACK_SZ|macro|DISK_THREAD_STACK_SZ
 DECL|ERROR|enumerator|ERROR, /* error */
 DECL|HIGH_BYTE|macro|HIGH_BYTE
 DECL|LOW_BYTE|macro|LOW_BYTE
@@ -23,22 +23,19 @@ DECL|csw|variable|csw
 DECL|defered_wr_sz|variable|defered_wr_sz
 DECL|disk_wait_sem|variable|disk_wait_sem
 DECL|fail|function|static void fail(void)
-DECL|fiber_memory_read_done|function|static void fiber_memory_read_done(void)
-DECL|fiber_memory_write_done|function|static void fiber_memory_write_done(void)
-DECL|fiber_op|variable|fiber_op
 DECL|infoTransfer|function|static bool infoTransfer(void)
 DECL|inquiryRequest|function|static bool inquiryRequest(void)
 DECL|interface_data|variable|interface_data
 DECL|length|variable|length
 DECL|mass_ep_data|variable|mass_ep_data
-DECL|mass_fiber_main|function|static void mass_fiber_main(int arg1, int unused)
-DECL|mass_fiber_stack|variable|mass_fiber_stack
 DECL|mass_storage_bulk_in|function|static void mass_storage_bulk_in(uint8_t ep, enum usb_dc_ep_cb_status_code ep_status)
 DECL|mass_storage_bulk_out|function|static void mass_storage_bulk_out(uint8_t ep,enum usb_dc_ep_cb_status_code ep_status)
 DECL|mass_storage_class_handle_req|function|static int mass_storage_class_handle_req(struct usb_setup_packet *pSetup,int32_t *len, uint8_t **data)
 DECL|mass_storage_config|variable|mass_storage_config
 DECL|mass_storage_init|function|static int mass_storage_init(struct device *dev)
 DECL|mass_storage_status_cb|function|static void mass_storage_status_cb(enum usb_dc_status_code status)
+DECL|mass_thread_main|function|static void mass_thread_main(int arg1, int unused)
+DECL|mass_thread_stack|variable|mass_thread_stack
 DECL|mass_usb_description|variable|mass_usb_description
 DECL|max_lun_count|variable|max_lun_count
 DECL|memOK|variable|memOK
@@ -56,4 +53,7 @@ DECL|requestSense|function|static bool requestSense(void)
 DECL|sendCSW|function|static void sendCSW(void)
 DECL|stage|variable|stage
 DECL|testUnitReady|function|static void testUnitReady(void)
+DECL|thread_memory_read_done|function|static void thread_memory_read_done(void)
+DECL|thread_memory_write_done|function|static void thread_memory_write_done(void)
+DECL|thread_op|variable|thread_op
 DECL|write|function|static bool write(uint8_t *buf, uint16_t size)
