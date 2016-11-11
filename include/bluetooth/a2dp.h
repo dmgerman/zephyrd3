5f93202a1796201ecbef5ec165d552953af643fd
Piyush Itankar <piyush.t.itankar@intel.com>
2016-11-11 14:01:01 +0530

    Bluetooth: A2DP: Stream End Point Registration
    Added an API to allow addition of stream end points by the application.
    
    Change-Id: I91b95fc5dc3b9b2950e8d55b5846e460e55f2453
    Signed-off-by: Piyush Itankar <piyush.t.itankar@intel.com>
    
    Former-commit-id: edb32b74d13efc63a62ff553499b1a8d2b3d6980

--- include/bluetooth/a2dp.h
+++ include/bluetooth/a2dp.h
@@ -1,3 +1,10 @@
+DECL|BT_A2DP_AUDIO|enumerator|BT_A2DP_AUDIO = 0x00,
+DECL|BT_A2DP_MULTIMEDIA|enumerator|BT_A2DP_MULTIMEDIA = 0x02
+DECL|BT_A2DP_SINK|enumerator|BT_A2DP_SINK = 1
+DECL|BT_A2DP_SOURCE|enumerator|BT_A2DP_SOURCE = 0,
+DECL|BT_A2DP_VIDEO|enumerator|BT_A2DP_VIDEO = 0x01,
+DECL|MEDIA_TYPE|enum|enum MEDIA_TYPE {
+DECL|ROLE_TYPE|enum|enum ROLE_TYPE {
 DECL|__BT_A2DP_H|macro|__BT_A2DP_H
 DECL|bt_a2dp_endpoint|struct|struct bt_a2dp_endpoint {
 DECL|bt_a2dp_stream|struct|struct bt_a2dp_stream {
