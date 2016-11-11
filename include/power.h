cc58555ae2a3a2f16fed2082af0124e457ecef95
Ramesh Thomas <ramesh.thomas@intel.com>
2016-11-10 21:16:12 -0800

    power_mgmt: Rename _sys_soc_resume notification disabling API
    The API to disable _sys_soc_resume notification is currently
    called _sys_soc_disable_wake_event_notification. This is
    misleading because it is possible that the ISR from which
    _sys_soc_resume is called could be from a different interrupt
    with higher priority that happened before interrupts were
    enabled. More accurately, it is a notification of exit from
    kernel idling after pm operations.
    
    Jira: ZEP-1271
    Change-Id: I83747f2cacac1bc17f135d12f4aa4478970fc02d
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 8367056efaf2a2195b822ff3a74ad5c0890af503

--- include/power.h
+++ include/power.h
@@ -3,4 +3,4 @@ DECL|SYS_PM_DEEP_SLEEP|macro|SYS_PM_DEEP_SLEEP
 DECL|SYS_PM_LOW_POWER_STATE|macro|SYS_PM_LOW_POWER_STATE
 DECL|SYS_PM_NOT_HANDLED|macro|SYS_PM_NOT_HANDLED
 DECL|__INCpower|macro|__INCpower
-DECL|_sys_soc_disable_wake_event_notification|function|static inline void _sys_soc_disable_wake_event_notification(void)
+DECL|_sys_soc_pm_idle_exit_notification_disable|function|static inline void _sys_soc_pm_idle_exit_notification_disable(void)
--- kernel/unified/idle.c
+++ kernel/unified/idle.c
@@ -1,9 +1,9 @@
 DECL|IDLE_YIELD_IF_COOP|macro|IDLE_YIELD_IF_COOP
 DECL|IDLE_YIELD_IF_COOP|macro|IDLE_YIELD_IF_COOP
 DECL|_sys_idle_threshold_ticks|variable|_sys_idle_threshold_ticks
+DECL|_sys_pm_idle_exit_notify|variable|_sys_pm_idle_exit_notify
 DECL|_sys_power_save_idle_exit|function|void _sys_power_save_idle_exit(int32_t ticks)
 DECL|_sys_power_save_idle|function|static void _sys_power_save_idle(int32_t ticks __unused)
-DECL|_sys_soc_notify_wake_event|variable|_sys_soc_notify_wake_event
 DECL|_sys_soc_resume_from_deep_sleep|function|void __attribute__((weak)) _sys_soc_resume_from_deep_sleep(void)
 DECL|_sys_soc_resume|function|void __attribute__((weak)) _sys_soc_resume(void)
 DECL|idle|function|void idle(void *unused1, void *unused2, void *unused3)
