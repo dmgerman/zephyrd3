a2dcb6c9a6c01c9eb89000f421dd593d3b100f1f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-11 11:21:34 +0200

    net: Add util to remove an IPv6 router from the network interface
    Change-Id: Ie9cc461187a25355819e685cec68b04d4eba8c87
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: b650b758c9fb2e12c2232e496a474662f719c10e

--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -49,6 +49,7 @@ DECL|net_if_ipv6_prefix_unset_timer|function|void net_if_ipv6_prefix_unset_timer
 DECL|net_if_ipv6_router_add|function|struct net_if_router *net_if_ipv6_router_add(struct net_if *iface, struct in6_addr *addr, uint16_t lifetime)
 DECL|net_if_ipv6_router_find_default|function|struct net_if_router *net_if_ipv6_router_find_default(struct net_if *iface, struct in6_addr *addr)
 DECL|net_if_ipv6_router_lookup|function|struct net_if_router *net_if_ipv6_router_lookup(struct net_if *iface,struct in6_addr *addr)
+DECL|net_if_ipv6_router_rm|function|bool net_if_ipv6_router_rm(struct net_if_router *router)
 DECL|net_if_ipv6_router_update_lifetime|function|void net_if_ipv6_router_update_lifetime(struct net_if_router *router,uint32_t lifetime)
 DECL|net_if_ipv6_select_src_addr|function|const struct in6_addr *net_if_ipv6_select_src_addr(struct net_if *dst_iface, struct in6_addr *dst)
 DECL|net_if_ipv6_start_dad|function|static void net_if_ipv6_start_dad(struct net_if *iface, struct net_if_addr *ifaddr)
