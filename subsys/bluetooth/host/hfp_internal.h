69a7cace3a50e7f7c016a798d76984471188ee8f
Sathish Narasimman <sathish.narasimman@intel.com>
2016-11-14 19:01:18 +0530

    Bluetooth: HFP HF: SLC query indicators present value
    Service level connection sending AT+CIND? and parse the response
    to get the present value of the available indicators.
    This patch also providing callback interface to application for
    the indicators value received.
    
    Also added doxygen comment for connected and disconnected callback.
    
    < ACL Data TX: Handle 256 flags 0x00 dlen 17                   [hci0] 25.251358
          Channel: 67 len 13 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x09 cr 0 dlci 0x02
             Control: 0xef poll/final 0
             Length: 9
             FCS: 0x40
            41 54 2b 43 49 4e 44 3f 0d 40                    AT+CIND?.@
    > HCI Event: Number of Completed Packets (0x13) plen 5         [hci0] 25.262012
            Num handles: 1
            Handle: 256
            Count: 1
    > ACL Data RX: Handle 256 flags 0x02 dlen 33                   [hci0] 25.293028
          Channel: 64 len 29 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x0b cr 1 dlci 0x02
             Control: 0xff poll/final 1
             Length: 24
             FCS: 0x86
             Credits: 1
            0d 0a 2b 43 49 4e 44 3a 20 30 2c 30 2c 31 2c 33  ..+CIND: 0,0,1,3
            2c 30 2c 33 2c 30 0d 0a 86                       ,0,3,0...
    > ACL Data RX: Handle 256 flags 0x02 dlen 14                   [hci0] 25.295006
          Channel: 64 len 10 [PSM 3 mode 0] {chan 0}
          RFCOMM: Unnumbered Info with Header Check (UIH) (0xef)
             Address: 0x0b cr 1 dlci 0x02
             Control: 0xef poll/final 0
             Length: 6
             FCS: 0x9a
            0d 0a 4f 4b 0d 0a 9a                             ..OK...
    
    Change-Id: I7b2a89487e2d481391c51201e32b3287808f82dc
    Signed-off-by: Sathish Narasimman <sathish.narasimman@intel.com>
    
    Former-commit-id: 7eacb0477cfd918bc2a72cbf381859eede6b0596

--- subsys/bluetooth/host/hfp_internal.h
+++ subsys/bluetooth/host/hfp_internal.h
@@ -23,12 +23,20 @@ DECL|BT_HFP_HF_FEATURE_HF_IND|macro|BT_HFP_HF_FEATURE_HF_IND
 DECL|BT_HFP_HF_FEATURE_VOICE_RECG|macro|BT_HFP_HF_FEATURE_VOICE_RECG
 DECL|BT_HFP_HF_FEATURE_VOLUME|macro|BT_HFP_HF_FEATURE_VOLUME
 DECL|BT_HFP_HF_SUPPORTED_FEATURES|macro|BT_HFP_HF_SUPPORTED_FEATURES
+DECL|HF_BATTERY_IND|enumerator|HF_BATTERY_IND
+DECL|HF_CALL_HELD_IND|enumerator|HF_CALL_HELD_IND,
+DECL|HF_CALL_IND|enumerator|HF_CALL_IND,
+DECL|HF_CALL_SETUP_IND|enumerator|HF_CALL_SETUP_IND,
 DECL|HF_MAX_AG_INDICATORS|macro|HF_MAX_AG_INDICATORS
 DECL|HF_MAX_BUF_LEN|macro|HF_MAX_BUF_LEN
+DECL|HF_ROAM_IND|enumerator|HF_ROAM_IND,
+DECL|HF_SERVICE_IND|enumerator|HF_SERVICE_IND,
+DECL|HF_SINGNAL_IND|enumerator|HF_SINGNAL_IND,
 DECL|ag_features|member|uint32_t ag_features;
 DECL|at|member|struct at_client at;
 DECL|bt_hfp_hf|struct|struct bt_hfp_hf {
 DECL|hf_buffer|member|char hf_buffer[HF_MAX_BUF_LEN];
 DECL|hf_features|member|uint32_t hf_features;
+DECL|hfp_hf_ag_indicators|enum|enum hfp_hf_ag_indicators {
 DECL|ind_table|member|int8_t ind_table[HF_MAX_AG_INDICATORS];
 DECL|rfcomm_dlc|member|struct bt_rfcomm_dlc rfcomm_dlc;
