66b123cb8b0101a9d0f5fbc34cb2f263a4741689
Erwan Gouriou <erwan.gouriou@linaro.org>
2016-11-14 11:53:52 +0100

    serial: Provide STM32Cube based serial driver on stm32f1, stm32f4
    STM32Cube based implementation allows single driver file for
    all stm32 based SoCs.
    By maximizing code reuse, use of STM32Cube eases new SoCs
    porting into Zephyr and provides better maintanability and
    maturity.
    
    Change-Id: Ief4b723add3dfc8b2a839683559c5a4c5d5eb837
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    
    Former-commit-id: 8c079e91c90f94ece822e3be068b18a36214d57f

--- drivers/serial/uart_stm32.c
+++ drivers/serial/uart_stm32.c
@@ -3,9 +3,9 @@ DECL|DEV_DATA|macro|DEV_DATA
 DECL|PORT_0_IRQ|macro|PORT_0_IRQ
 DECL|PORT_1_IRQ|macro|PORT_1_IRQ
 DECL|PORT_2_IRQ|macro|PORT_2_IRQ
+DECL|TIMEOUT|macro|TIMEOUT
 DECL|UART_STRUCT|macro|UART_STRUCT
 DECL|__uart_stm32_get_clock|function|static inline void __uart_stm32_get_clock(struct device *dev)
-DECL|set_baud_rate|function|static void set_baud_rate(struct device *dev, uint32_t rate)
 DECL|uart_stm32_dev_cfg_0|variable|uart_stm32_dev_cfg_0
 DECL|uart_stm32_dev_cfg_1|variable|uart_stm32_dev_cfg_1
 DECL|uart_stm32_dev_cfg_2|variable|uart_stm32_dev_cfg_2
@@ -16,7 +16,7 @@ DECL|uart_stm32_driver_api|variable|uart_stm32_driver_api
 DECL|uart_stm32_fifo_fill|function|static int uart_stm32_fifo_fill(struct device *dev, const uint8_t *tx_data, int size)
 DECL|uart_stm32_fifo_read|function|static int uart_stm32_fifo_read(struct device *dev, uint8_t *rx_data, const int size)
 DECL|uart_stm32_init|function|static int uart_stm32_init(struct device *dev)
-DECL|uart_stm32_irq_callback_set|function|static void uart_stm32_irq_callback_set(struct device *dev, uart_irq_callback_t cb)
+DECL|uart_stm32_irq_callback_set|function|static void uart_stm32_irq_callback_set(struct device *dev,uart_irq_callback_t cb)
 DECL|uart_stm32_irq_config_func_0|function|static void uart_stm32_irq_config_func_0(struct device *dev)
 DECL|uart_stm32_irq_config_func_1|function|static void uart_stm32_irq_config_func_1(struct device *dev)
 DECL|uart_stm32_irq_config_func_2|function|static void uart_stm32_irq_config_func_2(struct device *dev)
