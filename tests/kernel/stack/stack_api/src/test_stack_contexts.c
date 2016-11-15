7267171bab3a69f0152c16616b9ccd1de52a96f7
jing wang <jing.j.wang@intel.com>
2016-11-15 16:02:06 +0800

    tests: add stack test cases which use unified kernel
    add stack test cases which cover basic api usage across
    threads and thread/isr.
    
    Change-Id: I1f42fa1139899c932a14da18753b93972f561bc8
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 3b03060579de03a9193c4f628e5309ec5cea2127

---
+++ tests/kernel/stack/stack_api/src/test_stack_contexts.c
@@ -0,0 +1,15 @@
+DECL|STACK_LEN|macro|STACK_LEN
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|data|variable|data
+DECL|end_sema|variable|end_sema
+DECL|stack|variable|stack
+DECL|tIsr_entry_pop|function|static void tIsr_entry_pop(void *p)
+DECL|tIsr_entry_push|function|static void tIsr_entry_push(void *p)
+DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *p3)
+DECL|test_stack_thread2isr|function|void test_stack_thread2isr(void)
+DECL|test_stack_thread2thread|function|void test_stack_thread2thread(void)
+DECL|threadstack|variable|threadstack
+DECL|tstack_pop|function|static void tstack_pop(struct k_stack *pstack)
+DECL|tstack_push|function|static void tstack_push(struct k_stack *pstack)
+DECL|tstack_thread_isr|function|static void tstack_thread_isr(struct k_stack *pstack)
+DECL|tstack_thread_thread|function|static void tstack_thread_thread(struct k_stack *pstack)
---
+++ tests/kernel/stack/stack_api/src/test_stack_fail.c
@@ -0,0 +1,4 @@
+DECL|STACK_LEN|macro|STACK_LEN
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|data|variable|data
+DECL|test_stack_pop_fail|function|void test_stack_pop_fail(void *p1, void *p2, void *p3)
