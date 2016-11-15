6b0a03dab802bcae6c87158d83fbaa6cc34b5759
Leandro Pereira <leandro.pereira@intel.com>
2016-11-14 16:39:26 -0800

    samples: net: Finish TCP support in echo server
    This adds the required code to echo server so that it works with TCP as
    well.
    
    Change-Id: Ib45dd91a52a60ddb4d8f0ae11175ceb52199c398
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 2543b2f6ee9661fe77bbb0cf7262bd69f69298ac

--- samples/net/echo_server/src/echo-server-yaip.c
+++ samples/net/echo_server/src/echo-server-yaip.c
@@ -10,6 +10,7 @@ DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|build_reply_buf|function|static struct net_buf *build_reply_buf(const char *name, struct net_context *context, struct net_buf *buf)
 DECL|get_context|function|static inline bool get_context(struct net_context **udp_recv4, struct net_context **udp_recv6, struct net_context **tcp_recv4, struct net_context **tcp_recv6, struct net_context **mcast_recv6)
 DECL|in4addr_my|variable|in4addr_my
 DECL|in6addr_mcast|variable|in6addr_mcast
@@ -17,14 +18,14 @@ DECL|in6addr_my|variable|in6addr_my
 DECL|in6addr_my|variable|in6addr_my
 DECL|init_app|function|static inline void init_app(void)
 DECL|main|function|void main(void)
+DECL|pkt_sent|function|static inline void pkt_sent(struct net_context *context, int status, void *token, void *user_data)
 DECL|quit_lock|variable|quit_lock
 DECL|quit|function|static inline void quit(void)
 DECL|receive|function|void receive(void)
 DECL|set_dst_addr|function|static inline void set_dst_addr(sa_family_t family,struct net_buf *buf, struct sockaddr *dst_addr)
 DECL|setup_tcp_accept|function|static void setup_tcp_accept(struct net_context *tcp_recv4, struct net_context *tcp_recv6)
 DECL|setup_udp_recv|function|static void setup_udp_recv(struct net_context *udp_recv4, struct net_context *udp_recv6)
-DECL|tcp_received|function|static void tcp_received(struct net_context *context, struct sockaddr *addr, socklen_t addrlen, int error, void *user_data)
+DECL|tcp_accepted|function|static void tcp_accepted(struct net_context *context, struct sockaddr *addr, socklen_t addrlen, int error, void *user_data)
+DECL|tcp_received|function|static void tcp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
 DECL|thread_stack|variable|thread_stack
 DECL|udp_received|function|static void udp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
-DECL|udp_recv|function|static struct net_buf *udp_recv(const char *name,struct net_context *context, struct net_buf *buf)
-DECL|udp_sent|function|static inline void udp_sent(struct net_context *context, int status, void *token, void *user_data)
