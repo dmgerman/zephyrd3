331202fd3215b0cc897bd3f09942178035ec205e
Carles Cufi <carles.cufi@nordicsemi.no>
2016-11-16 16:24:27 +0100

    Bluetooth: Controller: Implement ECC commands
    Implement the 2 HCI commands that provide ECC public key
    and shared secret generation:
    
    - LE Read Local P-256 Public Key
    - LE Generate DHKey
    
    Jira: ZEP-1246
    
    Change-Id: I79388bfdb9f2e28b9377b4bb6ee2caca25f33f3e
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 3b1baa88ef3b4dfe9b02bdc04220140917d6bab1

--- subsys/bluetooth/controller/hci/hci_driver.c
+++ subsys/bluetooth/controller/hci/hci_driver.c
@@ -5,10 +5,13 @@ DECL|HCI_CMD|macro|HCI_CMD
 DECL|HCI_EVT|macro|HCI_EVT
 DECL|HCI_SCO|macro|HCI_SCO
 DECL|_hci_driver_init|function|static int _hci_driver_init(struct device *unused)
+DECL|bt_rand|function|int bt_rand(void *buf, size_t len)
 DECL|cmd_handle|function|static int cmd_handle(struct net_buf *buf)
 DECL|drv|variable|drv
 DECL|hci_driver_open|function|static int hci_driver_open(void)
 DECL|hci_driver_send|function|static int hci_driver_send(struct net_buf *buf)
+DECL|hci_le_rand|function|void hci_le_rand(void *buf, uint8_t len)
+DECL|mutex_rand|variable|mutex_rand
 DECL|radio_active_callback|function|void radio_active_callback(uint8_t active)
 DECL|radio_event_callback|function|void radio_event_callback(void)
 DECL|radio_nrf5_isr|function|static void radio_nrf5_isr(void *arg)
