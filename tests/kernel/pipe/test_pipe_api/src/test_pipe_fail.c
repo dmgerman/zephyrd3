5992fe762e67f191baea581811e4f2e28b79040a
jing wang <jing.j.wang@intel.com>
2016-11-16 10:56:29 +0800

    tests: add pipe test cases which use unified kernel
    Add kernel pipe test cases which cover basic pipe apis usage
    across thread and isr
    
    Change-Id: I11899411b305535297f2e25056678d5b7df8fb95
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    
    Former-commit-id: 6ffe10f50f72030495ecc2ee505d867727e90388

---
+++ tests/kernel/pipe/test_pipe_api/src/test_pipe_fail.c
@@ -0,0 +1,4 @@
+DECL|PIPE_LEN|macro|PIPE_LEN
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|test_pipe_get_fail|function|void test_pipe_get_fail(void *p1, void *p2, void *p3)
+DECL|test_pipe_put_fail|function|void test_pipe_put_fail(void *p1, void *p2, void *p3)
