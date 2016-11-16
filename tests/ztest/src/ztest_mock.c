4949da5610c2510488f67b344b3c6e9714ad4e8a
Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
2016-11-15 17:25:44 -0800

    tests/ztest/mock: remove usage of legacy k_fifo_get()
    Legacy FIFO operations were failing and thus the TC was failing to run.
    
    Stop using k_fifo_get() for allocation and use a bitmap allocator. A
    couple of the bitmap operations should be moved to a common header
    once ZEP-1347 is completed.
    
    Passes on all arches and boards, whitelist removed; ARM excluded
    though due to missing bitfield implementation as per ZEP-82.
    
    Note there is a false checkpatch positive in the decl of
    sys_bitfield_find_first_clear().
    
    Change-Id: I1d3ce8e988bf799573041026e419e3946d153590
    Signed-off-by: Inaky Perez-Gonzalez <inaky.perez-gonzalez@intel.com>
    
    Former-commit-id: 462c6efca404f90b67fc6ba0240f7196496ac8c4

--- tests/ztest/src/ztest_mock.c
+++ tests/ztest/src/ztest_mock.c
@@ -1,3 +1,5 @@
+DECL|BITS_PER_UL|macro|BITS_PER_UL
+DECL|DEFINE_BITFIELD|macro|DEFINE_BITFIELD
 DECL|_cleanup_mock|function|int _cleanup_mock(void)
 DECL|_init_mock|function|void _init_mock(void)
 DECL|_init_mock|function|void _init_mock(void)
@@ -6,13 +8,12 @@ DECL|_ztest_expect_value|function|void _ztest_expect_value(const char *fn, const
 DECL|_ztest_get_return_value|function|uintptr_t _ztest_get_return_value(const char *fn)
 DECL|_ztest_returns_value|function|void _ztest_returns_value(const char *fn, uintptr_t value)
 DECL|alloc_parameter|function|static struct parameter *alloc_parameter(void)
-DECL|alloc_parameter|function|static struct parameter *alloc_parameter(void)
-DECL|fifo|variable|fifo
+DECL|alloc_parameter|function|struct parameter *alloc_parameter(void)
 DECL|find_and_delete_value|function|static struct parameter *find_and_delete_value(struct parameter *param, const char *fn, const char *name)
 DECL|fn|member|const char *fn;
 DECL|free_param_list|function|static void free_param_list(struct parameter *param)
 DECL|free_parameter|function|static void free_parameter(struct parameter *param)
-DECL|free_parameter|function|static void free_parameter(struct parameter *param)
+DECL|free_parameter|function|void free_parameter(struct parameter *param)
 DECL|insert_value|function|static void insert_value(struct parameter *param, const char *fn, const char *name, uintptr_t val)
 DECL|name|member|const char *name;
 DECL|next|member|struct parameter *next;
@@ -20,4 +21,5 @@ DECL|parameter_list|variable|parameter_list
 DECL|parameter|struct|struct parameter {
 DECL|params|variable|params
 DECL|return_value_list|variable|return_value_list
+DECL|sys_bitfield_find_first_clear|function|int sys_bitfield_find_first_clear(const unsigned long *bitmap, unsigned int bits)
 DECL|value|member|uintptr_t value;
