0327da71a9f64f27c880bc245a4272c35c4bff4d
jing wang <jing.j.wang@intel.com>
2016-11-17 10:39:42 +0800

    tests: add mutex api test cases which use unified kernel
    add unified kernel mutext test cases which covere basic
    mutex apis under different conditions - timeout,
    no_wait, forever
    
    Change-Id: Iaab5bba80a6eebd89bfe675352d67b27024ad7bb
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 7553b4688b6535fad286a466760c4518a3e1f084

---
+++ tests/kernel/mutex/mutex_api/src/test_mutex_apis.c
@@ -0,0 +1,17 @@
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|kmutex|variable|kmutex
+DECL|mutex|variable|mutex
+DECL|tThread_entry_lock_forever|function|static void tThread_entry_lock_forever(void *p1, void *p2, void *p3)
+DECL|tThread_entry_lock_no_wait|function|static void tThread_entry_lock_no_wait(void *p1, void *p2, void *p3)
+DECL|tThread_entry_lock_timeout_fail|function|static void tThread_entry_lock_timeout_fail(void *p1, void *p2, void *p3)
+DECL|tThread_entry_lock_timeout_pass|function|static void tThread_entry_lock_timeout_pass(void *p1, void *p2, void *p3)
+DECL|test_mutex_lock_unlock|function|void test_mutex_lock_unlock(void)
+DECL|test_mutex_reent_lock_forever|function|void test_mutex_reent_lock_forever(void)
+DECL|test_mutex_reent_lock_no_wait|function|void test_mutex_reent_lock_no_wait(void)
+DECL|test_mutex_reent_lock_timeout_fail|function|void test_mutex_reent_lock_timeout_fail(void)
+DECL|test_mutex_reent_lock_timeout_pass|function|void test_mutex_reent_lock_timeout_pass(void)
+DECL|tmutex_test_lock_timeout|function|static void tmutex_test_lock_timeout(struct k_mutex *pmutex, void (*entry_fn)(void *, void *, void *))
+DECL|tmutex_test_lock_unlock|function|static void tmutex_test_lock_unlock(struct k_mutex *pmutex)
+DECL|tmutex_test_lock|function|static void tmutex_test_lock(struct k_mutex *pmutex, void (*entry_fn)(void *, void *, void *))
+DECL|tstack|variable|tstack
