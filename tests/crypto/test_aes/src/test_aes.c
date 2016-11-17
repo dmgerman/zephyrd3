9a4f4b83995d264402146ef70615f652b83c29fa
Flavio Santes <flavio.santes@intel.com>
2016-11-17 05:04:26 -0600

    tests/tinycrypt/aes: Remove struct declaration inside function
    This commit removes the declaration of a data type inside the tests
    3 & 4. The new data type 'struct kat_table' is declared at the
    beginning of the file.
    
    Change-Id: I7662cd164a865fe0e70f984b9b494d96b4a72af9
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 32f04293ca696ee2124dfdf8812163fbdac4b68d

--- tests/crypto/test_aes/src/test_aes.c
+++ tests/crypto/test_aes/src/test_aes.c
@@ -1,6 +1,9 @@
 DECL|NUM_OF_FIXED_KEYS|macro|NUM_OF_FIXED_KEYS
 DECL|NUM_OF_NIST_KEYS|macro|NUM_OF_NIST_KEYS
+DECL|in|member|uint8_t in[NUM_OF_NIST_KEYS];
+DECL|kat_table|struct|struct kat_table {
 DECL|main|function|void main(void)
+DECL|out|member|uint8_t out[NUM_OF_NIST_KEYS];
 DECL|test_1|function|uint32_t test_1(void)
 DECL|test_2|function|int32_t test_2(void)
 DECL|test_3|function|uint32_t test_3(void)
