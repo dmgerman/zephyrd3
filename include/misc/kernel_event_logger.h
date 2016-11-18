abb5c872322409a2d065b90e7d168cd881e854bb
Allan Stephens <allan.stephens@windriver.com>
2016-11-18 15:32:13 -0500

    kernel: Minor optimization to kernel event logger timestamping
    Rewrites the timestamping logic to always generate timestamps
    via a function pointer that is initialized to sys_cycle_get_32(),
    but can be changed to point to a user-supplied function. This
    eliminates the need for an if/then/else construct in every place
    that a timestamp is generated.
    
    Change-Id: Id11f8c41b193a93cece16565978a525056010f0e
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 66a07bbd48dfb64a2bc819b937413d47473aed77

--- include/misc/kernel_event_logger.h
+++ include/misc/kernel_event_logger.h
@@ -4,8 +4,7 @@ DECL|KERNEL_EVENT_LOGGER_SLEEP_EVENT_ID|macro|KERNEL_EVENT_LOGGER_SLEEP_EVENT_ID
 DECL|__KERNEL_EVENT_LOGGER_H__|macro|__KERNEL_EVENT_LOGGER_H__
 DECL|_sys_k_event_logger_enter_sleep|function|static inline void _sys_k_event_logger_enter_sleep(void) {};
 DECL|_sys_k_event_logger_interrupt|function|static inline void _sys_k_event_logger_interrupt(void) {};
-DECL|_sys_k_get_time|function|static inline uint32_t _sys_k_get_time(void)
-DECL|_sys_k_get_time|function|static inline uint32_t _sys_k_get_time(void)
+DECL|_sys_k_get_time|macro|_sys_k_get_time
 DECL|sys_k_event_logger_get_mask|function|static inline int sys_k_event_logger_get_mask(void)
 DECL|sys_k_event_logger_get_wait_timeout|function|static inline int sys_k_event_logger_get_wait_timeout(uint16_t *event_id,uint8_t *dropped, uint32_t *event_data, uint8_t *data_size, uint32_t timeout)
 DECL|sys_k_event_logger_get_wait|function|static inline int sys_k_event_logger_get_wait(uint16_t *event_id,uint8_t *dropped, uint32_t *event_data, uint8_t *data_size)
@@ -14,5 +13,6 @@ DECL|sys_k_event_logger_put_timed|function|static inline void sys_k_event_logger
 DECL|sys_k_event_logger_put|function|static inline void sys_k_event_logger_put(uint16_t event_id, uint32_t *event_data, uint8_t data_size)
 DECL|sys_k_event_logger_register_as_collector|function|static inline void sys_k_event_logger_register_as_collector(void) {};
 DECL|sys_k_event_logger_set_mask|function|static inline void sys_k_event_logger_set_mask(int value)
+DECL|sys_k_event_logger_set_timer|function|static inline void sys_k_event_logger_set_timer(sys_k_timer_func_t func)
 DECL|sys_k_must_log_event|function|static inline int sys_k_must_log_event(int event_type)
 DECL|sys_k_timer_func_t|typedef|typedef uint32_t (*sys_k_timer_func_t)(void);
