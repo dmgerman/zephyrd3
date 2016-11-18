ce59d507cebd7400b20bb4adbca2cfaab8fcae65
Allan Stephens <allan.stephens@windriver.com>
2016-11-18 15:15:59 -0500

    doc: Fix up API descriptions for kernel event logger
    Prepares the kernel event logger APIs for inclusion in the
    API guide. Also corrects a couple of other issues:
    
    * Gets rid of obsolete thread monitor code.
    * Renames "timer_func" global variable to "_sys_k_timer_func"
      to align it with kernel naming conventions.
    
    Change-Id: I93d403f83ae44ff45dda489c2ead7bfec6ce1fa3
    Signed-off-by: Allan Stephens <allan.stephens@windriver.com>
    
    Former-commit-id: 72efec3d2b7fef367c0b38e7c28d105eaefac33c

--- include/misc/kernel_event_logger.h
+++ include/misc/kernel_event_logger.h
@@ -1,26 +1,18 @@
 DECL|KERNEL_EVENT_LOGGER_CONTEXT_SWITCH_EVENT_ID|macro|KERNEL_EVENT_LOGGER_CONTEXT_SWITCH_EVENT_ID
 DECL|KERNEL_EVENT_LOGGER_INTERRUPT_EVENT_ID|macro|KERNEL_EVENT_LOGGER_INTERRUPT_EVENT_ID
 DECL|KERNEL_EVENT_LOGGER_SLEEP_EVENT_ID|macro|KERNEL_EVENT_LOGGER_SLEEP_EVENT_ID
-DECL|KERNEL_EVENT_LOGGER_TASK_MON_CMD_PACKET_EVENT_ID|macro|KERNEL_EVENT_LOGGER_TASK_MON_CMD_PACKET_EVENT_ID
-DECL|KERNEL_EVENT_LOGGER_TASK_MON_KEVENT_EVENT_ID|macro|KERNEL_EVENT_LOGGER_TASK_MON_KEVENT_EVENT_ID
-DECL|KERNEL_EVENT_LOGGER_TASK_MON_TASK_STATE_CHANGE_EVENT_ID|macro|KERNEL_EVENT_LOGGER_TASK_MON_TASK_STATE_CHANGE_EVENT_ID
 DECL|__KERNEL_EVENT_LOGGER_H__|macro|__KERNEL_EVENT_LOGGER_H__
-DECL|_sys_k_event_logger_enter_sleep|function|static inline void _sys_k_event_logger_enter_sleep(void) {};
 DECL|_sys_k_event_logger_enter_sleep|function|static inline void _sys_k_event_logger_enter_sleep(void) {};
 DECL|_sys_k_event_logger_interrupt|function|static inline void _sys_k_event_logger_interrupt(void) {};
 DECL|_sys_k_get_time|function|static inline uint32_t _sys_k_get_time(void)
 DECL|_sys_k_get_time|function|static inline uint32_t _sys_k_get_time(void)
 DECL|sys_k_event_logger_get_mask|function|static inline int sys_k_event_logger_get_mask(void)
-DECL|sys_k_event_logger_get_monitor_mask|function|static inline int sys_k_event_logger_get_monitor_mask(void)
-DECL|sys_k_event_logger_get_wait_timeout|macro|sys_k_event_logger_get_wait_timeout
-DECL|sys_k_event_logger_get_wait|macro|sys_k_event_logger_get_wait
-DECL|sys_k_event_logger_get|macro|sys_k_event_logger_get
+DECL|sys_k_event_logger_get_wait_timeout|function|static inline int sys_k_event_logger_get_wait_timeout(uint16_t *event_id,uint8_t *dropped, uint32_t *event_data, uint8_t *data_size, uint32_t timeout)
+DECL|sys_k_event_logger_get_wait|function|static inline int sys_k_event_logger_get_wait(uint16_t *event_id,uint8_t *dropped, uint32_t *event_data, uint8_t *data_size)
+DECL|sys_k_event_logger_get|function|static inline int sys_k_event_logger_get(uint16_t *event_id, uint8_t *dropped, uint32_t *event_data, uint8_t *data_size)
 DECL|sys_k_event_logger_put_timed|function|static inline void sys_k_event_logger_put_timed(uint16_t event_id) {};
-DECL|sys_k_event_logger_put|function|static inline void sys_k_event_logger_put(uint16_t event_id, uint32_t *event_data,uint8_t data_size)
-DECL|sys_k_event_logger_put|macro|sys_k_event_logger_put
+DECL|sys_k_event_logger_put|function|static inline void sys_k_event_logger_put(uint16_t event_id, uint32_t *event_data, uint8_t data_size)
 DECL|sys_k_event_logger_register_as_collector|function|static inline void sys_k_event_logger_register_as_collector(void) {};
 DECL|sys_k_event_logger_set_mask|function|static inline void sys_k_event_logger_set_mask(int value)
-DECL|sys_k_event_logger_set_monitor_mask|function|static inline void sys_k_event_logger_set_monitor_mask(int value)
-DECL|sys_k_event_logger|variable|sys_k_event_logger
 DECL|sys_k_must_log_event|function|static inline int sys_k_must_log_event(int event_type)
-DECL|sys_k_timer_func|typedef|typedef uint32_t (*sys_k_timer_func)(void);
+DECL|sys_k_timer_func_t|typedef|typedef uint32_t (*sys_k_timer_func_t)(void);
