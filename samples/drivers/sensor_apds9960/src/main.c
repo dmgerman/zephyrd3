d65d8abb0b7a87e549cf0d9c8ae7605bea9370ab
Juan Manuel Cruz <juan.m.cruz.alcaraz@intel.com>
2016-11-18 16:04:56 -0600

    sensor: fixes program hangs in the apds9960 sample
    If an i2c transaction fails the sample will hang the program into
    an infinite circle.
    
    This commit will remove the infinite circle and report back the
    error code from the i2c transaction.
    
    Change-Id: I38d350a805af6bec43f2fa8d4af6ce4e3cc27662
    Coverity-CID: 151991
    Coverity-CID: 151992
    Signed-off-by: Juan Manuel Cruz <juan.m.cruz.alcaraz@intel.com>
    
    Former-commit-id: ec27d03ed44fa8ea3ea0983603d05b4224514130

--- samples/drivers/sensor_apds9960/src/main.c
+++ samples/drivers/sensor_apds9960/src/main.c
@@ -12,8 +12,8 @@ DECL|SLEEP_MSEC|macro|SLEEP_MSEC
 DECL|apa102c_led_program|function|void apa102c_led_program(struct device *gpio_dev, uint32_t rgb)
 DECL|apa102c_rgb_send|function|void apa102c_rgb_send(struct device *gpio_dev, uint32_t rgb)
 DECL|apds9960_als_valid_wait|function|void apds9960_als_valid_wait(struct device *i2c_dev)
-DECL|apds9960_reg_read|function|void apds9960_reg_read(struct device *i2c_dev, uint8_t reg_addr, uint8_t *data, uint8_t data_len)
-DECL|apds9960_reg_write|function|void apds9960_reg_write(struct device *i2c_dev,uint8_t reg_addr, uint8_t reg_val)
+DECL|apds9960_reg_read|function|int apds9960_reg_read(struct device *i2c_dev, uint8_t reg_addr, uint8_t *data, uint8_t data_len)
+DECL|apds9960_reg_write|function|int apds9960_reg_write(struct device *i2c_dev,uint8_t reg_addr, uint8_t reg_val)
 DECL|apds9960_setup|function|void apds9960_setup(struct device *i2c_dev, int gain)
 DECL|bdatah|member|uint8_t bdatah;
 DECL|bdatal|member|uint8_t bdatal;
