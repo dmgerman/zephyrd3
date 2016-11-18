3ff6f1f508464c4c7d362ea09c7c5ceba3da595a
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-11-18 17:55:16 +0100

    kernel: declare main and idle stack as globals
    Renamed main_stack and idle_stack, to _main_stack and
    _idle_stack, respectively, and made them globals. This does
    not affect performance. They are still kept kernel private
    symbols and not part of kernel API.
    
    This will allow these symbols to be referenced in calls to
    stack_analyse misc functions to profile stack usage in
    applications.
    
    Change-id: Id6b746c5cfda617c26901c6e62c3e17114471f57
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 09ba96d85656b9975f07f77bc9e042f386fca4cb

--- kernel/unified/init.c
+++ kernel/unified/init.c
@@ -19,15 +19,15 @@ DECL|__main_tsc|variable|__main_tsc
 DECL|__start_tsc|variable|__start_tsc
 DECL|_bss_zero|function|void _bss_zero(void)
 DECL|_data_copy|function|void _data_copy(void)
+DECL|_idle_stack|variable|_idle_stack
 DECL|_idle_thread|variable|_idle_thread
 DECL|_interrupt_stack|variable|_interrupt_stack
+DECL|_main_stack|variable|_main_stack
 DECL|_main_thread|variable|_main_thread
 DECL|_main|function|static void _main(void *unused1, void *unused2, void *unused3)
 DECL|build_timestamp|variable|build_timestamp
-DECL|idle_stack|variable|idle_stack
 DECL|initialize_timeouts|macro|initialize_timeouts
 DECL|initialize_timeouts|macro|initialize_timeouts
-DECL|main_stack|variable|main_stack
 DECL|main|function|void __weak main(void)
 DECL|prepare_multithreading|function|static void prepare_multithreading(struct k_thread *dummy_thread)
 DECL|switch_to_main_thread|function|static void switch_to_main_thread(void)
