3262df8aaabe108b68588285a1836573ffb33f79
Szymon Janc <szymon.janc@codecoup.pl>
2016-11-20 22:06:37 +0100

    printk: Add basic support for width modifier and zero padding
    This adds basic support for width modifier when printing integers.
    Supported specifiers are u,i,d,x,X. Width '*' is not supported.
    Flag '0' for left-pading number with zero is also supported.
    
    examples:
    printk("0x%x 0x%02x 0x%04x 0x%08x\n", 1, 1, 1, 1);
    0x1 0x01 0x0001 0x00000001
    
    printk("0x%x 0x%2x 0x%4x 0x%8x\n", 1, 1, 1, 1);
    0x1 0x 1 0x   1 0x       1
    
    This should make printk usable for pretty printing u8 and u16 integers.
    
    Change-Id: I58fa869e9c295a052f97fbf052291ef4d132811e
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>
    
    Former-commit-id: bde20d5447c2f216e0389c10733315125359bdba

--- misc/printk.c
+++ misc/printk.c
@@ -1,7 +1,7 @@
 DECL|__printk_hook_install|function|void __printk_hook_install(int (*fn)(int))
 DECL|_char_out|variable|_char_out
 DECL|_nop_char_out|function|static int _nop_char_out(int c)
-DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(const unsigned long num)
-DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(const unsigned long num)
+DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(const unsigned long num, int pad_zero, int min_width)
+DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(const unsigned long num, int pad_zero, int min_width)
 DECL|_vprintk|function|static inline void _vprintk(const char *fmt, va_list ap)
 DECL|printk|function|void printk(const char *fmt, ...)
