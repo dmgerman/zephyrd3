7a3f5635c7a5af0d7d4b95ff1a1237e600764276
LIU, Sharron <sharron.liu@intel.com>
2016-11-22 00:22:30 +0800

    tests: kernel: added mbox api test
    tested APIs:
    K_MBOX_DEFINE
    k_mbox_init
    k_mbox_put
    k_mbox_async_put
    k_mbox_get
    k_mbox_data_get
    k_mbox_data_block_get
    
    Change-Id: I73e1bd27a69b23fb5b8a856ce092fb651066a6c6
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 6afbd387bfe7b91be0a4683bf52dcd98e521addb

---
+++ tests/kernel/mbox/mbox_api/src/test_mbox_api.c
@@ -0,0 +1,32 @@
+DECL|ASYNC_PUT_GET_BLOCK|enumerator|ASYNC_PUT_GET_BLOCK,
+DECL|ASYNC_PUT_GET_BUFFER|enumerator|ASYNC_PUT_GET_BUFFER,
+DECL|MAIL_LEN|macro|MAIL_LEN
+DECL|MAX_INFO_TYPE|enumerator|MAX_INFO_TYPE
+DECL|PUT_GET_BUFFER|enumerator|PUT_GET_BUFFER,
+DECL|PUT_GET_NULL|enumerator|PUT_GET_NULL = 0,
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TARGET_SOURCE_THREAD_BLOCK|enumerator|TARGET_SOURCE_THREAD_BLOCK,
+DECL|TARGET_SOURCE_THREAD_BUFFER|enumerator|TARGET_SOURCE_THREAD_BUFFER,
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|data|variable|data
+DECL|end_sema|variable|end_sema
+DECL|info_type|variable|info_type
+DECL|kmbox|variable|kmbox
+DECL|mbox|variable|mbox
+DECL|mmsg_type|enum|static enum mmsg_type {
+DECL|receiver_tid|variable|receiver_tid
+DECL|sender_tid|variable|sender_tid
+DECL|sync_sema|variable|sync_sema
+DECL|test_mbox_async_put_get_block|function|void test_mbox_async_put_get_block(void)
+DECL|test_mbox_async_put_get_buffer|function|void test_mbox_async_put_get_buffer(void)
+DECL|test_mbox_kdefine|function|void test_mbox_kdefine(void)
+DECL|test_mbox_kinit|function|void test_mbox_kinit(void)
+DECL|test_mbox_put_get_buffer|function|void test_mbox_put_get_buffer(void)
+DECL|test_mbox_put_get_null|function|void test_mbox_put_get_null(void)
+DECL|test_mbox_target_source_thread_block|function|void test_mbox_target_source_thread_block(void)
+DECL|test_mbox_target_source_thread_buffer|function|void test_mbox_target_source_thread_buffer(void)
+DECL|tmbox_entry|function|static void tmbox_entry(void *p1, void *p2, void *p3)
+DECL|tmbox_get|function|static void tmbox_get(struct k_mbox *pmbox)
+DECL|tmbox_put|function|static void tmbox_put(struct k_mbox *pmbox)
+DECL|tmbox|function|static void tmbox(struct k_mbox *pmbox)
+DECL|tstack|variable|tstack
