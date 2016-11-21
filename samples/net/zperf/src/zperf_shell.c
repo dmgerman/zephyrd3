144ee837a1a4c70ab6902867d2843b82f36616d6
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-21 15:51:56 +0200

    samples: zperf: Port to native IP stack
    Port the zperf network performance tool to native IP stack.
    
    TCP support is missing from this version.
    
    Change-Id: I2648e7d81014bb0cae8cf50854b0ae5f89a6567c
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 51da830b5e751c60b120872d1c8c7d16dc955a99

--- samples/net/zperf/src/zperf_shell.c
+++ samples/net/zperf/src/zperf_shell.c
@@ -1,23 +1,34 @@
 DECL|CONFIG|variable|CONFIG
 DECL|DEF_PORT|macro|DEF_PORT
 DECL|DEVICE_NAME|macro|DEVICE_NAME
-DECL|DST_IPADDR|macro|DST_IPADDR
-DECL|MY_IPADDR|macro|MY_IPADDR
-DECL|MY_PREFIX_LEN|macro|MY_PREFIX_LEN
 DECL|MY_SHELL_MODULE|macro|MY_SHELL_MODULE
 DECL|MY_SRC_PORT|macro|MY_SRC_PORT
+DECL|NET_DEBUG|macro|NET_DEBUG
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|commands|variable|commands
-DECL|in_addr_dst|variable|in_addr_dst
-DECL|in_addr_my|variable|in_addr_my
-DECL|mainloop|function|void mainloop(void)
+DECL|execute_upload|function|static int execute_upload(struct net_context *context6, struct net_context *context4, sa_family_t family, struct sockaddr_in6 *ipv6, struct sockaddr_in *ipv4,
+DECL|in4_addr_dst|variable|in4_addr_dst
+DECL|in4_addr_my|variable|in4_addr_my
+DECL|in6_addr_dst|variable|in6_addr_dst
+DECL|in6_addr_my|variable|in6_addr_my
+DECL|ipv4|variable|ipv4
+DECL|ipv6|variable|ipv6
+DECL|main|function|void main(void)
+DECL|setup_contexts|function|static int setup_contexts(struct net_context **context6, struct net_context **context4, sa_family_t family, struct sockaddr_in6 *ipv6, struct sockaddr_in *ipv4,
 DECL|shell_cmd_connectap|function|static int shell_cmd_connectap(int argc, char *argv[])
 DECL|shell_cmd_setip|function|static int shell_cmd_setip(int argc, char *argv[])
 DECL|shell_cmd_tcp_download|function|static int shell_cmd_tcp_download(int argc, char *argv[])
 DECL|shell_cmd_udp_download|function|static int shell_cmd_udp_download(int argc, char *argv[])
+DECL|shell_cmd_upload2|function|static int shell_cmd_upload2(int argc, char *argv[])
 DECL|shell_cmd_upload|function|static int shell_cmd_upload(int argc, char *argv[])
 DECL|shell_cmd_version|function|static int shell_cmd_version(int argc, char *argv[])
-DECL|shell_tcp_upload_print_stats|function|static void shell_tcp_upload_print_stats(zperf_results *results)
+DECL|shell_tcp_upload_print_stats|function|static void shell_tcp_upload_print_stats(struct zperf_results *results)
 DECL|shell_tcp_upload_usage|function|static void shell_tcp_upload_usage(void)
-DECL|shell_udp_upload_print_stats|function|static void shell_udp_upload_print_stats(zperf_results *results)
+DECL|shell_udp_upload2_usage|function|static void shell_udp_upload2_usage(void)
+DECL|shell_udp_upload_print_stats|function|static void shell_udp_upload_print_stats(struct zperf_results *results)
 DECL|shell_udp_upload_usage|function|static void shell_udp_upload_usage(void)
+DECL|zperf_get_ipv4_addr|function|int zperf_get_ipv4_addr(char *host, struct in_addr *addr, const char *str)
+DECL|zperf_get_ipv6_addr|function|int zperf_get_ipv6_addr(char *host, char *prefix_str, struct in6_addr *addr,const char *str)
+DECL|zperf_get_sin6|function|struct sockaddr_in6 *zperf_get_sin6(void)
+DECL|zperf_get_sin|function|struct sockaddr_in *zperf_get_sin(void)
 DECL|zperf_init|function|static void zperf_init(void)
--- samples/net/zperf/src/zperf_udp_uploader.c
+++ samples/net/zperf/src/zperf_udp_uploader.c
@@ -1,4 +1,5 @@
 DECL|TAG|macro|TAG
-DECL|zperf_udp_upload|function|void zperf_udp_upload(struct net_context *net_context,unsigned int duration_in_ms, unsigned int packet_size, unsigned int rate_in_kbps, zperf_results *results)
-DECL|zperf_upload_decode_stat|function|void zperf_upload_decode_stat(struct net_buf *net_stat,zperf_results *results)
-DECL|zperf_upload_fin|function|void zperf_upload_fin(struct net_context *net_context, uint32_t nb_packets,uint32_t end_time, uint32_t packet_size, zperf_results *results)
+DECL|stat_received|function|static void stat_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
+DECL|zperf_udp_upload|function|void zperf_udp_upload(struct net_context *context, unsigned int duration_in_ms, unsigned int packet_size, unsigned int rate_in_kbps, struct zperf_results *results)
+DECL|zperf_upload_decode_stat|function|static inline void zperf_upload_decode_stat(struct net_buf *buf, struct zperf_results *results)
+DECL|zperf_upload_fin|function|static inline void zperf_upload_fin(struct net_context *context, uint32_t nb_packets, uint32_t end_time, uint32_t packet_size, struct zperf_results *results)
