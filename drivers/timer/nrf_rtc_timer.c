a92439851743bd7557efa4aacb3bd711c966375a
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-11-22 17:03:32 +0100

    drivers: timer: nRF SoC Series RTC system clock implementation
    The nRF5x series SoCs do not implement systick, hence we disable
    CORTEX_M_SYSTICK.
    Instead, use nRF SoC Series NRF_RTC1 for system clock interfaces.
    The kernel system clock interface is implemented using the low
    power real time counter NRF_RTC1. NRF_RTC0 is used by the BLE
    controller.
    
    In addition, cleanup nRF5x series defconfig to be consistent.
    
    Jira: ZEP-742
    Jira: ZEP-1308
    Jira: ZEP-1315
    
    Change-id: I0f6cc1836fe0820a65f2cbb02cf5ae7e9eb92e1d
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: a100ada86639333dd0583ebf67cb9c1ff31d30e4

---
+++ drivers/timer/nrf_rtc_timer.c
@@ -0,0 +1,6 @@
+DECL|RTC_TICKS|macro|RTC_TICKS
+DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
+DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t ticks)
+DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
+DECL|rtc1_nrf5_isr|function|static void rtc1_nrf5_isr(void *arg)
+DECL|sys_cycle_get_32|function|uint32_t sys_cycle_get_32(void)
