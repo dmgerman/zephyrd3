e6dd778942c73b8de71734a75bd7ff2dc13da88f
Piyush Itankar <piyush.t.itankar@intel.com>
2016-11-22 17:43:14 +0530

    Bluetooth: A2DP: Added Preset Structure
    Added structure to account for the preset. The user can use the
    SBC codec.
    
    Change-Id: Ia2466b6fc166cae919f82a8042f4dd047ed28772
    Signed-off-by: Piyush Itankar <piyush.t.itankar@intel.com>
    
    Former-commit-id: 160dfdf7060485ee8b869d9a49804827bebd6b1c

--- include/bluetooth/a2dp.h
+++ include/bluetooth/a2dp.h
@@ -1,11 +1,23 @@
+DECL|BT_A2DP_ATRAC|enumerator|BT_A2DP_ATRAC = 0x04,
 DECL|BT_A2DP_AUDIO|enumerator|BT_A2DP_AUDIO = 0x00,
+DECL|BT_A2DP_MPEG1|enumerator|BT_A2DP_MPEG1 = 0x01,
+DECL|BT_A2DP_MPEG2|enumerator|BT_A2DP_MPEG2 = 0x02,
 DECL|BT_A2DP_MULTIMEDIA|enumerator|BT_A2DP_MULTIMEDIA = 0x02
+DECL|BT_A2DP_SBC|enumerator|BT_A2DP_SBC = 0x00,
 DECL|BT_A2DP_SINK|enumerator|BT_A2DP_SINK = 1
 DECL|BT_A2DP_SOURCE|enumerator|BT_A2DP_SOURCE = 0,
+DECL|BT_A2DP_VENDOR|enumerator|BT_A2DP_VENDOR = 0xff
 DECL|BT_A2DP_VIDEO|enumerator|BT_A2DP_VIDEO = 0x01,
 DECL|MEDIA_TYPE|enum|enum MEDIA_TYPE {
 DECL|ROLE_TYPE|enum|enum ROLE_TYPE {
 DECL|__BT_A2DP_H|macro|__BT_A2DP_H
+DECL|bt_a2dp_codec_id|enum|enum bt_a2dp_codec_id {
 DECL|bt_a2dp_endpoint|struct|struct bt_a2dp_endpoint {
+DECL|bt_a2dp_preset|struct|struct bt_a2dp_preset {
 DECL|bt_a2dp_stream|struct|struct bt_a2dp_stream {
+DECL|caps|member|struct bt_a2dp_preset *caps;
+DECL|codec_id|member|uint8_t codec_id;
 DECL|info|member|struct bt_avdtp_seid_lsep info;
+DECL|len|member|uint8_t len;
+DECL|preset|member|struct bt_a2dp_preset *preset;
+DECL|preset|member|uint8_t preset[0];
