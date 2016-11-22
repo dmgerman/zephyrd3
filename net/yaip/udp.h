e1c778e6e34c8307f2f0ac50c7be47e5897da4fb
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-22 08:41:58 +0200

    net: udp: Add util to create UDP packet
    Create utility function that creates an UDP packet.
    Use that function by net_udp_append().
    
    Change-Id: I65b911a41910f812f8754ac1c787790ba63c637d
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 5165eb7e7e44353dfc77b3e70c3fe9e8a98e5fda

--- net/yaip/udp.h
+++ net/yaip/udp.h
@@ -1,4 +1,6 @@
 DECL|__UDP_H|macro|__UDP_H
+DECL|net_udp_append_raw|function|static inline struct net_buf *net_udp_append_raw(struct net_buf *buf, uint16_t src_port, uint16_t dst_port)
+DECL|net_udp_append_raw|macro|net_udp_append_raw
 DECL|net_udp_append|function|static inline struct net_buf *net_udp_append(struct net_context *context, struct net_buf *buf, uint16_t port)
 DECL|net_udp_append|macro|net_udp_append
 DECL|net_udp_init|function|static inline void net_udp_init(void)
