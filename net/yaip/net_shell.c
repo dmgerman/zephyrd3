6afba62b021fac657e0771930956a94478ba8b5b
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-23 12:52:08 +0200

    net: shell: Add command to view active network contexts
    The "net conn" net-shell command will print out currently
    active network contexts (sockets).
    
    Change-Id: Iaf3ced1a4b7ab10291c0b87eecd9e9bedb486e06
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9148567dbd3eda15030567040e46e0e4827de635

--- net/yaip/net_shell.c
+++ net/yaip/net_shell.c
@@ -1,8 +1,10 @@
+DECL|ADDR_LEN|macro|ADDR_LEN
 DECL|GET_STAT|macro|GET_STAT
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|NET_SHELL_MODULE|macro|NET_SHELL_MODULE
 DECL|addrstate2str|function|static inline const char *addrstate2str(enum net_addr_state addr_state)
 DECL|addrtype2str|function|static inline const char *addrtype2str(enum net_addr_type addr_type)
+DECL|context_cb|function|static void context_cb(struct net_context *context, void *user_data)
 DECL|dhcpv4state2str|function|static inline const char *dhcpv4state2str(enum net_dhcpv4_state state)
 DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
 DECL|iface_per_mcast_route_cb|function|static void iface_per_mcast_route_cb(struct net_if *iface, void *user_data)
@@ -12,6 +14,7 @@ DECL|net_print_statistics|function|static inline void net_print_statistics(void)
 DECL|net_shell_init|function|void net_shell_init(void)
 DECL|route_cb|function|static void route_cb(struct net_route_entry *entry, void *user_data)
 DECL|route_mcast_cb|function|static void route_mcast_cb(struct net_route_entry_mcast *entry, void *user_data)
+DECL|shell_cmd_conn|function|static int shell_cmd_conn(int argc, char *argv[])
 DECL|shell_cmd_help|function|static int shell_cmd_help(int argc, char *argv[])
 DECL|shell_cmd_iface|function|static int shell_cmd_iface(int argc, char *argv[])
 DECL|shell_cmd_mem|function|static int shell_cmd_mem(int argc, char *argv[])
