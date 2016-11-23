75a63df9763abb89c64a6b4b3f34a7a6567036b9
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-11-23 20:28:28 +0000

    sensor: Add nRF5 temperature driver.
    Basic interrupt driven driver for the nRF5 onboard temperature sensor.
    
    Change-Id: Id0ac303293b8e8b8285b19bcda31284ee6617105
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: a94437c03c2fb2835eab6e4b350d6353361627ec

---
+++ drivers/sensor/nrf5/temp_nrf5.c
@@ -0,0 +1,14 @@
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|TEMP_NRF5_TEMP_SCALE|macro|TEMP_NRF5_TEMP_SCALE
+DECL|clk_m16_dev|member|struct device *clk_m16_dev;
+DECL|sample|member|int32_t sample;
+DECL|sync|member|device_sync_call_t sync;
+DECL|temp_nrf5_attr_set|function|static int temp_nrf5_attr_set(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|temp_nrf5_channel_get|function|static int temp_nrf5_channel_get(struct device *dev,enum sensor_channel chan, struct sensor_value *val)
+DECL|temp_nrf5_data|struct|struct temp_nrf5_data {
+DECL|temp_nrf5_driver_api|variable|temp_nrf5_driver_api
+DECL|temp_nrf5_driver|variable|temp_nrf5_driver
+DECL|temp_nrf5_init|function|static int temp_nrf5_init(struct device *dev)
+DECL|temp_nrf5_isr|function|static void temp_nrf5_isr(void *arg)
+DECL|temp_nrf5_sample_fetch|function|static int temp_nrf5_sample_fetch(struct device *dev, enum sensor_channel chan)
