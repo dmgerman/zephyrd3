3b2e8b68f5860ffed9ad2e55fb49491d7d062ad5
Ramesh Thomas <ramesh.thomas@intel.com>
2016-11-23 19:49:48 -0800

    quark_se_ss: power_mgmt: Remove redundant int trigger setting
    Zephyr is always setting irqs to be level triggered as
    required by the core. It is unnecessary to set it to
    level again while entering sleep states.
    
    Change-Id: I10f919d619af2e1ab05dc85a67766929b6ae9402
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: aae7ac07656a0d1c44a68a15f751aaffef6ec7c5

--- arch/arc/soc/quark_se_c1000_ss/power.c
+++ arch/arc/soc/quark_se_c1000_ss/power.c
@@ -5,7 +5,4 @@ DECL|SLEEP_MODE_CORE_OFF|macro|SLEEP_MODE_CORE_OFF
 DECL|SLEEP_MODE_CORE_TIMERS_RTC_OFF|macro|SLEEP_MODE_CORE_TIMERS_RTC_OFF
 DECL|_sys_soc_power_state_post_ops|function|void _sys_soc_power_state_post_ops(enum power_states state)
 DECL|_sys_soc_set_power_state|function|void _sys_soc_set_power_state(enum power_states state)
-DECL|aonpt_trigger|variable|aonpt_trigger
 DECL|enter_arc_state|function|static void enter_arc_state(int mode)
-DECL|restore_interrupt_trigger|function|static void restore_interrupt_trigger(void)
-DECL|rtc_trigger|variable|rtc_trigger
