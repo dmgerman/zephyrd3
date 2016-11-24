6cfed716033faf2a8a66de26df9ad36a59bdda63
Flavio Santes <flavio.santes@intel.com>
2016-11-24 13:58:25 -0600

    samples/nfc: Update sample app
    This commit updates the nfc_hello sample application:
    
    - Use new kernel API
    - Update testcase.ini platform whitelist
    - Coding style issues
    
    Jira: ZEP-1348
    
    Change-Id: I2c17e40d654e33a46af02ee2f20d4a88136fcf2d
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 5f52743f094eb13b9a7726e63916ee06f7dc8105

--- samples/nfc/nfc_hello/src/main.c
+++ samples/nfc/nfc_hello/src/main.c
@@ -1,9 +1,10 @@
 DECL|BUF_MAXSIZE|macro|BUF_MAXSIZE
-DECL|D|macro|D
-DECL|buf|variable|buf
+DECL|SLEEP_TIME|macro|SLEEP_TIME
 DECL|main|function|void main(void)
 DECL|msg_dump|function|static void msg_dump(const char *s, uint8_t *data, unsigned len)
 DECL|nci_reset|variable|nci_reset
+DECL|rx_buf|variable|rx_buf
+DECL|tx_buf|variable|tx_buf
 DECL|uart1_dev|variable|uart1_dev
 DECL|uart1_init|function|static void uart1_init(void)
 DECL|uart1_isr|function|static void uart1_isr(struct device *x)
