5cffadfa4105b1bd4f88336a2b725c0620efdaaf
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2016-11-25 20:10:58 +0200

    net: Check duplicate IPv6 address when adding address
    Same IPv6 address can only be added once to the network interface.
    
    Change-Id: Iabc02ffb4026bdccc46e5b2e7ffee82b3c0a59ea
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 852c117d6fa76e441bfad0acce2fbf9668816ff9

--- net/yaip/net_if.c
+++ net/yaip/net_if.c
@@ -13,6 +13,7 @@ DECL|debug_check_packet|macro|debug_check_packet
 DECL|get_length|function|static inline uint8_t get_length(struct in6_addr *src, struct in6_addr *dst)
 DECL|init_tx_queue|function|static inline void init_tx_queue(struct net_if *iface)
 DECL|ipv6_addr_expired|function|static void ipv6_addr_expired(struct k_work *work)
+DECL|ipv6_addr_find|function|static struct net_if_addr *ipv6_addr_find(struct net_if *iface, struct in6_addr *addr)
 DECL|ipv6_router_expired|function|static void ipv6_router_expired(struct k_work *work)
 DECL|is_proper_ipv6_address|function|static inline bool is_proper_ipv6_address(struct net_if_addr *addr)
 DECL|link_callbacks|variable|link_callbacks
