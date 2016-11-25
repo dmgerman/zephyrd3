95ba7a62521b3871238643a8f14abf7c3e4f9def
Erwan Gouriou <erwan.gouriou@linaro.org>
2016-11-25 17:21:26 +0100

    pwm/stm32: add PWM driver for STM32 F1/F4/L4
    Provide PWM driver for STM32 series F1/F4/L4.
    Driver is ported on STM32Cube HAL and should support other STM32
    series with minor updates.
    Configuration is done so that PWM sample driver could be run on
    all nucleo boards supporting PWM driver.
    
    Change-Id: I6522a565451085df932e0eefd8404268380b5bfe
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    
    Former-commit-id: 4827f2f3863d5957b5747e9faab1362d7a08f39c

---
+++ drivers/pwm/pwm_stm32.h
@@ -0,0 +1,9 @@
+DECL|__PWM_STM32_H__|macro|__PWM_STM32_H__
+DECL|clock_subsys|member|clock_control_subsys_t clock_subsys;
+DECL|clock|member|struct device *clock;
+DECL|hpwm|member|TIM_HandleTypeDef hpwm;
+DECL|pclken|member|struct stm32f4x_pclken pclken;
+DECL|pwm_base|member|uint32_t pwm_base;
+DECL|pwm_prescaler|member|uint32_t pwm_prescaler;
+DECL|pwm_stm32_config|struct|struct pwm_stm32_config {
+DECL|pwm_stm32_data|struct|struct pwm_stm32_data {
