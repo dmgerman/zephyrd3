ae8dc91afb212f4752b8ac0da58cdb8e3a54165d
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-11-27 10:23:07 +0000

    sensor/lsm9ds0_gyro: Limit name space, add static.
    Change-Id: Ifff624a2c0f619d7bb3e005b4e52b4602730a66d
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 40d2572d121a791238b73f774c1e75c41de32b97

--- drivers/sensor/lsm9ds0_gyro/lsm9ds0_gyro.c
+++ drivers/sensor/lsm9ds0_gyro/lsm9ds0_gyro.c
@@ -9,7 +9,7 @@ DECL|lsm9ds0_gyro_data|variable|lsm9ds0_gyro_data
 DECL|lsm9ds0_gyro_fs_table|variable|lsm9ds0_gyro_fs_table
 DECL|lsm9ds0_gyro_get_channel|function|static inline int lsm9ds0_gyro_get_channel(enum sensor_channel chan, struct sensor_value *val, struct lsm9ds0_gyro_data *data, float numerator)
 DECL|lsm9ds0_gyro_init_chip|function|static int lsm9ds0_gyro_init_chip(struct device *dev)
-DECL|lsm9ds0_gyro_init|function|int lsm9ds0_gyro_init(struct device *dev)
+DECL|lsm9ds0_gyro_init|function|static int lsm9ds0_gyro_init(struct device *dev)
 DECL|lsm9ds0_gyro_power_ctrl|function|static inline int lsm9ds0_gyro_power_ctrl(struct device *dev, int power, int x_en, int y_en, int z_en)
 DECL|lsm9ds0_gyro_samp_freq_table|variable|lsm9ds0_gyro_samp_freq_table
 DECL|lsm9ds0_gyro_sample_fetch|function|static int lsm9ds0_gyro_sample_fetch(struct device *dev, enum sensor_channel chan)
