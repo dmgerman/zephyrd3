c7b0b7e7a7266be6aed1c341a8e8a24cc893ced8
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-11-27 10:22:28 +0000

    sensor/lps25hb: Limit name space add static.
    Change-Id: I707fd3666f27a982f9b4ef0facf7a3a211db8914
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: f112e8e9bcdc7fa54518abb090791a4ff5892f1e

--- drivers/sensor/lps25hb/lps25hb.c
+++ drivers/sensor/lps25hb/lps25hb.c
@@ -3,7 +3,7 @@ DECL|lps25hb_channel_get|function|static int lps25hb_channel_get(struct device *
 DECL|lps25hb_config|variable|lps25hb_config
 DECL|lps25hb_data|variable|lps25hb_data
 DECL|lps25hb_init_chip|function|static int lps25hb_init_chip(struct device *dev)
-DECL|lps25hb_init|function|int lps25hb_init(struct device *dev)
+DECL|lps25hb_init|function|static int lps25hb_init(struct device *dev)
 DECL|lps25hb_power_ctrl|function|static inline int lps25hb_power_ctrl(struct device *dev, uint8_t value)
 DECL|lps25hb_press_convert|function|static inline void lps25hb_press_convert(struct sensor_value *val, int32_t raw_val)
 DECL|lps25hb_sample_fetch|function|static int lps25hb_sample_fetch(struct device *dev,enum sensor_channel chan)
