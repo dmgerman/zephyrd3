c76eb4e5264bf7cf9fb178452adcb2af76a3a836
Johan Hedberg <johan.hedberg@intel.com>
2016-11-28 09:55:19 +0200

    printk: Refactor to allow passing a parsing context around
    Pass a parsing context around, allowing counting the number of
    characters written, which is then returned in the return value of
    printk(). This makes printk more similar to printf and prepares the
    way to extend the implementation to support printing to strings.
    
    Change-Id: Ib28a18a4f36fc58b98b228fd5763b2c05f5af7bc
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 3ab60466ded5ac27192eee0c626a0278b24dce08

--- misc/printk.c
+++ misc/printk.c
@@ -1,7 +1,11 @@
 DECL|__printk_hook_install|function|void __printk_hook_install(int (*fn)(int))
 DECL|_char_out|variable|_char_out
 DECL|_nop_char_out|function|static int _nop_char_out(int c)
-DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(const unsigned long num, int pad_zero, int min_width)
-DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(const unsigned long num, int pad_zero, int min_width)
-DECL|_vprintk|function|static inline void _vprintk(const char *fmt, va_list ap)
-DECL|printk|function|void printk(const char *fmt, ...)
+DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(out_func_t out, void *ctx, const unsigned long num, int pad_zero, int min_width)
+DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(out_func_t out, void *ctx, const unsigned long num, int pad_zero, int min_width)
+DECL|_vprintk|function|static inline void _vprintk(out_func_t out, void *ctx, const char *fmt, va_list ap)
+DECL|char_out|function|static int char_out(int c, struct out_context *ctx)
+DECL|count|member|int count;
+DECL|out_context|struct|struct out_context {
+DECL|out_func_t|typedef|typedef int (*out_func_t)(int c, void *ctx);
+DECL|printk|function|int printk(const char *fmt, ...)
