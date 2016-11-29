f16a6bb74a8cb474871036ddbff312429866cf66
Benjamin Walsh <benjamin.walsh@windriver.com>
2016-11-29 17:09:31 -0500

    arc: move disabling/invalidation of caches earlier
    Too much code might run with a stale icache, before _PrepC(), so move
    the invalidation/disabling code earlier. The asm code does the exact
    same thing disable_icache() and invalidate_dcache() were doing.
    
    Change-Id: If52f4e4a1de546fb82873c91ead95614a44b106d
    Signed-off-by: Benjamin Walsh <benjamin.walsh@windriver.com>
    
    Former-commit-id: 4e8a54c1e398afce120854044443b4e1efa768ec

--- arch/arc/core/prep_c.c
+++ arch/arc/core/prep_c.c
@@ -1,4 +1,2 @@
 DECL|_PrepC|function|void _PrepC(void)
 DECL|adjust_vector_table_base|function|static void adjust_vector_table_base(void)
-DECL|disable_icache|function|static void disable_icache(void)
-DECL|invalidate_dcache|function|static void invalidate_dcache(void)
