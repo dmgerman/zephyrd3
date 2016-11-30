9c5956bd3450d34478b50070c66ca063e92414a7
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-11-30 17:34:28 +0100

    drivers: timer: Fix missing sys_clock_disable in nrf_rtc_timer
    Change-id: Ia453b8484050c8431d2f793e8f5726684afa21f4
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 883f9c39f7d2cce446e095e28e0e6f77db67ab18

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -3,4 +3,5 @@ DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *d
 DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t ticks)
 DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
 DECL|rtc1_nrf5_isr|function|static void rtc1_nrf5_isr(void *arg)
+DECL|sys_clock_disable|function|void sys_clock_disable(void)
 DECL|sys_cycle_get_32|function|uint32_t sys_cycle_get_32(void)
