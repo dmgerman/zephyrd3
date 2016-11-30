0e32ed73fcdeef1af0f1170d325eefb89e3ec941
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-11-30 18:06:06 +0100

    drivers: timer: replace deprecated interface in nrf_rtc_timer
    Rename sys_cycle_get_32 to k_cycle_get_32 in nrf_rtc_timer.
    
    Change-id: I50af876b254dc5bc3dbb2ccfdf27d33bc4b8d5e5
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 6276ca68c8dd73d4c3999c934bd8450fc1b03986

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -2,6 +2,6 @@ DECL|RTC_TICKS|macro|RTC_TICKS
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
 DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t ticks)
 DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
+DECL|k_cycle_get_32|function|uint32_t k_cycle_get_32(void)
 DECL|rtc1_nrf5_isr|function|static void rtc1_nrf5_isr(void *arg)
 DECL|sys_clock_disable|function|void sys_clock_disable(void)
-DECL|sys_cycle_get_32|function|uint32_t sys_cycle_get_32(void)
