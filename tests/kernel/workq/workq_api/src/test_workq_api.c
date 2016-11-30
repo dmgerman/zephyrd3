fc8078568918d9dcc97e8ca7bbb4b89ec26377af
Sharron LIU <sharron.liu@intel.com>
2016-11-30 15:14:22 +0800

    tests: kernel: added test_workq_api
    Change-Id: Iaadba1a720130a496a83c245c7da4b95dff168fd
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 67af6f88442e56d9632bf0e777abb41dbac73bf5

---
+++ tests/kernel/workq/workq_api/src/test_workq_api.c
@@ -0,0 +1,27 @@
+DECL|NUM_OF_WORK|macro|NUM_OF_WORK
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|delayed_work_sleepy|variable|delayed_work_sleepy
+DECL|delayed_work|variable|delayed_work
+DECL|sync_sema|variable|sync_sema
+DECL|tdelayed_work_cancel|function|static void tdelayed_work_cancel(void *data)
+DECL|tdelayed_work_submit|function|static void tdelayed_work_submit(void *data)
+DECL|test_delayed_work_cancel_from_queue_isr|function|void test_delayed_work_cancel_from_queue_isr(void)
+DECL|test_delayed_work_cancel_from_queue_thread|function|void test_delayed_work_cancel_from_queue_thread(void)
+DECL|test_delayed_work_cancel_isr|function|void test_delayed_work_cancel_isr(void)
+DECL|test_delayed_work_cancel_thread|function|void test_delayed_work_cancel_thread(void)
+DECL|test_delayed_work_submit_isr|function|void test_delayed_work_submit_isr(void)
+DECL|test_delayed_work_submit_thread|function|void test_delayed_work_submit_thread(void)
+DECL|test_delayed_work_submit_to_queue_isr|function|void test_delayed_work_submit_to_queue_isr(void)
+DECL|test_delayed_work_submit_to_queue_thread|function|void test_delayed_work_submit_to_queue_thread(void)
+DECL|test_work_submit_isr|function|void test_work_submit_isr(void)
+DECL|test_work_submit_thread|function|void test_work_submit_thread(void)
+DECL|test_work_submit_to_queue_isr|function|void test_work_submit_to_queue_isr(void)
+DECL|test_work_submit_to_queue_thread|function|void test_work_submit_to_queue_thread(void)
+DECL|test_workq_start_before_submit|function|void test_workq_start_before_submit(void)
+DECL|tstack|variable|tstack
+DECL|twork_submit|function|static void twork_submit(void *data)
+DECL|work_handler|function|static void work_handler(struct k_work *w)
+DECL|work_sleepy|function|static void work_sleepy(struct k_work *w)
+DECL|workq|variable|workq
+DECL|work|variable|work
