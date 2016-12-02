0f0a9bd31980b20945ec48b7a28671023122d934
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-12-02 10:33:30 +0100

    drivers: ieee802154: cc2520: Rename RX stack Kconfig option
    Fibers do not exist anymore in unified kernel.
    Also, let's just use that option directly so removing the internal
    define which is then useless.
    
    Change-Id: I1179d84401934b339d3a9ccd12f6145a120bcb2f
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 589a361e53bfa8685a9be4d39665d7937311c479

--- drivers/ieee802154/ieee802154_cc2520.h
+++ drivers/ieee802154/ieee802154_cc2520.h
@@ -1,4 +1,3 @@
-DECL|CONFIG_CC2520_RX_STACK_SIZE|macro|CONFIG_CC2520_RX_STACK_SIZE
 DECL|DEFINE_FREG_READ|macro|DEFINE_FREG_READ
 DECL|DEFINE_FREG_WRITE|macro|DEFINE_FREG_WRITE
 DECL|DEFINE_MEM_WRITE|macro|DEFINE_MEM_WRITE
@@ -12,7 +11,7 @@ DECL|__IEEE802154_CC2520_H__|macro|__IEEE802154_CC2520_H__
 DECL|_cc2520_command_strobe_snop|function|static inline bool _cc2520_command_strobe_snop(struct cc2520_spi *spi, uint8_t instruction)
 DECL|_cc2520_command_strobe|function|static inline bool _cc2520_command_strobe(struct cc2520_spi *spi, uint8_t instruction)
 DECL|cc2520_context|struct|struct cc2520_context {
-DECL|cc2520_rx_stack|member|char __stack cc2520_rx_stack[CONFIG_CC2520_RX_STACK_SIZE];
+DECL|cc2520_rx_stack|member|char __stack cc2520_rx_stack[CONFIG_TI_CC2520_RX_STACK_SIZE];
 DECL|cc2520_spi|struct|struct cc2520_spi {
 DECL|cmd_buf|member|uint8_t cmd_buf[12];
 DECL|dev|member|struct device *dev;
