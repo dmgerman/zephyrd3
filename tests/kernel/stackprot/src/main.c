a6a4ba8915449ea42cca9c68f01b9ed84e8f101c
Anas Nashif <anas.nashif@intel.com>
2016-12-03 16:06:16 -0500

    tests: import stack protection testcase to unified
    Change-Id: I5a867b7bfed56ebfe79b3f14f772c6ab7482d229
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 0dd774df105f34520ea2e098a539abd62e1de8b2

---
+++ tests/kernel/stackprot/src/main.c
@@ -0,0 +1,9 @@
+DECL|PRIORITY|macro|PRIORITY
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|alt_thread_stack_area|variable|alt_thread_stack_area
+DECL|alternate_thread|function|void alternate_thread(void)
+DECL|check_input|function|void check_input(const char *name, const char *input)
+DECL|count|variable|count
+DECL|main|function|void main(void)
+DECL|print_loop|function|void print_loop(const char *name)
+DECL|ret|variable|ret
