08c8d02b889d6f71104eb89a9d367d8d1ad1f288
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-12-06 21:58:49 +0000

    random: Rewrite sys_rand32_init() with SYS_INIT()
    Use the SYS_INIT() mechanism to invoke the sys_rand32_init() function
    in random drivers that require an initializer.  Remove all empty
    sys_rand32_init() instances.
    
    The existing explicit sys_rand32_init() function runs immediately after
    PRE_KERNEL_2 before stack canaries are initialized.  In order to get
    equivalent behaviour with sys_rand32_init() we set SYS_INIT() to
    initialize the random drivers at the lowest priority of PRE_KERNEL_2.
    
    Change-Id: I4521e44daac806bc4eef01ce7fdf2ba5367e0587
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: a715194d431e8fc81b9e938af1234e65e8065e8f

--- drivers/random/rand32_timer.c
+++ drivers/random/rand32_timer.c
@@ -1,4 +1,3 @@
 DECL|_RAND32_INC|macro|_RAND32_INC
 DECL|_rand32_counter|variable|_rand32_counter
 DECL|sys_rand32_get|function|uint32_t sys_rand32_get(void)
-DECL|sys_rand32_init|function|void sys_rand32_init(void)
