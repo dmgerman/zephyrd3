e20dd51448d41b0961bb35618c930e845592d370
Flavio Santes <flavio.santes@intel.com>
2016-12-06 10:31:05 -0600

    iot/dns: Introduce the dns_context structure
    This commit introduces the dns_context structure.
    
    This new structure will reduce stack overhead due to
    the simplication of the dns_resolve routine signature.
    
    Furthermore, the timeout parameter is now int32_t
    instead of uint32_t.
    
    The dns sample application is also updated.
    
    Change-Id: I5d789656bacbd23c4654edce5d116a88dc42c354
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 0d257a009af418a810c9746235bbee90187f895e

--- lib/iot/dns/dns_client.c
+++ lib/iot/dns/dns_client.c
@@ -9,11 +9,9 @@ DECL|DNS_RESOLVER_MAX_BUF_SIZE|macro|DNS_RESOLVER_MAX_BUF_SIZE
 DECL|DNS_RESOLVER_MIN_BUF|macro|DNS_RESOLVER_MIN_BUF
 DECL|DNS_RESOLVER_QUERIES|macro|DNS_RESOLVER_QUERIES
 DECL|cb_recv|function|void cb_recv(struct net_context *context, struct net_buf *buf, int status,void *user_data)
-DECL|dns4_resolve|function|int dns4_resolve(struct net_context *ctx, struct in_addr *addresses, int *items, int elements, char *name, struct sockaddr *dns_server, uint32_t timeout)
-DECL|dns6_resolve|function|int dns6_resolve(struct net_context *ctx, struct in6_addr *addresses, int *items, int elements, char *name, struct sockaddr *dns_server, uint32_t timeout)
 DECL|dns_init|function|int dns_init(void)
-DECL|dns_read|function|int dns_read(struct net_context *ctx, struct net_buf *dns_data, uint32_t timeout, uint16_t dns_id, enum dns_query_type type, uint8_t *addresses, int *items, int elements, uint8_t *cname, uint16_t *cname_len)
-DECL|dns_recv|function|int dns_recv(struct net_context *ctx, struct net_buf **buf, uint32_t timeout)
-DECL|dns_resolve|function|int dns_resolve(struct net_context *ctx, uint8_t *addresses, int *items,int elements, char *name, enum dns_query_type type, struct sockaddr *dns_server, uint32_t timeout)
-DECL|dns_write|function|int dns_write(struct net_context *ctx, struct net_buf *dns_data, uint32_t timeout, uint16_t dns_id, enum dns_query_type type, struct net_buf *dns_qname, struct sockaddr *dns_server)
+DECL|dns_read|function|int dns_read(struct net_context *net_ctx, struct net_buf *dns_data, int32_t timeout, uint16_t dns_id, enum dns_query_type type, uint8_t *addresses, uint8_t *items, uint8_t elements, uint8_t *cname, uint16_t *cname_len)
+DECL|dns_recv|function|int dns_recv(struct net_context *net_ctx, struct net_buf **buf, int32_t timeout)
+DECL|dns_resolve|function|int dns_resolve(struct dns_context *ctx)
+DECL|dns_write|function|int dns_write(struct net_context *net_ctx, struct net_buf *dns_data, int32_t timeout, uint16_t dns_id, enum dns_query_type type, struct net_buf *dns_qname, struct sockaddr *dns_server)
 DECL|nbuf_copy|function|int nbuf_copy(struct net_buf *dst, struct net_buf *src, int offset, int len)
