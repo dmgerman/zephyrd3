37156379a1d6a3f93e669d97af7c86a37cc2b3cd
Baohong Liu <baohong.liu@intel.com>
2016-12-07 14:52:36 -0800

    samples: spi_fram: correct syntax error and update comments
    Correct some syntax error and update some comments. There is
    no functionality change.
    
    Change-Id: I86c922c8aabcc4464b86f33ad7edf6056b1d765c
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 3b831465a4c581e4d9963dd6ec696aaa2bb312c2

--- samples/drivers/spi_fujitsu_fram/src/main.c
+++ samples/drivers/spi_fujitsu_fram/src/main.c
@@ -1,9 +1,9 @@
-DECL|MAX_DATA_LENGTH|macro|MAX_DATA_LENGTH
-DECL|MB85RS64V_MANUFACTURER_ID|macro|MB85RS64V_MANUFACTURER_ID
+DECL|MAX_USER_DATA_LENGTH|macro|MAX_USER_DATA_LENGTH
+DECL|MB85RS64V_MANUFACTURER_ID_CMD|macro|MB85RS64V_MANUFACTURER_ID_CMD
 DECL|cmp_data|variable|cmp_data
 DECL|data|variable|data
 DECL|main|function|void main(void)
-DECL|mb85rs64v_read_id|function|int mb85rs64v_read_id(struct device *dev)
-DECL|read_bytes|function|int read_bytes(struct device *dev, uint16_t addr, uint8_t *data, uint32_t num_bytes)
+DECL|mb85rs64v_read_id|function|static int mb85rs64v_read_id(struct device *dev)
+DECL|read_bytes|function|static int read_bytes(struct device *dev, uint16_t addr, uint8_t *data, uint32_t num_bytes)
 DECL|spi_buffer|variable|spi_buffer
-DECL|write_bytes|function|int write_bytes(struct device *dev, uint16_t addr,uint8_t *data, uint32_t num_bytes)
+DECL|write_bytes|function|static int write_bytes(struct device *dev, uint16_t addr, uint8_t *data, uint32_t num_bytes)
