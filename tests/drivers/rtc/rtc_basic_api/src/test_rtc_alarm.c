3fda6d57dc3840be3c810c19ef752731225c17f0
jing wang <jing.j.wang@intel.com>
2016-12-08 16:42:36 +0800

    tests: add rtc driver test case
    the test include 2 ztest cases, verfy below apis:
        rtc_enable()
        rtc_disable()
        rtc_read()
        rtc_set_config()
        rtc_set_alarm()
        rtc_get_pending_int()
    
    Change-Id: Ifca2541312c5ec8d450803f1b6e4f5064098e7fe
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: ed754ecf6dbd74aecabf2d03086bb847d9e061fc

---
+++ tests/drivers/rtc/rtc_basic_api/src/test_rtc_alarm.c
@@ -0,0 +1,4 @@
+DECL|rtc_alarm_callback|function|static void rtc_alarm_callback(struct device *rtc_dev)
+DECL|rtc_alarm_up|variable|rtc_alarm_up
+DECL|test_alarm|function|static int test_alarm(void)
+DECL|test_rtc_alarm|function|void test_rtc_alarm(void)
---
+++ tests/drivers/rtc/rtc_basic_api/src/test_rtc_calendar.c
@@ -0,0 +1,2 @@
+DECL|test_rtc_calendar|function|void test_rtc_calendar(void)
+DECL|test_task|function|static int test_task(void)
