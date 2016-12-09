8d8d7caa18c94e4a764c42d33a18065f8b767453
Baohong Liu <baohong.liu@intel.com>
2016-12-09 15:41:15 -0800

    samples: aio: fix some coding style issues
    Change-Id: I799b8543e50716fec1685d32810419c7c3d94bf2
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: e0359b5b5c9a2c84cd0a6f66c5d8fb2b4878bd1d

--- samples/drivers/aio_comparator/src/main.c
+++ samples/drivers/aio_comparator/src/main.c
@@ -2,7 +2,7 @@ DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|ain_idx|member|uint8_t ain_idx;
 DECL|cb_data_t|struct|struct cb_data_t {
 DECL|cb_data|variable|cb_data
-DECL|cb|function|void cb(void *param)
+DECL|cb|function|static void cb(void *param)
 DECL|main|function|void main(void)
 DECL|name|member|char name[50];
 DECL|pol|member|enum aio_cmp_polarity pol;
