4a02fc5d84bbbfd6867d18731349eb4b1ca90e37
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2016-12-09 14:38:56 -0800

    drivers: QMSI AON counter: Simplify driver reentrancy code
    With the use of the IS_ENABLED macro
    (https://gerrit.zephyrproject.org/r/#/c/9882), the driver
    reentrancy code is simplified:
    
    The original const variable which used to serve the purpose of
    making the build to compile out the reentrancy code when the
    reentrancy configuration flag is not enabled is no longer
    needed as the use of the IS_ENABLED macro achieves the same result.
    Also the original 3 wrapper functions around semaphores API are
    not necessary; instead, just use the semaphore API directly.
    
    Jira: ZEP-1251
    
    Change-Id: I23552b77d19044dc94de443018759cb3ec10a52b
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: c0a546ed333f98153f2dd1025500770d9250a727

--- drivers/counter/counter_qmsi_aonpt.c
+++ drivers/counter/counter_qmsi_aonpt.c
@@ -3,10 +3,7 @@ DECL|AONPT_CONTEXT|macro|AONPT_CONTEXT
 DECL|AONPT_HAS_CONTEXT_DATA|macro|AONPT_HAS_CONTEXT_DATA
 DECL|RP_GET|macro|RP_GET
 DECL|RP_GET|macro|RP_GET
-DECL|aon_critical_region_end|function|static void aon_critical_region_end(struct device *dev)
-DECL|aon_critical_region_start|function|static void aon_critical_region_start(struct device *dev)
 DECL|aon_data|struct|struct aon_data {
-DECL|aon_reentrancy_init|function|static void aon_reentrancy_init(struct device *dev)
 DECL|aon_timer_init|function|static int aon_timer_init(struct device *dev)
 DECL|aon_timer_qmsi_api|variable|aon_timer_qmsi_api
 DECL|aon_timer_qmsi_get_pending_int|function|static uint32_t aon_timer_qmsi_get_pending_int(struct device *dev)
@@ -23,7 +20,5 @@ DECL|aonpt_qmsi_set_power_state|macro|aonpt_qmsi_set_power_state
 DECL|aonpt_resume_device_from_suspend|function|static int aonpt_resume_device_from_suspend(struct device *dev)
 DECL|aonpt_suspend_device|function|static int aonpt_suspend_device(struct device *dev)
 DECL|device_power_state|member|uint32_t device_power_state;
-DECL|reentrancy_protection|variable|reentrancy_protection
-DECL|reentrancy_protection|variable|reentrancy_protection
 DECL|sem|member|struct k_sem sem;
 DECL|user_cb|variable|user_cb
