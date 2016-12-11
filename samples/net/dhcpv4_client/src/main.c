3c76f8d6b4af8c5d4d97ea6dff08085b7016557b
Anas Nashif <anas.nashif@intel.com>
2016-12-11 05:59:18 -0500

    kernel: remove NANOKERNEL and MICROKERNEL configs
    Those are legacy and not needed anymore.
    
    Change-Id: I8113114fd60880b3f538612db7702f6129af0a06
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 418058a1238e4ab78474d11aa54bbbd9595c22bd

--- samples/net/dhcpv4_client/src/main.c
+++ samples/net/dhcpv4_client/src/main.c
@@ -1,8 +1,6 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
-DECL|STACKSIZE|macro|STACKSIZE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
-DECL|fiberStack|variable|fiberStack
 DECL|init_app|function|static inline void init_app(void)
 DECL|main_fiber|function|void main_fiber(void)
 DECL|main|function|void main(void)
