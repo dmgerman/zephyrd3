5767cb5701494e542c6048d4347aee99c2a96027
Anas Nashif <anas.nashif@intel.com>
2016-12-11 06:35:54 -0500

    device: do not set struct as deprecated
    Change-Id: I6cbec81e02dc706ba86f27aab76740031f89dbd1
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 79631ad457eb126e94c3476d9b756c6568ce1473

--- include/device.h
+++ include/device.h
@@ -41,7 +41,7 @@ DECL|device_set_power_state|function|static inline int device_set_power_state(st
 DECL|device_suspend|function|static inline int __deprecated device_suspend(struct device *device,int pm_policy)
 DECL|device_sync_call_complete|function|device_sync_call_complete(device_sync_call_t *sync)
 DECL|device_sync_call_init|function|static inline void __deprecated device_sync_call_init(device_sync_call_t *sync)
-DECL|device_sync_call_t|typedef|} __deprecated device_sync_call_t;
+DECL|device_sync_call_t|typedef|} device_sync_call_t;
 DECL|device_sync_call_wait|function|static inline void __deprecated device_sync_call_wait(device_sync_call_t *sync)
 DECL|device|struct|struct device {
 DECL|driver_api|member|const void *driver_api;
