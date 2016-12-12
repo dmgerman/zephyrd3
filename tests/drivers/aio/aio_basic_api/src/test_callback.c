500cfac4bcb0e3b3e0441b5c17f86c08cd156c7d
jing wang <jing.j.wang@intel.com>
2016-12-12 15:55:20 +0800

    tests: add driver aio comparator test case
    the commit cover below apis by testing callback under RISE polarity
        *) aio_cmp_configure
        *) aio_cmd_disabel
    
    Change-Id: Idc9ec3a2694950d0074b69d164ee2c1edac35c9a
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 3d99a0c8783e8bd649c79f8a57d7433261f49dbf

---
+++ tests/drivers/aio/aio_basic_api/src/test_callback.c
@@ -0,0 +1,5 @@
+DECL|callback|function|static void callback(void *param)
+DECL|cb_cnt|variable|cb_cnt
+DECL|set_aio_callback|function|static void set_aio_callback(int polarity, int disable)
+DECL|test_aio_callback_rise_disable|function|void test_aio_callback_rise_disable(void)
+DECL|test_aio_callback_rise|function|void test_aio_callback_rise(void)
