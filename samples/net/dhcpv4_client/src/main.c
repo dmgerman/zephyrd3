d5ab466e0b945f52c53518232ad22dc9c17109a3
Marcus Shawcroft <marcus.shawcroft@arm.com>
2016-12-12 15:41:31 +0000

    dhcpv4: Report address acquisition.
    Use the network management interface to catch and report IPv4 address
    acquisition in the application.
    
    Change-Id: Idd1bcaac90fffbd617600a7713206599e547826f
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 0637511e68c0edcf213fc4b8e5810e57d16d6b9e

--- samples/net/dhcpv4_client/src/main.c
+++ samples/net/dhcpv4_client/src/main.c
@@ -1,8 +1,10 @@
 DECL|NET_DEBUG|macro|NET_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|handler|function|static void handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
 DECL|init_app|function|static inline void init_app(void)
 DECL|main_fiber|function|void main_fiber(void)
 DECL|main|function|void main(void)
+DECL|mgmt_cb|variable|mgmt_cb
 DECL|quit_lock|variable|quit_lock
 DECL|quit|function|static inline void quit(void)
