adcba29691af66e69e6ee1ed56a332c8e92595f4
Baohong Liu <baohong.liu@intel.com>
2016-12-12 21:52:22 -0800

    samples: bmi160: use direct GPIO trigger instead of ipm
    Directly use AON GPIO trigger instead of IPM.
    
    In the past, SW did not allow arc to receive AON GPIO interrupt.
    So, interrupt from BMI160 was routed to x86 cpu. Then, x86 cpu
    passed the event to arc through IPM. But, SW was updated and arc
    is able to directly receive interrupt from AON GPIO now. So, IPM
    is not needed any more. Both IPM and GPIO triggering code is there.
    We just need to change the configuration by modifying the prj
    config file.
    
    The x86 code will be removed since it will not be used any more.
    
    Change-Id: Ibc4250a34218cc52bf4c726cf70617cfdea3b2e8
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 9e4c2e92caca8c299d6014d0870256854322078a

--- samples/sensor/bmi160/x86/src/x86_bmi160.c
+++
@@ -1,6 +0,0 @@
-DECL|BMI160_INTERRUPT_PIN|macro|BMI160_INTERRUPT_PIN
-DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|cb|variable|cb
-DECL|gpio_callback|function|static void gpio_callback(struct device *port, struct gpio_callback *cb, uint32_t pins)
-DECL|ipm|variable|ipm
-DECL|main|function|void main(void)
