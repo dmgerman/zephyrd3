b7d2066209ef7360eca52dd36c5a113878298541
Carles Cufi <carles.cufi@nordicsemi.no>
2016-12-13 11:55:22 +0100

    drivers: timer: Optimize RTC driver and prevent past events
    The nRF RTC driver, which is used a system clock driver due to the lack
    of SysTick hardware on the SoC, was using too much CPU time in its
    _timer_idle_exit() implementation due to the use of 64-bit arithmetical
    operations. This was causing the ISR wrapper to add excessive latency to
    critical interrupts, causing BLE controller asserts.
    This patch addresses the issue by using exclusively RTC ticks instead of
    OS ticks, thus avoiding the necessity to convert during
    _timer_idle_exit() calls, which are the most critical to interrupt
    latency.
    
    In addition the driver is now able to detect setting tick events in the
    past due to it being interrupted by a higher priority context, and will
    reschedule and trigger the ISR at the same time.
    
    Change-id: I56a3be96b9fdd554c3650012d647af2f0415eb8a
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: ca0951d42d49d607c6fece71871b324b48ccba70

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -2,6 +2,10 @@ DECL|RTC_TICKS|macro|RTC_TICKS
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
 DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t ticks)
 DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
+DECL|isr_ack|variable|isr_ack
+DECL|isr_req|variable|isr_req
 DECL|k_cycle_get_32|function|uint32_t k_cycle_get_32(void)
 DECL|rtc1_nrf5_isr|function|static void rtc1_nrf5_isr(void *arg)
+DECL|rtc_clock_tick_count|variable|rtc_clock_tick_count
+DECL|rtc_compare_set|function|static uint32_t rtc_compare_set(uint32_t rtc_ticks)
 DECL|sys_clock_disable|function|void sys_clock_disable(void)
