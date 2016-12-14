25ea1b6a7cd0d1aea7a2b8efb47d243c49108dad
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2016-12-14 14:31:58 +0100

    net: statistics: Expose relevant information through net mgmt API
    User application can request the information it wants via the generic
    net_mgmt() call, following the NET_REQUEST_STATS_* codes.
    
    Change-Id: Ia9e7d318cf11b7bf8bfaf1ad63c8c985be846cc1
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: a79f9bcbc421dffe08370215842c66a610d5fd97

--- subsys/net/ip/net_stats.c
+++ subsys/net/ip/net_stats.c
@@ -1,4 +1,5 @@
 DECL|PRINT_STATISTICS_INTERVAL|macro|PRINT_STATISTICS_INTERVAL
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|net_print_statistics|function|void net_print_statistics(void)
+DECL|net_stats_get|function|static int net_stats_get(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|net_stats|variable|net_stats
