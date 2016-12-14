0637267956832e2037181976f3f65a96b1476582
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-12-14 15:58:47 -0800

    samples/mbedtls_sslclient: Using native IP stack
    The legacy IP stack was removed from the source tree, so this changes
    were made to adapt to the native IP stack, but this might not work on
    the galileo platform due the ethernet driver not being tested on the
    new stack on TCP, but to have the reference application ready when
    it gets integrated
    
    Jira: ZEP-1448
    
    Change-Id: Ifa6ea0afb3bc532dfb92b09debbf6eee59e38a25
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 71905be8573ef267c4228e1b52e8e41c0fbc20e9

--- samples/net/mbedtls_sslclient/src/tcp.c
+++ samples/net/mbedtls_sslclient/src/tcp.c
@@ -1,8 +1,9 @@
 DECL|CLIENT_IP_ADDR|macro|CLIENT_IP_ADDR
 DECL|INET_FAMILY|macro|INET_FAMILY
 DECL|SERVER_IP_ADDR|macro|SERVER_IP_ADDR
+DECL|client_addr|variable|client_addr
+DECL|server_addr|variable|server_addr
 DECL|tcp_init|function|int tcp_init(struct tcp_context *ctx)
+DECL|tcp_received|function|static void tcp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
 DECL|tcp_rx|function|int tcp_rx(void *context, unsigned char *buf, size_t size)
 DECL|tcp_tx|function|int tcp_tx(void *context, const unsigned char *buf, size_t size)
-DECL|uip_hostaddr|variable|uip_hostaddr
-DECL|uip_netmask|variable|uip_netmask
