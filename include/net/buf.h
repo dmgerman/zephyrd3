648e70c079fad7633a9a69ba9505424f6da45528
Johan Hedberg <johan.hedberg@intel.com>
2016-12-14 08:24:19 +0200

    net: buf: Switch from k_fifo to k_lifo for free buffers
    Using a LIFO instead of a FIFO has the potential benefit that more
    recently in-use buffers may be "cache-hot" and therefore accessed
    faster than least recently used (which is what we get with a FIFO).
    
    Change-Id: I59bb083ca2e00d0d404406540f7db216742a27cf
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 692771fbe95e9e0555011e3f96c073c078d85664

--- include/net/buf.h
+++ include/net/buf.h
@@ -16,7 +16,7 @@ DECL|data|member|uint8_t *data;
 DECL|destroy|member|void (*const destroy)(struct net_buf *buf);
 DECL|flags|member|uint8_t flags;
 DECL|frags|member|struct net_buf *frags;
-DECL|free|member|struct k_fifo free;
+DECL|free|member|struct k_lifo free;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
 DECL|len|member|uint16_t len;
