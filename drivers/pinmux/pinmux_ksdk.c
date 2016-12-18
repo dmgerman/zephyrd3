a420928df88960c0227eec6bf7d842b3f500cefd
Maureen Helm <maureen.helm@nxp.com>
2016-12-18 16:19:27 -0600

    pinmux: Merge ksdk pinmux dev into regular ksdk pinmux driver
    Merges the ksdk pinmux dev driver into the regular ksdk pinmux driver,
    which now exposes the public pinmux API. Removes the private ksdk pinmux
    API and converts the frdm_k64f and hexiwear_k64 boards to use the public
    pinmux API.
    
    Jira: ZEP-958, ZEP-1432
    Change-Id: Ie5f60b604133093050b9c596050cad776d7b7cb3
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: acca033468bf946517d99e6ee33a9509003efdbb

--- drivers/pinmux/pinmux_ksdk.c
+++ drivers/pinmux/pinmux_ksdk.c
@@ -1,1 +1,14 @@
-DECL|pinmux_ksdk_init|function|int pinmux_ksdk_init(void)
+DECL|base|member|PORT_Type *base;
+DECL|clock_ip_name|member|clock_ip_name_t clock_ip_name;
+DECL|pinmux_ksdk_config|struct|struct pinmux_ksdk_config {
+DECL|pinmux_ksdk_driver_api|variable|pinmux_ksdk_driver_api
+DECL|pinmux_ksdk_get|function|static int pinmux_ksdk_get(struct device *dev, uint32_t pin, uint32_t *func)
+DECL|pinmux_ksdk_init|function|static int pinmux_ksdk_init(struct device *dev)
+DECL|pinmux_ksdk_input|function|static int pinmux_ksdk_input(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_ksdk_porta_config|variable|pinmux_ksdk_porta_config
+DECL|pinmux_ksdk_portb_config|variable|pinmux_ksdk_portb_config
+DECL|pinmux_ksdk_portc_config|variable|pinmux_ksdk_portc_config
+DECL|pinmux_ksdk_portd_config|variable|pinmux_ksdk_portd_config
+DECL|pinmux_ksdk_porte_config|variable|pinmux_ksdk_porte_config
+DECL|pinmux_ksdk_pullup|function|static int pinmux_ksdk_pullup(struct device *dev, uint32_t pin, uint8_t func)
+DECL|pinmux_ksdk_set|function|static int pinmux_ksdk_set(struct device *dev, uint32_t pin, uint32_t func)
