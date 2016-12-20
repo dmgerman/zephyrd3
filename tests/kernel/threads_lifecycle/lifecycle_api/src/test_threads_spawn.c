ba589a6e12ee322620bddfcd4a9c0f6f470cd451
jing wang <jing.j.wang@intel.com>
2016-12-20 16:41:43 +0800

    tests: add zephyr thread lifecycle test case with unified kernel
    this commit cover zephyr thread lifecycle relavant apis testing
    k_thread_spawn
    k_current_get
    k_thread_abort
    k_thread_cancel
    k_thread_suspend
    k_thread_resume
    
    Change-Id: I99bff0dd803f6e68e165f3388a3be09425c0596c
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: e23b0ab49f97affaff005155677a71bb121e0204

---
+++ tests/kernel/threads_lifecycle/lifecycle_api/src/test_threads_spawn.c
@@ -0,0 +1,12 @@
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|spawn_prio|variable|spawn_prio
+DECL|test_threads_spawn_delay|function|void test_threads_spawn_delay(void)
+DECL|test_threads_spawn_params|function|void test_threads_spawn_params(void)
+DECL|test_threads_spawn_priority|function|void test_threads_spawn_priority(void)
+DECL|thread_entry_delay|function|static void thread_entry_delay(void *p1, void *p2, void *p3)
+DECL|thread_entry_params|function|static void thread_entry_params(void *p1, void *p2, void *p3)
+DECL|thread_entry_priority|function|static void thread_entry_priority(void *p1, void *p2, void *p3)
+DECL|tp1|variable|tp1
+DECL|tp2|variable|tp2
+DECL|tp3|variable|tp3
+DECL|tstack|variable|tstack
