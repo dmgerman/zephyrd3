ad8bc0c6ec60c0584f2a63c4bca81b0d8aff1e49
Anas Nashif <anas.nashif@intel.com>
2016-12-19 20:25:56 -0500

    kernel: fix all nanokernel usage in comments
    Also include kernel.h instead of nanokernel.h
    
    Change-Id: I65dc5e31b5409b809397296817e2d5e7adf28892
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: dc3d73bf584d146e1255514f1ca055bfc824d13f

--- kernel/include/wait_q.h
+++ kernel/include/wait_q.h
@@ -2,4 +2,4 @@ DECL|_abort_thread_timeout|function|static ALWAYS_INLINE int _abort_thread_timeo
 DECL|_add_thread_timeout|function|_add_thread_timeout(struct k_thread *thread, _wait_q_t *wait_q, int32_t timeout)
 DECL|_get_next_timeout_expiry|macro|_get_next_timeout_expiry
 DECL|_init_thread_timeout|function|static ALWAYS_INLINE void _init_thread_timeout(struct _thread_base *thread_base)
-DECL|_kernel_nanokernel_include_wait_q__h_|macro|_kernel_nanokernel_include_wait_q__h_
+DECL|_kernel_include_wait_q__h_|macro|_kernel_include_wait_q__h_
