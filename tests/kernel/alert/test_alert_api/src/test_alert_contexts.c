ab9db58513f1f83a993ffd348bfe121e87cf9d1b
jing wang <jing.j.wang@intel.com>
2016-12-20 08:28:07 +0800

    tests: add zephyr alerts test case
    the commit cover alert send and receive with 4 types -
    DEFAULT, IGNORE, PENDING, CONSUMED which across thread and isr
    context.
    
    Change-Id: I41dae9ba2dc980bcd768f1220f55b5492bc8ae37
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    
    Former-commit-id: 208fc48d81282eeb361969b4e2cc470a7ed56335

---
+++ tests/kernel/alert/test_alert_api/src/test_alert_contexts.c
@@ -0,0 +1,24 @@
+DECL|PENDING_MAX|macro|PENDING_MAX
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|alert_handler0|function|static int alert_handler0(struct k_alert *alt)
+DECL|alert_handler1|function|static int alert_handler1(struct k_alert *alt)
+DECL|alert_recv|function|static void alert_recv(void)
+DECL|alert_send|function|static void alert_send(void)
+DECL|handler_executed|variable|handler_executed
+DECL|isr_alert|function|static void isr_alert(void)
+DECL|palert|variable|palert
+DECL|tIsr_entry|function|static void tIsr_entry(void *p)
+DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *p3)
+DECL|test_isr_alert_consumed|function|void test_isr_alert_consumed(void)
+DECL|test_isr_alert_default|function|void test_isr_alert_default(void)
+DECL|test_isr_alert_ignore|function|void test_isr_alert_ignore(void)
+DECL|test_isr_alert_pending|function|void test_isr_alert_pending(void)
+DECL|test_isr_kinit_alert|function|void test_isr_kinit_alert(void)
+DECL|test_thread_alert_consumed|function|void test_thread_alert_consumed(void)
+DECL|test_thread_alert_default|function|void test_thread_alert_default(void)
+DECL|test_thread_alert_ignore|function|void test_thread_alert_ignore(void)
+DECL|test_thread_alert_pending|function|void test_thread_alert_pending(void)
+DECL|test_thread_kinit_alert|function|void test_thread_kinit_alert(void)
+DECL|thread_alert|function|static void thread_alert(void)
+DECL|tstack|variable|tstack
