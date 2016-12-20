0f209543784cea914f7892bd627865ac37c1b7de
jing wang <jing.j.wang@intel.com>
2016-12-20 11:22:25 +0800

    tests: add zephyr message queue test case with unified kernel
    the commit cover basic message queue api testing across contexts
    and some typical scenario
    
    Change-Id: I82bb0c6a5df9d4436f5a98f78d1ad989e32282c8
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 333e8d985d9dc517a6634e9593158f7defd72262

---
+++ tests/kernel/msgq/msgq_api/src/test_msgq_fail.c
@@ -0,0 +1,3 @@
+DECL|data|variable|data
+DECL|test_msgq_get_fail|function|void test_msgq_get_fail(void *p1, void *p2, void *p3)
+DECL|test_msgq_put_fail|function|void test_msgq_put_fail(void *p1, void *p2, void *p3)
