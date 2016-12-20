0633516a7fcc0b127d4486d07561fc3d2382dcde
Jithu Joseph <jithu.joseph@intel.com>
2016-12-19 20:28:07 -0800

    tests: kernel: test_slab: Porting memory map tests to unified
    In unified kernel, memory maps are renamed as memory slabs.
    This change ports tests/legacy/kernel/test_map stuff to
    use unified APIs.
    
    Change-Id: Ibf4d60fb53e45a119e6828a09f2638ee7def76b7
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: fb20912cb57c1aa0f66c516115bf173b5df93d07

---
+++ tests/kernel/mem_slab/src/slab.c
@@ -0,0 +1,9 @@
+DECL|HelperTask|function|void HelperTask(void)
+DECL|NUMBLOCKS|macro|NUMBLOCKS
+DECL|RegressionTask|function|void RegressionTask(void)
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|printPointers|function|void printPointers(void **pointer)
+DECL|tcRC|variable|tcRC
+DECL|testSlabFreeAllBlocks|function|int testSlabFreeAllBlocks(void **p)
+DECL|testSlabGetAllBlocks|function|int testSlabGetAllBlocks(void **p)
+DECL|verifyRetValue|function|bool verifyRetValue(int expectRetValue, int currentRetValue)
