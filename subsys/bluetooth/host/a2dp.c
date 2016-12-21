7e03f6758e4584c455a6ba7b59b5ef58ef739c9b
Piyush Itankar <piyush.t.itankar@intel.com>
2016-12-21 14:19:46 +0530

    Bluetooth: A2DP: Adds accept state callback handlers
    Adds state callback handler to allocate memory to AVDTP on an incoming
    connection. Also, abstracts searching of free session as a function call.
    
    Change-Id: Idee6b7a0507b0b75c0007717e351ca12de0cb5be
    Signed-off-by: Piyush Itankar <piyush.t.itankar@intel.com>
    
    Former-commit-id: 42a175f300e95138289f6b59dcbb0776c32e091e

--- subsys/bluetooth/host/a2dp.c
+++ subsys/bluetooth/host/a2dp.c
@@ -1,9 +1,12 @@
 DECL|A2DP_NO_SPACE|macro|A2DP_NO_SPACE
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|a2d_reset|function|void a2d_reset(struct bt_a2dp *a2dp_conn)
+DECL|a2dp_accept|function|int a2dp_accept(struct bt_conn *conn, struct bt_avdtp **session)
 DECL|avdtp_cb|variable|avdtp_cb
 DECL|bt_a2dp_connect|function|struct bt_a2dp *bt_a2dp_connect(struct bt_conn *conn)
 DECL|bt_a2dp_init|function|int bt_a2dp_init(void)
 DECL|bt_a2dp|struct|struct bt_a2dp {
 DECL|cb_ind|variable|cb_ind
 DECL|connection|variable|connection
+DECL|get_new_connection|function|struct bt_a2dp *get_new_connection(struct bt_conn *conn)
 DECL|session|member|struct bt_avdtp session;
