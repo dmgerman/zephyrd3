1e978937dcd4f6734361b00744bfcb8178392580
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2016-12-20 18:16:43 -0800

    tests: kernel: test_critical: Porting legacy tests to unified kernel
    This is the port of the legacy/kernel/test_critical test case to
    the unified kernel, and to use the ztest framework
    
    Change-Id: I10834cbb51446b4a12fc680c0b65438d550f4d85
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 5fbb695a01076d2e76711ca3fdabc9083dde7e12

---
+++ tests/kernel/critical/src/critical.c
@@ -0,0 +1,17 @@
+DECL|AlternateTask|function|void AlternateTask(void *arg1, void *arg2, void *arg3)
+DECL|NUM_MILLISECONDS|macro|NUM_MILLISECONDS
+DECL|RegressionTask|function|void RegressionTask(void *arg1, void *arg2, void *arg3)
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TEST_TIMEOUT|macro|TEST_TIMEOUT
+DECL|altTaskIterations|variable|altTaskIterations
+DECL|criticalLoop|function|uint32_t criticalLoop(uint32_t count)
+DECL|criticalRtn|function|void criticalRtn(struct k_work *unused)
+DECL|criticalVar|variable|criticalVar
+DECL|init_objects|function|static void init_objects(void)
+DECL|offload_work_q_stack|variable|offload_work_q_stack
+DECL|offload_work_q|variable|offload_work_q
+DECL|stack1|variable|stack1
+DECL|stack2|variable|stack2
+DECL|start_threads|function|static void start_threads(void)
+DECL|test_critical|function|void test_critical(void)
+DECL|test_main|function|void test_main(void)
