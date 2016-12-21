23a14e156f0336b43286874d52145a60419a2d9d
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2016-12-21 06:21:13 +0100

    Bluetooth: Controller: add memq_peek interface
    Change-id: I276eddd5e347a5930cff3158ee9aaec0cc7a1585
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 8bd73aeba714386bd3476330076f2b8a2cc26dc9

--- subsys/bluetooth/controller/util/memq.c
+++ subsys/bluetooth/controller/util/memq.c
@@ -1,4 +1,5 @@
 DECL|memq_dequeue|function|void *memq_dequeue(void *tail, void **head, void **mem)
 DECL|memq_enqueue|function|void *memq_enqueue(void *mem, void *link, void **tail)
 DECL|memq_init|function|void *memq_init(void *link, void **head, void **tail)
+DECL|memq_peek|function|void *memq_peek(void *tail, void *head, void **mem)
 DECL|memq_ut|function|uint32_t memq_ut(void)
