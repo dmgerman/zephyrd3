a9177c75af8444d081a9b7c83595d54699aa773f
Johan Hedberg <johan.hedberg@intel.com>
2016-12-25 15:21:39 +0200

    Bluetooth: Convert H:4 HCI driver to RECV_IS_RX_THREAD
    Completely redesign the H:4 driver to utilize its own thread and
    select the new RECV_IS_RX_THREAD Kconfig option.
    
    Jira: ZEP-1483
    
    Change-Id: I0ca0661b66d564e5edc1b8505706d6bb76632e79
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 95049fb99bb3f86831c71056fcac12806b0571f9

--- drivers/bluetooth/hci/h4.c
+++ drivers/bluetooth/hci/h4.c
@@ -3,14 +3,29 @@ DECL|BT_DBG|macro|BT_DBG
 DECL|H4_ACL|macro|H4_ACL
 DECL|H4_CMD|macro|H4_CMD
 DECL|H4_EVT|macro|H4_EVT
+DECL|H4_NONE|macro|H4_NONE
 DECL|H4_SCO|macro|H4_SCO
 DECL|_bt_uart_init|function|static int _bt_uart_init(struct device *unused)
+DECL|acl|member|struct bt_hci_acl_hdr acl;
 DECL|bt_uart_isr|function|static void bt_uart_isr(struct device *unused)
+DECL|buf_set_type|function|static inline void buf_set_type(struct net_buf *buf)
+DECL|buf|member|struct net_buf *buf;
+DECL|copy_hdr|function|static inline void copy_hdr(struct net_buf *buf)
+DECL|discard|member|uint16_t discard;
 DECL|drv|variable|drv
-DECL|h4_acl_recv|function|static struct net_buf *h4_acl_recv(int *remaining)
+DECL|evt|member|struct bt_hci_evt_hdr evt;
+DECL|get_acl_hdr|function|static inline void get_acl_hdr(void)
+DECL|get_evt_hdr|function|static inline void get_evt_hdr(void)
 DECL|h4_dev|variable|h4_dev
 DECL|h4_discard|function|static size_t h4_discard(struct device *uart, size_t len)
-DECL|h4_evt_recv|function|static struct net_buf *h4_evt_recv(int *remaining)
+DECL|h4_get_type|function|static inline void h4_get_type(void)
 DECL|h4_open|function|static int h4_open(void)
-DECL|h4_read|function|static int h4_read(struct device *uart, uint8_t *buf, size_t len, size_t min)
 DECL|h4_send|function|static int h4_send(struct net_buf *buf)
+DECL|have_hdr|member|bool have_hdr;
+DECL|read_header|function|static inline void read_header(void)
+DECL|read_payload|function|static inline bool read_payload(void)
+DECL|remaining|member|uint16_t remaining;
+DECL|rx_thread|function|static void rx_thread(void *p1, void *p2, void *p3)
+DECL|rx|variable|rx
+DECL|sem|member|struct k_sem sem;
+DECL|type|member|uint8_t type;
