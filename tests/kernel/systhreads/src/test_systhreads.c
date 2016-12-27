4fd7f30164a67677ca9932d951c2629b70fb50c5
jing wang <jing.j.wang@intel.com>
2016-12-27 16:06:58 +0800

    tests: add systhreads test case
    this commit check the priority of 2 system threads - main and idle
    
    Change-Id: Ie57e7fdab3d15c0b69d85ed6282bfa6aa04133b4
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    
    Former-commit-id: 4e5fa0ede21e2aac0e73640a53bb22f988d7c6eb

---
+++ tests/kernel/systhreads/src/test_systhreads.c
@@ -0,0 +1,5 @@
+DECL|_sys_soc_suspend|function|int _sys_soc_suspend(int32_t ticks)
+DECL|main_prio|variable|main_prio
+DECL|test_systhreads_idle|function|void test_systhreads_idle(void)
+DECL|test_systhreads_main|function|void test_systhreads_main(void)
+DECL|test_systhreads_setup|function|void test_systhreads_setup(void)
