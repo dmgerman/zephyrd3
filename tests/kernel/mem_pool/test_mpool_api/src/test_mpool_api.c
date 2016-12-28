86ed8b30cb14471cc29d9b175b9bae97601aa18c
Sharron LIU <sharron.liu@intel.com>
2016-12-28 11:36:47 +0800

    tests: kernel: added memory pool api test
    TestPurpose: verify memory pool APIs.
    All TESTPOINTs extracted from kernel-doc comments in <kernel.h>
    
    ZEP-1210
    
    Change-Id: I7dcc0638e7b9c4d6b5ffe282e4fe41ca520d003f
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    
    Former-commit-id: 415e71e04cf3ecbae031f48884855cdbf7a1128f

---
+++ tests/kernel/mem_pool/test_mpool_api/src/test_mpool_api.c
@@ -0,0 +1,6 @@
+DECL|test_mpool_alloc_free_isr|function|void test_mpool_alloc_free_isr(void)
+DECL|test_mpool_alloc_free_thread|function|void test_mpool_alloc_free_thread(void)
+DECL|test_mpool_alloc_size|function|void test_mpool_alloc_size(void)
+DECL|test_mpool_alloc_timeout|function|void test_mpool_alloc_timeout(void)
+DECL|test_mpool_defrag|function|void test_mpool_defrag(void)
+DECL|tmpool_alloc_free|function|void tmpool_alloc_free(void *data)
---
+++ tests/kernel/mem_pool/test_mpool_api/src/test_mpool_extern.c
@@ -0,0 +1,1 @@
+DECL|test_mpool_kdefine_extern|function|void test_mpool_kdefine_extern(void)
