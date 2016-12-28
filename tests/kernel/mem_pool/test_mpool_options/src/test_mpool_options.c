5b0fc4cd099d16657161fd0e471203684fb593b0
Sharron LIU <sharron.liu@intel.com>
2016-12-28 11:38:32 +0800

    tests: kernel: added memory pool configuration options test
    TestPurpose: verify memory pool configuration options.
    All TESTPOINTs extracted from kernel documentation.
    https://www.zephyrproject.org/doc/kernel/memory/pools.html#configuration-options
    
    Jira: ZEP-1210
    
    Change-Id: Ia5379aabd60e490c4566def21eda600c4c1b08dd
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    
    Former-commit-id: 6ec8c6ef14935168d30700e29db56075f51cae8f

---
+++ tests/kernel/mem_pool/test_mpool_options/src/test_mpool_options.c
@@ -0,0 +1,14 @@
+DECL|BLK_ALIGN|macro|BLK_ALIGN
+DECL|BLK_NUM_MAX|macro|BLK_NUM_MAX
+DECL|BLK_NUM_MIN|macro|BLK_NUM_MIN
+DECL|BLK_SIZE_MAX|macro|BLK_SIZE_MAX
+DECL|BLK_SIZE_MID|macro|BLK_SIZE_MID
+DECL|BLK_SIZE_MIN|macro|BLK_SIZE_MIN
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|block_count|variable|block_count
+DECL|block_size|variable|block_size
+DECL|block|variable|block
+DECL|test_mpool_alloc_options|function|void test_mpool_alloc_options(void)
+DECL|tmpool_defrag_before_split|function|static void tmpool_defrag_before_split(void)
+DECL|tmpool_split_before_defrag|function|static void tmpool_split_before_defrag(void)
+DECL|tmpool_split_only|function|static void tmpool_split_only(void)
