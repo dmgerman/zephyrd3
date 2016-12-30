ab86207d6c7dd30777ef711cacebb12448eb5e8a
Sharron LIU <sharron.liu@intel.com>
2016-12-30 12:32:06 +0800

    tests: kernel: added memory slab threadsafe test
    TestPurpose: verify API thread safe in multi-threads environment.
    Thread safe test is explained with more details here:
    https://gerrit.zephyrproject.org/r/#/c/9464/7/ test_mpool_threadsafe.c
    Please comment if you think it necessary as an extensive kernel test.
    
    Jira: ZEP-1209
    
    Change-Id: I52a7ff393d72785622c047289e7d92286e131cc7
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    
    Former-commit-id: d7a9ac21ce104cd09a6891371ffa4a5f55a4e32d

---
+++ tests/kernel/mem_slab/test_mslab_threadsafe/src/test_mslab_threadsafe.c
@@ -0,0 +1,16 @@
+DECL|BLK_ALIGN|macro|BLK_ALIGN
+DECL|BLK_NUM|macro|BLK_NUM
+DECL|BLK_SIZE1|macro|BLK_SIZE1
+DECL|BLK_SIZE2|macro|BLK_SIZE2
+DECL|LOOP|macro|LOOP
+DECL|SLAB_NUM|macro|SLAB_NUM
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|THREAD_NUM|macro|THREAD_NUM
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|mslab2|variable|mslab2
+DECL|slab_id|variable|slab_id
+DECL|slabs|variable|slabs
+DECL|sync_sema|variable|sync_sema
+DECL|test_mslab_threadsafe|function|void test_mslab_threadsafe(void)
+DECL|tmslab_api|function|static void tmslab_api(void *p1, void *p2, void *p3)
+DECL|tstack|variable|tstack
