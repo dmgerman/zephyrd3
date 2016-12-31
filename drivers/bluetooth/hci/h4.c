785b880594d5ca58ad5aee69d42725ffa913c7ce
Johan Hedberg <johan.hedberg@intel.com>
2016-12-31 19:45:00 +0200

    Bluetooth: h4: Convert TX path to be interrupt based
    Use interrupt based transmission instead of polling.
    
    Change-Id: Iebfd67372044d3e6de9bdbdd6f0c9c6d01d46cb9
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 3341d7d2b5f38df668f44f6d42fda54cd2097f63

--- drivers/bluetooth/hci/h4.c
+++ drivers/bluetooth/hci/h4.c
@@ -9,10 +9,12 @@ DECL|_bt_uart_init|function|static int _bt_uart_init(struct device *unused)
 DECL|acl|member|struct bt_hci_acl_hdr acl;
 DECL|bt_uart_isr|function|static void bt_uart_isr(struct device *unused)
 DECL|buf|member|struct net_buf *buf;
+DECL|buf|member|struct net_buf *buf;
 DECL|copy_hdr|function|static inline void copy_hdr(struct net_buf *buf)
 DECL|discard|member|uint16_t discard;
 DECL|drv|variable|drv
 DECL|evt|member|struct bt_hci_evt_hdr evt;
+DECL|fifo|member|struct k_fifo fifo;
 DECL|fifo|member|struct k_fifo fifo;
 DECL|get_acl_hdr|function|static inline void get_acl_hdr(void)
 DECL|get_evt_hdr|function|static inline void get_evt_hdr(void)
@@ -22,9 +24,13 @@ DECL|h4_get_type|function|static inline void h4_get_type(void)
 DECL|h4_open|function|static int h4_open(void)
 DECL|h4_send|function|static int h4_send(struct net_buf *buf)
 DECL|have_hdr|member|bool have_hdr;
+DECL|process_rx|function|static inline void process_rx(void)
+DECL|process_tx|function|static inline void process_tx(void)
 DECL|read_header|function|static inline void read_header(void)
-DECL|read_payload|function|static inline bool read_payload(void)
+DECL|read_payload|function|static inline void read_payload(void)
 DECL|remaining|member|uint16_t remaining;
 DECL|rx_thread|function|static void rx_thread(void *p1, void *p2, void *p3)
 DECL|rx|variable|rx
+DECL|tx|variable|tx
+DECL|type|member|uint8_t type;
 DECL|type|member|uint8_t type;
