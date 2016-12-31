aedba3b515c74162356ee0718f241b411ef338da
Johan Hedberg <johan.hedberg@intel.com>
2016-12-31 14:41:45 +0200

    Bluetooth: h4: Use k_fifo instead of k_sem
    So far the use of k_sem meant that there was no major benefit of
    having more than 2 or 3 RX buffers since there was no queuing
    mechanism. Instead of using k_sem, introduce a k_fifo and use that to
    queue up incoming buffers. This way the RX buffer count can be
    increased with measurable effects on throughput.
    
    Change-Id: I8122b233aeee7c8e145de3fff5f10bcfe348efaa
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 37aa1a1f8b4ed1c07c81189613c0d4ab93582f4b

--- drivers/bluetooth/hci/h4.c
+++ drivers/bluetooth/hci/h4.c
@@ -8,12 +8,12 @@ DECL|H4_SCO|macro|H4_SCO
 DECL|_bt_uart_init|function|static int _bt_uart_init(struct device *unused)
 DECL|acl|member|struct bt_hci_acl_hdr acl;
 DECL|bt_uart_isr|function|static void bt_uart_isr(struct device *unused)
-DECL|buf_set_type|function|static inline void buf_set_type(struct net_buf *buf)
 DECL|buf|member|struct net_buf *buf;
 DECL|copy_hdr|function|static inline void copy_hdr(struct net_buf *buf)
 DECL|discard|member|uint16_t discard;
 DECL|drv|variable|drv
 DECL|evt|member|struct bt_hci_evt_hdr evt;
+DECL|fifo|member|struct k_fifo fifo;
 DECL|get_acl_hdr|function|static inline void get_acl_hdr(void)
 DECL|get_evt_hdr|function|static inline void get_evt_hdr(void)
 DECL|h4_dev|variable|h4_dev
@@ -27,5 +27,4 @@ DECL|read_payload|function|static inline bool read_payload(void)
 DECL|remaining|member|uint16_t remaining;
 DECL|rx_thread|function|static void rx_thread(void *p1, void *p2, void *p3)
 DECL|rx|variable|rx
-DECL|sem|member|struct k_sem sem;
 DECL|type|member|uint8_t type;
