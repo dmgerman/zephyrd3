9916c552bf610c102df1b7a5f8b4731a041a4556
Maureen Helm <maureen.helm@nxp.com>
2017-01-02 14:24:58 -0600

    random: Rename ksdk to mcux
    Renames the ksdk random generator shim driver to mcux.
    
    Change-Id: I8bc376937fed3024c809782139a0a72c7332f89a
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: aa995f8d2fd18fb2bb20df4481bc0c8123c972fa

---
+++ drivers/random/random_mcux.c
@@ -0,0 +1,5 @@
+DECL|random_mcux_api_funcs|variable|random_mcux_api_funcs
+DECL|random_mcux_get_entropy|function|static int random_mcux_get_entropy(struct device *dev, uint8_t *buffer, uint16_t length)
+DECL|random_mcux_get_uint8|function|static uint8_t random_mcux_get_uint8(void)
+DECL|random_mcux_init|function|static int random_mcux_init(struct device *dev)
+DECL|sys_rand32_get|function|uint32_t sys_rand32_get(void)
