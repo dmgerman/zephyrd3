fda9618c532a92966422849150cad974a58f19fd
Johan Hedberg <johan.hedberg@intel.com>
2017-01-02 13:43:12 +0200

    Bluetooth: Create separate bt_recv_prio() API
    Since callers of bt_recv() have so far anyway been required to know in
    which context to call it (based on e.g. bt_hci_evt_is_prio) it's
    cleaner to have two separate APIs: bt_recv and bt_recv_prio.
    
    Change-Id: Icd0d9aed9c51ffd2def31432c4ffcc16a9f13ccd
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: d64f47d956981faa9de806f69fd93b0b7794a31b

--- subsys/bluetooth/host/hci_raw.c
+++ subsys/bluetooth/host/hci_raw.c
@@ -5,6 +5,7 @@ DECL|bt_dev|variable|bt_dev
 DECL|bt_enable_raw|function|int bt_enable_raw(struct k_fifo *rx_queue)
 DECL|bt_hci_driver_register|function|int bt_hci_driver_register(struct bt_hci_driver *drv)
 DECL|bt_hci_driver_unregister|function|void bt_hci_driver_unregister(struct bt_hci_driver *drv)
+DECL|bt_recv_prio|function|int bt_recv_prio(struct net_buf *buf)
 DECL|bt_recv|function|int bt_recv(struct net_buf *buf)
 DECL|bt_send|function|int bt_send(struct net_buf *buf)
 DECL|raw_rx|variable|raw_rx
