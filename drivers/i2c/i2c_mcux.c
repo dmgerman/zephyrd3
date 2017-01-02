ff5e9fa0ed47f5c90721f40bbd81945010d98d69
Maureen Helm <maureen.helm@nxp.com>
2017-01-02 14:16:23 -0600

    i2c: Rename ksdk to mcux
    Renames the ksdk i2c shim driver to mcux.
    
    Change-Id: I1bcae2fa30cb698af32d6abc609d77dee42c608d
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: b22bc6e69bb3cc7573d8331cdbb10d4e225ddf7e

---
+++ drivers/i2c/i2c_mcux.c
@@ -0,0 +1,25 @@
+DECL|DEV_BASE|macro|DEV_BASE
+DECL|DEV_CFG|macro|DEV_CFG
+DECL|DEV_DATA|macro|DEV_DATA
+DECL|base|member|I2C_Type *base;
+DECL|callback_status|member|status_t callback_status;
+DECL|clock_source|member|clock_name_t clock_source;
+DECL|default_cfg|member|union dev_config default_cfg;
+DECL|device_sync_sem|member|struct k_sem device_sync_sem;
+DECL|handle|member|i2c_master_handle_t handle;
+DECL|i2c_mcux_config_0|variable|i2c_mcux_config_0
+DECL|i2c_mcux_config_1|variable|i2c_mcux_config_1
+DECL|i2c_mcux_config_func_0|function|static void i2c_mcux_config_func_0(struct device *dev)
+DECL|i2c_mcux_config_func_1|function|static void i2c_mcux_config_func_1(struct device *dev)
+DECL|i2c_mcux_configure|function|static int i2c_mcux_configure(struct device *dev, uint32_t dev_config_raw)
+DECL|i2c_mcux_config|struct|struct i2c_mcux_config {
+DECL|i2c_mcux_convert_flags|function|static uint32_t i2c_mcux_convert_flags(int msg_flags)
+DECL|i2c_mcux_data_0|variable|i2c_mcux_data_0
+DECL|i2c_mcux_data_1|variable|i2c_mcux_data_1
+DECL|i2c_mcux_data|struct|struct i2c_mcux_data {
+DECL|i2c_mcux_driver_api|variable|i2c_mcux_driver_api
+DECL|i2c_mcux_init|function|static int i2c_mcux_init(struct device *dev)
+DECL|i2c_mcux_isr|function|static void i2c_mcux_isr(void *arg)
+DECL|i2c_mcux_master_transfer_callback|function|static void i2c_mcux_master_transfer_callback(I2C_Type *base,i2c_master_handle_t *handle, status_t status, void *userData)
+DECL|i2c_mcux_transfer|function|static int i2c_mcux_transfer(struct device *dev, struct i2c_msg *msgs,uint8_t num_msgs, uint16_t addr)
+DECL|irq_config_func|member|void (*irq_config_func)(struct device *dev);
