23894606b57fba12d261235384dcad0c741e280b
Piyush Itankar <piyush.t.itankar@intel.com>
2017-01-04 10:40:52 +0530

    Bluetooth: A2DP: Removes confirmation callback registration
    Confirmation callbacks needed not be registered with AVDTP
    during A2DP initialization
    
    Change-Id: I2cdf8c5a283775e49fa6e7e9404024e26d05a767
    Signed-off-by: Piyush Itankar <piyush.t.itankar@intel.com>
    
    Former-commit-id: 4b5e19d21c09fdde461293d78d3fe754f7b55f91

--- subsys/bluetooth/host/a2dp.c
+++ subsys/bluetooth/host/a2dp.c
@@ -5,7 +5,6 @@ DECL|avdtp_cb|variable|avdtp_cb
 DECL|bt_a2dp_connect|function|struct bt_a2dp *bt_a2dp_connect(struct bt_conn *conn)
 DECL|bt_a2dp_init|function|int bt_a2dp_init(void)
 DECL|bt_a2dp|struct|struct bt_a2dp {
-DECL|cb_cfm|variable|cb_cfm
 DECL|cb_ind|variable|cb_ind
 DECL|connection|variable|connection
 DECL|session|member|struct bt_avdtp session;
