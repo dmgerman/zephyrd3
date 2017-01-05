5b3526c7be5721033fe7d1d4b96d2bd9d70f9bb1
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-01-05 12:41:02 +0100

    net: ieee802154: Expose basic network parameters through net mgmt API
    It is then possible to set the channel, pan_id and short address through
    this API. Such features are mainly useful for testing purposes.
    
    Change-Id: I41aeb397afdb231458a3b13638f3e13d3ac28a6c
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 6b41aecd990db8bc87deea143ee1957287911af4

--- subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
+++ subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
@@ -7,3 +7,4 @@ DECL|ieee802154_handle_beacon|function|enum net_verdict ieee802154_handle_beacon
 DECL|ieee802154_handle_mac_command|function|enum net_verdict ieee802154_handle_mac_command(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_scan|function|static int ieee802154_scan(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|ieee802154_set_ack|function|static int ieee802154_set_ack(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
+DECL|ieee802154_set_parameters|function|static int ieee802154_set_parameters(uint32_t mgmt_request, struct net_if *iface, void *data, size_t len)
