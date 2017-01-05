4f85302bf7d6fcecc373875c8fef7e4b9ccc6de9
Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
2017-01-05 13:55:39 +0100

    Bluetooth: SDP: Get ProtocolDescriptorList attribute internals
    Adds API to get available specific information included in protocols UUID
    tree during parsing Protocol Descriptor List attribute. Usually it's to
    be remote Server Channel/PSM number operating on applicable protocol.
    There're a few helper functions added to be able parse and retrieve such
    information from raw record data.
    
    Jira: ZEP-1112
    
    Change-Id: I736a780b95ba54821d36e6011b739f5ff37cd64f
    Signed-off-by: Arkadiusz Lichwa <arkadiusz.lichwa@tieto.com>
    
    Former-commit-id: 15520c5ce952e96a3a3cb870de318c742a816be0

--- subsys/bluetooth/host/sdp.c
+++ subsys/bluetooth/host/sdp.c
@@ -9,18 +9,24 @@ DECL|SDP_PSM|macro|SDP_PSM
 DECL|SDP_SERVICE_HANDLE_BASE|macro|SDP_SERVICE_HANDLE_BASE
 DECL|UUID_NOT_RESOLVED|enumerator|UUID_NOT_RESOLVED,
 DECL|UUID_RESOLVED|enumerator|UUID_RESOLVED,
+DECL|attr_id|member|uint16_t attr_id;
+DECL|attr_id|member|uint16_t attr_id;
 DECL|bt_sdp_accept|function|static int bt_sdp_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
+DECL|bt_sdp_attr_item|struct|struct bt_sdp_attr_item {
 DECL|bt_sdp_client_pool|variable|bt_sdp_client_pool
 DECL|bt_sdp_client|struct|struct bt_sdp_client {
 DECL|bt_sdp_connected|function|static void bt_sdp_connected(struct bt_l2cap_chan *chan)
 DECL|bt_sdp_create_pdu|function|struct net_buf *bt_sdp_create_pdu(void)
 DECL|bt_sdp_disconnected|function|static void bt_sdp_disconnected(struct bt_l2cap_chan *chan)
 DECL|bt_sdp_discover|function|int bt_sdp_discover(struct bt_conn *conn, const struct bt_sdp_discover_params *params)
+DECL|bt_sdp_get_attr|function|static int bt_sdp_get_attr(const struct net_buf *buf, struct bt_sdp_attr_item *attr, uint16_t attr_id)
+DECL|bt_sdp_get_proto_param|function|int bt_sdp_get_proto_param(const struct net_buf *buf, enum bt_sdp_proto proto, uint16_t *param)
 DECL|bt_sdp_init|function|void bt_sdp_init(void)
 DECL|bt_sdp_pool|variable|bt_sdp_pool
 DECL|bt_sdp_recv|function|static void bt_sdp_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|bt_sdp_register_service|function|int bt_sdp_register_service(struct bt_sdp_record *service)
 DECL|bt_sdp_send|function|static void bt_sdp_send(struct bt_l2cap_chan *chan, struct net_buf *buf,uint8_t op, uint16_t tid)
+DECL|bt_sdp_uuid_desc|struct|struct bt_sdp_uuid_desc {
 DECL|bt_sdp|struct|struct bt_sdp {
 DECL|chan|member|struct bt_l2cap_br_chan chan;
 DECL|chan|member|struct bt_l2cap_br_chan chan;
@@ -29,8 +35,11 @@ DECL|db|variable|db
 DECL|func|member|uint16_t (*func)(struct bt_sdp *sdp, struct net_buf *buf,
 DECL|get_record_len|function|static uint16_t get_record_len(struct net_buf *buf)
 DECL|handlers|variable|handlers
+DECL|len|member|uint16_t len;
 DECL|num_services|variable|num_services
 DECL|op_code|member|uint8_t op_code;
+DECL|params_len|member|uint16_t params_len;
+DECL|params|member|uint8_t *params;
 DECL|param|member|const struct bt_sdp_discover_params *param;
 DECL|partial_resp_queue|member|struct k_fifo partial_resp_queue;
 DECL|rec_buf|member|struct net_buf *rec_buf;
@@ -47,6 +56,18 @@ DECL|sdp_client_notify_result|function|static void sdp_client_notify_result(stru
 DECL|sdp_client_params_iterator|function|static void sdp_client_params_iterator(struct bt_sdp_client *session)
 DECL|sdp_client_receive|function|static void sdp_client_receive(struct bt_l2cap_chan *chan, struct net_buf *buf)
 DECL|sdp_client_ssa_search|function|static int sdp_client_ssa_search(struct bt_sdp_client *session)
+DECL|sdp_get_attr_value_len|function|static ssize_t sdp_get_attr_value_len(const uint8_t *data, size_t len)
+DECL|sdp_get_int_len|function|static inline ssize_t sdp_get_int_len(const uint8_t *data, size_t len)
+DECL|sdp_get_param_item|function|static int sdp_get_param_item(struct bt_sdp_uuid_desc *pd_item, uint16_t *param)
+DECL|sdp_get_seq_len_item|function|static ssize_t sdp_get_seq_len_item(uint8_t **data, size_t len)
+DECL|sdp_get_seq_len|function|static inline ssize_t sdp_get_seq_len(const uint8_t *data, size_t len)
+DECL|sdp_get_str_len|function|static inline ssize_t sdp_get_str_len(const uint8_t *data, size_t len)
+DECL|sdp_get_uuid_data|function|static int sdp_get_uuid_data(const struct bt_sdp_attr_item *attr, struct bt_sdp_uuid_desc *pd, uint16_t proto_profile)
+DECL|sdp_get_uuid_len|function|static inline ssize_t sdp_get_uuid_len(const uint8_t *data, size_t len)
 DECL|send_err_rsp|function|static void send_err_rsp(struct bt_l2cap_chan *chan, uint16_t err, uint16_t tid)
 DECL|tid|member|uint16_t tid;
+DECL|uuid16|member|struct bt_uuid_16 uuid16;
+DECL|uuid32|member|struct bt_uuid_32 uuid32;
 DECL|uuid_state|enum|enum uuid_state {
+DECL|uuid|member|struct bt_uuid uuid;
+DECL|val|member|uint8_t *val;
