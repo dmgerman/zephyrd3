05227305bd6100a227ae3f0e782f3d624c13b38f
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-01-06 10:54:02 +0100

    samples: net: Add IEEE 802.15.4 shell module on cc2520 sample
    This is easier to test the 15.4 layer instead of hard-coding sample
    logic.
    
    Change-Id: I2376dec3dad9e2ceca3f61bd88bf5709f1bc1390
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 20404131aaf83cbf229fe2e66fe7ff826771dc30

--- samples/net/ieee802154/cc2520/src/ieee802154_test.c
+++ samples/net/ieee802154/cc2520/src/ieee802154_test.c
@@ -1,8 +1,8 @@
 DECL|PRINT|macro|PRINT
-DECL|PRINT|macro|PRINT
 DECL|init_device|function|static struct net_if *init_device(void)
 DECL|main|function|void main(void)
 DECL|scan_cb|variable|scan_cb
 DECL|scan_ctx|variable|scan_ctx
 DECL|scan_result_cb|function|static void scan_result_cb(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
 DECL|scan|function|static inline void scan(struct net_if *iface)
+DECL|scan|macro|scan
