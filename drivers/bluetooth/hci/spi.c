f16e9a4a0a8f7e6fd541d87345216d86907a77b9
Lee Jones <lee.jones@linaro.org>
2017-01-09 14:16:07 +0000

    Bluetooth: Add HCI SPI driver
    This driver acts as a pass-through, taking raw HCI data, converting
    it to SPI comms and vice versa.  It works in the same way as the
    existing H:4 and H:5 drivers, only it uses SPI instead of UART.
    
    In this first release, the only BLE board which has been tested is
    the X-NUCLEO-IDB05A1:
    
      http://www.st.com/en/ecosystems/x-nucleo-idb05a1.html
    
    Although the current supported SPI format works like the one below,
    it should be trivial to adapt it to support other chips with a
    different format.
    
    SANITY CHECK = 0x02
    SPI WRITE    = 0x0A
    SPI READ     = 0x0B
    
    Tx Format:
    
       [HOST] {SPI WRITE}     0x00          0x00  0x00  0x00  {HCI MESSAGE ...}
       [CHIP] {SANITY CHECK}  {FLASH SIZE}  0x00  0x00  0x00  {0xFF * MESSAGE LEN}
    
    Rx Format:
    
       {IRQ LINE GOES HIGH}
    
       [HOST] {SPI READ}  0x00          0x00  0x00             0x00  {0xFF * BYTES TO READ}
       [CHIP] 0x02        {FLASH SIZE}  0x00  {BYTES TO READ}  0x00  {HCI MESSAGE ...}
    
    Change-Id: I4a00711c922d9ea02c5e2afb0d16715e413b1ed5
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    
    Former-commit-id: 7890290e81a414c945b9c5374cc1523875b4c7a4

---
+++ drivers/bluetooth/hci/spi.c
@@ -0,0 +1,40 @@
+DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|CMD_OCF|macro|CMD_OCF
+DECL|CMD_OGF|macro|CMD_OGF
+DECL|EVT_BLUE_INITIALIZED|macro|EVT_BLUE_INITIALIZED
+DECL|EVT_HEADER_EVENT|macro|EVT_HEADER_EVENT
+DECL|EVT_HEADER_SIZE|macro|EVT_HEADER_SIZE
+DECL|EVT_HEADER_TYPE|macro|EVT_HEADER_TYPE
+DECL|EVT_VENDOR_CODE_LSB|macro|EVT_VENDOR_CODE_LSB
+DECL|EVT_VENDOR_CODE_MSB|macro|EVT_VENDOR_CODE_MSB
+DECL|GPIO_CS_PIN|macro|GPIO_CS_PIN
+DECL|GPIO_IRQ_PIN|macro|GPIO_IRQ_PIN
+DECL|GPIO_RESET_PIN|macro|GPIO_RESET_PIN
+DECL|HCI_ACL|macro|HCI_ACL
+DECL|HCI_CMD|macro|HCI_CMD
+DECL|HCI_EVT|macro|HCI_EVT
+DECL|HCI_SCO|macro|HCI_SCO
+DECL|MAX_RX_MSG_LEN|macro|MAX_RX_MSG_LEN
+DECL|MAX_TX_MSG_LEN|macro|MAX_TX_MSG_LEN
+DECL|READY_NOW|macro|READY_NOW
+DECL|SPI_READ|macro|SPI_READ
+DECL|SPI_WRITE|macro|SPI_WRITE
+DECL|STATUS_HEADER_READY|macro|STATUS_HEADER_READY
+DECL|STATUS_HEADER_TOREAD|macro|STATUS_HEADER_TOREAD
+DECL|_bt_spi_init|function|static int _bt_spi_init(struct device *unused)
+DECL|bt_spi_get_cmd|function|static inline uint16_t bt_spi_get_cmd(uint8_t *txmsg)
+DECL|bt_spi_get_evt|function|static inline uint16_t bt_spi_get_evt(uint8_t *rxmsg)
+DECL|bt_spi_handle_vendor_evt|function|static void bt_spi_handle_vendor_evt(uint8_t *rxmsg)
+DECL|bt_spi_isr|function|static void bt_spi_isr(struct device *unused1, struct gpio_callback *unused2,unsigned int unused3)
+DECL|bt_spi_open|function|static int bt_spi_open(void)
+DECL|bt_spi_rx_thread|function|static void bt_spi_rx_thread(void)
+DECL|bt_spi_send|function|static int bt_spi_send(struct net_buf *buf)
+DECL|cs_dev|variable|cs_dev
+DECL|drv|variable|drv
+DECL|gpio_cb|variable|gpio_cb
+DECL|irq_dev|variable|irq_dev
+DECL|rst_dev|variable|rst_dev
+DECL|spi_conf|variable|spi_conf
+DECL|spi_dev|variable|spi_dev
+DECL|spi_dump_message|function|static inline void spi_dump_message(const uint8_t *pre, uint8_t *buf, uint8_t size)
+DECL|spi_dump_message|function|void spi_dump_message(const uint8_t *pre, uint8_t *buf, uint8_t size) {}
