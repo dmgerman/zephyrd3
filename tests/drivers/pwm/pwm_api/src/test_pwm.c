d658e310cb3550eca50329ba1bd9703db4d9ee72
jing wang <jing.j.wang@intel.com>
2017-01-10 14:33:38 +0800

    tests: Add PWM driver api test
    The commit verify below PWM apis
        pwm_pin_set_cycles()
        pwm_pin_set_usec
    
    test PWM apis under always-on, half-on and alway-off modes
    
    Change-Id: I2251be23ad9c443703dac44e138651a63d2d7211
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    
    Former-commit-id: ae2cf51591e5dcccf23040483a547737bb4fd391

---
+++ tests/drivers/pwm/pwm_api/src/test_pwm.c
@@ -0,0 +1,9 @@
+DECL|DEFAULT_PERIOD_CYCLE|macro|DEFAULT_PERIOD_CYCLE
+DECL|DEFAULT_PERIOD_USEC|macro|DEFAULT_PERIOD_USEC
+DECL|DEFAULT_PULSE_CYCLE|macro|DEFAULT_PULSE_CYCLE
+DECL|DEFAULT_PULSE_USEC|macro|DEFAULT_PULSE_USEC
+DECL|DEFAULT_PWM_PORT|macro|DEFAULT_PWM_PORT
+DECL|PWM_DEV_NAME|macro|PWM_DEV_NAME
+DECL|test_pwm_cycle|function|void test_pwm_cycle(void)
+DECL|test_pwm_usec|function|void test_pwm_usec(void)
+DECL|test_task|function|static int test_task(uint32_t port, uint32_t period, uint32_t pulse, bool cycle)
