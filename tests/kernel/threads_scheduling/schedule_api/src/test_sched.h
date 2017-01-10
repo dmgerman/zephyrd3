ed8623b413d3701d4b923d8f162245d7a7694696
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-11 00:24:31 +0100

    tests: kernel: threads_scheduling: increased stack size to 512 for riscv32 architecture
    Otherwise, not passing sanitycheck
    
    Change-Id: I6dba149750a7d4266fd52851f7e0b139efdba210
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: d65dae20e3e03fa4a59828e8daa4095d8d1cf491

--- tests/kernel/threads_scheduling/schedule_api/src/test_sched.h
+++ tests/kernel/threads_scheduling/schedule_api/src/test_sched.h
@@ -1,4 +1,5 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|__TEST_SCHED_H__|macro|__TEST_SCHED_H__
 DECL|executed|member|int executed;
 DECL|priority|member|int priority;
