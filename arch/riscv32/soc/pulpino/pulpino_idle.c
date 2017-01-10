3bb8a2e0ae75817db407f52f941e22ccfe30c580
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-11 00:24:30 +0100

    riscv32: added support for the pulpino soc
    pulpino soc has custom-extended riscv ISA that is accounted
    for if CONFIG_RISCV_GENERIC_TOOLCHAIN is not set.
    (ex: bit manipulation asm opcodes)
    
    Change-Id: I4dafc4ebc2fedcc4eb6a3dedd0412816afea6004
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 2a8cfca4cf70dcf68dc297dd95d824d64a47c755

---
+++ arch/riscv32/soc/pulpino/pulpino_idle.c
@@ -0,0 +1,3 @@
+DECL|k_cpu_atomic_idle|function|void k_cpu_atomic_idle(unsigned int key)
+DECL|k_cpu_idle|function|void k_cpu_idle(void)
+DECL|pulpino_idle|function|static ALWAYS_INLINE void pulpino_idle(unsigned int key)
