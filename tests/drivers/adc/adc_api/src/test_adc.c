ed3b445c64fec941df46cb4007d87fabeeaf2453
jing wang <jing.j.wang@intel.com>
2017-01-10 17:00:08 +0800

    tests: add zephyr adc driver api test case
    the commit test below adc driver api with different resolutions
    and modes
        adc_enable()
        adc_read()
        adc_disable()
    
    move original adc test to adc_simple folder
    
    Change-Id: I016b5e67a5d89fc8d5ae76f33799e5d3eb3e1cf8
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    
    Former-commit-id: 484f07cec7168756b59e5ff6d24b3172439de34e

---
+++ tests/drivers/adc/adc_api/src/test_adc.c
@@ -0,0 +1,7 @@
+DECL|ADC_DEV_NAME|macro|ADC_DEV_NAME
+DECL|BUFFER_SIZE|macro|BUFFER_SIZE
+DECL|entry|variable|entry
+DECL|seq_buffer|variable|seq_buffer
+DECL|table|variable|table
+DECL|test_adc_sample|function|void test_adc_sample(void)
+DECL|test_task|function|static int test_task(void)
