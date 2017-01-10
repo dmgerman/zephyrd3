b2f992f84c85d3a4f7bafd2810022af18b32adf8
jing wang <jing.j.wang@intel.com>
2017-01-10 15:01:43 +0800

    tests: add zephyr i2c driver api test
    the commit verify below i2c apis by sensor
    GY271 or HMC58831
        i2c_configure()
        i2c_write()
        i2c_read()
        i2c_burst_write()
        i2c_burst_read()
    
    Change-Id: I072a9897a45636613e811baa7ff79b57206e4130
    Signed-off-by: jing wang <jing.j.wang@intel.com>
    
    Former-commit-id: 61e5438c5181393706c77f593d809e0c2a7c07cd

---
+++ tests/drivers/i2c/i2c_api/src/test_i2c.c
@@ -0,0 +1,7 @@
+DECL|I2C_DEV_NAME|macro|I2C_DEV_NAME
+DECL|I2C_DEV_NAME|macro|I2C_DEV_NAME
+DECL|i2c_cfg|variable|i2c_cfg
+DECL|test_burst_gy271|function|static int test_burst_gy271(void)
+DECL|test_gy271|function|static int test_gy271(void)
+DECL|test_i2c_burst_gy271|function|void test_i2c_burst_gy271(void)
+DECL|test_i2c_gy271|function|void test_i2c_gy271(void)
