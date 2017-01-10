3e2b0f434039e5bdaf7905aaa40234b2b92f7fef
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2017-01-10 11:23:25 +0200

    net: bt: Add shell support
    This adds a shell module called "net_bt" that exposes Bluetooth L2
    management commands.
    
    Change-Id: Ia6da1d38cfd51502119758a8f6abbb6d1cd31743
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 1bcd12d0d363a8a7e23ad4f2fb593f9d712b117b

---
+++ subsys/net/ip/l2/bluetooth_shell.c
@@ -0,0 +1,5 @@
+DECL|BT_SHELL_MODULE|macro|BT_SHELL_MODULE
+DECL|bt_commands|variable|bt_commands
+DECL|char2hex|function|static int char2hex(const char *c, uint8_t *x)
+DECL|shell_cmd_connect|function|static int shell_cmd_connect(int argc, char *argv[])
+DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
