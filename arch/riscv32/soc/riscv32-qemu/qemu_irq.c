64cc47b3770620886c8179dbe5ba5f97e1c73602
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-11 00:24:30 +0100

    riscv32: added support for the riscv32-qemu soc
    Change-Id: I7cf71f7a99fed7c83ed761ead9295697929d767d
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: dae36b97e8fa2da5c4dbda2ddf53a98765a3fc0f

---
+++ arch/riscv32/soc/riscv32-qemu/qemu_irq.c
@@ -0,0 +1,4 @@
+DECL|_arch_irq_disable|function|void _arch_irq_disable(unsigned int irq)
+DECL|_arch_irq_enable|function|void _arch_irq_enable(unsigned int irq)
+DECL|_arch_irq_is_enabled|function|int _arch_irq_is_enabled(unsigned int irq)
+DECL|soc_interrupt_init|function|void soc_interrupt_init(void)
