4e52f78d75d2589e737672e6c90b4b19d0d1dc11
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-11 00:24:31 +0100

    serial: added support for the riscv-qemu UART driver
    riscv-qemu UART:
    1) comprises only one register that is used to send or
       receive characters in a polling fashion.
    2) does not have a FIFO and is not interrupt-driven.
    
    Change-Id: I9408f1776eba4cec4aa203a5da759ec04bcddf1f
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 7cf3688740eb77e9ad30ed20d98e42a0233c345b

---
+++ drivers/serial/uart_riscv_qemu.c
@@ -0,0 +1,6 @@
+DECL|DEV_CFG|macro|DEV_CFG
+DECL|uart_riscv_qemu_dev_cfg_0|variable|uart_riscv_qemu_dev_cfg_0
+DECL|uart_riscv_qemu_driver_api|variable|uart_riscv_qemu_driver_api
+DECL|uart_riscv_qemu_init|function|static int uart_riscv_qemu_init(struct device *dev)
+DECL|uart_riscv_qemu_poll_in|function|static int uart_riscv_qemu_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_riscv_qemu_poll_out|function|static unsigned char uart_riscv_qemu_poll_out(struct device *dev, unsigned char c)
