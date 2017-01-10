f6356bbe8114aac2b6db8c8206059a1dbc93e3be
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-11 00:24:30 +0100

    gpio: added support for the pulpino GPIO controller driver
    tested with blinky, button and disco apps
    
    Change-Id: I4b520d4f3e42c97e4a723747ce4a6c67ca9f1d18
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 67dcd1b0b0f57c6b6e84149ca0dd811f95483321

---
+++ drivers/gpio/gpio_pulpino.c
@@ -0,0 +1,29 @@
+DECL|DEV_GPIO_CFG|macro|DEV_GPIO_CFG
+DECL|DEV_GPIO_DATA|macro|DEV_GPIO_DATA
+DECL|DEV_GPIO|macro|DEV_GPIO
+DECL|cb|member|sys_slist_t cb;
+DECL|gpio_base_addr|member|uint32_t gpio_base_addr;
+DECL|gpio_cfg_func|member|pulpino_cfg_func_t gpio_cfg_func;
+DECL|gpio_pulpino_cfg_0|function|static void gpio_pulpino_cfg_0(void)
+DECL|gpio_pulpino_config0|variable|gpio_pulpino_config0
+DECL|gpio_pulpino_config|function|static int gpio_pulpino_config(struct device *dev, int access_op, uint32_t pin, int flags)
+DECL|gpio_pulpino_config|struct|struct gpio_pulpino_config {
+DECL|gpio_pulpino_data0|variable|gpio_pulpino_data0
+DECL|gpio_pulpino_data|struct|struct gpio_pulpino_data {
+DECL|gpio_pulpino_disable_callback|function|static int gpio_pulpino_disable_callback(struct device *dev, int access_op, uint32_t pin)
+DECL|gpio_pulpino_driver|variable|gpio_pulpino_driver
+DECL|gpio_pulpino_enable_callback|function|static int gpio_pulpino_enable_callback(struct device *dev,int access_op, uint32_t pin)
+DECL|gpio_pulpino_init|function|static int gpio_pulpino_init(struct device *dev)
+DECL|gpio_pulpino_irq_handler|function|static void gpio_pulpino_irq_handler(void *arg)
+DECL|gpio_pulpino_manage_callback|function|static int gpio_pulpino_manage_callback(struct device *dev,struct gpio_callback *callback, bool set)
+DECL|gpio_pulpino_read|function|static int gpio_pulpino_read(struct device *dev, int access_op, uint32_t pin, uint32_t *value)
+DECL|gpio_pulpino_t|struct|struct gpio_pulpino_t {
+DECL|gpio_pulpino_write|function|static int gpio_pulpino_write(struct device *dev, int access_op, uint32_t pin, uint32_t value)
+DECL|inten|member|uint32_t inten;
+DECL|intstatus|member|uint32_t intstatus;
+DECL|inttype0|member|uint32_t inttype0;
+DECL|inttype1|member|uint32_t inttype1;
+DECL|paddir|member|uint32_t paddir;
+DECL|padin|member|uint32_t padin;
+DECL|padout|member|uint32_t padout;
+DECL|pulpino_cfg_func_t|typedef|typedef void (*pulpino_cfg_func_t)(void);
