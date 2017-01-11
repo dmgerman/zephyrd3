7b38e56634376c08c91b5769b082e9e58f805300
Michael Scott <michael.scott@linaro.org>
2017-01-11 14:57:42 -0800

    net: linkaddr: calculate linkaddr storage addr size via config
    - Introduce NET_LINK_ADDR_MAX_LENGTH which is either 6 or 8
      depending on whether CONFIG_NET_L2_IEEE802154 is used
    - Instead of being a placeholder single index array of uint8_t,
      let's use NET_LINK_ADDR_MAX_LENGTH to assign the size of the
      "addr" array field in the net_linkaddr_storage structure.
    - Now that the "addr" field of net_linkaddr_storage contains the
      true size of the link address, we can remove "storage" field
      which was hard coded to 8 bytes (2 uint32_t's).
    - Fix 2 references to the "storage" field of the net_linkaddr_storage
      structure.
    
    Change-Id: I2ea12058280b289f65085964eb7d503d4fd260c2
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Former-commit-id: 4aad7673280bf353b2ff39c1987185f9e6e067f6

--- include/net/net_linkaddr.h
+++ include/net/net_linkaddr.h
@@ -1,9 +1,10 @@
+DECL|NET_LINK_ADDR_MAX_LENGTH|macro|NET_LINK_ADDR_MAX_LENGTH
+DECL|NET_LINK_ADDR_MAX_LENGTH|macro|NET_LINK_ADDR_MAX_LENGTH
 DECL|__NET_LINKADDR_H__|macro|__NET_LINKADDR_H__
 DECL|addr|member|uint8_t *addr;
-DECL|addr|member|uint8_t addr[0];
+DECL|addr|member|uint8_t addr[NET_LINK_ADDR_MAX_LENGTH];
 DECL|len|member|uint8_t len;
 DECL|len|member|uint8_t len;
 DECL|net_linkaddr_cmp|function|static inline bool net_linkaddr_cmp(struct net_linkaddr *lladdr1, struct net_linkaddr *lladdr2)
 DECL|net_linkaddr_storage|struct|struct net_linkaddr_storage {
 DECL|net_linkaddr|struct|struct net_linkaddr {
-DECL|storage|member|uint32_t storage[2];
