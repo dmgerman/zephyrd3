b4f5d1e799e916e733a30ddfd8684de7d836e4e0
Leandro Pereira <leandro.pereira@intel.com>
2017-01-12 14:47:21 -0800

    net: tcp: Remove unused `recv_ack` field from TCP context
    This value is never set (always zero), so it's safe to remove it from
    the net_tcp struct.
    
    Change-Id: Ie4c1d90204a9834f2223b09828af42ee101bd045
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 9a558c2aaac021a2bb70522ea7e1e913271a0a9e

--- subsys/net/ip/tcp.h
+++ subsys/net/ip/tcp.h
@@ -44,7 +44,6 @@ DECL|net_tcp_register|function|static inline int net_tcp_register(const struct s
 DECL|net_tcp_state|enum|enum net_tcp_state {
 DECL|net_tcp_unregister|function|static inline int net_tcp_unregister(struct net_conn_handle *handle)
 DECL|net_tcp|struct|struct net_tcp {
-DECL|recv_ack|member|uint32_t recv_ack;
 DECL|recv_max_ack|member|uint32_t recv_max_ack;
 DECL|recv_mss|member|uint16_t recv_mss;
 DECL|recv_user_data|member|void *recv_user_data;
