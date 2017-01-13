87577fc329deaf4f43669eea8e61766544c6d77e
Anas Nashif <anas.nashif@intel.com>
2017-01-13 17:43:33 -0500

    doc: application porting guide to the unified kernel
    Change-Id: I463b50336f6438ed5dc4ae60bf0d3f7b6e1118a5
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 0ad13320cd018901a919f362b5061086a785351e

---
+++ doc/porting/sync_v1.c
@@ -0,0 +1,5 @@
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
+DECL|taskA|function|void taskA(void)
+DECL|taskB|function|void taskB(void)
---
+++ doc/porting/sync_v1_nano.c
@@ -0,0 +1,8 @@
+DECL|SLEEPTICKS|macro|SLEEPTICKS
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|fiberEntry|function|void fiberEntry(void)
+DECL|fiberStack|variable|fiberStack
+DECL|main|function|void main(void)
+DECL|nanoSemFiber|variable|nanoSemFiber
+DECL|nanoSemTask|variable|nanoSemTask
