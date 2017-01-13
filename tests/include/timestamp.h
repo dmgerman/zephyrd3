a65dd70c4e556f4900dbef8726f6bc2a4dffdb5a
Jithu Joseph <jithu.joseph@intel.com>
2017-01-13 15:50:29 -0800

    tests: include: Move timestamp.h into common location
    These were part of latency_measure benchmark, but was
    used in all benchmarks.
    
    legacy symbols in timestamp.h were replaced to unified
    ones. Tick based checks have been replaced with ms based
    checks in line with the new APIs.
    
    Change-Id: I1d27310023be4cafbceccf50cb87d72b6681443d
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 7c118882d24799807644661771ca251ed8efefbb

---
+++ tests/include/timestamp.h
@@ -0,0 +1,9 @@
+DECL|OS_GET_TIME|macro|OS_GET_TIME
+DECL|TICK_SYNCH|macro|TICK_SYNCH
+DECL|TIME_STAMP_DELTA_GET|function|static inline uint32_t TIME_STAMP_DELTA_GET(uint32_t ts)
+DECL|_TIMESTAMP_H_|macro|_TIMESTAMP_H_
+DECL|bench_test_end|function|static inline int bench_test_end(void)
+DECL|bench_test_init|function|static inline void bench_test_init(void)
+DECL|bench_test_start|function|static inline void bench_test_start(void)
+DECL|high_timer_overflow|function|static inline int high_timer_overflow(void)
+DECL|tCheck|variable|tCheck
