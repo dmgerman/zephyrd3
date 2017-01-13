fbd2c57cf8669eac3f22fabf134648f8351502dd
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-01-13 11:35:41 +0100

    net: mgmt: Silently ignore net_mgmt_event functions if not enabled
    When CONFIG_NET_MGMT_EVENT is not set, it will still be able to compile
    code using net_mgmt_event related functions.
    
    Change-Id: I6824c57093636867ea4228338aa6c02913975510
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 55d653f52fbac79230df8a2287f5312c92613310

--- include/net/net_mgmt.h
+++ include/net/net_mgmt.h
@@ -20,11 +20,13 @@ DECL|NET_MGMT_REGISTER_REQUEST_HANDLER|macro|NET_MGMT_REGISTER_REQUEST_HANDLER
 DECL|__NET_MGMT_H__|macro|__NET_MGMT_H__
 DECL|event_mask|member|uint32_t event_mask;
 DECL|handler|member|net_mgmt_event_handler_t handler;
+DECL|net_mgmt_add_event_callback|macro|net_mgmt_add_event_callback
 DECL|net_mgmt_event_callback|struct|struct net_mgmt_event_callback {
 DECL|net_mgmt_event_handler_t|typedef|typedef void (*net_mgmt_event_handler_t)(struct net_mgmt_event_callback *cb,
 DECL|net_mgmt_event_init|macro|net_mgmt_event_init
 DECL|net_mgmt_event_notify|macro|net_mgmt_event_notify
 DECL|net_mgmt_init_event_callback|function|void net_mgmt_init_event_callback(struct net_mgmt_event_callback *cb, net_mgmt_event_handler_t handler, uint32_t mgmt_event_mask)
+DECL|net_mgmt_init_event_callback|macro|net_mgmt_init_event_callback
 DECL|net_mgmt_request_handler_t|typedef|typedef int (*net_mgmt_request_handler_t)(uint32_t mgmt_request,
 DECL|net_mgmt|macro|net_mgmt
 DECL|node|member|sys_snode_t node;
