84a8e441c44aa2050923ff483f10b3e48a15ca4d
Anas Nashif <anas.nashif@intel.com>
2017-01-17 06:36:06 -0500

    net: zoap_client: fix usage of legacy APIs
    Change-Id: Ic3df2fb50897bd07c47ef987698c7fcaadcb6575
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: ae7df93a770f9365fd88d6f92d21aeb220d4c074

--- samples/net/zoap_client/src/zoap-client.c
+++ samples/net/zoap_client/src/zoap-client.c
@@ -9,7 +9,7 @@ DECL|msg_dump|function|static void msg_dump(const char *s, uint8_t *data, unsign
 DECL|pendings|variable|pendings
 DECL|replies|variable|replies
 DECL|resource_reply_cb|function|static int resource_reply_cb(const struct zoap_packet *response, struct zoap_reply *reply, const struct sockaddr *from)
-DECL|retransmit_request|function|static void retransmit_request(struct nano_work *work)
+DECL|retransmit_request|function|static void retransmit_request(struct k_work *work)
 DECL|retransmit_work|variable|retransmit_work
 DECL|test_path|variable|test_path
 DECL|udp_receive|function|static void udp_receive(struct net_context *context,struct net_buf *buf, int status, void *user_data)
