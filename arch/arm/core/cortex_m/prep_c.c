4a9dfe86f1b3b803b52f136ceb7eb81d15d51627
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-01-17 14:28:42 -0200

    arm: set default vector table address in prep_c when XIP
    It is called before early SoC initialization, so remove the duplicated
    code from other boards and just set it by default when using XIP.
    
    This can later be used when adding bootloader support, as an
    additional option could be created to move the VTOR offset to a
    different address.
    
    Change-Id: Ia1f5d9a066de61858ee287215cefdd58596b6b1c
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>
    
    Former-commit-id: 58460a52c9c9027bacc3df5d5d86bbf5fcc697cb

--- arch/arm/core/cortex_m/prep_c.c
+++ arch/arm/core/cortex_m/prep_c.c
@@ -2,4 +2,5 @@ DECL|_PrepC|function|void _PrepC(void)
 DECL|enable_floating_point|function|static inline void enable_floating_point(void)
 DECL|enable_floating_point|function|static inline void enable_floating_point(void)
 DECL|relocate_vector_table|function|static inline void relocate_vector_table(void)
+DECL|relocate_vector_table|function|static inline void relocate_vector_table(void)
 DECL|relocate_vector_table|function|static inline void relocate_vector_table(void) { /* do nothing */ }
