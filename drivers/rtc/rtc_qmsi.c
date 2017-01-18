e33586bb1787088d7b1f2069558887e9a8df5fa7
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2017-01-17 17:49:56 -0800

    drivers: QMSI RTC: simplify driver reentrancy code using IS_ENABLED
    This is one of the series of patches that simplifies the driver
    code by using the IS_ENABLED macro. This removes the need of the
    const variable and the wrapper functions on semaphore APIs.
    
    Jira: ZEP-1251
    
    Change-Id: I84a09a0aaa3a00452d8c2b4bca7be0f6b2015218
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: 8bf14d380b764c0b78c2b1f9e51e84b2d6d6c3d4

--- drivers/rtc/rtc_qmsi.c
+++ drivers/rtc/rtc_qmsi.c
@@ -5,11 +5,7 @@ DECL|RTC_CONTEXT|macro|RTC_CONTEXT
 DECL|RTC_HAS_CONTEXT_DATA|macro|RTC_HAS_CONTEXT_DATA
 DECL|api|variable|api
 DECL|device_power_state|member|uint32_t device_power_state;
-DECL|reentrancy_protection|variable|reentrancy_protection
-DECL|reentrancy_protection|variable|reentrancy_protection
 DECL|rtc_context|variable|rtc_context
-DECL|rtc_critical_region_end|function|static void rtc_critical_region_end(struct device *dev)
-DECL|rtc_critical_region_start|function|static void rtc_critical_region_start(struct device *dev)
 DECL|rtc_ctx|variable|rtc_ctx
 DECL|rtc_data|struct|struct rtc_data {
 DECL|rtc_qmsi_device_ctrl|function|static int rtc_qmsi_device_ctrl(struct device *dev, uint32_t ctrl_command,void *context)
@@ -23,7 +19,6 @@ DECL|rtc_qmsi_set_alarm|function|static int rtc_qmsi_set_alarm(struct device *de
 DECL|rtc_qmsi_set_config|function|static int rtc_qmsi_set_config(struct device *dev, struct rtc_config *cfg)
 DECL|rtc_qmsi_set_power_state|function|static void rtc_qmsi_set_power_state(struct device *dev, uint32_t power_state)
 DECL|rtc_qmsi_set_power_state|macro|rtc_qmsi_set_power_state
-DECL|rtc_reentrancy_init|function|static void rtc_reentrancy_init(struct device *dev)
 DECL|rtc_resume_device|function|static int rtc_resume_device(struct device *dev)
 DECL|rtc_suspend_device|function|static int rtc_suspend_device(struct device *dev)
 DECL|sem|member|struct k_sem sem;
