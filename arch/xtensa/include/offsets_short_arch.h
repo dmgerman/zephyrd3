2ab9447f4c0c4979057bfea69e3c26107c183bfb
Mazen NEIFER <mazen@nestwave.com>
2017-01-20 20:55:41 +0100

    Xtensa port: Added fields offset support for kernel and thread structures.
    Change-Id: I9cc44f60757a2f988e2a6341da9591a5e5f54561
    Signed-off-by: Mazen NEIFER <mazen@nestwave.com>
    
    Former-commit-id: 49b3c0028d45721cd81e3471896fb98e521c4131

---
+++ arch/xtensa/include/offsets_short_arch.h
@@ -0,0 +1,6 @@
+DECL|_kernel_offset_to_flags|macro|_kernel_offset_to_flags
+DECL|_offsets_short_arch__h_|macro|_offsets_short_arch__h_
+DECL|_thread_offset_to_coopCoprocReg|macro|_thread_offset_to_coopCoprocReg
+DECL|_thread_offset_to_preempCoprocReg|macro|_thread_offset_to_preempCoprocReg
+DECL|_thread_offset_to_retval|macro|_thread_offset_to_retval
+DECL|_thread_offset_to_sp|macro|_thread_offset_to_sp
