af34cbdd643802f18abca102e7c0ef19a89e119e
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2017-01-19 16:47:36 -0800

    drivers: QMSI WDT: simplify driver reentrancy code using IS_ENABLED macro
    This is one of the series of patches that simplifies the driver
    code by using the IS_ENABLED macro. This removes the need of the
    const variable and the three wrapper functions on semaphore APIs.
    
    Jira: ZEP-1251
    
    Change-Id: I2ea0b2d04252d6b28e7d5ca528480234545e6eed
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: dad41277618c5199eb37de9ab147edc71a9311cd

--- drivers/watchdog/wdt_qmsi.c
+++ drivers/watchdog/wdt_qmsi.c
@@ -9,21 +9,16 @@ DECL|disable|function|static void disable(struct device *dev)
 DECL|enable|function|static void enable(struct device *dev)
 DECL|get_config|function|static void get_config(struct device *dev, struct wdt_config *cfg)
 DECL|init|function|static int init(struct device *dev)
-DECL|reentrancy_protection|variable|reentrancy_protection
-DECL|reentrancy_protection|variable|reentrancy_protection
 DECL|reload|function|static void reload(struct device *dev)
 DECL|sem|member|struct k_sem sem;
 DECL|set_config|function|static int set_config(struct device *dev, struct wdt_config *cfg)
 DECL|user_cb|variable|user_cb
 DECL|wdt_context|variable|wdt_context
-DECL|wdt_critical_region_end|function|static void wdt_critical_region_end(struct device *dev)
-DECL|wdt_critical_region_start|function|static void wdt_critical_region_start(struct device *dev)
 DECL|wdt_ctx|variable|wdt_ctx
 DECL|wdt_data|struct|struct wdt_data {
 DECL|wdt_qmsi_device_ctrl|function|static int wdt_qmsi_device_ctrl(struct device *dev, uint32_t ctrl_command,void *context)
 DECL|wdt_qmsi_get_power_state|function|static uint32_t wdt_qmsi_get_power_state(struct device *dev)
 DECL|wdt_qmsi_set_power_state|function|static void wdt_qmsi_set_power_state(struct device *dev, uint32_t power_state)
 DECL|wdt_qmsi_set_power_state|macro|wdt_qmsi_set_power_state
-DECL|wdt_reentrancy_init|function|static void wdt_reentrancy_init(struct device *dev)
 DECL|wdt_resume_device_from_suspend|function|static int wdt_resume_device_from_suspend(struct device *dev)
 DECL|wdt_suspend_device|function|static int wdt_suspend_device(struct device *dev)
