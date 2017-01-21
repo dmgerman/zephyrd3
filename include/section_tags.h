310a85ec1ddfa82a03040dcef73ee089d6a28634
Maureen Helm <maureen.helm@nxp.com>
2017-01-21 11:17:59 -0600

    k64: Rename security_frdm_k64f section
    Renames the flash security section so it makes sense for other Kinetis
    devices, not just k64. In Kinetis reference manuals, this section is
    referred to as the 'flash configuration field'.
    
    Change-Id: I2b7c7cc1ec2541419d77878d367d96c9ceb7a0cf
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 8d5833ffaaaae0481bf65a91837562a1788ea336

--- include/section_tags.h
+++ include/section_tags.h
@@ -1,8 +1,8 @@
 DECL|__in_section|macro|__in_section
 DECL|__irq_vector_table|macro|__irq_vector_table
 DECL|__irq_vector_table|macro|__irq_vector_table
+DECL|__kinetis_flash_config_section|macro|__kinetis_flash_config_section
 DECL|__noinit|macro|__noinit
 DECL|__scp_section|macro|__scp_section
 DECL|__scs_section|macro|__scs_section
-DECL|__security_frdm_k64f_section|macro|__security_frdm_k64f_section
 DECL|_section_tags__h_|macro|_section_tags__h_
