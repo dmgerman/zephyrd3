64d3c055e914308273040908ae14d5b576be5d68
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-21 17:52:32 +0100

    tests: kernel: threads_customdata: increase STACK_SIZE to 512 for riscv32
    In RISCV, stack always grows by a multiple of 16 bytes, even if we are saving
    data of size < 16 bytes onto the stack.
    
    Hence, for riscv32 architecture increase stack size to 512 for
    threads_customdata, otherwise we experience stack overflow.
    
    Change-Id: I805bc346b8a2c2f4ad6d0db622eb262290af942b
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 2d1fd2c947f8467815752b0287258b139e78350e

--- tests/kernel/threads_customdata/cdata_api/src/test_customdata_api.c
+++ tests/kernel/threads_customdata/cdata_api/src/test_customdata_api.c
@@ -1,4 +1,5 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|customdata_entry|function|static void customdata_entry(void *p1, void *p2, void *p3)
 DECL|test_customdata_get_set_coop|function|void test_customdata_get_set_coop(void)
 DECL|test_customdata_get_set_preempt|function|void test_customdata_get_set_preempt(void)
