7a952c300e5bf16e274996922216e5e4cb361a75
Jean-Paul Etienne <fractalclone@gmail.com>
2017-01-24 01:03:56 +0100

    riscv32: timer: replace riscv_qemu_driver by the generic riscv_machine_driver
    riscv defines the machine-mode timer registers that are implemented
    by the all riscv SOCs that follow the riscv privileged architecture
    specification.
    
    The timer registers implemented in riscv-qemu follow this specification.
    To account for future riscv SOCs, reimplement the riscv_qemu_driver by
    the riscv_machine_driver.
    
    Change-Id: I645b03c91b4e07d0f2609908decc27ba9b8240d4
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: c989f0b40812664b891b7a8457735991c39de3b4

---
+++ drivers/timer/riscv_machine_timer.c
@@ -0,0 +1,11 @@
+DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
+DECL|accumulated_cycle_count|variable|accumulated_cycle_count
+DECL|k_cycle_get_32|function|uint32_t k_cycle_get_32(void)
+DECL|last_rtc_value|variable|last_rtc_value
+DECL|mtimecmp|variable|mtimecmp
+DECL|mtime|variable|mtime
+DECL|riscv_machine_rearm_timer|function|static ALWAYS_INLINE void riscv_machine_rearm_timer(void)
+DECL|riscv_machine_timer_irq_handler|function|static void riscv_machine_timer_irq_handler(void *unused)
+DECL|riscv_machine_timer_t|typedef|} riscv_machine_timer_t;
+DECL|val_high|member|uint32_t val_high;
+DECL|val_low|member|uint32_t val_low;
