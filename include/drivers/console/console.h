63e2ad1db6fa935e14905aa2de6bb025e49213ba
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-01-24 10:01:05 +0100

    drivers/console: Making console input generic
    Input line length can be modified via Kconfig.
    
    Change-Id: I3423fce9814e04b11d11e5d391f85fe1efbe8d17
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 3e65158936b3acd48c7df58b3ef76247778c9f2b

---
+++ include/drivers/console/console.h
@@ -0,0 +1,6 @@
+DECL|CONSOLE_MAX_LINE_LEN|macro|CONSOLE_MAX_LINE_LEN
+DECL|__CONSOLE_H__|macro|__CONSOLE_H__
+DECL|_unused|member|int _unused;
+DECL|console_input_fn|typedef|typedef void (*console_input_fn)(struct k_fifo *avail, struct k_fifo *lines,
+DECL|console_input|struct|struct console_input {
+DECL|line|member|char line[CONSOLE_MAX_LINE_LEN];
