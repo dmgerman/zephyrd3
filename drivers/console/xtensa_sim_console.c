4b6de1bf197067973fc92532d5fed5c95bd60d1f
Mazen NEIFER <mazen@nestwave.com>
2017-01-24 22:03:19 +0100

    Xtensa port: Added support for Xtensa simulator console driver.
    Change-Id: I58effa98fd6ff53bcfd21cb8de2fcd89651dc1d9
    Signed-off-by: Mazen NEIFER <mazen@nestwave.com>
    
    Former-commit-id: b0669a04b305603482e83d670a9da4816f7e3cfc

---
+++ drivers/console/xtensa_sim_console.c
@@ -0,0 +1,5 @@
+DECL|__printk_hook_install|macro|__printk_hook_install
+DECL|__stdout_hook_install|macro|__stdout_hook_install
+DECL|console_out|function|static int console_out(int c)
+DECL|xt_sim_console_hook_install|function|void xt_sim_console_hook_install(void)
+DECL|xt_sim_console_init|function|static int xt_sim_console_init(struct device *arg)
