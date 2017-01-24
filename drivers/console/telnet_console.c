5047d8ad41839abaf9a349e6967d941d4cdff4bc
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-01-24 14:59:11 +0100

    drivers/console/telnet: Provide minimal input handling.
    Telnet "Interpret As Command" (IAC) code is ignored.
    
    Change-Id: I882397389d77b8adfcbce62fbd9654c0b0412ae3
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 1d9402b3e334255e9ede0b323b4de3cd37fef82d

--- drivers/console/telnet_console.c
+++ drivers/console/telnet_console.c
@@ -2,13 +2,16 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|TELNET_LINES|macro|TELNET_LINES
 DECL|TELNET_LINE_SIZE|macro|TELNET_LINE_SIZE
+DECL|TELNET_MIN_MSG|macro|TELNET_MIN_MSG
 DECL|TELNET_PORT|macro|TELNET_PORT
 DECL|TELNET_PRIORITY|macro|TELNET_PRIORITY
 DECL|TELNET_STACK_SIZE|macro|TELNET_STACK_SIZE
 DECL|TELNET_THRESHOLD|macro|TELNET_THRESHOLD
 DECL|TELNET_TIMEOUT|macro|TELNET_TIMEOUT
+DECL|avail_queue|variable|avail_queue
 DECL|buf|member|char buf[TELNET_LINE_SIZE];
 DECL|client_cnx|variable|client_cnx
+DECL|input_queue|variable|input_queue
 DECL|l_bufs|member|struct line_buf l_bufs[TELNET_LINES];
 DECL|len|member|uint16_t len;
 DECL|line_buf_rb|struct|struct line_buf_rb {
@@ -21,12 +24,14 @@ DECL|telnet_accept|function|static void telnet_accept(struct net_context *client
 DECL|telnet_console_init|function|static int telnet_console_init(struct device *arg)
 DECL|telnet_console_out|function|static int telnet_console_out(int c)
 DECL|telnet_end_client_connection|function|static void telnet_end_client_connection(void)
+DECL|telnet_handle_input|function|static inline void telnet_handle_input(struct net_buf *buf)
 DECL|telnet_rb_get_line_in|function|static inline struct line_buf *telnet_rb_get_line_in(void)
 DECL|telnet_rb_get_line_out|function|static inline struct line_buf *telnet_rb_get_line_out(void)
 DECL|telnet_rb_init|function|static void telnet_rb_init(void)
 DECL|telnet_rb_switch|function|static void telnet_rb_switch(void)
 DECL|telnet_rb|variable|telnet_rb
 DECL|telnet_recv|function|static void telnet_recv(struct net_context *client,struct net_buf *buf, int status, void *user_data)
+DECL|telnet_register_input|function|void telnet_register_input(struct k_fifo *avail, struct k_fifo *lines, uint8_t (*completion)(char *str, uint8_t len))
 DECL|telnet_run|function|static void telnet_run(void)
 DECL|telnet_send_prematurely|function|static void telnet_send_prematurely(struct k_timer *timer)
 DECL|telnet_send|function|static inline bool telnet_send(void)
