c1b68d2843d34a3e67192b0c6b793303b3e2f943
Kumar Gala <kumar.gala@linaro.org>
2017-01-24 11:14:30 -0600

    arm: move exception priority to exc.h
    Move the exception priority related defines into exc.h out of nvic.h
    
    Change-Id: I7ded917a3f6f7cdbc506b2f70c22f37eddc5f5c7
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 8be0401af046a6110715d192f87de0c6da9963c1

--- include/arch/arm/cortex_m/nvic.h
+++ include/arch/arm/cortex_m/nvic.h
@@ -1,11 +1,6 @@
 DECL|BIT_FROM_IRQ|macro|BIT_FROM_IRQ
 DECL|NUM_IRQS_PER_REG|macro|NUM_IRQS_PER_REG
 DECL|REG_FROM_IRQ|macro|REG_FROM_IRQ
-DECL|_EXCEPTION_RESERVED_PRIO|macro|_EXCEPTION_RESERVED_PRIO
-DECL|_EXCEPTION_RESERVED_PRIO|macro|_EXCEPTION_RESERVED_PRIO
-DECL|_EXC_IRQ_DEFAULT_PRIO|macro|_EXC_IRQ_DEFAULT_PRIO
-DECL|_EXC_PRIO|macro|_EXC_PRIO
-DECL|_IRQ_PRIO_OFFSET|macro|_IRQ_PRIO_OFFSET
 DECL|_NVIC_H_|macro|_NVIC_H_
 DECL|_NvicIrqDisable|function|static inline void _NvicIrqDisable(unsigned int irq)
 DECL|_NvicIrqEnable|function|static inline void _NvicIrqEnable(unsigned int irq)
@@ -15,5 +10,3 @@ DECL|_NvicIrqPrioSet|function|static inline void _NvicIrqPrioSet(unsigned int ir
 DECL|_NvicIrqUnpend|function|static inline void _NvicIrqUnpend(unsigned int irq)
 DECL|_NvicIsIrqEnabled|function|static inline int _NvicIsIrqEnabled(unsigned int irq)
 DECL|_NvicIsIrqPending|function|static inline int _NvicIsIrqPending(unsigned int irq)
-DECL|_ZERO_LATENCY_IRQS_RESERVED_PRIO|macro|_ZERO_LATENCY_IRQS_RESERVED_PRIO
-DECL|_ZERO_LATENCY_IRQS_RESERVED_PRIO|macro|_ZERO_LATENCY_IRQS_RESERVED_PRIO
