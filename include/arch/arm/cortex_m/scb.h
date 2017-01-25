129f54d1d7cbe236d9eb973f5b8a9cd947a493c2
Kumar Gala <kumar.gala@linaro.org>
2017-01-25 09:33:03 -0600

    arm: cmsis: Convert _ClearFaults to use direct CMSIS register access
    The previous code incorrectly used the value 0xfe to clear the mem and
    bus faults. It attempted to handle the address register valid bits
    separately, but reversed the bit order.
    
    Jira: ZEP-1568
    
    Change-Id: I240d072610af9979ca93c0081ed862df08929372
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 94db81962664579fcf45e310521a62170aeda8e8

--- include/arch/arm/cortex_m/scb.h
+++ include/arch/arm/cortex_m/scb.h
@@ -1,7 +1,4 @@
 DECL|_SCB__H_|macro|_SCB__H_
-DECL|_ScbBusFaultAllFaultsReset|function|static inline void _ScbBusFaultAllFaultsReset(void)
 DECL|_ScbBusFaultBfarReset|function|static inline void _ScbBusFaultBfarReset(void)
-DECL|_ScbHardFaultAllFaultsReset|function|static inline int _ScbHardFaultAllFaultsReset(void)
-DECL|_ScbMemFaultAllFaultsReset|function|static inline void _ScbMemFaultAllFaultsReset(void)
 DECL|_ScbMemFaultMmfarReset|function|static inline void _ScbMemFaultMmfarReset(void)
 DECL|_ScbUsageFaultAllFaultsReset|function|static inline void _ScbUsageFaultAllFaultsReset(void)
