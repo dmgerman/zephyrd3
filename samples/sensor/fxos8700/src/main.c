e172b10c745ff6e90cb09a5c3515a774c360c7a9
Maureen Helm <maureen.helm@nxp.com>
2017-01-25 15:36:51 -0600

    samples: fxos8700: Decimate sensor data before printing to the console
    At the default sample rate of 400 Hz, there is not enough bandwidth on
    the UART at 115200 baud to print every sample. Fix by decimating the
    sensor data by 4 before printing to the console.
    
    This issue was discovered after fixing ZEP-1507.
    
    Change-Id: I451ec9873ca93fe417d5d5a2d6d13b5e921c7611
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 1938f5dadba48d9111798ca601b3b52b034acbc4

--- samples/sensor/fxos8700/src/main.c
+++ samples/sensor/fxos8700/src/main.c
@@ -1,2 +1,3 @@
+DECL|DECIMATION_FACTOR|macro|DECIMATION_FACTOR
 DECL|main|function|void main(void)
 DECL|trigger_handler|function|static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
