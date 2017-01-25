541e47d703d48bbf80b317088196de48cb4b97b8
Kumar Gala <kumar.gala@linaro.org>
2017-01-25 09:12:00 -0600

    arm: refactor clearing of exception faults to common code
    A number of SoCs clear out the Mem/Bus/Usage and Hard Fault exceptions
    during init.  Lets refactor that into a common function so we don't have
    to keep duplicating it over and over.
    
    Change-Id: Ida908a9092db37447abcf3c9872f36937982f729
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: f16cefea9f4450fab7159d32f9df4d6990dd260a

--- arch/arm/include/cortex_m/exc.h
+++ arch/arm/include/cortex_m/exc.h
@@ -1,4 +1,5 @@
 DECL|_ARM_CORTEXM_ISR__H_|macro|_ARM_CORTEXM_ISR__H_
+DECL|_ClearFaults|function|static ALWAYS_INLINE void _ClearFaults(void)
 DECL|_EXC_FAULT_PRIO|macro|_EXC_FAULT_PRIO
 DECL|_EXC_SVC_PRIO|macro|_EXC_SVC_PRIO
 DECL|_ExcSetup|function|static ALWAYS_INLINE void _ExcSetup(void)
