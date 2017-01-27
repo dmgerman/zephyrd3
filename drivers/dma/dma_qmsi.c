3c8f4b04692ed707f926e78a2b315a6f9f01333a
Baohong Liu <baohong.liu@intel.com>
2017-01-26 18:15:08 -0800

    drivers: dma_shim: update dma qmsi shim driver
    Update the qmsi dma shim driver based on the new dma api
    interface.
    
    Recently, a RFC was posted to update the dma api. The update
    to dma api interface was already posted. This change is to
    update the dma qmsi shim driver based on the dma api change.
    It is using the new data structures and new api function names.
    
    Jira: ZEP-873
    
    Change-Id: If9a772c5ff1c2b10fca05172c48f75223bbf940e
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 15b44146cd14f0ebb8563ac44d5792a742f86635

--- drivers/dma/dma_qmsi.c
+++ drivers/dma/dma_qmsi.c
@@ -1,9 +1,12 @@
+DECL|bst_index|function|static int bst_index(uint32_t num_units, uint32_t *index)
 DECL|callback_data|member|void *callback_data[QM_DMA_CHANNEL_NUM];
 DECL|device_power_state|member|uint32_t device_power_state;
 DECL|dev|member|struct device *dev;
 DECL|dma_callback|function|static void dma_callback(void *callback_context, uint32_t len, int error_code)
 DECL|dma_context|variable|dma_context
+DECL|dma_drv_callback|function|static void dma_drv_callback(void *callback_context, uint32_t len, int error_code)
 DECL|dma_funcs|variable|dma_funcs
+DECL|dma_qmsi_chan_config|function|static int dma_qmsi_chan_config(struct device *dev, uint32_t channel,struct dma_config *config)
 DECL|dma_qmsi_channel_config|function|static int dma_qmsi_channel_config(struct device *dev, uint32_t channel, struct dma_channel_config *config)
 DECL|dma_qmsi_config_data|variable|dma_qmsi_config_data
 DECL|dma_qmsi_config_info|struct|struct dma_qmsi_config_info {
@@ -16,13 +19,17 @@ DECL|dma_qmsi_get_power_state|function|static uint32_t dma_qmsi_get_power_state(
 DECL|dma_qmsi_init|function|int dma_qmsi_init(struct device *dev)
 DECL|dma_qmsi_set_power_state|function|static void dma_qmsi_set_power_state(struct device *dev, uint32_t power_state)
 DECL|dma_qmsi_set_power_state|macro|dma_qmsi_set_power_state
+DECL|dma_qmsi_start|function|static int dma_qmsi_start(struct device *dev, uint32_t channel)
+DECL|dma_qmsi_stop|function|static int dma_qmsi_stop(struct device *dev, uint32_t channel)
 DECL|dma_qmsi_transfer_config|function|static int dma_qmsi_transfer_config(struct device *dev, uint32_t channel, struct dma_transfer_config *config)
 DECL|dma_qmsi_transfer_start|function|static int dma_qmsi_transfer_start(struct device *dev, uint32_t channel)
 DECL|dma_qmsi_transfer_stop|function|static int dma_qmsi_transfer_stop(struct device *dev, uint32_t channel)
 DECL|dma_resume_device|function|static int dma_resume_device(struct device *dev)
 DECL|dma_suspend_device|function|static int dma_suspend_device(struct device *dev)
+DECL|dma_user_callback|member|void (*dma_user_callback[QM_DMA_CHANNEL_NUM])(struct device *dev,
 DECL|error|member|void (*error[QM_DMA_CHANNEL_NUM])(struct device *dev, void *data);
 DECL|index|member|uint32_t index;
 DECL|instance|member|qm_dma_t instance; /* Controller instance. */
 DECL|saved_ctx|member|qm_dma_context_t saved_ctx;
 DECL|transfer|member|void (*transfer[QM_DMA_CHANNEL_NUM])(struct device *dev, void *data);
+DECL|width_index|function|static int width_index(uint32_t num_bytes, uint32_t *index)
