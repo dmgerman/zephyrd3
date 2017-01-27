8f12b858114a6587ea9cd1b9a6fe7f05a6aad1c4
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-01-27 15:49:06 +0200

    net: tests: Add unit tests for net_addr_pton() utils function
    Change-Id: Ib1e89ce3bb7eff5afe14394062ee6f53a28cc4e6
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 10e5162fc2e51ae3a65148175326a6488a5b55c0

--- tests/net/utils/src/main.c
+++ tests/net/utils/src/main.c
@@ -1,11 +1,43 @@
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|addr|member|struct in6_addr addr;
+DECL|addr|member|struct in_addr addr;
+DECL|c_addr|member|char c_addr[16];
+DECL|c_addr|member|char c_addr[46];
+DECL|c_verify|member|char c_verify[16];
+DECL|c_verify|member|char c_verify[46];
+DECL|data|member|struct net_addr_test_data *data;
+DECL|family|member|sa_family_t family;
+DECL|ipv4_pton_1|variable|ipv4_pton_1
+DECL|ipv4_pton_2|variable|ipv4_pton_2
+DECL|ipv4_pton_3|variable|ipv4_pton_3
+DECL|ipv4_pton_4|variable|ipv4_pton_4
+DECL|ipv4_pton_5|variable|ipv4_pton_5
+DECL|ipv4_pton_6|variable|ipv4_pton_6
+DECL|ipv4_pton_7|variable|ipv4_pton_7
+DECL|ipv4_pton_8|variable|ipv4_pton_8
+DECL|ipv4|member|} ipv4;
+DECL|ipv6_pton_1|variable|ipv6_pton_1
+DECL|ipv6_pton_2|variable|ipv6_pton_2
+DECL|ipv6_pton_3|variable|ipv6_pton_3
+DECL|ipv6_pton_4|variable|ipv6_pton_4
+DECL|ipv6_pton_5|variable|ipv6_pton_5
+DECL|ipv6_pton_6|variable|ipv6_pton_6
+DECL|ipv6|member|} ipv6;
 DECL|main_thread|function|void main_thread(void)
 DECL|main|function|void main(void)
+DECL|name|member|const char *name;
+DECL|net_addr_test_data|struct|struct net_addr_test_data {
 DECL|pkt1|variable|pkt1
 DECL|pkt2|variable|pkt2
 DECL|pkt3|variable|pkt3
 DECL|pkt4|variable|pkt4
 DECL|pkt5|variable|pkt5
+DECL|pton|member|bool pton;
+DECL|run_net_addr_tests|function|static bool run_net_addr_tests(void)
 DECL|run_tests|function|static bool run_tests(void)
+DECL|test_net_addr|function|static bool test_net_addr(struct net_addr_test_data *data)
+DECL|tests|variable|tests
 DECL|thread_stack|variable|thread_stack
+DECL|verify|member|struct in6_addr verify;
+DECL|verify|member|struct in_addr verify;
