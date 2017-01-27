e09fc5434fc213657de61cc700f67dd70dbb0a40
Kumar Gala <kumar.gala@linaro.org>
2017-01-27 14:35:00 -0600

    Bluetooth: hci_raw: Make bt_buf_get_rx args consistent
    The hci_raw version of bt_buf_get_rx was expecting an int for timeout.
    Let us int32_t instead so we match both the hci_core version and the
    type that net_buf_alloc expects.
    
    This addresses a possible build issue if/when int32_t differs from our
    default (ie, newlib).
    
    Change-Id: I69374c48da8f2b96fa2bd418ff505fbaacda11f0
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: edcb6ceb12701b478199f7b9db10f33b5a6a3dbf

--- subsys/bluetooth/host/hci_raw.c
+++ subsys/bluetooth/host/hci_raw.c
@@ -1,7 +1,7 @@
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
 DECL|bt_buf_get_acl|function|struct net_buf *bt_buf_get_acl(int32_t timeout)
 DECL|bt_buf_get_evt|function|struct net_buf *bt_buf_get_evt(uint8_t opcode, int timeout)
-DECL|bt_buf_get_rx|function|struct net_buf *bt_buf_get_rx(int timeout)
+DECL|bt_buf_get_rx|function|struct net_buf *bt_buf_get_rx(int32_t timeout)
 DECL|bt_dev|variable|bt_dev
 DECL|bt_enable_raw|function|int bt_enable_raw(struct k_fifo *rx_queue)
 DECL|bt_hci_driver_register|function|int bt_hci_driver_register(struct bt_hci_driver *drv)
