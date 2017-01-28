f142b353df7f89f59238cfde9a4ddffa837d5b09
Kumar Gala <kumar.gala@linaro.org>
2017-01-27 20:39:33 -0600

    arm: cmsis: Convert _Scb*Fault*Reset to use direct CMSIS register access
    Coverted:
    	_ScbMemFaultMmfarReset
    	_ScbBusFaultBfarReset
    	_ScbUsageFaultAllFaultsReset
    
    To use direct CMSIS register access.
    
    Also removed scb.h and references as there is no longer any code in it.
    
    Jira: ZEP-1568
    
    Change-Id: I469f6af39d1bd41db712454b0b3e5ab331979033
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 52cf45c62a1aae5bd3f9e4c61345eae5c9747374

--- include/arch/arm/cortex_m/scb.h
+++
@@ -1,4 +0,0 @@
-DECL|_SCB__H_|macro|_SCB__H_
-DECL|_ScbBusFaultBfarReset|function|static inline void _ScbBusFaultBfarReset(void)
-DECL|_ScbMemFaultMmfarReset|function|static inline void _ScbMemFaultMmfarReset(void)
-DECL|_ScbUsageFaultAllFaultsReset|function|static inline void _ScbUsageFaultAllFaultsReset(void)
