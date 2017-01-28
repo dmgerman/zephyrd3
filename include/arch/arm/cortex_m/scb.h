cd6423dbd2e20a02f4d58cbc5b76b6a39051912e
Kumar Gala <kumar.gala@linaro.org>
2017-01-27 20:36:51 -0600

    arm: cmsis: Convert _Scb*FaultAddrGet to use direct CMSIS register access
    Coverted:
    	_ScbBusFaultAddrGet
    	_ScbMemFaultAddrGet
    
    To use direct CMSIS register access
    
    Jira: ZEP-1568
    
    Change-Id: Ic49b3ac3fc4fb63d413f273569c77f6539e4e572
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 737207f052ae6d48a9b2566d4c040e86e38615f6

--- include/arch/arm/cortex_m/scb.h
+++ include/arch/arm/cortex_m/scb.h
@@ -1,9 +1,7 @@
 DECL|_SCB__H_|macro|_SCB__H_
-DECL|_ScbBusFaultAddrGet|function|static inline uint32_t _ScbBusFaultAddrGet(void)
 DECL|_ScbBusFaultAllFaultsReset|function|static inline void _ScbBusFaultAllFaultsReset(void)
 DECL|_ScbBusFaultBfarReset|function|static inline void _ScbBusFaultBfarReset(void)
 DECL|_ScbHardFaultAllFaultsReset|function|static inline int _ScbHardFaultAllFaultsReset(void)
-DECL|_ScbMemFaultAddrGet|function|static inline uint32_t _ScbMemFaultAddrGet(void)
 DECL|_ScbMemFaultAllFaultsReset|function|static inline void _ScbMemFaultAllFaultsReset(void)
 DECL|_ScbMemFaultMmfarReset|function|static inline void _ScbMemFaultMmfarReset(void)
 DECL|_ScbUsageFaultAllFaultsReset|function|static inline void _ScbUsageFaultAllFaultsReset(void)
