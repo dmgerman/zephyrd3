5d36fb420969ee9f32ee80abe47c0dc0ac23062f
Benjamin Walsh <walsh.benj@gmail.com>
2017-01-28 16:56:18 -0500

    kernel: add _WAIT_Q_INIT()
    Dissociate wait queue initialization from doubly-linked lists if the
    underlying implementation is to be abstracted.
    
    Change-Id: Id7544c6ac506643437f9c4f0ae97e7eecab8d06d
    Signed-off-by: Benjamin Walsh <walsh.benj@gmail.com>
    
    Former-commit-id: fcdb0fd6eaeb4fc6ab9ba7ce8a9f217b97d343e8

--- kernel/include/wait_q.h
+++ kernel/include/wait_q.h
@@ -1,3 +1,4 @@
+DECL|_WAIT_Q_INIT|macro|_WAIT_Q_INIT
 DECL|_abort_thread_timeout|function|static ALWAYS_INLINE int _abort_thread_timeout(struct k_thread *thread)
 DECL|_add_thread_timeout|function|_add_thread_timeout(struct k_thread *thread, _wait_q_t *wait_q, int32_t timeout)
 DECL|_get_next_timeout_expiry|macro|_get_next_timeout_expiry
