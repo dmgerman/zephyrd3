1304c4f7a5f43fe9424016c1a96452924899189d
Benjamin Walsh <walsh.benj@gmail.com>
2017-01-29 16:34:53 -0500

    tests: add poll kernel test
    Change-Id: I87af7c404b9c77fd82746e5bc2baf4df74d9380c
    Signed-off-by: Benjamin Walsh <walsh.benj@gmail.com>
    
    Former-commit-id: ed536245f735d5b7f3ea24cc8bc1930d487726b7

---
+++ tests/kernel/poll/src/test_poll.c
@@ -0,0 +1,23 @@
+DECL|FIFO_MSG_VALUE|macro|FIFO_MSG_VALUE
+DECL|SIGNAL_RESULT|macro|SIGNAL_RESULT
+DECL|eaddrinuse_hogger_stack|variable|eaddrinuse_hogger_stack
+DECL|eaddrinuse_hogger|function|static void eaddrinuse_hogger(void *p1, void *p2, void *p3)
+DECL|eaddrinuse_ready_sem|variable|eaddrinuse_ready_sem
+DECL|eaddrinuse_reply|variable|eaddrinuse_reply
+DECL|eaddrinuse_sem|variable|eaddrinuse_sem
+DECL|fifo_msg|struct|struct fifo_msg {
+DECL|msg|member|uint32_t msg;
+DECL|no_wait_fifo|variable|no_wait_fifo
+DECL|no_wait_sem|variable|no_wait_sem
+DECL|no_wait_signal|variable|no_wait_signal
+DECL|poll_wait_helper_stack|variable|poll_wait_helper_stack
+DECL|poll_wait_helper|function|static void poll_wait_helper(void *use_fifo, void *p2, void *p3)
+DECL|private|member|void *private;
+DECL|test_poll_eaddrinuse|function|void test_poll_eaddrinuse(void)
+DECL|test_poll_no_wait|function|void test_poll_no_wait(void)
+DECL|test_poll_wait|function|void test_poll_wait(void)
+DECL|wait_events|variable|wait_events
+DECL|wait_fifo|variable|wait_fifo
+DECL|wait_msg|variable|wait_msg
+DECL|wait_sem|variable|wait_sem
+DECL|wait_signal|variable|wait_signal
