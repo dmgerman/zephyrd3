c84fcf310597be70cf02d13d5e9b44d352a61e5f
Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
2017-01-29 18:01:38 +0100

    flash/stm32: driver for STM32F4x series
    Change-Id: I0f90bcda7a694de81c594a6616da0faf40306702
    Signed-off-by: Jorge Ramirez-Ortiz <jorge.ramirez-ortiz@linaro.org>
    
    Former-commit-id: 29a8e0292e244842d40972de87d54c7835d9335d

--- arch/arm/soc/st_stm32/stm32f4/flash_registers.h
+++ arch/arm/soc/st_stm32/stm32f4/flash_registers.h
@@ -7,17 +7,17 @@ DECL|STM32F4X_FLASH_LATENCY_5|enumerator|STM32F4X_FLASH_LATENCY_5 = 0x5,
 DECL|_STM32F4X_FLASH_REGISTERS_H_|macro|_STM32F4X_FLASH_REGISTERS_H_
 DECL|__flash_acr|union|union __flash_acr {
 DECL|__setup_flash|function|static inline void __setup_flash(void)
-DECL|acr|member|union __flash_acr acr;
+DECL|acr|member|volatile union __flash_acr acr;
 DECL|bit|member|} bit;
 DECL|ctrl|member|volatile uint32_t ctrl;
 DECL|dcen|member|uint32_t dcen :1 __packed;
 DECL|dcrst|member|uint32_t dcrst :1 __packed;
 DECL|icen|member|uint32_t icen :1 __packed;
 DECL|icrst|member|uint32_t icrst :1 __packed;
-DECL|key|member|uint32_t key;
+DECL|key|member|volatile uint32_t key;
 DECL|latency|member|uint32_t latency :4 __packed;
-DECL|optctrl|member|uint32_t optctrl;
-DECL|optkey|member|uint32_t optkey;
+DECL|optctrl|member|volatile uint32_t optctrl;
+DECL|optkey|member|volatile uint32_t optkey;
 DECL|prften|member|uint32_t prften :1 __packed;
 DECL|rsvd__13_31|member|uint32_t rsvd__13_31 :19 __packed;
 DECL|rsvd__4_7|member|uint32_t rsvd__4_7 :4 __packed;
---
+++ drivers/flash/flash_stm32f4x.c
@@ -0,0 +1,16 @@
+DECL|check_status|function|static int check_status(struct stm32f4x_flash *regs)
+DECL|erase_sector|function|static int erase_sector(uint16_t sector, struct stm32f4x_flash *regs)
+DECL|flash_data|variable|flash_data
+DECL|flash_priv|struct|struct flash_priv {
+DECL|flash_stm32f4x_api|variable|flash_stm32f4x_api
+DECL|flash_stm32f4x_erase|function|static int flash_stm32f4x_erase(struct device *dev, off_t offset, size_t len)
+DECL|flash_stm32f4x_read|function|static int flash_stm32f4x_read(struct device *dev, off_t offset, void *data,size_t len)
+DECL|flash_stm32f4x_write_protection|function|static int flash_stm32f4x_write_protection(struct device *dev, bool enable)
+DECL|flash_stm32f4x_write|function|static int flash_stm32f4x_write(struct device *dev, off_t offset,const void *data, size_t len)
+DECL|flush_caches|function|static void flush_caches(struct stm32f4x_flash *regs)
+DECL|regs|member|struct stm32f4x_flash *regs;
+DECL|sem|member|struct k_sem sem;
+DECL|stm32f4x_flash_init|function|static int stm32f4x_flash_init(struct device *dev)
+DECL|valid_range|function|static bool valid_range(off_t offset, uint32_t len)
+DECL|wait_flash_idle|function|static int wait_flash_idle(struct stm32f4x_flash *regs)
+DECL|write_byte|function|static int write_byte(off_t offset, uint8_t val, struct stm32f4x_flash *regs)
