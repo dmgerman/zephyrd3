6313084d741b1bff16829e6882b439185724811d
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-01-30 12:30:42 +0200

    net: tests: Add unit tests for net_addr_ntop() utils function
    Change-Id: I72ee5f6444d2e45de0ee7a3aa96884282d41b72d
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 2de8bd928af35df41e5a7cb5db52275a841e446a

--- tests/net/utils/src/main.c
+++ tests/net/utils/src/main.c
@@ -8,6 +8,14 @@ DECL|c_verify|member|char c_verify[16];
 DECL|c_verify|member|char c_verify[46];
 DECL|data|member|struct net_addr_test_data *data;
 DECL|family|member|sa_family_t family;
+DECL|ipv4_ntop_1|variable|ipv4_ntop_1
+DECL|ipv4_ntop_2|variable|ipv4_ntop_2
+DECL|ipv4_ntop_3|variable|ipv4_ntop_3
+DECL|ipv4_ntop_4|variable|ipv4_ntop_4
+DECL|ipv4_ntop_5|variable|ipv4_ntop_5
+DECL|ipv4_ntop_6|variable|ipv4_ntop_6
+DECL|ipv4_ntop_7|variable|ipv4_ntop_7
+DECL|ipv4_ntop_8|variable|ipv4_ntop_8
 DECL|ipv4_pton_1|variable|ipv4_pton_1
 DECL|ipv4_pton_2|variable|ipv4_pton_2
 DECL|ipv4_pton_3|variable|ipv4_pton_3
@@ -17,6 +25,12 @@ DECL|ipv4_pton_6|variable|ipv4_pton_6
 DECL|ipv4_pton_7|variable|ipv4_pton_7
 DECL|ipv4_pton_8|variable|ipv4_pton_8
 DECL|ipv4|member|} ipv4;
+DECL|ipv6_ntop_1|variable|ipv6_ntop_1
+DECL|ipv6_ntop_2|variable|ipv6_ntop_2
+DECL|ipv6_ntop_3|variable|ipv6_ntop_3
+DECL|ipv6_ntop_4|variable|ipv6_ntop_4
+DECL|ipv6_ntop_5|variable|ipv6_ntop_5
+DECL|ipv6_ntop_6|variable|ipv6_ntop_6
 DECL|ipv6_pton_1|variable|ipv6_pton_1
 DECL|ipv6_pton_2|variable|ipv6_pton_2
 DECL|ipv6_pton_3|variable|ipv6_pton_3
