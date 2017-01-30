efd6e361804358fd074743524224579b2a6ad848
Sathish Narasimman <sathish.narasimman@intel.com>
2017-01-30 18:43:25 +0530

    Bluetooth: HFP HF: Handling AG Network error
    This patch includes handling AG network error i.e +CME ERROR and
    report the error number.
    
    Change-Id: I19a3158e44568ad0ad21fb0dd790ac2f554c0625
    Signed-off-by: Sathish Narasimman <sathish.narasimman@intel.com>
    
    Former-commit-id: 83bb08fbd93fcbbd1542f1b3bb370d231681e5e6

--- subsys/bluetooth/host/at.c
+++ subsys/bluetooth/host/at.c
@@ -16,6 +16,7 @@ DECL|at_register_unsolicited|function|void at_register_unsolicited(struct at_cli
 DECL|at_register|function|void at_register(struct at_client *at, at_resp_cb_t resp, at_finish_cb_t finish)
 DECL|at_state_get_cmd_string|function|static int at_state_get_cmd_string(struct at_client *at, struct net_buf *buf)
 DECL|at_state_get_result_string|function|static int at_state_get_result_string(struct at_client *at, struct net_buf *buf)
+DECL|at_state_process_ag_nw_err|function|static int at_state_process_ag_nw_err(struct at_client *at, struct net_buf *buf)
 DECL|at_state_process_cmd|function|static int at_state_process_cmd(struct at_client *at, struct net_buf *buf)
 DECL|at_state_process_result|function|static int at_state_process_result(struct at_client *at, struct net_buf *buf)
 DECL|at_state_start_cr|function|static int at_state_start_cr(struct at_client *at, struct net_buf *buf)
@@ -23,8 +24,10 @@ DECL|at_state_start_lf|function|static int at_state_start_lf(struct at_client *a
 DECL|at_state_start|function|static int at_state_start(struct at_client *at, struct net_buf *buf)
 DECL|at_state_unsolicited_cmd|function|static int at_state_unsolicited_cmd(struct at_client *at, struct net_buf *buf)
 DECL|cmd_parser_cb|variable|cmd_parser_cb
+DECL|cme_handle|function|int cme_handle(struct at_client *at)
 DECL|get_cmd_value|function|static int get_cmd_value(struct at_client *at, struct net_buf *buf, char stop_byte, enum at_cmd_state cmd_state)
 DECL|get_response_string|function|static int get_response_string(struct at_client *at, struct net_buf *buf, char stop_byte, enum at_state state)
+DECL|is_cmer|function|static bool is_cmer(struct at_client *at)
 DECL|next_list|function|static void next_list(struct at_client *at)
 DECL|parser_cb|variable|parser_cb
 DECL|reset_buffer|function|static void reset_buffer(struct at_client *at)
