b8011ec9256b50e7fa0ecd6d093b6e2976502192
Baohong Liu <baohong.liu@intel.com>
2017-02-01 11:33:42 -0800

    tests: dma: update dma loop transfer app
    Update the dma loop transfer sample app to use the new
    dma api interfaces. This change is based on the api change
    and the updated dma driver.
    
    A RFC was posted recently on this.
    
    Jira: ZEP-873
    
    Change-Id: I289e2e08d4c775a833bf3d585d2706a903edd0bc
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 59b8af5395c339ad4048941480397a159fddf36a

--- tests/drivers/dma/test_loop_transfer/src/dma.c
+++ tests/drivers/dma/test_loop_transfer/src/dma.c
@@ -2,9 +2,12 @@ DECL|DMA_DEVICE_NAME|macro|DMA_DEVICE_NAME
 DECL|RX_BUFF_SIZE|macro|RX_BUFF_SIZE
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|TRANSFER_LOOPS|macro|TRANSFER_LOOPS
+DECL|dma_block_cfg|variable|dma_block_cfg
+DECL|dma_cfg|variable|dma_cfg
+DECL|dma_user_callback|function|static void dma_user_callback(struct device *dev, uint32_t id, int error_code)
 DECL|main|function|void main(void)
 DECL|rx_data|variable|rx_data
-DECL|test_error|function|static void test_error(struct device *dev, void *data)
-DECL|test_transfer|function|static void test_transfer(struct device *dev, void *data)
+DECL|test_error|function|static void test_error(void)
+DECL|test_transfer|function|static void test_transfer(struct device *dev, uint32_t id)
 DECL|transfer_count|variable|transfer_count
 DECL|tx_data|variable|tx_data
