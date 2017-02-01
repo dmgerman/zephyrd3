b40dc496a61d99dd5865f7e8da9ef3d3ae2a2095
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-02-01 12:49:04 +0200

    tests: net: Add tests for network interface
    Change-Id: I35f0b3f1794c65b08d32ca561b6189ee59a7a76c
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: cc8eb4144b58c7d704365ed3fcb421a92d4aa930

---
+++ tests/net/iface/src/main.c
@@ -0,0 +1,37 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
+DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|idx|member|uint8_t idx;
+DECL|iface1|variable|iface1
+DECL|iface2|variable|iface2
+DECL|iface3|variable|iface3
+DECL|iface_setup|function|static void iface_setup(void)
+DECL|in6addr_mcast|variable|in6addr_mcast
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|ll_addr|variable|ll_addr
+DECL|mac_addr|member|uint8_t mac_addr[sizeof(struct net_eth_addr)];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|my_addr3|variable|my_addr3
+DECL|net_if_test|struct|struct net_if_test {
+DECL|net_iface1_data|variable|net_iface1_data
+DECL|net_iface2_data|variable|net_iface2_data
+DECL|net_iface3_data|variable|net_iface3_data
+DECL|net_iface_api|variable|net_iface_api
+DECL|net_iface_dev_init|function|static int net_iface_dev_init(struct device *dev)
+DECL|net_iface_get_mac|function|static uint8_t *net_iface_get_mac(struct device *dev)
+DECL|net_iface_init|function|static void net_iface_init(struct net_if *iface)
+DECL|send_iface1_down|function|static void send_iface1_down(void)
+DECL|send_iface1_up|function|static void send_iface1_up(void)
+DECL|send_iface1|function|static void send_iface1(void)
+DECL|send_iface2|function|static void send_iface2(void)
+DECL|send_iface3|function|static void send_iface3(void)
+DECL|send_iface|function|static bool send_iface(struct net_if *iface, int val, bool expect_fail)
+DECL|sender_iface|function|static int sender_iface(struct net_if *iface, struct net_buf *buf)
+DECL|test_failed|variable|test_failed
+DECL|test_main|function|void test_main(void)
+DECL|test_started|variable|test_started
+DECL|wait_data|variable|wait_data
