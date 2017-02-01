8be82ad2546fcd64838952b01487b9cbdd5acd5c
Baohong Liu <baohong.liu@intel.com>
2017-01-31 16:26:09 -0800

    tests: dma: update dma block transfer app
    Update the dma block transfer sample app to use the new
    dma api interfaces. This change is based on the api change
    and the updated dma driver.
    
    A RFC was posted recently on this.
    
    Jira: ZEP-873
    
    Change-Id: Icf3bec7260c2e499485b07a4a579956448a1a3fd
    Signed-off-by: Baohong Liu <baohong.liu@intel.com>
    
    Former-commit-id: 00210386c62617b652fee404ef80c1719a0f1abf

--- tests/drivers/dma/test_chan_blen_transfer/src/test_dma.c
+++ tests/drivers/dma/test_chan_blen_transfer/src/test_dma.c
@@ -5,7 +5,6 @@ DECL|test_dma_m2m_chan0_burst16|function|void test_dma_m2m_chan0_burst16(void)
 DECL|test_dma_m2m_chan0_burst8|function|void test_dma_m2m_chan0_burst8(void)
 DECL|test_dma_m2m_chan1_burst16|function|void test_dma_m2m_chan1_burst16(void)
 DECL|test_dma_m2m_chan1_burst8|function|void test_dma_m2m_chan1_burst8(void)
-DECL|test_done|function|static void test_done(struct device *dev, void *data)
-DECL|test_error|function|static void test_error(struct device *dev, void *data)
+DECL|test_done|function|static void test_done(struct device *dev, uint32_t id, int error_code)
 DECL|test_task|function|static int test_task(uint32_t chan_id, uint32_t blen)
 DECL|tx_data|variable|tx_data
