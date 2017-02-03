826cb9cf8a42dd0f64770d9843fba54e7b9df2fb
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2017-02-03 04:50:04 +0100

    Bluetooth: controller: Refactor LL Adv state to ll_adv.c file
    Move advertising state related implementation out into a
    separate ll_adv.c file.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 6117d1dda7c701f0d39a22a7475d5c0217c80529

--- subsys/bluetooth/controller/ll_sw/ll.c
+++ subsys/bluetooth/controller/ll_sw/ll.c
@@ -1,40 +1,27 @@
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
 DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(radio_nrf5_isr)
-DECL|_ll_adv_params|variable|_ll_adv_params
 DECL|_ll_context|variable|_ll_context
 DECL|_ll_scan_params|variable|_ll_scan_params
-DECL|adv_addr|member|u8_t adv_addr[BDADDR_SIZE];
-DECL|chl_map|member|u8_t chl_map:3;
-DECL|direct_addr|member|u8_t direct_addr[BDADDR_SIZE];
 DECL|filter_policy|member|u8_t filter_policy:1;
-DECL|filter_policy|member|u8_t filter_policy:2;
-DECL|interval|member|u16_t interval;
 DECL|interval|member|u16_t interval;
 DECL|ll_addr_get|function|u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
 DECL|ll_addr_set|function|void ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
-DECL|ll_adv_data_set|function|void ll_adv_data_set(u8_t len, u8_t const *const data)
-DECL|ll_adv_enable|function|u32_t ll_adv_enable(u8_t enable)
-DECL|ll_adv_params_set|function|void ll_adv_params_set(u16_t interval, u8_t adv_type, u8_t own_addr_type, u8_t direct_addr_type, u8_t const *const direct_addr, u8_t chl_map, u8_t filter_policy)
 DECL|ll_create_connection|function|u32_t ll_create_connection(u16_t scan_interval, u16_t scan_window, u8_t filter_policy, u8_t peer_addr_type, u8_t *peer_addr, u8_t own_addr_type, u16_t interval, u16_t latency, u16_t timeout)
 DECL|ll_init|function|int ll_init(struct k_sem *sem_rx)
-DECL|ll_scan_data_set|function|void ll_scan_data_set(u8_t len, u8_t const *const data)
 DECL|ll_scan_enable|function|u32_t ll_scan_enable(u8_t enable)
 DECL|ll_scan_params_set|function|void ll_scan_params_set(u8_t scan_type, u16_t interval, u16_t window,u8_t own_addr_type, u8_t filter_policy)
 DECL|mayfly_enable_cb|function|void mayfly_enable_cb(u8_t caller_id, u8_t callee_id, u8_t enable)
 DECL|mayfly_is_enabled|function|u32_t mayfly_is_enabled(u8_t caller_id, u8_t callee_id)
 DECL|mayfly_pend|function|void mayfly_pend(u8_t caller_id, u8_t callee_id)
 DECL|mayfly_prio_is_equal|function|u32_t mayfly_prio_is_equal(u8_t caller_id, u8_t callee_id)
-DECL|pdu_adv_type|member|u8_t pdu_adv_type:4;
 DECL|pub_addr|member|u8_t pub_addr[BDADDR_SIZE];
 DECL|radio_active_callback|function|void radio_active_callback(u8_t active)
 DECL|radio_event_callback|function|void radio_event_callback(void)
 DECL|rnd_addr|member|u8_t rnd_addr[BDADDR_SIZE];
 DECL|rng_nrf5_isr|function|static void rng_nrf5_isr(void *arg)
 DECL|rtc0_nrf5_isr|function|static void rtc0_nrf5_isr(void *arg)
-DECL|rx_addr|member|u8_t rx_addr:1;
 DECL|scan_type|member|u8_t scan_type:1;
 DECL|sem_recv|variable|sem_recv
 DECL|swi4_nrf5_isr|function|static void swi4_nrf5_isr(void *arg)
-DECL|tx_addr|member|u8_t tx_addr:1;
 DECL|tx_addr|member|u8_t tx_addr:1;
 DECL|window|member|u16_t window;
