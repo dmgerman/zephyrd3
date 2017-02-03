0cb0efa49b80358f6c0bdcb8df24c96398465540
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2017-02-03 04:50:04 +0100

    Bluetooth: controller: Refactor LL Master role to ll_master.c
    Move master role related implementation out into a separate
    ll_master.c file.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: d39d8224be14db6e119f116d413de196a4f9087f

--- subsys/bluetooth/controller/ll_sw/ll.c
+++ subsys/bluetooth/controller/ll_sw/ll.c
@@ -3,7 +3,6 @@ DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(radio_nrf5_isr)
 DECL|_ll_context|variable|_ll_context
 DECL|ll_addr_get|function|u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
 DECL|ll_addr_set|function|void ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
-DECL|ll_create_connection|function|u32_t ll_create_connection(u16_t scan_interval, u16_t scan_window, u8_t filter_policy, u8_t peer_addr_type, u8_t *peer_addr, u8_t own_addr_type, u16_t interval, u16_t latency, u16_t timeout)
 DECL|ll_init|function|int ll_init(struct k_sem *sem_rx)
 DECL|mayfly_enable_cb|function|void mayfly_enable_cb(u8_t caller_id, u8_t callee_id, u8_t enable)
 DECL|mayfly_is_enabled|function|u32_t mayfly_is_enabled(u8_t caller_id, u8_t callee_id)
---
+++ subsys/bluetooth/controller/ll_sw/ll_master.c
@@ -0,0 +1,1 @@
+DECL|ll_create_connection|function|u32_t ll_create_connection(u16_t scan_interval, u16_t scan_window, u8_t filter_policy, u8_t peer_addr_type, u8_t *peer_addr, u8_t own_addr_type, u16_t interval, u16_t latency, u16_t timeout)
