23454c1ee8f1893facaf6d0868f263e9414f3b79
Flavio Santes <flavio.santes@intel.com>
2017-02-03 23:02:28 -0600

    tests/net/dns: Add the test case for the DNS low-level routines
    This patch adds the test case for the DNS API low-level routines.
    
    The following routines are evaluated by this test case:
    
    - dns_msg_pack_qname
    - dns_msg_pack_query
    - dns_unpack_header_id
    - dns_header_qr
    - dns_header_opcode
    - dns_header_aa
    - dns_header_tc
    - dns_header_rd
    - dns_header_ra
    - dns_header_z
    - dns_header_rcode
    - dns_header_qdcount
    - dns_header_ancount
    - dns_header_nscount
    - dns_header_arcount
    - dns_unpack_query_qclass
    - dns_unpack_query_qtype
    
    Next iterations will consider IPv6 and DNS RR coverage.
    
    Jira: ZEP-1499
    
    Change-Id: I4ef46203e6fec57c6fe5a5db8132ed140b412fc2
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: bed2f43120548415ba459da29b90600922a5df76

---
+++ tests/net/lib/dns_packet/src/dns_packet.c
@@ -0,0 +1,12 @@
+DECL|DNAME1|macro|DNAME1
+DECL|DNS_HEADER_SIZE|macro|DNS_HEADER_SIZE
+DECL|MAX_BUF_SIZE|macro|MAX_BUF_SIZE
+DECL|buf_len|variable|buf_len
+DECL|buf|variable|buf
+DECL|eval_query|function|static int eval_query(const char *dname, uint16_t tid, enum dns_rr_type type, uint8_t *expected, uint16_t expected_len)
+DECL|qname_len|variable|qname_len
+DECL|qname|variable|qname
+DECL|query_ipv4|variable|query_ipv4
+DECL|test_dns_query|function|void test_dns_query(void)
+DECL|test_main|function|void test_main(void)
+DECL|tid1|variable|tid1
