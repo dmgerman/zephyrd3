45757967fccc20b032c502b0e4b5e78477497c08
Juan Solano <juanx.solano.menacho@intel.com>
2017-02-07 13:08:14 +0100

    quark_se_ss: Remove enter_arc_state and use QMSI functions
    This commit removes the local implementation of enter_arc_state, where
    the ARC is instructed to sleep, using instead the QMSI 1.4 functions.
    
    Change-Id: Id489ad53851be50fc5e50add698891fcfaef3abe
    Signed-off-by: Juan Solano <juanx.solano.menacho@intel.com>
    
    Former-commit-id: d4905739330e7a54ae336a9c67813d0f7aa2140e

--- arch/arc/soc/quark_se_c1000_ss/power.c
+++ arch/arc/soc/quark_se_c1000_ss/power.c
@@ -1,9 +1,3 @@
-DECL|ARC_SS1|macro|ARC_SS1
-DECL|ARC_SS2|macro|ARC_SS2
-DECL|ENABLE_INTERRUPTS|macro|ENABLE_INTERRUPTS
-DECL|SLEEP_MODE_CORE_OFF|macro|SLEEP_MODE_CORE_OFF
-DECL|SLEEP_MODE_CORE_TIMERS_RTC_OFF|macro|SLEEP_MODE_CORE_TIMERS_RTC_OFF
 DECL|_deep_sleep|function|static void _deep_sleep(enum power_states state)
 DECL|_sys_soc_power_state_post_ops|function|void _sys_soc_power_state_post_ops(enum power_states state)
 DECL|_sys_soc_set_power_state|function|void _sys_soc_set_power_state(enum power_states state)
-DECL|enter_arc_state|function|static void enter_arc_state(int mode)
