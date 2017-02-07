3ff51a17f4a45ae3ebc26d1447b20f3efdfd29f3
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-02-07 14:29:21 +0200

    net: nbuf: Fix net_nbuf_compact() API
    Current API description of net_nbuf_compact() is not very clear.
    The first parameter needs to be the first net_buf in the chain.
    
    The changes to this API are needed in order to clarify following
    use cases:
    1) User provides fragment that is not first of the chain and compact is
    successfully done. In this case there is no free space in fragment list
    after the input fragment. But there might be empty space in previous
    fragments. So fragment chain is not completely compacted.
    
    2) What if input fragment has been deleted and api returns the same
    buf?
    
    So this commit simplifies the API behavior. Now net_nbuf_compact()
    expects the first parameter to be either TX or RX net_buf and then it
    compacts it. It fails only if the input fragment is a data fragment.
    
    Change-Id: I9e02dfcb6f3f2e2998826522a25ec207850a8056
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 58ba1d834e1b8444585c80e3eddb3e9545f046aa

--- subsys/net/ip/nbuf.c
+++ subsys/net/ip/nbuf.c
@@ -46,7 +46,7 @@ DECL|insert_data|function|static inline bool insert_data(struct net_buf *buf, st
 DECL|is_from_data_pool|function|static inline bool is_from_data_pool(struct net_buf *buf)
 DECL|net_nbuf_append_bytes|function|static inline bool net_nbuf_append_bytes(struct net_buf *buf, uint8_t *value, uint16_t len, int32_t timeout)
 DECL|net_nbuf_append|function|bool net_nbuf_append(struct net_buf *buf, uint16_t len, uint8_t *data, int32_t timeout)
-DECL|net_nbuf_compact|function|struct net_buf *net_nbuf_compact(struct net_buf *buf)
+DECL|net_nbuf_compact|function|bool net_nbuf_compact(struct net_buf *buf)
 DECL|net_nbuf_copy|function|struct net_buf *net_nbuf_copy(struct net_buf *orig, size_t amount, size_t reserve, int32_t timeout)
 DECL|net_nbuf_get_data_debug|function|struct net_buf *net_nbuf_get_data_debug(struct net_context *context,int32_t timeout, const char *caller, int line)
 DECL|net_nbuf_get_data|function|struct net_buf *net_nbuf_get_data(struct net_context *context, int32_t timeout)
