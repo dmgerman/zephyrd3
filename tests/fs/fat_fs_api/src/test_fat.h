1f2b1885830f9059d8697470a7fcfb3cc773e60d
Qiu Peiyang <peiyangx.qiu@intel.com>
2017-02-08 15:14:51 +0800

    tests: add filesystem api test
    This commit verifies below FS APIs:
    	fs_open()
    	fs_close()
    	fs_read()
    	fs_write()
    	fs_truncate()
    	fs_seek()
    	fs_tell()
    	fs_sync()
    	fs_unlink()
    	fs_mkdir()
    	fs_opendir()
    	fs_closedir()
    	fs_readdir()
    	fs_stat()
    	fs_statvfs()
    
    Change-Id: I1a48b9e960b62a1c4986cc577f658199da7a28cd
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>
    
    Former-commit-id: 8a119a4319ae6b6e918e9404fb7006b628892e91

---
+++ tests/fs/fat_fs_api/src/test_fat.h
@@ -0,0 +1,3 @@
+DECL|TEST_DIR|macro|TEST_DIR
+DECL|TEST_FILE|macro|TEST_FILE
+DECL|filep|variable|filep
---
+++ tests/fs/fat_fs_api/src/test_fat_dir.c
@@ -0,0 +1,4 @@
+DECL|test_fat_dir|function|void test_fat_dir(void)
+DECL|test_lsdir|function|static int test_lsdir(const char *path)
+DECL|test_mkdir|function|static int test_mkdir(void)
+DECL|test_rmdir|function|static int test_rmdir(void)
