200e7faac2ecc763da19be8f9ad5859566a047e3
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2017-02-08 13:23:04 +0200

    tests/common: Add tests for sys_dlist_*
    Change-Id: I95fbaf902968b71ce42fd6d04a10c41a0be4ff03
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: f7100f80926aac618dd3035fafcd15c2415dcb97

---
+++ tests/kernel/common/src/dlist.c
@@ -0,0 +1,12 @@
+DECL|container_node|struct|struct container_node {
+DECL|dlist_test|function|void dlist_test(void)
+DECL|node|member|sys_dnode_t node;
+DECL|test_list|variable|test_list
+DECL|test_node_1|variable|test_node_1
+DECL|test_node_2|variable|test_node_2
+DECL|test_node_3|variable|test_node_3
+DECL|test_node_4|variable|test_node_4
+DECL|unused|member|int unused;
+DECL|verify_content_amount|function|static inline bool verify_content_amount(sys_dlist_t *list, int amount)
+DECL|verify_emptyness|function|static inline bool verify_emptyness(sys_dlist_t *list)
+DECL|verify_tail_head|function|static inline bool verify_tail_head(sys_dlist_t *list, sys_dnode_t *head, sys_dnode_t *tail, bool same)
