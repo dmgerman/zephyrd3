2d301b172a071fb524957a4c9aef3d2030bf2fc9
Marcus Shawcroft <marcus.shawcroft@arm.com>
2017-02-09 15:04:28 +0000

    net/dhcpv4: Refactor dhcpv4 message type representation.
    Change the message type representation from integer to enum, this will
    help prevent re-occurrence of recent issues where states and events
    are interchanged by making it more likely the compiler will see a type
    mismatch.
    
    Change-Id: Ia235afda428a9e5dfbd933e02beeae468b4c84a2
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: 5dcc56ebedaaa0fb8a389f8d732ad29965c70959

--- subsys/net/ip/dhcpv4.c
+++ subsys/net/ip/dhcpv4.c
@@ -6,14 +6,14 @@ DECL|DHCPV4_MAX_NUMBER_OF_ATTEMPTS|macro|DHCPV4_MAX_NUMBER_OF_ATTEMPTS
 DECL|DHCPV4_MSG_BOOT_REPLY|macro|DHCPV4_MSG_BOOT_REPLY
 DECL|DHCPV4_MSG_BOOT_REQUEST|macro|DHCPV4_MSG_BOOT_REQUEST
 DECL|DHCPV4_MSG_BROADCAST|macro|DHCPV4_MSG_BROADCAST
-DECL|DHCPV4_MSG_TYPE_ACK|macro|DHCPV4_MSG_TYPE_ACK
-DECL|DHCPV4_MSG_TYPE_DECLINE|macro|DHCPV4_MSG_TYPE_DECLINE
-DECL|DHCPV4_MSG_TYPE_DISCOVER|macro|DHCPV4_MSG_TYPE_DISCOVER
-DECL|DHCPV4_MSG_TYPE_INFORM|macro|DHCPV4_MSG_TYPE_INFORM
-DECL|DHCPV4_MSG_TYPE_NAK|macro|DHCPV4_MSG_TYPE_NAK
-DECL|DHCPV4_MSG_TYPE_OFFER|macro|DHCPV4_MSG_TYPE_OFFER
-DECL|DHCPV4_MSG_TYPE_RELEASE|macro|DHCPV4_MSG_TYPE_RELEASE
-DECL|DHCPV4_MSG_TYPE_REQUEST|macro|DHCPV4_MSG_TYPE_REQUEST
+DECL|DHCPV4_MSG_TYPE_ACK|enumerator|DHCPV4_MSG_TYPE_ACK = 5,
+DECL|DHCPV4_MSG_TYPE_DECLINE|enumerator|DHCPV4_MSG_TYPE_DECLINE = 4,
+DECL|DHCPV4_MSG_TYPE_DISCOVER|enumerator|DHCPV4_MSG_TYPE_DISCOVER = 1,
+DECL|DHCPV4_MSG_TYPE_INFORM|enumerator|DHCPV4_MSG_TYPE_INFORM = 8,
+DECL|DHCPV4_MSG_TYPE_NAK|enumerator|DHCPV4_MSG_TYPE_NAK = 6,
+DECL|DHCPV4_MSG_TYPE_OFFER|enumerator|DHCPV4_MSG_TYPE_OFFER = 2,
+DECL|DHCPV4_MSG_TYPE_RELEASE|enumerator|DHCPV4_MSG_TYPE_RELEASE = 7,
+DECL|DHCPV4_MSG_TYPE_REQUEST|enumerator|DHCPV4_MSG_TYPE_REQUEST = 3,
 DECL|DHCPV4_MSG_UNICAST|macro|DHCPV4_MSG_UNICAST
 DECL|DHCPV4_OPTIONS_DNS_SERVER|macro|DHCPV4_OPTIONS_DNS_SERVER
 DECL|DHCPV4_OPTIONS_END|macro|DHCPV4_OPTIONS_END
@@ -45,22 +45,23 @@ DECL|add_sname|function|static inline bool add_sname(struct net_buf *buf)
 DECL|chaddr|member|uint8_t chaddr[16]; /* Client hardware address */
 DECL|ciaddr|member|uint8_t ciaddr[4]; /* Client IP Address */
 DECL|dhcp_msg|struct|struct dhcp_msg {
+DECL|dhcpv4_msg_type|enum|enum dhcpv4_msg_type {
 DECL|dhcpv4_t1_timeout|function|static void dhcpv4_t1_timeout(struct k_work *work)
 DECL|dhcpv4_timeout|function|static void dhcpv4_timeout(struct k_work *work)
 DECL|flags|member|uint16_t flags; /* Broadcast or Unicast */
 DECL|get_dhcpv4_renewal_time|function|static inline uint32_t get_dhcpv4_renewal_time(struct net_if *iface)
 DECL|giaddr|member|uint8_t giaddr[4]; /* Relat agent IP address */
-DECL|handle_dhcpv4_reply|function|static inline void handle_dhcpv4_reply(struct net_if *iface, uint8_t msg_type)
+DECL|handle_dhcpv4_reply|function|static inline void handle_dhcpv4_reply(struct net_if *iface, enum dhcpv4_msg_type msg_type)
 DECL|hlen|member|uint8_t hlen; /* Hardware Address length */
 DECL|hops|member|uint8_t hops; /* used by relay agents when booting via relay
 DECL|htype|member|uint8_t htype; /* Hardware Address Type */
 DECL|magic_cookie|variable|magic_cookie
 DECL|net_dhcpv4_input|function|static enum net_verdict net_dhcpv4_input(struct net_conn *conn, struct net_buf *buf, void *user_data)
-DECL|net_dhcpv4_msg_type_name|function|net_dhcpv4_msg_type_name(uint8_t msg_type)
+DECL|net_dhcpv4_msg_type_name|function|net_dhcpv4_msg_type_name(enum dhcpv4_msg_type msg_type)
 DECL|net_dhcpv4_start|function|void net_dhcpv4_start(struct net_if *iface)
 DECL|net_dhcpv4_state_name|function|net_dhcpv4_state_name(enum net_dhcpv4_state state)
 DECL|op|member|uint8_t op; /* Message type, 1:BOOTREQUEST, 2:BOOTREPLY */
-DECL|parse_options|function|static enum net_verdict parse_options(struct net_if *iface, struct net_buf *buf, uint16_t offset, uint8_t *msg_type)
+DECL|parse_options|function|static enum net_verdict parse_options(struct net_if *iface, struct net_buf *buf, uint16_t offset, enum dhcpv4_msg_type *msg_type)
 DECL|prepare_message|function|static struct net_buf *prepare_message(struct net_if *iface, uint8_t type)
 DECL|secs|member|uint16_t secs; /* Seconds elapsed since client began address
 DECL|send_discover|function|static void send_discover(struct net_if *iface)
