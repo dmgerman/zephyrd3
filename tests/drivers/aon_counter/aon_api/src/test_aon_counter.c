8310af2cb500f66f43da2682a1d7fed49b103ca4
Qiu Peiyang <peiyangx.qiu@intel.com>
2017-02-09 09:00:18 +0800

    tests: add zephyr counter and timer api test
    The commit verifies below aon counter and timer apis:
    	counter_start()
    	counter_read()
    	counter_set_alarm()
    	counter_get_pending_int()
    
    Change-Id: Iaac9a224372ee1c1dd12a223ca222f4485957575
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>
    
    Former-commit-id: 45c579a27192a4de2c19d41dc1368a00dfc2d9a0

---
+++ tests/drivers/aon_counter/aon_api/src/test_aon_counter.c
@@ -0,0 +1,2 @@
+DECL|test_aon_counter|function|void test_aon_counter(void)
+DECL|test_counter|function|static int test_counter(void)
---
+++ tests/drivers/aon_counter/aon_api/src/test_aon_periodic_timer.c
@@ -0,0 +1,6 @@
+DECL|ALARM_CNT|macro|ALARM_CNT
+DECL|SLEEP_TIME|macro|SLEEP_TIME
+DECL|actual_alarm_cnt|variable|actual_alarm_cnt
+DECL|aon_timer_callback|function|static void aon_timer_callback(struct device *dev, void *user_data)
+DECL|test_aon_periodic_timer|function|void test_aon_periodic_timer(void)
+DECL|test_timer|function|static int test_timer(void)
