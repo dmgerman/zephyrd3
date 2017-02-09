0307a9d7e54513e3090e5615feb448b4a818753e
Andrew Boie <andrew.p.boie@intel.com>
2017-02-09 13:58:45 -0800

    tests: add test for gen_isr_table
    This test is intended to verify that the SW ISR and vector tables
    have been populated correctly.
    
    Change-Id: Ic7f50c02dc0807d7ddefa710da67f818ff707ad6
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 122467e9ee47a7df4b1ad3a8ad974587bb2a09b4

---
+++ tests/kernel/gen_isr_table/src/main.c
@@ -0,0 +1,16 @@
+DECL|HAS_DIRECT_IRQS|macro|HAS_DIRECT_IRQS
+DECL|IRQ_LINE|macro|IRQ_LINE
+DECL|ISR1_OFFSET|macro|ISR1_OFFSET
+DECL|ISR2_OFFSET|macro|ISR2_OFFSET
+DECL|ISR3_ARG|macro|ISR3_ARG
+DECL|ISR3_OFFSET|macro|ISR3_OFFSET
+DECL|ISR4_ARG|macro|ISR4_ARG
+DECL|ISR4_OFFSET|macro|ISR4_OFFSET
+DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(isr1)
+DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(isr2)
+DECL|TABLE_INDEX|macro|TABLE_INDEX
+DECL|check_sw_isr|function|static int check_sw_isr(void *isr, uint32_t arg, int offset)
+DECL|check_vector|function|static int check_vector(void *isr, int offset)
+DECL|isr3|function|void isr3(void *param)
+DECL|isr4|function|void isr4(void *param)
+DECL|main|function|void main(void)
