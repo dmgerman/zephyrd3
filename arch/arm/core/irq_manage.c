51378b39481278eb5fe44590ca3f868c5ffbddbb
Andrew Boie <andrew.p.boie@intel.com>
2017-02-09 12:40:08 -0800

    arm: enable direct interrupts feature
    Issue: ZEP-1038
    Change-Id: I5417e516cc994e2bbe6bb987d9ed95e912941aa0
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 27523579226b7dbff68d12ca28abce63d52e77d2

--- arch/arm/core/irq_manage.c
+++ arch/arm/core/irq_manage.c
@@ -4,5 +4,7 @@ DECL|REG_FROM_IRQ|macro|REG_FROM_IRQ
 DECL|_arch_irq_disable|function|void _arch_irq_disable(unsigned int irq)
 DECL|_arch_irq_enable|function|void _arch_irq_enable(unsigned int irq)
 DECL|_arch_irq_is_enabled|function|int _arch_irq_is_enabled(unsigned int irq)
+DECL|_arch_isr_direct_header|function|void _arch_isr_direct_header(void)
+DECL|_arch_isr_direct_pm|function|void _arch_isr_direct_pm(void)
 DECL|_irq_priority_set|function|void _irq_priority_set(unsigned int irq, unsigned int prio, uint32_t flags)
 DECL|_irq_spurious|function|void _irq_spurious(void *unused)
