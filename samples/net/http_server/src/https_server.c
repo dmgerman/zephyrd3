9a2ed63e752651a95c46079860ca19d315e0e55f
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2017-02-10 11:29:58 -0800

    samples/net/http: Add HTTP over TLS sample application
    Add the HTTPS (HTTP over TLS) server sample application on top
    of the current HTTP Parser Library and mbedTLS. This code uses
    TLS to stablish a secure connection and HTTP parser to identify
    the request and the proper response.
    
    Jira: ZEP-799
    
    Change-Id: Ifbbcd0347bec47d12158440e50a82dc2966334d3
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: de60e4c8cd4bf7ee27e720b874502626c10ea23e

---
+++ samples/net/http_server/src/https_server.c
@@ -0,0 +1,22 @@
+DECL|DEBUG_THRESHOLD|macro|DEBUG_THRESHOLD
+DECL|HTTP_NOT_FOUND|macro|HTTP_NOT_FOUND
+DECL|HTTP_RESPONSE|macro|HTTP_RESPONSE
+DECL|MBEDTLS_EXIT_FAILURE|macro|MBEDTLS_EXIT_FAILURE
+DECL|MBEDTLS_EXIT_SUCCESS|macro|MBEDTLS_EXIT_SUCCESS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|entropy_source|function|static int entropy_source(void *data, unsigned char *output, size_t len, size_t *olen)
+DECL|heap|variable|heap
+DECL|https_server_start|function|void https_server_start(void)
+DECL|https_server|function|void https_server(void)
+DECL|init_app|function|static inline int init_app(void)
+DECL|mbedtls_time_t|macro|mbedtls_time_t
+DECL|my_debug|function|static void my_debug(void *ctx, int level, const char *file, int line, const char *str)
+DECL|on_url|function|int on_url(struct http_parser *parser, const char *at, size_t length)
+DECL|parsed_url|struct|struct parsed_url {
+DECL|payload|variable|payload
+DECL|pers|variable|pers
+DECL|server_addr|variable|server_addr
+DECL|server_addr|variable|server_addr
+DECL|stack|variable|stack
+DECL|url_len|member|uint16_t url_len;
+DECL|url|member|const char *url;
