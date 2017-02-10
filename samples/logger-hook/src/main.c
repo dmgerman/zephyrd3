cf0e2ab77952789eb48e8c44b9621f054e5f66cc
Jithu Joseph <jithu.joseph@intel.com>
2017-02-10 13:38:02 -0800

    misc: fix more variable type mismatches
    These were reported by ISSM compiler.
    
    Jira: ZEP-1179
    
    Change-Id: Ic625749309773611c0c6ba2905e9420e98947dae
    Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
    
    Former-commit-id: 720400372b561f3bf5cfa2de8fbdc473760a0876

--- samples/logger-hook/src/main.c
+++ samples/logger-hook/src/main.c
@@ -4,7 +4,7 @@ DECL|log_cbuf_put|function|void log_cbuf_put(const char *format, ...)
 DECL|log_cbuffer|struct|struct log_cbuffer {
 DECL|log_cbuffer|variable|log_cbuffer
 DECL|logger_buffer|variable|logger_buffer
-DECL|logger_put|function|int logger_put(struct log_cbuffer *logger, uint8_t *data, uint32_t data_size)
+DECL|logger_put|function|int logger_put(struct log_cbuffer *logger, char *data, uint32_t data_size)
 DECL|main|function|void main(void)
 DECL|ring_buf_print|function|static inline void ring_buf_print(struct ring_buf *buf)
 DECL|ring_buffer|member|struct ring_buf ring_buffer;
