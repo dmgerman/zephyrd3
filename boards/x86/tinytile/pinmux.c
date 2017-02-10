16fdb55c90b7b427ff3554dc248ab767241d2984
Anas Nashif <anas.nashif@intel.com>
2017-02-10 08:48:38 +0530

    boards: add tinytile board
    Change-Id: I9661c21c25761e29b695f78236df4e44192f367f
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: abbd2129ed3d16a049af6104ac14266f5d463311

---
+++ boards/x86/tinytile/pinmux.c
@@ -0,0 +1,4 @@
+DECL|PINMUX_MAX_REGISTERS|macro|PINMUX_MAX_REGISTERS
+DECL|_pinmux_defaults|function|static void _pinmux_defaults(uint32_t base)
+DECL|_pinmux_pullups|function|static inline void _pinmux_pullups(uint32_t base_address)
+DECL|pinmux_initialize|function|static int pinmux_initialize(struct device *port)
