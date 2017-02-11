fe1637de3203e251b06051019ed109369ec59206
Anas Nashif <anas.nashif@intel.com>
2017-02-10 22:52:57 -0800

    tests: pipe: remove unsupported tests
    Remove tests that assert due to invocation from ISR which is not supported.
    
    Change-Id: Idd2360847a467af6afdd9fbed8f87a620d9ed2f7
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: f16f6ec2df95c2a783d8b282c5313943ca629eb1

--- tests/kernel/pipe/test_pipe_api/src/test_pipe_contexts.c
+++ tests/kernel/pipe/test_pipe_api/src/test_pipe_contexts.c
@@ -4,13 +4,9 @@ DECL|PIPE_LEN|macro|PIPE_LEN
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|end_sema|variable|end_sema
 DECL|pipe|variable|pipe
-DECL|tIsr_entry_get|function|static void tIsr_entry_get(void *p)
-DECL|tIsr_entry_put|function|static void tIsr_entry_put(void *p)
 DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *p3)
-DECL|test_pipe_thread2isr|function|void test_pipe_thread2isr(void)
 DECL|test_pipe_thread2thread|function|void test_pipe_thread2thread(void)
 DECL|tpipe_get|function|static void tpipe_get(struct k_pipe *ppipe)
 DECL|tpipe_put|function|static void tpipe_put(struct k_pipe *ppipe)
-DECL|tpipe_thread_isr|function|static void tpipe_thread_isr(struct k_pipe *ppipe)
 DECL|tpipe_thread_thread|function|static void tpipe_thread_thread(struct k_pipe *ppipe)
 DECL|tstack|variable|tstack
