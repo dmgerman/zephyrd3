3cadef9b1b6a5c2c82c020815cd7c45ed9d9d17f
Wojciech Bober <wojciech.bober@nordicsemi.no>
2017-02-13 16:31:53 +0100

    drivers/timer: Rework the nRF RTC driver.
    This is a reworked version of the previous RTC driver. The main
    changed is related to the handling _timer_idle_exit() on non-RTC
    wake-ups. The previous version didn't announce the elapsed time
    to the kernel in _timer_idle_exit(). Additionally, the driver now
    makes sure never to announce more idle ticks than the kernel asked
    for, since the kernel does not handle negative deltas in its timeout
    queues.
    
    Change-Id: I312a357a7ce8f0c22adf5153731064b92870e47e
    Signed-off-by: Wojciech Bober <wojciech.bober@nordicsemi.no>
    Signed-off-by: Øyvind Hovdsveen <oyvind.hovdsveen@nordicsemi.no>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 923560a95947e0fb4437e7ec4c4e8bcde921784a

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -1,11 +1,18 @@
-DECL|RTC_TICKS|macro|RTC_TICKS
+DECL|RTC_CC_EVENT|macro|RTC_CC_EVENT
+DECL|RTC_CC_VALUE|macro|RTC_CC_VALUE
+DECL|RTC_COUNTER|macro|RTC_COUNTER
+DECL|RTC_HALF|macro|RTC_HALF
+DECL|RTC_MASK|macro|RTC_MASK
+DECL|RTC_MIN_DELTA|macro|RTC_MIN_DELTA
+DECL|RTC_TICKS_PER_SYS_TICK|macro|RTC_TICKS_PER_SYS_TICK
+DECL|SYS_CLOCK_RTC|macro|SYS_CLOCK_RTC
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
 DECL|_timer_cycle_get_32|function|uint32_t _timer_cycle_get_32(void)
-DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t ticks)
+DECL|_timer_idle_enter|function|void _timer_idle_enter(int32_t sys_ticks)
 DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
-DECL|isr_ack|variable|isr_ack
-DECL|isr_req|variable|isr_req
+DECL|allowed_idle_sys_ticks|variable|allowed_idle_sys_ticks
 DECL|rtc1_nrf5_isr|function|static void rtc1_nrf5_isr(void *arg)
-DECL|rtc_clock_tick_count|variable|rtc_clock_tick_count
-DECL|rtc_compare_set|function|static uint32_t rtc_compare_set(uint32_t rtc_ticks)
+DECL|rtc_announce_set_next|function|static void rtc_announce_set_next(void)
+DECL|rtc_compare_set|function|static void rtc_compare_set(uint32_t rtc_ticks)
+DECL|rtc_past|variable|rtc_past
 DECL|sys_clock_disable|function|void sys_clock_disable(void)
