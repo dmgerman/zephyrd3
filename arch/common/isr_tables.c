ad95699576628d22932714d944411d31d61ad342
Andrew Boie <andrew.p.boie@intel.com>
2017-02-13 09:56:25 -0800

    gen_isr_tables: apply offset to irq parameter
    The interrupts would be placed at incorrect offsets on systems where
    some interrupt vectors are reserved for exceptions, such as ARC.
    
    Change-Id: I5b1f00eb9e8aecb84ae66e3d0461a734ffb5fbe6
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 3944d8313ed77f1d5d83df33aaaa5c0374a799b1

--- arch/common/isr_tables.c
+++ arch/common/isr_tables.c
@@ -1,7 +1,10 @@
 DECL|ISR_WRAPPER|macro|ISR_WRAPPER
 DECL|ISR_WRAPPER|macro|ISR_WRAPPER
-DECL|_irq_handler_ptr|variable|_irq_handler_ptr
-DECL|_irq_spurious_ptr|variable|_irq_spurious_ptr
-DECL|_irq_table_size|variable|_irq_table_size
+DECL|_iheader|variable|_iheader
 DECL|_irq_vector_table|variable|_irq_vector_table
 DECL|_sw_isr_table|variable|_sw_isr_table
+DECL|handler_ptr|member|void *handler_ptr;
+DECL|int_list_header|struct|struct int_list_header {
+DECL|offset|member|uint32_t offset;
+DECL|spurious_ptr|member|void *spurious_ptr;
+DECL|table_size|member|uint32_t table_size;
