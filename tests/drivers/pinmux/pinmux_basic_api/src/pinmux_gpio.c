91ee3e1a8a0efb52eee9a78bbc2610d3be881964
Qiu Peiyang <peiyangx.qiu@intel.com>
2017-02-14 14:07:39 +0800

    tests: add zephyr pinmux driver api test case
    This commit verifies the following pinmux driver apis:
    	pinmux_pin_set()
    	pinmux_pin_get()
    
    Change-Id: Iedf1b4e918b518b2205e9059b3b08f41cd243d37
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>
    
    Former-commit-id: 2d033483de467ff276e48dfa4ce77cbd649203d6

---
+++ tests/drivers/pinmux/pinmux_basic_api/src/pinmux_gpio.c
@@ -0,0 +1,26 @@
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|GPIO_IN|macro|GPIO_IN
+DECL|GPIO_IN|macro|GPIO_IN
+DECL|GPIO_IN|macro|GPIO_IN
+DECL|GPIO_IN|macro|GPIO_IN
+DECL|GPIO_IN|macro|GPIO_IN
+DECL|GPIO_OUT|macro|GPIO_OUT
+DECL|GPIO_OUT|macro|GPIO_OUT
+DECL|GPIO_OUT|macro|GPIO_OUT
+DECL|GPIO_OUT|macro|GPIO_OUT
+DECL|GPIO_OUT|macro|GPIO_OUT
+DECL|MAX_INT_CNT|macro|MAX_INT_CNT
+DECL|PINMUX_NAME|macro|PINMUX_NAME
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|callback|function|static void callback(struct device *dev, struct gpio_callback *gpio_cb, uint32_t pins)
+DECL|cb_triggered|variable|cb_triggered
+DECL|test_gpio|function|static int test_gpio(uint32_t pin, uint32_t func)
+DECL|test_pinmux_gpio|function|void test_pinmux_gpio(void)
