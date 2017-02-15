fb215be44058bef76d9b9374abef9dd839d2fafa
Andrew Boie <andrew.p.boie@intel.com>
2017-02-15 14:59:38 -0800

    riscv_machine_timer: fix k_cycle_get_32()
    There are race conditions trying to coordinate the value between
    the accumulated_cycle_count (updated at interrupt time) and
    trying to compute the delta from the last interrupt using the
    mtime registers. An unlucky call could result in the timestamp
    appearing to move backwards in time.
    
    the 'mtime' register isn't reset at every interrupt. Since we just
    want a cycle counter, report its raw value.
    
    Issue: ZEP-1546
    Change-Id: I9f404b33214d6502fea47374fcf0ecbf84ef8136
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 97d76cad669a856b4a0b986eb58cc62fff749a90

--- drivers/timer/riscv_machine_timer.c
+++ drivers/timer/riscv_machine_timer.c
@@ -1,7 +1,5 @@
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
-DECL|accumulated_cycle_count|variable|accumulated_cycle_count
 DECL|k_cycle_get_32|function|uint32_t k_cycle_get_32(void)
-DECL|last_rtc_value|variable|last_rtc_value
 DECL|mtimecmp|variable|mtimecmp
 DECL|mtime|variable|mtime
 DECL|riscv_machine_rearm_timer|function|static ALWAYS_INLINE void riscv_machine_rearm_timer(void)
