e3dc4c07c8c7d3c885b946a5b2c7a4130eacd315
Jean-Paul Etienne <fractalclone@gmail.com>
2017-02-15 21:12:20 +0100

    tests: gen_isr_table: account for riscv32 architecture
    Account for riscv32 SOCs supporting the riscv privileged architecture.
    
    Change-Id: I8c26a2bcc2baded5db252896abe6e1b5ab052113
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 9acda0f69509d8cdf6c7b818135ad9a9a8597fea

--- tests/kernel/gen_isr_table/src/main.c
+++ tests/kernel/gen_isr_table/src/main.c
@@ -1,15 +1,21 @@
 DECL|HAS_DIRECT_IRQS|macro|HAS_DIRECT_IRQS
 DECL|IRQ_LINE|macro|IRQ_LINE
+DECL|IRQ_LINE|macro|IRQ_LINE
 DECL|ISR1_OFFSET|macro|ISR1_OFFSET
 DECL|ISR2_OFFSET|macro|ISR2_OFFSET
 DECL|ISR3_ARG|macro|ISR3_ARG
 DECL|ISR3_OFFSET|macro|ISR3_OFFSET
+DECL|ISR3_OFFSET|macro|ISR3_OFFSET
 DECL|ISR4_ARG|macro|ISR4_ARG
+DECL|ISR4_OFFSET|macro|ISR4_OFFSET
 DECL|ISR4_OFFSET|macro|ISR4_OFFSET
 DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(isr1)
 DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(isr2)
 DECL|NO_TRIGGER_FROM_SW|macro|NO_TRIGGER_FROM_SW
 DECL|TABLE_INDEX|macro|TABLE_INDEX
+DECL|TABLE_INDEX|macro|TABLE_INDEX
+DECL|TRIG_CHECK_SIZE|macro|TRIG_CHECK_SIZE
+DECL|TRIG_CHECK_SIZE|macro|TRIG_CHECK_SIZE
 DECL|check_sw_isr|function|static int check_sw_isr(void *isr, uint32_t arg, int offset)
 DECL|check_vector|function|static int check_vector(void *isr, int offset)
 DECL|isr3|function|void isr3(void *param)
@@ -17,4 +23,5 @@ DECL|isr4|function|void isr4(void *param)
 DECL|main|function|void main(void)
 DECL|test_irq|function|int test_irq(int offset)
 DECL|trigger_check|variable|trigger_check
+DECL|trigger_irq|function|void trigger_irq(int irq)
 DECL|trigger_irq|function|void trigger_irq(int irq)
