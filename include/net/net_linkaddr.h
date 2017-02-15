ea344c836464d1f5698327886f5bb51d83c81c05
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-02-15 12:36:00 +0200

    net: Add network link technology type to linkaddr
    In order to know what kind of address the L2 link address is,
    add a type of the address into struct net_linkaddr.
    
    Change-Id: Icd4cb0374219583689cf9ee204c0840cad8559e9
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 24b66f4670d92b4e372ccff7546bf5aa0e66c8ef

--- include/net/net_linkaddr.h
+++ include/net/net_linkaddr.h
@@ -1,11 +1,19 @@
 DECL|NET_LINK_ADDR_MAX_LENGTH|macro|NET_LINK_ADDR_MAX_LENGTH
 DECL|NET_LINK_ADDR_MAX_LENGTH|macro|NET_LINK_ADDR_MAX_LENGTH
+DECL|NET_LINK_BLUETOOTH|enumerator|NET_LINK_BLUETOOTH,
+DECL|NET_LINK_DUMMY|enumerator|NET_LINK_DUMMY,
+DECL|NET_LINK_ETHERNET|enumerator|NET_LINK_ETHERNET,
+DECL|NET_LINK_IEEE802154|enumerator|NET_LINK_IEEE802154,
+DECL|NET_LINK_UNKNOWN|enumerator|NET_LINK_UNKNOWN = 0,
 DECL|__NET_LINKADDR_H__|macro|__NET_LINKADDR_H__
 DECL|addr|member|uint8_t *addr;
 DECL|addr|member|uint8_t addr[NET_LINK_ADDR_MAX_LENGTH];
 DECL|len|member|uint8_t len;
 DECL|len|member|uint8_t len;
+DECL|net_link_type|enum|enum net_link_type {
 DECL|net_linkaddr_cmp|function|static inline bool net_linkaddr_cmp(struct net_linkaddr *lladdr1, struct net_linkaddr *lladdr2)
 DECL|net_linkaddr_set|function|static inline int net_linkaddr_set(struct net_linkaddr_storage *lladdr_store, uint8_t *new_addr, uint8_t new_len)
 DECL|net_linkaddr_storage|struct|struct net_linkaddr_storage {
 DECL|net_linkaddr|struct|struct net_linkaddr {
+DECL|type|member|uint8_t type;
+DECL|type|member|uint8_t type;
