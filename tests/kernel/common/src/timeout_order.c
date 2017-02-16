d21ccb247b1395e87bfced205e403132690b3a4c
Benjamin Walsh <walsh.benj@gmail.com>
2017-02-15 22:26:28 -0500

    tests/kernel/common: add test to verify same tick timeout expiry order
    Timeouts, when expiring on the same tick, should be handled in the same
    order they were queued.
    
    Change-Id: I23a8e971a47ca056b32b8b48fe179d481bae27c0
    Signed-off-by: Benjamin Walsh <walsh.benj@gmail.com>
    
    Former-commit-id: d2654d31434b32008100b2fb81ba95b20fd85f27

---
+++ tests/kernel/common/src/timeout_order.c
@@ -0,0 +1,8 @@
+DECL|NUM_TIMEOUTS|macro|NUM_TIMEOUTS
+DECL|cur|variable|cur
+DECL|results|variable|results
+DECL|sem|variable|sem
+DECL|stacks|variable|stacks
+DECL|thread|function|static void thread(void *p1, void *p2, void *p3)
+DECL|timeout_order_test|function|void timeout_order_test(void)
+DECL|timer|variable|timer
