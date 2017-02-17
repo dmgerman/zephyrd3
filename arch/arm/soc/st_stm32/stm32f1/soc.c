e80a0be7679c7d32fe138d4084999f7bc77480ac
Erwan Gouriou <erwan.gouriou@linaro.org>
2017-02-17 17:07:09 +0100

    arch: stm32: provide tick to Cube HAL
    Cube HAL implements timeout based on 1ms tick.
    This commit allows Cube HAL to get Zephyr system clock.
    
    Change-Id: I9a59edcf6fa8e0ebfd5040348db537dadd9fcdfa
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    
    Former-commit-id: d39eedbad1073024257059eae47e29a862bf4dcf

--- arch/arm/soc/st_stm32/stm32f1/soc.c
+++ arch/arm/soc/st_stm32/stm32f1/soc.c
@@ -1,1 +1,2 @@
+DECL|HAL_GetTick|function|uint32_t HAL_GetTick(void)
 DECL|stm32f1_init|function|static int stm32f1_init(struct device *arg)
--- arch/arm/soc/st_stm32/stm32l4/soc.c
+++ arch/arm/soc/st_stm32/stm32l4/soc.c
@@ -1,1 +1,2 @@
+DECL|HAL_GetTick|function|uint32_t HAL_GetTick(void)
 DECL|stm32l4_init|function|static int stm32l4_init(struct device *arg)
