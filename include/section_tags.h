f1e80d286e6236ea49ac00b237899f43d071c287
Maureen Helm <maureen.helm@nxp.com>
2017-02-17 10:36:22 -0600

    section_tags.h: Remove unused security_frdm_k64f_section
    This section was renamed to kinetis_flash_config, but it slipped back in
    with the previous section_tags.h cleanup.
    
    Change-Id: I4a2558aa70379a75c604f3617483680a35ed3d90
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: b018257be29ba52d318c98fe72664e805c6aa816

--- include/section_tags.h
+++ include/section_tags.h
@@ -2,6 +2,5 @@ DECL|__irq_vector_table|macro|__irq_vector_table
 DECL|__kinetis_flash_config_section|macro|__kinetis_flash_config_section
 DECL|__noinit|macro|__noinit
 DECL|__scp_section|macro|__scp_section
-DECL|__security_frdm_k64f_section|macro|__security_frdm_k64f_section
 DECL|__sw_isr_table|macro|__sw_isr_table
 DECL|_section_tags__h_|macro|_section_tags__h_
