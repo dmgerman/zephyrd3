a599b47df278f54e735081310d7502dad8102360
Marcus Shawcroft <marcus.shawcroft@arm.com>
2017-02-18 16:59:55 +0000

    eth/eth_mcux: Provide start and stop operations on the PHY driver.
    Refactor the PHY state machine and add support for explicit start and
    stop.
    
    The stop implementation remains partial, the state machine will enter
    a disabled state but will not actual attempt to power down the PHY.
    This is deliberate, while implementing this it has become apparent
    that issuing a PHY power down command is an effective way of bricking
    frdm-k64f boards, hence explicit power down deliberately disabled
    until the issue is properly understood.
    
    Change-Id: I846a51b0ac48feed35d260cf20b50f4f1ac59298
    Signed-off-by: Marcus Shawcroft <marcus.shawcroft@arm.com>
    
    Former-commit-id: e675352e10db2c36fc7335fa2ff7a6d72b74557f

--- drivers/ethernet/eth_mcux.c
+++ drivers/ethernet/eth_mcux.c
@@ -3,6 +3,7 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|api_funcs_0|variable|api_funcs_0
 DECL|delayed_phy_work|member|struct k_delayed_work delayed_phy_work;
+DECL|enabled|member|bool enabled;
 DECL|enet_handle|member|enet_handle_t enet_handle;
 DECL|eth_0_config_func|function|static void eth_0_config_func(void)
 DECL|eth_0_context|variable|eth_0_context
@@ -13,15 +14,19 @@ DECL|eth_context|struct|struct eth_context {
 DECL|eth_mcux_decode_duplex_and_speed|function|static void eth_mcux_decode_duplex_and_speed(uint32_t status, phy_duplex_t *p_phy_duplex, phy_speed_t *p_phy_speed)
 DECL|eth_mcux_delayed_phy_work|function|static void eth_mcux_delayed_phy_work(struct k_work *item)
 DECL|eth_mcux_error_isr|function|static void eth_mcux_error_isr(void *p)
+DECL|eth_mcux_phy_enter_reset|function|static void eth_mcux_phy_enter_reset(struct eth_context *context)
 DECL|eth_mcux_phy_event|function|static void eth_mcux_phy_event(struct eth_context *context)
+DECL|eth_mcux_phy_start|function|static void eth_mcux_phy_start(struct eth_context *context)
 DECL|eth_mcux_phy_state_autoneg|enumerator|eth_mcux_phy_state_autoneg,
+DECL|eth_mcux_phy_state_closing|enumerator|eth_mcux_phy_state_closing
 DECL|eth_mcux_phy_state_initial|enumerator|eth_mcux_phy_state_initial,
 DECL|eth_mcux_phy_state_read_duplex|enumerator|eth_mcux_phy_state_read_duplex,
 DECL|eth_mcux_phy_state_read_status|enumerator|eth_mcux_phy_state_read_status,
 DECL|eth_mcux_phy_state_reset|enumerator|eth_mcux_phy_state_reset,
 DECL|eth_mcux_phy_state_restart|enumerator|eth_mcux_phy_state_restart,
-DECL|eth_mcux_phy_state_wait|enumerator|eth_mcux_phy_state_wait
+DECL|eth_mcux_phy_state_wait|enumerator|eth_mcux_phy_state_wait,
 DECL|eth_mcux_phy_state|enum|enum eth_mcux_phy_state {
+DECL|eth_mcux_phy_stop|function|void eth_mcux_phy_stop(struct eth_context *context)
 DECL|eth_mcux_phy_work|function|static void eth_mcux_phy_work(struct k_work *item)
 DECL|eth_mcux_rx_isr|function|static void eth_mcux_rx_isr(void *p)
 DECL|eth_mcux_tx_isr|function|static void eth_mcux_tx_isr(void *p)
