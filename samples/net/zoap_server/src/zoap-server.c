4c61cc6359ce48167e2149628c5b0a12431e2e0d
Vinicius Costa Gomes <vinicius.gomes@intel.com>
2017-02-21 11:11:16 -0800

    samples/zoap_server: Add more validation tests for block-wise
    Add the resource for the TD_COAP_BLOCK_04 ETSI testcase.
    
    Change-Id: Ied901db34ce79d3e1f7f8c7fd55bc398b1f88640
    Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    
    Former-commit-id: 8ce3d4ec827d822cc28e4c59ee34bda066bea5e9

--- samples/net/zoap_server/src/zoap-server.c
+++ samples/net/zoap_server/src/zoap-server.c
@@ -1,5 +1,5 @@
 DECL|ALL_NODES_LOCAL_COAP_MCAST|macro|ALL_NODES_LOCAL_COAP_MCAST
-DECL|LARGE_TRANSFER_SIZE|macro|LARGE_TRANSFER_SIZE
+DECL|BLOCK_WISE_TRANSFER_SIZE_GET|macro|BLOCK_WISE_TRANSFER_SIZE_GET
 DECL|MY_COAP_PORT|macro|MY_COAP_PORT
 DECL|MY_IP6ADDR|macro|MY_IP6ADDR
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
@@ -8,6 +8,8 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|context|variable|context
 DECL|join_coap_multicast_group|function|static bool join_coap_multicast_group(void)
+DECL|large_create_path|variable|large_create_path
+DECL|large_create_post|function|static int large_create_post(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
 DECL|large_get|function|static int large_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
 DECL|large_path|variable|large_path
 DECL|large_update_path|variable|large_update_path
