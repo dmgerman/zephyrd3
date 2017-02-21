35eb873f82ed48b5d09de5ce618f310db2cbbac1
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2017-02-21 05:07:29 +0100

    Bluetooth: Controller: Use direct ISR for Radio IRQ only
    The new hard real-time (direct) interrupt support is used
    only for Radio IRQ. Only Radio ISR has hard real-time
    requirement of completing its execution inside the tIFS of
    150us.
    
    This commit reverts back RTC0 and SWI4 ISR to using the
    normal IRQ_CONNECT.
    
    Observed on nRF51, Radio ISR (open text, notifications):
    Latencies: min. of 5us and max. of 10us.
    CPU utilized: min. of 44us and max. of 77us.
    
    Observed on nRF51, Radio ISR (LESC, fast encrypted,
    notifications):
    Latencies: min. of 5us and max. of 10us.
    CPU utilized: min. of 39us and max. of 112us.
    
    Jira: ZEP-1038
    
    Change-id: Id3d09df7bdbdfea090f21f6f58aaded8c5f1e10d
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 16ef73423edaa3b85ba95feb5e352334fe1863eb

--- subsys/bluetooth/controller/hci/hci_driver.c
+++ subsys/bluetooth/controller/hci/hci_driver.c
@@ -4,8 +4,6 @@ DECL|HCI_CMD|macro|HCI_CMD
 DECL|HCI_EVT|macro|HCI_EVT
 DECL|HCI_SCO|macro|HCI_SCO
 DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(radio_nrf5_isr)
-DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(rtc0_nrf5_isr)
-DECL|ISR_DIRECT_DECLARE|function|ISR_DIRECT_DECLARE(swi4_nrf5_isr)
 DECL|_hci_driver_init|function|static int _hci_driver_init(struct device *unused)
 DECL|bt_rand|function|int bt_rand(void *buf, size_t len)
 DECL|cmd_handle|function|static int cmd_handle(struct net_buf *buf)
@@ -22,3 +20,5 @@ DECL|radio_active_callback|function|void radio_active_callback(uint8_t active)
 DECL|radio_event_callback|function|void radio_event_callback(void)
 DECL|recv_thread|function|static void recv_thread(void *p1, void *p2, void *p3)
 DECL|rng_nrf5_isr|function|static void rng_nrf5_isr(void *arg)
+DECL|rtc0_nrf5_isr|function|static void rtc0_nrf5_isr(void *arg)
+DECL|swi4_nrf5_isr|function|static void swi4_nrf5_isr(void *arg)
