65933f99ba95d72e51244837b38ca96fcb26cc56
Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
2017-02-21 14:29:10 -0800

    samples: pwm: change hard-coded device name
    Use the config name defined by the driver Kconfig in device
    binding calls as that is safer because device name can change and
    the app does not need to change.
    
    Jira: ZEP-1764
    
    Change-Id: I3c7716fd72e77c99fd29e4e90634c33f47f32e4c
    Signed-off-by: Kuo-Lang Tseng <kuo-lang.tseng@intel.com>
    
    Former-commit-id: 5873fee475439b2dd188c659fbb5a3c9071e2bde

--- samples/basic/rgb_led/src/main.c
+++ samples/basic/rgb_led/src/main.c
@@ -1,4 +1,5 @@
 DECL|PERIOD|macro|PERIOD
+DECL|PWM_DEV|macro|PWM_DEV
 DECL|STEPSIZE|macro|STEPSIZE
 DECL|main|function|void main(void)
 DECL|write_pin|function|static int write_pin(struct device *pwm_dev, uint32_t pwm_pin, uint32_t pulse_width)
