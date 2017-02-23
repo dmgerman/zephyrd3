03a9cb812c7410d45725c6c47cdb8e41406cfb6e
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-02-23 14:53:10 +0200

    net: nbuf: Remove unnecessary debugging code
    As the pool contains now more accounting information, there is
    no need to keep track of this data in nbuf.c.
    
    Change-Id: I7fd44f9feda8fd9816356556d1e788ad56e6eedd
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9fce1626d7eb14be5d4886deef21645a4ed9bff7

--- subsys/net/ip/nbuf.c
+++ subsys/net/ip/nbuf.c
@@ -23,25 +23,7 @@ DECL|adjust_insert_offset|function|static inline struct net_buf *adjust_insert_o
 DECL|adjust_offset|function|static inline struct net_buf *adjust_offset(struct net_buf *buf, uint16_t offset, uint16_t *pos)
 DECL|adjust_write_offset|function|static inline struct net_buf *adjust_write_offset(struct net_buf *buf, struct net_buf *frag, uint16_t offset, uint16_t *pos, int32_t timeout)
 DECL|check_and_create_data|function|static inline struct net_buf *check_and_create_data(struct net_buf *buf, struct net_buf *data, int32_t timeout)
-DECL|data_buffers_pool|macro|data_buffers_pool
-DECL|dec_free_bufs|function|static inline void dec_free_bufs(struct net_buf_pool *pool, struct net_buf *buf)
-DECL|dec_free_bufs|macro|dec_free_bufs
-DECL|dec_free_data_bufs|function|static inline void dec_free_data_bufs(struct net_buf *buf)
-DECL|dec_free_data_bufs|macro|dec_free_data_bufs
-DECL|dec_free_rx_bufs|function|static inline void dec_free_rx_bufs(struct net_buf *buf)
-DECL|dec_free_rx_bufs|macro|dec_free_rx_bufs
-DECL|dec_free_tx_bufs|function|static inline void dec_free_tx_bufs(struct net_buf *buf)
-DECL|dec_free_tx_bufs|macro|dec_free_tx_bufs
-DECL|free_data_bufs_func|function|static inline void free_data_bufs_func(struct net_buf *buf)
-DECL|free_rx_bufs_func|function|static inline void free_rx_bufs_func(struct net_buf *buf)
-DECL|free_tx_bufs_func|function|static inline void free_tx_bufs_func(struct net_buf *buf)
-DECL|get_frees|function|static inline int get_frees(struct net_buf_pool *pool)
-DECL|inc_free_data_bufs|function|static inline void inc_free_data_bufs(struct net_buf *buf)
-DECL|inc_free_data_bufs|macro|inc_free_data_bufs
-DECL|inc_free_rx_bufs|function|static inline void inc_free_rx_bufs(struct net_buf *buf)
-DECL|inc_free_rx_bufs|macro|inc_free_rx_bufs
-DECL|inc_free_tx_bufs|function|static inline void inc_free_tx_bufs(struct net_buf *buf)
-DECL|inc_free_tx_bufs|macro|inc_free_tx_bufs
+DECL|get_frees|function|static inline int16_t get_frees(struct net_buf_pool *pool)
 DECL|insert_data|function|static inline bool insert_data(struct net_buf *buf, struct net_buf *frag, struct net_buf *temp, uint16_t offset, uint16_t len, uint8_t *data, int32_t timeout)
 DECL|is_from_data_pool|function|static inline bool is_from_data_pool(struct net_buf *buf)
 DECL|net_nbuf_append_bytes|function|static inline bool net_nbuf_append_bytes(struct net_buf *buf, const uint8_t *value, uint16_t len, int32_t timeout)
@@ -77,9 +59,4 @@ DECL|net_nbuf_read|function|struct net_buf *net_nbuf_read(struct net_buf *buf, u
 DECL|net_nbuf_ref_debug|function|struct net_buf *net_nbuf_ref_debug(struct net_buf *buf, const char *caller, int line) #else struct net_buf *net_nbuf_ref(struct net_buf *buf) #endif /* CONFIG_NET_DEBUG_NET_BUF */
 DECL|net_nbuf_unref_debug|function|void net_nbuf_unref_debug(struct net_buf *buf, const char *caller, int line)
 DECL|net_nbuf_write|function|struct net_buf *net_nbuf_write(struct net_buf *buf, struct net_buf *frag, uint16_t offset, uint16_t *pos, uint16_t len, uint8_t *data, int32_t timeout)
-DECL|num_free_data_bufs|variable|num_free_data_bufs
-DECL|num_free_rx_bufs|variable|num_free_rx_bufs
-DECL|num_free_tx_bufs|variable|num_free_tx_bufs
 DECL|pool2str|function|static inline const char *pool2str(struct net_buf_pool *pool)
-DECL|rx_buffers_pool|macro|rx_buffers_pool
-DECL|tx_buffers_pool|macro|tx_buffers_pool
