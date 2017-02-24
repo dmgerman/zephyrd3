598390538e06853944f525ce87f1a9538b40dad7
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-02-24 14:10:08 +0200

    net: Remove link layer reserve param from IP packet creator
    The ll_reserve parameter is useless in net_ipv{4|6}_create_raw()
    function as the reserve information is already stored in buf.
    
    Change-Id: I7815a78c001e3da532478c04b3dac9b37bbc723c
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 189ec2b6adefbf4d4ea43bf54e7e40cd4f1edf99

--- subsys/net/ip/ipv4.c
+++ subsys/net/ip/ipv4.c
@@ -1,7 +1,7 @@
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|net_ipv4_broadcast_address|function|const struct in_addr *net_ipv4_broadcast_address(void)
-DECL|net_ipv4_create_raw|function|struct net_buf *net_ipv4_create_raw(struct net_buf *buf, uint16_t reserve, const struct in_addr *src, const struct in_addr *dst, struct net_if *iface,
+DECL|net_ipv4_create_raw|function|struct net_buf *net_ipv4_create_raw(struct net_buf *buf, const struct in_addr *src, const struct in_addr *dst, struct net_if *iface, uint8_t next_header)
 DECL|net_ipv4_create|function|struct net_buf *net_ipv4_create(struct net_context *context,struct net_buf *buf, const struct in_addr *src, const struct in_addr *dst)
 DECL|net_ipv4_finalize_raw|function|struct net_buf *net_ipv4_finalize_raw(struct net_buf *buf, uint8_t next_header)
 DECL|net_ipv4_finalize|function|struct net_buf *net_ipv4_finalize(struct net_context *context, struct net_buf *buf)
