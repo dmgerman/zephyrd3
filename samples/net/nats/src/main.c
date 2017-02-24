e4702204f21076d16f9262531cd4f2b533672af7
Leandro Pereira <leandro.pereira@intel.com>
2017-02-24 11:03:30 -0800

    samples: net: NATS protocol sample
    NATS is a publisher/subscriber protocol implemented on top of TCP. It
    is specified in [1], and this is a sample implementation for Zephyr
    using the new IP stack.  The API is loosely based off of [2].
    
    With this sample, it's possible to subscribe/unsubscribe to a given
    subject, and be notified of changes asynchronously.  In order to
    conserve resources, the implementation does not keep its own track of
    subscribed subjects; that must be performed by the application itself,
    so it ignore unknown/undesired subjects.
    
    TLS is not supported yet, although basic auth is.  The client will
    indicate if it supports username/password if a certain callback is set
    in the struct nats.  This callback will then be called, and the user
    must copy the username/password to the supplied user/pass buffers.
    
    Content might be also published for a given subject.
    
    The sample application lets one observe the subject "led0", and turn it
    "on", "off", or "toggle" its value.  Changing the value will, if
    supported, act on a status LED on the development board.  The new
    status will be published.
    
    Also worth noting is that most of the networking and GPIO boilerplate
    has been shamelessly copied from the IRC bot example.  (Curiously, both
    protocols are similar.)
    
    [1] http://nats.io/documentation/internals/nats-protocol/
    [2] https://github.com/nats-io/go-nats
    
    Jira: ZEP-1012
    
    Change-Id: I204adc61c4c533661eacfb8c28c1c08870debd91
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 141c0a5f1ab5c5ed72038b33901e3bc0c6961c2a

---
+++ samples/net/nats/src/main.c
@@ -0,0 +1,32 @@
+DECL|DEFAULT_PORT|macro|DEFAULT_PORT
+DECL|DNS_PORT|macro|DNS_PORT
+DECL|DNS_SLEEP_MSECS|macro|DNS_SLEEP_MSECS
+DECL|LED_GPIO_NAME|macro|LED_GPIO_NAME
+DECL|LED_GPIO_NAME|macro|LED_GPIO_NAME
+DECL|LED_PIN|macro|LED_PIN
+DECL|LED_PIN|macro|LED_PIN
+DECL|NATS_AF_INET|macro|NATS_AF_INET
+DECL|NATS_AF_INET|macro|NATS_AF_INET
+DECL|NATS_LOCAL_IP_ADDR|macro|NATS_LOCAL_IP_ADDR
+DECL|NATS_LOCAL_IP_ADDR|macro|NATS_LOCAL_IP_ADDR
+DECL|NATS_LOCAL_IP_ADDR|macro|NATS_LOCAL_IP_ADDR
+DECL|NATS_LOCAL_IP_ADDR|macro|NATS_LOCAL_IP_ADDR
+DECL|NATS_PEER_IP_ADDR|macro|NATS_PEER_IP_ADDR
+DECL|NATS_PEER_IP_ADDR|macro|NATS_PEER_IP_ADDR
+DECL|NATS_PEER_IP_ADDR|macro|NATS_PEER_IP_ADDR
+DECL|NATS_PEER_IP_ADDR|macro|NATS_PEER_IP_ADDR
+DECL|NATS_SOCKADDR_IN|macro|NATS_SOCKADDR_IN
+DECL|NATS_SOCKADDR_IN|macro|NATS_SOCKADDR_IN
+DECL|connect|function|static int connect(struct nats *nats, uint16_t port)
+DECL|fake_led|variable|fake_led
+DECL|in_addr_set|function|static int in_addr_set(sa_family_t family, const char *ip_addr, int port, struct sockaddr *_sockaddr)
+DECL|initialize_hardware|function|static void initialize_hardware(void)
+DECL|initialize_network|function|static void initialize_network(void)
+DECL|led0|variable|led0
+DECL|main|function|void main(void)
+DECL|nats_client|function|static void nats_client(void)
+DECL|on_msg_received|function|static int on_msg_received(const struct nats *nats, const struct nats_msg *msg)
+DECL|panic|function|static void panic(const char *msg)
+DECL|read_led|function|static bool read_led(void)
+DECL|stack|variable|stack
+DECL|write_led|function|static void write_led(const struct nats *nats, const struct nats_msg *msg, bool state)
