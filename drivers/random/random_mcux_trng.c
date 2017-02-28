d282a45defbd4bccd0fd1fafd8d067ea09068e22
Bogdan Davidoaia <bogdan.davidoaia@linaro.org>
2017-02-28 13:24:43 +0200

    random: add MCUX TRNG driver
    Provide a random driver wrapped around the MCUX TRNG driver.
    
    Change-Id: Icbd7ab587aa18ecbd7eae52290aaa5d8ee504cf2
    Origin: Original
    Signed-off-by: Bogdan Davidoaia <bogdan.davidoaia@linaro.org>
    
    Former-commit-id: 54739069937954d759ebc02bed32d08457c90a15

---
+++ drivers/random/random_mcux_trng.c
@@ -0,0 +1,4 @@
+DECL|random_mcux_trng_api_funcs|variable|random_mcux_trng_api_funcs
+DECL|random_mcux_trng_get_entropy|function|static int random_mcux_trng_get_entropy(struct device *dev, uint8_t *buffer,uint16_t length)
+DECL|random_mcux_trng_init|function|static int random_mcux_trng_init(struct device *dev)
+DECL|sys_rand32_get|function|uint32_t sys_rand32_get(void)
