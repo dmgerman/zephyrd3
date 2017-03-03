a302d23b479db8148a3e6ac4ff637601a03dad60
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-03-03 09:58:15 +0200

    net: icmpv6: Remove unused variable 'len' from input calls
    Variable len is not used in net_icmpv6_input() and if required
    it can be obtained from 'buf'.
    
    Change-Id: I4b0710e1cf16cff9837173ad9d6908ec54ebafae
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 476f46f61bffa2a5dbfc4261efaae23b5c43c67f

--- subsys/net/ip/icmpv6.c
+++ subsys/net/ip/icmpv6.c
@@ -4,7 +4,7 @@ DECL|echo_request_handler|variable|echo_request_handler
 DECL|handle_echo_request|function|static enum net_verdict handle_echo_request(struct net_buf *orig)
 DECL|handlers|variable|handlers
 DECL|net_icmpv6_init|function|void net_icmpv6_init(void)
-DECL|net_icmpv6_input|function|enum net_verdict net_icmpv6_input(struct net_buf *buf, uint16_t len, uint8_t type, uint8_t code)
+DECL|net_icmpv6_input|function|enum net_verdict net_icmpv6_input(struct net_buf *buf, uint8_t type, uint8_t code)
 DECL|net_icmpv6_register_handler|function|void net_icmpv6_register_handler(struct net_icmpv6_handler *handler)
 DECL|net_icmpv6_send_echo_request|function|int net_icmpv6_send_echo_request(struct net_if *iface, struct in6_addr *dst, uint16_t identifier, uint16_t sequence)
 DECL|net_icmpv6_send_error|function|int net_icmpv6_send_error(struct net_buf *orig, uint8_t type, uint8_t code, uint32_t param)
