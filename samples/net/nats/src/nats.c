e0fa17005c9e3e9ca6e018a6a2c9ac9728ac53c1
Leandro Pereira <leandro.pereira@intel.com>
2017-03-06 14:36:12 -0800

    samples: net/nats: Fix parsing of MSG messages
    This addresses the issues found by QA in ZEP-1012 and clarify the
    documentated behavior as described in ZEP-1859.
    
    Change-Id: I602e5749db7f6f44cf5be449b8e6f0d2ba66b69b
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: bb4a2eef7fcc3a280d826c0c88701df97361e748

--- samples/net/nats/src/nats.c
+++ samples/net/nats/src/nats.c
@@ -1,3 +1,4 @@
+DECL|CMD_BUF_LEN|macro|CMD_BUF_LEN
 DECL|CMD|macro|CMD
 DECL|CMD|macro|CMD
 DECL|FIELD|macro|FIELD
@@ -6,13 +7,14 @@ DECL|TRANSMITV_LITERAL|macro|TRANSMITV_LITERAL
 DECL|auth_required|member|bool auth_required;
 DECL|base|member|const void *base;
 DECL|char_in_set|function|static bool char_in_set(char chr, const char *set)
+DECL|copy_nbuf_to_buf|function|static int copy_nbuf_to_buf(struct net_buf *src, uint16_t offset, char *dst, size_t dst_size, size_t n_bytes)
 DECL|go|member|const char *go;
-DECL|handle_server_cmd|function|static int handle_server_cmd(struct nats *nats, char *cmd, size_t len)
-DECL|handle_server_info|function|static int handle_server_info(struct nats *nats, char *payload, size_t len)
-DECL|handle_server_msg|function|static int handle_server_msg(struct nats *nats, char *payload, size_t len)
-DECL|handle_server_ping|function|static int handle_server_ping(struct nats *nats, char *payload, size_t len)
+DECL|handle_server_cmd|function|static int handle_server_cmd(struct nats *nats, char *cmd, size_t len, struct net_buf *buf, uint16_t offset)
+DECL|handle_server_info|function|static int handle_server_info(struct nats *nats, char *payload, size_t len, struct net_buf *buf, uint16_t offset)
+DECL|handle_server_msg|function|static int handle_server_msg(struct nats *nats, char *payload, size_t len, struct net_buf *buf, uint16_t offset)
+DECL|handle_server_ping|function|static int handle_server_ping(struct nats *nats, char *payload, size_t len, struct net_buf *buf, uint16_t offset)
 DECL|host|member|const char *host;
-DECL|ignore|function|static int ignore(struct nats *nats, char *payload, size_t len)
+DECL|ignore|function|static int ignore(struct nats *nats, char *payload, size_t len, struct net_buf *buf, uint16_t offset)
 DECL|io_vec|struct|struct io_vec {
 DECL|is_sid_valid|function|static bool is_sid_valid(const char *sid, size_t len)
 DECL|is_subject_valid|function|static bool is_subject_valid(const char *subject, size_t len)
