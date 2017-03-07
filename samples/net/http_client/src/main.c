790613694677c1e4c66cc7bfc55a1979fd60d1c9
Flavio Santes <flavio.santes@intel.com>
2017-03-06 22:58:56 -0600

    samples/net: Add the HTTP API to the HTTP client sample application
    This patch replaces some routines found at the HTTP client sample
    application by the ones defined at the HTTP API.
    
    Jira: ZEP-1824
    
    Change-Id: Id19e3e346c09716ac95b0c488ff6e6949a5fbabe
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 87bc93182693156ddb485b7d776e94b52e7e95bf

--- samples/net/http_client/src/main.c
+++ samples/net/http_client/src/main.c
@@ -4,4 +4,4 @@ DECL|POST_PAYLOAD|macro|POST_PAYLOAD
 DECL|http_ctx|variable|http_ctx
 DECL|main|function|void main(void)
 DECL|print_banner|function|void print_banner(enum http_method method)
-DECL|send_http_method|function|void send_http_method(enum http_method method, const char *url, const char *content_type, const char *payload)
+DECL|send_http_method|function|static void send_http_method(enum http_method method, char *url, char *content_type, char *payload)
