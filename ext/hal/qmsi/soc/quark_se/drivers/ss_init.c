a00d01555fdcf21a735f83890c693c694758b8d2
Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
2017-03-07 14:57:43 -0800

    ext qmsi: Update QMSI to 1.4 RC4
    No major fixes since RC3 were made, and mostly the security fixes
    for the Quark Bootloader were the main driver behind this new QMSI
    Release Candidate.
    
    There are no changes to shim drivers at this moment.
    
    JIRA: ZEP-1572
    
    Change-Id: I68d2b0ee90863d3def909de556314bd86712a059
    Signed-off-by: Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
    
    Former-commit-id: 0877517ca426b91bff155ebab00386a53b92640c

--- ext/hal/qmsi/soc/quark_se/drivers/ss_init.c
+++ ext/hal/qmsi/soc/quark_se/drivers/ss_init.c
@@ -1,3 +1,4 @@
 DECL|SS_APP_PTR_ADDR|macro|SS_APP_PTR_ADDR
 DECL|__sensor_reset_vector|variable|__sensor_reset_vector
 DECL|sensor_activation|function|void sensor_activation(void)
+DECL|sensor_vectors_protect|function|static void sensor_vectors_protect(void)
