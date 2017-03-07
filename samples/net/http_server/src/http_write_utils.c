2dd7f8be6a678162e189fda9e8ea19c6e3c15f1e
Flavio Santes <flavio.santes@intel.com>
2017-03-06 22:50:14 -0600

    samples/net: Add the HTTP API to the HTTP server sample application
    This patch removes some routines from the HTTP server sample
    application and replaces them by the ones defined at the HTTP API.
    
    Jira: ZEP-1824
    
    Change-Id: Ia5db424d62997e01896294e12224aa65a58db432
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: 848966be43559520e2534f4753172fbb4af6f304

--- samples/net/http_server/src/http_write_utils.c
+++ samples/net/http_server/src/http_write_utils.c
@@ -1,13 +1,9 @@
+DECL|HTML_FOOTER|macro|HTML_FOOTER
+DECL|HTML_HEADER|macro|HTML_HEADER
 DECL|HTTP_MAX_BODY_STR_SIZE|macro|HTTP_MAX_BODY_STR_SIZE
+DECL|HTTP_STATUS_200_OK|macro|HTTP_STATUS_200_OK
 DECL|html_body|variable|html_body
-DECL|http_add_chunk|function|int http_add_chunk(struct net_buf *tx, const char *str)
-DECL|http_add_header|function|int http_add_header(struct net_buf *tx, const char *str)
-DECL|http_strlen|function|uint16_t http_strlen(const char *str)
-DECL|http_write_400_bad_request|function|int http_write_400_bad_request(struct http_server_ctx *ctx)
-DECL|http_write_403_forbidden|function|int http_write_403_forbidden(struct http_server_ctx *ctx)
-DECL|http_write_404_not_found|function|int http_write_404_not_found(struct http_server_ctx *ctx)
-DECL|http_write_header_fields|function|int http_write_header_fields(struct http_server_ctx *ctx)
-DECL|http_write_it_works|function|int http_write_it_works(struct http_server_ctx *ctx)
-DECL|http_write_soft_404_not_found|function|int http_write_soft_404_not_found(struct http_server_ctx *ctx)
-DECL|http_write|function|int http_write(struct http_server_ctx *ctx, const char *http_header, const char *html_header, const char *html_body, const char *html_footer)
+DECL|http_response_header_fields|function|int http_response_header_fields(struct http_server_ctx *ctx)
+DECL|http_response_it_works|function|int http_response_it_works(struct http_server_ctx *ctx)
+DECL|http_response_soft_404|function|int http_response_soft_404(struct http_server_ctx *ctx)
 DECL|print_http_headers|function|static void print_http_headers(struct http_server_ctx *ctx, char *str, uint16_t size)
