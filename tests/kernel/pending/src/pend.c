55a8698d49382dccd5f92a0df58f04202c02c433
Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
2017-03-08 15:04:01 -0800

    tests: kernel: test_pend: Porting legacy tests to unified kernel
    This is the port of the legacy/kernel/test_pend test case to
    the unified kernel
    
    Jira: ZEP-932
    
    Change-Id: I81762b45ff7000ff9f6079c674b46d233b2645de
    Signed-off-by: Sergio Rodriguez <sergio.sf.rodriguez@intel.com>
    
    Former-commit-id: 9df2afc05847117976f6386dba6c6b6bd31e3ae1

---
+++ tests/kernel/pending/src/pend.c
@@ -0,0 +1,53 @@
+DECL|COOP_STACKSIZE|macro|COOP_STACKSIZE
+DECL|FIFO_TEST_END|macro|FIFO_TEST_END
+DECL|FIFO_TEST_START|macro|FIFO_TEST_START
+DECL|FOURTH_SECOND|macro|FOURTH_SECOND
+DECL|HALF_SECOND|macro|HALF_SECOND
+DECL|LIFO_TEST_END|macro|LIFO_TEST_END
+DECL|LIFO_TEST_START|macro|LIFO_TEST_START
+DECL|NON_NULL_PTR|macro|NON_NULL_PTR
+DECL|NUM_SECONDS|macro|NUM_SECONDS
+DECL|PREEM_STACKSIZE|macro|PREEM_STACKSIZE
+DECL|SEM_TEST_END|macro|SEM_TEST_END
+DECL|SEM_TEST_START|macro|SEM_TEST_START
+DECL|THIRD_SECOND|macro|THIRD_SECOND
+DECL|coop_high_state|variable|coop_high_state
+DECL|coop_high|function|static void coop_high(void *arg1, void *arg2, void *arg3)
+DECL|coop_low_state|variable|coop_low_state
+DECL|coop_low|function|static void coop_low(void *arg1, void *arg2, void *arg3)
+DECL|coop_stack|variable|coop_stack
+DECL|counter|variable|counter
+DECL|data|member|uint32_t data;
+DECL|data|member|uint32_t data;
+DECL|end_test_sem|variable|end_test_sem
+DECL|fifo_data|struct|struct fifo_data {
+DECL|fifo_test_data|variable|fifo_test_data
+DECL|fifo_tests|function|static void fifo_tests(int32_t timeout, volatile int *state, void *(*get)(struct k_fifo *, int32_t), int (*sem_take)(struct k_sem *, int32_t))
+DECL|fifo|variable|fifo
+DECL|increment_counter|function|static int increment_counter(void)
+DECL|lifo_data|struct|struct lifo_data {
+DECL|lifo_test_data|variable|lifo_test_data
+DECL|lifo_tests|function|static void lifo_tests(int32_t timeout, volatile int *state, void *(*get)(struct k_lifo *, int32_t), int (*sem_take)(struct k_sem *, int32_t))
+DECL|lifo|variable|lifo
+DECL|my_fifo_get|function|static inline void *my_fifo_get(struct k_fifo *fifo, int32_t timeout)
+DECL|my_lifo_get|function|static inline void *my_lifo_get(struct k_lifo *lifo, int32_t timeout)
+DECL|offload_work_q_stack|variable|offload_work_q_stack
+DECL|offload_work_q|variable|offload_work_q
+DECL|offload_work|struct|struct offload_work {
+DECL|reserved|member|uint32_t reserved;
+DECL|reserved|member|uint32_t reserved;
+DECL|sem|member|struct k_sem *sem;
+DECL|start_test_sem|variable|start_test_sem
+DECL|sync_test_sem|variable|sync_test_sem
+DECL|sync_threads|function|static void sync_threads(struct k_work *work)
+DECL|task_high_state|variable|task_high_state
+DECL|task_high|function|void task_high(void)
+DECL|task_low_state|variable|task_low_state
+DECL|task_low|function|void task_low(void)
+DECL|task_monitor|function|void task_monitor(void)
+DECL|timer_data|variable|timer_data
+DECL|timer_end_tick|variable|timer_end_tick
+DECL|timer_start_tick|variable|timer_start_tick
+DECL|timer_tests|function|static void timer_tests(void)
+DECL|timer|variable|timer
+DECL|work_item|member|struct k_work work_item;
