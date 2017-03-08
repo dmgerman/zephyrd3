b100e9491b1cd18895e613db8966daad145062ea
Sathish Narasimman <sathish.narasimman@intel.com>
2017-03-08 14:18:16 +0530

    Bluetooth: AT: Unit test application init
    AT Module unit testing application to verify at_parse_input,
    at_parse_cmd_input, at_get_number.
    
    Change-Id: I256c0f9b62c13bc6f22eac4b94118b5fc7643462
    Signed-off-by: Sathish Narasimman <sathish.narasimman@intel.com>
    
    Former-commit-id: 95c4d0c1f38b73599e2b5be6ce1b55abfff03b6e

---
+++ tests/unit/bluetooth/at/src/main.c
@@ -0,0 +1,7 @@
+DECL|at_handle|function|int at_handle(struct at_client *hf_at)
+DECL|at_resp|function|int at_resp(struct at_client *hf_at, struct net_buf *buf)
+DECL|at_test1|function|static void at_test1(void)
+DECL|at|variable|at
+DECL|buffer|variable|buffer
+DECL|example_data|variable|example_data
+DECL|test_main|function|void test_main(void)
