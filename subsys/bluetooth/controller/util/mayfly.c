40e21a71ab7538379b48295675d229eecd686e1e
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2017-03-08 13:42:34 +0100

    Bluetooth: Controller: mayfly enable to supercede over disable
    If mayfly enable is called before mayfly could be disabled,
    then enable shall supercede disabling, the mayfly will
    remain enabled. Any new mayfly enqueued by the caller that
    tried to disable mayfly will be chain for deferred
    executon under this condition.
    
    The BLE Controller's connection update procedure broke when
    mayfly implementation was updated to defer disabling until
    all queued mayfly where completed. Mayfly is disabled
    between ticker_stop and ticker_start calls to chain them
    so that ticker does not power off counter h/w if the ticker
    being stopped is last one.
    
    This commit fixes the connection update procedure which
    used the mayfly enable before mayfly disable could
    complete.
    
    Jira: ZEP-1839
    
    Change-id: I07d34c90d193b5eca9762acd8b7272e8d7a78474
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: 16608e9a51f33a37c1b9e6e386b70f2794506149

--- subsys/bluetooth/controller/util/mayfly.c
+++ subsys/bluetooth/controller/util/mayfly.c
@@ -1,6 +1,7 @@
-DECL|disable_ack|member|uint32_t disable_ack : 1;
-DECL|disable_req|member|uint32_t disable_req : 1;
-DECL|flags|member|uint32_t flags;
+DECL|disable_ack|member|uint8_t disable_ack;
+DECL|disable_req|member|uint8_t disable_req;
+DECL|enable_ack|member|uint8_t enable_ack;
+DECL|enable_req|member|uint8_t enable_req;
 DECL|head|member|void *head;
 DECL|mayfly_enable|function|void mayfly_enable(uint8_t caller_id, uint8_t callee_id, uint8_t enable)
 DECL|mayfly_enqueue|function|uint32_t mayfly_enqueue(uint8_t caller_id, uint8_t callee_id, uint8_t chain,struct mayfly *m)
