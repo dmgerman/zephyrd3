786dbbf2f3084daaf77d2ae779046fe19d49c0d4
Marc Moreno <marc.morenoberengue@linaro.org>
2017-03-09 11:12:48 +0000

    samples: Add an MPU test suite
    This patch contains a MPU test with a set of options to check
    the correct MPU configuration against the following security
    issues:
        * Read at an address that is reserved in the memory map.
        * Write into the boot Flash/ROM.
        * Run code located in SRAM.
    
    The MPU test application uses the Zephyr shell.
    
    Change-Id: Ib40dc76b082b800884fd636a1509a0712227d681
    Signed-off-by: Marc Moreno <marc.morenoberengue@linaro.org>
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>
    
    Former-commit-id: 3376c6769183d40af7551169209fd2fedccb2720

---
+++ samples/mpu_test/src/main.c
@@ -0,0 +1,14 @@
+DECL|FLASH_MEM|macro|FLASH_MEM
+DECL|MTEST_CMD_HELP|macro|MTEST_CMD_HELP
+DECL|RAM_MEM|macro|RAM_MEM
+DECL|READ_CMD_HELP|macro|READ_CMD_HELP
+DECL|RESERVED_MEM_MAP|macro|RESERVED_MEM_MAP
+DECL|RUN_CMD_HELP|macro|RUN_CMD_HELP
+DECL|SHELL_MODULE|macro|SHELL_MODULE
+DECL|WRITE_CMD_HELP|macro|WRITE_CMD_HELP
+DECL|commands|variable|commands
+DECL|main|function|void main(void)
+DECL|shell_cmd_mtest|function|static int shell_cmd_mtest(int argc, char *argv[])
+DECL|shell_cmd_read|function|static int shell_cmd_read(int argc, char *argv[])
+DECL|shell_cmd_run|function|static int shell_cmd_run(int argc, char *argv[])
+DECL|shell_cmd_write|function|static int shell_cmd_write(int argc, char *argv[])
