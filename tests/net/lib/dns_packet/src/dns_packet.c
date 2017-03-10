8e53bceb2dc4927b07de1e2ad2753550c7a4cf4e
Flavio Santes <flavio.santes@intel.com>
2017-03-10 01:15:07 -0600

    tests/net/dns: Add routines to evaluate DNS responses
    This patch adds some routines to evaluate DNS responses as specified
    by RFC 1035.
    
    Jira: ZEP-1876
    
    Change-Id: Ia78a1175229962128a0bcee985e507d72ed26aea
    Signed-off-by: Flavio Santes <flavio.santes@intel.com>
    
    Former-commit-id: f1ec62ed75a5dbe0d6f490ddeadf438f01d5bb3c

--- tests/net/lib/dns_packet/src/dns_packet.c
+++ tests/net/lib/dns_packet/src/dns_packet.c
@@ -1,12 +1,31 @@
+DECL|ANS_TTL_SIZE|macro|ANS_TTL_SIZE
 DECL|DNAME1|macro|DNAME1
 DECL|DNS_HEADER_SIZE|macro|DNS_HEADER_SIZE
+DECL|INT_SIZE|macro|INT_SIZE
 DECL|MAX_BUF_SIZE|macro|MAX_BUF_SIZE
+DECL|NAME_PTR_SIZE|macro|NAME_PTR_SIZE
+DECL|RESPONSE_MIN_SIZE|macro|RESPONSE_MIN_SIZE
+DECL|ancount|member|uint8_t ancount;
+DECL|answer_type|member|uint8_t answer_type;
 DECL|buf_len|variable|buf_len
 DECL|buf|variable|buf
+DECL|dname|member|const char *dname;
+DECL|dns_response_test|struct|struct dns_response_test {
 DECL|eval_query|function|static int eval_query(const char *dname, uint16_t tid, enum dns_rr_type type, uint8_t *expected, uint16_t expected_len)
+DECL|eval_response1|function|static int eval_response1(struct dns_response_test *resp)
 DECL|qname_len|variable|qname_len
 DECL|qname|variable|qname
 DECL|query_ipv4|variable|query_ipv4
+DECL|ra|member|uint8_t ra;
+DECL|rdata|member|const uint8_t *rdata;
+DECL|rdlen|member|uint8_t rdlen;
+DECL|res_len|member|uint16_t res_len;
+DECL|resp_ipv4_addr|variable|resp_ipv4_addr
+DECL|resp_ipv4|variable|resp_ipv4
+DECL|res|member|uint8_t *res;
 DECL|test_dns_query|function|void test_dns_query(void)
+DECL|test_dns_response|function|void test_dns_response(void)
 DECL|test_main|function|void test_main(void)
 DECL|tid1|variable|tid1
+DECL|tid|member|uint16_t tid;
+DECL|ttl|member|uint32_t ttl;
