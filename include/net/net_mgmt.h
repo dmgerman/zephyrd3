c9b0f625c8981fd820d7c464f2cd4c214dcdd349
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-03-13 11:54:37 +0100

    net/mgmt: Add a function to wait on a event synchronously
    Instead of creating a handler and a related callback structure on an
    event_mask: net_mgmt_event_wait() can be used to wait synchronously on
    such event_mask. The core mgmt part will seamlessly reuse the struct
    net_mgmt_event_callback so the whole internal notification mechanism is
    using the same code.
    
    Change-Id: I426d782c770e75e5222aa3c5b703172b1f1f2e5e
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 35e5aa88655423c71f94914bfa8b7471776ef9f7

--- include/net/net_mgmt.h
+++ include/net/net_mgmt.h
@@ -2,6 +2,7 @@ DECL|NET_MGMT_COMMAND_MASK|macro|NET_MGMT_COMMAND_MASK
 DECL|NET_MGMT_DEFINE_REQUEST_HANDLER|macro|NET_MGMT_DEFINE_REQUEST_HANDLER
 DECL|NET_MGMT_EVENT_BIT|macro|NET_MGMT_EVENT_BIT
 DECL|NET_MGMT_EVENT_MASK|macro|NET_MGMT_EVENT_MASK
+DECL|NET_MGMT_EVENT_SYNCHRONOUS|macro|NET_MGMT_EVENT_SYNCHRONOUS
 DECL|NET_MGMT_EVENT|macro|NET_MGMT_EVENT
 DECL|NET_MGMT_GET_COMMAND|macro|NET_MGMT_GET_COMMAND
 DECL|NET_MGMT_GET_LAYER_CODE|macro|NET_MGMT_GET_LAYER_CODE
@@ -17,6 +18,8 @@ DECL|NET_MGMT_LAYER|macro|NET_MGMT_LAYER
 DECL|NET_MGMT_ON_IFACE_MASK|macro|NET_MGMT_ON_IFACE_MASK
 DECL|NET_MGMT_ON_IFACE|macro|NET_MGMT_ON_IFACE
 DECL|NET_MGMT_REGISTER_REQUEST_HANDLER|macro|NET_MGMT_REGISTER_REQUEST_HANDLER
+DECL|NET_MGMT_SYNC_EVENT_BIT|macro|NET_MGMT_SYNC_EVENT_BIT
+DECL|NET_MGMT_SYNC_EVENT_MASK|macro|NET_MGMT_SYNC_EVENT_MASK
 DECL|__NET_MGMT_H__|macro|__NET_MGMT_H__
 DECL|event_mask|member|uint32_t event_mask;
 DECL|handler|member|net_mgmt_event_handler_t handler;
@@ -25,8 +28,11 @@ DECL|net_mgmt_event_callback|struct|struct net_mgmt_event_callback {
 DECL|net_mgmt_event_handler_t|typedef|typedef void (*net_mgmt_event_handler_t)(struct net_mgmt_event_callback *cb,
 DECL|net_mgmt_event_init|macro|net_mgmt_event_init
 DECL|net_mgmt_event_notify|macro|net_mgmt_event_notify
+DECL|net_mgmt_event_wait|function|static inline int net_mgmt_event_wait(uint32_t mgmt_event_mask, uint32_t *raised_event, struct net_if **iface, int timeout)
 DECL|net_mgmt_init_event_callback|function|void net_mgmt_init_event_callback(struct net_mgmt_event_callback *cb, net_mgmt_event_handler_t handler, uint32_t mgmt_event_mask)
 DECL|net_mgmt_init_event_callback|macro|net_mgmt_init_event_callback
 DECL|net_mgmt_request_handler_t|typedef|typedef int (*net_mgmt_request_handler_t)(uint32_t mgmt_request,
 DECL|net_mgmt|macro|net_mgmt
 DECL|node|member|sys_snode_t node;
+DECL|raised_event|member|uint32_t raised_event;
+DECL|sync_call|member|struct k_sem *sync_call;
