9cf377bf5d4c6b001494026f6b00db65007afc9d
Jean-Paul Etienne <fractalclone@gmail.com>
2017-03-14 22:15:59 +0100

    riscv32: added support for the SiFive Freedom E310 SOC
    The SiFive Freedom E310 SOC follows the riscv privilege
    architecture specification and hence is declared within
    the riscv privilege SOC family.
    
    It also provides support for a riscv
    Platform Level Interrupt Controller (PLIC)
    
    Change-Id: I19ff0997eacc248f48444fc96566a105c6c02663
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 4ae030c7b8bb8f05067dd7f3c590fba69fab7929

---
+++ include/arch/riscv32/fe310/asm_inline_gcc.h
@@ -0,0 +1,3 @@
+DECL|_ASM_INLINE_GCC_H|macro|_ASM_INLINE_GCC_H
+DECL|find_lsb_set|function|static ALWAYS_INLINE unsigned int find_lsb_set(uint32_t op)
+DECL|find_msb_set|function|static ALWAYS_INLINE unsigned int find_msb_set(uint32_t op)
