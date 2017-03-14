35bed66271aa0f494128fdac6f4b5f456ab4a4b4
Jean-Paul Etienne <fractalclone@gmail.com>
2017-03-14 22:13:58 +0100

    riscv32: riscv-privilege: Platform-Level Interrupt Controller support
    Updated the riscv-privilege SOC family to account for SOCs supporting
    a Platform-level Interrupt Controller (PLIC) as specified by the
    riscv privilege architecture.
    
    riscv-privilege SOCs supporting a PLIC have to implement the following
    list of APIs:
    void riscv_plic_irq_enable(uint32_t irq);
    void riscv_plic_irq_disable(uint32_t irq);
    int riscv_plic_irq_is_enabled(uint32_t irq);
    void riscv_plic_set_priority(uint32_t irq, uint32_t priority);
    int riscv_plic_get_irq(void);
    
    Change-Id: I0228574967348d572afc98a79257c697efc4309e
    Signed-off-by: Jean-Paul Etienne <fractalclone@gmail.com>
    
    Former-commit-id: 4b8ae8863c2e5af098337565c96bb84e85457ed9

--- include/arch/riscv32/arch.h
+++ include/arch/riscv32/arch.h
@@ -8,6 +8,7 @@ DECL|STACK_ROUND_DOWN|macro|STACK_ROUND_DOWN
 DECL|STACK_ROUND_UP|macro|STACK_ROUND_UP
 DECL|TOSTR|macro|TOSTR
 DECL|_ARCH_IRQ_CONNECT|macro|_ARCH_IRQ_CONNECT
+DECL|_ARCH_IRQ_CONNECT|macro|_ARCH_IRQ_CONNECT
 DECL|_RISCV32_ARCH_H_|macro|_RISCV32_ARCH_H_
 DECL|_arch_irq_lock|function|static ALWAYS_INLINE unsigned int _arch_irq_lock(void)
 DECL|_arch_irq_unlock|function|static ALWAYS_INLINE void _arch_irq_unlock(unsigned int key)
