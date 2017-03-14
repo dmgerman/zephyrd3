1ccd93747f519335ad28d86378919fba2ee7d6aa
Leandro Pereira <leandro.pereira@intel.com>
2017-03-14 16:50:33 -0700

    lib: json: Add encoding support
    Serializing an object in JSON is quite tricky to do by hand, and with
    an array of descriptor structs, there's enough information to do that
    programatically.
    
    The encoder takes a callback function, so that one can be written to
    write bytes to, for instance, a struct net_buf.  This way, there's no
    guesswork to determine the buffer size, reducing the possibility of
    overflowing the stack.
    
    Jira: ZEP-1607
    Change-Id: I5ccf1012e46c1db32fcfdf2ecee4a1ef44c927d5
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: b9b1c18cd728e8633a8b58613da2b30a54c94588

--- lib/json/json.c
+++ lib/json/json.c
@@ -1,19 +1,29 @@
+DECL|append_bytes_to_buf|function|static int append_bytes_to_buf(const uint8_t *bytes, size_t len, void *data)
+DECL|appender|struct|struct appender {
+DECL|arr_encode|function|static int arr_encode(const struct json_obj_descr *descr, const void *field, const void *val, json_append_bytes_t append_bytes, void *data)
 DECL|arr_next|function|static int arr_next(struct json_obj *json, struct token *value)
 DECL|arr_parse|function|static int arr_parse(struct json_obj *obj, const struct json_obj_descr *elem_descr, size_t max_elements, void *field, void *val)
 DECL|backup|function|static void backup(struct lexer *lexer)
+DECL|bool_encode|function|static int bool_encode(const bool *value, json_append_bytes_t append_bytes, void *data)
+DECL|buffer|member|char *buffer;
 DECL|decode_num|function|static int decode_num(const struct token *token, int32_t *num)
 DECL|decode_value|function|static int decode_value(struct json_obj *obj,const struct json_obj_descr *descr, struct token *value, void *field, void *val)
 DECL|element_token|function|static int element_token(enum json_tokens token)
 DECL|emit|function|static void emit(struct lexer *lexer, enum json_tokens token)
+DECL|encode|function|static int encode(const struct json_obj_descr *descr, const void *val, json_append_bytes_t append_bytes, void *data)
 DECL|end|member|char *end;
 DECL|end|member|char *end;
 DECL|equivalent_types|function|static bool equivalent_types(enum json_tokens type1, enum json_tokens type2)
 DECL|escapable|variable|escapable
 DECL|get_elem_size|function|static ptrdiff_t get_elem_size(const struct json_obj_descr *descr)
 DECL|ignore|function|static void ignore(struct lexer *lexer)
+DECL|json_calc_encoded_len|function|ssize_t json_calc_encoded_len(const struct json_obj_descr *descr, size_t descr_len, const void *val)
 DECL|json_calc_escaped_len|function|size_t json_calc_escaped_len(const char *str, size_t len)
-DECL|json_escape_internal|function|static int json_escape_internal(char *str, size_t *len, size_t buf_size)
+DECL|json_escape_buf|function|static int json_escape_buf(char *str, size_t *len, size_t buf_size)
+DECL|json_escape_internal|function|static int json_escape_internal(const char *str,json_append_bytes_t append_bytes, void *data)
 DECL|json_escape|function|ssize_t json_escape(char *str, size_t *len, size_t buf_size)
+DECL|json_obj_encode_buf|function|int json_obj_encode_buf(const struct json_obj_descr *descr, size_t descr_len,const void *val, char *buffer, size_t buf_size)
+DECL|json_obj_encode|function|int json_obj_encode(const struct json_obj_descr *descr, size_t descr_len, const void *val, json_append_bytes_t append_bytes, void *data)
 DECL|json_obj_key_value|struct|struct json_obj_key_value {
 DECL|json_obj_parse|function|int json_obj_parse(char *payload, size_t len, const struct json_obj_descr *descr, size_t descr_len, void *val)
 DECL|json_obj|struct|struct json_obj {
@@ -29,16 +39,22 @@ DECL|lexer_number|function|static void *lexer_number(struct lexer *lexer)
 DECL|lexer_string|function|static void *lexer_string(struct lexer *lexer)
 DECL|lexer|member|struct lexer lexer;
 DECL|lexer|struct|struct lexer {
+DECL|measure_bytes|function|static int measure_bytes(const uint8_t *bytes, size_t len, void *data)
 DECL|next|function|static char next(struct lexer *lexer)
+DECL|num_encode|function|static int num_encode(const int32_t *num, json_append_bytes_t append_bytes, void *data)
+DECL|obj_encode|function|static int obj_encode(const struct json_obj_descr *descr, size_t descr_len, const void *val, json_append_bytes_t append_bytes, void *data)
 DECL|obj_init|function|static int obj_init(struct json_obj *json, char *data, size_t len)
 DECL|obj_next|function|static int obj_next(struct json_obj *json, struct json_obj_key_value *kv)
 DECL|obj_parse|function|static int obj_parse(struct json_obj *obj, const struct json_obj_descr *descr, size_t descr_len, void *val)
 DECL|peek|function|static char peek(struct lexer *lexer)
 DECL|pos|member|char *pos;
+DECL|size|member|size_t size;
 DECL|start|member|char *start;
 DECL|start|member|char *start;
 DECL|state|member|void *(*state)(struct lexer *lexer);
+DECL|str_encode|function|static int str_encode(const char **str, json_append_bytes_t append_bytes, void *data)
 DECL|token|member|struct token token;
 DECL|token|struct|struct token {
 DECL|type|member|enum json_tokens type;
+DECL|used|member|size_t used;
 DECL|value|member|struct token value;
