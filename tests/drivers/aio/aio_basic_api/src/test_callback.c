b1e33da08412e3f1101817d9f28430e24e9498c3
Qiu Peiyang <peiyangx.qiu@intel.com>
2017-03-15 09:46:50 +0800

    tests: aio: port AIO cases to run on more platforms
    AIO should be supported on more platforms. Adapt
    this case to make it run on more platforms.
    Also keep reference voltage for comprator as internal.
    
    Signed-off-by: Qiu Peiyang <peiyangx.qiu@intel.com>
    
    Former-commit-id: b0962e3bac67698f1f18df2b6a22a7783e61d030

--- tests/drivers/aio/aio_basic_api/src/test_callback.c
+++ tests/drivers/aio/aio_basic_api/src/test_callback.c
@@ -1,5 +1,22 @@
+DECL|AIO_CMP_DEV_NAME|macro|AIO_CMP_DEV_NAME
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|GPIO_DEV_NAME|macro|GPIO_DEV_NAME
+DECL|PINMUX_NAME|macro|PINMUX_NAME
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_IN|macro|PIN_IN
+DECL|PIN_OUT|macro|PIN_OUT
+DECL|PIN_OUT|macro|PIN_OUT
+DECL|PIN_OUT|macro|PIN_OUT
+DECL|PIN_OUT|macro|PIN_OUT
+DECL|PIN_OUT|macro|PIN_OUT
+DECL|aio_dev|variable|aio_dev
 DECL|callback|function|static void callback(void *param)
 DECL|cb_cnt|variable|cb_cnt
-DECL|set_aio_callback|function|static void set_aio_callback(int polarity, int disable)
+DECL|gpio_dev|variable|gpio_dev
+DECL|set_aio_callback|function|static int set_aio_callback(int polarity, int disable)
+DECL|test_aio_callback_fall|function|void test_aio_callback_fall(void)
 DECL|test_aio_callback_rise_disable|function|void test_aio_callback_rise_disable(void)
 DECL|test_aio_callback_rise|function|void test_aio_callback_rise(void)
