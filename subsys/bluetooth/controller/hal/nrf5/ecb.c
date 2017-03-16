35e2e3d373efedd5fa2a02f1d1c0276583174154
Johan Hedberg <johan.hedberg@intel.com>
2017-03-16 10:40:19 +0200

    Bluetooth: Controller: Introduce big-endian variant for ECB
    There are some use cases where all parameters are in big-endian. To
    avoid excessive byte order reversals introduce a native big-endian
    API.
    
    Change-Id: I58fe9156c8819a3a43d715e70b6ba358bd2f844b
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6cb853fc04b5f42021b1a72b6b2a37078473c01c

--- subsys/bluetooth/controller/hal/nrf5/ecb.c
+++ subsys/bluetooth/controller/hal/nrf5/ecb.c
@@ -6,6 +6,7 @@ DECL|do_ecb|function|static void do_ecb(struct ecb_param *ecb)
 DECL|done|member|uint32_t volatile done;
 DECL|ecb_cb|function|static void ecb_cb(uint32_t status, uint8_t *cipher_be, void *context)
 DECL|ecb_cleanup|function|static void ecb_cleanup(void)
+DECL|ecb_encrypt_be|function|void ecb_encrypt_be(uint8_t const *const key_be, uint8_t const *const clear_text_be, uint8_t * const cipher_text_be)
 DECL|ecb_encrypt_nonblocking|function|uint32_t ecb_encrypt_nonblocking(struct ecb *ecb)
 DECL|ecb_encrypt|function|void ecb_encrypt(uint8_t const *const key_le, uint8_t const *const clear_text_le, uint8_t * const cipher_text_le, uint8_t * const cipher_text_be)
 DECL|ecb_param|struct|struct ecb_param {
