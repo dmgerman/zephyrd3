b32fe25e11b5cb862542ceca9d1afff54d3ee210
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2017-03-16 14:28:35 +0000

    arm: core: mpu: Add ARM MPU support
    This patch adds an initial driver for the ARM MPU.
    This driver has been tested on ARM Beetle and STM32F4.
    
    Change-Id: I2bc4031961ec5a1d569929249237646f4a349f16
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>
    
    Former-commit-id: e37214062cc3c8150aaa3e8d532c4d30748869b3

---
+++ arch/arm/core/cortex_m/mpu/arm_mpu.c
@@ -0,0 +1,5 @@
+DECL|ARM_MPU_DEV|macro|ARM_MPU_DEV
+DECL|_arm_mpu_config|function|static void _arm_mpu_config(void)
+DECL|_get_num_regions|function|static inline u8_t _get_num_regions(void)
+DECL|_region_init|function|static void _region_init(u32_t index, u32_t region_addr, u32_t region_attr)
+DECL|arm_mpu_init|function|static int arm_mpu_init(struct device *arg)
