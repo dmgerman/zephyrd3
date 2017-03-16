cafd556ee4aad0be122557d97c6934be9af760c8
Johan Hedberg <johan.hedberg@intel.com>
2017-03-16 10:56:35 +0200

    Bluetooth: Introduce public big-endian AES API
    There may be use cases where input and output is in big-endian rather
    than little-endian. Introduce a native big-endian API to avoid
    excessive byte order reversals in these cases.
    
    Change-Id: Ia7b3e01bb0a07c4560b23f60c2f615ec614eb431
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 94831a7589f2908d536c13c79416e0a50f7782da

--- subsys/bluetooth/controller/ll_sw/crypto.c
+++ subsys/bluetooth/controller/ll_sw/crypto.c
@@ -1,4 +1,5 @@
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
-DECL|bt_encrypt|function|int bt_encrypt(const uint8_t key[16], const uint8_t plaintext[16], uint8_t enc_data[16])
+DECL|bt_encrypt_be|function|int bt_encrypt_be(const uint8_t key[16], const uint8_t plaintext[16], uint8_t enc_data[16])
+DECL|bt_encrypt_le|function|int bt_encrypt_le(const uint8_t key[16], const uint8_t plaintext[16], uint8_t enc_data[16])
 DECL|bt_rand|function|int bt_rand(void *buf, size_t len)
 DECL|mutex_rand|variable|mutex_rand
