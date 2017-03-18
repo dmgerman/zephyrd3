3ef5606b9bdde8f1346cf80cf3f1d3179ffa7cdf
Maureen Helm <maureen.helm@nxp.com>
2017-03-17 19:59:26 -0500

    sensor: max30101: Add heart rate sensor driver
    Adds basic sensor driver support for the Maxim MAX30101 heart rate
    sensor.
    
    This driver does not yet support any sensor triggers such as the data
    ready trigger, or runtime changing of sensor attributes.
    
    Default configuration values were taken from the MikroE Hexiwear driver.
    https://github.com/MikroElektronika/HEXIWEAR
    
    Datasheet: https://datasheets.maximintegrated.com/en/ds/MAX30101.pdf
    
    Jira: ZEP-720
    Change-Id: Ie8981e124da36a56a214f133bf9a11b9f47d60fa
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 53678df5e2ce1ddd450464667333211dd32cdf7d

---
+++ drivers/sensor/max30101/max30101.c
@@ -0,0 +1,6 @@
+DECL|max30101_channel_get|function|static int max30101_channel_get(struct device *dev, enum sensor_channel chan,struct sensor_value *val)
+DECL|max30101_config|variable|max30101_config
+DECL|max30101_data|variable|max30101_data
+DECL|max30101_driver_api|variable|max30101_driver_api
+DECL|max30101_init|function|static int max30101_init(struct device *dev)
+DECL|max30101_sample_fetch|function|static int max30101_sample_fetch(struct device *dev, enum sensor_channel chan)
