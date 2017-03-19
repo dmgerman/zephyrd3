6d432ab7ef33e567202ff38dea36105616db0347
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-03-19 22:13:22 +0200

    net: dns: Add resolve API
    This commit introduces a generic DNS resolving API that can
    be used by applications. Later commits will introduce a system
    level DNS support which simplifies the DNS resolving so that
    DNS server names can be given from config file.
    
    Change-Id: I60fbc81e2a44928d2ca53d51e703b9cde222b382
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 79cd66f40ca7101769f783cede0ed7cc36020d48

---
+++ include/net/dns_resolve.h
@@ -0,0 +1,48 @@
+DECL|DNS_EAI_ADDRFAMILY|enumerator|DNS_EAI_ADDRFAMILY = -9, /* Address family for NAME not supported */
+DECL|DNS_EAI_AGAIN|enumerator|DNS_EAI_AGAIN = -3, /* Temporary failure in name resolution */
+DECL|DNS_EAI_ALLDONE|enumerator|DNS_EAI_ALLDONE = -103, /* All requests done */
+DECL|DNS_EAI_BADFLAGS|enumerator|DNS_EAI_BADFLAGS = -1, /* Invalid value for `ai_flags' field */
+DECL|DNS_EAI_CANCELED|enumerator|DNS_EAI_CANCELED = -101, /* Request canceled */
+DECL|DNS_EAI_FAIL|enumerator|DNS_EAI_FAIL = -4, /* Non-recoverable failure in name res */
+DECL|DNS_EAI_FAMILY|enumerator|DNS_EAI_FAMILY = -6, /* `ai_family' not supported */
+DECL|DNS_EAI_IDN_ENCODE|enumerator|DNS_EAI_IDN_ENCODE = -105, /* IDN encoding failed */
+DECL|DNS_EAI_INPROGRESS|enumerator|DNS_EAI_INPROGRESS = -100, /* Processing request in progress */
+DECL|DNS_EAI_MEMORY|enumerator|DNS_EAI_MEMORY = -10, /* Memory allocation failure */
+DECL|DNS_EAI_NODATA|enumerator|DNS_EAI_NODATA = -5, /* No address associated with NAME */
+DECL|DNS_EAI_NONAME|enumerator|DNS_EAI_NONAME = -2, /* NAME or SERVICE is unknown */
+DECL|DNS_EAI_NOTCANCELED|enumerator|DNS_EAI_NOTCANCELED = -102, /* Request not canceled */
+DECL|DNS_EAI_OVERFLOW|enumerator|DNS_EAI_OVERFLOW = -12, /* Argument buffer overflow */
+DECL|DNS_EAI_SERVICE|enumerator|DNS_EAI_SERVICE = -8, /* SRV not supported for `ai_socktype' */
+DECL|DNS_EAI_SOCKTYPE|enumerator|DNS_EAI_SOCKTYPE = -7, /* `ai_socktype' not supported */
+DECL|DNS_EAI_SYSTEM|enumerator|DNS_EAI_SYSTEM = -11, /* System error returned in `errno' */
+DECL|DNS_MAX_NAME_SIZE|macro|DNS_MAX_NAME_SIZE
+DECL|DNS_QUERY_TYPE_AAAA|enumerator|DNS_QUERY_TYPE_AAAA = 28 /* IPv6 */
+DECL|DNS_QUERY_TYPE_A|enumerator|DNS_QUERY_TYPE_A = 1, /* IPv4 */
+DECL|_DNS_RESOLVE_H|macro|_DNS_RESOLVE_H
+DECL|ai_addrlen|member|socklen_t ai_addrlen;
+DECL|ai_addr|member|struct sockaddr ai_addr;
+DECL|ai_canonname|member|char ai_canonname[DNS_MAX_NAME_SIZE + 1];
+DECL|ai_family|member|uint8_t ai_family;
+DECL|ai_flags|member|uint16_t ai_flags;
+DECL|ai_protocol|member|uint8_t ai_protocol;
+DECL|ai_socktype|member|uint8_t ai_socktype;
+DECL|buf_timeout|member|int32_t buf_timeout;
+DECL|cb|member|dns_resolve_cb_t cb;
+DECL|ctx|member|struct dns_resolve_context *ctx;
+DECL|dns_addrinfo|struct|struct dns_addrinfo {
+DECL|dns_pending_query|struct|struct dns_pending_query {
+DECL|dns_query_type|enum|enum dns_query_type {
+DECL|dns_resolve_cb_t|typedef|typedef void (*dns_resolve_cb_t)(enum dns_resolve_status status,
+DECL|dns_resolve_context|struct|struct dns_resolve_context {
+DECL|dns_resolve_status|enum|enum dns_resolve_status {
+DECL|dns_server|member|struct sockaddr dns_server;
+DECL|id|member|uint16_t id;
+DECL|is_used|member|bool is_used;
+DECL|net_ctx|member|struct net_context *net_ctx;
+DECL|queries|member|} queries[CONFIG_DNS_NUM_CONCUR_QUERIES];
+DECL|query_type|member|enum dns_query_type query_type;
+DECL|query|member|const char *query;
+DECL|servers|member|} servers[CONFIG_DNS_RESOLVER_MAX_SERVERS];
+DECL|timeout|member|int32_t timeout;
+DECL|timer|member|struct k_delayed_work timer;
+DECL|user_data|member|void *user_data;
---
+++ subsys/net/lib/dns/resolve.c
@@ -0,0 +1,22 @@
+DECL|DNS_ANSWER_PTR_LEN|macro|DNS_ANSWER_PTR_LEN
+DECL|DNS_BUF_TIMEOUT|macro|DNS_BUF_TIMEOUT
+DECL|DNS_IPV4_LEN|macro|DNS_IPV4_LEN
+DECL|DNS_IPV6_LEN|macro|DNS_IPV6_LEN
+DECL|DNS_MAX_NAME_LEN|macro|DNS_MAX_NAME_LEN
+DECL|DNS_QUERY_MAX_SIZE|macro|DNS_QUERY_MAX_SIZE
+DECL|DNS_QUERY_POS|macro|DNS_QUERY_POS
+DECL|DNS_RESOLVER_BUF_CTR|macro|DNS_RESOLVER_BUF_CTR
+DECL|DNS_RESOLVER_MAX_BUF_SIZE|macro|DNS_RESOLVER_MAX_BUF_SIZE
+DECL|DNS_RESOLVER_MIN_BUF|macro|DNS_RESOLVER_MIN_BUF
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|cb_recv|function|static void cb_recv(struct net_context *net_ctx, struct net_buf *buf, int status, void *user_data)
+DECL|dns_read|function|static int dns_read(struct dns_resolve_context *ctx, struct net_buf *buf, struct net_buf *dns_data, uint16_t *dns_id, struct net_buf *dns_cname,
+DECL|dns_resolve_cancel|function|int dns_resolve_cancel(struct dns_resolve_context *ctx, uint16_t dns_id)
+DECL|dns_resolve_close|function|int dns_resolve_close(struct dns_resolve_context *ctx)
+DECL|dns_resolve_init|function|int dns_resolve_init(struct dns_resolve_context *ctx, const char *servers[])
+DECL|dns_resolve_name|function|int dns_resolve_name(struct dns_resolve_context *ctx, const char *query, enum dns_query_type type, uint16_t *dns_id, dns_resolve_cb_t cb,
+DECL|dns_write|function|static int dns_write(struct dns_resolve_context *ctx, int server_idx, int query_idx, struct net_buf *dns_data, struct net_buf *dns_qname)
+DECL|get_cb_slot|function|static inline int get_cb_slot(struct dns_resolve_context *ctx)
+DECL|get_slot_by_id|function|static inline int get_slot_by_id(struct dns_resolve_context *ctx, uint16_t dns_id)
+DECL|query_timeout|function|static void query_timeout(struct k_work *work)
