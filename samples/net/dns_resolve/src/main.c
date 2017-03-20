108eaff05b85444ac6af3fe259bddc73b5a2ee1c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-03-20 22:22:23 +0200

    samples: net: DNS resolving sample application
    Change-Id: I4a0283efde4d0dd8ccd0933071acb277431d4a32
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c6cad93d8c9687d11abb5eeba8e0297d93424f02

---
+++ samples/net/dns_resolve/src/main.c
@@ -0,0 +1,23 @@
+DECL|DNS_TIMEOUT|macro|DNS_TIMEOUT
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|dns_result_cb|function|void dns_result_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
+DECL|do_ipv4_lookup|function|static void do_ipv4_lookup(struct k_work *work)
+DECL|do_ipv6_lookup|function|static void do_ipv6_lookup(struct k_work *work)
+DECL|ipv4_addr_add_handler|function|static void ipv4_addr_add_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
+DECL|ipv4_timer|variable|ipv4_timer
+DECL|ipv6_router_add_handler|function|static void ipv6_router_add_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
+DECL|ipv6_timer|variable|ipv6_timer
+DECL|main|function|void main(void)
+DECL|mgmt4_cb|variable|mgmt4_cb
+DECL|mgmt6_cb|variable|mgmt6_cb
+DECL|network_setup|function|static void network_setup(void)
+DECL|setup_dhcpv4|function|static void setup_dhcpv4(struct net_if *iface)
+DECL|setup_dhcpv4|macro|setup_dhcpv4
+DECL|setup_ipv4|function|static void setup_ipv4(struct net_if *iface)
+DECL|setup_ipv4|macro|setup_ipv4
+DECL|setup_ipv6|function|static void setup_ipv6(struct net_if *iface)
+DECL|setup_ipv6|macro|setup_ipv6
+DECL|thread_stack|variable|thread_stack
