c1ea0d937ca228343159be5bfd97168d44f38f90
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-03-20 10:49:03 +0100

    tests/net/ieee802154: Switch l2 to ztest framework
    Change-Id: Ibce93d4652e3991a06e870fc81e952788f0c6465
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: cca117d8b96c7f18538cd180470f9df44589971b

--- tests/net/ieee802154/l2/src/ieee802154_test.c
+++ tests/net/ieee802154/l2/src/ieee802154_test.c
@@ -1,3 +1,5 @@
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
 DECL|ack_pkt|variable|ack_pkt
 DECL|beacon_pkt|variable|beacon_pkt
 DECL|current_buf|variable|current_buf
@@ -8,19 +10,25 @@ DECL|fc_seq|member|struct ieee802154_fcf_seq *fc_seq;
 DECL|ieee802154_pkt_test|struct|struct ieee802154_pkt_test {
 DECL|ieee_addr_hexdump|function|static void ieee_addr_hexdump(uint8_t *addr, uint8_t length)
 DECL|iface|variable|iface
-DECL|initialize_test_environment|function|static inline int initialize_test_environment(void)
+DECL|init_test|function|static void init_test(void)
+DECL|initialize_test_environment|function|static bool initialize_test_environment(void)
 DECL|length|member|uint8_t length;
-DECL|main|function|void main(void)
 DECL|mhr_check|member|} mhr_check;
 DECL|name|member|char *name;
 DECL|ns_pkt|variable|ns_pkt
+DECL|parsing_ack_pkt|function|static void parsing_ack_pkt(void)
+DECL|parsing_beacon_pkt|function|static void parsing_beacon_pkt(void)
+DECL|parsing_ns_pkt|function|static void parsing_ns_pkt(void)
 DECL|pkt_hexdump|function|static void pkt_hexdump(uint8_t *pkt, uint8_t length)
 DECL|pkt|member|uint8_t *pkt;
+DECL|replying_ack_pkt|function|static void replying_ack_pkt(void)
+DECL|sending_ns_pkt|function|static void sending_ns_pkt(void)
 DECL|src_addr|member|struct ieee802154_address_field *src_addr;
 DECL|src|member|struct in6_addr src;
 DECL|test_ack_pkt|variable|test_ack_pkt
-DECL|test_ack_reply|function|static inline int test_ack_reply(struct ieee802154_pkt_test *t)
+DECL|test_ack_reply|function|static bool test_ack_reply(struct ieee802154_pkt_test *t)
 DECL|test_beacon_pkt|variable|test_beacon_pkt
+DECL|test_main|function|void test_main(void)
 DECL|test_ns_pkt|variable|test_ns_pkt
-DECL|test_ns_sending|function|static inline int test_ns_sending(struct ieee802154_pkt_test *t)
-DECL|test_packet_parsing|function|static inline int test_packet_parsing(struct ieee802154_pkt_test *t)
+DECL|test_ns_sending|function|static bool test_ns_sending(struct ieee802154_pkt_test *t)
+DECL|test_packet_parsing|function|static bool test_packet_parsing(struct ieee802154_pkt_test *t)
