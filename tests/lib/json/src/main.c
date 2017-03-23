c0bbc7eb5c2d62a18466c35c626fd4ac8167488d
Leandro Pereira <leandro.pereira@intel.com>
2017-03-23 16:37:22 -0700

    test: Add test for JSON library
    This adds a test suite for the JSON library, testing both encoding and
    decoding of all supported data types, including arrays, nested objects,
    and basic types such as booleans, numbers, and strings.
    
    Jira: ZEP-1607
    Change-Id: I4f6ad7e2859a142e06d405e0760e91751e01a31f
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: a50c1d062028197cb5f1c75f1306ace46bf7cd78

---
+++ tests/lib/json/src/main.c
@@ -0,0 +1,23 @@
+DECL|FIELD|macro|FIELD
+DECL|FIELD|macro|FIELD
+DECL|nested_bool|member|bool nested_bool;
+DECL|nested_descr|variable|nested_descr
+DECL|nested_int|member|int nested_int;
+DECL|nested_string|member|const char *nested_string;
+DECL|some_array_len|member|size_t some_array_len;
+DECL|some_array|member|int some_array[16];
+DECL|some_bool|member|bool some_bool;
+DECL|some_int|member|int some_int;
+DECL|some_nested_struct|member|struct test_nested some_nested_struct;
+DECL|some_string|member|const char *some_string;
+DECL|test_descr|variable|test_descr
+DECL|test_json_decoding|function|static void test_json_decoding(void)
+DECL|test_json_encoding|function|static void test_json_encoding(void)
+DECL|test_json_invalid_unicode|function|static void test_json_invalid_unicode(void)
+DECL|test_json_item_wrong_type|function|static void test_json_item_wrong_type(void)
+DECL|test_json_key_not_in_descr|function|static void test_json_key_not_in_descr(void)
+DECL|test_json_missing_quote|function|static void test_json_missing_quote(void)
+DECL|test_json_wrong_token|function|static void test_json_wrong_token(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_nested|struct|struct test_nested {
+DECL|test_struct|struct|struct test_struct {
