0a1ee90e7ecbfbc99e69634efaa3170c3718a988
Anas Nashif <anas.nashif@intel.com>
2017-03-23 08:53:24 -0400

    benchmarks: move footprint benchmark out of legacy
    Those can be used with unified kernel.
    
    Change-Id: I4bd024f7e645b99ac1b8df6dfbbdd94f2fda99c4
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 477a9281346e0496796799dc9a70949cd7757861

--- tests/benchmarks/footprint/src/main.c
+++ tests/benchmarks/footprint/src/main.c
@@ -3,8 +3,8 @@ DECL|IRQ_PRIORITY|macro|IRQ_PRIORITY
 DECL|MESSAGE|macro|MESSAGE
 DECL|MESSAGE|macro|MESSAGE
 DECL|TEST_SOFT_INT|macro|TEST_SOFT_INT
-DECL|dummyIsr|function|void dummyIsr(void *unused)
-DECL|fgTaskEntry|function|void fgTaskEntry(void)
+DECL|dummy_isr|function|void dummy_isr(void *unused)
 DECL|func_array|variable|func_array
 DECL|i|variable|i
-DECL|pfunc|typedef|typedef void* (*pfunc) (void*);
+DECL|main|function|void main(void)
+DECL|pfunc|typedef|typedef void* (*pfunc) (void *);
