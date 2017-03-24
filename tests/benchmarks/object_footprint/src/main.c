66aa305633be72834917c1073e3059a7b8a5ee40
Anas Nashif <anas.nashif@intel.com>
2017-03-24 17:57:11 -0400

    tests: object_footprint:  add missing file
    Change-Id: I2d26f0770ac81482d70a8101c22745b2baef64ae
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 898c8b639ba57f474c737a5c00398439552e7d1b

---
+++ tests/benchmarks/object_footprint/src/main.c
@@ -0,0 +1,13 @@
+DECL|IRQ_LINE|macro|IRQ_LINE
+DECL|IRQ_PRIORITY|macro|IRQ_PRIORITY
+DECL|MESSAGE|macro|MESSAGE
+DECL|TEST_IRQ_OFFLOAD_VECTOR|macro|TEST_IRQ_OFFLOAD_VECTOR
+DECL|TEST_SOFT_INT|macro|TEST_SOFT_INT
+DECL|THREAD_STACK_SIZE|macro|THREAD_STACK_SIZE
+DECL|dummy_isr|function|void dummy_isr(void *unused)
+DECL|func_array|variable|func_array
+DECL|i|variable|i
+DECL|main|function|void main(void)
+DECL|pStack|variable|pStack
+DECL|pfunc|typedef|typedef void * (*pfunc) (void *);
+DECL|thread_entry|function|static void thread_entry(void *arg1, void *arg2, void *arg3)
