fb28c582d32be212040452a184321db24015182d
Anas Nashif <anas.nashif@intel.com>
2017-03-25 07:54:49 -0400

    samples: remove legacy applications
    Change-Id: I790d66f2ab14b40dd6c6fe25c59ccd97850602e7
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: fdd0cb70ede96eaeafaebce23264bf10e60c4e7a

--- samples/legacy/task_profiler/microkernel/src/hello.c
+++
@@ -1,8 +0,0 @@
-DECL|MYFIBER_STACK_SIZE|macro|MYFIBER_STACK_SIZE
-DECL|SLEEPTICKS|macro|SLEEPTICKS
-DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|helloLoop|function|void helloLoop(const char *taskname, ksem_t mySem, ksem_t otherSem)
-DECL|myfiber_stack|variable|myfiber_stack
-DECL|myfiber|function|void myfiber(int param1, int param2)
-DECL|taskA|function|void taskA(void)
-DECL|taskB|function|void taskB(void)
