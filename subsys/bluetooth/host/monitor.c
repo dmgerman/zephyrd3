4b20476956d5fb0f957a8bc2d6f25d9382d4791a
Johan Hedberg <johan.hedberg@intel.com>
2017-03-28 12:59:22 +0300

    Bluetooth: monitor: Drop data instead of holding IRQ lock for long
    The IRQ lock should never be held for long periods of time. Instead of
    using the IRQ lock to prevent monitor protocol corruption simply drop
    the data. Follow-up patches will add additional drop count tracking so
    that these get reported properly over the protocol.
    
    Change-Id: If498125b29f1b58bed676c78ad2062e2aa206318
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 19d740c162b319298882a006ca50019d15843166

--- subsys/bluetooth/host/monitor.c
+++ subsys/bluetooth/host/monitor.c
@@ -1,9 +1,11 @@
+DECL|BT_LOG_BUSY|enumerator|BT_LOG_BUSY,
 DECL|MONITOR_INIT_PRIORITY|macro|MONITOR_INIT_PRIORITY
 DECL|bt_log|function|void bt_log(int prio, const char *fmt, ...)
 DECL|bt_monitor_init|function|static int bt_monitor_init(struct device *d)
 DECL|bt_monitor_new_index|function|void bt_monitor_new_index(uint8_t type, uint8_t bus, bt_addr_t *addr, const char *name)
 DECL|bt_monitor_send|function|void bt_monitor_send(uint16_t opcode, const void *data, size_t len)
 DECL|encode_hdr|function|static inline void encode_hdr(struct bt_monitor_hdr *hdr, uint16_t opcode, uint16_t len)
+DECL|flags|variable|flags
 DECL|log_out|function|static int log_out(int c, void *unused)
 DECL|monitor_console_out|function|static int monitor_console_out(int c)
 DECL|monitor_dev|variable|monitor_dev
