4cbba6158d4bf0643f7613cc1a43a3cb6e35785f
Johan Hedberg <johan.hedberg@intel.com>
2017-03-28 13:58:05 +0300

    Bluetooth: monitor: Add support for logging packet drops
    The monitor protocol provides support for logging packet drops with
    the help of the extended monitor header. Implement support for this.
    
    Change-Id: I7ef7894816cb8d1bd876842d0253ef0980471e69
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 46e649c536c1987a82c2d67928f4ead4c8c9507b

--- subsys/bluetooth/host/monitor.c
+++ subsys/bluetooth/host/monitor.c
@@ -1,12 +1,22 @@
 DECL|BT_LOG_BUSY|enumerator|BT_LOG_BUSY,
 DECL|MONITOR_INIT_PRIORITY|macro|MONITOR_INIT_PRIORITY
+DECL|acl_rx|member|atomic_t acl_rx;
+DECL|acl_tx|member|atomic_t acl_tx;
 DECL|bt_log|function|void bt_log(int prio, const char *fmt, ...)
 DECL|bt_monitor_init|function|static int bt_monitor_init(struct device *d)
 DECL|bt_monitor_new_index|function|void bt_monitor_new_index(uint8_t type, uint8_t bus, bt_addr_t *addr, const char *name)
 DECL|bt_monitor_send|function|void bt_monitor_send(uint16_t opcode, const void *data, size_t len)
+DECL|cmd|member|atomic_t cmd;
+DECL|drop_add|function|static void drop_add(uint16_t opcode)
+DECL|drops|variable|drops
+DECL|encode_drops|function|static void encode_drops(struct bt_monitor_hdr *hdr, uint8_t type, atomic_t *val)
 DECL|encode_hdr|function|static inline void encode_hdr(struct bt_monitor_hdr *hdr, uint16_t opcode, uint16_t len)
+DECL|evt|member|atomic_t evt;
 DECL|flags|variable|flags
 DECL|log_out|function|static int log_out(int c, void *unused)
 DECL|monitor_console_out|function|static int monitor_console_out(int c)
 DECL|monitor_dev|variable|monitor_dev
 DECL|monitor_send|function|static void monitor_send(const void *data, size_t len)
+DECL|other|member|atomic_t other;
+DECL|sco_rx|member|atomic_t sco_rx;
+DECL|sco_tx|member|atomic_t sco_tx;
--- subsys/bluetooth/host/monitor.h
+++ subsys/bluetooth/host/monitor.h
@@ -2,12 +2,15 @@ DECL|BT_MONITOR_ACL_RX_DROPS|macro|BT_MONITOR_ACL_RX_DROPS
 DECL|BT_MONITOR_ACL_RX_PKT|macro|BT_MONITOR_ACL_RX_PKT
 DECL|BT_MONITOR_ACL_TX_DROPS|macro|BT_MONITOR_ACL_TX_DROPS
 DECL|BT_MONITOR_ACL_TX_PKT|macro|BT_MONITOR_ACL_TX_PKT
+DECL|BT_MONITOR_BASE_HDR_LEN|macro|BT_MONITOR_BASE_HDR_LEN
 DECL|BT_MONITOR_CLOSE_INDEX|macro|BT_MONITOR_CLOSE_INDEX
 DECL|BT_MONITOR_COMMAND_DROPS|macro|BT_MONITOR_COMMAND_DROPS
 DECL|BT_MONITOR_COMMAND_PKT|macro|BT_MONITOR_COMMAND_PKT
 DECL|BT_MONITOR_DEL_INDEX|macro|BT_MONITOR_DEL_INDEX
 DECL|BT_MONITOR_EVENT_DROPS|macro|BT_MONITOR_EVENT_DROPS
 DECL|BT_MONITOR_EVENT_PKT|macro|BT_MONITOR_EVENT_PKT
+DECL|BT_MONITOR_EXT_HDR_MAX|macro|BT_MONITOR_EXT_HDR_MAX
+DECL|BT_MONITOR_EXT_HDR_MAX|macro|BT_MONITOR_EXT_HDR_MAX
 DECL|BT_MONITOR_INDEX_INFO|macro|BT_MONITOR_INDEX_INFO
 DECL|BT_MONITOR_NEW_INDEX|macro|BT_MONITOR_NEW_INDEX
 DECL|BT_MONITOR_NOP|macro|BT_MONITOR_NOP
@@ -26,15 +29,18 @@ DECL|BT_MONITOR_VENDOR_DIAG|macro|BT_MONITOR_VENDOR_DIAG
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
 DECL|bdaddr|member|uint8_t bdaddr[6];
 DECL|bt_monitor_hdr|struct|struct bt_monitor_hdr {
 DECL|bt_monitor_new_index|macro|bt_monitor_new_index
 DECL|bt_monitor_new_index|struct|struct bt_monitor_new_index {
 DECL|bt_monitor_opcode|function|static inline uint8_t bt_monitor_opcode(struct net_buf *buf)
 DECL|bt_monitor_send|macro|bt_monitor_send
+DECL|bt_monitor_ts32|struct|struct bt_monitor_ts32 {
 DECL|bt_monitor_user_logging|struct|struct bt_monitor_user_logging {
 DECL|bus|member|uint8_t bus;
 DECL|data_len|member|uint16_t data_len;
+DECL|ext|member|uint8_t ext[BT_MONITOR_EXT_HDR_MAX];
 DECL|flags|member|uint8_t flags;
 DECL|hdr_len|member|uint8_t hdr_len;
 DECL|ident_len|member|uint8_t ident_len;
