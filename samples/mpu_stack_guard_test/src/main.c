dfcf53a5dc48edd7cfd3c9d3e199cf85d7750683
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2017-03-29 13:05:44 +0100

    samples: Add mpu stack guard test
    This patch adds the mpu_stack_guard_test to the available samples.
    The purpose of this test is to exercize the thread stack guard enabled
    via MPU on arm platforms.
    
    Change-Id: I4665a20956d9e6d0dd4b5cc862e82040a53afafc
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>
    
    Former-commit-id: 14a892f7a4ca85ffcf440e88c915a0720511f02f

---
+++ samples/mpu_stack_guard_test/src/main.c
@@ -0,0 +1,11 @@
+DECL|PRIORITY|macro|PRIORITY
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|STACK_GUARD_CANARY|macro|STACK_GUARD_CANARY
+DECL|buf|variable|buf
+DECL|canary|member|volatile u32_t canary;
+DECL|main|function|void main(void)
+DECL|recursive_loop|function|u32_t recursive_loop(u32_t counter)
+DECL|stack_guard_buffer|struct|struct stack_guard_buffer {
+DECL|stack_guard_thread|function|void stack_guard_thread(void *dummy1, void *dummy2, void *dummy3)
+DECL|stack|member|u8_t stack[STACKSIZE];
