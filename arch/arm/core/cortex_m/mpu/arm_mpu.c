06082b01d2ed3f73791f3fe1d2e3b566600e77ea
Vincenzo Frascino <vincenzo.frascino@linaro.org>
2017-03-29 11:26:08 +0100

    arm: core: mpu: Add core support to ARM MPU
    This patch add arm core MPU support to ARM MPU driver.
    
    Change-Id: I5a61da4615ae687bf42f1c9947e291ebfd2d2c1d
    Signed-off-by: Vincenzo Frascino <vincenzo.frascino@linaro.org>
    
    Former-commit-id: b94c5b16fe7e1dc6ac9eff1b0db56cc9fe7b86ab

--- arch/arm/core/cortex_m/mpu/arm_mpu.c
+++ arch/arm/core/cortex_m/mpu/arm_mpu.c
@@ -1,5 +1,11 @@
 DECL|ARM_MPU_DEV|macro|ARM_MPU_DEV
 DECL|_arm_mpu_config|function|static void _arm_mpu_config(void)
 DECL|_get_num_regions|function|static inline u8_t _get_num_regions(void)
+DECL|_get_region_attr_by_type|function|static inline u32_t _get_region_attr_by_type(u32_t type, u32_t size)
+DECL|_get_region_attr|function|static inline u32_t _get_region_attr(u32_t xn, u32_t ap, u32_t tex, u32_t c, u32_t b, u32_t s, u32_t srd, u32_t size)
 DECL|_region_init|function|static void _region_init(u32_t index, u32_t region_addr, u32_t region_attr)
+DECL|arm_core_mpu_configure|function|void arm_core_mpu_configure(u8_t type, u32_t base, u32_t size)
+DECL|arm_core_mpu_disable|function|void arm_core_mpu_disable(void)
+DECL|arm_core_mpu_enable|function|void arm_core_mpu_enable(void)
+DECL|arm_mpu_enabled|variable|arm_mpu_enabled
 DECL|arm_mpu_init|function|static int arm_mpu_init(struct device *arg)
