99329cdc4fd56d07fe773baf60adb20ebbe05f8a
Anas Nashif <anas.nashif@intel.com>
2017-03-29 15:36:49 -0400

    samples: ipm sample using mailbox for Quark SE
    re-add sample that was dropped by mistake and add a top level Makefile
    for building and flashing two cores.
    
    Change-Id: Ie22ac1efa7b5373999997489a2c866de19553128
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 8c1a073bb1cc6c77c09a266ba04501698768bdad

---
+++ samples/subsys/ipc/ipm_mailbox/sensor/src/hello.c
@@ -0,0 +1,8 @@
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|counters|variable|counters
+DECL|dat1|variable|dat1
+DECL|dat2|variable|dat2
+DECL|main|function|void main(void)
+DECL|message_ipm_callback|function|void message_ipm_callback(void *context, uint32_t id, volatile void *data)
+DECL|ping_ipm_callback|function|void ping_ipm_callback(void *context, uint32_t id, volatile void *data)
