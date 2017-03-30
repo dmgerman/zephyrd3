6469fa81120d662825b1b89a48c1f0afa6c8ef21
Wu Jiequan <jiequanx.wu@intel.com>
2017-03-30 19:46:45 +0800

    tests: net: Add mqtt testcases
    Add two testcases for mqtt_publisher and mqtt_subscriber.
    The combination of the two, covering most of the MQTT APIs.
    
    Change-Id: Ib663ee1f550362ca493501046ddf8508a25ac956
    Signed-off-by: Wu Jiequan <jiequanx.wu@intel.com>
    
    Former-commit-id: 0e3d5826232fec1f46de099ec986c2563c3ec010

---
+++ tests/net/mqtt_subscriber/src/test_mqtt_subscribe.c
@@ -0,0 +1,32 @@
+DECL|PRINT_RESULT|macro|PRINT_RESULT
+DECL|RC_STR|macro|RC_STR
+DECL|client_ctx|variable|client_ctx
+DECL|connect_cb|function|static void connect_cb(struct mqtt_ctx *mqtt_ctx)
+DECL|connect_data|member|void *connect_data;
+DECL|connect_msg|member|struct mqtt_connect_msg connect_msg;
+DECL|disconnect_cb|function|static void disconnect_cb(struct mqtt_ctx *mqtt_ctx)
+DECL|disconnect_data|member|void *disconnect_data;
+DECL|get_mqtt_topic|function|static char *get_mqtt_topic(void)
+DECL|init_network|function|static int init_network(void)
+DECL|malformed_cb|function|static void malformed_cb(struct mqtt_ctx *mqtt_ctx, uint16_t pkt_type)
+DECL|mqtt_client_ctx|struct|struct mqtt_client_ctx {
+DECL|mqtt_ctx|member|struct mqtt_ctx mqtt_ctx;
+DECL|net_ctx|variable|net_ctx
+DECL|network_setup|function|static int network_setup(struct net_context **net_ctx, const char *local_addr,const char *server_addr, uint16_t server_port)
+DECL|pub_msg|member|struct mqtt_publish_msg pub_msg;
+DECL|publish_rx_cb|function|static int publish_rx_cb(struct mqtt_ctx *mqtt_ctx, struct mqtt_publish_msg*msg, uint16_t pkt_id, enum mqtt_packet type)
+DECL|set_addr|function|static int set_addr(struct sockaddr *sock_addr, const char *addr, uint16_t port)
+DECL|subscribe_data|member|void *subscribe_data;
+DECL|subscriber_cb|function|static int subscriber_cb(struct mqtt_ctx *mqtt_ctx, uint16_t pkt_id,uint8_t items, enum mqtt_qos qos[])
+DECL|test_connect|function|static int test_connect(void)
+DECL|test_disconnect|function|static int test_disconnect(void)
+DECL|test_mqtt_connect|function|void test_mqtt_connect(void)
+DECL|test_mqtt_disconnect|function|void test_mqtt_disconnect(void)
+DECL|test_mqtt_init|function|void test_mqtt_init(void)
+DECL|test_mqtt_subscribe|function|void test_mqtt_subscribe(void)
+DECL|test_mqtt_unsubscribe|function|void test_mqtt_unsubscribe(void)
+DECL|test_subscribe|function|static int test_subscribe(void)
+DECL|test_unsubscribe|function|static int test_unsubscribe(void)
+DECL|try_to_connect|function|static int try_to_connect(struct mqtt_client_ctx *client_ctx)
+DECL|unsubscribe_cb|function|static int unsubscribe_cb(struct mqtt_ctx *mqtt_ctx, uint16_t pkt_id)
+DECL|unsubscribe_data|member|void *unsubscribe_data;
