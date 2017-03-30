4641d4d0e364e7e3d65a724ea9d826d709b2b4e6
Patrik Flykt <patrik.flykt@intel.com>
2017-03-30 12:54:29 +0300

    crc16: Create function for computing CRC 16
    Add the simplest possible CRC 16 function that computes the CRC value
    without the help of lookup tables.
    
    Change-Id: I9163389adaa4a70c4e8ce8ce6d5f0661f40c7871
    Signed-off-by: Patrik Flykt <patrik.flykt@intel.com>
    
    Former-commit-id: bd75219ba00ca0f41be773c3dbbf5fa0bff0d908

---
+++ drivers/crc/crc16_sw.c
@@ -0,0 +1,1 @@
+DECL|crc16|function|uint16_t crc16(const uint8_t *src, size_t len, uint16_t polynomial, uint16_t initial_value)
---
+++ include/crc16.h
@@ -0,0 +1,3 @@
+DECL|__CRC16_H|macro|__CRC16_H
+DECL|crc16_ansi|function|static inline uint16_t crc16_ansi(const uint8_t *src, size_t len)
+DECL|crc16_ccitt|function|static inline uint16_t crc16_ccitt(const uint8_t *src, size_t len)
