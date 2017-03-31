6a22530621b85fd477b3d22a7a1c305adeb4b898
Gustavo Denardin <gustavo.denardin@gmail.com>
2017-03-31 20:07:45 -0300

    arm: Support for MKL25Z soc
    In order to suppport the FRDM-KL25Z board,
    it is necessary to make this soc available.
    
    Change-Id: Id93a51dcc9ef58118e27db02c30f662eb73d5adb
    Signed-off-by: Gustavo Denardin <gustavo.denardin@gmail.com>
    
    Former-commit-id: 32e15637afd4fcc327467b54d1081b4a66429fa9

---
+++ arch/arm/soc/nxp_kinetis/kl2x/soc.c
@@ -0,0 +1,3 @@
+DECL|__kinetis_flash_config|variable|__kinetis_flash_config
+DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
+DECL|kl2x_init|function|static int kl2x_init(struct device *arg)
