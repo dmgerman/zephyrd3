b7ddb3af0093911cadd440b0df70f799839b9394
Johan Hedberg <johan.hedberg@intel.com>
2017-04-04 13:01:22 +0300

    drivers: display: Enhance BBC micro:bit string output capabilities
    Add a new mb_display_print() API which takes printf-style parameters
    and outputs scrolling text instead of one character at a time. The
    existing mb_display_str() API is renamed to mb_display_string() for
    consistency, and now also takes printf-style parameters.
    
    Change-Id: I59c42bcd74c62f05ecb6d097dc808b9e5c1984c5
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7e5df0cc02b33e7116b1208ef63a4f8a28beeeed

--- drivers/display/mb_display.c
+++ drivers/display/mb_display.c
@@ -2,27 +2,37 @@ DECL|DISPLAY_COLS|macro|DISPLAY_COLS
 DECL|DISPLAY_ROWS|macro|DISPLAY_ROWS
 DECL|GET_PIXEL|macro|GET_PIXEL
 DECL|ROW_PIN|macro|ROW_PIN
+DECL|SCROLL_DURATION|macro|SCROLL_DURATION
+DECL|SCROLL_OFF|macro|SCROLL_OFF
+DECL|SCROLL_START|macro|SCROLL_START
 DECL|clear_display|function|static void clear_display(struct k_timer *timer)
 DECL|col_mask|variable|col_mask
 DECL|cur|member|int cur; /* Currently rendered row */
 DECL|dev|member|struct device *dev; /* GPIO device */
 DECL|display|variable|display
 DECL|duration|member|int32_t duration; /* Duration for each shown image */
-DECL|expiry|member|int64_t expiry; /* When to stop showing the current image */
+DECL|expiry|member|int64_t expiry; /* When to stop showing current image */
+DECL|get_font|function|static inline const struct mb_image *get_font(char ch)
+DECL|img_copy|function|static inline void img_copy(struct mb_image *dst, const struct mb_image *src)
+DECL|img|member|struct mb_image img[2]; /* Current and next image */
 DECL|map|variable|map
 DECL|mb_display_char|function|void mb_display_char(struct mb_display *disp, char chr, int32_t duration)
 DECL|mb_display_get|function|struct mb_display *mb_display_get(void)
 DECL|mb_display_image|function|void mb_display_image(struct mb_display *disp, const struct mb_image *img, int32_t duration)
 DECL|mb_display_init|function|static int mb_display_init(struct device *dev)
+DECL|mb_display_print|function|void mb_display_print(struct mb_display *disp, const char *fmt, ...)
 DECL|mb_display_stop|function|void mb_display_stop(struct mb_display *disp)
-DECL|mb_display_str|function|void mb_display_str(struct mb_display *disp, const char *str, int32_t duration)
+DECL|mb_display_string|function|void mb_display_string(struct mb_display *disp, int32_t duration, const char *fmt, ...)
 DECL|mb_display|struct|struct mb_display {
 DECL|row|member|uint32_t row[3]; /* Content (columns) for each row */
-DECL|set_img|function|static void set_img(struct mb_display *disp, const struct mb_image *img)
+DECL|scroll|member|uint16_t scroll; /* Scroll shift */
 DECL|show_row|function|static void show_row(struct k_timer *timer)
+DECL|start_image|function|static void start_image(struct mb_display *disp, const struct mb_image *img)
+DECL|str_buf|member|char str_buf[CONFIG_MICROBIT_DISPLAY_STR_MAX];
 DECL|str|member|const char *str; /* String to be shown */
 DECL|timer|member|struct k_timer timer; /* Rendering timer */
 DECL|update_pins|function|static inline void update_pins(struct mb_display *disp, uint32_t val)
+DECL|update_scroll|function|static void update_scroll(struct mb_display *disp)
 DECL|x_y|struct|struct x_y {
 DECL|x|member|uint8_t x:4,
 DECL|y|member|y:4;
