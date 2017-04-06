0d9ee9cbc5d130cef4c2d717222320986554d5fc
Ramesh Thomas <ramesh.thomas@intel.com>
2017-04-06 02:45:10 -0700

    timer: tickless: cortex_m: Add tickless kernel support
    Add tickless kernel support. Sets timer always in one
    shot mode to the time the kernel scheduler needs the
    next timer event. Uses mili seconds as the scheduling
    time unit.
    
    Jira: ZEP-1818
    Change-Id: I21ce037b571c4c6ff588033a15aa49624cba7a57
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 0beffbe6bef1d99b3bd93fb9e7c893874cd8d562

--- drivers/timer/cortex_m_systick.c
+++ drivers/timer/cortex_m_systick.c
@@ -2,6 +2,12 @@ DECL|IDLE_NOT_TICKLESS|macro|IDLE_NOT_TICKLESS
 DECL|IDLE_TICKLESS|macro|IDLE_TICKLESS
 DECL|TIMER_MODE_ONE_SHOT|macro|TIMER_MODE_ONE_SHOT
 DECL|TIMER_MODE_PERIODIC|macro|TIMER_MODE_PERIODIC
+DECL|_enable_sys_clock|function|void _enable_sys_clock(void)
+DECL|_get_elapsed_clock_time|function|u64_t _get_elapsed_clock_time(void)
+DECL|_get_elapsed_program_time|function|u32_t _get_elapsed_program_time(void)
+DECL|_get_program_time|function|u32_t _get_program_time(void)
+DECL|_get_remaining_program_time|function|u32_t _get_remaining_program_time(void)
+DECL|_set_time|function|void _set_time(u32_t time)
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
 DECL|_timer_cycle_get_32|function|u32_t _timer_cycle_get_32(void)
 DECL|_timer_idle_enter|function|void _timer_idle_enter(s32_t ticks /* system ticks */)
@@ -9,6 +15,7 @@ DECL|_timer_idle_exit|function|void _timer_idle_exit(void)
 DECL|_timer_int_handler|function|void _timer_int_handler(void *unused)
 DECL|clock_accumulated_count|variable|clock_accumulated_count
 DECL|default_load_value|variable|default_load_value
+DECL|get_elapsed_count|function|static inline u64_t get_elapsed_count(void)
 DECL|idle_mode|variable|idle_mode
 DECL|idle_original_count|variable|idle_original_count
 DECL|idle_original_ticks|variable|idle_original_ticks
@@ -21,5 +28,6 @@ DECL|sysTickStart|function|static ALWAYS_INLINE void sysTickStart(void)
 DECL|sysTickStop|function|static ALWAYS_INLINE void sysTickStop(void)
 DECL|sysTickTicklessIdleInit|function|static void sysTickTicklessIdleInit(void)
 DECL|sys_clock_disable|function|void sys_clock_disable(void)
+DECL|sys_tick_reload|function|static ALWAYS_INLINE void sys_tick_reload(void)
 DECL|timer_idle_skew|variable|timer_idle_skew
 DECL|timer_mode|variable|timer_mode
