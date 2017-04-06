99fe0369cccb2f28c5ab0f3629e5b439bbb59c78
Ramesh Thomas <ramesh.thomas@intel.com>
2017-04-06 15:30:27 -0700

    kernel: tickless: Rename _Swap to allow creation of macro
    Future tickless kernel patches would be inserting some
    code before call to Swap. To enable this it will create
    a mcro named as the current _Swap which would call first
    the tickless kernel code and then call the real __swap()
    
    Jira: ZEP-339
    Change-Id: Id778bfcee4f88982c958fcf22d7f04deb4bd572f
    Signed-off-by: Ramesh Thomas <ramesh.thomas@intel.com>
    
    Former-commit-id: 62eea121b357eae64fe00d996e2193d2b4638d1e

--- kernel/include/nano_internal.h
+++ kernel/include/nano_internal.h
@@ -1,5 +1,6 @@
 DECL|K_NUM_PRIORITIES|macro|K_NUM_PRIORITIES
 DECL|K_NUM_PRIO_BITMAPS|macro|K_NUM_PRIO_BITMAPS
 DECL|_NANO_INTERNAL__H_|macro|_NANO_INTERNAL__H_
+DECL|_Swap|macro|_Swap
 DECL|_data_copy|function|static inline void _data_copy(void)
 DECL|_thread_monitor_exit|macro|_thread_monitor_exit
