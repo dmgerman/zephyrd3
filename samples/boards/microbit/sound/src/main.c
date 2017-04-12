ac36fadf97d5a59605fe97c5a463a11c7b3403d0
Johan Hedberg <johan.hedberg@intel.com>
2017-04-12 11:44:38 +0300

    samples/boards/microbit: Add sound sample
    Add a sample demonstrating sound output using PWM to a piezo buzzer
    connected to the external edge connector port P0.
    
    Change-Id: I8c0902fd935f2eb3b9979476a7540e7a0216ca63
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 191cb2d0fc9ff9e5da393b75b9b2a4bda8da5ad9

---
+++ samples/boards/microbit/sound/src/main.c
@@ -0,0 +1,14 @@
+DECL|BEEP_DURATION|macro|BEEP_DURATION
+DECL|BUZZER_PIN|macro|BUZZER_PIN
+DECL|PERIOD_INIT|macro|PERIOD_INIT
+DECL|PERIOD_MAX|macro|PERIOD_MAX
+DECL|PERIOD_MIN|macro|PERIOD_MIN
+DECL|US_TO_HZ|macro|US_TO_HZ
+DECL|beep_active|variable|beep_active
+DECL|beep_work|variable|beep_work
+DECL|beep|function|static void beep(struct k_work *work)
+DECL|button_pressed|function|static void button_pressed(struct device *dev, struct gpio_callback *cb, uint32_t pins)
+DECL|gpio|variable|gpio
+DECL|main|function|void main(void)
+DECL|period|variable|period
+DECL|pwm|variable|pwm
