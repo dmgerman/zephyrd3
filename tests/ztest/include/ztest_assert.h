2ecac7fc5142572833c56587f9667b96a16945a0
Kumar Gala <kumar.gala@linaro.org>
2017-04-13 11:25:39 -0500

    tests/ztest: rename assert macros to be zephyr specific
    ztest has a number of assert style macros and used a baseline assert()
    that varies from the system definition of assert() so lets rename
    everything as zassert to be clear.
    
    Change-Id: I7f176b3bae94d1045054d665be8b5bda947e5bb0
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: c7bc90991470e2eb0b9b446827aef97c32673469

--- tests/ztest/include/ztest_assert.h
+++ tests/ztest/include/ztest_assert.h
@@ -1,14 +1,13 @@
 DECL|__ZTEST_ASSERT_H__|macro|__ZTEST_ASSERT_H__
-DECL|_assert_|function|static inline void _assert_(int cond, const char *file, int line)
-DECL|_assert|function|static inline void _assert(int cond, const char *msg, const char *default_msg, const char *file, int line, const char *func)
-DECL|_assert|macro|_assert
-DECL|assert_equal_ptr|macro|assert_equal_ptr
-DECL|assert_equal|macro|assert_equal
-DECL|assert_false|macro|assert_false
-DECL|assert_is_null|macro|assert_is_null
-DECL|assert_not_equal|macro|assert_not_equal
-DECL|assert_not_null|macro|assert_not_null
-DECL|assert_true|macro|assert_true
-DECL|assert_unreachable|macro|assert_unreachable
-DECL|assert|macro|assert
-DECL|assert|macro|assert
+DECL|_zassert_|function|static inline void _zassert_(int cond, const char *file, int line)
+DECL|_zassert|function|static inline void _zassert(int cond, const char *msg, const char *default_msg, const char *file, int line, const char *func)
+DECL|_zassert|macro|_zassert
+DECL|zassert_equal_ptr|macro|zassert_equal_ptr
+DECL|zassert_equal|macro|zassert_equal
+DECL|zassert_false|macro|zassert_false
+DECL|zassert_is_null|macro|zassert_is_null
+DECL|zassert_not_equal|macro|zassert_not_equal
+DECL|zassert_not_null|macro|zassert_not_null
+DECL|zassert_true|macro|zassert_true
+DECL|zassert_unreachable|macro|zassert_unreachable
+DECL|zassert|macro|zassert
