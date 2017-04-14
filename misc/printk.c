384ca94d74940fca3913b671af32bf61c0b2976a
Leandro Pereira <leandro.pereira@intel.com>
2017-04-14 12:47:58 -0700

    misc: Support left-justification in printk()
    This is supported by printf(), and is an important formatting feature
    to print out some complex, nicely-formatted information.  This is
    accomplished by using a negative padding in the formatting string.
    
    The following code:
    
          printk("none:         |%u| |%x|\n", 12345, 12345);
          printk("zero_before:  |%08u| |%08x|\n", 12345, 12345);
          printk("space_before: |%8u| |%8x|\n", 12345, 12345);
          printk("space_after:  |%-8u| |%-8x|\n", 12345, 12345);
    
    Will produce the following output:
    
          none:         |12345| |3039|
          zero_before:  |00012345| |0000000000003039|
          space_before: |   12345| |            3039|
          space_after:  |12345   | |3039            |
    
    Change-Id: I9c2d85a1790087d53b52b7713854adaf99282f09
    Jira: ZEP-1599
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 99a7168b083873238f6c26c54ef98457eabbdf14

--- misc/printk.c
+++ misc/printk.c
@@ -1,9 +1,13 @@
+DECL|PAD_NONE|enumerator|PAD_NONE,
+DECL|PAD_SPACE_AFTER|enumerator|PAD_SPACE_AFTER,
+DECL|PAD_SPACE_BEFORE|enumerator|PAD_SPACE_BEFORE,
+DECL|PAD_ZERO_BEFORE|enumerator|PAD_ZERO_BEFORE,
 DECL|__printk_get_hook|function|void *__printk_get_hook(void)
 DECL|__printk_hook_install|function|void __printk_hook_install(int (*fn)(int))
 DECL|_char_out|variable|_char_out
 DECL|_nop_char_out|function|static int _nop_char_out(int c)
-DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(out_func_t out, void *ctx, const unsigned long num, int pad_zero, int min_width)
-DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(out_func_t out, void *ctx, const unsigned long num, int pad_zero, int min_width)
+DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(out_func_t out, void *ctx, const unsigned long num, enum pad_type padding, int min_width)
+DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(out_func_t out, void *ctx, const unsigned long num, enum pad_type padding, int min_width)
 DECL|_vprintk|function|void _vprintk(out_func_t out, void *ctx, const char *fmt, va_list ap)
 DECL|char_out|function|static int char_out(int c, struct out_context *ctx)
 DECL|count|member|int count;
@@ -11,6 +15,7 @@ DECL|count|member|int count;
 DECL|max|member|int max;
 DECL|out_context|struct|struct out_context {
 DECL|out_func_t|typedef|typedef int (*out_func_t)(int c, void *ctx);
+DECL|pad_type|enum|enum pad_type {
 DECL|printk|function|int printk(const char *fmt, ...)
 DECL|snprintk|function|int snprintk(char *str, size_t size, const char *fmt, ...)
 DECL|str_context|struct|struct str_context {
