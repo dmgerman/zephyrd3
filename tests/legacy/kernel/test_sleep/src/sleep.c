8c6c33e0a1e20f2212feb80432a04460c5aa6775
Youvedeep Singh <youvedeep.singh@intel.com>
2017-04-17 17:00:10 +0530

    test_sleep: replacing depreciated APIs with new one.
    As part of Zephyr Release 1.9 some APIs will be depreciated.
    This patch replaces APIs related to semophore init, kernel sleep,
    task spawn etc.
    
    Jira: ZEP-2009
    
    Change-Id: I1fe09e9592f503c3413d51857fd740703173c042
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: d35380087921e34d2e2f3b5fe8da413f2474469f

--- tests/legacy/kernel/test_sleep/src/sleep.c
+++ tests/legacy/kernel/test_sleep/src/sleep.c
@@ -1,20 +1,21 @@
-DECL|FIBER_STACKSIZE|macro|FIBER_STACKSIZE
-DECL|FIBER_STACKSIZE|macro|FIBER_STACKSIZE
-DECL|HELPER_FIBER_PRIORITY|macro|HELPER_FIBER_PRIORITY
+DECL|HELPER_THREAD_PRIORITY|macro|HELPER_THREAD_PRIORITY
 DECL|ONE_SECOND|macro|ONE_SECOND
-DECL|TEST_FIBER_PRIORITY|macro|TEST_FIBER_PRIORITY
+DECL|TEST_THREAD_PRIORITY|macro|TEST_THREAD_PRIORITY
+DECL|THREAD_STACK|macro|THREAD_STACK
+DECL|THREAD_STACK|macro|THREAD_STACK
+DECL|TICKS_PER_MS|macro|TICKS_PER_MS
 DECL|align_to_tick_boundary|function|static void align_to_tick_boundary(void)
-DECL|helper_fiber_id|variable|helper_fiber_id
-DECL|helper_fiber_sem|variable|helper_fiber_sem
-DECL|helper_fiber_stack|variable|helper_fiber_stack
-DECL|helper_fiber|function|static void helper_fiber(int arg1, int arg2)
+DECL|helper_thread_id|variable|helper_thread_id
+DECL|helper_thread_sem|variable|helper_thread_sem
+DECL|helper_thread_stack|variable|helper_thread_stack
+DECL|helper_thread|function|static void helper_thread(int arg1, int arg2)
 DECL|irq_offload_isr|function|static void irq_offload_isr(void *arg)
 DECL|main|function|void main(void)
 DECL|sleep_time_valid|function|static int sleep_time_valid(uint32_t start, uint32_t end, uint32_t dur)
 DECL|task_sem|variable|task_sem
 DECL|test_failure|variable|test_failure
-DECL|test_fiber_id|variable|test_fiber_id
-DECL|test_fiber_sem|variable|test_fiber_sem
-DECL|test_fiber_stack|variable|test_fiber_stack
-DECL|test_fiber|function|static void test_fiber(int arg1, int arg2)
 DECL|test_objects_init|function|static void test_objects_init(void)
+DECL|test_thread_id|variable|test_thread_id
+DECL|test_thread_sem|variable|test_thread_sem
+DECL|test_thread_stack|variable|test_thread_stack
+DECL|test_thread|function|static void test_thread(int arg1, int arg2)
