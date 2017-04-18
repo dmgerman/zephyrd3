d6b964412e81695ea858c98d1d23976ef669f6e6
Erwan Gouriou <erwan.gouriou@linaro.org>
2017-04-18 14:24:04 +0200

    drivers: clock control: Provide LL based clock control for stm32f4 series
    This commit enables STM32Cube LL based driver for stm32f4 series.
    This generic driver provides a unified API to clock driver for all
    stm32 series.
    LL API allows driver to be lightweight and to keep genericity across
    stm32 family to ease further devlopment and maintenance.
    
    Change-Id: Ie31ae8f433313787f9c9eda77de41925721d54dd
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    
    Former-commit-id: fda01e52c2f6f3f55671334abd352ced8a89a565

--- drivers/clock_control/stm32l4x_ll_clock.c
+++ drivers/clock_control/stm32l4x_ll_clock.c
@@ -1,5 +1,6 @@
 DECL|_pllm|macro|_pllm
 DECL|_pllr|macro|_pllr
+DECL|config_enable_default_clocks|function|void config_enable_default_clocks(void)
 DECL|config_pll_init|function|void config_pll_init(LL_UTILS_PLLInitTypeDef *pllinit)
 DECL|pllm|macro|pllm
 DECL|pllr|macro|pllr
