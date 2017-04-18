6d3618b81ee9bdac756a6ed0be5d1ad720cc284e
Anas Nashif <anas.nashif@intel.com>
2017-04-18 17:34:46 -0400

    kernel: rename nanoArchInit->kernel_arch_init
    Change-Id: I094665e583f506cc71185cb6b8630046b2d4b2f8
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 8df439b40b5e6fd03b6f6710b005d3f83a103e67

--- arch/arm/include/kernel_arch_func.h
+++ arch/arm/include/kernel_arch_func.h
@@ -2,4 +2,4 @@ DECL|_arch_switch_to_main_thread|function|_arch_switch_to_main_thread(char *main
 DECL|_is_in_isr|macro|_is_in_isr
 DECL|_kernel_arch_func__h_|macro|_kernel_arch_func__h_
 DECL|_set_thread_return_value|function|_set_thread_return_value(struct k_thread *thread, unsigned int value)
-DECL|nanoArchInit|function|static ALWAYS_INLINE void nanoArchInit(void)
+DECL|kernel_arch_init|function|static ALWAYS_INLINE void kernel_arch_init(void)
--- arch/nios2/include/kernel_arch_func.h
+++ arch/nios2/include/kernel_arch_func.h
@@ -4,4 +4,4 @@ DECL|_kernel_arch_func__h_|macro|_kernel_arch_func__h_
 DECL|_nios2_dcache_flush_all|macro|_nios2_dcache_flush_all
 DECL|_nios2_icache_flush_all|macro|_nios2_icache_flush_all
 DECL|_set_thread_return_value|function|_set_thread_return_value(struct k_thread *thread, unsigned int value)
-DECL|nanoArchInit|function|static ALWAYS_INLINE void nanoArchInit(void)
+DECL|kernel_arch_init|function|static ALWAYS_INLINE void kernel_arch_init(void)
