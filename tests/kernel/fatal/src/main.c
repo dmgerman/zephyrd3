7778ced06698bcd40181d911b620edb2815b3de5
Andrew Boie <andrew.p.boie@intel.com>
2017-04-19 15:43:05 -0700

    tests: add fatal test case
    We want to show that if a non-essential thread gets a fatal exception,
    that thread gets aborted but the rest of the system works properly.
    
    We also test that k_oops() does the same.
    
    Issue: ZEP-2052
    Change-Id: I0f88bcae865bf12bb91bb55e50e8ac9721672434
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: ca441162a79ac1c3cea16513f7f0d2171726be38

---
+++ tests/kernel/fatal/src/main.c
@@ -0,0 +1,7 @@
+DECL|PRIORITY|macro|PRIORITY
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|alt_stack|variable|alt_stack
+DECL|alt_thread1|function|void alt_thread1(void)
+DECL|alt_thread2|function|void alt_thread2(void)
+DECL|main|function|void main(void)
+DECL|rv|variable|rv
