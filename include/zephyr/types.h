616c5ecf21f7c08a7e67feb7636bf43152591715
Kumar Gala <kumar.gala@linaro.org>
2017-04-19 10:32:08 -0500

    Introduce new sized integer typedefs
    This is a start to move away from the C99 {u}int{8,16,32,64}_t types to
    Zephyr defined u{8,16,32,64}_t and s{8,16,32,64}_t.  This allows Zephyr
    to define the sized types in a consistent manor across all the
    architectures we support and not conflict with what various compilers
    and libc might do with regards to the C99 types.
    
    We introduce <zephyr/types.h> as part of this and have it include
    <stdint.h> for now until we transition all the code away from the C99
    types.
    
    We go with u{8,16,32,64}_t and s{8,16,32,64}_t as there are some
    existing variables defined u8 & u16 as well as to be consistent with
    Zephyr naming conventions.
    
    Jira: ZEP-2051
    
    Change-Id: I451fed0623b029d65866622e478225dfab2c0ca8
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 789081673f576957f25d77fdba8d40293403d53f

---
+++ include/zephyr/types.h
@@ -0,0 +1,9 @@
+DECL|__Z_TYPES_H__|macro|__Z_TYPES_H__
+DECL|s16_t|typedef|typedef signed short s16_t;
+DECL|s32_t|typedef|typedef signed int s32_t;
+DECL|s64_t|typedef|typedef signed long long s64_t;
+DECL|s8_t|typedef|typedef signed char s8_t;
+DECL|u16_t|typedef|typedef unsigned short u16_t;
+DECL|u32_t|typedef|typedef unsigned int u32_t;
+DECL|u64_t|typedef|typedef unsigned long long u64_t;
+DECL|u8_t|typedef|typedef unsigned char u8_t;
