9ba95130dc1f23793f91e52da2e31b0aec9d5e51
Kumar Gala <kumar.gala@linaro.org>
2017-04-19 12:38:01 -0500

    boards: convert to using newly introduced integer sized types
    Convert code to use u{8,16,32,64}_t and s{8,16,32,64}_t instead of C99
    integer types.
    
    Jira: ZEP-2051
    
    Change-Id: I27d51c316144251939b20cfa6787ff7ab8035fe6
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 7352c4681d8d96a5298a67601361a54952419e37

--- boards/x86/galileo/pinmux.c
+++ boards/x86/galileo/pinmux.c
@@ -11,14 +11,14 @@ DECL|PIN_HIGH|enumerator|PIN_HIGH = 0x01,
 DECL|PIN_LOW|enumerator|PIN_LOW = 0x00,
 DECL|PWM0|enumerator|PWM0,
 DECL|_galileo_path|variable|_galileo_path
-DECL|_galileo_pinmux_get_pin|function|int _galileo_pinmux_get_pin(struct device *port, uint32_t pin, uint32_t *func)
-DECL|_galileo_pinmux_set_pin|function|int _galileo_pinmux_set_pin(struct device *port, uint8_t pin, uint32_t func)
+DECL|_galileo_pinmux_get_pin|function|int _galileo_pinmux_get_pin(struct device *port, u32_t pin, u32_t *func)
+DECL|_galileo_pinmux_set_pin|function|int _galileo_pinmux_set_pin(struct device *port, u8_t pin, u32_t func)
 DECL|api_funcs|variable|api_funcs
-DECL|cfg|member|uint32_t cfg;
-DECL|func|member|uint8_t func;
+DECL|cfg|member|u32_t cfg;
+DECL|func|member|u8_t func;
 DECL|galileo_pinmux_driver|variable|galileo_pinmux_driver
 DECL|gpio_chip|enum|enum gpio_chip {
-DECL|io_pin|member|uint8_t io_pin;
+DECL|io_pin|member|u8_t io_pin;
 DECL|level|member|enum pin_level level;
 DECL|mux_config|variable|mux_config
 DECL|mux_path|struct|struct mux_path {
@@ -27,8 +27,8 @@ DECL|mux|member|enum gpio_chip mux;
 DECL|path|member|struct mux_pin path[5];
 DECL|pin_level|enum|enum pin_level {
 DECL|pinmux_galileo_initialize|function|static int pinmux_galileo_initialize(struct device *port)
-DECL|pinmux_get|function|static int pinmux_get(struct device *dev,uint32_t pin, uint32_t *func)
-DECL|pinmux_input_enable|function|static int pinmux_input_enable(struct device *dev, uint32_t pin, uint8_t func)
-DECL|pinmux_pullup|function|static int pinmux_pullup(struct device *dev, uint32_t pin, uint8_t func)
-DECL|pinmux_set|function|static int pinmux_set(struct device *dev,uint32_t pin, uint32_t func)
-DECL|pin|member|uint8_t pin;
+DECL|pinmux_get|function|static int pinmux_get(struct device *dev,u32_t pin, u32_t *func)
+DECL|pinmux_input_enable|function|static int pinmux_input_enable(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_pullup|function|static int pinmux_pullup(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_set|function|static int pinmux_set(struct device *dev,u32_t pin, u32_t func)
+DECL|pin|member|u8_t pin;
