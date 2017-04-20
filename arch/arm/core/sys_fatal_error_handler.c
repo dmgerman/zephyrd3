62325a3de485232197d108e28111054cf7ec2214
Andrew Boie <andrew.p.boie@intel.com>
2017-04-20 15:32:10 -0700

    arm: fix exception handling
    For exceptions where we are just going to abort the current thread, we
    need to exit handler mode properly so that PendSV can run and perform a
    context switch. For ARM architecture this means that the fatal error
    handling code path can indeed return if we were 1) in handler mode and
    2) only wish to abort the current thread.
    
    Fixes a very long-standing bug where a thread that generates an
    exception, and should only abort the thread, instead takes down the
    entire system.
    
    Issue: ZEP-2052
    Change-Id: Ib356a34a6fda2e0f8aff39c4b3270efceb81e54d
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: e09a04f0689fd29aa909cc49ee94fd129798f986

--- arch/arm/core/sys_fatal_error_handler.c
+++ arch/arm/core/sys_fatal_error_handler.c
@@ -1,1 +1,1 @@
-DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
+DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
