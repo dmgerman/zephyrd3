824557b22b8cd7a8ed63cff50ae4f975ab825582
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-04-21 13:24:07 +0200

    api: New SPI API
    Such API improves many aspects of the former API by reducing the number
    of function, allowing more buffer flexibility etc... This leads in
    better memory usag and performance as well.
    
    However, as this will take sometime to get into use, the former API is
    still present and is the one enabled by default.
    
    Jira: ZEP-852
    Jira: ZEP-287
    Jira: ZEP-1725
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: dd0c35919b74a072904802b21941220d53f64635

--- include/spi.h
+++ include/spi.h
@@ -1,28 +1,36 @@
+DECL|SPI_LINES_DUAL|macro|SPI_LINES_DUAL
+DECL|SPI_LINES_MASK|macro|SPI_LINES_MASK
+DECL|SPI_LINES_QUAD|macro|SPI_LINES_QUAD
+DECL|SPI_LINES_SINGLE|macro|SPI_LINES_SINGLE
 DECL|SPI_MODE_CPHA|macro|SPI_MODE_CPHA
 DECL|SPI_MODE_CPOL|macro|SPI_MODE_CPOL
+DECL|SPI_MODE_GET|macro|SPI_MODE_GET
 DECL|SPI_MODE_LOOP|macro|SPI_MODE_LOOP
 DECL|SPI_MODE_MASK|macro|SPI_MODE_MASK
-DECL|SPI_MODE|macro|SPI_MODE
+DECL|SPI_OP_MODE_MASTER|macro|SPI_OP_MODE_MASTER
+DECL|SPI_OP_MODE_SLAVE|macro|SPI_OP_MODE_SLAVE
 DECL|SPI_TRANSFER_LSB|macro|SPI_TRANSFER_LSB
-DECL|SPI_TRANSFER_MASK|macro|SPI_TRANSFER_MASK
 DECL|SPI_TRANSFER_MSB|macro|SPI_TRANSFER_MSB
+DECL|SPI_WORD_SET|macro|SPI_WORD_SET
 DECL|SPI_WORD_SIZE_GET|macro|SPI_WORD_SIZE_GET
 DECL|SPI_WORD_SIZE_MASK|macro|SPI_WORD_SIZE_MASK
-DECL|SPI_WORD|macro|SPI_WORD
+DECL|SPI_WORD_SIZE_SHIFT|macro|SPI_WORD_SIZE_SHIFT
 DECL|__SPI_H__|macro|__SPI_H__
-DECL|configure|member|spi_api_configure configure;
-DECL|config|member|u32_t config;
-DECL|max_sys_freq|member|u32_t max_sys_freq;
-DECL|slave_select|member|spi_api_slave_select slave_select;
-DECL|spi_api_configure|typedef|typedef int (*spi_api_configure)(struct device *dev,
+DECL|buf|member|void *buf;
+DECL|cs|member|struct spi_cs_control *cs;
+DECL|delay|member|u32_t delay;
+DECL|frequency|member|u32_t frequency;
+DECL|gpio_dev|member|struct device *gpio_dev;
+DECL|gpio_pin|member|u32_t gpio_pin;
+DECL|len|member|size_t len;
+DECL|operation|member|u16_t operation;
+DECL|slave|member|u16_t slave;
 DECL|spi_api_io|typedef|typedef int (*spi_api_io)(struct device *dev,
-DECL|spi_api_slave_select|typedef|typedef int (*spi_api_slave_select)(struct device *dev, u32_t slave);
 DECL|spi_buf|struct|struct spi_buf {
-DECL|spi_configure|function|static inline int spi_configure(struct device *dev,struct spi_config *config)
 DECL|spi_config|struct|struct spi_config {
+DECL|spi_cs_control|struct|struct spi_cs_control {
 DECL|spi_driver_api|struct|struct spi_driver_api {
-DECL|spi_read|function|static inline int spi_read(struct device *dev, void *buf, u32_t len)
-DECL|spi_slave_select|function|static inline int spi_slave_select(struct device *dev, u32_t slave)
-DECL|spi_transceive|function|static inline int spi_transceive(struct device *dev, const void *tx_buf, u32_t tx_buf_len, void *rx_buf, u32_t rx_buf_len)
-DECL|spi_write|function|static inline int spi_write(struct device *dev, const void *buf, u32_t len)
+DECL|spi_read|function|static inline int spi_read(struct device *dev, struct spi_config *config, struct spi_buf **rx_bufs)
+DECL|spi_transceive|function|static inline int spi_transceive(struct device *dev, struct spi_config *config, const struct spi_buf **tx_bufs, struct spi_buf **rx_bufs)
+DECL|spi_write|function|static inline int spi_write(struct device *dev, struct spi_config *config, const struct spi_buf **tx_bufs)
 DECL|transceive|member|spi_api_io transceive;
