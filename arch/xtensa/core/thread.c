d10b7a35aad96b6fa0539ff4dd0120f04ae44060
Mazen NEIFER <mazen@nestwave.com>
2017-04-24 17:17:08 +0200

    xtensa port: Fixed compilation error introduced by recent changes.
    The error was introduced by
        971324b0709e7d08bebee2f07d4ac91a6963ffac [formerly b8823c4efdf8ac97e3e721e1a5609cb3d326b29a]
    
    Change-Id: Ibf930107a7a690e0cb0851b7c247d524e3cb89e5
    Signed-off-by: Mazen NEIFER <mazen@nestwave.com>
    
    Former-commit-id: 35a48decfa1b16b255a28ba1553a5559c6f16f42

--- arch/xtensa/core/thread.c
+++ arch/xtensa/core/thread.c
@@ -1,1 +1,1 @@
-DECL|_new_thread|function|void _new_thread(char *pStack, size_t stackSize,void (*pEntry)(void *, void *, void *), void *p1, void *p2, void *p3, int prio, unsigned int options)
+DECL|_new_thread|function|void _new_thread(char *pStack, size_t stackSize,void (*pEntry)(void *, void *, void *), void *p1, void *p2, void *p3, int priority, unsigned int options)
