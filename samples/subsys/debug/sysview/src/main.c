98d75bbbca973147ff899026cad0851a85f774ba
Leandro Pereira <leandro.pereira@intel.com>
2017-04-24 12:43:52 -0700

    samples: Add SEGGER SystemView sample application
    This sample application uses the kernel system logger already present
    in Zephyr and publishes events through the SEGGER RTT protocol, so it's
    available by the SEGGER SystemView application.
    
    [1] https://www.segger.com/systemview.html?p=1731
    
    Jira: ZEP-1463
    Change-Id: If1eba4644b95175660b3040bdc4b2717b2cfc9ad
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 6ccfe5aa14fdb33ec6cbaef69df219fea83c8537

---
+++ samples/subsys/debug/sysview/src/main.c
@@ -0,0 +1,23 @@
+DECL|ENABLE_SYSVIEW_EVENT|macro|ENABLE_SYSVIEW_EVENT
+DECL|ENABLE_SYSVIEW_EVENT|macro|ENABLE_SYSVIEW_EVENT
+DECL|MUST_LOG|macro|MUST_LOG
+DECL|MUST_LOG|macro|MUST_LOG
+DECL|calc_stack|variable|calc_stack
+DECL|calc_thread|function|static void calc_thread(void)
+DECL|interrupt|variable|interrupt
+DECL|main|function|void main(void)
+DECL|printer_stack|variable|printer_stack
+DECL|printer_thread|function|static void printer_thread(void)
+DECL|publish_context_switch|function|static void publish_context_switch(uint32_t *event_data)
+DECL|publish_interrupt|function|static void publish_interrupt(uint32_t *event_data)
+DECL|publish_sleep|function|static void publish_sleep(uint32_t *event_data)
+DECL|publish_task|function|static void publish_task(uint32_t *event_data)
+DECL|send_system_desc|function|static void send_system_desc(void)
+DECL|sysview_api_send_task_list|function|static void sysview_api_send_task_list(void)
+DECL|sysview_get_interrupt|function|uint32_t sysview_get_interrupt(void)
+DECL|sysview_get_timestamp|function|uint32_t sysview_get_timestamp(void)
+DECL|sysview_setup|function|static void sysview_setup(void)
+DECL|sysview_stack|variable|sysview_stack
+DECL|sysview_thread|function|static void sysview_thread(void)
+DECL|timestamp|variable|timestamp
+DECL|zephyr_to_sysview|function|static uint32_t zephyr_to_sysview(int event_type)
