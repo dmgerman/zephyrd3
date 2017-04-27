0bdf6ffa2aa77079b4eab6f7d3cb1309f67826e9
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-04-27 10:03:43 +0200

    samples/crypto: Add mbedtls shim driver support
    Normalizing variables names and make sure tag handling behavior, which
    might be different among backends, does not make the test failing.
    
    Also, improving debug logs in case of error.
    
    Change-Id: Ic317948aab459bfa75c9a72ac48cb2d12a0d0706
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 087354019f0fa2b252690190ccd4bd24694df408

--- samples/drivers/crypto/src/main.c
+++ samples/drivers/crypto/src/main.c
@@ -1,3 +1,5 @@
+DECL|CRYPTO_DRV_NAME|macro|CRYPTO_DRV_NAME
+DECL|CRYPTO_DRV_NAME|macro|CRYPTO_DRV_NAME
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|cap_flags|variable|cap_flags
 DECL|cbc_mode|function|void cbc_mode(void)
@@ -14,4 +16,5 @@ DECL|iv|variable|iv
 DECL|key|variable|key
 DECL|main|function|void main(void)
 DECL|plaintext|variable|plaintext
+DECL|print_buffer_comparison|function|static void print_buffer_comparison(u8_t *wanted_result, u8_t *result, size_t length)
 DECL|validate_hw_compatibility|function|int validate_hw_compatibility(struct device *dev)
