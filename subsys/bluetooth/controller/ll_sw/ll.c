0b144c08094c91a3aa00610bebf0f6e77ca53617
Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
2017-04-28 10:44:22 +0200

    Bluetooth: controller: Rename ll_address_* to ll_addr_*
    Rename ll_address_* to ll_addr_*. Also, update ll_addr_get
    to return reference to stored public or random address.
    
    Change-id: I22cb0135d2223f679c4d9321f4724f8b7de0aede
    Signed-off-by: Vinayak Chettimada <vinayak.kariappa.chettimada@nordicsemi.no>
    
    Former-commit-id: b29b3e2fdb2beee78b569b8d4d378f8ddf93b151

--- subsys/bluetooth/controller/ll_sw/ll.c
+++ subsys/bluetooth/controller/ll_sw/ll.c
@@ -10,8 +10,8 @@ DECL|filter_policy|member|u8_t filter_policy:1;
 DECL|filter_policy|member|u8_t filter_policy:2;
 DECL|interval|member|u16_t interval;
 DECL|interval|member|u16_t interval;
-DECL|ll_address_get|function|void ll_address_get(u8_t addr_type, u8_t *bdaddr)
-DECL|ll_address_set|function|void ll_address_set(u8_t addr_type, u8_t const *const bdaddr)
+DECL|ll_addr_get|function|u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
+DECL|ll_addr_set|function|void ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
 DECL|ll_adv_data_set|function|void ll_adv_data_set(u8_t len, u8_t const *const data)
 DECL|ll_adv_enable|function|u32_t ll_adv_enable(u8_t enable)
 DECL|ll_adv_params_set|function|void ll_adv_params_set(u16_t interval, u8_t adv_type, u8_t own_addr_type, u8_t direct_addr_type, u8_t const *const direct_addr, u8_t chl_map, u8_t filter_policy)
