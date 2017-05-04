6e718426ff132bb93d9f38b5db373af70c8b9f25
Roger Lendenmann <roger.lendenmann@intel.com>
2017-05-04 13:14:23 +0200

    sensor: lis2dh: Add support for lis2dh accelerometer
     * lis2dh 3 axis accelerometer support on SPI and I2C bus
     * data ready and anymotion type of trigger support
    
     Tested on Dynastream module D52QD2M4IA-A using SPI
    
    Origin: based on Zephyr lis3dh driver
    
    Change-Id: I2c4e9418b87f09c957bba8f73522bd78830bc809
    Signed-off-by: Roger Lendenmann <roger.lendenmann@intel.com>
    
    Former-commit-id: 0bc4a88bc764e3a63e39f5c2a8e229df36ffb3a7

---
+++ drivers/sensor/lis2dh/lis2dh.c
@@ -0,0 +1,17 @@
+DECL|fs_values|member|u8_t fs_values[4];
+DECL|lis2dh_acc_config|function|static int lis2dh_acc_config(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|lis2dh_acc_odr_set|function|static int lis2dh_acc_odr_set(struct device *dev, u16_t freq)
+DECL|lis2dh_acc_range_map|variable|lis2dh_acc_range_map
+DECL|lis2dh_acc_range_set|function|static int lis2dh_acc_range_set(struct device *dev, s32_t range)
+DECL|lis2dh_attr_set|function|static int lis2dh_attr_set(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|lis2dh_channel_get|function|static int lis2dh_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|lis2dh_convert|function|static void lis2dh_convert(s16_t raw_val, u16_t scale, struct sensor_value *val)
+DECL|lis2dh_driver_api|variable|lis2dh_driver_api
+DECL|lis2dh_driver|variable|lis2dh_driver
+DECL|lis2dh_freq_to_odr_val|function|static int lis2dh_freq_to_odr_val(u16_t freq)
+DECL|lis2dh_init|function|int lis2dh_init(struct device *dev)
+DECL|lis2dh_odr_map|variable|lis2dh_odr_map
+DECL|lis2dh_range_to_reg_val|function|static int lis2dh_range_to_reg_val(u16_t range)
+DECL|lis2dh_reg_field_update|function|int lis2dh_reg_field_update(struct device *bus, u8_t reg_addr, u8_t pos, u8_t mask, u8_t val)
+DECL|lis2dh_sample_fetch|function|static int lis2dh_sample_fetch(struct device *dev, enum sensor_channel chan)
+DECL|word_le32|member|u32_t word_le32;
---
+++ drivers/sensor/lis2dh/lis2dh_trigger.c
@@ -0,0 +1,19 @@
+DECL|LIS2DH_ANYM_CFG|macro|LIS2DH_ANYM_CFG
+DECL|LIS2DH_INT1_CFG|macro|LIS2DH_INT1_CFG
+DECL|LIS2DH_INT2_CFG|macro|LIS2DH_INT2_CFG
+DECL|START_TRIG_INT1|macro|START_TRIG_INT1
+DECL|START_TRIG_INT2|macro|START_TRIG_INT2
+DECL|TRIGGED_INT1|macro|TRIGGED_INT1
+DECL|TRIGGED_INT2|macro|TRIGGED_INT2
+DECL|lis2dh_acc_slope_config|function|int lis2dh_acc_slope_config(struct device *dev, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|lis2dh_gpio_int1_callback|function|static void lis2dh_gpio_int1_callback(struct device *dev, struct gpio_callback *cb, u32_t pins)
+DECL|lis2dh_gpio_int2_callback|function|static void lis2dh_gpio_int2_callback(struct device *dev, struct gpio_callback *cb, u32_t pins)
+DECL|lis2dh_init_interrupt|function|int lis2dh_init_interrupt(struct device *dev)
+DECL|lis2dh_start_trigger_int1|function|static int lis2dh_start_trigger_int1(const struct lis2dh_data *lis2dh)
+DECL|lis2dh_start_trigger_int2|function|static int lis2dh_start_trigger_int2(const struct lis2dh_data *lis2dh)
+DECL|lis2dh_thread_cb|function|static void lis2dh_thread_cb(void *arg)
+DECL|lis2dh_thread|function|static void lis2dh_thread(void *arg1, void *unused2, void *unused3)
+DECL|lis2dh_trigger_anym_set|function|static int lis2dh_trigger_anym_set(struct device *dev, sensor_trigger_handler_t handler)
+DECL|lis2dh_trigger_drdy_set|function|static int lis2dh_trigger_drdy_set(struct device *dev, enum sensor_channel chan, sensor_trigger_handler_t handler)
+DECL|lis2dh_trigger_set|function|int lis2dh_trigger_set(struct device *dev, const struct sensor_trigger *trig, sensor_trigger_handler_t handler)
+DECL|lis2dh_work_cb|function|static void lis2dh_work_cb(struct k_work *work)
