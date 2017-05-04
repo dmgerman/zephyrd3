de4b491186436d15dbc04910e21d94f06dcb6003
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-05-04 09:07:02 +0200

    samples: Add an SPI driver test application
    This application intents to test an SPI driver by looping over MISO/MOSI
    line where the controller will then send data to itself.
    
    It will test various buffer tx/rx schemes.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: cd54dc66f2f132d83eb3690df46f104e641a5d38

---
+++ samples/drivers/spi/src/spi.c
@@ -0,0 +1,30 @@
+DECL|BUF_SIZE|macro|BUF_SIZE
+DECL|CS_CTRL_GPIO_DRV_NAME|macro|CS_CTRL_GPIO_DRV_NAME
+DECL|CS_CTRL_GPIO_DRV_NAME|macro|CS_CTRL_GPIO_DRV_NAME
+DECL|CS_CTRL_GPIO_DRV_NAME|macro|CS_CTRL_GPIO_DRV_NAME
+DECL|CS_CTRL_GPIO_DRV_NAME|macro|CS_CTRL_GPIO_DRV_NAME
+DECL|SPI_CS|macro|SPI_CS
+DECL|SPI_CS|macro|SPI_CS
+DECL|SPI_CS|macro|SPI_CS
+DECL|SPI_CS|macro|SPI_CS
+DECL|SPI_CS|macro|SPI_CS
+DECL|SPI_DRV_NAME|macro|SPI_DRV_NAME
+DECL|SPI_DRV_NAME|macro|SPI_DRV_NAME
+DECL|SPI_DRV_NAME|macro|SPI_DRV_NAME
+DECL|SPI_SLAVE|macro|SPI_SLAVE
+DECL|SPI_SLAVE|macro|SPI_SLAVE
+DECL|SPI_SLAVE|macro|SPI_SLAVE
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|buffer_rx|variable|buffer_rx
+DECL|buffer_tx|variable|buffer_tx
+DECL|cs_ctrl_gpio_config|function|static int cs_ctrl_gpio_config(struct spi_cs_control *cs)
+DECL|main|function|void main(void)
+DECL|spi_complete_loop|function|static int spi_complete_loop(struct spi_config *spi_conf)
+DECL|spi_cs|variable|spi_cs
+DECL|spi_cs|variable|spi_cs
+DECL|spi_dev|variable|spi_dev
+DECL|spi_fast|variable|spi_fast
+DECL|spi_rx_every_4|function|static int spi_rx_every_4(struct spi_config *spi_conf)
+DECL|spi_rx_half_end|function|static int spi_rx_half_end(struct spi_config *spi_conf)
+DECL|spi_rx_half_start|function|static int spi_rx_half_start(struct spi_config *spi_conf)
+DECL|spi_slow|variable|spi_slow
