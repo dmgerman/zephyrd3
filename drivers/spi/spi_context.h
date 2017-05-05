96cca40df3794244a51e890afd31f6642b6cd354
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-05-05 13:28:17 +0200

    drivers/spi: Add reentrance support to DW driver in a generic manner
    Let's improve common SPI driver context by adding a lock and generic
    function to get/release it.
    
    It's statically initialized to save a bit of ROM.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 0bd83d21f26a1674892fec81863a7f8d1c6ba415

--- drivers/spi/spi_context.h
+++ drivers/spi/spi_context.h
@@ -1,13 +1,17 @@
+DECL|SPI_CONTEXT_INIT_LOCK|macro|SPI_CONTEXT_INIT_LOCK
 DECL|__SPI_DRIVER_COMMON_H__|macro|__SPI_DRIVER_COMMON_H__
 DECL|config|member|struct spi_config *config;
 DECL|current_rx|member|struct spi_buf **current_rx;
 DECL|current_tx|member|const struct spi_buf **current_tx;
+DECL|lock|member|struct k_sem lock;
 DECL|rx_buf|member|void *rx_buf;
 DECL|rx_len|member|u32_t rx_len;
 DECL|spi_context_buffers_setup|function|static inline void spi_context_buffers_setup(struct spi_context *ctx, const struct spi_buf **tx_bufs, struct spi_buf **rx_bufs, uint8_t dfs)
 DECL|spi_context_configured|function|static inline bool spi_context_configured(struct spi_context *ctx, struct spi_config *config)
 DECL|spi_context_cs_configure|function|static inline void spi_context_cs_configure(struct spi_context *ctx)
 DECL|spi_context_cs_control|function|static inline void spi_context_cs_control(struct spi_context *ctx, bool on)
+DECL|spi_context_lock|function|static inline void spi_context_lock(struct spi_context *ctx)
+DECL|spi_context_release|function|static inline void spi_context_release(struct spi_context *ctx)
 DECL|spi_context_rx_on|function|bool spi_context_rx_on(struct spi_context *ctx)
 DECL|spi_context_tx_on|function|bool spi_context_tx_on(struct spi_context *ctx)
 DECL|spi_context_update_rx|function|void spi_context_update_rx(struct spi_context *ctx, uint8_t dfs)
