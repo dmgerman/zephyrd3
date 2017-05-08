60d66f7a8796f534a2303e8f7c9a2ad05a07d92f
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-05-08 14:27:10 +0200

    samples/spi: Add an asynchronous call test
    This gives a quicke example on how to use SPI asynchronous calls with
    kernel's k_poll API.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: d9007a09f8c1fb5bc23a380602b315c526465194

--- samples/drivers/spi/src/spi.c
+++ samples/drivers/spi/src/spi.c
@@ -15,10 +15,17 @@ DECL|SPI_SLAVE|macro|SPI_SLAVE
 DECL|SPI_SLAVE|macro|SPI_SLAVE
 DECL|SPI_SLAVE|macro|SPI_SLAVE
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|async_evt|variable|async_evt
+DECL|async_sig|variable|async_sig
 DECL|buffer_rx|variable|buffer_rx
 DECL|buffer_tx|variable|buffer_tx
+DECL|caller|variable|caller
 DECL|cs_ctrl_gpio_config|function|static int cs_ctrl_gpio_config(struct spi_cs_control *cs)
 DECL|main|function|void main(void)
+DECL|result|variable|result
+DECL|spi_async_call_cb|function|static void spi_async_call_cb(struct k_poll_event *async_evt, struct k_sem *caller_sem, void *unused)
+DECL|spi_async_call|function|static int spi_async_call(struct spi_config *spi_conf)
+DECL|spi_async_stack|variable|spi_async_stack
 DECL|spi_complete_loop|function|static int spi_complete_loop(struct spi_config *spi_conf)
 DECL|spi_cs|variable|spi_cs
 DECL|spi_cs|variable|spi_cs
