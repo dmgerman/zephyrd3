d9fb80d7094ac6d65a1a5113551c681a3210ee0c
Youvedeep Singh <youvedeep.singh@intel.com>
2017-05-08 11:29:37 +0530

    Timer: Add TICKLESS_KERNEL support for nrf_rtc_timer.
    Add support for TICKLESS KERNEL in nrf_rtc_timer for Nordic family of
    processors. This patch includes :-
     1. Programming RTC based on "next timer" value from timeout queue while
         A. exiting idle_exit.
         B. on RTC interrupt.
     2. Impliments some of functions which will be required by sys_clock and
        scheduler.
    
    ZEP-1819
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: a4063f53622a4368576c8f4e709596e77fc23690

--- drivers/timer/nrf_rtc_timer.c
+++ drivers/timer/nrf_rtc_timer.c
@@ -6,6 +6,13 @@ DECL|RTC_MASK|macro|RTC_MASK
 DECL|RTC_MIN_DELTA|macro|RTC_MIN_DELTA
 DECL|RTC_TICKS_PER_SYS_TICK|macro|RTC_TICKS_PER_SYS_TICK
 DECL|SYS_CLOCK_RTC|macro|SYS_CLOCK_RTC
+DECL|_enable_sys_clock|function|void _enable_sys_clock(void)
+DECL|_get_elapsed_clock_time|function|uint64_t _get_elapsed_clock_time(void)
+DECL|_get_elapsed_program_time|function|uint32_t _get_elapsed_program_time(void)
+DECL|_get_max_clock_time|function|int32_t _get_max_clock_time(void)
+DECL|_get_program_time|function|uint32_t _get_program_time(void)
+DECL|_get_remaining_program_time|function|uint32_t _get_remaining_program_time(void)
+DECL|_set_time|function|void _set_time(uint32_t time)
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
 DECL|_timer_cycle_get_32|function|u32_t _timer_cycle_get_32(void)
 DECL|_timer_idle_enter|function|void _timer_idle_enter(s32_t sys_ticks)
