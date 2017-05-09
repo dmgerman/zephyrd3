1c89e2ea307dbec1f1f6b5238b066fcc65858c2b
Andrew Boie <andrew.p.boie@intel.com>
2017-05-09 12:12:54 -0700

    gpio_sch: use k_thread_create()
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: f21129180dfaa0c27158343e2ea72b2099dee77e

--- drivers/gpio/gpio_sch.h
+++ drivers/gpio/gpio_sch.h
@@ -19,6 +19,7 @@ DECL|gtpe|member|u32_t gtpe;
 DECL|int_regs|member|} int_regs;
 DECL|poll_timer|member|struct k_timer poll_timer;
 DECL|polling_stack|member|char __stack polling_stack[GPIO_SCH_POLLING_STACK_SIZE];
+DECL|polling_thread|member|struct k_thread polling_thread;
 DECL|poll|member|u8_t poll;
 DECL|regs|member|u32_t regs;
 DECL|stride|member|u8_t stride[3];
