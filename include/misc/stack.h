9f2f2ba0351ffa84f2cccc29b0263340bd30950f
Andrew Boie <andrew.p.boie@intel.com>
2017-05-09 12:06:30 -0700

    stack.h: add size computation function
    The computation of unused stack space is now split off from the function
    which sends the result to printk().
    
    The code now assumes that the struct k_thread is stored elsewhere.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: ef9efa0fc469858c2b275c90a5e52490773cf2b1

--- include/misc/stack.h
+++ include/misc/stack.h
@@ -1,2 +1,4 @@
-DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned size)
-DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned size)
+DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned int size)
+DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned int size)
+DECL|stack_unused_space_get|function|static inline size_t stack_unused_space_get(const char *stack, size_t size)
+DECL|stack_unused_space_get|function|static inline size_t stack_unused_space_get(const char *stack, size_t size)
