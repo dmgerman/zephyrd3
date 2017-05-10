260329291f9ec8f30fb6a5912f6a2856ce691d8f
Andrew Boie <andrew.p.boie@intel.com>
2017-05-10 13:28:38 -0700

    xtensa: move byte-order macros out of arch.h
    These are macros that are expected to be defined at all times by
    the compiler. We need them at the very beginning of kernel.h for
    the k_thread definition, before it's possible to include arch.h.
    
    Make a special toolchain header for XCC compiler and place these
    defines in there. Otherwise inherit all the other GCC defines.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: a2b324035c74635d0ca223902d6bb8acf432396c

--- include/arch/xtensa/arch.h
+++ include/arch/xtensa/arch.h
@@ -10,9 +10,6 @@ DECL|_NANO_ERR_KERNEL_OOPS|macro|_NANO_ERR_KERNEL_OOPS
 DECL|_NANO_ERR_KERNEL_PANIC|macro|_NANO_ERR_KERNEL_PANIC
 DECL|_NANO_ERR_RESERVED_IRQ|macro|_NANO_ERR_RESERVED_IRQ
 DECL|_NANO_ERR_STACK_CHK_FAIL|macro|_NANO_ERR_STACK_CHK_FAIL
-DECL|__BYTE_ORDER__|macro|__BYTE_ORDER__
-DECL|__ORDER_BIG_ENDIAN__|macro|__ORDER_BIG_ENDIAN__
-DECL|__ORDER_LITTLE_ENDIAN__|macro|__ORDER_LITTLE_ENDIAN__
 DECL|_arch_k_cycle_get_32|macro|_arch_k_cycle_get_32
 DECL|find_lsb_set|function|static ALWAYS_INLINE unsigned int find_lsb_set(u32_t op)
 DECL|find_msb_set|function|static ALWAYS_INLINE unsigned int find_msb_set(u32_t op)
