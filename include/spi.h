3f151183cc4f8023c9d36cb8e783af954799ba0b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-05-10 08:40:00 +0200

    api/spi: Add 2 specific control bits for special use cases.
    SPI_HOLD_ON_CS can be used to ask the SPI device to keep CS on, after
    the transaction. And this undefinitely, until another config is used.
    This will inhibate the gpio cs delay, if any. This might be useful when
    doing consecutive calls on one slave without releasing the CS.
    
    SPI_LOCK_ON is to be used with caution as it will keep the SPI device
    locked for the current config being used after each transaction. This
    can be necessary if one needs to do consecutive calls on a slave without
    any olher caller to interfere.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 10c1e49f848b08895e8acecc82cdcb54ac5d8ce7

--- include/spi.h
+++ include/spi.h
@@ -1,7 +1,9 @@
+DECL|SPI_HOLD_ON_CS|macro|SPI_HOLD_ON_CS
 DECL|SPI_LINES_DUAL|macro|SPI_LINES_DUAL
 DECL|SPI_LINES_MASK|macro|SPI_LINES_MASK
 DECL|SPI_LINES_QUAD|macro|SPI_LINES_QUAD
 DECL|SPI_LINES_SINGLE|macro|SPI_LINES_SINGLE
+DECL|SPI_LOCK_ON|macro|SPI_LOCK_ON
 DECL|SPI_MODE_CPHA|macro|SPI_MODE_CPHA
 DECL|SPI_MODE_CPOL|macro|SPI_MODE_CPOL
 DECL|SPI_MODE_GET|macro|SPI_MODE_GET
@@ -25,15 +27,18 @@ DECL|gpio_dev|member|struct device *gpio_dev;
 DECL|gpio_pin|member|u32_t gpio_pin;
 DECL|len|member|size_t len;
 DECL|operation|member|u16_t operation;
+DECL|release|member|spi_api_release release;
 DECL|slave|member|u16_t slave;
 DECL|spi_api_io_async|typedef|typedef int (*spi_api_io_async)(struct spi_config *config,
 DECL|spi_api_io|typedef|typedef int (*spi_api_io)(struct spi_config *config,
+DECL|spi_api_release|typedef|typedef int (*spi_api_release)(struct spi_config *config);
 DECL|spi_buf|struct|struct spi_buf {
 DECL|spi_config|struct|struct spi_config {
 DECL|spi_cs_control|struct|struct spi_cs_control {
 DECL|spi_driver_api|struct|struct spi_driver_api {
 DECL|spi_read_async|function|static inline int spi_read_async(struct spi_config *config, struct spi_buf **rx_bufs, struct k_poll_signal *async)
 DECL|spi_read|function|static inline int spi_read(struct spi_config *config, struct spi_buf **rx_bufs)
+DECL|spi_release|function|static inline int spi_release(struct spi_config *config)
 DECL|spi_transceive_async|function|static inline int spi_transceive_async(struct spi_config *config, const struct spi_buf **tx_bufs, struct spi_buf **rx_bufs, struct k_poll_signal *async)
 DECL|spi_transceive|function|static inline int spi_transceive(struct spi_config *config, const struct spi_buf **tx_bufs, struct spi_buf **rx_bufs)
 DECL|spi_write_async|function|static inline int spi_write_async(struct spi_config *config, const struct spi_buf **tx_bufs, struct k_poll_signal *async)
