7691026dbf5dfe424d5b5807f63fdec2fad5a0fa
Leandro Pereira <leandro.pereira@intel.com>
2017-05-10 17:12:12 -0700

    drivers: esp32: Add minimal UART driver based on ROM routines
    This is a minimal driver enabling console output during the port
    bringup.  While the driver works, only one of the three UART devices
    are supported, and there isn't any way to change any parameters or
    use interrupts.  This will most likely be superceded by a proper
    driver after the port has matured.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 37ea77173ceb90d1edaa187b578e923b92cf22e4

---
+++ drivers/serial/uart_esp32.c
@@ -0,0 +1,4 @@
+DECL|esp32_uart_api|variable|esp32_uart_api
+DECL|esp32_uart_init|function|static int esp32_uart_init(struct device *dev)
+DECL|esp32_uart_rx|function|static int esp32_uart_rx(struct device *dev, unsigned char *p_char)
+DECL|esp32_uart_tx|function|static unsigned char esp32_uart_tx(struct device *dev, unsigned char c)
