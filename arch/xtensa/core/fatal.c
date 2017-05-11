69a2e5e1452d1cab75b87411c8ebaab4a0f63a1b
Andrew Boie <andrew.p.boie@intel.com>
2017-05-11 09:41:10 -0700

    xtensa: merge crt1-*.S
    We had two assembly files to prepare for entry into C domain,
    one intended for the simulator and one intended for real boards.
    
    - Both files merged into a single crt1.S for either simulated or real
      targets
    - Extra logic to populate command line arguments from simulator removed,
      we don't use it.
    - BSS zeroing logic from crt1-boards.S used
    - Reference to missing reset-unneeded.S removed
    - exit() implementation moved to fatal.c, now invokes a kernel panic
      if we are not running under the simulator
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 51df312abc142ef1cc0db85286b4d73e12aa86e4

--- arch/xtensa/core/fatal.c
+++ arch/xtensa/core/fatal.c
@@ -5,5 +5,6 @@ DECL|_SysFatalErrorHandler|function|FUNC_NORETURN void _SysFatalErrorHandler(uns
 DECL|_default_esf|variable|_default_esf
 DECL|cause_str|function|static char *cause_str(unsigned int cause_code)
 DECL|dump_exc_state|function|static void dump_exc_state(void)
+DECL|exit|function|void exit(int return_code)
 DECL|get_bits|function|static inline unsigned int get_bits(int offset, int num_bits, unsigned int val)
 DECL|get_sreg|macro|get_sreg
