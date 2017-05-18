94eed8f2dc5944ee8b7be16cc4561471dc1614eb
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-05-18 12:32:05 +0300

    sample: net: http: Add Basic auth support to server sample
    Basic auth support was missing from HTTP server sample.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 11aee50eef2478aca06eef10045a01a53d6e9b94

--- samples/net/http_server/src/main.c
+++ samples/net/http_server/src/main.c
@@ -4,6 +4,7 @@ DECL|APP_BANNER|macro|APP_BANNER
 DECL|CONFIG_HTTPS_STACK_SIZE|macro|CONFIG_HTTPS_STACK_SIZE
 DECL|HTML_FOOTER|macro|HTML_FOOTER
 DECL|HTML_HEADER|macro|HTML_HEADER
+DECL|HTTP_401_STATUS_US|macro|HTTP_401_STATUS_US
 DECL|HTTP_MAX_BODY_STR_SIZE|macro|HTTP_MAX_BODY_STR_SIZE
 DECL|HTTP_STATUS_200_OK|macro|HTTP_STATUS_200_OK
 DECL|INSTANCE_INFO|macro|INSTANCE_INFO
@@ -13,7 +14,10 @@ DECL|RESULT_BUF_SIZE|macro|RESULT_BUF_SIZE
 DECL|RX_FIFO_DEPTH|macro|RX_FIFO_DEPTH
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|http_basic_auth|function|static int http_basic_auth(struct http_server_ctx *ctx)
 DECL|http_ctx|variable|http_ctx
+DECL|http_response_401|function|int http_response_401(struct http_server_ctx *ctx, s32_t timeout)
+DECL|http_response_auth|function|static int http_response_auth(struct http_server_ctx *ctx)
 DECL|http_response_header_fields|function|int http_response_header_fields(struct http_server_ctx *ctx)
 DECL|http_response_it_works|function|static int http_response_it_works(struct http_server_ctx *ctx)
 DECL|http_response_soft_404|function|static int http_response_soft_404(struct http_server_ctx *ctx)
