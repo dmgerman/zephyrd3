ff6d741717c67753fb83c5d25b2387f59c6c189a
Sharron LIU <sharron.liu@intel.com>
2017-05-19 14:30:59 +0800

    tests: kernel: added tests for k_mem_pool_alloc from isr
    Added tests to invoke k_mem_pool_alloc() from isr context
    
    Jira: ZEP-1631
    
    Signed-off-by: Sharron LIU <sharron.liu@intel.com>
    
    Former-commit-id: 189aa475e15e808d1fb1b86b2443a1fa06044fc6

--- tests/kernel/mem_pool/test_mpool_api/src/test_mpool_api.c
+++ tests/kernel/mem_pool/test_mpool_api/src/test_mpool_api.c
@@ -1,3 +1,4 @@
+DECL|test_mpool_alloc_free_isr|function|void test_mpool_alloc_free_isr(void)
 DECL|test_mpool_alloc_free_thread|function|void test_mpool_alloc_free_thread(void)
 DECL|test_mpool_alloc_size|function|void test_mpool_alloc_size(void)
 DECL|test_mpool_alloc_timeout|function|void test_mpool_alloc_timeout(void)
