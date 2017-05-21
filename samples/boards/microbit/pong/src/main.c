d58ce13cbbfd2ec59b317983cb66b544df51973c
Johan Hedberg <johan.hedberg@intel.com>
2017-05-21 10:43:52 -0700

    samples: microbit/pong: Add initial draft of a pong game
    Add initial draft of a pong game using the display on the micro:bit
    and its two buttons. This draft only supports a solo game where the
    ball bounces from the "roof" and lets the player play against himself.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 9a7cb7134ab278050fdcd3b3760b3809083103a1

---
+++ samples/boards/microbit/pong/src/main.c
@@ -0,0 +1,36 @@
+DECL|BALL_POS_X_MAX|macro|BALL_POS_X_MAX
+DECL|BALL_POS_X_MIN|macro|BALL_POS_X_MIN
+DECL|BALL_POS_Y_MAX|macro|BALL_POS_Y_MAX
+DECL|BALL_POS_Y_MIN|macro|BALL_POS_Y_MIN
+DECL|BALL_START|macro|BALL_START
+DECL|BALL_VEL_Y_START|macro|BALL_VEL_Y_START
+DECL|GAME_REFRESH|macro|GAME_REFRESH
+DECL|PADDLE_MAX|macro|PADDLE_MAX
+DECL|PADDLE_MIN|macro|PADDLE_MIN
+DECL|PADDLE_ROW|macro|PADDLE_ROW
+DECL|PIXEL_SIZE|macro|PIXEL_SIZE
+DECL|REAL_TO_VIRT|macro|REAL_TO_VIRT
+DECL|RESTART_THRESHOLD|macro|RESTART_THRESHOLD
+DECL|SOLO|macro|SOLO
+DECL|START_THRESHOLD|macro|START_THRESHOLD
+DECL|VIRT_TO_REAL|macro|VIRT_TO_REAL
+DECL|a_timestamp|variable|a_timestamp
+DECL|b_timestamp|variable|b_timestamp
+DECL|ball_pos|variable|ball_pos
+DECL|ball_vel|variable|ball_vel
+DECL|ball_visible|function|static bool ball_visible(void)
+DECL|button_pressed|function|static void button_pressed(struct device *dev, struct gpio_callback *cb, u32_t pins)
+DECL|check_start|function|static void check_start(void)
+DECL|configure_buttons|function|static void configure_buttons(void)
+DECL|disp_update|variable|disp_update
+DECL|ended|variable|ended
+DECL|game_ended|function|static void game_ended(bool won)
+DECL|game_init|function|static void game_init(void)
+DECL|game_refresh|function|static void game_refresh(struct k_work *work)
+DECL|main|function|void main(void)
+DECL|paddle_x|variable|paddle_x
+DECL|refresh|variable|refresh
+DECL|started|variable|started
+DECL|x_y|struct|struct x_y {
+DECL|x|member|int x;
+DECL|y|member|int y;
