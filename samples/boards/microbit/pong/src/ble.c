a05e7d1de099228e75db0326c36ee7c667b17bcd
Johan Hedberg <johan.hedberg@intel.com>
2017-05-21 18:53:45 -0700

    samples: microbit/pong: Add BLE-based multiplayer support
    Add support for connecting two micro:bit devices to play pong between
    themselves.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 5e59f13ca3ce217ad01b88bab2e6f58fb9801ae6

---
+++ samples/boards/microbit/pong/src/ble.c
@@ -0,0 +1,61 @@
+DECL|APPEARANCE|macro|APPEARANCE
+DECL|BALL_INFO_LEN|macro|BALL_INFO_LEN
+DECL|BLE_ADVERTISING|enumerator|BLE_ADVERTISING,
+DECL|BLE_ADV_START|enumerator|BLE_ADV_START,
+DECL|BLE_BALL_INFO|enumerator|BLE_BALL_INFO = 0x00,
+DECL|BLE_CONNECTED|enumerator|BLE_CONNECTED,
+DECL|BLE_CONNECT_CANCEL|enumerator|BLE_CONNECT_CANCEL,
+DECL|BLE_CONNECT_CREATE|enumerator|BLE_CONNECT_CREATE,
+DECL|BLE_DISCONNECTED|enumerator|BLE_DISCONNECTED,
+DECL|BLE_LOST|enumerator|BLE_LOST = 0x01,
+DECL|BLE_SCAN_START|enumerator|BLE_SCAN_START,
+DECL|BLE_SCAN|enumerator|BLE_SCAN,
+DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|PONG_CHR_UUID|macro|PONG_CHR_UUID
+DECL|PONG_SVC_UUID|macro|PONG_SVC_UUID
+DECL|SCAN_TIMEOUT|macro|SCAN_TIMEOUT
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|adv_timeout|function|static u32_t adv_timeout(void)
+DECL|ad|variable|ad
+DECL|ball|member|struct ble_ball_info ball;
+DECL|ble_ball_info|struct|struct ble_ball_info {
+DECL|ble_cancel_connect|function|void ble_cancel_connect(void)
+DECL|ble_connect|function|void ble_connect(void)
+DECL|ble_data|struct|struct ble_data {
+DECL|ble_init|function|void ble_init(void)
+DECL|ble_send_ball|function|void ble_send_ball(s8_t x_pos, s8_t y_pos, s8_t x_vel, s8_t y_vel)
+DECL|ble_send_lost|function|void ble_send_lost(void)
+DECL|ble_state|variable|ble_state
+DECL|ble_timeout|function|static void ble_timeout(struct k_work *work)
+DECL|ble_work|variable|ble_work
+DECL|cancel_connect|function|static void cancel_connect(void)
+DECL|conn_callbacks|variable|conn_callbacks
+DECL|connect_canceled|variable|connect_canceled
+DECL|connected|function|static void connected(struct bt_conn *conn, u8_t err)
+DECL|create_conn|function|static void create_conn(const bt_addr_le_t *addr)
+DECL|default_conn|variable|default_conn
+DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, s8_t rssi, u8_t type, struct net_buf_simple *ad)
+DECL|disconnected|function|static void disconnected(struct bt_conn *conn, u8_t reason)
+DECL|discov_param|variable|discov_param
+DECL|discover_func|function|static u8_t discover_func(struct bt_conn *conn, const struct bt_gatt_attr *attr, struct bt_gatt_discover_params *param)
+DECL|gatt_ccc_uuid|variable|gatt_ccc_uuid
+DECL|initiator|variable|initiator
+DECL|local_attr|variable|local_attr
+DECL|notify_func|function|static u8_t notify_func(struct bt_conn *conn,struct bt_gatt_subscribe_params *param, const void *buf, u16_t len)
+DECL|op|member|u8_t op;
+DECL|pong_attrs|variable|pong_attrs
+DECL|pong_ccc_cfg_changed|function|static void pong_ccc_cfg_changed(const struct bt_gatt_attr *attr, u16_t val)
+DECL|pong_ccc_cfg|variable|pong_ccc_cfg
+DECL|pong_chr_uuid|variable|pong_chr_uuid
+DECL|pong_svc_uuid|variable|pong_svc_uuid
+DECL|pong_uuid_match|function|static bool pong_uuid_match(const u8_t *data, u8_t len)
+DECL|remote_handle|variable|remote_handle
+DECL|remote_ready|variable|remote_ready
+DECL|sd|variable|sd
+DECL|subscribe_param|variable|subscribe_param
+DECL|x_pos|member|s8_t x_pos;
+DECL|x_vel|member|s8_t x_vel;
+DECL|y_pos|member|s8_t y_pos;
+DECL|y_vel|member|s8_t y_vel;
--- samples/boards/microbit/pong/src/main.c
+++ samples/boards/microbit/pong/src/main.c
@@ -4,14 +4,18 @@ DECL|BALL_POS_Y_MAX|macro|BALL_POS_Y_MAX
 DECL|BALL_POS_Y_MIN|macro|BALL_POS_Y_MIN
 DECL|BALL_START|macro|BALL_START
 DECL|BALL_VEL_Y_START|macro|BALL_VEL_Y_START
+DECL|CONNECTED|enumerator|CONNECTED,
 DECL|GAME_REFRESH|macro|GAME_REFRESH
+DECL|INIT|enumerator|INIT,
+DECL|MULTI|enumerator|MULTI,
 DECL|PADDLE_MAX|macro|PADDLE_MAX
 DECL|PADDLE_MIN|macro|PADDLE_MIN
 DECL|PADDLE_ROW|macro|PADDLE_ROW
 DECL|PIXEL_SIZE|macro|PIXEL_SIZE
 DECL|REAL_TO_VIRT|macro|REAL_TO_VIRT
 DECL|RESTART_THRESHOLD|macro|RESTART_THRESHOLD
-DECL|SOLO|macro|SOLO
+DECL|SCROLL_SPEED|macro|SCROLL_SPEED
+DECL|SINGLE|enumerator|SINGLE,
 DECL|START_THRESHOLD|macro|START_THRESHOLD
 DECL|VIRT_TO_REAL|macro|VIRT_TO_REAL
 DECL|a_timestamp|variable|a_timestamp
@@ -21,16 +25,38 @@ DECL|ball_vel|variable|ball_vel
 DECL|ball_visible|function|static bool ball_visible(void)
 DECL|button_pressed|function|static void button_pressed(struct device *dev, struct gpio_callback *cb, u32_t pins)
 DECL|check_start|function|static void check_start(void)
+DECL|choice_count|member|size_t choice_count;
+DECL|choice|member|const struct pong_choice *choice;
+DECL|complete|member|void (*complete)(int val);
 DECL|configure_buttons|function|static void configure_buttons(void)
 DECL|disp_update|variable|disp_update
 DECL|ended|variable|ended
 DECL|game_ended|function|static void game_ended(bool won)
-DECL|game_init|function|static void game_init(void)
+DECL|game_init|function|static void game_init(bool initiator)
 DECL|game_refresh|function|static void game_refresh(struct k_work *work)
 DECL|main|function|void main(void)
+DECL|mode_choice|variable|mode_choice
+DECL|mode_selected|function|static void mode_selected(int val)
+DECL|mode_selection|variable|mode_selection
 DECL|paddle_x|variable|paddle_x
+DECL|pong_ball_received|function|void pong_ball_received(s8_t x_pos, s8_t y_pos, s8_t x_vel, s8_t y_vel)
+DECL|pong_choice|struct|struct pong_choice {
+DECL|pong_conn_ready|function|void pong_conn_ready(bool initiator)
+DECL|pong_remote_disconnected|function|void pong_remote_disconnected(void)
+DECL|pong_remote_lost|function|void pong_remote_lost(void)
+DECL|pong_select_change|function|static void pong_select_change(void)
+DECL|pong_select_complete|function|static void pong_select_complete(void)
+DECL|pong_selection|struct|struct pong_selection {
+DECL|pong_select|function|static void pong_select(const struct pong_selection *sel)
+DECL|pong_state|enum|enum pong_state {
 DECL|refresh|variable|refresh
+DECL|remote_lost|variable|remote_lost
+DECL|select_idx|variable|select_idx
+DECL|select|variable|select
 DECL|started|variable|started
+DECL|state|variable|state
+DECL|str|member|const char *str;
+DECL|val|member|int val;
 DECL|x_y|struct|struct x_y {
 DECL|x|member|int x;
 DECL|y|member|int y;
