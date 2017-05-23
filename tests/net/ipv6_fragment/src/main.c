5d53272706efbf18917e8f267b8317d68ba70a35
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-05-23 14:00:01 +0300

    tests: net: ipv6: Test IPv6 fragmentation sending
    These tests make sure that the IPv6 fragments are build correctly
    when a large IPv6 packet is being sent.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 3f46248bb5299c3f54e763f265e1a0aeaaea16f1

---
+++ tests/net/ipv6_fragment/src/main.c
@@ -0,0 +1,48 @@
+DECL|ALLOC_TIMEOUT|macro|ALLOC_TIMEOUT
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|MAX_LEN|macro|MAX_LEN
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
+DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|add_nbr|function|static void add_nbr(struct net_if *iface, struct in6_addr *addr, struct net_linkaddr *lladdr)
+DECL|find_last_ipv6_fragment_hbho_frag|function|static void find_last_ipv6_fragment_hbho_frag(void)
+DECL|find_last_ipv6_fragment_hbho_udp|function|static void find_last_ipv6_fragment_hbho_udp(void)
+DECL|find_last_ipv6_fragment_udp|function|static void find_last_ipv6_fragment_udp(void)
+DECL|frag_count|variable|frag_count
+DECL|idx|member|u8_t idx;
+DECL|iface1|variable|iface1
+DECL|iface2|variable|iface2
+DECL|ipv6_first_frag|variable|ipv6_first_frag
+DECL|ipv6_hbho_frag|variable|ipv6_hbho_frag
+DECL|ipv6_hbho|variable|ipv6_hbho
+DECL|ipv6_second_frag|variable|ipv6_second_frag
+DECL|ipv6_udp|variable|ipv6_udp
+DECL|ll_addr2|variable|ll_addr2
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|ll_addr|variable|ll_addr
+DECL|mac2_addr|variable|mac2_addr
+DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|net_if_test|struct|struct net_if_test {
+DECL|net_iface1_data|variable|net_iface1_data
+DECL|net_iface2_data|variable|net_iface2_data
+DECL|net_iface_api|variable|net_iface_api
+DECL|net_iface_dev_init|function|static int net_iface_dev_init(struct device *dev)
+DECL|net_iface_get_mac|function|static u8_t *net_iface_get_mac(struct device *dev)
+DECL|net_iface_init|function|static void net_iface_init(struct net_if *iface)
+DECL|pkt_data_len|variable|pkt_data_len
+DECL|pkt_recv_data_len|variable|pkt_recv_data_len
+DECL|recv_ipv6_fragment|function|static void recv_ipv6_fragment(void)
+DECL|send_ipv6_fragment|function|static void send_ipv6_fragment(void)
+DECL|sender_iface|function|static int sender_iface(struct net_if *iface, struct net_pkt *pkt)
+DECL|setup_udp_handler|function|static void setup_udp_handler(const struct in6_addr *raddr, const struct in6_addr *laddr, u16_t remote_port, u16_t local_port)
+DECL|setup|function|static void setup(void)
+DECL|test_failed|variable|test_failed
+DECL|test_main|function|void test_main(void)
+DECL|test_started|variable|test_started
+DECL|udp_data_received|function|static enum net_verdict udp_data_received(struct net_conn *conn, struct net_pkt *pkt, void *user_data)
+DECL|verify_fragment|function|static int verify_fragment(struct net_pkt *pkt)
+DECL|wait_data|variable|wait_data
