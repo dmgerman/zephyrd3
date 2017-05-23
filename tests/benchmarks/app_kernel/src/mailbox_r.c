5141bf5331a527305ea30c741ddde31dc645c228
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2017-05-23 10:57:29 +0530

    tests: benchmark: app_kernel: Return values from kernel APIs are read.
    Static code analysis reported some kernel APIs were used without
    reading the return value. Since the benchmark doesn't need error
    conditions, a simple read of the values followed by a ARG_UNUSED
    is used to handle static code analysis errors.
    
    JIRA: ZEP-2134
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: c0549aae75f11dab85070f459da73e844794417c

--- tests/benchmarks/app_kernel/src/mailbox_r.c
+++ tests/benchmarks/app_kernel/src/mailbox_r.c
@@ -1,2 +1,2 @@
-DECL|mailbox_get|function|int mailbox_get(struct k_mbox *mailbox, int size, int count, unsigned int *time)
+DECL|mailbox_get|function|void mailbox_get(struct k_mbox *mailbox, int size, int count, unsigned int *time)
 DECL|mailrecvtask|function|void mailrecvtask(void)
