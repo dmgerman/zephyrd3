6243dce51800cb4bb91ca8f3061be43d1dd2132b
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-05-23 16:12:09 +0300

    net: http: Add HTTPS client support
    Add HTTPS support into http-client library. The init of the
    HTTPS client connection is different compared to HTTP client,
    but the actual HTTP request sending is using the same API as
    HTTP client.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1b82463deed4de95bb9664953e1f10420b6c2fe0

--- include/net/http.h
+++ include/net/http.h
@@ -17,12 +17,17 @@ DECL|MBEDTLS_EXIT_SUCCESS|macro|MBEDTLS_EXIT_SUCCESS
 DECL|__HTTP_H__|macro|__HTTP_H__
 DECL|body_found|member|u8_t body_found:1;
 DECL|body_start|member|u8_t *body_start;
+DECL|ca_cert|member|mbedtls_x509_crt ca_cert;
 DECL|cb|member|http_response_cb_t cb;
+DECL|cert_cb|member|https_ca_cert_cb_t cert_cb;
 DECL|cert_cb|member|https_server_cert_cb_t cert_cb;
+DECL|cert_host|member|const char *cert_host;
 DECL|cl_present|member|u8_t cl_present:1;
+DECL|conf|member|mbedtls_ssl_config conf;
 DECL|conf|member|mbedtls_ssl_config conf;
 DECL|content_length|member|size_t content_length;
 DECL|content_type_value|member|const char *content_type_value;
+DECL|ctr_drbg|member|mbedtls_ctr_drbg_context ctr_drbg;
 DECL|ctr_drbg|member|mbedtls_ctr_drbg_context ctr_drbg;
 DECL|ctx|member|struct net_context *ctx;
 DECL|data_len|member|size_t data_len;
@@ -31,6 +36,8 @@ DECL|default_url|member|struct http_root_url default_url;
 DECL|dns_id|member|u16_t dns_id;
 DECL|enabled|member|bool enabled;
 DECL|entropy_src_cb|member|https_entropy_src_cb_t entropy_src_cb;
+DECL|entropy_src_cb|member|https_entropy_src_cb_t entropy_src_cb;
+DECL|entropy|member|mbedtls_entropy_context entropy;
 DECL|entropy|member|mbedtls_entropy_context entropy;
 DECL|field_values_ctr|member|u16_t field_values_ctr;
 DECL|field_values|member|field_values[CONFIG_HTTP_HEADER_FIELD_ITEMS];
@@ -55,16 +62,20 @@ DECL|http_server_urls|struct|struct http_server_urls {
 DECL|http_status|member|char http_status[HTTP_STATUS_STR_SIZE];
 DECL|http_url_cb_t|typedef|typedef int (*http_url_cb_t)(struct http_server_ctx *ctx);
 DECL|http_url_flags|enum|enum http_url_flags {
+DECL|https_ca_cert_cb_t|typedef|typedef int (*https_ca_cert_cb_t)(struct http_client_ctx *ctx,
 DECL|https_context|struct|struct https_context {
 DECL|https_entropy_src_cb_t|typedef|typedef int (*https_entropy_src_cb_t)(void *data, unsigned char *output,
 DECL|https_server_cert_cb_t|typedef|typedef int (*https_server_cert_cb_t)(struct http_server_ctx *ctx,
 DECL|https|member|} https;
+DECL|https|member|} https;
+DECL|is_https|member|bool is_https;
 DECL|is_https|member|bool is_https;
 DECL|is_used|member|u8_t is_used;
 DECL|key_len|member|u16_t key_len;
 DECL|key|member|const char *key;
 DECL|local|member|struct sockaddr local;
 DECL|mbedtls_time_t|macro|mbedtls_time_t
+DECL|mbedtls|member|} mbedtls;
 DECL|mbedtls|member|} mbedtls;
 DECL|method|member|enum http_method method;
 DECL|method|member|enum http_method method;
@@ -76,12 +87,16 @@ DECL|parser|member|struct http_parser parser;
 DECL|payload_size|member|u16_t payload_size;
 DECL|payload|member|const char *payload;
 DECL|personalization_data_len|member|size_t personalization_data_len;
+DECL|personalization_data_len|member|size_t personalization_data_len;
+DECL|personalization_data|member|u8_t *personalization_data;
 DECL|personalization_data|member|u8_t *personalization_data;
 DECL|pkey|member|mbedtls_pk_context pkey;
+DECL|pool|member|struct k_mem_pool *pool;
 DECL|pool|member|struct k_mem_pool *pool;
 DECL|processed|member|size_t processed;
 DECL|protocol|member|const char *protocol;
 DECL|receive_cb|member|http_receive_cb_t receive_cb;
+DECL|recv_cb|member|net_context_recv_cb_t recv_cb;
 DECL|recv_cb|member|net_context_recv_cb_t recv_cb;
 DECL|remaining|member|int remaining;
 DECL|remote|member|struct sockaddr remote;
@@ -97,21 +112,29 @@ DECL|rsp|member|} rsp;
 DECL|rx_fifo|member|struct k_fifo rx_fifo;
 DECL|rx_pkt|member|struct net_pkt *rx_pkt;
 DECL|send_data|member|http_send_data_t send_data;
+DECL|send_data|member|http_send_data_t send_data;
 DECL|server|member|const char *server;
 DECL|settings|member|struct http_parser_settings settings;
 DECL|settings|member|struct http_parser_settings settings;
 DECL|srvcert|member|mbedtls_x509_crt srvcert;
 DECL|ssl_ctx|member|struct https_context ssl_ctx;
+DECL|ssl_ctx|member|struct https_context ssl_ctx;
+DECL|ssl|member|mbedtls_ssl_context ssl;
 DECL|ssl|member|mbedtls_ssl_context ssl;
 DECL|stack_size|member|int stack_size;
+DECL|stack_size|member|int stack_size;
+DECL|stack|member|u8_t *stack;
 DECL|stack|member|u8_t *stack;
 DECL|tcp|member|} tcp;
 DECL|thread|member|struct k_thread thread;
+DECL|thread|member|struct k_thread thread;
+DECL|tid|member|k_tid_t tid;
 DECL|tid|member|k_tid_t tid;
 DECL|timeout|member|s32_t timeout;
 DECL|timeout|member|s32_t timeout;
 DECL|timer_cancelled|member|u8_t timer_cancelled;
 DECL|timer|member|struct k_delayed_work timer;
+DECL|tx_fifo|member|struct k_fifo tx_fifo;
 DECL|tx_sem|member|struct k_sem tx_sem;
 DECL|url_len|member|u16_t url_len;
 DECL|urls|member|struct http_root_url urls[CONFIG_HTTP_SERVER_NUM_URLS];
