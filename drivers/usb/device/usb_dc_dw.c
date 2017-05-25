19b4c93983f94ac488f9fd8c96228353c733bfca
Andy Ross <andrew.j.ross@intel.com>
2017-05-25 15:12:33 -0700

    usb_dc_dw: Correctly assign endpoint FIFOs
    The designware hardware in dedicated FIFO mode (which is all we
    support right now for lack of shared-FIFO hardware) has one hardware
    FIFO per IN (i.e. transmit) endpoint.  But it doesn't assign them on
    its own, it's the drivers responsibility to populate the TxFNum field
    of the DIEPCTL registers with integer indices corresponding to the
    desired FIFO.
    
    We weren't doing that, which meant that all IN endpoints were sharing
    the same FIFO zero which is supposed to be dedicated to EP0 control
    transfers.  The net effect is that sometimes outbound transfers would
    be corrupted, showing data from the wrong endpoint.  More often that
    not this would leak from control transfers over to the
    higher-bandwidth bulk endpoints of the application, but occasionally
    you'd see a control transfer itself get borked and the USB device
    would glitch.
    
    Get this right and set the FIFOs properly.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: f591e582117c463ba8a3467110c7c9048d3967be

--- drivers/usb/device/usb_dc_dw.c
+++ drivers/usb/device/usb_dc_dw.c
@@ -8,8 +8,11 @@ DECL|attached|member|u8_t attached;
 DECL|cb|member|usb_dc_ep_callback cb;/* Endpoint callback function */
 DECL|data_len|member|u32_t data_len;
 DECL|ep_ena|member|u8_t ep_ena;
+DECL|fifo_num|member|u8_t fifo_num;
+DECL|fifo_size|member|u8_t fifo_size;
 DECL|in_ep_ctrl|member|struct usb_ep_ctrl_prv in_ep_ctrl[USB_DW_IN_EP_NUM];
 DECL|mps|member|u16_t mps; /* Max ep pkt size */
+DECL|n_tx_fifos|member|int n_tx_fifos;
 DECL|out_ep_ctrl|member|struct usb_ep_ctrl_prv out_ep_ctrl[USB_DW_OUT_EP_NUM];
 DECL|status_cb|member|usb_dc_status_callback status_cb;
 DECL|usb_dc_attach|function|int usb_dc_attach(void)
@@ -37,14 +40,18 @@ DECL|usb_dw_ctrl|variable|usb_dw_ctrl
 DECL|usb_dw_ep_is_enabled|function|static u8_t usb_dw_ep_is_enabled(u8_t ep)
 DECL|usb_dw_ep_is_valid|function|static u8_t usb_dw_ep_is_valid(u8_t ep)
 DECL|usb_dw_ep_set|function|static int usb_dw_ep_set(u8_t ep,u32_t ep_mps, enum usb_dc_ep_type ep_type)
+DECL|usb_dw_flush_tx_fifo|function|static void usb_dw_flush_tx_fifo(int ep)
 DECL|usb_dw_handle_enum_done|function|static void usb_dw_handle_enum_done(void)
 DECL|usb_dw_handle_reset|function|static void usb_dw_handle_reset(void)
 DECL|usb_dw_init|function|static int usb_dw_init(void)
 DECL|usb_dw_isr_handler|function|static void usb_dw_isr_handler(void)
+DECL|usb_dw_num_dev_eps|function|static int usb_dw_num_dev_eps(void)
 DECL|usb_dw_prep_rx|function|static void usb_dw_prep_rx(const u8_t ep, u8_t setup)
 DECL|usb_dw_reg_dump|function|static void usb_dw_reg_dump(void)
 DECL|usb_dw_reg_dump|macro|usb_dw_reg_dump
 DECL|usb_dw_reset|function|static int usb_dw_reset(void)
+DECL|usb_dw_set_fifo|function|static int usb_dw_set_fifo(u8_t ep)
+DECL|usb_dw_tx_fifo_avail|function|static int usb_dw_tx_fifo_avail(int ep)
 DECL|usb_dw_tx|function|static int usb_dw_tx(u8_t ep, const u8_t *const data,u32_t data_len)
 DECL|usb_dw_udelay|function|static inline void usb_dw_udelay(u32_t us)
 DECL|usb_ep_ctrl_prv|struct|struct usb_ep_ctrl_prv {
