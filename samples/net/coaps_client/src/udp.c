af4b1cd0c58b40fe48fd0ac904699a4d3c5544c3
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-05-29 15:44:48 +0300

    samples: net: coaps_client: Fix compile issues
    The coaps_client was bit rotted and did not compile
    correctly.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 5a0b4976d90633d879b17710ba98bcf7ec02d355

--- samples/net/coaps_client/src/udp.c
+++ samples/net/coaps_client/src/udp.c
@@ -4,5 +4,5 @@ DECL|server_addr|variable|server_addr
 DECL|set_destination|function|static void set_destination(struct sockaddr *addr)
 DECL|udp_init|function|int udp_init(struct udp_context *ctx)
 DECL|udp_received|function|static void udp_received(struct net_context *context, struct net_pkt *pkt, int status, void *user_data)
-DECL|udp_rx|function|int udp_rx(void *context, unsigned char *buf, size_t size, u32_t timeout)
+DECL|udp_rx|function|int udp_rx(void *context, unsigned char *buf, size_t size)
 DECL|udp_tx|function|int udp_tx(void *context, const unsigned char *buf, size_t size)
