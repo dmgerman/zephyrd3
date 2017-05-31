f240023c6084b04c99a8cca8bda4eda75d74f70d
Marti Bolivar <marti.bolivar@linaro.org>
2017-05-31 10:11:22 -0400

    stack.h: add missing include guard
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: d6204622af69455ba3a24b12681f6de271a8a5c6

--- include/misc/stack.h
+++ include/misc/stack.h
@@ -1,3 +1,4 @@
+DECL|_MISC_STACK_H_|macro|_MISC_STACK_H_
 DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned int size)
 DECL|stack_analyze|function|static inline void stack_analyze(const char *name, const char *stack, unsigned int size)
 DECL|stack_unused_space_get|function|static inline size_t stack_unused_space_get(const char *stack, size_t size)
