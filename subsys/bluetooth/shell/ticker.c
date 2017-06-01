d3dc18b87d2718d0ec0a2b4ee3ac3241c57abf04
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2017-06-01 08:12:54 +0200

    Bluetooth: shell: Add controller's ticker shell module
    Added shell module for the Bluetooth Controller's ticker
    interfaces.
    
    For now, info command enumerates active tickers' details.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: a3cba8bb908239bcdfc70c6e2cbf11a8323fdacb

---
+++ subsys/bluetooth/shell/ticker.c
@@ -0,0 +1,6 @@
+DECL|TICKERS_MAX|macro|TICKERS_MAX
+DECL|TICKERS_MAX|macro|TICKERS_MAX
+DECL|TICKER_SHELL_MODULE|macro|TICKER_SHELL_MODULE
+DECL|cmd_ticker_info|function|int cmd_ticker_info(int argc, char *argv[])
+DECL|ticker_commands|variable|ticker_commands
+DECL|ticker_op_done|function|static void ticker_op_done(u32_t err, void *context)
