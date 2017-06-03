6a9a4df2236c263530dca29235b93e929a4bfc49
Gil Pitney <gil.pitney@linaro.org>
2017-06-02 17:16:43 -0700

    cc32xx: Redefine CMSIS IRQn_Type enum to unsigned int
    Previously, calling NVIC_SetPriority(IRQn_Type irqn, ....) with
    the NWP interrupt number of 171 caused a hard fault during a
    subsequent svc #0 instruction during _Swap().
    
    GNU compiler is generating a bit extension instruction (sxtb) which
    converts a positive IRQ value argument to a negative value when
    casting to the CMSIS IRQn_Type enum parameter type.
    
    This generates a negative index, which then writes to an SCB
    control register instead of NVIC register, causing a hard
    fault later on.
    
    This issue only occurs when passing interrupt numbers > 0x80
    (eg: 171 (0xab) for the NWP) to the CMSIS NVIC apis.
    
    The solution here is simply to redefine IRQn_Type to be an
    unsigned 32 bit integer, while redefining the CMSIS IRQn_Type
    enum definitions for interrupts less than zero.
    
    Jira: ZEP-1958
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>
    
    Former-commit-id: 1a5537811f3faf7171f282321aa91f8ef1f8dd35

--- arch/arm/soc/ti_simplelink/cc32xx/soc.h
+++ arch/arm/soc/ti_simplelink/cc32xx/soc.h
@@ -1,1 +1,16 @@
-
+DECL|BusFault_IRQn|enumerator|BusFault_IRQn = -11,
+DECL|CMSIS_IRQn_Type|variable|CMSIS_IRQn_Type
+DECL|DebugMonitor_IRQn|enumerator|DebugMonitor_IRQn = -4,
+DECL|HardFault_IRQn|enumerator|HardFault_IRQn = -13,
+DECL|IRQn_Type|typedef|typedef u32_t IRQn_Type;
+DECL|MemoryManagement_IRQn|enumerator|MemoryManagement_IRQn = -12,
+DECL|NonMaskableInt_IRQn|enumerator|NonMaskableInt_IRQn = -14,
+DECL|PendSV_IRQn|enumerator|PendSV_IRQn = -2,
+DECL|Reset_IRQn|enumerator|Reset_IRQn = -15,
+DECL|SVCall_IRQn|enumerator|SVCall_IRQn = -5,
+DECL|SysTick_IRQn|enumerator|SysTick_IRQn = -1,
+DECL|UsageFault_IRQn|enumerator|UsageFault_IRQn = -10,
+DECL|__CM4_REV|macro|__CM4_REV
+DECL|__MPU_PRESENT|macro|__MPU_PRESENT
+DECL|__NVIC_PRIO_BITS|macro|__NVIC_PRIO_BITS
+DECL|__Vendor_SysTickConfig|macro|__Vendor_SysTickConfig
