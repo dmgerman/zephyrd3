48970dbdc3323476ae20fdf70f5ee5b62db5c528
Shiksha Patel <shiksha.patel@nxp.com>
2017-06-04 08:21:42 -0500

    pinmux: pinmux_dev_k64 driver and related references are removed.
    Functionality of a pinmux driver is now a part of a regular
    driver.
    
    Signed-off-by: Shiksha Patel <shiksha.patel@nxp.com>
    
    Former-commit-id: a931c8af0a4e468f101c4a9e4c3975e728d726cc

--- drivers/pinmux/dev/pinmux_dev_k64.c
+++
@@ -1,4 +0,0 @@
-DECL|api_funcs|variable|api_funcs
-DECL|fsl_k64_dev_get|function|static int fsl_k64_dev_get(struct device *dev, u32_t pin,u32_t *func)
-DECL|fsl_k64_dev_set|function|static int fsl_k64_dev_set(struct device *dev, u32_t pin,u32_t func)
-DECL|pinmux_fsl_k64_initialize|function|int pinmux_fsl_k64_initialize(struct device *port)
