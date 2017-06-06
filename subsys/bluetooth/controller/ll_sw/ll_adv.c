d6541fad4ce26942ad1cf82d73871c6b557d40c1
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2017-06-06 21:37:49 +0200

    Bluetooth: controller: Reduce ll_adv struct size
    Remove ll_adv struct members whose contents are stored in
    PDU buffers.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 08f3c629168fa81232afcf270c024f39335fae15

--- subsys/bluetooth/controller/ll_sw/ll_adv.c
+++ subsys/bluetooth/controller/ll_sw/ll_adv.c
@@ -1,6 +1,4 @@
-DECL|adv_addr|member|u8_t adv_addr[BDADDR_SIZE];
 DECL|chl_map|member|u8_t chl_map:3;
-DECL|direct_addr|member|u8_t direct_addr[BDADDR_SIZE];
 DECL|filter_policy|member|u8_t filter_policy:2;
 DECL|interval|member|u16_t interval;
 DECL|ll_adv_data_set|function|void ll_adv_data_set(u8_t len, u8_t const *const data)
@@ -8,6 +6,3 @@ DECL|ll_adv_enable|function|u32_t ll_adv_enable(u8_t enable)
 DECL|ll_adv_params_set|function|u32_t ll_adv_params_set(u16_t interval, u8_t adv_type,u8_t own_addr_type, u8_t direct_addr_type, u8_t const *const direct_addr, u8_t chl_map, u8_t filter_policy)
 DECL|ll_adv|variable|ll_adv
 DECL|ll_scan_data_set|function|void ll_scan_data_set(u8_t len, u8_t const *const data)
-DECL|pdu_adv_type|member|u8_t pdu_adv_type:4;
-DECL|rx_addr|member|u8_t rx_addr:1;
-DECL|tx_addr|member|u8_t tx_addr:1;
