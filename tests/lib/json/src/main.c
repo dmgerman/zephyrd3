6e003b9283f6f6d2ca4f459090e620d7ff736cd5
Marti Bolivar <marti.bolivar@linaro.org>
2017-06-07 14:13:15 -0400

    tests: json: add tests for arrays of objects
    Add tests for the newly-added JSON_OBJ_DESCR_OBJ_ARRAY. These pass.
    
    Note that this also adds test coverage for decoding an array of
    maximum length, to avoid regressing the recently-introduced fix for
    this edge case.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: 2a29bed8bb40b6bf8a087e3e8aac34a47048a173

--- tests/lib/json/src/main.c
+++ tests/lib/json/src/main.c
@@ -1,11 +1,19 @@
 DECL|another_array_len|member|size_t another_array_len;
 DECL|another_array|member|int another_array[10]; /* JSON: "another-array" */
 DECL|another_bxxl|member|bool another_bxxl; /* JSON field: "another_b!@l" */
+DECL|elements|member|struct elt elements[10];
+DECL|elt_descr|variable|elt_descr
+DECL|elt|struct|struct elt {
+DECL|height|member|int height;
 DECL|if_|member|bool if_; /* JSON: "if" */
+DECL|name|member|const char *name;
 DECL|nested_bool|member|bool nested_bool;
 DECL|nested_descr|variable|nested_descr
 DECL|nested_int|member|int nested_int;
 DECL|nested_string|member|const char *nested_string;
+DECL|num_elements|member|size_t num_elements;
+DECL|obj_array_descr|variable|obj_array_descr
+DECL|obj_array|struct|struct obj_array {
 DECL|some_array_len|member|size_t some_array_len;
 DECL|some_array|member|int some_array[16];
 DECL|some_bool|member|bool some_bool;
@@ -24,6 +32,8 @@ DECL|test_json_invalid_unicode|function|static void test_json_invalid_unicode(vo
 DECL|test_json_item_wrong_type|function|static void test_json_item_wrong_type(void)
 DECL|test_json_key_not_in_descr|function|static void test_json_key_not_in_descr(void)
 DECL|test_json_missing_quote|function|static void test_json_missing_quote(void)
+DECL|test_json_obj_arr_decoding|function|static void test_json_obj_arr_decoding(void)
+DECL|test_json_obj_arr_encoding|function|static void test_json_obj_arr_encoding(void)
 DECL|test_json_wrong_token|function|static void test_json_wrong_token(void)
 DECL|test_main|function|void test_main(void)
 DECL|test_nested|struct|struct test_nested {
