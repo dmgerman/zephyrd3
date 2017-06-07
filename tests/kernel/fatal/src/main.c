7e892de8dbd5a75a44b395556eedd0a7508440fd
Andrew Boie <andrew.p.boie@intel.com>
2017-06-07 10:57:01 -0700

    tests: fatal: increase coverage
    - _SysFatalErrorHandler is supposed to be user-overridable.
    The test case now installs its own handler to show that this
    has happened properly.
    
    - Use TC_PRINT() TC_ERROR() macros
    
    - Since we have out own _SysFatalErrorHandler, show that
    k_panic() works
    
    - Show that _SysFatalErrorHandler gets invoked with the expected
    reason code for some of the scenarios.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 000429c069cd8732e1f076e9eb511356c10a02a6

--- tests/kernel/fatal/src/main.c
+++ tests/kernel/fatal/src/main.c
@@ -3,11 +3,14 @@ DECL|OVERFLOW_STACKSIZE|macro|OVERFLOW_STACKSIZE
 DECL|OVERFLOW_STACKSIZE|macro|OVERFLOW_STACKSIZE
 DECL|PRIORITY|macro|PRIORITY
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|alt_stack|variable|alt_stack
 DECL|alt_thread1|function|void alt_thread1(void)
 DECL|alt_thread2|function|void alt_thread2(void)
+DECL|alt_thread3|function|void alt_thread3(void)
 DECL|alt_thread|variable|alt_thread
 DECL|blow_up_stack|function|void blow_up_stack(void)
+DECL|crash_reason|variable|crash_reason
 DECL|main|function|void main(void)
 DECL|overflow_stack|variable|overflow_stack
 DECL|rv|variable|rv
