8ac77221dbee2fc4759419abf4362a8b1304ee72
Andrew Boie <andrew.p.boie@intel.com>
2017-06-07 09:33:16 -0700

    stack_sentinel: change cooperative check
    One of the stack sentinel policies was to check the sentinel
    any time a cooperative context switch is done (i.e, _Swap is
    called).
    
    This was done by adding a hook to _check_stack_sentinel in
    every arch's __swap function.
    
    This way is cleaner as we just have the hook in one inline
    function rather than implemented in several different assembly
    dialects.
    
    The check upon interrupt is now made unconditionally rather
    than checking if we are calling __swap, since the check now
    is only called on cooperative _Swap(). The interrupt is always
    serviced first.
    
    Issue: ZEP-2244
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: ae1a75b82e97d6bbc8d85f44b6bfecf6b5ede538

--- kernel/include/nano_internal.h
+++ kernel/include/nano_internal.h
@@ -1,8 +1,6 @@
 DECL|K_NUM_PRIORITIES|macro|K_NUM_PRIORITIES
 DECL|K_NUM_PRIO_BITMAPS|macro|K_NUM_PRIO_BITMAPS
 DECL|_NANO_INTERNAL__H_|macro|_NANO_INTERNAL__H_
-DECL|_Swap|macro|_Swap
-DECL|_Swap|macro|_Swap
+DECL|_Swap|function|static inline unsigned int _Swap(unsigned int key)
 DECL|_data_copy|function|static inline void _data_copy(void)
 DECL|_thread_monitor_exit|macro|_thread_monitor_exit
-DECL|_time_slice_swap|function|static inline unsigned int _time_slice_swap(unsigned int key)
