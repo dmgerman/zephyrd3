a55b7eefcc6a1ef27bcab4416c99b76b33e7d148
Andrew Boie <andrew.p.boie@intel.com>
2017-06-07 10:40:29 -0700

    arches: declare _SysFatalErrorHandler __weak
    This function is intended to be easily overridable by applications.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 998f90544540ee563d69369a3dfa80944f2269bf

--- arch/arm/core/sys_fatal_error_handler.c
+++ arch/arm/core/sys_fatal_error_handler.c
@@ -1,1 +1,1 @@
-DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
+DECL|_SysFatalErrorHandler|function|void __weak _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
