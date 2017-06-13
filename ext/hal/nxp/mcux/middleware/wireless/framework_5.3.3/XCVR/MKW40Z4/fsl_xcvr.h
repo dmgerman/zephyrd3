ab1b9dc63019fd211dcdef5272645c835ea2aafc
Bogdan Davidoaia <bogdan.davidoaia@linaro.org>
2017-06-14 00:44:01 +0300

    ext: mcux: add minimal v2.2 interface for KW40Z XCVR driver
    Add minimal interface for the KW40Z XCVR driver to match the one offered
    in MCUX v2.2 by the KW41Z XCVR driver.
    
    This is needed to reuse the KW41Z IEEE 802.15.4 driver on KW40Z.
    
    Signed-off-by: Bogdan Davidoaia <bogdan.davidoaia@linaro.org>
    
    Former-commit-id: af98f0a806b4453cd27e68b22723589aa4a438a9

---
+++ ext/hal/nxp/mcux/middleware/wireless/framework_5.3.3/XCVR/MKW40Z4/fsl_xcvr.h
@@ -0,0 +1,18 @@
+DECL|DR_1MBPS|enumerator|DR_1MBPS = 0, /* Must match bit assignment in BITRATE field */
+DECL|DR_250KBPS|enumerator|DR_250KBPS = 2, /* Must match bit assignment in BITRATE field */
+DECL|DR_2MBPS|enumerator|DR_2MBPS = 3, /* Must match bit assignment in BITRATE field */
+DECL|DR_500KBPS|enumerator|DR_500KBPS = 1, /* Must match bit assignment in BITRATE field */
+DECL|DR_UNASSIGNED|enumerator|DR_UNASSIGNED = 4, /* Must match bit assignment in BITRATE field */
+DECL|Radio_1_IRQn|macro|Radio_1_IRQn
+DECL|XCVR_Init|function|xcvrStatus_t XCVR_Init(radio_mode_t radio_mode, data_rate_t data_rate)
+DECL|XCVR_TSM_END_OF_SEQ_END_OF_RX_WU_MASK|macro|XCVR_TSM_END_OF_SEQ_END_OF_RX_WU_MASK
+DECL|XCVR_TSM_END_OF_SEQ_END_OF_RX_WU_SHIFT|macro|XCVR_TSM_END_OF_SEQ_END_OF_RX_WU_SHIFT
+DECL|XCVR_TSM_END_OF_SEQ_END_OF_TX_WU_MASK|macro|XCVR_TSM_END_OF_SEQ_END_OF_TX_WU_MASK
+DECL|XCVR_TSM_END_OF_SEQ_END_OF_TX_WU_SHIFT|macro|XCVR_TSM_END_OF_SEQ_END_OF_TX_WU_SHIFT
+DECL|XCVR_TSM|macro|XCVR_TSM
+DECL|ZIGBEE_MODE|macro|ZIGBEE_MODE
+DECL|ZLL_RX_FRAME_FILTER_FRM_VER_FILTER_MASK|macro|ZLL_RX_FRAME_FILTER_FRM_VER_FILTER_MASK
+DECL|ZLL_RX_FRAME_FILTER_FRM_VER_FILTER|macro|ZLL_RX_FRAME_FILTER_FRM_VER_FILTER
+DECL|_FSL_XCVR_H_|macro|_FSL_XCVR_H_
+DECL|_data_rate|enum|typedef enum _data_rate
+DECL|data_rate_t|typedef|} data_rate_t;
