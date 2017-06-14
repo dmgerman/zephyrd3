0e5a3bb0dc5f986caec96e9ecd77ec1e4c7bd1ba
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-06-14 21:50:55 +0300

    tests: net: Add test for Sockets API UDP socket/bind/connect/send/recv
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: b650fa8a7282e5b07993a3f750cc88686e04bf86

---
+++ tests/net/socket/udp/src/main.c
@@ -0,0 +1,5 @@
+DECL|BUF_AND_SIZE|macro|BUF_AND_SIZE
+DECL|STRLEN|macro|STRLEN
+DECL|TEST_STR_SMALL|macro|TEST_STR_SMALL
+DECL|test_main|function|void test_main(void)
+DECL|test_send_recv_2_sock|function|void test_send_recv_2_sock(void)
