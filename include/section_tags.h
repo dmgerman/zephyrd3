58372533790f155896a71642ddc0be40b60b0ae1
Geoffrey Le Gourriérec <geoffrey.legourrierec@smile.fr>
2017-06-15 14:05:45 +0200

    arm: Modify linker script to accomodate need for flash footer.
    The porting of the TI CC2650 SoC introduces the need to
    write a specific configuration area (CCFG) at the end of the
    flash. It is read by the bootloader ROM of the SoC.
    
    For now, this is a quick hack and not a generic solution;
    similar needs may arise with other hardware.
    
    Signed-off-by: Geoffrey Le Gourriérec <geoffrey.legourrierec@smile.fr>
    
    Former-commit-id: 4ecab603d3da093809efce52a8efd0465d4c69b6

--- include/section_tags.h
+++ include/section_tags.h
@@ -2,4 +2,5 @@ DECL|__irq_vector_table|macro|__irq_vector_table
 DECL|__kinetis_flash_config_section|macro|__kinetis_flash_config_section
 DECL|__noinit|macro|__noinit
 DECL|__sw_isr_table|macro|__sw_isr_table
+DECL|__ti_ccfg_section|macro|__ti_ccfg_section
 DECL|_section_tags__h_|macro|_section_tags__h_
