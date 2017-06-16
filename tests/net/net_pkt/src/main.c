1d41b8921b8740edcead7bb56bd52768e67d4a25
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-06-16 13:14:24 +0300

    net: pkt: Fix net_pkt_split()
    The net_pkt_split() was incorrectly checking fragA pointer
    even before it was allocated.
    
    The unit test is fixed and converted to ztest.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 72567677e79dc469ec77d9d0bc37372b782be4c4

--- tests/net/net_pkt/src/main.c
+++ tests/net/net_pkt/src/main.c
@@ -1,7 +1,10 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
 DECL|FRAGA|macro|FRAGA
 DECL|FRAGB|macro|FRAGB
 DECL|FRAG_COUNT|macro|FRAG_COUNT
 DECL|LL_RESERVE|macro|LL_RESERVE
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|TEST_FRAG_COUNT|macro|TEST_FRAG_COUNT
 DECL|__packed|variable|__packed
@@ -18,14 +21,12 @@ DECL|dst|member|struct in6_addr dst;
 DECL|example_data|variable|example_data
 DECL|flow|member|u16_t flow;
 DECL|frag_data|variable|frag_data
-DECL|hexdump|function|static void hexdump(const char *str, const u8_t *packet, size_t length)
 DECL|hop_limit|member|u8_t hop_limit;
 DECL|icmp_hdr|struct|struct icmp_hdr {
 DECL|ipv6_hdr|struct|struct ipv6_hdr {
 DECL|len|member|u16_t len;
 DECL|len|member|u8_t len[2];
 DECL|linearize|function|static void linearize(struct net_pkt *pkt, char *buffer, int len)
-DECL|main|function|void main(void)
 DECL|net_pkt_is_compact|function|static bool net_pkt_is_compact(struct net_pkt *pkt)
 DECL|nexthdr|member|u8_t nexthdr;
 DECL|sample_data|variable|sample_data
@@ -33,12 +34,13 @@ DECL|src_port|member|u16_t src_port;
 DECL|src|member|struct in6_addr src;
 DECL|tcflow|member|u8_t tcflow;
 DECL|test_data|variable|test_data
-DECL|test_fragment_compact|function|static int test_fragment_compact(void)
-DECL|test_fragment_copy|function|static int test_fragment_copy(void)
-DECL|test_fragment_split|function|static int test_fragment_split(void)
-DECL|test_ipv6_multi_frags|function|static int test_ipv6_multi_frags(void)
-DECL|test_pkt_read_append|function|static int test_pkt_read_append(void)
-DECL|test_pkt_read_write_insert|function|static int test_pkt_read_write_insert(void)
+DECL|test_fragment_compact|function|static void test_fragment_compact(void)
+DECL|test_fragment_copy|function|static void test_fragment_copy(void)
+DECL|test_fragment_split|function|static void test_fragment_split(void)
+DECL|test_ipv6_multi_frags|function|static void test_ipv6_multi_frags(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_pkt_read_append|function|static void test_pkt_read_append(void)
+DECL|test_pkt_read_write_insert|function|static void test_pkt_read_write_insert(void)
 DECL|test_rw_long|variable|test_rw_long
 DECL|test_rw_short|variable|test_rw_short
 DECL|type|member|u8_t type;
