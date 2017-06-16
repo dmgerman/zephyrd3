62ea548747dc0c1a54b483f401fada23b30a4ea9
Geoffrey Le Gourriérec <geoffrey.legourrierec@smile.fr>
2017-06-16 17:42:07 +0200

    cc2650: Add pinmux driver.
    Signed-off-by: Geoffrey Le Gourriérec <geoffrey.legourrierec@smile.fr>
    
    Former-commit-id: e8a766830f25d76716b4c9443cc4dffee392d913

---
+++ drivers/pinmux/pinmux_cc2650.c
@@ -0,0 +1,7 @@
+DECL|IOCFG_REG|macro|IOCFG_REG
+DECL|pinmux_cc2650_funcs|variable|pinmux_cc2650_funcs
+DECL|pinmux_cc2650_get|function|static int pinmux_cc2650_get(struct device *dev, u32_t pin, u32_t *func)
+DECL|pinmux_cc2650_init|function|static int pinmux_cc2650_init(struct device *dev)
+DECL|pinmux_cc2650_input|function|static int pinmux_cc2650_input(struct device *dev, u32_t pin,u8_t func)
+DECL|pinmux_cc2650_pullup|function|static int pinmux_cc2650_pullup(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_cc2650_set|function|static int pinmux_cc2650_set(struct device *dev, u32_t pin, u32_t func)
