12c4c7d6a775fde12b747cceab8536e8827c9796
Geoffrey Le Gourriérec <geoffrey.legourrierec@smile.fr>
2017-06-16 17:42:03 +0200

    arm: Add support for TI's CC2650 SoC.
    Add support in arch/arm/soc/ti_simplelink, along with support
    for CC32xx SoC.
    
    Signed-off-by: Geoffrey Le Gourriérec <geoffrey.legourrierec@smile.fr>
    
    Former-commit-id: fdf178577568352c97c004844b00e1607d4a1a6d

---
+++ arch/arm/soc/ti_simplelink/cc2650/soc.c
@@ -0,0 +1,11 @@
+DECL|CCFG_SIZE|macro|CCFG_SIZE
+DECL|bit_is_set|function|int bit_is_set(u32_t reg, u8_t bit)
+DECL|clkloadctl|variable|clkloadctl
+DECL|gpioclkgr|variable|gpioclkgr
+DECL|pdctl0|variable|pdctl0
+DECL|pdstat0|variable|pdstat0
+DECL|secdmaclkgr|variable|secdmaclkgr
+DECL|setup_modules_prcm|function|static void setup_modules_prcm(void)
+DECL|ti_cc2650|function|static int ti_cc2650(struct device *dev)
+DECL|ti_ccfg|variable|ti_ccfg
+DECL|uartclkgr|variable|uartclkgr
