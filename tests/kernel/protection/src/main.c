ef8d77fe09a23f66f63915bfb5b02520a1c22559
Stephen Smalley <sds@tycho.nsa.gov>
2017-06-20 08:54:24 -0400

    tests: Add a self-protection test suite
    Add a self-protection test suite with a set of tests
    to check whether one can overwrite read-only data
    and text, and whether one can execute from data,
    stack, or heap buffers.  These tests are modeled after
    a subset of the lkdtm tests in the Linux kernel.
    
    These tests have twice caught bugs in the Zephyr NXP MPU
    driver, once during initial testing/review of the code
    (in its earliest forms on gerrit, reported to the original
    author there) and most recently the regression introduced
    by commit d1697cb7f6e2a456 [formerly bacbea6e21275bf5] ("arm: nxp: mpu: Rework handling
    of region descriptor 0"), which was fixed by
    commit a8aa9d4f3dbbe8 ("arm: nxp: mpu: Fix region descriptor
    0 attributes") after being reported.
    
    This is intended to be a testsuite of self-protection features
    rather than just a test of MPU functionality.  It is envisioned
    that these tests will be expanded to cover a wider range of
    protection features beyond just memory protection, and the
    current tests are independent of any particular enforcement
    mechanism (e.g. MPU, MMU, or other).
    
    The tests are intended to be cross-platform, and have been
    built and run on both x86- and ARM-based boards.  The tests
    currently fail on x86-based boards, but this is an accurate
    reflection of current protections and should change as MMU
    support arrives.
    
    The tests leverage the ztest framework, making them suitable
    for incorporation into automated regression testing for Zephyr.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    
    Former-commit-id: c997577670f4a48d923360fbf182eea694307c96

---
+++ tests/kernel/protection/src/main.c
@@ -0,0 +1,16 @@
+DECL|DO_BARRIERS|macro|DO_BARRIERS
+DECL|DO_BARRIERS|macro|DO_BARRIERS
+DECL|FUNC_TO_PTR|macro|FUNC_TO_PTR
+DECL|FUNC_TO_PTR|macro|FUNC_TO_PTR
+DECL|INFO|macro|INFO
+DECL|PTR_TO_FUNC|macro|PTR_TO_FUNC
+DECL|PTR_TO_FUNC|macro|PTR_TO_FUNC
+DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
+DECL|add_one|function|static int __attribute__((noinline)) add_one(int i)
+DECL|exec_data|function|static void exec_data(void)
+DECL|exec_heap|function|static void exec_heap(void)
+DECL|exec_stack|function|static void exec_stack(void)
+DECL|execute_from_buffer|function|static void execute_from_buffer(u8_t *dst)
+DECL|test_main|function|void test_main(void *unused1, void *unused2, void *unused3)
+DECL|write_ro|function|static void write_ro(void)
+DECL|write_text|function|static void write_text(void)
---
+++ tests/kernel/protection/src/targets.c
@@ -0,0 +1,2 @@
+DECL|overwrite_target|function|int overwrite_target(int i)
+DECL|rodata_var|variable|rodata_var
