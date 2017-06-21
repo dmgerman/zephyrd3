ae104318107b625ff1e8a803bcd7ec7a63bc4aa5
Adam Podogrocki <adam.apodogrocki@rndity.com>
2017-06-21 10:36:18 +0200

    iwdg: make independent watchdog driver compliant with STM32Cube LL API
    Appropriate inclusion done for F1/F3/F4/L4 families in SoC tree.
    
    JIRA: ZEP-2347
    
    Signed-off-by: Adam Podogrocki <adam.podogrocki@rndity.com>
    
    Former-commit-id: 6d4da19602dbcf68b99347bf6983712be79e397d

--- drivers/watchdog/iwdg_stm32.c
+++ drivers/watchdog/iwdg_stm32.c
@@ -1,9 +1,13 @@
-DECL|AS_IWDG|macro|AS_IWDG
-DECL|__iwdg_stm32_prescaler|function|static inline int __iwdg_stm32_prescaler(int setting)
+DECL|IS_IWDG_TIMEOUT|macro|IS_IWDG_TIMEOUT
+DECL|IWDG_DEFAULT_TIMEOUT|macro|IWDG_DEFAULT_TIMEOUT
+DECL|IWDG_TIMEOUT_MAX|macro|IWDG_TIMEOUT_MAX
+DECL|IWDG_TIMEOUT_MIN|macro|IWDG_TIMEOUT_MIN
 DECL|iwdg_stm32_api|variable|iwdg_stm32_api
+DECL|iwdg_stm32_convert_timeout|function|static void iwdg_stm32_convert_timeout(u32_t timeout, u32_t *prescaler, u32_t *reload)
+DECL|iwdg_stm32_dev_data|variable|iwdg_stm32_dev_data
 DECL|iwdg_stm32_disable|function|static void iwdg_stm32_disable(struct device *dev)
 DECL|iwdg_stm32_enable|function|static void iwdg_stm32_enable(struct device *dev)
-DECL|iwdg_stm32_get_config|function|static void iwdg_stm32_get_config(struct device *dev,struct wdt_config *config)
+DECL|iwdg_stm32_get_config|function|static void iwdg_stm32_get_config(struct device *dev, struct wdt_config *config)
 DECL|iwdg_stm32_init|function|static int iwdg_stm32_init(struct device *dev)
 DECL|iwdg_stm32_reload|function|static void iwdg_stm32_reload(struct device *dev)
-DECL|iwdg_stm32_set_config|function|static int iwdg_stm32_set_config(struct device *dev,struct wdt_config *config)
+DECL|iwdg_stm32_set_config|function|static int iwdg_stm32_set_config(struct device *dev, struct wdt_config *config)
--- drivers/watchdog/iwdg_stm32.h
+++ drivers/watchdog/iwdg_stm32.h
@@ -1,30 +1,5 @@
-DECL|STM32_IWDG_KR_RELOAD|macro|STM32_IWDG_KR_RELOAD
-DECL|STM32_IWDG_KR_START|macro|STM32_IWDG_KR_START
-DECL|STM32_IWDG_KR_UNLOCK|macro|STM32_IWDG_KR_UNLOCK
+DECL|IWDG_STM32_DATA|macro|IWDG_STM32_DATA
+DECL|IWDG_STM32_STRUCT|macro|IWDG_STM32_STRUCT
+DECL|Instance|member|IWDG_TypeDef *Instance;
 DECL|_STM32_IWDG_H_|macro|_STM32_IWDG_H_
-DECL|__iwdg_kr|union|union __iwdg_kr {
-DECL|__iwdg_pr|union|union __iwdg_pr {
-DECL|__iwdg_rlr|union|union __iwdg_rlr {
-DECL|__iwdg_sr|union|union __iwdg_sr {
-DECL|bit|member|} bit;
-DECL|bit|member|} bit;
-DECL|bit|member|} bit;
-DECL|bit|member|} bit;
-DECL|iwdg_stm32|struct|struct iwdg_stm32 {
-DECL|key|member|u16_t key;
-DECL|kr|member|union __iwdg_kr kr;
-DECL|pr|member|u32_t pr :3 __packed;
-DECL|pr|member|union __iwdg_pr pr;
-DECL|pvu|member|u32_t pvu :1 __packed;
-DECL|rlr|member|union __iwdg_rlr rlr;
-DECL|rl|member|u32_t rl :12 __packed;
-DECL|rsvd__12_31|member|u32_t rsvd__12_31 :20 __packed;
-DECL|rsvd__2_31|member|u32_t rsvd__2_31 :30 __packed;
-DECL|rsvd__3_31|member|u32_t rsvd__3_31 :29 __packed;
-DECL|rsvd|member|u16_t rsvd;
-DECL|rvu|member|u32_t rvu :1 __packed;
-DECL|sr|member|union __iwdg_sr sr;
-DECL|val|member|u32_t val;
-DECL|val|member|u32_t val;
-DECL|val|member|u32_t val;
-DECL|val|member|u32_t val;
+DECL|iwdg_stm32_data|struct|struct iwdg_stm32_data {
