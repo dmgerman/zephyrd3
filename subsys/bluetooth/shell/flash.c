a69c316fa136ecead0298af0b0178eb2ddba8466
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2017-06-22 09:59:16 +0200

    bluetooth: shell: add module for testing NRF5x flash driver
    Intention is to test flash driver along with BLE radio.
    Added flash shell module with commands for erase, write-check, read and
    co-operation with radio stress test.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    
    Former-commit-id: 9f964ba8d301d794ece9d2f44395ca2b2d0d3737

---
+++ subsys/bluetooth/shell/flash.c
@@ -0,0 +1,9 @@
+DECL|BUF_ARRAY_CNT|macro|BUF_ARRAY_CNT
+DECL|FLASH_SHELL_MODULE|macro|FLASH_SHELL_MODULE
+DECL|TEST_ARR_SIZE|macro|TEST_ARR_SIZE
+DECL|cmd_erase|function|static int cmd_erase(int argc, char *argv[])
+DECL|cmd_flash|function|static int cmd_flash(int argc, char *argv[])
+DECL|cmd_read|function|static int cmd_read(int argc, char *argv[])
+DECL|cmd_test|function|static int cmd_test(int argc, char *argv[])
+DECL|flash_commands|variable|flash_commands
+DECL|test_arr|variable|test_arr
