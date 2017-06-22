bdb8f758f2fdecc6b836ca47b2d70e36649039c6
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-06-22 16:23:39 +0300

    ext: lib: mbedtls: Enable and set heap size at device startup
    As the mbedtls heap is global for the whole device, enable it
    during device startup if configured so. The heap size can be
    set in config file. There is no default value for the heap as
    that depends very much on application needs.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: a9c0a3f9eb036f14938f32ff9853134625b33a5b

---
+++ ext/lib/crypto/mbedtls/zephyr_init.c
@@ -0,0 +1,4 @@
+DECL|_mbedtls_heap|variable|_mbedtls_heap
+DECL|_mbedtls_init|function|static int _mbedtls_init(struct device *device)
+DECL|init_heap|function|static void init_heap(void)
+DECL|init_heap|macro|init_heap
