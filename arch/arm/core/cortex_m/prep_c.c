bfc8d695fce8649ec7d52b44122a854c577639e3
Xiaorui Hu <xiaorui.hu@linaro.org>
2017-06-23 11:02:50 +0800

    arm: armv6-m: Support relocating vector table
    An abnormal crash was encountered in ARMv6-M SoCs that don't have flash
    starting at 0.  With Zephyr OS the reason for this crash is that, on
    ARMv6-M the system requires an exception vector table at the 0 address.
    
    We implement the relocate_vector_table function to move the vector table
    code to address 0 on systems which don't have the start of code already
    at 0.
    
    [kumar.gala: reworderd commit message, tweaked how we check if we need
     to copy vector table]
    
    Signed-off-by: Xiaorui Hu <xiaorui.hu@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: eb48a0a73c11c2a9cd4c3c91864ca4e0cf52dae8

--- arch/arm/core/cortex_m/prep_c.c
+++ arch/arm/core/cortex_m/prep_c.c
@@ -1,7 +1,8 @@
 DECL|VECTOR_ADDRESS|macro|VECTOR_ADDRESS
 DECL|VECTOR_ADDRESS|macro|VECTOR_ADDRESS
+DECL|VECTOR_ADDRESS|macro|VECTOR_ADDRESS
 DECL|_PrepC|function|void _PrepC(void)
 DECL|enable_floating_point|function|static inline void enable_floating_point(void)
 DECL|enable_floating_point|function|static inline void enable_floating_point(void)
 DECL|relocate_vector_table|function|static inline void relocate_vector_table(void)
-DECL|relocate_vector_table|function|static inline void relocate_vector_table(void) { /* do nothing */ }
+DECL|relocate_vector_table|function|static inline void relocate_vector_table(void)
