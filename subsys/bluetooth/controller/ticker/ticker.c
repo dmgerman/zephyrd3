6036fdd24e637cb5cd1e53c035774b69784d61d5
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2017-06-25 21:47:46 +0200

    Bluetooth: controller: Coding style and refactoring
    Refactoring of ticker implementation to conform to Zephyr
    coding style.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 07270e52ba454a02253ebf8a661ee1f169abfc68

--- subsys/bluetooth/controller/ticker/ticker.c
+++ subsys/bluetooth/controller/ticker/ticker.c
@@ -38,7 +38,6 @@ DECL|node|member|struct ticker_node *node;
 DECL|op_context|member|void *op_context;
 DECL|op|member|enum ticker_user_op_type op;
 DECL|params|member|} params;
-DECL|prepare_ticks_to_expire|function|static void prepare_ticks_to_expire(struct ticker_node *ticker, u32_t ticks_current, u32_t ticks_at_start)
 DECL|remainder_current|member|u32_t remainder_current;
 DECL|remainder_periodic|member|u32_t remainder_periodic;
 DECL|remainder_periodic|member|u32_t remainder_periodic;
@@ -63,23 +62,28 @@ DECL|ticker_instance1_sched|function|static void ticker_instance1_sched(u8_t cal
 DECL|ticker_instance|struct|struct ticker_instance {
 DECL|ticker_job_compare_update|function|static inline void ticker_job_compare_update(struct ticker_instance *instance, u8_t ticker_id_old_head)
 DECL|ticker_job_idle_get|function|u32_t ticker_job_idle_get(u8_t instance_index, u8_t user_id, ticker_op_func fp_op_func, void *op_context)
+DECL|ticker_job_insert|function|static inline u32_t ticker_job_insert(struct ticker_instance *instance, u8_t id_insert, struct ticker_node *ticker, u8_t *insert_head)
 DECL|ticker_job_list_inquire|function|static inline void ticker_job_list_inquire(struct ticker_instance *instance)
 DECL|ticker_job_list_insert|function|static inline void ticker_job_list_insert(struct ticker_instance *instance, u8_t insert_head)
 DECL|ticker_job_list_manage|function|static inline u8_t ticker_job_list_manage(struct ticker_instance *instance, u32_t ticks_elapsed, u8_t *insert_head)
+DECL|ticker_job_node_manage|function|static inline void ticker_job_node_manage(struct ticker_instance *instance, struct ticker_node *ticker, struct ticker_user_op *user_op, u32_t ticks_elapsed, u8_t *insert_head)
 DECL|ticker_job_node_update|function|static inline void ticker_job_node_update(struct ticker_node *ticker, struct ticker_user_op *user_op, u32_t ticks_current, u32_t ticks_elapsed, u8_t *insert_head)
+DECL|ticker_job_op_cb|function|static void ticker_job_op_cb(struct ticker_user_op *user_op, u32_t status)
+DECL|ticker_job_op_inquire|function|static inline void ticker_job_op_inquire(struct ticker_instance *instance, struct ticker_user_op *uop)
+DECL|ticker_job_op_start|function|static inline void ticker_job_op_start(struct ticker_node *ticker, struct ticker_user_op *user_op, u32_t ticks_current)
 DECL|ticker_job_sched|function|void ticker_job_sched(u8_t instance_index, u8_t user_id)
-DECL|ticker_job_worker_bottom_half|function|static inline void ticker_job_worker_bottom_half(struct ticker_instance *instance, u32_t ticks_previous, u32_t ticks_elapsed, u8_t *insert_head)
+DECL|ticker_job_worker_bh|function|static inline void ticker_job_worker_bh(struct ticker_instance *instance,u32_t ticks_previous, u32_t ticks_elapsed, u8_t *insert_head)
 DECL|ticker_job|function|static inline void ticker_job(struct ticker_instance *instance)
-DECL|ticker_next_slot_get|function|u32_t ticker_next_slot_get(u8_t instance_index, u8_t user_id, u8_t *_ticker_id, u32_t *ticks_current, u32_t *ticks_to_expire, ticker_op_func fp_op_func, void *op_context)
+DECL|ticker_next_slot_get|function|u32_t ticker_next_slot_get(u8_t instance_index, u8_t user_id, u8_t *ticker_id, u32_t *ticks_current, u32_t *ticks_to_expire, ticker_op_func fp_op_func, void *op_context)
 DECL|ticker_node|struct|struct ticker_node {
-DECL|ticker_remainder_decrement|function|static u8_t ticker_remainder_decrement(struct ticker_node *ticker)
-DECL|ticker_remainder_increment|function|static u8_t ticker_remainder_increment(struct ticker_node *ticker)
-DECL|ticker_start|function|u32_t ticker_start(u8_t instance_index, u8_t user_id, u8_t _ticker_id, u32_t ticks_anchor, u32_t ticks_first, u32_t ticks_periodic, u32_t remainder_periodic, u16_t lazy, u16_t ticks_slot, ticker_timeout_func ticker_timeout_func, void *context, ticker_op_func fp_op_func, void *op_context)
-DECL|ticker_stop|function|u32_t ticker_stop(u8_t instance_index, u8_t user_id, u8_t _ticker_id, ticker_op_func fp_op_func, void *op_context)
+DECL|ticker_remainder_dec|function|static u8_t ticker_remainder_dec(struct ticker_node *ticker)
+DECL|ticker_remainder_inc|function|static u8_t ticker_remainder_inc(struct ticker_node *ticker)
+DECL|ticker_start|function|u32_t ticker_start(u8_t instance_index, u8_t user_id, u8_t ticker_id, u32_t ticks_anchor, u32_t ticks_first, u32_t ticks_periodic, u32_t remainder_periodic, u16_t lazy, u16_t ticks_slot, ticker_timeout_func ticker_timeout_func, void *context, ticker_op_func fp_op_func, void *op_context)
+DECL|ticker_stop|function|u32_t ticker_stop(u8_t instance_index, u8_t user_id, u8_t ticker_id, ticker_op_func fp_op_func, void *op_context)
 DECL|ticker_ticks_diff_get|function|u32_t ticker_ticks_diff_get(u32_t ticks_now, u32_t ticks_old)
 DECL|ticker_ticks_now_get|function|u32_t ticker_ticks_now_get(void)
 DECL|ticker_trigger|function|void ticker_trigger(u8_t instance_index)
-DECL|ticker_update|function|u32_t ticker_update(u8_t instance_index, u8_t user_id, u8_t _ticker_id, u16_t ticks_drift_plus, u16_t ticks_drift_minus, u16_t ticks_slot_plus, u16_t ticks_slot_minus, u16_t lazy, u8_t force, ticker_op_func fp_op_func, void *op_context)
+DECL|ticker_update|function|u32_t ticker_update(u8_t instance_index, u8_t user_id, u8_t ticker_id, u16_t ticks_drift_plus, u16_t ticks_drift_minus, u16_t ticks_slot_plus, u16_t ticks_slot_minus, u16_t lazy, u8_t force, ticker_op_func fp_op_func, void *op_context)
 DECL|ticker_user_op_slot_get|struct|struct ticker_user_op_slot_get {
 DECL|ticker_user_op_start|struct|struct ticker_user_op_start {
 DECL|ticker_user_op_type|enum|enum ticker_user_op_type {
@@ -104,6 +108,7 @@ DECL|ticks_slot_previous|member|u16_t ticks_slot_previous;
 DECL|ticks_slot|member|u16_t ticks_slot;
 DECL|ticks_slot|member|u16_t ticks_slot;
 DECL|ticks_to_expire_minus|member|u16_t ticks_to_expire_minus;
+DECL|ticks_to_expire_prep|function|static void ticks_to_expire_prep(struct ticker_node *ticker, u32_t ticks_current, u32_t ticks_at_start)
 DECL|ticks_to_expire|member|u32_t *ticks_to_expire;
 DECL|ticks_to_expire|member|u32_t ticks_to_expire;
 DECL|timeout_func|member|ticker_timeout_func timeout_func;
