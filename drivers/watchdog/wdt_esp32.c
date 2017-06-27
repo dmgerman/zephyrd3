f96622de31c70caff7c0176dd6a876a509c5171a
Leandro Pereira <leandro.pereira@intel.com>
2017-06-27 16:20:45 -0700

    drivers: watchdog: Implement ESP32 watchdog driver
    Zephyr's watchdog API is badly designed in the sense that it's a 1:1
    abstraction on top of whatever Quark D2000 expects for its watchdog,
    instead of expecting a generic timeout value.
    
    This implementation tries as much as possible to calculate the watchdog
    timeout in a way that's compatible with a Quark D2000 running at 32MHz;
    a comment in adjust_timeout() explains this in more detail.
    
    Jira: ZEP-2296
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: d69104559217664789f6bb0d1c8ab5082eb37fe7

---
+++ drivers/watchdog/wdt_esp32.c
@@ -0,0 +1,15 @@
+DECL|adjust_timeout|function|static void adjust_timeout(u32_t timeout)
+DECL|config|member|struct wdt_config config;
+DECL|set_interrupt_enabled|function|static void set_interrupt_enabled(bool setting)
+DECL|shared_data|variable|shared_data
+DECL|wdt_api|variable|wdt_api
+DECL|wdt_esp32_data|struct|struct wdt_esp32_data {
+DECL|wdt_esp32_disable|function|static void wdt_esp32_disable(struct device *dev)
+DECL|wdt_esp32_enable|function|static void wdt_esp32_enable(struct device *dev)
+DECL|wdt_esp32_get_config|function|static void wdt_esp32_get_config(struct device *dev, struct wdt_config *config)
+DECL|wdt_esp32_init|function|static int wdt_esp32_init(struct device *dev)
+DECL|wdt_esp32_isr|function|static void wdt_esp32_isr(void *param)
+DECL|wdt_esp32_reload|function|static void wdt_esp32_reload(struct device *dev)
+DECL|wdt_esp32_seal|function|static inline void wdt_esp32_seal(void)
+DECL|wdt_esp32_set_config|function|static int wdt_esp32_set_config(struct device *dev, struct wdt_config *config)
+DECL|wdt_esp32_unseal|function|static inline void wdt_esp32_unseal(void)
