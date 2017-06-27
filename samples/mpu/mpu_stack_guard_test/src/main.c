4d02d0bd28c92c3bc86d51bf3ae47f1442fe5d51
Michel Jaouen <michel.jaouen@st.com>
2017-06-27 11:58:33 +0200

    mpu_stack_guard_test: add more thread for test
    This test launches several threads. Only last thread will overflow the
    stack and an mpu exception will occur. The other threads are regulary
    suspended, this triggers access to kthread structure. This tests is
    failed on st and nxp platform, if 32 bytes alignement is not set.
    
    Signed-off-by: Michel Jaouen <michel.jaouen@st.com>
    
    Former-commit-id: 3e6965d188629f71339e812a405facc5b9c3a68a

--- samples/mpu/mpu_stack_guard_test/src/main.c
+++ samples/mpu/mpu_stack_guard_test/src/main.c
@@ -1,11 +1,13 @@
+DECL|LAST_THREAD_NUM|macro|LAST_THREAD_NUM
 DECL|PRIORITY|macro|PRIORITY
+DECL|SCHEDTIME|macro|SCHEDTIME
 DECL|SLEEPTIME|macro|SLEEPTIME
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|STACK_GUARD_CANARY|macro|STACK_GUARD_CANARY
 DECL|buf|variable|buf
 DECL|canary|member|volatile u32_t canary;
 DECL|main|function|void main(void)
-DECL|recursive_loop|function|u32_t recursive_loop(u32_t counter)
+DECL|recursive_loop|function|u32_t recursive_loop(u32_t counter, int num, void *dummy)
 DECL|stack_guard_buffer|struct|struct stack_guard_buffer {
 DECL|stack_guard_thread|function|void stack_guard_thread(void *dummy1, void *dummy2, void *dummy3)
-DECL|test_thread|variable|test_thread
+DECL|thread|member|struct k_thread thread;
