f0d2f56b502e8d28cc5dc8138d20c1ea94953137
Leandro Pereira <leandro.pereira@intel.com>
2017-06-26 17:07:43 -0700

    drivers: pinmux: Add pin multiplexer driver for ESP32
    This implements a driver for the pin multiplexer as present in the ESP32
    SoCs.
    
    All APIs are supported.
    
    Jira: ZEP-2297
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: f3b05c6a58c68085421484fce0a8a092e7a39371

---
+++ drivers/pinmux/pinmux_esp32.c
@@ -0,0 +1,13 @@
+DECL|CFG|macro|CFG
+DECL|CFG|macro|CFG
+DECL|PIN|macro|PIN
+DECL|PIN|macro|PIN
+DECL|api_funcs|variable|api_funcs
+DECL|pin_mux_off|variable|pin_mux_off
+DECL|pinmux_get|function|static int pinmux_get(struct device *dev, u32_t pin, u32_t *func)
+DECL|pinmux_initialize|function|static int pinmux_initialize(struct device *device)
+DECL|pinmux_input|function|static int pinmux_input(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_pullup|function|static int pinmux_pullup(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_set|function|static int pinmux_set(struct device *dev, u32_t pin, u32_t func)
+DECL|reg_for_pin|function|static u32_t *reg_for_pin(u32_t pin)
+DECL|set_reg|function|static int set_reg(u32_t pin, u32_t clr_mask, u32_t set_mask)
