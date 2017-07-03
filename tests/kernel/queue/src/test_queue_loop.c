59d37f0b82cbc90fd65dc48f1ae3ac426618c84b
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2017-07-03 16:54:30 +0300

    tests: queue: Add tests for k_queue_remove
    Add another list of elements which is removed before k_queue_get is
    called.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 42d9aa785dd04c4d3d60302c1c8f84269ccb5c52

--- tests/kernel/queue/src/test_queue_loop.c
+++ tests/kernel/queue/src/test_queue_loop.c
@@ -2,6 +2,7 @@ DECL|LIST_LEN|macro|LIST_LEN
 DECL|LOOPS|macro|LOOPS
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|data_p|variable|data_p
+DECL|data_r|variable|data_r
 DECL|data|variable|data
 DECL|end_sema|variable|end_sema
 DECL|queue|variable|queue
@@ -10,5 +11,6 @@ DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *
 DECL|tdata|variable|tdata
 DECL|test_queue_loop|function|void test_queue_loop(void)
 DECL|tqueue_append|function|static void tqueue_append(struct k_queue *pqueue)
+DECL|tqueue_find_and_remove|function|static void tqueue_find_and_remove(struct k_queue *pqueue)
 DECL|tqueue_get|function|static void tqueue_get(struct k_queue *pqueue)
 DECL|tqueue_read_write|function|static void tqueue_read_write(struct k_queue *pqueue)
