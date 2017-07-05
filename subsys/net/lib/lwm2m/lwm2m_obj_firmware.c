807cfc57f54ef72df621e700f8cc2655108c250c
Robert Chou <robert.ch.chou@acer.com>
2017-07-05 23:07:41 +0800

    net: lwm2m: add state machine for firmware pull update
    1. Parse firmware pull URI
    2. Add lwm2m_firmware_get/set_update_cb() for application to register
       callback. This is because we want to check the update_state before
       we pass to the application
    3. Add lwm2m_firmware_get/set_update_result() and
       lwm2m_firmware_get/set_update_stat() to manage the state transition
       as well as the sanity check
    
    Signed-off-by: Robert Chou <robert.ch.chou@acer.com>
    [michael.scott@linaro.org: rebased on net_app framework and
    lwm2m_message refactoring.]
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Former-commit-id: d9f5fe745512bcf43b7a9683cfd0d909fcf87067

--- subsys/net/lib/lwm2m/lwm2m_obj_firmware.c
+++ subsys/net/lib/lwm2m/lwm2m_obj_firmware.c
@@ -17,15 +17,23 @@ DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|delivery_method|variable|delivery_method
 DECL|fields|variable|fields
 DECL|firmware_create|function|static struct lwm2m_engine_obj_inst *firmware_create(u16_t obj_inst_id)
+DECL|firmware_update_cb|function|static int firmware_update_cb(u16_t obj_inst_id)
 DECL|firmware|variable|firmware
 DECL|inst|variable|inst
+DECL|lwm2m_firmware_get_update_cb|function|lwm2m_engine_exec_cb_t lwm2m_firmware_get_update_cb(void)
+DECL|lwm2m_firmware_get_update_result|function|u8_t lwm2m_firmware_get_update_result(void)
+DECL|lwm2m_firmware_get_update_state|function|u8_t lwm2m_firmware_get_update_state(void)
 DECL|lwm2m_firmware_get_write_cb|function|lwm2m_engine_set_data_cb_t lwm2m_firmware_get_write_cb(void)
 DECL|lwm2m_firmware_init|function|static int lwm2m_firmware_init(struct device *dev)
+DECL|lwm2m_firmware_set_update_cb|function|void lwm2m_firmware_set_update_cb(lwm2m_engine_exec_cb_t cb)
+DECL|lwm2m_firmware_set_update_result|function|void lwm2m_firmware_set_update_result(u8_t result)
+DECL|lwm2m_firmware_set_update_state|function|void lwm2m_firmware_set_update_state(u8_t state)
 DECL|lwm2m_firmware_set_write_cb|function|void lwm2m_firmware_set_write_cb(lwm2m_engine_set_data_cb_t cb)
 DECL|package_uri_write_cb|function|static int package_uri_write_cb(u16_t obj_inst_id,u8_t *data, u16_t data_len, bool last_block, size_t total_size)
 DECL|package_uri|variable|package_uri
 DECL|package_write_cb|function|static int package_write_cb(u16_t obj_inst_id, u8_t *data, u16_t data_len, bool last_block, size_t total_size)
 DECL|res|variable|res
+DECL|update_cb|variable|update_cb
 DECL|update_result|variable|update_result
 DECL|update_state|variable|update_state
 DECL|write_cb|variable|write_cb
--- subsys/net/lib/lwm2m/lwm2m_obj_firmware_pull.c
+++ subsys/net/lib/lwm2m/lwm2m_obj_firmware_pull.c
@@ -2,14 +2,11 @@ DECL|BUF_ALLOC_TIMEOUT|macro|BUF_ALLOC_TIMEOUT
 DECL|NETWORK_CONNECT_TIMEOUT|macro|NETWORK_CONNECT_TIMEOUT
 DECL|NETWORK_INIT_TIMEOUT|macro|NETWORK_INIT_TIMEOUT
 DECL|PACKAGE_URI_LEN|macro|PACKAGE_URI_LEN
-DECL|STATE_CONNECTING|macro|STATE_CONNECTING
-DECL|STATE_IDLE|macro|STATE_IDLE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|default_block_size|function|static enum zoap_block_size default_block_size(void)
 DECL|do_firmware_transfer_reply_cb|function|do_firmware_transfer_reply_cb(const struct zoap_packet *response, struct zoap_reply *reply, const struct sockaddr *from)
 DECL|do_transmit_timeout_cb|function|static void do_transmit_timeout_cb(struct lwm2m_message *msg)
-DECL|firmware_addr|variable|firmware_addr
 DECL|firmware_block_ctx|variable|firmware_block_ctx
 DECL|firmware_ctx|variable|firmware_ctx
 DECL|firmware_transfer|function|static void firmware_transfer(struct k_work *work)
@@ -18,5 +15,5 @@ DECL|firmware_uri|variable|firmware_uri
 DECL|firmware_work|variable|firmware_work
 DECL|lwm2m_firmware_cancel_transfer|function|int lwm2m_firmware_cancel_transfer(void)
 DECL|lwm2m_firmware_start_transfer|function|int lwm2m_firmware_start_transfer(char *package_uri)
+DECL|parsed_uri|variable|parsed_uri
 DECL|transfer_request|function|static int transfer_request(struct zoap_block_context *ctx, const u8_t *token, u8_t tkl, zoap_reply_t reply_cb)
-DECL|transfer_state|variable|transfer_state
