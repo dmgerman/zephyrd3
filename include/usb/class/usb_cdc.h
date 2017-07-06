817dcfbd1a794874242c7d865d7aa6d5f5b6f5c5
Johann Fischer <j.fischer@phytec.de>
2017-07-06 11:39:18 +0200

    include: usb: add usb_cdc.h header for CDC ACM/ECM devices
    Add USB Communicatons Device Class header. Currently
    limited to ACM and ECM devices.
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: 39f0592f54a971976521a28f2c220a2e25665b65

---
+++ include/usb/class/usb_cdc.h
@@ -0,0 +1,86 @@
+DECL|ACM_FUNC_DESC|macro|ACM_FUNC_DESC
+DECL|ACM_SUBCLASS|macro|ACM_SUBCLASS
+DECL|AT_CMD_V250_PROTOCOL|macro|AT_CMD_V250_PROTOCOL
+DECL|CALL_MANAGEMENT_FUNC_DESC|macro|CALL_MANAGEMENT_FUNC_DESC
+DECL|CDC_SRN_1_20|macro|CDC_SRN_1_20
+DECL|COMMUNICATION_DEVICE_CLASS|macro|COMMUNICATION_DEVICE_CLASS
+DECL|CS_ENDPOINT|macro|CS_ENDPOINT
+DECL|CS_INTERFACE|macro|CS_INTERFACE
+DECL|DATA_INTERFACE_CLASS|macro|DATA_INTERFACE_CLASS
+DECL|ECM_SUBCLASS|macro|ECM_SUBCLASS
+DECL|ETHERNET_FUNC_DESC|macro|ETHERNET_FUNC_DESC
+DECL|GET_ETHERNET_PM_FILTER|macro|GET_ETHERNET_PM_FILTER
+DECL|GET_ETHERNET_STATISTIC|macro|GET_ETHERNET_STATISTIC
+DECL|GET_LINE_CODING|macro|GET_LINE_CODING
+DECL|HEADER_FUNC_DESC|macro|HEADER_FUNC_DESC
+DECL|PACKET_TYPE_ALL_MULTICAST|macro|PACKET_TYPE_ALL_MULTICAST
+DECL|PACKET_TYPE_BROADCAST|macro|PACKET_TYPE_BROADCAST
+DECL|PACKET_TYPE_DIRECTED|macro|PACKET_TYPE_DIRECTED
+DECL|PACKET_TYPE_MULTICAST|macro|PACKET_TYPE_MULTICAST
+DECL|PACKET_TYPE_PROMISCUOUS|macro|PACKET_TYPE_PROMISCUOUS
+DECL|SERIAL_STATE_BREAK|macro|SERIAL_STATE_BREAK
+DECL|SERIAL_STATE_FRAMING|macro|SERIAL_STATE_FRAMING
+DECL|SERIAL_STATE_OVERRUN|macro|SERIAL_STATE_OVERRUN
+DECL|SERIAL_STATE_PARITY|macro|SERIAL_STATE_PARITY
+DECL|SERIAL_STATE_RING|macro|SERIAL_STATE_RING
+DECL|SERIAL_STATE_RX_CARRIER|macro|SERIAL_STATE_RX_CARRIER
+DECL|SERIAL_STATE_TX_CARRIER|macro|SERIAL_STATE_TX_CARRIER
+DECL|SET_CONTROL_LINE_STATE_DTR|macro|SET_CONTROL_LINE_STATE_DTR
+DECL|SET_CONTROL_LINE_STATE_RTS|macro|SET_CONTROL_LINE_STATE_RTS
+DECL|SET_CONTROL_LINE_STATE|macro|SET_CONTROL_LINE_STATE
+DECL|SET_ETHERNET_MULTICAST_FILTERS|macro|SET_ETHERNET_MULTICAST_FILTERS
+DECL|SET_ETHERNET_PACKET_FILTER|macro|SET_ETHERNET_PACKET_FILTER
+DECL|SET_ETHERNET_PM_FILTER|macro|SET_ETHERNET_PM_FILTER
+DECL|SET_LINE_CODING|macro|SET_LINE_CODING
+DECL|UNION_FUNC_DESC|macro|UNION_FUNC_DESC
+DECL|__USB_CDC_H__|macro|__USB_CDC_H__
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|bCharFormat|member|u8_t bCharFormat;
+DECL|bControlInterface|member|u8_t bControlInterface;
+DECL|bDataBits|member|u8_t bDataBits;
+DECL|bDataInterface|member|u8_t bDataInterface;
+DECL|bDescriptorSubtype|member|u8_t bDescriptorSubtype;
+DECL|bDescriptorSubtype|member|u8_t bDescriptorSubtype;
+DECL|bDescriptorSubtype|member|u8_t bDescriptorSubtype;
+DECL|bDescriptorSubtype|member|u8_t bDescriptorSubtype;
+DECL|bDescriptorSubtype|member|u8_t bDescriptorSubtype;
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bFunctionLength|member|u8_t bFunctionLength;
+DECL|bFunctionLength|member|u8_t bFunctionLength;
+DECL|bFunctionLength|member|u8_t bFunctionLength;
+DECL|bFunctionLength|member|u8_t bFunctionLength;
+DECL|bFunctionLength|member|u8_t bFunctionLength;
+DECL|bNotificationType|member|u8_t bNotificationType;
+DECL|bNumberPowerFilters|member|u8_t bNumberPowerFilters;
+DECL|bParityType|member|u8_t bParityType;
+DECL|bSubordinateInterface0|member|u8_t bSubordinateInterface0;
+DECL|bcdCDC|member|u16_t bcdCDC;
+DECL|bmCapabilities|member|u8_t bmCapabilities;
+DECL|bmCapabilities|member|u8_t bmCapabilities;
+DECL|bmEthernetStatistics|member|u32_t bmEthernetStatistics;
+DECL|bmRequestType|member|u8_t bmRequestType;
+DECL|cdc_acm_descriptor|struct|struct cdc_acm_descriptor {
+DECL|cdc_acm_line_coding|struct|struct cdc_acm_line_coding {
+DECL|cdc_acm_notification|struct|struct cdc_acm_notification {
+DECL|cdc_cm_descriptor|struct|struct cdc_cm_descriptor {
+DECL|cdc_ecm_descriptor|struct|struct cdc_ecm_descriptor {
+DECL|cdc_header_descriptor|struct|struct cdc_header_descriptor {
+DECL|cdc_union_descriptor|struct|struct cdc_union_descriptor {
+DECL|data|member|u16_t data;
+DECL|dwDTERate|member|u32_t dwDTERate;
+DECL|iMACAddress|member|u8_t iMACAddress;
+DECL|wIndex|member|u16_t wIndex;
+DECL|wLength|member|u16_t wLength;
+DECL|wMaxSegmentSize|member|u16_t wMaxSegmentSize;
+DECL|wNumberMCFilters|member|u16_t wNumberMCFilters;
+DECL|wValue|member|u16_t wValue;
