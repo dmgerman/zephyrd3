7a3ca019fd8b43ab257519d810c52db6398db9f2
Michael Scott <michael.scott@linaro.org>
2017-07-07 11:04:04 -0700

    samples: lwm2m: initial sample for LWM2M client
    This sample utilizes the new LwM2M library by setting up default
    values for LwM2M device and firmware objects and then establisting
    a connection to a LwM2M server (for example Leshan Demo Server) via
    the registration interface.
    
    To use QEMU for this purpose please see:
    doc/subsystems/networking/qemu_setup.rst
    
    NOTE: This sample currently does not demonstrate DTLS/bootstrap as
    neither of these is supported by the LwM2M library.
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Former-commit-id: b8ac539e518d288b1dffb130cdd89bf3f1eec547

---
+++ samples/net/lwm2m_client/src/lwm2m-client.c
@@ -0,0 +1,36 @@
+DECL|APP_BANNER|macro|APP_BANNER
+DECL|CLIENT_DEVICE_TYPE|macro|CLIENT_DEVICE_TYPE
+DECL|CLIENT_FIRMWARE_VER|macro|CLIENT_FIRMWARE_VER
+DECL|CLIENT_HW_VER|macro|CLIENT_HW_VER
+DECL|CLIENT_MANUFACTURER|macro|CLIENT_MANUFACTURER
+DECL|CLIENT_MODEL_NUMBER|macro|CLIENT_MODEL_NUMBER
+DECL|CLIENT_SERIAL_NUMBER|macro|CLIENT_SERIAL_NUMBER
+DECL|CONFIG_NET_APP_PEER_IPV4_ADDR|macro|CONFIG_NET_APP_PEER_IPV4_ADDR
+DECL|CONFIG_NET_APP_PEER_IPV6_ADDR|macro|CONFIG_NET_APP_PEER_IPV6_ADDR
+DECL|CONNECT_TIME|macro|CONNECT_TIME
+DECL|ENDPOINT_LEN|macro|ENDPOINT_LEN
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|battery_current|variable|battery_current
+DECL|battery_voltage|variable|battery_voltage
+DECL|data_udp_pool|function|static struct net_buf_pool *data_udp_pool(void)
+DECL|data_udp_pool|macro|data_udp_pool
+DECL|device_factory_default_cb|function|static int device_factory_default_cb(u16_t obj_inst_id)
+DECL|device_reboot_cb|function|static int device_reboot_cb(u16_t obj_inst_id)
+DECL|firmware_block_received_cb|function|static int firmware_block_received_cb(u16_t obj_inst_id, u8_t *data, u16_t data_len, bool last_block, size_t total_size)
+DECL|firmware_update_cb|function|static int firmware_update_cb(u16_t obj_inst_id)
+DECL|lwm2m_setup|function|static int lwm2m_setup(void)
+DECL|main|function|void main(void)
+DECL|pwrsrc_bat|variable|pwrsrc_bat
+DECL|pwrsrc_usb|variable|pwrsrc_usb
+DECL|quit_lock|variable|quit_lock
+DECL|set_endpoint_name|function|static int set_endpoint_name(char *ep_name, sa_family_t family)
+DECL|setup_net_app_ctx|function|int setup_net_app_ctx(struct net_app_ctx *ctx, const char *peer)
+DECL|tx_udp_slab|function|static struct k_mem_slab *tx_udp_slab(void)
+DECL|tx_udp_slab|macro|tx_udp_slab
+DECL|udp4|variable|udp4
+DECL|udp6|variable|udp6
+DECL|usb_current|variable|usb_current
+DECL|usb_voltage|variable|usb_voltage
