fff13687e6c94b4680279879ed2995a8148e4b0a
Leandro Pereira <leandro.pereira@intel.com>
2017-07-07 10:35:18 -0700

    arch: xtensa: Convert Xtensa port to use gen_isr_table
    The Xtensa port was the only one remaining to be converted to the new
    way of connecting interrupts in Zephyr.  Some things are still
    unconverted, mainly the exception table, and this will be performed
    another time.
    
    Of note: _irq_priority_set() isn't called on _ARCH_IRQ_CONNECT(), since
    IRQs can't change priority on Xtensa: while the architecture has the
    concept of interrupt priority levels, each line has a fixed level and
    can't be changed.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 27ea2d8eb74edd834fd1ec2d6404fafd38860493

--- arch/xtensa/core/xtensa_intr.c
+++ arch/xtensa/core/xtensa_intr.c
@@ -1,3 +1,3 @@
+DECL|_irq_spurious|function|void _irq_spurious(void *arg)
 DECL|_xt_set_exception_handler|function|xt_exc_handler _xt_set_exception_handler(int n, xt_exc_handler f)
 DECL|xt_unhandled_exception|function|void xt_unhandled_exception(XtExcFrame *frame)
-DECL|xt_unhandled_interrupt|function|void xt_unhandled_interrupt(void *arg)
--- include/arch/xtensa/xtensa_irq.h
+++ include/arch/xtensa/xtensa_irq.h
@@ -1,3 +1,5 @@
+DECL|CONFIG_GEN_IRQ_START_VECTOR|macro|CONFIG_GEN_IRQ_START_VECTOR
+DECL|CONFIG_NUM_IRQS|macro|CONFIG_NUM_IRQS
 DECL|XTENSA_IRQ_H|macro|XTENSA_IRQ_H
 DECL|_arch_irq_disable|function|static ALWAYS_INLINE void _arch_irq_disable(u32_t irq)
 DECL|_arch_irq_enable|function|static ALWAYS_INLINE void _arch_irq_enable(u32_t irq)
