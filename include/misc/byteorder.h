65d5279488997eeffab1029e08a8fa08a1739fe1
Michael Scott <michael.scott@linaro.org>
2017-07-07 11:04:01 -0700

    misc/byteorder: add support for __bswap_64
    We currently support converting from cpu format to BE for
    u16_t and u32_t.  Let's add u64_t as well.
    
    NOTE: This will be used in LWM2M subsys later.
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Former-commit-id: 3aa347fa490db48f36a16ea5ff8f24c62caf910a

--- include/misc/byteorder.h
+++ include/misc/byteorder.h
@@ -1,18 +1,25 @@
 DECL|__BYTEORDER_H__|macro|__BYTEORDER_H__
 DECL|__bswap_16|macro|__bswap_16
 DECL|__bswap_32|macro|__bswap_32
+DECL|__bswap_64|macro|__bswap_64
 DECL|sys_be16_to_cpu|macro|sys_be16_to_cpu
 DECL|sys_be16_to_cpu|macro|sys_be16_to_cpu
 DECL|sys_be32_to_cpu|macro|sys_be32_to_cpu
 DECL|sys_be32_to_cpu|macro|sys_be32_to_cpu
+DECL|sys_be64_to_cpu|macro|sys_be64_to_cpu
+DECL|sys_be64_to_cpu|macro|sys_be64_to_cpu
 DECL|sys_cpu_to_be16|macro|sys_cpu_to_be16
 DECL|sys_cpu_to_be16|macro|sys_cpu_to_be16
 DECL|sys_cpu_to_be32|macro|sys_cpu_to_be32
 DECL|sys_cpu_to_be32|macro|sys_cpu_to_be32
+DECL|sys_cpu_to_be64|macro|sys_cpu_to_be64
+DECL|sys_cpu_to_be64|macro|sys_cpu_to_be64
 DECL|sys_cpu_to_le16|macro|sys_cpu_to_le16
 DECL|sys_cpu_to_le16|macro|sys_cpu_to_le16
 DECL|sys_cpu_to_le32|macro|sys_cpu_to_le32
 DECL|sys_cpu_to_le32|macro|sys_cpu_to_le32
+DECL|sys_cpu_to_le64|macro|sys_cpu_to_le64
+DECL|sys_cpu_to_le64|macro|sys_cpu_to_le64
 DECL|sys_get_be16|function|static inline u16_t sys_get_be16(const u8_t src[2])
 DECL|sys_get_be32|function|static inline u32_t sys_get_be32(const u8_t src[4])
 DECL|sys_get_le16|function|static inline u16_t sys_get_le16(const u8_t src[2])
@@ -22,6 +29,8 @@ DECL|sys_le16_to_cpu|macro|sys_le16_to_cpu
 DECL|sys_le16_to_cpu|macro|sys_le16_to_cpu
 DECL|sys_le32_to_cpu|macro|sys_le32_to_cpu
 DECL|sys_le32_to_cpu|macro|sys_le32_to_cpu
+DECL|sys_le64_to_cpu|macro|sys_le64_to_cpu
+DECL|sys_le64_to_cpu|macro|sys_le64_to_cpu
 DECL|sys_mem_swap|function|static inline void sys_mem_swap(void *buf, size_t length)
 DECL|sys_memcpy_swap|function|static inline void sys_memcpy_swap(void *dst, const void *src, size_t length)
 DECL|sys_put_be16|function|static inline void sys_put_be16(u16_t val, u8_t dst[2])
