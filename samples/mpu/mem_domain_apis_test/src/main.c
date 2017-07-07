d2b2aafb984b6dfc6b59b70900311faef5cadc00
Chunlin Han <chunlin.han@linaro.org>
2017-07-07 20:41:49 +0800

    sample: demonstrate the usage of memory domain APIs
    The demo application to demonstrate the usage of memory domain APIs.
    
    Signed-off-by: Chunlin Han <chunlin.han@linaro.org>
    
    Former-commit-id: fe48e9efde7e6bec0edf21d77f40ec1d7d009d7b

---
+++ samples/mpu/mem_domain_apis_test/src/main.c
@@ -0,0 +1,9 @@
+DECL|BUSY_WAITING_TIME|macro|BUSY_WAITING_TIME
+DECL|PRIORITY|macro|PRIORITY
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|app0_parts|variable|app0_parts
+DECL|app1_parts|variable|app1_parts
+DECL|app_domain|variable|app_domain
+DECL|app_thread_id|variable|app_thread_id
+DECL|app_thread|function|void app_thread(void *dummy1, void *dummy2, void *dummy3)
+DECL|main|function|void main(void)
