3b57c5c769802c8fc6ca403cb4636915ff5c595f
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-07-09 20:01:36 -0300

    net: lwm2m: add IPSO light control object
    As defined by IPSO-Smart-Objects "Section: 16. IPSO Object: Light
    Control".
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>
    
    Former-commit-id: 2175f783851e3a27f8f4a0cb8fb8afb5ddd62d31

---
+++ subsys/net/lib/lwm2m/ipso_light_control.c
@@ -0,0 +1,28 @@
+DECL|LIGHT_COLOUR_ID|macro|LIGHT_COLOUR_ID
+DECL|LIGHT_CUMULATIVE_ACTIVE_POWER_ID|macro|LIGHT_CUMULATIVE_ACTIVE_POWER_ID
+DECL|LIGHT_DIMMER_ID|macro|LIGHT_DIMMER_ID
+DECL|LIGHT_MAX_ID|macro|LIGHT_MAX_ID
+DECL|LIGHT_ON_OFF_ID|macro|LIGHT_ON_OFF_ID
+DECL|LIGHT_ON_TIME_ID|macro|LIGHT_ON_TIME_ID
+DECL|LIGHT_POWER_FACTOR_ID|macro|LIGHT_POWER_FACTOR_ID
+DECL|LIGHT_SENSOR_UNITS_ID|macro|LIGHT_SENSOR_UNITS_ID
+DECL|LIGHT_STRING_SHORT|macro|LIGHT_STRING_SHORT
+DECL|MAX_INSTANCE_COUNT|macro|MAX_INSTANCE_COUNT
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|colour|variable|colour
+DECL|cumulative_active_value|variable|cumulative_active_value
+DECL|dimmer_value|variable|dimmer_value
+DECL|fields|variable|fields
+DECL|inst|variable|inst
+DECL|ipso_light_control_init|function|static int ipso_light_control_init(struct device *dev)
+DECL|light_control_create|function|static struct lwm2m_engine_obj_inst *light_control_create(u16_t obj_inst_id)
+DECL|light_control|variable|light_control
+DECL|on_off_value|variable|on_off_value
+DECL|on_time_offset|variable|on_time_offset
+DECL|on_time_post_write_cb|function|static int on_time_post_write_cb(u16_t obj_inst_id, u8_t *data, u16_t data_len, bool last_block, size_t total_size)
+DECL|on_time_read_cb|function|static void *on_time_read_cb(u16_t obj_inst_id, size_t *data_len)
+DECL|on_time_value|variable|on_time_value
+DECL|power_factor_value|variable|power_factor_value
+DECL|res|variable|res
+DECL|units|variable|units
