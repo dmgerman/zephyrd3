b21571cc6a551c5db287916dd606cd338c08f549
Gil Pitney <gil.pitney@linaro.org>
2017-07-10 11:30:30 -0700

    ext: simplelink: Update HAL for CC3220sf to version 1.40.00.03
    The SimpleLink SDK provides a peripheral driver library and hardware
    register access header files for the Texas Instruments SimpleLink
    CC3220S and CC3220SF SoCs.
    
    This patch updates the SDK version in Zephyr from 1.30.01.03
    to 1.40.00.03.
    
    The SDK can be downloaded from:
    
    http://www.ti.com/tool/download/SIMPLELINK-CC3220-SDK
    
    Jira: ZEP-1958
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: b70633b95f46af6138b6e039376af41de6141192

---
+++ ext/hal/ti/simplelink/source/ti/devices/cc32xx/driverlib/pin.c
@@ -0,0 +1,26 @@
+DECL|PAD_CONFIG_BASE|macro|PAD_CONFIG_BASE
+DECL|PAD_MODE_MASK|macro|PAD_MODE_MASK
+DECL|PAD_STRENGTH_MASK|macro|PAD_STRENGTH_MASK
+DECL|PAD_TYPE_MASK|macro|PAD_TYPE_MASK
+DECL|PinConfigGet|function|void PinConfigGet(unsigned long ulPin,unsigned long *pulPinStrength,unsigned long *pulPinType)
+DECL|PinConfigSet|function|void PinConfigSet(unsigned long ulPin,unsigned long ulPinStrength,unsigned long ulPinType)
+DECL|PinDirModeGet|function|unsigned long PinDirModeGet(unsigned long ulPin)
+DECL|PinDirModeSet|function|void PinDirModeSet(unsigned long ulPin, unsigned long ulPinIO)
+DECL|PinFromPadGet|function|unsigned long PinFromPadGet(unsigned long ulPad)
+DECL|PinHysteresisSet|function|void PinHysteresisSet(unsigned long ulHysteresis)
+DECL|PinLockLevelSet|function|void PinLockLevelSet(unsigned long ulPin, unsigned char ucLevel)
+DECL|PinLock|function|void PinLock(unsigned long ulOutEnable)
+DECL|PinModeGet|function|unsigned long PinModeGet(unsigned long ulPin)
+DECL|PinModeSet|function|void PinModeSet(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinToPadGet|function|unsigned long PinToPadGet(unsigned long ulPin)
+DECL|PinTypeADC|function|void PinTypeADC(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeCamera|function|void PinTypeCamera(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeGPIO|function|void PinTypeGPIO(unsigned long ulPin,unsigned long ulPinMode,tBoolean bOpenDrain)
+DECL|PinTypeI2C|function|void PinTypeI2C(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeI2S|function|void PinTypeI2S(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeSDHost|function|void PinTypeSDHost(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeSPI|function|void PinTypeSPI(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeTimer|function|void PinTypeTimer(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinTypeUART|function|void PinTypeUART(unsigned long ulPin,unsigned long ulPinMode)
+DECL|PinUnlock|function|void PinUnlock()
+DECL|g_ulPinToPadMap|variable|g_ulPinToPadMap
