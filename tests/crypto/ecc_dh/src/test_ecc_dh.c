f02e7d754b325097910b5e3265228344d3eea2dc
Leandro Pereira <leandro.pereira@intel.com>
2017-07-13 12:54:12 -0700

    tests: crypto: Update ECC Diffie-Hellman and DSA tests
    These tests have been copied over from TinyCrypt and modified slightly
    so that they compile on a non-POSIX system, with a minimal C library:
    that's why it doesn't comply with Zephyr's coding style.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: e2ad85b696e2b7be18c46ee121ad0b693cf35610

--- tests/crypto/ecc_dh/src/test_ecc_dh.c
+++ tests/crypto/ecc_dh/src/test_ecc_dh.c
@@ -1,19 +1,8 @@
-DECL|RC_STR|macro|RC_STR
-DECL|cavp_ecdh|function|int cavp_ecdh(int verbose)
-DECL|cavp_keygen|function|int cavp_keygen(int verbose)
-DECL|cavp_pkv|function|int cavp_pkv(int verbose)
-DECL|dh_d|variable|dh_d
-DECL|dh_x|variable|dh_x
-DECL|dh_y|variable|dh_y
-DECL|dh_z|variable|dh_z
+DECL|cavp_ecdh|function|int cavp_ecdh(bool verbose)
+DECL|cavp_keygen|function|int cavp_keygen(bool verbose)
+DECL|cavp_pkv|function|int cavp_pkv(bool verbose)
+DECL|default_CSPRNG|function|int default_CSPRNG(u8_t *dest, unsigned int size)
 DECL|ecdh_vectors|function|int ecdh_vectors(char **qx_vec, char **qy_vec, char **d_vec, char **z_vec, int tests, int verbose)
-DECL|gen_d|variable|gen_d
-DECL|gen_x|variable|gen_x
-DECL|gen_y|variable|gen_y
 DECL|main|function|int main(void)
-DECL|montecarlo_ecdh|function|int montecarlo_ecdh(u32_t num, int verbose)
-DECL|names|variable|names
-DECL|pkv_vectors|function|int pkv_vectors(char **qx_vec, char **qy_vec, char **res_vec, int *results,int tests, int verbose)
-DECL|results|variable|results
-DECL|verify_x|variable|verify_x
-DECL|verify_y|variable|verify_y
+DECL|montecarlo_ecdh|function|int montecarlo_ecdh(int num_tests, bool verbose)
+DECL|pkv_vectors|function|int pkv_vectors(char **qx_vec, char **qy_vec, int res_vec[], int tests,bool verbose)
--- tests/crypto/ecc_dsa/src/test_ecc_dsa.c
+++ tests/crypto/ecc_dsa/src/test_ecc_dsa.c
@@ -1,22 +1,9 @@
 DECL|BUF_SIZE|macro|BUF_SIZE
-DECL|RC_STR|macro|RC_STR
-DECL|cavp_sign|function|int cavp_sign(int verbose)
-DECL|cavp_verify|function|int cavp_verify(int verbose)
+DECL|ENABLE_TESTS|macro|ENABLE_TESTS
+DECL|cavp_sign|function|int cavp_sign(bool verbose)
+DECL|cavp_verify|function|int cavp_verify(bool verbose)
+DECL|default_CSPRNG|function|int default_CSPRNG(u8_t *dest, unsigned int size)
 DECL|main|function|int main(void)
-DECL|montecarlo_signverify|function|int montecarlo_signverify(u32_t num, int verbose)
-DECL|sign_d|variable|sign_d
-DECL|sign_k|variable|sign_k
-DECL|sign_msg|variable|sign_msg
-DECL|sign_qx|variable|sign_qx
-DECL|sign_qy|variable|sign_qy
-DECL|sign_r|variable|sign_r
-DECL|sign_s|variable|sign_s
-DECL|sign_vectors|function|int sign_vectors(TCSha256State_t hash, char **d_vec, char **k_vec, char **msg_vec, char **qx_vec, char **qy_vec, char **r_vec, char **s_vec, int tests, int verbose)
-DECL|verify_msg|variable|verify_msg
-DECL|verify_names|variable|verify_names
-DECL|verify_qx|variable|verify_qx
-DECL|verify_qy|variable|verify_qy
-DECL|verify_results|variable|verify_results
-DECL|verify_r|variable|verify_r
-DECL|verify_s|variable|verify_s
-DECL|verify_vectors|function|int verify_vectors(TCSha256State_t hash, char **msg_vec, char **qx_vec, char **qy_vec, char **r_vec, char **s_vec, char **names, int *verify_results, int tests, int verbose)
+DECL|montecarlo_signverify|function|int montecarlo_signverify(int num_tests, bool verbose)
+DECL|sign_vectors|function|int sign_vectors(TCSha256State_t hash, char **d_vec, char **k_vec, char **msg_vec, char **qx_vec, char **qy_vec, char **r_vec, char **s_vec, int tests, bool verbose)
+DECL|vrfy_vectors|function|int vrfy_vectors(TCSha256State_t hash, char **msg_vec, char **qx_vec, char **qy_vec, char **r_vec, char **s_vec, int res_vec[], int tests, bool verbose)
