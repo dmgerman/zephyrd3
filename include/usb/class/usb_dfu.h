ef5719e6eff2d33b29e254890c537e012b25ee2c
Johann Fischer <j.fischer@phytec.de>
2017-07-13 18:24:40 +0200

    include: usb: add USB DFU class header
    Add USB DFU class header.
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: 46700eaa656ecae1c2fa0a234697e0087f9d864a

---
+++ include/usb/class/usb_dfu.h
@@ -0,0 +1,54 @@
+DECL|DFU_ABORT|macro|DFU_ABORT
+DECL|DFU_ATTR_CAN_DNLOAD|macro|DFU_ATTR_CAN_DNLOAD
+DECL|DFU_ATTR_CAN_UPLOAD|macro|DFU_ATTR_CAN_UPLOAD
+DECL|DFU_ATTR_MANIFESTATION_TOLERANT|macro|DFU_ATTR_MANIFESTATION_TOLERANT
+DECL|DFU_ATTR_WILL_DETACH|macro|DFU_ATTR_WILL_DETACH
+DECL|DFU_CLRSTATUS|macro|DFU_CLRSTATUS
+DECL|DFU_DETACH|macro|DFU_DETACH
+DECL|DFU_DNLOAD|macro|DFU_DNLOAD
+DECL|DFU_FUNC_DESC|macro|DFU_FUNC_DESC
+DECL|DFU_GETSTATE|macro|DFU_GETSTATE
+DECL|DFU_GETSTATUS|macro|DFU_GETSTATUS
+DECL|DFU_MODE_PROTOCOL|macro|DFU_MODE_PROTOCOL
+DECL|DFU_RT_PROTOCOL|macro|DFU_RT_PROTOCOL
+DECL|DFU_SUBCLASS|macro|DFU_SUBCLASS
+DECL|DFU_UPLOAD|macro|DFU_UPLOAD
+DECL|DFU_VERSION|macro|DFU_VERSION
+DECL|__USB_DFU_H__|macro|__USB_DFU_H__
+DECL|__packed|variable|__packed
+DECL|appDETACH|enumerator|appDETACH,
+DECL|appIDLE|enumerator|appIDLE,
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bLength|member|u8_t bLength;
+DECL|bcdDFUVersion|member|u16_t bcdDFUVersion;
+DECL|bmAttributes|member|u8_t bmAttributes;
+DECL|dfuDNBUSY|enumerator|dfuDNBUSY,
+DECL|dfuDNLOAD_IDLE|enumerator|dfuDNLOAD_IDLE,
+DECL|dfuDNLOAD_SYNC|enumerator|dfuDNLOAD_SYNC,
+DECL|dfuERROR|enumerator|dfuERROR,
+DECL|dfuIDLE|enumerator|dfuIDLE,
+DECL|dfuMANIFEST_SYNC|enumerator|dfuMANIFEST_SYNC,
+DECL|dfuMANIFEST_WAIT_RST|enumerator|dfuMANIFEST_WAIT_RST,
+DECL|dfuMANIFEST|enumerator|dfuMANIFEST,
+DECL|dfuUPLOAD_IDLE|enumerator|dfuUPLOAD_IDLE,
+DECL|dfu_runtime_descriptor|struct|struct dfu_runtime_descriptor {
+DECL|dfu_state|enum|enum dfu_state {
+DECL|dfu_status|enum|enum dfu_status {
+DECL|errADDRESS|enumerator|errADDRESS,
+DECL|errCHECK_ERASED|enumerator|errCHECK_ERASED,
+DECL|errERASE|enumerator|errERASE,
+DECL|errFILE|enumerator|errFILE,
+DECL|errFIRMWARE|enumerator|errFIRMWARE,
+DECL|errNOTDONE|enumerator|errNOTDONE,
+DECL|errPOR|enumerator|errPOR,
+DECL|errPROG|enumerator|errPROG,
+DECL|errSTALLEDPKT|enumerator|errSTALLEDPKT
+DECL|errTARGET|enumerator|errTARGET,
+DECL|errUNKNOWN|enumerator|errUNKNOWN,
+DECL|errUSB|enumerator|errUSB,
+DECL|errVENDOR|enumerator|errVENDOR,
+DECL|errVERIFY|enumerator|errVERIFY,
+DECL|errWRITE|enumerator|errWRITE,
+DECL|statusOK|enumerator|statusOK,
+DECL|wDetachTimeOut|member|u16_t wDetachTimeOut;
+DECL|wTransferSize|member|u16_t wTransferSize;
