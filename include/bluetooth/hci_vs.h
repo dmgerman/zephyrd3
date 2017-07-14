a3fc99f6c430018eec042529091b17e80d089bf1
Marcel Holtmann <marcel.holtmann@intel.com>
2017-07-14 11:46:52 +0200

    Bluetooth: controller: Add skeleton for vendor HCI commands
    Signed-off-by: Marcel Holtmann <marcel.holtmann@intel.com>
    
    Former-commit-id: aefcf547ade319170090c9a023283a204882d24b

---
+++ include/bluetooth/hci_vs.h
@@ -0,0 +1,21 @@
+DECL|BT_HCI_OP_VS_READ_SUPPORTED_COMMANDS|macro|BT_HCI_OP_VS_READ_SUPPORTED_COMMANDS
+DECL|BT_HCI_OP_VS_READ_SUPPORTED_FEATURES|macro|BT_HCI_OP_VS_READ_SUPPORTED_FEATURES
+DECL|BT_HCI_OP_VS_READ_VERSION_INFO|macro|BT_HCI_OP_VS_READ_VERSION_INFO
+DECL|__BT_HCI_VS_H|macro|__BT_HCI_VS_H
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|bt_hci_rp_vs_read_supported_commands|struct|struct bt_hci_rp_vs_read_supported_commands {
+DECL|bt_hci_rp_vs_read_supported_features|struct|struct bt_hci_rp_vs_read_supported_features {
+DECL|bt_hci_rp_vs_read_version_info|struct|struct bt_hci_rp_vs_read_version_info {
+DECL|commands|member|u8_t commands[64];
+DECL|features|member|u8_t features[8];
+DECL|fw_build|member|u32_t fw_build;
+DECL|fw_revision|member|u16_t fw_revision;
+DECL|fw_variant|member|u8_t fw_variant;
+DECL|fw_version|member|u8_t fw_version;
+DECL|hw_platform|member|u16_t hw_platform;
+DECL|hw_variant|member|u16_t hw_variant;
+DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
