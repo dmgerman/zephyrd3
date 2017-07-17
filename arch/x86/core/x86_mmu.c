36959939bc14e13b13770645b29cb1b55699b5ac
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2017-07-17 12:07:56 +0530

    x86: MMU: Validate user Buffer
    A user space buffer must be validated before required operation
    can proceed. This API will check the current MMU
    configuration to determine if the buffer held by the user is valid.
    
    Jira: ZEP-2326
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: 079b17b312dda8e13d8352fed0eefb7c0186e49c

---
+++ arch/x86/core/x86_mmu.c
@@ -0,0 +1,7 @@
+DECL|GET_RW_PERM|macro|GET_RW_PERM
+DECL|GET_US_PERM|macro|GET_US_PERM
+DECL|USER_PERM_BIT_POS|macro|USER_PERM_BIT_POS
+DECL|X86_MMU_PD|macro|X86_MMU_PD
+DECL|_x86_mmu_buffer_validate|function|int _x86_mmu_buffer_validate(void *addr, size_t size, int flags)
+DECL|check_pde_flags|function|static inline u32_t check_pde_flags(volatile union x86_mmu_pde_pt pde, u32_t rw_permission, u32_t us_permission)
+DECL|check_pte_flags|function|static inline u32_t check_pte_flags(union x86_mmu_pte pte, u32_t rw_permission, u32_t us_permission)
