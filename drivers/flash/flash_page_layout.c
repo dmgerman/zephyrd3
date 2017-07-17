6a90f0a723e0ed1a7c21700ff3206fef9a8fe681
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2017-07-17 12:03:10 +0200

    Drivers: flash: New API to obtain the flash layout
    Added an internal function to obtain the flash page layout in
    run-length encoded format. The API is simple and allows the actual
    public API implementations to be simple and maintainable.
    This feature can be enabled by using the FLASH_PAGE_LAYOUT Kconfig
    option. This API is required for the implementation of flash file
    system.
    
    Added a public API to get flash page information (size and start offset)
    by offset within the flash and by index of the page.
    Added a generic implementation of the internal flash_get_page_info API.
    Added an additional public API call to get the total count of pages in
    the flash memory and its generic implementation.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    
    Former-commit-id: ce9f0b571ec2a494e2e19d34ebffc0eb21022451

---
+++ drivers/flash/flash_page_layout.c
@@ -0,0 +1,4 @@
+DECL|_flash_get_page_info|function|static int _flash_get_page_info(struct device *dev, off_t offs, bool use_addr, struct flash_pages_info *info)
+DECL|flash_get_page_count|function|size_t flash_get_page_count(struct device *dev)
+DECL|flash_get_page_info_by_idx|function|int flash_get_page_info_by_idx(struct device *dev, u32_t page_index, struct flash_pages_info *info)
+DECL|flash_get_page_info_by_offs|function|int flash_get_page_info_by_offs(struct device *dev, off_t offs,struct flash_pages_info *info)
--- include/flash.h
+++ include/flash.h
@@ -1,14 +1,23 @@
 DECL|__FLASH_H__|macro|__FLASH_H__
 DECL|erase|member|flash_api_erase erase;
 DECL|flash_api_erase|typedef|typedef int (*flash_api_erase)(struct device *dev, off_t offset, size_t size);
+DECL|flash_api_pages_layout|typedef|typedef void (*flash_api_pages_layout)(struct device *dev,
 DECL|flash_api_read|typedef|typedef int (*flash_api_read)(struct device *dev, off_t offset, void *data,
 DECL|flash_api_write_protection|typedef|typedef int (*flash_api_write_protection)(struct device *dev, bool enable);
 DECL|flash_api_write|typedef|typedef int (*flash_api_write)(struct device *dev, off_t offset,
 DECL|flash_driver_api|struct|struct flash_driver_api {
 DECL|flash_erase|function|static inline int flash_erase(struct device *dev, off_t offset, size_t size)
+DECL|flash_pages_info|struct|struct flash_pages_info {
+DECL|flash_pages_layout|struct|struct flash_pages_layout {
 DECL|flash_read|function|static inline int flash_read(struct device *dev, off_t offset, void *data, size_t len)
 DECL|flash_write_protection_set|function|static inline int flash_write_protection_set(struct device *dev, bool enable)
 DECL|flash_write|function|static inline int flash_write(struct device *dev, off_t offset, const void *data, size_t len)
+DECL|index|member|u32_t index;
+DECL|page_layout|member|flash_api_pages_layout page_layout;
+DECL|pages_count|member|size_t pages_count; /* count of pages sequence of the same size */
+DECL|pages_size|member|size_t pages_size;
 DECL|read|member|flash_api_read read;
+DECL|size|member|size_t size;
+DECL|start_offset|member|off_t start_offset; /* offset from the base of flash address */
 DECL|write_protection|member|flash_api_write_protection write_protection;
 DECL|write|member|flash_api_write write;
