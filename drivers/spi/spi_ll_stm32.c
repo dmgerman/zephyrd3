6d429569f904fc0c3b0c92d82cf0293618220f2f
Marti Bolivar <marti.bolivar@linaro.org>
2017-07-17 14:20:13 -0400

    drivers: spi: stm32: add error checking to IRQ mode
    Now that struct spi_context supports passing errors from
    interrupt-driven I/O handlers to waiting threads, we can enable error
    interrupts and propagate errors to spi_transceive() callers.
    
    To make it easier for users to debug SPI-related issues, log any error
    bits set in SR when failures occur.
    
    A subsequent patch will add error checking to polled mode as well, but
    other cleanups and fixes will go in first to make this easier.
    
    Note that this breaks the spi_loopback test on some targets, but it's
    not a regression, as it wasn't working properly anyway. Subsequent
    patches the bugs that this error checking has exposed.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: 69bc5ebdf3e878e6b1f181cc437e4fe1708e12ae

--- drivers/spi/spi_ll_stm32.c
+++ drivers/spi/spi_ll_stm32.c
@@ -1,14 +1,18 @@
 DECL|CONFIG_CFG|macro|CONFIG_CFG
 DECL|CONFIG_DATA|macro|CONFIG_DATA
+DECL|SPI_STM32_ERR_MSK|macro|SPI_STM32_ERR_MSK
+DECL|SPI_STM32_ERR_MSK|macro|SPI_STM32_ERR_MSK
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|api_funcs|variable|api_funcs
 DECL|spi_stm32_cfg_1|variable|spi_stm32_cfg_1
 DECL|spi_stm32_cfg_2|variable|spi_stm32_cfg_2
 DECL|spi_stm32_cfg_3|variable|spi_stm32_cfg_3
+DECL|spi_stm32_complete|function|static void spi_stm32_complete(struct spi_stm32_data *data, SPI_TypeDef *spi, int status)
 DECL|spi_stm32_configure|function|static int spi_stm32_configure(struct spi_config *config)
 DECL|spi_stm32_dev_data_1|variable|spi_stm32_dev_data_1
 DECL|spi_stm32_dev_data_2|variable|spi_stm32_dev_data_2
 DECL|spi_stm32_dev_data_3|variable|spi_stm32_dev_data_3
+DECL|spi_stm32_get_err|function|static int spi_stm32_get_err(SPI_TypeDef *spi)
 DECL|spi_stm32_init|function|static int spi_stm32_init(struct device *dev)
 DECL|spi_stm32_irq_config_func_1|function|static void spi_stm32_irq_config_func_1(struct device *dev)
 DECL|spi_stm32_irq_config_func_2|function|static void spi_stm32_irq_config_func_2(struct device *dev)
