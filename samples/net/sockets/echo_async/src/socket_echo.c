34d60c925ae38d2f76f0b8c4d25a645453b51b61
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-07-18 16:25:31 +0300

    samples: sockets: Add async echo server example
    Implements asynchronous TCP echo server using non-blocking sockets
    and poll, with concurrent connections support.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: 00e8309f0316988f824882e96366d286c4494a8c

---
+++ samples/net/sockets/echo_async/src/socket_echo.c
@@ -0,0 +1,9 @@
+DECL|NUM_FDS|macro|NUM_FDS
+DECL|init_net|function|void init_net(void)
+DECL|init_net|macro|init_net
+DECL|main|function|int main(void)
+DECL|nonblock|function|static void nonblock(int fd)
+DECL|pollfds_add|function|int pollfds_add(int fd)
+DECL|pollfds_del|function|void pollfds_del(int fd)
+DECL|pollfds|variable|pollfds
+DECL|pollnum|variable|pollnum
