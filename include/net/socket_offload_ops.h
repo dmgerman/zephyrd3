166f83e1f3d4d61dfe5df8d430ae3257fa0676da
Gil Pitney <gil.pitney@linaro.org>
2017-07-18 13:54:53 -0700

    net: sockets: Add a socket offload module
    This patch enables BSD socket offload to a dedicated
    TCP/IP offload engine.
    
    This provides a simpler, more direct mechanism than going
    through NET_OFFLOAD (zsock -> net_context -> socket conversions)
    for those devices which provide complete TCP/IP offload at the
    BSD socket level, and whose use cases do not require
    IP routing between multiple network interfaces.
    
    To use, configure CONFIG_NET_SOCKETS_OFFLOAD=y, and register
    socket_offload_ops with this module.
    
    Fixes #3706
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>
    
    Former-commit-id: c21b0fb357c6cb0515af9c20b73cb6c0597d08c8

---
+++ include/net/socket_offload_ops.h
@@ -0,0 +1,15 @@
+DECL|__SOCKET_OFFLOAD_OPS_H__|macro|__SOCKET_OFFLOAD_OPS_H__
+DECL|accept|member|int (*accept)(int sock, struct sockaddr *addr, socklen_t *addrlen);
+DECL|bind|member|int (*bind)(int sock, const struct sockaddr *addr, socklen_t addrlen);
+DECL|close|member|int (*close)(int sock);
+DECL|connect|member|int (*connect)(int sock, const struct sockaddr *addr,
+DECL|getsockopt|member|int (*getsockopt)(int sock, int level, int optname, void *optval,
+DECL|listen|member|int (*listen)(int sock, int backlog);
+DECL|poll|member|int (*poll)(struct pollfd *fds, int nfds, int timeout);
+DECL|recvfrom|member|ssize_t (*recvfrom)(int sock, void *buf, short int len,
+DECL|recv|member|ssize_t (*recv)(int sock, void *buf, size_t max_len, int flags);
+DECL|sendto|member|ssize_t (*sendto)(int sock, const void *buf, size_t len, int flags,
+DECL|send|member|ssize_t (*send)(int sock, const void *buf, size_t len, int flags);
+DECL|setsockopt|member|int (*setsockopt)(int sock, int level, int optname,
+DECL|socket_offload|struct|struct socket_offload {
+DECL|socket|member|int (*socket)(int family, int type, int proto);
---
+++ subsys/net/lib/sockets/socket_offload.c
@@ -0,0 +1,2 @@
+DECL|socket_offload_register|function|void socket_offload_register(const struct socket_offload *ops)
+DECL|socket_ops|variable|socket_ops
