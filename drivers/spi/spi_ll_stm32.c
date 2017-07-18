bcc36f6c2c07e3a17730313268b3d3f828150ee0
Marti Bolivar <marti.bolivar@linaro.org>
2017-07-18 13:02:33 -0400

    drivers: spi: stm32: fix transmit/receive procedure
    The transmit and receive procedure used in the STM32 SPI driver is not
    correct.
    
    On STM32F4, this is causing OVR errors (per the logged error mask) and
    transmission of undesired 0x00 bytes (verified with a logic analyzer).
    
    The root cause is that the receive register is not read (via DR, when
    RXNE is set) each time the transmit register is written (also via DR,
    when TXE is set). This clearly causes OVR errors when there is no
    FIFO, as the receive register needs to be read each time a frame is
    transceived, or the IP block has no way of knowing that the
    overwritten data were not important.
    
    Adapt the I/O procedure so that every DR write is matched by a DR
    read, blocking until the relevant flags are set if necessary.
    
    This behavior is suboptimal for targets such as STM32L4, where there
    is a SPI FIFO. However, SPI I/O is broken on those targets, and this
    patch fixes them as well. Further optimizations for targets with FIFOs
    is left to future work.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: f3b18bc2e91fd120d3d2ca3496f26c385e49d0e2

--- drivers/spi/spi_ll_stm32.c
+++ drivers/spi/spi_ll_stm32.c
@@ -2,6 +2,7 @@ DECL|CONFIG_CFG|macro|CONFIG_CFG
 DECL|CONFIG_DATA|macro|CONFIG_DATA
 DECL|SPI_STM32_ERR_MSK|macro|SPI_STM32_ERR_MSK
 DECL|SPI_STM32_ERR_MSK|macro|SPI_STM32_ERR_MSK
+DECL|SPI_STM32_TX_NOP|macro|SPI_STM32_TX_NOP
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|api_funcs|variable|api_funcs
 DECL|spi_stm32_cfg_1|variable|spi_stm32_cfg_1
@@ -18,9 +19,12 @@ DECL|spi_stm32_irq_config_func_1|function|static void spi_stm32_irq_config_func_
 DECL|spi_stm32_irq_config_func_2|function|static void spi_stm32_irq_config_func_2(struct device *dev)
 DECL|spi_stm32_irq_config_func_3|function|static void spi_stm32_irq_config_func_3(struct device *dev)
 DECL|spi_stm32_isr|function|static void spi_stm32_isr(void *arg)
-DECL|spi_stm32_receive|function|static void spi_stm32_receive(SPI_TypeDef *spi, struct spi_stm32_data *data)
+DECL|spi_stm32_next_tx|function|static inline u8_t spi_stm32_next_tx(struct spi_stm32_data *data)
 DECL|spi_stm32_release|function|static int spi_stm32_release(struct spi_config *config)
+DECL|spi_stm32_shift_frames|function|static int spi_stm32_shift_frames(SPI_TypeDef *spi, struct spi_stm32_data *data)
+DECL|spi_stm32_shift_m|function|static void spi_stm32_shift_m(SPI_TypeDef *spi, struct spi_stm32_data *data)
+DECL|spi_stm32_shift_s|function|static void spi_stm32_shift_s(SPI_TypeDef *spi, struct spi_stm32_data *data)
 DECL|spi_stm32_transceive_async|function|static int spi_stm32_transceive_async(struct spi_config *config, const struct spi_buf *tx_bufs, size_t tx_count, struct spi_buf *rx_bufs, size_t rx_count,
 DECL|spi_stm32_transceive|function|static int spi_stm32_transceive(struct spi_config *config,const struct spi_buf *tx_bufs, u32_t tx_count, struct spi_buf *rx_bufs, u32_t rx_count)
-DECL|spi_stm32_transmit|function|static void spi_stm32_transmit(SPI_TypeDef *spi, struct spi_stm32_data *data)
+DECL|spi_stm32_transfer_ongoing|function|static bool spi_stm32_transfer_ongoing(struct spi_stm32_data *data)
 DECL|transceive|function|static int transceive(struct spi_config *config, const struct spi_buf *tx_bufs, u32_t tx_count, struct spi_buf *rx_bufs, u32_t rx_count, bool asynchronous, struct k_poll_signal *signal)
