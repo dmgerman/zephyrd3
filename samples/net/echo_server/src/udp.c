b0422bc9f3b06212b00150cc5bf5b9b7ccf666c1
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-07-18 14:03:19 +0300

    samples: net: echo-server: Add DTLS support
    Convert echo-server to use DTLS for UDP connections if DTLS
    is enabled by prj.conf file.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 72ac16642952aa25c2043710b7dfeabfff13641f

--- samples/net/echo_server/src/udp.c
+++ samples/net/echo_server/src/udp.c
@@ -1,9 +1,15 @@
+DECL|APP_BANNER|macro|APP_BANNER
+DECL|INSTANCE_INFO|macro|INSTANCE_INFO
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|RESULT_BUF_SIZE|macro|RESULT_BUF_SIZE
+DECL|RX_FIFO_DEPTH|macro|RX_FIFO_DEPTH
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|data_udp_pool|function|static struct net_buf_pool *data_udp_pool(void)
 DECL|data_udp_pool|macro|data_udp_pool
+DECL|dtls_result|variable|dtls_result
 DECL|set_dst_addr|function|static inline void set_dst_addr(sa_family_t family,struct net_pkt *pkt, struct sockaddr *dst_addr)
+DECL|setup_cert|function|static int setup_cert(struct net_app_ctx *ctx, mbedtls_x509_crt *cert, mbedtls_pk_context *pkey)
 DECL|start_udp|function|void start_udp(void)
 DECL|stop_udp|function|void stop_udp(void)
 DECL|tx_udp_slab|function|static struct k_mem_slab *tx_udp_slab(void)
