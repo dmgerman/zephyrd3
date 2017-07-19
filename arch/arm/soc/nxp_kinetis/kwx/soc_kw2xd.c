57e8cef077e9461fa90c6beebad5478a196adef9
Johann Fischer <j.fischer@phytec.de>
2017-07-19 16:49:35 +0200

    arch: nxp_kinetis: initial import KW2XD SiP
    Jira: ZEP-1471
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: af1a8fc9f16ddffb6396ef10f079ea22b2dbf8e1

---
+++ arch/arm/soc/nxp_kinetis/kwx/soc_kw2xd.c
@@ -0,0 +1,14 @@
+DECL|ER32KSEL_LPO1KHZ|macro|ER32KSEL_LPO1KHZ
+DECL|ER32KSEL_OSC32KCLK|macro|ER32KSEL_OSC32KCLK
+DECL|ER32KSEL_RTC|macro|ER32KSEL_RTC
+DECL|PLLFLLSEL_IRC48MHZ|macro|PLLFLLSEL_IRC48MHZ
+DECL|PLLFLLSEL_MCGFLLCLK|macro|PLLFLLSEL_MCGFLLCLK
+DECL|PLLFLLSEL_MCGPLLCLK|macro|PLLFLLSEL_MCGPLLCLK
+DECL|TIMESRC_OSCERCLK|macro|TIMESRC_OSCERCLK
+DECL|__kinetis_flash_config|variable|__kinetis_flash_config
+DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
+DECL|kw2xd_init|function|static int kw2xd_init(struct device *arg)
+DECL|oscConfig|variable|oscConfig
+DECL|pll0Config|variable|pll0Config
+DECL|set_modem_clock|function|static void set_modem_clock(void)
+DECL|simConfig|variable|simConfig
