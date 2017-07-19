45aaab3be5788f170ee8030aa428b4bd921b925b
Leandro Pereira <leandro.pereira@intel.com>
2017-07-19 13:50:50 -0700

    arch: xtensa: Move exception table to xtensa_intr.c
    This cleans up the exception handling by removing the table declaration
    from xtensa_intr_asm.S, and removing the unused
    _xt_set_exception_handler() function.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 99181eb661b417e35023f5627e5303d9019e06c8

--- arch/xtensa/core/xtensa_intr.c
+++ arch/xtensa/core/xtensa_intr.c
@@ -1,3 +1,2 @@
 DECL|_irq_spurious|function|void _irq_spurious(void *arg)
-DECL|_xt_set_exception_handler|function|xt_exc_handler _xt_set_exception_handler(int n, xt_exc_handler f)
-DECL|xt_unhandled_exception|function|void xt_unhandled_exception(XtExcFrame *frame)
+DECL|unhandled_exception_trampoline|function|static void unhandled_exception_trampoline(XtExcFrame *frame)
