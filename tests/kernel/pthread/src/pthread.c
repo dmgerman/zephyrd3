fc7587e2b1f7980f4905cc3abee6f9c3c3a84403
Andy Ross <andrew.j.ross@intel.com>
2017-07-24 15:02:24 -0700

    tests/kernel: Add pthread API test
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 40e669e798798c9fcf298170183e5446e5c3981a

---
+++ tests/kernel/pthread/src/pthread.c
@@ -0,0 +1,15 @@
+DECL|BOUNCES|macro|BOUNCES
+DECL|N_THR|macro|N_THR
+DECL|STACKSZ|macro|STACKSZ
+DECL|barrier_done|variable|barrier_done
+DECL|barrier_failed|variable|barrier_failed
+DECL|barrier_test_done|function|int barrier_test_done(void)
+DECL|bounce_done|variable|bounce_done
+DECL|bounce_failed|variable|bounce_failed
+DECL|bounce_test_done|function|int bounce_test_done(void)
+DECL|curr_bounce_thread|variable|curr_bounce_thread
+DECL|cvar0|variable|cvar0
+DECL|cvar1|variable|cvar1
+DECL|lock|variable|lock
+DECL|main|function|void main(void)
+DECL|thread_top|function|void *thread_top(void *p1, void *p2, void *p3)
