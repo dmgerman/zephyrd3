4a562c7efe53f671fd0159ca02b48ff07f79c1d4
Punit Vara <punit.vara@intel.com>
2017-07-25 16:25:46 +0530

    tests: errno: convert test case to use ztest
    This patch convert normal test case to use ztest framework
    APIs and remove unnecessary output.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: cbf40bc6d381acb51bfc4cfd5e635d265ee45ca8

---
+++ tests/kernel/errno/src/errno.c
@@ -0,0 +1,11 @@
+DECL|N_THREADS|macro|N_THREADS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|errno_fiber|function|static void errno_fiber(int n, int my_errno)
+DECL|errno_values|variable|errno_values
+DECL|fifo|variable|fifo
+DECL|pass|member|int pass;
+DECL|q|member|void *q;
+DECL|result|struct|struct result {
+DECL|result|variable|result
+DECL|test_thread_context|function|void test_thread_context(void)
+DECL|threads|variable|threads
