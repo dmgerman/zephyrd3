bb9f4742aba1a424f93b4a9b6cb4a5275e84e65a
Andrew Boie <andrew.p.boie@intel.com>
2017-07-25 19:05:16 -0700

    mpu_stack_guard_test: fix stack declaration
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 768e2a9617ff809e1cd8b5dbff99a26a7318083a

--- samples/mpu/mpu_stack_guard_test/src/main.c
+++ samples/mpu/mpu_stack_guard_test/src/main.c
@@ -8,5 +8,4 @@ DECL|main|function|void main(void)
 DECL|recursive_loop|function|u32_t recursive_loop(u32_t counter)
 DECL|stack_guard_buffer|struct|struct stack_guard_buffer {
 DECL|stack_guard_thread|function|void stack_guard_thread(void *dummy1, void *dummy2, void *dummy3)
-DECL|stack|member|u8_t stack[STACKSIZE];
 DECL|test_thread|variable|test_thread
