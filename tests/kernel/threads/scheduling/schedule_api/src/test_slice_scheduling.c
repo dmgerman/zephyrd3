8f2076f796107b6a77418442c6b83cd9bad8bb74
Youvedeep Singh <youvedeep.singh@intel.com>
2017-07-26 15:54:06 +0530

    tests: kernel: Stress test for round robin scheduling.
    This creates 10 threads with equal priority and tests predictibility
    of picking all threads in round robin fashion. Test fails when any
    thread consumes more time than time slice allocated to it or threads
    are not scheduled in round robin fashion.
    
    Jira: ZEP-2371
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: ee5d84ca9a2be7a8a2fe40f9f2881427002b8a88

---
+++ tests/kernel/threads/scheduling/schedule_api/src/test_slice_scheduling.c
@@ -0,0 +1,12 @@
+DECL|BASE_PRIORITY|macro|BASE_PRIORITY
+DECL|BUSY_MS|macro|BUSY_MS
+DECL|ITRERATION_COUNT|macro|ITRERATION_COUNT
+DECL|NUM_THREAD|macro|NUM_THREAD
+DECL|NUM_THREAD|macro|NUM_THREAD
+DECL|SLICE_SIZE|macro|SLICE_SIZE
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|elapsed_slice|variable|elapsed_slice
+DECL|test_slice_scheduling|function|void test_slice_scheduling(void)
+DECL|thread_idx|variable|thread_idx
+DECL|thread_tslice|function|static void thread_tslice(void *p1, void *p2, void *p3)
+DECL|t|variable|t
