51a9b58d79d4769b0886495442182f01400f15bd
Shiksha Patel <shiksha.patel@nxp.com>
2017-07-26 06:36:39 -0500

    lpc: Add pinmux shim driver for lpcxpresso54114
    Adds a new pinmux driver for lpcxpresso54114 based on mcux.
    
    Signed-off-by: Shiksha Patel <shiksha.patel@nxp.com>
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 40d9763611675401c6389bcae5494bb28777afbf

---
+++ drivers/pinmux/pinmux_mcux_lpc.c
@@ -0,0 +1,14 @@
+DECL|PORT0_IDX|macro|PORT0_IDX
+DECL|PORT1_IDX|macro|PORT1_IDX
+DECL|base|member|IOCON_Type *base;
+DECL|clock_ip_name|member|clock_ip_name_t clock_ip_name;
+DECL|pinmux_mcux_driver_api|variable|pinmux_mcux_driver_api
+DECL|pinmux_mcux_lpc_config|struct|struct pinmux_mcux_lpc_config {
+DECL|pinmux_mcux_lpc_get|function|static int pinmux_mcux_lpc_get(struct device *dev, u32_t pin, u32_t *func)
+DECL|pinmux_mcux_lpc_init|function|static int pinmux_mcux_lpc_init(struct device *dev)
+DECL|pinmux_mcux_lpc_input|function|static int pinmux_mcux_lpc_input(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_mcux_lpc_port0_config|variable|pinmux_mcux_lpc_port0_config
+DECL|pinmux_mcux_lpc_port1_config|variable|pinmux_mcux_lpc_port1_config
+DECL|pinmux_mcux_lpc_pullup|function|static int pinmux_mcux_lpc_pullup(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_mcux_lpc_set|function|static int pinmux_mcux_lpc_set(struct device *dev, u32_t pin, u32_t func)
+DECL|port_no|member|u32_t port_no;
