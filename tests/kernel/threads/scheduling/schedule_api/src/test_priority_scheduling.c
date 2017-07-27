275594bdf9c263b43374499a25f90ef53cc107f7
Youvedeep Singh <youvedeep.singh@intel.com>
2017-07-27 17:32:50 +0530

    tests: kernel: Stress test for preemptive scheduling.
    Tests if preemptive threads are picked up as per priority.
    This creates 10 threads with priority in increasing order
    from 1 to N and each thread prints an Alphabet.
    This test fails when threads are picked up out of order.
    
    Jira: ZEP-2370
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 1c856d2b10e80f86ed0b876526dc927b183a2ba2

---
+++ tests/kernel/threads/scheduling/schedule_api/src/test_priority_scheduling.c
@@ -0,0 +1,9 @@
+DECL|BASE_PRIORITY|macro|BASE_PRIORITY
+DECL|ITRERATION_COUNT|macro|ITRERATION_COUNT
+DECL|NUM_THREAD|macro|NUM_THREAD
+DECL|NUM_THREAD|macro|NUM_THREAD
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|test_priority_scheduling|function|void test_priority_scheduling(void)
+DECL|thread_idx|variable|thread_idx
+DECL|thread_tslice|function|static void thread_tslice(void *p1, void *p2, void *p3)
+DECL|t|variable|t
