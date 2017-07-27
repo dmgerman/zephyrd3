e77eae8bc3a2c322899f899d096f786f3cb15983
Maureen Helm <maureen.helm@nxp.com>
2017-07-27 14:37:25 -0500

    samples: rgb_led: Add support for hexiwear_k64
    The hexiwear_k64 board can drive the leds with a pwm, so update the
    rgb_led sample to work with this board.
    
    Jira: ZEP-2025
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: a580bc5b2ebdaed1f6da9c68e46dc45147accdd2

--- samples/basic/rgb_led/src/main.c
+++ samples/basic/rgb_led/src/main.c
@@ -1,5 +1,16 @@
 DECL|PERIOD|macro|PERIOD
-DECL|PWM_DEV|macro|PWM_DEV
+DECL|PWM_CH0|macro|PWM_CH0
+DECL|PWM_CH0|macro|PWM_CH0
+DECL|PWM_CH1|macro|PWM_CH1
+DECL|PWM_CH1|macro|PWM_CH1
+DECL|PWM_CH2|macro|PWM_CH2
+DECL|PWM_CH2|macro|PWM_CH2
+DECL|PWM_DEV0|macro|PWM_DEV0
+DECL|PWM_DEV0|macro|PWM_DEV0
+DECL|PWM_DEV1|macro|PWM_DEV1
+DECL|PWM_DEV1|macro|PWM_DEV1
+DECL|PWM_DEV2|macro|PWM_DEV2
+DECL|PWM_DEV2|macro|PWM_DEV2
 DECL|STEPSIZE|macro|STEPSIZE
 DECL|main|function|void main(void)
 DECL|write_pin|function|static int write_pin(struct device *pwm_dev, u32_t pwm_pin, u32_t pulse_width)
