64872c1a5592ebcb605364b343f486fe884d1dd0
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-07-28 15:32:05 +0300

    samples: net: http: client: Use net_buf context pool for TX
    If CONFIG_NET_CONTEXT_NET_PKT_POOL is defined, which is the
    default for Bluetooth, then create the pool and use it when
    sending network packets. This is needed when trying to send
    TCP packets using Bluetooth IPSP.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c724a5730cd7bbfd7f846a5618f86ee65aa71fbc

--- samples/net/https_client/src/main.c
+++ samples/net/https_client/src/main.c
@@ -7,6 +7,8 @@ DECL|RX_FIFO_DEPTH|macro|RX_FIFO_DEPTH
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|ctx|member|struct http_client_ctx *ctx;
+DECL|data_pool|function|static struct net_buf_pool *data_pool(void)
+DECL|data_pool|macro|data_pool
 DECL|do_async_http_req|function|static int do_async_http_req(struct http_client_ctx *ctx, enum http_method method, const char *url, const char *content_type, const char *payload)
 DECL|do_async_reqs|function|static inline int do_async_reqs(struct http_client_ctx *ctx, int count)
 DECL|do_sync_http_req|function|static int do_sync_http_req(struct http_client_ctx *ctx, enum http_method method, const char *url, const char *content_type, const char *payload)
@@ -19,5 +21,7 @@ DECL|response|function|void response(struct http_client_ctx *ctx, u8_t *data, si
 DECL|result|variable|result
 DECL|setup_cert|function|static int setup_cert(struct http_client_ctx *ctx, void *cert)
 DECL|total_len|member|size_t total_len;
+DECL|tx_slab|function|static struct k_mem_slab *tx_slab(void)
+DECL|tx_slab|macro|tx_slab
 DECL|waiter|struct|struct waiter {
 DECL|wait|member|struct k_sem wait;
