d636ba6765079e2f619ab044824afa45865c9525
Niranjhana N <niranjhana.n@intel.com>
2017-08-02 13:33:11 +0530

    tests: mbedtls: convert to ztest
    - replaced CONFIG_MAIN_STACK_SIZE with CONFIG_ZTEST_STACKSIZE
      in the config file
    - split the main file into two files:
        - main.c has ztest entry and runs separate functions for ztest
        - mbedtls.c has the original routines
    
    JIRA: ZEP-2449
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 4e40b843e436b8ebb04ef4c765d9abb69f3f1e70

--- tests/crypto/mbedtls/src/mbedtls.c
+++ tests/crypto/mbedtls/src/mbedtls.c
@@ -4,10 +4,10 @@ DECL|MBEDTLS_PRINT|macro|MBEDTLS_PRINT
 DECL|MBEDTLS_PRINT|macro|MBEDTLS_PRINT
 DECL|buf|variable|buf
 DECL|create_entropy_seed_file|function|static void create_entropy_seed_file(void)
-DECL|main|function|int main(void)
 DECL|mbedtls_exit|macro|mbedtls_exit
 DECL|mbedtls_printf|macro|mbedtls_printf
 DECL|mbedtls_snprintf|macro|mbedtls_snprintf
 DECL|rand|function|int rand(void)
 DECL|run_test_snprintf|function|static int run_test_snprintf(void)
+DECL|test_mbedtls|function|void test_mbedtls(void)
 DECL|test_snprintf|function|static int test_snprintf(size_t n, const char ref_buf[10], int ref_ret)
