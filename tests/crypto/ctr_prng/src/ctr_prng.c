9157292707d9bf3007c15c99e37c19eaa01c73f3
Niranjhana N <niranjhana.n@intel.com>
2017-08-02 13:26:34 +0530

    tests: ctr_prng: convert to ztest
    - replaced test points with ztest APIs wherever possible
    - replaced CONFIG_MAIN_STACK_SIZE with CONFIG_ZTEST_STACKSIZE
      in the config file
    - split the main file into two files:
        - main.c has ztest entry and runs separate functions for ztest
        - ctr_prng.c has the original routines
    
    JIRA: ZEP-2449
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 83f77de779a289c0e93b53eba9b60393ee7b65ce

--- tests/crypto/ctr_prng/src/ctr_prng.c
+++ tests/crypto/ctr_prng/src/ctr_prng.c
@@ -3,18 +3,18 @@ DECL|MAX_EXPECTED_STRING|macro|MAX_EXPECTED_STRING
 DECL|RC_STR|macro|RC_STR
 DECL|ai1|variable|ai1
 DECL|ai2|variable|ai2
-DECL|char_to_nibble|function|u8_t char_to_nibble(char c)
+DECL|char_to_nibble|function|static u8_t char_to_nibble(char c)
 DECL|entropy|member|char *entropy;
 DECL|expected|member|char *expected;
 DECL|extra1|member|char *extra1; /* may be null */
 DECL|extra2|member|char *extra2; /* may be null */
-DECL|hex_str_to_num|function|void hex_str_to_num(u8_t *buf, char *in)
-DECL|main|function|int main(void)
+DECL|hex_str_to_num|function|static void hex_str_to_num(u8_t *buf, char *in)
 DECL|personal|member|char *personal; /* may be null */
 DECL|per|variable|per
 DECL|prng_vector|struct|struct prng_vector {
-DECL|test_prng_vector|function|int test_prng_vector(struct prng_vector *v)
-DECL|test_reseed|function|int test_reseed(void)
-DECL|test_robustness|function|int test_robustness(void)
-DECL|test_uninstantiate|function|int test_uninstantiate(void)
+DECL|test_ctr_prng_vector|function|void test_ctr_prng_vector(void)
+DECL|test_prng_vector|function|static int test_prng_vector(struct prng_vector *v)
+DECL|test_reseed|function|void test_reseed(void)
+DECL|test_robustness|function|void test_robustness(void)
+DECL|test_uninstantiate|function|void test_uninstantiate(void)
 DECL|vectors|variable|vectors
