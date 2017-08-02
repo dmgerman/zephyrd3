1a8abedc650db2e3394b0768e2e41f70195f4e77
Niranjhana N <niranjhana.n@intel.com>
2017-08-02 13:29:28 +0530

    tests: ecc_dsa: convert to ztest
    - replaced test points with ztest APIs wherever possible
    - replaced CONFIG_MAIN_STACK_SIZE with CONFIG_ZTEST_STACKSIZE
      in the config file
    - split the main file into two files:
        - main.c has ztest entry and runs separate functions for ztest
        - ecc_dsa.c has the original routines
    
    JIRA: ZEP-2449
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 712e2a4dc0b2bd791a32d957e63a4ccfeb684e75

--- tests/crypto/ecc_dsa/src/ecc_dsa.c
+++ tests/crypto/ecc_dsa/src/ecc_dsa.c
@@ -3,7 +3,7 @@ DECL|ENABLE_TESTS|macro|ENABLE_TESTS
 DECL|cavp_sign|function|int cavp_sign(bool verbose)
 DECL|cavp_verify|function|int cavp_verify(bool verbose)
 DECL|default_CSPRNG|function|int default_CSPRNG(u8_t *dest, unsigned int size)
-DECL|main|function|int main(void)
 DECL|montecarlo_signverify|function|int montecarlo_signverify(int num_tests, bool verbose)
 DECL|sign_vectors|function|int sign_vectors(TCSha256State_t hash, char **d_vec, char **k_vec, char **msg_vec, char **qx_vec, char **qy_vec, char **r_vec, char **s_vec, int tests, bool verbose)
+DECL|test_ecc_dsa|function|void test_ecc_dsa(void)
 DECL|vrfy_vectors|function|int vrfy_vectors(TCSha256State_t hash, char **msg_vec, char **qx_vec, char **qy_vec, char **r_vec, char **s_vec, int res_vec[], int tests, bool verbose)
