3b9aefd409ec5927393906a7fc232167218625cb
Niranjhana N <niranjhana.n@intel.com>
2017-08-02 09:31:15 +0530

    tests: aes: convert to ztest
    - replaced test points with ztest APIs wherever possible
    - replaced CONFIG_MAIN_STACK_SIZE with CONFIG_ZTEST_STACKSIZE
      in the config file
    - split the main file into two files:
        - main.c has ztest entry and runs separate functions for ztest
        - aes.c has the original routines
    
    JIRA: ZEP-2449
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 851314ba8e0af05c7d530cd9e17e7e8d09bf9e36

--- tests/crypto/aes/src/aes.c
+++ tests/crypto/aes/src/aes.c
@@ -2,11 +2,10 @@ DECL|NUM_OF_FIXED_KEYS|macro|NUM_OF_FIXED_KEYS
 DECL|NUM_OF_NIST_KEYS|macro|NUM_OF_NIST_KEYS
 DECL|in|member|u8_t in[NUM_OF_NIST_KEYS];
 DECL|kat_table|struct|struct kat_table {
-DECL|main|function|void main(void)
 DECL|out|member|u8_t out[NUM_OF_NIST_KEYS];
-DECL|test_1|function|u32_t test_1(void)
-DECL|test_2|function|s32_t test_2(void)
-DECL|test_3|function|u32_t test_3(void)
-DECL|test_4|function|u32_t test_4(void)
+DECL|test_1|function|void test_1(void)
+DECL|test_2|function|void test_2(void)
+DECL|test_3|function|void test_3(void)
+DECL|test_4|function|void test_4(void)
 DECL|var_key_test|function|u32_t var_key_test(u32_t r, const u8_t *in, const u8_t *out)
 DECL|var_text_test|function|u32_t var_text_test(u32_t r, const u8_t *in, const u8_t *out, TCAesKeySched_t s)
