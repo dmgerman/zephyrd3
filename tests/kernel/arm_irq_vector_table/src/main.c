ea750590c6bb149d96893e38a52119bef939d987
Niranjhana N <niranjhana.n@intel.com>
2017-08-03 11:33:20 +0530

    tests: arm_irq_vector_table: convert to ztest
    - file already had ztest functions
    - separated the main file into two:
        - main.c, which has the ztest entry
        - arm_irq_vector_table.c, which has the original routines
    
    JIRA: ZEP-2382
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 8dc47d6a3b547b5b3c22e294355c2f37bf8f857f

--- tests/kernel/arm_irq_vector_table/src/main.c
+++ tests/kernel/arm_irq_vector_table/src/main.c
@@ -1,8 +1,1 @@
-DECL|_irq_vector_table|variable|_irq_vector_table
-DECL|isr0|function|void isr0(void)
-DECL|isr1|function|void isr1(void)
-DECL|isr2|function|void isr2(void)
-DECL|sem|variable|sem
-DECL|test_irq_vector_table|function|void test_irq_vector_table(void)
 DECL|test_main|function|void test_main(void)
-DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
