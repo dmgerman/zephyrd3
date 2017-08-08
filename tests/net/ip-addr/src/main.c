0ca9aef4aa0f147e242b8c6123fccd0102e094a2
Niranjhana N <niranjhana.n@intel.com>
2017-08-08 09:31:05 +0530

    tests: ip-addr: convert to ztest
    - replaced test points with ztest APIs wherever possible
    - replaced CONFIG_MAIN_STACK_SIZE with CONFIG_ZTEST_STACKSIZE
      in config file, reduced the size by half
    
    JIRA: ZEP-2249
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: fbb6cb4fdd21997613d1a32a96a02e8a9cb0012f

--- tests/net/ip-addr/src/main.c
+++ tests/net/ip-addr/src/main.c
@@ -10,12 +10,12 @@ DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
 DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
 DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|mac_addr|member|u8_t mac_addr[6];
-DECL|main|function|void main(void)
 DECL|net_test_context_data|variable|net_test_context_data
 DECL|net_test_context|struct|struct net_test_context {
 DECL|net_test_get_mac|function|static u8_t *net_test_get_mac(struct device *dev)
 DECL|net_test_if_api|variable|net_test_if_api
 DECL|net_test_iface_init|function|static void net_test_iface_init(struct net_if *iface)
 DECL|net_test_init|function|int net_test_init(struct device *dev)
-DECL|run_tests|function|static bool run_tests(void)
+DECL|run_tests|function|void run_tests(void)
+DECL|test_main|function|void test_main(void)
 DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_pkt *pkt)
