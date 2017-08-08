f63d7d46c17c4978e39e5e2685a665efa92ff262
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-08-08 12:05:49 +0300

    net: if: Helper to return first IEEE 802.15.4 interface
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 53928aae76c45b9a521e1d39f106787b0492557c

--- include/net/net_if.h
+++ include/net/net_if.h
@@ -62,6 +62,7 @@ DECL|net_if_addr|struct|struct net_if_addr {
 DECL|net_if_api|struct|struct net_if_api {
 DECL|net_if_cb_t|typedef|typedef void (*net_if_cb_t)(struct net_if *iface, void *user_data);
 DECL|net_if_get_device|function|static inline struct device *net_if_get_device(struct net_if *iface)
+DECL|net_if_get_ieee802154|function|static inline struct net_if *net_if_get_ieee802154(void)
 DECL|net_if_get_link_addr|function|static inline struct net_linkaddr *net_if_get_link_addr(struct net_if *iface)
 DECL|net_if_get_ll_reserve|function|static inline u16_t net_if_get_ll_reserve(struct net_if *iface, const struct in6_addr *dst_ip6)
 DECL|net_if_get_mtu|function|static inline u16_t net_if_get_mtu(struct net_if *iface)
