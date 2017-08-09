76444248840cb178a81290258f5e60f1d317a686
Maciej Debski <maciej.debski@rndity.com>
2017-08-09 11:21:53 +0200

    drivers: clock_control: provide support for stm32f0.
    Fixes #3923
    
    Signed-off-by: Maciej Debski <maciej.debski@rndity.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    
    Former-commit-id: d0678201c311341d2d8881753cd3561e85743f5f

---
+++ drivers/clock_control/stm32f0x_ll_clock.c
@@ -0,0 +1,3 @@
+DECL|LL_RCC_MSI_Disable|function|void LL_RCC_MSI_Disable(void)
+DECL|config_enable_default_clocks|function|void config_enable_default_clocks(void)
+DECL|config_pll_init|function|void config_pll_init(LL_UTILS_PLLInitTypeDef *pllinit)
