aa03c42e1983a53cd60f1de40697e86065d272b7
Niranjhana N <niranjhana.n@intel.com>
2017-08-09 09:50:47 +0530

    tests: http_header_fields: convert to ztest
    - replaced test points with ztest APIs wherever possible
    - added CONFIG_ZTEST_STACKSIZE=320
    
    JIRA: ZEP-2249
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 9f182e07b197c441e996d72dc353f50a877a6e83

--- tests/net/lib/http_header_fields/src/http_header_fields.c
+++ tests/net/lib/http_header_fields/src/http_header_fields.c
@@ -1,20 +1,21 @@
 DECL|RC_STR|macro|RC_STR
 DECL|is_connect|member|int is_connect;
-DECL|main|function|void main(void)
 DECL|name|member|const char *name;
 DECL|rv|member|int rv;
 DECL|settings_null|variable|settings_null
 DECL|test_chunked_content_length_error|function|int test_chunked_content_length_error(int req)
 DECL|test_double_content_length_error|function|int test_double_content_length_error(int req)
 DECL|test_header_cr_no_lf_error|function|int test_header_cr_no_lf_error(int req)
-DECL|test_header_nread_value|function|int test_header_nread_value(void)
+DECL|test_header_nread_value|function|void test_header_nread_value(void)
+DECL|test_http_header_fields|function|void test_http_header_fields(void)
 DECL|test_invalid_header_content|function|int test_invalid_header_content(int req, const char *str)
 DECL|test_invalid_header_field_content_error|function|int test_invalid_header_field_content_error(int req)
 DECL|test_invalid_header_field_token_error|function|int test_invalid_header_field_token_error(int req)
 DECL|test_invalid_header_field|function|int test_invalid_header_field(int req, const char *str)
-DECL|test_method_str|function|int test_method_str(void)
-DECL|test_parse_url|function|int test_parse_url(void)
-DECL|test_preserve_data|function|int test_preserve_data(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_method_str|function|void test_method_str(void)
+DECL|test_parse_url|function|void test_parse_url(void)
+DECL|test_preserve_data|function|void test_preserve_data(void)
 DECL|url_tests|variable|url_tests
 DECL|url_test|struct|struct url_test {
 DECL|url|member|const char *url;
