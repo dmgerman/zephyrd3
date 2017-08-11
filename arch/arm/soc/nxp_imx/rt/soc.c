dec6f64d67ef1ce6b053d8c11d556c1e6bf693f9
Maureen Helm <maureen.helm@nxp.com>
2017-08-11 14:42:40 -0500

    arm: Introduce NXP i.MX family, RT series, and mimxrt1052 SoC
    Adds the mimxrt1052 SoC, which belongs to a new family (nxp_imx) and
    series (rt) of SoCs. The mimxrt1052 integrates an Arm Cortex-M7 core,
    512 KB TCM, and many peripherals including 2D graphics, an LCD display
    controller, camera interface, SPDIF and I2S. Unlike other SoCs in
    Zephyr, the mimxrt1052 has no internal flash.
    
    This initial port to mimxrt1052 configures the system clock to operate
    at 528 MHz, and enables the serial/uart and gpio interfaces to support
    the hello_world and blinky samples. Support for additional Zephyr driver
    interfaces will come later.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 41d5808321acdc0ac936b8da342ba00521e30d4d

---
+++ arch/arm/soc/nxp_imx/rt/soc.c
@@ -0,0 +1,6 @@
+DECL|BOARD_BootClockGate|function|static void BOARD_BootClockGate(void)
+DECL|armPllConfig|variable|armPllConfig
+DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
+DECL|imxrt_init|function|static int imxrt_init(struct device *arg)
+DECL|sysPllConfig|variable|sysPllConfig
+DECL|usb1PllConfig|variable|usb1PllConfig
