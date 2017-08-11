1ebbe1a102def612f4bbf7e8c0b7eaf22c0f105f
Aska Wu <aska.wu@linaro.org>
2017-08-11 15:40:36 +0800

    tests: sockets: Add tests of sendto() and recvfrom()
    - Add test cases of ipv4/ipv6 sendto() and recvfrom()
    - Set the main stack size to 2048 to enable both ipv4 and ipv6 on
      qemu_x86.
    - Use net app for network setup.
    
    Signed-off-by: Aska Wu <aska.wu@linaro.org>
    
    Former-commit-id: 7c60eadd5858b7242328aa2afbda346b4ce750f7

--- tests/net/socket/udp/src/main.c
+++ tests/net/socket/udp/src/main.c
@@ -1,5 +1,15 @@
 DECL|BUF_AND_SIZE|macro|BUF_AND_SIZE
+DECL|LOCAL_PORT|macro|LOCAL_PORT
+DECL|REMOTE_PORT|macro|REMOTE_PORT
 DECL|STRLEN|macro|STRLEN
 DECL|TEST_STR_SMALL|macro|TEST_STR_SMALL
+DECL|V4_ANY_ADDR|macro|V4_ANY_ADDR
+DECL|V4_REMOTE_ADDR|macro|V4_REMOTE_ADDR
+DECL|V6_ANY_ADDR|macro|V6_ANY_ADDR
+DECL|V6_REMOTE_ADDR|macro|V6_REMOTE_ADDR
 DECL|test_main|function|void test_main(void)
 DECL|test_send_recv_2_sock|function|void test_send_recv_2_sock(void)
+DECL|test_v4_bind_sendto|function|static void test_v4_bind_sendto(void)
+DECL|test_v4_sendto_recvfrom|function|static void test_v4_sendto_recvfrom(void)
+DECL|test_v6_bind_sendto|function|static void test_v6_bind_sendto(void)
+DECL|test_v6_sendto_recvfrom|function|static void test_v6_sendto_recvfrom(void)
