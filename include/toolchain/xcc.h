eb01aaf77ee335268d275dac8726235ea808e3e2
Andrew Boie <andrew.p.boie@intel.com>
2017-08-14 13:26:23 -0700

    xcc: fix __in_section_unique() and __COUNTER__
    XCC assembler freaks out if a section name has __FILE__ in it,
    forward slashes and quotation marks confuse it and result in
    build errors.
    
    This is not a perfect fix, its possible for two sections to collide,
    but at worst this will result is some unnecessary space in noinit,
    fooling gc-sections.
    
    XCC also doesn't support __COUNTER__, use __LINE__ as a substitute.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 8b141dd83ecd06b385a81dfcb4ca8d1fd61a8703

--- include/toolchain/xcc.h
+++ include/toolchain/xcc.h
@@ -1,4 +1,7 @@
 DECL|_XCC_TOOLCHAIN_H_|macro|_XCC_TOOLCHAIN_H_
 DECL|__BYTE_ORDER__|macro|__BYTE_ORDER__
+DECL|__COUNTER__|macro|__COUNTER__
 DECL|__ORDER_BIG_ENDIAN__|macro|__ORDER_BIG_ENDIAN__
 DECL|__ORDER_LITTLE_ENDIAN__|macro|__ORDER_LITTLE_ENDIAN__
+DECL|__in_section_unique|macro|__in_section_unique
+DECL|__in_section_unique|macro|__in_section_unique
