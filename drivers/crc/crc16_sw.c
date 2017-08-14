cc6c885b4e5bd8ca7320eded37940b5867f4a44e
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2017-08-14 14:24:04 +0200

    drivers: crc: Add 'pad' parameter to crc16()
    'pad' parameter controls whether crc16() should add padding at the end
    of input bytes or not. This allows to compute CRC16 for data stored in
    non-contiguous buffers where CRC value is calculated using subsequent
    calls to crc16() with padding added only for last chunk.
    
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
    
    Former-commit-id: 5d8eadd7b1aa19b1b33ce88f2a06ca57f6222c10

--- drivers/crc/crc16_sw.c
+++ drivers/crc/crc16_sw.c
@@ -1,1 +1,1 @@
-DECL|crc16|function|u16_t crc16(const u8_t *src, size_t len, u16_t polynomial, u16_t initial_value)
+DECL|crc16|function|u16_t crc16(const u8_t *src, size_t len, u16_t polynomial, u16_t initial_value, bool pad)
