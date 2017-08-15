c5b3a4c6562ea5b55581d79ebc1247a730eae3ae
Wayne Ren <wei.ren@synopsys.com>
2017-08-15 12:20:42 +0800

    arch: arc: Add mpu support
    * add arc mpu driver
    * modify the corresponding kconfig and kbuild
    * currently only em_starterkit 2.2's em7d configuration
      has mpu feature (mpu version 2)
    * as the minimum region size of arc mpu version 2 is 2048 bytes and
      region size should be power of 2, the stack size of threads
      (including main thread and idle thread) should be at least
      2048 bytes and power of 2
    * for mpu stack guard feature, a stack guard region of 2048 bytes
      is generated. This brings more memory footprint
    * For arc mpu version 3, the minimum region size is 32 bytes.
    * the codes are tested by the mpu_stack_guard_test and stackprot
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>
    
    Former-commit-id: 12cc6598b0410904d0b1b7799d683f0f4298b9bc

---
+++ arch/arc/core/mpu/arc_core_mpu.c
@@ -0,0 +1,1 @@
+DECL|configure_mpu_stack_guard|function|void configure_mpu_stack_guard(struct k_thread *thread)
---
+++ arch/arc/core/mpu/arc_mpu.c
@@ -0,0 +1,17 @@
+DECL|ARC_FEATURE_MPU_ALIGNMENT_BITS|macro|ARC_FEATURE_MPU_ALIGNMENT_BITS
+DECL|ARC_FEATURE_MPU_ALIGNMENT_BITS|macro|ARC_FEATURE_MPU_ALIGNMENT_BITS
+DECL|AUX_MPU_EN_DISABLE|macro|AUX_MPU_EN_DISABLE
+DECL|AUX_MPU_EN_ENABLE|macro|AUX_MPU_EN_ENABLE
+DECL|AUX_MPU_RDB_VALID_MASK|macro|AUX_MPU_RDB_VALID_MASK
+DECL|AUX_MPU_RDP_REGION_SIZE|macro|AUX_MPU_RDP_REGION_SIZE
+DECL|_ARC_V2_MPU_EN|macro|_ARC_V2_MPU_EN
+DECL|_ARC_V2_MPU_RDB0|macro|_ARC_V2_MPU_RDB0
+DECL|_ARC_V2_MPU_RDP0|macro|_ARC_V2_MPU_RDP0
+DECL|_arc_mpu_config|function|static void _arc_mpu_config(void)
+DECL|_get_num_regions|function|static inline u8_t _get_num_regions(void)
+DECL|_get_region_attr_by_type|function|static inline u32_t _get_region_attr_by_type(u32_t type, u32_t size)
+DECL|_region_init|function|static inline void _region_init(u32_t index, u32_t region_addr, u32_t region_attr)
+DECL|arc_core_mpu_configure|function|void arc_core_mpu_configure(u8_t type, u32_t base, u32_t size)
+DECL|arc_core_mpu_disable|function|void arc_core_mpu_disable(void)
+DECL|arc_core_mpu_enable|function|void arc_core_mpu_enable(void)
+DECL|arc_mpu_init|function|static int arc_mpu_init(struct device *arg)
