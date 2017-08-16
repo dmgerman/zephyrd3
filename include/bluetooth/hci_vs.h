2f2df9008d120055967d79019d9f779df6c90618
Carles Cufi <carles.cufi@nordicsemi.no>
2017-08-16 11:51:35 +0200

    Bluetooth: VS: Fill the Vendor-Specific header with the spec
    The Vendor-Specific header file defines the commands and events used to
    communicate with a Zephyr Vendor-Specific capable controller from a
    Host. Translate the existing specification fully into the header file.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 01338fd3eda68f33aafb85d2fb840eaaf642c31d

--- include/bluetooth/hci_vs.h
+++ include/bluetooth/hci_vs.h
@@ -1,14 +1,97 @@
+DECL|BT_EVT_MASK_VS_FATAL_ERROR|macro|BT_EVT_MASK_VS_FATAL_ERROR
+DECL|BT_EVT_MASK_VS_SCAN_REQ_RX|macro|BT_EVT_MASK_VS_SCAN_REQ_RX
+DECL|BT_EVT_MASK_VS_TRACE_INFO|macro|BT_EVT_MASK_VS_TRACE_INFO
+DECL|BT_HCI_EVT_VS_FATAL_ERROR|macro|BT_HCI_EVT_VS_FATAL_ERROR
+DECL|BT_HCI_EVT_VS_SCAN_REQ_RX|macro|BT_HCI_EVT_VS_SCAN_REQ_RX
+DECL|BT_HCI_EVT_VS_TRACE_INFO|macro|BT_HCI_EVT_VS_TRACE_INFO
+DECL|BT_HCI_OP_VS_READ_BUILD_INFO|macro|BT_HCI_OP_VS_READ_BUILD_INFO
+DECL|BT_HCI_OP_VS_READ_CHIP_TEMP|macro|BT_HCI_OP_VS_READ_CHIP_TEMP
+DECL|BT_HCI_OP_VS_READ_HOST_STACK_CMDS|macro|BT_HCI_OP_VS_READ_HOST_STACK_CMDS
+DECL|BT_HCI_OP_VS_READ_KEY_HIERARCHY_ROOTS|macro|BT_HCI_OP_VS_READ_KEY_HIERARCHY_ROOTS
+DECL|BT_HCI_OP_VS_READ_STATIC_ADDRS|macro|BT_HCI_OP_VS_READ_STATIC_ADDRS
 DECL|BT_HCI_OP_VS_READ_SUPPORTED_COMMANDS|macro|BT_HCI_OP_VS_READ_SUPPORTED_COMMANDS
 DECL|BT_HCI_OP_VS_READ_SUPPORTED_FEATURES|macro|BT_HCI_OP_VS_READ_SUPPORTED_FEATURES
 DECL|BT_HCI_OP_VS_READ_VERSION_INFO|macro|BT_HCI_OP_VS_READ_VERSION_INFO
+DECL|BT_HCI_OP_VS_RESET|macro|BT_HCI_OP_VS_RESET
+DECL|BT_HCI_OP_VS_SET_EVENT_MASK|macro|BT_HCI_OP_VS_SET_EVENT_MASK
+DECL|BT_HCI_OP_VS_SET_SCAN_REQ_REPORTS|macro|BT_HCI_OP_VS_SET_SCAN_REQ_REPORTS
+DECL|BT_HCI_OP_VS_SET_TRACE_ENABLE|macro|BT_HCI_OP_VS_SET_TRACE_ENABLE
+DECL|BT_HCI_OP_VS_WRITE_BD_ADDR|macro|BT_HCI_OP_VS_WRITE_BD_ADDR
+DECL|BT_HCI_VS_FW_VAR_FW_LOADER|macro|BT_HCI_VS_FW_VAR_FW_LOADER
+DECL|BT_HCI_VS_FW_VAR_RESCUE_IMG|macro|BT_HCI_VS_FW_VAR_RESCUE_IMG
+DECL|BT_HCI_VS_FW_VAR_STANDARD_CTLR|macro|BT_HCI_VS_FW_VAR_STANDARD_CTLR
+DECL|BT_HCI_VS_FW_VAR_VS_CTLR|macro|BT_HCI_VS_FW_VAR_VS_CTLR
+DECL|BT_HCI_VS_HW_PLAT_INTEL|macro|BT_HCI_VS_HW_PLAT_INTEL
+DECL|BT_HCI_VS_HW_PLAT_NORDIC|macro|BT_HCI_VS_HW_PLAT_NORDIC
+DECL|BT_HCI_VS_HW_PLAT_NXP|macro|BT_HCI_VS_HW_PLAT_NXP
+DECL|BT_HCI_VS_HW_VAR_NORDIC_NRF51X|macro|BT_HCI_VS_HW_VAR_NORDIC_NRF51X
+DECL|BT_HCI_VS_HW_VAR_NORDIC_NRF52X|macro|BT_HCI_VS_HW_VAR_NORDIC_NRF52X
+DECL|BT_HCI_VS_RESET_HARD|macro|BT_HCI_VS_RESET_HARD
+DECL|BT_HCI_VS_RESET_SOFT|macro|BT_HCI_VS_RESET_SOFT
+DECL|BT_HCI_VS_SCAN_REQ_REPORTS_DISABLED|macro|BT_HCI_VS_SCAN_REQ_REPORTS_DISABLED
+DECL|BT_HCI_VS_SCAN_REQ_REPORTS_ENABLED|macro|BT_HCI_VS_SCAN_REQ_REPORTS_ENABLED
+DECL|BT_HCI_VS_TRACE_DISABLED|macro|BT_HCI_VS_TRACE_DISABLED
+DECL|BT_HCI_VS_TRACE_ENABLED|macro|BT_HCI_VS_TRACE_ENABLED
+DECL|BT_HCI_VS_TRACE_HCI_EVTS|macro|BT_HCI_VS_TRACE_HCI_EVTS
+DECL|BT_HCI_VS_TRACE_LE_CONN_IND|macro|BT_HCI_VS_TRACE_LE_CONN_IND
+DECL|BT_HCI_VS_TRACE_LLCP_RX|macro|BT_HCI_VS_TRACE_LLCP_RX
+DECL|BT_HCI_VS_TRACE_LLCP_TX|macro|BT_HCI_VS_TRACE_LLCP_TX
+DECL|BT_HCI_VS_TRACE_LMP_RX|macro|BT_HCI_VS_TRACE_LMP_RX
+DECL|BT_HCI_VS_TRACE_LMP_TX|macro|BT_HCI_VS_TRACE_LMP_TX
+DECL|BT_HCI_VS_TRACE_VDC|macro|BT_HCI_VS_TRACE_VDC
+DECL|BT_HCI_VS_VID_ANDROID|macro|BT_HCI_VS_VID_ANDROID
+DECL|BT_HCI_VS_VID_MICROSOFT|macro|BT_HCI_VS_VID_MICROSOFT
 DECL|__BT_HCI_VS_H|macro|__BT_HCI_VS_H
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|__packed|variable|__packed
+DECL|addr|member|bt_addr_le_t addr;
+DECL|a|member|struct bt_hci_vs_static_addr a[0];
+DECL|bdaddr|member|bt_addr_t bdaddr;
+DECL|bdaddr|member|bt_addr_t bdaddr;
+DECL|bt_hci_cp_vs_reset|struct|struct bt_hci_cp_vs_reset {
+DECL|bt_hci_cp_vs_set_event_mask|struct|struct bt_hci_cp_vs_set_event_mask {
+DECL|bt_hci_cp_vs_set_scan_req_reports|struct|struct bt_hci_cp_vs_set_scan_req_reports {
+DECL|bt_hci_cp_vs_set_trace_enable|struct|struct bt_hci_cp_vs_set_trace_enable {
+DECL|bt_hci_cp_vs_write_bd_addr|struct|struct bt_hci_cp_vs_write_bd_addr {
+DECL|bt_hci_evt_vs_fatal_error|struct|struct bt_hci_evt_vs_fatal_error {
+DECL|bt_hci_evt_vs_scan_req_rx|struct|struct bt_hci_evt_vs_scan_req_rx {
+DECL|bt_hci_evt_vs_trace_info|struct|struct bt_hci_evt_vs_trace_info {
+DECL|bt_hci_evt_vs|struct|struct bt_hci_evt_vs {
+DECL|bt_hci_rp_vs_read_build_info|struct|struct bt_hci_rp_vs_read_build_info {
+DECL|bt_hci_rp_vs_read_chip_temp|struct|struct bt_hci_rp_vs_read_chip_temp {
+DECL|bt_hci_rp_vs_read_host_stack_cmds|struct|struct bt_hci_rp_vs_read_host_stack_cmds {
+DECL|bt_hci_rp_vs_read_key_hierarchy_roots|struct|struct bt_hci_rp_vs_read_key_hierarchy_roots {
+DECL|bt_hci_rp_vs_read_static_addrs|struct|struct bt_hci_rp_vs_read_static_addrs {
 DECL|bt_hci_rp_vs_read_supported_commands|struct|struct bt_hci_rp_vs_read_supported_commands {
 DECL|bt_hci_rp_vs_read_supported_features|struct|struct bt_hci_rp_vs_read_supported_features {
 DECL|bt_hci_rp_vs_read_version_info|struct|struct bt_hci_rp_vs_read_version_info {
+DECL|bt_hci_vs_cmd|struct|struct bt_hci_vs_cmd {
+DECL|bt_hci_vs_static_addr|struct|struct bt_hci_vs_static_addr {
 DECL|commands|member|u8_t commands[64];
+DECL|c|member|struct bt_hci_vs_cmd c[0];
+DECL|data|member|u8_t data[0];
+DECL|enable|member|u8_t enable;
+DECL|enable|member|u8_t enable;
+DECL|err_info|member|u8_t err_info[0];
+DECL|er|member|u8_t er[16];
+DECL|event_mask|member|u8_t event_mask[8];
 DECL|features|member|u8_t features[8];
 DECL|fw_build|member|u32_t fw_build;
 DECL|fw_revision|member|u16_t fw_revision;
@@ -16,6 +99,24 @@ DECL|fw_variant|member|u8_t fw_variant;
 DECL|fw_version|member|u8_t fw_version;
 DECL|hw_platform|member|u16_t hw_platform;
 DECL|hw_variant|member|u16_t hw_variant;
+DECL|info|member|u8_t info[0];
+DECL|irk|member|u8_t irk[16];
+DECL|irk|member|u8_t irk[16];
+DECL|num_addrs|member|u8_t num_addrs;
+DECL|num_cmds|member|u8_t num_cmds;
+DECL|opcode_base|member|u16_t opcode_base;
+DECL|pc|member|u64_t pc;
 DECL|status|member|u8_t status;
 DECL|status|member|u8_t status;
 DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
+DECL|status|member|u8_t status;
+DECL|subevent|member|u8_t subevent;
+DECL|temps|member|s8_t temps;
+DECL|type|member|u8_t type;
+DECL|type|member|u8_t type;
+DECL|type|member|u8_t type;
+DECL|vendor_id|member|u16_t vendor_id;
