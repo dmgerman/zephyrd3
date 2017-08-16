dd3e77c5cbbb8304d2e360dab9a8bfb7e1e8d1a9
Wayne Ren <wei.ren@synopsys.com>
2017-08-16 16:59:54 +0800

    arch: arc: apply STACK_GUARD_SIZE and optimize the mpu driver
    * apply STACK_GUARD_SIZE, no extra space will be added if
      MPU_STACK_GUARD is disabled
    * When ARC_STACK_CHECKING is enabled, MPU_STACK_GUARD will be
      disabled
    * add two new api: arc_core_mpu_default and arc_core_mpu_region
      to configure mpu regions
    * improve arc_core_mpu_enable and arc_core_mpu_disable
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>
    
    Former-commit-id: 480cfac4f22dae93f11f2abb5492fa21aec44d2d

--- arch/arc/core/mpu/arc_mpu.c
+++ arch/arc/core/mpu/arc_mpu.c
@@ -3,6 +3,7 @@ DECL|ARC_FEATURE_MPU_ALIGNMENT_BITS|macro|ARC_FEATURE_MPU_ALIGNMENT_BITS
 DECL|AUX_MPU_EN_DISABLE|macro|AUX_MPU_EN_DISABLE
 DECL|AUX_MPU_EN_ENABLE|macro|AUX_MPU_EN_ENABLE
 DECL|AUX_MPU_RDB_VALID_MASK|macro|AUX_MPU_RDB_VALID_MASK
+DECL|AUX_MPU_RDP_ATTR_MASK|macro|AUX_MPU_RDP_ATTR_MASK
 DECL|AUX_MPU_RDP_REGION_SIZE|macro|AUX_MPU_RDP_REGION_SIZE
 DECL|_ARC_V2_MPU_EN|macro|_ARC_V2_MPU_EN
 DECL|_ARC_V2_MPU_RDB0|macro|_ARC_V2_MPU_RDB0
@@ -12,6 +13,8 @@ DECL|_get_num_regions|function|static inline u8_t _get_num_regions(void)
 DECL|_get_region_attr_by_type|function|static inline u32_t _get_region_attr_by_type(u32_t type, u32_t size)
 DECL|_region_init|function|static inline void _region_init(u32_t index, u32_t region_addr, u32_t region_attr)
 DECL|arc_core_mpu_configure|function|void arc_core_mpu_configure(u8_t type, u32_t base, u32_t size)
+DECL|arc_core_mpu_default|function|void arc_core_mpu_default(u32_t region_attr)
 DECL|arc_core_mpu_disable|function|void arc_core_mpu_disable(void)
 DECL|arc_core_mpu_enable|function|void arc_core_mpu_enable(void)
+DECL|arc_core_mpu_region|function|void arc_core_mpu_region(u32_t index, u32_t base, u32_t region_attr)
 DECL|arc_mpu_init|function|static int arc_mpu_init(struct device *arg)
