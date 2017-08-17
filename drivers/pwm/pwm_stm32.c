a841bef6dacc966fe426db434ac1604cc4e02d5e
Florian Vaussard <florian.vaussard@gmail.com>
2017-08-17 15:01:32 +0200

    pwm: stm32: Use macro to simplify registration
    The registration of each PWM differs only by a few details.
    These differences can be factorized in order to create a generic
    registration macro.
    
    This has several advantages:
    - Less code
    - Easier to add new PWM
    
    Signed-off-by: Florian Vaussard <florian.vaussard@gmail.com>
    
    Former-commit-id: 0bdcef9e00ae02c0ae9f0d8fdd83cfd5e214e6f4

--- drivers/pwm/pwm_stm32.c
+++ drivers/pwm/pwm_stm32.c
@@ -1,15 +1,10 @@
 DECL|CHANNEL_LENGTH|macro|CHANNEL_LENGTH
 DECL|DEV_CFG|macro|DEV_CFG
 DECL|DEV_DATA|macro|DEV_DATA
+DECL|PWM_DEVICE_INIT_STM32|macro|PWM_DEVICE_INIT_STM32
 DECL|PWM_STRUCT|macro|PWM_STRUCT
 DECL|__get_tim_clk|function|static u32_t __get_tim_clk(u32_t bus_clk, clock_control_subsys_t *sub_system)
 DECL|__pwm_stm32_get_clock|function|static inline void __pwm_stm32_get_clock(struct device *dev)
-DECL|pwm_stm32_dev_cfg_1|variable|pwm_stm32_dev_cfg_1
-DECL|pwm_stm32_dev_cfg_2|variable|pwm_stm32_dev_cfg_2
-DECL|pwm_stm32_dev_cfg_3|variable|pwm_stm32_dev_cfg_3
-DECL|pwm_stm32_dev_data_1|variable|pwm_stm32_dev_data_1
-DECL|pwm_stm32_dev_data_2|variable|pwm_stm32_dev_data_2
-DECL|pwm_stm32_dev_data_3|variable|pwm_stm32_dev_data_3
 DECL|pwm_stm32_drv_api_funcs|variable|pwm_stm32_drv_api_funcs
 DECL|pwm_stm32_get_cycles_per_sec|function|static int pwm_stm32_get_cycles_per_sec(struct device *dev, u32_t pwm,u64_t *cycles)
 DECL|pwm_stm32_init|function|static int pwm_stm32_init(struct device *dev)
