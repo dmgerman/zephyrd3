381222537e61a3e3b26614c408dcfc6db954b07a
Timo Teräs <timo.teras@iki.fi>
2017-08-17 19:54:19 +0300

    arm: corex_m: add byte/half-word sized memory accessors
    The byte ones are required for ns16550 uart driver which is
    present on some arm socs. Add half-word ones for completeness.
    
    Signed-off-by: Timo Teräs <timo.teras@iki.fi>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 5d9a6aa9dfb921e6adfb8ab81a4b9eb27c4c13c5

--- include/arch/arm/cortex_m/sys_io.h
+++ include/arch/arm/cortex_m/sys_io.h
@@ -1,5 +1,9 @@
 DECL|_CORTEX_M_SYS_IO_H_|macro|_CORTEX_M_SYS_IO_H_
 DECL|sys_clear_bit|function|static inline void sys_clear_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_read16|function|static inline u16_t sys_read16(mem_addr_t addr)
 DECL|sys_read32|function|static inline u32_t sys_read32(mem_addr_t addr)
+DECL|sys_read8|function|static inline u8_t sys_read8(mem_addr_t addr)
 DECL|sys_set_bit|function|static inline void sys_set_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_write16|function|static inline void sys_write16(u16_t data, mem_addr_t addr)
 DECL|sys_write32|function|static inline void sys_write32(u32_t data, mem_addr_t addr)
+DECL|sys_write8|function|static inline void sys_write8(u8_t data, mem_addr_t addr)
