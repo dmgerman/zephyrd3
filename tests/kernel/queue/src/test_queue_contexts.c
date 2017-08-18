9814e1269fa44f110076b809c65bb2d6ec745f1e
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2017-08-18 14:44:03 +0300

    test: queue: Add test for multiple threads using k_queue_get
    This tests the situation when there are multiple threads calling
    k_queue_get which was causing issues when using k_poll.
    
    Jira: ZEP-2553
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 251a8a8c28c03f9604286695a55992be8daa09ca

--- tests/kernel/queue/src/test_queue_contexts.c
+++ tests/kernel/queue/src/test_queue_contexts.c
@@ -10,11 +10,15 @@ DECL|queue|variable|queue
 DECL|tIsr_entry_append|function|static void tIsr_entry_append(void *p)
 DECL|tIsr_entry_get|function|static void tIsr_entry_get(void *p)
 DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *p3)
+DECL|tThread_get|function|static void tThread_get(void *p1, void *p2, void *p3)
+DECL|tdata1|variable|tdata1
 DECL|tdata|variable|tdata
+DECL|test_queue_get_2threads|function|void test_queue_get_2threads(void)
 DECL|test_queue_isr2thread|function|void test_queue_isr2thread(void)
 DECL|test_queue_thread2isr|function|void test_queue_thread2isr(void)
 DECL|test_queue_thread2thread|function|void test_queue_thread2thread(void)
 DECL|tqueue_append|function|static void tqueue_append(struct k_queue *pqueue)
+DECL|tqueue_get_2threads|function|static void tqueue_get_2threads(struct k_queue *pqueue)
 DECL|tqueue_get|function|static void tqueue_get(struct k_queue *pqueue)
 DECL|tqueue_isr_thread|function|static void tqueue_isr_thread(struct k_queue *pqueue)
 DECL|tqueue_thread_isr|function|static void tqueue_thread_isr(struct k_queue *pqueue)
