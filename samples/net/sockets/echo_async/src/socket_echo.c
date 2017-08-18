a6d58b85d57a279a7bb7bbd5d5ce24d8bea515e2
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-08-18 11:26:16 +0300

    samples: net: echo_async: Add IPv6 support
    The main reason is allowing to run on 6LoWPAN devices, though also to
    demonstrate IPv6 support with BSD Sockets in general.
    
    Tested on 96b_carbon.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: 756e729c006e72cbbf360efba1b0645b335ccba8

--- samples/net/sockets/echo_async/src/socket_echo.c
+++ samples/net/sockets/echo_async/src/socket_echo.c
@@ -1,4 +1,5 @@
 DECL|NUM_FDS|macro|NUM_FDS
+DECL|NUM_FDS|macro|NUM_FDS
 DECL|main|function|int main(void)
 DECL|nonblock|function|static void nonblock(int fd)
 DECL|pollfds_add|function|int pollfds_add(int fd)
