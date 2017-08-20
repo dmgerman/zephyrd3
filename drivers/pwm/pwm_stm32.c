fb0612e15a9bd66f1781ba878c5efab7e2671d4e
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-08-19 21:04:59 -0300

    pwm: stm32: Add PWM 3 (TIM3)
    Add support for TIM3 as it is widely available and pins available via
    headers on several devices.
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>
    
    Former-commit-id: 0dca1f5bd39f7d25aaca941df483bff4492405cf

--- drivers/pwm/pwm_stm32.c
+++ drivers/pwm/pwm_stm32.c
@@ -6,8 +6,10 @@ DECL|__get_tim_clk|function|static u32_t __get_tim_clk(u32_t bus_clk, clock_cont
 DECL|__pwm_stm32_get_clock|function|static inline void __pwm_stm32_get_clock(struct device *dev)
 DECL|pwm_stm32_dev_cfg_1|variable|pwm_stm32_dev_cfg_1
 DECL|pwm_stm32_dev_cfg_2|variable|pwm_stm32_dev_cfg_2
+DECL|pwm_stm32_dev_cfg_3|variable|pwm_stm32_dev_cfg_3
 DECL|pwm_stm32_dev_data_1|variable|pwm_stm32_dev_data_1
 DECL|pwm_stm32_dev_data_2|variable|pwm_stm32_dev_data_2
+DECL|pwm_stm32_dev_data_3|variable|pwm_stm32_dev_data_3
 DECL|pwm_stm32_drv_api_funcs|variable|pwm_stm32_drv_api_funcs
 DECL|pwm_stm32_get_cycles_per_sec|function|static int pwm_stm32_get_cycles_per_sec(struct device *dev, u32_t pwm,u64_t *cycles)
 DECL|pwm_stm32_init|function|static int pwm_stm32_init(struct device *dev)
