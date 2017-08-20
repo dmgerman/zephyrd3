8ecf8104584adab24fb80685525128726cfa46e1
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-08-20 23:15:07 +0300

    net: websocket: Initial support for server websocket
    This commit creates a websocket library that can be used by
    applications. The websocket library implements currently only
    server role and it uses services provided by net-app API.
    The library supports TLS if enabled in configuration file.
    
    This also adds websocket calls to HTTP app server if websocket
    connection is established.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d70c7383ded0b4f67223553d1c2a040712fbc17d

---
+++ include/net/websocket.h
@@ -0,0 +1,15 @@
+DECL|WS_FLAG_BINARY|macro|WS_FLAG_BINARY
+DECL|WS_FLAG_CLOSE|macro|WS_FLAG_CLOSE
+DECL|WS_FLAG_FINAL|macro|WS_FLAG_FINAL
+DECL|WS_FLAG_PING|macro|WS_FLAG_PING
+DECL|WS_FLAG_PONG|macro|WS_FLAG_PONG
+DECL|WS_FLAG_TEXT|macro|WS_FLAG_TEXT
+DECL|WS_OPCODE_CLOSE|enumerator|WS_OPCODE_CLOSE = 0x08,
+DECL|WS_OPCODE_CONTINUE|enumerator|WS_OPCODE_CONTINUE = 0x00,
+DECL|WS_OPCODE_DATA_BINARY|enumerator|WS_OPCODE_DATA_BINARY = 0x02,
+DECL|WS_OPCODE_DATA_TEXT|enumerator|WS_OPCODE_DATA_TEXT = 0x01,
+DECL|WS_OPCODE_PING|enumerator|WS_OPCODE_PING = 0x09,
+DECL|WS_OPCODE_PONG|enumerator|WS_OPCODE_PONG = 0x0A,
+DECL|__WEBSOCKET_H__|macro|__WEBSOCKET_H__
+DECL|ws_opcode|enum|enum ws_opcode {
+DECL|ws_send_msg_to_client|function|static inline int ws_send_msg_to_client(struct http_ctx *ctx,u8_t *payload, size_t payload_len, enum ws_opcode opcode, bool final,
---
+++ subsys/net/lib/websocket/websocket.c
@@ -0,0 +1,14 @@
+DECL|BUF_ALLOC_TIMEOUT|macro|BUF_ALLOC_TIMEOUT
+DECL|HTTP_CRLF|macro|HTTP_CRLF
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|WS_MAGIC|macro|WS_MAGIC
+DECL|check_ws_headers|function|static bool check_ws_headers(struct http_ctx *ctx, struct http_parser *parser, int *ws_sec_key, int *host, int *subprotocol)
+DECL|field_contains|function|static bool field_contains(const char *field, int field_len, const char *str, int str_len)
+DECL|prepare_reply|function|static struct net_pkt *prepare_reply(struct http_ctx *ctx, int ws_sec_key, int host, int subprotocol)
+DECL|ws_headers_complete|function|int ws_headers_complete(struct http_parser *parser)
+DECL|ws_mask_payload|function|static void ws_mask_payload(u8_t *payload, size_t payload_len, u32_t masking_value)
+DECL|ws_mask_pkt|function|void ws_mask_pkt(struct net_pkt *pkt, u32_t masking_value, u32_t *data_read)
+DECL|ws_send_msg|function|int ws_send_msg(struct http_ctx *ctx, u8_t *payload, size_t payload_len,enum ws_opcode opcode, bool mask, bool final, const struct sockaddr *dst, void *user_send_data)
+DECL|ws_strip_header|function|int ws_strip_header(struct net_pkt *pkt, bool *masked, u32_t *mask_value, u32_t *message_length, u32_t *message_type_flag, u32_t *header_len)
