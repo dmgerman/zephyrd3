780a9fc0d989b3bdb381baf0475742fb70e4b196
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-08-22 11:16:16 +0300

    net: app: Introduce missing enable/disable functions for server
    The server needs global enable/disable status instead of only being
    able to enable or disable just the TLS server part.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c200de1b53f13ec45a07b9015791116b4b3424a3

--- subsys/net/lib/app/server.c
+++ subsys/net/lib/app/server.c
@@ -5,10 +5,12 @@ DECL|PORT_STR|macro|PORT_STR
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|TLS_STARTUP_TIMEOUT|macro|TLS_STARTUP_TIMEOUT
 DECL|_net_app_accept_cb|function|void _net_app_accept_cb(struct net_context *net_ctx,struct sockaddr *addr, socklen_t addrlen, int status, void *data)
+DECL|_net_app_server_tls_disable|function|bool _net_app_server_tls_disable(struct net_app_ctx *ctx)
+DECL|_net_app_server_tls_enable|function|bool _net_app_server_tls_enable(struct net_app_ctx *ctx)
 DECL|net_app_init_server|function|int net_app_init_server(struct net_app_ctx *ctx,enum net_sock_type sock_type, enum net_ip_protocol proto, struct sockaddr *server_addr, u16_t port,
 DECL|net_app_listen|function|int net_app_listen(struct net_app_ctx *ctx)
-DECL|net_app_server_tls_disable|function|bool net_app_server_tls_disable(struct net_app_ctx *ctx)
-DECL|net_app_server_tls_enable|function|bool net_app_server_tls_enable(struct net_app_ctx *ctx)
+DECL|net_app_server_disable|function|bool net_app_server_disable(struct net_app_ctx *ctx)
+DECL|net_app_server_enable|function|bool net_app_server_enable(struct net_app_ctx *ctx)
 DECL|net_app_server_tls|function|int net_app_server_tls(struct net_app_ctx *ctx, u8_t *request_buf, size_t request_buf_len, const char *server_banner, u8_t *personalization_data,
 DECL|new_client|function|static void new_client(struct net_context *net_ctx, const struct sockaddr *addr)
 DECL|new_server|function|static inline void new_server(struct net_app_ctx *ctx, const char *server_banner)
