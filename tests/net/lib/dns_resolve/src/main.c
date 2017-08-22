d9d6e846300ce1b08cfa290ad3eeeba44b7e089b
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-08-22 20:55:41 +0300

    net: dns: Do not resolve numeric IP address
    If the query name is already in numeric format, there is no
    need to send the query string to DNS server as we can just
    convert it ourselves.
    
    Jira: ZEP-2562
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: bd425e685702aa6c4cdb1945ad88ff99a67b68df

--- tests/net/lib/dns_resolve/src/main.c
+++ tests/net/lib/dns_resolve/src/main.c
@@ -3,11 +3,15 @@ DECL|DBG|macro|DBG
 DECL|DNS_TIMEOUT|macro|DNS_TIMEOUT
 DECL|NAME4|macro|NAME4
 DECL|NAME6|macro|NAME6
+DECL|NAME_IPV4|macro|NAME_IPV4
+DECL|NAME_IPV6|macro|NAME_IPV6
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
 DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
 DECL|addrinfo|variable|addrinfo
+DECL|addr|member|struct sockaddr addr;
+DECL|caller|member|const char *caller;
 DECL|caller|member|const char *caller;
 DECL|current_dns_id|variable|current_dns_id
 DECL|dns_query_invalid_callback|function|static void dns_query_invalid_callback(void)
@@ -15,10 +19,12 @@ DECL|dns_query_invalid_context|function|static void dns_query_invalid_context(vo
 DECL|dns_query_invalid_query|function|static void dns_query_invalid_query(void)
 DECL|dns_query_invalid_timeout|function|static void dns_query_invalid_timeout(void)
 DECL|dns_query_ipv4_cancel|function|static void dns_query_ipv4_cancel(void)
+DECL|dns_query_ipv4_numeric|function|static void dns_query_ipv4_numeric(void)
 DECL|dns_query_ipv4_server_count|function|static void dns_query_ipv4_server_count(void)
 DECL|dns_query_ipv4_timeout|function|static void dns_query_ipv4_timeout(void)
 DECL|dns_query_ipv4|function|static void dns_query_ipv4(void)
 DECL|dns_query_ipv6_cancel|function|static void dns_query_ipv6_cancel(void)
+DECL|dns_query_ipv6_numeric|function|static void dns_query_ipv6_numeric(void)
 DECL|dns_query_ipv6_server_count|function|static void dns_query_ipv6_server_count(void)
 DECL|dns_query_ipv6_timeout|function|static void dns_query_ipv6_timeout(void)
 DECL|dns_query_ipv6|function|static void dns_query_ipv6(void)
@@ -27,6 +33,8 @@ DECL|dns_query_too_many|function|static void dns_query_too_many(void)
 DECL|dns_result_cb_dummy|function|void dns_result_cb_dummy(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 DECL|dns_result_cb_timeout|function|void dns_result_cb_timeout(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 DECL|dns_result_cb|function|void dns_result_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
+DECL|dns_result_numeric_cb|function|void dns_result_numeric_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
+DECL|expected_addr_status|struct|struct expected_addr_status {
 DECL|expected_status|struct|struct expected_status {
 DECL|get_slot_by_id|function|static inline int get_slot_by_id(struct dns_resolve_context *ctx, u16_t dns_id)
 DECL|idx|member|u8_t idx;
@@ -36,6 +44,7 @@ DECL|ll_addr|variable|ll_addr
 DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
 DECL|my_addr1|variable|my_addr1
 DECL|my_addr2|variable|my_addr2
+DECL|my_addr3|variable|my_addr3
 DECL|net_if_test|struct|struct net_if_test {
 DECL|net_iface1_data|variable|net_iface1_data
 DECL|net_iface_api|variable|net_iface_api
@@ -44,6 +53,8 @@ DECL|net_iface_get_mac|function|static u8_t *net_iface_get_mac(struct device *de
 DECL|net_iface_init|function|static void net_iface_init(struct net_if *iface)
 DECL|sender_iface|function|static int sender_iface(struct net_if *iface, struct net_pkt *pkt)
 DECL|status1|member|int status1;
+DECL|status1|member|int status1;
+DECL|status2|member|int status2;
 DECL|status2|member|int status2;
 DECL|test_failed|variable|test_failed
 DECL|test_init|function|static void test_init(void)
