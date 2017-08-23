ffb27b4f9f7d2de8fd915e214e4948e55fc11574
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-08-23 15:43:45 -0300

    net: udp: replace net_udp_get/set_hdr macros with static inline
    Replace net_udp_get_hdr and net_udp_set_hdr macros with static inline
    function definitions to avoid unused variable build warnings when
    NET_UDP is not defined.
    
    This fixes the following warning:
    
    subsys/net/ip/6lo.c: In function 'compress_IPHC_header':
    subsys/net/ip/6lo.c:759:22: warning: unused variable 'hdr' [-Wunused-variable]
       struct net_udp_hdr hdr, *udp;
                          ^~~
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>
    
    Former-commit-id: d0df530a509f2ffe473ec97f64eceda9e3b47b73

--- include/net/udp.h
+++ include/net/udp.h
@@ -1,3 +1,3 @@
 DECL|__UDP_H|macro|__UDP_H
-DECL|net_udp_get_hdr|macro|net_udp_get_hdr
-DECL|net_udp_set_hdr|macro|net_udp_set_hdr
+DECL|net_udp_get_hdr|function|static inline struct net_udp_hdr *net_udp_get_hdr(struct net_pkt *pkt, struct net_udp_hdr *hdr)
+DECL|net_udp_set_hdr|function|static inline struct net_udp_hdr *net_udp_set_hdr(struct net_pkt *pkt, struct net_udp_hdr *hdr)
