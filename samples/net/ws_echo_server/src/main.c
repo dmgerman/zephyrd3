392279e5fd08a27354045bb2d0c4890550554b39
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-08-26 17:45:42 +0300

    samples: net: websocket echo-server application
    This is a http(s) server that supports also websocket.
    It sends back any data sent to it over a websocket.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1e6ed0335a4b92ac05329b0d58e5ca0f59ee08bf

---
+++ samples/net/ws_echo_server/src/main.c
@@ -0,0 +1,12 @@
+DECL|APP_BANNER|macro|APP_BANNER
+DECL|APP_STARTUP_TIME|macro|APP_STARTUP_TIME
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|build_reply_pkt|function|struct net_pkt *build_reply_pkt(const char *name,struct net_app_ctx *ctx, struct net_pkt *pkt)
+DECL|init_app|function|static inline int init_app(void)
+DECL|main|function|void main(void)
+DECL|panic|function|void panic(const char *msg)
+DECL|pkt_sent|function|void pkt_sent(struct net_app_ctx *ctx, int status, void *user_data_send, void *user_data)
+DECL|quit_lock|variable|quit_lock
+DECL|quit|function|void quit(void)
