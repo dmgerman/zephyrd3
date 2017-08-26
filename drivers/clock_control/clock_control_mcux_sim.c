4a85e9f251a6b4d08f9ad3f0d3ba4450d5f782f4
Maureen Helm <maureen.helm@nxp.com>
2017-08-26 08:43:53 -0500

    clock_control: Introduce mcux sim driver
    Adds a new clock control driver for Kinetis SoCs that have the system
    integration (SIM) module. This will allow mcux shim drivers, such as
    uart and i2c, to abstract the call to CLOCK_GetFreq() behind the
    clock_control interface and thus be reused for SoCs with different clock
    architectures.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 7a60782f6767e22e502f5d24c8956de477c9b2fb

---
+++ drivers/clock_control/clock_control_mcux_sim.c
@@ -0,0 +1,6 @@
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|mcux_sim_driver_api|variable|mcux_sim_driver_api
+DECL|mcux_sim_get_subsys_rate|function|static int mcux_sim_get_subsys_rate(struct device *dev, clock_control_subsys_t sub_system, u32_t *rate)
+DECL|mcux_sim_init|function|static int mcux_sim_init(struct device *dev)
+DECL|mcux_sim_off|function|static int mcux_sim_off(struct device *dev, clock_control_subsys_t sub_system)
+DECL|mcux_sim_on|function|static int mcux_sim_on(struct device *dev, clock_control_subsys_t sub_system)
