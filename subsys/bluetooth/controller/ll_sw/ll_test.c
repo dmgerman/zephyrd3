7c891ba1f9a2bdc77141caf0b55ed878dfd5d878
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2017-08-27 11:13:50 +0200

    Bluetooth: controller: Add Direct Test Mode support
    Added controller implementation to support Direct Test Mode.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 37c7551120d1b78395b2d223e696516daadf9c8f

---
+++ subsys/bluetooth/controller/ll_sw/ll_test.c
@@ -0,0 +1,14 @@
+DECL|CNTR_MIN_DELTA|macro|CNTR_MIN_DELTA
+DECL|init|function|static u32_t init(u8_t chan, u8_t phy, void (*isr)(void))
+DECL|isr_rx|function|static void isr_rx(void)
+DECL|isr_tx|function|static void isr_tx(void)
+DECL|ll_test_end|function|u32_t ll_test_end(u16_t *num_rx)
+DECL|ll_test_rx|function|u32_t ll_test_rx(u8_t chan, u8_t phy, u8_t mod_idx)
+DECL|ll_test_tx|function|u32_t ll_test_tx(u8_t chan, u8_t len, u8_t type, u8_t phy)
+DECL|prbs15|variable|prbs15
+DECL|prbs9|variable|prbs9
+DECL|started|variable|started
+DECL|test_num_rx|variable|test_num_rx
+DECL|test_phy_flags|variable|test_phy_flags
+DECL|test_phy|variable|test_phy
+DECL|test_sync_word|variable|test_sync_word
