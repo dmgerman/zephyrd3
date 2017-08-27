6a580c03fe0f680fbbad9128c87dd13c7c9e2dcc
Andrew Boie <andrew.p.boie@intel.com>
2017-08-26 17:11:29 -0700

    tests: add object validation test case
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: d76f0a9644f02664dee061e1514c51cdc69b6f82

---
+++ tests/kernel/obj_validation/src/main.c
@@ -0,0 +1,7 @@
+DECL|SEM_ARRAY_SIZE|macro|SEM_ARRAY_SIZE
+DECL|bad_sem|variable|bad_sem
+DECL|main|function|void main(void) {
+DECL|sem2|variable|sem2
+DECL|sem3|variable|sem3
+DECL|semarray|variable|semarray
+DECL|test_bad_object|function|static int test_bad_object(struct k_sem *sem)
