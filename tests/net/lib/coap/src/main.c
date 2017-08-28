89c73b3f3a22ff0c973abeb0310b98c0e95444bb
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-08-28 13:55:28 +0300

    net: tests: Update CoAP tests to use new API
    ZoAP tests are modified to use new CoAP API. Also modified tests
    name from 'zoap' to 'coap'.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 86fe6a3bd69dd0b4dceaa20db2313ee1dd496b2e

--- tests/net/lib/coap/src/main.c
+++ tests/net/lib/coap/src/main.c
@@ -1,25 +1,33 @@
+DECL|COAP_BUF_SIZE|macro|COAP_BUF_SIZE
+DECL|COAP_LIMITED_BUF_SIZE|macro|COAP_LIMITED_BUF_SIZE
 DECL|MY_PORT|macro|MY_PORT
 DECL|NUM_OBSERVERS|macro|NUM_OBSERVERS
 DECL|NUM_PENDINGS|macro|NUM_PENDINGS
 DECL|NUM_REPLIES|macro|NUM_REPLIES
-DECL|ZOAP_BUF_SIZE|macro|ZOAP_BUF_SIZE
-DECL|ZOAP_LIMITED_BUF_SIZE|macro|ZOAP_LIMITED_BUF_SIZE
 DECL|dummy_addr|variable|dummy_addr
 DECL|func|member|int (*func)(void);
+DECL|get_from_ip_addr|function|static void get_from_ip_addr(struct coap_packet *cpkt, struct sockaddr_in6 *from)
 DECL|ipaddr_cmp|function|static bool ipaddr_cmp(const struct sockaddr *a, const struct sockaddr *b)
+DECL|ipv6_block|variable|ipv6_block
+DECL|ipv6_empty_pdu|variable|ipv6_empty_pdu
+DECL|ipv6_not_found_req|variable|ipv6_not_found_req
+DECL|ipv6_obs_client|variable|ipv6_obs_client
+DECL|ipv6_resource1|variable|ipv6_resource1
+DECL|ipv6_simple_pdu|variable|ipv6_simple_pdu
+DECL|ipv6_valid_req|variable|ipv6_valid_req
 DECL|main|function|int main(int argc, char *argv[])
 DECL|name|member|const char *name;
 DECL|observers|variable|observers
+DECL|peer_addr|macro|peer_addr
 DECL|pendings|variable|pendings
 DECL|replies|variable|replies
-DECL|resource_reply_cb|function|static int resource_reply_cb(const struct zoap_packet *response, struct zoap_reply *reply, const struct sockaddr *from)
-DECL|server_notify_callback|function|static void server_notify_callback(struct zoap_resource *resource, struct zoap_observer *observer)
-DECL|server_resource_1_get|function|static int server_resource_1_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|resource_reply_cb|function|static int resource_reply_cb(const struct coap_packet *response, struct coap_reply *reply, const struct sockaddr *from)
+DECL|server_notify_callback|function|static void server_notify_callback(struct coap_resource *resource, struct coap_observer *observer)
+DECL|server_resource_1_get|function|static int server_resource_1_get(struct coap_resource *resource, struct coap_packet *request)
 DECL|server_resource_1_path|variable|server_resource_1_path
 DECL|server_resources|variable|server_resources
 DECL|test_block_size|function|static int test_block_size(void)
 DECL|test_build_empty_pdu|function|static int test_build_empty_pdu(void)
-DECL|test_build_no_size_for_options|function|static int test_build_no_size_for_options(void)
 DECL|test_build_simple_pdu|function|static int test_build_simple_pdu(void)
 DECL|test_match_path_uri|function|static int test_match_path_uri(void)
 DECL|test_observer_client|function|static int test_observer_client(void)
