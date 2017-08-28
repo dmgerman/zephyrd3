823e3916e80eb5e6abe7a03e13ac88d5a05eb298
Vikram Singh Shekhawat <vikramx.shekhawat@intel.com>
2017-08-28 19:30:38 +0530

    tests: entropy: Add Entropy driver test case
    This test runs with ztest suite. It will verify get_entropy
    operation of entropy driver.
    
    Signed-off-by: Vikram Singh Shekhawat <vikramx.shekhawat@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: cbbec3db7062d1793af9aa4bbc55fad9351b9d14

---
+++ tests/drivers/entropy/api/src/main.c
@@ -0,0 +1,6 @@
+DECL|BUFFER_LENGTH|macro|BUFFER_LENGTH
+DECL|RECHECK_RANDOM_ENTROPY|macro|RECHECK_RANDOM_ENTROPY
+DECL|get_entropy|function|static int get_entropy(void)
+DECL|random_entropy|function|static int random_entropy(struct device *dev, char *buffer, char num)
+DECL|test_main|function|void test_main(void)
+DECL|test_random_get_entropy|function|static void test_random_get_entropy(void)
