ecdb014be8a2fc266e6bb2af6a096835c83b2cd5
Maureen Helm <maureen.helm@nxp.com>
2017-08-28 14:09:55 -0500

    clock_control: Introduce mcux ccm driver
    Adds a new clock control driver for i.MX SoCs that have the clock
    control module (CCM).
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 0f3b4909052e716a5862c72bc6ae3c706ca39941

---
+++ drivers/clock_control/clock_control_mcux_ccm.c
@@ -0,0 +1,6 @@
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|mcux_ccm_driver_api|variable|mcux_ccm_driver_api
+DECL|mcux_ccm_get_subsys_rate|function|static int mcux_ccm_get_subsys_rate(struct device *dev, clock_control_subsys_t sub_system, u32_t *rate)
+DECL|mcux_ccm_init|function|static int mcux_ccm_init(struct device *dev)
+DECL|mcux_ccm_off|function|static int mcux_ccm_off(struct device *dev, clock_control_subsys_t sub_system)
+DECL|mcux_ccm_on|function|static int mcux_ccm_on(struct device *dev, clock_control_subsys_t sub_system)
