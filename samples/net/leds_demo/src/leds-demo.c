98f532973f8ddcbe4e504133e80f1ce61e0987c8
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-08-28 13:56:38 +0300

    net: samples: Modify leds_demo to use new CoAP api's
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: e6bda227106e2a1d190dc0d1060ddd2f099aeb6e

--- samples/net/leds_demo/src/leds-demo.c
+++ samples/net/leds_demo/src/leds-demo.c
@@ -10,22 +10,24 @@ DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|context|variable|context
 DECL|dummy_attributes|variable|dummy_attributes
-DECL|dummy_get|function|static int dummy_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|dummy_get|function|static int dummy_get(struct coap_resource *resource, struct coap_packet *request)
 DECL|dummy_path|variable|dummy_path
 DECL|fake_led|variable|fake_led
+DECL|get_from_ip_addr|function|static void get_from_ip_addr(struct coap_packet *cpkt, struct sockaddr_in6 *from)
 DECL|join_coap_multicast_group|function|static bool join_coap_multicast_group(void)
 DECL|led0|variable|led0
 DECL|led_default_attributes|variable|led_default_attributes
 DECL|led_default_path|variable|led_default_path
-DECL|led_get|function|static int led_get(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|led_get|function|static int led_get(struct coap_resource *resource, struct coap_packet *request)
 DECL|led_off|variable|led_off
 DECL|led_on|variable|led_on
-DECL|led_post|function|static int led_post(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
-DECL|led_put|function|static int led_put(struct zoap_resource *resource, struct zoap_packet *request, const struct sockaddr *from)
+DECL|led_post|function|static int led_post(struct coap_resource *resource, struct coap_packet *request)
+DECL|led_put|function|static int led_put(struct coap_resource *resource, struct coap_packet *request)
 DECL|led_toggle_off|variable|led_toggle_off
 DECL|led_toggle_on|variable|led_toggle_on
 DECL|main|function|void main(void)
 DECL|read_led|function|static bool read_led(void)
 DECL|resources|variable|resources
 DECL|udp_receive|function|static void udp_receive(struct net_context *context,struct net_pkt *pkt, int status, void *user_data)
+DECL|well_known_core_get|function|static int well_known_core_get(struct coap_resource *resource, struct coap_packet *request)
 DECL|write_led|function|static void write_led(bool led)
