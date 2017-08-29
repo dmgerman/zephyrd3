e6b7294917831f5b2dfa5813a844b6da2cf72ed5
Akhilesh Kumar Upadhyay <akhilesh.kumarx.upadhyay@intel.com>
2017-08-30 00:49:10 +0530

    tests: kernel: x86 : App to validate x86 specific boot time page table
    Testcase developed to validate x86 specific boot time page table faults.
    
    Signed-off-by: Akhilesh Kumar Upadhyay <akhilesh.kumarx.upadhyay@intel.com>
    
    Former-commit-id: 149c341ec639d7599e05687d68d3b8b43c3cb8e3

---
+++ tests/kernel/boot_page_table/src/boot_page_table.c
@@ -0,0 +1,8 @@
+DECL|MEMORY_REG_NUM|macro|MEMORY_REG_NUM
+DECL|before_start_addr_range|function|static int before_start_addr_range(u32_t start_addr_range)
+DECL|boot_page_table|function|int boot_page_table(void)
+DECL|check_param_nonset_region|function|static int check_param_nonset_region(union x86_mmu_pae_pte *value, uint32_t perm)
+DECL|check_param|function|static int check_param(union x86_mmu_pae_pte *value, uint32_t perm)
+DECL|ending_start_addr_range|function|static int ending_start_addr_range(u32_t start_addr_range)
+DECL|starting_addr_range|function|static int starting_addr_range(u32_t start_addr_range)
+DECL|test_boot_page_table|function|void test_boot_page_table(void)
