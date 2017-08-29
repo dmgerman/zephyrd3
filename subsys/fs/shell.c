baf68742b4ff3d3ccad8f3728684f7dc6ad82703
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2017-08-29 12:45:43 +0200

    fs: shell: Add more shell functions
    Add following commands to fs shell:
    - rf <filename>
    - read <filename> [<offset> [<length>]]
    - write <filename> [-o <offset>] <byte1> [<byte2> ... [<byteN>]]
    - trunc <filename> [<length>]
    
    For writes without offset specified, new data is appended to file.
    For truncaces without length specified, 0 is default value.
    
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
    
    Former-commit-id: 12e2dfd90acdf3a7ce5f9baeabbb383fcc719afe

--- subsys/fs/shell.c
+++ subsys/fs/shell.c
@@ -1,8 +1,15 @@
+DECL|BUF_CNT|macro|BUF_CNT
+DECL|MAX_FILENAME_LEN|macro|MAX_FILENAME_LEN
+DECL|MAX_INPUT_LEN|macro|MAX_INPUT_LEN
 DECL|MAX_PATH_LEN|macro|MAX_PATH_LEN
 DECL|cmd_cd|function|static int cmd_cd(int argc, char *argv[])
 DECL|cmd_ls|function|static int cmd_ls(int argc, char *argv[])
 DECL|cmd_mkdir|function|static int cmd_mkdir(int argc, char *argv[])
 DECL|cmd_pwd|function|static int cmd_pwd(int argc, char *argv[])
+DECL|cmd_read|function|static int cmd_read(int argc, char *argv[])
+DECL|cmd_rm|function|static int cmd_rm(int argc, char *argv[])
+DECL|cmd_trunc|function|static int cmd_trunc(int argc, char *argv[])
+DECL|cmd_write|function|static int cmd_write(int argc, char *argv[])
 DECL|create_abs_path|function|static void create_abs_path(const char *name, char *path, size_t len)
 DECL|cwd|variable|cwd
 DECL|fs_commands|variable|fs_commands
