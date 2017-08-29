7cafbad2c6e1e6dab03d77f20cd5b7b68a5a507e
Andrew Boie <andrew.p.boie@intel.com>
2017-08-29 12:59:51 -0700

    x86: cleanup _new_thread()
    Years of iterative development had made this function more complicated
    than it needed to be. Fixed some errors in the documentation as well.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 8eeb09765b69c98c77aa9f92f63b9cc2c8d0b272

--- arch/x86/core/thread.c
+++ arch/x86/core/thread.c
@@ -1,2 +1,13 @@
-DECL|_new_thread_internal|function|static void _new_thread_internal(char *pStackMem, unsigned int stackSize, int priority, unsigned int options, struct k_thread *thread)
-DECL|_new_thread|function|void _new_thread(struct k_thread *thread, k_thread_stack_t stack, size_t stackSize, _thread_entry_t pEntry, void *parameter1, void *parameter2, void *parameter3, int priority, unsigned int options)
+DECL|_new_thread|function|void _new_thread(struct k_thread *thread, k_thread_stack_t stack, size_t stack_size, _thread_entry_t entry, void *parameter1, void *parameter2, void *parameter3, int priority, unsigned int options)
+DECL|_thread_entry|member|void *_thread_entry;
+DECL|_x86_initial_frame|struct|struct _x86_initial_frame {
+DECL|ebp|member|u32_t ebp;
+DECL|ebx|member|u32_t ebx;
+DECL|edi|member|u32_t edi;
+DECL|eflags|member|u32_t eflags;
+DECL|entry|member|_thread_entry_t entry;
+DECL|esi|member|u32_t esi;
+DECL|p1|member|void *p1;
+DECL|p2|member|void *p2;
+DECL|p3|member|void *p3;
+DECL|swap_retval|member|u32_t swap_retval;
