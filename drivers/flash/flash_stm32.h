cc90162e527d0fe8ef0c3cb91a972ddf904464d5
Marti Bolivar <marti.bolivar@linaro.org>
2017-08-30 16:39:26 -0400

    flash: stm32: stm32f4: add page layout support
    Add flash page layout support for STM32F4 SoCs.
    
    This almost eliminates the need for flash_map.h, except for
    STM32F4X_SECTOR_MASK, so delete the file and move the define into the
    F4 implementation, to keep things simple.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: 32482e9cb88f82fcd56894f38e76b3d7767c4c90

--- drivers/flash/flash_stm32.h
+++ drivers/flash/flash_stm32.h
@@ -2,5 +2,6 @@ DECL|DRIVERS_FLASH_STM32_H|macro|DRIVERS_FLASH_STM32_H
 DECL|FLASH_STM32_PRIV|macro|FLASH_STM32_PRIV
 DECL|FLASH_STM32_REGS|macro|FLASH_STM32_REGS
 DECL|flash_stm32_priv|struct|struct flash_stm32_priv {
+DECL|flash_stm32_range_exists|function|static inline bool flash_stm32_range_exists(struct device *dev, off_t offset, u32_t len)
 DECL|regs|member|struct stm32f4x_flash *regs;
 DECL|sem|member|struct k_sem sem;
--- drivers/flash/flash_stm32f4x.c
+++ drivers/flash/flash_stm32f4x.c
@@ -1,5 +1,13 @@
-DECL|erase_sector|function|static int erase_sector(struct device *dev, u16_t sector)
+DECL|STM32F4X_SECTOR_MASK|macro|STM32F4X_SECTOR_MASK
+DECL|erase_sector|function|static int erase_sector(struct device *dev, u32_t sector)
 DECL|flash_stm32_block_erase_loop|function|int flash_stm32_block_erase_loop(struct device *dev, unsigned int offset, unsigned int len)
+DECL|flash_stm32_page_layout|function|void flash_stm32_page_layout(struct device *dev, const struct flash_pages_layout **layout, size_t *layout_size)
 DECL|flash_stm32_valid_range|function|bool flash_stm32_valid_range(struct device *dev, off_t offset, u32_t len, bool write)
 DECL|flash_stm32_write_range|function|int flash_stm32_write_range(struct device *dev, unsigned int offset, const void *data, unsigned int len)
+DECL|stm32f4_flash_layout|variable|stm32f4_flash_layout
+DECL|stm32f4_flash_layout|variable|stm32f4_flash_layout
+DECL|stm32f4_flash_layout|variable|stm32f4_flash_layout
+DECL|stm32f4_flash_layout|variable|stm32f4_flash_layout
+DECL|stm32f4_flash_layout|variable|stm32f4_flash_layout
+DECL|stm32f4_flash_layout|variable|stm32f4_flash_layout
 DECL|write_byte|function|static int write_byte(struct device *dev, off_t offset, u8_t val)
