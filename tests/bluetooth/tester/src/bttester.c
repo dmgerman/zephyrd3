7e2643c63573e67edf5d73850affbf8dc5db6a1c
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2017-08-30 13:45:31 +0300

    Bluetooth: tester: Fix using invalid elements with k_fifo
    The documentation of k_fifo_put mentions that the items must have 32
    bits reserved for kernel use as that is where the sys_snode_t pointer
    is placed but this detail was not taken into consideration when
    designing the cmd_queue which access each element as a btp_cmd.
    
    Note: This probably used to work just because k_fifo_get used to place
    data directly into thread's swap_data so the kernel never really touched
    the list but with the changes to use k_poll the list is always updated
    exposing this issue.
    
    Jira: ZEP-2568
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: d229ec07ad3ea438da1c05155fd4f242cce4572a

--- tests/bluetooth/tester/src/bttester.c
+++ tests/bluetooth/tester/src/bttester.c
@@ -1,9 +1,13 @@
 DECL|CMD_QUEUED|macro|CMD_QUEUED
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|_reserved|member|u32_t _reserved;
+DECL|btp_buf|struct|struct btp_buf {
 DECL|cmd_buf|variable|cmd_buf
 DECL|cmd_handler|function|static void cmd_handler(void *p1, void *p2, void *p3)
 DECL|cmd_thread|variable|cmd_thread
+DECL|data|member|u8_t data[BTP_MTU];
 DECL|handle_core|function|static void handle_core(u8_t opcode, u8_t index, u8_t *data,u16_t len)
+DECL|hdr|member|struct btp_hdr hdr;
 DECL|recv_cb|function|static u8_t *recv_cb(u8_t *buf, size_t *off)
 DECL|register_service|function|static void register_service(u8_t *data, u16_t len)
 DECL|supported_commands|function|static void supported_commands(u8_t *data, u16_t len)
