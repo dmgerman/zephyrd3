4e53bb1a46ea3c0011d9f2f4ed236df10b336b60
Youvedeep Singh <youvedeep.singh@intel.com>
2017-08-31 20:22:18 +0530

    tests: benchmark: timing_info: Add support for nrf series processors
    nrf SOC uses nrf rtc timer (not sys tick), which is 32kHz,
    whereas CPU runs at higher speed (nrf52 runs at 64MHz).
    So 32Khz is too slow to measure critical kernel parameters.
    
    This patch does :-
    1. Add support for nrf SOC for timing_info benchmarking.
    2. Uses SOC timer to measure kernel parameters.
    
    Jira: ZEP-2314
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 91063a33a41dd864fb5614361370854d785d2a2d

--- tests/benchmarks/timing_info/src/timing_info.h
+++ tests/benchmarks/timing_info/src/timing_info.h
@@ -1,4 +1,6 @@
 DECL|CALCULATE_TIME|macro|CALCULATE_TIME
+DECL|CYCLES_PER_SEC|macro|CYCLES_PER_SEC
+DECL|CYCLES_TO_NS|macro|CYCLES_TO_NS
 DECL|CYCLES_TO_NS|macro|CYCLES_TO_NS
 DECL|DECLARE_VAR|macro|DECLARE_VAR
 DECL|FORMAT|macro|FORMAT
@@ -6,8 +8,20 @@ DECL|FORMAT|macro|FORMAT
 DECL|GET_2ND_ARG|macro|GET_2ND_ARG
 DECL|GET_3ND_ARG|macro|GET_3ND_ARG
 DECL|GET_CURRENT_TIME|macro|GET_CURRENT_TIME
+DECL|GET_CURRENT_TIME|macro|GET_CURRENT_TIME
+DECL|NANOSECS_PER_SEC|macro|NANOSECS_PER_SEC
 DECL|PRINT_F|macro|PRINT_F
 DECL|PRINT_F|macro|PRINT_F
 DECL|PRINT_STATS|macro|PRINT_STATS
+DECL|PRINT_STATS|macro|PRINT_STATS
 DECL|SUBTRACT_CLOCK_CYCLES|macro|SUBTRACT_CLOCK_CYCLES
 DECL|SUBTRACT_CLOCK_CYCLES|macro|SUBTRACT_CLOCK_CYCLES
+DECL|SUBTRACT_CLOCK_CYCLES|macro|SUBTRACT_CLOCK_CYCLES
+DECL|benchmark_timer_init|function|static inline void benchmark_timer_init(void)
+DECL|benchmark_timer_init|function|static inline void benchmark_timer_init(void) { }
+DECL|benchmark_timer_start|function|static inline void benchmark_timer_start(void)
+DECL|benchmark_timer_start|function|static inline void benchmark_timer_start(void) { }
+DECL|benchmark_timer_stop|function|static inline void benchmark_timer_stop(void)
+DECL|benchmark_timer_stop|function|static inline void benchmark_timer_stop(void) { }
+DECL|get_core_freq_MHz|function|static inline u32_t get_core_freq_MHz(void)
+DECL|get_core_freq_MHz|function|static inline u32_t get_core_freq_MHz(void)
