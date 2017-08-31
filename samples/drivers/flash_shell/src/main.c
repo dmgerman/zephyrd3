64e58e818fb601b2e22255b16ee3f10d410ef398
Marti Bolivar <marti.bolivar@linaro.org>
2017-08-31 16:26:42 -0400

    samples: drivers: add flash_shell
    This is a simple shell module that allows arbitrary boards with flash
    driver support to explore the flash device.
    
    - Reading, erasing, and writing by device offsets are supported in all
      cases.
    
    - If the flash page layout is available, it can be printed, and I/O
      can also be done to a specified page as well.
    
    One known issue is that writing to flash on targets that require
    doubleword-sized writes (e.g STM32L4) will fail since the number of
    arguments required exceeds ARGC_MAX in shell.c. Addressing that is
    left to future work.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: 219b3cd06712377c83c5ae3486effe888836a518

---
+++ samples/drivers/flash_shell/src/main.c
@@ -0,0 +1,34 @@
+DECL|ARGC_MAX|macro|ARGC_MAX
+DECL|CANONICALIZE_ARGS|macro|CANONICALIZE_ARGS
+DECL|ERASE_HELP|macro|ERASE_HELP
+DECL|ERASE_PAGE_HELP|macro|ERASE_PAGE_HELP
+DECL|FLASH_DEV_NAME|macro|FLASH_DEV_NAME
+DECL|FLASH_DEV_NAME|macro|FLASH_DEV_NAME
+DECL|FLASH_DEV_NAME|macro|FLASH_DEV_NAME
+DECL|PAGE_COUNT_HELP|macro|PAGE_COUNT_HELP
+DECL|PAGE_LAYOUT_HELP|macro|PAGE_LAYOUT_HELP
+DECL|READ_HELP|macro|READ_HELP
+DECL|READ_PAGE_HELP|macro|READ_PAGE_HELP
+DECL|SET_DEVICE_HELP|macro|SET_DEVICE_HELP
+DECL|THIS_MODULE_NAME|macro|THIS_MODULE_NAME
+DECL|WRITE_HELP|macro|WRITE_HELP
+DECL|WRITE_PAGE_HELP|macro|WRITE_PAGE_HELP
+DECL|check_flash_device|function|static int check_flash_device(void)
+DECL|commands|variable|commands
+DECL|do_erase|function|static int do_erase(off_t offset, size_t size)
+DECL|do_read|function|static int do_read(off_t offset, size_t len)
+DECL|do_write|function|static int do_write(off_t offset, u8_t *buf, size_t len, bool read_back)
+DECL|dump_buffer|function|static void dump_buffer(u8_t *buf, size_t size)
+DECL|flash_device|variable|flash_device
+DECL|flash_shell_erase_page|function|static int flash_shell_erase_page(int argc, char *argv[])
+DECL|flash_shell_erase|function|static int flash_shell_erase(int argc, char *argv[])
+DECL|flash_shell_page_count|function|static int flash_shell_page_count(int argc, char *argv[])
+DECL|flash_shell_page_layout|function|static int flash_shell_page_layout(int argc, char *argv[])
+DECL|flash_shell_read_page|function|static int flash_shell_read_page(int argc, char *argv[])
+DECL|flash_shell_read|function|static int flash_shell_read(int argc, char *argv[])
+DECL|flash_shell_set_device|function|static int flash_shell_set_device(int argc, char *argv[])
+DECL|flash_shell_write_page|function|static int flash_shell_write_page(int argc, char *argv[])
+DECL|flash_shell_write|function|static int flash_shell_write(int argc, char *argv[])
+DECL|main|function|void main(void)
+DECL|parse_u8|function|static int parse_u8(const char *str, u8_t *result)
+DECL|parse_ul|function|static int parse_ul(const char *str, unsigned long *result)
