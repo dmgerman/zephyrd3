a3e230e55bbd626bf7ea8a5a698a351aaf46f99f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-09-03 22:12:23 +0300

    samples: net: dns: Add mDNS client support to DNS resolver sample
    The DNS resolver example enables mDNS client support and then
    queries zephyr.local hostname. The net-tools project has example
    avahi-daemon script that will response these .local queries and
    can be used in testing.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: af63ae28ec4dabb2d4f5df8337484d67d9501415

--- samples/net/dns_resolve/src/main.c
+++ samples/net/dns_resolve/src/main.c
@@ -4,14 +4,16 @@ DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|dns_result_cb|function|void dns_result_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 DECL|do_ipv4_lookup|function|static void do_ipv4_lookup(struct k_work *work)
-DECL|do_ipv6_lookup|function|static void do_ipv6_lookup(struct k_work *work)
+DECL|do_ipv6_lookup|function|static void do_ipv6_lookup(void)
+DECL|do_mdns_ipv4_lookup|function|static void do_mdns_ipv4_lookup(struct k_work *work)
+DECL|do_mdns_ipv6_lookup|function|static void do_mdns_ipv6_lookup(struct k_work *work)
 DECL|ipv4_addr_add_handler|function|static void ipv4_addr_add_handler(struct net_mgmt_event_callback *cb, u32_t mgmt_event, struct net_if *iface)
 DECL|ipv4_timer|variable|ipv4_timer
-DECL|ipv6_router_add_handler|function|static void ipv6_router_add_handler(struct net_mgmt_event_callback *cb, u32_t mgmt_event, struct net_if *iface)
-DECL|ipv6_timer|variable|ipv6_timer
 DECL|main|function|void main(void)
+DECL|mdns_ipv4_timer|variable|mdns_ipv4_timer
+DECL|mdns_ipv6_timer|variable|mdns_ipv6_timer
+DECL|mdns_result_cb|function|void mdns_result_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 DECL|mgmt4_cb|variable|mgmt4_cb
-DECL|mgmt6_cb|variable|mgmt6_cb
 DECL|setup_dhcpv4|function|static void setup_dhcpv4(struct net_if *iface)
 DECL|setup_dhcpv4|macro|setup_dhcpv4
 DECL|setup_ipv4|function|static void setup_ipv4(struct net_if *iface)
