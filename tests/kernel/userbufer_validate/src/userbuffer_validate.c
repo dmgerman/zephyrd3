f4abbb6866b31ce64d2b2fd4a201496b8e20ade9
Akhilesh Kumar Upadhyay <akhilesh.kumarx.upadhyay@intel.com>
2017-09-04 23:51:22 +0530

    x86: mmu: kernel: Validate existing APIs
    Testcase developed x86mmu specific, to validate
    existing  APIs. This checks for the PDE/PTE set
    on the address and returns if some violation occurs or not.
    
    Signed-off-by: Akhilesh Kumar Upadhyay <akhilesh.kumarx.upadhyay@intel.com>
    
    Former-commit-id: 3d612b7a8b8b2caf00c5d3c0d64d405d77f49e47

---
+++ tests/kernel/userbufer_validate/src/userbuffer_validate.c
@@ -0,0 +1,45 @@
+DECL|ADDR_PAGE_1|macro|ADDR_PAGE_1
+DECL|ADDR_PAGE_2|macro|ADDR_PAGE_2
+DECL|BUFF_READABLE|macro|BUFF_READABLE
+DECL|BUFF_SIZE|macro|BUFF_SIZE
+DECL|BUFF_USER|macro|BUFF_USER
+DECL|BUFF_WRITEABLE|macro|BUFF_WRITEABLE
+DECL|PRESET_PAGE_1_VALUE|macro|PRESET_PAGE_1_VALUE
+DECL|PRESET_PAGE_2_VALUE|macro|PRESET_PAGE_2_VALUE
+DECL|SKIP_SIZE|macro|SKIP_SIZE
+DECL|buffer_readable_read|function|static int buffer_readable_read(void)
+DECL|buffer_readable_write|function|static int buffer_readable_write(void)
+DECL|buffer_rw_read|function|static int buffer_rw_read(void)
+DECL|buffer_supervisor_rw|function|static int buffer_supervisor_rw(void)
+DECL|buffer_supervisor_w|function|static int buffer_supervisor_w(void)
+DECL|buffer_user_rw_supervisor|function|static int buffer_user_rw_supervisor(void)
+DECL|buffer_user_rw_user|function|static int buffer_user_rw_user(void)
+DECL|buffer_writeable_write|function|static int buffer_writeable_write(void)
+DECL|multi_page_buffer_read_user|function|static int multi_page_buffer_read_user(void)
+DECL|multi_page_buffer_read|function|static int multi_page_buffer_read(void)
+DECL|multi_page_buffer_user|function|static int multi_page_buffer_user(void)
+DECL|multi_page_buffer_write_user|function|static int multi_page_buffer_write_user(void)
+DECL|multi_pde_buffer_readable_read|function|static int multi_pde_buffer_readable_read(void)
+DECL|multi_pde_buffer_readable_write|function|static int multi_pde_buffer_readable_write(void)
+DECL|multi_pde_buffer_rw|function|static int multi_pde_buffer_rw(void)
+DECL|multi_pde_buffer_writeable_write|function|static int multi_pde_buffer_writeable_write(void)
+DECL|reset_flag|function|void reset_flag(void)
+DECL|reset_multi_pde_flag|function|void reset_multi_pde_flag(void)
+DECL|reset_multi_pte_page_flag|function|void reset_multi_pte_page_flag(void)
+DECL|status|variable|status
+DECL|test_buffer_readable_read|function|void test_buffer_readable_read(void)
+DECL|test_buffer_readable_write|function|void test_buffer_readable_write(void)
+DECL|test_buffer_rw_read|function|void test_buffer_rw_read(void)
+DECL|test_buffer_supervisor_rw|function|void test_buffer_supervisor_rw(void)
+DECL|test_buffer_supervisor_w|function|void test_buffer_supervisor_w(void)
+DECL|test_buffer_user_rw_supervisor|function|void test_buffer_user_rw_supervisor(void)
+DECL|test_buffer_user_rw_user|function|void test_buffer_user_rw_user(void)
+DECL|test_buffer_writeable_write|function|void test_buffer_writeable_write(void)
+DECL|test_multi_page_buffer_read_user|function|void test_multi_page_buffer_read_user(void)
+DECL|test_multi_page_buffer_read|function|void test_multi_page_buffer_read(void)
+DECL|test_multi_page_buffer_user|function|void test_multi_page_buffer_user(void)
+DECL|test_multi_page_buffer_write_user|function|void test_multi_page_buffer_write_user(void)
+DECL|test_multi_pde_buffer_readable_read|function|void test_multi_pde_buffer_readable_read(void)
+DECL|test_multi_pde_buffer_readable_write|function|void test_multi_pde_buffer_readable_write(void)
+DECL|test_multi_pde_buffer_rw|function|void test_multi_pde_buffer_rw(void)
+DECL|test_multi_pde_buffer_writeable_write|function|void test_multi_pde_buffer_writeable_write(void)
