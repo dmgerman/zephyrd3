edcfd35ee27412ae43a0ab2261c13bbffcef80af
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-04 13:06:49 +0200

    net/ieee802154: Add a way to retrieve hw capabilities from the devices
    This will be useful to know various generic hardware aspects that can be
    used relevantly by the L2 layer.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 9c4605583e9668a8faf9aea38eda3371236adf5d

--- include/net/ieee802154_radio.h
+++ include/net/ieee802154_radio.h
@@ -1,6 +1,13 @@
+DECL|IEEE802154_HW_2_4_GHZ|enumerator|IEEE802154_HW_2_4_GHZ = BIT(4), /* 2.4Ghz radio supported */
+DECL|IEEE802154_HW_CSMA|enumerator|IEEE802154_HW_CSMA = BIT(3), /* CSMA-CA supported */
+DECL|IEEE802154_HW_FCS|enumerator|IEEE802154_HW_FCS = BIT(0), /* Frame Check-Sum supported */
+DECL|IEEE802154_HW_FILTER|enumerator|IEEE802154_HW_FILTER = BIT(2), /* Filters PAN ID, long/short addr */
+DECL|IEEE802154_HW_PROMISC|enumerator|IEEE802154_HW_PROMISC = BIT(1), /* Promiscuous mode supported */
 DECL|__IEEE802154_RADIO_H__|macro|__IEEE802154_RADIO_H__
 DECL|__packed|variable|__packed
 DECL|cca|member|int (*cca)(struct device *dev);
+DECL|get_capabilities|member|enum ieee802154_hw_caps (*get_capabilities)(struct device *dev);
+DECL|ieee802154_hw_caps|enum|enum ieee802154_hw_caps {
 DECL|ieee802154_radio_api|struct|struct ieee802154_radio_api {
 DECL|iface_api|member|struct net_if_api iface_api;
 DECL|set_channel|member|int (*set_channel)(struct device *dev, u16_t channel);
