cf29193bab165f52b64cdf5b1253cf1f6ea1c929
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-05 13:50:36 +0200

    net/ieee802154: Add a filter function to radio API
    This will replace the current mandatory set_ieee_addr, set_short_addr
    and set_pan_id functions, which are only valid if the hardware is
    supporting filtering. Which is not the case on some chips.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 3d63a3411d21f50a14bfd799740a9d407d7c2ef5

--- include/net/ieee802154_radio.h
+++ include/net/ieee802154_radio.h
@@ -1,3 +1,6 @@
+DECL|IEEE802154_FILTER_TYPE_IEEE_ADDR|enumerator|IEEE802154_FILTER_TYPE_IEEE_ADDR,
+DECL|IEEE802154_FILTER_TYPE_PAN_ID|enumerator|IEEE802154_FILTER_TYPE_PAN_ID,
+DECL|IEEE802154_FILTER_TYPE_SHORT_ADDR|enumerator|IEEE802154_FILTER_TYPE_SHORT_ADDR,
 DECL|IEEE802154_HW_2_4_GHZ|enumerator|IEEE802154_HW_2_4_GHZ = BIT(4), /* 2.4Ghz radio supported */
 DECL|IEEE802154_HW_CSMA|enumerator|IEEE802154_HW_CSMA = BIT(3), /* CSMA-CA supported */
 DECL|IEEE802154_HW_FCS|enumerator|IEEE802154_HW_FCS = BIT(0), /* Frame Check-Sum supported */
@@ -7,14 +10,20 @@ DECL|__IEEE802154_RADIO_H__|macro|__IEEE802154_RADIO_H__
 DECL|__packed|variable|__packed
 DECL|cca|member|int (*cca)(struct device *dev);
 DECL|get_capabilities|member|enum ieee802154_hw_caps (*get_capabilities)(struct device *dev);
+DECL|ieee802154_filter_type|enum|enum ieee802154_filter_type {
+DECL|ieee802154_filter|struct|struct ieee802154_filter {
 DECL|ieee802154_hw_caps|enum|enum ieee802154_hw_caps {
 DECL|ieee802154_radio_api|struct|struct ieee802154_radio_api {
+DECL|ieee_addr|member|u8_t *ieee_addr;
 DECL|iface_api|member|struct net_if_api iface_api;
+DECL|pan_id|member|u16_t pan_id;
 DECL|set_channel|member|int (*set_channel)(struct device *dev, u16_t channel);
+DECL|set_filter|member|int (*set_filter)(struct device *dev,
 DECL|set_ieee_addr|member|int (*set_ieee_addr)(struct device *dev, const u8_t *ieee_addr);
 DECL|set_pan_id|member|int (*set_pan_id)(struct device *dev, u16_t pan_id);
 DECL|set_short_addr|member|int (*set_short_addr)(struct device *dev, u16_t short_addr);
 DECL|set_txpower|member|int (*set_txpower)(struct device *dev, s16_t dbm);
+DECL|short_addr|member|u16_t short_addr;
 DECL|start|member|int (*start)(struct device *dev);
 DECL|stop|member|int (*stop)(struct device *dev);
 DECL|tx|member|int (*tx)(struct device *dev,
