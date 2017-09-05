b655fe5881a100979e6a334eae150aeb49fb43a6
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-05 13:52:29 +0200

    net/ieee802154: Apply hw filters when applicable
    If the hw supports filtering, L2 will apply the ieee address, short
    address or PAN ID filters.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: c65c2a1130918e20e3901dcf26244ce9fa56a267

---
+++ subsys/net/ip/l2/ieee802154/ieee802154_utils.h
@@ -0,0 +1,4 @@
+DECL|__IEEE802154_UTILS_H__|macro|__IEEE802154_UTILS_H__
+DECL|ieee802154_filter_ieee_addr|function|static inline void ieee802154_filter_ieee_addr(struct net_if *iface, u8_t *ieee_addr)
+DECL|ieee802154_filter_pan_id|function|static inline void ieee802154_filter_pan_id(struct net_if *iface, u16_t pan_id)
+DECL|ieee802154_filter_short_addr|function|static inline void ieee802154_filter_short_addr(struct net_if *iface,u16_t short_addr)
