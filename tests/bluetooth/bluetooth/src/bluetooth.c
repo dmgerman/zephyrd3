e526c9f6ffe77e709d62bf9b0778de61cddb2fed
Punit Vara <punit.vara@intel.com>
2017-09-05 17:03:05 +0530

    tests: bluetooth: Use ztest apis
    This patch converts legacy test case to use ztest apis.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 7ce27c6b49d3b7b214cc0b1a30175dd4ad118d96

--- tests/bluetooth/bluetooth/src/bluetooth.c
+++ tests/bluetooth/bluetooth/src/bluetooth.c
@@ -3,4 +3,5 @@ DECL|driver_init|function|static void driver_init(void)
 DECL|driver_open|function|static int driver_open(void)
 DECL|driver_send|function|static int driver_send(struct net_buf *buf)
 DECL|drv|variable|drv
-DECL|main|function|void main(void)
+DECL|test_bluetooth_entry|function|void test_bluetooth_entry(void)
+DECL|test_main|function|void test_main(void)
