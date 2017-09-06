02ee5840568bdf912396b61018cd4b11cb6be61f
Aska Wu <aska.wu@linaro.org>
2017-09-06 11:40:49 +0800

    net: sample: Add sntp client sample
    This sample demostrates how to use SNTP client library to get the
    current time from SNTP/NTP server.
    
    Signed-off-by: Aska Wu <aska.wu@linaro.org>
    
    Former-commit-id: 89f28ac7c8c731bd30e73e3af53c969fc75974f8

---
+++ samples/net/sntp_client/src/main.c
@@ -0,0 +1,4 @@
+DECL|SNTP_PORT|macro|SNTP_PORT
+DECL|main|function|void main(void)
+DECL|resp_callback|function|void resp_callback(struct sntp_ctx *ctx, int status, u64_t epoch_time, void *user_data)
+DECL|sem|variable|sem
