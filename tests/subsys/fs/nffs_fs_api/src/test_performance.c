c39c8696d2dda1f76b5c034bbfe1cf3d49db438b
Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
2017-09-06 17:46:52 +0200

    tests: nffs_fs_api: Add performance test for nffs
    This adds performance test for nffs with open, close, read, write and
    unlink sub test cases.
    
    Origin: Original
    
    Signed-off-by: Grzegorz Kolodziejczyk <grzegorz.kolodziejczyk@codecoup.pl>
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
    
    Former-commit-id: 4e916f40f7c7138ed2170a29be492c379785a750

---
+++ tests/subsys/fs/nffs_fs_api/src/test_performance.c
@@ -0,0 +1,6 @@
+DECL|RW_CHUNK_LENGTH|macro|RW_CHUNK_LENGTH
+DECL|RW_DATA_LENGTH|macro|RW_DATA_LENGTH
+DECL|TEST_DATA_LEN|macro|TEST_DATA_LEN
+DECL|TEST_NUM_FILES|macro|TEST_NUM_FILES
+DECL|area_descs|variable|area_descs
+DECL|test_performance|function|void test_performance(void)
