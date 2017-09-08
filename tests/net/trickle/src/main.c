6e2da195c65bf9b9321e519084eb76e7887c90fd
Punit Vara <punit.vara@intel.com>
2017-09-08 17:14:45 +0530

    tests: trickle: convert legacy test to ztest
    This patch makes legacy test to use ztest APIs.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 68e00378d05cbda54d522c4b3a5f448ef01ec63b

--- tests/net/trickle/src/main.c
+++ tests/net/trickle/src/main.c
@@ -13,25 +13,21 @@ DECL|cb_1_called|variable|cb_1_called
 DECL|cb_1|function|static void cb_1(struct net_trickle *trickle, bool do_suppress, void *user_data)
 DECL|cb_2_called|variable|cb_2_called
 DECL|cb_2|function|static void cb_2(struct net_trickle *trickle, bool do_suppress, void *user_data)
-DECL|func|member|bool (*func)(void);
-DECL|main|function|void main(void)
-DECL|name|member|const char *name;
 DECL|t1|variable|t1
 DECL|t2|variable|t2
-DECL|test_failed|variable|test_failed
-DECL|test_init|function|static bool test_init(void)
-DECL|test_trickle_1_status|function|static bool test_trickle_1_status(void)
-DECL|test_trickle_1_stopped|function|static bool test_trickle_1_stopped(void)
-DECL|test_trickle_1_update|function|static bool test_trickle_1_update(void)
+DECL|test_init|function|static void test_init(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_trickle_1_status|function|static void test_trickle_1_status(void)
+DECL|test_trickle_1_stopped|function|static void test_trickle_1_stopped(void)
+DECL|test_trickle_1_update|function|static void test_trickle_1_update(void)
 DECL|test_trickle_1_wait_long|function|static bool test_trickle_1_wait_long(void)
-DECL|test_trickle_1_wait|function|static bool test_trickle_1_wait(void)
-DECL|test_trickle_2_inc|function|static bool test_trickle_2_inc(void)
-DECL|test_trickle_2_status|function|static bool test_trickle_2_status(void)
-DECL|test_trickle_2_wait|function|static bool test_trickle_2_wait(void)
-DECL|test_trickle_create|function|static bool test_trickle_create(void)
-DECL|test_trickle_start|function|static bool test_trickle_start(void)
-DECL|test_trickle_stop|function|static bool test_trickle_stop(void)
-DECL|tests|variable|tests
+DECL|test_trickle_1_wait|function|static void test_trickle_1_wait(void)
+DECL|test_trickle_2_inc|function|static void test_trickle_2_inc(void)
+DECL|test_trickle_2_status|function|static void test_trickle_2_status(void)
+DECL|test_trickle_2_wait|function|static void test_trickle_2_wait(void)
+DECL|test_trickle_create|function|static void test_trickle_create(void)
+DECL|test_trickle_start|function|static void test_trickle_start(void)
+DECL|test_trickle_stop|function|static void test_trickle_stop(void)
 DECL|token1|variable|token1
 DECL|token2|variable|token2
 DECL|wait|variable|wait
