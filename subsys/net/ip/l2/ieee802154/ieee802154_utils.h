b37663eb2932726b6ced74448eb7a35aceefd8d0
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-11 10:14:48 +0200

    net/ieee802154: Verify in L2 the Sub-Ghz channel value
    Depending on device's band, the upper channel limit can vary a lot in
    Sub-Ghz. Thus verifying it directly in L2 before requesting it to the
    device.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 6fa3d0fa3e779b5f1fb3be3cb60aef7e3f90ea13

--- subsys/net/ip/l2/ieee802154/ieee802154_utils.h
+++ subsys/net/ip/l2/ieee802154/ieee802154_utils.h
@@ -2,3 +2,4 @@ DECL|__IEEE802154_UTILS_H__|macro|__IEEE802154_UTILS_H__
 DECL|ieee802154_filter_ieee_addr|function|static inline void ieee802154_filter_ieee_addr(struct net_if *iface, u8_t *ieee_addr)
 DECL|ieee802154_filter_pan_id|function|static inline void ieee802154_filter_pan_id(struct net_if *iface, u16_t pan_id)
 DECL|ieee802154_filter_short_addr|function|static inline void ieee802154_filter_short_addr(struct net_if *iface,u16_t short_addr)
+DECL|ieee802154_verify_channel|function|static inline bool ieee802154_verify_channel(struct device *dev, u16_t channel)
