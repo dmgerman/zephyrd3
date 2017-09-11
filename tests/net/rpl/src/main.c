defbc55dfcabd9664820751362ee8a52433d7ab9
Punit Vara <punit.vara@intel.com>
2017-09-11 12:35:19 +0530

    tests: rpl: convert legacy test in ztest
    This patch makes use of ztest apis to make output unified with
    other test cases
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 658a5a2ddbaec04395f22f62ad9c8281893adcbe

--- tests/net/rpl/src/main.c
+++ tests/net/rpl/src/main.c
@@ -8,7 +8,6 @@ DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
 DECL|data_failure|variable|data_failure
 DECL|expected_icmpv6|variable|expected_icmpv6
 DECL|feed_data|variable|feed_data
-DECL|func|member|bool (*func)(void);
 DECL|in6addr_ll|variable|in6addr_ll
 DECL|in6addr_mcast|variable|in6addr_mcast
 DECL|in6addr_my|variable|in6addr_my
@@ -18,10 +17,8 @@ DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|lladdr_src_storage|variable|lladdr_src_storage
 DECL|lladdr_src|variable|lladdr_src
 DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
-DECL|main|function|void main(void)
 DECL|msg_sending|variable|msg_sending
-DECL|name|member|const char *name;
-DECL|net_ctx_create|function|static bool net_ctx_create(void)
+DECL|net_ctx_create|function|static void net_ctx_create(void)
 DECL|net_rpl_data|variable|net_rpl_data
 DECL|net_rpl_dev_init|function|int net_rpl_dev_init(struct device *dev)
 DECL|net_rpl_get_mac|function|static u8_t *net_rpl_get_mac(struct device *dev)
@@ -31,17 +28,17 @@ DECL|net_rpl_test|struct|struct net_rpl_test {
 DECL|net_test_nbr_lookup_ok|function|static bool net_test_nbr_lookup_ok(void)
 DECL|net_test_send_ns|function|static bool net_test_send_ns(void)
 DECL|peer_addr|variable|peer_addr
-DECL|populate_nbr_cache|function|static bool populate_nbr_cache(void)
+DECL|populate_nbr_cache|function|static void populate_nbr_cache(void)
 DECL|send_link_cb|function|static void send_link_cb(struct net_if *iface, struct net_linkaddr *lladdr, int status)
 DECL|set_pkt_ll_addr|function|static void set_pkt_ll_addr(struct device *dev, struct net_pkt *pkt)
-DECL|test_dao_sending_fail|function|static bool test_dao_sending_fail(void)
-DECL|test_dio_dummy_input|function|static bool test_dio_dummy_input(void)
-DECL|test_dio_receive_dest|function|static bool test_dio_receive_dest(void)
-DECL|test_dis_sending|function|static bool test_dis_sending(void)
+DECL|test_dao_sending_fail|function|static void test_dao_sending_fail(void)
+DECL|test_dio_dummy_input|function|static void test_dio_dummy_input(void)
+DECL|test_dio_receive_dest|function|static void test_dio_receive_dest(void)
+DECL|test_dis_sending|function|static void test_dis_sending(void)
 DECL|test_failed|variable|test_failed
-DECL|test_init|function|static bool test_init(void)
-DECL|test_rpl_mcast_addr|function|static bool test_rpl_mcast_addr(void)
+DECL|test_init|function|static void test_init(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_rpl_mcast_addr|function|static void test_rpl_mcast_addr(void)
 DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_pkt *pkt)
-DECL|tests|variable|tests
 DECL|udp_ctx|variable|udp_ctx
 DECL|wait_data|variable|wait_data
