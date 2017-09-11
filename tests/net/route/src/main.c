9f10ed8f8b7cff6505c63fce2e9dc871cfbe95df
Punit Vara <punit.vara@intel.com>
2017-09-11 12:05:12 +0530

    tests: route: Convert legacy test to use ztest framework
    This patch makes use of ztest apis to make output unified with other
    test cases.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: a29f67beed85ee0e61abf732bb03a4873fa1c260

--- tests/net/route/src/main.c
+++ tests/net/route/src/main.c
@@ -10,19 +10,16 @@ DECL|dest_addresses|variable|dest_addresses
 DECL|dest_addr|variable|dest_addr
 DECL|entry|variable|entry
 DECL|feed_data|variable|feed_data
-DECL|func|member|bool (*func)(void);
 DECL|generic_addr|variable|generic_addr
 DECL|in6addr_mcast|variable|in6addr_mcast
 DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|ll_addr|variable|ll_addr
 DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
-DECL|main|function|void main(void)
 DECL|max_routes|variable|max_routes
 DECL|msg_sending|variable|msg_sending
 DECL|my_addr|variable|my_addr
 DECL|my_iface|variable|my_iface
-DECL|name|member|const char *name;
-DECL|net_ctx_create|function|static bool net_ctx_create(void)
+DECL|net_ctx_create|function|static void net_ctx_create(void)
 DECL|net_route_data_peer|variable|net_route_data_peer
 DECL|net_route_data|variable|net_route_data
 DECL|net_route_dev_init|function|int net_route_dev_init(struct device *dev)
@@ -35,23 +32,23 @@ DECL|net_test_nbr_lookup_ok|function|static bool net_test_nbr_lookup_ok(struct n
 DECL|net_test_send_ns|function|static bool net_test_send_ns(struct net_if *iface, struct in6_addr *addr)
 DECL|peer_addr|variable|peer_addr
 DECL|peer_iface|variable|peer_iface
-DECL|populate_nbr_cache|function|static bool populate_nbr_cache(void)
+DECL|populate_nbr_cache|function|static void populate_nbr_cache(void)
 DECL|recipient|variable|recipient
-DECL|route_add_many|function|static bool route_add_many(void)
-DECL|route_add|function|static bool route_add(void)
-DECL|route_del_again|function|static bool route_del_again(void)
-DECL|route_del_many|function|static bool route_del_many(void)
-DECL|route_del_nexthop_again|function|static bool route_del_nexthop_again(void)
-DECL|route_del_nexthop|function|static bool route_del_nexthop(void)
-DECL|route_del|function|static bool route_del(void)
-DECL|route_get_nexthop|function|static bool route_get_nexthop(void)
-DECL|route_lookup_fail|function|static bool route_lookup_fail(void)
-DECL|route_lookup_ok|function|static bool route_lookup_ok(void)
-DECL|route_update|function|static bool route_update(void)
+DECL|route_add_many|function|static void route_add_many(void)
+DECL|route_add|function|static void route_add(void)
+DECL|route_del_again|function|static void route_del_again(void)
+DECL|route_del_many|function|static void route_del_many(void)
+DECL|route_del_nexthop_again|function|static void route_del_nexthop_again(void)
+DECL|route_del_nexthop|function|static void route_del_nexthop(void)
+DECL|route_del|function|static void route_del(void)
+DECL|route_get_nexthop|function|static void route_get_nexthop(void)
+DECL|route_lookup_fail|function|static void route_lookup_fail(void)
+DECL|route_lookup_ok|function|static void route_lookup_ok(void)
+DECL|route_update|function|static void route_update(void)
 DECL|test_failed|variable|test_failed
-DECL|test_init|function|static bool test_init(void)
+DECL|test_init|function|static void test_init(void)
+DECL|test_main|function|void test_main(void)
 DECL|test_routes|variable|test_routes
 DECL|tester_send_peer|function|static int tester_send_peer(struct net_if *iface, struct net_pkt *pkt)
 DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_pkt *pkt)
-DECL|tests|variable|tests
 DECL|udp_ctx|variable|udp_ctx
