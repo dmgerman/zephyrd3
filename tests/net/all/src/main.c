41454441e536db68d8195005b3fd5386ae1d44a3
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-09-12 12:55:45 +0300

    net: Wrong networking defines used in the code
    There were bunch of config options in tests/net, net-shell and
    wpan_serial sample, and those options had wrong name so they
    were ignored by the code.
    
    Fixes #1428
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: e71a31e22faf0ae2789d3dcba3b117443f129534

--- tests/net/all/src/main.c
+++ tests/net/all/src/main.c
@@ -1,2 +1,6 @@
+DECL|none|member|void *none;
+DECL|offload_context_data|variable|offload_context_data
+DECL|offload_context|struct|static struct offload_context {
+DECL|offload_if_api|variable|offload_if_api
 DECL|ok|function|static void ok(void)
 DECL|test_main|function|void test_main(void)
