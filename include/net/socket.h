6b0638fb38de4e18e3c1c0baa2c1b3efd5fa93ec
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-09-13 23:41:10 +0300

    net: sockets: Implement getaddrinfo()
    POSIX-compatible getaddrinfo() call, implemented on top of native
    Zephyr DNS resolver.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: d3c7152528ccf7e88749da9de2dcce367e31753f

--- include/net/socket.h
+++ include/net/socket.h
@@ -1,15 +1,32 @@
+DECL|EAI_AGAIN|macro|EAI_AGAIN
+DECL|EAI_BADFLAGS|macro|EAI_BADFLAGS
+DECL|EAI_FAIL|macro|EAI_FAIL
+DECL|EAI_NODATA|macro|EAI_NODATA
+DECL|EAI_NONAME|macro|EAI_NONAME
 DECL|POLLIN|macro|POLLIN
 DECL|POLLOUT|macro|POLLOUT
 DECL|ZSOCK_POLLIN|macro|ZSOCK_POLLIN
 DECL|ZSOCK_POLLOUT|macro|ZSOCK_POLLOUT
 DECL|__NET_SOCKET_H|macro|__NET_SOCKET_H
+DECL|_ai_addr|member|struct sockaddr _ai_addr;
+DECL|_ai_canonname|member|char _ai_canonname[DNS_MAX_NAME_SIZE + 1];
 DECL|accept|macro|accept
+DECL|addrinfo|macro|addrinfo
+DECL|ai_addrlen|member|socklen_t ai_addrlen;
+DECL|ai_addr|member|struct sockaddr *ai_addr;
+DECL|ai_canonname|member|char *ai_canonname;
+DECL|ai_family|member|int ai_family;
+DECL|ai_flags|member|int ai_flags;
+DECL|ai_next|member|struct zsock_addrinfo *ai_next;
+DECL|ai_protocol|member|int ai_protocol;
+DECL|ai_socktype|member|int ai_socktype;
 DECL|bind|macro|bind
 DECL|close|macro|close
 DECL|connect|macro|connect
 DECL|events|member|short events;
 DECL|fcntl|macro|fcntl
 DECL|fd|member|int fd;
+DECL|getaddrinfo|macro|getaddrinfo
 DECL|inet_ntop|macro|inet_ntop
 DECL|inet_pton|macro|inet_pton
 DECL|listen|macro|listen
@@ -19,4 +36,5 @@ DECL|recv|macro|recv
 DECL|revents|member|short revents;
 DECL|send|macro|send
 DECL|socket|macro|socket
+DECL|zsock_addrinfo|struct|struct zsock_addrinfo {
 DECL|zsock_pollfd|struct|struct zsock_pollfd {
