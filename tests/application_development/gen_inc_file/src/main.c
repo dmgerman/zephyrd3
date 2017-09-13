3929a4d0762e5f903662959bdf89a67323a790ed
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-09-13 11:02:38 +0300

    tests: Add testing for embedding a hex file
    Add a simple test which generates a file that can be included
    into a .c file. Then verify in zephyr that the file contains
    the same bytes as the original file.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 7830e602529816357a787ec78901f2592ef684e7

---
+++ tests/application_development/gen_inc_file/src/main.c
@@ -0,0 +1,6 @@
+DECL|compressed_inc_file|variable|compressed_inc_file
+DECL|gz_inc_file|variable|gz_inc_file
+DECL|inc_file|variable|inc_file
+DECL|test_gen_gz_inc_file|function|static void test_gen_gz_inc_file(void)
+DECL|test_gen_inc_file|function|static void test_gen_inc_file(void)
+DECL|test_main|function|void test_main(void)
