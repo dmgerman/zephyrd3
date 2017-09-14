fcd2ea194acee737b652735cda4fcbf6f0b001ab
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-09-14 20:53:31 +0300

    samples: net: sockets: Add HTTP GET example
    This example shows usage of client connection and getaddrinfo().
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: 4a771bad3790fdd7626b7c7ec5718547f3b61508

---
+++ samples/net/sockets/http_get/src/http_get.c
@@ -0,0 +1,6 @@
+DECL|CHECK|macro|CHECK
+DECL|REQUEST|macro|REQUEST
+DECL|SSTRLEN|macro|SSTRLEN
+DECL|dump_addrinfo|function|void dump_addrinfo(const struct addrinfo *ai)
+DECL|main|function|int main(void)
+DECL|response|variable|response
