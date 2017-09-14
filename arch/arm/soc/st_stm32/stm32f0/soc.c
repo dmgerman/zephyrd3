937aa8c9fa645d381a7b81124752255b47a0e2a4
Bobby Noelte <b0661n0e17e@gmail.com>
2017-09-14 20:23:53 +0200

    arch: arm: core: fix vector table relocate write to flash
    Some SOCs (e.g. STM32F0) can map the flash to address 0 and
    the flash base address at the same time. Prevent writing to
    duplicate flash address which stops the SOC.
    
    Allow Cortex M SOCs to create their own vector table relocation
    function.
    
    Provide a relocation function for STM32F0x SOCs.
    
    Fixes #3923
    
    Signed-off-by: Bobby Noelte <b0661n0e17e@gmail.com>
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    
    Former-commit-id: 2f7688bfd3d3114ff039b5b3d1795a9388d6f355

--- arch/arm/soc/st_stm32/stm32f0/soc.c
+++ arch/arm/soc/st_stm32/stm32f0/soc.c
@@ -1,2 +1,3 @@
 DECL|HAL_GetTick|function|uint32_t HAL_GetTick(void)
+DECL|relocate_vector_table|function|void relocate_vector_table(void)
 DECL|stm32f0_init|function|static int stm32f0_init(struct device *arg)
