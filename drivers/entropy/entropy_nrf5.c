22bc31d80c08a79d78fabc15fa1fedcd2035d52c
Thiago Silveira <thiago@exati.com.br>
2017-09-19 05:01:27 -0300

    drivers: entropy: Add nRF5 entropy generator driver
    Origin: Original
    
    Signed-off-by: Thiago Silveira <thiago@exati.com.br>
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
    
    Former-commit-id: 912a51957e534a372298b483b7393e639b474896

---
+++ drivers/entropy/entropy_nrf5.c
@@ -0,0 +1,8 @@
+DECL|DEV_DATA|macro|DEV_DATA
+DECL|entropy_nrf5_api_funcs|variable|entropy_nrf5_api_funcs
+DECL|entropy_nrf5_data|variable|entropy_nrf5_data
+DECL|entropy_nrf5_dev_data|struct|struct entropy_nrf5_dev_data {
+DECL|entropy_nrf5_get_entropy|function|static int entropy_nrf5_get_entropy(struct device *device, u8_t *buf, u16_t len)
+DECL|entropy_nrf5_get_u8|function|static inline u8_t entropy_nrf5_get_u8(void)
+DECL|entropy_nrf5_init|function|static int entropy_nrf5_init(struct device *device)
+DECL|user_count|member|atomic_t user_count;
