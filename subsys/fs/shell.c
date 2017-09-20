df5e163811b609cd1c613ed583468426bebedb3b
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2017-09-20 10:36:50 +0200

    fs: shell: Extract common path handling code to helper
    This patch moves code to create absolute path of file/directory to
    separate helper since the same code is used in few places.
    
    Signed-off-by: Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
    
    Former-commit-id: 7feacf26c2e0040cf1bd078db8dc4a0ff1cc0b52

--- subsys/fs/shell.c
+++ subsys/fs/shell.c
@@ -3,5 +3,6 @@ DECL|cmd_cd|function|static int cmd_cd(int argc, char *argv[])
 DECL|cmd_ls|function|static int cmd_ls(int argc, char *argv[])
 DECL|cmd_mkdir|function|static int cmd_mkdir(int argc, char *argv[])
 DECL|cmd_pwd|function|static int cmd_pwd(int argc, char *argv[])
+DECL|create_abs_path|function|static void create_abs_path(const char *name, char *path, size_t len)
 DECL|cwd|variable|cwd
 DECL|fs_commands|variable|fs_commands
