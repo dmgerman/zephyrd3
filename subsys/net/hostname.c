dbd466361b523c2a3119c09b428c320365a73da6
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-09-21 23:30:32 +0300

    net: Add hostname setting to Kconfig
    User can configure hostname of the device in Kconfig. This can
    be used by mDNS responder to answer <hostname>.local queries.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: e3adbf845c6baabae67e96cc17892f5e2bcf3ac2

---
+++ subsys/net/hostname.c
@@ -0,0 +1,8 @@
+DECL|EXTRA_SPACE|macro|EXTRA_SPACE
+DECL|EXTRA_SPACE|macro|EXTRA_SPACE
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|hostname|variable|hostname
+DECL|net_hostname_get|function|const char *net_hostname_get(void)
+DECL|net_hostname_init|function|void net_hostname_init(void)
+DECL|net_hostname_set_postfix|function|int net_hostname_set_postfix(const u8_t *hostname_postfix, int postfix_len)
