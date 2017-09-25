7123f8d22a37cb2aaba692ac6d23c308f7d40692
Andrew Boie <andrew.p.boie@intel.com>
2017-09-25 14:43:10 -0700

    tests: add CONFIG_APPLICATION_MEMORY test
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: b60867fb32748008c044138057a35e0bf15915a9

---
+++ tests/kernel/mem_protect/app_memory/src/main.c
@@ -0,0 +1,15 @@
+DECL|app_bss|variable|app_bss
+DECL|app_data|variable|app_data
+DECL|app_loc|function|int app_loc(void *ptr)
+DECL|app_noinit|variable|app_noinit
+DECL|bar|member|int bar;
+DECL|baz|member|char *baz;
+DECL|data_loc|function|int data_loc(char *start, char *end, void *ptr)
+DECL|foo|member|int foo;
+DECL|kernel_bss|variable|kernel_bss
+DECL|kernel_data|variable|kernel_data
+DECL|kernel_loc|function|int kernel_loc(void *ptr)
+DECL|kernel_noinit|variable|kernel_noinit
+DECL|test_app_memory|function|void test_app_memory(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_struct|struct|struct test_struct {
