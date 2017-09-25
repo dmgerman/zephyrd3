40b19adcf83c6757a5ca3c17b06db40b075423af
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-25 09:41:49 +0200

    tests/net: Add a test for net_mgmt_event_notify_with_info()
    As previous test without info, this one will be thrown with/without
    receiver 1 and 2 times.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: dedecdaed8b36e4b176d329c46cb9f5b63d9c9ae

--- tests/net/mgmt/src/mgmt.c
+++ tests/net/mgmt/src/mgmt.c
@@ -9,20 +9,24 @@ DECL|fake_dev_init|function|int fake_dev_init(struct device *dev)
 DECL|fake_iface_api|variable|fake_iface_api
 DECL|fake_iface_init|function|static void fake_iface_init(struct net_if *iface)
 DECL|fake_iface_send|function|static int fake_iface_send(struct net_if *iface, struct net_pkt *pkt)
+DECL|info_data|variable|info_data
 DECL|initialize_event_tests|function|static void initialize_event_tests(void)
 DECL|receiver_cb|function|static void receiver_cb(struct net_mgmt_event_callback *cb,u32_t nm_event, struct net_if *iface)
 DECL|rx_calls|variable|rx_calls
 DECL|rx_cb|variable|rx_cb
 DECL|rx_event|variable|rx_event
+DECL|sending_event|function|static int sending_event(u32_t times, bool receiver, bool info)
 DECL|test_core_event|function|static int test_core_event(u32_t event, bool (*func)(void))
 DECL|test_main|function|void test_main(void)
 DECL|test_mgmt_request|function|static int test_mgmt_request(u32_t mgmt_request, struct net_if *iface, void *data, u32_t len)
 DECL|test_mgmt|function|void test_mgmt(void)
 DECL|test_requesting_nm|function|void test_requesting_nm(void)
-DECL|test_sending_event|function|static inline int test_sending_event(u32_t times, bool receiver)
+DECL|test_sending_event_info|function|static int test_sending_event_info(u32_t times, bool receiver)
+DECL|test_sending_event|function|static int test_sending_event(u32_t times, bool receiver)
 DECL|test_synchronous_event_listener|function|static int test_synchronous_event_listener(u32_t times, bool on_iface)
 DECL|throw_sleep|variable|throw_sleep
 DECL|throw_times|variable|throw_times
 DECL|thrower_lock|variable|thrower_lock
 DECL|thrower_thread_data|variable|thrower_thread_data
 DECL|thrower_thread|function|static void thrower_thread(void)
+DECL|with_info|variable|with_info
