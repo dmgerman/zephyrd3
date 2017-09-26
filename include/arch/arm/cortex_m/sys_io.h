2fe5d8fb6268f0d09ef35e461db0478f39c7219e
Chunlin Han <chunlin.han@linaro.org>
2017-09-26 20:59:35 +0800

    arch: arm: add initial support for CONFIG_USERSPACE
    add related configs & (stub) functions for enabling
    CONFIG_USERSPACE on arm w/o build errors.
    
    Signed-off-by: Chunlin Han <chunlin.han@linaro.org>
    
    Former-commit-id: 95d28e53bb7b7aa88b20c340bb169f03da3b42a2

--- include/arch/arm/cortex_m/sys_io.h
+++ include/arch/arm/cortex_m/sys_io.h
@@ -1,9 +1,13 @@
 DECL|_CORTEX_M_SYS_IO_H_|macro|_CORTEX_M_SYS_IO_H_
+DECL|sys_bitfield_clear_bit|function|void sys_bitfield_clear_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_bitfield_set_bit|function|void sys_bitfield_set_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_bitfield_test_bit|function|int sys_bitfield_test_bit(mem_addr_t addr, unsigned int bit)
 DECL|sys_clear_bit|function|static inline void sys_clear_bit(mem_addr_t addr, unsigned int bit)
 DECL|sys_read16|function|static inline u16_t sys_read16(mem_addr_t addr)
 DECL|sys_read32|function|static inline u32_t sys_read32(mem_addr_t addr)
 DECL|sys_read8|function|static inline u8_t sys_read8(mem_addr_t addr)
 DECL|sys_set_bit|function|static inline void sys_set_bit(mem_addr_t addr, unsigned int bit)
+DECL|sys_test_bit|function|static inline int sys_test_bit(mem_addr_t addr, unsigned int bit)
 DECL|sys_write16|function|static inline void sys_write16(u16_t data, mem_addr_t addr)
 DECL|sys_write32|function|static inline void sys_write32(u32_t data, mem_addr_t addr)
 DECL|sys_write8|function|static inline void sys_write8(u8_t data, mem_addr_t addr)
