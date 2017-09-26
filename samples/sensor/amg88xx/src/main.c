cbd72566c58280abd11a225eb9a6493e59c1c70e
Johann Fischer <j.fischer@phytec.de>
2017-09-26 17:40:42 +0200

    samples: sensor: add amg88xx sample
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: 7817622b4b9abc6265a631d8a2718f9ad3df1275

---
+++ samples/sensor/amg88xx/src/main.c
@@ -0,0 +1,4 @@
+DECL|main|function|void main(void)
+DECL|print_buffer|function|void print_buffer(void *ptr, size_t l)
+DECL|temp_value|variable|temp_value
+DECL|trigger_handler|function|static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
