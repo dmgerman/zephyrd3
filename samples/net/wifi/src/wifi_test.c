b97d7aa58e414737a8541e58a1c07d5167044316
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-27 15:22:40 +0200

    samples/net: Add a simple sample to run wifi shell module
    Enabling relevant hardware, currently only winc1500 on
    quark_se_c1000_devboard. But it could be easily extended to other
    drivers/boards.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 554999d32039d62e5555402d907d40df3d272d36

---
+++ samples/net/wifi/src/wifi_test.c
@@ -0,0 +1,3 @@
+DECL|main|function|void main(void)
+DECL|winc1500_configure_gpios|function|struct winc1500_gpio_configuration *winc1500_configure_gpios(void)
+DECL|winc1500_gpios|variable|winc1500_gpios
