53ca3c364079059294adde70d9372411f801b2f6
Andrew Boie <andrew.p.boie@intel.com>
2017-09-29 03:33:06 -0700

    kernel: convert mutex APIs to system calls
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 2f7519bfd2bf805e37c7ee92343bd1fd05b3e4fb

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -1,9 +1,12 @@
 DECL|RECORD_CONFLICT|macro|RECORD_CONFLICT
 DECL|RECORD_STATE_CHANGE|macro|RECORD_STATE_CHANGE
+DECL|_handler_k_mutex_init|function|u32_t _handler_k_mutex_init(u32_t mutex, u32_t arg2, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_k_mutex_lock|function|u32_t _handler_k_mutex_lock(u32_t mutex, u32_t timeout, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_k_mutex_unlock|function|u32_t _handler_k_mutex_unlock(u32_t mutex, u32_t arg2, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_impl_k_mutex_init|function|void _impl_k_mutex_init(struct k_mutex *mutex)
+DECL|_impl_k_mutex_lock|function|int _impl_k_mutex_lock(struct k_mutex *mutex, s32_t timeout)
+DECL|_impl_k_mutex_unlock|function|void _impl_k_mutex_unlock(struct k_mutex *mutex)
 DECL|_trace_list_k_mutex|variable|_trace_list_k_mutex
 DECL|adjust_owner_prio|function|static void adjust_owner_prio(struct k_mutex *mutex, int new_prio)
 DECL|init_mutex_module|function|static int init_mutex_module(struct device *dev)
-DECL|k_mutex_init|function|void k_mutex_init(struct k_mutex *mutex)
-DECL|k_mutex_lock|function|int k_mutex_lock(struct k_mutex *mutex, s32_t timeout)
-DECL|k_mutex_unlock|function|void k_mutex_unlock(struct k_mutex *mutex)
 DECL|new_prio_for_inheritance|function|static int new_prio_for_inheritance(int target, int limit)
