60836786a5b1de64b822e1e6a7b0256ebf96e4c1
Andrew Boie <andrew.p.boie@intel.com>
2017-09-29 04:41:15 -0700

    kernel: convert alert APIs to system calls
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 310e987dd55cf7025e49fe4d620fc6af302b96a6

--- kernel/alert.c
+++ kernel/alert.c
@@ -1,6 +1,8 @@
 DECL|_alert_deliver|function|void _alert_deliver(struct k_work *work)
+DECL|_handler_k_alert_recv|function|u32_t _handler_k_alert_recv(u32_t alert, u32_t timeout, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_k_alert_send|function|u32_t _handler_k_alert_send(u32_t alert, u32_t arg2, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_impl_k_alert_recv|function|int _impl_k_alert_recv(struct k_alert *alert, s32_t timeout)
+DECL|_impl_k_alert_send|function|void _impl_k_alert_send(struct k_alert *alert)
 DECL|_trace_list_k_alert|variable|_trace_list_k_alert
 DECL|init_alert_module|function|static int init_alert_module(struct device *dev)
 DECL|k_alert_init|function|void k_alert_init(struct k_alert *alert, k_alert_handler_t handler, unsigned int max_num_pending_alerts)
-DECL|k_alert_recv|function|int k_alert_recv(struct k_alert *alert, s32_t timeout)
-DECL|k_alert_send|function|void k_alert_send(struct k_alert *alert)
