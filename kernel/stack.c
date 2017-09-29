cd6cadb8c08ecfb84e307657042bdafd0965f6d0
Andrew Boie <andrew.p.boie@intel.com>
2017-09-29 16:42:07 -0700

    kernel: convert stack APIs to system calls
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: e8734463a67f29a3ac259ea301422062185f25d0

--- kernel/stack.c
+++ kernel/stack.c
@@ -1,5 +1,8 @@
+DECL|_handler_k_stack_init|function|u32_t _handler_k_stack_init(u32_t stack, u32_t buffer, u32_t num_entries_p, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_k_stack_pop|function|u32_t _handler_k_stack_pop(u32_t stack, u32_t data, u32_t timeout, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_k_stack_push|function|u32_t _handler_k_stack_push(u32_t stack_p, u32_t data, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_impl_k_stack_init|function|void _impl_k_stack_init(struct k_stack *stack, u32_t *buffer, int num_entries)
+DECL|_impl_k_stack_pop|function|int _impl_k_stack_pop(struct k_stack *stack, u32_t *data, s32_t timeout)
+DECL|_impl_k_stack_push|function|void _impl_k_stack_push(struct k_stack *stack, u32_t data)
 DECL|_trace_list_k_stack|variable|_trace_list_k_stack
 DECL|init_stack_module|function|static int init_stack_module(struct device *dev)
-DECL|k_stack_init|function|void k_stack_init(struct k_stack *stack, u32_t *buffer, int num_entries)
-DECL|k_stack_pop|function|int k_stack_pop(struct k_stack *stack, u32_t *data, s32_t timeout)
-DECL|k_stack_push|function|void k_stack_push(struct k_stack *stack, u32_t data)
