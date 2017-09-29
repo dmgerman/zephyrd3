351c281512f099a44da36b73e5f435859852063b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-09-29 09:33:25 +0200

    drivers/ieee802154: Switch CC2520 to new SPI API
    Finally switching to new SPI API. It makes code simpler and performs
    better.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: d4065ae73e8d2dcbeae640223bc15f906ee610a8

--- drivers/ieee802154/ieee802154_cc2520.c
+++ drivers/ieee802154/ieee802154_cc2520.c
@@ -3,6 +3,7 @@ DECL|CC2520_FCS_LENGTH|macro|CC2520_FCS_LENGTH
 DECL|CC2520_TX_THRESHOLD|macro|CC2520_TX_THRESHOLD
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|_cc2520_access|function|bool _cc2520_access(struct cc2520_context *ctx, bool read, u8_t ins, u16_t addr, void *data, size_t length)
 DECL|_cc2520_crypto_ccm|function|static int _cc2520_crypto_ccm(struct cipher_ctx *ctx, struct cipher_aead_pkt *apkt, u8_t *ccm_nonce)
 DECL|_cc2520_crypto_uccm|function|static int _cc2520_crypto_uccm(struct cipher_ctx *ctx, struct cipher_aead_pkt *apkt, u8_t *ccm_nonce)
 DECL|_cc2520_print_errors|function|static inline void _cc2520_print_errors(struct cc2520_context *cc2520)
@@ -11,14 +12,12 @@ DECL|_cc2520_print_exceptions|function|static inline void _cc2520_print_exceptio
 DECL|_cc2520_print_exceptions|macro|_cc2520_print_exceptions
 DECL|_cc2520_print_gpio_config|function|static inline void _cc2520_print_gpio_config(struct device *dev)
 DECL|_cc2520_print_gpio_config|macro|_cc2520_print_gpio_config
-DECL|_cc2520_read_ram|function|static bool _cc2520_read_ram(struct cc2520_spi *spi, u16_t addr, u8_t *data_buf, u8_t len)
-DECL|_cc2520_read_reg|function|u8_t _cc2520_read_reg(struct cc2520_spi *spi, bool freg, u8_t addr)
+DECL|_cc2520_read_ram|function|static inline bool _cc2520_read_ram(struct cc2520_context *ctx, u16_t addr, u8_t *data_buf, u8_t len)
 DECL|_cc2520_set_ieee_addr|function|static int _cc2520_set_ieee_addr(struct device *dev, const u8_t *ieee_addr)
 DECL|_cc2520_set_pan_id|function|static int _cc2520_set_pan_id(struct device *dev, u16_t pan_id)
 DECL|_cc2520_set_short_addr|function|static int _cc2520_set_short_addr(struct device *dev, u16_t short_addr)
-DECL|_cc2520_status|function|static u8_t _cc2520_status(struct cc2520_spi *spi)
-DECL|_cc2520_write_ram|function|bool _cc2520_write_ram(struct cc2520_spi *spi, u16_t addr, u8_t *data_buf, u8_t len)
-DECL|_cc2520_write_reg|function|bool _cc2520_write_reg(struct cc2520_spi *spi, bool freg, u8_t addr, u8_t value)
+DECL|_cc2520_status|function|static inline u8_t _cc2520_status(struct cc2520_context *ctx)
+DECL|_cc2520_write_ram|function|static inline bool _cc2520_write_ram(struct cc2520_context *ctx, u16_t addr, u8_t *data_buf, u8_t len)
 DECL|_usleep|macro|_usleep
 DECL|cc2520_cca|function|static int cc2520_cca(struct device *dev)
 DECL|cc2520_context_data|variable|cc2520_context_data
@@ -39,6 +38,7 @@ DECL|cc2520_start|function|static int cc2520_start(struct device *dev)
 DECL|cc2520_stop|function|static int cc2520_stop(struct device *dev)
 DECL|cc2520_tx|function|static int cc2520_tx(struct device *dev, struct net_pkt *pkt, struct net_buf *frag)
 DECL|configure_spi|function|static inline int configure_spi(struct device *dev)
+DECL|cs_ctrl|variable|cs_ctrl
 DECL|enable_fifop_interrupt|function|static void enable_fifop_interrupt(struct cc2520_context *cc2520, bool enable)
 DECL|enable_sfd_interrupt|function|static void enable_sfd_interrupt(struct cc2520_context *cc2520, bool enable)
 DECL|fifop_int_handler|function|static inline void fifop_int_handler(struct device *port, struct gpio_callback *cb, u32_t pins)
@@ -50,19 +50,18 @@ DECL|get_fifo|function|static inline u32_t get_fifo(struct cc2520_context *cc252
 DECL|get_mac|function|static inline u8_t *get_mac(struct device *dev)
 DECL|insert_crypto_parameters|function|static int insert_crypto_parameters(struct cipher_ctx *ctx, struct cipher_aead_pkt *apkt, u8_t *ccm_nonce, u8_t *auth_crypt)
 DECL|insert_radio_noise_details|function|static inline void insert_radio_noise_details(struct net_pkt *pkt, u8_t *buf)
-DECL|instruct_ccm|function|static inline bool instruct_ccm(struct cc2520_context *cc2520,u8_t key_addr, u8_t auth_crypt, u8_t nonce_addr, u16_t input_addr,
-DECL|instruct_uccm|function|static inline bool instruct_uccm(struct cc2520_context *cc2520, u8_t key_addr, u8_t auth_crypt, u8_t nonce_addr, u16_t input_addr,
+DECL|instruct_uccm_ccm|function|static inline bool instruct_uccm_ccm(struct cc2520_context *cc2520, bool uccm, u8_t key_addr, u8_t auth_crypt, u8_t nonce_addr,
 DECL|power_on_and_setup|function|static int power_on_and_setup(struct device *dev)
-DECL|read_rxfifo_content|function|static inline bool read_rxfifo_content(struct cc2520_spi *spi, struct net_buf *frag, u8_t len)
-DECL|read_rxfifo_length|function|static inline u8_t read_rxfifo_length(struct cc2520_spi *spi)
+DECL|read_rxfifo_content|function|static inline bool read_rxfifo_content(struct cc2520_context *ctx, struct net_buf *frag, u8_t len)
+DECL|read_rxfifo_length|function|static inline u8_t read_rxfifo_length(struct cc2520_context *ctx)
 DECL|set_reset|function|static inline void set_reset(struct device *dev, u32_t value)
 DECL|set_vreg_en|function|static inline void set_vreg_en(struct device *dev, u32_t value)
 DECL|setup_gpio_callbacks|function|static inline void setup_gpio_callbacks(struct device *dev)
 DECL|sfd_int_handler|function|static inline void sfd_int_handler(struct device *port, struct gpio_callback *cb, u32_t pins)
-DECL|verify_crc|function|static inline bool verify_crc(struct cc2520_context *cc2520, struct net_pkt *pkt)
+DECL|verify_crc|function|static inline bool verify_crc(struct cc2520_context *ctx, struct net_pkt *pkt)
 DECL|verify_osc_stabilization|function|static bool verify_osc_stabilization(struct cc2520_context *cc2520)
-DECL|verify_rxfifo_validity|function|static inline bool verify_rxfifo_validity(struct cc2520_spi *spi, u8_t pkt_len)
+DECL|verify_rxfifo_validity|function|static inline bool verify_rxfifo_validity(struct cc2520_context *ctx, u8_t pkt_len)
 DECL|verify_tx_done|function|static inline bool verify_tx_done(struct cc2520_context *cc2520)
 DECL|verify_txfifo_status|function|static inline bool verify_txfifo_status(struct cc2520_context *cc2520,u8_t len)
-DECL|write_txfifo_content|function|static inline bool write_txfifo_content(struct cc2520_spi *spi,u8_t *frame, u8_t len)
-DECL|write_txfifo_length|function|static inline bool write_txfifo_length(struct cc2520_spi *spi, u8_t len)
+DECL|write_txfifo_content|function|static inline bool write_txfifo_content(struct cc2520_context *ctx,u8_t *frame, u8_t len)
+DECL|write_txfifo_length|function|static inline bool write_txfifo_length(struct cc2520_context *ctx, u8_t len)
--- drivers/ieee802154/ieee802154_cc2520.h
+++ drivers/ieee802154/ieee802154_cc2520.h
@@ -1,21 +1,16 @@
 DECL|DEFINE_FREG_READ|macro|DEFINE_FREG_READ
 DECL|DEFINE_FREG_WRITE|macro|DEFINE_FREG_WRITE
 DECL|DEFINE_MEM_WRITE|macro|DEFINE_MEM_WRITE
-DECL|DEFINE_REG_READ|macro|DEFINE_REG_READ
-DECL|DEFINE_REG_WRITE|macro|DEFINE_REG_WRITE
 DECL|DEFINE_SREG_READ|macro|DEFINE_SREG_READ
 DECL|DEFINE_SREG_WRITE|macro|DEFINE_SREG_WRITE
 DECL|DEFINE_STROBE_INSTRUCTION|macro|DEFINE_STROBE_INSTRUCTION
 DECL|DEFINE_STROBE_SNOP_INSTRUCTION|macro|DEFINE_STROBE_SNOP_INSTRUCTION
 DECL|__IEEE802154_CC2520_H__|macro|__IEEE802154_CC2520_H__
-DECL|_cc2520_command_strobe_snop|function|static inline bool _cc2520_command_strobe_snop(struct cc2520_spi *spi, u8_t instruction)
-DECL|_cc2520_command_strobe|function|static inline bool _cc2520_command_strobe(struct cc2520_spi *spi, u8_t instruction)
+DECL|_cc2520_command_strobe_snop|function|static inline bool _cc2520_command_strobe_snop(struct cc2520_context *ctx, u8_t instruction)
+DECL|_cc2520_command_strobe|function|static inline bool _cc2520_command_strobe(struct cc2520_context *ctx, u8_t instruction)
 DECL|access_lock|member|struct k_sem access_lock;
 DECL|cc2520_context|struct|struct cc2520_context {
 DECL|cc2520_rx_thread|member|struct k_thread cc2520_rx_thread;
-DECL|cc2520_spi|struct|struct cc2520_spi {
-DECL|cmd_buf|member|u8_t cmd_buf[12];
-DECL|dev|member|struct device *dev;
 DECL|fifop_cb|member|struct gpio_callback fifop_cb;
 DECL|gpios|member|struct cc2520_gpio_configuration *gpios;
 DECL|iface|member|struct net_if *iface;
@@ -23,7 +18,7 @@ DECL|mac_addr|member|u8_t mac_addr[8];
 DECL|overflow|member|bool overflow;
 DECL|rx_lock|member|struct k_sem rx_lock;
 DECL|sfd_cb|member|struct gpio_callback sfd_cb;
-DECL|slave|member|u32_t slave;
-DECL|spi|member|struct cc2520_spi spi;
+DECL|spi_cfg|member|struct spi_config spi_cfg;
+DECL|spi|member|struct device *spi;
 DECL|tx_sync|member|struct k_sem tx_sync;
 DECL|tx|member|atomic_t tx;
