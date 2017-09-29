7d5bad2fd10a5720bad608c8a96f68ecf8810d31
Andrew Boie <andrew.p.boie@intel.com>
2017-09-29 02:56:55 -0700

    sensors: implement system calls
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 6d79fe54da2eed43e0f1989378dc80fc1db5c24d

--- include/sensor.h
+++ include/sensor.h
@@ -43,24 +43,24 @@ DECL|SENSOR_TRIG_TAP|enumerator|SENSOR_TRIG_TAP,
 DECL|SENSOR_TRIG_THRESHOLD|enumerator|SENSOR_TRIG_THRESHOLD,
 DECL|SENSOR_TRIG_TIMER|enumerator|SENSOR_TRIG_TIMER,
 DECL|__SENSOR_H__|macro|__SENSOR_H__
+DECL|_impl_sensor_attr_set|function|static inline int _impl_sensor_attr_set(struct device *dev,enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|_impl_sensor_channel_get|function|static inline int _impl_sensor_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|_impl_sensor_sample_fetch_chan|function|static inline int _impl_sensor_sample_fetch_chan(struct device *dev, enum sensor_channel type)
+DECL|_impl_sensor_sample_fetch|function|static inline int _impl_sensor_sample_fetch(struct device *dev)
 DECL|attr_set|member|sensor_attr_set_t attr_set;
 DECL|channel_get|member|sensor_channel_get_t channel_get;
 DECL|chan|member|enum sensor_channel chan;
 DECL|sample_fetch|member|sensor_sample_fetch_t sample_fetch;
 DECL|sensor_attr_set_t|typedef|typedef int (*sensor_attr_set_t)(struct device *dev,
-DECL|sensor_attr_set|function|static inline int sensor_attr_set(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
 DECL|sensor_attribute|enum|enum sensor_attribute {
 DECL|sensor_channel_get_t|typedef|typedef int (*sensor_channel_get_t)(struct device *dev,
-DECL|sensor_channel_get|function|static inline int sensor_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
 DECL|sensor_channel|enum|enum sensor_channel {
 DECL|sensor_degrees_to_rad|function|static inline void sensor_degrees_to_rad(s32_t d, struct sensor_value *rad)
 DECL|sensor_driver_api|struct|struct sensor_driver_api {
 DECL|sensor_g_to_ms2|function|static inline void sensor_g_to_ms2(s32_t g, struct sensor_value *ms2)
 DECL|sensor_ms2_to_g|function|static inline s32_t sensor_ms2_to_g(const struct sensor_value *ms2)
 DECL|sensor_rad_to_degrees|function|static inline s32_t sensor_rad_to_degrees(const struct sensor_value *rad)
-DECL|sensor_sample_fetch_chan|function|static inline int sensor_sample_fetch_chan(struct device *dev, enum sensor_channel type)
 DECL|sensor_sample_fetch_t|typedef|typedef int (*sensor_sample_fetch_t)(struct device *dev,
-DECL|sensor_sample_fetch|function|static inline int sensor_sample_fetch(struct device *dev)
 DECL|sensor_trigger_handler_t|typedef|typedef void (*sensor_trigger_handler_t)(struct device *dev,
 DECL|sensor_trigger_set_t|typedef|typedef int (*sensor_trigger_set_t)(struct device *dev,
 DECL|sensor_trigger_set|function|static inline int sensor_trigger_set(struct device *dev, struct sensor_trigger *trig, sensor_trigger_handler_t handler)
---
+++ subsys/driver_handlers/sensor.c
@@ -0,0 +1,4 @@
+DECL|_handler_sensor_attr_set|function|u32_t _handler_sensor_attr_set(u32_t dev, u32_t chan, u32_t attr, u32_t val, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_sensor_channel_get|function|u32_t _handler_sensor_channel_get(u32_t dev, u32_t chan, u32_t val, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_sensor_sample_fetch_chan|function|u32_t _handler_sensor_sample_fetch_chan(u32_t dev, u32_t type, u32_t arg3,u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
+DECL|_handler_sensor_sample_fetch|function|u32_t _handler_sensor_sample_fetch(u32_t dev, u32_t arg2, u32_t arg3, u32_t arg4, u32_t arg5, u32_t arg6, void *ssf)
