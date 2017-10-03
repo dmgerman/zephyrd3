6cc4bf3776fbd6b67b51568003340c2ca63cf60f
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-10-03 10:03:37 +0200

    net/ieee802154: Use lqi from net_pkt relevantly
    This only affects beacon handling logic, not really used feature for
    now.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: aedece5240ee5b195b35cc90d67c4c4979f540a2

--- subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
+++ subsys/net/ip/l2/ieee802154/ieee802154_mgmt.c
@@ -5,7 +5,7 @@ DECL|ieee802154_cancel_scan|function|static int ieee802154_cancel_scan(u32_t mgm
 DECL|ieee802154_disassociate|function|static int ieee802154_disassociate(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|ieee802154_get_parameters|function|static int ieee802154_get_parameters(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|ieee802154_get_security_settings|function|static int ieee802154_get_security_settings(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
-DECL|ieee802154_handle_beacon|function|enum net_verdict ieee802154_handle_beacon(struct net_if *iface, struct ieee802154_mpdu *mpdu)
+DECL|ieee802154_handle_beacon|function|enum net_verdict ieee802154_handle_beacon(struct net_if *iface, struct ieee802154_mpdu *mpdu, u8_t lqi)
 DECL|ieee802154_handle_mac_command|function|enum net_verdict ieee802154_handle_mac_command(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_scan|function|static int ieee802154_scan(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|ieee802154_set_ack|function|static int ieee802154_set_ack(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
