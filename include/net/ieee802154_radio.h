c6eaeda29fa61ac1a33b9a068a02add87e925c6d
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-10-03 10:09:58 +0200

    net/ieee802154: Remove get_lqi from radio API
    Now that lqi and rssi are embedded into net_pkt, there is no need for
    that function.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 7be649ea6fa46b2418b10ce1734ef4389c23439c

--- include/net/ieee802154_radio.h
+++ include/net/ieee802154_radio.h
@@ -1,7 +1,6 @@
 DECL|__IEEE802154_RADIO_H__|macro|__IEEE802154_RADIO_H__
 DECL|__packed|variable|__packed
 DECL|cca|member|int (*cca)(struct device *dev);
-DECL|get_lqi|member|u8_t (*get_lqi)(struct device *dev);
 DECL|ieee802154_radio_api|struct|struct ieee802154_radio_api {
 DECL|iface_api|member|struct net_if_api iface_api;
 DECL|set_channel|member|int (*set_channel)(struct device *dev, u16_t channel);
