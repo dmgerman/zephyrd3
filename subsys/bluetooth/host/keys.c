31bcc54c6f5c911ae29bcdd438a491394f507481
Johan Hedberg <johan.hedberg@intel.com>
2017-10-03 14:57:03 +0300

    Bluetooth: Add helper to iterate all key objects
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 3993814e94a763624ea28974265fee34f70e8f0a

--- subsys/bluetooth/host/keys.c
+++ subsys/bluetooth/host/keys.c
@@ -5,6 +5,7 @@ DECL|bt_keys_clear|function|void bt_keys_clear(struct bt_keys *keys)
 DECL|bt_keys_find_addr|function|struct bt_keys *bt_keys_find_addr(const bt_addr_le_t *addr)
 DECL|bt_keys_find_irk|function|struct bt_keys *bt_keys_find_irk(const bt_addr_le_t *addr)
 DECL|bt_keys_find|function|struct bt_keys *bt_keys_find(int type, const bt_addr_le_t *addr)
+DECL|bt_keys_foreach|function|void bt_keys_foreach(int type, void (*func)(struct bt_keys *keys))
 DECL|bt_keys_get_addr|function|struct bt_keys *bt_keys_get_addr(const bt_addr_le_t *addr)
 DECL|bt_keys_get_type|function|struct bt_keys *bt_keys_get_type(int type, const bt_addr_le_t *addr)
 DECL|key_pool|variable|key_pool
