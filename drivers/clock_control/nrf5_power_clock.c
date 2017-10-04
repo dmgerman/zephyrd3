ff649a672a63b7f7b59d7fa40e413272cc856fab
Thiago Silveira <thiago@exati.com.br>
2017-10-04 16:04:40 -0300

    clock_control: nrf5_power: Let the LF clock be configured at least once
    If the LF clock was already started, but wasn't initialized with
    _k32src_start yet, allow it to run once. This is needed because if a
    soft reset is triggered while watchdog is active, the LF clock will
    already be running, but won't be configured yet (watchdog forces LF
    clock to be running).
    
    If the LF clock isn't configured with _k32src_start, the nRF5 RTC
    won't count and the whole system will malfunction, as the kernel
    depends on the clock source being working properly.
    
    Signed-off-by: Thiago Silveira <thiago@exati.com.br>
    
    Former-commit-id: 1d8c8ac3afad102248a448bd2e17f16af55a4ba2

--- drivers/clock_control/nrf5_power_clock.c
+++ drivers/clock_control/nrf5_power_clock.c
@@ -5,5 +5,6 @@ DECL|_m16src_clock_control_api|variable|_m16src_clock_control_api
 DECL|_m16src_start|function|static int _m16src_start(struct device *dev, clock_control_subsys_t sub_system)
 DECL|_m16src_stop|function|static int _m16src_stop(struct device *dev, clock_control_subsys_t sub_system)
 DECL|_power_clock_isr|function|static void _power_clock_isr(void *arg)
+DECL|k32src_initialized|variable|k32src_initialized
 DECL|m16src_grd|variable|m16src_grd
 DECL|m16src_ref|variable|m16src_ref
