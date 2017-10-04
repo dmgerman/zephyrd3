bcf7bb8cae40f013ad42c0a3fb5764ae62159620
Andrew Boie <andrew.p.boie@intel.com>
2017-10-04 14:29:51 -0700

    ztest: add user thread support
    We add macros to define test cases that should be run with the CPU
    in user mode, if the CPU supports it.
    
    ztest_test_suite() declarations are now static as they can't go on
    the main thread stack; the data gets shared between multiple threads.
    It's better here anyway as a large test suite could fill up the main
    stack, which is by default reduced to 512 bytes.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: b2bce215723c6ee3baacf5a562c6297db39744ab

--- tests/ztest/include/ztest_test.h
+++ tests/ztest/include/ztest_test.h
@@ -3,9 +3,12 @@ DECL|name|member|const char *name;
 DECL|setup|member|void (*setup)(void);
 DECL|teardown|member|void (*teardown)(void);
 DECL|test|member|void (*test)(void);
+DECL|thread_options|member|u32_t thread_options;
 DECL|unit_test_noop|function|static inline void unit_test_noop(void)
 DECL|unit_test|struct|struct unit_test {
 DECL|ztest_run_test_suite|macro|ztest_run_test_suite
 DECL|ztest_test_suite|macro|ztest_test_suite
 DECL|ztest_unit_test_setup_teardown|macro|ztest_unit_test_setup_teardown
 DECL|ztest_unit_test|macro|ztest_unit_test
+DECL|ztest_user_unit_test_setup_teardown|macro|ztest_user_unit_test_setup_teardown
+DECL|ztest_user_unit_test|macro|ztest_user_unit_test
