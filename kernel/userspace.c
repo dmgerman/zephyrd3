f12e729fc5ae49e0f6f961ca86ef918f9620f7b9
Andrew Boie <andrew.p.boie@intel.com>
2017-10-05 12:21:36 -0700

    userspace: add _k_object_uninit()
    API to assist with re-using objects, such as terminated threads or
    kernel objects returned to a pool.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 4a9a4240c6a381c5764ea0ca5fdffd14b6c0bf6b

--- kernel/userspace.c
+++ kernel/userspace.c
@@ -5,6 +5,7 @@ DECL|_impl_k_object_access_all_grant|function|void _impl_k_object_access_all_gra
 DECL|_impl_k_object_access_grant|function|void _impl_k_object_access_grant(void *object, struct k_thread *thread)
 DECL|_impl_k_object_access_revoke|function|void _impl_k_object_access_revoke(void *object, struct k_thread *thread)
 DECL|_k_object_init|function|void _k_object_init(void *object)
+DECL|_k_object_uninit|function|void _k_object_uninit(void *object)
 DECL|_k_object_validate|function|int _k_object_validate(struct _k_object *ko, enum k_objects otype, int init)
 DECL|_thread_perms_all_set|function|void _thread_perms_all_set(struct _k_object *ko)
 DECL|_thread_perms_clear|function|void _thread_perms_clear(struct _k_object *ko, struct k_thread *thread)
