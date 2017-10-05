c553f39f072b8adedccef9b021aa1e3ef81aa5a0
Gil Pitney <gil.pitney@linaro.org>
2017-10-05 16:57:02 -0700

    ext: simplelink: Add SimpleLink DPL porting layer to Zephyr
    This ports the SimpleLink WiFi host driver via its OS adaptation
    layer to Zephyr OS primitives.
    
    This was validated using an out-of-tree SimpleLink shell
    application including functions for:
    * WLAN connect, disconnect and scan
    * Socket: UDP server and client
    
    Signed-off-by: Gil Pitney <gil.pitney@linaro.org>
    
    Former-commit-id: d3b7e80ab5f40039fcc4bf1ab8fa5c4aba915ec3

---
+++ ext/hal/ti/simplelink/kernel/zephyr/dpl/HwiP_zephyr.c
@@ -0,0 +1,20 @@
+DECL|EXCEPTION_LSPI|macro|EXCEPTION_LSPI
+DECL|EXCEPTION_NWPIC|macro|EXCEPTION_NWPIC
+DECL|EXCEPTION_UDMAERR|macro|EXCEPTION_UDMAERR
+DECL|EXCEPTION_UDMA|macro|EXCEPTION_UDMA
+DECL|HwiP_Params_init|function|void HwiP_Params_init(HwiP_Params *params)
+DECL|HwiP_clearInterrupt|function|void HwiP_clearInterrupt(int interruptNum)
+DECL|HwiP_create|function|HwiP_Handle HwiP_create(int interruptNum, HwiP_Fxn hwiFxn, HwiP_Params *params)
+DECL|HwiP_delete|function|void HwiP_delete(HwiP_Handle handle)
+DECL|HwiP_disableInterrupt|function|void HwiP_disableInterrupt(int interruptNum)
+DECL|HwiP_disable|function|uintptr_t HwiP_disable(void)
+DECL|HwiP_enableInterrupt|function|void HwiP_enableInterrupt(int interruptNum)
+DECL|HwiP_restore|function|void HwiP_restore(uintptr_t key)
+DECL|arg|member|uintptr_t arg;
+DECL|cb|member|HwiP_Fxn cb;
+DECL|sl_LSPI_cb|variable|sl_LSPI_cb
+DECL|sl_NWPIC_cb|variable|sl_NWPIC_cb
+DECL|sl_UDMAERR_cb|variable|sl_UDMAERR_cb
+DECL|sl_UDMA_cb|variable|sl_UDMA_cb
+DECL|sl_isr_args|struct|struct sl_isr_args
+DECL|sl_isr|function|static void sl_isr(void *isr_arg)
---
+++ ext/hal/ti/simplelink/kernel/zephyr/dpl/SemaphoreP_zephyr.c
@@ -0,0 +1,10 @@
+DECL|DPL_MAX_SEMAPHORES|macro|DPL_MAX_SEMAPHORES
+DECL|SemaphoreP_Params_init|function|void SemaphoreP_Params_init(SemaphoreP_Params *params)
+DECL|SemaphoreP_createBinary|function|SemaphoreP_Handle SemaphoreP_createBinary(unsigned int count)
+DECL|SemaphoreP_create|function|SemaphoreP_Handle SemaphoreP_create(unsigned int count, SemaphoreP_Params *params)
+DECL|SemaphoreP_delete|function|void SemaphoreP_delete(SemaphoreP_Handle handle)
+DECL|SemaphoreP_pend|function|SemaphoreP_Status SemaphoreP_pend(SemaphoreP_Handle handle, uint32_t timeout)
+DECL|SemaphoreP_post|function|void SemaphoreP_post(SemaphoreP_Handle handle)
+DECL|dpl_convert_timeout|function|static int32_t dpl_convert_timeout(uint32_t timeout)
+DECL|dpl_sem_pool_alloc|function|static struct k_sem *dpl_sem_pool_alloc()
+DECL|dpl_sem_pool_free|function|static SemaphoreP_Status dpl_sem_pool_free(struct k_sem *sem)
