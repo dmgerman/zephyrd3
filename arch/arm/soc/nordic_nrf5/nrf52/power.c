af1862aca59b31c0ba49d4f0313d080e27223bff
Youvedeep Singh <youvedeep.singh@intel.com>
2017-10-05 17:57:38 +0530

    ARM: nrf52: Power Management for nrf52 series SOC
    Add support for nrf52 series SOC. This patch Adds :-
    1. Architecture specific Power Management APIs.
    2. APIs for invoking various Power Management tasks into nrf52.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: fc78ddf7d3a20d99669e49c0aba9ea1c9b8c702f

---
+++ arch/arm/soc/nordic_nrf5/nrf52/power.c
@@ -0,0 +1,6 @@
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|_issue_low_power_command|function|static void _issue_low_power_command(void)
+DECL|_low_power_mode|function|static void _low_power_mode(enum power_states state)
+DECL|_sys_soc_power_state_post_ops|function|void _sys_soc_power_state_post_ops(enum power_states state)
+DECL|_sys_soc_set_power_state|function|void _sys_soc_set_power_state(enum power_states state)
+DECL|_system_off|function|static void _system_off(void)
