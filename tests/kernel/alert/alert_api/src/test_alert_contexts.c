621d2d5444dd0ef8608701923ade7d8cb62d1437
Andrew Boie <andrew.p.boie@intel.com>
2017-10-07 14:33:29 -0700

    tests: alert_api: run in user mode
    main.c and test_alert_contexts.c merged.
    
    User threads can't look inside the alert structures, so an extra
    variable 'htype' introduced to track expectations for any given
    alert object in alert_recv().
    
    Alert objects have to be initialized by supervisor threads since
    they register callbacks. An array of toplevel alert objects created
    and initialized in test_main(), replacing the ones that used to
    live on thread stacks.
    
    Added userspace tag to testcase.yaml
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 39d55ac6a97eaf661e26ad7f6d5ea4c3dab3d2fd

--- tests/kernel/alert/alert_api/src/test_alert_contexts.c
+++ tests/kernel/alert/alert_api/src/test_alert_contexts.c
@@ -1,3 +1,7 @@
+DECL|HANDLER_0|enumerator|HANDLER_0,
+DECL|HANDLER_1|enumerator|HANDLER_1
+DECL|HANDLER_DEFAULT|enumerator|HANDLER_DEFAULT,
+DECL|HANDLER_IGNORE|enumerator|HANDLER_IGNORE,
 DECL|PENDING_MAX|macro|PENDING_MAX
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|TIMEOUT|macro|TIMEOUT
@@ -5,7 +9,9 @@ DECL|alert_handler0|function|static int alert_handler0(struct k_alert *alt)
 DECL|alert_handler1|function|static int alert_handler1(struct k_alert *alt)
 DECL|alert_recv|function|static void alert_recv(void)
 DECL|alert_send|function|static void alert_send(void)
+DECL|handle_type|enum|enum handle_type {
 DECL|handler_executed|variable|handler_executed
+DECL|htype|variable|htype
 DECL|isr_alert|function|static void isr_alert(void)
 DECL|palert|variable|palert
 DECL|tdata|variable|tdata
@@ -14,11 +20,13 @@ DECL|test_isr_alert_default|function|void test_isr_alert_default(void)
 DECL|test_isr_alert_ignore|function|void test_isr_alert_ignore(void)
 DECL|test_isr_alert_pending|function|void test_isr_alert_pending(void)
 DECL|test_isr_kinit_alert|function|void test_isr_kinit_alert(void)
+DECL|test_main|function|void test_main(void)
 DECL|test_thread_alert_consumed|function|void test_thread_alert_consumed(void)
 DECL|test_thread_alert_default|function|void test_thread_alert_default(void)
 DECL|test_thread_alert_ignore|function|void test_thread_alert_ignore(void)
 DECL|test_thread_alert_pending|function|void test_thread_alert_pending(void)
 DECL|test_thread_kinit_alert|function|void test_thread_kinit_alert(void)
+DECL|thread_alerts|variable|thread_alerts
 DECL|thread_alert|function|static void thread_alert(void)
 DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
 DECL|tisr_entry|function|static void tisr_entry(void *p)
