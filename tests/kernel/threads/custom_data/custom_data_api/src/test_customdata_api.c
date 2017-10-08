8843fefe3e5c06ce59932795f8f24daa30cb9afe
Andrew Boie <andrew.p.boie@intel.com>
2017-10-08 12:52:28 -0700

    tests: custom_data: run in user mode
    main.c and test_customdata_api.c merged.
    
    Preemptive priority case now run in user mode.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: a9557ef219df7c55270d01ccaa7ed08408b65f3c

--- tests/kernel/threads/custom_data/custom_data_api/src/test_customdata_api.c
+++ tests/kernel/threads/custom_data/custom_data_api/src/test_customdata_api.c
@@ -4,3 +4,4 @@ DECL|customdata_entry|function|static void customdata_entry(void *p1, void *p2, 
 DECL|tdata|variable|tdata
 DECL|test_customdata_get_set_coop|function|void test_customdata_get_set_coop(void)
 DECL|test_customdata_get_set_preempt|function|void test_customdata_get_set_preempt(void)
+DECL|test_main|function|void test_main(void)
