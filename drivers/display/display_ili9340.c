4f75f9a2418a0bb629526b33b5ffa2a49388bdd5
Jan Van Winkel <jan.van_winkel@dxplore.eu>
2017-10-09 21:19:12 +0200

    driver: Updated ILI9340 driver to use display API
    Updated ILI9340 display driver and sample application to make use off
    the display API
    
    Signed-off-by: Jan Van Winkel <jan.van_winkel@dxplore.eu>
    
    Former-commit-id: d332ae5b6af18ebaa480e3bf5ca349a669c3695a

--- drivers/display/display_ili9340.c
+++ drivers/display/display_ili9340.c
@@ -4,16 +4,23 @@ DECL|LOG_LEVEL|macro|LOG_LEVEL
 DECL|command_data_gpio|member|struct device *command_data_gpio;
 DECL|cs_ctrl|member|struct spi_cs_control cs_ctrl;
 DECL|display|variable|display
+DECL|ili9340_api|variable|ili9340_api
 DECL|ili9340_data|struct|struct ili9340_data {
 DECL|ili9340_data|variable|ili9340_data
-DECL|ili9340_display_off|function|void ili9340_display_off(struct device *dev)
-DECL|ili9340_display_on|function|void ili9340_display_on(struct device *dev)
-DECL|ili9340_exit_sleep|function|void ili9340_exit_sleep(struct ili9340_data *data)
-DECL|ili9340_init|function|int ili9340_init(struct device *dev)
-DECL|ili9340_set_mem_area|function|void ili9340_set_mem_area(struct ili9340_data *data, const u16_t x, const u16_t y, const u16_t w, const u16_t h)
+DECL|ili9340_display_blanking_off|function|static int ili9340_display_blanking_off(const struct device *dev)
+DECL|ili9340_display_blanking_on|function|static int ili9340_display_blanking_on(const struct device *dev)
+DECL|ili9340_exit_sleep|function|static void ili9340_exit_sleep(struct ili9340_data *data)
+DECL|ili9340_get_capabilities|function|static void ili9340_get_capabilities(const struct device *dev, struct display_capabilities *capabilities)
+DECL|ili9340_get_framebuffer|function|static void *ili9340_get_framebuffer(const struct device *dev)
+DECL|ili9340_init|function|static int ili9340_init(struct device *dev)
+DECL|ili9340_read|function|static int ili9340_read(const struct device *dev, const u16_t x,const u16_t y, const struct display_buffer_descriptor *desc, void *buf)
+DECL|ili9340_set_brightness|function|static int ili9340_set_brightness(const struct device *dev, const u8_t brightness)
+DECL|ili9340_set_contrast|function|static int ili9340_set_contrast(const struct device *dev, const u8_t contrast)
+DECL|ili9340_set_mem_area|function|static void ili9340_set_mem_area(struct ili9340_data *data, const u16_t x, const u16_t y, const u16_t w, const u16_t h)
+DECL|ili9340_set_orientation|function|static int ili9340_set_orientation(const struct device *dev, const enum display_orientation orientation)
+DECL|ili9340_set_pixel_format|function|static int ili9340_set_pixel_format(const struct device *dev, const enum display_pixel_format pixel_format)
 DECL|ili9340_transmit|function|void ili9340_transmit(struct ili9340_data *data, u8_t cmd, void *tx_data, size_t tx_len)
-DECL|ili9340_write_bitmap|function|void ili9340_write_bitmap(const struct device *dev, const u16_t x, const u16_t y, const u16_t w, const u16_t h, const u8_t *rgb_data)
-DECL|ili9340_write_pixel|function|void ili9340_write_pixel(const struct device *dev, const u16_t x, const u16_t y, const u8_t r, const u8_t g, const u8_t b)
+DECL|ili9340_write|function|static int ili9340_write(const struct device *dev, const u16_t x, const u16_t y, const struct display_buffer_descriptor *desc, const void *buf)
 DECL|reset_gpio|member|struct device *reset_gpio;
 DECL|spi_config|member|struct spi_config spi_config;
 DECL|spi_dev|member|struct device *spi_dev;
--- samples/display/ili9340/src/main.c
+++ samples/display/ili9340/src/main.c
@@ -1,5 +1,3 @@
 DECL|LOG_LEVEL|macro|LOG_LEVEL
-DECL|SCREEN_HEIGHT|macro|SCREEN_HEIGHT
-DECL|SCREEN_WIDTH|macro|SCREEN_WIDTH
 DECL|main|function|void main(void)
 DECL|main|variable|main
