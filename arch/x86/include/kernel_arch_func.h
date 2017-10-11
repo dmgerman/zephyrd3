051a7f2028227346c8ab4666082996548382bdca
Gustavo Lima Chaves <gustavo.lima.chaves@intel.com>
2017-10-11 14:17:12 -0700

    jailhouse: add support for x2APIC mode for all LOAPIC accesses
    Besides the fact that we did not have that for the current supported
    boards, that makes sense for this new, virtualized mode, that is meant
    to be run on top of full-fledged x86 64 CPUs.
    
    By having xAPIC mode access only, Jailhouse has to intercept those MMIO
    reads and writes, in order to examine what they do and arbitrate if it's
    safe or not (e.g. not all values are accepted to ICR register). This
    means that we can't run away from having a VM-exit event for each and
    every access to APIC memory region and this impacts the latency the
    guest OS observes over bare metal a lot.
    
    When in x2APIC mode, Jailhouse does not require VM-exits for MSR
    accesses other that writes to the ICR register, so the latency the guest
    observes is reduced to almost zero.
    
    Here are some outputs of the the command line
    
      $ sudo ./tools/jailhouse cell stats tiny-demo
    
    on a Jailhouse's root cell console, for one of the Zephyr demos using
    LOAPIC timers, left for a couple of seconds:
    
    Statistics for tiny-demo cell (x2APIC root, x2APIC inmate)
    
    COUNTER                              SUM   PER SEC
    vmexits_total                          7         0
    vmexits_management                     3         0
    vmexits_cr                             2         0
    vmexits_cpuid                          1         0
    vmexits_msr                            1         0
    vmexits_exception                      0         0
    vmexits_hypercall                      0         0
    vmexits_mmio                           0         0
    vmexits_pio                            0         0
    vmexits_xapic                          0         0
    vmexits_xsetbv                         0         0
    
    Statistics for tiny-demo cell (xAPIC root, xAPIC inmate)
    
    COUNTER                              SUM   PER SEC
    vmexits_total                       4087        40
    vmexits_xapic                       4080        40
    vmexits_management                     3         0
    vmexits_cr                             2         0
    vmexits_cpuid                          1         0
    vmexits_msr                            1         0
    vmexits_exception                      0         0
    vmexits_hypercall                      0         0
    vmexits_mmio                           0         0
    vmexits_pio                            0         0
    vmexits_xsetbv                         0         0
    
    Statistics for tiny-demo cell (xAPIC root, x2APIC inmate)
    
    COUNTER                              SUM   PER SEC
    vmexits_total                       4087        40
    vmexits_msr                         4080        40
    vmexits_management                     3         0
    vmexits_cr                             2         0
    vmexits_cpuid                          1         0
    vmexits_exception                      0         0
    vmexits_hypercall                      0         0
    vmexits_mmio                           0         0
    vmexits_pio                            0         0
    vmexits_xapic                          0         0
    vmexits_xsetbv                         0         0
    
    See that under x2APIC mode on both Jailhouse/root-cell and guest, the
    interruptions from the hypervisor are minimal. That is not the case when
    Jailhouse is on xAPIC mode, though. Note also that, as a plus, x2APIC
    accesses on the guest will map to xAPIC MMIO on the hypervisor just
    fine.
    
    Signed-off-by: Gustavo Lima Chaves <gustavo.lima.chaves@intel.com>
    
    Former-commit-id: 9bb07ff69af766e2ef751cec727355cb759dde75

--- arch/x86/include/kernel_arch_func.h
+++ arch/x86/include/kernel_arch_func.h
@@ -1,3 +1,4 @@
+DECL|MSR_X2APIC_BASE|macro|MSR_X2APIC_BASE
 DECL|STACK_ROUND_DOWN|macro|STACK_ROUND_DOWN
 DECL|STACK_ROUND_UP|macro|STACK_ROUND_UP
 DECL|_IntLibInit|function|static inline void _IntLibInit(void)
@@ -5,3 +6,5 @@ DECL|_is_in_isr|macro|_is_in_isr
 DECL|_kernel_arch_func__h_|macro|_kernel_arch_func__h_
 DECL|_set_thread_return_value|function|_set_thread_return_value(struct k_thread *thread, unsigned int value)
 DECL|kernel_arch_init|function|static inline void kernel_arch_init(void)
+DECL|read_x2apic|function|static inline u32_t read_x2apic(unsigned int reg)
+DECL|write_x2apic|function|static inline void write_x2apic(unsigned int reg, u32_t val)
