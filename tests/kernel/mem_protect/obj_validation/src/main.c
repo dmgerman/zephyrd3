ed8a0a1170fe8611b473fb72b5a376b624d7ca26
Andrew Boie <andrew.p.boie@intel.com>
2017-10-12 12:11:49 -0700

    tests: obj_validation: cleanup
    Improved test coverage to reflect current policy and converted to
    ztest.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: e5b5407ece7630fa9e792fc1279a4ec67d733d28

--- tests/kernel/mem_protect/obj_validation/src/main.c
+++ tests/kernel/mem_protect/obj_validation/src/main.c
@@ -1,7 +1,9 @@
 DECL|SEM_ARRAY_SIZE|macro|SEM_ARRAY_SIZE
 DECL|bad_sem|variable|bad_sem
-DECL|main|function|void main(void) {
+DECL|object_permission_checks|function|void object_permission_checks(struct k_sem *sem, bool skip_init)
 DECL|sem2|variable|sem2
 DECL|sem3|variable|sem3
 DECL|semarray|variable|semarray
-DECL|test_bad_object|function|static int test_bad_object(struct k_sem *sem)
+DECL|test_generic_object|function|void test_generic_object(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_object|function|static int test_object(struct k_sem *sem, int retval)
