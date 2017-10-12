11d856014a43c85581877c42dd65bfa4be870e2f
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2017-10-12 18:32:23 +0530

    xtensa: intel_s1000: implement interrupt mechanism
    intel_s1000 has multiple levels of interrupts consisting of core, CAVS
    Logic and designware interrupt controller. This patchset modifies
    the regular gen_isr mechanism to support these multiple levels.
    
    Change-Id: I0450666d4e601dfbc8cadc9c9d8100afb61a214c
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: dadf9e7a81de66e2578fe85c2d66cc3d284225f4

---
+++ arch/xtensa/soc/intel_s1000/soc.c
@@ -0,0 +1,2 @@
+DECL|_soc_irq_disable|function|void _soc_irq_disable(u32_t irq)
+DECL|_soc_irq_enable|function|void _soc_irq_enable(u32_t irq)
--- include/arch/xtensa/xtensa_irq.h
+++ include/arch/xtensa/xtensa_irq.h
@@ -1,7 +1,12 @@
 DECL|CONFIG_GEN_IRQ_START_VECTOR|macro|CONFIG_GEN_IRQ_START_VECTOR
 DECL|CONFIG_NUM_IRQS|macro|CONFIG_NUM_IRQS
+DECL|CONFIG_NUM_IRQS|macro|CONFIG_NUM_IRQS
 DECL|XTENSA_IRQ_H|macro|XTENSA_IRQ_H
-DECL|_arch_irq_disable|function|static ALWAYS_INLINE void _arch_irq_disable(u32_t irq)
-DECL|_arch_irq_enable|function|static ALWAYS_INLINE void _arch_irq_enable(u32_t irq)
+DECL|_arch_irq_disable|macro|_arch_irq_disable
+DECL|_arch_irq_disable|macro|_arch_irq_disable
+DECL|_arch_irq_enable|macro|_arch_irq_enable
+DECL|_arch_irq_enable|macro|_arch_irq_enable
 DECL|_arch_irq_lock|function|static ALWAYS_INLINE unsigned int _arch_irq_lock(void)
 DECL|_arch_irq_unlock|function|static ALWAYS_INLINE void _arch_irq_unlock(unsigned int key)
+DECL|_xtensa_irq_disable|function|static ALWAYS_INLINE void _xtensa_irq_disable(u32_t irq)
+DECL|_xtensa_irq_enable|function|static ALWAYS_INLINE void _xtensa_irq_enable(u32_t irq)
