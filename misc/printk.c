155d7116ec7fc88e1debdbb8c18abdcbb368b5e4
Andrew Boie <andrew.p.boie@intel.com>
2017-10-13 10:24:30 -0700

    printk: only do buffering in user mode
    This has extra stack overhead, no need to impose this on the caller
    unless it's needed to avoid doing a system call for every
    character.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: ef2b138130620f12d2a31b84dda01e58c1e40a9b

--- misc/printk.c
+++ misc/printk.c
@@ -11,16 +11,16 @@ DECL|_nop_char_out|function|static int _nop_char_out(int c)
 DECL|_printk_dec_ulong|function|static void _printk_dec_ulong(out_func_t out, void *ctx, const unsigned long num, enum pad_type padding, int min_width)
 DECL|_printk_hex_ulong|function|static void _printk_hex_ulong(out_func_t out, void *ctx, const unsigned long num, enum pad_type padding, int min_width)
 DECL|_vprintk|function|void _vprintk(out_func_t out, void *ctx, const char *fmt, va_list ap)
+DECL|buf_char_out|function|static int buf_char_out(int c, void *ctx_p)
 DECL|buf_count|member|unsigned int buf_count;
-DECL|buf_flush|function|static void buf_flush(struct out_context *ctx)
+DECL|buf_flush|function|static void buf_flush(struct buf_out_context *ctx)
+DECL|buf_out_context|struct|struct buf_out_context {
 DECL|buf|member|char buf[CONFIG_PRINTK_BUFFER_SIZE];
-DECL|char_out|function|static int char_out(int c, struct out_context *ctx)
-DECL|char_out|function|static int char_out(int c, struct out_context *ctx)
+DECL|char_out|function|static int char_out(int c, void *ctx_p)
 DECL|count|member|int count;
 DECL|count|member|int count;
 DECL|count|member|int count;
 DECL|max|member|int max;
-DECL|out_context|struct|struct out_context {
 DECL|out_context|struct|struct out_context {
 DECL|out_func_t|typedef|typedef int (*out_func_t)(int c, void *ctx);
 DECL|pad_type|enum|enum pad_type {
@@ -29,5 +29,6 @@ DECL|snprintk|function|int snprintk(char *str, size_t size, const char *fmt, ...
 DECL|str_context|struct|struct str_context {
 DECL|str_out|function|static int str_out(int c, struct str_context *ctx)
 DECL|str|member|char *str;
+DECL|vprintk|function|int vprintk(const char *fmt, va_list ap)
 DECL|vprintk|function|int vprintk(const char *fmt, va_list ap)
 DECL|vsnprintk|function|int vsnprintk(char *str, size_t size, const char *fmt, va_list ap)
