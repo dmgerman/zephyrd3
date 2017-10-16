b1460e9e9e42cabb332ba214c9b3281536f9f107
Marti Bolivar <marti.bolivar@linaro.org>
2017-10-16 16:02:23 -0400

    drivers: led_strip: add public API for addressable LED strips
    This API covers drivers for strips, or strings, of individually
    addressable LEDs. Both RGB and grayscale LED strip drivers can be
    implemented within these APIs.
    
    The API only provides for updating the entire strip, since not all
    strips support updating individual LEDs without affecting the others.
    
    Subsequent patches will add individual driver support.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: b533841bf06b3069ed63686d5c87e26a784e9720

---
+++ include/led_strip.h
@@ -0,0 +1,13 @@
+DECL|_ZEPHYR_LED_STRIP_H|macro|_ZEPHYR_LED_STRIP_H
+DECL|b|member|u8_t b;
+DECL|g|member|u8_t g;
+DECL|led_api_update_channels|typedef|typedef int (*led_api_update_channels)(struct device *dev, u8_t *channels,
+DECL|led_api_update_rgb|typedef|typedef int (*led_api_update_rgb)(struct device *dev, struct led_rgb *pixels,
+DECL|led_rgb|struct|struct led_rgb {
+DECL|led_strip_driver_api|struct|struct led_strip_driver_api {
+DECL|led_strip_update_channels|function|static inline int led_strip_update_channels(struct device *dev, u8_t *channels, size_t num_channels)
+DECL|led_strip_update_rgb|function|static inline int led_strip_update_rgb(struct device *dev, struct led_rgb *pixels, size_t num_pixels)
+DECL|r|member|u8_t r;
+DECL|scratch|member|u8_t scratch;
+DECL|update_channels|member|led_api_update_channels update_channels;
+DECL|update_rgb|member|led_api_update_rgb update_rgb;
