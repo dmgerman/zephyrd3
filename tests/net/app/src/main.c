1dab284e261210f7cfd4ecd7ba9e186eb0b196c2
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-10-17 14:56:41 +0300

    tests: net: app: Simple testcases for net-app testing
    These are simple tests for net-app testing. These do not try to
    actually send anything to net but test mainly net-app setup and
    close functionality.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: faeab7f2bf7641f8222acb2ea3d6132817c54bc0

---
+++ tests/net/app/src/main.c
@@ -0,0 +1,55 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
+DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|app_close_server|function|static void app_close_server(void)
+DECL|app_init|function|static void app_init(void)
+DECL|app_tcp4_client_hostname_fail|function|static void app_tcp4_client_hostname_fail(void)
+DECL|app_tcp4_client_hostname|function|static void app_tcp4_client_hostname(void)
+DECL|app_tcp4_client_peer_addr|function|static void app_tcp4_client_peer_addr(void)
+DECL|app_tcp4_client_peer_with_port|function|static void app_tcp4_client_peer_with_port(void)
+DECL|app_tcp4_client_peer|function|static void app_tcp4_client_peer(void)
+DECL|app_tcp6_client_hostname_fail|function|static void app_tcp6_client_hostname_fail(void)
+DECL|app_tcp6_client_hostname|function|static void app_tcp6_client_hostname(void)
+DECL|app_tcp6_client_peer_addr|function|static void app_tcp6_client_peer_addr(void)
+DECL|app_tcp6_client_peer_with_port|function|static void app_tcp6_client_peer_with_port(void)
+DECL|app_tcp6_client_peer|function|static void app_tcp6_client_peer(void)
+DECL|app_tcp_server_init|function|static void app_tcp_server_init(void)
+DECL|app_tcp_server_listen|function|static void app_tcp_server_listen(void)
+DECL|app_udp4_client_hostname_fail|function|static void app_udp4_client_hostname_fail(void)
+DECL|app_udp4_client_hostname|function|static void app_udp4_client_hostname(void)
+DECL|app_udp4_client_peer_addr|function|static void app_udp4_client_peer_addr(void)
+DECL|app_udp4_client_peer_with_port|function|static void app_udp4_client_peer_with_port(void)
+DECL|app_udp4_client_peer|function|static void app_udp4_client_peer(void)
+DECL|app_udp6_client_hostname_fail|function|static void app_udp6_client_hostname_fail(void)
+DECL|app_udp6_client_hostname|function|static void app_udp6_client_hostname(void)
+DECL|app_udp6_client_peer_addr|function|static void app_udp6_client_peer_addr(void)
+DECL|app_udp6_client_peer_with_port|function|static void app_udp6_client_peer_with_port(void)
+DECL|app_udp6_client_peer|function|static void app_udp6_client_peer(void)
+DECL|app_udp_server_init|function|static void app_udp_server_init(void)
+DECL|app_udp_server_listen|function|static void app_udp_server_listen(void)
+DECL|idx|member|u8_t idx;
+DECL|iface1|variable|iface1
+DECL|iface_setup|function|static void iface_setup(void)
+DECL|in6addr_mcast|variable|in6addr_mcast
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|ll_addr|variable|ll_addr
+DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|my_addr4|variable|my_addr4
+DECL|net_if_test|struct|struct net_if_test {
+DECL|net_iface1_data|variable|net_iface1_data
+DECL|net_iface_api|variable|net_iface_api
+DECL|net_iface_dev_init|function|static int net_iface_dev_init(struct device *dev)
+DECL|net_iface_get_mac|function|static u8_t *net_iface_get_mac(struct device *dev)
+DECL|net_iface_init|function|static void net_iface_init(struct net_if *iface)
+DECL|sender_iface|function|static int sender_iface(struct net_if *iface, struct net_pkt *pkt)
+DECL|tcp_server_ctx|variable|tcp_server_ctx
+DECL|test_failed|variable|test_failed
+DECL|test_main|function|void test_main(void)
+DECL|test_started|variable|test_started
+DECL|udp_server_ctx|variable|udp_server_ctx
+DECL|wait_data|variable|wait_data
