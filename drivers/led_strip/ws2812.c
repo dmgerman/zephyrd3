32373a1546053b5b7ba122114574d486e033f6cb
Marti Bolivar <marti.bolivar@linaro.org>
2017-10-16 23:28:15 -0400

    drivers: led_strip: add WS2812-ish driver
    The WS2812 LED driver IC has a one-wire interface which encodes bit
    values as pulse widths.
    
    The ICs themselves are basically shift registers. Roughly speaking, a
    "short" pulse shifts in a zero bit, a "long" pulse shifts in a one
    bit, and an inter-pulse gap exceeding a reset time threshold causes a
    pixel to latch the shifted-in color values. Each chip has an output
    pin for daisy chaining. Refer to the chip datsheets and comments in
    Kconfig.ws2812 for more details.
    
    To meet timing without hogging the core, this driver generates pulses
    using SPI. To work, this requires the MOSI line to stay low between
    SPI frames, and for inter-frame delays to be less than the reset pulse
    time.
    
    There are other ways do it (PWM + DMA on some SoCs, GPIO bit-banging
    if no other tasks need the core), but this is a reasonably
    general-purpose implementation.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: 669327137b3757aebd251e922cc26c40080d85e2

---
+++ drivers/led_strip/ws2812.c
@@ -0,0 +1,21 @@
+DECL|BLU_OFFSET|macro|BLU_OFFSET
+DECL|GRN_OFFSET|macro|GRN_OFFSET
+DECL|ONE_FRAME|macro|ONE_FRAME
+DECL|RED_OFFSET|macro|RED_OFFSET
+DECL|RESET_NFRAMES|macro|RESET_NFRAMES
+DECL|SPI_FREQ|macro|SPI_FREQ
+DECL|SPI_OPER|macro|SPI_OPER
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|WHT_OFFSET|macro|WHT_OFFSET
+DECL|WHT_OFFSET|macro|WHT_OFFSET
+DECL|ZERO_FRAME|macro|ZERO_FRAME
+DECL|config|member|struct spi_config config;
+DECL|ws2812_data|struct|struct ws2812_data {
+DECL|ws2812_reset_strip|function|static int ws2812_reset_strip(struct spi_config *config)
+DECL|ws2812_serialize_color|function|static inline void ws2812_serialize_color(u8_t buf[8], u8_t color)
+DECL|ws2812_serialize_pixel|function|static size_t ws2812_serialize_pixel(u8_t px[32], struct led_rgb *pixel)
+DECL|ws2812_strip_api|variable|ws2812_strip_api
+DECL|ws2812_strip_data|variable|ws2812_strip_data
+DECL|ws2812_strip_init|function|static int ws2812_strip_init(struct device *dev)
+DECL|ws2812_strip_update_channels|function|static int ws2812_strip_update_channels(struct device *dev, u8_t *channels,size_t num_channels)
+DECL|ws2812_strip_update_rgb|function|static int ws2812_strip_update_rgb(struct device *dev, struct led_rgb *pixels, size_t num_pixels)
