417349b13b443ad171685775d9b4f1b056d7fba3
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-10-18 17:41:04 +0300

    net: ieee802154: Add more information when hexdumping the pkt
    When hexdumping the packet, print also information if we
    received / transmitted packet. Also print information if the
    hexdump is before compression or not.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 4fe8d17bca9378ebfef736c9f2c4d819af8a7595

--- subsys/net/ip/l2/ieee802154/ieee802154.c
+++ subsys/net/ip/l2/ieee802154/ieee802154.c
@@ -1,5 +1,8 @@
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|PKT_TITLE|macro|PKT_TITLE
+DECL|RX_PKT_TITLE|macro|RX_PKT_TITLE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|TX_PKT_TITLE|macro|TX_PKT_TITLE
 DECL|ieee802154_acknowledge|function|static inline void ieee802154_acknowledge(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_acknowledge|macro|ieee802154_acknowledge
 DECL|ieee802154_init|function|void ieee802154_init(struct net_if *iface)
@@ -10,6 +13,6 @@ DECL|ieee802154_manage_send_packet|macro|ieee802154_manage_send_packet
 DECL|ieee802154_recv|function|static enum net_verdict ieee802154_recv(struct net_if *iface,struct net_pkt *pkt)
 DECL|ieee802154_reserve|function|static u16_t ieee802154_reserve(struct net_if *iface, void *data)
 DECL|ieee802154_send|function|static enum net_verdict ieee802154_send(struct net_if *iface,struct net_pkt *pkt)
-DECL|pkt_hexdump|function|static inline void pkt_hexdump(struct net_pkt *pkt, bool full)
+DECL|pkt_hexdump|function|static inline void pkt_hexdump(const char *title, struct net_pkt *pkt, bool full)
 DECL|pkt_hexdump|macro|pkt_hexdump
 DECL|set_pkt_ll_addr|function|static inline void set_pkt_ll_addr(struct net_linkaddr *addr, bool comp, enum ieee802154_addressing_mode mode, struct ieee802154_address_field *ll)
