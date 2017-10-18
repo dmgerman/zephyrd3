92082f2a80e3f1f32dbc8f918a54458c4885d716
Punit Vara <punit.vara@intel.com>
2017-10-18 14:25:48 +0530

    tests: ztest: Add formatting in assert APIs
    All ztest_* apis now support format specifier which will
    evetually help to pass variable arguments to ztest assert
    apis in different test cases while showing error messages.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 273c0fdcfb624541dc5bd163daf28709691f6544

--- tests/ztest/include/ztest_assert.h
+++ tests/ztest/include/ztest_assert.h
@@ -1,6 +1,6 @@
 DECL|__ZTEST_ASSERT_H__|macro|__ZTEST_ASSERT_H__
 DECL|_zassert_|function|static inline void _zassert_(int cond, const char *file, int line)
-DECL|_zassert|function|static inline void _zassert(int cond, const char *msg, const char *default_msg, const char *file, int line, const char *func)
+DECL|_zassert|function|static inline void _zassert(int cond, const char *default_msg, const char *file, int line, const char *func, const char *msg, ...)
 DECL|_zassert|macro|_zassert
 DECL|zassert_equal_ptr|macro|zassert_equal_ptr
 DECL|zassert_equal|macro|zassert_equal
