42e9ea1518b72072d32fb1195ab8de2d86b63642
Marti Bolivar <marti.bolivar@linaro.org>
2017-10-19 19:13:33 -0400

    samples: drivers: add WS2812 sample application
    This is similar to the LPD8806 sample, but uses the WS2812 driver
    instead. The app configuration is a bit more finicky, so try to
    provide helpful references. This could be made more beginner-friendly
    with the addition of timing diagrams, etc., but this should be enough
    for an experienced developer to use.
    
    Signed-off-by: Marti Bolivar <marti.bolivar@linaro.org>
    
    Former-commit-id: 306c717b9eddc98d72b6a6264baa181ec916a71d

---
+++ samples/drivers/led_ws2812/src/main.c
@@ -0,0 +1,10 @@
+DECL|DELAY_TIME|macro|DELAY_TIME
+DECL|SPI_DEV_NAME|macro|SPI_DEV_NAME
+DECL|STRIP_DEV_NAME|macro|STRIP_DEV_NAME
+DECL|STRIP_NUM_LEDS|macro|STRIP_NUM_LEDS
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|black|variable|black
+DECL|color_at|function|const struct led_rgb *color_at(size_t time, size_t i)
+DECL|colors|variable|colors
+DECL|main|function|void main(void)
+DECL|strip_colors|variable|strip_colors
