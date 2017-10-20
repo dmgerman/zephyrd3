78604fb1c7a885643bb064203e243393f07e6a9d
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-10-20 15:00:39 +0300

    drivers: net: loopback: Network loopback interface driver
    Loopback is a networking interface which doesn't actually transfer
    any data via link layer externally, and instead just mirrors back
    (i.e. any packet send to the loopback interface will be received from
    it). This interface very useful for testing.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: 15c64b2c8416657728d3503f4e421ac44d1ffb08

---
+++ drivers/net/loopback.c
@@ -0,0 +1,6 @@
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|loopback_dev_init|function|int loopback_dev_init(struct device *dev)
+DECL|loopback_if_api|variable|loopback_if_api
+DECL|loopback_init|function|static void loopback_init(struct net_if *iface)
+DECL|loopback_send|function|static int loopback_send(struct net_if *iface, struct net_pkt *pkt)
