e3856fc283b85ee14afb5f9818e032e36574941b
David Leach <david.leach@nxp.com>
2017-10-21 00:05:34 -0500

    drivers/ieee802154_kw41z: Stabilize the KW41Z IEEE802.15.4 driver
    - Reworked the driver logic around TX/RX to correctly handle the
      expectations of the underlying 802.15.4 hardware IP.
    - Fixed a problem with TX always reporting an error to the stack
      which resulted in constant retries.
    - Fixed bug in RX to TX transition which would occasionally cause the
      driver to error the TX.
    - Changed RX logic to ensure that invalid RX frames were not passed up
      the stack.
    - Simplified hardware timer usage to only use TMR3.
    - Added RX watermark and TMR3 support to fix a hardware problem where
      the hw IP can get stuck on a receive in noisy environments.
    - Modified samples/net/echo_client and echo_server kw41z project config
      files to provide enanced debug visibility into stacks and threads.
    
    Signed-off-by: David Leach <david.leach@nxp.com>
    
    Former-commit-id: e8431daac0adbccceb4486ed17b582d4e429f255

--- drivers/ieee802154/ieee802154_kw41z.c
+++ drivers/ieee802154/ieee802154_kw41z.c
@@ -1,14 +1,14 @@
+DECL|BM_ZLL_IRQSTS_TMRxMSK|macro|BM_ZLL_IRQSTS_TMRxMSK
 DECL|KW40Z_POST_SEQ_WAIT_TIME|macro|KW40Z_POST_SEQ_WAIT_TIME
 DECL|KW41Z_ACK_WAIT_TIME|macro|KW41Z_ACK_WAIT_TIME
 DECL|KW41Z_AUTOACK_ENABLED|macro|KW41Z_AUTOACK_ENABLED
-DECL|KW41Z_CCA_ED|enumerator|KW41Z_CCA_ED,
-DECL|KW41Z_CCA_MODE1|enumerator|KW41Z_CCA_MODE1,
-DECL|KW41Z_CCA_MODE2|enumerator|KW41Z_CCA_MODE2,
-DECL|KW41Z_CCA_MODE3|enumerator|KW41Z_CCA_MODE3
+DECL|KW41Z_CCA_ED|enumerator|KW41Z_CCA_ED, /* Energy detect */
+DECL|KW41Z_CCA_MODE1|enumerator|KW41Z_CCA_MODE1, /* Energy above threshold */
+DECL|KW41Z_CCA_MODE2|enumerator|KW41Z_CCA_MODE2, /* Carrier sense only */
+DECL|KW41Z_CCA_MODE3|enumerator|KW41Z_CCA_MODE3 /* Mode 1 + Mode 2 */
 DECL|KW41Z_CCA_TIME|macro|KW41Z_CCA_TIME
 DECL|KW41Z_DEFAULT_CHANNEL|macro|KW41Z_DEFAULT_CHANNEL
 DECL|KW41Z_FCS_LENGTH|macro|KW41Z_FCS_LENGTH
-DECL|KW41Z_IDLE_WAIT_RETRIES|macro|KW41Z_IDLE_WAIT_RETRIES
 DECL|KW41Z_OUTPUT_POWER_MAX|macro|KW41Z_OUTPUT_POWER_MAX
 DECL|KW41Z_OUTPUT_POWER_MIN|macro|KW41Z_OUTPUT_POWER_MIN
 DECL|KW41Z_PER_BYTE_TIME|macro|KW41Z_PER_BYTE_TIME
@@ -37,8 +37,10 @@ DECL|kw41z_get_instant_state|function|static inline u8_t kw41z_get_instant_state
 DECL|kw41z_get_seq_state|function|static inline u8_t kw41z_get_seq_state(void)
 DECL|kw41z_iface_init|function|static void kw41z_iface_init(struct net_if *iface)
 DECL|kw41z_init|function|static int kw41z_init(struct device *dev)
+DECL|kw41z_isr_seq_cleanup|function|static void kw41z_isr_seq_cleanup(void)
+DECL|kw41z_isr_timeout_cleanup|function|static void kw41z_isr_timeout_cleanup(void)
 DECL|kw41z_isr|function|static void kw41z_isr(int unused)
-DECL|kw41z_prepare_for_new_state|function|static int kw41z_prepare_for_new_state(void)
+DECL|kw41z_phy_abort|function|static void kw41z_phy_abort(void)
 DECL|kw41z_radio_api|variable|kw41z_radio_api
 DECL|kw41z_rx|function|static inline void kw41z_rx(struct kw41z_context *kw41z, u8_t len)
 DECL|kw41z_set_channel|function|static int kw41z_set_channel(struct device *dev, u16_t channel)
@@ -50,10 +52,8 @@ DECL|kw41z_set_short_addr|function|static int kw41z_set_short_addr(struct device
 DECL|kw41z_set_txpower|function|static int kw41z_set_txpower(struct device *dev, s16_t dbm)
 DECL|kw41z_start|function|static int kw41z_start(struct device *dev)
 DECL|kw41z_stop|function|static int kw41z_stop(struct device *dev)
-DECL|kw41z_tmr1_disable|function|static inline void kw41z_tmr1_disable(void)
-DECL|kw41z_tmr1_set_timeout|function|static void kw41z_tmr1_set_timeout(u32_t timeout)
-DECL|kw41z_tmr2_disable|function|static inline void kw41z_tmr2_disable(void)
-DECL|kw41z_tmr2_set_timeout|function|static void kw41z_tmr2_set_timeout(u32_t timeout)
+DECL|kw41z_tmr3_disable|function|static void kw41z_tmr3_disable(void)
+DECL|kw41z_tmr3_set_timeout|function|static void kw41z_tmr3_set_timeout(u32_t timeout)
 DECL|kw41z_tx|function|static int kw41z_tx(struct device *dev, struct net_pkt *pkt, struct net_buf *frag)
 DECL|kw41z_wait_for_idle|function|static inline void kw41z_wait_for_idle(void)
 DECL|mac_addr|member|u8_t mac_addr[8];
