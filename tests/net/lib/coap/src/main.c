1c027e43dde512631cc2b8c066026e76e65754b7
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2017-10-24 15:31:23 +0300

    net: tests: Add more CoAP test cases
    More test cases related to malformed packets (invalid options).
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: a3a1772218b9d287f13441b38003cef530fc5a33

--- tests/net/lib/coap/src/main.c
+++ tests/net/lib/coap/src/main.c
@@ -9,12 +9,17 @@ DECL|func|member|int (*func)(void);
 DECL|get_from_ip_addr|function|static void get_from_ip_addr(struct coap_packet *cpkt, struct sockaddr_in6 *from)
 DECL|ipaddr_cmp|function|static bool ipaddr_cmp(const struct sockaddr *a, const struct sockaddr *b)
 DECL|ipv6_block|variable|ipv6_block
+DECL|ipv6_empty_pdu_1|variable|ipv6_empty_pdu_1
 DECL|ipv6_empty_pdu|variable|ipv6_empty_pdu
 DECL|ipv6_not_found_req|variable|ipv6_not_found_req
 DECL|ipv6_obs_client|variable|ipv6_obs_client
 DECL|ipv6_resource1|variable|ipv6_resource1
 DECL|ipv6_simple_pdu|variable|ipv6_simple_pdu
 DECL|ipv6_valid_req|variable|ipv6_valid_req
+DECL|ipv6_wrong_opt_ext|variable|ipv6_wrong_opt_ext
+DECL|ipv6_wrong_opt_len_ext|variable|ipv6_wrong_opt_len_ext
+DECL|ipv6_wrong_opt_len|variable|ipv6_wrong_opt_len
+DECL|ipv6_wrong_opt|variable|ipv6_wrong_opt
 DECL|main|function|int main(int argc, char *argv[])
 DECL|name|member|const char *name;
 DECL|observers|variable|observers
@@ -32,7 +37,12 @@ DECL|test_build_simple_pdu|function|static int test_build_simple_pdu(void)
 DECL|test_match_path_uri|function|static int test_match_path_uri(void)
 DECL|test_observer_client|function|static int test_observer_client(void)
 DECL|test_observer_server|function|static int test_observer_server(void)
+DECL|test_parse_empty_pdu_1|function|static int test_parse_empty_pdu_1(void)
 DECL|test_parse_empty_pdu|function|static int test_parse_empty_pdu(void)
+DECL|test_parse_malformed_opt_ext|function|static int test_parse_malformed_opt_ext(void)
+DECL|test_parse_malformed_opt_len_ext|function|static int test_parse_malformed_opt_len_ext(void)
+DECL|test_parse_malformed_opt_len|function|static int test_parse_malformed_opt_len(void)
+DECL|test_parse_malformed_opt|function|static int test_parse_malformed_opt(void)
 DECL|test_parse_simple_pdu|function|static int test_parse_simple_pdu(void)
 DECL|test_retransmit_second_round|function|static int test_retransmit_second_round(void)
 DECL|tests|variable|tests
