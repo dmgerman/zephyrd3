47ae387f7a6af69dd4bc83424f2324e5472367f9
Andrew Boie <andrew.p.boie@intel.com>
2017-10-25 11:27:37 -0700

    drivers: pwm: add system call handlers
    pwm_pin_set_usec now defined in terms of the other two APIs.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 0f66b9f7d85bc14ee60e611cebb72cef05c656ec

---
+++ drivers/pwm/pwm_handlers.c
@@ -0,0 +1,2 @@
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(pwm_get_cycles_per_sec, dev, pwm, cycles)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(pwm_pin_set_cycles, dev, pwm, period, pulse)
--- include/pwm.h
+++ include/pwm.h
@@ -1,11 +1,11 @@
 DECL|PWM_ACCESS_ALL|macro|PWM_ACCESS_ALL
 DECL|PWM_ACCESS_BY_PIN|macro|PWM_ACCESS_BY_PIN
 DECL|__PWM_H__|macro|__PWM_H__
+DECL|_impl_pwm_get_cycles_per_sec|function|static inline int _impl_pwm_get_cycles_per_sec(struct device *dev, u32_t pwm, u64_t *cycles)
+DECL|_impl_pwm_pin_set_cycles|function|static inline int _impl_pwm_pin_set_cycles(struct device *dev, u32_t pwm, u32_t period, u32_t pulse)
 DECL|get_cycles_per_sec|member|pwm_get_cycles_per_sec_t get_cycles_per_sec;
 DECL|pin_set|member|pwm_pin_set_t pin_set;
 DECL|pwm_driver_api|struct|struct pwm_driver_api {
 DECL|pwm_get_cycles_per_sec_t|typedef|typedef int (*pwm_get_cycles_per_sec_t)(struct device *dev, u32_t pwm,
-DECL|pwm_get_cycles_per_sec|function|static inline int pwm_get_cycles_per_sec(struct device *dev, u32_t pwm, u64_t *cycles)
-DECL|pwm_pin_set_cycles|function|static inline int pwm_pin_set_cycles(struct device *dev, u32_t pwm, u32_t period, u32_t pulse)
 DECL|pwm_pin_set_t|typedef|typedef int (*pwm_pin_set_t)(struct device *dev, u32_t pwm,
 DECL|pwm_pin_set_usec|function|static inline int pwm_pin_set_usec(struct device *dev, u32_t pwm, u32_t period, u32_t pulse)
