23764773962b648503ca0f138751ed9727c98f9f
Andrew Boie <andrew.p.boie@intel.com>
2017-10-25 12:36:51 -0700

    drivers: flash: add system calls
    The page_layout API returns pointers to kernel memory and is not
    exposed to user mode. This is fine for flash_get_page_count()
    and flash_get_page_info APIs since these copy the values, but some
    redesign work will be needed to get flash_page_foreach() working in
    user mode since we do not want the callback running in a privileged
    state.
    
    Due to the way that (even unimplemented) system call prototypes are
    generated, the definition of struct flash_pages_info needed to be
    moved outside of the #ifdef.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 66aefcfcf4549e01e52aa5ddb824f711884ebefb

--- drivers/flash/flash_page_layout.c
+++ drivers/flash/flash_page_layout.c
@@ -1,5 +1,5 @@
 DECL|_flash_get_page_info|function|static int _flash_get_page_info(struct device *dev, off_t offs, bool use_addr, struct flash_pages_info *info)
-DECL|flash_get_page_count|function|size_t flash_get_page_count(struct device *dev)
-DECL|flash_get_page_info_by_idx|function|int flash_get_page_info_by_idx(struct device *dev, u32_t page_index, struct flash_pages_info *info)
-DECL|flash_get_page_info_by_offs|function|int flash_get_page_info_by_offs(struct device *dev, off_t offs,struct flash_pages_info *info)
+DECL|_impl_flash_get_page_count|function|size_t _impl_flash_get_page_count(struct device *dev)
+DECL|_impl_flash_get_page_info_by_idx|function|int _impl_flash_get_page_info_by_idx(struct device *dev, u32_t page_index, struct flash_pages_info *info)
+DECL|_impl_flash_get_page_info_by_offs|function|int _impl_flash_get_page_info_by_offs(struct device *dev, off_t offs, struct flash_pages_info *info)
 DECL|flash_page_foreach|function|void flash_page_foreach(struct device *dev, flash_page_cb cb, void *data)
--- include/flash.h
+++ include/flash.h
@@ -1,4 +1,9 @@
 DECL|__FLASH_H__|macro|__FLASH_H__
+DECL|_impl_flash_erase|function|static inline int _impl_flash_erase(struct device *dev, off_t offset, size_t size)
+DECL|_impl_flash_get_write_block_size|function|static inline size_t _impl_flash_get_write_block_size(struct device *dev)
+DECL|_impl_flash_read|function|static inline int _impl_flash_read(struct device *dev, off_t offset, void *data, size_t len)
+DECL|_impl_flash_write_protection_set|function|static inline int _impl_flash_write_protection_set(struct device *dev, bool enable)
+DECL|_impl_flash_write|function|static inline int _impl_flash_write(struct device *dev, off_t offset, const void *data, size_t len)
 DECL|erase|member|flash_api_erase erase;
 DECL|flash_api_erase|typedef|typedef int (*flash_api_erase)(struct device *dev, off_t offset, size_t size);
 DECL|flash_api_pages_layout|typedef|typedef void (*flash_api_pages_layout)(struct device *dev,
@@ -6,14 +11,9 @@ DECL|flash_api_read|typedef|typedef int (*flash_api_read)(struct device *dev, of
 DECL|flash_api_write_protection|typedef|typedef int (*flash_api_write_protection)(struct device *dev, bool enable);
 DECL|flash_api_write|typedef|typedef int (*flash_api_write)(struct device *dev, off_t offset,
 DECL|flash_driver_api|struct|struct flash_driver_api {
-DECL|flash_erase|function|static inline int flash_erase(struct device *dev, off_t offset, size_t size)
-DECL|flash_get_write_block_size|function|static inline size_t flash_get_write_block_size(struct device *dev)
 DECL|flash_page_cb|typedef|typedef bool (*flash_page_cb)(const struct flash_pages_info *info, void *data);
 DECL|flash_pages_info|struct|struct flash_pages_info {
 DECL|flash_pages_layout|struct|struct flash_pages_layout {
-DECL|flash_read|function|static inline int flash_read(struct device *dev, off_t offset, void *data, size_t len)
-DECL|flash_write_protection_set|function|static inline int flash_write_protection_set(struct device *dev, bool enable)
-DECL|flash_write|function|static inline int flash_write(struct device *dev, off_t offset, const void *data, size_t len)
 DECL|index|member|u32_t index;
 DECL|page_layout|member|flash_api_pages_layout page_layout;
 DECL|pages_count|member|size_t pages_count; /* count of pages sequence of the same size */
