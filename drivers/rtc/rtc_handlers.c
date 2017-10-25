a50eb4e571cc6468970130c7eda73d62cb33c5b0
Andrew Boie <andrew.p.boie@intel.com>
2017-10-25 13:26:14 -0700

    drivers: rtc: add system calls
    rtc_set_config() omitted since it registers a callback.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 9ca42fb03351884004ca6f1d034a6c03912587a9

---
+++ drivers/rtc/rtc_handlers.c
@@ -0,0 +1,1 @@
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(rtc_set_alarm, dev, alarm_val)
--- include/rtc.h
+++ include/rtc.h
@@ -20,6 +20,11 @@ DECL|RTC_CLK_DIV_8192|enumerator|RTC_CLK_DIV_8192,
 DECL|RTC_CLK_DIV_8|enumerator|RTC_CLK_DIV_8,
 DECL|RTC_DIVIDER|macro|RTC_DIVIDER
 DECL|_RTC_H_|macro|_RTC_H_
+DECL|_impl_rtc_disable|function|static inline void _impl_rtc_disable(struct device *dev)
+DECL|_impl_rtc_enable|function|static inline void _impl_rtc_enable(struct device *dev)
+DECL|_impl_rtc_get_pending_int|function|static inline int _impl_rtc_get_pending_int(struct device *dev)
+DECL|_impl_rtc_read|function|static inline u32_t _impl_rtc_read(struct device *dev)
+DECL|_impl_rtc_set_alarm|function|static inline int _impl_rtc_set_alarm(struct device *dev, const u32_t alarm_val)
 DECL|alarm_enable|member|u8_t alarm_enable;
 DECL|alarm_val|member|u32_t alarm_val;
 DECL|cb_fn|member|void (*cb_fn)(struct device *dev);
@@ -36,12 +41,7 @@ DECL|rtc_api_read|typedef|typedef u32_t (*rtc_api_read)(struct device *dev);
 DECL|rtc_api_set_alarm|typedef|typedef int (*rtc_api_set_alarm)(struct device *dev,
 DECL|rtc_api_set_config|typedef|typedef int (*rtc_api_set_config)(struct device *dev,
 DECL|rtc_config|struct|struct rtc_config {
-DECL|rtc_disable|function|static inline void rtc_disable(struct device *dev)
 DECL|rtc_driver_api|struct|struct rtc_driver_api {
-DECL|rtc_enable|function|static inline void rtc_enable(struct device *dev)
-DECL|rtc_get_pending_int|function|static inline int rtc_get_pending_int(struct device *dev)
-DECL|rtc_read|function|static inline u32_t rtc_read(struct device *dev)
-DECL|rtc_set_alarm|function|static inline int rtc_set_alarm(struct device *dev,const u32_t alarm_val)
 DECL|rtc_set_config|function|static inline int rtc_set_config(struct device *dev, struct rtc_config *cfg)
 DECL|set_alarm|member|rtc_api_set_alarm set_alarm;
 DECL|set_config|member|rtc_api_set_config set_config;
