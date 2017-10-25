b745591d9ccb1d84b53f34346bd410e09b94f000
Punit Vara <punit.vara@intel.com>
2017-10-25 16:03:22 +0530

    tests: obj_tracing: Convert legacy test to ztest
    Use ztest API in legacy test to support ztest
    framework and also update README according to new
    output.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 72a5f06cebeae11a691c3e092960fa4ce5dcf29d

--- tests/kernel/obj_tracing/src/main.c
+++ tests/kernel/obj_tracing/src/main.c
@@ -1,5 +1,6 @@
 DECL|STSIZE|macro|STSIZE
 DECL|forks|variable|forks
-DECL|main|function|int main(void)
 DECL|mon_data|variable|mon_data
 DECL|phil_data|variable|phil_data
+DECL|test_main|function|void test_main(void)
+DECL|test_tracing|function|void test_tracing(void)
