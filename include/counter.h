224435bb922154773229a0b36c4dc068b5e699ba
Andrew Boie <andrew.p.boie@intel.com>
2017-10-25 11:59:13 -0700

    drivers: counter: add syscall handlers
    counter_set_alarm() registers a callback and has been skipped.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 678314b7b277d144ce4efe5c9f5012a07d8fb786

--- include/counter.h
+++ include/counter.h
@@ -1,4 +1,8 @@
 DECL|__COUNTER_H__|macro|__COUNTER_H__
+DECL|_impl_counter_get_pending_int|function|static inline int _impl_counter_get_pending_int(struct device *dev)
+DECL|_impl_counter_read|function|static inline u32_t _impl_counter_read(struct device *dev)
+DECL|_impl_counter_start|function|static inline int _impl_counter_start(struct device *dev)
+DECL|_impl_counter_stop|function|static inline int _impl_counter_stop(struct device *dev)
 DECL|counter_api_get_pending_int|typedef|typedef u32_t (*counter_api_get_pending_int)(struct device *dev);
 DECL|counter_api_read|typedef|typedef u32_t (*counter_api_read)(struct device *dev);
 DECL|counter_api_set_alarm|typedef|typedef int (*counter_api_set_alarm)(struct device *dev,
@@ -6,11 +10,7 @@ DECL|counter_api_start|typedef|typedef int (*counter_api_start)(struct device *d
 DECL|counter_api_stop|typedef|typedef int (*counter_api_stop)(struct device *dev);
 DECL|counter_callback_t|typedef|typedef void (*counter_callback_t)(struct device *dev, void *user_data);
 DECL|counter_driver_api|struct|struct counter_driver_api {
-DECL|counter_get_pending_int|function|static inline int counter_get_pending_int(struct device *dev)
-DECL|counter_read|function|static inline u32_t counter_read(struct device *dev)
 DECL|counter_set_alarm|function|static inline int counter_set_alarm(struct device *dev, counter_callback_t callback, u32_t count, void *user_data)
-DECL|counter_start|function|static inline int counter_start(struct device *dev)
-DECL|counter_stop|function|static inline int counter_stop(struct device *dev)
 DECL|get_pending_int|member|counter_api_get_pending_int get_pending_int;
 DECL|read|member|counter_api_read read;
 DECL|set_alarm|member|counter_api_set_alarm set_alarm;
