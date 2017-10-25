f65990d8ef373967b46fab1274df80a55b273458
Andrew Boie <andrew.p.boie@intel.com>
2017-10-25 13:14:49 -0700

    drivers: ipm: add system calls
    ipm_register_callback() omitted since it registers a callback.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 2e60b24a809148d6a4915d4fd1b08ebeabd4a9db

---
+++ drivers/ipm/ipm_handlers.c
@@ -0,0 +1,2 @@
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(ipm_send, dev, wait, id, data, size)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(ipm_set_enabled, dev, enable)
--- include/ipm.h
+++ include/ipm.h
@@ -1,16 +1,16 @@
 DECL|__INCipmh|macro|__INCipmh
+DECL|_impl_ipm_max_data_size_get|function|static inline int _impl_ipm_max_data_size_get(struct device *ipmdev)
+DECL|_impl_ipm_max_id_val_get|function|static inline u32_t _impl_ipm_max_id_val_get(struct device *ipmdev)
+DECL|_impl_ipm_send|function|static inline int _impl_ipm_send(struct device *ipmdev, int wait, u32_t id, const void *data, int size)
+DECL|_impl_ipm_set_enabled|function|static inline int _impl_ipm_set_enabled(struct device *ipmdev, int enable)
 DECL|ipm_callback_t|typedef|typedef void (*ipm_callback_t)(void *context, u32_t id, volatile void *data);
 DECL|ipm_driver_api|struct|struct ipm_driver_api {
 DECL|ipm_max_data_size_get_t|typedef|typedef int (*ipm_max_data_size_get_t)(struct device *ipmdev);
-DECL|ipm_max_data_size_get|function|static inline int ipm_max_data_size_get(struct device *ipmdev)
 DECL|ipm_max_id_val_get_t|typedef|typedef u32_t (*ipm_max_id_val_get_t)(struct device *ipmdev);
-DECL|ipm_max_id_val_get|function|static inline u32_t ipm_max_id_val_get(struct device *ipmdev)
 DECL|ipm_register_callback_t|typedef|typedef void (*ipm_register_callback_t)(struct device *port, ipm_callback_t cb,
 DECL|ipm_register_callback|function|static inline void ipm_register_callback(struct device *ipmdev, ipm_callback_t cb, void *context)
 DECL|ipm_send_t|typedef|typedef int (*ipm_send_t)(struct device *ipmdev, int wait, u32_t id,
-DECL|ipm_send|function|static inline int ipm_send(struct device *ipmdev, int wait, u32_t id, const void *data, int size)
 DECL|ipm_set_enabled_t|typedef|typedef int (*ipm_set_enabled_t)(struct device *ipmdev, int enable);
-DECL|ipm_set_enabled|function|static inline int ipm_set_enabled(struct device *ipmdev, int enable)
 DECL|max_data_size_get|member|ipm_max_data_size_get_t max_data_size_get;
 DECL|max_id_val_get|member|ipm_max_id_val_get_t max_id_val_get;
 DECL|register_callback|member|ipm_register_callback_t register_callback;
