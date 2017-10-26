9af8856f10936ca167e610a007d73774dfa18b73
Andrew Boie <andrew.p.boie@intel.com>
2017-10-26 12:42:13 -0700

    drivers: pinmux: add system call handlers
    pinmux_pin_get() needs memory validated for the func parameter since
    it's a pointer that gets written to.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 4d91f5abe9648893f74c11ec1c2cc922769022e4

---
+++ drivers/pinmux/pinmux_handlers.c
@@ -0,0 +1,4 @@
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(pinmux_pin_get, dev, pin, func)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(pinmux_pin_input_enable, dev, pin, func)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(pinmux_pin_pullup, dev, pin, func)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(pinmux_pin_set, dev, pin, func)
