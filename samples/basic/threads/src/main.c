d6e5b07d310cdf8c82ed612174ec739065e194d7
Sahaj Sarup <sahaj.sarup@linaro.org>
2017-10-26 23:35:38 +0530

    samples: Basic Thread Demo
    A basic demo to showcase multi-threading using K_THREAD_DEFINE
    
    Signed-off-by: Sahaj Sarup <sahaj.sarup@linaro.org>
    
    Former-commit-id: b6c70fd331e9b2687445f9a7e39602622c08c6a3

---
+++ samples/basic/threads/src/main.c
@@ -0,0 +1,9 @@
+DECL|LED0|macro|LED0
+DECL|LED1|macro|LED1
+DECL|PORT0|macro|PORT0
+DECL|PORT1|macro|PORT1
+DECL|PRIORITY|macro|PRIORITY
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|blink1|function|void blink1(void)
+DECL|blink2|function|void blink2(void)
+DECL|uart_out|function|void uart_out(void)
