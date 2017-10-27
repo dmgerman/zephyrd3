1887811f8e055ed12c7d6215612636bdb6b1cafd
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-10-27 15:43:34 +0200

    Introduce cmake-based rewrite of KBuild
    Introducing CMake is an important step in a larger effort to make
    Zephyr easy to use for application developers working on different
    platforms with different development environment needs.
    
    Simplified, this change retains Kconfig as-is, and replaces all
    Makefiles with CMakeLists.txt. The DSL-like Make language that KBuild
    offers is replaced by a set of CMake extentions. These extentions have
    either provided simple one-to-one translations of KBuild features or
    introduced new concepts that replace KBuild concepts.
    
    This is a breaking change for existing test infrastructure and build
    scripts that are maintained out-of-tree. But for FW itself, no porting
    should be necessary.
    
    For users that just want to continue their work with minimal
    disruption the following should suffice:
    
    Install CMake 3.8.2+
    
    Port any out-of-tree Makefiles to CMake.
    
    Learn the absolute minimum about the new command line interface:
    
    $ cd samples/hello_world
    $ mkdir build && cd build
    $ cmake -DBOARD=nrf52_pca10040 ..
    
    $ cd build
    $ make
    
    PR: zephyrproject-rtos#4692
    docs: http://docs.zephyrproject.org/getting_started/getting_started.html
    
    Signed-off-by: Sebastian Boe <sebastian.boe@nordicsemi.no>
    
    Former-commit-id: 12f8f7616567f224b920512b6f81a65421e30bae

---
+++ tests/kernel/test_build/src/main.c
@@ -0,0 +1,7 @@
+DECL|PRIORITY|macro|PRIORITY
+DECL|SLEEPTIME|macro|SLEEPTIME
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|helloLoop|function|void helloLoop(const char *my_name, struct k_sem *my_sem, struct k_sem *other_sem)
+DECL|threadA|function|void threadA(void *dummy1, void *dummy2, void *dummy3)
+DECL|threadB_data|variable|threadB_data
+DECL|threadB|function|void threadB(void *dummy1, void *dummy2, void *dummy3)
