a9e8a09955e66c161361245e22249ecbf55b7c02
Youvedeep Singh <youvedeep.singh@intel.com>
2017-10-27 21:38:42 +0530

    timer: xtensa_sys_timer: Tickless Kernel Implementation for Xtensa
    Implement Tickless Kernel support for Xtensa Architecture.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 833025dd942d4234fceca5a06aba1d37e824e25e

--- drivers/timer/xtensa_sys_timer.c
+++ drivers/timer/xtensa_sys_timer.c
@@ -4,6 +4,7 @@ DECL|GET_TIMER_FIRE_TIME|macro|GET_TIMER_FIRE_TIME
 DECL|GET_TIMER_FIRE_TIME|macro|GET_TIMER_FIRE_TIME
 DECL|IDLE_NOT_TICKLESS|macro|IDLE_NOT_TICKLESS
 DECL|IDLE_TICKLESS|macro|IDLE_TICKLESS
+DECL|MAX_TIMER_CYCLES|macro|MAX_TIMER_CYCLES
 DECL|MIN_TIMER_PROG_DELAY|macro|MIN_TIMER_PROG_DELAY
 DECL|MIN_TIMER_PROG_DELAY|macro|MIN_TIMER_PROG_DELAY
 DECL|SET_TIMER_FIRE_TIME|macro|SET_TIMER_FIRE_TIME
@@ -21,6 +22,14 @@ DECL|XT_WSR_CCOMPARE0|macro|XT_WSR_CCOMPARE0
 DECL|XT_WSR_CCOMPARE1|macro|XT_WSR_CCOMPARE1
 DECL|XT_WSR_CCOMPARE2|macro|XT_WSR_CCOMPARE2
 DECL|_XT_SR_CCOMPARE|macro|_XT_SR_CCOMPARE
+DECL|_enable_sys_clock|function|void _enable_sys_clock(void)
+DECL|_get_elapsed_clock_time|function|u64_t _get_elapsed_clock_time(void)
+DECL|_get_elapsed_program_time|function|u32_t _get_elapsed_program_time(void)
+DECL|_get_max_clock_time|function|static inline int32_t _get_max_clock_time(void)
+DECL|_get_program_time|function|u32_t _get_program_time(void)
+DECL|_get_remaining_program_time|function|u32_t _get_remaining_program_time(void)
+DECL|_set_max_clock_time|function|static inline void _set_max_clock_time(void)
+DECL|_set_time|function|void _set_time(u32_t time)
 DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
 DECL|_timer_cycle_get_32|function|u32_t _timer_cycle_get_32(void)
 DECL|_timer_idle_enter|function|void _timer_idle_enter(s32_t ticks)
@@ -31,6 +40,7 @@ DECL|_xt_tick_divisor|variable|_xt_tick_divisor
 DECL|cycles_per_tick|variable|cycles_per_tick
 DECL|idle_mode|variable|idle_mode
 DECL|idle_original_ticks|variable|idle_original_ticks
+DECL|last_timer_value|variable|last_timer_value
 DECL|max_load_value|variable|max_load_value
 DECL|max_system_ticks|variable|max_system_ticks
 DECL|tickless_idle_init|function|static ALWAYS_INLINE void tickless_idle_init(void)
