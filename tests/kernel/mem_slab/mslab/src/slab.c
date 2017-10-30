404916c74e61cb7b32bbe4bc096f5a6ffab6e5e6
Punit Vara <punit.vara@intel.com>
2017-10-30 11:27:18 +0530

    tests: mslab: convert legacy test to ztest
    This patch removes checkpatch warnings as well as
    make use of ztest apis to convert legacy test to ztest.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: e2a974497b6699a24521df0b4154988d774b440d

--- tests/kernel/mem_slab/mslab/src/slab.c
+++ tests/kernel/mem_slab/mslab/src/slab.c
@@ -1,9 +1,10 @@
 DECL|NUMBLOCKS|macro|NUMBLOCKS
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|helper_thread|function|void helper_thread(void)
-DECL|main|function|void main(void)
 DECL|print_pointers|function|void print_pointers(void **pointer)
 DECL|tc_rc|variable|tc_rc
+DECL|test_main|function|void test_main(void)
 DECL|test_slab_free_all_blocks|function|int test_slab_free_all_blocks(void **p)
 DECL|test_slab_get_all_blocks|function|int test_slab_get_all_blocks(void **p)
+DECL|testing_mslab|function|void testing_mslab(void)
 DECL|verify_ret_value|function|bool verify_ret_value(int expect_ret_value, int current_ret_current)
