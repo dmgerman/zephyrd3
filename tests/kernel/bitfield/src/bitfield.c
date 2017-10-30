d0c0df98384d4f66c785e2ed654f503a540d45c5
Punit Vara <punit.vara@intel.com>
2017-10-30 13:50:45 +0530

    tests: bitfield: Convert legacy test case to ztest
    migrate testcase to ztest and use ztest macros.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: c2f60ebe567f9c42c5f829e8a53fffcf8aa2e71a

--- tests/kernel/bitfield/src/bitfield.c
+++ tests/kernel/bitfield/src/bitfield.c
@@ -1,4 +1,5 @@
 DECL|BITFIELD_SIZE|macro|BITFIELD_SIZE
 DECL|BIT_INDEX|macro|BIT_INDEX
 DECL|BIT_VAL|macro|BIT_VAL
-DECL|main|function|void main(void)
+DECL|test_main|function|void test_main(void)
+DECL|testing_bitfield|function|void testing_bitfield(void)
