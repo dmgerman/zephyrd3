491600c1e11134a668a3af6955ee26396faf4c6f
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-11-02 12:07:25 +0100

    drivers/wifi: Generalize GPIO configuration for the WINC1500 driver
    Previous way was cumbersome (based on an old way for cc2520 actually),
    so moving towards the more recent and proper way.
    
    This will enable anybody to actually provide gpio configuration for a
    winc1500 out of any board specific location.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 2a1d222adc9c39a75830e3a37d392463bc365570

---
+++ include/drivers/wifi/winc1500.h
@@ -0,0 +1,9 @@
+DECL|WINC1500_GPIO_IDX_CHIP_EN|enumerator|WINC1500_GPIO_IDX_CHIP_EN = 0,
+DECL|WINC1500_GPIO_IDX_IRQN|enumerator|WINC1500_GPIO_IDX_IRQN,
+DECL|WINC1500_GPIO_IDX_MAX|enumerator|WINC1500_GPIO_IDX_MAX
+DECL|WINC1500_GPIO_IDX_RESET_N|enumerator|WINC1500_GPIO_IDX_RESET_N,
+DECL|WINC1500_GPIO_IDX_WAKE|enumerator|WINC1500_GPIO_IDX_WAKE,
+DECL|dev|member|struct device *dev;
+DECL|pin|member|u32_t pin;
+DECL|winc1500_gpio_configuration|struct|struct winc1500_gpio_configuration {
+DECL|winc1500_gpio_index|enum|enum winc1500_gpio_index {
