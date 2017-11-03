866d8edd8b7f9f264abce5ae30ea1784d026107b
Loic Poulain <loic.poulain@linaro.org>
2017-11-03 14:27:46 +0100

    usb: netusb: Add netusb_recv method
    This method can be use by netusb functions on pkt reception.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    
    Former-commit-id: 0188881599bf63243f101325e2d4a4c108907e08

--- subsys/usb/class/netusb/netusb.c
+++ subsys/usb/class/netusb/netusb.c
@@ -16,6 +16,7 @@ DECL|netusb_disconnect_media|function|static int netusb_disconnect_media(void)
 DECL|netusb_ep_data|variable|netusb_ep_data
 DECL|netusb_init_dev|function|static int netusb_init_dev(struct device *dev)
 DECL|netusb_init|function|static void netusb_init(struct net_if *iface)
+DECL|netusb_recv|function|void netusb_recv(struct net_pkt *pkt)
 DECL|netusb_send|function|static int netusb_send(struct net_if *iface, struct net_pkt *pkt)
 DECL|netusb_status_cb|function|static void netusb_status_cb(enum usb_dc_status_code status, u8_t *param)
 DECL|netusb_status_configured|function|static void netusb_status_configured(u8_t *conf)
