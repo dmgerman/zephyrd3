f607ffe612fb8d7b41fbf8ef9103fbc5f41ed7cf
Loic Poulain <loic.poulain@linaro.org>
2017-11-03 15:21:26 +0100

    usb: netusb: Remove ecm_register_function
    The only access to the function is now the function interface.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    
    Former-commit-id: c82d1446bdd7634bc31313035920b665e32d88fb

--- subsys/usb/class/netusb/function_ecm.c
+++ subsys/usb/class/netusb/function_ecm.c
@@ -11,7 +11,6 @@ DECL|ecm_class_handler|function|static int ecm_class_handler(struct usb_setup_pa
 DECL|ecm_ep_data|variable|ecm_ep_data
 DECL|ecm_function|variable|ecm_function
 DECL|ecm_int_in|function|static void ecm_int_in(u8_t ep, enum usb_dc_ep_cb_status_code ep_status)
-DECL|ecm_register_function|function|struct netusb_function *ecm_register_function(struct net_if *iface, u8_t in)
 DECL|ecm_send|function|static int ecm_send(struct net_pkt *pkt)
 DECL|ecm|struct|struct ecm {
 DECL|ecm|variable|ecm
--- subsys/usb/class/netusb/netusb.h
+++ subsys/usb/class/netusb/netusb.h
@@ -1,5 +1,7 @@
+DECL|NETUSB_IFACE_IDX|macro|NETUSB_IFACE_IDX
 DECL|class_handler|member|int (*class_handler)(struct usb_setup_packet *setup, s32_t *len,
 DECL|connect_media|member|int (*connect_media)(bool status);
+DECL|ecm_function|variable|ecm_function
 DECL|ep|member|struct usb_ep_cfg_data *ep;
 DECL|netusb_function|struct|struct netusb_function {
 DECL|num_ep|member|int num_ep;
