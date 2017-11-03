5a2467d75d23cf11edef09205f7f78133a9ad682
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2017-11-03 16:49:59 -0400

    drivers: crypto: Fix mbedtls Kconfig dependencies
    Fix mbedtls shim driver Kconfig dependencies.
    
    Limit the scope of crypto_driver_api functions to driver file only.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: bb5a2985d194fd97ee836c7219ec7b760cb21f64

--- drivers/crypto/crypto_mtls_shim.c
+++ drivers/crypto/crypto_mtls_shim.c
@@ -6,9 +6,9 @@ DECL|mtls_ccm_decrypt_auth|function|static int mtls_ccm_decrypt_auth(struct ciph
 DECL|mtls_ccm_encrypt_auth|function|static int mtls_ccm_encrypt_auth(struct cipher_ctx *ctx, struct cipher_aead_pkt *apkt, u8_t *nonce)
 DECL|mtls_crypto_funcs|variable|mtls_crypto_funcs
 DECL|mtls_get_unused_session_index|function|static int mtls_get_unused_session_index(void)
-DECL|mtls_query_caps|function|int mtls_query_caps(struct device *dev)
-DECL|mtls_session_free|function|int mtls_session_free(struct device *dev, struct cipher_ctx *ctx)
-DECL|mtls_session_setup|function|int mtls_session_setup(struct device *dev, struct cipher_ctx *ctx, enum cipher_algo algo, enum cipher_mode mode, enum cipher_op op_type)
+DECL|mtls_query_caps|function|static int mtls_query_caps(struct device *dev)
+DECL|mtls_session_free|function|static int mtls_session_free(struct device *dev, struct cipher_ctx *ctx)
+DECL|mtls_session_setup|function|static int mtls_session_setup(struct device *dev, struct cipher_ctx *ctx, enum cipher_algo algo, enum cipher_mode mode, enum cipher_op op_type)
 DECL|mtls_sessions|variable|mtls_sessions
 DECL|mtls_shim_init|function|static int mtls_shim_init(struct device *dev)
 DECL|mtls_shim_session|struct|struct mtls_shim_session {
