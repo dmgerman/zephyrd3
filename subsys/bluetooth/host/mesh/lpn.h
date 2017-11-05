210b181ef4a478abf8ba2772b26831f1143c91ea
Johan Hedberg <johan.hedberg@intel.com>
2017-11-05 22:58:22 +0200

    Bluetooth: Mesh: Fix friendship credential usage for segmented messages
    The friend_cred hint needs to be set already at the point of
    segmenting, i.e. doing it in bt_mesh_net_send() is too late. Move the
    setting to bt_mesh_trans_send() and bt_mesh_ctl_send().
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 9b2a6d4866a401b8ebf8016936221cbd19efbfb3

--- subsys/bluetooth/host/mesh/lpn.h
+++ subsys/bluetooth/host/mesh/lpn.h
@@ -1,2 +1,3 @@
 DECL|bt_mesh_lpn_established|function|static inline bool bt_mesh_lpn_established(void)
+DECL|bt_mesh_lpn_match|function|static inline bool bt_mesh_lpn_match(u16_t addr)
 DECL|bt_mesh_lpn_waiting_update|function|static inline bool bt_mesh_lpn_waiting_update(void)
