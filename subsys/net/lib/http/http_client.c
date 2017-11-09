9e6447dd11b7c7db2ff98fda59641384a5c3d945
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-11-09 16:44:55 +0200

    net: http: Remove the old legacy API
    There are no internal users for old HTTP API so removing it.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d1675bf3e658c945599dad1d786ce8a7590c327e

--- subsys/net/lib/http/http_client.c
+++ subsys/net/lib/http/http_client.c
@@ -1,38 +1,26 @@
 DECL|BUF_ALLOC_TIMEOUT|macro|BUF_ALLOC_TIMEOUT
-DECL|DEBUG_THRESHOLD|macro|DEBUG_THRESHOLD
-DECL|DNS_WAIT_SEM|macro|DNS_WAIT_SEM
-DECL|DNS_WAIT|macro|DNS_WAIT
-DECL|HTTPS_STARTUP_TIMEOUT|macro|HTTPS_STARTUP_TIMEOUT
+DECL|HTTP_CONTENT_LEN|macro|HTTP_CONTENT_LEN
 DECL|HTTP_CONTENT_TYPE|macro|HTTP_CONTENT_TYPE
 DECL|HTTP_CONT_LEN_SIZE|macro|HTTP_CONT_LEN_SIZE
 DECL|HTTP_EOF|macro|HTTP_EOF
 DECL|HTTP_HOST|macro|HTTP_HOST
-DECL|HTTP_NETWORK_TIMEOUT|macro|HTTP_NETWORK_TIMEOUT
 DECL|MAX_NUM_DIGITS|macro|MAX_NUM_DIGITS
 DECL|MAX_OUTPUT_LEN|macro|MAX_OUTPUT_LEN
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
-DECL|RX_EXTRA_DEBUG|macro|RX_EXTRA_DEBUG
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|RC_STR|macro|RC_STR
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
-DECL|block|member|struct k_mem_block block;
-DECL|block|member|struct k_mem_block block;
-DECL|client_reset|function|int client_reset(struct http_client_ctx *ctx)
-DECL|ctx|member|struct http_client_ctx *ctx;
-DECL|dns_cb|function|static void dns_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
-DECL|entropy_source|function|static int entropy_source(void *data, unsigned char *output, size_t len, size_t *olen)
-DECL|get_local_addr|function|static int get_local_addr(struct http_client_ctx *ctx)
-DECL|http_client_init|function|int http_client_init(struct http_client_ctx *ctx, const char *server, u16_t server_port)
-DECL|http_client_release|function|void http_client_release(struct http_client_ctx *ctx)
-DECL|http_client_send_req|function|int http_client_send_req(struct http_client_ctx *ctx, struct http_client_request *req, http_response_cb_t cb, u8_t *response_buf, size_t response_buf_len,
-DECL|http_client_set_net_pkt_pool|function|int http_client_set_net_pkt_pool(struct http_client_ctx *ctx, net_pkt_get_slab_func_t tx_slab, net_pkt_get_pool_func_t data_pool)
-DECL|http_receive_cb|function|static void http_receive_cb(struct http_client_ctx *ctx, struct net_pkt *pkt)
-DECL|http_request|function|int http_request(struct http_client_ctx *ctx, struct http_client_request *req, s32_t timeout)
-DECL|https_client_init|function|int https_client_init(struct http_client_ctx *ctx, const char *server, u16_t server_port, u8_t *personalization_data, size_t personalization_data_len, https_ca_cert_cb_t cert_cb,
-DECL|https_handler|function|static void https_handler(struct http_client_ctx *ctx, struct k_sem *startup_sync)
-DECL|https_init|function|static int https_init(struct http_client_ctx *ctx)
-DECL|https_send|function|static int https_send(struct net_pkt *pkt, net_context_send_cb_t cb, s32_t timeout, void *token, void *user_data)
-DECL|https_shutdown|function|static void https_shutdown(struct http_client_ctx *ctx)
-DECL|my_debug|function|static void my_debug(void *ctx, int level, const char *file, int line, const char *str)
+DECL|client_reset|function|int client_reset(struct http_ctx *ctx)
+DECL|http_client_init|function|int http_client_init(struct http_ctx *ctx, const char *server, u16_t server_port, struct sockaddr *server_addr, s32_t timeout)
+DECL|http_client_send_req|function|int http_client_send_req(struct http_ctx *ctx, struct http_request *req, http_response_cb_t cb, u8_t *response_buf, size_t response_buf_len,
+DECL|http_client_set_tls|function|int http_client_set_tls(struct http_ctx *ctx,u8_t *request_buf, size_t request_buf_len, u8_t *personalization_data, size_t personalization_data_len,
+DECL|http_closed|function|static void http_closed(struct net_app_ctx *app_ctx,int status, void *user_data)
+DECL|http_connected|function|static void http_connected(struct net_app_ctx *app_ctx, int status, void *user_data)
+DECL|http_data_sent|function|static void http_data_sent(struct net_app_ctx *app_ctx, int status, void *user_data_send, void *user_data)
+DECL|http_received|function|static void http_received(struct net_app_ctx *app_ctx, struct net_pkt *pkt, int status, void *user_data)
+DECL|http_request_cancel|function|int http_request_cancel(struct http_ctx *ctx)
+DECL|http_request|function|int http_request(struct http_ctx *ctx, struct http_request *req, s32_t timeout, void *user_data)
 DECL|on_body|function|static int on_body(struct http_parser *parser, const char *at, size_t length)
 DECL|on_chunk_complete|function|static int on_chunk_complete(struct http_parser *parser)
 DECL|on_chunk_header|function|static int on_chunk_header(struct http_parser *parser)
@@ -43,26 +31,6 @@ DECL|on_message_begin|function|static int on_message_begin(struct http_parser *p
 DECL|on_message_complete|function|static int on_message_complete(struct http_parser *parser)
 DECL|on_status|function|static int on_status(struct http_parser *parser, const char *at, size_t length)
 DECL|on_url|function|static int on_url(struct http_parser *parser, const char *at, size_t length)
-DECL|pkt|member|struct net_pkt *pkt;
-DECL|print_error|macro|print_error
-DECL|print_error|macro|print_error
 DECL|print_header_field|function|static void print_header_field(size_t len, const char *str)
-DECL|print_info|function|static inline void print_info(struct http_client_ctx *ctx,enum http_method method)
-DECL|recv_cb|function|static void recv_cb(struct net_context *net_ctx, struct net_pkt *pkt, int status, void *data)
-DECL|req|member|struct http_client_request *req;
-DECL|resolve_name|function|static int resolve_name(struct http_client_ctx *ctx,const char *server, enum dns_query_type type)
-DECL|rx_fifo_block|struct|struct rx_fifo_block {
-DECL|set_remote_addr|function|static inline int set_remote_addr(struct http_client_ctx *ctx, const char *server, u16_t server_port)
-DECL|snode|member|sys_snode_t snode;
+DECL|print_info|function|static inline void print_info(struct http_ctx *ctx, enum http_method method)
 DECL|sprint_addr|function|static void sprint_addr(char *buf, int len,sa_family_t family, struct sockaddr *addr)
-DECL|ssl_received|function|static void ssl_received(struct net_context *context, struct net_pkt *pkt, int status, void *user_data)
-DECL|ssl_rx|function|int ssl_rx(void *context, unsigned char *buf, size_t size)
-DECL|ssl_sent|function|static void ssl_sent(struct net_context *context, int status, void *token, void *user_data)
-DECL|ssl_tx|function|static int ssl_tx(void *context, const unsigned char *buf, size_t size)
-DECL|start_https|function|static int start_https(struct http_client_ctx *ctx)
-DECL|start_https|macro|start_https
-DECL|tcp_connect|function|static int tcp_connect(struct http_client_ctx *ctx)
-DECL|tcp_disconnect|function|static void tcp_disconnect(struct http_client_ctx *ctx)
-DECL|tx_fifo_block|struct|struct tx_fifo_block {
-DECL|waiter|struct|struct waiter {
-DECL|wait|member|struct k_sem wait;
--- subsys/net/lib/http/http_server.c
+++ subsys/net/lib/http/http_server.c
@@ -1,80 +1,48 @@
+DECL|ADDR_LEN|macro|ADDR_LEN
 DECL|BUF_ALLOC_TIMEOUT|macro|BUF_ALLOC_TIMEOUT
-DECL|DEBUG_THRESHOLD|macro|DEBUG_THRESHOLD
 DECL|HTTPS_DEFAULT_PORT|macro|HTTPS_DEFAULT_PORT
 DECL|HTTP_DEFAULT_PORT|macro|HTTP_DEFAULT_PORT
-DECL|HTTP_STATUS_200_OK|macro|HTTP_STATUS_200_OK
 DECL|HTTP_STATUS_400_BR|macro|HTTP_STATUS_400_BR
-DECL|HTTP_STATUS_403_FBD|macro|HTTP_STATUS_403_FBD
-DECL|HTTP_STATUS_404_NF|macro|HTTP_STATUS_404_NF
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
-DECL|PORT_STR|macro|PORT_STR
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|PORT_LEN|macro|PORT_LEN
 DECL|PORT_STR|macro|PORT_STR
 DECL|RC_STR|macro|RC_STR
-DECL|RX_EXTRA_DEBUG|macro|RX_EXTRA_DEBUG
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
-DECL|accept_cb|function|static void accept_cb(struct net_context *net_ctx, struct sockaddr *addr, socklen_t addrlen, int status, void *data)
-DECL|block|member|struct k_mem_block block;
+DECL|_http_change_state|function|void _http_change_state(struct http_ctx *ctx,enum http_state new_state, const char *func, int line)
 DECL|ctx_mon|variable|ctx_mon
-DECL|entropy_source|function|static int entropy_source(void *data, unsigned char *output, size_t len, size_t *olen)
-DECL|http_add_chunk|function|static int http_add_chunk(struct net_pkt *pkt, s32_t timeout, const char *str)
-DECL|http_add_header|function|static int http_add_header(struct net_pkt *pkt, s32_t timeout, const char *str)
+DECL|get_server_ctx|function|static struct net_context *get_server_ctx(struct net_app_ctx *ctx, const struct sockaddr *dst)
+DECL|http_closed|function|static void http_closed(struct net_app_ctx *app_ctx,int status, void *user_data)
 DECL|http_conn|variable|http_conn
-DECL|http_process_recv|function|static int http_process_recv(struct http_server_ctx *http_ctx)
-DECL|http_recv|function|static void http_recv(struct net_context *net_ctx, struct net_pkt *pkt, int status, void *user_data)
-DECL|http_response_400|function|int http_response_400(struct http_server_ctx *ctx, const char *html_payload)
-DECL|http_response_403|function|int http_response_403(struct http_server_ctx *ctx, const char *html_payload)
-DECL|http_response_404|function|int http_response_404(struct http_server_ctx *ctx, const char *html_payload)
-DECL|http_response_send_data|function|int http_response_send_data(struct http_server_ctx *ctx, const char *http_header, const char *html_payload, s32_t timeout)
-DECL|http_response_wait|function|int http_response_wait(struct http_server_ctx *ctx, const char *http_header, const char *html_payload, s32_t timeout)
-DECL|http_response|function|int http_response(struct http_server_ctx *ctx, const char *http_header, const char *html_payload)
-DECL|http_server_add_default|function|struct http_root_url *http_server_add_default(struct http_server_urls *my, http_url_cb_t write_cb)
-DECL|http_server_add_url|function|struct http_root_url *http_server_add_url(struct http_server_urls *my, const char *url, u8_t flags, http_url_cb_t write_cb)
-DECL|http_server_conn_add|function|static void http_server_conn_add(struct http_server_ctx *ctx)
-DECL|http_server_conn_add|macro|http_server_conn_add
-DECL|http_server_conn_del|function|static void http_server_conn_del(struct http_server_ctx *ctx)
-DECL|http_server_conn_del|macro|http_server_conn_del
+DECL|http_data_sent|function|static void http_data_sent(struct net_app_ctx *app_ctx, int status, void *user_data_send, void *user_data)
+DECL|http_process_recv|function|static int http_process_recv(struct http_ctx *ctx)
+DECL|http_received|function|static void http_received(struct net_app_ctx *app_ctx, struct net_pkt *pkt, int status, void *user_data)
+DECL|http_send_error|function|int http_send_error(struct http_ctx *ctx, int code, u8_t *html_payload, size_t html_len)
+DECL|http_server_add_default|function|struct http_root_url *http_server_add_default(struct http_server_urls *my, http_url_cb_t cb)
+DECL|http_server_add_url|function|struct http_root_url *http_server_add_url(struct http_server_urls *my, const char *url, u8_t flags)
+DECL|http_server_conn_add|function|void http_server_conn_add(struct http_ctx *ctx)
+DECL|http_server_conn_del|function|void http_server_conn_del(struct http_ctx *ctx)
 DECL|http_server_conn_foreach|function|void http_server_conn_foreach(http_server_cb_t cb, void *user_data)
 DECL|http_server_conn_monitor|function|void http_server_conn_monitor(http_server_cb_t cb, void *user_data)
 DECL|http_server_del_default|function|int http_server_del_default(struct http_server_urls *my)
 DECL|http_server_del_url|function|int http_server_del_url(struct http_server_urls *my, const char *url)
-DECL|http_server_disable|function|bool http_server_disable(struct http_server_ctx *http_ctx)
-DECL|http_server_enable|function|bool http_server_enable(struct http_server_ctx *http_ctx)
-DECL|http_server_init|function|int http_server_init(struct http_server_ctx *http_ctx, struct http_server_urls *urls, struct sockaddr *server_addr, u8_t *request_buf, size_t request_buf_len,
-DECL|http_server_release|function|void http_server_release(struct http_server_ctx *http_ctx)
-DECL|http_server_set_local_addr|function|int http_server_set_local_addr(struct sockaddr *addr, const char *myaddr, u16_t port)
-DECL|http_server_set_net_pkt_pool|function|int http_server_set_net_pkt_pool(struct http_server_ctx *ctx, net_pkt_get_slab_func_t tx_slab, net_pkt_get_pool_func_t data_pool)
-DECL|http_strlen|function|static inline u16_t http_strlen(const char *str)
+DECL|http_server_disable|function|int http_server_disable(struct http_ctx *ctx)
+DECL|http_server_enable|function|int http_server_enable(struct http_ctx *ctx)
+DECL|http_server_init|function|int http_server_init(struct http_ctx *ctx, struct http_server_urls *urls, struct sockaddr *server_addr, u8_t *request_buf, size_t request_buf_len,
+DECL|http_server_set_tls|function|int http_server_set_tls(struct http_ctx *ctx,const char *server_banner, u8_t *personalization_data, size_t personalization_data_len, net_app_cert_cb_t cert_cb,
+DECL|http_state_str|function|const char * const http_state_str(enum http_state state)
 DECL|http_url_cmp|function|static int http_url_cmp(const char *url, u16_t url_len,const char *root_url, u16_t root_url_len)
-DECL|http_url_find|function|static struct http_root_url *http_url_find(struct http_server_ctx *http_ctx)
-DECL|https_disable|function|static void https_disable(struct http_server_ctx *ctx)
-DECL|https_enable|function|static void https_enable(struct http_server_ctx *ctx)
-DECL|https_handler|function|static void https_handler(struct http_server_ctx *ctx)
-DECL|https_init|function|static int https_init(struct http_server_ctx *ctx)
-DECL|https_send|function|static int https_send(struct net_pkt *pkt, net_context_send_cb_t cb, s32_t timeout, void *token, void *user_data)
-DECL|https_server_init|function|int https_server_init(struct http_server_ctx *ctx, struct http_server_urls *urls, struct sockaddr *server_addr, u8_t *request_buf, size_t request_buf_len,
-DECL|init_net|function|static int init_net(struct http_server_ctx *ctx, struct sockaddr *server_addr, u16_t port)
+DECL|http_url_find|function|struct http_root_url *http_url_find(struct http_ctx *ctx, enum http_url_flags flags)
+DECL|init_http_parser|function|static int init_http_parser(struct http_ctx *ctx)
+DECL|init_net|function|static void init_net(struct http_ctx *ctx, struct sockaddr *server_addr, u16_t port)
 DECL|mon_user_data|variable|mon_user_data
-DECL|my_debug|function|static void my_debug(void *ctx, int level, const char *file, int line, const char *str)
-DECL|new_client|function|static inline void new_client(struct http_server_ctx *http_ctx, struct net_context *net_ctx, const struct sockaddr *addr)
-DECL|new_server|function|static inline void new_server(struct http_server_ctx *ctx, const char *server_banner, const struct sockaddr *addr)
+DECL|new_client|function|static inline void new_client(struct http_ctx *ctx, enum http_connection_type type, struct net_app_ctx *app_ctx)
+DECL|new_server|function|static inline void new_server(struct http_ctx *ctx, const char *server_banner, const struct sockaddr *addr)
 DECL|on_header_field|function|static int on_header_field(struct http_parser *parser, const char *at, size_t length)
 DECL|on_header_value|function|static int on_header_value(struct http_parser *parser, const char *at, size_t length)
+DECL|on_headers_complete|function|static int on_headers_complete(struct http_parser *parser)
 DECL|on_url|function|static int on_url(struct http_parser *parser, const char *at, size_t length)
-DECL|parser_init|function|static int parser_init(struct http_server_ctx *ctx)
-DECL|pkt_sent|function|static void pkt_sent(struct net_context *context, int status, void *token, void *user_data)
-DECL|pkt|member|struct net_pkt *pkt;
-DECL|print_error|macro|print_error
-DECL|print_error|macro|print_error
-DECL|req_timeout|function|static void req_timeout(struct k_work *work)
-DECL|req_timer_cancel|function|static void req_timer_cancel(struct http_server_ctx *ctx)
-DECL|rx_fifo_block|struct|struct rx_fifo_block {
-DECL|set_net_ctx|function|static int set_net_ctx(struct http_server_ctx *http_ctx, struct net_context *ctx, struct sockaddr *addr, socklen_t socklen)
-DECL|setup_ipv4_ctx|function|static int setup_ipv4_ctx(struct http_server_ctx *http_ctx, struct sockaddr *addr)
-DECL|setup_ipv6_ctx|function|int setup_ipv6_ctx(struct http_server_ctx *http_ctx, struct sockaddr *addr)
-DECL|snode|member|sys_snode_t snode;
 DECL|sprint_ipaddr|function|static char *sprint_ipaddr(char *buf, int buflen, const struct sockaddr *addr)
-DECL|ssl_received|function|static void ssl_received(struct net_context *context, struct net_pkt *pkt, int status, void *user_data)
-DECL|ssl_rx|function|static int ssl_rx(void *context, unsigned char *buf, size_t size)
-DECL|ssl_sent|function|static void ssl_sent(struct net_context *context, int status, void *token, void *user_data)
-DECL|ssl_tx|function|static int ssl_tx(void *context, const unsigned char *buf, size_t size)
+DECL|url_connected|function|static void url_connected(struct http_ctx *ctx, enum http_connection_type type)
+DECL|validate_state_transition|function|static void validate_state_transition(struct http_ctx *ctx, enum http_state current, enum http_state new)
