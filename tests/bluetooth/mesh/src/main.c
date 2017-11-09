669464e88dfea29c76d322e3fd5b8775f2882bb4
Johan Hedberg <johan.hedberg@intel.com>
2017-11-09 19:20:01 +0200

    Bluetooth: tests/mesh: Remove manual LPN enabling
    The default of the stack is now to automatically enable LPN
    functionality after a given timeout.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 58b476fb33b0e2b5ebbd45d76ec7e65d4d76ffa5

--- tests/bluetooth/mesh/src/main.c
+++ tests/bluetooth/mesh/src/main.c
@@ -4,7 +4,6 @@ DECL|cfg_srv|variable|cfg_srv
 DECL|comp|variable|comp
 DECL|dev_uuid|variable|dev_uuid
 DECL|elements|variable|elements
-DECL|enable_lpn|function|static void enable_lpn(struct k_work *work)
 DECL|fault_clear|function|static int fault_clear(struct bt_mesh_model *model, uint16_t company_id)
 DECL|fault_get_cur|function|static int fault_get_cur(struct bt_mesh_model *model, u8_t *test_id, u16_t *company_id, u8_t *faults, u8_t *fault_count)
 DECL|fault_get_reg|function|static int fault_get_reg(struct bt_mesh_model *model, u16_t company_id, u8_t *test_id, u8_t *faults, u8_t *fault_count)
@@ -15,7 +14,6 @@ DECL|main|function|void main(void)
 DECL|prov_complete|function|static void prov_complete(void)
 DECL|prov|variable|prov
 DECL|root_models|variable|root_models
-DECL|timer|variable|timer
 DECL|vnd_models|variable|vnd_models
 DECL|vnd_ops|variable|vnd_ops
 DECL|vnd_pub2|variable|vnd_pub2
