39cc260bb75cd6abe4894533a9bbb564c48a968b
Andrew Boie <andrew.p.boie@intel.com>
2017-11-08 16:38:03 -0800

    userspace: add support for dynamic kernel objects
    A red-black tree is maintained containing the metadata for all
    dynamically created kernel objects, which are allocated out of the
    system heap.
    
    Currently, k_object_alloc() and k_object_free() are supervisor-only.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 31bdfc014e786d94608b4405694520f41c5066bf

--- tests/kernel/mem_protect/obj_validation/src/main.c
+++ tests/kernel/mem_protect/obj_validation/src/main.c
@@ -1,5 +1,6 @@
 DECL|SEM_ARRAY_SIZE|macro|SEM_ARRAY_SIZE
 DECL|bad_sem|variable|bad_sem
+DECL|dyn_sem|variable|dyn_sem
 DECL|object_permission_checks|function|void object_permission_checks(struct k_sem *sem, bool skip_init)
 DECL|sem2|variable|sem2
 DECL|sem3|variable|sem3
