9abb079aed04d014c34a919ebf20ac5146291a9e
Johan Hedberg <johan.hedberg@intel.com>
2017-11-10 22:17:40 +0200

    Bluetooth: Mesh: shell: Add basic skeleton
    Add a basic shell skeleton for Mesh, containing basic command for
    initialization, provisioning and reset.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: e2e74f705bba4b723fbf8addb73900eba491480d

---
+++ subsys/bluetooth/host/mesh/shell.c
@@ -0,0 +1,19 @@
+DECL|cfg_srv|variable|cfg_srv
+DECL|cmd_init|function|static int cmd_init(int argc, char *argv[])
+DECL|cmd_input_num|function|static int cmd_input_num(int argc, char *argv[])
+DECL|cmd_input_str|function|static int cmd_input_str(int argc, char *argv[])
+DECL|cmd_reset|function|static int cmd_reset(int argc, char *argv[])
+DECL|comp|variable|comp
+DECL|dev_uuid|variable|dev_uuid
+DECL|elements|variable|elements
+DECL|health_srv|variable|health_srv
+DECL|input_act|variable|input_act
+DECL|input_size|variable|input_size
+DECL|input|function|static int input(bt_mesh_input_action act, u8_t size)
+DECL|mesh_commands|variable|mesh_commands
+DECL|output_number|function|static int output_number(bt_mesh_output_action action, uint32_t number)
+DECL|output_string|function|static int output_string(const char *str)
+DECL|prov_complete|function|static void prov_complete(void)
+DECL|prov|variable|prov
+DECL|root_models|variable|root_models
+DECL|static_val|variable|static_val
