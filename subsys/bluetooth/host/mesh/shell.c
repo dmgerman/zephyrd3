a3274b0f87a58b83b0376ddd251556c83242dba8
Johan Hedberg <johan.hedberg@intel.com>
2017-11-12 17:48:20 +0200

    Bluetooth: Mesh: shell: Add support for getting composition data
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7a1c0b979ef17182465ae21a12ad016a09e10290

--- subsys/bluetooth/host/mesh/shell.c
+++ subsys/bluetooth/host/mesh/shell.c
@@ -1,4 +1,6 @@
+DECL|cfg_cli|variable|cfg_cli
 DECL|cfg_srv|variable|cfg_srv
+DECL|cmd_get_comp|function|static int cmd_get_comp(int argc, char *argv[])
 DECL|cmd_ident|function|static int cmd_ident(int argc, char *argv[])
 DECL|cmd_init|function|static int cmd_init(int argc, char *argv[])
 DECL|cmd_input_num|function|static int cmd_input_num(int argc, char *argv[])
@@ -7,6 +9,7 @@ DECL|cmd_lpn|function|static int cmd_lpn(int argc, char *argv[])
 DECL|cmd_reset|function|static int cmd_reset(int argc, char *argv[])
 DECL|comp|variable|comp
 DECL|dev_uuid|variable|dev_uuid
+DECL|dst|variable|dst
 DECL|elements|variable|elements
 DECL|health_srv|variable|health_srv
 DECL|input_act|variable|input_act
@@ -15,6 +18,7 @@ DECL|input|function|static int input(bt_mesh_input_action act, u8_t size)
 DECL|link_close|function|static void link_close(void)
 DECL|link_open|function|static void link_open(void)
 DECL|mesh_commands|variable|mesh_commands
+DECL|net_idx|variable|net_idx
 DECL|output_number|function|static int output_number(bt_mesh_output_action action, uint32_t number)
 DECL|output_string|function|static int output_string(const char *str)
 DECL|prov_complete|function|static void prov_complete(u16_t addr)
