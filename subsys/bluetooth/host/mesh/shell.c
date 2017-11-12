9b17284aed469781bff774067dab5a1f7925e92d
Johan Hedberg <johan.hedberg@intel.com>
2017-11-12 17:53:23 +0200

    Bluetooth: Mesh: shell: Add "dst" & "netidx" commands
    Add commands to modify the used destination address and NetIdx.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 96934bd3d4da090c9795c6920bcc4ef5125d7dc8

--- subsys/bluetooth/host/mesh/shell.c
+++ subsys/bluetooth/host/mesh/shell.c
@@ -1,11 +1,13 @@
 DECL|cfg_cli|variable|cfg_cli
 DECL|cfg_srv|variable|cfg_srv
+DECL|cmd_dst|function|static int cmd_dst(int argc, char *argv[])
 DECL|cmd_get_comp|function|static int cmd_get_comp(int argc, char *argv[])
 DECL|cmd_ident|function|static int cmd_ident(int argc, char *argv[])
 DECL|cmd_init|function|static int cmd_init(int argc, char *argv[])
 DECL|cmd_input_num|function|static int cmd_input_num(int argc, char *argv[])
 DECL|cmd_input_str|function|static int cmd_input_str(int argc, char *argv[])
 DECL|cmd_lpn|function|static int cmd_lpn(int argc, char *argv[])
+DECL|cmd_netidx|function|static int cmd_netidx(int argc, char *argv[])
 DECL|cmd_reset|function|static int cmd_reset(int argc, char *argv[])
 DECL|comp|variable|comp
 DECL|dev_uuid|variable|dev_uuid
@@ -17,6 +19,7 @@ DECL|input_size|variable|input_size
 DECL|input|function|static int input(bt_mesh_input_action act, u8_t size)
 DECL|link_close|function|static void link_close(void)
 DECL|link_open|function|static void link_open(void)
+DECL|local|variable|local
 DECL|mesh_commands|variable|mesh_commands
 DECL|net_idx|variable|net_idx
 DECL|output_number|function|static int output_number(bt_mesh_output_action action, uint32_t number)
