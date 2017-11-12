380228fbf462e9fb27c422fc0e1ef745c639058d
Johan Hedberg <johan.hedberg@intel.com>
2017-11-13 00:31:29 +0200

    Bluetooth: samples/mesh_demo: Convert to using Configuration Client API
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 6f4d6279833c001505b678c60eb03c4c90d9b9ec

--- samples/bluetooth/mesh_demo/src/main.c
+++ samples/bluetooth/mesh_demo/src/main.c
@@ -1,10 +1,6 @@
 DECL|CID_INTEL|macro|CID_INTEL
 DECL|GROUP_ADDR|macro|GROUP_ADDR
 DECL|MOD_INTEL|macro|MOD_INTEL
-DECL|OP_APP_KEY_ADD|macro|OP_APP_KEY_ADD
-DECL|OP_HEARTBEAT_SUB_SET|macro|OP_HEARTBEAT_SUB_SET
-DECL|OP_MOD_APP_BIND|macro|OP_MOD_APP_BIND
-DECL|OP_MOD_SUB_ADD|macro|OP_MOD_SUB_ADD
 DECL|OP_VENDOR_BUTTON|macro|OP_VENDOR_BUTTON
 DECL|PROVISIONER_ADDR|macro|PROVISIONER_ADDR
 DECL|PROV_ADDR|macro|PROV_ADDR
@@ -18,6 +14,7 @@ DECL|board_button_1_pressed|function|void board_button_1_pressed(void)
 DECL|board_play|function|void board_play(const char *str)
 DECL|board_set_target|function|u16_t board_set_target(void)
 DECL|bt_ready|function|static void bt_ready(int err)
+DECL|cfg_cli|variable|cfg_cli
 DECL|cfg_srv|variable|cfg_srv
 DECL|comp|variable|comp
 DECL|configure|function|static void configure(void)
@@ -27,7 +24,6 @@ DECL|flags|variable|flags
 DECL|health_srv|variable|health_srv
 DECL|heartbeat|function|static void heartbeat(u8_t hops, u16_t feat)
 DECL|iv_index|variable|iv_index
-DECL|key_idx_pack|function|static inline void key_idx_pack(struct net_buf_simple *buf,u16_t idx1, u16_t idx2)
 DECL|main|function|void main(void)
 DECL|net_idx|variable|net_idx
 DECL|net_key|variable|net_key
