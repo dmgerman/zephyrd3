8cccd30009614647a137e5f6382977bba30a7288
Johan Hedberg <johan.hedberg@intel.com>
2017-11-12 17:47:00 +0200

    Bluetooth: Mesh: Add support for sending Get Composition Data
    Add Get Composition Data support to the Configuration Client Model.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 3d1ce43eb0cdaeebc99c1a4a3011b82d8537bfd5

--- include/bluetooth/mesh/cfg_cli.h
+++ include/bluetooth/mesh/cfg_cli.h
@@ -1,3 +1,7 @@
 DECL|BT_MESH_MODEL_CFG_CLI|macro|BT_MESH_MODEL_CFG_CLI
 DECL|__BT_MESH_CFG_CLI_H|macro|__BT_MESH_CFG_CLI_H
-DECL|bt_mesh_cli|struct|struct bt_mesh_cli {
+DECL|bt_mesh_cfg_cli|struct|struct bt_mesh_cfg_cli {
+DECL|model|member|struct bt_mesh_model *model;
+DECL|op_param|member|void *op_param;
+DECL|op_pending|member|u32_t op_pending;
+DECL|op_sync|member|struct k_sem op_sync;
--- subsys/bluetooth/host/mesh/cfg_cli.c
+++ subsys/bluetooth/host/mesh/cfg_cli.c
@@ -1,2 +1,10 @@
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|MSG_TIMEOUT|macro|MSG_TIMEOUT
+DECL|bt_mesh_cfg_cli_init|function|int bt_mesh_cfg_cli_init(struct bt_mesh_model *model, bool primary)
 DECL|bt_mesh_cfg_cli_op|variable|bt_mesh_cfg_cli_op
+DECL|bt_mesh_cfg_comp_data_get|function|int bt_mesh_cfg_comp_data_get(u16_t net_idx, u16_t addr, u8_t page, u8_t *status, struct net_buf_simple *comp)
+DECL|cli|variable|cli
+DECL|comp_data_status|function|static void comp_data_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+DECL|comp_data|struct|struct comp_data {
+DECL|comp|member|struct net_buf_simple *comp;
+DECL|status|member|u8_t *status;
