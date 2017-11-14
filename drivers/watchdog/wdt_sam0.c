c242c3ff469eef60bf43b7d9054822b0b3e10ef2
Michael Hope <mlhx@google.com>
2017-11-14 20:02:55 +0100

    watchdog: add a watchdog driver for the SAM0 family.
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: be8cba4bad5646929e0de41f189dafde962fb9e8

---
+++ drivers/watchdog/wdt_sam0.c
@@ -0,0 +1,13 @@
+DECL|WDT_REGS|macro|WDT_REGS
+DECL|cb|member|void (*cb)(struct device *dev);
+DECL|wdt_sam0_api|variable|wdt_sam0_api
+DECL|wdt_sam0_data|variable|wdt_sam0_data
+DECL|wdt_sam0_dev_data|struct|struct wdt_sam0_dev_data {
+DECL|wdt_sam0_disable|function|static void wdt_sam0_disable(struct device *dev)
+DECL|wdt_sam0_enable|function|static void wdt_sam0_enable(struct device *dev)
+DECL|wdt_sam0_get_config|function|static void wdt_sam0_get_config(struct device *dev, struct wdt_config *config)
+DECL|wdt_sam0_init|function|static int wdt_sam0_init(struct device *dev)
+DECL|wdt_sam0_isr|function|static void wdt_sam0_isr(struct device *dev)
+DECL|wdt_sam0_reload|function|static void wdt_sam0_reload(struct device *dev)
+DECL|wdt_sam0_set_config|function|static int wdt_sam0_set_config(struct device *dev, struct wdt_config *config)
+DECL|wdt_sam0_wait_synchronization|function|static void wdt_sam0_wait_synchronization(void)
