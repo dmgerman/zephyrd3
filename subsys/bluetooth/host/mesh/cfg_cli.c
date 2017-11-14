84f211e42df7f339d6f8578bed9cb2e3a1578780
Johan Hedberg <johan.hedberg@intel.com>
2017-11-14 10:46:22 +0200

    Bluetooth: Mesh: Add support for sending Heartbeat Publication Set
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 5e3804b11d9c4e574cbffd4363bf1dd9a5f1957b

--- subsys/bluetooth/host/mesh/cfg_cli.c
+++ subsys/bluetooth/host/mesh/cfg_cli.c
@@ -14,6 +14,7 @@ DECL|bt_mesh_cfg_friend_get|function|int bt_mesh_cfg_friend_get(u16_t net_idx, u
 DECL|bt_mesh_cfg_friend_set|function|int bt_mesh_cfg_friend_set(u16_t net_idx, u16_t addr, u8_t val, u8_t *status)
 DECL|bt_mesh_cfg_gatt_proxy_get|function|int bt_mesh_cfg_gatt_proxy_get(u16_t net_idx, u16_t addr, u8_t *status)
 DECL|bt_mesh_cfg_gatt_proxy_set|function|int bt_mesh_cfg_gatt_proxy_set(u16_t net_idx, u16_t addr, u8_t val, u8_t *status)
+DECL|bt_mesh_cfg_hb_pub_set|function|int bt_mesh_cfg_hb_pub_set(u16_t net_idx, u16_t addr, u16_t pub_dst, u8_t count, u8_t period, u8_t ttl, u16_t feat, u16_t pub_net_idx, u8_t *status)
 DECL|bt_mesh_cfg_hb_sub_set|function|int bt_mesh_cfg_hb_sub_set(u16_t net_idx, u16_t addr, u16_t src, u16_t dst, u8_t period, u8_t *status)
 DECL|bt_mesh_cfg_mod_app_bind_vnd|function|int bt_mesh_cfg_mod_app_bind_vnd(u16_t net_idx, u16_t addr, u16_t elem_addr, u16_t mod_app_idx, u16_t mod_id, u16_t cid, u8_t *status)
 DECL|bt_mesh_cfg_mod_app_bind|function|int bt_mesh_cfg_mod_app_bind(u16_t net_idx, u16_t addr, u16_t elem_addr, u16_t mod_app_idx, u16_t mod_id, u8_t *status)
@@ -31,12 +32,17 @@ DECL|comp_data_status|function|static void comp_data_status(struct bt_mesh_model
 DECL|comp_data|struct|struct comp_data {
 DECL|comp|member|struct net_buf_simple *comp;
 DECL|count|member|u8_t *count;
+DECL|count|member|u8_t *count;
+DECL|dst|member|u16_t *dst;
 DECL|dst|member|u16_t dst;
 DECL|elem_addr|member|u16_t elem_addr;
 DECL|elem_addr|member|u16_t elem_addr;
+DECL|feat|member|u16_t *feat;
 DECL|friend_status|function|static void friend_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|gatt_proxy_status|function|static void gatt_proxy_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|get_state_u8|function|static int get_state_u8(u16_t net_idx, u16_t addr, u32_t op, u32_t rsp,u8_t *val)
+DECL|hb_pub_param|struct|struct hb_pub_param {
+DECL|hb_pub_status|function|static void hb_pub_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|hb_sub_param|struct|struct hb_sub_param {
 DECL|hb_sub_status|function|static void hb_sub_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|max|member|u8_t *max;
@@ -50,7 +56,9 @@ DECL|mod_id|member|u16_t mod_id;
 DECL|mod_sub_add|function|int mod_sub_add(u16_t net_idx, u16_t addr, u16_t elem_addr, u16_t sub_addr,u16_t mod_id, bool vnd, u16_t cid, u8_t *status)
 DECL|mod_sub_param|struct|struct mod_sub_param {
 DECL|mod_sub_status|function|static void mod_sub_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+DECL|net_idx|member|u16_t *net_idx;
 DECL|net_idx|member|u16_t net_idx;
+DECL|period|member|u8_t *period;
 DECL|period|member|u8_t *period;
 DECL|relay_param|struct|struct relay_param {
 DECL|relay_status|function|static void relay_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
@@ -63,8 +71,10 @@ DECL|status|member|u8_t *status;
 DECL|status|member|u8_t *status;
 DECL|status|member|u8_t *status;
 DECL|status|member|u8_t *status;
+DECL|status|member|u8_t *status;
 DECL|sub_addr|member|u16_t sub_addr;
 DECL|transmit|member|u8_t *transmit;
 DECL|ttl_status|function|static void ttl_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+DECL|ttl|member|u8_t *ttl;
 DECL|vnd|member|bool vnd;
 DECL|vnd|member|bool vnd;
