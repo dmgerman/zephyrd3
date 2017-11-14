a9c9b6e7d089bc66ec05b57fa0321af182058a22
Andrew Boie <andrew.p.boie@intel.com>
2017-11-14 12:13:20 -0800

    tests: mheap_api_concept: test k_calloc()
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 02d6c11f1dd9b37bd9fe6156fe10ad85cea3595a

--- tests/kernel/mem_heap/mheap_api_concept/src/test_mheap_api.c
+++ tests/kernel/mem_heap/mheap_api_concept/src/test_mheap_api.c
@@ -1,1 +1,5 @@
+DECL|BOUNDS|macro|BOUNDS
+DECL|NMEMB|macro|NMEMB
+DECL|SIZE|macro|SIZE
+DECL|test_mheap_calloc|function|void test_mheap_calloc(void)
 DECL|test_mheap_malloc_free|function|void test_mheap_malloc_free(void)
