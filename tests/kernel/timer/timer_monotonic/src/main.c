df9219f8b2f07a1f876df66d9615868ed7602659
Punit Vara <punit.vara@intel.com>
2017-11-14 11:25:17 +0530

    tests: timer_monotonic: Migrate legacy test to ztest
    Use ztest macros and apis in legacy test to support ztest
    framework.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 1604a9351f831fff3d89a438b59f82b9b921c4e2

--- tests/kernel/timer/timer_monotonic/src/main.c
+++ tests/kernel/timer/timer_monotonic/src/main.c
@@ -1,2 +1,3 @@
-DECL|main|function|void main(void)
 DECL|test_frequency|function|int test_frequency(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_timer|function|void test_timer(void)
