4555678806a6099b93afaa511dfe66a097408c8b
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2017-11-14 17:22:41 -0500

    drivers: timer: Add Altera shim driver for timer soft IP
    Delete the native timer soft IP driver as we will be reusing
    the Altera's HAL drivers for most of the soft IP's.
    
    Add shim driver support for Altera timer system clock soft IP.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: eb51886d8c49868a200b3f3f765b05c95f2dc28a

---
+++ drivers/timer/altera_avalon_timer_hal.c
@@ -0,0 +1,4 @@
+DECL|_sys_clock_driver_init|function|int _sys_clock_driver_init(struct device *device)
+DECL|_timer_cycle_get_32|function|u32_t _timer_cycle_get_32(void)
+DECL|accumulated_cycle_count|variable|accumulated_cycle_count
+DECL|timer_irq_handler|function|static void timer_irq_handler(void *unused)
