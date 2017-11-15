d126bd84b82697ec7d69c89f54f5fb56d8b06f00
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-11-15 12:45:28 +0200

    tests: net: websocket: Add tests for websocket
    This tests websocket by creating a websocket support http server
    and sending data to it and verifying the returned data is the same.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 4d4c7bf3ea99aa58797537728e6e14a7931584f0

---
+++ tests/net/websocket/src/main.c
@@ -0,0 +1,53 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|app_ctx_v4|variable|app_ctx_v4
+DECL|app_ctx_v6|variable|app_ctx_v6
+DECL|bytes_received|variable|bytes_received
+DECL|failure|variable|failure
+DECL|hdr_len|macro|hdr_len
+DECL|http_msg|variable|http_msg
+DECL|mask_value|variable|mask_value
+DECL|progress|variable|progress
+DECL|recv_cb|function|static void recv_cb(struct net_app_ctx *ctx, struct net_pkt *pkt, int status, void *user_data)
+DECL|server_addr|variable|server_addr
+DECL|test_close|function|static void test_close(struct net_app_ctx *ctx)
+DECL|test_connect|function|static void test_connect(struct net_app_ctx *ctx)
+DECL|test_init|function|void test_init(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_send_multi_msg|function|static void test_send_multi_msg(struct net_app_ctx *ctx)
+DECL|test_send_recv|function|static void test_send_recv(int chunk_size, struct net_app_ctx *ctx)
+DECL|test_v4_close|function|void test_v4_close(void)
+DECL|test_v4_connect|function|void test_v4_connect(void)
+DECL|test_v4_init|function|void test_v4_init(void)
+DECL|test_v4_send_multi_msg|function|void test_v4_send_multi_msg(void)
+DECL|test_v4_send_recv_1|function|void test_v4_send_recv_1(void)
+DECL|test_v4_send_recv_2|function|void test_v4_send_recv_2(void)
+DECL|test_v4_send_recv_3|function|void test_v4_send_recv_3(void)
+DECL|test_v4_send_recv_4|function|void test_v4_send_recv_4(void)
+DECL|test_v4_send_recv_5|function|void test_v4_send_recv_5(void)
+DECL|test_v4_send_recv_6|function|void test_v4_send_recv_6(void)
+DECL|test_v4_send_recv_7|function|void test_v4_send_recv_7(void)
+DECL|test_v4_send_recv_n|function|void test_v4_send_recv_n(int chunk_size)
+DECL|test_v4_send_recv|function|static void test_v4_send_recv(int chunk_size)
+DECL|test_v6_close|function|void test_v6_close(void)
+DECL|test_v6_connect|function|void test_v6_connect(void)
+DECL|test_v6_init|function|void test_v6_init(void)
+DECL|test_v6_send_multi_msg|function|void test_v6_send_multi_msg(void)
+DECL|test_v6_send_recv_1|function|void test_v6_send_recv_1(void)
+DECL|test_v6_send_recv_2|function|void test_v6_send_recv_2(void)
+DECL|test_v6_send_recv_3|function|void test_v6_send_recv_3(void)
+DECL|test_v6_send_recv_4|function|void test_v6_send_recv_4(void)
+DECL|test_v6_send_recv_5|function|void test_v6_send_recv_5(void)
+DECL|test_v6_send_recv_6|function|void test_v6_send_recv_6(void)
+DECL|test_v6_send_recv_7|function|void test_v6_send_recv_7(void)
+DECL|test_v6_send_recv_n|function|void test_v6_send_recv_n(int chunk_size)
+DECL|test_v6_send_recv|function|static void test_v6_send_recv(int chunk_size)
+DECL|timeout|variable|timeout
+DECL|total_data_len|variable|total_data_len
+DECL|wait_data|variable|wait_data
+DECL|ws_mask_payload|function|static void ws_mask_payload(u8_t *payload, size_t payload_len, u32_t masking_value)
+DECL|ws_test_msg_orig|variable|ws_test_msg_orig
+DECL|ws_test_msg|variable|ws_test_msg
+DECL|ws_unmasked_msg|variable|ws_unmasked_msg
---
+++ tests/net/websocket/src/server.c
@@ -0,0 +1,37 @@
+DECL|ADDR_OPTION|macro|ADDR_OPTION
+DECL|ALLOC_TIMEOUT|macro|ALLOC_TIMEOUT
+DECL|APP_BANNER|macro|APP_BANNER
+DECL|CONFIG_NET_APP_TLS_STACK_SIZE|macro|CONFIG_NET_APP_TLS_STACK_SIZE
+DECL|HTML_FOOTER|macro|HTML_FOOTER
+DECL|HTML_HEADER|macro|HTML_HEADER
+DECL|HTTP_STATUS_200_OK_CSS|macro|HTTP_STATUS_200_OK_CSS
+DECL|HTTP_STATUS_200_OK|macro|HTTP_STATUS_200_OK
+DECL|INSTANCE_INFO|macro|INSTANCE_INFO
+DECL|MAX_BUF_LEN|macro|MAX_BUF_LEN
+DECL|MAX_URL_LEN|macro|MAX_URL_LEN
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|RESULT_BUF_SIZE|macro|RESULT_BUF_SIZE
+DECL|RX_FIFO_DEPTH|macro|RX_FIFO_DEPTH
+DECL|SEND_TIMEOUT|macro|SEND_TIMEOUT
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|data_tcp_pool|function|static struct net_buf_pool *data_tcp_pool(void)
+DECL|data_tcp_pool|macro|data_tcp_pool
+DECL|default_handler|function|static enum http_verdict default_handler(struct http_ctx *ctx, enum http_connection_type type, const struct sockaddr *dst)
+DECL|echo_apps_cert_der|variable|echo_apps_cert_der
+DECL|echo_apps_key_der|variable|echo_apps_key_der
+DECL|get_string|function|static const char *get_string(int str_len, const char *str)
+DECL|http_ctx|variable|http_ctx
+DECL|http_urls|variable|http_urls
+DECL|result|variable|result
+DECL|setup_cert|function|static int setup_cert(struct net_app_ctx *ctx, mbedtls_x509_crt *cert, mbedtls_pk_context *pkey)
+DECL|test_websocket_cleanup_server|function|void test_websocket_cleanup_server(void)
+DECL|test_websocket_init_server|function|void test_websocket_init_server(void)
+DECL|tx_tcp_slab|function|static struct k_mem_slab *tx_tcp_slab(void)
+DECL|tx_tcp_slab|macro|tx_tcp_slab
+DECL|ws_closed|function|static void ws_closed(struct http_ctx *ctx, int status, void *user_data)
+DECL|ws_connected|function|static void ws_connected(struct http_ctx *ctx, enum http_connection_type type, const struct sockaddr *dst, void *user_data)
+DECL|ws_ctx|variable|ws_ctx
+DECL|ws_received|function|static void ws_received(struct http_ctx *ctx,struct net_pkt *pkt, int status, u32_t flags, const struct sockaddr *dst,
+DECL|ws_sent|function|static void ws_sent(struct http_ctx *ctx, int status, void *user_data_send, void *user_data)
+DECL|ws_works|function|static int ws_works(struct http_ctx *ctx, const struct sockaddr *dst)
