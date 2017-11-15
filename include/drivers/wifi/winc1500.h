1217ad79d004343ba4b63ab6bb4edca748db7602
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-11-15 14:26:45 +0100

    drivers/wifi: WINC1500 driver is not using WAKE pin
    It does not seem to be useful atm. So let's remove it.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 24bb24e696124a52a90e926bb347aaf85e08f7bf

--- include/drivers/wifi/winc1500.h
+++ include/drivers/wifi/winc1500.h
@@ -2,7 +2,6 @@ DECL|WINC1500_GPIO_IDX_CHIP_EN|enumerator|WINC1500_GPIO_IDX_CHIP_EN = 0,
 DECL|WINC1500_GPIO_IDX_IRQN|enumerator|WINC1500_GPIO_IDX_IRQN,
 DECL|WINC1500_GPIO_IDX_MAX|enumerator|WINC1500_GPIO_IDX_MAX
 DECL|WINC1500_GPIO_IDX_RESET_N|enumerator|WINC1500_GPIO_IDX_RESET_N,
-DECL|WINC1500_GPIO_IDX_WAKE|enumerator|WINC1500_GPIO_IDX_WAKE,
 DECL|dev|member|struct device *dev;
 DECL|pin|member|u32_t pin;
 DECL|winc1500_gpio_configuration|struct|struct winc1500_gpio_configuration {
