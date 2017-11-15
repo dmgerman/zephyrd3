f735a9c9ce8865944f8803544e87d44b577a5ec5
Johan Hedberg <johan.hedberg@intel.com>
2017-11-15 12:39:15 +0200

    shell: Redesign internal command handling
    Make the internal commands (exit, select & help) as any other
    commands, so that e.g. "help help" works as expected. Also redesign
    the way commands are looked up to avoid duplicate lookups.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 22b5ff9b298e866200dd414a120c40b7531f539b

--- subsys/shell/shell.c
+++ subsys/shell/shell.c
@@ -10,22 +10,23 @@ DECL|app_cmd_handler|variable|app_cmd_handler
 DECL|app_prompt_handler|variable|app_prompt_handler
 DECL|avail_queue|variable|avail_queue
 DECL|buf|variable|buf
+DECL|cmd_exit|function|static int cmd_exit(int argc, char *argv[])
+DECL|cmd_help|function|static int cmd_help(int argc, char *argv[])
+DECL|cmd_select|function|static int cmd_select(int argc, char *argv[])
 DECL|cmds_queue|variable|cmds_queue
 DECL|completion|function|static u8_t completion(char *line, u8_t len)
 DECL|default_module_prompt|variable|default_module_prompt
 DECL|default_module|variable|default_module
-DECL|exit_module|function|static int exit_module(int argc, char *argv[])
-DECL|get_cb|function|static shell_cmd_function_t get_cb(int module, const char *command)
-DECL|get_command_and_module|function|static const char *get_command_and_module(char *argv[], int *module)
+DECL|get_cmd|function|static const struct shell_cmd *get_cmd(const struct shell_cmd cmds[], const char *cmd_str)
 DECL|get_command_to_complete|function|static int get_command_to_complete(char *str, char **command_prefix)
 DECL|get_destination_module|function|static int get_destination_module(const char *module_str)
-DECL|get_internal|function|static shell_cmd_function_t get_internal(const char *command)
+DECL|get_internal|function|static const struct shell_cmd *get_internal(const char *command)
+DECL|get_mod_cmd|function|static const struct shell_cmd *get_mod_cmd(int module, const char *cmd_str)
 DECL|get_prompt|function|static const char *get_prompt(void)
 DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t size)
 DECL|line_queue_init|function|static void line_queue_init(void)
 DECL|print_module_commands|function|static void print_module_commands(const int module)
 DECL|prompt|variable|prompt
-DECL|select_module|function|static int select_module(int argc, char *argv[])
 DECL|set_default_module|function|static int set_default_module(const char *name)
 DECL|shell_exec|function|int shell_exec(char *line)
 DECL|shell_init|function|void shell_init(const char *str)
@@ -34,5 +35,4 @@ DECL|shell_register_default_module|function|void shell_register_default_module(c
 DECL|shell_register_prompt_handler|function|void shell_register_prompt_handler(shell_prompt_function_t handler)
 DECL|shell_thread|variable|shell_thread
 DECL|shell|function|static void shell(void *p1, void *p2, void *p3)
-DECL|show_cmd_help|function|static int show_cmd_help(char *argv[], bool full)
-DECL|show_help|function|static int show_help(int argc, char *argv[])
+DECL|show_cmd_help|function|static int show_cmd_help(const struct shell_cmd *cmd, bool full)
