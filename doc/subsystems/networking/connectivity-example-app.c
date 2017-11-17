2e9abee602495577057525d00b1c343023154532
Ding Tao <miyatsu@qq.com>
2017-11-17 22:26:32 +0800

    doc: Replace nano_sem with k_sem
    The struct nano_sem is obsolete, so replace with struct k_sem.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>
    
    Former-commit-id: fddcfa73d44199eb1346f89b2fb32f5b8aaacf99

--- doc/subsystems/networking/connectivity-example-app.c
+++ doc/subsystems/networking/connectivity-example-app.c
@@ -12,7 +12,6 @@ DECL|in6addr_my|variable|in6addr_my
 DECL|init_app|function|static inline void init_app(void)
 DECL|main|function|void main(void)
 DECL|my_addr6|variable|my_addr6
-DECL|quit_lock|variable|quit_lock
 DECL|quit|function|static inline void quit(void)
 DECL|receive_data|function|static int receive_data(void)
 DECL|ret|variable|ret
@@ -20,3 +19,4 @@ DECL|set_dst_addr|function|static inline void set_dst_addr(sa_family_t family,st
 DECL|udp_received|function|static void udp_received(struct net_context *context, struct net_buf *buf, int status, void *user_data)
 DECL|udp_recv|function|static struct net_buf *udp_recv(const char *name,struct net_context *context, struct net_buf *buf)
 DECL|udp_sent|function|static inline void udp_sent(struct net_context *context, int status, void *token, void *user_data)
+DECL|waiter|variable|waiter
