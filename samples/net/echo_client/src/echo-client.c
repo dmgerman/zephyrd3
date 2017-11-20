4ec2fabb28abee21f506b2c71a083d302180e15c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-11-20 14:28:28 +0200

    samples: net: echo_client: Only send allowable number of bytes
    Honor the device MTU when sending data.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9d99691e7bf927f3dc613274e2faa37be7ef39fc

--- samples/net/echo_client/src/echo-client.c
+++ samples/net/echo_client/src/echo-client.c
@@ -8,6 +8,6 @@ DECL|ipsum_len|variable|ipsum_len
 DECL|lorem_ipsum|variable|lorem_ipsum
 DECL|main|function|void main(void)
 DECL|panic|function|void panic(const char *msg)
-DECL|prepare_send_pkt|function|struct net_pkt *prepare_send_pkt(struct net_app_ctx *ctx, const char *name, int expecting_len)
+DECL|prepare_send_pkt|function|struct net_pkt *prepare_send_pkt(struct net_app_ctx *ctx, const char *name, int *expecting_len)
 DECL|quit_lock|variable|quit_lock
 DECL|tcp_ready|variable|tcp_ready
