f090d73f0156b3e57a02bdc6a0b324ee86840735
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-11-22 20:49:01 +0100

    net/ieee802154: Introduce helpers to call radio api function
    Basically to make code nicer and more readable.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 6f51ac7f2b484c293d0e118220e0bfd7ff27023d

--- subsys/net/ip/l2/ieee802154/ieee802154_utils.h
+++ subsys/net/ip/l2/ieee802154/ieee802154_utils.h
@@ -1,5 +1,12 @@
 DECL|__IEEE802154_UTILS_H__|macro|__IEEE802154_UTILS_H__
+DECL|ieee802154_cca|function|static inline int ieee802154_cca(struct net_if *iface)
 DECL|ieee802154_filter_ieee_addr|function|static inline void ieee802154_filter_ieee_addr(struct net_if *iface, u8_t *ieee_addr)
 DECL|ieee802154_filter_pan_id|function|static inline void ieee802154_filter_pan_id(struct net_if *iface, u16_t pan_id)
 DECL|ieee802154_filter_short_addr|function|static inline void ieee802154_filter_short_addr(struct net_if *iface,u16_t short_addr)
+DECL|ieee802154_get_hw_capabilities|function|enum ieee802154_hw_caps ieee802154_get_hw_capabilities(struct net_if *iface)
+DECL|ieee802154_set_channel|function|static inline int ieee802154_set_channel(struct net_if *iface, u16_t channel)
+DECL|ieee802154_set_tx_power|function|static inline int ieee802154_set_tx_power(struct net_if *iface, s16_t dbm)
+DECL|ieee802154_start|function|static inline int ieee802154_start(struct net_if *iface)
+DECL|ieee802154_stop|function|static inline int ieee802154_stop(struct net_if *iface)
+DECL|ieee802154_tx|function|static inline int ieee802154_tx(struct net_if *iface,struct net_pkt *pkt, struct net_buf *frag)
 DECL|ieee802154_verify_channel|function|static inline bool ieee802154_verify_channel(struct device *dev, u16_t channel)
