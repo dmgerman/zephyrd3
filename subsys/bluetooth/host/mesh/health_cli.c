4d6d0b3b6e9f28476be9ca5dc903a8eeb92b8377
Johan Hedberg <johan.hedberg@intel.com>
2017-11-22 19:05:51 +0200

    Bluetooth: Mesh: Add support for Health Attention messages
    Add support for sending Health Attention messages, as well as commands
    to use these new APIs from the shell.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: d588850c0c0f0200f8bc04912cc809c0404f94d8

--- subsys/bluetooth/host/mesh/health_cli.c
+++ subsys/bluetooth/host/mesh/health_cli.c
@@ -1,4 +1,7 @@
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|attention|member|u8_t *attention;
+DECL|bt_mesh_health_attention_get|function|int bt_mesh_health_attention_get(u16_t net_idx, u16_t addr, u16_t app_idx, u8_t *attention)
+DECL|bt_mesh_health_attention_set|function|int bt_mesh_health_attention_set(u16_t net_idx, u16_t addr, u16_t app_idx, u8_t attention, u8_t *updated_attention)
 DECL|bt_mesh_health_cli_init|function|int bt_mesh_health_cli_init(struct bt_mesh_model *model, bool primary)
 DECL|bt_mesh_health_cli_op|variable|bt_mesh_health_cli_op
 DECL|bt_mesh_health_cli_set|function|int bt_mesh_health_cli_set(struct bt_mesh_model *model)
@@ -16,6 +19,8 @@ DECL|divisor|member|u8_t *divisor;
 DECL|expect_test_id|member|u8_t *expect_test_id;
 DECL|fault_count|member|size_t *fault_count;
 DECL|faults|member|u8_t *faults;
+DECL|health_attention_param|struct|struct health_attention_param {
+DECL|health_attention_status|function|static void health_attention_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|health_cli|variable|health_cli
 DECL|health_current_status|function|static void health_current_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|health_fault_param|struct|struct health_fault_param {
