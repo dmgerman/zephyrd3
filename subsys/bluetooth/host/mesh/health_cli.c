4ddad515330da1f2053fe0bb14218d3668675560
Johan Hedberg <johan.hedberg@intel.com>
2017-11-22 18:16:42 +0200

    Bluetooth: Mesh: Add support for Health Period messages
    Add support for sending Health Period messages, as well as commands to
    use these new APIs from the shell.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7e8f9afee524241e161af47749150c84ed7ee80c

--- subsys/bluetooth/host/mesh/health_cli.c
+++ subsys/bluetooth/host/mesh/health_cli.c
@@ -7,8 +7,11 @@ DECL|bt_mesh_health_cli_timeout_set|function|void bt_mesh_health_cli_timeout_set
 DECL|bt_mesh_health_fault_clear|function|int bt_mesh_health_fault_clear(u16_t net_idx, u16_t addr, u16_t app_idx, u16_t cid, u8_t *test_id, u8_t *faults, size_t *fault_count)
 DECL|bt_mesh_health_fault_get|function|int bt_mesh_health_fault_get(u16_t net_idx, u16_t addr, u16_t app_idx, u16_t cid, u8_t *test_id, u8_t *faults, size_t *fault_count)
 DECL|bt_mesh_health_fault_test|function|int bt_mesh_health_fault_test(u16_t net_idx, u16_t addr, u16_t app_idx, u16_t cid, u8_t test_id, u8_t *faults, size_t *fault_count)
+DECL|bt_mesh_health_period_get|function|int bt_mesh_health_period_get(u16_t net_idx, u16_t addr, u16_t app_idx, u8_t *divisor)
+DECL|bt_mesh_health_period_set|function|int bt_mesh_health_period_set(u16_t net_idx, u16_t addr, u16_t app_idx, u8_t divisor, u8_t *updated_divisor)
 DECL|check_cli|function|static int check_cli(void)
 DECL|cid|member|u16_t cid;
+DECL|divisor|member|u8_t *divisor;
 DECL|expect_test_id|member|u8_t *expect_test_id;
 DECL|fault_count|member|size_t *fault_count;
 DECL|faults|member|u8_t *faults;
@@ -16,5 +19,7 @@ DECL|health_cli|variable|health_cli
 DECL|health_current_status|function|static void health_current_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|health_fault_param|struct|struct health_fault_param {
 DECL|health_fault_status|function|static void health_fault_status(struct bt_mesh_model *model,struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
+DECL|health_period_param|struct|struct health_period_param {
+DECL|health_period_status|function|static void health_period_status(struct bt_mesh_model *model, struct bt_mesh_msg_ctx *ctx, struct net_buf_simple *buf)
 DECL|msg_timeout|variable|msg_timeout
 DECL|test_id|member|u8_t *test_id;
