0e4345eeccb6df153b52392415a7511292215fc9
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-11-22 17:29:31 +0100

    net/ieee802154: Do not set the device UP by default
    It will be up to the user to configure a valid channel, through
    net_mgmt, and call net_if_up() in order to get the device up.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 9078378bed7f2601b1509947dc046a2450a5bab3

--- subsys/net/ip/l2/ieee802154/ieee802154.c
+++ subsys/net/ip/l2/ieee802154/ieee802154.c
@@ -6,6 +6,7 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|TX_PKT_TITLE|macro|TX_PKT_TITLE
 DECL|ieee802154_acknowledge|function|static inline void ieee802154_acknowledge(struct net_if *iface, struct ieee802154_mpdu *mpdu)
 DECL|ieee802154_acknowledge|macro|ieee802154_acknowledge
+DECL|ieee802154_enable|function|static int ieee802154_enable(struct net_if *iface, bool state)
 DECL|ieee802154_init|function|void ieee802154_init(struct net_if *iface)
 DECL|ieee802154_manage_recv_packet|function|enum net_verdict ieee802154_manage_recv_packet(struct net_if *iface, struct net_pkt *pkt)
 DECL|ieee802154_manage_recv_packet|macro|ieee802154_manage_recv_packet
