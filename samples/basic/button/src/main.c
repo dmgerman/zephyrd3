c787fd765344875090888add122c43937e01a00f
Anders Pitman <tapitman11@gmail.com>
2017-11-24 15:16:57 -0700

    samples: button: Fix button sample GPIO for nRF52
    Applies to #4008 and #5159 for this board. Problem is that the
    button interrupt callback was only firing once. Solution is to
    set the pin pull up flag to GPIO_PUD_PULL_UP.
    
    Signed-off-by: Anders Pitman <tapitman11@gmail.com>
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: dbba22397c439ff4675de61407635ae4b5e5ac38

--- samples/basic/button/src/main.c
+++ samples/basic/button/src/main.c
@@ -3,6 +3,7 @@ DECL|EDGE|macro|EDGE
 DECL|PIN|macro|PIN
 DECL|PORT|macro|PORT
 DECL|PULL_UP|macro|PULL_UP
+DECL|PULL_UP|macro|PULL_UP
 DECL|SLEEP_TIME|macro|SLEEP_TIME
 DECL|button_pressed|function|void button_pressed(struct device *gpiob, struct gpio_callback *cb, u32_t pins)
 DECL|gpio_cb|variable|gpio_cb
