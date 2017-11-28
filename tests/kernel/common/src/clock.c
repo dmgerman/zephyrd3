30e82f284ae5096ae284e63db1a8d9bc72b06cde
Alberto Escolar Piedras <alpi@oticon.com>
2017-11-28 16:03:05 +0100

    tests: kernel/common : fix for native
    fix in busy waits in test/kernel/common for the POSIX
    arch
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: ebd87d872d6354c153c4f2c9ac604fdddd884b7d

--- tests/kernel/common/src/clock.c
+++ tests/kernel/common/src/clock.c
@@ -1,4 +1,5 @@
 DECL|ALIGN_MS_BOUNDARY|macro|ALIGN_MS_BOUNDARY
+DECL|ALIGN_MS_BOUNDARY|macro|ALIGN_MS_BOUNDARY
 DECL|clock_test|function|void clock_test(void)
 DECL|tclock_cycle|function|static void tclock_cycle(void)
 DECL|tclock_uptime|function|static void tclock_uptime(void)
