7315b9eb55612c97a7cc8fe5c5852e06388792ad
Robert Chou <robert.ch.chou@acer.com>
2017-11-28 11:51:36 +0800

    net: lwm2m: report attributes on discover op
    Since we've added storing notification attributes written by server.
    We can now append these attributes as part of link-format for discover
    op.
    
    Signed-off-by: Robert Chou <robert.ch.chou@acer.com>
    
    Former-commit-id: b0e7a039ee0fa47606befe78859081323141324c

--- subsys/net/lib/lwm2m/lwm2m_engine.c
+++ subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -137,6 +137,7 @@ DECL|observe_node|struct|struct observe_node {
 DECL|path|member|struct lwm2m_obj_path path;
 DECL|pmax|member|s32_t pmax;
 DECL|pmin|member|s32_t pmin;
+DECL|print_attr|function|static int print_attr(struct net_pkt *pkt, char *buf, u16_t buflen, sys_slist_t *attr_list)
 DECL|retransmit_request|function|static void retransmit_request(struct k_work *work)
 DECL|select_reader|function|static u16_t select_reader(struct lwm2m_input_context *in, u16_t format)
 DECL|select_writer|function|static u16_t select_writer(struct lwm2m_output_context *out, u16_t accept)
