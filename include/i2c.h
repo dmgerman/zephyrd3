38023f72e6b6a5f9ad6a5e1836b635e60525f85d
Neil Armstrong <narmstrong@baylibre.com>
2017-11-30 16:36:38 +0100

    i2c: Add new I2C Slave syscalls
    This patchs adds new I2C Syscalls for :
    - I2C controllers slave support
    - I2C Slave drivers support
    
    I2C Controllers slave support adds 2 new ops to :
    - register new slave driver
    - unregister slave driver
    
    Slave funcs consists on all the I2C phases :
    - read or write request once address matches
    - read or write done once the byte has been received/sent
    - stop when the trasmit stops
    
    I2C Slave drivers syscall are also added to make new
    "I2C Slave" drivers to :
    - register them to their I2C controller
    - unregister them to their I2C controller
    
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    
    Former-commit-id: d55a6aa32da18b0ba2214b0d30f8c907bf4c4f53

--- include/i2c.h
+++ include/i2c.h
@@ -9,6 +9,7 @@ DECL|I2C_MSG_RESTART|macro|I2C_MSG_RESTART
 DECL|I2C_MSG_RW_MASK|macro|I2C_MSG_RW_MASK
 DECL|I2C_MSG_STOP|macro|I2C_MSG_STOP
 DECL|I2C_MSG_WRITE|macro|I2C_MSG_WRITE
+DECL|I2C_SLAVE_FLAGS_ADDR_10_BITS|macro|I2C_SLAVE_FLAGS_ADDR_10_BITS
 DECL|I2C_SPEED_FAST_PLUS|macro|I2C_SPEED_FAST_PLUS
 DECL|I2C_SPEED_FAST|macro|I2C_SPEED_FAST
 DECL|I2C_SPEED_GET|macro|I2C_SPEED_GET
@@ -21,15 +22,26 @@ DECL|I2C_SPEED_ULTRA|macro|I2C_SPEED_ULTRA
 DECL|__DRIVERS_I2C_H|macro|__DRIVERS_I2C_H
 DECL|__bits|struct|struct __bits {
 DECL|_impl_i2c_configure|function|static inline int _impl_i2c_configure(struct device *dev, u32_t dev_config)
+DECL|_impl_i2c_slave_driver_register|function|static inline int _impl_i2c_slave_driver_register(struct device *dev)
+DECL|_impl_i2c_slave_driver_unregister|function|static inline int _impl_i2c_slave_driver_unregister(struct device *dev)
+DECL|_impl_i2c_slave_register|function|static inline int _impl_i2c_slave_register(struct device *dev, struct i2c_slave_config *cfg)
+DECL|_impl_i2c_slave_unregister|function|static inline int _impl_i2c_slave_unregister(struct device *dev, struct i2c_slave_config *cfg)
 DECL|_impl_i2c_transfer|function|static inline int _impl_i2c_transfer(struct device *dev, struct i2c_msg *msgs, u8_t num_msgs, u16_t addr)
+DECL|address|member|u16_t address;
 DECL|bits|member|} bits;
 DECL|buf|member|u8_t *buf;
+DECL|callbacks|member|const struct i2c_slave_callbacks *callbacks;
 DECL|configure|member|i2c_api_configure_t configure;
 DECL|dev_config|union|union __deprecated dev_config {
+DECL|driver_register|member|i2c_slave_api_register_t driver_register;
+DECL|driver_unregister|member|i2c_slave_api_unregister_t driver_unregister;
+DECL|flags|member|u8_t flags;
 DECL|flags|member|u8_t flags;
 DECL|i2c_addr|member|u16_t i2c_addr;
 DECL|i2c_api_configure_t|typedef|typedef int (*i2c_api_configure_t)(struct device *dev,
 DECL|i2c_api_full_io_t|typedef|typedef int (*i2c_api_full_io_t)(struct device *dev,
+DECL|i2c_api_slave_register_t|typedef|typedef int (*i2c_api_slave_register_t)(struct device *dev,
+DECL|i2c_api_slave_unregister_t|typedef|typedef int (*i2c_api_slave_unregister_t)(struct device *dev,
 DECL|i2c_burst_read16|function|static inline int i2c_burst_read16(struct device *dev, u16_t dev_addr, u16_t start_addr, u8_t *buf, u32_t num_bytes)
 DECL|i2c_burst_read_addr|function|static inline int i2c_burst_read_addr(struct device *dev, u16_t dev_addr, u8_t *start_addr, const u8_t addr_size, u8_t *buf, u32_t num_bytes)
 DECL|i2c_burst_read|function|static inline int i2c_burst_read(struct device *dev, u16_t dev_addr, u8_t start_addr, u8_t *buf, u32_t num_bytes)
@@ -50,11 +62,29 @@ DECL|i2c_reg_update_byte|function|static inline int i2c_reg_update_byte(struct d
 DECL|i2c_reg_write16|function|static inline int i2c_reg_write16(struct device *dev, u16_t dev_addr, u16_t reg_addr, u8_t value)
 DECL|i2c_reg_write_addr|function|static inline int i2c_reg_write_addr(struct device *dev, u16_t dev_addr, u8_t *reg_addr, const u8_t addr_size, u8_t value)
 DECL|i2c_reg_write_byte|function|static inline int i2c_reg_write_byte(struct device *dev, u16_t dev_addr, u8_t reg_addr, u8_t value)
+DECL|i2c_slave_api_register_t|typedef|typedef int (*i2c_slave_api_register_t)(struct device *dev);
+DECL|i2c_slave_api_unregister_t|typedef|typedef int (*i2c_slave_api_unregister_t)(struct device *dev);
+DECL|i2c_slave_callbacks|struct|struct i2c_slave_callbacks {
+DECL|i2c_slave_config|struct|struct i2c_slave_config {
+DECL|i2c_slave_driver_api|struct|struct i2c_slave_driver_api {
+DECL|i2c_slave_read_processed_cb_t|typedef|typedef int (*i2c_slave_read_processed_cb_t)(
+DECL|i2c_slave_read_requested_cb_t|typedef|typedef int (*i2c_slave_read_requested_cb_t)(
+DECL|i2c_slave_stop_cb_t|typedef|typedef int (*i2c_slave_stop_cb_t)(struct i2c_slave_config *config);
+DECL|i2c_slave_write_received_cb_t|typedef|typedef int (*i2c_slave_write_received_cb_t)(
+DECL|i2c_slave_write_requested_cb_t|typedef|typedef int (*i2c_slave_write_requested_cb_t)(
 DECL|i2c_write|function|static inline int i2c_write(struct device *dev, u8_t *buf, u32_t num_bytes, u16_t addr)
 DECL|is_master_device|member|u32_t is_master_device : 1;
 DECL|len|member|u32_t len;
+DECL|node|member|sys_snode_t node;
 DECL|raw|member|u32_t raw;
+DECL|read_processed|member|i2c_slave_read_processed_cb_t read_processed;
+DECL|read_requested|member|i2c_slave_read_requested_cb_t read_requested;
 DECL|reserved|member|u32_t reserved : 26;
+DECL|slave_register|member|i2c_api_slave_register_t slave_register;
+DECL|slave_unregister|member|i2c_api_slave_unregister_t slave_unregister;
 DECL|speed|member|u32_t speed : 3;
+DECL|stop|member|i2c_slave_stop_cb_t stop;
 DECL|transfer|member|i2c_api_full_io_t transfer;
 DECL|use_10_bit_addr|member|u32_t use_10_bit_addr : 1;
+DECL|write_received|member|i2c_slave_write_received_cb_t write_received;
+DECL|write_requested|member|i2c_slave_write_requested_cb_t write_requested;
