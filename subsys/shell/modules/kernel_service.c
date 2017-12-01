a71dcf25e4ce6e7f230712e3a9cc73fe40fd1454
Anas Nashif <anas.nashif@intel.com>
2017-12-01 15:44:26 -0500

    shell: kernel: rename tasks -> threads
    Rename tasks -> threads, task was the common name for threads before the
    unified kernel was introduced.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 16424cf0bc602dcbe55b2b4b3121bc40d6d6f5a7

--- subsys/shell/modules/kernel_service.c
+++ subsys/shell/modules/kernel_service.c
@@ -2,6 +2,6 @@ DECL|SHELL_KERNEL|macro|SHELL_KERNEL
 DECL|kernel_commands|variable|kernel_commands
 DECL|shell_cmd_cycles|function|static int shell_cmd_cycles(int argc, char *argv[])
 DECL|shell_cmd_stack|function|static int shell_cmd_stack(int argc, char *argv[])
-DECL|shell_cmd_tasks|function|static int shell_cmd_tasks(int argc, char *argv[])
+DECL|shell_cmd_threads|function|static int shell_cmd_threads(int argc, char *argv[])
 DECL|shell_cmd_uptime|function|static int shell_cmd_uptime(int argc, char *argv[])
 DECL|shell_cmd_version|function|static int shell_cmd_version(int argc, char *argv[])
