e398138bd69c919e7b23a173fcbd5474dc3e3efd
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2017-12-04 17:24:08 +0200

    drivers: eth_mcux: Implement IPv6 multicast group joining/leaving
    IPv6 mcast addr to MAC mcast conversion was factored out to
    subsys/net/ip/l2/ethernet.c for reuse by other drivers.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: 2a795a19ffc5ca7d05c92a18ce314ac089fdba2b

--- subsys/net/ip/l2/ethernet.c
+++ subsys/net/ip/l2/ethernet.c
@@ -9,5 +9,6 @@ DECL|ethernet_send|function|static enum net_verdict ethernet_send(struct net_if 
 DECL|ethernet_update_length|function|static inline void ethernet_update_length(struct net_if *iface, struct net_pkt *pkt)
 DECL|multicast_eth_addr|variable|multicast_eth_addr
 DECL|net_eth_broadcast_addr|function|const struct net_eth_addr *net_eth_broadcast_addr(void)
+DECL|net_eth_ipv6_mcast_to_mac_addr|function|void net_eth_ipv6_mcast_to_mac_addr(const struct in6_addr *ipv6_addr, struct net_eth_addr *mac_addr)
 DECL|print_ll_addrs|macro|print_ll_addrs
 DECL|print_ll_addrs|macro|print_ll_addrs
