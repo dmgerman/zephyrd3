babd6f22944af68756cf69a0518045b205110155
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2017-12-04 13:17:42 +0100

    net/mgmt/wifi: Add dedicated net mgmt hooks for WiFi offload devices
    Exposing connect, disconnect and scan for now.
    
    In case the iface is an instance of a WiFi offload device, the way it
    manages scanning, connecting and disconnecting will be specific to that
    device (not the mgmt interface obviously). In such case the device will
    have to export relevantly a dedicated bunch of function to serve the
    mgmt interface in a generic way.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: eab3f168fd978c5d7fdf75b05921bd53ba9d128e

--- include/net/wifi_mgmt.h
+++ include/net/wifi_mgmt.h
@@ -1,8 +1,10 @@
 DECL|NET_EVENT_WIFI_CMD_CONNECT_RESULT|enumerator|NET_EVENT_WIFI_CMD_CONNECT_RESULT,
 DECL|NET_EVENT_WIFI_CMD_DISCONNECT_RESULT|enumerator|NET_EVENT_WIFI_CMD_DISCONNECT_RESULT,
+DECL|NET_EVENT_WIFI_CMD_SCAN_DONE|enumerator|NET_EVENT_WIFI_CMD_SCAN_DONE,
 DECL|NET_EVENT_WIFI_CMD_SCAN_RESULT|enumerator|NET_EVENT_WIFI_CMD_SCAN_RESULT = 1,
 DECL|NET_EVENT_WIFI_CONNECT_RESULT|macro|NET_EVENT_WIFI_CONNECT_RESULT
 DECL|NET_EVENT_WIFI_DISCONNECT_RESULT|macro|NET_EVENT_WIFI_DISCONNECT_RESULT
+DECL|NET_EVENT_WIFI_SCAN_DONE|macro|NET_EVENT_WIFI_SCAN_DONE
 DECL|NET_EVENT_WIFI_SCAN_RESULT|macro|NET_EVENT_WIFI_SCAN_RESULT
 DECL|NET_REQUEST_WIFI_CMD_CONNECT|enumerator|NET_REQUEST_WIFI_CMD_CONNECT,
 DECL|NET_REQUEST_WIFI_CMD_DISCONNECT|enumerator|NET_REQUEST_WIFI_CMD_DISCONNECT,
@@ -18,5 +20,26 @@ DECL|_NET_WIFI_CODE|macro|_NET_WIFI_CODE
 DECL|_NET_WIFI_EVENT|macro|_NET_WIFI_EVENT
 DECL|_NET_WIFI_LAYER|macro|_NET_WIFI_LAYER
 DECL|__WIFI_MGMT_H__|macro|__WIFI_MGMT_H__
+DECL|channel|member|u8_t channel;
+DECL|channel|member|u8_t channel;
+DECL|connect|member|int (*connect)(struct device *dev,
+DECL|disconnect|member|int (*disconnect)(struct device *dev);
+DECL|iface_api|member|struct net_if_api iface_api;
 DECL|net_event_wifi_cmd|enum|enum net_event_wifi_cmd {
 DECL|net_request_wifi_cmd|enum|enum net_request_wifi_cmd {
+DECL|net_wifi_mgmt_offload|struct|struct net_wifi_mgmt_offload {
+DECL|psk_length|member|u8_t psk_length; /* Min 8 - Max 64 */
+DECL|psk|member|u8_t *psk;
+DECL|rssi|member|s8_t rssi;
+DECL|scan_result_cb_t|typedef|typedef void (*scan_result_cb_t)(struct net_if *iface, int status,
+DECL|scan|member|int (*scan)(struct device *dev, scan_result_cb_t cb);
+DECL|security|member|enum wifi_security_type security;
+DECL|security|member|enum wifi_security_type security;
+DECL|ssid_length|member|u8_t ssid_length;
+DECL|ssid_length|member|u8_t ssid_length; /* Max 32 */
+DECL|ssid|member|u8_t *ssid;
+DECL|ssid|member|u8_t ssid[WIFI_SSID_MAX_LEN];
+DECL|status|member|int status;
+DECL|wifi_connect_req_params|struct|struct wifi_connect_req_params {
+DECL|wifi_scan_result|struct|struct wifi_scan_result {
+DECL|wifi_status|struct|struct wifi_status {
--- subsys/net/ip/l2/wifi_mgmt.c
+++ subsys/net/ip/l2/wifi_mgmt.c
@@ -1,5 +1,8 @@
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|_scan_result_cb|function|static void _scan_result_cb(struct net_if *iface, int status, struct wifi_scan_result *entry)
 DECL|wifi_connect|function|static int wifi_connect(u32_t mgmt_request, struct net_if *iface,void *data, size_t len)
 DECL|wifi_disconnect|function|static int wifi_disconnect(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
+DECL|wifi_mgmt_raise_connect_result_event|function|void wifi_mgmt_raise_connect_result_event(struct net_if *iface, int status)
+DECL|wifi_mgmt_raise_disconnect_result_event|function|void wifi_mgmt_raise_disconnect_result_event(struct net_if *iface, int status)
 DECL|wifi_scan|function|static int wifi_scan(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
