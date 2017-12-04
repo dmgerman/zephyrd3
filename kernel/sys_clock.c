820e411990ba27322c44c77c36d795150505dd3c
Anas Nashif <anas.nashif@intel.com>
2017-12-04 11:22:31 -0500

    kernel: sys_clock: remove obsolete and unused functions
    Those functions are duplicated and leftovers from migration to unified
    kernel.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 5efb6a1d94ee2f50a275ac187298a075dc8517e2

--- kernel/sys_clock.c
+++ kernel/sys_clock.c
@@ -4,7 +4,6 @@ DECL|_handling_timeouts|variable|_handling_timeouts
 DECL|_impl_k_uptime_get_32|function|u32_t _impl_k_uptime_get_32(void)
 DECL|_impl_k_uptime_get|function|s64_t _impl_k_uptime_get(void)
 DECL|_nano_sys_clock_tick_announce|function|void _nano_sys_clock_tick_announce(s32_t ticks)
-DECL|_nano_tick_delta|function|static ALWAYS_INLINE s64_t _nano_tick_delta(s64_t *reftime)
 DECL|_sys_clock_always_on|variable|_sys_clock_always_on
 DECL|_sys_clock_tick_count|variable|_sys_clock_tick_count
 DECL|_sys_idle_elapsed_ticks|variable|_sys_idle_elapsed_ticks
@@ -26,5 +25,3 @@ DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_hw_cycles_per_tick|variable|sys_clock_hw_cycles_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
 DECL|sys_clock_us_per_tick|variable|sys_clock_us_per_tick
-DECL|sys_tick_delta_32|function|u32_t sys_tick_delta_32(s64_t *reftime)
-DECL|sys_tick_delta|function|s64_t sys_tick_delta(s64_t *reftime)
