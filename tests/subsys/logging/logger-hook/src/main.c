668b923f01137eb620c5078eb7e74cd37e300ee9
Punit Vara <punit.vara@intel.com>
2017-12-04 11:49:14 +0530

    tests: logger-hook: convert sample to ztest and move to test/
    issue: #3302
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 71948788eb92a48e0ec8ef5adc35e9a8937add7c

--- tests/subsys/logging/logger-hook/src/main.c
+++ tests/subsys/logging/logger-hook/src/main.c
@@ -5,7 +5,8 @@ DECL|log_cbuffer|struct|struct log_cbuffer {
 DECL|log_cbuffer|variable|log_cbuffer
 DECL|logger_buffer|variable|logger_buffer
 DECL|logger_put|function|int logger_put(struct log_cbuffer *logger, char *data, u32_t data_size)
-DECL|main|function|void main(void)
 DECL|ring_buf_print|function|static inline void ring_buf_print(struct ring_buf *buf)
 DECL|ring_buffer|member|struct ring_buf ring_buffer;
+DECL|test_logging|function|void test_logging(void)
+DECL|test_main|function|void test_main(void)
 DECL|vlog_cbuf_put|function|void vlog_cbuf_put(const char *format, va_list args)
