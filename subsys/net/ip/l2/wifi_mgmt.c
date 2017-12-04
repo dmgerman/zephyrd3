6eb52bd310f57d1925d0f287e097aa54249ee2bf
Patrik Flykt <patrik.flykt@intel.com>
2017-12-04 12:17:39 +0200

    net/mgmt: Add initial WiFi management API definitions
    Add empty WiFi network management functions that only return -ENETDOWN.
    Define management handlers for scan, connect and disconnect requests,
    again without any implementation nor parameters defined.
    
    Signed-off-by: Patrik Flykt <patrik.flykt@intel.com>
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 7bde51d86a5572f13a56c75a840be355fb58ec33

---
+++ subsys/net/ip/l2/wifi_mgmt.c
@@ -0,0 +1,5 @@
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|wifi_connect|function|static int wifi_connect(u32_t mgmt_request, struct net_if *iface,void *data, size_t len)
+DECL|wifi_disconnect|function|static int wifi_disconnect(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
+DECL|wifi_scan|function|static int wifi_scan(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
