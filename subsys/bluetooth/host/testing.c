d7bbb772b8ead97cd938cc05126e949552f6b955
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2017-12-06 14:59:09 +0100

    Bluetooth: Introduce Bluetooth Testing API
    This introduces Bluetooth internal API intended to be used for
    qualification purposes. Application may register callbacks to get
    data that is not exposed by public API.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>
    
    Former-commit-id: 700328ac55015607169cd67dfda15d4420086956

---
+++ subsys/bluetooth/host/testing.c
@@ -0,0 +1,4 @@
+DECL|bt_test_cb_register|function|void bt_test_cb_register(struct bt_test_cb *cb)
+DECL|bt_test_cb_unregister|function|void bt_test_cb_unregister(struct bt_test_cb *cb)
+DECL|bt_test_mesh_net_recv|function|void bt_test_mesh_net_recv(u8_t ttl, u8_t ctl, u16_t src, u16_t dst, const void *payload, size_t payload_len)
+DECL|cb_slist|variable|cb_slist
---
+++ subsys/bluetooth/host/testing.h
@@ -0,0 +1,3 @@
+DECL|bt_test_cb|struct|struct bt_test_cb {
+DECL|mesh_net_recv|member|void (*mesh_net_recv)(u8_t ttl, u8_t ctl, u16_t src, u16_t dst,
+DECL|node|member|sys_snode_t node;
