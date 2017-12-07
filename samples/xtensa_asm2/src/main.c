bf3ad47580ad75cf9ec1dc000916ca2cdc9ff5ab
Andy Ross <andrew.j.ross@intel.com>
2017-12-07 15:04:51 -0800

    samples/xtensa-asm2: Unit test for new Xtensa assembly primitives
    This sample (which should eventually become a proper test) suite
    builds from simple applications of the new primitives to a full
    context switch test and interrupt handling suite (based on the
    CPU-internal CCOMPARE2 timer).
    
    It's been extraordinarily useful finding regressing as the asm2 code
    gets modified and should probably stick around as long as possible.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 837dd99a0e2b13bc4448b263918dd523aada93df

---
+++ samples/xtensa_asm2/src/main.c
@@ -0,0 +1,54 @@
+DECL|HAL_SPILL|enumerator|NO_SPILL, HAL_SPILL, ZEPHYR_SPILL, NUM_MODES
+DECL|NO_SPILL|enumerator|NO_SPILL, HAL_SPILL, ZEPHYR_SPILL, NUM_MODES
+DECL|NUM_MODES|enumerator|NO_SPILL, HAL_SPILL, ZEPHYR_SPILL, NUM_MODES
+DECL|TIMER_INT|macro|TIMER_INT
+DECL|TIMER_INT|macro|TIMER_INT
+DECL|XSTACK_CANARY|macro|XSTACK_CANARY
+DECL|XSTACK_SIZE|macro|XSTACK_SIZE
+DECL|ZEPHYR_SPILL|enumerator|NO_SPILL, HAL_SPILL, ZEPHYR_SPILL, NUM_MODES
+DECL|_test_highreg_a0_save|variable|_test_highreg_a0_save
+DECL|_test_highreg_sp_save|variable|_test_highreg_sp_save
+DECL|ccount|function|static inline unsigned int ccount(void)
+DECL|disable_timer|function|void disable_timer(void)
+DECL|enable_timer|function|void enable_timer(void)
+DECL|excint_cpu|variable|excint_cpu
+DECL|excint_stack_top|variable|excint_stack_top
+DECL|excint_stack|variable|excint_stack
+DECL|handle_int5_c|function|void *handle_int5_c(void *handle)
+DECL|highreg_tests|variable|highreg_tests
+DECL|int5_result|variable|int5_result
+DECL|interrupt_test|function|int interrupt_test(void)
+DECL|main|function|void main(void)
+DECL|nest|member|static struct { int nest; void *stack_top; } excint_cpu;
+DECL|rfi_jump_c|function|void rfi_jump_c(void)
+DECL|spill_end|variable|spill_end
+DECL|spill_expect|variable|spill_expect
+DECL|spill_fnp|variable|spill_fnp
+DECL|spill_fn|function|static int spill_fn(int level, int a, int b, int c)
+DECL|spill_mode|variable|spill_mode
+DECL|spill_start|variable|spill_start
+DECL|spill_wb0|variable|spill_wb0
+DECL|spill_wb1|variable|spill_wb1
+DECL|spill_ws0|variable|spill_ws0
+DECL|spill_ws1|variable|spill_ws1
+DECL|stack_top|member|static struct { int nest; void *stack_top; } excint_cpu;
+DECL|switch_count|variable|switch_count
+DECL|switch_handle0|variable|switch_handle0
+DECL|switch_handle1|variable|switch_handle1
+DECL|test_fn_t|typedef|typedef void (*test_fn_t)(void);
+DECL|test_highreg_handle|variable|test_highreg_handle
+DECL|test_highreg_save|function|int test_highreg_save(void)
+DECL|test_highreg_sp_top|variable|test_highreg_sp_top
+DECL|test_highreg_stack|variable|test_highreg_stack
+DECL|test_reg_spill|function|int test_reg_spill(void)
+DECL|test_switch_top|function|void test_switch_top(void)
+DECL|test_switch|function|int test_switch(void)
+DECL|test_xstack|function|int test_xstack(void)
+DECL|testfw_wb|variable|testfw_wb
+DECL|testfw_ws|variable|testfw_ws
+DECL|timer2_fired|variable|timer2_fired
+DECL|white|variable|white
+DECL|xstack_bottom|function|void xstack_bottom(void)
+DECL|xstack_ok|variable|xstack_ok
+DECL|xstack_stack2|variable|xstack_stack2
+DECL|xstack_top|function|void xstack_top(void)
