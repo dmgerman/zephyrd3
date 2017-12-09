203a9023eb4df1cdb972838da23e4b022c15e5bb
Andy Ross <andrew.j.ross@intel.com>
2017-12-09 09:32:21 -0800

    xtensa/asm2: Add a _new_thread implementation for asm2/switch
    Implement _new_thread in terms of the asm2 switch mechanism.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: b2c74e017e472a04e9a3e055985a41cf871f7ba2

--- arch/xtensa/core/xtensa-asm2.c
+++ arch/xtensa/core/xtensa-asm2.c
@@ -1,1 +1,2 @@
+DECL|_new_thread|function|void _new_thread(struct k_thread *thread, k_thread_stack_t *stack, size_t sz, k_thread_entry_t entry, void *p1, void *p2, void *p3, int prio, unsigned int opts)
 DECL|xtensa_init_stack|function|void *xtensa_init_stack(int *stack_top,void (*entry)(void *, void *, void *), void *arg1, void *arg2, void *arg3)
