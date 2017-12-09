948ba968fb739761940943a595db75f4054e8326
Andy Ross <andrew.j.ross@intel.com>
2017-12-08 17:38:12 -0800

    kernel: Move some macros
    K_NUM_PRIORITIES and K_NUM_PRIO_BITMAPS were defined in
    nano_internal.h, but used in only a handful of places.  Move to
    kernel_structs.h (somewhat higher up in the hierarchy) to help with
    include file cycle-breaking.  Arguably they are a better fit there
    anyway.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 8ac9c082e6afbca05d6707b2ecaca9968393d8b4

--- kernel/include/kernel_internal.h
+++ kernel/include/kernel_internal.h
@@ -1,5 +1,3 @@
-DECL|K_NUM_PRIORITIES|macro|K_NUM_PRIORITIES
-DECL|K_NUM_PRIO_BITMAPS|macro|K_NUM_PRIO_BITMAPS
 DECL|_NANO_INTERNAL__H_|macro|_NANO_INTERNAL__H_
 DECL|_Swap|function|static inline unsigned int _Swap(unsigned int key)
 DECL|_data_copy|function|static inline void _data_copy(void)
