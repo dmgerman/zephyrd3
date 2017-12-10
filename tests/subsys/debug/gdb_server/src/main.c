62c895a0f24a4249eb48ba3c6e6fe891ff6fa552
Anas Nashif <anas.nashif@intel.com>
2017-12-10 08:32:59 -0500

    tests: move gdb_server to test_build
    No need for this to be a standalone test.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 2292d7881dd59c557db11da3af946331309b8629

--- tests/subsys/debug/gdb_server/src/main.c
+++
@@ -1,7 +0,0 @@
-DECL|PRIORITY|macro|PRIORITY
-DECL|SLEEPTIME|macro|SLEEPTIME
-DECL|STACKSIZE|macro|STACKSIZE
-DECL|hello_loop|function|void hello_loop(const char *my_name, struct k_sem *my_sem, struct k_sem *other_sem)
-DECL|threada|function|void threada(void *dummy1, void *dummy2, void *dummy3)
-DECL|threadb_data|variable|threadb_data
-DECL|threadb|function|void threadb(void *dummy1, void *dummy2, void *dummy3)
