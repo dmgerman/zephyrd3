fc7e674cb20f42640596046207ed5b4ed13b4bfd
Andy Ross <andrew.j.ross@intel.com>
2017-12-11 15:35:32 -0800

    xtensa: Interrupt generator script and output for qemu & esp32
    This python script reads the core-isa.h interrupt definitions (via
    running a template file through the toolchain preprocessor to generate
    an input file) and emits a fully populated, optimized C handling code
    that binary searches only the declared interrupts at a given level and
    correctly detects spurious interrupts (and/or incorrect core-isa.h
    definitions).
    
    The generated code, alas, turns out not to be any faster than simply
    searching the interrupt mask with CLZ (er, NSAU in xtensese), though
    it could be faster in theory if the compiler made different choices,
    see comments.  But I like this for the robustness of the fully
    populated search trees and the checking of level vs. mask.
    
    This simply commits the script output into the source tree, including
    some checking code to force a build error if the toolchain changes the
    headers incompatibly.  It would be better long term to have these
    headers be generated at build time, but that requires more cmake fu
    than I have.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 7de010b5e558a31ead87fd78d1e9fd654d22acbc

---
+++ arch/xtensa/soc/sample_controller/include/_soc_inthandlers.h
@@ -0,0 +1,8 @@
+DECL|_xtensa_handle_one_int0|function|static inline int _xtensa_handle_one_int0(unsigned int mask)
+DECL|_xtensa_handle_one_int1|function|static inline int _xtensa_handle_one_int1(unsigned int mask)
+DECL|_xtensa_handle_one_int2|function|static inline int _xtensa_handle_one_int2(unsigned int mask)
+DECL|_xtensa_handle_one_int3|function|static inline int _xtensa_handle_one_int3(unsigned int mask)
+DECL|_xtensa_handle_one_int4|function|static inline int _xtensa_handle_one_int4(unsigned int mask)
+DECL|_xtensa_handle_one_int5|function|static inline int _xtensa_handle_one_int5(unsigned int mask)
+DECL|_xtensa_handle_one_int6|function|static inline int _xtensa_handle_one_int6(unsigned int mask)
+DECL|_xtensa_handle_one_int7|function|static inline int _xtensa_handle_one_int7(unsigned int mask)
