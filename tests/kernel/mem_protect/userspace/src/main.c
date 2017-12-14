a88704d3bbb96ea3fe2c57aabbe8377767dd5d8f
Stephen Smalley <sds@tycho.nsa.gov>
2017-12-14 10:51:01 -0500

    tests/kernel/mem_protect/userspace: test access to other thread stack
    Add tests of the ability to read or write the stack of another thread.
    Use semaphores for explicit synchronization of the start and end of the
    other thread to ensure that the attempted stack access occurs while the
    thread is alive.  This ensures that the MMU/MPU has been configured at
    least once to allow userspace access to the stack, and that any
    removal of access upon thread termination has not yet occurred.  This
    therefore should exercise changing the MMU/MPU configuration to remove
    access to the other thread's stack when switching back to our
    thread.
    
    Tested on qemu_x86 (pass) and on frdm_k64f (with and without the ARM
    userspace patches; with them, the tests pass; without, they fail as
    expected).  Also, as with most of the other tests, if you replace
    ztest_user_unit_test() with ztest_unit_test(), then the tests fail as
    expected.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>
    
    Former-commit-id: e3fe3ebb3fd73dc96c16bc2c4ddd4fc1b09ca2e3

--- tests/kernel/mem_protect/userspace/src/main.c
+++ tests/kernel/mem_protect/userspace/src/main.c
@@ -2,6 +2,7 @@ DECL|INFO|macro|INFO
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|disable_mmu_mpu|function|static void disable_mmu_mpu(void)
+DECL|give_uthread_end_sem|variable|give_uthread_end_sem
 DECL|is_usermode|function|static void is_usermode(void)
 DECL|kernel_data|variable|kernel_data
 DECL|ksem|variable|ksem
@@ -12,6 +13,7 @@ DECL|ptr|variable|ptr
 DECL|read_kernel_data|function|static void read_kernel_data(void)
 DECL|read_kernel_stack|function|static void read_kernel_stack(void)
 DECL|read_kernram|function|static void read_kernram(void)
+DECL|read_other_stack|function|static void read_other_stack(void)
 DECL|sem|variable|sem
 DECL|size|variable|size
 DECL|size|variable|size
@@ -19,9 +21,12 @@ DECL|size|variable|size
 DECL|start_kernel_thread|function|static void start_kernel_thread(void)
 DECL|test_main|function|void test_main(void)
 DECL|thread_body|function|void thread_body(void)
+DECL|uthread_body|function|static void uthread_body(void)
+DECL|uthread_thread|variable|uthread_thread
 DECL|write_control|function|static void write_control(void)
 DECL|write_kernel_data|function|static void write_kernel_data(void)
 DECL|write_kernel_stack|function|static void write_kernel_stack(void)
 DECL|write_kernram|function|static void write_kernram(void)
 DECL|write_kernro|function|static void write_kernro(void)
 DECL|write_kerntext|function|static void write_kerntext(void)
+DECL|write_other_stack|function|static void write_other_stack(void)
