2760b0ee261948e135e71703faaaff83c2877c36
Andy Ross <andrew.j.ross@intel.com>
2017-12-14 10:08:04 -0800

    xtensa: Implement _xt_ints_on/off for asm2
    Legacy xtensa had a rather complicated implementation of en/disabling
    interrupts, owing to the "software priority" feature (which plays
    games with INTENABLE and INTLEVEL to allow for interrupts to interrupt
    each other outside their normal priorities).  But that's not a Zephyr
    feature, it's enabled by a XT_USE_SWPRI value that comes from platform
    headers and isn't enabled on any of our boards.  Dead code, basically.
    
    Replace with the obvious implementation when asm2 is in use.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 6f3036091a335f134d1e6e542563d525052a845a

--- arch/xtensa/include/xtensa_api.h
+++ arch/xtensa/include/xtensa_api.h
@@ -1,3 +1,5 @@
 DECL|__XTENSA_API_H__|macro|__XTENSA_API_H__
+DECL|_xt_ints_off|function|static inline void _xt_ints_off(unsigned int mask)
+DECL|_xt_ints_on|function|static inline void _xt_ints_on(unsigned int mask)
 DECL|_xt_set_intclear|function|static inline void _xt_set_intclear(unsigned int arg)
 DECL|_xt_set_intset|function|static inline void _xt_set_intset(unsigned int arg)
