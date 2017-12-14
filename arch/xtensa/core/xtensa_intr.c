a5371bc5f6cf250265e3e2194f694b45ae174e20
Andy Ross <andrew.j.ross@intel.com>
2017-12-14 09:58:43 -0800

    xtensa: Remove _xt_set_exception_handler()
    This was a dead API.  Nothing ever used it, it wasn't exposed in any
    API headers.  It never appeared in documentation.  It's not
    particularly clear why a Zephy app would want to hook
    architecture-specific exceptions instead of simply using the portable
    error framework anyway. And it's not supported by asm2.  Delete.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: a0dd2de6fdcb76305ee3b7f97d15c646b72b2bf9

--- arch/xtensa/core/xtensa_intr.c
+++ arch/xtensa/core/xtensa_intr.c
@@ -1,2 +1,3 @@
 DECL|_irq_spurious|function|void _irq_spurious(void *arg)
 DECL|unhandled_exception_trampoline|function|static void unhandled_exception_trampoline(XtExcFrame *frame)
+DECL|xt_exc_handler|typedef|typedef void (*xt_exc_handler)(XtExcFrame *);
--- arch/xtensa/include/xtensa_api.h
+++ arch/xtensa/include/xtensa_api.h
@@ -1,5 +1,3 @@
 DECL|__XTENSA_API_H__|macro|__XTENSA_API_H__
 DECL|_xt_set_intclear|function|static inline void _xt_set_intclear(unsigned int arg)
 DECL|_xt_set_intset|function|static inline void _xt_set_intset(unsigned int arg)
-DECL|xt_exc_handler|typedef|typedef void (*xt_exc_handler)(XtExcFrame *);
-DECL|xt_handler|typedef|typedef void (*xt_handler)(void *);
