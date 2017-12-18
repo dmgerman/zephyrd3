9fd5189dddf9851b10490174f220c164f4a7007e
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2017-12-18 11:55:33 +0100

    Bluetooth: Expose Bluetooth Testing API
    This moves Bluetooth testing header file to the include/bluetooth.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>
    
    Former-commit-id: a87840e8e2e1dac59fc8f245c40f50c68d511d05

--- include/bluetooth/testing.h
+++ include/bluetooth/testing.h
@@ -1,3 +1,4 @@
+DECL|__BT_TESTING_H|macro|__BT_TESTING_H
 DECL|bt_test_cb|struct|struct bt_test_cb {
 DECL|mesh_net_recv|member|void (*mesh_net_recv)(u8_t ttl, u8_t ctl, u16_t src, u16_t dst,
 DECL|node|member|sys_snode_t node;
