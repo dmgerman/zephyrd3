0500511505f4af844eac7f44c3f554b3e3c36695
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2017-12-21 15:03:27 +0100

    samples: threads: Rewrite misleading sample
    The basic/threads sample is printing a log that looks like this:
    
    Toggle USR0 LED: Counter = 0
    Toggle USR1 LED: Counter = 0
    Toggle USR0 LED: Counter = 1
    Toggle USR0 LED: Counter = 2
    Toggle USR0 LED: Counter = 3
    Toggle USR0 LED: Counter = 4
    
    From that log you would think that it was logging when leds were
    blinking, but actually the led-threads might have crashed, and it
    would just continue logging anyway. All it's doing is executing
    printk's at roughly the same frequency as the LED's are blinking.
    
    This patch rewrites the sample to use a FIFO so that the printk's only
    trigger if the LEDs are actually blinking.
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>
    
    Former-commit-id: 830b8bdc17865b236db529ca66db9dca0e79f7ed

--- samples/basic/threads/src/main.c
+++ samples/basic/threads/src/main.c
@@ -6,4 +6,10 @@ DECL|PRIORITY|macro|PRIORITY
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|blink1|function|void blink1(void)
 DECL|blink2|function|void blink2(void)
+DECL|blink|function|void blink(const char *port, u32_t sleep_ms, u32_t led, u32_t id)
+DECL|cnt|member|u32_t cnt;
+DECL|fifo_reserved|member|void *fifo_reserved; /* 1st word reserved for use by fifo */
+DECL|led|member|u32_t led;
+DECL|printk_data_t|struct|struct printk_data_t {
+DECL|printk_fifo|variable|printk_fifo
 DECL|uart_out|function|void uart_out(void)
