7af8f9578344d2290afa2aab0dc6ab3f5993f653
Wayne Ren <wei.ren@synopsys.com>
2017-12-22 10:41:27 +0800

    arch: arc: Add _arch_mem_domain_configure
    add the _arch_mem_domain_configure api to match the PR #5065 to avoid
    compile failure in the future
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>
    
    Former-commit-id: 5ca2c0531691d15d227c47b5a37da03dbdd01f46

--- arch/arc/core/mpu/arc_core_mpu.c
+++ arch/arc/core/mpu/arc_core_mpu.c
@@ -1,4 +1,5 @@
 DECL|_arch_buffer_validate|function|int _arch_buffer_validate(void *addr, size_t size, int write)
+DECL|_arch_mem_domain_configure|function|void _arch_mem_domain_configure(struct k_thread *thread)
 DECL|_arch_mem_domain_destroy|function|void _arch_mem_domain_destroy(struct k_mem_domain *domain)
 DECL|_arch_mem_domain_max_partitions_get|function|int _arch_mem_domain_max_partitions_get(void)
 DECL|_arch_mem_domain_partition_remove|function|void _arch_mem_domain_partition_remove(struct k_mem_domain *domain, u32_t partition_id)
