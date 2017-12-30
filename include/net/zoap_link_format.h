9a8b6e5419096e958c1dc0bfc4b288459c506b2e
Anas Nashif <anas.nashif@intel.com>
2017-12-30 16:30:04 -0500

    net: remove zoap library
    We now have coap, all dependencies have been removed, so cleanup for the
    next release.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 13b08296e1bf110639c0bf5c4c7b3170b47de238

--- include/net/zoap_link_format.h
+++
@@ -1,6 +0,0 @@
-DECL|ZOAP_WELL_KNOWN_CORE_RESOURCE|macro|ZOAP_WELL_KNOWN_CORE_RESOURCE
-DECL|_ZOAP_WELL_KNOWN_CORE_PATH|macro|_ZOAP_WELL_KNOWN_CORE_PATH
-DECL|__LINK_FORMAT_H__|macro|__LINK_FORMAT_H__
-DECL|attributes|member|const char * const *attributes;
-DECL|user_data|member|void *user_data;
-DECL|zoap_core_metadata|struct|struct zoap_core_metadata {
