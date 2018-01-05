7265a0b0e81ebaa27d2134c4f92cd7994dd197e3
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-01-05 08:09:05 -0800

    drivers: sensor: Add support for CCS811 Digital Gas Sensor
    Add support for AMS CCS811 Digital Gas Sensor for monitoring
    indoor air quality. This sensor reports the following parameters:
    
    1. Co2 concentration
    2. VOC concentration
    3. Sensor voltage
    4. Sensor current
    
    This driver only supports polling mode as of now.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    
    Former-commit-id: 9e88120fdf3677acd173aef4a15193535aa3563d

---
+++ drivers/sensor/ccs811/ccs811.c
@@ -0,0 +1,6 @@
+DECL|ccs811_channel_get|function|static int ccs811_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|ccs811_driver_api|variable|ccs811_driver_api
+DECL|ccs811_driver|variable|ccs811_driver
+DECL|ccs811_init|function|int ccs811_init(struct device *dev)
+DECL|ccs811_sample_fetch|function|static int ccs811_sample_fetch(struct device *dev, enum sensor_channel chan)
+DECL|switch_to_app_mode|function|static int switch_to_app_mode(struct device *i2c)
