9f7fe6c8b997f6a71261accafc4d80d57c1fe24f
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2018-01-09 15:17:15 +0100

    Bluetooth: controller: Conditional compile BT_LL_SW variant
    Refactor Kconfig and CMakelists.txt to be able to
    conditionally compile in BT_LL_SW variant in the controller
    subsystem. This is done to support future controller with
    vendor specific variant implementations.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 5b0cd2c69bd2a08ff97f806b955660975332d55f

--- subsys/bluetooth/controller/ll_sw/ll.c
+++ subsys/bluetooth/controller/ll_sw/ll.c
@@ -9,19 +9,14 @@ DECL|RAND_THREAD_THRESHOLD|macro|RAND_THREAD_THRESHOLD
 DECL|TICKER_NODES|macro|TICKER_NODES
 DECL|TICKER_USER_APP_OPS|macro|TICKER_USER_APP_OPS
 DECL|TICKER_USER_OPS|macro|TICKER_USER_OPS
-DECL|_ll_context|variable|_ll_context
-DECL|ll_addr_get|function|u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
-DECL|ll_addr_set|function|void ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
 DECL|ll_init|function|int ll_init(struct k_sem *sem_rx)
 DECL|ll_timeslice_ticker_id_get|function|void ll_timeslice_ticker_id_get(u8_t * const instance_index, u8_t * const user_id)
 DECL|mayfly_enable_cb|function|void mayfly_enable_cb(u8_t caller_id, u8_t callee_id, u8_t enable)
 DECL|mayfly_is_enabled|function|u32_t mayfly_is_enabled(u8_t caller_id, u8_t callee_id)
 DECL|mayfly_pend|function|void mayfly_pend(u8_t caller_id, u8_t callee_id)
 DECL|mayfly_prio_is_equal|function|u32_t mayfly_prio_is_equal(u8_t caller_id, u8_t callee_id)
-DECL|pub_addr|member|u8_t pub_addr[BDADDR_SIZE];
 DECL|radio_active_callback|function|void radio_active_callback(u8_t active)
 DECL|radio_event_callback|function|void radio_event_callback(void)
-DECL|rnd_addr|member|u8_t rnd_addr[BDADDR_SIZE];
 DECL|rng_nrf5_isr|function|static void rng_nrf5_isr(void *arg)
 DECL|rtc0_nrf5_isr|function|static void rtc0_nrf5_isr(void *arg)
 DECL|sem_recv|variable|sem_recv
---
+++ subsys/bluetooth/controller/ll_sw/ll_tx_pwr.c
@@ -0,0 +1,2 @@
+DECL|ll_tx_pwr_get|function|void ll_tx_pwr_get(s8_t *min, s8_t *max)
+DECL|ll_tx_pwr_lvl_get|function|u32_t __weak ll_tx_pwr_lvl_get(u16_t handle, u8_t type, s8_t *tx_pwr_lvl)
