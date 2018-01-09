f109acec2c57dbac83ef26e0ab1e273589270b86
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-01-09 12:23:51 +0530

    tests: kernel: fatal: x86: Fixed the issue with stack alignment.
    The test case used a stack which was not aligned to 4kB. Hence an
    assert was catching this issue.
    
    GH-5539
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: 34b8b3b5ee6c24401a9490c0dd2b2cc11ad65479

--- tests/kernel/fatal/src/main.c
+++ tests/kernel/fatal/src/main.c
@@ -3,6 +3,7 @@ DECL|OVERFLOW_STACKSIZE|macro|OVERFLOW_STACKSIZE
 DECL|OVERFLOW_STACKSIZE|macro|OVERFLOW_STACKSIZE
 DECL|PRIORITY|macro|PRIORITY
 DECL|STACKSIZE|macro|STACKSIZE
+DECL|STACKSIZE|macro|STACKSIZE
 DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|alt_thread1|function|void alt_thread1(void)
 DECL|alt_thread2|function|void alt_thread2(void)
