4d3312402f13fa99f9196b4338be56c4ec1cbee6
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2018-01-09 15:03:33 +0100

    Bluetooth: controller: Refactor mayfly integration into hal
    Refactor mayfly based execution context solution out into
    the HAL folder. This opens up the possibility to use
    tasklets (if and when kernel gets the support) style
    execution contexts.
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 619bcf56c4aa37ef21063d9ac252fdadfa4672cd

---
+++ subsys/bluetooth/controller/hal/nrf5/mayfly.c
@@ -0,0 +1,6 @@
+DECL|MAYFLY_CALL_ID_JOB|macro|MAYFLY_CALL_ID_JOB
+DECL|MAYFLY_CALL_ID_WORKER|macro|MAYFLY_CALL_ID_WORKER
+DECL|mayfly_enable_cb|function|void mayfly_enable_cb(u8_t caller_id, u8_t callee_id, u8_t enable)
+DECL|mayfly_is_enabled|function|u32_t mayfly_is_enabled(u8_t caller_id, u8_t callee_id)
+DECL|mayfly_pend|function|void mayfly_pend(u8_t caller_id, u8_t callee_id)
+DECL|mayfly_prio_is_equal|function|u32_t mayfly_prio_is_equal(u8_t caller_id, u8_t callee_id)
--- subsys/bluetooth/controller/ll_sw/ll.c
+++ subsys/bluetooth/controller/ll_sw/ll.c
@@ -11,10 +11,6 @@ DECL|TICKER_USER_APP_OPS|macro|TICKER_USER_APP_OPS
 DECL|TICKER_USER_OPS|macro|TICKER_USER_OPS
 DECL|ll_init|function|int ll_init(struct k_sem *sem_rx)
 DECL|ll_timeslice_ticker_id_get|function|void ll_timeslice_ticker_id_get(u8_t * const instance_index, u8_t * const user_id)
-DECL|mayfly_enable_cb|function|void mayfly_enable_cb(u8_t caller_id, u8_t callee_id, u8_t enable)
-DECL|mayfly_is_enabled|function|u32_t mayfly_is_enabled(u8_t caller_id, u8_t callee_id)
-DECL|mayfly_pend|function|void mayfly_pend(u8_t caller_id, u8_t callee_id)
-DECL|mayfly_prio_is_equal|function|u32_t mayfly_prio_is_equal(u8_t caller_id, u8_t callee_id)
 DECL|radio_active_callback|function|void radio_active_callback(u8_t active)
 DECL|radio_event_callback|function|void radio_event_callback(void)
 DECL|rng_nrf5_isr|function|static void rng_nrf5_isr(void *arg)
