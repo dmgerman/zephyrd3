f873d782ddb3b03a5242737d429800231af30fdf
Alberto Escolar Piedras <alpi@oticon.com>
2018-01-09 14:20:49 +0100

    native: Accept command line arguments
    The native_posix board will accept command line arguments
    which the application / test may pick by calling
    native_get_cmd_line_args()
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: 66a1454ce61d3893179cd31d01a0fc1396b3fe72

---
+++ boards/posix/native_posix/cmdline.c
@@ -0,0 +1,4 @@
+DECL|native_get_cmd_line_args|function|void native_get_cmd_line_args(int *argc, char ***argv)
+DECL|native_handle_cmd_line|function|void native_handle_cmd_line(int argc, char *argv[])
+DECL|s_argc|variable|s_argc
+DECL|s_argv|variable|s_argv
--- boards/posix/native_posix/main.c
+++ boards/posix/native_posix/main.c
@@ -1,3 +1,3 @@
 DECL|STOP_AFTER_5_SECONDS|macro|STOP_AFTER_5_SECONDS
 DECL|main_clean_up|function|void main_clean_up(int exit_code)
-DECL|main|function|int main(void)
+DECL|main|function|int main(int argc, char *argv[])
