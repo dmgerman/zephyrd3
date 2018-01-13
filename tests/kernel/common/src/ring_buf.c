cdd2e74ed7f6e0c08afacdf8ea97d17a0de63595
Anas Nashif <anas.nashif@intel.com>
2018-01-13 18:48:33 -0500

    tests: add ringbuffer api test and combine original test
    We already have a test for ring buffers, this combines it with an API
    test.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: a5bde70d3a37202e8b775c290ddae7ddd8e2f339

--- tests/kernel/common/src/ring_buf.c
+++
@@ -1,5 +0,0 @@
-DECL|INITIAL_SIZE|macro|INITIAL_SIZE
-DECL|TYPE|macro|TYPE
-DECL|VALUE|macro|VALUE
-DECL|data|variable|data
-DECL|ring_buffer_test|function|void ring_buffer_test(void)
---
+++ tests/sys/ringbuffer/src/main.c
@@ -0,0 +1,26 @@
+DECL|DATA_MAX_SIZE|macro|DATA_MAX_SIZE
+DECL|INITIAL_SIZE|macro|INITIAL_SIZE
+DECL|POW|macro|POW
+DECL|RINGBUFFER_SIZE|macro|RINGBUFFER_SIZE
+DECL|TYPE|macro|TYPE
+DECL|VALUE|macro|VALUE
+DECL|buffer|member|u32_t buffer[DATA_MAX_SIZE];
+DECL|buffer|variable|buffer
+DECL|data|variable|data
+DECL|length|member|u8_t length;
+DECL|pbuf|variable|pbuf
+DECL|ring_buffer_main_test|function|void ring_buffer_main_test(void)
+DECL|ringbuf|variable|ringbuf
+DECL|test_main|function|void test_main(void)
+DECL|test_ringbuffer_declare_pow2|function|void test_ringbuffer_declare_pow2(void)
+DECL|test_ringbuffer_declare_size|function|void test_ringbuffer_declare_size(void)
+DECL|test_ringbuffer_init|function|void test_ringbuffer_init(void)
+DECL|test_ringbuffer_pow2_put_get_thread_isr|function|void test_ringbuffer_pow2_put_get_thread_isr(void)
+DECL|test_ringbuffer_put_get_isr|function|void test_ringbuffer_put_get_isr(void)
+DECL|test_ringbuffer_put_get_thread_isr|function|void test_ringbuffer_put_get_thread_isr(void)
+DECL|test_ringbuffer_put_get_thread|function|void test_ringbuffer_put_get_thread(void)
+DECL|test_ringbuffer_size_put_get_thread_isr|function|void test_ringbuffer_size_put_get_thread_isr(void)
+DECL|tringbuf_get|function|static void tringbuf_get(void *p)
+DECL|tringbuf_put|function|static void tringbuf_put(void *p)
+DECL|type|member|u16_t type;
+DECL|value|member|u8_t value;
