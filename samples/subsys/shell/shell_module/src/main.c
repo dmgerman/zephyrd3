382e6268f4040a4756faf1e17bba970ddc1d95ae
Anas Nashif <anas.nashif@intel.com>
2018-01-15 13:58:50 -0500

    samples: shell: add standalone version command
    Combine modules with standalone commands.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: ff0d690e79d9d276f868a53578383609e4d9ee84

--- samples/subsys/shell/shell_module/src/main.c
+++ samples/subsys/shell/shell_module/src/main.c
@@ -1,5 +1,7 @@
 DECL|MY_SHELL_MODULE|macro|MY_SHELL_MODULE
+DECL|SHELL_CMD_VERSION|macro|SHELL_CMD_VERSION
 DECL|commands|variable|commands
 DECL|main|function|void main(void)
 DECL|shell_cmd_params|function|static int shell_cmd_params(int argc, char *argv[])
 DECL|shell_cmd_ping|function|static int shell_cmd_ping(int argc, char *argv[])
+DECL|shell_cmd_version|function|static int shell_cmd_version(int argc, char *argv[])
