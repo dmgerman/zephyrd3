6f420c3fdcc2a8d3ee4ad77c261b98480328769c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-01-16 16:54:30 +0200

    samples: net: vlan: Simple app for setting virtual lan settings
    This application just enables VLAN tag for ethernet interface.
    Set CONFIG_SAMPLE_VLAN_TAG option to define the desired VLAN tag.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 5fbd48077b272d246011de5c51d059c08e375699

---
+++ samples/net/vlan/src/main.c
@@ -0,0 +1,10 @@
+DECL|CREATE_MULTIPLE_TAGS|macro|CREATE_MULTIPLE_TAGS
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|first|member|struct net_if *first;
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
+DECL|init_app|function|static int init_app(void)
+DECL|main|function|void main(void)
+DECL|second|member|struct net_if *second;
+DECL|ud|struct|struct ud {
