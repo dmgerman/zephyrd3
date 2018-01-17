30d3080c7bc686b4c432c64bd3592f2734b05a84
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-01-17 16:44:34 +0200

    net: dns: Allow to initialize DNS servers from struct sockaddr[]
    In some cases, we need to initialize DNS servers from a binary
    addresses, e.g. in case of DHCP processing. With existing API,
    such addresses would need to be converted to strings, just to
    be converted back to struct sockaddr in dns_resolve_init().
    This is not efficient, and with a number of addresses quite
    cumbersome. So instead, allow to pass DNS server either as
    strings, or as struct sockaddr's (or both).
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: d965598ee23f4266bd1d0a88dbc03659f3416502

--- subsys/net/lib/dns/resolve.c
+++ subsys/net/lib/dns/resolve.c
@@ -17,11 +17,12 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|cb_recv|function|static void cb_recv(struct net_context *net_ctx, struct net_pkt *pkt, int status, void *user_data)
 DECL|dns_default_ctx|variable|dns_default_ctx
 DECL|dns_init_resolver|function|void dns_init_resolver(void)
+DECL|dns_postprocess_server|function|static void dns_postprocess_server(struct dns_resolve_context *ctx, int idx)
 DECL|dns_read|function|static int dns_read(struct dns_resolve_context *ctx, struct net_pkt *pkt, struct net_buf *dns_data, u16_t *dns_id, struct net_buf *dns_cname,
 DECL|dns_resolve_cancel|function|int dns_resolve_cancel(struct dns_resolve_context *ctx, u16_t dns_id)
 DECL|dns_resolve_close|function|int dns_resolve_close(struct dns_resolve_context *ctx)
 DECL|dns_resolve_get_default|function|struct dns_resolve_context *dns_resolve_get_default(void)
-DECL|dns_resolve_init|function|int dns_resolve_init(struct dns_resolve_context *ctx, const char *servers[])
+DECL|dns_resolve_init|function|int dns_resolve_init(struct dns_resolve_context *ctx, const char *servers[], const struct sockaddr *servers_sa[])
 DECL|dns_resolve_name|function|int dns_resolve_name(struct dns_resolve_context *ctx, const char *query, enum dns_query_type type, u16_t *dns_id, dns_resolve_cb_t cb,
 DECL|dns_write|function|static int dns_write(struct dns_resolve_context *ctx, int server_idx, int query_idx, struct net_buf *dns_data, struct net_buf *dns_qname)
 DECL|get_cb_slot|function|static inline int get_cb_slot(struct dns_resolve_context *ctx)
