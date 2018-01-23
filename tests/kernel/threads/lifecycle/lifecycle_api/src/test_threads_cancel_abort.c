299686424fd24aecdbfc042660db99cd2c608a12
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-01-23 15:37:10 +0530

    tests: abort: Testcase for repeated thread abort
    Test case to showcase the repeated abort of a thread.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: 61b7757da01671b43e4b03b51060c785fa4ea0fd

--- tests/kernel/threads/lifecycle/lifecycle_api/src/test_threads_cancel_abort.c
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/test_threads_cancel_abort.c
@@ -1,6 +1,7 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|execute_flag|variable|execute_flag
 DECL|test_threads_abort_others|function|void test_threads_abort_others(void)
+DECL|test_threads_abort_repeat|function|void test_threads_abort_repeat(void)
 DECL|test_threads_abort_self|function|void test_threads_abort_self(void)
 DECL|test_threads_cancel_delayed|function|void test_threads_cancel_delayed(void)
 DECL|test_threads_cancel_started|function|void test_threads_cancel_started(void)
