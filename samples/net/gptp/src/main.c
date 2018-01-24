573c227092906585614fcfaaee3a3442b50a3228
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-01-24 16:12:45 +0200

    samples: net: gptp: Sample application for gPTP support
    The application does not do much, it just registers to a callback
    in order to get information about gPTP.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 67b4c5d5d4a804bf437e3059dff9ac4a3b28af87

---
+++ samples/net/gptp/src/main.c
@@ -0,0 +1,14 @@
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|first|member|struct net_if *first;
+DECL|gptp_phase_dis_cb|function|static void gptp_phase_dis_cb(u8_t *gm_identity, u16_t *time_base, struct gptp_scaled_ns *last_gm_ph_change, double *last_gm_freq_change)
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
+DECL|init_app|function|static int init_app(void)
+DECL|init_vlan|function|static int init_vlan(void)
+DECL|main|function|void main(void)
+DECL|phase_dis|variable|phase_dis
+DECL|second|member|struct net_if *second;
+DECL|setup_iface|function|static int setup_iface(struct net_if *iface, const char *ipv6_addr, const char *ipv4_addr, u16_t vlan_tag)
+DECL|third|member|struct net_if *third;
+DECL|ud|struct|struct ud {
