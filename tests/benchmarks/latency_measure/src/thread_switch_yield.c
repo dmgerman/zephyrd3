cbded5b6c1ad6bc8f781ffa152ed2306aedabca8
Kumar Gala <kumar.gala@linaro.org>
2018-01-24 07:42:00 -0600

    tests: benchmarks: latency_measure: Fix build issue with abs()
    We now have a define for abs() in stdlib, so we can remove the version
    implemented in the test.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 9a87b8535ef803603bccf565480ecb0df5a7d857

--- tests/benchmarks/latency_measure/src/thread_switch_yield.c
+++ tests/benchmarks/latency_measure/src/thread_switch_yield.c
@@ -1,7 +1,6 @@
 DECL|NB_OF_YIELD|macro|NB_OF_YIELD
 DECL|Y_PRIORITY|macro|Y_PRIORITY
 DECL|Y_STACK_SIZE|macro|Y_STACK_SIZE
-DECL|abs|function|static int abs(int i)
 DECL|helper_thread_iterations|variable|helper_thread_iterations
 DECL|thread_switch_yield|function|void thread_switch_yield(void)
 DECL|y_thread|variable|y_thread
