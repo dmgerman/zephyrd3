30a43ac8812bae39975671719a24419c97438c40
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-01-24 20:54:15 +0200

    syslog: Add networking backend
    If network based syslog backend is enabled in Kconfig file,
    then syslog messages are sent to external system using UDP.
    See RFC 5424 and RFC 5426 for details about the syslog protocol.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 9f73d5e2fd1c3280b244aedd144536a16225e12b

---
+++ subsys/logging/sys_log_net.c
@@ -0,0 +1,15 @@
+DECL|DATE_EPOCH|macro|DATE_EPOCH
+DECL|DEBUG_PRINTING|macro|DEBUG_PRINTING
+DECL|MAX_HOSTNAME_LEN|macro|MAX_HOSTNAME_LEN
+DECL|MAX_HOSTNAME_LEN|macro|MAX_HOSTNAME_LEN
+DECL|ctx|variable|ctx
+DECL|date|variable|date
+DECL|facility|variable|facility
+DECL|fill_header|function|static void fill_header(struct net_buf *buf)
+DECL|get_data_pool|function|struct net_buf_pool *get_data_pool(void)
+DECL|get_tx_slab|function|static struct k_mem_slab *get_tx_slab(void)
+DECL|hostname|variable|hostname
+DECL|server_addr|variable|server_addr
+DECL|severity|variable|severity
+DECL|syslog_hook_net|function|static void syslog_hook_net(const char *fmt, ...)
+DECL|syslog_net_hook_install|function|void syslog_net_hook_install(void)
