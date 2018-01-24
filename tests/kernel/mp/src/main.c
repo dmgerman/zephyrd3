e7d5b6ca7803457851509a4eca450de889d8e6b9
Andy Ross <andrew.j.ross@intel.com>
2018-01-24 15:50:45 -0800

    tests/kernel: Simple test for multiprocessor start API
    Starts the second CPU and verifies that it can set a variable while we
    spin on the first.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 86ff14824da2ab234b2a4a50aada0422ac16c9b9

---
+++ tests/kernel/mp/src/main.c
@@ -0,0 +1,6 @@
+DECL|CPU1_STACK_SIZE|macro|CPU1_STACK_SIZE
+DECL|cpu1_fn|function|void cpu1_fn(int key, void *arg)
+DECL|cpu_arg|variable|cpu_arg
+DECL|cpu_running|variable|cpu_running
+DECL|test_main|function|void test_main(void)
+DECL|test_mp_start|function|void test_mp_start(void)
