0063cc2cf1de6ac3a16ed2f9336a8c44388fdcf1
Andy Ross <andrew.j.ross@intel.com>
2018-01-24 15:48:32 -0800

    kernel, esp32: Add _arch_start_cpu API
    This is a mostly-internal API to start a secondary system CPU, with an
    implementation for the ESP-32 "APP" cpu.  Exposed in kernel.h because
    it's plausibly useful for asymmetric MP code managed by an app.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: e717267abfab4d515e7894f4fa5499fde8823638

---
+++ arch/xtensa/soc/esp32/esp32-mp.c
@@ -0,0 +1,30 @@
+DECL|DPORT_APPCPU_CLKGATE_EN|macro|DPORT_APPCPU_CLKGATE_EN
+DECL|DPORT_APPCPU_CTRL_A|macro|DPORT_APPCPU_CTRL_A
+DECL|DPORT_APPCPU_CTRL_B|macro|DPORT_APPCPU_CTRL_B
+DECL|DPORT_APPCPU_CTRL_C|macro|DPORT_APPCPU_CTRL_C
+DECL|DPORT_APPCPU_RESETTING|macro|DPORT_APPCPU_RESETTING
+DECL|DPORT_APPCPU_RUNSTALL|macro|DPORT_APPCPU_RUNSTALL
+DECL|DPORT_BASE|macro|DPORT_BASE
+DECL|RTC_CNTL_BASE|macro|RTC_CNTL_BASE
+DECL|RTC_CNTL_OPTIONS0|macro|RTC_CNTL_OPTIONS0
+DECL|RTC_CNTL_SW_CPU_STALL|macro|RTC_CNTL_SW_CPU_STALL
+DECL|RTC_CNTL_SW_STALL_APPCPU_C0|macro|RTC_CNTL_SW_STALL_APPCPU_C0
+DECL|RTC_CNTL_SW_STALL_APPCPU_C1|macro|RTC_CNTL_SW_STALL_APPCPU_C1
+DECL|_REG|macro|_REG
+DECL|_arch_start_cpu|function|void _arch_start_cpu(int cpu_num, k_thread_stack_t *stack, int sz, void (*fn)(int, void *), void *arg)
+DECL|alive|member|volatile int *alive;
+DECL|appcpu_entry1|function|static void appcpu_entry1(void)
+DECL|appcpu_entry2|function|static void appcpu_entry2(void)
+DECL|appcpu_start|function|static void appcpu_start(void)
+DECL|appcpu_top|variable|appcpu_top
+DECL|arg|member|void *arg;
+DECL|cpustart_rec|struct|struct cpustart_rec {
+DECL|cpu|member|int cpu;
+DECL|esp32rom_Cache_Flush|variable|esp32rom_Cache_Flush
+DECL|esp32rom_Cache_Read_Enable|variable|esp32rom_Cache_Read_Enable
+DECL|esp32rom_call_t|typedef|typedef void (*esp32rom_call_t)(int);
+DECL|esp32rom_ets_set_appcpu_boot_addr|variable|esp32rom_ets_set_appcpu_boot_addr
+DECL|fn|member|void (*fn)(int, void *);
+DECL|stack_top|member|char *stack_top;
+DECL|start_rec|variable|start_rec
+DECL|vecbase|member|int vecbase;
