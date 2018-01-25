da21b12cd304506db805f5a64ff39804130f9d64
Andy Ross <andrew.j.ross@intel.com>
2018-01-25 15:24:15 -0800

    kernel: Add kswap.h header to unbreak cycles
    The xtensa-asm2 work included a patch that added nano_internal.h
    includes in lots of places that needed to have _Swap defined, because
    it had to break a cycle and this no longer got pulled in from the arch
    headers.
    
    Unfortunately those new includes created new and more amusing cycles
    elsewhere which led to breakage on other platforms.
    
    Break out the _Swap definition (only) into a separate header and use
    that instead.  Cleaner.  Seems not to have any more hidden gotchas.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 9c62cc677d3963ed4f5bff2da5d841e49ca91545

--- kernel/include/kernel_internal.h
+++ kernel/include/kernel_internal.h
@@ -1,7 +1,3 @@
 DECL|_NANO_INTERNAL__H_|macro|_NANO_INTERNAL__H_
-DECL|_Swap|function|static inline unsigned int _Swap(unsigned int key)
-DECL|_Swap|function|static inline unsigned int _Swap(unsigned int key)
-DECL|_check_stack_sentinel|macro|_check_stack_sentinel
 DECL|_data_copy|function|static inline void _data_copy(void)
 DECL|_thread_monitor_exit|macro|_thread_monitor_exit
-DECL|_update_time_slice_before_swap|macro|_update_time_slice_before_swap
