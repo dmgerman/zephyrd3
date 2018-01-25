c7e0183b4bdddccfc0edaf23605e76c7c3988488
Ding Tao <miyatsu@qq.com>
2018-01-25 18:44:43 +0000

    arch: arm: vector_table: Support vector table soft relay
    For SoCs that don't support vector table relocation in hardware, may not
    support bootloader like mcuboot.
    
    We introduce a way to relocate vector table in software by forwarding
    the control of incoming IRQs to a new vector table which address is save
    at fixed SRAM address.
    
    User can change the data in that fixed SRAM address in order to relocate
    vector table in software way.
    
    Signed-off-by: Ding Tao <miyatsu@qq.com>
    
    Former-commit-id: bbd307edfdc64ee0904b0c36349af3fa9b96bc3d

--- arch/arm/core/cortex_m/prep_c.c
+++ arch/arm/core/cortex_m/prep_c.c
@@ -2,6 +2,7 @@ DECL|VECTOR_ADDRESS|macro|VECTOR_ADDRESS
 DECL|VECTOR_ADDRESS|macro|VECTOR_ADDRESS
 DECL|VECTOR_ADDRESS|macro|VECTOR_ADDRESS
 DECL|_PrepC|function|void _PrepC(void)
+DECL|_vector_table_pointer|variable|_vector_table_pointer
 DECL|enable_floating_point|function|static inline void enable_floating_point(void)
 DECL|enable_floating_point|function|static inline void enable_floating_point(void)
 DECL|relocate_vector_table|function|static inline void relocate_vector_table(void)
