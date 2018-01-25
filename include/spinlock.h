b484c91019e5e8685bdbf351310da4ce44a7d0b7
Andy Ross <andrew.j.ross@intel.com>
2018-01-25 14:04:32 -0800

    kernel: Simple spinlock API
    Minimal spinlock API based on the existing atomic.h layer.  Usage
    works just like irq_lock(), but takes an argument to a specific struct
    k_spinlock_t to un/lock.  No attempt at implementing fairness or
    backoff semantics.  No attempt made at architecture-specific assembly.
    
    When CONFIG_SMP is not enabled, this code falls back to a zero-size
    struct and becomes functionally identical to irq_lock/unlock().
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 7a023cfb89cb427ebaf8affc4373595b3cf87abb

---
+++ include/spinlock.h
@@ -0,0 +1,9 @@
+DECL|_SPINLOCK_H|macro|_SPINLOCK_H
+DECL|k_spin_lock|function|static inline k_spinlock_key_t k_spin_lock(struct k_spinlock *l)
+DECL|k_spin_unlock|function|static inline void k_spin_unlock(struct k_spinlock *l, k_spinlock_key_t key)
+DECL|k_spinlock_key_t|typedef|typedef struct k_spinlock_key k_spinlock_key_t;
+DECL|k_spinlock_key|struct|struct k_spinlock_key {
+DECL|k_spinlock|struct|struct k_spinlock {
+DECL|key|member|int key;
+DECL|locked|member|atomic_t locked;
+DECL|saved_key|member|int saved_key;
