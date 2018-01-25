be19d8cfe4fe7d86205b992e5e7ed08d240e4be2
Andy Ross <andrew.j.ross@intel.com>
2018-01-25 14:09:24 -0800

    tests/kernel: Add spinlock test
    Simple test of spinlock semantics.  Bounce between two CPUs locking
    and releasing, validating that nothing changes at unexpected times.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 73678c43882cf55c18f0030fc59d6e6a0f8ead89

---
+++ tests/kernel/spinlock/src/main.c
@@ -0,0 +1,9 @@
+DECL|CPU1_STACK_SIZE|macro|CPU1_STACK_SIZE
+DECL|bounce_done|variable|bounce_done
+DECL|bounce_lock|variable|bounce_lock
+DECL|bounce_once|function|void bounce_once(int id)
+DECL|bounce_owner|variable|bounce_owner
+DECL|cpu1_fn|function|void cpu1_fn(int key, void *arg)
+DECL|test_main|function|void test_main(void)
+DECL|test_spinlock_basic|function|void test_spinlock_basic(void)
+DECL|test_spinlock_bounce|function|void test_spinlock_bounce(void)
