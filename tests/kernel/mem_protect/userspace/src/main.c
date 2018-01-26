fef18ca6663230e98782dc75a3fcee3ece6cc8db
Joshua Domagalski <jedomag@tycho.nsa.gov>
2018-01-26 11:31:05 -0500

    tests: userspace: test revoke, user_mode_enter
    Added three tests for kernel objects focusing on 1) revoking
    access to a k_object that the thread does not have
    permissions to access, 2) accessing a k_object after
    permissions to access it were revoked, and 3) trying to
    revoke access to a k_object from a parent thread by a
    child thread.  Additionally, added a test for
    k_thread_user_mode_enter().
    
    revoke_noperms_object() tests by calling
    k_object_access_revoke() on a semaphore (kernel object) that it
    does not have access to (ksem).
    
    access_after_revoke() tests ability to access a semaphore after
    access has been revoked by itself.
    
    revoke_other_thread() tests whether a thread can revoke access
    for an object for which it has permissions from a thread for
    which it does not have permissions.
    
    user_mode_enter() tests whether k_thread_user_mode_enter()
    truly enters user mode.
    
    Tested on qemu_x86 and frdm_k64 with pr-4974 applied, passes
    on qemu_x86 but requires small fix for ARM (will submit
    separately).
    
    Signed-off-by: Joshua Domagalski <jedomag@tycho.nsa.gov>
    
    Former-commit-id: 3ad0207321550dc3b4d27ef28e15f724e3c37067

--- tests/kernel/mem_protect/userspace/src/main.c
+++ tests/kernel/mem_protect/userspace/src/main.c
@@ -1,6 +1,7 @@
 DECL|INFO|macro|INFO
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
+DECL|access_after_revoke|function|static void access_after_revoke(void)
 DECL|disable_mmu_mpu|function|static void disable_mmu_mpu(void)
 DECL|give_uthread_end_sem|variable|give_uthread_end_sem
 DECL|is_usermode|function|static void is_usermode(void)
@@ -14,6 +15,9 @@ DECL|read_kernel_data|function|static void read_kernel_data(void)
 DECL|read_kernel_stack|function|static void read_kernel_stack(void)
 DECL|read_kernram|function|static void read_kernram(void)
 DECL|read_other_stack|function|static void read_other_stack(void)
+DECL|revoke_from_parent|function|static void revoke_from_parent(k_tid_t parentThread)
+DECL|revoke_noperms_object|function|static void revoke_noperms_object(void)
+DECL|revoke_other_thread|function|static void revoke_other_thread(void)
 DECL|sem|variable|sem
 DECL|size|variable|size
 DECL|size|variable|size
@@ -21,6 +25,8 @@ DECL|size|variable|size
 DECL|start_kernel_thread|function|static void start_kernel_thread(void)
 DECL|test_main|function|void test_main(void)
 DECL|thread_body|function|void thread_body(void)
+DECL|umode_enter_func|function|static void umode_enter_func(void)
+DECL|user_mode_enter|function|static void user_mode_enter(void)
 DECL|uthread_body|function|static void uthread_body(void)
 DECL|uthread_thread|variable|uthread_thread
 DECL|write_control|function|static void write_control(void)
