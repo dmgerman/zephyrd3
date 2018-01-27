d84c2f85b24ec8b3d693075e71d8c4e90bc2b789
Michael Hope <mlhx@google.com>
2018-01-27 09:37:56 +0100

    gpio: sam0: add pull up/pull down support.
    Also trim back the headers.
    
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: c83f0782b67576eca78715fc022290b7a086b88d

--- drivers/gpio/gpio_sam0.c
+++ drivers/gpio/gpio_sam0.c
@@ -4,7 +4,7 @@ DECL|gpio_sam0_config_0|variable|gpio_sam0_config_0
 DECL|gpio_sam0_config_1|variable|gpio_sam0_config_1
 DECL|gpio_sam0_config|function|static int gpio_sam0_config(struct device *dev, int access_op, u32_t pin, int flags)
 DECL|gpio_sam0_config|struct|struct gpio_sam0_config {
-DECL|gpio_sam0_init|function|int gpio_sam0_init(struct device *dev) { return 0; }
+DECL|gpio_sam0_init|function|static int gpio_sam0_init(struct device *dev) { return 0; }
 DECL|gpio_sam0_read|function|static int gpio_sam0_read(struct device *dev, int access_op, u32_t pin, u32_t *value)
 DECL|gpio_sam0_write|function|static int gpio_sam0_write(struct device *dev, int access_op, u32_t pin, u32_t value)
 DECL|regs|member|PortGroup *regs;
