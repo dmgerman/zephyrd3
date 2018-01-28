75aa9eae3eb32defdbe13ca6731d0631441142b2
Vinayak Kariappa Chettimada <vich@nordicsemi.no>
2018-01-28 05:38:10 +0100

    Bluetooth: controller: Fix to restrict addr set in active states
    Fixed implementation to disallow setting Bluetooth device
    address under active advertising or scanning states.
    
    Fixes LL.TS.5.0.2 conformance tests:
    LL/CON/INI/BV-01-C [Connection Initiation]
    LL/SEC/ADV/BV-01-C [Advertising With Static Address]
    LL/SEC/SCN/BV-01-C [Random Address Scanning]
    
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: f11750577361692753327a3db46b6a88b1fc6510

--- subsys/bluetooth/controller/ll_sw/ll_addr.c
+++ subsys/bluetooth/controller/ll_sw/ll_addr.c
@@ -1,4 +1,4 @@
 DECL|ll_addr_get|function|u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
-DECL|ll_addr_set|function|void ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
+DECL|ll_addr_set|function|u32_t ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
 DECL|pub_addr|variable|pub_addr
 DECL|rnd_addr|variable|rnd_addr
