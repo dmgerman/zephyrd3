8795dfc57f941ded47ab0813cd772cfe1569349e
Alberto Escolar Piedras <alpi@oticon.com>
2018-01-28 16:18:06 +0100

    native: minor consistency rename
    rename main_clean_up() to posix_exit() for consistency
    with all other global functions of this architecture
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: a35364dd1aebd52301044c4b139f891a51e778f9

--- boards/posix/native_posix/main.c
+++ boards/posix/native_posix/main.c
@@ -1,3 +1,3 @@
 DECL|STOP_AFTER_5_SECONDS|macro|STOP_AFTER_5_SECONDS
-DECL|main_clean_up|function|void main_clean_up(int exit_code)
 DECL|main|function|int main(int argc, char *argv[])
+DECL|posix_exit|function|void posix_exit(int exit_code)
