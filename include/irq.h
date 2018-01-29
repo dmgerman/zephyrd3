09089bc057dba0e95b49bf62f9924d41e19fdd0e
Andy Ross <andrew.j.ross@intel.com>
2018-01-29 09:23:49 -0800

    kernel: Make irq_{un}lock() APIs into a global spinlock in SMP mode
    In SMP mode, the idea of a single "IRQ lock" goes away.  Long term,
    all usage needs to migrate to spinlocks (which become simple IRQ locks
    in the uniprocessor case).  For the near term, we can ease the
    migration (at the expense of performance) by providing a compatibility
    implementation around a single global lock.
    
    Note that one complication is that the older lock was recursive, while
    spinlocks will deadlock if you try to lock them twice.  So we
    implement a simple "count" semantic to handle multiple locks.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 364cbae4128cbad61451446b8c724db210f0f109

--- include/irq.h
+++ include/irq.h
@@ -9,4 +9,6 @@ DECL|irq_disable|macro|irq_disable
 DECL|irq_enable|macro|irq_enable
 DECL|irq_is_enabled|macro|irq_is_enabled
 DECL|irq_lock|macro|irq_lock
+DECL|irq_lock|macro|irq_lock
+DECL|irq_unlock|macro|irq_unlock
 DECL|irq_unlock|macro|irq_unlock
---
+++ kernel/smp.c
@@ -0,0 +1,5 @@
+DECL|KEY_RECURSIVE|macro|KEY_RECURSIVE
+DECL|_smp_global_lock|function|unsigned int _smp_global_lock(void)
+DECL|_smp_global_unlock|function|void _smp_global_unlock(unsigned int key)
+DECL|global_spinlock|variable|global_spinlock
+DECL|recursive_count|variable|recursive_count
