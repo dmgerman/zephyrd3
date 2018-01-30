a618c56cc2348987d881bac8a80c549806a66d3c
Michael Hope <mlhx@google.com>
2018-01-30 20:37:46 +0100

    pinmux: sam0: add a pinmux driver for the SAM0 series.
    Pin multiplexing is a function of the PORT peripheral.  This change
    defines a separate pinmux device at the same address as the PORTs
    themselves.
    
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: 0a5725e85809e9939c355ebad9286836eeac969c

---
+++ drivers/pinmux/pinmux_sam0.c
@@ -0,0 +1,10 @@
+DECL|pinmux_sam0_api|variable|pinmux_sam0_api
+DECL|pinmux_sam0_config_0|variable|pinmux_sam0_config_0
+DECL|pinmux_sam0_config_1|variable|pinmux_sam0_config_1
+DECL|pinmux_sam0_config|struct|struct pinmux_sam0_config {
+DECL|pinmux_sam0_get|function|static int pinmux_sam0_get(struct device *dev, u32_t pin, u32_t *func)
+DECL|pinmux_sam0_init|function|static int pinmux_sam0_init(struct device *dev)
+DECL|pinmux_sam0_input|function|static int pinmux_sam0_input(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_sam0_pullup|function|static int pinmux_sam0_pullup(struct device *dev, u32_t pin, u8_t func)
+DECL|pinmux_sam0_set|function|static int pinmux_sam0_set(struct device *dev, u32_t pin, u32_t func)
+DECL|regs|member|PortGroup *regs;
