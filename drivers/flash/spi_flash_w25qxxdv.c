a7fc01dc5a19ba6c57fe17f7871418fe213c1887
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-01-30 15:01:36 +0100

    drivers/flash: Switch W25QXXDV driver to new SPI API
    This saves quite some memory.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 16cb7ab883332fce0b6075c0e840c1919db23862

--- drivers/flash/spi_flash_w25qxxdv.c
+++ drivers/flash/spi_flash_w25qxxdv.c
@@ -1,13 +1,14 @@
 DECL|spi_flash_api|variable|spi_flash_api
 DECL|spi_flash_init|function|static int spi_flash_init(struct device *dev)
 DECL|spi_flash_memory_data|variable|spi_flash_memory_data
-DECL|spi_flash_wb_config|function|static int spi_flash_wb_config(struct device *dev)
+DECL|spi_flash_wb_access|function|static int spi_flash_wb_access(struct spi_flash_data *ctx, u8_t cmd, bool addressed, off_t offset, void *data, size_t length, bool write)
+DECL|spi_flash_wb_configure|function|static int spi_flash_wb_configure(struct device *dev)
 DECL|spi_flash_wb_erase_internal|function|static inline int spi_flash_wb_erase_internal(struct device *dev, off_t offset, size_t size)
 DECL|spi_flash_wb_erase|function|static int spi_flash_wb_erase(struct device *dev, off_t offset, size_t size)
 DECL|spi_flash_wb_id|function|static inline int spi_flash_wb_id(struct device *dev)
 DECL|spi_flash_wb_read|function|static int spi_flash_wb_read(struct device *dev, off_t offset, void *data, size_t len)
-DECL|spi_flash_wb_reg_read|function|static int spi_flash_wb_reg_read(struct device *dev, u8_t *data)
-DECL|spi_flash_wb_reg_write|function|static int spi_flash_wb_reg_write(struct device *dev, u8_t *data)
+DECL|spi_flash_wb_reg_read|function|static u8_t spi_flash_wb_reg_read(struct device *dev, u8_t reg)
+DECL|spi_flash_wb_reg_write|function|static int spi_flash_wb_reg_write(struct device *dev, u8_t reg)
 DECL|spi_flash_wb_write_protection_set|function|static int spi_flash_wb_write_protection_set(struct device *dev, bool enable)
 DECL|spi_flash_wb_write|function|static int spi_flash_wb_write(struct device *dev, off_t offset, const void *data, size_t len)
 DECL|wait_for_flash_idle|function|static inline void wait_for_flash_idle(struct device *dev)
--- drivers/flash/spi_flash_w25qxxdv.h
+++ drivers/flash/spi_flash_w25qxxdv.h
@@ -1,5 +1,5 @@
 DECL|__SPI_FLASH_W25QXXDV_H__|macro|__SPI_FLASH_W25QXXDV_H__
-DECL|buf|member|u8_t buf[CONFIG_SPI_FLASH_W25QXXDV_MAX_DATA_LEN +
 DECL|sem|member|struct k_sem sem;
+DECL|spi_cfg|member|struct spi_config spi_cfg;
 DECL|spi_flash_data|struct|struct spi_flash_data {
 DECL|spi|member|struct device *spi;
