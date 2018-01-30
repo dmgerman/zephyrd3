693cdf499fca4e992f793e9eb6d72611a28389dd
Alberto Escolar Piedras <alpi@oticon.com>
2018-01-30 10:31:36 +0100

    shell: Added noprompt command
    Added noprompt command to shell. It will disable printing
    the prompt.
    For the native port, when feeding commands from a file or
    pipe the prompt reprinting after each command (without echoing)
    just confuses the user.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: bfabdf1de5ff84de70cb3b95bf0d2027196c9c23

--- subsys/shell/shell.c
+++ subsys/shell/shell.c
@@ -6,6 +6,7 @@ DECL|NUM_OF_SHELL_CMDS|macro|NUM_OF_SHELL_CMDS
 DECL|NUM_OF_SHELL_ENTITIES|macro|NUM_OF_SHELL_ENTITIES
 DECL|PROMPT_MAX_LEN|macro|PROMPT_MAX_LEN
 DECL|PROMPT_SUFFIX|macro|PROMPT_SUFFIX
+DECL|SHELL_CMD_NOPROMPT|macro|SHELL_CMD_NOPROMPT
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|app_cmd_handler|variable|app_cmd_handler
 DECL|app_prompt_handler|variable|app_prompt_handler
@@ -13,6 +14,7 @@ DECL|avail_queue|variable|avail_queue
 DECL|buf|variable|buf
 DECL|cmd_exit|function|static int cmd_exit(int argc, char *argv[])
 DECL|cmd_help|function|static int cmd_help(int argc, char *argv[])
+DECL|cmd_noprompt|function|static int cmd_noprompt(int argc, char *argv[])
 DECL|cmd_select|function|static int cmd_select(int argc, char *argv[])
 DECL|cmds_queue|variable|cmds_queue
 DECL|completion|function|static u8_t completion(char *line, u8_t len)
@@ -27,6 +29,7 @@ DECL|get_prompt|function|static const char *get_prompt(void)
 DECL|get_standalone|function|static const struct shell_cmd *get_standalone(const char *command)
 DECL|line2argv|function|static size_t line2argv(char *str, char *argv[], size_t size)
 DECL|line_queue_init|function|static void line_queue_init(void)
+DECL|no_promt|variable|no_promt
 DECL|print_module_commands|function|static void print_module_commands(struct shell_module *module)
 DECL|prompt|variable|prompt
 DECL|set_default_module|function|static int set_default_module(const char *name)
