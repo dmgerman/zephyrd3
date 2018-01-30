7b31a661216a6c1010e58e72791699ee79f0c252
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-01-30 10:47:26 +0530

    tests: boards: Add QSPI flash test app for altera_max10
    Add test application to demonstrate the usage of Nios-II
    QSPI Flash soft IP.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: 727653333ee6a6619c24a9072ae2d353e528139c

---
+++ tests/boards/altera_max10/qspi/src/qspi_flash.c
@@ -0,0 +1,6 @@
+DECL|FLASH_SECTOR_SIZE|macro|FLASH_SECTOR_SIZE
+DECL|MAX_NUM_OF_SECTORS|macro|MAX_NUM_OF_SECTORS
+DECL|NUM_OF_SECTORS_TO_TEST|macro|NUM_OF_SECTORS_TO_TEST
+DECL|TEST_DATA_LEN|macro|TEST_DATA_LEN
+DECL|test_main|function|void test_main(void)
+DECL|testing_qspi_flash|function|void testing_qspi_flash(void)
