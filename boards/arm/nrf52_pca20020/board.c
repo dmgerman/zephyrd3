7c25c5c76f59ede9233be42b683f8894551f0f7c
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-01-30 23:03:57 +0200

    boards: Add support for nRF52 Thingy:52 (PCA20020)
    A new board configuration is added for Thingy:52, also known as
    PCA20020. This includes support for some of the on-board sensors, and
    other peripherals such as the on-board GPIO expander.
    
    The board power rail sequencing is also implemented, as the rails need
    to be powered up in a correct order and before certain device driver
    init calls are made.
    
    Signed-off-by: Aapo Vienamo <aapo.vienamo@iki.fi>
    
    Former-commit-id: 439a63daacec35af8349928baf0bc94a5ca61a12

---
+++ boards/arm/nrf52_pca20020/board.c
@@ -0,0 +1,8 @@
+DECL|CCS_VDD_PWR_CTRL_GPIO_PIN|macro|CCS_VDD_PWR_CTRL_GPIO_PIN
+DECL|VDD_PWR_CTRL_GPIO_PIN|macro|VDD_PWR_CTRL_GPIO_PIN
+DECL|ccs_vdd_pwr_ctrl_cfg|variable|ccs_vdd_pwr_ctrl_cfg
+DECL|pin|member|u32_t pin;
+DECL|port|member|const char *port;
+DECL|pwr_ctrl_cfg|struct|struct pwr_ctrl_cfg {
+DECL|pwr_ctrl_init|function|static int pwr_ctrl_init(struct device *dev)
+DECL|vdd_pwr_ctrl_cfg|variable|vdd_pwr_ctrl_cfg
