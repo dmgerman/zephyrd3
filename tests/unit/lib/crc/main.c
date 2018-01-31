557de4a0b18025b4164e8c73a8b093f3765bde81
Michael Hope <mlhx@google.com>
2018-01-31 19:45:05 +0100

    crc: add test vectors for the ANSI version.
    The test cases match
    http://reveng.sourceforge.net/crc-catalogue/16.htm for
    CRC-16/DDS-110.
    
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: 88b1ad138eb9b33c0351c7d1decfddc2bacd8c33

--- tests/unit/lib/crc/main.c
+++ tests/unit/lib/crc/main.c
@@ -1,3 +1,4 @@
+DECL|test_crc16_ansi|function|void test_crc16_ansi(void)
 DECL|test_crc16|function|void test_crc16(void)
 DECL|test_crc8_ccitt|function|void test_crc8_ccitt(void)
 DECL|test_main|function|void test_main(void)
