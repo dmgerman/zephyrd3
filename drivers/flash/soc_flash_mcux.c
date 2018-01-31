8cd75400c11739eab3205033028ea02a2659a3da
Maureen Helm <maureen.helm@nxp.com>
2018-01-31 14:22:11 -0600

    drivers: flash: Implement flash page layout api in mcux driver
    Uses the new flash erase-block-size dts property to implement the flash
    page layout api in the mcux flash driver.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 262595f09ff23078e1382338b22634fcc70861ec

--- drivers/flash/soc_flash_mcux.c
+++ drivers/flash/soc_flash_mcux.c
@@ -1,8 +1,10 @@
 DECL|config|member|flash_config_t config;
+DECL|dev_layout|variable|dev_layout
 DECL|flash_data|variable|flash_data
 DECL|flash_mcux_api|variable|flash_mcux_api
 DECL|flash_mcux_erase|function|static int flash_mcux_erase(struct device *dev, off_t offset, size_t len)
 DECL|flash_mcux_init|function|static int flash_mcux_init(struct device *dev)
+DECL|flash_mcux_pages_layout|function|static void flash_mcux_pages_layout(struct device *dev,const struct flash_pages_layout **layout, size_t *layout_size)
 DECL|flash_mcux_read|function|static int flash_mcux_read(struct device *dev, off_t offset,void *data, size_t len)
 DECL|flash_mcux_write_protection|function|static int flash_mcux_write_protection(struct device *dev, bool enable)
 DECL|flash_mcux_write|function|static int flash_mcux_write(struct device *dev, off_t offset,const void *data, size_t len)
