ca25d12483e5198ce47bd5ab1760b12f7471cddb
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-02-01 11:21:48 +0100

    samples/drivers: Switch Fujistu FRAM sample to new SPI API
    Replacing legacy API calls by news ones.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 308f4df91d0c813acf028f592e02b43b1694152c

--- samples/drivers/spi_fujitsu_fram/src/main.c
+++ samples/drivers/spi_fujitsu_fram/src/main.c
@@ -1,9 +1,12 @@
 DECL|MAX_USER_DATA_LENGTH|macro|MAX_USER_DATA_LENGTH
 DECL|MB85RS64V_MANUFACTURER_ID_CMD|macro|MB85RS64V_MANUFACTURER_ID_CMD
+DECL|MB85RS64V_READ_CMD|macro|MB85RS64V_READ_CMD
+DECL|MB85RS64V_WRITE_CMD|macro|MB85RS64V_WRITE_CMD
+DECL|MB85RS64V_WRITE_ENABLE_CMD|macro|MB85RS64V_WRITE_ENABLE_CMD
 DECL|cmp_data|variable|cmp_data
 DECL|data|variable|data
 DECL|main|function|void main(void)
-DECL|mb85rs64v_read_id|function|static int mb85rs64v_read_id(struct device *dev)
-DECL|read_bytes|function|static int read_bytes(struct device *dev, u16_t addr, u8_t *data, u32_t num_bytes)
-DECL|spi_buffer|variable|spi_buffer
-DECL|write_bytes|function|static int write_bytes(struct device *dev, u16_t addr, u8_t *data, u32_t num_bytes)
+DECL|mb85rs64v_access|function|static int mb85rs64v_access(struct device *spi, struct spi_config *spi_cfg, u8_t cmd, u16_t addr, void *data, size_t len)
+DECL|mb85rs64v_read_id|function|static int mb85rs64v_read_id(struct device *spi, struct spi_config *spi_cfg)
+DECL|read_bytes|function|static int read_bytes(struct device *spi, struct spi_config *spi_cfg, u16_t addr, u8_t *data, u32_t num_bytes)
+DECL|write_bytes|function|static int write_bytes(struct device *spi, struct spi_config *spi_cfg, u16_t addr, u8_t *data, u32_t num_bytes)
