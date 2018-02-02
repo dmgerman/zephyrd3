9a8feba889efe440873ad36f7d26a745b3f30067
Andy Ross <andrew.j.ross@intel.com>
2018-02-02 13:49:30 -0800

    tests/kernel: SMP test
    Simple SMP test to validate the two threads can be simultaneously
    scheduled.  Arranges things such that both threads are at different
    priorities and never yield the CPU, so on a uniprocessor build they
    cannot be fairly scheduled.  Checks that both are nonetheless making
    progress.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 59cdfe6e441cd670780bc25b0ef63ef1e4afbff3

---
+++ tests/kernel/smp/src/main.c
@@ -0,0 +1,6 @@
+DECL|DELAY_US|macro|DELAY_US
+DECL|T2_STACK_SIZE|macro|T2_STACK_SIZE
+DECL|t2_count|variable|t2_count
+DECL|t2_fn|function|void t2_fn(void *a, void *b, void *c)
+DECL|t2|variable|t2
+DECL|test_main|function|void test_main(void)
