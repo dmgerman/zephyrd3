3c68d27ca7a9eed9023a69ef6ffc3fd0c61e4289
Youvedeep Singh <youvedeep.singh@intel.com>
2018-02-04 11:40:52 +0530

    tests: kernel: posix: pthread_join: Add pthread join test.
    Implement pthread_join test application.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 2e43d001eab7f0b933728bcb0136deb08e70db98

---
+++ tests/kernel/posix/pthread_join/src/pthread.c
@@ -0,0 +1,8 @@
+DECL|N_THR|macro|N_THR
+DECL|ONE_SECOND|macro|ONE_SECOND
+DECL|STACKSZ|macro|STACKSZ
+DECL|THREAD_PRIORITY|macro|THREAD_PRIORITY
+DECL|exit_count|variable|exit_count
+DECL|is_sched_prio_valid|function|static bool is_sched_prio_valid(int prio, int policy)
+DECL|main|function|void main(void)
+DECL|thread_top|function|void *thread_top(void *p1)
