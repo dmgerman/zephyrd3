7435d6187502b48cf2a0046be034a233e65409ee
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-02-05 16:41:57 +0530

    tests: pipe: pipe_api: Converted pipe test cases to run in usermode.
    Converted few test cases to run in usermode.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: db8c5439c1e2ed3877ca7440528a0a4035d07ed4

--- tests/kernel/pipe/pipe_api/src/test_pipe_fail.c
+++ tests/kernel/pipe/pipe_api/src/test_pipe_fail.c
@@ -1,4 +1,5 @@
 DECL|PIPE_LEN|macro|PIPE_LEN
 DECL|TIMEOUT|macro|TIMEOUT
+DECL|put_get_pipe|variable|put_get_pipe
 DECL|test_pipe_get_fail|function|void test_pipe_get_fail(void *p1, void *p2, void *p3)
 DECL|test_pipe_put_fail|function|void test_pipe_put_fail(void *p1, void *p2, void *p3)
