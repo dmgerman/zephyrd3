091216fc8f8b6d4f9d8edd11bcd57a4e357b0389
Loic Poulain <loic.poulain@linaro.org>
2018-02-05 16:47:12 +0100

    usb: Add transfer management API
    The transfer API provides 'high' level functions to manage sending and
    reception of USB data. A USB (class) driver has to register the generic
    usb_transfer_ep_callback as endpoint status callback in order to use
    the API.
    
    With this API, the class driver does not need to take care of low-level
    usb transfer management (packet splitting, ZLP, synchronization...).
    The usb_transfer methods will split transfer into multiple transactions
    depending endpoint max size and controller capabilities.
    
    Once the transfer is completed, class driver is notified by a callback.
    The usb_transfer method can be executed in IRQ/atomic context.
    A usb_transfer synchronous helper exists which block-waits until
    transfer completion.
    
    In write case, a transfer is complete when all data has been sent.
    In read case, a transfer is complete when the exact amount of data
    requested has been received or if a short-pkt (including ZLP) is
    received.
    
    transfer methods are thread-safe.
    
    A transfer can be cancelled at any time.
    
    Signed-off-by: Loic Poulain <loic.poulain@linaro.org>
    
    Former-commit-id: 16921b069869e95bca835c9dd5b55e5db350512f

--- include/usb/usb_device.h
+++ include/usb/usb_device.h
@@ -1,5 +1,8 @@
 DECL|MAX_PACKET_SIZE0|macro|MAX_PACKET_SIZE0
 DECL|USB_DEVICE_H_|macro|USB_DEVICE_H_
+DECL|USB_TRANS_NO_ZLP|macro|USB_TRANS_NO_ZLP
+DECL|USB_TRANS_READ|macro|USB_TRANS_READ
+DECL|USB_TRANS_WRITE|macro|USB_TRANS_WRITE
 DECL|bRequest|member|u8_t bRequest; /**< specific request */
 DECL|bmRequestType|member|u8_t bmRequestType; /**< characteristics of the specific request */
 DECL|cb_usb_status|member|usb_status_callback cb_usb_status;
@@ -19,6 +22,7 @@ DECL|usb_interface_cfg_data|struct|struct usb_interface_cfg_data {
 DECL|usb_request_handler|typedef|typedef int (*usb_request_handler) (struct usb_setup_packet *detup,
 DECL|usb_setup_packet|struct|struct usb_setup_packet {
 DECL|usb_status_callback|typedef|typedef void (*usb_status_callback)(enum usb_dc_status_code status_code,
+DECL|usb_transfer_callback|typedef|typedef void (*usb_transfer_callback)(u8_t ep, int tsize, void *priv);
 DECL|vendor_data|member|u8_t *vendor_data;
 DECL|vendor_handler|member|usb_request_handler vendor_handler;
 DECL|wIndex|member|u16_t wIndex; /**< request specific parameter */
--- subsys/usb/usb_device.c
+++ subsys/usb/usb_device.c
@@ -10,11 +10,15 @@ DECL|INTF_DESC_bAlternateSetting|macro|INTF_DESC_bAlternateSetting
 DECL|INTF_DESC_bInterfaceNumber|macro|INTF_DESC_bInterfaceNumber
 DECL|MAX_DESC_HANDLERS|macro|MAX_DESC_HANDLERS
 DECL|MAX_NUM_REQ_HANDLERS|macro|MAX_NUM_REQ_HANDLERS
+DECL|MAX_NUM_TRANSFERS|macro|MAX_NUM_TRANSFERS
 DECL|MAX_STD_REQ_MSG_SIZE|macro|MAX_STD_REQ_MSG_SIZE
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|SYS_LOG_NO_NEWLINE|macro|SYS_LOG_NO_NEWLINE
 DECL|USB_CONTROL_IN_EP0|macro|USB_CONTROL_IN_EP0
 DECL|USB_CONTROL_OUT_EP0|macro|USB_CONTROL_OUT_EP0
+DECL|bsize|member|size_t bsize;
+DECL|buffer|member|u8_t *buffer;
+DECL|cb|member|usb_transfer_callback cb;
 DECL|configuration|member|u8_t configuration;
 DECL|custom_req_handler|member|usb_request_handler custom_req_handler;
 DECL|data_buf_len|member|s32_t data_buf_len;
@@ -23,10 +27,20 @@ DECL|data_buf|member|u8_t *data_buf;
 DECL|data_store|member|u8_t *data_store[MAX_NUM_REQ_HANDLERS];
 DECL|descriptors|member|const u8_t *descriptors;
 DECL|enabled|member|bool enabled;
+DECL|ep|member|u8_t ep;
+DECL|flags|member|unsigned int flags;
+DECL|priv|member|void *priv;
 DECL|req_handlers|member|usb_request_handler req_handlers[MAX_NUM_REQ_HANDLERS];
+DECL|sem|member|struct k_sem sem;
+DECL|sem|member|struct k_sem sem;
 DECL|setup|member|struct usb_setup_packet setup;
 DECL|status_callback|member|usb_status_callback status_callback;
+DECL|status|member|int status;
 DECL|std_req_data|member|u8_t std_req_data[MAX_STD_REQ_MSG_SIZE];
+DECL|transfer|member|struct usb_transfer_data transfer[MAX_NUM_TRANSFERS];
+DECL|tsize|member|int tsize;
+DECL|tsize|member|size_t tsize;
+DECL|usb_cancel_transfer|function|void usb_cancel_transfer(u8_t ep)
 DECL|usb_data_to_host|function|static void usb_data_to_host(void)
 DECL|usb_deconfig|function|int usb_deconfig(void)
 DECL|usb_dev_priv|struct|static struct usb_dev_priv {
@@ -34,6 +48,7 @@ DECL|usb_dev|variable|usb_dev
 DECL|usb_disable|function|int usb_disable(void)
 DECL|usb_enable|function|int usb_enable(struct usb_cfg_data *config)
 DECL|usb_ep_clear_stall|function|int usb_ep_clear_stall(u8_t ep)
+DECL|usb_ep_get_transfer|function|static struct usb_transfer_data *usb_ep_get_transfer(u8_t ep)
 DECL|usb_ep_read_continue|function|int usb_ep_read_continue(u8_t ep)
 DECL|usb_ep_read_wait|function|int usb_ep_read_wait(u8_t ep, u8_t *data, u32_t max_data_len,u32_t *ret_bytes)
 DECL|usb_ep_set_stall|function|int usb_ep_set_stall(u8_t ep)
@@ -53,5 +68,13 @@ DECL|usb_register_status_callback|function|static void usb_register_status_callb
 DECL|usb_set_configuration|function|static bool usb_set_configuration(u8_t config_index, u8_t alt_setting)
 DECL|usb_set_config|function|int usb_set_config(struct usb_cfg_data *config)
 DECL|usb_set_interface|function|static bool usb_set_interface(u8_t iface, u8_t alt_setting)
+DECL|usb_transfer_data|struct|struct usb_transfer_data {
+DECL|usb_transfer_ep_callback|function|void usb_transfer_ep_callback(u8_t ep, enum usb_dc_ep_cb_status_code status)
+DECL|usb_transfer_sync_cb|function|static void usb_transfer_sync_cb(u8_t ep, int size, void *priv)
+DECL|usb_transfer_sync_priv|struct|struct usb_transfer_sync_priv {
+DECL|usb_transfer_sync|function|int usb_transfer_sync(u8_t ep, u8_t *data, size_t dlen, unsigned int flags)
+DECL|usb_transfer_work|function|static void usb_transfer_work(struct k_work *item)
+DECL|usb_transfer|function|int usb_transfer(u8_t ep, u8_t *data, size_t dlen, unsigned int flags, usb_transfer_callback cb, void *cb_data)
 DECL|usb_vbus_set|function|static int usb_vbus_set(bool on)
 DECL|usb_write|function|int usb_write(u8_t ep, const u8_t *data, u32_t data_len,u32_t *bytes_ret)
+DECL|work|member|struct k_work work;
