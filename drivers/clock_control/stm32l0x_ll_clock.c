18489a73e8258de3149a58e7133be554e4bb2a7f
Endre Karlson <endre.karlson@gmail.com>
2018-02-05 07:31:07 +0100

    drivers: clock_control: Provide support for stm32l0x
    Add Clock Control support for the stm32l0x based on reference manuals
    
    Signed-off-by: Endre Karlson <endre.karlson@gmail.com>
    
    Former-commit-id: e99a79a9b1359aef38e9e03f9d1a3698872a1677

---
+++ drivers/clock_control/stm32l0x_ll_clock.c
@@ -0,0 +1,6 @@
+DECL|_pll_div|macro|_pll_div
+DECL|_pll_mul|macro|_pll_mul
+DECL|config_enable_default_clocks|function|void config_enable_default_clocks(void)
+DECL|config_pll_init|function|void config_pll_init(LL_UTILS_PLLInitTypeDef *pllinit)
+DECL|pll_div|macro|pll_div
+DECL|pll_mul|macro|pll_mul
