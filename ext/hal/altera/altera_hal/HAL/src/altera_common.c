069253bd332c4c4015d48b49ce5b6a9fc6bddbc5
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-02-06 23:26:18 +0530

    ext: hal: altera: Add wrapper functions for Altera HAL runtime API's
    Add wrapper or helper functions for Altera HAL runtime API's to
    enable Altera HAL drivers on Zephyr.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: b574a9ba9bae54b6bb58805cc918fa5e8f7e208c

--- ext/hal/altera/altera_hal/HAL/src/altera_common.c
+++ ext/hal/altera/altera_hal/HAL/src/altera_common.c
@@ -1,4 +1,6 @@
 DECL|ALTERA_MAX_IRQ|macro|ALTERA_MAX_IRQ
+DECL|alt_busy_sleep|function|unsigned int alt_busy_sleep (unsigned int us)
+DECL|alt_dev_llist_insert|function|int alt_dev_llist_insert (alt_dev_llist* dev, alt_llist* list)
 DECL|alt_hal_isr|variable|alt_hal_isr
 DECL|alt_handle_irq|function|void alt_handle_irq(void* base, alt_u32 id)
 DECL|alt_ic_isr_register|function|int alt_ic_isr_register(alt_u32 ic_id, alt_u32 irq,alt_isr_func isr, void *isr_context, void *flags)
