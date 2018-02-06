41b47fc147f6b9b658bd618292997e26f3f17b23
Johan Hedberg <johan.hedberg@intel.com>
2018-02-06 19:18:43 +0200

    tests: net/buf: Update to new API
    Update the net_buf unit test to test the features from the new API.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 4637620a66daf4c09ed1128b6640d2d0aedc071d

--- tests/net/buf/src/main.c
+++ tests/net/buf/src/main.c
@@ -9,10 +9,8 @@ DECL|destroy_called|variable|destroy_called
 DECL|dst_port|member|u16_t dst_port;
 DECL|dst|member|struct in6_addr dst;
 DECL|example_data|variable|example_data
+DECL|fixed_destroy|function|static void fixed_destroy(struct net_buf *buf)
 DECL|flow|member|u16_t flow;
-DECL|frag_destroy_big|function|static void frag_destroy_big(struct net_buf *buf)
-DECL|frag_destroy_called|variable|frag_destroy_called
-DECL|frag_destroy|function|static void frag_destroy(struct net_buf *buf)
 DECL|hci_opcode|member|u16_t hci_opcode;
 DECL|hci_sync|member|void *hci_sync;
 DECL|hop_limit|member|u8_t hop_limit;
@@ -26,7 +24,10 @@ DECL|net_buf_test_2|function|static void net_buf_test_2(void)
 DECL|net_buf_test_3|function|static void net_buf_test_3(void)
 DECL|net_buf_test_4|function|static void net_buf_test_4(void)
 DECL|net_buf_test_big_buf|function|static void net_buf_test_big_buf(void)
+DECL|net_buf_test_clone|function|static void net_buf_test_clone(void)
+DECL|net_buf_test_fixed_pool|function|static void net_buf_test_fixed_pool(void)
 DECL|net_buf_test_multi_frags|function|static void net_buf_test_multi_frags(void)
+DECL|net_buf_test_var_pool|function|static void net_buf_test_var_pool(void)
 DECL|nexthdr|member|u8_t nexthdr;
 DECL|s6_addr16|macro|s6_addr16
 DECL|s6_addr32|macro|s6_addr32
@@ -41,4 +42,5 @@ DECL|u6_addr16|member|u16_t u6_addr16[8]; /* In big endian */
 DECL|u6_addr32|member|u32_t u6_addr32[4]; /* In big endian */
 DECL|u6_addr8|member|u8_t u6_addr8[16];
 DECL|udp_hdr|struct|struct udp_hdr {
+DECL|var_destroy|function|static void var_destroy(struct net_buf *buf)
 DECL|vtc|member|u8_t vtc;
