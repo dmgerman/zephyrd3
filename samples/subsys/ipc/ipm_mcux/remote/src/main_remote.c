6e813acb6a9ca65d08eabb3f8f4f1278d4ffa966
Stanislav Poboril <stanislav.poboril@nxp.com>
2018-02-07 21:34:35 +0100

    sample: Add MCUX IPM sample application
    Add MCUX IPM sample application. It can be run on lpcxpresso54114
    board at the moment.
    
    We first build the slave core image out of the remote/ dir than the
    primary core image is build which includes the slave core image.
    
    Origin: Original
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 37b7caa6bbdd021997e66f2b0bb5ec84cd86646e

---
+++ samples/subsys/ipc/ipm_mcux/remote/src/main_remote.c
@@ -0,0 +1,3 @@
+DECL|ipm|variable|ipm
+DECL|main|function|void main(void)
+DECL|ping_ipm_callback|function|void ping_ipm_callback(void *context, u32_t id, volatile void *data)
---
+++ samples/subsys/ipc/ipm_mcux/src/main_master.c
@@ -0,0 +1,4 @@
+DECL|gcounter|variable|gcounter
+DECL|ipm|variable|ipm
+DECL|main|function|void main(void)
+DECL|ping_ipm_callback|function|void ping_ipm_callback(void *context, u32_t id, volatile void *data)
