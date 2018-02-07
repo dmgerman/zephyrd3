addbb3238d5c4d61eee6f8f34cbdb5ac0257cd72
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-02-07 14:45:44 +0200

    net: core: Add array support to stack info helpers
    Allow caller to create array of thread stacks using
    NET_STACK_ARRAY_DEFINE() macro. This allows more debug information
    to be printed by "net stacks" command.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 159aaf17400b219c7f01c8c9ace721891051a51e

--- include/net/net_core.h
+++ include/net/net_core.h
@@ -11,12 +11,16 @@ DECL|NET_ERR|macro|NET_ERR
 DECL|NET_INFO|macro|NET_INFO
 DECL|NET_INFO|macro|NET_INFO
 DECL|NET_OK|enumerator|NET_OK, /** Packet has been taken care of */
+DECL|NET_STACK_ARRAY_DEFINE|macro|NET_STACK_ARRAY_DEFINE
+DECL|NET_STACK_ARRAY_DEFINE|macro|NET_STACK_ARRAY_DEFINE
 DECL|NET_STACK_DEFINE_EMBEDDED|macro|NET_STACK_DEFINE_EMBEDDED
 DECL|NET_STACK_DEFINE|macro|NET_STACK_DEFINE
 DECL|NET_STACK_DEFINE|macro|NET_STACK_DEFINE
 DECL|NET_STACK_GET_NAME|macro|NET_STACK_GET_NAME
+DECL|NET_STACK_INFO_ADDR_ARRAY|macro|NET_STACK_INFO_ADDR_ARRAY
 DECL|NET_STACK_INFO_ADDR|macro|NET_STACK_INFO_ADDR
 DECL|NET_STACK_INFO_ADDR|macro|NET_STACK_INFO_ADDR
+DECL|NET_STACK_INFO_ARRAY|macro|NET_STACK_INFO_ARRAY
 DECL|NET_STACK_INFO|macro|NET_STACK_INFO
 DECL|NET_STACK_INFO|macro|NET_STACK_INFO
 DECL|NET_WARN|macro|NET_WARN
@@ -26,6 +30,7 @@ DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|__NET_CORE_H|macro|__NET_CORE_H
+DECL|idx|member|int idx;
 DECL|name|member|const char *name;
 DECL|net_analyze_stack_get_values|function|static inline void net_analyze_stack_get_values(const char *stack,size_t size, unsigned *pcnt, unsigned *unused)
 DECL|net_analyze_stack_get_values|macro|net_analyze_stack_get_values
@@ -35,5 +40,6 @@ DECL|net_stack_info|struct|struct net_stack_info {
 DECL|net_verdict|enum|enum net_verdict {
 DECL|orig_size|member|size_t orig_size;
 DECL|pretty_name|member|const char *pretty_name;
+DECL|prio|member|int prio;
 DECL|size|member|size_t size;
 DECL|stack|member|k_thread_stack_t *stack;
