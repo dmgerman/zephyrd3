e74f689ec19cb6a8523ad6c07dc3e0a8f2904160
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-02-12 21:18:26 +0200

    net: sockets: getaddrinfo() buffer overflow, etc. fixes
    The existing implementation assumed DNS resolv callback will be
    called just once, but that's not always the case (apparently,
    for multi-homes hosts or something). So, apply array bounds
    checking (and do pointer arithmetic only after it, as the C
    standard otherwise warns of "undefined behavior"). In such a case,
    the port number wasn't set in each entry too, so rework how it's
    done.
    
    The issues discovered while resolving archive.ubuntu.com.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: ede25c12a31597a9ddd18fef30ec32a207a2bd13

--- subsys/net/lib/sockets/getaddrinfo.c
+++ subsys/net/lib/sockets/getaddrinfo.c
@@ -5,7 +5,8 @@ DECL|ai_state|variable|ai_state
 DECL|dns_resolve_cb|function|static void dns_resolve_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 DECL|getaddrinfo_state|struct|struct getaddrinfo_state {
 DECL|hints|member|const struct zsock_addrinfo *hints;
-DECL|idx|member|int idx;
+DECL|idx|member|u16_t idx;
+DECL|port|member|u16_t port;
 DECL|sem|member|struct k_sem sem;
 DECL|status|member|int status;
 DECL|zsock_getaddrinfo|function|int zsock_getaddrinfo(const char *host, const char *service, const struct zsock_addrinfo *hints, struct zsock_addrinfo **res)
