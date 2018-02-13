a82abd46b14e55cac4e8f03cf5716bf1e62e48ff
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-02-13 14:28:00 +0200

    samples: sockets: Add big_http_download sample
    This sample downloads a more or less big file (~6MB as preconfigured)
    over HTTP and checks its hash for integrity. It also repeat such a
    download indefinitely, counting total number of bytes transferred.
    
    This is thus a kind of traffic load testing sample. (Ported to C
    from MicroPython original).
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: f303e9d11589e7e35cb6094b7292386a95eb9c7a

---
+++ samples/net/sockets/big_http_download/src/big_http_download.c
@@ -0,0 +1,20 @@
+DECL|CHECK|macro|CHECK
+DECL|SSTRLEN|macro|SSTRLEN
+DECL|cur_bytes|variable|cur_bytes
+DECL|download_hash|variable|download_hash
+DECL|download_url|variable|download_url
+DECL|download|function|void download(struct addrinfo *ai)
+DECL|dump_addrinfo|function|void dump_addrinfo(const struct addrinfo *ai)
+DECL|fatal|function|void fatal(const char *msg)
+DECL|hash_ctx|variable|hash_ctx
+DECL|hash_info|variable|hash_info
+DECL|host|variable|host
+DECL|main|function|int main(void)
+DECL|port|variable|port
+DECL|print_hex|function|void print_hex(const unsigned char *p, int len)
+DECL|response_hash|variable|response_hash
+DECL|response|variable|response
+DECL|sendall|function|ssize_t sendall(int sock, const void *buf, size_t len)
+DECL|skip_headers|function|int skip_headers(int sock)
+DECL|sleep|macro|sleep
+DECL|uri_path|variable|uri_path
