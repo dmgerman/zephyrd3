f7db3d96eadf4ba98adc0ced6fd57c028cd7572a
Pedro Martucci <pedropaulomartucci@gmail.com>
2018-02-14 11:03:15 -0200

    drivers/ieee802154: Add hw filtering support for upipe driver
    Implements HW filtering for IEEE802.15.4 UART Pipe driver.
    
    Signed-off-by: Pedro Martucci <pedropaulomartucci@gmail.com>
    
    Former-commit-id: ede5b8ffc3bc6642949e2590cd49397a86662f17

--- drivers/ieee802154/ieee802154_uart_pipe.c
+++ drivers/ieee802154/ieee802154_uart_pipe.c
@@ -1,6 +1,20 @@
+DECL|BROADCAST_ADDRESS|macro|BROADCAST_ADDRESS
+DECL|DEST_ADDR_OFFSET|macro|DEST_ADDR_OFFSET
+DECL|DEST_ADDR_TYPE_EXTENDED|macro|DEST_ADDR_TYPE_EXTENDED
+DECL|DEST_ADDR_TYPE_MASK|macro|DEST_ADDR_TYPE_MASK
+DECL|DEST_ADDR_TYPE_OFFSET|macro|DEST_ADDR_TYPE_OFFSET
+DECL|DEST_ADDR_TYPE_SHORT|macro|DEST_ADDR_TYPE_SHORT
+DECL|EXTENDED_ADDRESS_SIZE|macro|EXTENDED_ADDRESS_SIZE
+DECL|PAN_ID_OFFSET|macro|PAN_ID_OFFSET
+DECL|PAN_ID_SIZE|macro|PAN_ID_SIZE
+DECL|SHORT_ADDRESS_SIZE|macro|SHORT_ADDRESS_SIZE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|dev_ext_addr|variable|dev_ext_addr
+DECL|dev_pan_id|variable|dev_pan_id
+DECL|dev_short_addr|variable|dev_short_addr
 DECL|get_mac|function|static inline u8_t *get_mac(struct device *dev)
+DECL|received_dest_addr_matched|function|static bool received_dest_addr_matched(u8_t *rx_buffer)
 DECL|upipe_cca|function|static int upipe_cca(struct device *dev)
 DECL|upipe_context_data|variable|upipe_context_data
 DECL|upipe_dev|variable|upipe_dev
@@ -10,6 +24,10 @@ DECL|upipe_init|function|static int upipe_init(struct device *dev)
 DECL|upipe_radio_api|variable|upipe_radio_api
 DECL|upipe_rx|function|static u8_t *upipe_rx(u8_t *buf, size_t *off)
 DECL|upipe_set_channel|function|static int upipe_set_channel(struct device *dev, u16_t channel)
+DECL|upipe_set_filter|function|static int upipe_set_filter(struct device *dev, enum ieee802154_filter_type type, const struct ieee802154_filter *filter)
+DECL|upipe_set_ieee_addr|function|static int upipe_set_ieee_addr(struct device *dev, const u8_t *ieee_addr)
+DECL|upipe_set_pan_id|function|static int upipe_set_pan_id(struct device *dev, u16_t pan_id)
+DECL|upipe_set_short_addr|function|static int upipe_set_short_addr(struct device *dev, u16_t short_addr)
 DECL|upipe_set_txpower|function|static int upipe_set_txpower(struct device *dev, s16_t dbm)
 DECL|upipe_start|function|static int upipe_start(struct device *dev)
 DECL|upipe_stop|function|static int upipe_stop(struct device *dev)
