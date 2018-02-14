adf6066a1192e7edce25bc70239f0e2cdc62b13e
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-02-14 16:19:58 +0200

    usb: sample: Add support for composite configuration for HID
    The current problem with HID is that we need to "patch" static
    descriptor table with the size of report descriptor defined in the
    user application.
    
    The proposed solution is to have composite_pre_init() defined with
    priority CONFIG_KERNEL_INIT_PRIORITY_DEVICE which should run before
    composite_init() defined with priority
    CONFIG_APPLICATION_INIT_PRIORITY.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 0a39c158394e1bd3fa8ac0fa2ed7af2626e4dbdd

--- samples/subsys/usb/hid/src/main.c
+++ samples/subsys/usb/hid/src/main.c
@@ -2,6 +2,7 @@ DECL|REPORT_ID_1|macro|REPORT_ID_1
 DECL|REPORT_ID_2|macro|REPORT_ID_2
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|composite_pre_init|function|static int composite_pre_init(struct device *dev)
 DECL|debug_cb|function|int debug_cb(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
 DECL|get_report_cb|function|int get_report_cb(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
 DECL|hid_report_desc|variable|hid_report_desc
