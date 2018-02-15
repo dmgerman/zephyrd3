dffea87b5bc1beaaeac0672767e651c9c1f8ba22
Christopher Collins <ccollins@apache.org>
2018-02-14 17:20:03 -0800

    samples: subsys: mgmt: smp_svr: add sample app
    This commit adds the sample `smp_svr` app from the mcumgr repo.
    
    Signed-off-by: Christopher Collins <ccollins@apache.org>
    
    Former-commit-id: 3e30e8164fecb9358622c42661af21ab050f17ae

---
+++ samples/subsys/mgmt/mcumgr/smp_svr/src/main.c
@@ -0,0 +1,11 @@
+DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|STATS_NAME_END|variable|STATS_NAME_END
+DECL|advertise|function|static void advertise(void)
+DECL|ad|variable|ad
+DECL|bt_ready|function|static void bt_ready(int err)
+DECL|conn_callbacks|variable|conn_callbacks
+DECL|connected|function|static void connected(struct bt_conn *conn, u8_t err)
+DECL|disconnected|function|static void disconnected(struct bt_conn *conn, u8_t reason)
+DECL|main|function|void main(void)
+DECL|sd|variable|sd
