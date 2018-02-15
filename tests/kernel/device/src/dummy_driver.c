6b72320e09846a35b6d5f961ddc100cd6300f8f7
Anas Nashif <anas.nashif@intel.com>
2018-02-14 22:36:01 -0600

    tests: add test for device class/API
    Test APIs of device model and verify execution with power management
    enabled.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 91b681a7242b4888b9dd73fb408bc14fae066791

---
+++ tests/kernel/device/src/dummy_driver.c
@@ -0,0 +1,7 @@
+DECL|DUMMY_DRIVER_NAME|macro|DUMMY_DRIVER_NAME
+DECL|configure|member|dummy_api_configure_t configure;
+DECL|dummy_api_configure_t|typedef|typedef int (*dummy_api_configure_t)(struct device *dev,
+DECL|dummy_configure|function|static int dummy_configure(struct device *dev, u32_t config)
+DECL|dummy_driver_api|struct|struct dummy_driver_api {
+DECL|dummy_init|function|int dummy_init(struct device *dev)
+DECL|funcs|variable|funcs
---
+++ tests/kernel/device/src/main.c
@@ -0,0 +1,6 @@
+DECL|DUMMY_PORT_1|macro|DUMMY_PORT_1
+DECL|DUMMY_PORT_2|macro|DUMMY_PORT_2
+DECL|build_suspend_device_list|function|static void build_suspend_device_list(void)
+DECL|test_dummy_device_pm|function|void test_dummy_device_pm(void)
+DECL|test_dummy_device|function|void test_dummy_device(void)
+DECL|test_main|function|void test_main(void)
