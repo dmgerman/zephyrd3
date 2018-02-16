bd17a926dfa193a98c4ce611a40abdbc2cb2ded7
Anas Nashif <anas.nashif@intel.com>
2018-02-16 17:16:48 -0600

    tests: work_queue: use ztest properly
    Convert test to ztest in a clean way and other cleanup.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 516ded7dff73b54fa6ace72ab3995d4bf6ec4ec8

--- tests/kernel/workq/work_queue/src/main.c
+++ tests/kernel/workq/work_queue/src/main.c
@@ -2,11 +2,11 @@ DECL|NUM_TEST_ITEMS|macro|NUM_TEST_ITEMS
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|SUBMIT_WAIT|macro|SUBMIT_WAIT
 DECL|WORK_ITEM_WAIT|macro|WORK_ITEM_WAIT
-DECL|check_results|function|static int check_results(int num_tests)
+DECL|check_results|function|static void check_results(int num_tests)
 DECL|co_op_data|variable|co_op_data
 DECL|coop_delayed_work_cancel_main|function|static void coop_delayed_work_cancel_main(int arg1, int arg2)
 DECL|coop_delayed_work_main|function|static void coop_delayed_work_main(int arg1, int arg2)
-DECL|coop_delayed_work_resubmit|function|static void coop_delayed_work_resubmit(int arg1, int arg2)
+DECL|coop_delayed_work_resubmit|function|static void coop_delayed_work_resubmit(void)
 DECL|coop_work_main|function|static void coop_work_main(int arg1, int arg2)
 DECL|delayed_resubmit_work_handler|function|static void delayed_resubmit_work_handler(struct k_work *work)
 DECL|delayed_work_handler|function|static void delayed_work_handler(struct k_work *work)
@@ -15,19 +15,18 @@ DECL|num_results|variable|num_results
 DECL|reset_results|function|static void reset_results(void)
 DECL|resubmit_work_handler|function|static void resubmit_work_handler(struct k_work *work)
 DECL|results|variable|results
-DECL|test_delayed_cancel|function|static int test_delayed_cancel(void)
+DECL|test_delayed_cancel|function|static void test_delayed_cancel(void)
 DECL|test_delayed_init|function|static void test_delayed_init(void)
-DECL|test_delayed_resubmit_thread|function|static int test_delayed_resubmit_thread(void)
-DECL|test_delayed_resubmit|function|static int test_delayed_resubmit(void)
-DECL|test_delayed_submit|function|static int test_delayed_submit(void)
-DECL|test_delayed|function|static int test_delayed(void)
+DECL|test_delayed_resubmit_thread|function|static void test_delayed_resubmit_thread(void)
+DECL|test_delayed_resubmit|function|static void test_delayed_resubmit(void)
+DECL|test_delayed_submit|function|static void test_delayed_submit(void)
+DECL|test_delayed|function|static void test_delayed(void)
 DECL|test_items_init|function|static void test_items_init(void)
 DECL|test_items_submit|function|static void test_items_submit(void)
 DECL|test_item|struct|struct test_item {
 DECL|test_main|function|void test_main(void)
-DECL|test_resubmit|function|static int test_resubmit(void)
-DECL|test_sequence|function|static int test_sequence(void)
-DECL|testing_workq|function|void testing_workq(void)
+DECL|test_resubmit|function|static void test_resubmit(void)
+DECL|test_sequence|function|static void test_sequence(void)
 DECL|tests|variable|tests
 DECL|work_handler|function|static void work_handler(struct k_work *work)
 DECL|work|member|struct k_delayed_work work;
