e553b2fabc5045b7ca1ac9a16eb4f3bdca918f8c
Youvedeep Singh <youvedeep.singh@intel.com>
2018-02-16 12:58:07 +0530

    tests: kernel: posix: pthread_cancel: POSIX thread cancel test.
    Added test for POSIX thread cancel APIs.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: f0b678b03b9de10caad7b9eb9b5449d2054032ca

---
+++ tests/kernel/posix/pthread_cancel/src/pthread_cancel.c
@@ -0,0 +1,7 @@
+DECL|N_THR|macro|N_THR
+DECL|ONE_SECOND|macro|ONE_SECOND
+DECL|STACKSZ|macro|STACKSZ
+DECL|THREAD_PRIORITY|macro|THREAD_PRIORITY
+DECL|exit_count|variable|exit_count
+DECL|main|function|void main(void)
+DECL|thread_top|function|void *thread_top(void *p1)
