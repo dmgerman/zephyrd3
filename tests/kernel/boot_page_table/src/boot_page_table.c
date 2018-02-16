dd8795b756183cfc7c06d84bcdf70ad80075421c
Anas Nashif <anas.nashif@intel.com>
2018-02-16 15:11:06 -0600

    tests: boot_page_table cleanup
    Use ztest macro for asserts instead of plain conditionals. Cleanup style
    a bit.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 7e5853888cbf12337cf7c51104afa850829af626

--- tests/kernel/boot_page_table/src/boot_page_table.c
+++ tests/kernel/boot_page_table/src/boot_page_table.c
@@ -1,8 +1,7 @@
 DECL|MEMORY_REG_NUM|macro|MEMORY_REG_NUM
-DECL|before_start_addr_range|function|static int before_start_addr_range(u32_t start_addr_range)
-DECL|boot_page_table|function|int boot_page_table(void)
+DECL|before_start_addr_range|function|static void before_start_addr_range(u32_t start_addr_range)
 DECL|check_param_nonset_region|function|static int check_param_nonset_region(union x86_mmu_pae_pte *value, uint32_t perm)
 DECL|check_param|function|static int check_param(union x86_mmu_pae_pte *value, uint32_t perm)
-DECL|ending_start_addr_range|function|static int ending_start_addr_range(u32_t start_addr_range)
-DECL|starting_addr_range|function|static int starting_addr_range(u32_t start_addr_range)
+DECL|ending_start_addr_range|function|static void ending_start_addr_range(u32_t start_addr_range)
+DECL|starting_addr_range|function|static void starting_addr_range(u32_t start_addr_range)
 DECL|test_boot_page_table|function|void test_boot_page_table(void)
