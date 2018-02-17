1e9b0d79fb23c174fe4c94cffc6739ca77caf3da
Anas Nashif <anas.nashif@intel.com>
2018-02-17 14:11:18 -0600

    tests: static_idt: cleanup test
    Use some more ztest magic and cleanup test.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 0ebaaf400b869d561928f42987680e9f8d3f21d3

--- tests/kernel/static_idt/src/static_idt.c
+++ tests/kernel/static_idt/src/static_idt.c
@@ -5,10 +5,10 @@ DECL|TEST_SPUR_INT|macro|TEST_SPUR_INT
 DECL|exc_divide_error_handler|function|void exc_divide_error_handler(NANO_ESF *p_esf)
 DECL|exc_handler_executed|variable|exc_handler_executed
 DECL|idt_spur_task|function|void idt_spur_task(void *arg1, void *arg2, void *arg3)
-DECL|idt_stub_test|function|int idt_stub_test(void)
 DECL|int_handler_executed|variable|int_handler_executed
 DECL|isr_handler|function|void isr_handler(void)
 DECL|my_thread|variable|my_thread
 DECL|spur_handler_aborted_thread|variable|spur_handler_aborted_thread
+DECL|test_idt_stub|function|void test_idt_stub(void)
 DECL|test_main|function|void test_main(void)
-DECL|testing_static_idt|function|void testing_static_idt(void)
+DECL|test_static_idt|function|void test_static_idt(void)
