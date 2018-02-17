2273a5486b10379641c7cc6fd8cf5e03f2233301
Anas Nashif <anas.nashif@intel.com>
2018-02-17 10:02:31 -0600

    tests: move bitfields test to common/
    Test is very minimal and can be combined with an existing common
    testsuite.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 84d14e823b04af3f6009a50e8c206ce8e6b8bfa8

--- tests/kernel/bitfield/src/bitfield.c
+++
@@ -1,5 +0,0 @@
-DECL|BITFIELD_SIZE|macro|BITFIELD_SIZE
-DECL|BIT_INDEX|macro|BIT_INDEX
-DECL|BIT_VAL|macro|BIT_VAL
-DECL|test_main|function|void test_main(void)
-DECL|testing_bitfield|function|void testing_bitfield(void)
---
+++ tests/kernel/common/src/bitfield.c
@@ -0,0 +1,4 @@
+DECL|BITFIELD_SIZE|macro|BITFIELD_SIZE
+DECL|BIT_INDEX|macro|BIT_INDEX
+DECL|BIT_VAL|macro|BIT_VAL
+DECL|test_bitfield|function|void test_bitfield(void)
