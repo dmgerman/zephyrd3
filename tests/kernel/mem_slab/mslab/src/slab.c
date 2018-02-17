a4adf1966b0b1e7756acb43d488295799fd74d0d
Anas Nashif <anas.nashif@intel.com>
2018-02-17 17:50:36 -0600

    tests: mslab: cleanup output and use ztest
    Get the reporting right and consistent with other tests. Use ztest
    possible where possible and remove too many lines and confusing output.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 10c0bea562f450b634a0d23070653c7ab4997786

--- tests/kernel/mem_slab/mslab/src/slab.c
+++ tests/kernel/mem_slab/mslab/src/slab.c
@@ -1,10 +1,7 @@
 DECL|NUMBLOCKS|macro|NUMBLOCKS
 DECL|STACKSIZE|macro|STACKSIZE
 DECL|helper_thread|function|void helper_thread(void)
-DECL|print_pointers|function|void print_pointers(void **pointer)
-DECL|tc_rc|variable|tc_rc
 DECL|test_main|function|void test_main(void)
-DECL|test_slab_free_all_blocks|function|int test_slab_free_all_blocks(void **p)
-DECL|test_slab_get_all_blocks|function|int test_slab_get_all_blocks(void **p)
-DECL|testing_mslab|function|void testing_mslab(void)
-DECL|verify_ret_value|function|bool verify_ret_value(int expect_ret_value, int current_ret_current)
+DECL|test_mslab|function|void test_mslab(void)
+DECL|test_slab_free_all_blocks|function|void test_slab_free_all_blocks(void **p)
+DECL|test_slab_get_all_blocks|function|void test_slab_get_all_blocks(void **p)
