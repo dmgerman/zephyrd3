9003fff566909ad01671d2099c67a97017d9cf8f
Anas Nashif <anas.nashif@intel.com>
2018-02-17 09:51:41 -0600

    tests: libc: cleanup naming and expand string tests
    move to a more cosnistent test naming using test_ to improve reporting
    and parsing. Expand string tests and run them as separate ztest tests.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: c797a9ef7b5b064972c4a5bde2bed6ddd9abaa5a

--- tests/kernel/libs/src/main.c
+++ tests/kernel/libs/src/main.c
@@ -1,1 +1,21 @@
+DECL|BUFSIZE|macro|BUFSIZE
+DECL|buffer|variable|buffer
+DECL|long_max|variable|long_max
+DECL|long_one|variable|long_one
+DECL|long_variable|variable|long_variable
+DECL|size_of_long_variable|variable|size_of_long_variable
+DECL|test_limits|function|void test_limits(void)
 DECL|test_main|function|void test_main(void)
+DECL|test_memcmp|function|void test_memcmp(void)
+DECL|test_memset|function|void test_memset(void)
+DECL|test_stdbool|function|void test_stdbool(void)
+DECL|test_stddef|function|void test_stddef(void)
+DECL|test_stdint|function|void test_stdint(void)
+DECL|test_strchr|function|void test_strchr(void)
+DECL|test_strcmp|function|void test_strcmp(void)
+DECL|test_strcpy|function|void test_strcpy(void)
+DECL|test_strlen|function|void test_strlen(void)
+DECL|test_strncmp|function|void test_strncmp(void)
+DECL|test_strncpy|function|void test_strncpy(void)
+DECL|unsigned_byte|variable|unsigned_byte
+DECL|unsigned_int|variable|unsigned_int
