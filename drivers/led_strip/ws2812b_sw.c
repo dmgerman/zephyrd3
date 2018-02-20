f2c15b6f7d4a93567a16123b0d23207492f9f64b
Johan Hedberg <johan.hedberg@intel.com>
2018-02-20 15:10:54 +0200

    drivers: led_strip: Add driver for software-based WS2812B operation
    This driver uses a bit-banging based technique of generating a signal
    for the WS2812B LED strip. Since bit-banging is very timing sensitive,
    where each CPU cycle counts, the driver uses inline assembly to
    perform the most critical operataions. This initial version of the
    driver only supports a Cortex-M0 implementation, and can e.g. be used
    with the ZIP Halo LED strip for the BBC microbit:
    
    https://www.kitronik.co.uk/5625-zip-halo-for-the-bbc-microbit.html
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 9639c00fbb06f77faeceb09c97e3db4ca8a7d249

---
+++ drivers/led_strip/ws2812b_sw.c
@@ -0,0 +1,7 @@
+DECL|BLOCKING|macro|BLOCKING
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|send_buf|function|static int send_buf(u8_t *buf, size_t len)
+DECL|ws2812b_sw_api|variable|ws2812b_sw_api
+DECL|ws2812b_sw_init|function|static int ws2812b_sw_init(struct device *dev)
+DECL|ws2812b_sw_update_channels|function|static int ws2812b_sw_update_channels(struct device *dev, u8_t *channels, size_t num_channels)
+DECL|ws2812b_sw_update_rgb|function|static int ws2812b_sw_update_rgb(struct device *dev, struct led_rgb *pixels, size_t num_pixels)
