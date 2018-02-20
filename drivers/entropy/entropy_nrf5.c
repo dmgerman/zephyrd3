bfae84b3d04e3c725ce5903c0fda1516f9b288d2
Carles Cufi <carles.cufi@nordicsemi.no>
2018-02-20 21:24:07 +0100

    drivers: entropy: Port the LL rand driver to entropy
    The rand driver present in the BLE Link Layer is able to provide
    entropy for both threads and ISRs, and so it is more suited to be used
    as a generic nRF5x entropy driver instead of the current one. This will
    allow applications and the Link Layer to use the same driver without
    duplicating it.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>
    
    Former-commit-id: 813b41beec749ad0cf70a7f57844f5d82c178a30

--- drivers/entropy/entropy_nrf5.c
+++ drivers/entropy/entropy_nrf5.c
@@ -1,8 +1,22 @@
 DECL|DEV_DATA|macro|DEV_DATA
+DECL|RAND_DEFINE|macro|RAND_DEFINE
+DECL|RAND_ISR_LEN|macro|RAND_ISR_LEN
+DECL|RAND_THREAD_LEN|macro|RAND_THREAD_LEN
+DECL|count|member|u8_t count;
+DECL|entropy_get_entropy_isr|function|u8_t entropy_get_entropy_isr(struct device *dev, u8_t *buf, u8_t len)
 DECL|entropy_nrf5_api_funcs|variable|entropy_nrf5_api_funcs
 DECL|entropy_nrf5_data|variable|entropy_nrf5_data
 DECL|entropy_nrf5_dev_data|struct|struct entropy_nrf5_dev_data {
 DECL|entropy_nrf5_get_entropy|function|static int entropy_nrf5_get_entropy(struct device *device, u8_t *buf, u16_t len)
-DECL|entropy_nrf5_get_u8|function|static inline u8_t entropy_nrf5_get_u8(void)
 DECL|entropy_nrf5_init|function|static int entropy_nrf5_init(struct device *device)
-DECL|user_count|member|atomic_t user_count;
+DECL|first|member|u8_t first;
+DECL|get|function|static u8_t get(struct rand *rng, u8_t octets, u8_t *rand)
+DECL|init|function|static void init(struct rand *rng, u8_t len, u8_t threshold)
+DECL|isr_rand|function|static void isr_rand(void *arg)
+DECL|isr|function|static int isr(struct rand *rng, bool store)
+DECL|last|member|u8_t last;
+DECL|rand|member|u8_t rand[0];
+DECL|rand|struct|struct rand {
+DECL|sem_lock|member|struct k_sem sem_lock;
+DECL|sem_sync|member|struct k_sem sem_sync;
+DECL|threshold|member|u8_t threshold;
