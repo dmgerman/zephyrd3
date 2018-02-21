f9c6b80809f2db9b6148f151249f8a16d52058f6
Spoorthi K <spoorthi.k@intel.com>
2018-02-21 11:50:58 +0530

    tests: kernel: Test for essential thread set/clear
    The test verifies the API functionality of _thread_essential_clear(),
        _thread_essential_set() and _is_thread_essential()
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: bdfa021772bc53c4bf8db423c27c4aa8a2ea5898

---
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/test_essential_thread.c
@@ -0,0 +1,4 @@
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|kthread_thread|variable|kthread_thread
+DECL|test_essential_thread_operation|function|void test_essential_thread_operation(void)
+DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
