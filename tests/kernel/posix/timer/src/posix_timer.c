9d781a01be1ff1cd837ddc5c3b66ef058f73a231
Youvedeep Singh <youvedeep.singh@intel.com>
2018-02-21 21:06:28 +0530

    tests: kernel: posix: timer: POSIX timer test.
    Added test for POSIX timer APIs.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 49fbfbb01548db0f5d8fb1d5e917c3216597fdb0

---
+++ tests/kernel/posix/timer/src/posix_timer.c
@@ -0,0 +1,8 @@
+DECL|DURATION_NSECS|macro|DURATION_NSECS
+DECL|DURATION_SECS|macro|DURATION_SECS
+DECL|PERIOD_NSECS|macro|PERIOD_NSECS
+DECL|PERIOD_SECS|macro|PERIOD_SECS
+DECL|SECS_TO_SLEEP|macro|SECS_TO_SLEEP
+DECL|exp_count|variable|exp_count
+DECL|handler|function|void handler(union sigval val)
+DECL|main|function|void main(void)
