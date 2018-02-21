338dfc2ad04a7ce2c31aaa44bdaf55a5a264fd36
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-02-21 16:18:35 +0200

    drivers: eth: Add ethernet driver for native posix arch
    This creates zeth network interface in your host and allows user
    to send and receive data sent to this network interface.
    
    Fixes #6007
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 0f66426f4a83fc2e205a72c1715915918c199091

---
+++ drivers/ethernet/eth_native_posix.c
@@ -0,0 +1,27 @@
+DECL|NET_BUF_TIMEOUT|macro|NET_BUF_TIMEOUT
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
+DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|_ETH_MTU|macro|_ETH_MTU
+DECL|create_rx_handler|function|static void create_rx_handler(struct eth_context *ctx)
+DECL|dev_fd|member|int dev_fd;
+DECL|eth_context_data|variable|eth_context_data
+DECL|eth_context|struct|struct eth_context {
+DECL|eth_get_mac|function|static struct net_linkaddr *eth_get_mac(struct eth_context *ctx)
+DECL|eth_if_api|variable|eth_if_api
+DECL|eth_iface_init|function|static void eth_iface_init(struct net_if *iface)
+DECL|eth_init|function|static int eth_init(struct device *dev)
+DECL|eth_rx|function|static void eth_rx(struct eth_context *ctx)
+DECL|eth_send|function|static int eth_send(struct net_if *iface, struct net_pkt *pkt)
+DECL|get_context|function|static struct eth_context *get_context(struct net_if *iface)
+DECL|if_name|member|const char *if_name;
+DECL|iface|member|struct net_if *iface;
+DECL|init_done|member|bool init_done;
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|mac_addr|member|u8_t mac_addr[6];
+DECL|read_data|function|static int read_data(struct eth_context *ctx, int fd)
+DECL|recv|member|u8_t recv[_ETH_MTU + sizeof(struct net_eth_hdr)];
+DECL|rx_thread_data|variable|rx_thread_data
+DECL|send|member|u8_t send[_ETH_MTU + sizeof(struct net_eth_hdr)];
+DECL|status|member|bool status;
---
+++ drivers/ethernet/eth_native_posix_adapt.c
@@ -0,0 +1,10 @@
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|_DEFAULT_SOURCE|macro|_DEFAULT_SOURCE
+DECL|eth_iface_create|function|int eth_iface_create(const char *if_name, bool tun_only)
+DECL|eth_iface_remove|function|int eth_iface_remove(int fd)
+DECL|eth_read_data|function|ssize_t eth_read_data(int fd, void *buf, size_t buf_len)
+DECL|eth_setup_host|function|int eth_setup_host(const char *if_name)
+DECL|eth_wait_data|function|int eth_wait_data(int fd)
+DECL|eth_write_data|function|ssize_t eth_write_data(int fd, void *buf, size_t buf_len)
+DECL|ssystem|function|static int ssystem(const char *fmt, ...)
