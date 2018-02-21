1742b26f6fd4db15c7398ca0e0926c402a2f1ffd
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-02-21 19:28:55 +0200

    samples: net: echo_async: Better error and write handling
    Initially this sample was intended to be very simple and schematic,
    but as this sample going to become a base for stream API conversion,
    time to handle all the edge conditions.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: a3a5c48d93bdc0ba351d0e88ad192474c68cedf9

--- samples/net/sockets/echo_async/src/socket_echo.c
+++ samples/net/sockets/echo_async/src/socket_echo.c
@@ -1,6 +1,7 @@
 DECL|NUM_FDS|macro|NUM_FDS
 DECL|NUM_FDS|macro|NUM_FDS
 DECL|PORT|macro|PORT
+DECL|block|function|static void block(int fd)
 DECL|main|function|int main(void)
 DECL|nonblock|function|static void nonblock(int fd)
 DECL|pollfds_add|function|int pollfds_add(int fd)
