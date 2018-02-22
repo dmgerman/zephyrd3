2c2321728a096c5b1e3ad7f7cc39390d92751a4d
Yannis Damigos <giannis.damigos@gmail.com>
2018-02-22 10:18:32 +0200

    dts/arm/st: Add USB support for stm32f070/72
    Add USB support for stm32f072 and stm32f070 SoCs
    
    Signed-off-by: Yannis Damigos <giannis.damigos@gmail.com>
    
    Former-commit-id: 47fe4ee78b0a0becb4963e773f17070f3cdc1dba

--- arch/arm/soc/st_stm32/stm32f0/soc.c
+++ arch/arm/soc/st_stm32/stm32f0/soc.c
@@ -1,3 +1,4 @@
+DECL|HAL_Delay|function|void HAL_Delay(__IO uint32_t Delay)
 DECL|HAL_GetTick|function|uint32_t HAL_GetTick(void)
 DECL|relocate_vector_table|function|void relocate_vector_table(void)
 DECL|stm32f0_init|function|static int stm32f0_init(struct device *arg)
