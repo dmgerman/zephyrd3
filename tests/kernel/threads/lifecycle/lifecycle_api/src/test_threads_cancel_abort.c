421360f185a3f4a5090a8c55d3fd68e7cb4b235f
Spoorthi K <spoorthi.k@intel.com>
2018-02-22 13:05:59 +0530

    tests: kernel: Verify the call to abort handler
    The test sets a abort handler and checks if it is called
    when the thread is aborted.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: b1ba49be7c475a902fed895082253a40668d0f4d

--- tests/kernel/threads/lifecycle/lifecycle_api/src/test_threads_cancel_abort.c
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/test_threads_cancel_abort.c
@@ -1,5 +1,10 @@
+DECL|BLOCK_SIZE|macro|BLOCK_SIZE
 DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|abort_called|variable|abort_called
+DECL|abort_function|function|static void abort_function(void)
+DECL|block|variable|block
 DECL|execute_flag|variable|execute_flag
+DECL|test_abort_handler|function|void test_abort_handler(void)
 DECL|test_threads_abort_others|function|void test_threads_abort_others(void)
 DECL|test_threads_abort_repeat|function|void test_threads_abort_repeat(void)
 DECL|test_threads_abort_self|function|void test_threads_abort_self(void)
@@ -9,3 +14,4 @@ DECL|test_threads_cancel_undelayed|function|void test_threads_cancel_undelayed(v
 DECL|thread_entry_abort|function|static void thread_entry_abort(void *p1, void *p2, void *p3)
 DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
 DECL|tstack|variable|tstack
+DECL|uthread_entry|function|static void uthread_entry(void)
