f2177ddb9c935f64766905e9a84b35443c29f38a
Niranjhana N <niranjhana.n@intel.com>
2018-02-23 15:19:01 +0530

    tests: kernel: posix: add ztest to pthread_cancel
    Added ztest to pthread_cancel.
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: a8d3286a313bcacc953cecaabaeca1856db5a97c

--- tests/kernel/posix/pthread_cancel/src/pthread_cancel.c
+++ tests/kernel/posix/pthread_cancel/src/pthread_cancel.c
@@ -3,5 +3,6 @@ DECL|ONE_SECOND|macro|ONE_SECOND
 DECL|STACKSZ|macro|STACKSZ
 DECL|THREAD_PRIORITY|macro|THREAD_PRIORITY
 DECL|exit_count|variable|exit_count
-DECL|main|function|void main(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_pthread_cancel|function|void test_pthread_cancel(void)
 DECL|thread_top|function|void *thread_top(void *p1)
