e1b109ca0491affd5525e1370f923d3f0a03384b
Niranjhana N <niranjhana.n@intel.com>
2018-02-23 09:15:43 +0530

    tests: kernel: posix: add pthread tests
    This test verifies pthread_equal returns the
    expected values.
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 423c8df8f74e8b5ae67ce95fe28727f0a51d3db4

---
+++ tests/kernel/posix/pthread_equal/src/pthread_equal.c
@@ -0,0 +1,5 @@
+DECL|STACKSZ|macro|STACKSZ
+DECL|test_main|function|void test_main(void)
+DECL|test_pthread_equal|function|void test_pthread_equal(void)
+DECL|thread_top|function|void *thread_top(void *p1)
+DECL|thread|variable|thread
