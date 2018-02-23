6cb33e7523ad0ace1e66e8f0202ccbcfc049af58
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-02-23 12:25:06 +0530

    tests: mbox: mbox_api: Added new test cases.
    Added new test cases to increase the impact of the test cases.
    The following are the brief description of the testcases:
    1. Incorrect receiver address.
    2. Incorrect transmit address.
    3. Mbox_get timeout occurs when not able to retrive a msg.
    4. Discard a msg by passing an invalid pool id.
    5. Msg id mismatch between the receiver and transmitter.
    6. Copy tx buffer to a new block in the pool.
    7. Copy a big tx buffer to a block in the pool but no block
       can fit it.
    8. Dispose a msg as soon as it is read.
    9. Free tx pool when we read the msg.
    10. A Asynchronous put unblocks a waiting get.
    11. A Asynchronous put sends a wrong address to a waiting get.
    12. Multiple Asynchronous put populates the msg_q and mbox_get
        retrives it.
    13. Multiple waiting mbox_get and each put unblocks it one at a time.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: e20abd55fa382ab3f511d184023186598061eb03

--- tests/kernel/mbox/mbox_api/src/test_mbox_api.c
+++ tests/kernel/mbox/mbox_api/src/test_mbox_api.c
@@ -1,32 +1,63 @@
+DECL|ASYNC_MULTIPLE_PUT|enumerator|ASYNC_MULTIPLE_PUT,
 DECL|ASYNC_PUT_GET_BLOCK|enumerator|ASYNC_PUT_GET_BLOCK,
 DECL|ASYNC_PUT_GET_BUFFER|enumerator|ASYNC_PUT_GET_BUFFER,
+DECL|ASYNC_PUT_TO_WAITING_GET|enumerator|ASYNC_PUT_TO_WAITING_GET,
+DECL|BLOCK_GET_BUFF_TO_POOL|enumerator|BLOCK_GET_BUFF_TO_POOL,
+DECL|BLOCK_GET_BUFF_TO_SMALLER_POOL|enumerator|BLOCK_GET_BUFF_TO_SMALLER_POOL,
+DECL|BLOCK_GET_INVALID_POOL|enumerator|BLOCK_GET_INVALID_POOL,
+DECL|CLEAN_UP_TX_POOL|enumerator|CLEAN_UP_TX_POOL,
+DECL|DISPOSE_SIZE_0_MSG|enumerator|DISPOSE_SIZE_0_MSG,
+DECL|GET_WAITING_PUT_INCORRECT_TID|enumerator|GET_WAITING_PUT_INCORRECT_TID,
+DECL|INCORRECT_RECEIVER_TID|enumerator|INCORRECT_RECEIVER_TID,
+DECL|INCORRECT_TRANSMIT_TID|enumerator|INCORRECT_TRANSMIT_TID,
 DECL|MAIL_LEN|macro|MAIL_LEN
-DECL|MAX_INFO_TYPE|enumerator|MAX_INFO_TYPE
+DECL|MAX_INFO_TYPE|enumerator|MAX_INFO_TYPE,
+DECL|MSG_TID_MISMATCH|enumerator|MSG_TID_MISMATCH,
+DECL|MULTIPLE_WAITING_GET|enumerator|MULTIPLE_WAITING_GET
 DECL|PUT_GET_BUFFER|enumerator|PUT_GET_BUFFER,
 DECL|PUT_GET_NULL|enumerator|PUT_GET_NULL = 0,
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|TARGET_SOURCE_THREAD_BLOCK|enumerator|TARGET_SOURCE_THREAD_BLOCK,
 DECL|TARGET_SOURCE_THREAD_BUFFER|enumerator|TARGET_SOURCE_THREAD_BUFFER,
+DECL|TIMED_OUT_MBOX_GET|enumerator|TIMED_OUT_MBOX_GET,
 DECL|TIMEOUT|macro|TIMEOUT
+DECL|async_put_sema_give|function|static void async_put_sema_give(void *p1, void *p2, void *p3)
+DECL|async_tid|variable|async_tid
 DECL|data|variable|data
 DECL|end_sema|variable|end_sema
 DECL|info_type|variable|info_type
 DECL|kmbox|variable|kmbox
+DECL|mbox_get_waiting_thread|function|static void mbox_get_waiting_thread(void *thread_number, void *pmbox, void *p3)
 DECL|mbox|variable|mbox
 DECL|mmsg_type|enum|static enum mmsg_type {
+DECL|random_tid|variable|random_tid
 DECL|receiver_tid|variable|receiver_tid
 DECL|sender_tid|variable|sender_tid
 DECL|sync_sema|variable|sync_sema
 DECL|tdata|variable|tdata
+DECL|test_mbox_async_multiple_put|function|void test_mbox_async_multiple_put(void)
 DECL|test_mbox_async_put_get_block|function|void test_mbox_async_put_get_block(void)
 DECL|test_mbox_async_put_get_buffer|function|void test_mbox_async_put_get_buffer(void)
+DECL|test_mbox_async_put_to_waiting_get|function|void test_mbox_async_put_to_waiting_get(void)
+DECL|test_mbox_block_get_buff_to_pool|function|void test_mbox_block_get_buff_to_pool(void)
+DECL|test_mbox_block_get_buff_to_smaller_pool|function|void test_mbox_block_get_buff_to_smaller_pool(void)
+DECL|test_mbox_block_get_invalid_pool|function|void test_mbox_block_get_invalid_pool(void)
+DECL|test_mbox_clean_up_tx_pool|function|void test_mbox_clean_up_tx_pool(void)
+DECL|test_mbox_dispose_size_0_msg|function|void test_mbox_dispose_size_0_msg(void)
+DECL|test_mbox_get_waiting_put_incorrect_tid|function|void test_mbox_get_waiting_put_incorrect_tid(void)
+DECL|test_mbox_incorrect_receiver_tid|function|void test_mbox_incorrect_receiver_tid(void)
+DECL|test_mbox_incorrect_transmit_tid|function|void test_mbox_incorrect_transmit_tid(void)
 DECL|test_mbox_kdefine|function|void test_mbox_kdefine(void)
 DECL|test_mbox_kinit|function|void test_mbox_kinit(void)
+DECL|test_mbox_msg_tid_mismatch|function|void test_mbox_msg_tid_mismatch(void)
+DECL|test_mbox_multiple_waiting_get|function|void test_mbox_multiple_waiting_get(void)
 DECL|test_mbox_put_get_buffer|function|void test_mbox_put_get_buffer(void)
 DECL|test_mbox_put_get_null|function|void test_mbox_put_get_null(void)
 DECL|test_mbox_target_source_thread_block|function|void test_mbox_target_source_thread_block(void)
 DECL|test_mbox_target_source_thread_buffer|function|void test_mbox_target_source_thread_buffer(void)
+DECL|test_mbox_timed_out_mbox_get|function|void test_mbox_timed_out_mbox_get(void)
 DECL|tmbox_entry|function|static void tmbox_entry(void *p1, void *p2, void *p3)
 DECL|tmbox_get|function|static void tmbox_get(struct k_mbox *pmbox)
 DECL|tmbox_put|function|static void tmbox_put(struct k_mbox *pmbox)
 DECL|tmbox|function|static void tmbox(struct k_mbox *pmbox)
+DECL|waiting_get_tid|variable|waiting_get_tid
