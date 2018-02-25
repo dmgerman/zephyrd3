efdf39d65f11c9676d87102e17904931d8111f88
Michael Hope <mlhx@google.com>
2018-02-25 15:39:16 +0100

    uart_pipe: re-work the RX function to match the API and work with USB.
    include/uart.h says that an interrupt may be an edge or a level.
    Re-work the uart_pipe interrupt handler to support both.  This makes
    uart_pipe work with the USB CDC ACM driver.
    
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: 8f7e5bd0a573201a5b019643ea7318e6f3524d0d

--- drivers/console/uart_pipe.c
+++ drivers/console/uart_pipe.c
@@ -3,7 +3,8 @@ DECL|recv_buf_len|variable|recv_buf_len
 DECL|recv_buf|variable|recv_buf
 DECL|recv_off|variable|recv_off
 DECL|uart_pipe_dev|variable|uart_pipe_dev
-DECL|uart_pipe_isr|function|static void uart_pipe_isr(struct device *unused)
+DECL|uart_pipe_isr|function|static void uart_pipe_isr(struct device *dev)
 DECL|uart_pipe_register|function|void uart_pipe_register(u8_t *buf, size_t len, uart_pipe_recv_cb cb)
+DECL|uart_pipe_rx|function|static void uart_pipe_rx(struct device *dev)
 DECL|uart_pipe_send|function|int uart_pipe_send(const u8_t *data, int len)
 DECL|uart_pipe_setup|function|static void uart_pipe_setup(struct device *uart)
