fbdf677f58e70c318a7c7ef1e0dac87919d1dea9
Niranjhana N <niranjhana.n@intel.com>
2018-02-26 15:22:10 +0530

    tests: kernel: posix: add ztest to pthread_join
    Added ztest to pthread_join.
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 92e87e59232cf646f23738190b0ce9f44a91bb7f

--- tests/kernel/posix/pthread_join/src/pthread.c
+++ tests/kernel/posix/pthread_join/src/pthread.c
@@ -4,5 +4,6 @@ DECL|STACKSZ|macro|STACKSZ
 DECL|THREAD_PRIORITY|macro|THREAD_PRIORITY
 DECL|exit_count|variable|exit_count
 DECL|is_sched_prio_valid|function|static bool is_sched_prio_valid(int prio, int policy)
-DECL|main|function|void main(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_pthread_join|function|void test_pthread_join(void)
 DECL|thread_top|function|void *thread_top(void *p1)
