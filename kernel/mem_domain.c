cab507e61006cd9e035c7fda7b5d21dc83038f54
Leandro Pereira <leandro.pereira@intel.com>
2018-02-28 14:11:41 -0800

    kernel: mem_domain: Check for overlapping regions when considering W^X
    Multiple partitions can be added to a domain, and if they overlap, they
    can have different attributes.  The previous check would only check for
    W^X for individual partitions, and this is insufficient.  Overlapping
    partitions could have W^X attributes, but in the end, a memory region
    would be writable and executable.
    
    The way this is performed is quite "heavyweight", as it is implemented
    in a O(n^2) operation.  The number of partitions per domain is small on
    most devices, so this isn't an issue.  CONFIG_EXECUTE_XOR_WRITE is
    still an optional feature.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: db094b8d885196215473485093a7f03c90e421c6

--- kernel/mem_domain.c
+++ kernel/mem_domain.c
@@ -1,9 +1,12 @@
-DECL|ensure_w_xor_x|function|static void ensure_w_xor_x(k_mem_partition_attr_t attrs)
 DECL|init_mem_domain_module|function|static int init_mem_domain_module(struct device *arg)
-DECL|k_mem_domain_add_partition|function|void k_mem_domain_add_partition(struct k_mem_domain *domain, struct k_mem_partition *part)
+DECL|k_mem_domain_add_partition|function|void k_mem_domain_add_partition(struct k_mem_domain *domain,struct k_mem_partition *part)
 DECL|k_mem_domain_add_thread|function|void k_mem_domain_add_thread(struct k_mem_domain *domain, k_tid_t thread)
 DECL|k_mem_domain_destroy|function|void k_mem_domain_destroy(struct k_mem_domain *domain)
 DECL|k_mem_domain_init|function|void k_mem_domain_init(struct k_mem_domain *domain, u32_t num_parts,struct k_mem_partition *parts[])
 DECL|k_mem_domain_remove_partition|function|void k_mem_domain_remove_partition(struct k_mem_domain *domain, struct k_mem_partition *part)
 DECL|k_mem_domain_remove_thread|function|void k_mem_domain_remove_thread(k_tid_t thread)
 DECL|max_partitions|variable|max_partitions
+DECL|sane_partition_domain|function|static inline bool sane_partition_domain(const struct k_mem_domain *domain, const struct k_mem_partition *part)
+DECL|sane_partition_domain|macro|sane_partition_domain
+DECL|sane_partition|function|static bool sane_partition(const struct k_mem_partition *part, const struct k_mem_partition *parts, u32_t num_parts)
+DECL|sane_partition|macro|sane_partition
