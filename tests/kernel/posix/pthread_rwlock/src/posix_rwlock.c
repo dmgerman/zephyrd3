e1d29d960d18ae579521c74e0c9a2a324b42992c
Youvedeep Singh <youvedeep.singh@intel.com>
2018-02-28 17:39:40 +0530

    tests: kernel: posix: pthread_rwlock: POSIX rw lock test.
    Added test for POSIX read-write lock APIs.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: cd3ef98ee1786d57283f8ea286015462ca37389d

---
+++ tests/kernel/posix/pthread_rwlock/src/posix_rwlock.c
@@ -0,0 +1,6 @@
+DECL|N_THR|macro|N_THR
+DECL|STACKSZ|macro|STACKSZ
+DECL|rwlock|variable|rwlock
+DECL|test_main|function|void test_main(void)
+DECL|test_rw_lock|function|static void test_rw_lock(void)
+DECL|thread_top|function|static void *thread_top(void *p1)
