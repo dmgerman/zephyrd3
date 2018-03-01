e67b4741fae16bdabbc349af34c0860a79cfcba5
Maureen Helm <maureen.helm@nxp.com>
2018-03-01 17:39:50 -0600

    ext: mcux: Remove clock_config.c/h
    We have never used the mcux clock_config.c/h files, so remove them. We
    configure clocks in soc.c instead to allow more options with Kconfigs.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 4624f132901a22600607a7ae165a52ccb11d7521

--- ext/hal/nxp/mcux/devices/LPC54114/clock_config.c
+++
@@ -1,3 +0,0 @@
-DECL|BOARD_BootClockFRO12M|function|void BOARD_BootClockFRO12M(void)
-DECL|BOARD_BootClockFROHF48M|function|void BOARD_BootClockFROHF48M(void)
-DECL|BOARD_BootClockFROHF96M|function|void BOARD_BootClockFROHF96M(void)
--- ext/hal/nxp/mcux/devices/MIMXRT1052/clock_config.c
+++
@@ -1,5 +0,0 @@
-DECL|BOARD_BootClockGate|function|static void BOARD_BootClockGate(void)
-DECL|BOARD_BootClockRUN|function|void BOARD_BootClockRUN(void)
-DECL|armPllConfig|variable|armPllConfig
-DECL|sysPllConfig|variable|sysPllConfig
-DECL|usb1PllConfig|variable|usb1PllConfig
