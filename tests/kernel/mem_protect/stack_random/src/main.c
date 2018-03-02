526fc86399a4ba4bfb8a62b17b6fefd5ef2fec87
Andrew Boie <andrew.p.boie@intel.com>
2018-03-02 07:54:13 -0800

    kernel: introduce initial stack randomization
    This is a component of address space layout randomization that we can
    implement even though we have a physical address space.
    
    Support for upward-growing stacks omitted for now, it's not done
    currently on any of our current or planned architectures.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 83752c1cfe074542294005bc8fdd9b08a716a06d

---
+++ tests/kernel/mem_protect/stack_random/src/main.c
@@ -0,0 +1,8 @@
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|THREAD_COUNT|macro|THREAD_COUNT
+DECL|VERBOSE|macro|VERBOSE
+DECL|alt_thread_data|variable|alt_thread_data
+DECL|alternate_thread|function|void alternate_thread(void)
+DECL|changed|variable|changed
+DECL|last_sp|variable|last_sp
+DECL|main|function|void main(void)
