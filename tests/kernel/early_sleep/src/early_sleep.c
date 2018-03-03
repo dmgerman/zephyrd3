65dc72b5f80a989b29437cc5bf455688e43d0144
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-03-03 20:46:01 +0530

    tests: kernel: Add a test to verify early sleep
    Add a test to verify that k_sleep() can be used to put
    the calling thread to sleep for a specified number of
    ticks during system initialization.
    
    This test is inspired from legacy test_early_sleep test
    from v1.7.0 release.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: 2a44e8ea4d12f71153ba49094d6803f2e225c127

---
+++ tests/kernel/early_sleep/src/early_sleep.c
@@ -0,0 +1,14 @@
+DECL|TEST_TICKS_TO_SLEEP|macro|TEST_TICKS_TO_SLEEP
+DECL|THREAD_STACK|macro|THREAD_STACK
+DECL|actual_app_sleep_ticks|variable|actual_app_sleep_ticks
+DECL|actual_post_kernel_sleep_ticks|variable|actual_post_kernel_sleep_ticks
+DECL|actual_sleep_ticks|variable|actual_sleep_ticks
+DECL|helper_tdata|variable|helper_tdata
+DECL|helper_thread|function|static void helper_thread(void *p1, void *p2, void *p3)
+DECL|helper_ttid|variable|helper_ttid
+DECL|test_early_sleep_app|function|static int test_early_sleep_app(struct device *unused)
+DECL|test_early_sleep_post_kernel|function|static int test_early_sleep_post_kernel(struct device *unused)
+DECL|test_failure|variable|test_failure
+DECL|test_main|function|void test_main(void)
+DECL|ticks_to_sleep|function|static int ticks_to_sleep(int ticks)
+DECL|verify_early_sleep|function|static void verify_early_sleep(void)
