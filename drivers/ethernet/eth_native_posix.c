2cc91b71c07e9fd22841601a65ce17627415c243
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-03 13:04:53 +0200

    drivers: eth: native_posix: Add VLAN support
    Support also virtual LAN (VLAN) with native_posix ethernet driver.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 73b43e002408e08cd34d55a941836c7ed2bb7590

--- drivers/ethernet/eth_native_posix.c
+++ drivers/ethernet/eth_native_posix.c
@@ -1,11 +1,12 @@
+DECL|ETH_HDR_LEN|macro|ETH_HDR_LEN
+DECL|ETH_HDR_LEN|macro|ETH_HDR_LEN
 DECL|NET_BUF_TIMEOUT|macro|NET_BUF_TIMEOUT
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
-DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
-DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
 DECL|_ETH_MTU|macro|_ETH_MTU
 DECL|create_rx_handler|function|static void create_rx_handler(struct eth_context *ctx)
 DECL|dev_fd|member|int dev_fd;
+DECL|eth_capabilities|function|static enum eth_hw_caps eth_capabilities(struct device *dev)
 DECL|eth_context_data|variable|eth_context_data
 DECL|eth_context|struct|struct eth_context {
 DECL|eth_get_mac|function|static struct net_linkaddr *eth_get_mac(struct eth_context *ctx)
@@ -15,13 +16,14 @@ DECL|eth_init|function|static int eth_init(struct device *dev)
 DECL|eth_rx|function|static void eth_rx(struct eth_context *ctx)
 DECL|eth_send|function|static int eth_send(struct net_if *iface, struct net_pkt *pkt)
 DECL|get_context|function|static struct eth_context *get_context(struct net_if *iface)
+DECL|get_iface|function|static inline struct net_if *get_iface(struct eth_context *ctx, u16_t vlan_tag)
 DECL|if_name|member|const char *if_name;
 DECL|iface|member|struct net_if *iface;
 DECL|init_done|member|bool init_done;
 DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|mac_addr|member|u8_t mac_addr[6];
 DECL|read_data|function|static int read_data(struct eth_context *ctx, int fd)
-DECL|recv|member|u8_t recv[_ETH_MTU + sizeof(struct net_eth_hdr)];
+DECL|recv|member|u8_t recv[_ETH_MTU + ETH_HDR_LEN];
 DECL|rx_thread_data|variable|rx_thread_data
-DECL|send|member|u8_t send[_ETH_MTU + sizeof(struct net_eth_hdr)];
+DECL|send|member|u8_t send[_ETH_MTU + ETH_HDR_LEN];
 DECL|status|member|bool status;
