58307c0ed1959f76a753c0981bd287fe130c2b91
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-03-05 14:38:50 +0100

    drivers/uart: Use dts to set uart options for ns16550 driver
    Fix the ns16550 uart driver and relevant SoCs accordingly.
    All generic settings are now DTS based.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 61ef30d10eca817ca76cb388a1bc5e98ad4a30e0

--- arch/arc/soc/quark_se_c1000_ss/soc_config.c
+++ arch/arc/soc/quark_se_c1000_ss/soc_config.c
@@ -1,4 +1,3 @@
 DECL|arc_quark_se_ipm_init|function|static int arc_quark_se_ipm_init(void)
 DECL|ipm_controller_config|variable|ipm_controller_config
 DECL|quark_se_ipm_sender_config|variable|quark_se_ipm_sender_config
-DECL|uart_ns16550_init|function|static int uart_ns16550_init(struct device *dev)
