10cc2ea1f6407ec3da627df5e22a44a061d761fe
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-05 17:33:18 +0200

    tests: net: Add unit test for traffic class support
    Test that we can set the priority of the network packet and
    that high priority packets are sent before low priority ones.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 2a3de97ce9eeac624bc7db1ee234dbfb0a5a940e

---
+++ tests/net/traffic_class/src/main.c
@@ -0,0 +1,80 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|MAX_PKT_TO_RECV|macro|MAX_PKT_TO_RECV
+DECL|MAX_PKT_TO_SEND|macro|MAX_PKT_TO_SEND
+DECL|MAX_PRIORITIES|macro|MAX_PRIORITIES
+DECL|MAX_TC|macro|MAX_TC
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|PORT|macro|PORT
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|add_neighbor|function|static bool add_neighbor(struct net_if *iface, struct in6_addr *addr)
+DECL|add_neighbor|macro|add_neighbor
+DECL|address_setup|function|static void address_setup(void)
+DECL|api_funcs|variable|api_funcs
+DECL|check_higher_priority_pkt_recv|function|static bool check_higher_priority_pkt_recv(int tc, struct net_pkt *pkt)
+DECL|check_higher_priority_pkt_sent|function|static bool check_higher_priority_pkt_sent(int tc, struct net_pkt *pkt)
+DECL|ctx|member|struct net_context *ctx;
+DECL|dst_addr6|variable|dst_addr6
+DECL|dst_addr|variable|dst_addr
+DECL|eth_context|struct|struct eth_context {
+DECL|eth_context|variable|eth_context
+DECL|eth_iface_init|function|static void eth_iface_init(struct net_if *iface)
+DECL|eth_init|function|static int eth_init(struct device *dev)
+DECL|eth_tx|function|static int eth_tx(struct net_if *iface, struct net_pkt *pkt)
+DECL|expecting_tag|member|u16_t expecting_tag;
+DECL|generate_mac|function|static void generate_mac(u8_t *mac_addr)
+DECL|iface|member|struct net_if *iface;
+DECL|ll_addr|variable|ll_addr
+DECL|mac_addr|member|u8_t mac_addr[6];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|my_addr3|variable|my_addr3
+DECL|net_ctxs|variable|net_ctxs
+DECL|priority_setup|function|static void priority_setup(void)
+DECL|recv_cb_called|variable|recv_cb_called
+DECL|recv_cb|function|static void recv_cb(struct net_context *context, struct net_pkt *pkt, int status, void *user_data)
+DECL|recv_priorities|variable|recv_priorities
+DECL|rx_tc2prio|variable|rx_tc2prio
+DECL|send_priorities|variable|send_priorities
+DECL|setup_net_context|function|static void setup_net_context(struct net_context **ctx)
+DECL|start_receiving|variable|start_receiving
+DECL|test_data|variable|test_data
+DECL|test_failed|variable|test_failed
+DECL|test_main|function|void test_main(void)
+DECL|test_started|variable|test_started
+DECL|traffic_class_cleanup_rx|function|static void traffic_class_cleanup_rx(void)
+DECL|traffic_class_cleanup_tx|function|static void traffic_class_cleanup_tx(void)
+DECL|traffic_class_cleanup|function|static void traffic_class_cleanup(int count)
+DECL|traffic_class_general_setup|function|static void traffic_class_general_setup(void)
+DECL|traffic_class_recv_data_mix_all_1|function|static void traffic_class_recv_data_mix_all_1(void)
+DECL|traffic_class_recv_data_mix_all_2|function|static void traffic_class_recv_data_mix_all_2(void)
+DECL|traffic_class_recv_data_mix|function|static void traffic_class_recv_data_mix(void)
+DECL|traffic_class_recv_data_prio_be|function|static void traffic_class_recv_data_prio_be(void)
+DECL|traffic_class_recv_data_prio_bk|function|static void traffic_class_recv_data_prio_bk(void)
+DECL|traffic_class_recv_data_prio_ca|function|static void traffic_class_recv_data_prio_ca(void)
+DECL|traffic_class_recv_data_prio_ee|function|static void traffic_class_recv_data_prio_ee(void)
+DECL|traffic_class_recv_data_prio_ic|function|static void traffic_class_recv_data_prio_ic(void)
+DECL|traffic_class_recv_data_prio_nc|function|static void traffic_class_recv_data_prio_nc(void)
+DECL|traffic_class_recv_data_prio_vi|function|static void traffic_class_recv_data_prio_vi(void)
+DECL|traffic_class_recv_data_prio_vo|function|static void traffic_class_recv_data_prio_vo(void)
+DECL|traffic_class_recv_packets_with_prio|function|static void traffic_class_recv_packets_with_prio(enum net_priority prio, int pkt_count)
+DECL|traffic_class_recv_priority|function|static void traffic_class_recv_priority(enum net_priority prio,int num_packets, bool wait_for_packets)
+DECL|traffic_class_send_data_mix_all_1|function|static void traffic_class_send_data_mix_all_1(void)
+DECL|traffic_class_send_data_mix_all_2|function|static void traffic_class_send_data_mix_all_2(void)
+DECL|traffic_class_send_data_mix|function|static void traffic_class_send_data_mix(void)
+DECL|traffic_class_send_data_prio_be|function|static void traffic_class_send_data_prio_be(void)
+DECL|traffic_class_send_data_prio_bk|function|static void traffic_class_send_data_prio_bk(void)
+DECL|traffic_class_send_data_prio_ca|function|static void traffic_class_send_data_prio_ca(void)
+DECL|traffic_class_send_data_prio_ee|function|static void traffic_class_send_data_prio_ee(void)
+DECL|traffic_class_send_data_prio_ic|function|static void traffic_class_send_data_prio_ic(void)
+DECL|traffic_class_send_data_prio_nc|function|static void traffic_class_send_data_prio_nc(void)
+DECL|traffic_class_send_data_prio_vi|function|static void traffic_class_send_data_prio_vi(void)
+DECL|traffic_class_send_data_prio_vo|function|static void traffic_class_send_data_prio_vo(void)
+DECL|traffic_class_send_packets_with_prio|function|static void traffic_class_send_packets_with_prio(enum net_priority prio, int pkt_count)
+DECL|traffic_class_send_priority|function|static void traffic_class_send_priority(enum net_priority prio,int num_packets, bool wait_for_packets)
+DECL|traffic_class_setup_recv|function|static void traffic_class_setup_recv(void)
+DECL|traffic_class_setup_rx|function|static void traffic_class_setup_rx(void)
+DECL|traffic_class_setup_tx|function|static void traffic_class_setup_tx(void)
+DECL|traffic_class_setup|function|static void traffic_class_setup(enum net_priority *tc2prio, int count)
+DECL|tx_tc2prio|variable|tx_tc2prio
+DECL|wait_data|variable|wait_data
