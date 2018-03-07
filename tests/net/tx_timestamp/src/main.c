3b3217370a7887be1983b4c853e623a689b6bc4f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-07 16:23:06 +0200

    tests: net: Add unit test for network pkt timestamping
    Test that Tx timestamp callback code is called properly if network
    packet timestamping is enabled.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: c63363711d50276251c38859f2a02d4fcbefe5c2

---
+++ tests/net/tx_timestamp/src/main.c
@@ -0,0 +1,47 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|PORT|macro|PORT
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|add_neighbor|function|static bool add_neighbor(struct net_if *iface, struct in6_addr *addr)
+DECL|address_setup|function|static void address_setup(void)
+DECL|api_funcs|variable|api_funcs
+DECL|check_timestamp_after_enabling|function|static void check_timestamp_after_enabling(void)
+DECL|check_timestamp_before_enabling|function|static void check_timestamp_before_enabling(void)
+DECL|do_timestamp|variable|do_timestamp
+DECL|dst_addr|variable|dst_addr
+DECL|eth_context2|variable|eth_context2
+DECL|eth_context|struct|struct eth_context {
+DECL|eth_context|variable|eth_context
+DECL|eth_get_capabilities|function|static enum ethernet_hw_caps eth_get_capabilities(struct device *dev)
+DECL|eth_if_count|member|int eth_if_count;
+DECL|eth_iface_init|function|static void eth_iface_init(struct net_if *iface)
+DECL|eth_init|function|static int eth_init(struct device *dev)
+DECL|eth_interfaces|variable|eth_interfaces
+DECL|eth_tx|function|static int eth_tx(struct net_if *iface, struct net_pkt *pkt)
+DECL|generate_mac|function|static void generate_mac(u8_t *mac_addr)
+DECL|iface2str|function|static const char *iface2str(struct net_if *iface)
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
+DECL|iface|member|struct net_if *iface;
+DECL|ll_addr|variable|ll_addr
+DECL|mac_addr|member|u8_t mac_addr[6];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|send_some_data|function|static struct net_pkt *send_some_data(struct net_if *iface, bool ref_pkt)
+DECL|test_data|variable|test_data
+DECL|test_failed|variable|test_failed
+DECL|test_main|function|void test_main(void)
+DECL|test_started|variable|test_started
+DECL|timestamp_callback_2|function|static void timestamp_callback_2(struct net_pkt *pkt)
+DECL|timestamp_callback|function|static void timestamp_callback(struct net_pkt *pkt)
+DECL|timestamp_cb_2|variable|timestamp_cb_2
+DECL|timestamp_cb_3|variable|timestamp_cb_3
+DECL|timestamp_cb_called|variable|timestamp_cb_called
+DECL|timestamp_cb|variable|timestamp_cb
+DECL|timestamp_cleanup|function|static void timestamp_cleanup(void)
+DECL|timestamp_setup_2nd_iface|function|static void timestamp_setup_2nd_iface(void)
+DECL|timestamp_setup_all|function|static void timestamp_setup_all(void)
+DECL|timestamp_setup|function|static void timestamp_setup(void)
+DECL|total_if_count|member|int total_if_count;
+DECL|udp_v6_ctx|variable|udp_v6_ctx
+DECL|user_data|struct|struct user_data {
