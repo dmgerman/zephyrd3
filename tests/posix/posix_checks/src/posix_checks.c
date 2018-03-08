43611afc95e2e10dffef648c13d3d3fe4866fbd3
Niranjhana N <niranjhana.n@intel.com>
2018-03-08 15:45:15 +0530

    tests: posix: add more tests
    Added posix_checks, which checks supported priorities,
    clocks and timer APIs, mainly in the point of negative
    tests to increase code coverage.
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 80e221618d5788e1f850d8eb65b7d6ca7aeb7631

---
+++ tests/posix/posix_checks/src/posix_checks.c
@@ -0,0 +1,19 @@
+DECL|DURATION_NSECS|macro|DURATION_NSECS
+DECL|DURATION_SECS|macro|DURATION_SECS
+DECL|INVAL|enumerator|INVAL = 10,
+DECL|PERIOD_NSECS|macro|PERIOD_NSECS
+DECL|PERIOD_SECS|macro|PERIOD_SECS
+DECL|SCHED_DEADLINE|enumerator|SCHED_DEADLINE = 3,
+DECL|SCHED_IDLE|enumerator|SCHED_IDLE = 2,
+DECL|SCHED_OTHER|enumerator|SCHED_OTHER = 4,
+DECL|check_clock_support|function|void check_clock_support(int clock_id)
+DECL|check_sched_policy|function|void check_sched_policy(int policy)
+DECL|check_timer_support|function|void check_timer_support(int clock_id)
+DECL|exp_count|variable|exp_count
+DECL|handler|function|void handler(union sigval val)
+DECL|sched_policy_type|enum|enum sched_policy_type {
+DECL|spt_map|variable|spt_map
+DECL|test_clock_support|function|void test_clock_support(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_sched_policy|function|void test_sched_policy(void)
+DECL|test_timer_support|function|void test_timer_support(void)
