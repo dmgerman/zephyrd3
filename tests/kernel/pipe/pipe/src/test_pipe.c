b6f8af89c87ba38789e91d7ca9587f292590685b
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-03-08 16:09:50 +0530

    tests: kernel: pipe: Added new test cases for pipe.
    The following are the test cases added for pipe.
    1. Test pipe APIs on single data element.
    2. Test pipe APIs on multiple data elements.
    3. Test pipe APIs with forever wait.
    4. Test pipe APIs with timeout.
    5. Test pipe APIs on an empty pipe.
    6. Test pipe get API with forever timeout.
    7. Test pipe get API with limited timeout.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: 84fed5623621bbe22df237791f2bd0947a89c8a0

---
+++ tests/kernel/pipe/pipe/src/test_pipe.c
@@ -0,0 +1,46 @@
+DECL|ALL_BYTES|macro|ALL_BYTES
+DECL|ATLEAST_1|macro|ATLEAST_1
+DECL|NO_CONSTRAINT|macro|NO_CONSTRAINT
+DECL|PIPE_SIZE|macro|PIPE_SIZE
+DECL|RETURN_SUCCESS|macro|RETURN_SUCCESS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TIMEOUT_200MSEC|macro|TIMEOUT_200MSEC
+DECL|TIMEOUT_VAL|macro|TIMEOUT_VAL
+DECL|TOTAL_ELEMENTS|macro|TOTAL_ELEMENTS
+DECL|TOTAL_TIMEOUT_ELEMENTS|macro|TOTAL_TIMEOUT_ELEMENTS
+DECL|TOTAL_WAIT_ELEMENTS|macro|TOTAL_WAIT_ELEMENTS
+DECL|_SysFatalErrorHandler|function|void _SysFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
+DECL|get_single_tid|variable|get_single_tid
+DECL|min_size|member|u32_t min_size;
+DECL|multiple_elements|variable|multiple_elements
+DECL|pipe_get_forever_timeout|function|void pipe_get_forever_timeout(void *p1, void *p2, void *p3)
+DECL|pipe_get_forever_wait|function|void pipe_get_forever_wait(void *pi, void *p2, void *p3)
+DECL|pipe_get_multiple|function|void pipe_get_multiple(void *p1, void *p2, void *p3)
+DECL|pipe_get_on_empty_pipe|function|void pipe_get_on_empty_pipe(void)
+DECL|pipe_get_single|function|void pipe_get_single(void *p1, void *p2, void *p3)
+DECL|pipe_get_timeout|function|void pipe_get_timeout(void *pi, void *p2, void *p3)
+DECL|pipe_put_forever_timeout|function|void pipe_put_forever_timeout(void)
+DECL|pipe_put_forever_wait|function|void pipe_put_forever_wait(void)
+DECL|pipe_put_get_timeout|function|void pipe_put_get_timeout(void)
+DECL|pipe_put_multiple|function|void pipe_put_multiple(void)
+DECL|pipe_put_single|function|void pipe_put_single(void)
+DECL|pipe_put_timeout|function|void pipe_put_timeout(void)
+DECL|pipe_sequence|struct|struct pipe_sequence {
+DECL|return_value|member|int return_value;
+DECL|rx_buffer_check|function|u32_t rx_buffer_check(char *buffer, u32_t size)
+DECL|rx_buffer|variable|rx_buffer
+DECL|sent_bytes|member|u32_t sent_bytes;
+DECL|single_elements|variable|single_elements
+DECL|size|member|u32_t size;
+DECL|test_pipe_forever_timeout|function|void test_pipe_forever_timeout(void)
+DECL|test_pipe_forever_wait|function|void test_pipe_forever_wait(void)
+DECL|test_pipe_get_invalid_size|function|void test_pipe_get_invalid_size(void)
+DECL|test_pipe_get_on_empty_pipe|function|void test_pipe_get_on_empty_pipe(void)
+DECL|test_pipe_get_timeout|function|void test_pipe_get_timeout(void)
+DECL|test_pipe_on_multiple_elements|function|void test_pipe_on_multiple_elements(void)
+DECL|test_pipe_on_single_elements|function|void test_pipe_on_single_elements(void)
+DECL|test_pipe_timeout|function|void test_pipe_timeout(void)
+DECL|timeout_elements|variable|timeout_elements
+DECL|tx_buffer|variable|tx_buffer
+DECL|valid_fault|variable|valid_fault
+DECL|wait_elements|variable|wait_elements
