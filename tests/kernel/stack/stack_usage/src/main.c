f74ca08a021ac91e06557fdb49527c2db0df670a
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-03-09 02:17:16 +0530

    tests: kernel: Add stack usage scenario tests
    Add stack object usage scenario tests between thread
    to thread and thread to ISR.
    
    This test is inspired from test_stack legacy test from
    Zephyr v1.7.0 release.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: e2a1682c57d56bec961a48a6034868919ac9556c

---
+++ tests/kernel/stack/stack_usage/src/main.c
@@ -0,0 +1,16 @@
+DECL|STACK_LEN|macro|STACK_LEN
+DECL|TSTACK_SIZE|macro|TSTACK_SIZE
+DECL|data1|variable|data1
+DECL|data2|variable|data2
+DECL|data_isr|variable|data_isr
+DECL|end_sema|variable|end_sema
+DECL|tIsr_entry_pop|function|static void tIsr_entry_pop(void *p)
+DECL|tIsr_entry_push|function|static void tIsr_entry_push(void *p)
+DECL|test_dual_stack_play|function|static void test_dual_stack_play(void)
+DECL|test_isr_stack_play|function|static void test_isr_stack_play(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_single_stack_play|function|static void test_single_stack_play(void)
+DECL|thread_data|variable|thread_data
+DECL|thread_entry_fn_dual|function|static void thread_entry_fn_dual(void *p1, void *p2, void *p3)
+DECL|thread_entry_fn_isr|function|static void thread_entry_fn_isr(void *p1, void *p2, void *p3)
+DECL|thread_entry_fn_single|function|static void thread_entry_fn_single(void *p1, void *p2, void *p3)
