6c699321af817bc6372372241ab3d9a636d1ee0f
Punit Vara <punit.vara@intel.com>
2018-03-09 15:26:36 +0530

    tests: msgq_api: Improve scenario testing
    Pend multiple thread to wait for writing on msgq.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: fcf942afce0426a900e55c9ad238de228321072c

--- tests/kernel/msgq/msgq_api/src/test_msgq_contexts.c
+++ tests/kernel/msgq/msgq_api/src/test_msgq_contexts.c
@@ -2,12 +2,15 @@ DECL|data|variable|data
 DECL|end_sema|variable|end_sema
 DECL|get_msgq|function|static void get_msgq(struct k_msgq *pmsgq)
 DECL|msgq_isr|function|static void msgq_isr(struct k_msgq *pmsgq)
+DECL|msgq_thread_overflow|function|static void msgq_thread_overflow(struct k_msgq *pmsgq)
 DECL|msgq_thread|function|static void msgq_thread(struct k_msgq *pmsgq)
 DECL|msgq|variable|msgq
 DECL|purge_msgq|function|static void purge_msgq(struct k_msgq *pmsgq)
 DECL|put_msgq|function|static void put_msgq(struct k_msgq *pmsgq)
 DECL|tdata|variable|tdata
 DECL|test_msgq_isr|function|void test_msgq_isr(void)
+DECL|test_msgq_thread_overflow|function|void test_msgq_thread_overflow(void)
 DECL|test_msgq_thread|function|void test_msgq_thread(void)
+DECL|thread_entry_overflow|function|static void thread_entry_overflow(void *p1, void *p2, void *p3)
 DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
 DECL|tisr_entry|function|static void tisr_entry(void *p)
