e0f0075ea5201477e4dcf029d59e3f128a59f2d8
Savinay Dharmappa <savinay.dharmappa@intel.com>
2018-03-09 06:06:10 +0530

    tests: kernel: work_q: Add testcases
    Add a testcase where single work is submitted to
    multiple queue. In this case handler invoked only
    once as single work cannot be submitted to multiple
    queue.
    
    Add a test case which submit a work to a queue twice. This testcase
    is added to test neagtive case when  k_delayed_work_cancel() fails
    in k_delayed_work_submit_to_queue API
    
    Signed-off-by: Savinay Dharmappa <savinay.dharmappa@intel.com>
    
    Former-commit-id: 02347f9c6f52f6868dc026661f8e73b039105ec4

--- tests/kernel/workq/work_queue_api/src/test_workq_api.c
+++ tests/kernel/workq/work_queue_api/src/test_workq_api.c
@@ -3,6 +3,8 @@ DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|TIMEOUT|macro|TIMEOUT
 DECL|delayed_work_sleepy|variable|delayed_work_sleepy
 DECL|delayed_work|variable|delayed_work
+DECL|new_work_handler|function|static void new_work_handler(struct k_work *w)
+DECL|new_work|variable|new_work
 DECL|sync_sema|variable|sync_sema
 DECL|tdelayed_work_cancel|function|static void tdelayed_work_cancel(void *data)
 DECL|tdelayed_work_submit|function|static void tdelayed_work_submit(void *data)
@@ -14,11 +16,15 @@ DECL|test_delayed_work_submit_isr|function|void test_delayed_work_submit_isr(voi
 DECL|test_delayed_work_submit_thread|function|void test_delayed_work_submit_thread(void)
 DECL|test_delayed_work_submit_to_queue_isr|function|void test_delayed_work_submit_to_queue_isr(void)
 DECL|test_delayed_work_submit_to_queue_thread|function|void test_delayed_work_submit_to_queue_thread(void)
+DECL|test_work_resubmit_to_queue|function|void test_work_resubmit_to_queue(void)
 DECL|test_work_submit_isr|function|void test_work_submit_isr(void)
 DECL|test_work_submit_thread|function|void test_work_submit_thread(void)
+DECL|test_work_submit_to_multipleq|function|void test_work_submit_to_multipleq(void)
 DECL|test_work_submit_to_queue_isr|function|void test_work_submit_to_queue_isr(void)
 DECL|test_work_submit_to_queue_thread|function|void test_work_submit_to_queue_thread(void)
 DECL|test_workq_start_before_submit|function|void test_workq_start_before_submit(void)
+DECL|twork_resubmit|function|static void twork_resubmit(void *data)
+DECL|twork_submit_multipleq|function|static void twork_submit_multipleq(void *data)
 DECL|twork_submit|function|static void twork_submit(void *data)
 DECL|work_handler|function|static void work_handler(struct k_work *w)
 DECL|work_sleepy|function|static void work_sleepy(struct k_work *w)
