4f1e85991ccead1754fd2e461fbbb6908be609f5
Michael Hope <mlhx@google.com>
2018-03-10 22:20:18 +0100

    crc: make crc8_ccitt() match the other CRC functions.
    This is a minor change that makes the data pointer const and shifts
    the length to a size_t to match the other CRC functions.
    
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: dc37f985e9e4b0f37c0653a2b058916788b9692d

--- lib/crc/crc8_sw.c
+++ lib/crc/crc8_sw.c
@@ -1,2 +1,2 @@
 DECL|crc8_ccitt_small_table|variable|crc8_ccitt_small_table
-DECL|crc8_ccitt|function|u8_t crc8_ccitt(u8_t val, void *buf, int cnt)
+DECL|crc8_ccitt|function|u8_t crc8_ccitt(u8_t val, const void *buf, size_t cnt)
