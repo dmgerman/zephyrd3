c5dfa93616d2eb6723ace1747d9e91f9ae3e720e
Kumar Gala <kumar.gala@linaro.org>
2018-03-10 16:17:30 -0600

    tests: arm_runtime_nmi: Make test build on v8m
    The ICSR[NMIPENDSET] bit got renamed to ICSR[PENDNMISET] in the v8m
    architecture.  So we map SCB_ICSR_PENDNMISET_Msk to
    SCB_ICSR_NMIPENDSET_Msk to the tests builds and functions.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 924e8aad124651b9d41f1acb0df4e384e6025f74

--- tests/kernel/arm_runtime_nmi/src/arm_runtime_nmi.c
+++ tests/kernel/arm_runtime_nmi/src/arm_runtime_nmi.c
@@ -1,2 +1,3 @@
+DECL|SCB_ICSR_NMIPENDSET_Msk|macro|SCB_ICSR_NMIPENDSET_Msk
 DECL|nmi_test_isr|function|static void nmi_test_isr(void)
 DECL|test_arm_runtime_nmi|function|void test_arm_runtime_nmi(void)
