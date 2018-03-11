fb1902ad2c362417ee41e5f0d610ad9fa92ca7c1
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-03-11 22:29:25 +0100

    arch: arm: thread built-in stack guard implementation
    This commit activates the built-in stack guard on the main_thread
    before jumping to it upon system initialization. Stack guard is
    activated if BUILTIN_STACK_GUARD is enabled by the user. The
    commit also activates built-in thread stack guards at every
    context switch, if BUILTIN_STACK_GUARD enabled by the user.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: 8d1b013f3c70e588f5f5793e32ac528e5fd2821c

--- arch/arm/core/thread.c
+++ arch/arm/core/thread.c
@@ -1,2 +1,3 @@
 DECL|_arch_user_mode_enter|function|FUNC_NORETURN void _arch_user_mode_enter(k_thread_entry_t user_entry,void *p1, void *p2, void *p3)
 DECL|_new_thread|function|void _new_thread(struct k_thread *thread, k_thread_stack_t *stack, size_t stackSize, k_thread_entry_t pEntry, void *parameter1, void *parameter2, void *parameter3, int priority, unsigned int options)
+DECL|configure_builtin_stack_guard|function|void configure_builtin_stack_guard(struct k_thread *thread)
