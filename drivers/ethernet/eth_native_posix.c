b7e41e23a960ea6427ad587d2d06dbea7528afd4
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-12 21:54:19 +0200

    drivers: eth: native_posix: Enable gPTP support
    Allow gPTP code to be run as a linux process and communicate
    with gPTP daemon running in linux host.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 16f31f1d3c58cef59e77db6bdde96a288d8d90d4

--- drivers/ethernet/eth_native_posix.c
+++ drivers/ethernet/eth_native_posix.c
@@ -4,11 +4,15 @@ DECL|NET_BUF_TIMEOUT|macro|NET_BUF_TIMEOUT
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|_ETH_MTU|macro|_ETH_MTU
+DECL|api|variable|api
+DECL|check_gptp_msg|function|static struct gptp_hdr *check_gptp_msg(struct net_if *iface, struct net_pkt *pkt)
 DECL|create_rx_handler|function|static void create_rx_handler(struct eth_context *ctx)
 DECL|dev_fd|member|int dev_fd;
 DECL|eth_context_data|variable|eth_context_data
+DECL|eth_context|member|struct eth_context *eth_context;
 DECL|eth_context|struct|struct eth_context {
 DECL|eth_get_mac|function|static struct net_linkaddr *eth_get_mac(struct eth_context *ctx)
+DECL|eth_get_ptp_clock|function|static struct device *eth_get_ptp_clock(struct device *dev)
 DECL|eth_if_api|variable|eth_if_api
 DECL|eth_iface_init|function|static void eth_iface_init(struct net_if *iface)
 DECL|eth_init|function|static int eth_init(struct device *dev)
@@ -22,9 +26,21 @@ DECL|iface|member|struct net_if *iface;
 DECL|init_done|member|bool init_done;
 DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|mac_addr|member|u8_t mac_addr[6];
+DECL|need_timestamping|function|static bool need_timestamping(struct gptp_hdr *hdr)
+DECL|ptp_0_context|variable|ptp_0_context
+DECL|ptp_clock_adjust_native_posix|function|static int ptp_clock_adjust_native_posix(struct device *clk, int increment)
+DECL|ptp_clock_get_native_posix|function|static int ptp_clock_get_native_posix(struct device *clk, struct net_ptp_time *tm)
+DECL|ptp_clock_rate_adjust_native_posix|function|static int ptp_clock_rate_adjust_native_posix(struct device *clk, float ratio)
+DECL|ptp_clock_set_native_posix|function|static int ptp_clock_set_native_posix(struct device *clk, struct net_ptp_time *tm)
+DECL|ptp_clock|member|struct device *ptp_clock;
+DECL|ptp_context|struct|struct ptp_context {
+DECL|ptp_init|function|static int ptp_init(struct device *port)
 DECL|read_data|function|static int read_data(struct eth_context *ctx, int fd)
 DECL|recv|member|u8_t recv[_ETH_MTU + ETH_HDR_LEN];
 DECL|rx_thread_data|variable|rx_thread_data
 DECL|send|member|u8_t send[_ETH_MTU + ETH_HDR_LEN];
 DECL|stats|member|struct net_stats_eth stats;
 DECL|status|member|bool status;
+DECL|update_gptp|function|static void update_gptp(struct net_if *iface, struct net_pkt *pkt,bool send)
+DECL|update_gptp|macro|update_gptp
+DECL|update_pkt_priority|function|static void update_pkt_priority(struct gptp_hdr *hdr, struct net_pkt *pkt)
--- drivers/ethernet/eth_native_posix_adapt.c
+++ drivers/ethernet/eth_native_posix_adapt.c
@@ -1,6 +1,7 @@
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|_DEFAULT_SOURCE|macro|_DEFAULT_SOURCE
+DECL|eth_clock_gettime|function|int eth_clock_gettime(struct net_ptp_time *time)
 DECL|eth_iface_create|function|int eth_iface_create(const char *if_name, bool tun_only)
 DECL|eth_iface_remove|function|int eth_iface_remove(int fd)
 DECL|eth_read_data|function|ssize_t eth_read_data(int fd, void *buf, size_t buf_len)
