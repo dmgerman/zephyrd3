3e57ae449601e8f57fac275fcb5b171a5fdf9642
Michael Hope <mlhx@google.com>
2018-03-12 21:34:16 +0100

    hid: change the API table to const.
    A minor change to put the API function table into flash.
    
    Signed-off-by: Michael Hope <mlhx@google.com>
    
    Former-commit-id: 4d6d04d3ea030cbd76e58073cb7047bfc2f24064

--- subsys/usb/class/hid/core.c
+++ subsys/usb/class/hid/core.c
@@ -9,8 +9,8 @@ DECL|hid_ep_data|variable|hid_ep_data
 DECL|hid_int_in|function|static void hid_int_in(u8_t ep, enum usb_dc_ep_cb_status_code ep_status)
 DECL|hid_status_cb|function|static void hid_status_cb(enum usb_dc_status_code status, u8_t *param)
 DECL|interface_data|variable|interface_data
-DECL|ops|member|struct hid_ops *ops;
+DECL|ops|member|const struct hid_ops *ops;
 DECL|report_desc|member|const u8_t *report_desc;
 DECL|report_size|member|size_t report_size;
 DECL|usb_hid_init|function|int usb_hid_init(void)
-DECL|usb_hid_register_device|function|void usb_hid_register_device(const u8_t *desc, size_t size, struct hid_ops *ops)
+DECL|usb_hid_register_device|function|void usb_hid_register_device(const u8_t *desc, size_t size, const struct hid_ops *ops)
