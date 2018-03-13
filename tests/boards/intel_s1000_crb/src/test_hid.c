3b6216f47ddb144e5502fbfb349d4daba09db832
Savinay Dharmappa <savinay.dharmappa@intel.com>
2018-03-14 00:16:13 +0530

    test: board: intel_S1000: Add test application for HID
    patch add a hid test application which sends continues
    reports to the host
    
    Change-Id: I3a638caded629b502b4cfc6c7bdcba0b67428350
    Signed-off-by: Savinay Dharmappa <savinay.dharmappa@intel.com>
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: a2afa6cf8ce64f3f25bfc577c29738f6a0da168a

---
+++ tests/boards/intel_s1000_crb/src/test_hid.c
@@ -0,0 +1,12 @@
+DECL|PRIORITY|macro|PRIORITY
+DECL|REPORT_ID_1|macro|REPORT_ID_1
+DECL|REPORT_ID_2|macro|REPORT_ID_2
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|debug_cb|function|int debug_cb(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
+DECL|get_report_cb|function|int get_report_cb(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
+DECL|hid_report_desc|variable|hid_report_desc
+DECL|hid_thread|function|void hid_thread(void)
+DECL|ops|variable|ops
+DECL|set_idle_cb|function|int set_idle_cb(struct usb_setup_packet *setup, s32_t *len,u8_t **data)
