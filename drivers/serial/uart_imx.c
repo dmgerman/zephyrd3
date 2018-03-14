fa94a5d7e5d86fefb04139d182df46273d9908bc
Diego Sueiro <diego.sueiro@gmail.com>
2018-03-14 18:36:53 +0000

    serial: Add imx uart driver shim
    Adds a shim layer around the imx uart driver to adapt it to the Zephyr
    serial interface.
    
    Modem mode was introduce to control it as DCE and DTE and can be
    configured in the device tree:
        modem-mode:
            type: int
            category: required
            description: Set the UART Port to modem mode 0 (dce) 1 (dte)
            generation: define
    
    For now only the UART 2 was tested.
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>
    
    Former-commit-id: 346165b2e8e4662224106778b12e340d4a30fc3c

---
+++ drivers/serial/uart_imx.c
@@ -0,0 +1,48 @@
+DECL|DEV_CFG|macro|DEV_CFG
+DECL|UART_STRUCT|macro|UART_STRUCT
+DECL|base|member|UART_Type *base;
+DECL|baud_rate|member|u32_t baud_rate;
+DECL|callback|member|uart_irq_callback_t callback;
+DECL|imx_uart_1_config|variable|imx_uart_1_config
+DECL|imx_uart_1_data|variable|imx_uart_1_data
+DECL|imx_uart_2_config|variable|imx_uart_2_config
+DECL|imx_uart_2_data|variable|imx_uart_2_data
+DECL|imx_uart_3_config|variable|imx_uart_3_config
+DECL|imx_uart_3_data|variable|imx_uart_3_data
+DECL|imx_uart_4_config|variable|imx_uart_4_config
+DECL|imx_uart_4_data|variable|imx_uart_4_data
+DECL|imx_uart_5_config|variable|imx_uart_5_config
+DECL|imx_uart_5_data|variable|imx_uart_5_data
+DECL|imx_uart_6_config|variable|imx_uart_6_config
+DECL|imx_uart_6_data|variable|imx_uart_6_data
+DECL|imx_uart_7_config|variable|imx_uart_7_config
+DECL|imx_uart_7_data|variable|imx_uart_7_data
+DECL|imx_uart_config|struct|struct imx_uart_config {
+DECL|imx_uart_data|struct|struct imx_uart_data {
+DECL|irq_config_func_1|function|static void irq_config_func_1(struct device *dev)
+DECL|irq_config_func_2|function|static void irq_config_func_2(struct device *dev)
+DECL|irq_config_func_3|function|static void irq_config_func_3(struct device *dev)
+DECL|irq_config_func_4|function|static void irq_config_func_4(struct device *dev)
+DECL|irq_config_func_5|function|static void irq_config_func_5(struct device *dev)
+DECL|irq_config_func_6|function|static void irq_config_func_6(struct device *dev)
+DECL|irq_config_func_7|function|static void irq_config_func_7(struct device *dev)
+DECL|irq_config_func|member|void (*irq_config_func)(struct device *dev);
+DECL|modem_mode|member|u8_t modem_mode;
+DECL|uart_imx_driver_api|variable|uart_imx_driver_api
+DECL|uart_imx_fifo_fill|function|static int uart_imx_fifo_fill(struct device *dev, const u8_t *tx_data, int size)
+DECL|uart_imx_fifo_read|function|static int uart_imx_fifo_read(struct device *dev, u8_t *rx_data, const int size)
+DECL|uart_imx_init|function|static int uart_imx_init(struct device *dev)
+DECL|uart_imx_irq_callback_set|function|static void uart_imx_irq_callback_set(struct device *dev,uart_irq_callback_t cb)
+DECL|uart_imx_irq_err_disable|function|static void uart_imx_irq_err_disable(struct device *dev)
+DECL|uart_imx_irq_err_enable|function|static void uart_imx_irq_err_enable(struct device *dev)
+DECL|uart_imx_irq_is_pending|function|static int uart_imx_irq_is_pending(struct device *dev)
+DECL|uart_imx_irq_rx_disable|function|static void uart_imx_irq_rx_disable(struct device *dev)
+DECL|uart_imx_irq_rx_enable|function|static void uart_imx_irq_rx_enable(struct device *dev)
+DECL|uart_imx_irq_rx_ready|function|static int uart_imx_irq_rx_ready(struct device *dev)
+DECL|uart_imx_irq_tx_disable|function|static void uart_imx_irq_tx_disable(struct device *dev)
+DECL|uart_imx_irq_tx_enable|function|static void uart_imx_irq_tx_enable(struct device *dev)
+DECL|uart_imx_irq_tx_ready|function|static int uart_imx_irq_tx_ready(struct device *dev)
+DECL|uart_imx_irq_update|function|static int uart_imx_irq_update(struct device *dev)
+DECL|uart_imx_isr|function|void uart_imx_isr(void *arg)
+DECL|uart_imx_poll_in|function|static int uart_imx_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_imx_poll_out|function|static unsigned char uart_imx_poll_out(struct device *dev, unsigned char c)
