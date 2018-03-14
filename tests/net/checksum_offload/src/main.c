2a1f9da245bf1318a62e7264f3c6b35716ef1206
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-14 09:45:14 +0200

    tests: net: Network packet checksum offloading tests
    Test that if the checksum offloading is enabled, then we do not
    calculate the checksum. Also the normal case, where offloading is
    disabled and we need to calculate the checksum, is tested.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 22eeb70a26f88d25cdce2235f593f6b0c092a588

---
+++ tests/net/checksum_offload/src/main.c
@@ -0,0 +1,54 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|PORT|macro|PORT
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|add_neighbor|function|static bool add_neighbor(struct net_if *iface, struct in6_addr *addr)
+DECL|address_setup|function|static void address_setup(void)
+DECL|api_funcs_offloading_disabled|variable|api_funcs_offloading_disabled
+DECL|api_funcs_offloading_enabled|variable|api_funcs_offloading_enabled
+DECL|dst_addr|variable|dst_addr
+DECL|eth_context_offloading_disabled|variable|eth_context_offloading_disabled
+DECL|eth_context_offloading_enabled|variable|eth_context_offloading_enabled
+DECL|eth_context|struct|struct eth_context {
+DECL|eth_if_count|member|int eth_if_count;
+DECL|eth_iface_init|function|static void eth_iface_init(struct net_if *iface)
+DECL|eth_init|function|static int eth_init(struct device *dev)
+DECL|eth_interfaces|variable|eth_interfaces
+DECL|eth_offloading_enabled|function|static enum eth_hw_caps eth_offloading_enabled(struct device *dev)
+DECL|eth_setup|function|static void eth_setup(void)
+DECL|eth_tx_offloading_disabled|function|static int eth_tx_offloading_disabled(struct net_if *iface, struct net_pkt *pkt)
+DECL|eth_tx_offloading_enabled|function|static int eth_tx_offloading_enabled(struct net_if *iface, struct net_pkt *pkt)
+DECL|expecting_tag|member|u16_t expecting_tag;
+DECL|generate_mac|function|static void generate_mac(u8_t *mac_addr)
+DECL|iface2str|function|static const char *iface2str(struct net_if *iface)
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
+DECL|iface|member|struct net_if *iface;
+DECL|in4addr_dst|variable|in4addr_dst
+DECL|in4addr_my2|variable|in4addr_my2
+DECL|in4addr_my|variable|in4addr_my
+DECL|ll_addr|variable|ll_addr
+DECL|mac_addr|member|u8_t mac_addr[6];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|recv_cb_offload_disabled|function|static void recv_cb_offload_disabled(struct net_context *context, struct net_pkt *pkt, int status, void *user_data)
+DECL|recv_cb_offload_enabled|function|static void recv_cb_offload_enabled(struct net_context *context, struct net_pkt *pkt, int status, void *user_data)
+DECL|rx_chksum_offload_disabled_test_v4|function|static void rx_chksum_offload_disabled_test_v4(void)
+DECL|rx_chksum_offload_disabled_test_v6|function|static void rx_chksum_offload_disabled_test_v6(void)
+DECL|rx_chksum_offload_enabled_test_v4|function|static void rx_chksum_offload_enabled_test_v4(void)
+DECL|rx_chksum_offload_enabled_test_v6|function|static void rx_chksum_offload_enabled_test_v6(void)
+DECL|start_receiving|variable|start_receiving
+DECL|test_data|variable|test_data
+DECL|test_failed|variable|test_failed
+DECL|test_main|function|void test_main(void)
+DECL|test_started|variable|test_started
+DECL|total_if_count|member|int total_if_count;
+DECL|tx_chksum_offload_disabled_test_v4|function|static void tx_chksum_offload_disabled_test_v4(void)
+DECL|tx_chksum_offload_disabled_test_v6|function|static void tx_chksum_offload_disabled_test_v6(void)
+DECL|tx_chksum_offload_enabled_test_v4|function|static void tx_chksum_offload_enabled_test_v4(void)
+DECL|tx_chksum_offload_enabled_test_v6|function|static void tx_chksum_offload_enabled_test_v6(void)
+DECL|udp_v4_ctx_1|variable|udp_v4_ctx_1
+DECL|udp_v4_ctx_2|variable|udp_v4_ctx_2
+DECL|udp_v6_ctx_1|variable|udp_v6_ctx_1
+DECL|udp_v6_ctx_2|variable|udp_v6_ctx_2
+DECL|user_data|struct|struct user_data {
