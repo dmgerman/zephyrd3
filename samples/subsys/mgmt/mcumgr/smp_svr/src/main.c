7ad90a79dc674c0d468250198df143bd51bd835e
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-03-14 14:30:57 +0100

    samples: subsys: mgmt: mcumgr: Fix FS part of smp_svr example
    After introducing of Virtual File System Switch (#6318)
    it i required to mount file system in order to use it.
    This patch add to the example code which mounts NFFS
    file system in the example.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    
    Former-commit-id: ca6ebbdf7487c6b77a6839656ad709f79fffb74b

--- samples/subsys/mgmt/mcumgr/smp_svr/src/main.c
+++ samples/subsys/mgmt/mcumgr/smp_svr/src/main.c
@@ -7,5 +7,7 @@ DECL|bt_ready|function|static void bt_ready(int err)
 DECL|conn_callbacks|variable|conn_callbacks
 DECL|connected|function|static void connected(struct bt_conn *conn, u8_t err)
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn, u8_t reason)
+DECL|flash_desc|variable|flash_desc
 DECL|main|function|void main(void)
+DECL|nffs_mnt|variable|nffs_mnt
 DECL|sd|variable|sd
