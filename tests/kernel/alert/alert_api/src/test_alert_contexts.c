e4f159836794f1b1804b4f5753da32f1dc0e5f60
Savinay Dharmappa <savinay.dharmappa@intel.com>
2018-03-15 01:44:23 +0530

    tests: kernel: alert: Add testcases
    Add testcases for following scenario
    
    Test to check alert_recv(timeout) against the following cases
    
    1. The current task times out while waiting for the event.
    2. There is already an event waiting (signalled from a task).
    3. The current task must wait on the event until it is signalled
       from either another task or an ISR
    
    Test to check alert_recv(K_FOREVER) against the following cases:
    
    1. There is already an event waiting (signalled from a task and ISR).
    2. The current task must wait on the event until it is signalled
       from either another task or an ISR
    
    Test to checks that the event handler is set up properly when
    alert_event_handler_set() is called.  It shows that event handlers
    are tied to the specified event and that the return value from the
    handler affects whether the event wakes a task waiting upon that
    event
    
    Signed-off-by: Savinay Dharmappa <savinay.dharmappa@intel.com>
    
    Former-commit-id: 04d2abb118bc138a3d7f9eec261138296f519ddf

--- tests/kernel/alert/alert_api/src/test_alert_contexts.c
+++ tests/kernel/alert/alert_api/src/test_alert_contexts.c
@@ -3,17 +3,24 @@ DECL|HANDLER_1|enumerator|HANDLER_1
 DECL|HANDLER_DEFAULT|enumerator|HANDLER_DEFAULT,
 DECL|HANDLER_IGNORE|enumerator|HANDLER_IGNORE,
 DECL|PENDING_MAX|macro|PENDING_MAX
+DECL|SEM_INITIAL|macro|SEM_INITIAL
+DECL|SEM_LIMIT|macro|SEM_LIMIT
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|TIMEOUT|macro|TIMEOUT
 DECL|alert_handler0|function|static int alert_handler0(struct k_alert *alt)
 DECL|alert_handler1|function|static int alert_handler1(struct k_alert *alt)
 DECL|alert_recv|function|static void alert_recv(void)
 DECL|alert_send|function|static void alert_send(void)
+DECL|eventHandler|function|int eventHandler(struct k_alert *alt)
 DECL|handle_type|enum|enum handle_type {
 DECL|handler_executed|variable|handler_executed
+DECL|handler_val|variable|handler_val
 DECL|htype|variable|htype
 DECL|isr_alert|function|static void isr_alert(void)
 DECL|palert|variable|palert
+DECL|signal_task|function|void signal_task(void *p1, void *p2, void *p3)
+DECL|sync_entry|function|static void sync_entry(void *p)
+DECL|sync_tdata|variable|sync_tdata
 DECL|tdata|variable|tdata
 DECL|test_isr_alert_consumed|function|void test_isr_alert_consumed(void)
 DECL|test_isr_alert_default|function|void test_isr_alert_default(void)
@@ -23,8 +30,11 @@ DECL|test_isr_kinit_alert|function|void test_isr_kinit_alert(void)
 DECL|test_main|function|void test_main(void)
 DECL|test_thread_alert_consumed|function|void test_thread_alert_consumed(void)
 DECL|test_thread_alert_default|function|void test_thread_alert_default(void)
+DECL|test_thread_alert_handler|function|void test_thread_alert_handler(void)
 DECL|test_thread_alert_ignore|function|void test_thread_alert_ignore(void)
 DECL|test_thread_alert_pending|function|void test_thread_alert_pending(void)
+DECL|test_thread_alert_timeout|function|void test_thread_alert_timeout(void)
+DECL|test_thread_alert_wait|function|void test_thread_alert_wait(void)
 DECL|test_thread_kinit_alert|function|void test_thread_kinit_alert(void)
 DECL|thread_alerts|variable|thread_alerts
 DECL|thread_alert|function|static void thread_alert(void)
