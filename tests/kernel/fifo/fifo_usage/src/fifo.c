022657b726275279270fe3568c653fc06be4140f
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-03-19 19:23:30 +0530

    tests: kernel: Add fifo usage scenario tests
    Add k_fifo kernel object usage scenario tests between thread
    to thread and thread to ISR.
    
    This test is inspired from test_fifo legacy test from
    Zephyr v1.7.0 release.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: f112b3eee1bc739639ee9668bd9be62e08208016

---
+++ tests/kernel/fifo/fifo_usage/src/fifo.c
@@ -0,0 +1,19 @@
+DECL|LIST_LEN|macro|LIST_LEN
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|data1|variable|data1
+DECL|data2|variable|data2
+DECL|data_isr|variable|data_isr
+DECL|data|member|u32_t data;
+DECL|end_sema|variable|end_sema
+DECL|fdata_t|struct|struct fdata_t {
+DECL|snode|member|sys_snode_t snode;
+DECL|tIsr_entry_get|function|static void tIsr_entry_get(void *p)
+DECL|tIsr_entry_put|function|static void tIsr_entry_put(void *p)
+DECL|tdata|variable|tdata
+DECL|test_dual_fifo_play|function|static void test_dual_fifo_play(void)
+DECL|test_isr_fifo_play|function|static void test_isr_fifo_play(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_single_fifo_play|function|static void test_single_fifo_play(void)
+DECL|thread_entry_fn_dual|function|static void thread_entry_fn_dual(void *p1, void *p2, void *p3)
+DECL|thread_entry_fn_isr|function|static void thread_entry_fn_isr(void *p1, void *p2, void *p3)
+DECL|thread_entry_fn_single|function|static void thread_entry_fn_single(void *p1, void *p2, void *p3)
