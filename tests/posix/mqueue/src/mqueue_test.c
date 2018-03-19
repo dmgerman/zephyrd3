8319f11ed2815d946a381150ad72d6d07e12c775
Youvedeep Singh <youvedeep.singh@intel.com>
2018-03-19 22:30:00 +0530

    tests: posix: mqueue: Add test for posix message queue APIs.
    Added new test for message queue APIs.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 2486d5a2d2395e53c08c9004181df0adafe20bb8

---
+++ tests/posix/mqueue/src/mqueue_test.c
@@ -0,0 +1,12 @@
+DECL|MESG_COUNT_PERMQ|macro|MESG_COUNT_PERMQ
+DECL|MESSAGE_SIZE|macro|MESSAGE_SIZE
+DECL|N_THR|macro|N_THR
+DECL|RECEIVER_THREAD|macro|RECEIVER_THREAD
+DECL|SENDER_THREAD|macro|SENDER_THREAD
+DECL|STACKSZ|macro|STACKSZ
+DECL|queue|variable|queue
+DECL|receiver_thread|function|void *receiver_thread(void *p1)
+DECL|send_data|variable|send_data
+DECL|sender_thread|function|void *sender_thread(void *p1)
+DECL|test_main|function|void test_main(void)
+DECL|test_mqueue|function|void test_mqueue(void)
