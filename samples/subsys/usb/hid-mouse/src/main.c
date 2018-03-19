8999e09d33a753e2d5c3f1b1f24ff61753133912
qianfan Zhao <qianfanguijin@163.com>
2018-03-19 18:36:19 +0800

    samples: usb: add usb hid mouse sample
    this sample expect there has a button in you board, that was
    the mouse's left button, tested in linux by using evtest.
    
    Signed-off-by: qianfan Zhao <qianfanguijin@163.com>
    
    Former-commit-id: b2570d2a559a8d242521774fcb6d63eb86c2de1e

---
+++ samples/subsys/usb/hid-mouse/src/main.c
@@ -0,0 +1,12 @@
+DECL|MOUSE_BTN_LEFT|macro|MOUSE_BTN_LEFT
+DECL|MOUSE_BTN_MIDDLE|macro|MOUSE_BTN_MIDDLE
+DECL|MOUSE_BTN_RIGHT|macro|MOUSE_BTN_RIGHT
+DECL|PIN|macro|PIN
+DECL|PORT|macro|PORT
+DECL|button_pressed|function|void button_pressed(struct device *gpio, struct gpio_callback *cb, u32_t pins)
+DECL|def_val|variable|def_val
+DECL|get_report_cb|function|static int get_report_cb(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
+DECL|hid_report_desc|variable|hid_report_desc
+DECL|main|function|void main(void)
+DECL|ops|variable|ops
+DECL|status|variable|status
