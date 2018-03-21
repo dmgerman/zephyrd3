d44894b734fad0bc5994df05671968c524a982e1
Andy Ross <andrew.j.ross@intel.com>
2018-03-21 13:46:34 -0700

    tests: rbtree test
    Test for the rbtree balanced tree.
    
    The theory here is that the test uses a PRNG to randomly build and
    modify trees build from an array of node objects (whose comparison
    operator is just their memory address).  It begins with tiny node
    sets, so as to catch edge cases (e.g. nodes being the root, or a leaf,
    or parent/child) in the balancing algorithm before increasing the size
    and continuing.
    
    At each iteration the resulting tree is walked, verifying that it
    contains exactly the nodes that we expect it should, that they appear
    in the correct sorted order in the walk, that their local structure
    matches a correct binary tree, and that the red/black rules are
    followed always:
    
    * The root must be black
    * Red nodes can have only black children
    * The path from the root to any NULL child pointer in the tree must
      cross the same number of black nodes.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: c625ab85d41c1464615d72a50b10b1ec07829cb3

---
+++ tests/lib/rbtree/src/main.c
@@ -0,0 +1,19 @@
+DECL|CHECK|macro|CHECK
+DECL|MAX_NODES|macro|MAX_NODES
+DECL|check_rbnode|function|void check_rbnode(struct rbnode *node, int blacks_above)
+DECL|check_rb|function|void check_rb(void)
+DECL|check_tree|function|void check_tree(int size)
+DECL|get_node_mask|function|int get_node_mask(int node)
+DECL|last_black_height|variable|last_black_height
+DECL|next_rand_mod|function|static unsigned int next_rand_mod(unsigned int mod)
+DECL|node_index|function|int node_index(struct rbnode *n)
+DECL|node_lessthan|function|int node_lessthan(struct rbnode *a, struct rbnode *b)
+DECL|node_mask|variable|node_mask
+DECL|nodes|variable|nodes
+DECL|set_node_mask|function|void set_node_mask(int node, int val)
+DECL|test_main|function|void test_main(void)
+DECL|test_rbtree_spam|function|void test_rbtree_spam(void)
+DECL|test_tree|function|void test_tree(int size)
+DECL|tree|variable|tree
+DECL|visit_node|function|void visit_node(struct rbnode *node, void *cookie)
+DECL|walked_nodes|variable|walked_nodes
