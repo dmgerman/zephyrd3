705722ef02731067b5b9688afe5547e51becb6b6
Carles Cufi <carles.cufi@nordicsemi.no>
2018-03-22 20:58:57 +0100

    test: lib: base64: Add a base64 test
    Add a test imported from the original mbedTLS suite.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: ca1bb5eaf543dfc32dea0c9ac7dbedc5f6c0f866

---
+++ tests/lib/base64/src/main.c
@@ -0,0 +1,4 @@
+DECL|base64_test_dec|variable|base64_test_dec
+DECL|base64_test_enc|variable|base64_test_enc
+DECL|test_base64_codec|function|static void test_base64_codec(void)
+DECL|test_main|function|void test_main(void)
