5550828b32f6a1efd85e2ee8d14c715f644c4119
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-03-22 15:26:26 +0100

    arch: arm: configure non-secure registers from secure firmware
    Define and implement an API to configure the Non-Secure instances
    of core registers in ARM Cortex-M23 and Cortex-M33.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: 034499af1a97f67325e4fafca8f1e4e4993bd0d3

---
+++ arch/arm/core/cortex_m/tz/arm_core_tz.c
@@ -0,0 +1,5 @@
+DECL|configure_nonsecure_control|function|static void configure_nonsecure_control(u32_t spsel_ns, u32_t npriv_ns)
+DECL|configure_nonsecure_msp|function|static void configure_nonsecure_msp(u32_t msp_ns)
+DECL|configure_nonsecure_psp|function|static void configure_nonsecure_psp(u32_t psp_ns)
+DECL|configure_nonsecure_vtor_offset|function|static void configure_nonsecure_vtor_offset(u32_t vtor_ns)
+DECL|tz_nonsecure_state_setup|function|void tz_nonsecure_state_setup(const tz_nonsecure_setup_conf_t *p_ns_conf)
---
+++ arch/arm/include/cortex_m/tz.h
@@ -0,0 +1,10 @@
+DECL|_ARM_CORTEXM_TZ__H_|macro|_ARM_CORTEXM_TZ__H_
+DECL|control_ns|member|} control_ns;
+DECL|msp_ns|member|u32_t msp_ns;
+DECL|npriv|member|u32_t npriv:1;
+DECL|psp_ns|member|u32_t psp_ns;
+DECL|reserved|member|u32_t reserved:30;
+DECL|spsel|member|u32_t spsel:1;
+DECL|tz_nonsecure_setup_conf_t|typedef|} tz_nonsecure_setup_conf_t;
+DECL|tz_nonsecure_setup_conf|struct|typedef struct tz_nonsecure_setup_conf {
+DECL|vtor_ns|member|u32_t vtor_ns;
