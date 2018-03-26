d44d25f7c909af8e6e0fc31c275da5091f24bcd2
Youvedeep Singh <youvedeep.singh@intel.com>
2018-03-26 18:28:47 +0530

    include: posix: correcting the return type of sleep.
    Currently return type of sleep is int, but as per POSIX 1003.1 it
    should be unsigned.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: 43383701cac45ee1fa37bd17111848d88d583492

--- include/posix/unistd.h
+++ include/posix/unistd.h
@@ -1,3 +1,3 @@
 DECL|__POSIX_UNISTD_H__|macro|__POSIX_UNISTD_H__
-DECL|sleep|function|static inline int sleep(unsigned int seconds)
+DECL|sleep|function|static inline unsigned sleep(unsigned int seconds)
 DECL|usleep|function|static inline int usleep(useconds_t useconds)
