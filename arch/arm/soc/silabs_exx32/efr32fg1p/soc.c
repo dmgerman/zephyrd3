ef7b0c252402f793780567209b51115bb5ec1c52
Christian Taedcke <hacking@taedcke.com>
2018-03-27 10:10:12 +0200

    arm: exx32: Add Silabs EFR32FG1P soc files
    The Silicon Labs EFR32FG1 Flex Gecko MCU includes:
    	* Cortex-M4F core at 40MHz
    	* up to 256KB of flash and 32KB of RAM
    	* integrated Sub-GHz and/or 2.4GHz radio
    	* multiple low power peripherals
    
    Signed-off-by: Christian Taedcke <hacking@taedcke.com>
    
    Former-commit-id: 496b799474ef32ab4c8121e267931e27e02a6785

---
+++ arch/arm/soc/silabs_exx32/efr32fg1p/soc.c
@@ -0,0 +1,4 @@
+DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
+DECL|hfxoInit|variable|hfxoInit
+DECL|lfxoInit|variable|lfxoInit
+DECL|silabs_efr32fg1p_init|function|static int silabs_efr32fg1p_init(struct device *arg)
