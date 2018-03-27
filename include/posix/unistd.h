86488b42373b2cf34a00eed05048d3e9a2fb9ccd
Youvedeep Singh <youvedeep.singh@intel.com>
2018-03-27 13:54:01 +0530

    kernel: posix: move sleep and usleep functions into c file.
    Currently sleep and usleep functions are into unistd.h file.
    unistd includes toold chain secific unistd.h file and this file
    too has declaration for these functions. This is in conflict when
    posix specific unistd.h is included.
    
    Signed-off-by: Youvedeep Singh <youvedeep.singh@intel.com>
    
    Former-commit-id: f762fdf4828f1c530cc0aac352405d93521fb79c

--- include/posix/unistd.h
+++ include/posix/unistd.h
@@ -1,3 +1,1 @@
 DECL|__POSIX_UNISTD_H__|macro|__POSIX_UNISTD_H__
-DECL|sleep|function|static inline unsigned sleep(unsigned int seconds)
-DECL|usleep|function|static inline int usleep(useconds_t useconds)
---
+++ kernel/posix/sleep.c
@@ -0,0 +1,2 @@
+DECL|sleep|function|unsigned sleep(unsigned int seconds)
+DECL|usleep|function|int usleep(useconds_t useconds)
