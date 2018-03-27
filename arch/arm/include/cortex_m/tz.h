3591efda87003be2c705c749e32249007fcf2239
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-03-27 15:30:51 +0200

    arch: arm: API macros for CMSE non-secure function pointers
    This commit defines and implements and internal ARM macro API
    that allows the TrustZone user to declare, define, and evaluate
    pointers of non-secure function type.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: a05081804ad1b9f242062badcbae2f22f9a67875

--- arch/arm/include/cortex_m/tz.h
+++ arch/arm/include/cortex_m/tz.h
@@ -1,3 +1,6 @@
+DECL|TZ_NONSECURE_FUNC_PTR_CREATE|macro|TZ_NONSECURE_FUNC_PTR_CREATE
+DECL|TZ_NONSECURE_FUNC_PTR_DECLARE|macro|TZ_NONSECURE_FUNC_PTR_DECLARE
+DECL|TZ_NONSECURE_FUNC_PTR_IS_NS|macro|TZ_NONSECURE_FUNC_PTR_IS_NS
 DECL|_ARM_CORTEXM_TZ__H_|macro|_ARM_CORTEXM_TZ__H_
 DECL|control_ns|member|} control_ns;
 DECL|msp_ns|member|u32_t msp_ns;
@@ -5,6 +8,8 @@ DECL|npriv|member|u32_t npriv:1;
 DECL|psp_ns|member|u32_t psp_ns;
 DECL|reserved|member|u32_t reserved:30;
 DECL|spsel|member|u32_t spsel:1;
+DECL|typeof|macro|typeof
 DECL|tz_nonsecure_setup_conf_t|typedef|} tz_nonsecure_setup_conf_t;
 DECL|tz_nonsecure_setup_conf|struct|typedef struct tz_nonsecure_setup_conf {
+DECL|tz_ns_func_ptr_t|typedef|typedef void __attribute__((cmse_nonsecure_call)) (*tz_ns_func_ptr_t) (void);
 DECL|vtor_ns|member|u32_t vtor_ns;
