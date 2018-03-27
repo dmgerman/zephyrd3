aff0296c0606b89fa02292b8e940c77503c8d816
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-27 22:25:40 +0300

    sample: net: stats: Example how to use net_mgmt for statistics
    Simple application that queries network statistics periodically
    via net_mgmt interface.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 15ccd9cc0d0ea048ca1ffb460a572758fe60bf3f

---
+++ samples/net/stats/src/main.c
@@ -0,0 +1,11 @@
+DECL|GET_STAT|macro|GET_STAT
+DECL|GET_STAT|macro|GET_STAT
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
+DECL|init_app|function|static void init_app(void)
+DECL|main|function|void main(void)
+DECL|print_stats|function|static void print_stats(struct net_if *iface, struct net_stats *data)
+DECL|stats_timer|variable|stats_timer
+DECL|stats|function|static void stats(struct k_work *work)
