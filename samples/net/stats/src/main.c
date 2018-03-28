c74e53ba49430b3800af702d32488b6d17c53953
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-03-28 17:08:29 +0300

    samples: net: stats: Print ethernet statistics
    If ethernet statistics collection is enabled, then print it
    to the user.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 435d173870133c91581d480963be2eb1351b537e

--- samples/net/stats/src/main.c
+++ samples/net/stats/src/main.c
@@ -3,9 +3,11 @@ DECL|GET_STAT|macro|GET_STAT
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|eth_iface_cb|function|static void eth_iface_cb(struct net_if *iface, void *user_data)
 DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
 DECL|init_app|function|static void init_app(void)
 DECL|main|function|void main(void)
+DECL|print_eth_stats|function|static void print_eth_stats(struct net_if *iface, struct net_stats_eth *data)
 DECL|print_stats|function|static void print_stats(struct net_if *iface, struct net_stats *data)
 DECL|stats_timer|variable|stats_timer
 DECL|stats|function|static void stats(struct k_work *work)
