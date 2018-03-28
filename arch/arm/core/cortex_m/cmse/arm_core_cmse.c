1f1896470bfc0a6ba823a5f1b5d28a7a3aed46c7
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-03-28 15:30:40 +0200

    arch: arm: Define & implement API for test target (Non-Secure)
    This commit defines and implements an internal ARMv8-M API
    that allows the user to evaluate access permissions of memory
    locations, based on the ARMv8-M Test Target (TT) instruction
    support.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: c842f32dddb2e318b66ce882c3aa110f9f9a2df2

---
+++ arch/arm/core/cortex_m/cmse/arm_core_cmse.c
@@ -0,0 +1,4 @@
+DECL|arm_cmse_addr_read_ok|function|int arm_cmse_addr_read_ok(u32_t addr, int force_npriv)
+DECL|arm_cmse_addr_read_write_ok|function|static int arm_cmse_addr_read_write_ok(u32_t addr, int force_npriv, int rw)
+DECL|arm_cmse_addr_readwrite_ok|function|int arm_cmse_addr_readwrite_ok(u32_t addr, int force_npriv)
+DECL|arm_cmse_mpu_region_get|function|int arm_cmse_mpu_region_get(u32_t addr, u8_t *p_region)
