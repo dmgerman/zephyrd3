9bd3cd77e9c39f83dacb943ab90519aca4570b8b
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-03-28 13:58:14 +0530

    tests: kernel: threads: Additional tests for set_priority
    Added some additional tests for setting priority of threads.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: 3c8b3875c6167f79101bfb55283395252dc56bde

---
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/test_threads_set_priority.c
@@ -0,0 +1,5 @@
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|test_threads_priority_set|function|void test_threads_priority_set(void)
+DECL|thread2_data|variable|thread2_data
+DECL|thread2_set_prio_test|function|void thread2_set_prio_test(void)
+DECL|tstack|variable|tstack
