93a942158c2f206b8cb571657178b7a86258f8c5
Punit Vara <punit.vara@intel.com>
2018-03-29 15:08:20 +0530

    test: mbox_usage: add legacy test case for mailbox
    Add different scenario for mailbox testing
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 4fc2ccbdabf6182cf93dd89d3f4e162bd93afaaa

---
+++ tests/kernel/mbox/mbox_usage/src/mbox.c
@@ -0,0 +1,19 @@
+DECL|MAIL_LEN|macro|MAIL_LEN
+DECL|PUT_GET_NULL|enumerator|PUT_GET_NULL = 0,
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|TARGET_SOURCE|enumerator|TARGET_SOURCE
+DECL|info_type|variable|info_type
+DECL|mbox|variable|mbox
+DECL|mmsg_type|enum|static enum mmsg_type {
+DECL|msg_receiver_unlimited|function|void msg_receiver_unlimited(void)
+DECL|msg_receiver|function|static void msg_receiver(struct k_mbox *pmbox, k_tid_t thd_id, s32_t timeout)
+DECL|msg_sender|function|static void msg_sender(struct k_mbox *pmbox, s32_t timeout)
+DECL|receiver_tid|variable|receiver_tid
+DECL|sync_sema|variable|sync_sema
+DECL|tdata|variable|tdata
+DECL|test_main|function|void test_main(void)
+DECL|test_mbox_init|function|static void test_mbox_init(void)
+DECL|test_msg_receiver|function|void test_msg_receiver(void)
+DECL|test_send_un|function|void test_send_un(void *p1, void *p2, void *p3)
+DECL|test_send|function|void test_send(void *p1, void *p2, void *p3)
+DECL|tid1|variable|tid1
