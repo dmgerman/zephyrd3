0b5c70ce4c1d7cfa53963d3ea2b19bbfe414f2ca
Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
2018-03-30 21:25:37 +0200

    net: sockets: Check for errors from dns_get_addr_info
    When dns_get_addr_info() returns an error it does not call the
    resolve callback, and thus the semaphore will not be given.
    
    This fix will avoid a deadlock situation for various errors.
    
    Added some small tests for getaddrinfo().
    
    Signed-off-by: Stig Bjørlykke <stig.bjorlykke@nordicsemi.no>
    
    Former-commit-id: 97b83ffc2c9b1cdde01bdea8412fc5f8a921410b

---
+++ tests/net/socket/getaddrinfo/src/main.c
@@ -0,0 +1,3 @@
+DECL|test_getaddrinfo_no_host|function|void test_getaddrinfo_no_host(void)
+DECL|test_getaddrinfo_ok|function|void test_getaddrinfo_ok(void)
+DECL|test_main|function|void test_main(void)
