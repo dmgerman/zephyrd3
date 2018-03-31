dbaf40c38ce75b9790e9e7817e4469deaef5a258
Anas Nashif <anas.nashif@intel.com>
2018-03-31 10:43:18 -0500

    tests: classify net tests and cleanup
    More tagging and filterting cleanup.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 848c105485c324e6330976ecce6d1fb3c9db21e7

--- tests/net/ieee802154/l2/src/ieee802154_test.c
+++ tests/net/ieee802154/l2/src/ieee802154_test.c
@@ -9,28 +9,28 @@ DECL|fc_seq|member|struct ieee802154_fcf_seq *fc_seq;
 DECL|ieee802154_pkt_test|struct|struct ieee802154_pkt_test {
 DECL|ieee_addr_hexdump|function|static void ieee_addr_hexdump(u8_t *addr, u8_t length)
 DECL|iface|variable|iface
-DECL|init_test|function|static void init_test(void)
 DECL|initialize_test_environment|function|static bool initialize_test_environment(void)
 DECL|length|member|u8_t length;
 DECL|mhr_check|member|} mhr_check;
 DECL|name|member|char *name;
 DECL|ns_pkt|variable|ns_pkt
-DECL|parsing_ack_pkt|function|static void parsing_ack_pkt(void)
-DECL|parsing_beacon_pkt|function|static void parsing_beacon_pkt(void)
-DECL|parsing_ns_pkt|function|static void parsing_ns_pkt(void)
-DECL|parsing_sec_data_pkt|function|static void parsing_sec_data_pkt(void)
 DECL|pkt_hexdump|function|static void pkt_hexdump(u8_t *pkt, u8_t length)
 DECL|pkt|member|u8_t *pkt;
-DECL|replying_ack_pkt|function|static void replying_ack_pkt(void)
 DECL|sec_data_pkt|variable|sec_data_pkt
-DECL|sending_ns_pkt|function|static void sending_ns_pkt(void)
 DECL|src_addr|member|struct ieee802154_address_field *src_addr;
 DECL|src|member|struct in6_addr src;
 DECL|test_ack_pkt|variable|test_ack_pkt
 DECL|test_ack_reply|function|static bool test_ack_reply(struct ieee802154_pkt_test *t)
 DECL|test_beacon_pkt|variable|test_beacon_pkt
+DECL|test_init|function|static void test_init(void)
 DECL|test_main|function|void test_main(void)
 DECL|test_ns_pkt|variable|test_ns_pkt
 DECL|test_ns_sending|function|static bool test_ns_sending(struct ieee802154_pkt_test *t)
 DECL|test_packet_parsing|function|static bool test_packet_parsing(struct ieee802154_pkt_test *t)
+DECL|test_parsing_ack_pkt|function|static void test_parsing_ack_pkt(void)
+DECL|test_parsing_beacon_pkt|function|static void test_parsing_beacon_pkt(void)
+DECL|test_parsing_ns_pkt|function|static void test_parsing_ns_pkt(void)
+DECL|test_parsing_sec_data_pkt|function|static void test_parsing_sec_data_pkt(void)
+DECL|test_replying_ack_pkt|function|static void test_replying_ack_pkt(void)
 DECL|test_sec_data_pkt|variable|test_sec_data_pkt
+DECL|test_sending_ns_pkt|function|static void test_sending_ns_pkt(void)
