2aeae04e153cb3818faf41496e00360f958299c0
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-04-03 12:17:32 +0530

    tests: boards: altera_max10: Add test for Nios-II MSGDMA soft IP
    Add test for Altera Nios-II MSGDMA soft IP core. This IP on
    Altera MAX10 support Memory to Memory DMA transfers.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: ee1e9886a9d7cea3c6c721079f2e2bce87078eac

---
+++ tests/boards/altera_max10/msgdma/src/dma.c
@@ -0,0 +1,13 @@
+DECL|DMA_BUFF_SIZE|macro|DMA_BUFF_SIZE
+DECL|DMA_OP_STAT_ERR|enumerator|DMA_OP_STAT_ERR,
+DECL|DMA_OP_STAT_NONE|enumerator|DMA_OP_STAT_NONE = 0,
+DECL|DMA_OP_STAT_SUCCESS|enumerator|DMA_OP_STAT_SUCCESS,
+DECL|dma_block_cfg|variable|dma_block_cfg
+DECL|dma_cfg|variable|dma_cfg
+DECL|dma_op_status|enum|enum dma_op_status {
+DECL|dma_stat|variable|dma_stat
+DECL|dma_user_callback|function|static void dma_user_callback(struct device *dev, u32_t id, int error_code)
+DECL|rx_data|variable|rx_data
+DECL|test_main|function|void test_main(void)
+DECL|test_msgdma|function|void test_msgdma(void)
+DECL|tx_data|variable|tx_data
