d166a08c69dfaf81382575fd6b0fe70db16f5c3d
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-04-03 10:23:36 +0530

    arch: nios2: Add _nios2_dcache_flush_no_writeback() routine
    Add _nios2_dcache_flush_no_writeback() routine to invalidate
    cache contents of address provided.
    
    This routine is ported from Altera Nios-II SDK v17.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: 4073db79d709585d6ac853a18d276ada5ad2eac7

--- arch/nios2/core/cache.c
+++ arch/nios2/core/cache.c
@@ -1,2 +1,3 @@
 DECL|_nios2_dcache_flush_all|function|void _nios2_dcache_flush_all(void)
+DECL|_nios2_dcache_flush_no_writeback|function|void _nios2_dcache_flush_no_writeback(void *start, u32_t len)
 DECL|_nios2_icache_flush_all|function|void _nios2_icache_flush_all(void)
--- arch/nios2/include/kernel_arch_func.h
+++ arch/nios2/include/kernel_arch_func.h
@@ -2,6 +2,7 @@ DECL|_IntLibInit|function|static inline void _IntLibInit(void)
 DECL|_is_in_isr|macro|_is_in_isr
 DECL|_kernel_arch_func__h_|macro|_kernel_arch_func__h_
 DECL|_nios2_dcache_flush_all|macro|_nios2_dcache_flush_all
+DECL|_nios2_dcache_flush_no_writeback|macro|_nios2_dcache_flush_no_writeback
 DECL|_nios2_icache_flush_all|macro|_nios2_icache_flush_all
 DECL|_set_thread_return_value|function|_set_thread_return_value(struct k_thread *thread, unsigned int value)
 DECL|kernel_arch_init|function|static ALWAYS_INLINE void kernel_arch_init(void)
