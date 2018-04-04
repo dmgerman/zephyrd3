96726a9f1b94c4bbc60e8e91d58e43c85ef08018
Tomasz Jurtsch <tjurtsch@antmicro.com>
2018-04-04 22:11:07 +0200

    boards: SiFive HiFive1 board
    Added support for the HiFive1 board from SiFive
    
    Signed-off-by: Tomasz Michalak <tmichalak@antmicro.com>
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>
    Signed-off-by: Tomasz Jurtsch <tjurtsch@antmicro.com>
    
    Former-commit-id: 938a969926da4b082ac94f38b70bef66987bfb6b

---
+++ boards/riscv32/hifive1/clock.c
@@ -0,0 +1,1 @@
+DECL|hifive1_clock_init|function|static int hifive1_clock_init(struct device *dev)
---
+++ boards/riscv32/hifive1/pinmux.c
@@ -0,0 +1,1 @@
+DECL|hifive1_pinmux_init|function|static int hifive1_pinmux_init(struct device *dev)
