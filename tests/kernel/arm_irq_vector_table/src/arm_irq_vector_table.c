e666ef931996815cdb85fd7224594d7ace16d228
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-04-04 18:02:38 +0200

    tests: kernel: fix irq_vector_table test for nRF52X platforms
    This commit disables Bluetooth and adds a customized vector
    table for nRF52X-based platforms in the irq_vector_table kernel
    test, in order to prevent UsageFault during system timer
    interrupts, and, therefore, let the test execute properly.
    
    This commit fixes #6890.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: 316ffff6f22d69871d1f888eecbfb3b790bc507f

--- tests/kernel/arm_irq_vector_table/src/arm_irq_vector_table.c
+++ tests/kernel/arm_irq_vector_table/src/arm_irq_vector_table.c
@@ -1,7 +1,9 @@
 DECL|_irq_vector_table|variable|_irq_vector_table
+DECL|_irq_vector_table|variable|_irq_vector_table
 DECL|isr0|function|void isr0(void)
 DECL|isr1|function|void isr1(void)
 DECL|isr2|function|void isr2(void)
 DECL|sem|variable|sem
 DECL|test_arm_irq_vector_table|function|void test_arm_irq_vector_table(void)
+DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
 DECL|vth|typedef|typedef void (*vth)(void); /* Vector Table Handler */
