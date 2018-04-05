e44c4ef321be6ecde967f3c6b48d007fbb601d4e
Spoorthi K <spoorthi.k@intel.com>
2018-04-05 12:09:14 +0530

    tests: stack_random: Add Ztest support
    Add Ztest support for stack randomization test case.Ztest support for
    stack randomization test case.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: d155e88624e8c8ef0ede9f185f94c129f00c6a4c

--- tests/kernel/mem_protect/stack_random/src/main.c
+++ tests/kernel/mem_protect/stack_random/src/main.c
@@ -5,4 +5,5 @@ DECL|alt_thread_data|variable|alt_thread_data
 DECL|alternate_thread|function|void alternate_thread(void)
 DECL|changed|variable|changed
 DECL|last_sp|variable|last_sp
-DECL|main|function|void main(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_stack_pt_randomization|function|void test_stack_pt_randomization(void)
