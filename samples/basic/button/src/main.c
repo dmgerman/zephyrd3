1a890c81b6cf92cc13e4647838b38fc94b5b0297
Erwan Gouriou <erwan.gouriou@linaro.org>
2018-04-10 15:08:41 +0200

    samples: Allow use of "CONTROLLER" postfix for LED and GPIO
    Rework samples using LED and GPIO to enforce use of "CONTROLLER"
    as LED and PGIO postifx.
    Change impact all samples that could be run by boards yet moved to
    GPIO node generations, which is only STM32 for now
    
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    
    Former-commit-id: 162b6e4b35fa8473ccab21d99269ad2104141ee8

--- samples/basic/button/src/main.c
+++ samples/basic/button/src/main.c
@@ -5,6 +5,7 @@ DECL|PORT|macro|PORT
 DECL|PULL_UP|macro|PULL_UP
 DECL|PULL_UP|macro|PULL_UP
 DECL|SLEEP_TIME|macro|SLEEP_TIME
+DECL|SW0_GPIO_CONTROLLER|macro|SW0_GPIO_CONTROLLER
 DECL|button_pressed|function|void button_pressed(struct device *gpiob, struct gpio_callback *cb, u32_t pins)
 DECL|gpio_cb|variable|gpio_cb
 DECL|main|function|void main(void)
--- samples/subsys/usb/hid-mouse/src/main.c
+++ samples/subsys/usb/hid-mouse/src/main.c
@@ -3,6 +3,7 @@ DECL|MOUSE_BTN_MIDDLE|macro|MOUSE_BTN_MIDDLE
 DECL|MOUSE_BTN_RIGHT|macro|MOUSE_BTN_RIGHT
 DECL|PIN|macro|PIN
 DECL|PORT|macro|PORT
+DECL|SW0_GPIO_CONTROLLER|macro|SW0_GPIO_CONTROLLER
 DECL|button_pressed|function|void button_pressed(struct device *gpio, struct gpio_callback *cb, u32_t pins)
 DECL|def_val|variable|def_val
 DECL|get_report_cb|function|static int get_report_cb(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
