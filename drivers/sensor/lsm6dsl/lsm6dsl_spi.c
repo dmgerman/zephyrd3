e230bf3f5a35a43f24eec1e9cdb968c86b1f730b
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-04-10 18:13:38 +0200

    sensors/lsm5dsl: Fix SPI API usage
    The driver was not moved to the recent SPI API changes.
    
    Fixes #7009
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: a09a0b2c49758e5baa3b921d9093fa24567fc7b6

--- drivers/sensor/lsm6dsl/lsm6dsl_spi.c
+++ drivers/sensor/lsm6dsl/lsm6dsl_spi.c
@@ -1,8 +1,8 @@
 DECL|LSM6DSL_SPI_READ|macro|LSM6DSL_SPI_READ
 DECL|SPI_CS|macro|SPI_CS
 DECL|lsm6dsl_cs_ctrl|variable|lsm6dsl_cs_ctrl
-DECL|lsm6dsl_raw_read|function|static int lsm6dsl_raw_read(struct spi_config *spi_cfg, u8_t reg_addr, u8_t *value, u8_t len)
-DECL|lsm6dsl_raw_write|function|static int lsm6dsl_raw_write(struct spi_config *spi_cfg, u8_t reg_addr, u8_t *value, u8_t len)
+DECL|lsm6dsl_raw_read|function|static int lsm6dsl_raw_read(struct lsm6dsl_data *data, u8_t reg_addr, u8_t *value, u8_t len)
+DECL|lsm6dsl_raw_write|function|static int lsm6dsl_raw_write(struct lsm6dsl_data *data, u8_t reg_addr, u8_t *value, u8_t len)
 DECL|lsm6dsl_spi_conf|variable|lsm6dsl_spi_conf
 DECL|lsm6dsl_spi_init|function|int lsm6dsl_spi_init(struct device *dev)
 DECL|lsm6dsl_spi_read_data|function|static int lsm6dsl_spi_read_data(struct lsm6dsl_data *data, u8_t reg_addr, u8_t *value, u8_t len)
