91b63b536bd4bd1a8c1a564d8ca90934c9dab39f
Anas Nashif <anas.nashif@intel.com>
2018-04-12 14:01:27 -0500

    tests: stackprot: move to ztest
    Move test to use ztest instead of freestyle.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 910a569ea7b7f2a93582996ce220fb65dafd85aa

--- tests/kernel/mem_protect/stackprot/src/main.c
+++ tests/kernel/mem_protect/stackprot/src/main.c
@@ -4,6 +4,7 @@ DECL|alt_thread_data|variable|alt_thread_data
 DECL|alternate_thread|function|void alternate_thread(void)
 DECL|check_input|function|void check_input(const char *name, const char *input)
 DECL|count|variable|count
-DECL|main|function|void main(void)
 DECL|print_loop|function|void print_loop(const char *name)
 DECL|ret|variable|ret
+DECL|test_main|function|void test_main(void)
+DECL|test_stackprot|function|void test_stackprot(void)
