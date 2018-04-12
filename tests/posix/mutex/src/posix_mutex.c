0e4f8f3125adaa99f61812ea675d24ecaf69ee56
Punit Vara <punit.vara@intel.com>
2018-04-12 16:56:34 +0530

    tests: mutex: Add test case to test mutex API
    This test case tests posix mutex apis.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: f0eaa8e87904f972fad23a38abde9cfd94e402ea

---
+++ tests/posix/mutex/src/posix_mutex.c
@@ -0,0 +1,9 @@
+DECL|SLEEP_MS|macro|SLEEP_MS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|mutex1|variable|mutex1
+DECL|mutex2|variable|mutex2
+DECL|normal_mutex_entry|function|void *normal_mutex_entry(void *p1)
+DECL|recursive_mutex_entry|function|void *recursive_mutex_entry(void *p1)
+DECL|test_main|function|void test_main(void)
+DECL|test_mutex_normal|function|static void test_mutex_normal(void)
+DECL|test_recursive_mutex|function|static void test_recursive_mutex(void)
