82c56351c47caf644a00c4e35f07d0ffbbc3c27c
Iván Sánchez Ortega <ivan@sanchezortega.es>
2018-04-16 10:52:57 +0200

    samples: usb: webusb: Add MS-OS descriptors for compatible IDs
    This adds some vendor-specific microsoft USB descriptors, which
    makes the win8/win10 enumeration process to report a "WINUSB"
    capability, which in turn automatically binds a WinUSB.sys driver
    to a specific set of interfaces. This, in theory, makes userspace
    drivers easier to handle in win32/win64 platforms.
    
    Signed-off-by: Iván Sánchez Ortega <ivan@sanchezortega.es>
    
    Former-commit-id: 3b8c6c70e642cc73cb73b5dac59350179bf7e54c

--- samples/subsys/usb/webusb/src/main.c
+++ samples/subsys/usb/webusb/src/main.c
@@ -5,6 +5,7 @@ DECL|data_buf|variable|data_buf
 DECL|data_transmitted|variable|data_transmitted
 DECL|interrupt_handler|function|static void interrupt_handler(struct device *dev)
 DECL|main|function|void main(void)
+DECL|msos2_descriptor|variable|msos2_descriptor
 DECL|read_and_echo_data|function|static void read_and_echo_data(struct device *dev, int *bytes_read)
 DECL|req_handlers|variable|req_handlers
 DECL|vendor_handle_req|function|int vendor_handle_req(struct usb_setup_packet *pSetup,s32_t *len, u8_t **data)
