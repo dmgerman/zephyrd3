dc828c31e268a2b75fcc6f69e98ea913fd4dd9b1
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-04-18 11:54:35 +0300

    usb: Add Bluetooth device class core functionality
    Implement Bluetooth over USB functionality through Bluetooth raw
    access to the Bluetooth controller. Most devices with Bluetooth and
    USB controllers supported by Zephyr can export themselves as USB
    Bluetooth dongles.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 53410af994527519e6f9bdfe4a70222435774e60

---
+++ subsys/usb/class/bluetooth.c
@@ -0,0 +1,14 @@
+DECL|BT_BUF_ACL_SIZE|macro|BT_BUF_ACL_SIZE
+DECL|BT_L2CAP_MTU|macro|BT_L2CAP_MTU
+DECL|CMD_BUF_SIZE|macro|CMD_BUF_SIZE
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|bluetooth_bulk_out_callback|function|static void bluetooth_bulk_out_callback(u8_t ep,enum usb_dc_ep_cb_status_code ep_status)
+DECL|bluetooth_class_handler|function|static int bluetooth_class_handler(struct usb_setup_packet *setup, s32_t *len, u8_t **data)
+DECL|bluetooth_config|variable|bluetooth_config
+DECL|bluetooth_ep_data|variable|bluetooth_ep_data
+DECL|bluetooth_init|function|static int bluetooth_init(struct device *dev)
+DECL|bluetooth_status_cb|function|static void bluetooth_status_cb(enum usb_dc_status_code status, u8_t *param)
+DECL|bluetooth_thread_data|variable|bluetooth_thread_data
+DECL|bluetooth_thread|function|static void bluetooth_thread(void)
+DECL|interface_data|variable|interface_data
