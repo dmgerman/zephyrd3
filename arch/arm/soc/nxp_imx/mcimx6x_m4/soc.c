b6640b5b585e80c449cbceae2df2a36a737c20cc
Stanislav Poboril <stanislav.poboril@nxp.com>
2018-04-18 17:53:39 +0200

    arch: Add imx6sx m4 soc support
    The i.MX 6SoloX SoC is a hybrid multi-core processor composed by one
    Cortex A9 core and one Cortex M4 core.
    
    Zephyr was ported to run on the M4 core. In a later release, it will
    also communicate with the A9 core (running Linux) via RPMsg.
    
    The low level drivers come from NXP FreeRTOS BSP and are located at
    ext/hal/nxp/imx. More details can be found at ext/hal/nxp/imx/README
    
    The A9 core is responsible to load the M4 binary application into the
    RAM, put the M4 in reset, set the M4 Program Counter and Stack Pointer,
    and get the M4 out of reset.
    The A9 can perform these steps at bootloader level after the Linux
    system has booted.
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>
    
    Former-commit-id: 631eedd33480b2cfbfea61bab433a7c8bef178d4

---
+++ arch/arm/soc/nxp_imx/mcimx6x_m4/soc.c
@@ -0,0 +1,4 @@
+DECL|SOC_CacheInit|function|static void SOC_CacheInit(void)
+DECL|SOC_ClockInit|function|static void SOC_ClockInit(void)
+DECL|SOC_RdcInit|function|static void SOC_RdcInit(void)
+DECL|mcimx6x_m4_init|function|static int mcimx6x_m4_init(struct device *arg)
