b8f7a3bdbbddeb8dcacdd5870bcdcb44a3359489
Armando Visconti <armando.visconti@st.com>
2018-04-19 11:26:54 +0200

    samples/boards: Add ArgonKey test
    Test ArgonKey board. It provides an example of how to test
    all the sensors on-board. It currently enables by default
    following sensors:
    
       LPS22HB pressure/temp
       HTS221 humidity
       LSM6DSL accel/gyro plus LIS2MDL magn connected to it.
       VL53L0x proximity
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>
    
    Former-commit-id: 02bb83bec285e411799333cf2c12231e332099f2

---
+++ samples/boards/96b_argonkey/src/main.c
@@ -0,0 +1,6 @@
+DECL|WHOAMI_ALT_REG|macro|WHOAMI_ALT_REG
+DECL|WHOAMI_REG|macro|WHOAMI_REG
+DECL|lsm6dsl_trig_cnt|variable|lsm6dsl_trig_cnt
+DECL|lsm6dsl_trigger_handler|function|static void lsm6dsl_trigger_handler(struct device *dev, struct sensor_trigger *trig)
+DECL|main|function|void main(void)
+DECL|out_ev|function|static inline float out_ev(struct sensor_value *val)
