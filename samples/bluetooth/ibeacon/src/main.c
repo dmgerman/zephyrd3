14bb8220c7833e5388fec1065ab9081159c9588b
Henrik Brix Andersen <henrik@brixandersen.dk>
2018-04-21 21:25:16 +0200

    samples: bluetooth: Add Apple iBeacon demo application
    Add a simple demo implementation of an Apple iBeacon BLE broadcaster.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>
    
    Former-commit-id: b27d96850fae9b731a5579e50040fe55af0b090b

---
+++ samples/bluetooth/ibeacon/src/main.c
@@ -0,0 +1,7 @@
+DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
+DECL|DEVICE_NAME|macro|DEVICE_NAME
+DECL|IBEACON_RSSI|macro|IBEACON_RSSI
+DECL|ad|variable|ad
+DECL|bt_ready|function|static void bt_ready(int err)
+DECL|main|function|void main(void)
+DECL|sd|variable|sd
