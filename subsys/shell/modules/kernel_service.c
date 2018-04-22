968bd75b95c1b0a2073de57e2e478f08881701d1
Henrik Brix Andersen <henrik@brixandersen.dk>
2018-04-22 22:41:36 +0200

    shell: kernel: Add reboot command
    Add a reboot command for requesting a warm or cold system reboot
    through the kernel shell.
    
    Signed-off-by: Henrik Brix Andersen <henrik@brixandersen.dk>
    
    Former-commit-id: 83ac3e24d82967eafef3f50da9eee6108a5cc593

--- subsys/shell/modules/kernel_service.c
+++ subsys/shell/modules/kernel_service.c
@@ -1,6 +1,7 @@
 DECL|SHELL_KERNEL|macro|SHELL_KERNEL
 DECL|kernel_commands|variable|kernel_commands
 DECL|shell_cmd_cycles|function|static int shell_cmd_cycles(int argc, char *argv[])
+DECL|shell_cmd_reboot|function|static int shell_cmd_reboot(int argc, char *argv[])
 DECL|shell_cmd_stack|function|static int shell_cmd_stack(int argc, char *argv[])
 DECL|shell_cmd_threads|function|static int shell_cmd_threads(int argc, char *argv[])
 DECL|shell_cmd_uptime|function|static int shell_cmd_uptime(int argc, char *argv[])
