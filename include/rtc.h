c4611d415505a51d9eb7f159fa189f7747e6915d
Andy Gross <agross@kernel.org>
2018-04-24 13:27:59 -0500

    rtc: Add prescalar configuration option
    This patch adds a prescaler configuration option that denotes the
    number of RTC ticks per second.  This is used to calculate the value
    for 1 second.
    
    Signed-off-by: Andy Gross <agross@kernel.org>
    
    Former-commit-id: 27bdb83308301a1dbe3db9e1fbaf6560facda654

--- include/rtc.h
+++ include/rtc.h
@@ -2,23 +2,6 @@ DECL|RTC_ALARM_DAY|macro|RTC_ALARM_DAY
 DECL|RTC_ALARM_HOUR|macro|RTC_ALARM_HOUR
 DECL|RTC_ALARM_MINUTE|macro|RTC_ALARM_MINUTE
 DECL|RTC_ALARM_SECOND|macro|RTC_ALARM_SECOND
-DECL|RTC_CLK_DIV_1024|enumerator|RTC_CLK_DIV_1024,
-DECL|RTC_CLK_DIV_128|enumerator|RTC_CLK_DIV_128,
-DECL|RTC_CLK_DIV_16384|enumerator|RTC_CLK_DIV_16384,
-DECL|RTC_CLK_DIV_16|enumerator|RTC_CLK_DIV_16,
-DECL|RTC_CLK_DIV_1|enumerator|RTC_CLK_DIV_1,
-DECL|RTC_CLK_DIV_2048|enumerator|RTC_CLK_DIV_2048,
-DECL|RTC_CLK_DIV_256|enumerator|RTC_CLK_DIV_256,
-DECL|RTC_CLK_DIV_2|enumerator|RTC_CLK_DIV_2,
-DECL|RTC_CLK_DIV_32768|enumerator|RTC_CLK_DIV_32768
-DECL|RTC_CLK_DIV_32|enumerator|RTC_CLK_DIV_32,
-DECL|RTC_CLK_DIV_4096|enumerator|RTC_CLK_DIV_4096,
-DECL|RTC_CLK_DIV_4|enumerator|RTC_CLK_DIV_4,
-DECL|RTC_CLK_DIV_512|enumerator|RTC_CLK_DIV_512,
-DECL|RTC_CLK_DIV_64|enumerator|RTC_CLK_DIV_64,
-DECL|RTC_CLK_DIV_8192|enumerator|RTC_CLK_DIV_8192,
-DECL|RTC_CLK_DIV_8|enumerator|RTC_CLK_DIV_8,
-DECL|RTC_DIVIDER|macro|RTC_DIVIDER
 DECL|_RTC_H_|macro|_RTC_H_
 DECL|_impl_rtc_disable|function|static inline void _impl_rtc_disable(struct device *dev)
 DECL|_impl_rtc_enable|function|static inline void _impl_rtc_enable(struct device *dev)
@@ -28,7 +11,6 @@ DECL|_impl_rtc_set_alarm|function|static inline int _impl_rtc_set_alarm(struct d
 DECL|alarm_enable|member|u8_t alarm_enable;
 DECL|alarm_val|member|u32_t alarm_val;
 DECL|cb_fn|member|void (*cb_fn)(struct device *dev);
-DECL|clk_rtc_div|enum|enum clk_rtc_div {
 DECL|disable|member|rtc_api_disable disable;
 DECL|enable|member|rtc_api_enable enable;
 DECL|get_pending_int|member|rtc_api_get_pending_int get_pending_int;
