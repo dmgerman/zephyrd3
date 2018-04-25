ef3d010f0febfb496132410384bb2aea4f3cfa76
Pushpal Sidhu <psidhu.devel@gmail.com>
2018-04-25 13:42:42 -0700

    arch: stm32l4: add HAL_Delay for hal library hooks
    Hook required for hal library to delay (in ms)
    
    Signed-off-by: Pushpal Sidhu <psidhu.devel@gmail.com>
    
    Former-commit-id: 816e3d876755f027f016042fd9e7c7461b92ad89

--- arch/arm/soc/st_stm32/stm32l4/soc.c
+++ arch/arm/soc/st_stm32/stm32l4/soc.c
@@ -1,2 +1,3 @@
+DECL|HAL_Delay|function|void HAL_Delay(__IO uint32_t Delay)
 DECL|HAL_GetTick|function|uint32_t HAL_GetTick(void)
 DECL|stm32l4_init|function|static int stm32l4_init(struct device *arg)
