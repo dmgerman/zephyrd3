c2a4225ab78f524bbc3a462179a7ba626b17f4db
Johan Hedberg <johan.hedberg@intel.com>
2018-04-26 22:05:26 +0300

    Bluetooth: Introduce skeleton for settings-based storage
    Introduce a basic skeleton for peristent storage based on the settings
    subsystem. Also enable support for this to the peripheral sample
    application, so the new code gets exersized by CI. For now, the
    implementation provides the same level support as the bt_storage API
    ever did, i.e. for the identity address and the IRK.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: b997a283f7ec3d13728b7ca9c92258cd3bcac3e0

---
+++ subsys/bluetooth/host/settings.c
@@ -0,0 +1,7 @@
+DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|bt_settings_init|function|int bt_settings_init(void)
+DECL|bt_settings|variable|bt_settings
+DECL|commit|function|static int commit(void)
+DECL|generate_irk|function|static void generate_irk(void)
+DECL|generate_static_addr|function|static void generate_static_addr(void)
+DECL|set|function|static int set(int argc, char **argv, char *val)
