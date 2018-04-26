39a34c0c125f5efb7b3d3709504f6eab58649817
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-04-26 14:02:00 +0200

    tests: subsys: settings: Add FCB-beckend initialization test
    Added test for initialization with FCB backend.
    This test should goes over reset so it test blank and
    subsequent initialization and data restoring capability
    after the reset.
    
    
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    
    Former-commit-id: 3298d60ff94d2f90d1dbb0f6c3cd28bc056b7ad8

---
+++ tests/subsys/settings/fcb_init/src/settings_test_fcb_init.c
@@ -0,0 +1,7 @@
+DECL|c1_export|function|static int c1_export(int (*export_func)(const char *name, char *val),enum settings_export_tgt tgt)
+DECL|c1_settings|variable|c1_settings
+DECL|c1_set|function|static int c1_set(int argc, char **argv, char *val)
+DECL|test_init_setup|function|void test_init_setup(void)
+DECL|test_init|function|void test_init(void)
+DECL|test_main|function|void test_main(void)
+DECL|val32|variable|val32
