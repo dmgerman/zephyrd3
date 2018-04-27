9223d29ad1ed8c5fff288d4202346f23e98bdbea
Johan Hedberg <johan.hedberg@intel.com>
2018-04-27 13:24:02 +0300

    Bluetooth: settings: Add support for per-submodule handlers
    Add a new linker section for a list of submodule settings handlers,
    and iterate the list from the various settings callbacks.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 470349c25a7c0604de02cf0585bb3d8cc5b6463f

--- subsys/bluetooth/host/settings.c
+++ subsys/bluetooth/host/settings.c
@@ -2,6 +2,7 @@ DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
 DECL|bt_settings_init|function|int bt_settings_init(void)
 DECL|bt_settings|variable|bt_settings
 DECL|commit|function|static int commit(void)
+DECL|export|function|static int export(int (*func)(const char *name, char *val), enum settings_export_tgt tgt)
 DECL|generate_irk|function|static void generate_irk(void)
 DECL|generate_static_addr|function|static void generate_static_addr(void)
 DECL|set|function|static int set(int argc, char **argv, char *val)
--- subsys/bluetooth/host/settings.h
+++ subsys/bluetooth/host/settings.h
@@ -1,1 +1,6 @@
-
+DECL|BT_SETTINGS_DEFINE|macro|BT_SETTINGS_DEFINE
+DECL|bt_settings_handler|struct|struct bt_settings_handler {
+DECL|commit|member|int (*commit)(void);
+DECL|export|member|int (*export)(int (*func)(const char *name, char *val));
+DECL|name|member|const char *name;
+DECL|set|member|int (*set)(int argc, char **argv, char *val);
