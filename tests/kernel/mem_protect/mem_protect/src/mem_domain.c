0cc73764146ba062461db1d831b9e70f909052ec
Spoorthi K <spoorthi.k@intel.com>
2018-04-27 23:26:03 +0530

    tests: mem_protect: Add memory domain testcases
    Add few tests to check access permissions of memory partitions
    of a memory domain, validate memory domain destroy.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: 79a0fa68e3cbbc5b3ddd480a6ec49e4f42993ed3

--- tests/kernel/mem_protect/mem_protect/src/mem_domain.c
+++ tests/kernel/mem_protect/mem_protect/src/mem_domain.c
@@ -1,7 +1,9 @@
 DECL|ERROR_STR|macro|ERROR_STR
+DECL|mem_domain1|variable|mem_domain1
 DECL|mem_domain_1_tid|variable|mem_domain_1_tid
 DECL|mem_domain_2_tid|variable|mem_domain_2_tid
 DECL|mem_domain_6_tid|variable|mem_domain_6_tid
+DECL|mem_domain_buf1|variable|mem_domain_buf1
 DECL|mem_domain_buf|variable|mem_domain_buf
 DECL|mem_domain_for_user_tc3|function|void mem_domain_for_user_tc3(void *max_partitions, void *p2, void *p3)
 DECL|mem_domain_for_user_tc4|function|void mem_domain_for_user_tc4(void *max_partitions, void *p2, void *p3)
@@ -10,6 +12,7 @@ DECL|mem_domain_for_user_tc7|function|void mem_domain_for_user_tc7(void *p1, voi
 DECL|mem_domain_for_user|function|void mem_domain_for_user(void *tc_number, void *p2, void *p3)
 DECL|mem_domain_init|function|static inline void mem_domain_init(void)
 DECL|mem_domain_mem_domain|variable|mem_domain_mem_domain
+DECL|mem_domain_memory_partition_array1|variable|mem_domain_memory_partition_array1
 DECL|mem_domain_memory_partition_array|variable|mem_domain_memory_partition_array
 DECL|mem_domain_tc3_mem_domain|variable|mem_domain_tc3_mem_domain
 DECL|mem_domain_tc3_part1|variable|mem_domain_tc3_part1
@@ -27,8 +30,14 @@ DECL|mem_domain_test_6_2|function|void mem_domain_test_6_2(void *p1, void *p2, v
 DECL|set_valid_fault_value|function|static inline void set_valid_fault_value(int test_case_number)
 DECL|test_mem_domain_add_partitions_invalid|function|void test_mem_domain_add_partitions_invalid(void *p1, void *p2, void *p3)
 DECL|test_mem_domain_add_partitions_simple|function|void test_mem_domain_add_partitions_simple(void *p1, void *p2, void *p3)
+DECL|test_mem_domain_destroy|function|void test_mem_domain_destroy(void)
 DECL|test_mem_domain_invalid_access|function|void test_mem_domain_invalid_access(void *p1, void *p2, void *p3)
+DECL|test_mem_domain_partitions_supervisor_rw|function|void test_mem_domain_partitions_supervisor_rw(void)
+DECL|test_mem_domain_partitions_user_ro|function|void test_mem_domain_partitions_user_ro(void)
+DECL|test_mem_domain_partitions_user_rw|function|void test_mem_domain_partitions_user_rw(void)
 DECL|test_mem_domain_remove_partitions_simple|function|void test_mem_domain_remove_partitions_simple(void *p1, void *p2, void *p3)
 DECL|test_mem_domain_remove_partitions|function|void test_mem_domain_remove_partitions(void *p1, void *p2, void *p3)
 DECL|test_mem_domain_remove_thread|function|void test_mem_domain_remove_thread(void *p1, void *p2, void *p3)
 DECL|test_mem_domain_valid_access|function|void test_mem_domain_valid_access(void *p1, void *p2, void *p3)
+DECL|thread_entry_rw|function|static void thread_entry_rw(void *p1, void *p2, void *p3)
+DECL|user_thread_entry_ro|function|static void user_thread_entry_ro(void *p1, void *p2, void *p3)
