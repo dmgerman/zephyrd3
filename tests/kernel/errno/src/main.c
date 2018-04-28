6c15e9375ed9288080f302f806e2d7a38ba4de45
Anas Nashif <anas.nashif@intel.com>
2018-04-28 13:20:09 +0530

    tests: errno: document test functions
    Use doxygen to document test and cleanup test.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 67194a40ef5026e8d86bd5dccff6bc395359ac55

--- tests/kernel/errno/src/main.c
+++ tests/kernel/errno/src/main.c
@@ -1,1 +1,12 @@
+DECL|N_THREADS|macro|N_THREADS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|errno_thread|function|static void errno_thread(int n, int my_errno)
+DECL|errno_values|variable|errno_values
+DECL|fifo|variable|fifo
+DECL|pass|member|int pass;
+DECL|q|member|void *q;
+DECL|result|struct|struct result {
+DECL|result|variable|result
 DECL|test_main|function|void test_main(void)
+DECL|test_thread_context|function|void test_thread_context(void)
+DECL|threads|variable|threads
