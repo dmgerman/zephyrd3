64ad2b9e049340abb6c58b79ad64e29de19bf345
Anas Nashif <anas.nashif@intel.com>
2018-04-28 12:48:36 +0530

    tests: irq_offload: document test functions
    Use doxygen to document test and cleanup test.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 2182a53fecc23a4fa3626bdcf915dc5d0405b991

--- tests/kernel/irq_offload/src/irq_offload.c
+++
@@ -1,4 +0,0 @@
-DECL|SENTINEL_VALUE|macro|SENTINEL_VALUE
-DECL|offload_function|function|static void offload_function(void *param)
-DECL|sentinel|variable|sentinel
-DECL|test_irq_offload|function|void test_irq_offload(void)
--- tests/kernel/irq_offload/src/main.c
+++ tests/kernel/irq_offload/src/main.c
@@ -1,1 +1,5 @@
+DECL|SENTINEL_VALUE|macro|SENTINEL_VALUE
+DECL|offload_function|function|static void offload_function(void *param)
+DECL|sentinel|variable|sentinel
+DECL|test_irq_offload|function|void test_irq_offload(void)
 DECL|test_main|function|void test_main(void)
