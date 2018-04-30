a671d1a32f8c571b9a84f408c370f9635473d1f3
Andrew Boie <andrew.p.boie@intel.com>
2018-04-30 16:11:54 -0700

    list_gen: slist: mark some APIs are private
    The new APIs added are used behind the scenes by list_gen.h
    to abstract away details on how lists and nodes work.
    
    Name them with z_ prefix as they are not intended for use
    by applications.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 79215adceb0b0a1abb5f4d9660ace5c46bd69ee3

--- include/misc/slist.h
+++ include/misc/slist.h
@@ -15,13 +15,13 @@ DECL|head|member|sys_snode_t *head;
 DECL|next|member|struct _snode *next;
 DECL|slist|variable|slist
 DECL|slist|variable|slist
-DECL|sys_slist_head_set|function|static inline void sys_slist_head_set(sys_slist_t *list, sys_snode_t *node)
 DECL|sys_slist_init|function|static inline void sys_slist_init(sys_slist_t *list)
 DECL|sys_slist_peek_head|function|static inline sys_snode_t *sys_slist_peek_head(sys_slist_t *list)
 DECL|sys_slist_peek_tail|function|static inline sys_snode_t *sys_slist_peek_tail(sys_slist_t *list)
-DECL|sys_slist_tail_set|function|static inline void sys_slist_tail_set(sys_slist_t *list, sys_snode_t *node)
 DECL|sys_slist_t|typedef|typedef struct _slist sys_slist_t;
-DECL|sys_snode_next_peek|function|static inline sys_snode_t *sys_snode_next_peek(sys_snode_t *node)
-DECL|sys_snode_next_set|function|static inline void sys_snode_next_set(sys_snode_t *parent, sys_snode_t *child)
 DECL|sys_snode_t|typedef|typedef struct _snode sys_snode_t;
 DECL|tail|member|sys_snode_t *tail;
+DECL|z_slist_head_set|function|static inline void z_slist_head_set(sys_slist_t *list, sys_snode_t *node)
+DECL|z_slist_tail_set|function|static inline void z_slist_tail_set(sys_slist_t *list, sys_snode_t *node)
+DECL|z_snode_next_peek|function|static inline sys_snode_t *z_snode_next_peek(sys_snode_t *node)
+DECL|z_snode_next_set|function|static inline void z_snode_next_set(sys_snode_t *parent, sys_snode_t *child)
