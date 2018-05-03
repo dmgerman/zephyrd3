cf7a8620ed6700137a3c14c031036ee82b48cf6d
Alexander Wachter <alexander.wachter@student.tugraz.at>
2018-05-03 10:52:35 +0200

    drivers: can: Add syscall handlers for CAN API
    This commit adds generic syscall handlers for the CAN API
    
    Signed-off-by: Alexander Wachter <alexander.wachter@student.tugraz.at>
    
    Former-commit-id: d3101b1fa4117ec9e475b9db4a5baf15d610857d

---
+++ drivers/can/can_handlers.c
@@ -0,0 +1,5 @@
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(can_attach_isr, dev, isr, filter) {
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(can_attach_msgq, dev, msgq, filter) {
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(can_configure, dev, mode, bitrate) {
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(can_detach, dev, filter_id) {
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(can_send, dev, msg) {
