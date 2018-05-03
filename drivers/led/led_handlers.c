0db660ca4a047e52cefcaa79b407b0f60363d32c
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2018-05-03 23:46:58 +0530

    drivers: led: Add system call handler support
    Add system call handler support to LED subsystem. No buffers are
    involved in any of the API's and hence the syscall support is
    straightforward.
    
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
    
    Former-commit-id: 26babfc622e6044e03ca82f2b398356b94166ff0

---
+++ drivers/led/led_handlers.c
@@ -0,0 +1,4 @@
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(led_blink, dev, led, delay_on, delay_off)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(led_off, dev, led)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(led_on, dev, led)
+DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(led_set_brightness, dev, led, value)
