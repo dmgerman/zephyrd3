6e2202a580e31d620debfcd501f9289b088dc1e8
Anas Nashif <anas.nashif@intel.com>
2018-05-05 14:06:42 -0500

    tests: neighbor: use ztest asserts
    Use ztest macro and convert to ztest proper.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: efb52c5bc09904720eb43b4c02ba572e2a97b8d6

--- tests/net/neighbor/src/main.c
+++ tests/net/neighbor/src/main.c
@@ -9,6 +9,5 @@ DECL|hwaddr5|variable|hwaddr5
 DECL|net_neighbor_data_remove|function|static void net_neighbor_data_remove(struct net_nbr *nbr)
 DECL|net_neighbor_table_clear|function|static void net_neighbor_table_clear(struct net_nbr_table *table)
 DECL|remove_count|variable|remove_count
-DECL|run_tests|function|static bool run_tests(void)
 DECL|test_main|function|void test_main(void)
-DECL|test_neighbor|function|void test_neighbor(void)
+DECL|test_neighbor|function|static void test_neighbor(void)
