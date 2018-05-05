b1d2bbd9c349469bf0877016da35ba64e3990c7f
Anas Nashif <anas.nashif@intel.com>
2018-05-05 15:16:01 -0500

    tests: ipv6: convert to ztest
    Use ztest instead of custom routine to iterate over tests.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: ee6b8761cd1688d796452988fb97a39cbbb0df77

--- tests/net/ipv6/src/main.c
+++ tests/net/ipv6/src/main.c
@@ -7,37 +7,34 @@ DECL|WAIT_TIME_LONG|macro|WAIT_TIME_LONG
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
 DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
-DECL|func|member|bool (*func)(void);
 DECL|icmpv6_ns_invalid|variable|icmpv6_ns_invalid
 DECL|icmpv6_ns_no_sllao|variable|icmpv6_ns_no_sllao
 DECL|icmpv6_ra|variable|icmpv6_ra
 DECL|ipv6_hbho|variable|ipv6_hbho
 DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
-DECL|main|function|void main(void)
 DECL|mcast_addr|variable|mcast_addr
 DECL|my_addr|variable|my_addr
-DECL|name|member|const char *name;
-DECL|net_test_add_neighbor|function|static bool net_test_add_neighbor(void)
-DECL|net_test_change_ll_addr|function|static bool net_test_change_ll_addr(void)
-DECL|net_test_cmp_prefix|function|static bool net_test_cmp_prefix(void)
 DECL|net_test_data|variable|net_test_data
 DECL|net_test_dev_init|function|int net_test_dev_init(struct device *dev)
 DECL|net_test_get_mac|function|static u8_t *net_test_get_mac(struct device *dev)
-DECL|net_test_hbho_message|function|static bool net_test_hbho_message(void)
 DECL|net_test_if_api|variable|net_test_if_api
 DECL|net_test_iface_init|function|static void net_test_iface_init(struct net_if *iface)
 DECL|net_test_ipv6|struct|struct net_test_ipv6 {
-DECL|net_test_nbr_lookup_fail|function|static bool net_test_nbr_lookup_fail(void)
-DECL|net_test_nbr_lookup_ok|function|static bool net_test_nbr_lookup_ok(void)
-DECL|net_test_prefix_timeout|function|static bool net_test_prefix_timeout(void)
-DECL|net_test_ra_message|function|static bool net_test_ra_message(void)
-DECL|net_test_send_ns_extra_options|function|static bool net_test_send_ns_extra_options(void)
-DECL|net_test_send_ns_no_options|function|static bool net_test_send_ns_no_options(void)
 DECL|peer_addr|variable|peer_addr
 DECL|prepare_ra_message|function|static struct net_pkt *prepare_ra_message(void)
+DECL|test_add_neighbor|function|static void test_add_neighbor(void)
+DECL|test_change_ll_addr|function|static void test_change_ll_addr(void)
+DECL|test_cmp_prefix|function|static void test_cmp_prefix(void)
 DECL|test_failed|variable|test_failed
-DECL|test_init|function|static bool test_init(void)
+DECL|test_hbho_message|function|static void test_hbho_message(void)
+DECL|test_init|function|static void test_init(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_nbr_lookup_fail|function|static void test_nbr_lookup_fail(void)
+DECL|test_nbr_lookup_ok|function|static void test_nbr_lookup_ok(void)
+DECL|test_prefix_timeout|function|static void test_prefix_timeout(void)
+DECL|test_ra_message|function|static void test_ra_message(void)
+DECL|test_send_ns_extra_options|function|static void test_send_ns_extra_options(void)
+DECL|test_send_ns_no_options|function|static void test_send_ns_no_options(void)
 DECL|tester_send|function|static int tester_send(struct net_if *iface, struct net_pkt *pkt)
-DECL|tests|variable|tests
 DECL|wait_data|variable|wait_data
