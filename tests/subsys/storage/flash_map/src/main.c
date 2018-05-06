d7b3f2887673f332990374bcd12166015efaccea
Anas Nashif <anas.nashif@intel.com>
2018-05-05 19:10:03 -0500

    tests: flash_map: use proper test name
    Use a meaningful name for the test.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 8603f7a4ce9dd3d5ef78e8d3947f574ba876522d

--- tests/subsys/storage/flash_map/src/main.c
+++ tests/subsys/storage/flash_map/src/main.c
@@ -2,6 +2,6 @@ DECL|FLASH_AREA_BOOTLOADER|macro|FLASH_AREA_BOOTLOADER
 DECL|FLASH_AREA_IMAGE_0|macro|FLASH_AREA_IMAGE_0
 DECL|FLASH_AREA_IMAGE_1|macro|FLASH_AREA_IMAGE_1
 DECL|FLASH_AREA_IMAGE_SCRATCH|macro|FLASH_AREA_IMAGE_SCRATCH
-DECL|flash_map_test_case_2|function|void flash_map_test_case_2(void)
 DECL|fs_sectors|variable|fs_sectors
+DECL|test_flash_area_to_sectors|function|void test_flash_area_to_sectors(void)
 DECL|test_main|function|void test_main(void)
