37892862a3ece485357679f24415d930a5257826
Johan Hedberg <johan.hedberg@intel.com>
2018-05-07 14:14:02 +0300

    Bluetooth: Mesh: Remove redundant 'provisioned' variable
    This variable is both redundant as well as problematic when it comes
    to adding persistent-storage-based provisioning information, which
    will not come through main.c or the bt_mesh_provision() API. Just
    remove it and use bt_mesh.valid which serves the same purpose in
    practice.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 3f30d12ce5428cb0a384ea49d50296aece5ac08d

--- subsys/bluetooth/host/mesh/main.c
+++ subsys/bluetooth/host/mesh/main.c
@@ -5,4 +5,3 @@ DECL|bt_mesh_prov_disable|function|int bt_mesh_prov_disable(bt_mesh_prov_bearer_
 DECL|bt_mesh_prov_enable|function|int bt_mesh_prov_enable(bt_mesh_prov_bearer_t bearers)
 DECL|bt_mesh_provision|function|int bt_mesh_provision(const u8_t net_key[16], u16_t net_idx, u8_t flags, u32_t iv_index, u32_t seq, u16_t addr, const u8_t dev_key[16])
 DECL|bt_mesh_reset|function|void bt_mesh_reset(void)
-DECL|provisioned|variable|provisioned
