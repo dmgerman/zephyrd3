87e1d5b325b172f7455d7bf25ba9b48faa6a4afc
Johan Hedberg <johan.hedberg@intel.com>
2018-05-08 23:34:56 +0300

    Bluetooth: Mesh: Introduce generic storage timer
    Instead of having an RPL-specific storage timer, introduce a generic
    one that'll eventually be used for all persistent storage.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 9e2189c4c1096c0dd88dc40f3eba5907a54ba986

--- subsys/bluetooth/host/mesh/settings.c
+++ subsys/bluetooth/host/mesh/settings.c
@@ -6,23 +6,31 @@ DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|app_key_set|function|static int app_key_set(int argc, char **argv, char *val)
 DECL|app_key_val|struct|struct app_key_val {
+DECL|app_key|member|app_key:1, /* 1 if this is an AppKey, 0 if a NetKey */
 DECL|bt_mesh_clear_app_key|function|void bt_mesh_clear_app_key(struct bt_mesh_app_key *key)
 DECL|bt_mesh_clear_net|function|void bt_mesh_clear_net(void)
 DECL|bt_mesh_clear_rpl|function|void bt_mesh_clear_rpl(void)
 DECL|bt_mesh_clear_subnet|function|void bt_mesh_clear_subnet(struct bt_mesh_subnet *sub)
 DECL|bt_mesh_settings_init|function|void bt_mesh_settings_init(void)
-DECL|bt_mesh_store_app_key|function|void bt_mesh_store_app_key(struct bt_mesh_app_key *app)
+DECL|bt_mesh_store_app_key|function|void bt_mesh_store_app_key(struct bt_mesh_app_key *key)
 DECL|bt_mesh_store_iv|function|void bt_mesh_store_iv(void)
-DECL|bt_mesh_store_net|function|void bt_mesh_store_net(u16_t primary_addr, const u8_t dev_key[16])
+DECL|bt_mesh_store_net|function|void bt_mesh_store_net(void)
 DECL|bt_mesh_store_rpl|function|void bt_mesh_store_rpl(struct bt_mesh_rpl *entry)
 DECL|bt_mesh_store_seq|function|void bt_mesh_store_seq(void)
 DECL|bt_mesh_store_subnet|function|void bt_mesh_store_subnet(struct bt_mesh_subnet *sub)
+DECL|clear_app_key|function|static void clear_app_key(u16_t app_idx)
+DECL|clear_net_key|function|static void clear_net_key(u16_t net_idx)
+DECL|clear|member|clear:1; /* 1 if key needs clearing, 0 if storing */
 DECL|dev_key|member|u8_t dev_key[16];
 DECL|func|member|int (*func)(int argc, char **argv, char *val);
 DECL|iv_index|member|u32_t iv_index;
 DECL|iv_set|function|static int iv_set(int argc, char **argv, char *val)
 DECL|iv_update|member|bool iv_update;
 DECL|iv_val|struct|struct iv_val {
+DECL|key_idx|member|u16_t key_idx:12, /* AppKey or NetKey Index */
+DECL|key_update_find|function|static struct key_update *key_update_find(bool app_key, u16_t key_idx, struct key_update **free_slot)
+DECL|key_updates|variable|key_updates
+DECL|key_update|struct|static struct key_update {
 DECL|kr_flag|member|u8_t kr_flag:1,
 DECL|kr_phase|member|kr_phase:7;
 DECL|mesh_commit|function|static int mesh_commit(void)
@@ -41,15 +49,23 @@ DECL|rpl_alloc|function|static struct bt_mesh_rpl *rpl_alloc(u16_t src)
 DECL|rpl_find|function|static struct bt_mesh_rpl *rpl_find(u16_t src)
 DECL|rpl_set|function|static int rpl_set(int argc, char **argv, char *val)
 DECL|rpl_val|struct|struct rpl_val {
+DECL|schedule_store|function|static void schedule_store(int flag)
 DECL|seq_set|function|static int seq_set(int argc, char **argv, char *val)
 DECL|seq_val|struct|struct seq_val {
 DECL|seq|member|u32_t seq:24,
 DECL|settings|variable|settings
+DECL|store_app_key|function|static void store_app_key(struct bt_mesh_app_key *app)
+DECL|store_net_key|function|static void store_net_key(struct bt_mesh_subnet *sub)
+DECL|store_pending_iv|function|static void store_pending_iv(void)
+DECL|store_pending_keys|function|static void store_pending_keys(void)
+DECL|store_pending_net|function|static void store_pending_net(void)
 DECL|store_pending_rpl|function|static void store_pending_rpl(void)
+DECL|store_pending_seq|function|static void store_pending_seq(void)
 DECL|store_pending|function|static void store_pending(struct k_work *work)
 DECL|store_rpl|function|static void store_rpl(struct bt_mesh_rpl *entry)
 DECL|subnet_init|function|static int subnet_init(struct bt_mesh_subnet *sub)
 DECL|updated|member|bool updated;
+DECL|valid|member|valid:1, /* 1 if this entry is valid, 0 if not */
 DECL|val|member|u8_t val[2][16];
 DECL|val|member|u8_t val[2][16];
 DECL|val|member|u8_t val[3];
