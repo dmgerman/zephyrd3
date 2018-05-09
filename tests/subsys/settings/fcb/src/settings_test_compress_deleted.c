5a44253567b0ffd3db01c86b9d9885fa7d09f158
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2018-05-09 15:31:06 +0200

    tests/subsys/settings/fcb: add deleted settings compression test
    New test for check whether deleted item is not copied to the newest
    fcb sector during compression procedure.
    
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    
    Former-commit-id: 3be0c56d50ec4eca188be45e6ce35b952a381935

---
+++ tests/subsys/settings/fcb/src/settings_test_compress_deleted.c
@@ -0,0 +1,11 @@
+DECL|NAME_DELETABLE|macro|NAME_DELETABLE
+DECL|c4_handle_export|function|int c4_handle_export(int (*cb)(const char *name, char *value), enum settings_export_tgt tgt)
+DECL|c4_test_handler|variable|c4_test_handler
+DECL|check_compressed_cb|function|static int check_compressed_cb(struct fcb_entry_ctx *entry_ctx, void *arg)
+DECL|deletable_s|struct|struct deletable_s {
+DECL|deletable_val|variable|deletable_val
+DECL|fcb_small_sectors|variable|fcb_small_sectors
+DECL|test_config_compress_deleted|function|void test_config_compress_deleted(void)
+DECL|val32|member|u32_t val32;
+DECL|val4v2|variable|val4v2
+DECL|valid|member|bool valid;
