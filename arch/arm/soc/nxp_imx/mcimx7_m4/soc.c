edcf22e8b75405609fd5bbd3d07deacef912bb2c
Diego Sueiro <diego.sueiro@gmail.com>
2018-05-11 22:13:59 +0100

    arch: Add imx7d_m4 i2c definitions
    Adds all necessary i2c definitions and configurations for imx7d_m4 soc.
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>
    
    Former-commit-id: 970c4f9cf3a0a6a4a5291700840d18bdd0b41e1c

--- arch/arm/soc/nxp_imx/mcimx7_m4/soc.c
+++ arch/arm/soc/nxp_imx/mcimx7_m4/soc.c
@@ -1,5 +1,6 @@
 DECL|SOC_ClockInit|function|void SOC_ClockInit(void)
 DECL|SOC_RdcInit|function|void SOC_RdcInit(void)
 DECL|nxp_mcimx7_gpio_config|function|static void nxp_mcimx7_gpio_config(void)
+DECL|nxp_mcimx7_i2c_config|function|static void nxp_mcimx7_i2c_config(void)
 DECL|nxp_mcimx7_init|function|static int nxp_mcimx7_init(struct device *arg)
 DECL|nxp_mcimx7_uart_config|function|static void nxp_mcimx7_uart_config(void)
