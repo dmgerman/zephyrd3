1461eef385b50eb4cf6125301b13a7002962ce01
Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
2018-05-14 09:08:54 +0200

    drivers: watchdog: Add shim for nrfx WDT driver
    Changes add a translation layer to make nrfx WDT driver works
    with Zephyr API.
    
    Signed-off-by: Karol Lasończyk <karol.lasonczyk@nordicsemi.no>
    
    Former-commit-id: 30a24e8ad0f4f90648d4d5cdbd15b89dbdf5aab8

---
+++ drivers/watchdog/wdt_nrfx.c
@@ -0,0 +1,18 @@
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|init_wdt|function|static int init_wdt(struct device *dev)
+DECL|m_allocated_channels|variable|m_allocated_channels
+DECL|m_callbacks|variable|m_callbacks
+DECL|m_timeout|variable|m_timeout
+DECL|wdt_event_handler|function|static void wdt_event_handler(void)
+DECL|wdt_nrf_api|variable|wdt_nrf_api
+DECL|wdt_nrf_disable|function|static int wdt_nrf_disable(struct device *dev)
+DECL|wdt_nrf_enable|function|static void wdt_nrf_enable(struct device *dev)
+DECL|wdt_nrf_feed|function|static int wdt_nrf_feed(struct device *dev, int channel_id)
+DECL|wdt_nrf_get_config|function|static void wdt_nrf_get_config(struct device *dev, struct wdt_config *config)
+DECL|wdt_nrf_install_timeout|function|static int wdt_nrf_install_timeout(struct device *dev, const struct wdt_timeout_cfg *cfg)
+DECL|wdt_nrf_isr|function|static void wdt_nrf_isr(struct device *dev)
+DECL|wdt_nrf_reload|function|static void wdt_nrf_reload(struct device *dev)
+DECL|wdt_nrf_set_config|function|static int wdt_nrf_set_config(struct device *dev, struct wdt_config *config)
+DECL|wdt_nrf_setup|function|static int wdt_nrf_setup(struct device *dev, u8_t options)
+DECL|wdt_nrfx|variable|wdt_nrfx
