2805765f6727bbe7c46eab8813ededeb9003b3d6
Leandro Pereira <leandro.pereira@intel.com>
2018-05-14 13:27:06 -0700

    drivers: spi: Fix TOCTOU while transceiving SPI messages
    To avoid another thread from modifying the spi_buf_set and spi_config
    structures passed to the spi_transceive() syscall, copy those to the
    kernel stack before validating.
    
    Fixes #7378.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 845ac3ef4666d23332158321570930433d450824

--- drivers/spi/spi_handlers.c
+++ drivers/spi/spi_handlers.c
@@ -1,3 +1,4 @@
 DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(spi_release, dev, config_p)
 DECL|_SYSCALL_HANDLER|function|_SYSCALL_HANDLER(spi_transceive, dev, config_p, tx_bufs, rx_bufs)
-DECL|verify_spi_buf_array|function|static void verify_spi_buf_array(const struct spi_buf *bufs, size_t len, int writable, void *ssf)
+DECL|copy_and_check|function|static void copy_and_check(struct spi_buf_set *bufs, struct spi_buf *buf_copy, int writable, void *ssf)
+DECL|copy_bufs_and_transceive|function|static u32_t copy_bufs_and_transceive(struct device *dev, const struct spi_config *config, struct spi_buf_set *tx_bufs, struct spi_buf_set *rx_bufs, void *ssf)
