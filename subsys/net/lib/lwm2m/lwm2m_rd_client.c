837a8987cf2908371fb36671b1cfab3155681b18
Michael Scott <michael@opensourcefoundries.com>
2018-05-15 11:45:21 -0700

    net: lwm2m: simplify registration client
    When designing the registration client for LwM2M, I understood
    that the LwM2M Technical Specification allows for a multi-server
    connection setup where the client makes several connections
    to various LwM2M servers and allows each of them to manage
    various aspects of the LwM2M client based on Access Controls.
    
    However, the way I implemented it was not well thought out and
    as we look forward to adding Bootstrap support, it needs a
    do over.
    
    Let's remove all of the code dedicated to handling multiple LwM2M
    client connections.  This will simplify and reduce the code size
    of the registration client considerably.
    
    Later, once Bootstrap support has been added, we can implement
    multi-server connections in a cleaner manner.
    
    Signed-off-by: Michael Scott <michael@opensourcefoundries.com>
    
    Former-commit-id: 198b358638ccd017c6d5064a1db2800936cdcbf7

--- subsys/net/lib/lwm2m/lwm2m_rd_client.c
+++ subsys/net/lib/lwm2m/lwm2m_rd_client.c
@@ -1,5 +1,4 @@
 DECL|CLIENT_EP_LEN|macro|CLIENT_EP_LEN
-DECL|CLIENT_INSTANCE_COUNT|macro|CLIENT_INSTANCE_COUNT
 DECL|ENGINE_BOOTSTRAP_DONE|enumerator|ENGINE_BOOTSTRAP_DONE,
 DECL|ENGINE_BOOTSTRAP_SENT|enumerator|ENGINE_BOOTSTRAP_SENT,
 DECL|ENGINE_DEREGISTERED|enumerator|ENGINE_DEREGISTERED
@@ -18,9 +17,8 @@ DECL|STATE_MACHINE_UPDATE_INTERVAL|macro|STATE_MACHINE_UPDATE_INTERVAL
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|bootstrapped|member|u8_t bootstrapped; /* bootstrap done */
-DECL|client_count|variable|client_count
 DECL|client_data|variable|client_data
-DECL|clients|variable|clients
+DECL|client|variable|client
 DECL|ctx|member|struct lwm2m_ctx *ctx;
 DECL|do_bootstrap_reply_cb|function|static int do_bootstrap_reply_cb(const struct coap_packet *response, struct coap_reply *reply, const struct sockaddr *from)
 DECL|do_bootstrap_timeout_cb|function|static void do_bootstrap_timeout_cb(struct lwm2m_message *msg)
@@ -34,9 +32,7 @@ DECL|engine_state|member|u8_t engine_state;
 DECL|engine_trigger_update|function|void engine_trigger_update(void)
 DECL|ep_name|member|char ep_name[CLIENT_EP_LEN];
 DECL|event_cb|member|lwm2m_ctx_event_cb_t event_cb;
-DECL|find_clients_index|function|static int find_clients_index(const struct sockaddr *addr)
-DECL|find_rd_client_from_msg|function|static int find_rd_client_from_msg(struct lwm2m_message *msg, struct lwm2m_rd_client_info *rd_clients, size_t len)
-DECL|get_sm_state|function|static u8_t get_sm_state(int index)
+DECL|get_sm_state|function|static u8_t get_sm_state(void)
 DECL|has_bs_server_info|member|u8_t has_bs_server_info;
 DECL|has_registration_info|member|u8_t has_registration_info;
 DECL|last_update|member|s64_t last_update;
@@ -47,17 +43,17 @@ DECL|lwm2m_rd_client_service|function|static void lwm2m_rd_client_service(void)
 DECL|lwm2m_rd_client_start|function|int lwm2m_rd_client_start(struct lwm2m_ctx *client_ctx, char *peer_str, u16_t peer_port, const char *ep_name, lwm2m_ctx_event_cb_t event_cb)
 DECL|query_buffer|variable|query_buffer
 DECL|server_ep|member|char server_ep[CLIENT_EP_LEN];
-DECL|set_sm_state|function|static void set_sm_state(int index, u8_t sm_state)
-DECL|sm_bootstrap_done|function|static int sm_bootstrap_done(int index)
-DECL|sm_do_bootstrap|function|static int sm_do_bootstrap(int index)
-DECL|sm_do_deregister|function|static int sm_do_deregister(int index)
-DECL|sm_do_init|function|static int sm_do_init(int index)
-DECL|sm_do_registration|function|static int sm_do_registration(int index)
+DECL|set_sm_state|function|static void set_sm_state(u8_t sm_state)
+DECL|sm_bootstrap_done|function|static int sm_bootstrap_done(void)
+DECL|sm_do_bootstrap|function|static int sm_do_bootstrap(void)
+DECL|sm_do_deregister|function|static int sm_do_deregister(void)
+DECL|sm_do_init|function|static int sm_do_init(void)
+DECL|sm_do_registration|function|static int sm_do_registration(void)
 DECL|sm_engine_state|enum|enum sm_engine_state {
 DECL|sm_handle_timeout_state|function|static void sm_handle_timeout_state(struct lwm2m_message *msg, enum sm_engine_state sm_state)
-DECL|sm_is_registered|function|static bool sm_is_registered(int index)
-DECL|sm_registration_done|function|static int sm_registration_done(int index)
-DECL|sm_send_registration|function|static int sm_send_registration(int index, bool send_obj_support_data,coap_reply_t reply_cb, lwm2m_message_timeout_cb_t timeout_cb)
+DECL|sm_is_registered|function|static bool sm_is_registered(void)
+DECL|sm_registration_done|function|static int sm_registration_done(void)
+DECL|sm_send_registration|function|static int sm_send_registration(bool send_obj_support_data,coap_reply_t reply_cb, lwm2m_message_timeout_cb_t timeout_cb)
 DECL|trigger_update|member|u8_t trigger_update;
 DECL|use_bootstrap|member|u8_t use_bootstrap;
 DECL|use_registration|member|u8_t use_registration;
