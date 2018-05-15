b2b66c66a255d3270912e280e6e57b2ddd5e1fbd
Kumar Gala <kumar.gala@linaro.org>
2018-05-15 18:52:23 -0500

    arch: arm: nxp_lpc: Added support for init of slave core
    Added ability to load and bootup the slave (Cortex-M0+) core on the
    LPC54xxx SoC.  Currently we expect a binary image to be specified via
    Kconfig for the code that will run on the slave core.  We also specify
    via Kconfig the boot/load address for the slave core.
    
    Origin: Original
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    
    Former-commit-id: 7514a92cbeb783dd3dd7eeddb2863467ed52b1f3

--- arch/arm/soc/nxp_lpc/lpc54xxx/soc.c
+++ arch/arm/soc/nxp_lpc/lpc54xxx/soc.c
@@ -1,2 +1,5 @@
+DECL|CORE_M0_BOOT_ADDRESS|macro|CORE_M0_BOOT_ADDRESS
+DECL|_slave_init|function|int _slave_init(struct device *arg)
 DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
+DECL|core_m0|variable|core_m0
 DECL|nxp_lpc54114_init|function|static int nxp_lpc54114_init(struct device *arg)
