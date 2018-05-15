3fccffbbe210ee8549937c23c17b8c46323f0fd8
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-05-15 16:22:51 +0300

    usb: netusb: Implement status callback for EEM
    Implement status callback.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: 1d5692a7b6419bb49d65d19efc96b9202fca3d8e

--- subsys/usb/class/netusb/function_eem.c
+++ subsys/usb/class/netusb/function_eem.c
@@ -6,5 +6,7 @@ DECL|eem_function|variable|eem_function
 DECL|eem_pkt_size|function|static inline u16_t eem_pkt_size(u16_t hdr)
 DECL|eem_read_cb|function|static void eem_read_cb(u8_t ep, int size, void *priv)
 DECL|eem_send|function|static int eem_send(struct net_pkt *pkt)
+DECL|eem_status_cb|function|static void eem_status_cb(enum usb_dc_status_code status, u8_t *param)
+DECL|eem_status_interface|function|static inline void eem_status_interface(u8_t *iface)
 DECL|rx_buf|variable|rx_buf
 DECL|tx_buf|variable|tx_buf
