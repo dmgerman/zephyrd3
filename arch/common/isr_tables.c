6e6e6deffb3bde7792c422a6d961006436c35131
Sebastian Bøe <sebastian.boe@nordicsemi.no>
2018-05-16 14:08:47 +0200

    isr_tables: Simplify how the sw_irq_handler function is used
    This is a migration from using code generation to using the C language
    which we in the general case we should aways strive towards.
    
    It is equivalent to the simplification that was done with
    _irq_spurious here:
    https://github.com/zephyrproject-rtos/zephyr/pull/7574
    
    Signed-off-by: Sebastian Bøe <sebastian.boe@nordicsemi.no>
    
    Former-commit-id: aed0b6c4bd85f95a3efd772a6ca5b06fa428715b

--- arch/common/isr_tables.c
+++ arch/common/isr_tables.c
@@ -1,9 +1,6 @@
-DECL|ISR_WRAPPER|macro|ISR_WRAPPER
-DECL|ISR_WRAPPER|macro|ISR_WRAPPER
 DECL|_iheader|variable|_iheader
 DECL|_irq_vector_table|variable|_irq_vector_table
 DECL|_sw_isr_table|variable|_sw_isr_table
-DECL|handler_ptr|member|void *handler_ptr;
 DECL|int_list_header|struct|struct int_list_header {
 DECL|offset|member|u32_t offset;
 DECL|table_size|member|u32_t table_size;
