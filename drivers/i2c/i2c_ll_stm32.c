cb896afcc922f7283bfba1ffbb55ca110d1d2a7a
Florian Vaussard <florian.vaussard@gmail.com>
2018-05-17 15:45:50 +0200

    i2c: stm32: add support for I2C4
    Add the necessary Kconfigs and supporting code to enable I2C4 which can
    be found on certain STM32L4.
    
    Signed-off-by: Florian Vaussard <florian.vaussard@gmail.com>
    
    Former-commit-id: af601c22e6ec536adcda4c4ca7af17e817eaaf7d

--- drivers/i2c/i2c_ll_stm32.c
+++ drivers/i2c/i2c_ll_stm32.c
@@ -4,12 +4,15 @@ DECL|api_funcs|variable|api_funcs
 DECL|i2c_stm32_cfg_1|variable|i2c_stm32_cfg_1
 DECL|i2c_stm32_cfg_2|variable|i2c_stm32_cfg_2
 DECL|i2c_stm32_cfg_3|variable|i2c_stm32_cfg_3
+DECL|i2c_stm32_cfg_4|variable|i2c_stm32_cfg_4
 DECL|i2c_stm32_dev_data_1|variable|i2c_stm32_dev_data_1
 DECL|i2c_stm32_dev_data_2|variable|i2c_stm32_dev_data_2
 DECL|i2c_stm32_dev_data_3|variable|i2c_stm32_dev_data_3
+DECL|i2c_stm32_dev_data_4|variable|i2c_stm32_dev_data_4
 DECL|i2c_stm32_init|function|static int i2c_stm32_init(struct device *dev)
 DECL|i2c_stm32_irq_config_func_1|function|static void i2c_stm32_irq_config_func_1(struct device *dev)
 DECL|i2c_stm32_irq_config_func_2|function|static void i2c_stm32_irq_config_func_2(struct device *dev)
 DECL|i2c_stm32_irq_config_func_3|function|static void i2c_stm32_irq_config_func_3(struct device *dev)
+DECL|i2c_stm32_irq_config_func_4|function|static void i2c_stm32_irq_config_func_4(struct device *dev)
 DECL|i2c_stm32_runtime_configure|function|static int i2c_stm32_runtime_configure(struct device *dev, u32_t config)
 DECL|i2c_stm32_transfer|function|static int i2c_stm32_transfer(struct device *dev, struct i2c_msg *msg, u8_t num_msgs, u16_t slave)
