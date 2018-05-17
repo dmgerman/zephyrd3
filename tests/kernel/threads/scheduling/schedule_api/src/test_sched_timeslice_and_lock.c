7a4ffce5075588c90a2f878c6539955587c5c32c
Spoorthi K <spoorthi.k@intel.com>
2018-05-17 12:45:30 +0530

    tests: threads: Add test case to verify k_wakeup()
    Test case to verify wakeup() of pending thread
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: 7393cb2478874bc4ffb339f320a8f72bd2799460

--- tests/kernel/threads/scheduling/schedule_api/src/test_sched_timeslice_and_lock.c
+++ tests/kernel/threads/scheduling/schedule_api/src/test_sched_timeslice_and_lock.c
@@ -1,4 +1,6 @@
 DECL|THREADS_NUM|macro|THREADS_NUM
+DECL|coop_thread|function|static void coop_thread(void *p1, void *p2, void *p3)
+DECL|executed|variable|executed
 DECL|init_prio|variable|init_prio
 DECL|old_prio|variable|old_prio
 DECL|setup_threads|function|static void setup_threads(void)
@@ -7,6 +9,7 @@ DECL|tdata|variable|tdata
 DECL|teardown_threads|function|static void teardown_threads(void)
 DECL|test_busy_wait_cooperative|function|void test_busy_wait_cooperative(void)
 DECL|test_lock_preemptible|function|void test_lock_preemptible(void)
+DECL|test_pending_thread_wakeup|function|void test_pending_thread_wakeup(void)
 DECL|test_sleep_cooperative|function|void test_sleep_cooperative(void)
 DECL|test_sleep_wakeup_preemptible|function|void test_sleep_wakeup_preemptible(void)
 DECL|test_time_slicing_disable_preemptible|function|void test_time_slicing_disable_preemptible(void)
@@ -15,3 +18,4 @@ DECL|test_unlock_preemptible|function|void test_unlock_preemptible(void)
 DECL|test_yield_cooperative|function|void test_yield_cooperative(void)
 DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
 DECL|tthread|variable|tthread
+DECL|t|variable|t
