34a65ced5ee5226911ea30fa3abdde33c31f927e
Leandro Pereira <leandro.pereira@intel.com>
2018-05-18 12:17:07 -0700

    lib: posix: Do not redefine PATH_MAX in unistd.h
    This constant should be defined in limits.h.  Define it in limits.h in
    the minimal libc, and use the definition found in newlib's includes.
    Values in newlib includes range from 1024 to 4096.
    
    The rationale is that all code should use the same value; having
    buffers specified with different sizes will lead to interoperability
    and out of bounds array writes.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: 0f1d30aa672db47f9b6be185204f64c7b47ae559

--- include/posix/unistd.h
+++ include/posix/unistd.h
@@ -1,6 +1,4 @@
 DECL|DIR|typedef|typedef struct fs_dir_t DIR;
-DECL|PATH_MAX|macro|PATH_MAX
-DECL|PATH_MAX|macro|PATH_MAX
 DECL|__POSIX_UNISTD_H__|macro|__POSIX_UNISTD_H__
 DECL|d_ino|member|unsigned int d_ino;
 DECL|d_name|member|char d_name[PATH_MAX + 1];
