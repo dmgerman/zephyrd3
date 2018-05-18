ef92e09bd7f8299ee6ee0ed5b6d4f84400a4a9de
Niranjhana N <niranjhana.n@intel.com>
2018-05-18 15:07:04 +0530

    tests: posix: add test for pthread_key
    Added a simple test case for pthread_key, which
    tests all the APIs - the thread creates a key,
    sets a value to the key, retrieves and asserts
    they are equal, and then deletes the key.
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 8891beaf8473008a02cc7f5b2a4cc89af2e92266

---
+++ tests/posix/pthread_key/src/pthread_key.c
@@ -0,0 +1,8 @@
+DECL|BUFFSZ|macro|BUFFSZ
+DECL|STACKSZ|macro|STACKSZ
+DECL|key_once|variable|key_once
+DECL|key|variable|key
+DECL|make_key|function|static void make_key(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_pthread_key|function|void test_pthread_key(void)
+DECL|thread_top|function|void *thread_top(void *p1)
