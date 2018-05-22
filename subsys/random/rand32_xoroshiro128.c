7acd144849aaa2eccff7132998b11a96fef7b3f1
Leandro Pereira <leandro.pereira@intel.com>
2018-05-22 15:10:14 -0700

    subsys: random: xoroshiro128: Use SYS_INIT hook to initialize properly
    Not only removes a branch during normal operation, but also ensures
    that, by initializing at the PRE_KERNEL_2 stage, and granting
    privileges to all threads to the semaphore, this code will work in
    early boot situations and in user mode.
    
    This assumes that entropy drivers will all initialize during
    PRE_KERNEL_1 stage.  All in-tree drivers do that.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: fcc56e315f1dcc0c25422cb528662fe8dfe0b059

--- subsys/random/rand32_xoroshiro128.c
+++ subsys/random/rand32_xoroshiro128.c
@@ -1,6 +1,5 @@
-DECL|initialized|variable|initialized
 DECL|rotl|function|static inline u64_t rotl(const u64_t x, int k)
 DECL|state|variable|state
 DECL|sys_rand32_get|function|u32_t sys_rand32_get(void)
-DECL|xoroshiro128_initialize|function|static bool xoroshiro128_initialize(void)
+DECL|xoroshiro128_initialize|function|static int xoroshiro128_initialize(struct device *dev)
 DECL|xoroshiro128_next|function|static u32_t xoroshiro128_next(void)
