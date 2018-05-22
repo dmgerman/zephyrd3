dd8f7fb12d3b4ab36cb07e687a609dd72cafc4d4
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-05-22 14:47:27 +0300

    usb: webusb: Use struct string_desc instead of char array
    Use proper structures for dealing with MS OS v1 string descriptors.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: a3fb48c5fd2071ec8e5f7464cb70043add248ab8

--- samples/subsys/usb/webusb/src/main.c
+++ samples/subsys/usb/webusb/src/main.c
@@ -1,5 +1,9 @@
+DECL|MSOS_STRING_LENGTH|macro|MSOS_STRING_LENGTH
 DECL|NUMBER_OF_ALLOWED_ORIGINS|macro|NUMBER_OF_ALLOWED_ORIGINS
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|bDescriptorType|member|u8_t bDescriptorType;
+DECL|bLength|member|u8_t bLength;
+DECL|bString|member|u8_t bString[MSOS_STRING_LENGTH];
 DECL|bos_cap_msosv2|variable|bos_cap_msosv2
 DECL|bos_cap_webusb|variable|bos_cap_webusb
 DECL|cap|member|struct usb_bos_capability_msos cap;
@@ -12,6 +16,7 @@ DECL|msos2_descriptor|variable|msos2_descriptor
 DECL|platform|member|struct usb_bos_platform_descriptor platform;
 DECL|platform|member|struct usb_bos_platform_descriptor platform;
 DECL|req_handlers|variable|req_handlers
+DECL|string_desc|struct|static struct string_desc {
 DECL|usb_bos_msosv2_desc|struct|USB_DEVICE_BOS_DESC_DEFINE_CAP struct usb_bos_msosv2_desc {
 DECL|usb_bos_webusb_desc|struct|USB_DEVICE_BOS_DESC_DEFINE_CAP struct usb_bos_webusb_desc {
 DECL|vendor_handle_req|function|int vendor_handle_req(struct usb_setup_packet *pSetup,s32_t *len, u8_t **data)
