b801255db5c5784c050dd63f771e422ba3af14b2
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2018-05-22 13:49:57 +0300

    samples: net: Fix coap-client sample
    Removed unwanted code, fine tuned a bit. Original use case of
    sample (verify CoAP GET, PUT and POST methods) against coap-server
    not changed.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 55680f884a75cd5ef07b5569694e8e93989f9a6d

--- samples/net/coap_client/src/coap-client.c
+++ samples/net/coap_client/src/coap-client.c
@@ -1,19 +1,12 @@
-DECL|ALL_NODES_LOCAL_COAP_MCAST|macro|ALL_NODES_LOCAL_COAP_MCAST
 DECL|MY_COAP_PORT|macro|MY_COAP_PORT
-DECL|NUM_PENDINGS|macro|NUM_PENDINGS
-DECL|NUM_REPLIES|macro|NUM_REPLIES
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|context|variable|context
-DECL|event_iface_init|function|static void event_iface_init(void)
-DECL|get_from_ip_addr|function|static void get_from_ip_addr(struct coap_packet *cpkt, struct sockaddr_in6 *from)
+DECL|dump_payload|function|static int dump_payload(const struct coap_packet *response)
+DECL|init_app|function|static int init_app(void)
 DECL|main|function|void main(void)
-DECL|mcast_addr|variable|mcast_addr
-DECL|msg_dump|function|static void msg_dump(const char *s, u8_t *data, unsigned len)
-DECL|pendings|variable|pendings
-DECL|replies|variable|replies
-DECL|resource_reply_cb|function|static int resource_reply_cb(const struct coap_packet *response, struct coap_reply *reply, const struct sockaddr *from)
-DECL|retransmit_request|function|static void retransmit_request(struct k_work *work)
-DECL|retransmit_work|variable|retransmit_work
+DECL|peer_addr|variable|peer_addr
 DECL|send_coap_request|function|static void send_coap_request(u8_t method)
-DECL|strip_headers|function|static void strip_headers(struct net_pkt *pkt)
 DECL|test_path|variable|test_path
 DECL|udp_receive|function|static void udp_receive(struct net_context *context,struct net_pkt *pkt, int status, void *user_data)
