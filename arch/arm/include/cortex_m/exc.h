4c0f82e452a2142fd38e2138bb359acca8551c8a
Joakim Andersson <joakim.andersson@nordicsemi.no>
2018-05-22 14:18:22 +0200

    arch: arm: Fix zero interrupt latency priority level
    Change the zero latency interrupt priority level from 2 to 1.
    This is the priority level that the kernel has reserved for the
    zero latency IRQ feature by the _IRQ_PRIO_OFFSET constant.
    The zero latency IRQ will now not be masked by the irq_lock function.
    
    Update comments to reflect the priority levels reserved by the kernel.
    
    Fixes: #8073
    
    Signed-off-by: Joakim Andersson <joakim.andersson@nordicsemi.no>
    
    Former-commit-id: 45b75dd7ffff76e8a9a4b2b32bcb41966065d447

--- arch/arm/include/cortex_m/exc.h
+++ arch/arm/include/cortex_m/exc.h
@@ -1,6 +1,4 @@
 DECL|_ARM_CORTEXM_ISR__H_|macro|_ARM_CORTEXM_ISR__H_
 DECL|_ClearFaults|function|static ALWAYS_INLINE void _ClearFaults(void)
-DECL|_EXC_FAULT_PRIO|macro|_EXC_FAULT_PRIO
-DECL|_EXC_SVC_PRIO|macro|_EXC_SVC_PRIO
 DECL|_ExcSetup|function|static ALWAYS_INLINE void _ExcSetup(void)
 DECL|_IsInIsr|function|static ALWAYS_INLINE int _IsInIsr(void)
