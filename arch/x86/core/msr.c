e8a9a5e7cb03a5594ea75a3350df3a624aa59737
Leandro Pereira <leandro.pereira@intel.com>
2018-05-22 12:11:44 -0700

    arch: x86: Rename MSR-handling functions to conform to convention
    Rename _MsrRead() and _MsrWrite() to _x86_msr_read() and
    _x86_msr_write() respectively.
    
    Given that these functions are essentially implemented in assembly.
    make them static inline.  They can be inlined by the compiler quite
    well, most of the time incurring in space savings due to better
    handling of the cobbled registers.
    
    Also simplifies the inline assembly, using constraints instead of
    moving registers ourselves.  Should shave off a few bytes from code
    using these functions.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: d4221f9a7176d952faf1c638e484ea743598e122

--- arch/x86/core/msr.c
+++
@@ -1,2 +0,0 @@
-DECL|_MsrRead|function|u64_t _MsrRead(unsigned int msr)
-DECL|_MsrWrite|function|void _MsrWrite(unsigned int msr, u64_t msr_data)
--- arch/x86/include/kernel_arch_func.h
+++ arch/x86/include/kernel_arch_func.h
@@ -5,6 +5,8 @@ DECL|_IntLibInit|function|static inline void _IntLibInit(void)
 DECL|_is_in_isr|macro|_is_in_isr
 DECL|_kernel_arch_func__h_|macro|_kernel_arch_func__h_
 DECL|_set_thread_return_value|function|_set_thread_return_value(struct k_thread *thread, unsigned int value)
+DECL|_x86_msr_read|function|static inline u64_t _x86_msr_read(unsigned int msr)
+DECL|_x86_msr_write|function|static inline void _x86_msr_write(unsigned int msr, u64_t data)
 DECL|kernel_arch_init|function|static inline void kernel_arch_init(void)
 DECL|read_x2apic|function|static inline u32_t read_x2apic(unsigned int reg)
 DECL|write_x2apic|function|static inline void write_x2apic(unsigned int reg, u32_t val)
