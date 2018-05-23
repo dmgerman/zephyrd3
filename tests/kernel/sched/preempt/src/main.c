f06329e2d65400757a41e910081d575848ede489
Andy Ross <andrew.j.ross@intel.com>
2018-05-23 09:26:39 -0700

    tests/kernel: Add preemption priority test
    This test exaustively tests preemption points between threads of all
    priority classes (cooperative, preemptible, and metairq), done both
    from a synchronous reschedule (via k_sem_give() and from interrupt
    context (via irq_offload()), and with and without the sched_lock()
    held.  It then detects the next thread that runs and validates vs. the
    documented priority rules.
    
    Note that there is a whitelisted case on ARM, where irq_offload()
    seems not to be working like a true interrupt (it always returns to
    the interrupted context and doesn't seem to hit the normal exception
    return path which can context switch).  And native_posix is excluded
    because of failures and the fact that it's actually never possible to
    truly preempt a thread there (they run to completion inside _Swap()
    et. al. and then block themselves).  Both of these should be fixable
    in the future but don't (seem to) directly relate to this test.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 86b53643350f4883d5ed70219db37d14c92489b6

---
+++ tests/kernel/sched/preempt/src/main.c
@@ -0,0 +1,22 @@
+DECL|COOP|enumerator|const enum { METAIRQ, COOP, PREEMPTIBLE } worker_priorities[] = {
+DECL|METAIRQ|enumerator|const enum { METAIRQ, COOP, PREEMPTIBLE } worker_priorities[] = {
+DECL|NUM_THREADS|macro|NUM_THREADS
+DECL|PREEMPTIBLE|enumerator|const enum { METAIRQ, COOP, PREEMPTIBLE } worker_priorities[] = {
+DECL|PRI|macro|PRI
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|do_irq|variable|do_irq
+DECL|do_lock|variable|do_lock
+DECL|irq_waker|function|void irq_waker(void *p)
+DECL|last_thread|variable|last_thread
+DECL|manager_thread|variable|manager_thread
+DECL|manager_thread|variable|manager_thread
+DECL|manager|function|void manager(void *p1, void *p2, void *p3)
+DECL|target|variable|target
+DECL|test_main|function|void test_main(void)
+DECL|test_preempt|function|void test_preempt(void)
+DECL|validate_wakeup|function|void validate_wakeup(int src, int target, k_tid_t last_thread)
+DECL|wakeup_src_thread|function|void wakeup_src_thread(int id)
+DECL|worker_priorities|variable|worker_priorities
+DECL|worker_sems|variable|worker_sems
+DECL|worker_threads|variable|worker_threads
+DECL|worker|function|void worker(void *p1, void *p2, void *p3)
