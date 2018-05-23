6efe30b3ee426ddda1292357203ac2d9c573a75b
qianfan Zhao <qianfanguijin@163.com>
2018-05-23 10:23:21 +0800

    usb: set SN string descriptor at runtime
    implement a method that can set SN string descriptor at runtime.
    but runtime SN and default SN configured in Kconfig must has the
    same length.
    
    Fixes: #6593
    
    Signed-off-by: qianfan Zhao <qianfanguijin@163.com>
    
    Former-commit-id: 32e6d0ca879ddbbd31097c9ef75a257bf6a1f3f3

--- subsys/usb/usb_descriptor.c
+++ subsys/usb/usb_descriptor.c
@@ -1,6 +1,9 @@
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|USB_BSTRING_ASCII_IDX_MAX|macro|USB_BSTRING_ASCII_IDX_MAX
 DECL|USB_BSTRING_UTF16LE_IDX_MAX|macro|USB_BSTRING_UTF16LE_IDX_MAX
+DECL|USB_DESC_MANUFACTURER_IDX|macro|USB_DESC_MANUFACTURER_IDX
+DECL|USB_DESC_PRODUCT_IDX|macro|USB_DESC_PRODUCT_IDX
+DECL|USB_DESC_SERIAL_NUMBER_IDX|macro|USB_DESC_SERIAL_NUMBER_IDX
 DECL|__packed|variable|__packed
 DECL|__packed|variable|__packed
 DECL|ascii7_to_utf16le|function|static void ascii7_to_utf16le(void *descriptor)
@@ -17,6 +20,7 @@ DECL|cfg_descr|member|struct usb_cfg_descriptor cfg_descr;
 DECL|common_descriptor|struct|struct common_descriptor {
 DECL|device_descriptor|member|struct usb_device_descriptor device_descriptor;
 DECL|lang_descr|member|struct usb_string_descriptor lang_descr;
+DECL|usb_fix_ascii_sn_string_descriptor|function|usb_fix_ascii_sn_string_descriptor(struct usb_sn_descriptor *sn)
 DECL|usb_fix_descriptor|function|static int usb_fix_descriptor(struct usb_desc_header *head)
 DECL|usb_get_cfg_data|function|static struct usb_cfg_data *usb_get_cfg_data(struct usb_if_descriptor *iface)
 DECL|usb_get_device_descriptor|function|u8_t *usb_get_device_descriptor(void)
@@ -25,6 +29,7 @@ DECL|usb_mfr_descriptor|struct|struct usb_mfr_descriptor {
 DECL|usb_product_descriptor|struct|struct usb_product_descriptor {
 DECL|usb_sn_descriptor|struct|struct usb_sn_descriptor {
 DECL|usb_string_desription|struct|struct usb_string_desription {
+DECL|usb_update_sn_string_descriptor|function|__weak u8_t *usb_update_sn_string_descriptor(void)
 DECL|usb_validate_ep_cfg_data|function|static int usb_validate_ep_cfg_data(struct usb_ep_descriptor * const ep_descr, struct usb_cfg_data * const cfg_data, u32_t *requested_ep)
 DECL|utf16le_mfr|member|} __packed utf16le_mfr;
 DECL|utf16le_product|member|} __packed utf16le_product;
