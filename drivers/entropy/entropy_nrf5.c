c8a37cd7dfd40cdcf6fd33abb049bc766f679b3e
Carles Cufi <carles.cufi@nordicsemi.no>
2018-05-24 20:13:42 +0200

    drivers: entropy: nrf5: Implement standard ISR-specific call
    Implement the new entropy_get_entropy_isr() function to allow the kernel
    to collect entropy before the scheduler and kernel data structures are
    ready. Switch to an nrf-specific version for high-performance
    requirements in the BLE Link Layer.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 6f534e45519a07fe49492e53cee7ccae23adf8ac

--- drivers/entropy/entropy_nrf5.c
+++ drivers/entropy/entropy_nrf5.c
@@ -3,12 +3,13 @@ DECL|RAND_DEFINE|macro|RAND_DEFINE
 DECL|RAND_ISR_LEN|macro|RAND_ISR_LEN
 DECL|RAND_THREAD_LEN|macro|RAND_THREAD_LEN
 DECL|count|member|u8_t count;
-DECL|entropy_get_entropy_isr|function|u8_t entropy_get_entropy_isr(struct device *dev, u8_t *buf, u8_t len)
 DECL|entropy_nrf5_api_funcs|variable|entropy_nrf5_api_funcs
 DECL|entropy_nrf5_data|variable|entropy_nrf5_data
 DECL|entropy_nrf5_dev_data|struct|struct entropy_nrf5_dev_data {
+DECL|entropy_nrf5_get_entropy_isr|function|static int entropy_nrf5_get_entropy_isr(struct device *dev, u8_t *buf, u16_t len,u32_t flags)
 DECL|entropy_nrf5_get_entropy|function|static int entropy_nrf5_get_entropy(struct device *device, u8_t *buf, u16_t len)
 DECL|entropy_nrf5_init|function|static int entropy_nrf5_init(struct device *device)
+DECL|entropy_nrf_get_entropy_isr|function|u8_t entropy_nrf_get_entropy_isr(struct device *dev, u8_t *buf, u8_t len)
 DECL|first|member|u8_t first;
 DECL|get|function|static inline u8_t get(struct rand *rng, u8_t octets, u8_t *rand)
 DECL|init|function|static void init(struct rand *rng, u8_t len, u8_t threshold)
