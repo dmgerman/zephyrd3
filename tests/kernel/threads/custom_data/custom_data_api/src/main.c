21690f7930f017efdd217ae3377d4163019440aa
Anas Nashif <anas.nashif@intel.com>
2018-05-23 23:47:15 -0400

    tests: threads: fold customdata tests into main test
    Move this small test to the main thread test project.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: fe4693bd9fd5ff264fb1256dd0dd7bb7f4d0af36

--- tests/kernel/threads/custom_data/custom_data_api/src/main.c
+++
@@ -1,7 +0,0 @@
-DECL|STACK_SIZE|macro|STACK_SIZE
-DECL|STACK_SIZE|macro|STACK_SIZE
-DECL|customdata_entry|function|static void customdata_entry(void *p1, void *p2, void *p3)
-DECL|tdata|variable|tdata
-DECL|test_customdata_get_set_coop|function|void test_customdata_get_set_coop(void)
-DECL|test_customdata_get_set_preempt|function|void test_customdata_get_set_preempt(void)
-DECL|test_main|function|void test_main(void)
--- tests/kernel/threads/lifecycle/lifecycle_api/src/main.c
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/main.c
@@ -1,6 +1,10 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|customdata_entry|function|static void customdata_entry(void *p1, void *p2, void *p3)
 DECL|main_prio|variable|main_prio
+DECL|tdata_custom|variable|tdata_custom
 DECL|tdata|variable|tdata
+DECL|test_customdata_get_set_coop|function|void test_customdata_get_set_coop(void)
+DECL|test_customdata_get_set_preempt|function|void test_customdata_get_set_preempt(void)
 DECL|test_main|function|void test_main(void)
 DECL|test_systhreads_idle|function|void test_systhreads_idle(void)
 DECL|test_systhreads_main|function|void test_systhreads_main(void)
