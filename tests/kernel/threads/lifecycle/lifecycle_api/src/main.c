35c238b5faa46df33ecc6ca52b1f6fbfaedd2f0e
Anas Nashif <anas.nashif@intel.com>
2018-05-23 23:21:28 -0400

    tests: threads: fold systemthread tests into main test
    such a simple test does not deserve to be on its own, lets just put it
    with other thread tests.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: fa4aa9fec0524941d10e19dcd173fd172d5a28e2

--- tests/kernel/threads/lifecycle/lifecycle_api/src/main.c
+++ tests/kernel/threads/lifecycle/lifecycle_api/src/main.c
@@ -1,3 +1,6 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|main_prio|variable|main_prio
 DECL|tdata|variable|tdata
 DECL|test_main|function|void test_main(void)
+DECL|test_systhreads_idle|function|void test_systhreads_idle(void)
+DECL|test_systhreads_main|function|void test_systhreads_main(void)
