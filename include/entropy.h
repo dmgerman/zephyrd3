4d75e2da31cc3ec164866f30b90037dcb47bb9ce
Carles Cufi <carles.cufi@nordicsemi.no>
2018-05-24 20:10:53 +0200

    drivers: entropy: Introduce ISR-specific entropy function
    In order to address the requirements of the kernel boot process, an
    optional ISR-specific function is declared to allow entropy collection
    outside of thread mode and without using any kernel primitives. It also
    includes a parameter to optionally busy-wait for hardware entropy
    generation.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 668e31f7dba2e149693edde068e9eb9fa326643f

--- include/entropy.h
+++ include/entropy.h
@@ -1,5 +1,9 @@
+DECL|ENTROPY_BUSYWAIT|macro|ENTROPY_BUSYWAIT
 DECL|__ENTROPY_H__|macro|__ENTROPY_H__
 DECL|_impl_entropy_get_entropy|function|static inline int _impl_entropy_get_entropy(struct device *dev, u8_t *buffer, u16_t length)
 DECL|entropy_driver_api|struct|struct entropy_driver_api {
+DECL|entropy_get_entropy_isr_t|typedef|typedef int (*entropy_get_entropy_isr_t)(struct device *dev,
+DECL|entropy_get_entropy_isr|function|static inline int entropy_get_entropy_isr(struct device *dev, u8_t *buffer, u16_t length, u32_t flags)
 DECL|entropy_get_entropy_t|typedef|typedef int (*entropy_get_entropy_t)(struct device *dev,
+DECL|get_entropy_isr|member|entropy_get_entropy_isr_t get_entropy_isr;
 DECL|get_entropy|member|entropy_get_entropy_t get_entropy;
