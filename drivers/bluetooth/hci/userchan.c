b740b5d89e9a85a4d00bdc234da52de145e0e2f7
Johan Hedberg <johan.hedberg@intel.com>
2018-05-25 13:32:34 +0300

    Bluetooth: Add HCI User Channel driver for native POSIX port
    Introduce a custom HCI driver for the native POSIX port, which opens a
    HCI User Channel socket to the Linux kernel to gain access to a local
    Bluetooth controller.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: 7d9896575b00c54d5f8cd38cef9d9667aa3b0bf9

---
+++ drivers/bluetooth/hci/userchan.c
@@ -0,0 +1,22 @@
+DECL|BTPROTO_HCI|macro|BTPROTO_HCI
+DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|H4_ACL|macro|H4_ACL
+DECL|H4_CMD|macro|H4_CMD
+DECL|H4_EVT|macro|H4_EVT
+DECL|H4_SCO|macro|H4_SCO
+DECL|HCI_CHANNEL_USER|macro|HCI_CHANNEL_USER
+DECL|SOL_HCI|macro|SOL_HCI
+DECL|_bt_uc_init|function|static int _bt_uc_init(struct device *unused)
+DECL|drv|variable|drv
+DECL|get_rx|function|static struct net_buf *get_rx(const u8_t *buf)
+DECL|hci_channel|member|unsigned short hci_channel;
+DECL|hci_dev|member|unsigned short hci_dev;
+DECL|hci_family|member|sa_family_t hci_family;
+DECL|rx_thread_data|variable|rx_thread_data
+DECL|rx_thread|function|static void rx_thread(void *p1, void *p2, void *p3)
+DECL|sockaddr_hci|struct|struct sockaddr_hci {
+DECL|uc_fd|variable|uc_fd
+DECL|uc_open|function|static int uc_open(void)
+DECL|uc_ready|function|static bool uc_ready(void)
+DECL|uc_send|function|static int uc_send(struct net_buf *buf)
+DECL|user_chan_open|function|static int user_chan_open(u16_t index)
