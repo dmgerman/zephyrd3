72671fadf446784bce865a961398ab81e8424602
Carles Cufi <carles.cufi@nordicsemi.no>
2018-05-25 18:59:12 +0200

    tests: Fix sizing for several test for chips with 24KB of RAM
    When adding the nRF52810, which has 24KB of RAM, some of the tests don't
    compile anymore due to lack of SRAM. Address this by either filtering
    the test out or reducing the amount of memory allocation.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 6eeeb2a3e5795a5e30c15ee4ee5d8f8015b23011

--- tests/kernel/sched/schedule_api/src/test_priority_scheduling.c
+++ tests/kernel/sched/schedule_api/src/test_priority_scheduling.c
@@ -2,6 +2,7 @@ DECL|BASE_PRIORITY|macro|BASE_PRIORITY
 DECL|ITRERATION_COUNT|macro|ITRERATION_COUNT
 DECL|NUM_THREAD|macro|NUM_THREAD
 DECL|NUM_THREAD|macro|NUM_THREAD
+DECL|NUM_THREAD|macro|NUM_THREAD
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|test_priority_scheduling|function|void test_priority_scheduling(void)
 DECL|thread_idx|variable|thread_idx
