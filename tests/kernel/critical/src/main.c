73a76bbb04bca494ee7d1c6250c1a8256a591d8e
Anas Nashif <anas.nashif@intel.com>
2018-05-26 08:27:05 -0500

    tests: critical: fix naming and comments
    We were referring to old function names and still using the task
    terminology which can be confusing.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 43293b901640c28b5297f89cb16862a87392a13d

--- tests/kernel/critical/src/main.c
+++ tests/kernel/critical/src/main.c
@@ -1,14 +1,14 @@
 DECL|NUM_MILLISECONDS|macro|NUM_MILLISECONDS
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|TEST_TIMEOUT|macro|TEST_TIMEOUT
-DECL|alt_task_iterations|variable|alt_task_iterations
-DECL|alternate_task|function|void alternate_task(void *arg1, void *arg2, void *arg3)
+DECL|alt_thread_iterations|variable|alt_thread_iterations
+DECL|alternate_thread|function|void alternate_thread(void *arg1, void *arg2, void *arg3)
 DECL|critical_loop|function|u32_t critical_loop(u32_t count)
 DECL|critical_rtn|function|void critical_rtn(struct k_work *unused)
 DECL|critical_var|variable|critical_var
 DECL|init_objects|function|static void init_objects(void)
 DECL|offload_work_q|variable|offload_work_q
-DECL|regression_task|function|void regression_task(void *arg1, void *arg2, void *arg3)
+DECL|regression_thread|function|void regression_thread(void *arg1, void *arg2, void *arg3)
 DECL|start_threads|function|static void start_threads(void)
 DECL|test_critical|function|void test_critical(void)
 DECL|test_main|function|void test_main(void)
