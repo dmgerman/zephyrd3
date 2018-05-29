92839e69f8305d2525f5db143f7fb9eabd2dc6cd
Leandro Pereira <leandro.pereira@intel.com>
2018-05-29 14:37:21 -0700

    samples: crypto: Ensure cap_flags is always initialized
    Find the device and query its capabilities just once at boot, then
    go through a list of tests to perform.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>
    
    Former-commit-id: cf3da3c6c48fbeec88b02a6f273372126004b82f

--- samples/drivers/crypto/src/main.c
+++ samples/drivers/crypto/src/main.c
@@ -2,19 +2,22 @@ DECL|CRYPTO_DRV_NAME|macro|CRYPTO_DRV_NAME
 DECL|CRYPTO_DRV_NAME|macro|CRYPTO_DRV_NAME
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|cap_flags|variable|cap_flags
-DECL|cbc_mode|function|void cbc_mode(void)
+DECL|cbc_mode|function|void cbc_mode(struct device *dev)
 DECL|ccm_data|variable|ccm_data
 DECL|ccm_expected|variable|ccm_expected
 DECL|ccm_hdr|variable|ccm_hdr
 DECL|ccm_key|variable|ccm_key
-DECL|ccm_mode|function|void ccm_mode(void)
+DECL|ccm_mode|function|void ccm_mode(struct device *dev)
 DECL|ccm_nonce|variable|ccm_nonce
 DECL|ciphertext|variable|ciphertext
 DECL|ctr_ciphertext|variable|ctr_ciphertext
-DECL|ctr_mode|function|void ctr_mode(void)
+DECL|ctr_mode|function|void ctr_mode(struct device *dev)
 DECL|iv|variable|iv
 DECL|key|variable|key
 DECL|main|function|void main(void)
+DECL|mode_func|member|void (*mode_func)(struct device *dev);
+DECL|mode_test|struct|struct mode_test {
+DECL|mode|member|const char *mode;
 DECL|plaintext|variable|plaintext
 DECL|print_buffer_comparison|function|static void print_buffer_comparison(u8_t *wanted_result, u8_t *result, size_t length)
 DECL|validate_hw_compatibility|function|int validate_hw_compatibility(struct device *dev)
