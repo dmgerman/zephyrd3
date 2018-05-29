1e8f39981af01d36970f0468bd05ec8507e1af1d
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-05-29 16:52:53 +0200

    drivers/flash: A support for SPI gpio-cs logic into w25qxxdv driver
    Up to SPI API user code to handle providing the right SPI GPIO-CS
    parameters now.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: a6d8378377dfba7392f7632bc7acc3438fcefdb4

--- drivers/flash/spi_flash_w25qxxdv.h
+++ drivers/flash/spi_flash_w25qxxdv.h
@@ -1,4 +1,5 @@
 DECL|__SPI_FLASH_W25QXXDV_H__|macro|__SPI_FLASH_W25QXXDV_H__
+DECL|cs_ctrl|member|struct spi_cs_control cs_ctrl;
 DECL|sem|member|struct k_sem sem;
 DECL|spi_cfg|member|struct spi_config spi_cfg;
 DECL|spi_flash_data|struct|struct spi_flash_data {
