458165e6702e1477697d1d070d7db40a7cd1ae7e
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-05-31 16:30:14 +0300

    usb: bos: unit: Add unit test for BOS testing
    Add testing USB Binary Device Object Store descriptors and macros.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: b9c82121c6014e1db9066397206b7c4fab8f1465

---
+++ tests/subsys/usb/bos/src/test_bos.c
@@ -0,0 +1,20 @@
+DECL|bos|member|struct usb_bos_descriptor bos;
+DECL|cap_msosv2|variable|cap_msosv2
+DECL|cap_webusb|variable|cap_webusb
+DECL|capability_data_msos|member|struct usb_bos_capability_msos capability_data_msos;
+DECL|capability_data_webusb|member|struct usb_bos_capability_webusb capability_data_webusb;
+DECL|cap|member|struct usb_bos_capability_msos cap;
+DECL|cap|member|struct usb_bos_capability_webusb cap;
+DECL|dummy_descriptor|variable|dummy_descriptor
+DECL|hexdump|function|static void hexdump(const u8_t *data, size_t len)
+DECL|platform_msos|member|struct usb_bos_platform_descriptor platform_msos;
+DECL|platform_webusb|member|struct usb_bos_platform_descriptor platform_webusb;
+DECL|platform|member|struct usb_bos_platform_descriptor platform;
+DECL|platform|member|struct usb_bos_platform_descriptor platform;
+DECL|test_main|function|void test_main(void)
+DECL|test_usb_bos_macros|function|static void test_usb_bos_macros(void)
+DECL|test_usb_bos|function|static void test_usb_bos(void)
+DECL|usb_bos_msosv2|struct|USB_DEVICE_BOS_DESC_DEFINE_CAP struct usb_bos_msosv2 {
+DECL|usb_bos_webusb|struct|USB_DEVICE_BOS_DESC_DEFINE_CAP struct usb_bos_webusb {
+DECL|webusb_bos_descriptor|variable|webusb_bos_descriptor
+DECL|webusb_bos_desc|struct|static struct webusb_bos_desc {
