9fc61f96dd16e97c8c255d0983a749d3890604aa
Laczen JMS <laczenjms@gmail.com>
2018-06-01 16:13:54 +0200

    subsys: fs/nvs: Improved nvs for larger blocksizes
    The nvs module has some disadvantages for larger block size. The data
    header and slot are taking up to much space. A rewrite is proposed that
    reduces the used storage space for systems with write block size > 4.
    
    The data storage in flash is now one unit consisting of: data_length,
    data_id, data and data_length again in a multiple of the write block
    size. The data_length at the end is used to validate the correctness of
    the flash write and also allows to travel backwards in the filesystem.
    
    As a comparison, on a system with block size 8 byte, a 32 bit values
    now fits 1 block including the metadata (length and id). This used to
    be 3 blocks.
    
    The data_length will occupy 1 byte if the data length is less than 128
    byte, it will occupy 2 byte if the data length is 128 byte or more. The
    data length is limited to 16383 byte.
    
    Each write to flash is verified by a read back of the data.
    
    The read performance is improved because reading is done backwards so
    the latest items are found first.
    
    When the filesystem is locked it can be unlocked by calling
    reinit(), this will clear flash and setup everything for storage.
    
    add sample documentation - README.rst
    
    Update dtsi to include erase_block_size, use erase_block_size in sample
    
    Update prj.conf to include CONFIG_MPU_ALLOW_FLASH_WRITE
    
    Signed-off-by: Laczen JMS <laczenjms@gmail.com>
    
    Former-commit-id: b9dead0a424769defbe0d35622e96a9a19ad3c8f

--- include/nvs/nvs.h
+++ include/nvs/nvs.h
@@ -1,14 +1,12 @@
 DECL|__NVS_H_|macro|__NVS_H_
-DECL|entry_sector|member|u8_t entry_sector; /* oldest sector in use */
+DECL|entry_location|member|off_t entry_location; /* start of the filesystem */
 DECL|flash_device|member|struct device *flash_device;
-DECL|free_space|member|u16_t free_space; /* Indicator for available space in the file system.
-DECL|magic|member|u32_t magic; /* filesystem magic, repeated at start of each sector */
+DECL|locked|member|bool locked; /* the filesystem is locked after an error occurred
 DECL|max_len|member|u16_t max_len; /* maximum size of stored item, set to sector_size/4 */
 DECL|nvs_fs|struct|struct nvs_fs {
 DECL|nvs_lock|member|struct k_mutex nvs_lock;
 DECL|offset|member|off_t offset; /* filesystem offset in flash */
 DECL|sector_count|member|u8_t sector_count; /* how many sectors in the filesystem */
-DECL|sector_id|member|u16_t sector_id; /* sector id, a counter for each created sector */
 DECL|sector_size|member|u16_t sector_size; /* filesystem is divided into sectors,
 DECL|write_block_size|member|u8_t write_block_size; /* write block size for alignment */
-DECL|write_location|member|off_t write_location; /* next write location for entry header */
+DECL|write_location|member|off_t write_location; /* next write location */
--- subsys/fs/nvs/nvs_priv.h
+++ subsys/fs/nvs/nvs_priv.h
@@ -1,20 +1,8 @@
 DECL|NVS_ID_EMPTY|macro|NVS_ID_EMPTY
-DECL|NVS_ID_GT|macro|NVS_ID_GT
-DECL|NVS_ID_SECTOR_END|macro|NVS_ID_SECTOR_END
-DECL|NVS_MOVE_BLOCK_SIZE|macro|NVS_MOVE_BLOCK_SIZE
+DECL|NVS_ID_JUMP|macro|NVS_ID_JUMP
+DECL|NVS_MAX_LEN|macro|NVS_MAX_LEN
+DECL|NVS_MIN_WRITE_BLOCK_SIZE|macro|NVS_MIN_WRITE_BLOCK_SIZE
 DECL|NVS_STATUS_NOSPACE|macro|NVS_STATUS_NOSPACE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|__NVS_PRIV_H_|macro|__NVS_PRIV_H_
-DECL|_nvs_data_hdr|struct|struct _nvs_data_hdr {
-DECL|_nvs_data_slt|struct|struct _nvs_data_slt {
-DECL|_nvs_sector_hdr|struct|struct _nvs_sector_hdr {
-DECL|crc16|member|u16_t crc16;
-DECL|data_addr|member|off_t data_addr; /* address in flash to write data */
-DECL|fd_id|member|u16_t fd_id;
-DECL|fd_magic|member|u32_t fd_magic;
-DECL|id|member|u16_t id;
-DECL|id|member|u16_t id; /* entry id, is 0xFFFFF when empty, set to 0xFFFE
-DECL|len|member|u16_t len;
-DECL|len|member|u16_t len; /* entry length in bytes */
-DECL|nvs_entry|struct|struct nvs_entry {
