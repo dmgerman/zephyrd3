5da07eef13722e21e73fdef4cfd3ac6680cb359c
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2018-06-01 07:30:57 +0200

    drivers: console: rtt: RTT session awareness
    In order to avoid bytes dropping when RTT output buffer is full,
    RTT session detection algorithm is introduced to the RTT console.
    If host session is detected console retries after wait state
    expecting host to empty the buffer. If host session is not detected
    console drops the byte without retrying.
    
    Signed-off-by: Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
    
    Former-commit-id: 5a1313f80401cfb61127fb8c360aca9e49ef9c0b

--- drivers/console/rtt_console.c
+++ drivers/console/rtt_console.c
@@ -1,2 +1,4 @@
+DECL|host_present|variable|host_present
 DECL|rtt_console_init|function|static int rtt_console_init(struct device *d)
 DECL|rtt_console_out|function|static int rtt_console_out(int character)
+DECL|wait|function|static void wait(void)
