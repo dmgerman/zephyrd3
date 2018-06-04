3a3a5bd449453d790c17e97c5625e35cf6d34327
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-06-04 10:58:31 +0530

    tests: kernel: Move k_thread_foreach() API test to thread_apis test
    Move k_thread_foreach() API test from tests/kernel/profiling to
    tests/kernel/threads/thread_apis.
    
    Fixes #7966
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: 00e29c176d6d0a4b61c65889f7f34ccd27d92766

--- tests/kernel/profiling/profiling_api/src/main.c
+++ tests/kernel/profiling/profiling_api/src/main.c
@@ -1,21 +1,13 @@
 DECL|NUM_OF_WORK|macro|NUM_OF_WORK
 DECL|SLEEP_MS|macro|SLEEP_MS
-DECL|STACKSIZE|macro|STACKSIZE
 DECL|TEST_STRING|macro|TEST_STRING
 DECL|_sys_soc_resume|function|void _sys_soc_resume(void)
 DECL|_sys_soc_suspend|function|int _sys_soc_suspend(s32_t ticks)
-DECL|stack_flag|variable|stack_flag
 DECL|sync_sema|variable|sync_sema
-DECL|tcount|variable|tcount
 DECL|tdata_dump_callback|function|static void tdata_dump_callback(const struct k_thread *thread, void *user_data)
 DECL|test_call_stacks_analyze_idle|function|void test_call_stacks_analyze_idle(void)
 DECL|test_call_stacks_analyze_main|function|void test_call_stacks_analyze_main(void)
 DECL|test_call_stacks_analyze_workq|function|void test_call_stacks_analyze_workq(void)
-DECL|test_k_thread_foreach|function|static void test_k_thread_foreach(void)
 DECL|test_main|function|void test_main(void)
-DECL|test_thread|variable|test_thread
-DECL|thread_callback|function|static void thread_callback(const struct k_thread *thread, void *user_data)
-DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
-DECL|thread_flag|variable|thread_flag
 DECL|work_handler|function|static void work_handler(struct k_work *w)
 DECL|work|variable|work
---
+++ tests/kernel/threads/thread_apis/src/test_kthread_for_each.c
@@ -0,0 +1,10 @@
+DECL|SLEEP_MS|macro|SLEEP_MS
+DECL|STACKSIZE|macro|STACKSIZE
+DECL|TEST_STRING|macro|TEST_STRING
+DECL|stack_flag|variable|stack_flag
+DECL|tcount|variable|tcount
+DECL|test_k_thread_foreach|function|void test_k_thread_foreach(void)
+DECL|thread_callback|function|static void thread_callback(const struct k_thread *thread, void *user_data)
+DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
+DECL|thread_flag|variable|thread_flag
+DECL|tstack|variable|tstack
