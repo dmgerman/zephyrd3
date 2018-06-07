2a840c172095bd9e174ed669c9245fbfb1127346
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-06-07 21:24:14 +0300

    subsys: console: getchar: Use consistent var names for RX path
    Initially, there was support only for buffered input, and adhoc var
    names used. Later, buffered output support was added, with variables
    consistently using "tx_" prefix. Now, rename the original RX path
    to use symmetric "rx_" prefix.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: c524ff6b8c366a7d5b788e0c97f0cf23a7d46dc1

--- subsys/console/getchar.c
+++ subsys/console/getchar.c
@@ -2,11 +2,11 @@ DECL|console_getchar|function|u8_t console_getchar(void)
 DECL|console_init|function|void console_init(void)
 DECL|console_irq_input_hook|function|static int console_irq_input_hook(u8_t c)
 DECL|console_putchar|function|int console_putchar(char c)
-DECL|i_get|variable|i_get
-DECL|i_put|variable|i_put
+DECL|rx_get|variable|rx_get
+DECL|rx_put|variable|rx_put
+DECL|rx_ringbuf|variable|rx_ringbuf
 DECL|tx_get|variable|tx_get
 DECL|tx_put|variable|tx_put
 DECL|tx_ringbuf|variable|tx_ringbuf
 DECL|uart_dev|variable|uart_dev
 DECL|uart_isr|function|static void uart_isr(struct device *dev)
-DECL|uart_ringbuf|variable|uart_ringbuf
