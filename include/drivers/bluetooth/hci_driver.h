27807d534454b4dc8ff9909b0319c489fcce1892
Johan Hedberg <johan.hedberg@intel.com>
2018-06-07 08:41:14 +0200

    Bluetooth: Introduce HCI driver quirks
    Introduce a quirks field to the HCI driver struct, which can be used
    to create exceptions in host behavior for non-standard or unusual
    controller behavior. An initial quirk is added to prevent the host
    from sending the HCI_Reset command (in which case the controller is
    responsible for performing the reset).
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: b20aff2f80271364f6dd865993cfbaa82401d9d7

--- include/drivers/bluetooth/hci_driver.h
+++ include/drivers/bluetooth/hci_driver.h
@@ -7,6 +7,7 @@ DECL|BT_HCI_DRIVER_BUS_SPI|enumerator|BT_HCI_DRIVER_BUS_SPI = 7,
 DECL|BT_HCI_DRIVER_BUS_UART|enumerator|BT_HCI_DRIVER_BUS_UART = 3,
 DECL|BT_HCI_DRIVER_BUS_USB|enumerator|BT_HCI_DRIVER_BUS_USB = 1,
 DECL|BT_HCI_DRIVER_BUS_VIRTUAL|enumerator|BT_HCI_DRIVER_BUS_VIRTUAL = 0,
+DECL|BT_QUIRK_NO_RESET|enumerator|BT_QUIRK_NO_RESET = BIT(0),
 DECL|__BT_HCI_DRIVER_H|macro|__BT_HCI_DRIVER_H
 DECL|bt_hci_driver_bus|enum|enum bt_hci_driver_bus {
 DECL|bt_hci_driver|struct|struct bt_hci_driver {
@@ -14,4 +15,5 @@ DECL|bt_hci_evt_is_prio|function|static inline bool bt_hci_evt_is_prio(u8_t evt)
 DECL|bus|member|enum bt_hci_driver_bus bus;
 DECL|name|member|const char *name;
 DECL|open|member|int (*open)(void);
+DECL|quirks|member|u32_t quirks;
 DECL|send|member|int (*send)(struct net_buf *buf);
