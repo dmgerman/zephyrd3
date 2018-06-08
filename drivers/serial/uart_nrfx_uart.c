eb56c9ae69a67adf45302f78f2a1257cdaf7c4d1
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2018-06-08 15:29:24 +0200

    drivers: uart: Rename nrf5 namings to nrfx
    UART driver renamed to keep the same convention as SPI and TWI drivers.
    All substrings: "UART_NRF5" in defines renamed to  "UART_NRFX_UART".
    
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
    
    Former-commit-id: 3f99eefe5af74aad2214b2f1db851ad0973f9f49

---
+++ drivers/serial/uart_nrfx_uart.c
@@ -0,0 +1,23 @@
+DECL|baudrate_set|function|static int baudrate_set(struct device *dev, u32_t baudrate)
+DECL|m_irq_callback|variable|m_irq_callback
+DECL|uart_nrfx_err_check|function|static int uart_nrfx_err_check(struct device *dev)
+DECL|uart_nrfx_fifo_fill|function|static int uart_nrfx_fifo_fill(struct device *dev, const u8_t *tx_data, int len)
+DECL|uart_nrfx_fifo_read|function|static int uart_nrfx_fifo_read(struct device *dev, u8_t *rx_data, const int size)
+DECL|uart_nrfx_init|function|static int uart_nrfx_init(struct device *dev)
+DECL|uart_nrfx_irq_callback_set|function|static void uart_nrfx_irq_callback_set(struct device *dev, uart_irq_callback_t cb)
+DECL|uart_nrfx_irq_err_disable|function|static void uart_nrfx_irq_err_disable(struct device *dev)
+DECL|uart_nrfx_irq_err_enable|function|static void uart_nrfx_irq_err_enable(struct device *dev)
+DECL|uart_nrfx_irq_is_pending|function|static int uart_nrfx_irq_is_pending(struct device *dev)
+DECL|uart_nrfx_irq_rx_disable|function|static void uart_nrfx_irq_rx_disable(struct device *dev)
+DECL|uart_nrfx_irq_rx_enable|function|static void uart_nrfx_irq_rx_enable(struct device *dev)
+DECL|uart_nrfx_irq_rx_ready|function|static int uart_nrfx_irq_rx_ready(struct device *dev)
+DECL|uart_nrfx_irq_tx_complete|function|static int uart_nrfx_irq_tx_complete(struct device *dev)
+DECL|uart_nrfx_irq_tx_disable|function|static void uart_nrfx_irq_tx_disable(struct device *dev)
+DECL|uart_nrfx_irq_tx_enable|function|static void uart_nrfx_irq_tx_enable(struct device *dev)
+DECL|uart_nrfx_irq_tx_ready|function|static int uart_nrfx_irq_tx_ready(struct device *dev)
+DECL|uart_nrfx_irq_update|function|static int uart_nrfx_irq_update(struct device *dev)
+DECL|uart_nrfx_isr|function|static void uart_nrfx_isr(void *arg)
+DECL|uart_nrfx_poll_in|function|static int uart_nrfx_poll_in(struct device *dev, unsigned char *c)
+DECL|uart_nrfx_poll_out|function|static unsigned char uart_nrfx_poll_out(struct device *dev,unsigned char c)
+DECL|uart_nrfx_uart0|variable|uart_nrfx_uart0
+DECL|uart_nrfx_uart_driver_api|variable|uart_nrfx_uart_driver_api
