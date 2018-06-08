1701ff20caa96f9321f70e8d4979601970b2cb87
Tomasz Gorochowik <tgorochowik@antmicro.com>
2018-06-08 12:50:47 +0200

    drivers: eth: gmac: Fix cache support for SAM GMAC
    What needs to be done for the cache to work properly:
    * Make sure cache operations are aligned to 32B
    * Make sure to clean and invalidate the operations on gmac descriptors
      (thus all the helper functions)
    
    This commit is needed for SAM GMAC to work when caches are enabled and
    MPU mapping is changed to cacheable (See #8185)
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>
    
    Former-commit-id: a313e5c74f01750411bf65e9903278f913328585

--- drivers/ethernet/eth_sam_gmac.c
+++ drivers/ethernet/eth_sam_gmac.c
@@ -1,6 +1,10 @@
 DECL|MODULO_INC|macro|MODULO_INC
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|dcache_clean|function|static inline void dcache_clean(u32_t addr, u32_t size)
+DECL|dcache_clean|macro|dcache_clean
+DECL|dcache_invalidate|function|static inline void dcache_invalidate(u32_t addr, u32_t size)
+DECL|dcache_invalidate|macro|dcache_invalidate
 DECL|eth0_config|variable|eth0_config
 DECL|eth0_data|variable|eth0_data
 DECL|eth0_iface_init|function|static void eth0_iface_init(struct net_if *iface)
@@ -15,6 +19,12 @@ DECL|free_rx_bufs|function|static void free_rx_bufs(struct ring_buf *rx_frag_lis
 DECL|get_iface|function|static inline struct net_if *get_iface(struct eth_sam_dev_data *ctx, u16_t vlan_tag)
 DECL|get_mac_addr_from_i2c_eeprom|function|void get_mac_addr_from_i2c_eeprom(u8_t mac_addr[6])
 DECL|get_mck_clock_divisor|function|static int get_mck_clock_divisor(u32_t mck)
+DECL|gmac_desc_append_w0|function|static inline void gmac_desc_append_w0(struct gmac_desc *desc, u32_t value)
+DECL|gmac_desc_append_w1|function|static inline void gmac_desc_append_w1(struct gmac_desc *desc, u32_t value)
+DECL|gmac_desc_get_w0|function|static inline u32_t gmac_desc_get_w0(struct gmac_desc *desc)
+DECL|gmac_desc_get_w1|function|static inline u32_t gmac_desc_get_w1(struct gmac_desc *desc)
+DECL|gmac_desc_set_w0|function|static inline void gmac_desc_set_w0(struct gmac_desc *desc, u32_t value)
+DECL|gmac_desc_set_w1|function|static inline void gmac_desc_set_w1(struct gmac_desc *desc, u32_t value)
 DECL|gmac_init|function|static int gmac_init(Gmac *gmac, u32_t gmac_ncfgr_val)
 DECL|link_configure|function|static void link_configure(Gmac *gmac, u32_t flags)
 DECL|mac_addr_set|function|static void mac_addr_set(Gmac *gmac, u8_t index, u8_t mac_addr[6])
