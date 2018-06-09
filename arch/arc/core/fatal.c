35db5df14be3c4b897e2bb641269999ea512d6b1
Wayne Ren <wei.ren@synopsys.com>
2018-06-09 22:08:04 +0800

    arch: arc: use top of isr stack as exception stack and bug fixes
    * re-use top of isr stack as exception stack
    * bug fixes in irq offload's implementation
    * improve kernel oops implementation
    
    Signed-off-by: Wayne Ren <wei.ren@synopsys.com>
    
    Former-commit-id: 97d043648690eeb7ea0164d8f228075adbcc8cb9

--- arch/arc/core/fatal.c
+++ arch/arc/core/fatal.c
@@ -1,3 +1,2 @@
 DECL|_NanoFatalErrorHandler|function|void _NanoFatalErrorHandler(unsigned int reason, const NANO_ESF *pEsf)
 DECL|_arch_syscall_oops|function|FUNC_NORETURN void _arch_syscall_oops(void *ssf_ptr)
-DECL|_do_kernel_oops|function|void _do_kernel_oops(const NANO_ESF *esf)
