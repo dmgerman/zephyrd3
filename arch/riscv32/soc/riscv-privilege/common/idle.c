6cb6b037ce42dfb589878620ca4850ef8da7df09
Karol Gugala <kgugala@antmicro.com>
2018-06-10 19:02:14 +0200

    riscv32: riscv-privilege: integrate common code
    This commit moves code from fe310 platform into RISC-V privilege common
    folder. This way the code can be reused by other platforms in future.
    
    signed-off-by: Karol Gugala <kgugala@antmicro.com>
    
    Former-commit-id: 23a5b5d171c87d354a4204111ccd6ab95c87e39f

--- arch/riscv32/soc/riscv-privilege/common/idle.c
+++ arch/riscv32/soc/riscv-privilege/common/idle.c
@@ -1,3 +1,3 @@
-DECL|fe310_idle|function|static ALWAYS_INLINE void fe310_idle(unsigned int key)
 DECL|k_cpu_atomic_idle|function|void k_cpu_atomic_idle(unsigned int key)
 DECL|k_cpu_idle|function|void k_cpu_idle(void)
+DECL|riscv_idle|function|static ALWAYS_INLINE void riscv_idle(unsigned int key)
--- drivers/interrupt_controller/plic.c
+++ drivers/interrupt_controller/plic.c
@@ -1,9 +1,7 @@
-DECL|PLIC_FE310_EN_SIZE|macro|PLIC_FE310_EN_SIZE
-DECL|PLIC_FE310_IRQS|macro|PLIC_FE310_IRQS
 DECL|claim_complete|member|u32_t claim_complete;
-DECL|plic_fe310_init|function|static int plic_fe310_init(struct device *dev)
-DECL|plic_fe310_irq_handler|function|static void plic_fe310_irq_handler(void *arg)
-DECL|plic_fe310_regs_t|struct|struct plic_fe310_regs_t {
+DECL|plic_init|function|static int plic_init(struct device *dev)
+DECL|plic_irq_handler|function|static void plic_irq_handler(void *arg)
+DECL|plic_regs_t|struct|struct plic_regs_t {
 DECL|riscv_plic_get_irq|function|int riscv_plic_get_irq(void)
 DECL|riscv_plic_irq_disable|function|void riscv_plic_irq_disable(u32_t irq)
 DECL|riscv_plic_irq_enable|function|void riscv_plic_irq_enable(u32_t irq)
