c844ef4697056b5be30a9b4cc4a4897a3c44e6c7
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-06-11 09:42:03 +0200

    net/arp: Clear cache per-iface when relevant
    When disabling an ethernet interface, only its cache entries need to be
    cleared up and not the whole cache. This is meaninful in case there is
    2+ ethernet interface instances.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 97699537f300ba46f9d36aae5bc1761de0644049

--- subsys/net/ip/l2/ethernet/arp.c
+++ subsys/net/ip/l2/ethernet/arp.c
@@ -5,7 +5,7 @@ DECL|arp_table|variable|arp_table
 DECL|arp_update|function|static inline void arp_update(struct net_if *iface, struct in_addr *src, struct net_eth_addr *hwaddr)
 DECL|find_entry|function|static inline struct arp_entry *find_entry(struct net_if *iface, struct in_addr *dst, struct arp_entry **free_entry, struct arp_entry **non_pending)
 DECL|if_get_addr|function|static inline struct in_addr *if_get_addr(struct net_if *iface)
-DECL|net_arp_clear_cache|function|void net_arp_clear_cache(void)
+DECL|net_arp_clear_cache|function|void net_arp_clear_cache(struct net_if *iface)
 DECL|net_arp_foreach|function|int net_arp_foreach(net_arp_cb_t cb, void *user_data)
 DECL|net_arp_init|function|void net_arp_init(void)
 DECL|net_arp_input|function|enum net_verdict net_arp_input(struct net_pkt *pkt)
