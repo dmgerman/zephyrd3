ee5caab78b1399365ab98e3a62feff28598757c6
Spoorthi K <spoorthi.k@intel.com>
2018-06-12 16:35:49 +0530

    tests: obj_tracing: Enhance object counter logic
    Restructured the test, added description and enhanced
    semaphore count logic to check for semaphores created
    only in the test.
    
    Fixes: #7106
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: a3fe7af2dda5f1a181aed86643a895eaf88ae61a

--- tests/kernel/obj_tracing/src/main.c
+++ tests/kernel/obj_tracing/src/main.c
@@ -1,6 +1,23 @@
+DECL|FORK|macro|FORK
+DECL|GIVE|macro|GIVE
+DECL|IPM_THREAD|macro|IPM_THREAD
+DECL|IPM_THREAD|macro|IPM_THREAD
+DECL|N_PHILOSOPHERS|macro|N_PHILOSOPHERS
+DECL|OBJ_LIST_NAME|macro|OBJ_LIST_NAME
+DECL|OBJ_LIST_TYPE|macro|OBJ_LIST_TYPE
+DECL|RANDDELAY|macro|RANDDELAY
 DECL|STSIZE|macro|STSIZE
+DECL|TAKE|macro|TAKE
+DECL|TOTAL_OBJECTS|macro|TOTAL_OBJECTS
+DECL|TOTAL_TEST_NUMBER|macro|TOTAL_TEST_NUMBER
+DECL|TOTAL_THREADS|macro|TOTAL_THREADS
+DECL|ZTEST_THREADS_CREATED|macro|ZTEST_THREADS_CREATED
+DECL|force_sys_work_q_in|variable|force_sys_work_q_in
 DECL|forks|variable|forks
 DECL|mon_data|variable|mon_data
+DECL|object_monitor|function|static void object_monitor(void)
 DECL|phil_data|variable|phil_data
+DECL|phil_entry|function|static void phil_entry(void)
 DECL|test_main|function|void test_main(void)
+DECL|test_thread_monitor|function|static inline int test_thread_monitor(void)
 DECL|test_tracing|function|void test_tracing(void)
--- tests/kernel/obj_tracing/src/philosopher.c
+++
@@ -1,5 +0,0 @@
-DECL|FORK|macro|FORK
-DECL|GIVE|macro|GIVE
-DECL|RANDDELAY|macro|RANDDELAY
-DECL|TAKE|macro|TAKE
-DECL|phil_entry|function|void phil_entry(void)
