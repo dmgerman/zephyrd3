8fb71ce8546c5ceab4d84e0bc0c9367812554cb6
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-06-15 00:25:23 +0200

    arch: arm: refactor function to align with the adopted api
    Refactor arm_cmse_mpu_region_get(.) function, so its signature,
    documentation, and implementation align with the rest of functions
    for obtaining MPU, SAU and IDAU region numbers via the TT instruction.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: d426adccaa6fa94726c526fb2f5272854fe3dc48

--- arch/arm/core/cortex_m/cmse/arm_core_cmse.c
+++ arch/arm/core/cortex_m/cmse/arm_core_cmse.c
@@ -10,5 +10,5 @@ DECL|arm_cmse_addr_read_write_ok|function|static int arm_cmse_addr_read_write_ok
 DECL|arm_cmse_addr_readwrite_ok|function|int arm_cmse_addr_readwrite_ok(u32_t addr, int force_npriv)
 DECL|arm_cmse_idau_region_get|function|int arm_cmse_idau_region_get(u32_t addr)
 DECL|arm_cmse_mpu_nonsecure_region_get|function|int arm_cmse_mpu_nonsecure_region_get(u32_t addr)
-DECL|arm_cmse_mpu_region_get|function|int arm_cmse_mpu_region_get(u32_t addr, u8_t *p_region)
+DECL|arm_cmse_mpu_region_get|function|int arm_cmse_mpu_region_get(u32_t addr)
 DECL|arm_cmse_sau_region_get|function|int arm_cmse_sau_region_get(u32_t addr)
