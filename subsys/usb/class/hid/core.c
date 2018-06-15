a9c42d80b763c4fcd817f9c955a51541ff1b7f92
Johann Fischer <j.fischer@phytec.de>
2018-06-16 01:35:51 +0200

    usb: hid: fix write to interrupt IN endpoint
    A HID application can no longer write to the default
    interrupt IN endpoint because the addresses are assigned
    dynamically. Add hid_int_ep_write() function  and leave
    it to the hid-core to call the usb_write() with the correct
    endpoint address.
    
    fixes: #8424
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: 085a8b75c571a19daaa8425e07edc0c32cfa9e27

--- subsys/usb/class/hid/core.c
+++ subsys/usb/class/hid/core.c
@@ -1,3 +1,5 @@
+DECL|HID_INT_IN_EP_IDX|macro|HID_INT_IN_EP_IDX
+DECL|HID_INT_OUT_EP_IDX|macro|HID_INT_OUT_EP_IDX
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|__packed|variable|__packed
@@ -6,6 +8,7 @@ DECL|hid_custom_handle_req|function|static int hid_custom_handle_req(struct usb_
 DECL|hid_device_info|struct|static struct hid_device_info {
 DECL|hid_device|variable|hid_device
 DECL|hid_ep_data|variable|hid_ep_data
+DECL|hid_int_ep_write|function|int hid_int_ep_write(const u8_t *data, u32_t data_len, u32_t *bytes_ret)
 DECL|hid_int_in|function|static void hid_int_in(u8_t ep, enum usb_dc_ep_cb_status_code ep_status)
 DECL|hid_interface_config|function|static void hid_interface_config(u8_t bInterfaceNumber)
 DECL|hid_status_cb|function|static void hid_status_cb(enum usb_dc_status_code status, u8_t *param)
