dmgerman/zephyrd3

commit 371e1d32164c9547a969e68d0ba3e6d291d227d4

driver: usb: add check for endpoint capabilities
Add function to check capabilities of an endpoint.
Only basic properties are checked, especially on STM32
capabilities of different USB controller configurations
have to be considered in the future.

Signed-off-by: Johann Fischer <j.fischer@phytec.de>

Former-commit-id: 52eacf16a24e4c335dae2c3aed2f427d61e49128

drivers/usb/device/usb_dc_stm32.c
@@ -45,6 +45,7 @@ DECL|read_count|member|u32_t read_count; /** Number of bytes in read buffer */
 DECL|read_offset|member|u32_t read_offset; /** Current offset in read buffer */
 DECL|status_cb|member|usb_dc_status_callback status_cb; /* Status callback */
 DECL|usb_dc_attach|function|int usb_dc_attach(void)
+DECL|usb_dc_ep_check_cap|function|int usb_dc_ep_check_cap(const struct usb_dc_ep_cfg_data * const cfg)
 DECL|usb_dc_ep_clear_stall|function|int usb_dc_ep_clear_stall(const u8_t ep)
 DECL|usb_dc_ep_configure|function|int usb_dc_ep_configure(const struct usb_dc_ep_cfg_data * const ep_cfg)
 DECL|usb_dc_ep_disable|function|int usb_dc_ep_disable(const u8_t ep)

drivers/usb/device/usb_dc_dw.c
@@ -17,6 +17,7 @@ DECL|out_ep_ctrl|member|struct usb_ep_ctrl_prv out_ep_ctrl[USB_DW_OUT_EP_NUM];
 DECL|status_cb|member|usb_dc_status_callback status_cb;
 DECL|usb_dc_attach|function|int usb_dc_attach(void)
 DECL|usb_dc_detach|function|int usb_dc_detach(void)
+DECL|usb_dc_ep_check_cap|function|int usb_dc_ep_check_cap(const struct usb_dc_ep_cfg_data * const cfg)
 DECL|usb_dc_ep_clear_stall|function|int usb_dc_ep_clear_stall(const u8_t ep)
 DECL|usb_dc_ep_configure|function|int usb_dc_ep_configure(const struct usb_dc_ep_cfg_data * const ep_cfg)
 DECL|usb_dc_ep_disable|function|int usb_dc_ep_disable(const u8_t ep)

drivers/usb/device/usb_dc_kinetis.c
@@ -71,6 +71,7 @@ DECL|tok_pid|member|u32_t tok_pid : 4;
 DECL|type|member|u8_t type;
 DECL|usb_dc_attach|function|int usb_dc_attach(void)
 DECL|usb_dc_detach|function|int usb_dc_detach(void)
+DECL|usb_dc_ep_check_cap|function|int usb_dc_ep_check_cap(const struct usb_dc_ep_cfg_data * const cfg)
 DECL|usb_dc_ep_clear_stall|function|int usb_dc_ep_clear_stall(const u8_t ep)
 DECL|usb_dc_ep_configure|function|int usb_dc_ep_configure(const struct usb_dc_ep_cfg_data * const cfg)
 DECL|usb_dc_ep_disable|function|int usb_dc_ep_disable(const u8_t ep)

drivers/usb/device/usb_dc_nrf5.c
@@ -136,6 +136,7 @@ DECL|status_code|member|enum usb_dc_status_code status_code;
 DECL|type|member|enum usb_dc_ep_type type;
 DECL|usb_dc_attach|function|int usb_dc_attach(void)
 DECL|usb_dc_detach|function|int usb_dc_detach(void)
+DECL|usb_dc_ep_check_cap|function|int usb_dc_ep_check_cap(const struct usb_dc_ep_cfg_data * const cfg)
 DECL|usb_dc_ep_clear_stall|function|int usb_dc_ep_clear_stall(const u8_t ep)
 DECL|usb_dc_ep_configure|function|int usb_dc_ep_configure(const struct usb_dc_ep_cfg_data * const ep_cfg)
 DECL|usb_dc_ep_disable|function|int usb_dc_ep_disable(const u8_t ep)

drivers/usb/device/usb_dc_sam0.c
@@ -18,6 +18,7 @@ DECL|ep_cb|member|usb_dc_ep_callback ep_cb[2][USB_NUM_ENDPOINTS];
 DECL|out_at|member|u32_t out_at;
 DECL|usb_dc_attach|function|int usb_dc_attach(void)
 DECL|usb_dc_detach|function|int usb_dc_detach(void)
+DECL|usb_dc_ep_check_cap|function|int usb_dc_ep_check_cap(const struct usb_dc_ep_cfg_data * const cfg)
 DECL|usb_dc_ep_clear_stall|function|int usb_dc_ep_clear_stall(const u8_t ep)
 DECL|usb_dc_ep_configure|function|int usb_dc_ep_configure(const struct usb_dc_ep_cfg_data *const cfg)
 DECL|usb_dc_ep_enable|function|int usb_dc_ep_enable(const u8_t ep)

commit c25a854e09b44a1ef61e4ffdeabf8d57052726cd

drivers: usb_dc_stm32: change all endpoints to bidirectional
The various STM32 reference manuals sometimes define the USB endpoints
as IN or OUT only and sometimes as bidirectional, even in the same
manual. This is likely because the OTG implementation has one set of
registers for the IN endpoints and one other set for OUT endpoints.
However at the end a given endpoint address can both transmit and
receive data.

This causes some confusion how to declare the endpoints in the device
tree, and depending on the SoC, they are either the same number of IN
and OUT endpoints declared, or they are declared as bidirectional. At
the end it doesn't really matter given how the driver uses those values:

    #define NUM_IN_EP (CONFIG_USB_NUM_BIDIR_ENDPOINTS + \
                       CONFIG_USB_NUM_IN_ENDPOINTS)

    #define NUM_OUT_EP (CONFIG_USB_NUM_BIDIR_ENDPOINTS + \
                        CONFIG_USB_NUM_OUT_ENDPOINTS)

    #define NUM_BIDIR_EP NUM_OUT_EP

This patch therefore cleanup the driver, the DTS, and the DTS fixups to
only define the number of bidirectional endpoints.

In addition to the cleanup, that fixes a regression introduced by commit
371e1d3216 [formerly 52eacf16a2] ("driver: usb: add check for endpoint capabilities"), which
introduced a wrong check for SoC only defining the number of
bidirectional endpoints.

Signed-off-by: Aurelien Jarno <aurelien@aurel32.net>

Former-commit-id: 7688f490652a58a4485d6eeb41aeeea15ba0fd77

arch/arm/soc/st_stm32/stm32f0/dts.fixup
@@ -54,8 +54,6 @@
 #define CONFIG_USB_IRQ				ST_STM32_USB_40005C00_IRQ_USB
 #define CONFIG_USB_IRQ_PRI			ST_STM32_USB_40005C00_IRQ_USB_PRIORITY
 #define CONFIG_USB_NUM_BIDIR_ENDPOINTS		ST_STM32_USB_40005C00_NUM_BIDIR_ENDPOINTS
-#define CONFIG_USB_NUM_IN_ENDPOINTS		ST_STM32_USB_40005C00_NUM_IN_ENDPOINTS
-#define CONFIG_USB_NUM_OUT_ENDPOINTS		ST_STM32_USB_40005C00_NUM_OUT_ENDPOINTS
 #define CONFIG_USB_RAM_SIZE			ST_STM32_USB_40005C00_RAM_SIZE
 
 #define CONFIG_PWM_STM32_1_DEV_NAME             ST_STM32_PWM_40012C00_PWM_LABEL

arch/arm/soc/st_stm32/stm32f3/dts.fixup
@@ -57,8 +57,6 @@
 #define CONFIG_USB_IRQ				ST_STM32_USB_40005C00_IRQ_USB
 #define CONFIG_USB_IRQ_PRI			ST_STM32_USB_40005C00_IRQ_USB_PRIORITY
 #define CONFIG_USB_NUM_BIDIR_ENDPOINTS		ST_STM32_USB_40005C00_NUM_BIDIR_ENDPOINTS
-#define CONFIG_USB_NUM_IN_ENDPOINTS		ST_STM32_USB_40005C00_NUM_IN_ENDPOINTS
-#define CONFIG_USB_NUM_OUT_ENDPOINTS		ST_STM32_USB_40005C00_NUM_OUT_ENDPOINTS
 #define CONFIG_USB_RAM_SIZE			ST_STM32_USB_40005C00_RAM_SIZE
 
 #define CONFIG_PWM_STM32_1_DEV_NAME             ST_STM32_PWM_40012C00_PWM_LABEL

arch/arm/soc/st_stm32/stm32f4/dts.fixup
@@ -87,8 +87,6 @@
 #define CONFIG_USB_IRQ				ST_STM32_OTGFS_50000000_IRQ_OTGFS
 #define CONFIG_USB_IRQ_PRI			ST_STM32_OTGFS_50000000_IRQ_OTGFS_PRIORITY
 #define CONFIG_USB_NUM_BIDIR_ENDPOINTS		ST_STM32_OTGFS_50000000_NUM_BIDIR_ENDPOINTS
-#define CONFIG_USB_NUM_IN_ENDPOINTS		ST_STM32_OTGFS_50000000_NUM_IN_ENDPOINTS
-#define CONFIG_USB_NUM_OUT_ENDPOINTS		ST_STM32_OTGFS_50000000_NUM_OUT_ENDPOINTS
 #define CONFIG_USB_RAM_SIZE			ST_STM32_OTGFS_50000000_RAM_SIZE
 
 #define CONFIG_PWM_STM32_1_DEV_NAME             ST_STM32_PWM_40010000_PWM_LABEL

arch/arm/soc/st_stm32/stm32l0/dts.fixup
@@ -52,8 +52,6 @@
 #define CONFIG_USB_IRQ				ST_STM32_USB_40005C00_IRQ_USB
 #define CONFIG_USB_IRQ_PRI			ST_STM32_USB_40005C00_IRQ_USB_PRIORITY
 #define CONFIG_USB_NUM_BIDIR_ENDPOINTS		ST_STM32_USB_40005C00_NUM_BIDIR_ENDPOINTS
-#define CONFIG_USB_NUM_IN_ENDPOINTS		ST_STM32_USB_40005C00_NUM_IN_ENDPOINTS
-#define CONFIG_USB_NUM_OUT_ENDPOINTS		ST_STM32_USB_40005C00_NUM_OUT_ENDPOINTS
 #define CONFIG_USB_RAM_SIZE			ST_STM32_USB_40005C00_RAM_SIZE
 
 /* End of SoC Level DTS fixup file */

arch/arm/soc/st_stm32/stm32l4/dts.fixup
@@ -93,8 +93,6 @@
 #define CONFIG_USB_IRQ				ST_STM32_OTGFS_50000000_IRQ_OTGFS
 #define CONFIG_USB_IRQ_PRI			ST_STM32_OTGFS_50000000_IRQ_OTGFS_PRIORITY
 #define CONFIG_USB_NUM_BIDIR_ENDPOINTS		ST_STM32_OTGFS_50000000_NUM_BIDIR_ENDPOINTS
-#define CONFIG_USB_NUM_IN_ENDPOINTS		ST_STM32_OTGFS_50000000_NUM_IN_ENDPOINTS
-#define CONFIG_USB_NUM_OUT_ENDPOINTS		ST_STM32_OTGFS_50000000_NUM_OUT_ENDPOINTS
 #define CONFIG_USB_RAM_SIZE			ST_STM32_OTGFS_50000000_RAM_SIZE
 #endif
 

drivers/usb/device/usb_dc_stm32.c
@@ -1,5 +1,3 @@
-DECL|CONFIG_USB_NUM_IN_ENDPOINTS|macro|CONFIG_USB_NUM_IN_ENDPOINTS
-DECL|CONFIG_USB_NUM_OUT_ENDPOINTS|macro|CONFIG_USB_NUM_OUT_ENDPOINTS
 DECL|EP0_IDX|macro|EP0_IDX
 DECL|EP0_IN|macro|EP0_IN
 DECL|EP0_MPS|macro|EP0_MPS
@@ -26,19 +24,16 @@ DECL|HAL_PCD_ResetCallback|function|void HAL_PCD_ResetCallback(PCD_HandleTypeDef
 DECL|HAL_PCD_ResumeCallback|function|void HAL_PCD_ResumeCallback(PCD_HandleTypeDef *hpcd)
 DECL|HAL_PCD_SetupStageCallback|function|void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef *hpcd)
 DECL|HAL_PCD_SuspendCallback|function|void HAL_PCD_SuspendCallback(PCD_HandleTypeDef *hpcd)
-DECL|NUM_BIDIR_EP|macro|NUM_BIDIR_EP
-DECL|NUM_IN_EP|macro|NUM_IN_EP
-DECL|NUM_OUT_EP|macro|NUM_OUT_EP
 DECL|SETUP_SIZE|macro|SETUP_SIZE
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|USB_BTABLE_SIZE|macro|USB_BTABLE_SIZE
 DECL|cb|member|usb_dc_ep_callback cb; /** Endpoint callback function */
-DECL|ep_buf|member|u8_t ep_buf[NUM_OUT_EP][EP_MPS];
+DECL|ep_buf|member|u8_t ep_buf[CONFIG_USB_NUM_BIDIR_ENDPOINTS][EP_MPS];
 DECL|ep_mps|member|u16_t ep_mps; /** Endpoint max packet size */
 DECL|ep_stalled|member|u8_t ep_stalled; /** Endpoint stall flag */
 DECL|ep_type|member|u8_t ep_type; /** Endpoint type (STM32 HAL enum) */
-DECL|in_ep_state|member|struct usb_dc_stm32_ep_state in_ep_state[NUM_IN_EP];
-DECL|out_ep_state|member|struct usb_dc_stm32_ep_state out_ep_state[NUM_OUT_EP];
+DECL|in_ep_state|member|struct usb_dc_stm32_ep_state in_ep_state[CONFIG_USB_NUM_BIDIR_ENDPOINTS];
+DECL|out_ep_state|member|struct usb_dc_stm32_ep_state out_ep_state[CONFIG_USB_NUM_BIDIR_ENDPOINTS];
 DECL|pcd|member|PCD_HandleTypeDef pcd; /* Storage for the HAL_PCD api */
 DECL|pma_offset|member|u32_t pma_offset;
 DECL|read_count|member|u32_t read_count; /** Number of bytes in read buffer */

dts/arm/st/stm32f070.dtsi
@@ -24,8 +24,6 @@
 			interrupts = <31 0>;
 			interrupt-names = "usb";
 			num-bidir-endpoints = <8>;
-			num-in-endpoints = <0>;
-			num-out-endpoints = <0>;
 			ram-size = <1024>;
 			status = "disabled";
 			label= "USB";

dts/arm/st/stm32f072.dtsi
@@ -51,8 +51,6 @@
 			interrupts = <31 0>;
 			interrupt-names = "usb";
 			num-bidir-endpoints = <8>;
-			num-in-endpoints = <0>;
-			num-out-endpoints = <0>;
 			ram-size = <1024>;
 			status = "disabled";
 			label= "USB";

dts/arm/st/stm32f3.dtsi
@@ -161,8 +161,6 @@
 			interrupts = <20 0>;
 			interrupt-names = "usb";
 			num-bidir-endpoints = <8>;
-			num-in-endpoints = <0>;
-			num-out-endpoints = <0>;
 			ram-size = <512>;
 			status = "disabled";
 			label= "USB";

dts/arm/st/stm32f4.dtsi
@@ -196,9 +196,7 @@
 			reg = <0x50000000 0x40000>;
 			interrupts = <67 0>;
 			interrupt-names = "otgfs";
-			num-bidir-endpoints = <1>;
-			num-in-endpoints = <3>;
-			num-out-endpoints = <3>;
+			num-bidir-endpoints = <4>;
 			ram-size = <1280>;
 			status = "disabled";
 			label= "OTGFS";

dts/arm/st/stm32l072.dtsi
@@ -60,8 +60,6 @@
 			interrupts = <31 0>;
 			interrupt-names = "usb";
 			num-bidir-endpoints = <8>;
-			num-in-endpoints = <0>;
-			num-out-endpoints = <0>;
 			ram-size = <1024>;
 			status = "disabled";
 			label= "USB";

dts/arm/st/stm32l073.dtsi
@@ -48,8 +48,6 @@
 			interrupts = <31 0>;
 			interrupt-names = "usb";
 			num-bidir-endpoints = <8>;
-			num-in-endpoints = <0>;
-			num-out-endpoints = <0>;
 			ram-size = <1024>;
 			status = "disabled";
 			label= "USB";

dts/arm/st/stm32l475.dtsi
@@ -104,9 +104,7 @@
 			reg = <0x50000000 0x40000>;
 			interrupts = <67 0>;
 			interrupt-names = "otgfs";
-			num-bidir-endpoints = <1>;
-			num-in-endpoints = <5>;
-			num-out-endpoints = <5>;
+			num-bidir-endpoints = <6>;
 			ram-size = <1280>;
 			status = "disabled";
 			label= "OTGFS";