056491037e79cc838c5bd7f5c7e32a345d028ec4
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2018-06-18 10:35:43 +0200

    arch: arm: nrf: Use SystemInit() from MDK in SoC initialization
    Replace code that handles erratas and performs other SoC-specific
    initialization, that was actually copied from SystemInit() provided
    in MDK for particular SoCs, with a call to SystemInit().
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
    
    Former-commit-id: 687355c9af5ab8bd5f739e22eba26d94f75d615c

--- arch/arm/soc/nordic_nrf/nrf51/soc.c
+++ arch/arm/soc/nordic_nrf/nrf51/soc.c
@@ -1,7 +1,3 @@
 DECL|NMI_INIT|macro|NMI_INIT
 DECL|NMI_INIT|macro|NMI_INIT
-DECL|__SYSTEM_CLOCK|macro|__SYSTEM_CLOCK
-DECL|__used|variable|__used
-DECL|ftpan_26|function|static bool ftpan_26(void)
-DECL|ftpan_59|function|static bool ftpan_59(void)
 DECL|nordicsemi_nrf51_init|function|static int nordicsemi_nrf51_init(struct device *arg)
--- arch/arm/soc/nordic_nrf/nrf52/soc.c
+++ arch/arm/soc/nordic_nrf/nrf52/soc.c
@@ -1,18 +1,3 @@
 DECL|NMI_INIT|macro|NMI_INIT
 DECL|NMI_INIT|macro|NMI_INIT
-DECL|__SYSTEM_CLOCK_64M|macro|__SYSTEM_CLOCK_64M
-DECL|__used|variable|__used
-DECL|clock_init|function|static void clock_init(void)
-DECL|errata_103|function|static bool errata_103(void)
-DECL|errata_115|function|static bool errata_115(void)
-DECL|errata_120|function|static bool errata_120(void)
-DECL|errata_136_nrf52832|function|static bool errata_136_nrf52832(void)
-DECL|errata_136_nrf52840|function|static bool errata_136_nrf52840(void)
-DECL|errata_36|function|static bool errata_36(void)
-DECL|errata_98|function|static bool errata_98(void)
-DECL|ftpan_32|function|static bool ftpan_32(void)
-DECL|ftpan_36|function|static bool ftpan_36(void)
-DECL|ftpan_37|function|static bool ftpan_37(void)
-DECL|nordicsemi_nrf52832_init|function|static void nordicsemi_nrf52832_init(void)
-DECL|nordicsemi_nrf52840_init|function|static void nordicsemi_nrf52840_init(void)
 DECL|nordicsemi_nrf52_init|function|static int nordicsemi_nrf52_init(struct device *arg)
