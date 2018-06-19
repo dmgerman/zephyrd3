80e4b5ec0b156738c8a6583c6a87b502ae460c9c
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-06-19 09:35:35 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of Thread APIs
    Some test programs to illustrate the usage of thread APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: 0ab496b5bea22dda30b6d8ab1598479c14a4bfac

---
+++ tests/cmsis_rtos_v1/src/thread_apis.c
@@ -0,0 +1,8 @@
+DECL|STACKSZ|macro|STACKSZ
+DECL|test_thread_apis|function|void test_thread_apis(void)
+DECL|test_thread_prio|function|void test_thread_prio(void)
+DECL|thread1|function|void thread1(void const *argument)
+DECL|thread2|function|void thread2(void const *argument)
+DECL|thread3_state|variable|thread3_state
+DECL|thread3|function|void thread3(void const *argument)
+DECL|thread_yield_check|variable|thread_yield_check
