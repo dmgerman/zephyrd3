bc00e0fab898dfa141ba39e0871209f7b85994b8
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-06-19 10:13:51 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of Timer APIs
    Some test programs to illustrate the usage of timer APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: a57d994aaafe2319f91b27770993a477c0ad9b19

---
+++ tests/cmsis_rtos_v1/src/timer.c
@@ -0,0 +1,8 @@
+DECL|NUM_PERIODS|macro|NUM_PERIODS
+DECL|ONESHOT_TIME|macro|ONESHOT_TIME
+DECL|PERIOD|macro|PERIOD
+DECL|Timer1_Callback|function|void Timer1_Callback(void const *arg)
+DECL|Timer2_Callback|function|void Timer2_Callback(void const *arg)
+DECL|num_oneshots_executed|variable|num_oneshots_executed
+DECL|num_periods_executed|variable|num_periods_executed
+DECL|test_timer|function|void test_timer(void)
