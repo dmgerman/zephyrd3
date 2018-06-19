164a985eb973a4ca8945a977ed97a6cb6a43b03a
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-06-19 16:07:39 +0530

    benchmarks: timing_info: Add hooks in the kernel for userspace.
    Added sampling hooks in the kernel needed for userspace benchmarks.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: a8b0b0d5e8352d7258c10c7170802ce8bf0ee3e9

--- arch/common/timing_info_bench.c
+++ arch/common/timing_info_bench.c
@@ -5,6 +5,7 @@ DECL|TIMING_INFO_OS_GET_TIME|macro|TIMING_INFO_OS_GET_TIME
 DECL|TIMING_INFO_PRE_READ|macro|TIMING_INFO_PRE_READ
 DECL|TIMING_INFO_PRE_READ|macro|TIMING_INFO_PRE_READ
 DECL|__common_var_swap_end_time|variable|__common_var_swap_end_time
+DECL|__end_drop_to_usermode_time|variable|__end_drop_to_usermode_time
 DECL|__end_intr_time|variable|__end_intr_time
 DECL|__end_swap_time|variable|__end_swap_time
 DECL|__end_tick_time|variable|__end_tick_time
@@ -15,6 +16,7 @@ DECL|__start_tick_time|variable|__start_tick_time
 DECL|read_timer_end_of_isr|function|void read_timer_end_of_isr(void)
 DECL|read_timer_end_of_swap|function|void read_timer_end_of_swap(void)
 DECL|read_timer_end_of_tick_handler|function|void read_timer_end_of_tick_handler(void)
+DECL|read_timer_end_of_userspace_enter|function|void read_timer_end_of_userspace_enter(void)
 DECL|read_timer_start_of_isr|function|void read_timer_start_of_isr(void)
 DECL|read_timer_start_of_swap|function|void read_timer_start_of_swap(void)
 DECL|read_timer_start_of_tick_handler|function|void read_timer_start_of_tick_handler(void)
