c0b24cf2f7e9d62c21012efeddbfd5079d14cad5
Alberto Escolar Piedras <alpi@oticon.com>
2018-06-22 09:55:50 +0200

    drivers: entropy: native: implement standard ISR-specific call
    For the native_posix board, implement the new get_entropy_isr call.
    The old native get_entropy was already safe for ISRs, so we just
    add a wrapper to it.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: 5ea637d23b42aff5a6fd6656ef9270ed24242f3b

--- drivers/entropy/fake_entropy_native_posix.c
+++ drivers/entropy/fake_entropy_native_posix.c
@@ -1,4 +1,5 @@
 DECL|entropy_native_posix_api_funcs|variable|entropy_native_posix_api_funcs
+DECL|entropy_native_posix_get_entropy_isr|function|static int entropy_native_posix_get_entropy_isr(struct device *dev, u8_t *buf,u16_t len, u32_t flags)
 DECL|entropy_native_posix_get_entropy|function|static int entropy_native_posix_get_entropy(struct device *dev, u8_t *buffer, u16_t length)
 DECL|entropy_native_posix_init|function|static int entropy_native_posix_init(struct device *dev)
 DECL|entropy_native_posix_set_seed|function|void entropy_native_posix_set_seed(unsigned int seed_i)
