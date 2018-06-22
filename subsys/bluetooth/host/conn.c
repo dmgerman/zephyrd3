dmgerman/zephyrd3

commit ac26b7c2f06db3d07fdc5fa8bd4b9ddbcb1321f5

Bluetooth: Compress bt_keys struct
There's a bit of unnecessary space in the bt_keys struct. Re-design
some fields for a more compact format, which is particularly helpful
now that the struct gets stored as-is to flash through the settings
API.

Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>

Former-commit-id: 6af5d1cd1f81e4e195a94dbc6f2ab513bb5400fc

subsys/bluetooth/host/conn.c
@@ -37,7 +37,7 @@ DECL|bt_conn_identity_resolved|function|void bt_conn_identity_resolved(struct bt
 DECL|bt_conn_init|function|int bt_conn_init(void)
 DECL|bt_conn_le_conn_update|function|int bt_conn_le_conn_update(struct bt_conn *conn, const struct bt_le_conn_param *param)
 DECL|bt_conn_le_param_update|function|int bt_conn_le_param_update(struct bt_conn *conn, const struct bt_le_conn_param *param)
-DECL|bt_conn_le_start_encryption|function|int bt_conn_le_start_encryption(struct bt_conn *conn, u64_t rand,u16_t ediv, const u8_t *ltk, size_t len)
+DECL|bt_conn_le_start_encryption|function|int bt_conn_le_start_encryption(struct bt_conn *conn, u8_t rand[8], u16_t ediv,const u8_t *ltk, size_t len)
 DECL|bt_conn_lookup_addr_br|function|struct bt_conn *bt_conn_lookup_addr_br(const bt_addr_t *peer)
 DECL|bt_conn_lookup_addr_le|function|struct bt_conn *bt_conn_lookup_addr_le(const bt_addr_le_t *peer)
 DECL|bt_conn_lookup_addr_sco|function|struct bt_conn *bt_conn_lookup_addr_sco(const bt_addr_t *peer)

subsys/bluetooth/host/keys.h
@@ -1,20 +1,18 @@
 DECL|BT_KEYS_ALL|enumerator|BT_KEYS_ALL = (BT_KEYS_SLAVE_LTK | BT_KEYS_IRK | \
-DECL|BT_KEYS_AUTHENTICATED|enumerator|BT_KEYS_AUTHENTICATED,
-DECL|BT_KEYS_DEBUG|enumerator|BT_KEYS_DEBUG,
-DECL|BT_KEYS_ID_PENDING_ADD|enumerator|BT_KEYS_ID_PENDING_ADD,
-DECL|BT_KEYS_ID_PENDING_DEL|enumerator|BT_KEYS_ID_PENDING_DEL,
+DECL|BT_KEYS_AUTHENTICATED|enumerator|BT_KEYS_AUTHENTICATED = BIT(0),
+DECL|BT_KEYS_DEBUG|enumerator|BT_KEYS_DEBUG = BIT(1),
+DECL|BT_KEYS_ID_PENDING_ADD|enumerator|BT_KEYS_ID_PENDING_ADD = BIT(2),
+DECL|BT_KEYS_ID_PENDING_DEL|enumerator|BT_KEYS_ID_PENDING_DEL = BIT(3),
 DECL|BT_KEYS_IRK|enumerator|BT_KEYS_IRK = BIT(1),
 DECL|BT_KEYS_LOCAL_CSRK|enumerator|BT_KEYS_LOCAL_CSRK = BIT(3),
 DECL|BT_KEYS_LTK_P256|enumerator|BT_KEYS_LTK_P256 = BIT(5),
 DECL|BT_KEYS_LTK|enumerator|BT_KEYS_LTK = BIT(2),
-DECL|BT_KEYS_NUM_FLAGS|enumerator|BT_KEYS_NUM_FLAGS,
 DECL|BT_KEYS_REMOTE_CSRK|enumerator|BT_KEYS_REMOTE_CSRK = BIT(4),
 DECL|BT_KEYS_SLAVE_LTK|enumerator|BT_KEYS_SLAVE_LTK = BIT(0),
 DECL|BT_KEYS_STORAGE_LEN|macro|BT_KEYS_STORAGE_LEN
-DECL|BT_LINK_KEY_AUTHENTICATED|enumerator|BT_LINK_KEY_AUTHENTICATED,
-DECL|BT_LINK_KEY_DEBUG|enumerator|BT_LINK_KEY_DEBUG,
-DECL|BT_LINK_KEY_NUM_FLAGS|enumerator|BT_LINK_KEY_NUM_FLAGS,
-DECL|BT_LINK_KEY_SC|enumerator|BT_LINK_KEY_SC,
+DECL|BT_LINK_KEY_AUTHENTICATED|enumerator|BT_LINK_KEY_AUTHENTICATED = BIT(0),
+DECL|BT_LINK_KEY_DEBUG|enumerator|BT_LINK_KEY_DEBUG = BIT(1),
+DECL|BT_LINK_KEY_SC|enumerator|BT_LINK_KEY_SC = BIT(2),
 DECL|addr|member|bt_addr_le_t addr;
 DECL|addr|member|bt_addr_t addr;
 DECL|bt_csrk|struct|struct bt_csrk {
@@ -27,11 +25,13 @@ DECL|bt_ltk|struct|struct bt_ltk {
 DECL|cnt|member|u32_t cnt;
 DECL|ediv|member|u16_t ediv;
 DECL|enc_size|member|u8_t enc_size;
+DECL|flags|member|u8_t flags;
+DECL|flags|member|u8_t flags;
 DECL|irk|member|struct bt_irk irk;
 DECL|keys|member|u16_t keys;
 DECL|local_csrk|member|struct bt_csrk local_csrk;
 DECL|ltk|member|struct bt_ltk ltk;
-DECL|rand|member|u64_t rand;
+DECL|rand|member|u8_t rand[8];
 DECL|remote_csrk|member|struct bt_csrk remote_csrk;
 DECL|rpa|member|bt_addr_t rpa;
 DECL|slave_ltk|member|struct bt_ltk slave_ltk;

commit 6fd801632430c431f8bf56aa782f0c72a139cb1f

Bluetooth: Fix central from failing to start encryption
This fixes a regression introduced in commit ac26b7c2f06d [formerly 6af5d1cd1f81]
("Bluetooth: Compress bt_keys struct").

Instead of passing a value zero as the random number, the
value at the RAM address zero was being used by the start
encryption function call. It is now fixed by consistently
using byte-array to store EDiv and Rand values.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

Former-commit-id: b35ed7e79ca3139018876b5585921934cf62ce63

subsys/bluetooth/host/conn.c
@@ -37,7 +37,7 @@ DECL|bt_conn_identity_resolved|function|void bt_conn_identity_resolved(struct bt
 DECL|bt_conn_init|function|int bt_conn_init(void)
 DECL|bt_conn_le_conn_update|function|int bt_conn_le_conn_update(struct bt_conn *conn, const struct bt_le_conn_param *param)
 DECL|bt_conn_le_param_update|function|int bt_conn_le_param_update(struct bt_conn *conn, const struct bt_le_conn_param *param)
-DECL|bt_conn_le_start_encryption|function|int bt_conn_le_start_encryption(struct bt_conn *conn, u8_t rand[8], u16_t ediv,const u8_t *ltk, size_t len)
+DECL|bt_conn_le_start_encryption|function|int bt_conn_le_start_encryption(struct bt_conn *conn, u8_t rand[8],u8_t ediv[2], const u8_t *ltk, size_t len)
 DECL|bt_conn_lookup_addr_br|function|struct bt_conn *bt_conn_lookup_addr_br(const bt_addr_t *peer)
 DECL|bt_conn_lookup_addr_le|function|struct bt_conn *bt_conn_lookup_addr_le(const bt_addr_le_t *peer)
 DECL|bt_conn_lookup_addr_sco|function|struct bt_conn *bt_conn_lookup_addr_sco(const bt_addr_t *peer)

subsys/bluetooth/host/keys.h
@@ -23,7 +23,7 @@ DECL|bt_keys_store|function|static inline int bt_keys_store(struct bt_keys *keys
 DECL|bt_keys|struct|struct bt_keys {
 DECL|bt_ltk|struct|struct bt_ltk {
 DECL|cnt|member|u32_t cnt;
-DECL|ediv|member|u16_t ediv;
+DECL|ediv|member|u8_t ediv[2];
 DECL|enc_size|member|u8_t enc_size;
 DECL|flags|member|u8_t flags;
 DECL|flags|member|u8_t flags;

subsys/bluetooth/host/smp.h
@@ -76,7 +76,7 @@ DECL|bt_smp_security_request|struct|struct bt_smp_security_request {
 DECL|bt_smp_signing_info|struct|struct bt_smp_signing_info {
 DECL|code|member|u8_t code;
 DECL|csrk|member|u8_t csrk[16];
-DECL|ediv|member|u16_t ediv;
+DECL|ediv|member|u8_t ediv[2];
 DECL|e|member|u8_t e[16];
 DECL|init_key_dist|member|u8_t init_key_dist;
 DECL|io_capability|member|u8_t io_capability;
@@ -84,7 +84,7 @@ DECL|irk|member|u8_t irk[16];
 DECL|ltk|member|u8_t ltk[16];
 DECL|max_key_size|member|u8_t max_key_size;
 DECL|oob_flag|member|u8_t oob_flag;
-DECL|rand|member|u64_t rand;
+DECL|rand|member|u8_t rand[8];
 DECL|reason|member|u8_t reason;
 DECL|resp_key_dist|member|u8_t resp_key_dist;
 DECL|val|member|u8_t val[16];