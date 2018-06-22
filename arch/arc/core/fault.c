99b5f47e97d7cfb63cf47bb55f3657c3703d0a69
Andrew Boie <andrew.p.boie@intel.com>
2018-06-22 15:15:12 -0700

    arc: add z_arch_user_string_nlen
    Safely measure the length of a potentially dubious string.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: b1dec2992e6e013a00a9aa4f29854177e9c08fc2

--- arch/arc/core/fault.c
+++ arch/arc/core/fault.c
@@ -1,1 +1,3 @@
-DECL|_Fault|function|void _Fault(const NANO_ESF *esf)
+DECL|_Fault|function|void _Fault(NANO_ESF *esf)
+DECL|exceptions|variable|exceptions
+DECL|z_arch_user_string_nlen|variable|z_arch_user_string_nlen
