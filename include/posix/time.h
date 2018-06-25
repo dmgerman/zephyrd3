1bc7570b71bc6603baab824d0dc58007ccf12406
David Brown <david.brown@linaro.org>
2018-06-25 16:43:54 -0600

    lib: posix: clock: Implement clock_settime
    Set a base time, using the current uptime.
    
    Signed-off-by: David Brown <david.brown@linaro.org>
    
    Former-commit-id: 9d6f1c600fe40e2320589409baa87afab7f91f6d

--- include/posix/time.h
+++ include/posix/time.h
@@ -4,7 +4,6 @@ DECL|NSEC_PER_MSEC|macro|NSEC_PER_MSEC
 DECL|TIMER_ABSTIME|macro|TIMER_ABSTIME
 DECL|__POSIX_TIME_H__|macro|__POSIX_TIME_H__
 DECL|_ts_to_ms|function|static inline s32_t _ts_to_ms(const struct timespec *to)
-DECL|clock_settime|function|static inline int clock_settime(clockid_t clock_id, const struct timespec *ts)
 DECL|it_interval|member|struct timespec it_interval; /* Timer interval */
 DECL|it_value|member|struct timespec it_value; /* Timer expiration */
 DECL|itimerspec|struct|struct itimerspec {
--- lib/posix/clock.c
+++ lib/posix/clock.c
@@ -1,3 +1,4 @@
 DECL|clock_gettime|function|int clock_gettime(clockid_t clock_id, struct timespec *ts)
+DECL|clock_settime|function|int clock_settime(clockid_t clock_id, const struct timespec *tp)
 DECL|gettimeofday|function|int gettimeofday(struct timeval *tv, const void *tz)
 DECL|rt_clock_base|variable|rt_clock_base
