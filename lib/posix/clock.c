62ffbb6a3202ba6d6d3bf357f55e52564a035e1d
David Brown <david.brown@linaro.org>
2018-06-25 11:24:53 -0600

    lib: posix: clock: Add read of CLOCK_REALTIME
    Add a private variable `rt_clock_base` that can be used to determine a
    real-time clock by using the `k_uptime_get` clock.  Once `clock_settime`
    is added, this can allow us to have a meaningful real time clock.
    
    Signed-off-by: David Brown <david.brown@linaro.org>
    
    Former-commit-id: 9921eb329d0ed8f899ca513ec57067c7209d1608

--- lib/posix/clock.c
+++ lib/posix/clock.c
@@ -1,2 +1,3 @@
 DECL|clock_gettime|function|int clock_gettime(clockid_t clock_id, struct timespec *ts)
 DECL|gettimeofday|function|int gettimeofday(struct timeval *tv, const void *tz)
+DECL|rt_clock_base|variable|rt_clock_base
