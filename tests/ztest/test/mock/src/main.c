82c2c8cfb143630c13e16a02dd6b4de7e3091840
Morten Priess <mtpr@oticon.com>
2018-06-26 14:48:56 +0200

    tests: ztest: added test case for multiple mock expects
    The test verifies that the mock framework can handle functions
    under test which call more than one mock function, and
    correctly processes each call.
    
    Signed-off-by: Morten Priess <mtpr@oticon.com>
    
    Former-commit-id: 80e02a93d3ff294289f3ca6c6fb8b8f4aefed568

--- tests/ztest/test/mock/src/main.c
+++ tests/ztest/test/mock/src/main.c
@@ -1,5 +1,6 @@
 DECL|expect_one_parameter|function|static void expect_one_parameter(int a)
 DECL|expect_two_parameters|function|static void expect_two_parameters(int a, int b)
+DECL|multi_value_tests|function|static void multi_value_tests(void)
 DECL|parameter_tests|function|static void parameter_tests(void)
 DECL|return_value_tests|function|static void return_value_tests(void)
 DECL|returns_int|function|static int returns_int(void)
