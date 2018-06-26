8c7c94dad6a05350e187a19b57b8e7092decf8bb
Christophe Priouzeau <christophe.priouzeau@st.com>
2018-06-26 12:07:23 +0200

    bt: hci driver over spi: Configure BlueNRG-MS in controller mode
    Configure BlueNRG in controller w/o host.
    By disabling 'HCI_reset' via the quirk, we can request to
    BluesNRG hardware to be on controller mode just after it is ready.
    By the way it's no necessary to manage a 'HCI_reset' when BluesNRG are
    initialised (and on controler mode).
    
    Signed-off-by: Christophe Priouzeau <christophe.priouzeau@st.com>
    Signed-off-by: Erwan Gouriou <erwan.gouriou@linaro.org>
    
    Former-commit-id: b0c294ce48744d31d0ecb1c6da7519ba90b19786

--- drivers/bluetooth/hci/spi.c
+++ drivers/bluetooth/hci/spi.c
@@ -1,3 +1,6 @@
+DECL|BLUENRG_ACI_LL_MODE|macro|BLUENRG_ACI_LL_MODE
+DECL|BLUENRG_ACI_WRITE_CONFIG_CMD_LL|macro|BLUENRG_ACI_WRITE_CONFIG_CMD_LL
+DECL|BLUENRG_ACI_WRITE_CONFIG_DATA|macro|BLUENRG_ACI_WRITE_CONFIG_DATA
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
 DECL|CMD_OCF|macro|CMD_OCF
 DECL|CMD_OGF|macro|CMD_OGF
@@ -24,14 +27,17 @@ DECL|STATUS_HEADER_READY|macro|STATUS_HEADER_READY
 DECL|STATUS_HEADER_TOREAD|macro|STATUS_HEADER_TOREAD
 DECL|_bt_spi_init|function|static int _bt_spi_init(struct device *unused)
 DECL|attempts|variable|attempts
+DECL|bluenrg_aci_cmd_ll_param|struct|struct bluenrg_aci_cmd_ll_param {
 DECL|bt_spi_get_cmd|function|static inline u16_t bt_spi_get_cmd(u8_t *txmsg)
 DECL|bt_spi_get_evt|function|static inline u16_t bt_spi_get_evt(u8_t *rxmsg)
 DECL|bt_spi_handle_vendor_evt|function|static void bt_spi_handle_vendor_evt(u8_t *rxmsg)
 DECL|bt_spi_isr|function|static void bt_spi_isr(struct device *unused1, struct gpio_callback *unused2, unsigned int unused3)
 DECL|bt_spi_open|function|static int bt_spi_open(void)
 DECL|bt_spi_rx_thread|function|static void bt_spi_rx_thread(void)
+DECL|bt_spi_send_aci_config_data_controller_mode|function|static int bt_spi_send_aci_config_data_controller_mode(void)
 DECL|bt_spi_send|function|static int bt_spi_send(struct net_buf *buf)
 DECL|bt_spi_transceive|function|static inline int bt_spi_transceive(void *tx, u32_t tx_len, void *rx, u32_t rx_len)
+DECL|cmd|member|u8_t cmd;
 DECL|configure_cs|function|static int configure_cs(void)
 DECL|configure_cs|macro|configure_cs
 DECL|cs_dev|variable|cs_dev
@@ -46,6 +52,7 @@ DECL|irq_pin_high|function|static bool irq_pin_high(void)
 DECL|irq_pin_high|macro|irq_pin_high
 DECL|kick_cs|function|static void kick_cs(void)
 DECL|kick_cs|macro|kick_cs
+DECL|length|member|u8_t length;
 DECL|release_cs|function|static void release_cs(void)
 DECL|release_cs|macro|release_cs
 DECL|rst_dev|variable|rst_dev
@@ -60,3 +67,4 @@ DECL|spi_rx|variable|spi_rx
 DECL|spi_tx_buf|variable|spi_tx_buf
 DECL|spi_tx|variable|spi_tx
 DECL|txmsg|variable|txmsg
+DECL|value|member|u8_t value;
