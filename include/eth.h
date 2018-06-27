3b4f0c97cde5cf3a682a71945918fb28aa484dc9
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-06-27 08:51:22 +0200

    include: Remove unused header
    This eth.h is not used anywhere and is a legacy header from first
    network stack.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: 59df651ceea25737f03610e53f1221f91a176de7

--- include/eth.h
+++
@@ -1,6 +0,0 @@
-DECL|__INCLUDE_ETH_H__|macro|__INCLUDE_ETH_H__
-DECL|eth_driver_api|struct|struct eth_driver_api {
-DECL|eth_register_callback|function|static inline void eth_register_callback(struct device *dev,void (*cb)(u8_t *buffer, u16_t len))
-DECL|eth_send|function|static inline int eth_send(struct device *dev, u8_t *buffer, u16_t len)
-DECL|register_callback|member|void (*register_callback)(struct device *dev,
-DECL|send|member|int (*send)(struct device *dev, u8_t *buffer, u16_t len);
