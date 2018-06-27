ac65a2c893b3a4cb28a8780aa899dbe7e054ab30
Olivier Martin <olivier.martin@proglove.de>
2018-06-27 16:53:26 +0200

    drivers: serial: Fix syntax error
    `uart_handler.c` does not build when `CONFIG_UART_LINE_CTRL`
    is defined.
    
    Signed-off-by: Olivier Martin <olivier.martin@proglove.de>
    
    Former-commit-id: 0ba41f5b466837ae5e048dbe6f774331367ebd17

--- drivers/serial/uart_handlers.c
+++ drivers/serial/uart_handlers.c
@@ -3,4 +3,5 @@ DECL|UART_SIMPLE_VOID|macro|UART_SIMPLE_VOID
 DECL|UART_SIMPLE|function|UART_SIMPLE(err_check)
 DECL|UART_SIMPLE|macro|UART_SIMPLE
 DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(uart_drv_cmd, dev, cmd, p)
+DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(uart_line_ctrl_get, dev, ctrl, val)
 DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(uart_poll_out, dev, out_char)
