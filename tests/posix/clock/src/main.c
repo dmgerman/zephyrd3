b6f33e0e6be8a20e6a4292c18d4e7757de2b1b4d
David Brown <david.brown@linaro.org>
2018-06-27 10:05:30 -0600

    tests: posix: Test realtime clock
    Create a basic test for the realtime clock.  This sets the clock value
    to a particular time, half a second in, and monitors the clock for a few
    seconds, making sure that the value returned is reasonable.
    
    Signed-off-by: David Brown <david.brown@linaro.org>
    
    Former-commit-id: ced1c03c408a082f199a9b4e453fb94db7ec6c8b

--- tests/posix/clock/src/main.c
+++ tests/posix/clock/src/main.c
@@ -1,3 +1,4 @@
 DECL|SLEEP_SECONDS|macro|SLEEP_SECONDS
 DECL|test_main|function|void test_main(void)
 DECL|test_posix_clock|function|void test_posix_clock(void)
+DECL|test_posix_realtime|function|void test_posix_realtime(void)
