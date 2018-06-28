6d3df328d0ba50447c2ec4accfc21d02d32c4792
Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
2018-06-28 22:16:39 +0200

    drivers: uart: nrf: fixing interrupt driven API
    1. API function: uart_nrfx_irq_tx_enable is expected to enable and
    trigger TXDRDY interrupt. Due to HW limiation before first byte has
    been sent TXDRDY interrupt will not be triggered so it must be forced
    by the software.
    
    2. Common function: uart_nrfx_irq_tx_ready_complete is used for two
    API entries: irq_tx_ready, irq_tx_complete because Nordic hardware does
    not distinguish between them. This function returns 1 when driver is
    ready to accept data and 0 otherwise.
    
    3. Removed Hungarian prefixes.
    
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
    
    Former-commit-id: 197740bdfe3fe1d9ac00e22bfb5c20d4a65edb1b

--- drivers/serial/uart_nrfx_uart.c
+++ drivers/serial/uart_nrfx_uart.c
@@ -1,5 +1,7 @@
 DECL|baudrate_set|function|static int baudrate_set(struct device *dev, u32_t baudrate)
-DECL|m_irq_callback|variable|m_irq_callback
+DECL|event_txdrdy_check|function|static bool event_txdrdy_check(void)
+DECL|event_txdrdy_clear|function|static void event_txdrdy_clear(void)
+DECL|irq_callback|variable|irq_callback
 DECL|uart_nrfx_err_check|function|static int uart_nrfx_err_check(struct device *dev)
 DECL|uart_nrfx_fifo_fill|function|static int uart_nrfx_fifo_fill(struct device *dev, const u8_t *tx_data, int len)
 DECL|uart_nrfx_fifo_read|function|static int uart_nrfx_fifo_read(struct device *dev, u8_t *rx_data, const int size)
@@ -11,13 +13,13 @@ DECL|uart_nrfx_irq_is_pending|function|static int uart_nrfx_irq_is_pending(struc
 DECL|uart_nrfx_irq_rx_disable|function|static void uart_nrfx_irq_rx_disable(struct device *dev)
 DECL|uart_nrfx_irq_rx_enable|function|static void uart_nrfx_irq_rx_enable(struct device *dev)
 DECL|uart_nrfx_irq_rx_ready|function|static int uart_nrfx_irq_rx_ready(struct device *dev)
-DECL|uart_nrfx_irq_tx_complete|function|static int uart_nrfx_irq_tx_complete(struct device *dev)
 DECL|uart_nrfx_irq_tx_disable|function|static void uart_nrfx_irq_tx_disable(struct device *dev)
 DECL|uart_nrfx_irq_tx_enable|function|static void uart_nrfx_irq_tx_enable(struct device *dev)
-DECL|uart_nrfx_irq_tx_ready|function|static int uart_nrfx_irq_tx_ready(struct device *dev)
+DECL|uart_nrfx_irq_tx_ready_complete|function|static int uart_nrfx_irq_tx_ready_complete(struct device *dev)
 DECL|uart_nrfx_irq_update|function|static int uart_nrfx_irq_update(struct device *dev)
 DECL|uart_nrfx_isr|function|static void uart_nrfx_isr(void *arg)
 DECL|uart_nrfx_poll_in|function|static int uart_nrfx_poll_in(struct device *dev, unsigned char *c)
 DECL|uart_nrfx_poll_out|function|static unsigned char uart_nrfx_poll_out(struct device *dev,unsigned char c)
 DECL|uart_nrfx_uart0|variable|uart_nrfx_uart0
 DECL|uart_nrfx_uart_driver_api|variable|uart_nrfx_uart_driver_api
+DECL|uart_sw_event_txdrdy|variable|uart_sw_event_txdrdy
