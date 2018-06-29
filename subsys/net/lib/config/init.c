2b934374320c0ad00460b9199186f7cc77daf325
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-06-29 18:46:47 +0300

    net: config: Introduce a dedicated header for the library
    Split out definition of net_app_init() and its parameter flags from
    net_app.h header to new net_config.h header. As we do this, rename
    the function to net_config_init() and flags to NET_CONFIG_NEED_*.
    This is a second step in splitting out network configuration API
    out of net_app API, started in the c60df1311 commit.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: c89a06dbc1ca3443821762b06f3073343a984b08

--- subsys/net/lib/config/init.c
+++ subsys/net/lib/config/init.c
@@ -9,7 +9,7 @@ DECL|ipv6_event_handler|function|static void ipv6_event_handler(struct net_mgmt_
 DECL|laddr|variable|laddr
 DECL|mgmt4_cb|variable|mgmt4_cb
 DECL|mgmt6_cb|variable|mgmt6_cb
-DECL|net_app_init|function|int net_app_init(const char *app_info, u32_t flags, s32_t timeout)
+DECL|net_config_init|function|int net_config_init(const char *app_info, u32_t flags, s32_t timeout)
 DECL|setup_dhcpv4|function|static void setup_dhcpv4(struct net_if *iface)
 DECL|setup_dhcpv4|macro|setup_dhcpv4
 DECL|setup_ipv4|function|static void setup_ipv4(struct net_if *iface)
