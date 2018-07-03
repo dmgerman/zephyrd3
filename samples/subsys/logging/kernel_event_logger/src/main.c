228013ab13c24e077b3e3b616b8663c849011482
Anas Nashif <anas.nashif@intel.com>
2018-07-02 22:19:17 -0500

    samples: remove kernel_event_logger sample
    We are replacing kernel_event_logger with a more generic tracing API.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 3f02e0d5d38655dd1f28fb044623a662c4a7813a

--- samples/subsys/logging/kernel_event_logger/src/main.c
+++
@@ -1,8 +0,0 @@
-DECL|event_logger|function|static void event_logger(void)
-DECL|event_type|variable|event_type
-DECL|main|function|void main(void)
-DECL|threadA_data|variable|threadA_data
-DECL|threadA|function|static void threadA(void)
-DECL|threadB_data|variable|threadB_data
-DECL|threadB|function|static void threadB(void)
-DECL|timestamp|variable|timestamp
