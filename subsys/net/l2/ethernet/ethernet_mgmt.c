8088fe410205615467868180978c0c59540d5471
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-07-03 12:56:08 +0300

    net: mgmt: Add VLAN enabled / disabled event support
    Send network management event if VLAN tag is enabled or disabled.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 3fd2d53e5696198f7d51cefbae42be83c27fc013

--- subsys/net/l2/ethernet/ethernet_mgmt.c
+++ subsys/net/l2/ethernet/ethernet_mgmt.c
@@ -2,5 +2,7 @@ DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|ethernet_mgmt_raise_carrier_off_event|function|void ethernet_mgmt_raise_carrier_off_event(struct net_if *iface)
 DECL|ethernet_mgmt_raise_carrier_on_event|function|void ethernet_mgmt_raise_carrier_on_event(struct net_if *iface)
+DECL|ethernet_mgmt_raise_vlan_disabled_event|function|void ethernet_mgmt_raise_vlan_disabled_event(struct net_if *iface, u16_t tag)
+DECL|ethernet_mgmt_raise_vlan_enabled_event|function|void ethernet_mgmt_raise_vlan_enabled_event(struct net_if *iface, u16_t tag)
 DECL|ethernet_set_config|function|static int ethernet_set_config(u32_t mgmt_request, struct net_if *iface, void *data, size_t len)
 DECL|is_hw_caps_supported|function|static inline bool is_hw_caps_supported(struct device *dev,enum ethernet_hw_caps caps)
