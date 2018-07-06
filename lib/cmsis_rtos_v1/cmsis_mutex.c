d8fb8639a0b3a6424a4a578df59b2884d3f642fa
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-06 15:44:11 +0530

    lib/cmsis_rtos_v1: Implement support for mutex APIs
    These APIs allow creating, acquiring, releasing and deleting
    the mutexes.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: 055da73184d77569f8e65bf98583dcabfee7fc9d

---
+++ lib/cmsis_rtos_v1/cmsis_mutex.c
@@ -0,0 +1,4 @@
+DECL|osMutexCreate|function|osMutexId osMutexCreate(const osMutexDef_t *mutex_def)
+DECL|osMutexDelete|function|osStatus osMutexDelete(osMutexId mutex_id)
+DECL|osMutexRelease|function|osStatus osMutexRelease(osMutexId mutex_id)
+DECL|osMutexWait|function|osStatus osMutexWait(osMutexId mutex_id, uint32_t timeout)
