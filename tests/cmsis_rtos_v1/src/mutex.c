f47df7f6ee7e400c6d3a4553619fad626d782b61
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-09 11:08:02 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of Mutex APIs
    Some test programs to illustrate the usage of Mutex APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: a76556a22cef6b51028751f98279e800eb7cc632

---
+++ tests/cmsis_rtos_v1/src/mutex.c
@@ -0,0 +1,6 @@
+DECL|Mutex_1|variable|Mutex_1
+DECL|Mutex_2|variable|Mutex_2
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|tThread_entry_lock_timeout|function|void tThread_entry_lock_timeout(void const *arg)
+DECL|test_mutex_lock_timeout|function|void test_mutex_lock_timeout(void)
+DECL|test_mutex|function|void test_mutex(void)
