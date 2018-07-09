4c8ddf227888325ab352a0d80497dc41262df772
Laczen JMS <laczenjms@gmail.com>
2018-07-09 16:50:57 +0200

    subsys: fs/nvs: Rewrite for improved robustness
    On flash NVS was stored one entry after another including the metadata
    of each entry. This has the disadvantage that when an incomplete write
    is performed (e.g. due to power failure) the complete sector had to be
    rewritten to get a completely functional system.
    
    The present rewrite changed the storage in flash of the data. For each
    sector the data is now written as follows: the data itself at the
    beginning of the sector (one after the other), the metadata (id, length,
    data offset in the sector, and a crc of the metadata) is written from
    the end of the sector. The metadata is of fixed size (8 byte) and for
    a sector that is completely occupied a metadata entry of all zeros is
    used.
    
    Writing data to flash always is done by:
    1. Writing the data,
    2. Writing the metadata.
    
    If an incomplete write is done NVS will ignore this incomplete write.
    
    At the same time the following improvements were done:
    1. NVS now support 65536 sectors of each 65536 byte.
    2. The sector size no longer requires to be a power of 2 (but it
    still needs to be a multiple of the flash erase page size).
    3. NVS now also keeps track of the free space available.
    
    Signed-off-by: Laczen JMS <laczenjms@gmail.com>
    
    Former-commit-id: 7d2e59813f7025118874454e7dc236320a53c3d4

--- include/nvs/nvs.h
+++ include/nvs/nvs.h
@@ -1,12 +1,12 @@
 DECL|__NVS_H_|macro|__NVS_H_
-DECL|entry_location|member|off_t entry_location; /* start of the filesystem */
+DECL|ate_wra|member|u32_t ate_wra; /* next alloc table entry write address */
+DECL|data_wra|member|u32_t data_wra; /* next data write address */
 DECL|flash_device|member|struct device *flash_device;
+DECL|free_space|member|u32_t free_space; /* free space available in file system */
 DECL|locked|member|bool locked; /* the filesystem is locked after an error occurred
-DECL|max_len|member|u16_t max_len; /* maximum size of stored item, set to sector_size/4 */
 DECL|nvs_fs|struct|struct nvs_fs {
 DECL|nvs_lock|member|struct k_mutex nvs_lock;
 DECL|offset|member|off_t offset; /* filesystem offset in flash */
-DECL|sector_count|member|u8_t sector_count; /* how many sectors in the filesystem */
+DECL|sector_count|member|u16_t sector_count; /* amount of sectors in the filesystem */
 DECL|sector_size|member|u16_t sector_size; /* filesystem is divided into sectors,
 DECL|write_block_size|member|u8_t write_block_size; /* write block size for alignment */
-DECL|write_location|member|off_t write_location; /* next write location */
--- subsys/fs/nvs/nvs_priv.h
+++ subsys/fs/nvs/nvs_priv.h
@@ -1,8 +1,14 @@
-DECL|NVS_ID_EMPTY|macro|NVS_ID_EMPTY
-DECL|NVS_ID_JUMP|macro|NVS_ID_JUMP
-DECL|NVS_MAX_LEN|macro|NVS_MAX_LEN
-DECL|NVS_MIN_WRITE_BLOCK_SIZE|macro|NVS_MIN_WRITE_BLOCK_SIZE
+DECL|ADDR_OFFS_MASK|macro|ADDR_OFFS_MASK
+DECL|ADDR_SECT_MASK|macro|ADDR_SECT_MASK
+DECL|ADDR_SECT_SHIFT|macro|ADDR_SECT_SHIFT
+DECL|NVS_BLOCK_SIZE|macro|NVS_BLOCK_SIZE
 DECL|NVS_STATUS_NOSPACE|macro|NVS_STATUS_NOSPACE
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|__NVS_PRIV_H_|macro|__NVS_PRIV_H_
+DECL|crc8|member|u8_t crc8; /* crc8 check of the entry */
+DECL|id|member|u16_t id; /* data id */
+DECL|len|member|u16_t len; /* data len in sector */
+DECL|nvs_ate|struct|struct nvs_ate {
+DECL|offset|member|u16_t offset; /* data offset in sector */
+DECL|part|member|u8_t part; /* part of a multipart data - future extension */
