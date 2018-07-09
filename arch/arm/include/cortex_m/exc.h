7bb04f193e83ea8fe3439453c980a2f41518de5a
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-07-09 11:47:18 +0200

    arch: arm: add macro definition for vector key write permit value
    This commit adds a macro definition for the Vector Key value of
    AIRCR register that permits the accompanying writes. The macro
    is then used instead of the hard-coded numerical value.
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: 28acaeadc9b283e368b7f23a8c5fb572c65633b8

--- arch/arm/include/cortex_m/exc.h
+++ arch/arm/include/cortex_m/exc.h
@@ -1,3 +1,4 @@
+DECL|AIRCR_VECT_KEY_PERMIT_WRITE|macro|AIRCR_VECT_KEY_PERMIT_WRITE
 DECL|_ARM_CORTEXM_ISR__H_|macro|_ARM_CORTEXM_ISR__H_
 DECL|_ClearFaults|function|static ALWAYS_INLINE void _ClearFaults(void)
 DECL|_ExcSetup|function|static ALWAYS_INLINE void _ExcSetup(void)
