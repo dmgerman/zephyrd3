30fdf3a871674d035e64e3b5e6d8940c000391ff
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-10 10:57:06 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of Semaphore APIs
    Some test programs to illustrate the usage of Semaphore APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: c996d0581f51f78e90b2515d42fd2519a1b1df59

---
+++ tests/cmsis_rtos_v1/src/semaphore.c
@@ -0,0 +1,4 @@
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|semaphore_1|variable|semaphore_1
+DECL|test_semaphore|function|void test_semaphore(void)
+DECL|thread_sema|function|void thread_sema(void const *arg)
