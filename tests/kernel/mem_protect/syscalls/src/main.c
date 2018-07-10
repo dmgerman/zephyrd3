86cee522c3b5104c58fb7525136414e5ce54b8c7
Andrew Boie <andrew.p.boie@intel.com>
2018-07-10 10:17:51 -0700

    userspace: add syscalls test case
    Test that we can define our own system calls in application code
    and that fault handling works properly.
    
    Additional tests for base system call infrastructure, outside of
    specific system calls, go here.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: cef074868762c1988c212d6da759696f6d7cf1f5

---
+++ tests/kernel/mem_protect/syscalls/src/main.c
@@ -0,0 +1,17 @@
+DECL|BUF_SIZE|macro|BUF_SIZE
+DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(string_alloc_copy, src)
+DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(string_copy, src)
+DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(string_nlen, src, maxlen, err)
+DECL|Z_SYSCALL_HANDLER|function|Z_SYSCALL_HANDLER(to_copy, dest)
+DECL|_impl_string_alloc_copy|function|int _impl_string_alloc_copy(char *src)
+DECL|_impl_string_copy|function|int _impl_string_copy(char *src)
+DECL|_impl_string_nlen|function|size_t _impl_string_nlen(char *src, size_t maxlen, int *err)
+DECL|_impl_to_copy|function|int _impl_to_copy(char *dest)
+DECL|kernel_buf|variable|kernel_buf
+DECL|kernel_string|variable|kernel_string
+DECL|test_main|function|void test_main(void)
+DECL|test_string_nlen|function|void test_string_nlen(void)
+DECL|test_to_copy|function|void test_to_copy(void)
+DECL|test_user_string_alloc_copy|function|void test_user_string_alloc_copy(void)
+DECL|test_user_string_copy|function|void test_user_string_copy(void)
+DECL|user_string|variable|user_string
