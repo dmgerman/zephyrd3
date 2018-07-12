3459257891e1023227c6df03a2f3051150f38795
Michael Hennerich <michael.hennerich@analog.com>
2018-07-12 09:17:23 +0200

    samples: sensor: adt7420: Add ADT7420 sample application
    This simple application periodically prints the ambient temperature.
    Optional support for threshold triggers is provided.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    
    Former-commit-id: 92c1c2a24b0c0591b5396cb292db1a5dd727664a

---
+++ samples/sensor/adt7420/src/main.c
@@ -0,0 +1,4 @@
+DECL|main|function|void main(void)
+DECL|process|function|static void process(struct device *dev)
+DECL|sensor_set_attribute|function|static int sensor_set_attribute(struct device *dev, enum sensor_channel chan,enum sensor_attribute attr, int value)
+DECL|trigger_handler|function|static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
