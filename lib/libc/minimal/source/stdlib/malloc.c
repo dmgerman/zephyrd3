722efa6fe141e5de194be0311eddcf8e361174de
Andrew Boie <andrew.p.boie@intel.com>
2018-07-12 15:00:58 -0700

    libc: minimal: add malloc functions
    We base the malloc() implementation on a common sys_mem_pool whose
    size is specified by Kconfig.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 6a8649f806d90bc8e6d820316ac4d813600f7122

---
+++ lib/libc/minimal/source/stdlib/malloc.c
@@ -0,0 +1,9 @@
+DECL|calloc|function|void *calloc(size_t nmemb, size_t size)
+DECL|free|function|void free(void *ptr)
+DECL|malloc_mutex|variable|malloc_mutex
+DECL|malloc_prepare|function|static int malloc_prepare(struct device *unused)
+DECL|malloc|function|void *malloc(size_t size)
+DECL|malloc|function|void *malloc(size_t size)
+DECL|reallocarray|function|void *reallocarray(void *ptr, size_t nmemb, size_t size)
+DECL|realloc|function|void *realloc(void *ptr, size_t requested_size)
+DECL|size_t_mul_overflow|function|static bool size_t_mul_overflow(size_t a, size_t b, size_t *res)
