48bd7f9cff48c1259e54ef772b94aa12f30afb24
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-12 15:15:57 +0530

    lib/cmsis_rtos_v1: Implement support for mempool APIs
    These APIs allow creating, allocating and freeing
    of mempools.
    
    Note: "Mempool" in CMSIS actually means memslabs in Zephyr.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: aff8c511281ec781cf212fd5ae234f147b7e73ee

---
+++ lib/cmsis_rtos_v1/cmsis_mempool.c
@@ -0,0 +1,5 @@
+DECL|TIME_OUT|macro|TIME_OUT
+DECL|osPoolAlloc|function|void *osPoolAlloc(osPoolId pool_id)
+DECL|osPoolCAlloc|function|void *osPoolCAlloc(osPoolId pool_id)
+DECL|osPoolCreate|function|osPoolId osPoolCreate(const osPoolDef_t *pool_def)
+DECL|osPoolFree|function|osStatus osPoolFree(osPoolId pool_id, void *block)
