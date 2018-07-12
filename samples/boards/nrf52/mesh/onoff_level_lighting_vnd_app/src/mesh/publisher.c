bd373892fa2127832753e9bd44389b98cf21c9d9
Vikrant More <vikrant8051@gmail.com>
2018-07-12 10:10:32 +0530

    samples: mesh: boards: nrf52: randomize publishers TID on boot
    Now on reboot, NODE as client do not start message publishing
    with TID = 1. Instead of that it would start with any random value
    from 0 to 255.
    
    Signed-off-by: Vikrant More <vikrant8051@gmail.com>
    
    Former-commit-id: 2708ce561d4f1d71a096f8e235a48f2704066762

--- samples/boards/nrf52/mesh/onoff_level_lighting_vnd_app/src/mesh/publisher.c
+++ samples/boards/nrf52/mesh/onoff_level_lighting_vnd_app/src/mesh/publisher.c
@@ -1,3 +1,8 @@
 DECL|GENERIC_LEVEL|macro|GENERIC_LEVEL
 DECL|button_read|function|static u32_t button_read(struct device *port, u32_t pin)
+DECL|is_randomization_of_TIDs_done|variable|is_randomization_of_TIDs_done
 DECL|publish|function|void publish(struct k_work *work)
+DECL|randomize_publishers_TID|function|void randomize_publishers_TID(void)
+DECL|tid_level|variable|tid_level
+DECL|tid_onoff|variable|tid_onoff
+DECL|tid_vnd|variable|tid_vnd
