7baad1c3fa3ce390a00579d422fb1f614d7878b4
Alberto Escolar Piedras <alpi@oticon.com>
2018-07-15 19:00:53 +0200

    test: Add test for native_posix RTC and real time control
    Added a simple test to validate the real time control and
    RTC functionality
    
    Note that this test depends too much on not having a high host
    load to pass.
    Therefore it is better to not run in it automated regression
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: 8ba0ddf4baeb967a03ea36bb61b150bf3890adaf

---
+++ tests/boards/native_posix/rtc/src/main.c
@@ -0,0 +1,7 @@
+DECL|TICK_MS|macro|TICK_MS
+DECL|TOLERANCE|macro|TOLERANCE
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|test_main|function|void test_main(void)
+DECL|test_realtime|function|static void test_realtime(void)
+DECL|test_rtc_offset|function|static void test_rtc_offset(void)
+DECL|us_time_to_str|function|static char *us_time_to_str(char *dest, u64_t time)
