d5015518b9281de4e0f783da2c9e6d8337dee611
Andrew Boie <andrew.p.boie@intel.com>
2018-07-16 09:48:16 -0700

    arch: introduce exc_handle.h
    Defines some common macros and data structures for declaring
    text section ranges with fixup handler addresses if an
    exception occurs.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 3369a5ed0795db897b510afeb53622a077e9e888

---
+++ include/exc_handle.h
@@ -0,0 +1,7 @@
+DECL|Z_EXC_DECLARE|macro|Z_EXC_DECLARE
+DECL|Z_EXC_HANDLE|macro|Z_EXC_HANDLE
+DECL|_ZEPHYR_EXC_HANDLE_H_|macro|_ZEPHYR_EXC_HANDLE_H_
+DECL|end|member|void *end;
+DECL|fixup|member|void *fixup;
+DECL|start|member|void *start;
+DECL|z_exc_handle|struct|struct z_exc_handle {
