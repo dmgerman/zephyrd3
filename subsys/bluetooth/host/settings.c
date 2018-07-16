50b3e6fce1976d207e27950384e52614b8073cb7
Johan Hedberg <johan.hedberg@intel.com>
2018-07-16 14:39:06 +0300

    Bluetooth: Add full storage support for multiple identities
    Update the storage handling to take into account multiple identities.
    We can save a bit of code by using the new bt_id_create() API from
    within settings.c.
    
    Also make the treatment of addr & irk parameters to bt_id_create()
    consistent, in that NULL is acceptable for both of them.
    
    Signed-off-by: Johan Hedberg <johan.hedberg@intel.com>
    
    Former-commit-id: e70c556dcc16ddfdf7296a71801e3f318b67f6f0

--- subsys/bluetooth/host/settings.c
+++ subsys/bluetooth/host/settings.c
@@ -1,10 +1,13 @@
 DECL|BT_DBG_ENABLED|macro|BT_DBG_ENABLED
+DECL|ID_DATA_LEN|macro|ID_DATA_LEN
+DECL|ID_SIZE_MAX|macro|ID_SIZE_MAX
+DECL|ID_SIZE_MAX|macro|ID_SIZE_MAX
 DECL|bt_settings_decode_key|function|int bt_settings_decode_key(char *key, bt_addr_le_t *addr)
 DECL|bt_settings_encode_key|function|void bt_settings_encode_key(char *path, size_t path_size, const char *subsys, bt_addr_le_t *addr, const char *key)
 DECL|bt_settings_init|function|int bt_settings_init(void)
+DECL|bt_settings_save_id|function|void bt_settings_save_id(void)
 DECL|bt_settings|variable|bt_settings
 DECL|commit|function|static int commit(void)
 DECL|export|function|static int export(int (*func)(const char *name, char *val), enum settings_export_tgt tgt)
-DECL|generate_irk|function|static void generate_irk(void)
-DECL|generate_static_addr|function|static void generate_static_addr(void)
+DECL|save_id|function|static void save_id(struct k_work *work)
 DECL|set|function|static int set(int argc, char **argv, char *val)
