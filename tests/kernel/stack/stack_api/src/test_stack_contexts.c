e8380775f2e65d3bc23497e671acdeb9f1de384b
Ajay Kishore <ajay.kishore@intel.com>
2018-07-17 06:05:13 +0530

    tests: kernel: Add test to validate k_stack_alloc_init, k_stack_cleanup
    This test is intended to validate k_stack_cleanup() and
    k_stack_alloc_init(). Passed stack pointer in k_stack_alloc_init()
    is statically defined.
    
    Signed-off-by: Ajay Kishore <ajay.kishore@intel.com>
    
    Former-commit-id: cea73067cec3abf7d81ec5671f106ea42557e151

--- tests/kernel/stack/stack_api/src/test_stack_contexts.c
+++ tests/kernel/stack/stack_api/src/test_stack_contexts.c
@@ -6,6 +6,7 @@ DECL|stack|variable|stack
 DECL|tIsr_entry_pop|function|static void tIsr_entry_pop(void *p)
 DECL|tIsr_entry_push|function|static void tIsr_entry_push(void *p)
 DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *p3)
+DECL|test_stack_alloc_thread2thread|function|void test_stack_alloc_thread2thread(void)
 DECL|test_stack_thread2isr|function|void test_stack_thread2isr(void)
 DECL|test_stack_thread2thread|function|void test_stack_thread2thread(void)
 DECL|test_stack_user_thread2thread|function|void test_stack_user_thread2thread(void)
