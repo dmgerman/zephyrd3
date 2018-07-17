48ab989f002d6d6ab20338feb92a78d829658782
Diego Sueiro <diego.sueiro@gmail.com>
2018-07-17 06:42:30 +0100

    arch: Add i.MX7 PWM get clock frequency function
    This patch adds the get_pwm_clock_freq function to be used by the
    i.MX PWM driver.
    
    Signed-off-by: Diego Sueiro <diego.sueiro@gmail.com>
    
    Former-commit-id: c2454309de76ae440f6db44d20b2b084be323f05

---
+++ arch/arm/soc/nxp_imx/mcimx7_m4/soc_clk_freq.c
@@ -0,0 +1,1 @@
+DECL|get_pwm_clock_freq|function|u32_t get_pwm_clock_freq(PWM_Type *base)
---
+++ arch/arm/soc/nxp_imx/mcimx7_m4/soc_clk_freq.h
@@ -0,0 +1,20 @@
+DECL|__SOC_CLOCK_FREQ_H__|macro|__SOC_CLOCK_FREQ_H__
+DECL|_ccm_ccgr_gate_extra|enum|enum _ccm_ccgr_gate_extra {
+DECL|_ccm_root_control_extra|enum|enum _ccm_root_control_extra {
+DECL|_ccm_rootmux_pwm|enum|enum _ccm_rootmux_pwm {
+DECL|ccmCcgrGatePwm1|enumerator|ccmCcgrGatePwm1 = (u32_t)(&CCM_CCGR132),
+DECL|ccmCcgrGatePwm2|enumerator|ccmCcgrGatePwm2 = (u32_t)(&CCM_CCGR133),
+DECL|ccmCcgrGatePwm3|enumerator|ccmCcgrGatePwm3 = (u32_t)(&CCM_CCGR134),
+DECL|ccmCcgrGatePwm4|enumerator|ccmCcgrGatePwm4 = (u32_t)(&CCM_CCGR135),
+DECL|ccmRootPwm1|enumerator|ccmRootPwm1 = (u32_t)(&CCM_TARGET_ROOT106),
+DECL|ccmRootPwm2|enumerator|ccmRootPwm2 = (u32_t)(&CCM_TARGET_ROOT107),
+DECL|ccmRootPwm3|enumerator|ccmRootPwm3 = (u32_t)(&CCM_TARGET_ROOT108),
+DECL|ccmRootPwm4|enumerator|ccmRootPwm4 = (u32_t)(&CCM_TARGET_ROOT109),
+DECL|ccmRootmuxPwmAudioPll|enumerator|ccmRootmuxPwmAudioPll = 4U,
+DECL|ccmRootmuxPwmEnetPllDiv10|enumerator|ccmRootmuxPwmEnetPllDiv10 = 1U,
+DECL|ccmRootmuxPwmEnetPllDiv25|enumerator|ccmRootmuxPwmEnetPllDiv25 = 3U,
+DECL|ccmRootmuxPwmExtClk2|enumerator|ccmRootmuxPwmExtClk2 = 5U,
+DECL|ccmRootmuxPwmOsc24m|enumerator|ccmRootmuxPwmOsc24m = 0U,
+DECL|ccmRootmuxPwmRef1m|enumerator|ccmRootmuxPwmRef1m = 6U,
+DECL|ccmRootmuxPwmSysPllDiv4|enumerator|ccmRootmuxPwmSysPllDiv4 = 2U,
+DECL|ccmRootmuxPwmVideoPll|enumerator|ccmRootmuxPwmVideoPll = 7U,
