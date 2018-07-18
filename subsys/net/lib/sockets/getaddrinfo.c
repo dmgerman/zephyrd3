bbead1c9e8663fb5217a5110ad5f1534cffabd71
Andrew Boie <andrew.p.boie@intel.com>
2018-07-18 11:25:49 -0700

    net: getaddrinfo: ai_state no longer global
    Move this onto the call stack of the getaddrinfo() function,
    there's no need for it to be global.
    
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>
    
    Former-commit-id: 1b37602859475667c0ccc751d706c904615e14e5

--- subsys/net/lib/sockets/getaddrinfo.c
+++ subsys/net/lib/sockets/getaddrinfo.c
@@ -1,7 +1,6 @@
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|ai_arr|variable|ai_arr
-DECL|ai_state|variable|ai_state
 DECL|dns_resolve_cb|function|static void dns_resolve_cb(enum dns_resolve_status status, struct dns_addrinfo *info, void *user_data)
 DECL|getaddrinfo_state|struct|struct getaddrinfo_state {
 DECL|hints|member|const struct zsock_addrinfo *hints;
