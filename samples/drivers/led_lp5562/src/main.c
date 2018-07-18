910386748374f82c71ebe0af33ebfcd855896049
Johannes Hutter <johannes@proglove.de>
2018-07-18 18:19:35 +0200

    samples: drivers: Add sample application for LP5562
    Add a sample application to show the LP5562 driver. The three RGB
    channels are used to set multiple colors after each other under the
    assumption that accordingly colored LEDs are connected. Blinking is also
    shown in multiple colors.
    
    Signed-off-by: Johannes Hutter <johannes@proglove.de>
    
    Former-commit-id: f8d53e5d208ca79234ed7d3d8997646cc6be2220

---
+++ samples/drivers/led_lp5562/src/main.c
@@ -0,0 +1,17 @@
+DECL|BLINK_DELAY_OFF|macro|BLINK_DELAY_OFF
+DECL|BLINK_DELAY_ON|macro|BLINK_DELAY_ON
+DECL|COLORS_TO_SHOW|macro|COLORS_TO_SHOW
+DECL|DELAY_TIME|macro|DELAY_TIME
+DECL|LED_B|macro|LED_B
+DECL|LED_DEV_NAME|macro|LED_DEV_NAME
+DECL|LED_G|macro|LED_G
+DECL|LED_R|macro|LED_R
+DECL|NUM_LEDS|macro|NUM_LEDS
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|VALUES_PER_COLOR|macro|VALUES_PER_COLOR
+DECL|blink_color|function|static int blink_color(struct device *dev, bool r, bool g, bool b,u32_t delay_on, u32_t delay_off)
+DECL|colors|variable|colors
+DECL|main|function|void main(void)
+DECL|scale_color_to_percent|function|static inline u8_t scale_color_to_percent(u8_t hex)
+DECL|set_static_color|function|static int set_static_color(struct device *dev, u8_t r, u8_t g, u8_t b)
+DECL|turn_off_all_leds|function|static int turn_off_all_leds(struct device *dev)
