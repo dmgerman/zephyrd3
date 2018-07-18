44ecb2801379c57e639451fc50e68c9cd46ac33e
Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
2018-07-18 11:54:10 +0200

    arch: arm: re-organize arm fault handling
    This commit does several things:
    - It unifies the way the fault inspection is implemented,
      irrespective of the selected fault dumping level.
    - It removes the dumping of the Thread ID and faulting
      instruction address, as these are now displayed by
      _NanoFatalErrorHandler.
    - It introduces a new debug print directive, to be used
      for the highest dumping level (2).
    
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    
    Former-commit-id: f713fa5d527d39a73848293cd8f45a4eb9bd0d7d

--- arch/arm/core/fault.c
+++ arch/arm/core/fault.c
@@ -26,23 +26,23 @@ DECL|EXC_RETURN_STACK_FRAME_TYPE_EXTENDED|macro|EXC_RETURN_STACK_FRAME_TYPE_EXTE
 DECL|EXC_RETURN_STACK_FRAME_TYPE_Msk|macro|EXC_RETURN_STACK_FRAME_TYPE_Msk
 DECL|EXC_RETURN_STACK_FRAME_TYPE_Pos|macro|EXC_RETURN_STACK_FRAME_TYPE_Pos
 DECL|EXC_RETURN_STACK_FRAME_TYPE_STANDARD|macro|EXC_RETURN_STACK_FRAME_TYPE_STANDARD
-DECL|FAULT_DUMP|macro|FAULT_DUMP
-DECL|FAULT_DUMP|macro|FAULT_DUMP
 DECL|INTEGRITY_SIGNATURE_EXT|macro|INTEGRITY_SIGNATURE_EXT
 DECL|INTEGRITY_SIGNATURE_STD|macro|INTEGRITY_SIGNATURE_STD
 DECL|INTEGRITY_SIGNATURE|macro|INTEGRITY_SIGNATURE
 DECL|PR_EXC|macro|PR_EXC
 DECL|PR_EXC|macro|PR_EXC
+DECL|PR_FAULT_INFO|macro|PR_FAULT_INFO
+DECL|PR_FAULT_INFO|macro|PR_FAULT_INFO
 DECL|SECURE_STACK_DUMP|macro|SECURE_STACK_DUMP
 DECL|SECURE_STACK_DUMP|macro|SECURE_STACK_DUMP
 DECL|STORE_xFAR|macro|STORE_xFAR
 DECL|STORE_xFAR|macro|STORE_xFAR
 DECL|_BusFault|function|static void _BusFault(const NANO_ESF *esf, int fromHardFault)
 DECL|_DebugMonitor|function|static void _DebugMonitor(const NANO_ESF *esf)
-DECL|_FaultDump|function|static u32_t _FaultDump(const NANO_ESF *esf, int fault)
-DECL|_FaultDump|function|u32_t _FaultDump(const NANO_ESF *esf, int fault)
+DECL|_FaultHandle|function|static u32_t _FaultHandle(const NANO_ESF *esf, int fault)
 DECL|_FaultInit|function|void _FaultInit(void)
-DECL|_FaultThreadShow|function|static void _FaultThreadShow(const NANO_ESF *esf)
+DECL|_FaultShow|function|static void _FaultShow(const NANO_ESF *esf, int fault)
+DECL|_FaultShow|function|static void _FaultShow(const NANO_ESF *esf, int fault)
 DECL|_Fault|function|void _Fault(NANO_ESF *esf, u32_t exc_return)
 DECL|_HardFault|function|static u32_t _HardFault(const NANO_ESF *esf)
 DECL|_MpuFault|function|static u32_t _MpuFault(const NANO_ESF *esf, int fromHardFault)
