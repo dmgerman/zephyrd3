6d1bc7aaa3c9c7ceb2e0f0a0ce94cfbd374e3d76
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-07-18 12:47:50 +0300

    samples: net: tp: Avoid compiler warning
    If -fno-strict-overflow compiler flag is disabled, then this warning
    is printed:
    
    samples/net/throughput_server/src/server.c:157:6: \
      error: assuming signed overflow does not occur when assuming \
      that (X + c) >= X is always true [-Werror=strict-overflow]
         if (new_print > curr) {
    
    Fixes #8924
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: af34779510e8334cf0bbac92df77c6c73bbe74ae

--- samples/net/throughput_server/src/server.c
+++ samples/net/throughput_server/src/server.c
@@ -6,6 +6,7 @@ DECL|PRINT_STATISTICS_INTERVAL|macro|PRINT_STATISTICS_INTERVAL
 DECL|PRINT_STATS_SECS|macro|PRINT_STATS_SECS
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|build_reply_pkt|function|struct net_pkt *build_reply_pkt(const char *name,struct net_app_ctx *ctx, struct net_pkt *pkt)
+DECL|cmp_val|function|static inline s64_t cmp_val(u64_t val1, u64_t val2)
 DECL|init_app|function|static inline int init_app(void)
 DECL|main|function|void main(void)
 DECL|pkt_sent|function|void pkt_sent(struct net_app_ctx *ctx, int status, void *user_data_send, void *user_data)
