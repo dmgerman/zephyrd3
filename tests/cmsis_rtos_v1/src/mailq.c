c317e47b0f6757422e661f7e46b4fc1116401631
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-18 12:04:31 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of mailq APIs
    Some test programs to illustrate the usage of mailq APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: 1b6eb7d1ad871664970253a20251eebd51163181

---
+++ tests/cmsis_rtos_v1/src/mailq.c
@@ -0,0 +1,14 @@
+DECL|MAIL1_DATA1|macro|MAIL1_DATA1
+DECL|MAIL1_DATA2|macro|MAIL1_DATA2
+DECL|MAIL1_DATA3|macro|MAIL1_DATA3
+DECL|MAIL2_DATA1|macro|MAIL2_DATA1
+DECL|MAIL2_DATA2|macro|MAIL2_DATA2
+DECL|MAIL2_DATA3|macro|MAIL2_DATA3
+DECL|data1|member|int data1;
+DECL|data2|member|unsigned char data2;
+DECL|data3|member|unsigned int data3;
+DECL|mail_id|variable|mail_id
+DECL|mail_recv|function|void mail_recv(void)
+DECL|sample_data|struct|struct sample_data {
+DECL|send_thread|function|void send_thread(void const *argument)
+DECL|test_mailq|function|void test_mailq(void)
