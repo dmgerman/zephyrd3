7b93f2bdd80d03787e04a18283158929142d2721
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-18 09:55:57 +0530

    lib/cmsis_rtos_v1: Implement support for kernel APIs
    These APIs allow for checking whether the kernel is initialized,
    started or running.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: 76fc443b1e8983d4ba21e48c18bf2f8552143980

--- lib/cmsis_rtos_v1/cmsis_kernel.c
+++ lib/cmsis_rtos_v1/cmsis_kernel.c
@@ -1,1 +1,4 @@
+DECL|osKernelInitialize|function|osStatus osKernelInitialize(void)
+DECL|osKernelRunning|function|int32_t osKernelRunning(void)
+DECL|osKernelStart|function|osStatus osKernelStart(void)
 DECL|osKernelSysTick|function|uint32_t osKernelSysTick(void)
