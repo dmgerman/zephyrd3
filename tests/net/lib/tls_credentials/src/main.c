21b8fd67d565ccaae134600555cc08d51cb2df22
Robert Lubos <robert.lubos@nordicsemi.no>
2018-07-19 10:24:51 +0200

    tests: net: TLS credentials subsystem tests
    Add some tests to verify basic TLS credentials subsystem operation.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    
    Former-commit-id: 6101e232f813fafaff0f7cff6e16cef1136f0902

---
+++ tests/net/lib/tls_credentials/src/main.c
@@ -0,0 +1,11 @@
+DECL|common_tag|variable|common_tag
+DECL|invalid_tag|variable|invalid_tag
+DECL|test_ca_cert|variable|test_ca_cert
+DECL|test_credential_add|function|static void test_credential_add(void)
+DECL|test_credential_delete|function|static void test_credential_delete(void)
+DECL|test_credential_get|function|static void test_credential_get(void)
+DECL|test_credential_internal_iterate|function|static void test_credential_internal_iterate(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_server_cert|variable|test_server_cert
+DECL|test_server_key|variable|test_server_key
+DECL|unused_tag|variable|unused_tag
