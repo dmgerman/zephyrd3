38b215ea5919aafee7162b42a8791b6aee4df111
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-07-20 16:49:07 +0300

    tests: net: if: Add promisc mode tests
    Make sure that user is able to toggle promiscuous mode flag
    in network interface level and that flag value is passed
    to device driver.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: e18900c5c597184371246435263119b2116c5cad

--- tests/net/iface/src/main.c
+++ tests/net/iface/src/main.c
@@ -4,14 +4,29 @@ DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|WAIT_TIME|macro|WAIT_TIME
 DECL|_ETH_L2_CTX_TYPE|macro|_ETH_L2_CTX_TYPE
 DECL|_ETH_L2_LAYER|macro|_ETH_L2_LAYER
+DECL|check_promisc_mode_off|function|static void check_promisc_mode_off(void)
+DECL|check_promisc_mode_on|function|static void check_promisc_mode_on(void)
+DECL|eth_fake_api_funcs|variable|eth_fake_api_funcs
+DECL|eth_fake_context|struct|struct eth_fake_context {
+DECL|eth_fake_data|variable|eth_fake_data
+DECL|eth_fake_get_capabilities|function|static enum ethernet_hw_caps eth_fake_get_capabilities(struct device *dev)
+DECL|eth_fake_iface_init|function|static void eth_fake_iface_init(struct net_if *iface)
+DECL|eth_fake_init|function|static int eth_fake_init(struct device *dev)
+DECL|eth_fake_send|function|static int eth_fake_send(struct net_if *iface, struct net_pkt *pkt)
+DECL|eth_fake_set_config|function|static int eth_fake_set_config(struct device *dev, enum ethernet_config_type type, const struct ethernet_config *config)
 DECL|idx|member|u8_t idx;
 DECL|iface1|variable|iface1
+DECL|iface2str|function|static const char *iface2str(struct net_if *iface)
 DECL|iface2|variable|iface2
 DECL|iface3|variable|iface3
+DECL|iface4|variable|iface4
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
 DECL|iface_setup|function|static void iface_setup(void)
+DECL|iface|member|struct net_if *iface;
 DECL|in6addr_mcast|variable|in6addr_mcast
 DECL|ll_addr|member|struct net_linkaddr ll_addr;
 DECL|ll_addr|variable|ll_addr
+DECL|mac_address|member|u8_t mac_address[6];
 DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
 DECL|my_addr1|variable|my_addr1
 DECL|my_addr2|variable|my_addr2
@@ -24,6 +39,7 @@ DECL|net_iface_api|variable|net_iface_api
 DECL|net_iface_dev_init|function|static int net_iface_dev_init(struct device *dev)
 DECL|net_iface_get_mac|function|static u8_t *net_iface_get_mac(struct device *dev)
 DECL|net_iface_init|function|static void net_iface_init(struct net_if *iface)
+DECL|promisc_mode|member|bool promisc_mode;
 DECL|send_iface1_down|function|static void send_iface1_down(void)
 DECL|send_iface1_up|function|static void send_iface1_up(void)
 DECL|send_iface1|function|static void send_iface1(void)
@@ -31,6 +47,9 @@ DECL|send_iface2|function|static void send_iface2(void)
 DECL|send_iface3|function|static void send_iface3(void)
 DECL|send_iface|function|static bool send_iface(struct net_if *iface, int val, bool expect_fail)
 DECL|sender_iface|function|static int sender_iface(struct net_if *iface, struct net_pkt *pkt)
+DECL|set_promisc_mode_off|function|static void set_promisc_mode_off(void)
+DECL|set_promisc_mode_on_again|function|static void set_promisc_mode_on_again(void)
+DECL|set_promisc_mode_on|function|static void set_promisc_mode_on(void)
 DECL|test_failed|variable|test_failed
 DECL|test_main|function|void test_main(void)
 DECL|test_started|variable|test_started
