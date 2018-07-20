8078dd277ef11438264694b285498e18baba76e7
Spoorthi K <spoorthi.k@intel.com>
2018-07-20 16:03:44 +0530

    tests: queue: Enhance tests to improve coverage
    Modify test to cover k_queue_insert() and k_queue_alloc_prepend(),
           and allocation failure/success scenario.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: 7c8aa526cb8618b077686a0437fb83faffce07dc

--- tests/kernel/queue/src/test_queue_contexts.c
+++ tests/kernel/queue/src/test_queue_contexts.c
@@ -1,6 +1,8 @@
 DECL|LIST_LEN|macro|LIST_LEN
 DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|data_append|variable|data_append
 DECL|data_l|variable|data_l
+DECL|data_prepend|variable|data_prepend
 DECL|data_p|variable|data_p
 DECL|data_sl|variable|data_sl
 DECL|data|variable|data
@@ -13,10 +15,12 @@ DECL|tThread_entry|function|static void tThread_entry(void *p1, void *p2, void *
 DECL|tThread_get|function|static void tThread_get(void *p1, void *p2, void *p3)
 DECL|tdata1|variable|tdata1
 DECL|tdata|variable|tdata
+DECL|test_queue_alloc|function|void test_queue_alloc(void)
 DECL|test_queue_get_2threads|function|void test_queue_get_2threads(void)
 DECL|test_queue_isr2thread|function|void test_queue_isr2thread(void)
 DECL|test_queue_thread2isr|function|void test_queue_thread2isr(void)
 DECL|test_queue_thread2thread|function|void test_queue_thread2thread(void)
+DECL|tqueue_alloc|function|static void tqueue_alloc(struct k_queue *pqueue)
 DECL|tqueue_append|function|static void tqueue_append(struct k_queue *pqueue)
 DECL|tqueue_get_2threads|function|static void tqueue_get_2threads(struct k_queue *pqueue)
 DECL|tqueue_get|function|static void tqueue_get(struct k_queue *pqueue)
