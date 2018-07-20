73ae1b693af430bdb4cc84139018675ed9e9f45b
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-07-20 16:02:24 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of messageQ APIs
    Some test programs to illustrate the usage of msgq APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: f4999f79491e3c3f24ca4a7b16144a187b9f5581

---
+++ tests/cmsis_rtos_v1/src/msgq.c
@@ -0,0 +1,6 @@
+DECL|MESSAGE1|macro|MESSAGE1
+DECL|MESSAGE2|macro|MESSAGE2
+DECL|message_id|variable|message_id
+DECL|message_recv|function|void message_recv(void)
+DECL|send_msg_thread|function|void send_msg_thread(void const *argument)
+DECL|test_messageq|function|void test_messageq(void)
