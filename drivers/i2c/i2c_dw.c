d5ad0a1c4b6df8e276c35edcdc90ec6ec7033a58
Daniel Leung <daniel.leung@intel.com>
2018-07-23 15:06:28 -0700

    drivers/i2c: i2c_dw: extends driver to support 8 controllers
    This adds the necessary driver structs to support a total of 8
    I2C controllers. This also allows each I2C controller to have
    its own PCI vendor/device IDs.
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 67b48ab2a8aa876c375f3d81fa04573383d36aae

--- drivers/i2c/i2c_dw.c
+++ drivers/i2c/i2c_dw.c
@@ -1,3 +1,9 @@
+DECL|I2C_2_runtime|variable|I2C_2_runtime
+DECL|I2C_3_runtime|variable|I2C_3_runtime
+DECL|I2C_4_runtime|variable|I2C_4_runtime
+DECL|I2C_5_runtime|variable|I2C_5_runtime
+DECL|I2C_6_runtime|variable|I2C_6_runtime
+DECL|I2C_7_runtime|variable|I2C_7_runtime
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|_i2c_dw_data_ask|function|static inline void _i2c_dw_data_ask(struct device *dev)
 DECL|_i2c_dw_data_read|function|static void _i2c_dw_data_read(struct device *dev)
@@ -9,8 +15,20 @@ DECL|i2c_0_runtime|variable|i2c_0_runtime
 DECL|i2c_1_runtime|variable|i2c_1_runtime
 DECL|i2c_config_0|function|static void i2c_config_0(struct device *port)
 DECL|i2c_config_1|function|static void i2c_config_1(struct device *port)
+DECL|i2c_config_2|function|static void i2c_config_2(struct device *port)
+DECL|i2c_config_3|function|static void i2c_config_3(struct device *port)
+DECL|i2c_config_4|function|static void i2c_config_4(struct device *port)
+DECL|i2c_config_5|function|static void i2c_config_5(struct device *port)
+DECL|i2c_config_6|function|static void i2c_config_6(struct device *port)
+DECL|i2c_config_7|function|static void i2c_config_7(struct device *port)
 DECL|i2c_config_dw_0|variable|i2c_config_dw_0
 DECL|i2c_config_dw_1|variable|i2c_config_dw_1
+DECL|i2c_config_dw_2|variable|i2c_config_dw_2
+DECL|i2c_config_dw_3|variable|i2c_config_dw_3
+DECL|i2c_config_dw_4|variable|i2c_config_dw_4
+DECL|i2c_config_dw_5|variable|i2c_config_dw_5
+DECL|i2c_config_dw_6|variable|i2c_config_dw_6
+DECL|i2c_config_dw_7|variable|i2c_config_dw_7
 DECL|i2c_dw_initialize|function|static int i2c_dw_initialize(struct device *dev)
 DECL|i2c_dw_isr|function|static void i2c_dw_isr(void *arg)
 DECL|i2c_dw_pci_setup|function|static inline int i2c_dw_pci_setup(struct device *dev)
