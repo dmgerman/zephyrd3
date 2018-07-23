e9c2880e58a686714f4c0e81451f4a9aa3931ab5
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-07-23 14:34:15 +0200

    net/ipv4: Remove useless return value
    net_ipv4_finalize is always successful.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: abf68bc5ea5fa7aaccd922acfa17a11a37cd0e5e

--- subsys/net/ip/ipv4.c
+++ subsys/net/ip/ipv4.c
@@ -2,6 +2,6 @@ DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|net_ipv4_broadcast_address|function|const struct in_addr *net_ipv4_broadcast_address(void)
 DECL|net_ipv4_create|function|struct net_pkt *net_ipv4_create(struct net_pkt *pkt,const struct in_addr *src, const struct in_addr *dst, struct net_if *iface, u8_t next_header_proto)
-DECL|net_ipv4_finalize|function|int net_ipv4_finalize(struct net_pkt *pkt, u8_t next_header_proto)
+DECL|net_ipv4_finalize|function|void net_ipv4_finalize(struct net_pkt *pkt, u8_t next_header_proto)
 DECL|net_ipv4_process_pkt|function|enum net_verdict net_ipv4_process_pkt(struct net_pkt *pkt)
 DECL|net_ipv4_unspecified_address|function|const struct in_addr *net_ipv4_unspecified_address(void)
