3b53b4a9f5ae3b014e17454ff6400eec5b7c226c
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-07-24 11:29:00 +0300

    tests: net: Add tests for net_promisc API
    Test that user is able to turn on/off promiscuous mode and
    also receive any data from network interface.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d202834e867659fe2e6d0dcded2c239210625426

---
+++ tests/net/promiscuous/src/main.c
@@ -0,0 +1,44 @@
+DECL|DBG|macro|DBG
+DECL|DBG|macro|DBG
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|WAIT_TIME|macro|WAIT_TIME
+DECL|_recv_data|function|static void _recv_data(struct net_if *iface, struct net_pkt **pkt)
+DECL|_set_promisc_mode_off_again|function|static void _set_promisc_mode_off_again(struct net_if *iface)
+DECL|_set_promisc_mode_off|function|static void _set_promisc_mode_off(struct net_if *iface)
+DECL|_set_promisc_mode_on_again|function|static void _set_promisc_mode_on_again(struct net_if *iface)
+DECL|_set_promisc_mode_on|function|static void _set_promisc_mode_on(struct net_if *iface)
+DECL|eth_fake_api_funcs|variable|eth_fake_api_funcs
+DECL|eth_fake_context|struct|struct eth_fake_context {
+DECL|eth_fake_data1|variable|eth_fake_data1
+DECL|eth_fake_data2|variable|eth_fake_data2
+DECL|eth_fake_get_capabilities|function|static enum ethernet_hw_caps eth_fake_get_capabilities(struct device *dev)
+DECL|eth_fake_iface_init|function|static void eth_fake_iface_init(struct net_if *iface)
+DECL|eth_fake_init|function|static int eth_fake_init(struct device *dev)
+DECL|eth_fake_send|function|static int eth_fake_send(struct net_if *iface, struct net_pkt *pkt)
+DECL|eth_fake_set_config|function|static int eth_fake_set_config(struct device *dev, enum ethernet_config_type type, const struct ethernet_config *config)
+DECL|idx|member|u8_t idx;
+DECL|iface1|variable|iface1
+DECL|iface2str|function|static const char *iface2str(struct net_if *iface)
+DECL|iface2|variable|iface2
+DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
+DECL|iface_setup|function|static void iface_setup(void)
+DECL|iface|member|struct net_if *iface;
+DECL|in6addr_mcast|variable|in6addr_mcast
+DECL|ll_addr|member|struct net_linkaddr ll_addr;
+DECL|ll_addr|variable|ll_addr
+DECL|mac_address|member|u8_t mac_address[6];
+DECL|mac_addr|member|u8_t mac_addr[sizeof(struct net_eth_addr)];
+DECL|my_addr1|variable|my_addr1
+DECL|my_addr2|variable|my_addr2
+DECL|my_addr3|variable|my_addr3
+DECL|net_if_test|struct|struct net_if_test {
+DECL|pkt1|variable|pkt1
+DECL|pkt2|variable|pkt2
+DECL|promisc_mode|member|bool promisc_mode;
+DECL|recv_data|function|static void recv_data(void)
+DECL|set_promisc_mode_off_again|function|static void set_promisc_mode_off_again(void)
+DECL|set_promisc_mode_off|function|static void set_promisc_mode_off(void)
+DECL|set_promisc_mode_on_again|function|static void set_promisc_mode_on_again(void)
+DECL|set_promisc_mode_on|function|static void set_promisc_mode_on(void)
+DECL|test_main|function|void test_main(void)
+DECL|verify_data|function|static void verify_data(void)
