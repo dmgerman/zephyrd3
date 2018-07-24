beca112e88670fdb41c02c09ad60d49ae97752e5
Niranjhana N <niranjhana.n@intel.com>
2018-07-24 10:56:11 +0530

    tests: posix: improve pthread_key test
    In addition to testing all the APIs, this enhancement
    tests the following two scenarios
      - when multiple threads use a single key for storage
      - when a single thread associates its value with
        multiple keys
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: e21e6c1b970d5298651eecc757fde89f186ad3c6

--- tests/posix/pthread_key/src/pthread_key.c
+++ tests/posix/pthread_key/src/pthread_key.c
@@ -1,8 +1,15 @@
 DECL|BUFFSZ|macro|BUFFSZ
+DECL|N_KEY|macro|N_KEY
+DECL|N_THR|macro|N_THR
 DECL|STACKSZ|macro|STACKSZ
 DECL|key_once|variable|key_once
+DECL|keys_once|variable|keys_once
+DECL|keys|variable|keys
 DECL|key|variable|key
+DECL|make_keys|function|static void make_keys(void)
 DECL|make_key|function|static void make_key(void)
 DECL|test_main|function|void test_main(void)
-DECL|test_pthread_key|function|void test_pthread_key(void)
+DECL|test_multiple_threads_single_key|function|void test_multiple_threads_single_key(void)
+DECL|test_single_thread_multiple_keys|function|void test_single_thread_multiple_keys(void)
+DECL|thread_func|function|void *thread_func(void *p1)
 DECL|thread_top|function|void *thread_top(void *p1)
