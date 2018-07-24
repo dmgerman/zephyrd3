d80f482f1998fe6dfa9e85ff2cf1f50b281dfa34
Ajay Kishore <ajay.kishore@intel.com>
2018-07-24 23:49:33 +0530

    tests: msgq: Enhance tests to improve code coverage
    This test is intended to validate k_msgq_alloc_init()
    and k_msgq_cleanup(), when CONFIG_USERSPACE is not defined.
    Also added test to validate k_msgq_get() api, if any thread is
    pending to write in message queue.
    
    Signed-off-by: Ajay Kishore <ajay.kishore@intel.com>
    
    Former-commit-id: 7b24690e143a59fe20f3524a65dc1f8863b64ae6

--- tests/kernel/msgq/msgq_api/src/test_msgq_contexts.c
+++ tests/kernel/msgq/msgq_api/src/test_msgq_contexts.c
@@ -1,18 +1,26 @@
 DECL|data|variable|data
 DECL|end_sema|variable|end_sema
 DECL|get_msgq|function|static void get_msgq(struct k_msgq *pmsgq)
+DECL|msgq1|variable|msgq1
 DECL|msgq_isr|function|static void msgq_isr(struct k_msgq *pmsgq)
+DECL|msgq_thread_data_passing|function|static void msgq_thread_data_passing(struct k_msgq *pmsgq)
 DECL|msgq_thread_overflow|function|static void msgq_thread_overflow(struct k_msgq *pmsgq)
 DECL|msgq_thread|function|static void msgq_thread(struct k_msgq *pmsgq)
 DECL|msgq|variable|msgq
+DECL|pend_thread_entry|function|static void pend_thread_entry(void *p1, void *p2, void *p3)
 DECL|purge_msgq|function|static void purge_msgq(struct k_msgq *pmsgq)
 DECL|put_msgq|function|static void put_msgq(struct k_msgq *pmsgq)
+DECL|tdata1|variable|tdata1
+DECL|tdata2|variable|tdata2
 DECL|tdata|variable|tdata
+DECL|test_msgq_alloc|function|void test_msgq_alloc(void)
 DECL|test_msgq_isr|function|void test_msgq_isr(void)
+DECL|test_msgq_pend_thread|function|void test_msgq_pend_thread(void)
 DECL|test_msgq_thread_overflow|function|void test_msgq_thread_overflow(void)
 DECL|test_msgq_thread|function|void test_msgq_thread(void)
 DECL|test_msgq_user_thread_overflow|function|void test_msgq_user_thread_overflow(void)
 DECL|test_msgq_user_thread|function|void test_msgq_user_thread(void)
+DECL|thread_entry_get_data|function|static void thread_entry_get_data(void *p1, void *p2, void *p3)
 DECL|thread_entry_overflow|function|static void thread_entry_overflow(void *p1, void *p2, void *p3)
 DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
 DECL|tisr_entry|function|static void tisr_entry(void *p)
