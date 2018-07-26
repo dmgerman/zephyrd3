a1a0b055ed35c9e2fc5910a94716a76a69f851f4
Lars Knudsen <larsgk@gmail.com>
2018-07-26 22:29:25 +0200

    drivers: sensor: Add support for MMA8451Q (3-axis accelerometer)
    Only basic features supported initially but more could be added from:
    https://www.nxp.com/docs/en/data-sheet/MMA8451Q.pdf
    
    fixes #9006
    
    A sample app will be provided in a separate PR.
    
    Signed-off-by: Lars Knudsen <larsgk@gmail.com>
    
    Former-commit-id: 9c0d7813e55e323f6c2613faef0d6772fb016196

---
+++ drivers/sensor/mma8451q/mma8451q.c
@@ -0,0 +1,7 @@
+DECL|mma8451q_accel_convert|function|static void mma8451q_accel_convert(struct sensor_value *val, s16_t raw, enum mma8451q_range range)
+DECL|mma8451q_channel_get|function|static int mma8451q_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
+DECL|mma8451q_config|variable|mma8451q_config
+DECL|mma8451q_data|variable|mma8451q_data
+DECL|mma8451q_driver_api|variable|mma8451q_driver_api
+DECL|mma8451q_init|function|int mma8451q_init(struct device *dev)
+DECL|mma8451q_sample_fetch|function|static int mma8451q_sample_fetch(struct device *dev, enum sensor_channel chan)
