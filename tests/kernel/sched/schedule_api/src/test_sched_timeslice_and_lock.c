e5428a12a047c3cc3f52f5107a8e265d7e827c7b
Ajay Kishore <ajay.kishore@intel.com>
2018-07-27 19:44:35 +0530

    tests: sched: Enhance tests to improve code coverage
    call k_wakeup() if the timer has expired and the thread
    is not even in the sleep state.
    
    Signed-off-by: Ajay Kishore <ajay.kishore@intel.com>
    
    Former-commit-id: dbc8789a19f777a325041205050326fc68e94af8

--- tests/kernel/sched/schedule_api/src/test_sched_timeslice_and_lock.c
+++ tests/kernel/sched/schedule_api/src/test_sched_timeslice_and_lock.c
@@ -1,3 +1,4 @@
+DECL|DURATION|macro|DURATION
 DECL|THREADS_NUM|macro|THREADS_NUM
 DECL|coop_thread|function|static void coop_thread(void *p1, void *p2, void *p3)
 DECL|executed|variable|executed
@@ -15,7 +16,11 @@ DECL|test_sleep_wakeup_preemptible|function|void test_sleep_wakeup_preemptible(v
 DECL|test_time_slicing_disable_preemptible|function|void test_time_slicing_disable_preemptible(void)
 DECL|test_time_slicing_preemptible|function|void test_time_slicing_preemptible(void)
 DECL|test_unlock_preemptible|function|void test_unlock_preemptible(void)
+DECL|test_wakeup_expired_timer_thread|function|void test_wakeup_expired_timer_thread(void)
 DECL|test_yield_cooperative|function|void test_yield_cooperative(void)
 DECL|thread_entry|function|static void thread_entry(void *p1, void *p2, void *p3)
+DECL|thread_handler|function|static void thread_handler(void *p1, void *p2, void *p3)
+DECL|timer_handler|function|static void timer_handler(struct k_timer *timer)
+DECL|timer|variable|timer
 DECL|tthread|variable|tthread
 DECL|t|variable|t
