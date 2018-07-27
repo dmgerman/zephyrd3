5b0076b1cb4e0165ea5c0de3316e53fa036558bb
Praful Swarnakar <praful.swarnakar@intel.com>
2018-07-28 02:50:49 +0530

    coverage: tests: poll: Add test to validate multiple polling threads
    Add testcase for validating poll events by manipultaing thread
    state to improve code coverage. Also, add multiple threads to
    wait on same event.
    
    Signed-off-by: Praful Swarnakar <praful.swarnakar@intel.com>
    
    Former-commit-id: 94acc18b3e02455743d5dcfcba38363fdc78ac28

--- tests/kernel/poll/src/test_poll.c
+++ tests/kernel/poll/src/test_poll.c
@@ -5,6 +5,8 @@ DECL|TAG_1|macro|TAG_1
 DECL|TAG_2|macro|TAG_2
 DECL|fifo_msg|struct|struct fifo_msg {
 DECL|msg|member|u32_t msg;
+DECL|multi_lowprio|function|static void multi_lowprio(void *p1, void *p2, void *p3)
+DECL|multi_thread_lowprio|variable|multi_thread_lowprio
 DECL|multi_thread|variable|multi_thread
 DECL|multi|function|static void multi(void *p1, void *p2, void *p3)
 DECL|no_wait_fifo|variable|no_wait_fifo
@@ -13,10 +15,14 @@ DECL|no_wait_signal|variable|no_wait_signal
 DECL|poll_wait_helper_thread|variable|poll_wait_helper_thread
 DECL|poll_wait_helper|function|static void poll_wait_helper(void *use_fifo, void *p2, void *p3)
 DECL|private|member|void *private;
+DECL|signal_thread|variable|signal_thread
+DECL|signal|variable|signal
 DECL|test_poll_grant_access|function|void test_poll_grant_access(void)
 DECL|test_poll_multi|function|void test_poll_multi(void)
 DECL|test_poll_no_wait|function|void test_poll_no_wait(void)
+DECL|test_poll_threadstate|function|void test_poll_threadstate(void)
 DECL|test_poll_wait|function|void test_poll_wait(void)
+DECL|threadstate|function|static void threadstate(void *p1, void *p2, void *p3)
 DECL|wait_events|variable|wait_events
 DECL|wait_msg|variable|wait_msg
 DECL|wait_signal|variable|wait_signal
