e70a6c48260314813044c3415b8da5781b2d3cd4
Alberto Escolar Piedras <alpi@oticon.com>
2018-07-29 14:38:49 +0200

    native: Allow cmd line options to be registered dynamically
    Allow command line arguments to be registered dynamically
    instead of being statically defined.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: f742d0e2a251d4f0ebd993563d9ac3ab1779d0ee

--- boards/posix/native_posix/cmdline.c
+++ boards/posix/native_posix/cmdline.c
@@ -1,3 +1,6 @@
+DECL|ARGS_ALLOC_CHUNK_SIZE|macro|ARGS_ALLOC_CHUNK_SIZE
+DECL|args_aval|variable|args_aval
+DECL|args_struct|variable|args_struct
 DECL|args|variable|args
 DECL|bt_dev_index|variable|bt_dev_index
 DECL|cmd_bt_dev_found|function|static void cmd_bt_dev_found(char *argv, int offset)
@@ -9,10 +12,16 @@ DECL|cmd_rtcoffset_found|function|static void cmd_rtcoffset_found(char *argv, in
 DECL|cmd_rtcreset_found|function|static void cmd_rtcreset_found(char *argv, int offset)
 DECL|cmd_seed_found|function|static void cmd_seed_found(char *argv, int offset)
 DECL|cmd_stop_at_found|function|static void cmd_stop_at_found(char *argv, int offset)
+DECL|native_add_btuserchan_option|function|void native_add_btuserchan_option(void)
+DECL|native_add_command_line_opts|function|void native_add_command_line_opts(struct args_struct_t *args)
+DECL|native_add_fakeentropy_option|function|void native_add_fakeentropy_option(void)
+DECL|native_add_testargs_option|function|void native_add_testargs_option(void)
+DECL|native_add_time_options|function|void native_add_time_options(void)
+DECL|native_cleanup_cmd_line|function|void native_cleanup_cmd_line(void)
 DECL|native_get_cmd_line_args|function|void native_get_cmd_line_args(int *argc, char ***argv)
 DECL|native_get_test_cmd_line_args|function|void native_get_test_cmd_line_args(int *argc, char ***argv)
-DECL|native_handle_cmd_line|function|void native_handle_cmd_line(int argc, char *argv[])
 DECL|s_argc|variable|s_argc
 DECL|s_argv|variable|s_argv
 DECL|test_argc|variable|test_argc
 DECL|test_argv|variable|test_argv
+DECL|used_args|variable|used_args
