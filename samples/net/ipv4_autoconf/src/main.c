0ad548bc198635cb37bea5577f106defa92817bb
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-07-30 18:49:43 +0300

    samples: net: IPv4 autoconf sample application
    Adding IPv4 autoconf sample application that can be used to
    test IPv4 autoconf functionality.
    
    Signed-off-by: Matthias Boesl <matthias.boesl@gmail.com>
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: fd1fec8ad6b67b7cea8932ad31b71179c9d53459

---
+++ samples/net/ipv4_autoconf/src/main.c
@@ -0,0 +1,6 @@
+DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
+DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|handler|function|static void handler(struct net_mgmt_event_callback *cb, u32_t mgmt_event, struct net_if *iface)
+DECL|main|function|void main(void)
+DECL|mgmt_cb|variable|mgmt_cb
