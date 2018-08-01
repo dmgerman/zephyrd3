59979d340db9ba65e6443af98b4649460ad09934
Tomasz Gorochowik <tgorochowik@antmicro.com>
2018-08-01 12:55:23 +0200

    tests: net: ethernet_mgmt: Improve and finish Qav testing
    With this commit all Qav parameters are tested and the fake driver
    properly stores and recalculates info about all the Qav parameters to
    make them available for read-back.
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>
    
    Former-commit-id: 7930419ad6dcdba9fda176e871228e7b25310d6f

--- tests/net/ethernet_mgmt/src/main.c
+++ tests/net/ethernet_mgmt/src/main.c
@@ -1,14 +1,19 @@
 DECL|auto_negotiation|member|bool auto_negotiation;
+DECL|delta_bandwidth|member|int delta_bandwidth;
 DECL|eth_fake_api_funcs|variable|eth_fake_api_funcs
 DECL|eth_fake_context|struct|struct eth_fake_context {
 DECL|eth_fake_data|variable|eth_fake_data
 DECL|eth_fake_get_capabilities|function|static enum ethernet_hw_caps eth_fake_get_capabilities(struct device *dev)
 DECL|eth_fake_get_config|function|static int eth_fake_get_config(struct device *dev, enum ethernet_config_type type, struct ethernet_config *config)
+DECL|eth_fake_get_total_bandwidth|function|static int eth_fake_get_total_bandwidth(struct eth_fake_context *ctx)
 DECL|eth_fake_iface_init|function|static void eth_fake_iface_init(struct net_if *iface)
 DECL|eth_fake_init|function|static int eth_fake_init(struct device *dev)
+DECL|eth_fake_recalc_qav_delta_bandwidth|function|static void eth_fake_recalc_qav_delta_bandwidth(struct eth_fake_context *ctx)
+DECL|eth_fake_recalc_qav_idle_slopes|function|static void eth_fake_recalc_qav_idle_slopes(struct eth_fake_context *ctx)
 DECL|eth_fake_send|function|static int eth_fake_send(struct net_if *iface, struct net_pkt *pkt)
 DECL|eth_fake_set_config|function|static int eth_fake_set_config(struct device *dev, enum ethernet_config_type type, const struct ethernet_config *config)
 DECL|full_duplex|member|bool full_duplex;
+DECL|idle_slope|member|int idle_slope;
 DECL|iface|member|struct net_if *iface;
 DECL|link_100bt|member|bool link_100bt;
 DECL|link_10bt|member|bool link_10bt;
