2c98477c27d80a1e68cc55775857eac0820f8163
Michael Scott <mike@foundries.io>
2018-08-01 12:03:00 -0800

    drivers: modem: add modem shell extension
    Add a set of modem shell commands to support modem development.
    
    Start with:
    modem list: Lists all registered modems and related information
    modem send <modem receiver index> <command>: Send command to modem
    
    Signed-off-by: Michael Scott <mike@foundries.io>
    
    Former-commit-id: 8b3d4ab6b899b6848edfb1e186c1838ed3e11d5b

---
+++ drivers/modem/modem_shell.c
@@ -0,0 +1,4 @@
+DECL|MODEM_SHELL_MODULE|macro|MODEM_SHELL_MODULE
+DECL|modem_commands|variable|modem_commands
+DECL|modem_shell_cmd_list|function|int modem_shell_cmd_list(int argc, char *argv[])
+DECL|modem_shell_cmd_send|function|int modem_shell_cmd_send(int argc, char *argv[])
