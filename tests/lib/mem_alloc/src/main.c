d8f6c88e4bb81bd02d43127a80a9d431b728ca31
Praful Swarnakar <praful.swarnakar@intel.com>
2018-08-02 08:36:59 +0530

    tests: lib: mem_alloc: Add tests to validate dynamic allocation
    Add new test cases to validate dynamic memory allocation
    functions such as malloc, calloc, realloc using minimal libc
    and newlibc implementation of standard C library.
    
    Signed-off-by: Praful Swarnakar <praful.swarnakar@intel.com>
    
    Former-commit-id: 3a82aef5df31049aa8a75869a046fbde891d0688

---
+++ tests/lib/mem_alloc/src/main.c
@@ -0,0 +1,15 @@
+DECL|BUF_LEN|macro|BUF_LEN
+DECL|CALLOC_BUFLEN|macro|CALLOC_BUFLEN
+DECL|MAX_HEAP_SIZE|macro|MAX_HEAP_SIZE
+DECL|MAX_LEN|macro|MAX_LEN
+DECL|filled_buf|variable|filled_buf
+DECL|test_calloc|function|void test_calloc(void)
+DECL|test_free|function|void test_free(void)
+DECL|test_main|function|void test_main(void)
+DECL|test_malloc|function|void test_malloc(void)
+DECL|test_memalloc_all|function|void test_memalloc_all(void)
+DECL|test_memalloc_max|function|void test_memalloc_max(void)
+DECL|test_reallocarray|function|void test_reallocarray(void)
+DECL|test_reallocarray|function|void test_reallocarray(void)
+DECL|test_realloc|function|void test_realloc(void)
+DECL|zerobuf|variable|zerobuf
