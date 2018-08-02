abc103f15ada449ea757897085f7433b5928756d
Spoorthi K <spoorthi.k@intel.com>
2018-08-02 22:01:23 +0530

    tests: obj_tracing: Enhance object tracing test
    Enhance object tracing test to improve code coverage
    of kernel object initialization.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: db9f6a9094ce3d70fd62191ebfb68cf9da853e6a

--- tests/kernel/obj_tracing/src/trace_obj.c
+++ tests/kernel/obj_tracing/src/trace_obj.c
@@ -1,20 +1,35 @@
 DECL|ALERT|enumerator|ALERT,
+DECL|BLOCK_SIZE|macro|BLOCK_SIZE
 DECL|MBOX|enumerator|MBOX,
 DECL|MEM_SLAB|enumerator|MEM_SLAB,
 DECL|MSGQ|enumerator|MSGQ,
 DECL|MUTEX|enumerator|MUTEX,
+DECL|NUM_BLOCKS|macro|NUM_BLOCKS
 DECL|PIPE|enumerator|PIPE,
 DECL|QUEUE|enumerator|QUEUE
 DECL|SEM|enumerator|SEM,
 DECL|STACK|enumerator|STACK,
 DECL|TIMER|enumerator|TIMER,
 DECL|alert_handler_dummy|function|static inline void alert_handler_dummy(struct k_alert *alert)
+DECL|alert|variable|alert
+DECL|buffer|variable|buffer
+DECL|data|variable|data
 DECL|expiry_dummy_fn|function|static inline void expiry_dummy_fn(struct k_timer *timer)
 DECL|get_obj_count|function|static void get_obj_count(int obj_type)
 DECL|kmbox|variable|kmbox
 DECL|kmutex|variable|kmutex
 DECL|kqueue|variable|kqueue
+DECL|mbox|variable|mbox
+DECL|msgq|variable|msgq
+DECL|mslab|variable|mslab
 DECL|mutex|variable|mutex
 DECL|obj_name|enum|enum obj_name {
+DECL|pipe|variable|pipe
+DECL|queue|variable|queue
+DECL|sdata|variable|sdata
+DECL|sema|variable|sema
+DECL|slab|variable|slab
+DECL|stack|variable|stack
 DECL|stop_dummy_fn|function|static inline void stop_dummy_fn(struct k_timer *timer)
 DECL|test_obj_tracing|function|void test_obj_tracing(void)
+DECL|timer|variable|timer
