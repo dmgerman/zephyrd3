56509ee8e389116bc5c5ffca5bcab02ac25c7af5
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-08-06 09:45:40 +0530

    tests/cmsis_rtos_v1: Introduce tests to make use of Signal APIs
    Some test programs to illustrate the usage of Signal Event APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: f7779895fd37fea203e8aec4487f48abfbd2a9cb

---
+++ tests/cmsis_rtos_v1/src/signal.c
@@ -0,0 +1,9 @@
+DECL|SIGNAL1|macro|SIGNAL1
+DECL|SIGNAL2|macro|SIGNAL2
+DECL|SIGNAL|macro|SIGNAL
+DECL|TIMEOUT|macro|TIMEOUT
+DECL|Thread_1|function|void Thread_1(void const *arg)
+DECL|Thread_2|function|void Thread_2(void const *arg)
+DECL|test_signal_events_no_wait|function|void test_signal_events_no_wait(void)
+DECL|test_signal_events_signalled|function|void test_signal_events_signalled(void)
+DECL|test_signal_events_timeout|function|void test_signal_events_timeout(void)
