0c0f4faa896776b8f33def2e8e997ecdbe604475
Robert Lubos <robert.lubos@nordicsemi.no>
2018-08-06 14:31:14 +0200

    net: tls: Handle HELLO_VERIFY_REQUIRED during DTLS handshake
    DTLS handshake can return MBEDTLS_ERR_SSL_HELLO_VERIFY_REQUIRED, which
    indicate that TLS session context should be reset.
    
    Also, store information whether TLS connection has beed established.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
    
    Former-commit-id: 6c5aaa070c0f864c267e6d7308143f4da2afedf8

--- subsys/net/lib/sockets/sockets_tls.c
+++ subsys/net/lib/sockets/sockets_tls.c
@@ -41,9 +41,11 @@ DECL|tls_ctr_drbg|variable|tls_ctr_drbg
 DECL|tls_debug|function|static void tls_debug(void *ctx, int level, const char *file, int line, const char *str)
 DECL|tls_entropy_func|function|static int tls_entropy_func(void *ctx, unsigned char *buf, size_t len)
 DECL|tls_entropy_func|function|static int tls_entropy_func(void *ctx, unsigned char *buf, size_t len)
+DECL|tls_established|member|bool tls_established;
 DECL|tls_init|function|static int tls_init(struct device *unused)
 DECL|tls_mbedtls_handshake|function|static int tls_mbedtls_handshake(struct net_context *context)
 DECL|tls_mbedtls_init|function|static int tls_mbedtls_init(struct net_context *context, bool is_server)
+DECL|tls_mbedtls_reset|function|static int tls_mbedtls_reset(struct net_context *context)
 DECL|tls_mbedtls_set_credentials|function|static int tls_mbedtls_set_credentials(struct tls_context *tls)
 DECL|tls_opt_ciphersuite_list_get|function|static int tls_opt_ciphersuite_list_get(struct net_context *context,void *optval, socklen_t *optlen)
 DECL|tls_opt_ciphersuite_list_set|function|static int tls_opt_ciphersuite_list_set(struct net_context *context,const void *optval, socklen_t optlen)
