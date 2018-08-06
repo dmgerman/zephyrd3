89e1ead1d167c2240911fe5769e3cc048e14d477
Maureen Helm <maureen.helm@nxp.com>
2018-08-06 15:53:10 -0500

    adc: Convert mcux adc16 driver to the new adc api
    Major rework of the mcux adc16 driver to convert it to the new adc api.
    Currently supports a subset of the api features including synchronous
    and asynchronous reads, and consecutive reads triggered by the kernel
    timer.
    
    Does not yet support some of the channel configuration options such as
    gain and reference voltage because the hardware only allows these
    options to be configured by peripheral instance rather than by channel.
    The values are currently hardcoded in the driver, but in the future we
    could introduce some flexibility per instance via device tree
    attributes.
    
    Does not yet support consecutive reads triggered by a hardware timer.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: c0ce7d02d5ba0db46ebcfeea3af34dbca8ea7181

--- drivers/adc/adc_mcux_adc16.c
+++ drivers/adc/adc_mcux_adc16.c
@@ -1,6 +1,15 @@
+DECL|ADC_CONTEXT_USES_KERNEL_TIMER|macro|ADC_CONTEXT_USES_KERNEL_TIMER
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|adc_context_start_sampling|function|static void adc_context_start_sampling(struct adc_context *ctx)
+DECL|adc_context_update_buffer_pointer|function|static void adc_context_update_buffer_pointer(struct adc_context *ctx, bool repeat_sampling)
 DECL|base|member|ADC_Type *base;
-DECL|channel_group|member|u32_t channel_group;
+DECL|buffer|member|u16_t *buffer;
+DECL|channel_id|member|u8_t channel_id;
+DECL|channels|member|u32_t channels;
+DECL|ctx|member|struct adc_context ctx;
+DECL|dev|member|struct device *dev;
 DECL|irq_config_func|member|void (*irq_config_func)(struct device *dev);
+DECL|mcux_adc16_channel_setup|function|static int mcux_adc16_channel_setup(struct device *dev, const struct adc_channel_cfg *channel_cfg)
 DECL|mcux_adc16_config_0|variable|mcux_adc16_config_0
 DECL|mcux_adc16_config_1|variable|mcux_adc16_config_1
 DECL|mcux_adc16_config_func_0|function|static void mcux_adc16_config_func_0(struct device *dev)
@@ -9,11 +18,11 @@ DECL|mcux_adc16_config|struct|struct mcux_adc16_config {
 DECL|mcux_adc16_data_0|variable|mcux_adc16_data_0
 DECL|mcux_adc16_data_1|variable|mcux_adc16_data_1
 DECL|mcux_adc16_data|struct|struct mcux_adc16_data {
-DECL|mcux_adc16_disable|function|static void mcux_adc16_disable(struct device *dev)
 DECL|mcux_adc16_driver_api|variable|mcux_adc16_driver_api
-DECL|mcux_adc16_enable|function|static void mcux_adc16_enable(struct device *dev)
 DECL|mcux_adc16_init|function|static int mcux_adc16_init(struct device *dev)
 DECL|mcux_adc16_isr|function|static void mcux_adc16_isr(void *arg)
-DECL|mcux_adc16_read|function|static int mcux_adc16_read(struct device *dev, struct adc_seq_table *seq_table)
-DECL|result|member|u32_t result;
-DECL|sync|member|struct k_sem sync;
+DECL|mcux_adc16_read_async|function|static int mcux_adc16_read_async(struct device *dev, const struct adc_sequence *sequence, struct k_poll_signal *async)
+DECL|mcux_adc16_read|function|static int mcux_adc16_read(struct device *dev, const struct adc_sequence *sequence)
+DECL|mcux_adc16_start_channel|function|static void mcux_adc16_start_channel(struct device *dev)
+DECL|repeat_buffer|member|u16_t *repeat_buffer;
+DECL|start_read|function|static int start_read(struct device *dev, const struct adc_sequence *sequence)
