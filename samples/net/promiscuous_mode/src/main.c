99d3c39e507f74711aa22cef5f34a69fc863f426
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-08-06 15:22:15 +0300

    samples: net: promiscuous: Convert to new logging system
    Use new logging system with support for network log level.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 1bcf1cf4756afebfdbba0f55a3d050052b90cc96

--- samples/net/promiscuous_mode/src/main.c
+++ samples/net/promiscuous_mode/src/main.c
@@ -1,7 +1,5 @@
 DECL|LOG_MODULE_NAME|macro|LOG_MODULE_NAME
-DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
-DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
-DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|NET_LOG_LEVEL|macro|NET_LOG_LEVEL
 DECL|get_ports|function|static int get_ports(struct net_pkt *pkt, u16_t *src, u16_t *dst)
 DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_data)
 DECL|main|function|void main(void)
