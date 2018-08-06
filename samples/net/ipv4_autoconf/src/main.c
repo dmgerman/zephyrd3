a4b1cf0c9640aaa5ef55e2b80fc560a7b23a3cfc
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-08-06 14:50:50 +0300

    samples: net: ipv4_autoconf: Convert to new logging system
    Use new logging system with support for network log level.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: 31a6d93c419337d3f26ff5f0bc8ddc8d52c50bf5

--- samples/net/ipv4_autoconf/src/main.c
+++ samples/net/ipv4_autoconf/src/main.c
@@ -1,6 +1,5 @@
-DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
-DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
-DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|LOG_MODULE_NAME|macro|LOG_MODULE_NAME
+DECL|NET_LOG_LEVEL|macro|NET_LOG_LEVEL
 DECL|handler|function|static void handler(struct net_mgmt_event_callback *cb, u32_t mgmt_event, struct net_if *iface)
 DECL|main|function|void main(void)
 DECL|mgmt_cb|variable|mgmt_cb
