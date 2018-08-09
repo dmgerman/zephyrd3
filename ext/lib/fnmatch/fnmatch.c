b6850eb5acfcc1a1f5c45e5d54a207f26e1e186b
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-08-09 08:25:07 +0200

    ext: lib: Add fnmatch library
    Library will be used by new shell implementation.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
    
    Former-commit-id: 486b731b40717a16f619b07e5d58aaf7d5143e78

---
+++ ext/lib/fnmatch/fnmatch.c
@@ -0,0 +1,6 @@
+DECL|EOS|macro|EOS
+DECL|FOLDCASE|macro|FOLDCASE
+DECL|fnmatchx|function|static int fnmatchx(const char *pattern, const char *string, int flags, size_t recursion)
+DECL|fnmatch|function|int fnmatch(const char *pattern, const char *string, int flags)
+DECL|foldcase|function|static inline int foldcase(int ch, int flags)
+DECL|rangematch|function|static const char * rangematch(const char *pattern, int test, int flags)
