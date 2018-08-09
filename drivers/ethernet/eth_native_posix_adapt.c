a30e30b60c2182397fdb6e3269fbe1e1f9db9ad9
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-08-09 16:10:55 +0300

    drivers: eth: native_posix: Allow user to manipulate zeth status
    User can take the zeth interface down by issuing "net iface down <idx>"
    shell command. It is possible to take the interface up by typing
    "net iface up <idx>" in shell. These commands are important for
    native_posix as there is no physical cable that can be connected
    or disconnected.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: f9aa9783d0013049726006234e1cc9eafbb7dd1e

--- drivers/ethernet/eth_native_posix_adapt.c
+++ drivers/ethernet/eth_native_posix_adapt.c
@@ -2,6 +2,8 @@ DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|_DEFAULT_SOURCE|macro|_DEFAULT_SOURCE
 DECL|eth_clock_gettime|function|int eth_clock_gettime(struct net_ptp_time *time)
+DECL|eth_if_down|function|int eth_if_down(const char *if_name)
+DECL|eth_if_up|function|int eth_if_up(const char *if_name)
 DECL|eth_iface_create|function|int eth_iface_create(const char *if_name, bool tun_only)
 DECL|eth_iface_remove|function|int eth_iface_remove(int fd)
 DECL|eth_promisc_mode|function|int eth_promisc_mode(const char *if_name, bool enable)
