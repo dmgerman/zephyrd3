35128bd4c9cbd2880f27cf1d38d8ae53e9edbaf4
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-08-09 10:57:07 +0200

    shell: Add built-in shell commands
    Added optional shell commands:
    - clear - for clearing terminal
    - history - commands history
    - resize - terminal resize
    - shell - controling echo and colors
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    Signed-off-by: Jakub Rzeszutko <jakub.rzeszutko@nordicsemi.no>
    
    Former-commit-id: 989fef9c0e178d605dafcc7a426fdfdc61099d7d

---
+++ subsys/shell/shell_cmds.c
@@ -0,0 +1,46 @@
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(m_sub_backspace_mode)
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(m_sub_colors)
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(m_sub_echo)
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(m_sub_resize)
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(m_sub_shell)
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(m_sub_shell_stats)
+DECL|SHELL_CURSOR_POSITION_BUFFER|macro|SHELL_CURSOR_POSITION_BUFFER
+DECL|SHELL_HELP_BACKSPACE_MODE_BACKSPACE|macro|SHELL_HELP_BACKSPACE_MODE_BACKSPACE
+DECL|SHELL_HELP_BACKSPACE_MODE_DELETE|macro|SHELL_HELP_BACKSPACE_MODE_DELETE
+DECL|SHELL_HELP_BACKSPACE_MODE|macro|SHELL_HELP_BACKSPACE_MODE
+DECL|SHELL_HELP_CLEAR|macro|SHELL_HELP_CLEAR
+DECL|SHELL_HELP_COLORS_OFF|macro|SHELL_HELP_COLORS_OFF
+DECL|SHELL_HELP_COLORS_ON|macro|SHELL_HELP_COLORS_ON
+DECL|SHELL_HELP_COLORS|macro|SHELL_HELP_COLORS
+DECL|SHELL_HELP_ECHO_OFF|macro|SHELL_HELP_ECHO_OFF
+DECL|SHELL_HELP_ECHO_ON|macro|SHELL_HELP_ECHO_ON
+DECL|SHELL_HELP_ECHO|macro|SHELL_HELP_ECHO
+DECL|SHELL_HELP_HISTORY|macro|SHELL_HELP_HISTORY
+DECL|SHELL_HELP_RESIZE_DEFAULT|macro|SHELL_HELP_RESIZE_DEFAULT
+DECL|SHELL_HELP_RESIZE|macro|SHELL_HELP_RESIZE
+DECL|SHELL_HELP_SHELL|macro|SHELL_HELP_SHELL
+DECL|SHELL_HELP_STATISTICS_RESET|macro|SHELL_HELP_STATISTICS_RESET
+DECL|SHELL_HELP_STATISTICS_SHOW|macro|SHELL_HELP_STATISTICS_SHOW
+DECL|SHELL_HELP_STATISTICS|macro|SHELL_HELP_STATISTICS
+DECL|SHELL_MAX_TERMINAL_SIZE|macro|SHELL_MAX_TERMINAL_SIZE
+DECL|SHELL_MSG_UNKNOWN_PARAMETER|macro|SHELL_MSG_UNKNOWN_PARAMETER
+DECL|cmd_bacskpace_mode_backspace|function|static void cmd_bacskpace_mode_backspace(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_bacskpace_mode_delete|function|static void cmd_bacskpace_mode_delete(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_bacskpace_mode|function|static void cmd_bacskpace_mode(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_clear|function|static void cmd_clear(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_colors_off|function|static void cmd_colors_off(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_colors_on|function|static void cmd_colors_on(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_colors|function|static void cmd_colors(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_echo_off|function|static void cmd_echo_off(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_echo_on|function|static void cmd_echo_on(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_echo|function|static void cmd_echo(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_help|function|static void cmd_help(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_history|function|static void cmd_history(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_resize_default|function|static void cmd_resize_default(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_resize|function|static void cmd_resize(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_shell_stats_reset|function|static void cmd_shell_stats_reset(const struct shell *shell,size_t argc, char **argv)
+DECL|cmd_shell_stats_show|function|static void cmd_shell_stats_show(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_shell_stats|function|static void cmd_shell_stats(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_shell|function|static void cmd_shell(const struct shell *shell, size_t argc, char **argv)
+DECL|cursor_position_get|function|static int cursor_position_get(const struct shell *shell, u16_t *x, u16_t *y)
+DECL|terminal_size_get|function|static int terminal_size_get(const struct shell *shell)
