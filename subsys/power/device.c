f5d0be9ed9e1cdb47bed3041798b030f72f25e6f
Kumar Gala <kumar.gala@linaro.org>
2018-08-09 08:48:18 -0500

    log: make name param explicit
    Rather than having some implied name for the logging name, explicitly
    pass it in the macros LOG_MODULE_REGISTER & LOG_MODULE_DECLARE.
    
    Signed-off-by: Kumar Gala <kumar.gala@linaro.org>
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    
    Former-commit-id: 4fede8dd0b9fb0ec52a7b5deea62f7e65421f5b8

--- subsys/power/device.c
+++ subsys/power/device.c
@@ -1,5 +1,4 @@
 DECL|LOG_LEVEL|macro|LOG_LEVEL
-DECL|LOG_MODULE_NAME|macro|LOG_MODULE_NAME
 DECL|MAX_DEV_NAME_LEN|macro|MAX_DEV_NAME_LEN
 DECL|MAX_PM_DEVICES|macro|MAX_PM_DEVICES
 DECL|NUM_CORE_DEVICES|macro|NUM_CORE_DEVICES
@@ -8,6 +7,7 @@ DECL|device_count|variable|device_count
 DECL|device_ordered_list|variable|device_ordered_list
 DECL|device_retval|variable|device_retval
 DECL|pm_device_list|variable|pm_device_list
+DECL|power|variable|power
 DECL|sys_pm_create_device_list|function|void sys_pm_create_device_list(void)
 DECL|sys_pm_resume_devices|function|void sys_pm_resume_devices(void)
 DECL|sys_pm_suspend_devices|function|int sys_pm_suspend_devices(void)
