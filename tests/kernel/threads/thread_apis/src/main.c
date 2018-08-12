6cee511b4233b167673fc0a909a519288f953af3
Anas Nashif <anas.nashif@intel.com>
2018-08-12 15:58:43 -0500

    tests: test k_thread_name_set
    Basic test for new API: k_thread_name_set.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: f6e7e98909786d38bf3b320fc5da8fe158bc6c96

--- tests/kernel/threads/thread_apis/src/main.c
+++ tests/kernel/threads/thread_apis/src/main.c
@@ -2,12 +2,15 @@ DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|customdata_entry|function|static void customdata_entry(void *p1, void *p2, void *p3)
 DECL|main_prio|variable|main_prio
 DECL|tdata_custom|variable|tdata_custom
+DECL|tdata_name|variable|tdata_name
 DECL|tdata|variable|tdata
 DECL|test_customdata_get_set_coop|function|void test_customdata_get_set_coop(void)
 DECL|test_customdata_get_set_preempt|function|void test_customdata_get_set_preempt(void)
 DECL|test_main|function|void test_main(void)
 DECL|test_systhreads_idle|function|void test_systhreads_idle(void)
 DECL|test_systhreads_main|function|void test_systhreads_main(void)
+DECL|test_thread_name_get_set|function|void test_thread_name_get_set(void)
 DECL|test_user_mode|function|void test_user_mode(void)
 DECL|test_user_mode|function|void test_user_mode(void)
+DECL|thread_name_entry|function|static void thread_name_entry(void)
 DECL|umode_entry|function|static void umode_entry(void *thread_id, void *p2, void *p3)
