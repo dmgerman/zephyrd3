e32e5a0469511817f084abc060f9d4838d5fce82
Praful Swarnakar <praful.swarnakar@intel.com>
2018-08-15 01:38:01 +0530

    lib: cmsis_rtos_v1: Fix timeout calculation in signals
    Fix the osSignalWait timeout calculation in cases when
    waiting on more than one signal event.
    
    Signed-off-by: Praful Swarnakar <praful.swarnakar@intel.com>
    
    Former-commit-id: 2fc836d2fe54d67e0a9a58208c9d10d572649bda

--- lib/cmsis_rtos_v1/cmsis_signal.c
+++ lib/cmsis_rtos_v1/cmsis_signal.c
@@ -1,3 +1,4 @@
+DECL|NSEC_PER_MSEC|macro|NSEC_PER_MSEC
 DECL|k_thread_other_custom_data_get|function|void *k_thread_other_custom_data_get(struct k_thread *thread_id)
 DECL|osSignalClear|function|int32_t osSignalClear(osThreadId thread_id, int32_t signals)
 DECL|osSignalSet|function|int32_t osSignalSet(osThreadId thread_id, int32_t signals)
