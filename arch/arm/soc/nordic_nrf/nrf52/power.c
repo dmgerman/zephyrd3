38e8b3478041d9c89b1267941fe18feb5271966e
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-08-14 13:08:43 +0530

    arch: arm: nordic_nrf: Add an API to check for valid PM state
    Add an API _sys_soc_is_valid_power_state to check if a PM state
    is valid or not.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: 1301cc636b9dfb8594d7c4ead3e717d2f335e9aa

--- arch/arm/soc/nordic_nrf/nrf52/power.c
+++ arch/arm/soc/nordic_nrf/nrf52/power.c
@@ -1,6 +1,7 @@
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|_issue_low_power_command|function|static void _issue_low_power_command(void)
 DECL|_low_power_mode|function|static void _low_power_mode(enum power_states state)
+DECL|_sys_soc_is_valid_power_state|function|bool _sys_soc_is_valid_power_state(enum power_states state)
 DECL|_sys_soc_power_state_post_ops|function|void _sys_soc_power_state_post_ops(enum power_states state)
 DECL|_sys_soc_set_power_state|function|void _sys_soc_set_power_state(enum power_states state)
 DECL|_system_off|function|static void _system_off(void)
