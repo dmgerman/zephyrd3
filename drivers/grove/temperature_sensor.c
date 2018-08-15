fee023d1bb076423d7c76e64c1cc9e20236e454f
Punit Vara <punit.vara@intel.com>
2018-08-15 09:49:50 +0530

    drivers: grove: Update temperature driver
    Replace old ADC driver APIs with new ones.
    
    Signed-off-by: Punit Vara <punit.vara@intel.com>
    
    Former-commit-id: 003f0176a085c8414a160bbe6588701bdbf284fb

--- drivers/grove/temperature_sensor.c
+++ drivers/grove/temperature_sensor.c
@@ -1,14 +1,16 @@
+DECL|ADC_RESOLUTION|macro|ADC_RESOLUTION
 DECL|B_CONST|macro|B_CONST
 DECL|B_CONST|macro|B_CONST
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|adc_buffer|member|u8_t adc_buffer[4];
-DECL|adc_table|member|struct adc_seq_table adc_table;
+DECL|adc_table|variable|adc_table
 DECL|adc|member|struct device *adc;
+DECL|ch10_cfg|member|struct adc_channel_cfg ch10_cfg;
 DECL|gts_api|variable|gts_api
 DECL|gts_channel_get|function|static int gts_channel_get(struct device *dev, enum sensor_channel chan, struct sensor_value *val)
 DECL|gts_data|struct|struct gts_data {
 DECL|gts_data|variable|gts_data
 DECL|gts_init|function|static int gts_init(struct device *dev)
 DECL|gts_sample_fetch|function|static int gts_sample_fetch(struct device *dev, enum sensor_channel chan)
-DECL|sample|member|struct adc_seq_entry sample;
+DECL|options|variable|options
