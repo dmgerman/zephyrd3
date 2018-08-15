575569d4f9004b14f1b7dd925c9c2a65c91e0301
Justin Watson <jwatson5@gmail.com>
2018-08-15 15:01:57 -0700

    sam: adc: Updated SAM ADC driver.
    Updated to work with new ADC API.
    
    Signed-off-by: Justin Watson <jwatson5@gmail.com>
    
    Former-commit-id: 4e551aabb6c315a3094aabee8ed4d74e4fd238d0

--- drivers/adc/adc_sam_afec.c
+++ drivers/adc/adc_sam_afec.c
@@ -1,36 +1,37 @@
-DECL|ADC_CHANNELS|macro|ADC_CHANNELS
+DECL|ADC_CONTEXT_USES_KERNEL_TIMER|macro|ADC_CONTEXT_USES_KERNEL_TIMER
 DECL|CONF_ADC_PRESCALER|macro|CONF_ADC_PRESCALER
 DECL|DEV_CFG|macro|DEV_CFG
 DECL|DEV_DATA|macro|DEV_DATA
-DECL|DEV_NAME|macro|DEV_NAME
+DECL|NUM_CHANNELS|macro|NUM_CHANNELS
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
-DECL|active_chan_last|member|u16_t active_chan_last;
-DECL|active_channels|member|u16_t active_channels;
-DECL|adc0_irq_config|function|static void adc0_irq_config(void)
-DECL|adc0_sam_config|variable|adc0_sam_config
+DECL|adc0_sam_cfg_func|function|static void adc0_sam_cfg_func(struct device *dev)
+DECL|adc0_sam_cfg|variable|adc0_sam_cfg
 DECL|adc0_sam_data|variable|adc0_sam_data
-DECL|adc1_irq_config|function|static void adc1_irq_config(void)
-DECL|adc1_sam_config|variable|adc1_sam_config
+DECL|adc1_sam_cfg_func|function|static void adc1_sam_cfg_func(struct device *dev)
+DECL|adc1_sam_cfg|variable|adc1_sam_cfg
 DECL|adc1_sam_data|variable|adc1_sam_data
-DECL|adc_sam_configure|function|static void adc_sam_configure(struct device *dev)
-DECL|adc_sam_dev_cfg|struct|struct adc_sam_dev_cfg {
-DECL|adc_sam_dev_data|struct|struct adc_sam_dev_data {
-DECL|adc_sam_driver_api|variable|adc_sam_driver_api
-DECL|adc_sam_initialize|function|static int adc_sam_initialize(struct device *dev)
+DECL|adc_context_start_sampling|function|static void adc_context_start_sampling(struct adc_context *ctx)
+DECL|adc_context_update_buffer_pointer|function|static void adc_context_update_buffer_pointer(struct adc_context *ctx, bool repeat_sampling)
+DECL|adc_sam_api|variable|adc_sam_api
+DECL|adc_sam_cfg|struct|struct adc_sam_cfg {
+DECL|adc_sam_channel_setup|function|static int adc_sam_channel_setup(struct device *dev, const struct adc_channel_cfg *channel_cfg)
+DECL|adc_sam_data|struct|struct adc_sam_data {
+DECL|adc_sam_init|function|static int adc_sam_init(struct device *dev)
 DECL|adc_sam_isr|function|static void adc_sam_isr(void *arg)
-DECL|adc_sam_read|function|static int adc_sam_read(struct device *dev, struct adc_seq_table *seq_tbl)
+DECL|adc_sam_read_async|function|static int adc_sam_read_async(struct device *dev, const struct adc_sequence *sequence, struct k_poll_signal *async)
+DECL|adc_sam_read|function|static int adc_sam_read(struct device *dev,const struct adc_sequence *sequence)
+DECL|adc_sam_start_conversion|function|static void adc_sam_start_conversion(struct device *dev)
+DECL|afec_trg_pin|member|struct soc_gpio_pin afec_trg_pin;
 DECL|buffer|member|u16_t *buffer;
-DECL|channel_samples|struct|struct channel_samples {
-DECL|irq_config|member|void (*irq_config)(void);
-DECL|irq_id|member|u8_t irq_id;
-DECL|length|member|u32_t length;
-DECL|measured_channels|member|u16_t measured_channels;
-DECL|mutex_thread|member|struct k_sem mutex_thread;
-DECL|periph_id|member|u8_t periph_id;
-DECL|pin_adc0|variable|pin_adc0
-DECL|pin_adc1|variable|pin_adc1
-DECL|pin_trigger|member|const struct soc_gpio_pin *pin_trigger;
+DECL|cfg_func_t|typedef|typedef void (*cfg_func_t)(struct device *dev);
+DECL|cfg_func|member|cfg_func_t cfg_func;
+DECL|channel_id|member|u8_t channel_id;
+DECL|channels|member|u32_t channels;
+DECL|check_buffer_size|function|static int check_buffer_size(const struct adc_sequence *sequence, u8_t active_channels)
+DECL|ctx|member|struct adc_context ctx;
+DECL|dev|member|struct device *dev;
+DECL|periph_id|member|u32_t periph_id;
 DECL|regs|member|Afec *regs;
-DECL|samples|member|struct channel_samples samples[ADC_CHANNELS];
-DECL|sem_meas|member|struct k_sem sem_meas;
+DECL|repeat_buffer|member|u16_t *repeat_buffer;
+DECL|start_read|function|static int start_read(struct device *dev, const struct adc_sequence *sequence)
