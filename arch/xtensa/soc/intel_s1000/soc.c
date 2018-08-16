d8c15cf2d73d61767f3378ac07ac3c4005673886
Sathish Kuttan <sathish.k.kuttan@intel.com>
2018-08-16 15:06:36 -0700

    xtensa: intel_s1000: Add SoC level SYS_INIT
    Added a SYS_INIT for SoC level initialization of Intel S1000
    Added routines for setting up resource ownership for
        DMA, I2S
    Added routine to setup power gating and clock configuration
    
    Signed-off-by: Sathish Kuttan <sathish.k.kuttan@intel.com>
    
    Former-commit-id: 824bcaca52f21417213b56b5d370a9bdf052333d

--- arch/xtensa/soc/intel_s1000/soc.c
+++ arch/xtensa/soc/intel_s1000/soc.c
@@ -1,9 +1,12 @@
+DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
+DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
 DECL|_soc_irq_disable|function|void _soc_irq_disable(u32_t irq)
 DECL|_soc_irq_enable|function|void _soc_irq_enable(u32_t irq)
 DECL|dcache_invalidate_region|function|void dcache_invalidate_region(void *addr, size_t size)
 DECL|dcache_writeback_region|function|void dcache_writeback_region(void *addr, size_t size)
-DECL|setup_ownership_dma0|function|void setup_ownership_dma0(void)
-DECL|setup_ownership_dma1|function|void setup_ownership_dma1(void)
-DECL|setup_ownership_dma2|function|void setup_ownership_dma2(void)
-DECL|setup_ownership_i2s|function|void setup_ownership_i2s(void)
+DECL|ref_clk_freq|variable|ref_clk_freq
 DECL|soc_get_ref_clk_freq|function|u32_t soc_get_ref_clk_freq(void)
+DECL|soc_init|function|static int soc_init(struct device *dev)
+DECL|soc_read_bootstraps|function|static void soc_read_bootstraps(void)
+DECL|soc_set_power_and_clock|function|static void soc_set_power_and_clock(void)
+DECL|soc_set_resource_ownership|function|static inline void soc_set_resource_ownership(void)
