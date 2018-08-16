b0caed499c51ada55c3addf539251adfbc76eb29
Spoorthi K <spoorthi.k@intel.com>
2018-08-16 10:38:27 +0530

    tests: cmsis_rtos_v1: Add and enhance test suite
    Add tests for kernel managament APIs and enhance memory
    pool tests with bulk allocation, free and negative cases.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: c9ca19949e6b254377048f975defbc90890dba34

---
+++ tests/cmsis_rtos_v1/src/kernel_apis.c
@@ -0,0 +1,3 @@
+DECL|WAIT_TIME_US|macro|WAIT_TIME_US
+DECL|test_kernel_start|function|void test_kernel_start(void)
+DECL|test_kernel_systick|function|void test_kernel_systick(void)
--- tests/cmsis_rtos_v1/src/mempool.c
+++ tests/cmsis_rtos_v1/src/mempool.c
@@ -1,3 +1,4 @@
+DECL|MAX_BLOCKS|macro|MAX_BLOCKS
 DECL|mem_block|struct|struct mem_block {
 DECL|member1|member|int member1;
 DECL|member2|member|int member2;
