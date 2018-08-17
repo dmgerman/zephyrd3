68d984decffc31f4783827f1ef4c2de9095ef47e
Michael Hennerich <michael.hennerich@analog.com>
2018-08-17 09:31:42 +0200

    samples: sensor: adxl372: Add ADXL372 sample application
    This sample application produces slightly different outputs based on
    the chosen driver configuration mode. In Measuring Mode with trigger
    support, the acceleration on all three axis is printed in m/s^2 at
    the sampling rate (ODR). In polled Measuring Mode the instantaneous
    acceleration is polled every 2 seconds. In most high-g applications,
    a single (3-axis) acceleration sample at the peak of an impact event
    contains sufficient information about the event, and the full
    acceleration history is not required.
    In this Max Peak Detect Mode the device returns only the over
    threshold peak acceleration between two consecutive sample fetches or
    trigger events. Instead of printing the acceleration on all three axis,
    the sample application calculates the vector magnitude
    (root sum squared) and displays the result in g rather than in m/s^2,
    together with an bar graph.
    
    Signed-off-by: Michael Hennerich <michael.hennerich@analog.com>
    
    Former-commit-id: 84c352d0f351d4fc02cf1d2aaac8fec8e6646188

---
+++ samples/sensor/adxl372/src/main.c
@@ -0,0 +1,4 @@
+DECL|main|function|void main(void)
+DECL|pow2|macro|pow2
+DECL|sqrt|function|static double sqrt(double value)
+DECL|trigger_handler|function|static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
