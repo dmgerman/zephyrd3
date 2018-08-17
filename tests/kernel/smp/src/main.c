77f8d23bb1ef0ca727897a2b30ddc46860316440
Spoorthi K <spoorthi.k@intel.com>
2018-08-17 20:11:27 +0530

    tests: smp: Modify test to verify thread delay
    Improved test with thread delay and removed few prints.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: 2a72f500cb14cd1bf35a4e708439757895772a03

--- tests/kernel/smp/src/main.c
+++ tests/kernel/smp/src/main.c
@@ -3,6 +3,7 @@ DECL|EQUAL_PRIORITY|macro|EQUAL_PRIORITY
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|T2_STACK_SIZE|macro|T2_STACK_SIZE
 DECL|THREADS_NUM|macro|THREADS_NUM
+DECL|THREAD_DELAY|macro|THREAD_DELAY
 DECL|TIMEOUT|macro|TIMEOUT
 DECL|TIME_SLICE_MS|macro|TIME_SLICE_MS
 DECL|abort_threads|function|static void abort_threads(int num)
@@ -13,7 +14,7 @@ DECL|cpu_id|member|int cpu_id;
 DECL|executed|member|int executed;
 DECL|pending|variable|pending
 DECL|priority|member|int priority;
-DECL|spawn_threads|function|static void spawn_threads(int prio, int thread_num, int equal_prio, k_thread_entry_t thread_entry)
+DECL|spawn_threads|function|static void spawn_threads(int prio, int thread_num, int equal_prio, k_thread_entry_t thread_entry, int delay)
 DECL|sync_count|variable|sync_count
 DECL|t2_count|variable|t2_count
 DECL|t2_fn|function|static void t2_fn(void *a, void *b, void *c)
