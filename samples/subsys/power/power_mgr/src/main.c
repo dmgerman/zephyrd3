d9c82d642ed8d88f061a1f76b9731cff457e1baa
Ramakrishna Pallala <ramakrishna.pallala@intel.com>
2018-08-17 20:14:56 +0530

    samples: subsys: Add sample app to demo OS managed PM
    Add a sample application to demonstrate OS managed
    Power Management framework.
    
    Signed-off-by: Ramakrishna Pallala <ramakrishna.pallala@intel.com>
    
    Former-commit-id: b69d2861801be9dd885265a12432f6f48b52b08c

---
+++ samples/subsys/power/power_mgr/src/main.c
@@ -0,0 +1,8 @@
+DECL|BUSY_WAIT_DELAY_US|macro|BUSY_WAIT_DELAY_US
+DECL|DEMO_DESCRIPTION|macro|DEMO_DESCRIPTION
+DECL|LPS1_STATE_ENTER_TO|macro|LPS1_STATE_ENTER_TO
+DECL|LPS_STATE_ENTER_TO|macro|LPS_STATE_ENTER_TO
+DECL|SECONDS_TO_SLEEP|macro|SECONDS_TO_SLEEP
+DECL|main|function|void main(void)
+DECL|sys_pm_notify_lps_entry|function|void sys_pm_notify_lps_entry(enum power_states state)
+DECL|sys_pm_notify_lps_exit|function|void sys_pm_notify_lps_exit(enum power_states state)
