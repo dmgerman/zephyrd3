aa53d72417b8cf853351ad8ee55eca63384d6ff0
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2018-08-20 11:46:09 +0300

    samples: lldp: Add simple parsing to sample
    Add simple parsing using LLDP RX API
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>
    
    Former-commit-id: e02dcf254568dfb8c17fe596b912f4c8b0e04321

--- samples/net/lldp/src/main.c
+++ samples/net/lldp/src/main.c
@@ -6,7 +6,9 @@ DECL|iface_cb|function|static void iface_cb(struct net_if *iface, void *user_dat
 DECL|init_app|function|static int init_app(void)
 DECL|init_vlan|function|static int init_vlan(void)
 DECL|main|function|void main(void)
+DECL|parse_lldp|function|static enum net_verdict parse_lldp(struct net_if *iface, struct net_pkt *pkt)
 DECL|second|member|struct net_if *second;
 DECL|setup_iface|function|static int setup_iface(struct net_if *iface, const char *ipv6_addr, const char *ipv4_addr, u16_t vlan_tag)
 DECL|third|member|struct net_if *third;
 DECL|ud|struct|struct ud {
+DECL|ud|variable|ud
