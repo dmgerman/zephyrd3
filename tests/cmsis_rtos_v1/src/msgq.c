e529c1b520fc9f3a4d9fb27bc27c43a71e089df3
Niranjhana N <niranjhana.n@intel.com>
2018-08-20 11:56:06 +0530

    tests: cmsis_rtos_v1: add and enhance msgq tests
    Added few more tests to cover all the message
    send and receive scenarios. Added negative tests
    to increase code coverage.
    
    Signed-off-by: Niranjhana N <niranjhana.n@intel.com>
    
    Former-commit-id: 30d478a5fd7d8397ee8e837f2ae7c2a2a421b28a

--- tests/cmsis_rtos_v1/src/msgq.c
+++ tests/cmsis_rtos_v1/src/msgq.c
@@ -1,5 +1,7 @@
 DECL|MESSAGE1|macro|MESSAGE1
 DECL|MESSAGE2|macro|MESSAGE2
+DECL|Q_LEN|macro|Q_LEN
+DECL|TIMEOUT|macro|TIMEOUT
 DECL|message_id|variable|message_id
 DECL|message_recv|function|void message_recv(void)
 DECL|send_msg_thread|function|void send_msg_thread(void const *argument)
