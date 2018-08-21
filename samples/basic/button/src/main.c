3224b54087a4066b8ab82ff5f4121aa4007ea5ae
Emanuele Di Santo <emdi@nordicsemi.no>
2018-08-21 17:01:55 +0200

    samples: basic: button: use dts-generated defines
    This commit updates the button sample to use the dts-generated
    define to configure the button pull-up when the custom define
    is not present.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>
    
    Former-commit-id: 53cd7c249627c4bc76c15b7d05e330e880a4a977

--- samples/basic/button/src/main.c
+++ samples/basic/button/src/main.c
@@ -3,9 +3,10 @@ DECL|EDGE|macro|EDGE
 DECL|PIN|macro|PIN
 DECL|PORT|macro|PORT
 DECL|PULL_UP|macro|PULL_UP
-DECL|PULL_UP|macro|PULL_UP
 DECL|SLEEP_TIME|macro|SLEEP_TIME
 DECL|SW0_GPIO_CONTROLLER|macro|SW0_GPIO_CONTROLLER
+DECL|SW0_GPIO_FLAGS|macro|SW0_GPIO_FLAGS
+DECL|SW0_GPIO_FLAGS|macro|SW0_GPIO_FLAGS
 DECL|button_pressed|function|void button_pressed(struct device *gpiob, struct gpio_callback *cb, u32_t pins)
 DECL|gpio_cb|variable|gpio_cb
 DECL|main|function|void main(void)
