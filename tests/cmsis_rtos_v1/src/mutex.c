c13ee9397502a61147ee00a154287ab9d641e60a
Praful Swarnakar <praful.swarnakar@intel.com>
2018-08-23 07:45:24 +0530

    tests: cmsis_rtos_v1: Add more test scenarios in mutex
    Add additional test usecases in cmsis mutex to check
    maximum number of mutexes allowed at a time. Also, add
    couple of negative tests for invalid parameter check.
    
    Signed-off-by: Praful Swarnakar <praful.swarnakar@intel.com>
    
    Former-commit-id: 9dd63f7ddfa22a7e11be7a5dc88e4516966b4609

--- tests/cmsis_rtos_v1/src/mutex.c
+++ tests/cmsis_rtos_v1/src/mutex.c
@@ -1,6 +1,10 @@
 DECL|Mutex_1|variable|Mutex_1
 DECL|Mutex_2|variable|Mutex_2
+DECL|Mutex_multi|variable|Mutex_multi
 DECL|TIMEOUT|macro|TIMEOUT
+DECL|cleanup_max_mutex|function|void cleanup_max_mutex(osMutexId *mutex_ids)
+DECL|max_mtx_cnt|variable|max_mtx_cnt
 DECL|tThread_entry_lock_timeout|function|void tThread_entry_lock_timeout(void const *arg)
+DECL|test_max_mutex|function|void test_max_mutex(void)
 DECL|test_mutex_lock_timeout|function|void test_mutex_lock_timeout(void)
 DECL|test_mutex|function|void test_mutex(void)
