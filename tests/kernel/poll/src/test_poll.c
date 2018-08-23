e83e374d1992df1e89204d2edf3fcbdfebc672f9
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-08-23 22:11:39 +0300

    tests: k_poll: Add testcase to poll fifo which gets k_fifo_cancel_wait
    In this case k_poll() returns -EINTR, while still fills in event
    states.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: 14742d79feb2386fd98de4b7dc598d7c0ecd2cd0

--- tests/kernel/poll/src/test_poll.c
+++ tests/kernel/poll/src/test_poll.c
@@ -3,6 +3,7 @@ DECL|SIGNAL_RESULT|macro|SIGNAL_RESULT
 DECL|TAG_0|macro|TAG_0
 DECL|TAG_1|macro|TAG_1
 DECL|TAG_2|macro|TAG_2
+DECL|cancel_fifo|variable|cancel_fifo
 DECL|fifo_msg|struct|struct fifo_msg {
 DECL|msg|member|u32_t msg;
 DECL|multi_lowprio|function|static void multi_lowprio(void *p1, void *p2, void *p3)
@@ -12,11 +13,17 @@ DECL|multi|function|static void multi(void *p1, void *p2, void *p3)
 DECL|no_wait_fifo|variable|no_wait_fifo
 DECL|no_wait_sem|variable|no_wait_sem
 DECL|no_wait_signal|variable|no_wait_signal
+DECL|non_cancel_fifo|variable|non_cancel_fifo
+DECL|poll_cancel_helper_thread|variable|poll_cancel_helper_thread
+DECL|poll_cancel_helper|function|static void poll_cancel_helper(void *p1, void *p2, void *p3)
 DECL|poll_wait_helper_thread|variable|poll_wait_helper_thread
 DECL|poll_wait_helper|function|static void poll_wait_helper(void *use_fifo, void *p2, void *p3)
 DECL|private|member|void *private;
 DECL|signal_thread|variable|signal_thread
 DECL|signal|variable|signal
+DECL|test_poll_cancel_main_high_prio|function|void test_poll_cancel_main_high_prio(void)
+DECL|test_poll_cancel_main_low_prio|function|void test_poll_cancel_main_low_prio(void)
+DECL|test_poll_cancel|function|void test_poll_cancel(bool is_main_low_prio)
 DECL|test_poll_grant_access|function|void test_poll_grant_access(void)
 DECL|test_poll_multi|function|void test_poll_multi(void)
 DECL|test_poll_no_wait|function|void test_poll_no_wait(void)
