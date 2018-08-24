4ac9b83023abf56b3d81646b99b4e872d8978b57
Adithya Baglody <adithya.nagaraj.baglody@intel.com>
2018-08-24 13:08:51 +0530

    tests: kernel: sched: schedule_api: Increase stack size.
    The stack size was way too less. Increasing the size to minimum
    of 512 for all platforms.
    
    Signed-off-by: Adithya Baglody <adithya.nagaraj.baglody@intel.com>
    
    Former-commit-id: 871cc3232fe8629c0954fdf2ff488f0df6d323ee

--- tests/kernel/sched/schedule_api/src/test_sched.h
+++ tests/kernel/sched/schedule_api/src/test_sched.h
@@ -1,5 +1,4 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
-DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|__TEST_SCHED_H__|macro|__TEST_SCHED_H__
 DECL|executed|member|int executed;
 DECL|priority|member|int priority;
