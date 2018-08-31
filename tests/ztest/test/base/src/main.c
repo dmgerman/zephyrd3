333e71e2c2d90404a0fe37ccfa77407403205c01
Anas Nashif <anas.nashif@intel.com>
2018-08-31 12:17:06 -0500

    tests: rename test entry functions
    Be compatible with the rest and enable parsing of sub-cases.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: f2883ec169bdaa5ac2e82dbc5a767f3fa5a4904b

--- tests/ztest/test/base/src/main.c
+++ tests/ztest/test/base/src/main.c
@@ -1,3 +1,3 @@
-DECL|assert_tests|function|static void assert_tests(void)
-DECL|empty_test|function|static void empty_test(void)
+DECL|test_assert_tests|function|static void test_assert_tests(void)
+DECL|test_empty_test|function|static void test_empty_test(void)
 DECL|test_main|function|void test_main(void)
