ce848afa6c3c11f1143e25edd31ef826117af013
Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
2018-09-04 11:49:12 +0300

    samples: net: coap: Remove apps using raw mbedtls APIs
    This removes these network sample applications
      samples/net/coaps_client
      samples/net/coaps_server
    
    as they are using low level mbedtls APIs. You should use
    preferably socket based or net-app based applications.
    
    Signed-off-by: Ravi kumar Veeramally <ravikumar.veeramally@linux.intel.com>
    
    Former-commit-id: 42c19497b6360cc97bb6ee797bccb52e2a950c33

--- samples/net/coaps_server/src/udp.h
+++
@@ -1,7 +0,0 @@
-DECL|_UDP_H_|macro|_UDP_H_
-DECL|client_id|member|char client_id;
-DECL|net_ctx|member|struct net_context *net_ctx;
-DECL|remaining|member|int remaining;
-DECL|rx_pkt|member|struct net_pkt *rx_pkt;
-DECL|rx_sem|member|struct k_sem rx_sem;
-DECL|udp_context|struct|struct udp_context {
