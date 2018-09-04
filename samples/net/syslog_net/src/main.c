2b20281b7796304465315c3b3981aa4a3ca4ae68
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2018-09-04 13:07:14 +0300

    samples: net: syslog: Remove the sample application
    The application won't work anymore as it uses the old syslog
    logging and networking is now converted to use the new logger
    so there is no need for this sample application.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>
    
    Former-commit-id: d67414e96c983499e86d34fa0ca238d8472144a4

--- samples/net/syslog_net/src/main.c
+++
@@ -1,5 +0,0 @@
-DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
-DECL|NET_SYS_LOG_LEVEL|macro|NET_SYS_LOG_LEVEL
-DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
-DECL|init_app|function|static void init_app(void)
-DECL|main|function|void main(void)
