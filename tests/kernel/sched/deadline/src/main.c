9496c7c291bd33d08a81e112e7d04acba9ddffb2
Andy Ross <andrew.j.ross@intel.com>
2018-09-05 09:42:46 -0700

    tests: Add kernel/sched/deadline test for EDF validation
    Simple test for CONFIG_SCHED_DEADLINE.  It creates a bunch of threads
    at randome deadlines but within the same priority, and validates that
    they run in the correct order.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>
    
    Former-commit-id: 02aa9800424453f4442e279048c84b9e4266063e

---
+++ tests/kernel/sched/deadline/src/main.c
@@ -0,0 +1,9 @@
+DECL|NUM_THREADS|macro|NUM_THREADS
+DECL|STACK_SIZE|macro|STACK_SIZE
+DECL|exec_order|variable|exec_order
+DECL|n_exec|variable|n_exec
+DECL|test_deadline|function|void test_deadline(void)
+DECL|test_main|function|void test_main(void)
+DECL|thread_deadlines|variable|thread_deadlines
+DECL|worker_threads|variable|worker_threads
+DECL|worker|function|void worker(void *p1, void *p2, void *p3)
