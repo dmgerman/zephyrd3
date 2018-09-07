9c551c87f468ebe7d34b8e690c4622eabfae92dd
Praful Swarnakar <praful.swarnakar@intel.com>
2018-09-08 05:00:35 +0530

    tests: cmsis_rtos_v1: Add additional test scenarios
    Add additional test scenarios to validate signal event
    APIs such as setting signal from ISR, setting/clearing
    each unique flag and few negative test cases too.
    
    Signed-off-by: Praful Swarnakar <praful.swarnakar@intel.com>
    
    Former-commit-id: 50930300d9975171ab361b45e15c4c5ae6c1edf9

--- tests/cmsis_rtos_v1/src/signal.c
+++ tests/cmsis_rtos_v1/src/signal.c
@@ -1,9 +1,17 @@
+DECL|ISR_SIGNAL|macro|ISR_SIGNAL
 DECL|SIGNAL1|macro|SIGNAL1
 DECL|SIGNAL2|macro|SIGNAL2
+DECL|SIGNAL_ALL_FLAGS|macro|SIGNAL_ALL_FLAGS
+DECL|SIGNAL_FLAG|macro|SIGNAL_FLAG
+DECL|SIGNAL_OUTOFLIMIT_FLAG|macro|SIGNAL_OUTOFLIMIT_FLAG
 DECL|SIGNAL|macro|SIGNAL
 DECL|TIMEOUT|macro|TIMEOUT
 DECL|Thread_1|function|void Thread_1(void const *arg)
 DECL|Thread_2|function|void Thread_2(void const *arg)
+DECL|offload_function|function|static void offload_function(void *param)
+DECL|test_multiple_signal_flags|function|void test_multiple_signal_flags(void const *thread_id)
+DECL|test_signal_events_isr|function|void test_signal_events_isr(void)
 DECL|test_signal_events_no_wait|function|void test_signal_events_no_wait(void)
 DECL|test_signal_events_signalled|function|void test_signal_events_signalled(void)
 DECL|test_signal_events_timeout|function|void test_signal_events_timeout(void)
+DECL|test_signal_from_isr|function|void test_signal_from_isr(void const *thread_id)
