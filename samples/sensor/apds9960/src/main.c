3d6bc085e52cdb6103724138a65fa4b755661491
Johann Fischer <j.fischer@phytec.de>
2018-09-10 16:48:52 +0200

    samples: apds9960: rework APDS9960 sample
    Rework APDS9960 sample.
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: cc56de700577e45d8f2f4079e11cbddfd69cee3a

--- samples/sensor/apds9960/src/main.c
+++ samples/sensor/apds9960/src/main.c
@@ -1,2 +1,2 @@
-DECL|do_main|function|static void do_main(struct device *dev)
 DECL|main|function|void main(void)
+DECL|trigger_handler|function|static void trigger_handler(struct device *dev, struct sensor_trigger *trigger)
