d816d6645234368cb34676a5a21e74af8395a3dd
Rajavardhan Gundi <rajavardhan.gundi@intel.com>
2018-09-10 14:32:38 +0530

    tests/cmsis_rtos_v1: Introduce negative tests for mailQ
    Introduces some negative test cases to check all possible values
    returned by the APIs.
    
    Signed-off-by: Rajavardhan Gundi <rajavardhan.gundi@intel.com>
    
    Former-commit-id: 8f3c3384970cce293eea56b7c8a931c46761031e

--- tests/cmsis_rtos_v1/src/mailq.c
+++ tests/cmsis_rtos_v1/src/mailq.c
@@ -4,6 +4,8 @@ DECL|MAIL1_DATA3|macro|MAIL1_DATA3
 DECL|MAIL2_DATA1|macro|MAIL2_DATA1
 DECL|MAIL2_DATA2|macro|MAIL2_DATA2
 DECL|MAIL2_DATA3|macro|MAIL2_DATA3
+DECL|Q_LEN|macro|Q_LEN
+DECL|TIMEOUT|macro|TIMEOUT
 DECL|data1|member|int data1;
 DECL|data2|member|unsigned char data2;
 DECL|data3|member|unsigned int data3;
