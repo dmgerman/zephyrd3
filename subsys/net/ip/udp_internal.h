d8b107a2780680e77659c333f274768c74d11d8e
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-09-12 12:59:26 +0200

    net/udp: Removing useless init function
    There is nothing to initialize, so it can be removed.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: a7ddb1fb825e144cb0509a7b4302c3d23115a5f2

--- subsys/net/ip/udp_internal.h
+++ subsys/net/ip/udp_internal.h
@@ -2,7 +2,6 @@ DECL|__UDP_INTERNAL_H|macro|__UDP_INTERNAL_H
 DECL|net_udp_append_raw|macro|net_udp_append_raw
 DECL|net_udp_append|macro|net_udp_append
 DECL|net_udp_get_chksum|macro|net_udp_get_chksum
-DECL|net_udp_init|macro|net_udp_init
 DECL|net_udp_insert_raw|macro|net_udp_insert_raw
 DECL|net_udp_insert|macro|net_udp_insert
 DECL|net_udp_set_chksum|macro|net_udp_set_chksum
