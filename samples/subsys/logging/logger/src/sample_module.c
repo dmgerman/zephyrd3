e35a3feb34cf1eefed9f35f656d4444a0efe7092
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-09-12 09:24:10 +0200

    samples: subsys: logging: example of kconfig.log_template usage
    Logger sample extended to use template for configuration of
    sample module.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    
    Former-commit-id: 4add83c5e292cb3734549f3995c2953f931d7690

--- samples/subsys/logging/logger/src/sample_module.c
+++ samples/subsys/logging/logger/src/sample_module.c
@@ -1,3 +1,4 @@
+DECL|LOG_LEVEL|macro|LOG_LEVEL
 DECL|LOG_MODULE_NAME|macro|LOG_MODULE_NAME
 DECL|LOG_MODULE_NAME|variable|LOG_MODULE_NAME
 DECL|sample_module_func|function|void sample_module_func(void)
