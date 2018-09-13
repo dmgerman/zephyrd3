90aef233b639a1aa2a879200c4ba369056fd69b2
Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
2018-09-13 09:40:43 +0200

    net/udp: Separating net_context from udp and other cleanup
    - Up to net_context to give the source port.
    - net_udp_append is unused anywhere: let's remove it.
    - left over macros on _raw versions removed as well.
    
    Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>
    
    Former-commit-id: e8dee89079e086d7e5a8581f9a436674f936db24

--- subsys/net/ip/udp.c
+++ subsys/net/ip/udp.c
@@ -1,10 +1,9 @@
 DECL|NET_LOG_ENABLED|macro|NET_LOG_ENABLED
 DECL|PKT_WAIT_TIME|macro|PKT_WAIT_TIME
 DECL|SYS_LOG_DOMAIN|macro|SYS_LOG_DOMAIN
-DECL|net_udp_append|function|struct net_pkt *net_udp_append(struct net_context *context, struct net_pkt *pkt, u16_t dst_port)
 DECL|net_udp_get_chksum|function|u16_t net_udp_get_chksum(struct net_pkt *pkt, struct net_buf *frag)
 DECL|net_udp_get_hdr|function|struct net_udp_hdr *net_udp_get_hdr(struct net_pkt *pkt, struct net_udp_hdr *hdr)
-DECL|net_udp_insert|function|struct net_pkt *net_udp_insert(struct net_context *context, struct net_pkt *pkt, u16_t offset, u16_t dst_port)
+DECL|net_udp_insert|function|struct net_pkt *net_udp_insert(struct net_pkt *pkt, u16_t offset, u16_t src_port, u16_t dst_port)
 DECL|net_udp_register|function|int net_udp_register(const struct sockaddr *remote_addr, const struct sockaddr *local_addr, u16_t remote_port, u16_t local_port, net_conn_cb_t cb,
 DECL|net_udp_set_chksum|function|struct net_buf *net_udp_set_chksum(struct net_pkt *pkt, struct net_buf *frag)
 DECL|net_udp_set_hdr|function|struct net_udp_hdr *net_udp_set_hdr(struct net_pkt *pkt, struct net_udp_hdr *hdr)
--- subsys/net/ip/udp_internal.h
+++ subsys/net/ip/udp_internal.h
@@ -1,7 +1,4 @@
 DECL|__UDP_INTERNAL_H|macro|__UDP_INTERNAL_H
-DECL|net_udp_append_raw|macro|net_udp_append_raw
-DECL|net_udp_append|macro|net_udp_append
 DECL|net_udp_get_chksum|macro|net_udp_get_chksum
-DECL|net_udp_insert_raw|macro|net_udp_insert_raw
 DECL|net_udp_insert|macro|net_udp_insert
 DECL|net_udp_set_chksum|macro|net_udp_set_chksum
