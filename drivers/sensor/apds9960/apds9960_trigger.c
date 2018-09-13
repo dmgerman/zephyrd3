fc18ddd576fe9dddf7a79e7697be985beab76a5d
Johann Fischer <j.fischer@phytec.de>
2018-09-13 14:08:41 +0200

    drivers: apds9960: add trigger mode for proximity
    Add trigger mode for proximity
    
    Signed-off-by: Johann Fischer <j.fischer@phytec.de>
    
    Former-commit-id: b192b2b575f573cff787e8f768acb9c5183b18d5

---
+++ drivers/sensor/apds9960/apds9960_trigger.c
@@ -0,0 +1,3 @@
+DECL|apds9960_attr_set|function|int apds9960_attr_set(struct device *dev, enum sensor_channel chan, enum sensor_attribute attr, const struct sensor_value *val)
+DECL|apds9960_trigger_set|function|int apds9960_trigger_set(struct device *dev,const struct sensor_trigger *trig, sensor_trigger_handler_t handler)
+DECL|apds9960_work_cb|function|void apds9960_work_cb(struct k_work *work)
