46109a50ddc0a741d9990777b6c8ec0abed06ba2
Alberto Escolar Piedras <alpi@oticon.com>
2018-09-14 09:16:52 +0200

    subsys: logging: for native_posix use appropriate tracing
    For the native_posix backend, use the appropiate tracing
    functions and enable color if it should be
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: 2f6370c0dd4ffdc1212a3aaa64f5646d477dd6c8

--- subsys/logging/log_backend_native_posix.c
+++ subsys/logging/log_backend_native_posix.c
@@ -1,5 +1,9 @@
+DECL|_STDOUT_BUF_SIZE|macro|_STDOUT_BUF_SIZE
 DECL|buf|variable|buf
 DECL|char_out|function|int char_out(u8_t *data, size_t length, void *ctx)
 DECL|log_backend_native_posix_api|variable|log_backend_native_posix_api
+DECL|n_pend|variable|n_pend
 DECL|panic|function|static void panic(struct log_backend const *const backend)
+DECL|preprint_char|function|static void preprint_char(int c)
 DECL|put|function|static void put(const struct log_backend *const backend,struct log_msg *msg)
+DECL|stdout_buff|variable|stdout_buff
