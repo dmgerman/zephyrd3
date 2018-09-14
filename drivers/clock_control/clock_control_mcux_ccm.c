96622f80c3b521fe91105e4d50bdb22306ea1414
Maureen Helm <maureen.helm@nxp.com>
2018-09-14 15:56:13 -0500

    clock_control: Add support for lpspi clock in mcux ccm driver
    Adds support for getting the lpspi clock frequency in the mcux ccm
    driver.
    
    Signed-off-by: Maureen Helm <maureen.helm@nxp.com>
    
    Former-commit-id: 83a678150713bffbb4e6355376eaa046ffe01dee

--- drivers/clock_control/clock_control_mcux_ccm.c
+++ drivers/clock_control/clock_control_mcux_ccm.c
@@ -1,4 +1,5 @@
 DECL|SYS_LOG_LEVEL|macro|SYS_LOG_LEVEL
+DECL|lpspi_clocks|variable|lpspi_clocks
 DECL|mcux_ccm_driver_api|variable|mcux_ccm_driver_api
 DECL|mcux_ccm_get_subsys_rate|function|static int mcux_ccm_get_subsys_rate(struct device *dev, clock_control_subsys_t sub_system, u32_t *rate)
 DECL|mcux_ccm_init|function|static int mcux_ccm_init(struct device *dev)
