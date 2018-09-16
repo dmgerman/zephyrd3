340e10ab8d6aec064ef9c1e0f77819d07b495592
Christian Taedcke <hacking@taedcke.com>
2018-09-16 12:01:04 +0200

    arm: exx32: Use one common soc.c for all exx32 socs
    The differentiation between the MCUs is handled in the HAL from Silabs.
    So all EXX32 MCUs should use the same soc.c file.
    
    Signed-off-by: Christian Taedcke <hacking@taedcke.com>
    
    Former-commit-id: 2eb347b174046037139c024e1ab2f63caf549315

--- soc/arm/silabs_exx32/common/soc.c
+++ soc/arm/silabs_exx32/common/soc.c
@@ -1,4 +1,4 @@
 DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
 DECL|hfxoInit|variable|hfxoInit
 DECL|lfxoInit|variable|lfxoInit
-DECL|silabs_efr32fg1p_init|function|static int silabs_efr32fg1p_init(struct device *arg)
+DECL|silabs_exx32_init|function|static int silabs_exx32_init(struct device *arg)
--- soc/arm/silabs_exx32/efm32wg/soc.c
+++
@@ -1,4 +0,0 @@
-DECL|clkInit|function|static ALWAYS_INLINE void clkInit(void)
-DECL|hfxoInit|variable|hfxoInit
-DECL|lfxoInit|variable|lfxoInit
-DECL|silabs_efm32wg_init|function|static int silabs_efm32wg_init(struct device *arg)
