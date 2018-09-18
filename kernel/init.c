6907dc84fefe223737deea962285e98b33f5b28a
Anas Nashif <anas.nashif@intel.com>
2018-09-17 23:02:34 -0500

    kernel: remove deprecate k_call_stacks_analyze
    This API was deperecated and is not being used in the tree anymore, so
    remove it.
    
    Signed-off-by: Anas Nashif <anas.nashif@intel.com>
    
    Former-commit-id: 0a73ea04fa6b266e57eb28ef9002d6e46dafba24

--- kernel/init.c
+++ kernel/init.c
@@ -29,8 +29,6 @@ DECL|boot_delay|variable|boot_delay
 DECL|init_idle_thread|function|static void init_idle_thread(struct k_thread *thr, k_thread_stack_t *stack)
 DECL|initialize_timeouts|macro|initialize_timeouts
 DECL|initialize_timeouts|macro|initialize_timeouts
-DECL|k_call_stacks_analyze|function|void k_call_stacks_analyze(void)
-DECL|k_call_stacks_analyze|function|void k_call_stacks_analyze(void) { }
 DECL|main|function|void __weak main(void)
 DECL|prepare_multithreading|function|static void prepare_multithreading(struct k_thread *dummy_thread)
 DECL|switch_to_main_thread|function|static void switch_to_main_thread(void)
