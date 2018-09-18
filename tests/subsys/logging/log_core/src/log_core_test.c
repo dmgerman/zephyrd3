ca2fb6e516e4505b8d0407eb488d8092986d9144
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-09-18 07:30:25 +0200

    tests: subsys; logging: log_core test to cover LOG_MODULE_DECLARE()
    Extending log_core test suite to test LOG_MODULE_DECLARE() macro.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    
    Former-commit-id: cd850647d5c49fc342552e8b4d79421d15505b0d

--- tests/subsys/logging/log_core/src/log_core_test.c
+++ tests/subsys/logging/log_core/src/log_core_test.c
@@ -4,9 +4,11 @@ DECL|backend1_cb|variable|backend1_cb
 DECL|backend2_cb|variable|backend2_cb
 DECL|backend_cb|struct|struct backend_cb {
 DECL|check_args|member|bool check_args;
+DECL|check_id|member|bool check_id;
 DECL|check_timestamp|member|bool check_timestamp;
 DECL|counter|member|size_t counter;
 DECL|data|variable|data
+DECL|exp_id|member|u32_t exp_id[100];
 DECL|exp_nargs|member|u32_t exp_nargs[100];
 DECL|exp_timestamps|member|u32_t exp_timestamps[100];
 DECL|keep_msgs|member|bool keep_msgs;
@@ -19,6 +21,7 @@ DECL|put|function|static void put(struct log_backend const *const backend,struct
 DECL|stamp|variable|stamp
 DECL|test_log_arguments|function|static void test_log_arguments(void)
 DECL|test_log_backend_runtime_filtering|function|static void test_log_backend_runtime_filtering(void)
+DECL|test_log_from_declared_module|function|static void test_log_from_declared_module(void)
 DECL|test_log_overflow|function|static void test_log_overflow(void)
 DECL|test_log_panic|function|static void test_log_panic(void)
 DECL|test_main|function|void test_main(void)
---
+++ tests/subsys/logging/log_core/src/test_module.c
@@ -0,0 +1,2 @@
+DECL|test_func|function|void test_func(void)
+DECL|test|variable|test
