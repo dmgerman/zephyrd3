d37a36e9fb2d651ec11ce9e5b04f2401f113dd34
Daniel Leung <daniel.leung@intel.com>
2018-09-19 10:15:12 -0700

    boards/x86: up_squared: add sample app for GPIO
    This adds a sample app to utilize GPIOs on the UP Squared
    board.
    
    Origin: Original
    
    Signed-off-by: Daniel Leung <daniel.leung@intel.com>
    
    Former-commit-id: 79e8f79498ef394f187bb0edabc034caca6292f2

---
+++ samples/boards/up_squared/gpio_counter/src/main.c
@@ -0,0 +1,12 @@
+DECL|INTR_PIN_FLAGS|macro|INTR_PIN_FLAGS
+DECL|MASK|macro|MASK
+DECL|NUM_PINS|macro|NUM_PINS
+DECL|_pin|struct|struct _pin {
+DECL|button_cb|function|void button_cb(struct device *gpiodev, struct gpio_callback *cb, u32_t pin)
+DECL|counter_pins|variable|counter_pins
+DECL|counter|variable|counter
+DECL|gpio_cb|variable|gpio_cb
+DECL|hat_num|member|u32_t hat_num;
+DECL|intr_pin|variable|intr_pin
+DECL|main|function|void main(void)
+DECL|pin|member|u32_t pin;
