7439cb526f31b5ae94fdc35babe3b5e89cadd684
Szymon Janc <szymon.janc@codecoup.pl>
2018-09-20 20:13:23 +0200

    Bluetooth: Controller: Add support for setting public address
    This allows to provide public address for controller without using
    VS HCI command from host. Useful for controller only builds or
    combined builds that are not using VS HCI commands.
    
    Signed-off-by: Szymon Janc <szymon.janc@codecoup.pl>
    
    Former-commit-id: 6c89c4b5f70953bb29f883ceffd5c3fecdafd6c1

--- subsys/bluetooth/controller/ll_sw/ll_addr.c
+++ subsys/bluetooth/controller/ll_sw/ll_addr.c
@@ -1,3 +1,4 @@
+DECL|bt_ctlr_set_public_addr|function|void bt_ctlr_set_public_addr(const u8_t *addr)
 DECL|ll_addr_get|function|u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
 DECL|ll_addr_set|function|u32_t ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
 DECL|pub_addr|variable|pub_addr
