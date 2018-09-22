0ee96c80055a3419df8fedd0ae2b2ccbd029bf27
Alberto Escolar Piedras <alpi@oticon.com>
2018-09-22 19:19:59 +0200

    uart: Add driver for posix arch
    Added a new UART driver for posix arch boards.
    The driver can be configured to either attach to a new
    pseudo-terminal, or to connect to the invoking shell
    stdin-out.
    
    Signed-off-by: Alberto Escolar Piedras <alpi@oticon.com>
    
    Former-commit-id: 013f27911192e6bb9c7c4619965b82badbe68f03

---
+++ drivers/serial/uart_native_posix.c
@@ -0,0 +1,18 @@
+DECL|ERROR|macro|ERROR
+DECL|WARN|macro|WARN
+DECL|attach_to_tty|function|static void attach_to_tty(const char *slave_tty)
+DECL|auto_attach_cmd|variable|auto_attach_cmd
+DECL|auto_attach|variable|auto_attach
+DECL|default_cmd|variable|default_cmd
+DECL|in_fd|member|int in_fd; /* File descriptor used for input */
+DECL|native_uart_status_0|variable|native_uart_status_0
+DECL|native_uart_status|struct|struct native_uart_status {
+DECL|np_add_uart_options|function|static void np_add_uart_options(void)
+DECL|np_cleanup_uart|function|static void np_cleanup_uart(void)
+DECL|np_uart_driver_api|variable|np_uart_driver_api
+DECL|np_uart_init|function|static int np_uart_init(struct device *dev)
+DECL|np_uart_poll_out|function|static unsigned char np_uart_poll_out(struct device *dev, unsigned char out_char)
+DECL|np_uart_stdin_poll_in|function|static int np_uart_stdin_poll_in(struct device *dev, unsigned char *p_char)
+DECL|np_uart_tty_poll_in|function|static int np_uart_tty_poll_in(struct device *dev, unsigned char *p_char)
+DECL|open_tty|function|static int open_tty(void)
+DECL|out_fd|member|int out_fd; /* File descriptor used for output */
