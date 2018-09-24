7e35f6a4663e9f6de8f8ea8852180ef04b9ddf5d
Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
2018-09-24 12:55:18 +0200

    tests: logging: add log_strdup test to log_core test
    Extended log_core test suite to test string duplication.
    
    Signed-off-by: Krzysztof Chruscinski <krzysztof.chruscinski@nordicsemi.no>
    
    Former-commit-id: d0776bcc0228e1e6fcdaaeb5f65d391b18374122

--- tests/subsys/logging/log_core/src/log_core_test.c
+++ tests/subsys/logging/log_core/src/log_core_test.c
@@ -3,13 +3,17 @@ DECL|LOG_MODULE_NAME|variable|LOG_MODULE_NAME
 DECL|backend1_cb|variable|backend1_cb
 DECL|backend2_cb|variable|backend2_cb
 DECL|backend_cb|struct|struct backend_cb {
+DECL|callback|member|custom_put_callback_t callback;
 DECL|check_args|member|bool check_args;
 DECL|check_id|member|bool check_id;
+DECL|check_strdup|member|bool check_strdup;
 DECL|check_timestamp|member|bool check_timestamp;
 DECL|counter|member|size_t counter;
+DECL|custom_put_callback_t|typedef|typedef void (*custom_put_callback_t)(struct log_backend const *const backend,
 DECL|data|variable|data
 DECL|exp_id|member|u32_t exp_id[100];
 DECL|exp_nargs|member|u32_t exp_nargs[100];
+DECL|exp_strdup|member|bool exp_strdup[100];
 DECL|exp_timestamps|member|u32_t exp_timestamps[100];
 DECL|keep_msgs|member|bool keep_msgs;
 DECL|log_backend_test_api|variable|log_backend_test_api
@@ -19,11 +23,14 @@ DECL|panic|function|static void panic(struct log_backend const *const backend)
 DECL|panic|member|bool panic;
 DECL|put|function|static void put(struct log_backend const *const backend,struct log_msg *msg)
 DECL|stamp|variable|stamp
+DECL|strdup_trim_callback|function|static void strdup_trim_callback(struct log_backend const *const backend, struct log_msg *msg, size_t counter)
 DECL|test_log_arguments|function|static void test_log_arguments(void)
 DECL|test_log_backend_runtime_filtering|function|static void test_log_backend_runtime_filtering(void)
 DECL|test_log_from_declared_module|function|static void test_log_from_declared_module(void)
 DECL|test_log_overflow|function|static void test_log_overflow(void)
 DECL|test_log_panic|function|static void test_log_panic(void)
+DECL|test_log_strdup_gc|function|static void test_log_strdup_gc(void)
 DECL|test_main|function|void test_main(void)
 DECL|test_source_id|variable|test_source_id
+DECL|test_strdup_trimming|function|static void test_strdup_trimming(void)
 DECL|timestamp_get|function|static u32_t timestamp_get(void)
