01a675164ba7462f5ee4c6f67c85df010f6a302b
Flavio Ceolin <flavio.ceolin@intel.com>
2018-09-25 14:54:25 -0700

    kernel: Using the same paramenters names in a specific function
    MISRA-C requires that all declarations of a specific function, or
    object, use the same names and type qualifiers.
    
    MISRA-C rule 8.3
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>
    
    Former-commit-id: 78f27a81f5bee3efea7257bb50aaea88b16bd329

--- arch/posix/core/cpuhalt.c
+++ arch/posix/core/cpuhalt.c
@@ -1,2 +1,2 @@
-DECL|k_cpu_atomic_idle|function|void k_cpu_atomic_idle(unsigned int imask)
+DECL|k_cpu_atomic_idle|function|void k_cpu_atomic_idle(unsigned int key)
 DECL|k_cpu_idle|function|void k_cpu_idle(void)
