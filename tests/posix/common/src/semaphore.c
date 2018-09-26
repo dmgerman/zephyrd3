0e8c2fa1fbc66f3055d9e5560964bb400d606bac
Spoorthi K <spoorthi.k@intel.com>
2018-09-26 16:24:48 +0530

    tests: POSIX: Enhance semaphore tests
    Enhance semaphore test to validate sem_timedwait()
        and sem_trywait(). This is intended to improve
        code coverage.
    
    Also modify ztest check with zassert_equal with
    return value instead of zassert_false for better
    understandability.
    
    Signed-off-by: Spoorthi K <spoorthi.k@intel.com>
    
    Former-commit-id: dc8bbce35735e76f8ba1077dfd58dfae72be6966

--- tests/posix/common/src/semaphore.c
+++ tests/posix/common/src/semaphore.c
@@ -1,4 +1,8 @@
 DECL|STACK_SIZE|macro|STACK_SIZE
 DECL|child_func|function|static void *child_func(void *p1)
+DECL|dummy_sem|variable|dummy_sem
+DECL|initialize_thread_attr|function|void initialize_thread_attr(pthread_attr_t *attr)
+DECL|schedparam|variable|schedparam
+DECL|schedpolicy|variable|schedpolicy
 DECL|sema|variable|sema
 DECL|test_posix_semaphore|function|void test_posix_semaphore(void)
