60f495bead7dc5b904b77d925ef7881294837fb5
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2018-09-26 13:57:54 +0300

    Bluetooth: shell: Split L2CAP commands
    This splits L2CAP commands under 'bt' to 'l2cap' removing 'l2cap-'
    prefix from the commands:
    
    l2cap - Bluetooth L2CAP shell commands
    Options:
      -h, --help  :Show command help.
    Subcommands:
      connect     :<psm>
      disconnect  :[none]
      metrics     :<value on, off>
      recv        :[delay (in miliseconds)
      register    :<psm> [sec_level]
      send        :<number of packets>
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: fb4785d27477fc933cd503b3e715a30b3cc4a3a2

--- subsys/bluetooth/shell/bt.c
+++ subsys/bluetooth/shell/bt.c
@@ -1,12 +1,7 @@
-DECL|CREDITS|macro|CREDITS
-DECL|DATA_MTU|macro|DATA_MTU
 DECL|DEVICE_NAME_LEN|macro|DEVICE_NAME_LEN
 DECL|DEVICE_NAME|macro|DEVICE_NAME
 DECL|HELP_ADDR_LE|macro|HELP_ADDR_LE
 DECL|HELP_NONE|macro|HELP_NONE
-DECL|L2CAP_CHAN|macro|L2CAP_CHAN
-DECL|L2CH_CHAN|macro|L2CH_CHAN
-DECL|L2CH_WORK|macro|L2CH_WORK
 DECL|NAME_LEN|macro|NAME_LEN
 DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(bt_cmds) {
 DECL|ad_discov|variable|ad_discov
@@ -25,7 +20,6 @@ DECL|auth_passkey_entry|function|static void auth_passkey_entry(struct bt_conn *
 DECL|auth_pincode_entry|function|static void auth_pincode_entry(struct bt_conn *conn, bool highsec)
 DECL|bt_ready|function|static void bt_ready(int err)
 DECL|char2hex|function|static int char2hex(const char *c, u8_t *x)
-DECL|ch|member|struct bt_l2cap_le_chan ch;
 DECL|cmd_active_scan_on|function|static void cmd_active_scan_on(const struct shell *shell, int dups)
 DECL|cmd_advertise|function|static void cmd_advertise(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_auth_cancel|function|static void cmd_auth_cancel(const struct shell *shell, size_t argc, char *argv[])
@@ -50,12 +44,6 @@ DECL|cmd_id_reset|function|static void cmd_id_reset(const struct shell *shell, s
 DECL|cmd_id_select|function|static void cmd_id_select(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_id_show|function|static void cmd_id_show(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_init|function|static void cmd_init(const struct shell *shell, size_t argc, char *argv[])
-DECL|cmd_l2cap_connect|function|static void cmd_l2cap_connect(const struct shell *shell, size_t argc, char *argv[])
-DECL|cmd_l2cap_disconnect|function|static void cmd_l2cap_disconnect(const struct shell *shell, size_t argc, char *argv[])
-DECL|cmd_l2cap_metrics|function|static void cmd_l2cap_metrics(const struct shell *shell, size_t argc, char *argv[])
-DECL|cmd_l2cap_recv|function|static void cmd_l2cap_recv(const struct shell *shell, size_t argc, char *argv[])
-DECL|cmd_l2cap_register|function|static void cmd_l2cap_register(const struct shell *shell, size_t argc, char *argv[])
-DECL|cmd_l2cap_send|function|static void cmd_l2cap_send(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_name|function|static void cmd_name(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_oob|function|static void cmd_oob(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_passive_scan_on|function|static void cmd_passive_scan_on(const struct shell *shell, int dups)
@@ -71,27 +59,13 @@ DECL|data_cb|function|static bool data_cb(struct bt_data *data, void *user_data)
 DECL|default_conn|variable|default_conn
 DECL|device_found|function|static void device_found(const bt_addr_le_t *addr, s8_t rssi, u8_t evtype, struct net_buf_simple *buf)
 DECL|disconnected|function|static void disconnected(struct bt_conn *conn, u8_t reason)
-DECL|hexdump|function|static void hexdump(const struct shell *shell, const u8_t *data, size_t len)
+DECL|hexdump|function|void hexdump(const struct shell *shell, const u8_t *data, size_t len)
 DECL|hexstr2array|function|static int hexstr2array(const char *str, u8_t *array, u8_t size)
 DECL|identity_resolved|function|static void identity_resolved(struct bt_conn *conn, const bt_addr_le_t *rpa, const bt_addr_le_t *identity)
-DECL|l2cap_accept|function|static int l2cap_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
-DECL|l2cap_alloc_buf|function|static struct net_buf *l2cap_alloc_buf(struct bt_l2cap_chan *chan)
-DECL|l2cap_connected|function|static void l2cap_connected(struct bt_l2cap_chan *chan)
-DECL|l2cap_disconnected|function|static void l2cap_disconnected(struct bt_l2cap_chan *chan)
-DECL|l2cap_ops|variable|l2cap_ops
-DECL|l2cap_rate|variable|l2cap_rate
-DECL|l2cap_recv_cb|function|static void l2cap_recv_cb(struct k_work *work)
-DECL|l2cap_recv_delay|variable|l2cap_recv_delay
-DECL|l2cap_recv_metrics|function|static int l2cap_recv_metrics(struct bt_l2cap_chan *chan, struct net_buf *buf)
-DECL|l2cap_recv|function|static int l2cap_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
-DECL|l2ch_chan|variable|l2ch_chan
-DECL|l2ch|struct|struct l2ch {
 DECL|le_param_req|function|static bool le_param_req(struct bt_conn *conn, struct bt_le_conn_param *param)
 DECL|le_param_updated|function|static void le_param_updated(struct bt_conn *conn, u16_t interval, u16_t latency, u16_t timeout)
 DECL|pairing_conn|variable|pairing_conn
-DECL|recv_work|member|struct k_delayed_work recv_work;
 DECL|security_changed|function|static void security_changed(struct bt_conn *conn, bt_security_t level)
 DECL|selected_id|variable|selected_id
-DECL|server|variable|server
 DECL|str2bt_addr_le|function|static int str2bt_addr_le(const char *str, const char *type, bt_addr_le_t *addr)
 DECL|str2bt_addr|function|int str2bt_addr(const char *str, bt_addr_t *addr)
---
+++ subsys/bluetooth/shell/l2cap.c
@@ -0,0 +1,29 @@
+DECL|CREDITS|macro|CREDITS
+DECL|DATA_MTU|macro|DATA_MTU
+DECL|HELP_NONE|macro|HELP_NONE
+DECL|L2CAP_CHAN|macro|L2CAP_CHAN
+DECL|L2CH_CHAN|macro|L2CH_CHAN
+DECL|L2CH_WORK|macro|L2CH_WORK
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(l2cap_cmds) {
+DECL|ch|member|struct bt_l2cap_le_chan ch;
+DECL|cmd_connect|function|static void cmd_connect(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_disconnect|function|static void cmd_disconnect(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_l2cap|function|static void cmd_l2cap(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_metrics|function|static void cmd_metrics(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_recv|function|static void cmd_recv(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_register|function|static void cmd_register(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_send|function|static void cmd_send(const struct shell *shell, size_t argc, char *argv[])
+DECL|l2cap_accept|function|static int l2cap_accept(struct bt_conn *conn, struct bt_l2cap_chan **chan)
+DECL|l2cap_alloc_buf|function|static struct net_buf *l2cap_alloc_buf(struct bt_l2cap_chan *chan)
+DECL|l2cap_connected|function|static void l2cap_connected(struct bt_l2cap_chan *chan)
+DECL|l2cap_disconnected|function|static void l2cap_disconnected(struct bt_l2cap_chan *chan)
+DECL|l2cap_ops|variable|l2cap_ops
+DECL|l2cap_rate|variable|l2cap_rate
+DECL|l2cap_recv_cb|function|static void l2cap_recv_cb(struct k_work *work)
+DECL|l2cap_recv_delay|variable|l2cap_recv_delay
+DECL|l2cap_recv_metrics|function|static int l2cap_recv_metrics(struct bt_l2cap_chan *chan, struct net_buf *buf)
+DECL|l2cap_recv|function|static int l2cap_recv(struct bt_l2cap_chan *chan, struct net_buf *buf)
+DECL|l2ch_chan|variable|l2ch_chan
+DECL|l2ch|struct|struct l2ch {
+DECL|recv_work|member|struct k_delayed_work recv_work;
+DECL|server|variable|server
