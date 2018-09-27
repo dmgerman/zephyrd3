af084d4d8993a17bb03de3ae85f51d74166f9e8a
Paul Sokolovsky <paul.sokolovsky@linaro.org>
2018-09-27 17:54:24 +0300

    include: posix: Split dirent.h from unistd.h
    From POSIX
    http://pubs.opengroup.org/onlinepubs/9699919799/basedefs/dirent.h.html
    
    """
    The <dirent.h> header shall define the following type:
    DIR
    
    ...
    
    also define the structure dirent
    """
    
    etc.
    
    Signed-off-by: Paul Sokolovsky <paul.sokolovsky@linaro.org>
    
    Former-commit-id: c152ebd6344c6e3e07ea4aa9c6f9d7562683fd46

---
+++ include/posix/dirent.h
@@ -0,0 +1,5 @@
+DECL|DIR|typedef|typedef struct fs_dir_t DIR;
+DECL|ZEPHYR_INCLUDE_POSIX_DIRENT_H_|macro|ZEPHYR_INCLUDE_POSIX_DIRENT_H_
+DECL|d_ino|member|unsigned int d_ino;
+DECL|d_name|member|char d_name[PATH_MAX + 1];
+DECL|dirent|struct|struct dirent {
--- include/posix/unistd.h
+++ include/posix/unistd.h
@@ -1,6 +1,2 @@
-DECL|DIR|typedef|typedef struct fs_dir_t DIR;
 DECL|ZEPHYR_INCLUDE_POSIX_UNISTD_H_|macro|ZEPHYR_INCLUDE_POSIX_UNISTD_H_
-DECL|d_ino|member|unsigned int d_ino;
-DECL|d_name|member|char d_name[PATH_MAX + 1];
-DECL|dirent|struct|struct dirent {
 DECL|mode_t|typedef|typedef unsigned int mode_t;
