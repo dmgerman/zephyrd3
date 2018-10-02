a257d1632ed68afa171a8ea426257007646137cd
Carles Cufi <carles.cufi@nordicsemi.no>
2018-10-02 13:47:16 +0200

    Bluetooth: shell: Transition ticker.c to the new shell
    Convert the ticker.c functions to the new shell format.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 63f4d9f509c5b865b7429f7e0ea3b56aa7e9f3db

--- subsys/bluetooth/shell/ticker.c
+++ subsys/bluetooth/shell/ticker.c
@@ -1,6 +1,7 @@
+DECL|HELP_NONE|macro|HELP_NONE
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(ticker_cmds) {
 DECL|TICKERS_MAX|macro|TICKERS_MAX
 DECL|TICKERS_MAX|macro|TICKERS_MAX
-DECL|TICKER_SHELL_MODULE|macro|TICKER_SHELL_MODULE
-DECL|cmd_ticker_info|function|int cmd_ticker_info(int argc, char *argv[])
-DECL|ticker_commands|variable|ticker_commands
+DECL|cmd_ticker_info|function|int cmd_ticker_info(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_ticker|function|static int cmd_ticker(const struct shell *shell, size_t argc, char **argv)
 DECL|ticker_op_done|function|static void ticker_op_done(u32_t err, void *context)
