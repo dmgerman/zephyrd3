bcc9d3460a3479b07abe4f589a785bc6b3964816
Carles Cufi <carles.cufi@nordicsemi.no>
2018-10-02 13:47:30 +0200

    Bluetooth: shell: Transition flash.c to the new shell
    Convert the flash.c functions to the new shell format.
    
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>
    
    Former-commit-id: 990ab2de703b05ac4e9af846b6beec227a72a31a

--- subsys/bluetooth/shell/flash.c
+++ subsys/bluetooth/shell/flash.c
@@ -1,9 +1,11 @@
 DECL|BUF_ARRAY_CNT|macro|BUF_ARRAY_CNT
 DECL|FLASH_SHELL_MODULE|macro|FLASH_SHELL_MODULE
+DECL|HELP_NONE|macro|HELP_NONE
+DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(flash_cmds) {
 DECL|TEST_ARR_SIZE|macro|TEST_ARR_SIZE
-DECL|cmd_erase|function|static int cmd_erase(int argc, char *argv[])
-DECL|cmd_flash|function|static int cmd_flash(int argc, char *argv[])
-DECL|cmd_read|function|static int cmd_read(int argc, char *argv[])
-DECL|cmd_test|function|static int cmd_test(int argc, char *argv[])
-DECL|flash_commands|variable|flash_commands
+DECL|cmd_erase|function|static int cmd_erase(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_flash|function|static int cmd_flash(const struct shell *shell, size_t argc, char **argv)
+DECL|cmd_read|function|static int cmd_read(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_test|function|static int cmd_test(const struct shell *shell, size_t argc, char *argv[])
+DECL|cmd_write|function|static int cmd_write(const struct shell *shell, size_t argc, char *argv[])
 DECL|test_arr|variable|test_arr
