122814f38274cb511bd7bec70226d7c962bc711c
Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
2018-10-02 15:27:10 +0300

    Bluetooth: samples: shell: Don't use printk
    Make use of shell_fprintf whenever possible.
    
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    
    Former-commit-id: 4058f724bc6384bd64210cc846cdf2137614c488

--- tests/bluetooth/shell/src/main.c
+++ tests/bluetooth/shell/src/main.c
@@ -4,6 +4,8 @@ DECL|HELP_NONE|macro|HELP_NONE
 DECL|SHELL_CREATE_STATIC_SUBCMD_SET|function|SHELL_CREATE_STATIC_SUBCMD_SET(hrs_cmds) {
 DECL|cmd_hrs_simulate|function|static int cmd_hrs_simulate(const struct shell *shell, size_t argc, char *argv[])
 DECL|cmd_hrs|function|static int cmd_hrs(const struct shell *shell, size_t argc, char **argv)
+DECL|error|macro|error
 DECL|hrs_simulate|variable|hrs_simulate
 DECL|main|function|void main(void)
+DECL|print|macro|print
 DECL|shell_transport_uart|variable|shell_transport_uart
